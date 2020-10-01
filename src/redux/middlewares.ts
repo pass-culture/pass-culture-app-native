bd2c7a8bebda52ac0663fc091c3b25127e3c3c58
Benjamin Robert Stirrup <benjamin.stirrup@gmail.com>
2020-10-01 10:29:26 +0200

    Remove redux saga
    We decided to use redux-toolkit instead, which comes with redux-thunk included.

--- src/redux/middlewares.ts
+++
@@ -1,5 +0,0 @@
-import createSagaMiddleware from 'redux-saga'; // @redux
-
-export const sagaMiddleware = createSagaMiddleware();
-
-export const middlewares = [sagaMiddleware];
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,9 @@
-import { createStore, applyMiddleware } from 'redux'; // @redux
-import { composeWithDevTools } from 'redux-devtools-extension'; // @redux
+import { createStore } from 'redux'; // @redux
 import { persistStore } from 'redux-persist'; // @redux-persist
-import { middlewares, sagaMiddleware } from './middlewares';
 
 import { rootReducer } from './rootReducer'; // @redux-persist-sensitive
-import { rootSaga } from './sagas'; // @redux
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+export const store = createStore(rootReducer);
 // @ts-ignore persistStore doesn't infer store types,
 // possible solution here: https://github.com/rt2zz/redux-persist/pull/1085
 export const persistor = persistStore(store);
-
-sagaMiddleware.run(rootSaga);
