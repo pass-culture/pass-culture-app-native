41f697010c9b33b8cb6048a939224af38ae3271e
Benjamin Robert Stirrup <benjamin.stirrup@gmail.com>
2020-10-02 10:24:10 +0200

    [4552] Enable import paths starting from base directory

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['macros'],
+  plugins: [
+    'macros',
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        root: ['./src/'],
+        alias: {
+          features: './src/features',
+          libs: './src/libs',
+          locales: './src/locales',
+        },
+      },
+    ],
+  ],
 };
