606c67bd4e297f480a3e3904efe93287e629e9d8
Gireg Maury <gireg.maury@gmail.com>
2020-10-14 17:38:45 +0200

    (PC-4710) prune: remove redondant (and useless) dep check

--- scripts/print_project_version.sh
+++ scripts/print_project_version.sh
@@ -11,12 +11,6 @@ if [ "$#" -ne 1 ]; then
  exit 1
 fi
 
-if ! which jq >/dev/null
-then
-  echo "Please install jq at: https://stedolan.github.io/jq/download/" 
-  exit 1
-fi
-
 VERSION=$(jq -r '.version' "${1}/package.json")
 BUILD=$(jq -r '.build' "${1}/package.json")
 
