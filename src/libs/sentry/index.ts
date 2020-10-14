d7d360c82b661e8150a4b2dd0b85f9dfc5678414
Gireg Maury <gireg.maury@gmail.com>
2020-10-14 16:44:41 +0200

    (PC-4710) feat: use package.json version

--- src/libs/sentry/index.ts
+++ src/libs/sentry/index.ts
@@ -2,12 +2,14 @@ import * as Sentry from '@sentry/react-native'
 
 import { env } from 'libs/environment'
 
+import { version } from '../../../package.json'
+
 // Disable Sentry when executing tests. See Packgage.json script 'test:unit'
 if (process.env.JEST !== 'true') {
   Sentry.init({
     dsn: env.SENTRY_DSN,
     environment: env.ENV,
-    release: env.VERSION,
+    release: version,
   })
 }
 
