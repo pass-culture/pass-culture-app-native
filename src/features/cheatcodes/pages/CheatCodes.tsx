c58e1172a0718c201bf97ac5ffb34a43e26bab23
Gireg Maury <gireg.maury@gmail.com>
2020-10-20 15:19:54 +0200

    (PC-4626) debug: display installationID on cheatcodes page

--- src/features/cheatcodes/pages/CheatCodes.tsx
+++ src/features/cheatcodes/pages/CheatCodes.tsx
@@ -1,6 +1,7 @@
+import { BatchUser } from '@bam.tech/react-native-batch'
 import { StackNavigationProp } from '@react-navigation/stack'
-import React, { FunctionComponent } from 'react'
-import { View } from 'react-native'
+import React, { FunctionComponent, useCallback, useEffect, useState } from 'react'
+import { View, Text, Alert } from 'react-native'
 
 import { RootStackParamList } from 'features/navigation/RootNavigator'
 
@@ -13,9 +14,23 @@ type Props = {
 }
 
 export const CheatCodes: FunctionComponent<Props> = function () {
+  const getBatchInstallationID = useCallback(async (callback: (installationId: string) => void) => {
+    try {
+      const installationId = await BatchUser.getInstallationID()
+      callback(installationId)
+    } catch (e) {
+      Alert.alert('error', e)
+      callback('error')
+    }
+  }, [])
+  const [batchInstallationId, setBatchInstallationId] = useState('none')
+  useEffect(() => {
+    getBatchInstallationID(setBatchInstallationId)
+  }, [])
   return (
     <View>
       <CrashTestButton />
+      <Text>{batchInstallationId}</Text>
     </View>
   )
 }
