ec744fdd950574b87299c7afa49e70f9cc96aa61
blixit <celestinya@gmail.com>
2020-10-21 19:26:03 +0200

    [PC-4792] Integrate React-query

--- src/features/home/pages/Home.tsx
+++ src/features/home/pages/Home.tsx
@@ -1,8 +1,9 @@
 import { t } from '@lingui/macro'
 import { StackNavigationProp } from '@react-navigation/stack'
 import React, { FunctionComponent, useCallback } from 'react'
-import { Button, StyleSheet, View, Text } from 'react-native'
+import { Button, StyleSheet, View, Text, ActivityIndicator } from 'react-native'
 
+import { useCurrentUser } from 'features/auth/api'
 import { RootStackParamList } from 'features/navigation/RootNavigator'
 import { env } from 'libs/environment'
 import { useGeolocation, CoordinatesView } from 'libs/geolocation'
@@ -19,13 +20,18 @@ type Props = {
 const styles = StyleSheet.create({
   container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
   geolocation: { position: 'absolute', alignItems: 'center', bottom: 50 },
+  userInformations: { alignItems: 'center', paddingVertical: 20 },
 })
 
 export const Home: FunctionComponent<Props> = function ({ navigation }) {
   const position = useGeolocation()
+  const { user, isFetching, refetch, isError } = useCurrentUser()
+
   const goToLoginPageWithParams = useCallback((): void => {
     navigation.navigate('Login', { userId: 'I have been Set by params' })
   }, [])
+
+  const refetchUserInformations = useCallback((): void => void refetch(), [])
 
   return (
     <View style={styles.container}>
@@ -34,6 +40,19 @@ export const Home: FunctionComponent<Props> = function ({ navigation }) {
         title={_(t`Aller sur la page de connexion avec params`)}
         onPress={goToLoginPageWithParams}
       />
+      {isFetching && <ActivityIndicator testID="user-activity-indicator" />}
+      {user && !isError && (
+        <View style={styles.userInformations}>
+          <Text>{`FIRSTNAME: ${user.firstName}`}</Text>
+          <Text>{`LASTNAME: ${user.lastName}`}</Text>
+          <Text>{`EMAIL: ${user.email}`}</Text>
+          <Button
+            title={_(t`Rafraîchir les données de l'utilisateur`)}
+            onPress={refetchUserInformations}
+            testID="refreshUserInformationsButton"
+          />
+        </View>
+      )}
       <CoordinatesView position={position} style={styles.geolocation} />
       {env.FEATURE_FLAG_CODE_PUSH_MANUAL && <CodePushButton />}
     </View>
