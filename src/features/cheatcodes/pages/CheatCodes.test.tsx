e536a81ee20200e3e46b3b08e484a924ad75bb5f
Gireg Maury <gireg.maury@gmail.com>
2020-10-21 10:28:07 +0200

    (PC-4626) test: test cheatCodes

--- src/features/cheatcodes/pages/CheatCodes.test.tsx
+++ src/features/cheatcodes/pages/CheatCodes.test.tsx
@@ -1,9 +1,15 @@
-import { render } from '@testing-library/react-native'
+import { render, waitFor } from '@testing-library/react-native'
 import React from 'react'
+
+import { BatchUser } from '../../../../__mocks__/@bam.tech/react-native-batch'
 
 import { CheatCodes } from './CheatCodes'
 
+const installationID = 'installationID'
 describe('CheatCodes component', () => {
+  beforeAll(() => {
+    BatchUser.getInstallationID.mockImplementation(() => Promise.resolve(installationID))
+  })
   const navigation = {
     dispatch: jest.fn(),
   } as any // eslint-disable-line @typescript-eslint/no-explicit-any
@@ -11,5 +17,10 @@ describe('CheatCodes component', () => {
   it('should render correctly', async () => {
     const cheatCodes = render(<CheatCodes navigation={navigation} />)
     expect(cheatCodes).toMatchSnapshot()
+  })
+  it('should call installationID and display it', async () => {
+    const { queryByText } = render(<CheatCodes navigation={navigation} />)
+    expect(BatchUser.getInstallationID).toHaveBeenCalled()
+    await waitFor(() => expect(queryByText(installationID)).toBeTruthy())
   })
 })
