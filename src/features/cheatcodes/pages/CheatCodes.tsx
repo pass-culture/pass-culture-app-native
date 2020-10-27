6d58cce9238c0ddfcc923c3dcafdca97ff9ea456
Gireg Maury <gireg.maury@gmail.com>
2020-10-27 11:13:51 +0100

    (PC-4408) feat: move codepush button to cheatCodes page

--- src/features/cheatcodes/pages/CheatCodes.tsx
+++ src/features/cheatcodes/pages/CheatCodes.tsx
@@ -5,8 +5,10 @@ import React, { FunctionComponent, useCallback, useEffect, useState } from 'reac
 import { View, Text, Alert } from 'react-native'
 
 import { RootStackParamList } from 'features/navigation/RootNavigator'
+import { env } from 'libs/environment'
 import { _ } from 'libs/i18n'
 
+import { CodePushButton } from '../components/CodePushButton'
 import { CrashTestButton } from '../components/CrashTestButton'
 
 type CheatCodesNavigationProp = StackNavigationProp<RootStackParamList, 'CheatCodes'>
@@ -38,6 +40,7 @@ export const CheatCodes: FunctionComponent<Props> = function () {
     <View>
       <CrashTestButton />
       <Text>{batchInstallationId}</Text>
+      {env.FEATURE_FLAG_CODE_PUSH_MANUAL && <CodePushButton />}
     </View>
   )
 }
