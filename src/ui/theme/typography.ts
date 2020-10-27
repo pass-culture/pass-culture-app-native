1b97adafc96cdf4730abdfd6f210d8893a3673dd
Gireg Maury <gireg.maury@gmail.com>
2020-10-27 11:06:08 +0100

    (PC-4408) feat: add wordings

--- src/ui/theme/typography.ts
+++ src/ui/theme/typography.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native'
 
+import { ColorsEnum } from './colors'
 import { getSpacing } from './spacing'
 
 export enum fontWeight {
@@ -9,56 +10,62 @@ export enum fontWeight {
   MEDIUM = 500,
   NORMAL = 'normal',
 }
+interface TextProp {
+  color?: ColorsEnum
+}
+const ColoredText = styled.Text<TextProp>(({ color }) => ({
+  color: color ?? ColorsEnum.BLACK,
+}))
 
-const Hero = styled.Text({
+const Hero = styled(ColoredText)({
   fontFamily: 'Montserrat-Medium',
   fontSize: getSpacing(9.5),
   fontWeight: fontWeight.MEDIUM,
   lineHeight: getSpacing(12),
 })
 
-const Title1 = styled.Text({
+const Title1 = styled(ColoredText)({
   fontFamily: 'Montserrat-ExtraBoldItalic',
   fontSize: getSpacing(7),
   fontWeight: fontWeight.EXTRA_BOLD,
   lineHeight: getSpacing(8.5),
 })
 
-const Title2 = styled.Text({
+const Title2 = styled(ColoredText)({
   fontFamily: 'Montserrat-MediumItalic',
   fontSize: getSpacing(6),
   fontWeight: fontWeight.MEDIUM,
   lineHeight: getSpacing(7),
 })
 
-const Title3 = styled.Text({
+const Title3 = styled(ColoredText)({
   fontFamily: 'Montserrat-Bold',
   fontSize: getSpacing(5),
   fontWeight: fontWeight.BOLD,
   lineHeight: getSpacing(6),
 })
 
-const Title4 = styled.Text({
+const Title4 = styled(ColoredText)({
   fontFamily: 'Montserrat-Medium',
   fontSize: getSpacing(4.5),
   fontWeight: fontWeight.MEDIUM,
   lineHeight: getSpacing(5.5),
 })
 
-const ButtonText = styled.Text({
+const ButtonText = styled(ColoredText)({
   fontFamily: 'Montserrat-Bold',
   fontSize: getSpacing(3.75),
   fontWeight: fontWeight.BOLD,
   lineHeight: getSpacing(5),
 })
 
-const Body = styled.Text({
+const Body = styled(ColoredText)({
   fontFamily: 'Montserrat-Regular',
   fontSize: getSpacing(3.75),
   fontWeight: fontWeight.NORMAL,
   lineHeight: getSpacing(5),
 })
-const Caption = styled.Text({
+const Caption = styled(ColoredText)({
   fontFamily: 'Montserrat-SemiBold',
   fontSize: getSpacing(3),
   fontWeight: fontWeight.SEMI_BOLD,
