85bf10a588339d0e998bd1f2710845d65a79d40e
Gireg Maury <gireg.maury@gmail.com>
2020-10-27 11:56:32 +0100

    (PC-4408) feat: use string lineHeight to avoid warning

--- src/ui/theme/typography.ts
+++ src/ui/theme/typography.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native'
 
 import { ColorsEnum } from './colors'
-import { getSpacing } from './spacing'
+import { getSpacing, getSpacingString } from './spacing'
 
 export enum fontWeight {
   EXTRA_BOLD = 800,
@@ -21,55 +21,55 @@ const Hero = styled(ColoredText)({
   fontFamily: 'Montserrat-Medium',
   fontSize: getSpacing(9.5),
   fontWeight: fontWeight.MEDIUM,
-  lineHeight: getSpacing(12),
+  lineHeight: getSpacingString(12),
 })
 
 const Title1 = styled(ColoredText)({
   fontFamily: 'Montserrat-ExtraBoldItalic',
   fontSize: getSpacing(7),
   fontWeight: fontWeight.EXTRA_BOLD,
-  lineHeight: getSpacing(8.5),
+  lineHeight: getSpacingString(8.5),
 })
 
 const Title2 = styled(ColoredText)({
   fontFamily: 'Montserrat-MediumItalic',
   fontSize: getSpacing(6),
   fontWeight: fontWeight.MEDIUM,
-  lineHeight: getSpacing(7),
+  lineHeight: getSpacingString(7),
 })
 
 const Title3 = styled(ColoredText)({
   fontFamily: 'Montserrat-Bold',
   fontSize: getSpacing(5),
   fontWeight: fontWeight.BOLD,
-  lineHeight: getSpacing(6),
+  lineHeight: getSpacingString(6),
 })
 
 const Title4 = styled(ColoredText)({
   fontFamily: 'Montserrat-Medium',
   fontSize: getSpacing(4.5),
   fontWeight: fontWeight.MEDIUM,
-  lineHeight: getSpacing(5.5),
+  lineHeight: getSpacingString(5.5),
 })
 
 const ButtonText = styled(ColoredText)({
   fontFamily: 'Montserrat-Bold',
   fontSize: getSpacing(3.75),
   fontWeight: fontWeight.BOLD,
-  lineHeight: getSpacing(5),
+  lineHeight: getSpacingString(5),
 })
 
 const Body = styled(ColoredText)({
   fontFamily: 'Montserrat-Regular',
   fontSize: getSpacing(3.75),
   fontWeight: fontWeight.NORMAL,
-  lineHeight: getSpacing(5),
+  lineHeight: getSpacingString(5),
 })
 const Caption = styled(ColoredText)({
   fontFamily: 'Montserrat-SemiBold',
   fontSize: getSpacing(3),
   fontWeight: fontWeight.SEMI_BOLD,
-  lineHeight: getSpacing(4),
+  lineHeight: getSpacingString(4),
 })
 
 export const Typo = {
