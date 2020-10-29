15c0ca4a9a12e31f5d9aecc864480a3bae6349bd
Justine Mignot <justinem@bam.tech>
2020-10-29 15:05:14 +0100

    :label: (PC-4413) add typing on home api call

--- src/features/home/api.ts
+++ src/features/home/api.ts
@@ -2,7 +2,7 @@ import { t } from '@lingui/macro'
 import resolveResponse from 'contentful-resolve-response'
 import { Alert } from 'react-native'
 
-import { EntryCollection, EntryFields } from 'features/home/contentful.d'
+import { EntryCollection, EntryFields, HomepageEntries } from 'features/home/contentful.d'
 import { env } from 'libs/environment'
 import { getExternal } from 'libs/fetch'
 import { _ } from 'libs/i18n'
@@ -10,7 +10,7 @@ import { _ } from 'libs/i18n'
 const CONTENTFUL_BASE_URL = 'https://cdn.contentful.com'
 const DEPTH_LEVEL = 2
 
-export const getHomepageEntries = async () => {
+export const getHomepageEntries = async (): Promise<HomepageEntries | undefined> => {
   try {
     const homepageData: EntryCollection<EntryFields> = await getExternal(
       `${CONTENTFUL_BASE_URL}/spaces/${env.CONTENTFUL_SPACE_ID}/environments/${env.CONTENTFUL_ENVIRONMENT}/entries?include=${DEPTH_LEVEL}&content_type=homepage&access_token=${env.CONTENTFUL_ACCESS_TOKEN}`
@@ -20,10 +20,11 @@ export const getHomepageEntries = async () => {
     Alert.alert(
       _(t`Échec de la récupération du contenu Contentful pour la homepage ${error.message}`)
     )
+    return
   }
 }
 
-const adaptHomepageEntries = (homepageData: EntryCollection<EntryFields>) => {
+const adaptHomepageEntries = (homepageData: EntryCollection<EntryFields>): HomepageEntries => {
   const formattedResponse = resolveResponse(homepageData)
   /* Support good practice is to configure on Contentful dashboard only 1 contenttype homepage
     But there is not blocking on the dashboard, that's why we select first one here */
