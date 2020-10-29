72087f5ed711d2bf49831c73078a328706fe32e0
Justine Mignot <justinem@bam.tech>
2020-10-29 11:25:19 +0100

    :recycle: (PC-4413) add adapter to separe responsibilities

--- src/features/home/api.ts
+++ src/features/home/api.ts
@@ -15,13 +15,17 @@ export const getHomepageEntries = async () => {
     const homepageData: EntryCollection<EntryFields> = await getExternal(
       `${CONTENTFUL_BASE_URL}/spaces/${env.CONTENTFUL_SPACE_ID}/environments/${env.CONTENTFUL_ENVIRONMENT}/entries?include=${DEPTH_LEVEL}&content_type=homepage&access_token=${env.CONTENTFUL_ACCESS_TOKEN}`
     )
-    const formattedResponse = resolveResponse(homepageData)
-    /* Support good practice is to configure on Contentful dashboard only 1 contenttype homepage
-    But there is not blocking on the dashboard, that's why we select first one here */
-    return formattedResponse[0]
+    return adaptHomepageEntries(homepageData)
   } catch (error) {
     Alert.alert(
       _(t`Échec de la récupération du contenu Contentful pour la homepage ${error.message}`)
     )
   }
+}
+
+const adaptHomepageEntries = (homepageData: EntryCollection<EntryFields>) => {
+  const formattedResponse = resolveResponse(homepageData)
+  /* Support good practice is to configure on Contentful dashboard only 1 contenttype homepage
+    But there is not blocking on the dashboard, that's why we select first one here */
+  return formattedResponse[0]
 }
