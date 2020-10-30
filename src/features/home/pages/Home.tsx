a91dbfe9ebd7c5f579dddf52065c1b6c230af991
AntoineG <antoine.garcia@polyconseil.fr>
2020-10-30 14:07:49 +0100

    Make style generic: height/width/borderRadius and place the Home in a ScrollView

--- src/features/home/pages/Home.tsx
+++ src/features/home/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { t } from '@lingui/macro'
 import { StackNavigationProp } from '@react-navigation/stack'
 import React, { FunctionComponent, useCallback } from 'react'
-import { Button, Image, Text, ActivityIndicator } from 'react-native'
+import { ActivityIndicator, Button, ScrollView, Text } from 'react-native'
 import styled from 'styled-components/native'
 
 import { useCurrentUser } from 'features/auth/api'
@@ -31,47 +31,48 @@ export const Home: FunctionComponent<Props> = function ({ navigation }) {
   const refetchUserInformations = useCallback((): void => void refetch(), [])
 
   return (
-    <Container>
-      <HeaderBackgroundWrapper>
-        <HeaderBackground />
-      </HeaderBackgroundWrapper>
-      <Spacer.Column numberOfSpaces={18} />
-      <Typo.Title1 color={ColorsEnum.WHITE}>
-        {_(/*i18n: Welcome title message */ t`Bienvenue !`)}
-      </Typo.Title1>
-      <Spacer.Column numberOfSpaces={2} />
-      <Typo.Body color={ColorsEnum.WHITE}>
-        {_(/*i18n: Welcome body message */ t`Toute la culture dans votre main`)}
-      </Typo.Body>
-      <Spacer.Flex />
-      <ExclusivityModule
-        alt={"Samedi, c'est Nuit Blanche !"}
-        image={
-          'https://images.ctfassets.net/2bg01iqy0isv/4fHAxzZIXhsGwAHvOZKdFK/3744c6947170eeb9205239ec57bba42f/73880_UDEucG5n.png'
-        }
-        offerId={'G7JPK'}
-      />
-      <Spacer.Flex />
-      <Text>{_(t`Bienvenue à Pass Culture`)}</Text>
-      <Button
-        title={_(t`Aller sur la page de connexion avec params`)}
-        onPress={goToLoginPageWithParams}
-      />
-      {isFetching && <ActivityIndicator testID="user-activity-indicator" />}
-      {email && !isError && (
-        <UserInformationContainer>
-          <Text>{`EMAIL: ${email}`}</Text>
-          <Button
-            title={_(t`Rafraîchir les données de l'utilisateur`)}
-            onPress={refetchUserInformations}
-            testID="refreshUserInformationsButton"
-          />
-        </UserInformationContainer>
-      )}
-      <Button title={_(t`Récupérer les données Contentful`)} onPress={getHomepageModules} />
-      <CoordinatesViewContainer position={position} />
-      <Spacer.Flex />
-    </Container>
+    <ScrollView>
+      <Container>
+        <HeaderBackgroundWrapper>
+          <HeaderBackground />
+        </HeaderBackgroundWrapper>
+        <Spacer.Column numberOfSpaces={18} />
+        <Typo.Title1 color={ColorsEnum.WHITE}>
+          {_(/*i18n: Welcome title message */ t`Bienvenue !`)}
+        </Typo.Title1>
+        <Spacer.Column numberOfSpaces={2} />
+        <Typo.Body color={ColorsEnum.WHITE}>
+          {_(/*i18n: Welcome body message */ t`Toute la culture dans votre main`)}
+        </Typo.Body>
+        <Spacer.Column numberOfSpaces={8} />
+        <ExclusivityModule
+          alt="Samedi, c'est Nuit Blanche !"
+          image="https://images.ctfassets.net/2bg01iqy0isv/1ljtAvN1oEGY2DL2mntdo8/e3efe474291f0daee3b30cf7d7a68049/Group_6__5_.png" // minotaure
+          offerId="G7JPK"
+        />
+        <Spacer.Column numberOfSpaces={6} />
+        <Spacer.Flex />
+        <Text>{_(t`Bienvenue à Pass Culture`)}</Text>
+        <Button
+          title={_(t`Aller sur la page de connexion avec params`)}
+          onPress={goToLoginPageWithParams}
+        />
+        {isFetching && <ActivityIndicator testID="user-activity-indicator" />}
+        {email && !isError && (
+          <UserInformationContainer>
+            <Text>{`EMAIL: ${email}`}</Text>
+            <Button
+              title={_(t`Rafraîchir les données de l'utilisateur`)}
+              onPress={refetchUserInformations}
+              testID="refreshUserInformationsButton"
+            />
+          </UserInformationContainer>
+        )}
+        <Button title={_(t`Récupérer les données Contentful`)} onPress={getHomepageModules} />
+        <CoordinatesViewContainer position={position} />
+        <Spacer.Flex />
+      </Container>
+    </ScrollView>
   )
 }
 
