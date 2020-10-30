c13532131d781dd3f319ac7fb6adc12251ac6e44
Justine Mignot <justinem@bam.tech>
2020-10-30 17:29:21 +0100

    :recycle: (PC-4413) make processHomepageEntries function more clear, step 1

--- src/features/home/pages/useHomepageModules.test.ts
+++ src/features/home/pages/useHomepageModules.test.ts
@@ -10,5 +10,27 @@ describe('useHomepageModules', () => {
     it('should format homepage entries so we can use it to display infos on home page', () => {
       expect(processHomepageEntries(adaptedHomepageEntries)).toMatchSnapshot()
     })
+
+    it('should format homepage entries: no fields case', () => {
+      const sys = {
+        space: { sys: { type: 'Link', linkType: 'Space', id: '2bg01iqy0isv' } },
+        id: '16PgpnlCOYYIhUTclR0oO4',
+        type: 'Entry',
+        createdAt: '2020-07-02T13:36:02.919Z',
+        updatedAt: '2020-10-30T10:07:29.549Z',
+        environment: { sys: { id: 'testing', type: 'Link', linkType: 'Environment' } },
+        revision: 154,
+        contentType: { sys: { type: 'Link', linkType: 'ContentType', id: 'homepage' } },
+        locale: 'en-US',
+      }
+
+      const emptyModulesHomepageEntries = {
+        sys,
+        fields: {
+          modules: [],
+        },
+      }
+      expect(processHomepageEntries(emptyModulesHomepageEntries)).toEqual([])
+    })
   })
 })
