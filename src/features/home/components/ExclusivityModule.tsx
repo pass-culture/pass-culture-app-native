24e894512dd1dafa6e8ea87f2d6563622007ac0e
AntoineG <antoine.garcia@polyconseil.fr>
2020-10-30 14:07:49 +0100

    Make style generic: height/width/borderRadius and place the Home in a ScrollView

--- src/features/home/components/ExclusivityModule.tsx
+++ src/features/home/components/ExclusivityModule.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Image, TouchableHighlight } from 'react-native'
+import { Dimensions, PixelRatio } from 'react-native'
+import styled from 'styled-components/native'
 
 type Props = {
   alt: string
@@ -11,15 +12,45 @@ export const ExclusivityModule = (props: Props) => {
   const { alt, image, offerId } = props
 
   return (
-    <TouchableHighlight
-      onPress={() => console.log(`Opening offer ${offerId}...`)}
-      style={{ width: '90%', height: 292, borderRadius: 12 }}>
-      <Image
-        source={{ uri: image }}
-        style={{ width: 375, height: 292, borderRadius: 12 }}
-        accessible={!!alt}
-        accessibilityLabel={alt}
-      />
-    </TouchableHighlight>
+    <Container>
+      {/** Explicitly defining Margin component for easier use for other components, with gutters */}
+      <Margin />
+      <ImageContainer>
+        <TouchableHighlight
+          onPress={() => console.log(`Opening offer ${offerId}...`)} // eslint-disable-line no-console
+        >
+          <ExcluImage source={{ uri: image }} accessible={!!alt} accessibilityLabel={alt} />
+        </TouchableHighlight>
+      </ImageContainer>
+      <Margin />
+    </Container>
   )
 }
+
+// TODO(agarcia): place these constants in a file for style
+const MARGIN_DP = 24
+const BORDER_RADIUS = 8
+
+const imageWidth = Dimensions.get('window').width - 2 * MARGIN_DP
+const imageHeight = PixelRatio.roundToNearestPixel((imageWidth * 292) / 333)
+
+const Container = styled.View({
+  flexDirection: 'row',
+})
+
+const ImageContainer = styled.View({
+  flex: 1,
+})
+
+const Margin = styled.View({
+  width: PixelRatio.roundToNearestPixel(MARGIN_DP),
+})
+
+const ExcluImage = styled.Image({
+  height: imageHeight,
+  borderRadius: BORDER_RADIUS,
+})
+
+const TouchableHighlight = styled.TouchableHighlight({
+  borderRadius: BORDER_RADIUS,
+})
