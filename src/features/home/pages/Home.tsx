1bd2088b4e5e44d6de3c6b4594636e93b0a23172
Benjamin Robert Stirrup <benjamin.stirrup@gmail.com>
2020-11-02 16:22:34 +0100

    No use : remove user information on home page

--- src/features/home/pages/Home.tsx
+++ src/features/home/pages/Home.tsx
@@ -1,10 +1,9 @@
 import { t } from '@lingui/macro'
 import { StackNavigationProp } from '@react-navigation/stack'
 import React, { FunctionComponent, useCallback } from 'react'
-import { ActivityIndicator, Button, ScrollView, Text } from 'react-native'
+import { Button, ScrollView } from 'react-native'
 import styled from 'styled-components/native'
 
-import { useCurrentUser } from 'features/auth/api'
 import { useHomepageModules } from 'features/home/api'
 import { ExclusivityModule } from 'features/home/components/ExclusivityModule'
 import { ExclusivityPane, ProcessedModule } from 'features/home/contentful'
@@ -12,7 +11,7 @@ import { RootStackParamList } from 'features/navigation/RootNavigator'
 import { useGeolocation, CoordinatesView } from 'libs/geolocation'
 import { _ } from 'libs/i18n'
 import { HeaderBackground } from 'ui/svg/HeaderBackground'
-import { ColorsEnum, Spacer, Typo, getSpacing } from 'ui/theme'
+import { ColorsEnum, Spacer, Typo } from 'ui/theme'
 
 type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>
 
@@ -22,14 +21,11 @@ type Props = {
 
 export const Home: FunctionComponent<Props> = function ({ navigation }) {
   const position = useGeolocation()
-  const { email, isFetching, refetch, isError } = useCurrentUser()
   const { data: modules = [] } = useHomepageModules()
 
-  const goToLoginPageWithParams = useCallback((): void => {
-    navigation.navigate('Login', { userId: 'I have been Set by params' })
+  const goToLoginPage = useCallback((): void => {
+    navigation.navigate('Login')
   }, [])
-
-  const refetchUserInformations = useCallback((): void => void refetch(), [])
 
   return (
     <ScrollView>
@@ -53,23 +49,7 @@ export const Home: FunctionComponent<Props> = function ({ navigation }) {
           return <React.Fragment key={index} />
         })}
         <Spacer.Column numberOfSpaces={6} />
-        <Text>{_(t`Bienvenue à Pass Culture`)}</Text>
-        <Button
-          title={_(t`Aller sur la page de connexion avec params`)}
-          onPress={goToLoginPageWithParams}
-        />
-        <Spacer.Column numberOfSpaces={6} />
-        {isFetching && <ActivityIndicator testID="user-activity-indicator" />}
-        {email && !isError && (
-          <UserInformationContainer>
-            <Text>{`EMAIL: ${email}`}</Text>
-            <Button
-              title={_(t`Rafraîchir les données de l'utilisateur`)}
-              onPress={refetchUserInformations}
-              testID="refreshUserInformationsButton"
-            />
-          </UserInformationContainer>
-        )}
+        <Button title={_(t`Aller sur la page de connexion`)} onPress={goToLoginPage} />
         <Spacer.Column numberOfSpaces={6} />
         <CoordinatesViewContainer position={position} />
         <Spacer.Column numberOfSpaces={6} />
@@ -87,11 +67,6 @@ const CoordinatesViewContainer = styled(CoordinatesView)({
   position: 'absolute',
   alignItems: 'center',
   bottom: 50,
-})
-
-const UserInformationContainer = styled.View({
-  alignItems: 'center',
-  paddingVertical: getSpacing(5),
 })
 
 const HeaderBackgroundWrapper = styled.View({
