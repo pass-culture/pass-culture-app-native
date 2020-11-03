a006a2350d01b1cbe3d4eced477da03ca52b0138
blixit <celestinya@gmail.com>
2020-11-03 10:47:09 +0100

    Displaying shared components

--- src/features/navigation/RootNavigator.tsx
+++ src/features/navigation/RootNavigator.tsx
@@ -3,12 +3,14 @@ import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 
 import { Login } from 'features/auth/pages/Login'
+import AppComponents from 'features/cheatcodes/pages/AppComponents'
 import { CheatCodes } from 'features/cheatcodes/pages/CheatCodes'
 import { Home } from 'features/home/pages/Home'
 
 import { onNavigationStateChange } from './services'
 
 export type RootStackParamList = {
+  AppComponents: undefined
   CheatCodes: undefined
   Home: undefined
   Login?: { userId: string }
@@ -27,6 +29,7 @@ export const RootNavigator: React.FC = function () {
           initialParams={{ userId: 'test_user_id' }}
         />
         <RootStack.Screen name="CheatCodes" component={CheatCodes} />
+        <RootStack.Screen name="AppComponents" component={AppComponents} />
       </RootStack.Navigator>
     </NavigationContainer>
   )
