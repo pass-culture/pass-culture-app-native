885caf62e7c521bf248aa5b854a5e93ba708de28
AntoineG <antoine.garcia@polyconseil.fr>
2020-11-04 17:48:08 +0100

    more precise ratio

--- src/ui/theme/grid.ts
+++ src/ui/theme/grid.ts
@@ -14,9 +14,9 @@ export const LENGTH_L = PixelRatio.roundToNearestPixel(12 * MARGIN_DP)
 export const LENGTH_XL = PixelRatio.roundToNearestPixel(15 * MARGIN_DP)
 
 // Ratios used for homepage modules (height / width). Source: Zeplin
-export const RATIO_BUSINESS = 116 / 332
-export const RATIO_ALGOLIA = 146 / 220
-export const RATIO_EXCLU = 292 / 333
+export const RATIO_BUSINESS = 1 / 3
+export const RATIO_ALGOLIA = 2 / 3
+export const RATIO_EXCLU = 5 / 6
 
 interface Props {
   horizontal?: boolean
