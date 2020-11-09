1f31085a43d4f00b41c646346b9081b14c3f5781
Gireg Maury <gireg.maury@gmail.com>
2020-11-09 18:15:17 +0100

    (PC-4942) feat: add enums to params from webapp

---
+++ src/libs/algolia/enums/facetsEnums.ts
@@ -0,0 +1,12 @@
+export enum FACETS_ENUM {
+  OFFER_CATEGORY = 'offer.category',
+  OFFER_IS_DIGITAL = 'offer.isDigital',
+  OFFER_IS_DUO = 'offer.isDuo',
+  OFFER_IS_EVENT = 'offer.isEvent',
+  OFFER_PRICE = 'offer.prices',
+  OFFER_IS_THING = 'offer.isThing',
+  OFFER_DATE = 'offer.dates',
+  OFFER_TIME = 'offer.times',
+  OFFER_TAGS = 'offer.tags',
+  OFFER_STOCKS_DATE_CREATED = 'offer.stocksDateCreated',
+}
---
+++ src/libs/algolia/enums/filtersEnums.ts
@@ -0,0 +1,14 @@
+export enum DATE_FILTER_OPTIONS {
+  TODAY = 'today',
+  CURRENT_WEEK = 'currentWeek',
+  CURRENT_WEEK_END = 'currentWeekEnd',
+  USER_PICK = 'picked',
+}
+
+export const PRICE_FILTER = {
+  DEFAULT_RANGE: [0, 500],
+}
+
+export const TIME_FILTER = {
+  DEFAULT_RANGE: [8, 24],
+}
