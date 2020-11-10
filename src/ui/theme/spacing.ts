5f4633a8e9226db926138bac2481909e9b0b7443
Gireg Maury <gireg.maury@gmail.com>
2020-11-10 15:53:04 +0100

    (PC-4942) feat: use DP as a base unit

--- src/ui/theme/spacing.ts
+++ src/ui/theme/spacing.ts
@@ -1,14 +1,17 @@
+import { PixelRatio } from 'react-native'
 import styled from 'styled-components/native'
 
-const UNIT_SPACE = 4
+const UNIT_SPACE_DP = 4
 
-export const getSpacing = (numberOfSpaces: number): number => UNIT_SPACE * numberOfSpaces
+export const getSpacing = (numberOfSpaces: number): number =>
+  PixelRatio.roundToNearestPixel(UNIT_SPACE_DP * numberOfSpaces)
 export const getSpacingString = (numberOfSpaces: number): string =>
-  `${UNIT_SPACE * numberOfSpaces}px`
+  `${getSpacing(numberOfSpaces)}px`
 
 interface SpacerProps {
   numberOfSpaces: number
 }
+
 const RowSpacer = styled.View<SpacerProps>(({ numberOfSpaces }) => ({
   width: getSpacing(numberOfSpaces),
 }))
@@ -22,7 +25,11 @@ interface FlexSpacerProps {
 const FlexSpacer = styled.View<FlexSpacerProps>(({ flex }) => ({
   flex: flex ? flex : 1,
 }))
-
+/**
+ * margin : `numberOfSpaces={6}`
+ *
+ * gutter : `numberOfSpaces={4}`
+ */
 export const Spacer = {
   Flex: FlexSpacer,
   Row: RowSpacer,
