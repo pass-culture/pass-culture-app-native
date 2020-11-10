1f79c2f0e13ca528fb32e205c17784bb106420f4
blixit <celestinya@gmail.com>
2020-11-10 15:44:38 +0100

    (PC-4934) New password : Deeplink listener installed

--- src/features/cheatcodes/pages/Navigation.tsx
+++ src/features/cheatcodes/pages/Navigation.tsx
@@ -1,13 +1,18 @@
 import { t } from '@lingui/macro'
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { Button, ScrollView } from 'react-native'
+import { Button, Linking, ScrollView } from 'react-native'
+import styled from 'styled-components/native'
 
+import { formatDeeplinkDomain } from 'features/deeplinks'
 import { _ } from 'libs/i18n'
 import { SafeContainer } from 'ui/components/SafeContainer'
-import { Spacer } from 'ui/theme'
+import { Spacer, Typo } from 'ui/theme'
 
 import { CheatCodesButton } from '../components/CheatCodesButton'
+
+const MdpDeeplink =
+  formatDeeplinkDomain() + 'mot-de-passe-perdu?token=etjkdfldkfsd&expiration_date=4567894123'
 
 export function Navigation(): JSX.Element {
   const navigation = useNavigation()
@@ -18,7 +23,32 @@ export function Navigation(): JSX.Element {
         <Button title={_(t`Page Login`)} onPress={() => navigation.navigate('Login')} />
         <Spacer.Column numberOfSpaces={5} />
         <CheatCodesButton />
+        <Spacer.Column numberOfSpaces={5} />
+        <Button title={_(t`Page Login`)} onPress={() => navigation.navigate('Login')} />
+        <Spacer.Column numberOfSpaces={5} />
+        <Button
+          title={_(t`Page Reinitialisation de Mdp`)}
+          onPress={() => {
+            if (Linking.canOpenURL(MdpDeeplink)) {
+              Linking.openURL(MdpDeeplink)
+            }
+          }}
+        />
+        <LittleDescription>{_(t`simulate email deeplink clic with:`)}</LittleDescription>
+        <LittleDescription>{MdpDeeplink}</LittleDescription>
+        <Spacer.Column numberOfSpaces={5} />
       </ScrollView>
     </SafeContainer>
   )
 }
+
+export default Navigation
+
+type Props = { fontSize?: number }
+const LittleDescription = styled(Typo.Caption).attrs<Props>(({ fontSize = 11 }) => ({
+  fontSize,
+}))<Props>(({ fontSize }) => ({
+  width: '100%',
+  textAlign: 'center',
+  fontSize,
+}))
---
+++ src/features/deeplinks/listener.android.ts
@@ -0,0 +1,24 @@
+import { t } from '@lingui/macro'
+import { useEffect } from 'react'
+import { Linking } from 'react-native'
+
+import { _ } from 'libs/i18n'
+
+import { useDeeplinkUrlHandler, useOnDeeplinkError } from './useDeeplinkUrlHandler'
+
+export function useListenDeepLinksEffect() {
+  const onError = useOnDeeplinkError()
+  const handleDeeplinkUrl = useDeeplinkUrlHandler()
+
+  useEffect(() => {
+    Linking.getInitialURL()
+      .then((url) => {
+        if (url) {
+          handleDeeplinkUrl({ url })
+        } else {
+          onError(_(t`Impossible d'ouvrir le lien`))
+        }
+      })
+      .catch(() => onError(_(t`Impossible d'ouvrir le lien`)))
+  }, [])
+}
