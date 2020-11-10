00896bbc4b179e471c2593679b11070f1010b15d
Anouk Hello <anouk.hello@polyconseil.fr>
2020-11-10 15:56:31 +0100

    (PC-4933) add ResetPasswordEmailSent button to cheatcodes page
    	It is temporary, waiting for PC-4356

--- src/features/cheatcodes/pages/CheatCodes.tsx
+++ src/features/cheatcodes/pages/CheatCodes.tsx
@@ -2,7 +2,7 @@ import { BatchUser } from '@bam.tech/react-native-batch'
 import { t } from '@lingui/macro'
 import { StackNavigationProp } from '@react-navigation/stack'
 import React, { FunctionComponent, useEffect, useState } from 'react'
-import { Text, Alert } from 'react-native'
+import { Text, Alert, Button } from 'react-native'
 import styled from 'styled-components/native'
 
 import { CodePushButton } from 'features/cheatcodes/components/CodePushButton'
@@ -21,7 +21,7 @@ type Props = {
   navigation: CheatCodesNavigationProp
 }
 
-export const CheatCodes: FunctionComponent<Props> = function () {
+export const CheatCodes: FunctionComponent<Props> = function ({ navigation }) {
   const [batchInstallationId, setBatchInstallationId] = useState('none')
 
   useEffect(() => {
@@ -34,6 +34,13 @@ export const CheatCodes: FunctionComponent<Props> = function () {
         <CrashTestButton />
         <NavigateHomeButton />
         <IdCheckButton />
+        <Button
+          title={'Reset password email sent'}
+          onPress={() =>
+            // TODO => PC-4356
+            navigation.navigate('ResetPasswordEmailSent', { userEmail: 'jean.dupont@gmail.com' })
+          }
+        />
         <Text>{batchInstallationId}</Text>
         <Spacer.Flex />
         {env.FEATURE_FLAG_CODE_PUSH_MANUAL && <CodePushButton />}
