6b4e77fa0c6665608b9c42d09992de30f75fc2b6
Benjamin Robert Stirrup <benjamin.stirrup@gmail.com>
2020-11-11 15:12:37 +0100

    (PC-4401) Add android splashscreen config

--- android/app/src/main/java/com/passculture/MainActivity.java
+++ android/app/src/main/java/com/passculture/MainActivity.java
@@ -1,12 +1,13 @@
 package com.passculture;
 
+import android.os.Bundle; // react-native-splash-screen
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // react-native-splash-screen
 import com.facebook.react.ReactActivityDelegate; //@react-navigation
 import com.facebook.react.ReactRootView; //@react-navigation
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView; //@react-navigation
 
 public class MainActivity extends ReactActivity {
-
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -16,13 +17,20 @@ public class MainActivity extends ReactActivity {
         return "PassCulture";
     }
 
+    // react-native-splash-screen
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }
+
     // @react-navigation (https://reactnavigation.org/docs/en/next/getting-started.html)
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
         return new ReactActivityDelegate(this, getMainComponentName()) {
             @Override
             protected ReactRootView createRootView() {
-            return new RNGestureHandlerEnabledRootView(MainActivity.this);
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
             }
         };
     }
