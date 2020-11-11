70e31e94cae4af83ed1ed8fd95d572bdc9cc2281
Gireg Maury <gireg.maury@gmail.com>
2020-11-11 10:12:57 +0100

    Add cheatcodes button to navigation page

--- src/features/cheatcodes/components/CheatCodesButton.test.tsx
+++ src/features/cheatcodes/components/CheatCodesButton.test.tsx
@@ -1,19 +1,17 @@
 import { render, fireEvent } from '@testing-library/react-native'
 import React from 'react'
 
+import { navigate } from '__mocks__/@react-navigation/native'
+
 import { CheatCodesButton } from './CheatCodesButton'
 
 describe('CheatCodesButton component', () => {
-  const navigation = {
-    navigate: jest.fn(),
-  } as any // eslint-disable-line @typescript-eslint/no-explicit-any
-
   it('should have a button to go to the Login', () => {
-    const component = render(<CheatCodesButton navigation={navigation} />)
+    const component = render(<CheatCodesButton />)
 
     const Button = component.getByText('CheatCodes')
     fireEvent.press(Button)
 
-    expect(navigation.navigate).toHaveBeenCalledWith('CheatCodes')
+    expect(navigate).toHaveBeenCalledWith('CheatCodes')
   })
 })
