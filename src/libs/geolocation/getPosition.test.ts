dc0e81138a8d34bc661d56c84fef428928ccab84
blixit <celestinya@gmail.com>
2020-11-12 00:26:26 +0100

    Remove useless alerts

--- src/libs/geolocation/getPosition.test.ts
+++ src/libs/geolocation/getPosition.test.ts
@@ -1,13 +1,8 @@
 import { waitFor } from '@testing-library/react-native'
-import { Alert } from 'react-native'
 import Geolocation from 'react-native-geolocation-service'
 
 import { getPosition } from './getPosition'
-import {
-  EiffelTourCoordinates,
-  getCurrentPositionFail,
-  getCurrentPositionSuccess,
-} from './tests.utils'
+import { EiffelTourCoordinates, getCurrentPositionSuccess } from './tests.utils'
 
 describe('getPosition', () => {
   afterEach(() => jest.restoreAllMocks())
@@ -22,17 +17,5 @@ describe('getPosition', () => {
 
     await waitFor(() => expect(getCurrentPosition).toHaveBeenCalled())
     expect(setInitialPosition).toHaveBeenCalledWith(EiffelTourCoordinates)
-  })
-  it('should reject with empty coordinates', async () => {
-    const getCurrentPosition = jest
-      .spyOn(Geolocation, 'getCurrentPosition')
-      .mockImplementation(getCurrentPositionFail)
-    const alert = jest.spyOn(Alert, 'alert')
-
-    const setInitialPosition = jest.fn()
-    getPosition(setInitialPosition)
-
-    await waitFor(() => expect(getCurrentPosition).toHaveBeenCalled())
-    expect(alert).toBeCalled()
   })
 })
--- src/libs/geolocation/getPosition.ts
+++ src/libs/geolocation/getPosition.ts
@@ -1,9 +1,10 @@
-import { Alert } from 'react-native'
 import AgonTukGeolocation, { GeoCoordinates } from 'react-native-geolocation-service'
 
 export const getPosition = (setInitialPosition: (coordinates: GeoCoordinates) => void): void =>
   AgonTukGeolocation.getCurrentPosition(
     (position) => setInitialPosition(position.coords),
-    (error) => Alert.alert('Error', JSON.stringify(error)),
+    () => {
+      /**/
+    },
     { enableHighAccuracy: false, timeout: 20000, maximumAge: 10000 }
   )
