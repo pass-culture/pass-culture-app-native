c7f28a037668accb50dcbdef4d3c949e57ee3dee
Benjamin Robert Stirrup <benjamin.stirrup@gmail.com>
2020-11-12 17:12:01 +0100

    Remove React test warning related to act

--- src/features/auth/pages/ResetPasswordEmailSent.test.tsx
+++ src/features/auth/pages/ResetPasswordEmailSent.test.tsx
@@ -1,11 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
-import { fireEvent, render } from '@testing-library/react-native'
+import { act, fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
 import { Linking } from 'react-native'
 import { openInbox } from 'react-native-email-link'
 import waitForExpect from 'wait-for-expect'
 
 import { RootStack } from 'features/navigation/RootNavigator'
+import { flushAllPromises } from 'tests/utils'
 
 import { ResetPasswordEmailSent } from './ResetPasswordEmailSent'
 
@@ -14,9 +15,12 @@ jest.mock('@react-navigation/native', () => jest.requireActual('@react-navigatio
 describe('<ResetPasswordEmailSent />', () => {
   it('should match snapshot', async () => {
     const instance = renderPage()
-    await waitForExpect(() => {
-      expect(instance.toJSON()).toMatchSnapshot()
+
+    await act(async () => {
+      await flushAllPromises()
     })
+
+    expect(instance).toMatchSnapshot()
   })
 
   it('should open mail app when clicking on contact support button', async () => {
