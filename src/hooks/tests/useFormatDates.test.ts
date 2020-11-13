780ebbf91d0e4cca901f94790615d33d966826be
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-11-13 12:01:10 +0100

    correct formatDate to only show dates in the future

--- src/hooks/tests/useFormatDates.test.ts
+++ src/hooks/tests/useFormatDates.test.ts
@@ -1,6 +1,13 @@
+import mockdate from 'mockdate'
+
 import { getDisplayDates } from '../useFormatDates'
 
 describe('useFormatDates', () => {
+  beforeAll(() => {
+    // 1 nov 2020
+    mockdate.set(new Date(2020, 10, 1, 10, 0, 0))
+  })
+
   it.each`
     dates                        | expected
     ${undefined}                 | ${undefined}
@@ -10,6 +17,8 @@ describe('useFormatDates', () => {
     ${[1607205600]}              | ${'5 déc 2020'}
     ${[1607205600, 1605205600]}  | ${'Dès le 12 nov 2020'}
     ${[1607205600, -1605205600]} | ${'5 déc 2020'}
+    ${[1600205600, 1600205600]}  | ${undefined}
+    ${[1600205600, 1605205600]}  | ${'12 nov 2020'}
   `('getDisplayDates($dates) \t= $expected', ({ dates, expected }) => {
     expect(getDisplayDates(dates)).toBe(expected)
   })
