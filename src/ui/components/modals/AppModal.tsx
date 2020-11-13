fb9746fd2084e810944b29749ea72e05f60e3c09
blixit <celestinya@gmail.com>
2020-11-13 12:07:28 +0100

    Fix modal background animation

--- src/ui/components/modals/AppModal.tsx
+++ src/ui/components/modals/AppModal.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent } from 'react'
-import { Modal, TouchableWithoutFeedback } from 'react-native'
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import { Animated, Easing, Modal, Platform, TouchableOpacity, ViewProps } from 'react-native'
 import styled from 'styled-components/native'
 
 import { IconInterface } from 'ui/svg/icons/types'
@@ -25,39 +25,76 @@ export const AppModal: FunctionComponent<Props> = ({
   onRightIconPress,
   children,
 }) => (
-  <Modal animationType="slide" statusBarTranslucent transparent={true} visible={visible}>
-    <TouchableWithoutFeedback onPress={onRightIconPress}>
-      <Overlay>
-        <TouchableWithoutFeedback>
-          <Container>
-            <ModalHeader
-              title={title}
-              leftIcon={leftIcon}
-              onLeftIconPress={onLeftIconPress}
-              rightIcon={rightIcon}
-              onRightIconPress={onRightIconPress}
-            />
-            <Content>{children}</Content>
-          </Container>
-        </TouchableWithoutFeedback>
-      </Overlay>
-    </TouchableWithoutFeedback>
-  </Modal>
+  <React.Fragment>
+    <OverlayComponent visible={visible} />
+    <Modal animationType="slide" statusBarTranslucent transparent={true} visible={visible}>
+      <ClicAwayArea activeOpacity={1} onPress={onRightIconPress}>
+        <Container activeOpacity={1}>
+          <ModalHeader
+            title={title}
+            leftIcon={leftIcon}
+            onLeftIconPress={onLeftIconPress}
+            rightIcon={rightIcon}
+            onRightIconPress={onRightIconPress}
+          />
+          <Content>{children}</Content>
+        </Container>
+      </ClicAwayArea>
+    </Modal>
+  </React.Fragment>
 )
 
-const Overlay = styled.View({
-  flex: 1,
+const ClicAwayArea = styled(TouchableOpacity)({
+  flexGrow: 1,
   flexDirection: 'column',
   justifyContent: 'flex-end',
-  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  height: '100%',
+  width: '100%',
 })
 
-const Container = styled.View({
+const OverlayComponent: FunctionComponent<ViewProps & { visible: boolean }> = (props) => {
+  const [opacity] = useState(new Animated.Value(0))
+  const [isDisplayed, setIsDisplayed] = useState(props.visible)
+
+  useEffect(() => {
+    if (props.visible) {
+      setIsDisplayed(true)
+      Animated.timing(opacity, {
+        duration: 300,
+        toValue: 1,
+        easing: Easing.linear,
+        useNativeDriver: Platform.OS == 'android',
+      }).start()
+    }
+
+    if (!props.visible) {
+      Animated.timing(opacity, {
+        duration: 300,
+        toValue: 0,
+        useNativeDriver: Platform.OS == 'android',
+      }).start(() => setIsDisplayed(false))
+    }
+  }, [props.visible])
+
+  return isDisplayed ? <AnimatedOverlay opacity={opacity} /> : null
+}
+
+const AnimatedOverlay = styled(Animated.View)<{ opacity: Animated.Value }>({
+  flex: 1,
+  position: 'absolute',
+  top: 0,
+  height: '100%',
+  width: '100%',
+  flexDirection: 'column',
+  justifyContent: 'flex-end',
+  backgroundColor: `rgba(0, 0, 0, 0.5)`,
+})
+
+const Container = styled(TouchableOpacity)({
   flexDirection: 'column',
   backgroundColor: ColorsEnum.WHITE,
   justifyContent: 'flex-start',
   alignItems: 'center',
-  minHeight: 300,
   width: '100%',
   borderTopStartRadius: getSpacing(4),
   borderTopEndRadius: getSpacing(4),
@@ -67,4 +104,6 @@ const Container = styled.View({
 const Content = styled.View({
   paddingTop: getSpacing(5),
   width: '100%',
+  alignItems: 'center',
+  maxWidth: getSpacing(125),
 })
