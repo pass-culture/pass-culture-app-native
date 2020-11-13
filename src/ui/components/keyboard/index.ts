2ccac03a9ca32871e12160f3a0b2eed2d9b2bd5f
blixit <celestinya@gmail.com>
2020-11-13 12:17:16 +0100

    Feedbacks

--- src/ui/components/keyboard/index.ts
+++ src/ui/components/keyboard/index.ts
@@ -1,7 +1,1 @@
-import { Platform } from 'react-native'
-
-import { AvoidingKeyboardContainer as AvoidingKeyboardContainerAndroid } from './AvoidingKeyboardContainer.android'
-import { AvoidingKeyboardContainer as AvoidingKeyboardContainerIos } from './AvoidingKeyboardContainer.ios'
-
-export const AvoidingKeyboardContainer =
-  Platform.OS === 'ios' ? AvoidingKeyboardContainerIos : AvoidingKeyboardContainerAndroid
+export { AvoidingKeyboardContainer } from './AvoidingKeyboardContainer'
