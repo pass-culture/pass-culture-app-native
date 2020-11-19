cd07537d86e077f9c85cc9e6e3b15ee81bda46e8
Anouk Hello <anouk.hello@polyconseil.fr>
2020-11-19 18:14:29 +0100

    (PC-5142) introduce PasswordSecurityRules component

---
+++ src/ui/components/inputs/rules/PasswordRule.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from 'react'
+
+import { InputRule } from 'ui/components/inputs/rules/InputRule'
+import { Check } from 'ui/svg/icons/Check'
+import { Close } from 'ui/svg/icons/Close'
+import { ColorsEnum } from 'ui/theme'
+
+type Props = {
+  title: string
+  isValidated: boolean
+}
+
+export const PasswordRule: FunctionComponent<Props> = ({ title, isValidated }) => {
+  return (
+    <InputRule
+      title={title}
+      icon={isValidated ? Check : Close}
+      testIdSuffix={isValidated ? 'check' : 'close'}
+      color={isValidated ? ColorsEnum.GREEN_VALID : ColorsEnum.ERROR}
+    />
+  )
+}
