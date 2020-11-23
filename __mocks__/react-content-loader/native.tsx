a784c488ac860e42fb5dd85538061c29536a2d14
Gireg Maury <gireg.maury@gmail.com>
2020-11-23 10:21:18 +0100

    (PC-5001) feat: add HomeBodyPlaceholder, mock and test it

---
+++ __mocks__/react-content-loader/native.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Svg } from 'react-native-svg'
+
+const MockContentLoader = ({ children }) => {
+  return <Svg>{children}</Svg>
+}
+export default MockContentLoader
