7b3358c468f212d15b28d0c7ff1de1b2f05e01c7
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-11-23 13:00:49 +0100

    Add the Arrows on the cover

--- src/features/home/atoms/Cover.tsx
+++ src/features/home/atoms/Cover.tsx
@@ -3,7 +3,16 @@ import { PixelRatio } from 'react-native'
 import styled from 'styled-components/native'
 
 import { Layout } from 'features/home/contentful'
-import { BORDER_RADIUS, LENGTH_M, LENGTH_L, RATIO_ALGOLIA, MARGIN_DP } from 'ui/theme'
+import { ArrowNextDouble } from 'ui/svg/icons/ArrowNextDouble'
+import {
+  BORDER_RADIUS,
+  LENGTH_M,
+  LENGTH_L,
+  RATIO_ALGOLIA,
+  MARGIN_DP,
+  ColorsEnum,
+  getSpacing,
+} from 'ui/theme'
 
 interface CoverProps {
   layout: Layout
@@ -15,14 +24,39 @@ export const Cover = ({ layout, uri }: CoverProps) => {
   const imageWidth = PixelRatio.roundToNearestPixel(imageHeight * RATIO_ALGOLIA)
 
   return (
-    <Image imageHeight={imageHeight} imageWidth={imageWidth} source={{ uri }} testID="coverImage" />
+    <Container imageHeight={imageHeight} imageWidth={imageWidth}>
+      <Image source={{ uri }} testID="coverImage" />
+      <ArrowsContainer>
+        <ArrowMarginContainer>
+          <ArrowNextDouble color={ColorsEnum.WHITE} size={56} />
+        </ArrowMarginContainer>
+      </ArrowsContainer>
+    </Container>
   )
 }
 
-const Image = styled.Image<{ imageWidth: number; imageHeight: number }>(
+const Container = styled.View<{ imageWidth: number; imageHeight: number }>(
   ({ imageWidth, imageHeight }) => ({
     height: imageHeight,
     width: imageWidth,
-    borderRadius: BORDER_RADIUS,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
   })
 )
+
+const Image = styled.Image({
+  height: '100%',
+  width: '100%',
+  borderRadius: BORDER_RADIUS,
+})
+
+const ArrowsContainer = styled.View({
+  position: 'absolute',
+  zIndex: 1,
+  flexDirection: 'row',
+  alignItems: 'center',
+})
+
+const ArrowMarginContainer = styled.View({
+  marginRight: getSpacing(-6),
+})
---
+++ src/ui/svg/icons/ArrowNextDouble.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import Svg, { Path, G } from 'react-native-svg'
+
+import { ColorsEnum } from 'ui/theme'
+
+import { IconInterface } from './types'
+
+export const ArrowNextDouble: React.FunctionComponent<IconInterface> = ({
+  size = 32,
+  color = ColorsEnum.BLACK,
+  testID,
+}) => (
+  <Svg width={size} height={size} viewBox="0 0 52 53" testID={testID}>
+    <G fill="none" fill-rule="evenodd">
+      <G fill={color}>
+        <Path
+          d="M7.482.418c.322-.336.81-.34 1.134-.027l.078.085 6.927 8.544c2.059 2.54 2.11 6.41.154 9.016l-.154.198-6.927 8.544c-.32.395-.863.421-1.212.058-.322-.336-.367-.886-.12-1.28l.069-.094 6.926-8.544c1.461-1.803 1.507-4.543.137-6.405l-.137-.177-6.926-8.544c-.321-.395-.298-1.01.05-1.374z"
+          transform="translate(-186 -338) translate(22 166) translate(0 40) translate(164 132.454)"
+        />
+        <Path
+          d="M.515 4.54c.352-.316.878-.31 1.223-.005l.083.083 5.215 5.912c1.508 1.71 1.556 4.257.146 6.02l-.146.174-5.215 5.912c-.34.385-.924.42-1.306.078-.353-.316-.409-.842-.149-1.224l.072-.092 5.215-5.913c.894-1.014.933-2.519.117-3.575l-.117-.141L.438 5.856c-.34-.385-.305-.974.077-1.316z"
+          transform="translate(-186 -338) translate(22 166) translate(0 40) translate(164 132.454)"
+        />
+      </G>
+    </G>
+  </Svg>
+)
