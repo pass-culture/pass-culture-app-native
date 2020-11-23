9c736209940d330122c36c4dc852f44a37db85d8
Gireg Maury <gireg.maury@gmail.com>
2020-11-23 10:21:43 +0100

    (PC-5001) feat: display 3s of HomeBodyPlaceholder in Home Adapt tests

--- src/features/home/pages/Home.test.tsx
+++ src/features/home/pages/Home.test.tsx
@@ -1,5 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native'
-import { render, waitFor, act } from '@testing-library/react-native'
+import { render, act } from '@testing-library/react-native'
 import React from 'react'
 
 import { HomeStack } from 'features/home/navigation/HomeNavigator'
@@ -9,7 +9,6 @@ import { flushAllPromises } from 'tests/utils'
 import { HomeComponent } from './Home'
 
 jest.mock('@react-navigation/native', () => jest.requireActual('@react-navigation/native'))
-
 jest.mock('libs/environment', () => ({
   env: {
     FEATURE_FLAG_CODE_PUSH: true,
@@ -22,81 +21,79 @@ jest.mock('features/auth/AuthContext', () => ({
 }))
 
 describe('Home component', () => {
+  beforeAll(() => {
+    jest.useFakeTimers()
+  })
+  afterEach(() => {
+    jest.clearAllTimers()
+  })
   it('should render correctly without login modal', async () => {
-    const home = await renderHomeWithoutLoginModal()
+    const home = await homeRenderer(false, false)
 
     expect(home).toMatchSnapshot()
+    home.unmount()
   })
 
   it('should render modal correctly', async () => {
-    const homeWithLoginModal = await renderHomeWithLoginModal()
-    const homeWithoutLoginModal = await renderHomeWithoutLoginModal()
+    const homeWithLoginModal = await homeRenderer(true, false)
+    const homeWithoutLoginModal = await homeRenderer(false, false)
 
     expect(homeWithoutLoginModal).toMatchDiffSnapshot(homeWithLoginModal)
+    homeWithLoginModal.unmount()
+    homeWithoutLoginModal.unmount()
   })
 
   it('should have a welcome message', async () => {
-    const { getByText } = await renderHomeWithoutLoginModal()
+    const { getByText, unmount } = await homeRenderer(false, false)
 
-    const welcomeText = await waitFor(() => getByText('Bienvenue !'))
+    const welcomeText = getByText('Bienvenue !')
     expect(welcomeText.props.children).toBe('Bienvenue !')
+    unmount()
   })
 
   it('should not have code push button', async () => {
     env.FEATURE_FLAG_CODE_PUSH_MANUAL = false
-    const home = await renderHomeWithoutLoginModal()
+    const home = await homeRenderer(false, false)
 
     expect(() => home.getByText('Check update')).toThrowError()
+    home.unmount()
   })
 
   it('should have components and navigation buttons when CHEAT_BUTTONS_ENABLED', async () => {
     env.CHEAT_BUTTONS_ENABLED = true
-    const home = await renderHomeWithoutLoginModal()
+    const home = await homeRenderer(false, false)
 
     expect(() => home.getByText('Composants')).toBeTruthy()
     expect(() => home.getByText('Navigation')).toBeTruthy()
+    home.unmount()
   })
 
   it('should NOT have components or navigation buttons when NOT CHEAT_BUTTONS_ENABLED', async () => {
     env.CHEAT_BUTTONS_ENABLED = false
-    const home = await renderHomeWithoutLoginModal()
+    const home = await homeRenderer(false, false)
 
     expect(() => home.getByText('Composants')).toThrowError()
     expect(() => home.getByText('Navigation')).toThrowError()
+    home.unmount()
   })
 })
 
-async function renderHomeWithoutLoginModal() {
+async function homeRenderer(withModal: boolean, withLoadingPlaceholder: boolean) {
   const renderAPI = render(
     <NavigationContainer>
       <HomeStack.Navigator initialRouteName="Home">
         <HomeStack.Screen
           name="Home"
           component={HomeComponent}
-          initialParams={{ shouldDisplayLoginModal: false }}
+          initialParams={{ shouldDisplayLoginModal: withModal }}
         />
       </HomeStack.Navigator>
     </NavigationContainer>
   )
   await act(async () => {
-    await flushAllPromises()
-  })
-  return renderAPI
-}
-
-async function renderHomeWithLoginModal() {
-  const renderAPI = render(
-    <NavigationContainer>
-      <HomeStack.Navigator initialRouteName="Home">
-        <HomeStack.Screen
-          name="Home"
-          component={HomeComponent}
-          initialParams={{ shouldDisplayLoginModal: true }}
-        />
-      </HomeStack.Navigator>
-    </NavigationContainer>
-  )
-  await act(async () => {
+    if (!withLoadingPlaceholder) {
+      jest.advanceTimersByTime(3000)
+    }
     await flushAllPromises()
   })
   return renderAPI
