690e880759afbb1430d87167990bc1621877b794
Gireg Maury <gireg.maury@gmail.com>
2020-11-24 14:59:15 +0100

    (PC-5221) move routes without tab bar to rootstack

--- src/features/home/navigation/HomeNavigator.tsx
+++ src/features/home/navigation/HomeNavigator.tsx
@@ -2,31 +2,12 @@ import { RouteProp, NavigationProp } from '@react-navigation/native'
 import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack'
 import React from 'react'
 
-import { ForgottenPassword } from 'features/auth/pages/ForgottenPassword'
-import { Login } from 'features/auth/pages/Login'
-import { ReinitializePassword } from 'features/auth/pages/ReinitializePassword'
-import { ResetPasswordEmailSent } from 'features/auth/pages/ResetPasswordEmailSent'
-import { ResetPasswordExpiredLink } from 'features/auth/pages/ResetPasswordExpiredLink'
-import { AppComponents } from 'features/cheatcodes/pages/AppComponents'
-import { CheatCodes } from 'features/cheatcodes/pages/CheatCodes'
-import { IdCheck } from 'features/cheatcodes/pages/IdCheck'
-import { Navigation } from 'features/cheatcodes/pages/Navigation'
 import { Home } from 'features/home/pages/Home'
 import { navigationRef } from 'features/navigation/RootNavigator'
-import { Offer } from 'features/offer'
 
+// Comming next: remove this navigator
 export type HomeStackParamList = {
-  AppComponents: undefined
-  Navigation: undefined
-  CheatCodes: undefined
   Home: { shouldDisplayLoginModal: boolean }
-  IdCheck: undefined
-  Login: undefined
-  ReinitializePassword: { token: string; expiration_timestamp: number }
-  ForgottenPassword: undefined
-  ResetPasswordEmailSent: { email: string }
-  ResetPasswordExpiredLink: { email: string }
-  Offer: { offerId: string }
 }
 
 export const HomeStack = createStackNavigator<HomeStackParamList>()
@@ -40,35 +21,11 @@ export const HomeNavigator: React.FC = function () {
         initialParams={{ shouldDisplayLoginModal: false }}
         options={{ headerShown: false }}
       />
-      <HomeStack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-      <HomeStack.Screen name="Offer" component={Offer} />
-      <HomeStack.Screen
-        name="ReinitializePassword"
-        component={ReinitializePassword}
-        options={{ headerShown: false }}
-      />
-      <HomeStack.Screen name="IdCheck" component={IdCheck} options={{ headerShown: false }} />
-      <HomeStack.Screen name="AppComponents" component={AppComponents} />
-      <HomeStack.Screen name="Navigation" component={Navigation} />
-      <HomeStack.Screen name="CheatCodes" component={CheatCodes} />
-      <HomeStack.Screen
-        name="ResetPasswordEmailSent"
-        component={ResetPasswordEmailSent}
-        options={{ headerShown: false }}
-      />
-      <HomeStack.Screen
-        name="ForgottenPassword"
-        component={ForgottenPassword}
-        options={{ headerShown: false }}
-      />
-      <HomeStack.Screen
-        name="ResetPasswordExpiredLink"
-        component={ResetPasswordExpiredLink}
-        options={{ headerShown: false }}
-      />
     </HomeStack.Navigator>
   )
 }
+
+// Comming next: move these helpers to RootNavigator
 
 /** Type helper to share screen names */
 export type ScreenNames = keyof HomeStackParamList
--- src/features/navigation/RootNavigator.tsx
+++ src/features/navigation/RootNavigator.tsx
@@ -2,11 +2,32 @@ import { NavigationContainer, NavigationContainerRef } from '@react-navigation/n
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 
+import { ForgottenPassword } from 'features/auth/pages/ForgottenPassword'
+import { Login } from 'features/auth/pages/Login'
+import { ReinitializePassword } from 'features/auth/pages/ReinitializePassword'
+import { ResetPasswordEmailSent } from 'features/auth/pages/ResetPasswordEmailSent'
+import { ResetPasswordExpiredLink } from 'features/auth/pages/ResetPasswordExpiredLink'
+import { AppComponents } from 'features/cheatcodes/pages/AppComponents'
+import { CheatCodes } from 'features/cheatcodes/pages/CheatCodes'
+import { IdCheck } from 'features/cheatcodes/pages/IdCheck'
+import { Navigation } from 'features/cheatcodes/pages/Navigation'
+import { Offer } from 'features/offer'
+
 import { onNavigationStateChange } from './services'
 import { TabNavigator } from './TabBar/TabNavigator'
 
 export type RootStackParamList = {
   TabNavigator: undefined
+  Login: undefined
+  Offer: { offerId: string }
+  ReinitializePassword: { token: string; expiration_timestamp: number }
+  IdCheck: undefined
+  AppComponents: undefined
+  Navigation: undefined
+  CheatCodes: undefined
+  ResetPasswordEmailSent: { email: string }
+  ForgottenPassword: undefined
+  ResetPasswordExpiredLink: { email: string }
 }
 
 const RootStack = createStackNavigator<RootStackParamList>()
@@ -17,6 +38,32 @@ export const RootNavigator: React.FC = () => {
     <NavigationContainer onStateChange={onNavigationStateChange} ref={navigationRef}>
       <RootStack.Navigator initialRouteName="TabNavigator" screenOptions={{ headerShown: false }}>
         <RootStack.Screen name="TabNavigator" component={TabNavigator} />
+        <RootStack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <RootStack.Screen name="Offer" component={Offer} />
+        <RootStack.Screen
+          name="ReinitializePassword"
+          component={ReinitializePassword}
+          options={{ headerShown: false }}
+        />
+        <RootStack.Screen name="IdCheck" component={IdCheck} options={{ headerShown: false }} />
+        <RootStack.Screen name="AppComponents" component={AppComponents} />
+        <RootStack.Screen name="Navigation" component={Navigation} />
+        <RootStack.Screen name="CheatCodes" component={CheatCodes} />
+        <RootStack.Screen
+          name="ResetPasswordEmailSent"
+          component={ResetPasswordEmailSent}
+          options={{ headerShown: false }}
+        />
+        <RootStack.Screen
+          name="ForgottenPassword"
+          component={ForgottenPassword}
+          options={{ headerShown: false }}
+        />
+        <RootStack.Screen
+          name="ResetPasswordExpiredLink"
+          component={ResetPasswordExpiredLink}
+          options={{ headerShown: false }}
+        />
       </RootStack.Navigator>
     </NavigationContainer>
   )
