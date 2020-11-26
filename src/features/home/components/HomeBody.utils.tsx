112468615f5d6f4ba7e4bbb6b2f41e8e6d3698bd
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-11-26 13:30:34 +0100

    (PC-5076): load algolia modules on HomeBody and filtered the displayed modules

---
+++ src/features/home/components/HomeBody.utils.tsx
@@ -0,0 +1,60 @@
+import {
+  BusinessPane,
+  ExclusivityPane,
+  Offers,
+  OffersWithCover,
+  ProcessedModule,
+} from 'features/home/contentful'
+import { AlgoliaHit } from 'libs/algolia'
+
+import { isArrayOfOfferTypeguard } from './typeguards'
+
+export type AlgoliaModuleResponse = {
+  [moduleId: string]: {
+    /*  module: ProcessedModule */
+    hits: AlgoliaHit[]
+    nbHits: number
+  }
+}
+
+export const showBusinessModule = (
+  targetNotConnectedUsersOnly: boolean | undefined,
+  connected: boolean
+): boolean => {
+  // Target both type of users
+  if (targetNotConnectedUsersOnly === undefined) return true
+
+  // Target only NON connected users
+  if (!connected && targetNotConnectedUsersOnly) return true
+
+  // Target only connected users
+  if (connected && !targetNotConnectedUsersOnly) return true
+  return false
+}
+
+export const getOfferModules = (modules: ProcessedModule[]): Array<Offers | OffersWithCover> => {
+  const filteredModules = modules.filter(
+    (module) => module instanceof Offers || module instanceof OffersWithCover
+  )
+  return isArrayOfOfferTypeguard(filteredModules) ? filteredModules : []
+}
+
+export const getModulesToDisplay = (
+  modules: ProcessedModule[],
+  algoliaModules: AlgoliaModuleResponse,
+  isLoggedIn: boolean
+) =>
+  modules.filter((module: ProcessedModule) => {
+    if (module instanceof BusinessPane) {
+      return showBusinessModule(module.targetNotConnectedUsersOnly, isLoggedIn)
+    }
+    if (module instanceof ExclusivityPane) return true
+
+    if (module.moduleId in algoliaModules) {
+      return (
+        algoliaModules[module.moduleId].hits.length > 0 &&
+        algoliaModules[module.moduleId].nbHits >= module.display.minOffers
+      )
+    }
+    return false
+  })
