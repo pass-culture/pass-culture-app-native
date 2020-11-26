42eb9806600340795787d3981daada112ba36cb3
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-11-26 15:12:13 +0100

    (PC-5076) Test white module title for first module

--- src/features/home/components/tests/OffersModule.test.tsx
+++ src/features/home/components/tests/OffersModule.test.tsx
@@ -1,9 +1,10 @@
-import { render, waitFor } from '@testing-library/react-native'
+import { render } from '@testing-library/react-native'
 import mockdate from 'mockdate'
 import React from 'react'
 
 import { mockedAlgoliaResponse } from 'libs/algolia/mockedResponses/mockedAlgoliaResponse'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { ColorsEnum } from 'ui/theme'
 
 import { AlgoliaParametersFields, DisplayParametersFields } from '../../contentful/contentful'
 import { OffersModule } from '../OffersModule'
@@ -13,27 +14,20 @@ mockdate.set(new Date(2020, 10, 16))
 const props = {
   algolia: {} as AlgoliaParametersFields,
   display: { minOffers: 0 } as DisplayParametersFields,
-  moduleId: 'module-id',
+  hits: mockedAlgoliaResponse.hits,
+  nbHits: mockedAlgoliaResponse.nbHits,
+  cover: null,
+  position: null,
 }
-jest.mock('libs/algolia/fetchAlgolia', () => ({
-  fetchAlgolia: () => mockedAlgoliaResponse,
-}))
 
 describe('OffersModule component', () => {
-  afterAll(() => jest.resetAllMocks())
-
-  it('should render correctly', async () => {
-    const OffersModuleComp = render(
-      reactQueryProviderHOC(
-        <OffersModule
-          algolia={props.algolia}
-          display={props.display}
-          moduleId={props.moduleId}
-          position={null}
-        />
-      )
-    )
-    await waitFor(() => OffersModuleComp.getAllByText('Musique'))
-    expect(OffersModuleComp).toMatchSnapshot()
+  it('should render correctly - with black title', () => {
+    const component = render(reactQueryProviderHOC(<OffersModule {...props} index={1} />))
+    expect(component).toMatchSnapshot()
+    expect(component.getByTestId('moduleTitle').props.color).toBe(ColorsEnum.BLACK)
+  })
+  it('should render with white title if firert module displayed', async () => {
+    const component = render(reactQueryProviderHOC(<OffersModule {...props} index={0} />))
+    expect(component.getByTestId('moduleTitle').props.color).toBe(ColorsEnum.WHITE)
   })
 })
