5a7ecb4a136c642945603ebfe68194de73d26a09
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-11-26 13:28:41 +0100

    (PC-5076): add typeguards for offer modules

---
+++ src/features/home/components/typeguards.ts
@@ -0,0 +1,11 @@
+import { Offers, OffersWithCover, ProcessedModule } from '../contentful'
+
+export const isOfferModuleTypeguard = (
+  module: ProcessedModule
+): module is Offers | OffersWithCover =>
+  module instanceof Offers || module instanceof OffersWithCover
+
+export const isArrayOfOfferTypeguard = (
+  modules: ProcessedModule[]
+): modules is Array<Offers | OffersWithCover> =>
+  modules.reduce<boolean>((acc: boolean, module) => acc && isOfferModuleTypeguard(module), true)
