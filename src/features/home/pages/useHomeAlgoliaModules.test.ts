89be611ad26e882efce2f50f85c52e57282f0cee
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-11-26 15:25:10 +0100

    (PC-5076) add test for useHomeAlgoliaModules

---
+++ src/features/home/pages/useHomeAlgoliaModules.test.ts
@@ -0,0 +1,51 @@
+import { renderHook, act, cleanup } from '@testing-library/react-hooks'
+
+import { parseAlgoliaParameters } from 'libs/algolia/parseAlgoliaParameters'
+import { FetchAlgoliaParameters } from 'libs/algolia/types'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+
+import { Offers } from '../contentful'
+
+import { useHomeAlgoliaModules } from './useHomeAlgoliaModules'
+
+const mockFetchAlgolia = jest.fn().mockResolvedValue({ hits: ['data'], nbHits: 10 })
+
+jest.mock('libs/algolia/fetchAlgolia', () => ({
+  fetchAlgolia: (arg: FetchAlgoliaParameters) => mockFetchAlgolia(arg),
+}))
+
+const offerModules = [
+  new Offers({
+    algolia: { title: 'tile', hitsPerPage: 4 },
+    display: { minOffers: 1, title: 'title', layout: 'one-item-medium' },
+    moduleId: 'algoliaModuleShown',
+  }),
+]
+
+describe('useHomeAlgoliaModules', () => {
+  afterEach(async () => {
+    jest.clearAllMocks()
+    await cleanup()
+  })
+
+  it('calls fetchAlgolia with params and returns data', async () => {
+    const { result, waitForNextUpdate } = renderHook(
+      () => useHomeAlgoliaModules(offerModules, null),
+      // eslint-disable-next-line react/display-name
+      { wrapper: ({ children }) => reactQueryProviderHOC(children) }
+    )
+
+    expect(mockFetchAlgolia).toHaveBeenCalledWith({
+      ...parseAlgoliaParameters({
+        geolocation: null,
+        parameters: { title: 'tile', hitsPerPage: 4 },
+      }),
+    })
+
+    await act(async () => {
+      await waitForNextUpdate()
+    })
+
+    expect(result.current['algoliaModuleShown']).toEqual({ hits: ['data'], nbHits: 10 })
+  })
+})
