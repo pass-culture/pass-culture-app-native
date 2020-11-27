6ffbe455be046ff71f01982fbaf5b383bde3dc2f
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-11-27 17:44:44 +0100

    (PC-5473) Log AllModulesSeen once we reach the end of the Home

--- src/libs/analytics.ts
+++ src/libs/analytics.ts
@@ -1,3 +1,10 @@
 import firebaseAnalyticsModule from '@react-native-firebase/analytics'
 
 export const analytics = firebaseAnalyticsModule()
+
+export enum AnalyticsEvent {
+  ALL_MODULES_SEEN = 'AllModulesSeen',
+}
+
+export const logAllModulesSeen = async (numberOfModules: number) =>
+  await analytics.logEvent(AnalyticsEvent.ALL_MODULES_SEEN, { numberOfModules })
