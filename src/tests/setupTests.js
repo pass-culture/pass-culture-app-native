9303bed46db76c1bdd7e95f72a91be8890a6ef6c
Justine Mignot <justinem@bam.tech>
2020-11-27 16:00:23 +0100

    (PC-4894) refacto msw use

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -6,6 +6,7 @@ global.expect.extend({ toMatchDiffSnapshot })
 
 global.beforeAll(() => server.listen())
 
-global.afterEach(() => server.resetHandlers())
-
-global.afterAll(() => server.close())
+global.afterAll(() => {
+  server.resetHandlers()
+  server.close()
+})
