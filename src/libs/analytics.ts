d5e6c9aa857efba3104b1a29f3dad13672140bb3
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-11-30 14:22:07 +0100

    (PC-5487) Log AllTilesSeen when reachiing the end of a module

--- src/libs/analytics.ts
+++ src/libs/analytics.ts
@@ -4,7 +4,11 @@ export const analytics = firebaseAnalyticsModule()
 
 export enum AnalyticsEvent {
   ALL_MODULES_SEEN = 'AllModulesSeen',
+  ALL_TILES_SEEN = 'AllTilesSeen',
 }
 
 export const logAllModulesSeen = async (numberOfModules: number) =>
   await analytics.logEvent(AnalyticsEvent.ALL_MODULES_SEEN, { numberOfModules })
+
+export const logAllTilesSeen = async (moduleName: string, numberOfTiles: number) =>
+  await analytics.logEvent(AnalyticsEvent.ALL_TILES_SEEN, { moduleName, numberOfTiles })
