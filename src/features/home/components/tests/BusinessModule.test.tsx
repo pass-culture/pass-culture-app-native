a71913655981aa57314f304d0d196b72260f03e8
Gireg Maury <gireg.maury@gmail.com>
2020-11-30 16:24:48 +0100

    (PC-4431) feat: typeform support in business module

--- src/features/home/components/tests/BusinessModule.test.tsx
+++ src/features/home/components/tests/BusinessModule.test.tsx
@@ -1,12 +1,30 @@
 import { render, fireEvent } from '@testing-library/react-native'
 import React from 'react'
 import { Linking } from 'react-native'
+import { UseQueryResult } from 'react-query'
+import waitForExpect from 'wait-for-expect'
 
-import { reactQueryProviderHOC } from '../../../../tests/reactQueryProviderHOC'
-import { useAuthContext } from '../../../auth/AuthContext'
+import { UserProfileResponse } from 'api/gen'
+import { useAuthContext } from 'features/auth/AuthContext'
+import * as HomeAPI from 'features/home/api'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { SnackBarHelperSettings } from 'ui/components/snackBar/types'
+
+import { BusinessPane } from '../../contentful'
 import { BusinessModule } from '../BusinessModule'
 jest.mock('features/auth/AuthContext')
-const props = {
+const mockUseAuthContext = useAuthContext as jest.Mock
+
+const mockDisplayInfosSnackBar = jest.fn()
+jest.mock('ui/components/snackBar/SnackBarContext', () => ({
+  useSnackBarContext: () => ({
+    displayInfosSnackBar: jest.fn((props: SnackBarHelperSettings) =>
+      mockDisplayInfosSnackBar(props)
+    ),
+  }),
+}))
+
+const props: BusinessPane = {
   firstLine: 'firstLine',
   secondLine: 'secondLine',
   image: 'https://fr.web.img6.acsta.net/medias/nmedia/18/96/46/01/20468669.jpg',
@@ -16,30 +34,77 @@ const props = {
   leftIcon: undefined,
 }
 
-const mockUseAuthContext = useAuthContext as jest.Mock
+const openUrlSpy = jest.spyOn(Linking, 'openURL')
+const homeAPISpy = jest.spyOn(HomeAPI, 'useUserProfileInfo')
 describe('BusinessModule component', () => {
   afterEach(() => jest.clearAllMocks())
-  afterAll(() => jest.resetAllMocks())
 
   it('should render correctly - with leftIcon = Idea by default', () => {
-    const { toJSON } = render(<BusinessModule {...props} />)
+    const { toJSON } = renderModule(props)
     expect(toJSON()).toMatchSnapshot()
   })
 
   it('should render correctly - with leftIcon provided', () => {
-    const { toJSON } = render(
-      <BusinessModule
-        {...props}
-        leftIcon="https://images.ctfassets.net/2bg01iqy0isv/1Sh2Ter3f4GgW9m926jqB5/83adbbd38e399d0089ff7b8f0efadf4c/Europe.png"
-      />
-    )
+    const { toJSON } = renderModule({
+      ...props,
+      leftIcon:
+        'https://images.ctfassets.net/2bg01iqy0isv/1Sh2Ter3f4GgW9m926jqB5/83adbbd38e399d0089ff7b8f0efadf4c/Europe.png',
+    })
     expect(toJSON()).toMatchSnapshot()
   })
 
-  it('should open url when clicking on the image', async () => {
-    const { getByTestId } = render(reactQueryProviderHOC(<BusinessModule {...props} />))
-    const openUrlSpy = jest.spyOn(Linking, 'openURL')
+  it('should open url when clicking on the image', () => {
+    const { getByTestId } = renderModule(props)
     fireEvent.press(getByTestId('imageBusiness'))
     expect(openUrlSpy).toHaveBeenCalledWith('url')
   })
+  it('should open url with replaced Email when connected and adequate url and display snackbar waiting for email', async () => {
+    mockUseAuthContext.mockImplementationOnce(() => ({ isLoggedIn: true }))
+
+    const { getByTestId } = renderModule({ ...props, url: 'some_url_with_email={email}' })
+
+    fireEvent.press(getByTestId('imageBusiness'))
+
+    await waitForExpect(() =>
+      expect(mockDisplayInfosSnackBar).toHaveBeenCalledWith({ message: 'Redirection en cours' })
+    )
+    await waitForExpect(() =>
+      expect(openUrlSpy).toHaveBeenCalledWith('some_url_with_email=email@domain.ext')
+    )
+  })
+  it('should redirect with filled email when required without the snackbar being displayed when email is already okay', async () => {
+    homeAPISpy.mockImplementation(() => {
+      return {
+        isLoading: false,
+        data: { email: 'email2@domain.ext', first_name: 'Jean' },
+      } as UseQueryResult<UserProfileResponse>
+    })
+
+    const { getByTestId } = renderModule({ ...props, url: 'some_url_with_email={email}' })
+
+    fireEvent.press(getByTestId('imageBusiness'))
+    await waitForExpect(() =>
+      expect(openUrlSpy).toHaveBeenCalledWith('some_url_with_email=email2@domain.ext')
+    )
+    expect(mockDisplayInfosSnackBar).not.toHaveBeenCalled()
+    homeAPISpy.mockReset()
+  })
+  it('should not display a snackbar when user profile data is yet to be received but the email is not needed', async () => {
+    homeAPISpy.mockImplementation(() => {
+      return {
+        isLoading: true,
+        data: undefined,
+      } as UseQueryResult<UserProfileResponse>
+    })
+
+    const { getByTestId } = renderModule({ ...props, url: 'some_url_with_no_email' })
+
+    fireEvent.press(getByTestId('imageBusiness'))
+    await waitForExpect(() => expect(openUrlSpy).toHaveBeenCalledWith('some_url_with_no_email'))
+    expect(mockDisplayInfosSnackBar).not.toHaveBeenCalled()
+    homeAPISpy.mockReset()
+  })
 })
+
+const renderModule = (props: BusinessPane) =>
+  render(reactQueryProviderHOC(<BusinessModule {...props} />))
