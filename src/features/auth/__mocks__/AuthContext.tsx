de1d26c96edd86b0636c94e8af91fcf2c79895f5
Gireg Maury <gireg.maury@gmail.com>
2020-11-30 17:14:18 +0100

    (PC-4431) feat: type context, better test syntax

--- src/features/auth/__mocks__/AuthContext.tsx
+++ src/features/auth/__mocks__/AuthContext.tsx
@@ -1,10 +1,14 @@
+import { IAuthContext } from '../AuthContext'
+
 export const signIn = jest.fn()
 export const signOut = jest.fn()
 
 export const useSignIn = jest.fn(() => signIn)
 export const useSignOut = jest.fn(() => signOut)
-export const useAuthContext = jest.fn(() => ({
-  signIn,
-  signOut,
-  isLoggedIn: false,
-}))
+export const useAuthContext = jest.fn(
+  (): IAuthContext => ({
+    signIn,
+    signOut,
+    isLoggedIn: false,
+  })
+)
