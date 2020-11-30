55c342cc2fb8176181b90841f0e93e9c09402293
Justine Mignot <justinem@bam.tech>
2020-11-30 19:06:08 +0100

    :sparkles: (PC-4894) remove profile data from CheatCodes

--- src/features/cheatcodes/pages/CheatCodes.tsx
+++ src/features/cheatcodes/pages/CheatCodes.tsx
@@ -9,11 +9,10 @@ import { CodePushButton } from 'features/cheatcodes/components/CodePushButton'
 import { CrashTestButton } from 'features/cheatcodes/components/CrashTestButton'
 import { LogoutButton } from 'features/cheatcodes/components/LogoutButton'
 import { NavigateHomeButton } from 'features/cheatcodes/components/NavigateHomeButton/NavigateHomeButton'
-import { useUserProfileInfo } from 'features/home/api'
 import { RootStackParamList } from 'features/navigation/RootNavigator'
 import { env } from 'libs/environment'
 import { _ } from 'libs/i18n'
-import { Spacer, Typo } from 'ui/theme'
+import { Spacer } from 'ui/theme'
 
 type CheatCodesNavigationProp = StackNavigationProp<RootStackParamList, 'CheatCodes'>
 
@@ -23,7 +22,6 @@ type Props = {
 
 export const CheatCodes: FunctionComponent<Props> = function () {
   const [batchInstallationId, setBatchInstallationId] = useState('none')
-  const { data: profileData } = useUserProfileInfo()
   useEffect(() => {
     getBatchInstallationID().then(setBatchInstallationId)
   }, [])
@@ -38,9 +36,6 @@ export const CheatCodes: FunctionComponent<Props> = function () {
       <Spacer.Flex />
       <Text>{batchInstallationId}</Text>
       <Spacer.Flex />
-      <Typo.Title3>
-        {profileData ? `${profileData.email} ${profileData.first_name}` : 'no profile data'}
-      </Typo.Title3>
       <Spacer.Flex />
       {env.FEATURE_FLAG_CODE_PUSH_MANUAL && <CodePushButton />}
 
