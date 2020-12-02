dae45f1156156b86ac41593a2f8ed25956ad9599
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-12-02 18:42:45 +0100

    (PC-5555) Grant permission to access location then smoothly refetch and update

--- src/libs/geolocation/useRequestGeolocPermission.ios.ts
+++ src/libs/geolocation/useRequestGeolocPermission.ios.ts
@@ -1,8 +1,14 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Geolocation from 'react-native-geolocation-service'
 
 export const useRequestGeolocPermission = () => {
+  const [permissionGranted, setPermissionGranted] = useState<boolean>(false)
+
   useEffect(() => {
-    Geolocation.requestAuthorization('whenInUse')
+    Geolocation.requestAuthorization('whenInUse').then((value) => {
+      if (value === 'granted') setPermissionGranted(true)
+    })
   }, [])
+
+  return permissionGranted
 }
