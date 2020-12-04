d9a5f14f8544ec7b67fe5b0749082a504db24bc9
Gireg Maury <gireg.maury@gmail.com>
2020-12-04 17:01:53 +0100

    (PC-5563) feat: add findUrlChunks function

---
+++ src/features/offer/services/__tests__/parseDescription.test.tsx
@@ -0,0 +1,37 @@
+import { customFindUrlChunks } from '../parseDescription'
+const description1 = `PRESSE / ILS EN PARLENT !
+« Drôle, intelligente, intéressante, intéressée, chaleureuse et humble » Libération
+« Irrésistible ! » Elle
+« Intelligent et revigorant » Le Parisien
+« Hilarant » Vanity Fair 
+Voir plus de critiques en suivant le lien suivant : https://fauxliencritique.com/`
+
+const description1WithoutUrl = `PRESSE / ILS EN PARLENT !
+« Drôle, intelligente, intéressante, intéressée, chaleureuse et humble » Libération
+« Irrésistible ! » Elle
+« Intelligent et revigorant » Le Parisien
+« Hilarant » Vanity Fair 
+Voir plus de critiques en suivant le lien suivant : `
+
+const description2 = `https://www.google.com/ Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+https://www.google.com/ Lorem ipsum dolor sit amet, consectetur http://another-wronglyplacedurl.extension_is_6_car_max elit. 
+http://www.google.com/ Lorem ipsum dolor sit amet, consecteturhttps://wrongly-placed.urladipiscing elit. 
+some text here as well http://www.google.com/?key=valeu&key2=value2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+
+https://www.google.com/`
+describe('customFindUrlChunks', () => {
+  it('finds url chunks and mark them as highlited', () => {
+    const highlightedChunks1 = customFindUrlChunks({
+      textToHighlight: description1,
+      searchWords: [],
+    })
+    expect(highlightedChunks1.length).toBe(1)
+    expect(description1.slice(0, highlightedChunks1[0].start)).toBe(description1WithoutUrl)
+
+    const highlightedChunks2 = customFindUrlChunks({
+      textToHighlight: description2,
+      searchWords: [],
+    })
+    expect(highlightedChunks2.length).toBe(5)
+  })
+})
