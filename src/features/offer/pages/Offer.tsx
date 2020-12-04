8367eefc05e11543962fa3b1c6e78043c046186c
Gireg Maury <gireg.maury@gmail.com>
2020-12-04 12:51:22 +0100

    (PC-4419) feat : add offer title

--- src/features/offer/pages/Offer.tsx
+++ src/features/offer/pages/Offer.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components/native'
 
 import { RootStackParamList } from 'features/navigation/RootNavigator'
 import { _ } from 'libs/i18n'
-import { Spacer, Typo } from 'ui/theme'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
 
 type Props = StackScreenProps<RootStackParamList, 'Offer'>
 
@@ -17,10 +17,15 @@ export const Offer: FunctionComponent<Props> = ({ route }: Props) => {
       <Spacer.Flex />
       <Typo.Hero>{_(t`Offer`)}</Typo.Hero>
       <Typo.Caption testID="offerId">{id}</Typo.Caption>
+      <Title testID="offerTitle" numberOfLines={3} adjustsFontSizeToFit>
+        {algoliaHit?.offer.name}
+      </Title>
+
       <Typo.Caption>{algoliaHit?.offer.category}</Typo.Caption>
       <Spacer.Flex />
     </Container>
   )
 }
 
-const Container = styled.View({ flex: 1, alignItems: 'center' })
+const Container = styled.View({ flex: 1, alignItems: 'center', paddingHorizontal: getSpacing(6) })
+const Title = styled(Typo.Title3)({ textAlign: 'center' })
