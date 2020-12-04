255c29c7ae4361b885ea559fe19434f1d9a6752a
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-12-04 13:53:28 +0100

    (PC-5559) Add icons categories

---
+++ src/ui/svg/icons/categories/index.tsx
@@ -0,0 +1,27 @@
+import { Atelier } from './Atelier'
+import { Cinéma } from './Cinéma'
+import { Conférence } from './Conférence'
+import { Exposition } from './Exposition'
+import { Instrument } from './Instrument'
+import { JeuxVidéos } from './JeuxVidéos'
+import { Livres } from './Livres'
+import { Musique } from './Musique'
+import { OeuvreDArt } from './OeuvreDArt'
+import { Presse } from './Presse'
+import { Spectacles } from './Spectacles'
+import { Streaming } from './Streaming'
+
+export const Category = {
+  Atelier: Atelier,
+  Cinéma: Cinéma,
+  Conférence: Conférence,
+  Exposition: Exposition,
+  Instrument: Instrument,
+  JeuxVidéos: JeuxVidéos,
+  Livres: Livres,
+  Musique: Musique,
+  OeuvreDArt: OeuvreDArt,
+  Presse: Presse,
+  Spectacles: Spectacles,
+  Streaming: Streaming,
+}
