501a42f44ed6fc0dadcceb84ffd554a327db25af
Gireg Maury <gireg.maury@gmail.com>
2020-12-04 12:51:22 +0100

    (PC-4419) feat : add offer title

--- src/features/offer/pages/Offer.test.tsx
+++ src/features/offer/pages/Offer.test.tsx
@@ -3,11 +3,14 @@ import { act, render } from '@testing-library/react-native'
 import React from 'react'
 
 import { RootStack } from 'features/navigation/RootNavigator'
+import { AlgoliaHit } from 'libs/algolia'
+import { mockedAlgoliaResponse } from 'libs/algolia/mockedResponses/mockedAlgoliaResponse'
 import { flushAllPromises } from 'tests/utils'
 
 import { Offer } from './Offer'
 
 jest.mock('@react-navigation/native', () => jest.requireActual('@react-navigation/native'))
+const defaultAlgoliaHit: AlgoliaHit = mockedAlgoliaResponse.hits[0]
 
 describe('<Offer />', () => {
   it('should match snapshot', async () => {
@@ -17,11 +20,15 @@ describe('<Offer />', () => {
   })
 })
 
-async function renderOfferPage() {
+async function renderOfferPage(algoliaHit?: AlgoliaHit) {
   const wrapper = render(
     <NavigationContainer>
       <RootStack.Navigator initialRouteName="Offer">
-        <RootStack.Screen name="Offer" component={Offer} initialParams={{ id: 'ABCDE' }} />
+        <RootStack.Screen
+          name="Offer"
+          component={Offer}
+          initialParams={{ id: 'ABCDE', algoliaHit: algoliaHit ?? defaultAlgoliaHit }}
+        />
       </RootStack.Navigator>
     </NavigationContainer>
   )
