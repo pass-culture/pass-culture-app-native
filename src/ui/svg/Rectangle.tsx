dad5452f394980b1bcb8b942c67aa85493a367d2
Gireg Maury <gireg.maury@gmail.com>
2020-12-08 10:46:06 +0100

    (PC-5503) feat : add Rectangle SVG

---
+++ src/ui/svg/Rectangle.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import Svg, { Defs, LinearGradient, Stop, Path, G } from 'react-native-svg'
+
+import { ColorsEnum, getSpacing } from '../theme'
+
+import { IconInterface } from './icons/types'
+
+export const Rectangle: React.FC<Omit<IconInterface, 'color'>> = ({ size = 32, testID }) => {
+  return (
+    <Svg
+      width={size}
+      height={getSpacing(2)}
+      viewBox="0 0 375 8"
+      preserveAspectRatio="none"
+      testID={testID}>
+      <Defs>
+        <LinearGradient id="prefix__a" x1="0%" x2="100%" y1="49.977%" y2="50.023%">
+          <Stop offset="0%" stopColor={ColorsEnum.PRIMARY} />
+          <Stop offset="100%" stopColor={ColorsEnum.SECONDARY} />
+        </LinearGradient>
+      </Defs>
+      <G fill="none">
+        <G stroke="url(#prefix__a)" strokeWidth={getSpacing(2)} transform="translate(0 -309)">
+          <Path d="M4 313H371V314H4z" />
+        </G>
+      </G>
+    </Svg>
+  )
+}
