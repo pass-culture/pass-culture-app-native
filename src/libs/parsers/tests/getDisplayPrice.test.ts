acb4acd21ec9a6a5a7aa9bed22680aa557589c3d
Justine Mignot <justinem@bam.tech>
2020-12-08 14:48:47 +0100

    :sparkles: (PC-4429) add duo gestion for price display

--- src/libs/parsers/tests/getDisplayPrice.test.ts
+++ src/libs/parsers/tests/getDisplayPrice.test.ts
@@ -2,18 +2,23 @@ import { getDisplayPrice } from '../getDisplayPrice'
 
 describe('getDisplayPrice', () => {
   it.each`
-    prices         | expected
-    ${undefined}   | ${''}
-    ${[]}          | ${''}
-    ${[0]}         | ${'Gratuit'}
-    ${[0.0, 7]}    | ${'Gratuit'}
-    ${[1]}         | ${'1 €'}
-    ${[2.0]}       | ${'2 €'}
-    ${[3.45]}      | ${'3,45 €'}
-    ${[5.6, 3.0]}  | ${'Dès 3 €'}
-    ${[-3.0, 5.6]} | ${'5,6 €'}
-    ${[8, 8]}      | ${'8 €'}
-  `('getDisplayPrice($prices) \t= $expected', ({ prices, expected }) => {
-    expect(getDisplayPrice(prices)).toBe(expected)
+    prices         | isDuo    | expected
+    ${undefined}   | ${false} | ${''}
+    ${[]}          | ${false} | ${''}
+    ${[0]}         | ${false} | ${'Gratuit'}
+    ${[0.0, 7]}    | ${false} | ${'Gratuit'}
+    ${[1]}         | ${false} | ${'1 €'}
+    ${[2.0]}       | ${false} | ${'2 €'}
+    ${[3.45]}      | ${false} | ${'3,45 €'}
+    ${[5.6, 3.0]}  | ${false} | ${'Dès 3 €'}
+    ${[-3.0, 5.6]} | ${false} | ${'5,6 €'}
+    ${[8, 8]}      | ${false} | ${'8 €'}
+    ${[8, 8]}      | ${true}  | ${'8 € /place'}
+    ${[0.0, 7]}    | ${true}  | ${'Gratuit'}
+    ${[]}          | ${true}  | ${''}
+    ${[3.45]}      | ${true}  | ${'3,45 € /place'}
+    ${[9, 5, 2]}   | ${true}  | ${'Dès 2 € /place'}
+  `('getDisplayPrice($prices) \t= $expected', ({ prices, isDuo, expected }) => {
+    expect(getDisplayPrice(prices, isDuo)).toBe(expected)
   })
 })
