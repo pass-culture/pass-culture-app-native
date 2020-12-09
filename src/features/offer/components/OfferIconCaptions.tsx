6492300f1132019b05dc2bb9f0ce181594c9bbbd
Justine Mignot <justinem@bam.tech>
2020-12-09 12:30:59 +0100

    :recycle: (PC-4429) all display logic outside component

--- src/features/offer/components/OfferIconCaptions.tsx
+++ src/features/offer/components/OfferIconCaptions.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components/native'
 import { useUserProfileInfo } from 'features/home/api'
 import { AlgoliaHit } from 'libs/algolia'
 import { _ } from 'libs/i18n'
-import { getDisplayPrice } from 'libs/parsers'
+import { getDisplayPrice, getDisplayPriceWithDuoMention } from 'libs/parsers'
 import { Duo } from 'ui/svg/icons/Duo'
 import { Euro } from 'ui/svg/icons/Euro'
 import { ColorsEnum, getSpacing, Spacer } from 'ui/theme'
@@ -14,15 +14,10 @@ import { IconWithCaption, OfferCategory } from '../atoms'
 
 export const OfferIconCaptions = ({ algoliaHit }: { algoliaHit: AlgoliaHit | undefined }) => {
   const { data: profileInfo } = useUserProfileInfo()
-  const priceWithoutDuoMention = getDisplayPrice(algoliaHit?.offer.prices)
-  const displayPriceDuoPrecision =
-    algoliaHit?.offer.isDuo &&
-    profileInfo?.isBeneficiary &&
-    priceWithoutDuoMention !== 'Gratuit' &&
-    priceWithoutDuoMention !== ''
-  const displayedPrice = `${priceWithoutDuoMention}${
-    displayPriceDuoPrecision ? ` ${_(t`/ place`)}` : ''
-  }`
+  const price =
+    algoliaHit?.offer.isDuo && profileInfo?.isBeneficiary
+      ? getDisplayPriceWithDuoMention(algoliaHit?.offer.prices)
+      : getDisplayPrice(algoliaHit?.offer.prices)
 
   if (!algoliaHit) return <React.Fragment />
 
@@ -40,7 +35,7 @@ export const OfferIconCaptions = ({ algoliaHit }: { algoliaHit: AlgoliaHit | und
         </React.Fragment>
       )}
       <Separator />
-      <IconWithCaption testID="iconEuro" Icon={Euro} caption={displayedPrice} />
+      <IconWithCaption testID="iconEuro" Icon={Euro} caption={price} />
       <Spacer.Row numberOfSpaces={6} />
     </Row>
   )
--- src/libs/parsers/getDisplayPrice.ts
+++ src/libs/parsers/getDisplayPrice.ts
@@ -1,13 +1,26 @@
+import { t } from '@lingui/macro'
+
+import { _ } from 'libs/i18n'
+
 const EURO_SYMBOL = '€'
 
 const formatToFrenchDecimal = (value: number) =>
   `${value.toString().replace('.', ',')} ${EURO_SYMBOL}`
 
-export const getDisplayPrice = (prices: number[] | undefined): string => {
-  if (!prices || prices.length === 0) return ''
-  if (prices.includes(0)) return 'Gratuit'
-
+const getPricePerPlace = (prices: number[]): string => {
   const uniquePrices = Array.from(new Set(prices.filter((p) => p > 0)))
   if (uniquePrices.length === 1) return `${formatToFrenchDecimal(uniquePrices[0])}`
   return `Dès ${formatToFrenchDecimal(uniquePrices.sort()[0])}`
+}
+
+export const getDisplayPrice = (prices: number[] | undefined): string => {
+  if (!prices || prices.length === 0) return ''
+  if (prices.includes(0)) return 'Gratuit'
+  return getPricePerPlace(prices)
+}
+
+export const getDisplayPriceWithDuoMention = (prices: number[] | undefined): string => {
+  if (!prices || prices.length === 0) return ''
+  if (prices.includes(0)) return 'Gratuit'
+  return `${getPricePerPlace(prices)} ${_(t`/ place`)}`
 }
