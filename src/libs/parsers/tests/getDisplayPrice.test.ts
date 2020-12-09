60e45d13e93250ba13c39e95de4988f56a42039e
Justine Mignot <justinem@bam.tech>
2020-12-09 12:30:59 +0100

    :recycle: (PC-4429) all display logic outside component

--- src/libs/parsers/tests/getDisplayPrice.test.ts
+++ src/libs/parsers/tests/getDisplayPrice.test.ts
@@ -1,4 +1,4 @@
-import { getDisplayPrice } from '../getDisplayPrice'
+import { getDisplayPrice, getDisplayPriceWithDuoMention } from '../getDisplayPrice'
 
 describe('getDisplayPrice', () => {
   it.each`
@@ -15,5 +15,21 @@ describe('getDisplayPrice', () => {
     ${[8, 8]}      | ${'8 €'}
   `('getDisplayPrice($prices) \t= $expected', ({ prices, expected }) => {
     expect(getDisplayPrice(prices)).toBe(expected)
+  })
+
+  it.each`
+    prices         | expected
+    ${undefined}   | ${''}
+    ${[]}          | ${''}
+    ${[0]}         | ${'Gratuit'}
+    ${[0.0, 7]}    | ${'Gratuit'}
+    ${[1]}         | ${'1 € / place'}
+    ${[2.0]}       | ${'2 € / place'}
+    ${[3.45]}      | ${'3,45 € / place'}
+    ${[5.6, 3.0]}  | ${'Dès 3 € / place'}
+    ${[-3.0, 5.6]} | ${'5,6 € / place'}
+    ${[8, 8]}      | ${'8 € / place'}
+  `('getDisplayPriceWithDuoMention($prices) \t= $expected', ({ prices, expected }) => {
+    expect(getDisplayPriceWithDuoMention(prices)).toBe(expected)
   })
 })
