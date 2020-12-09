62411285ae885f6faeec059ce3992ad9054cfc72
Gireg Maury <gireg.maury@gmail.com>
2020-12-09 15:53:15 +0100

    (PC-5660) test icon presence and update snaps

--- src/features/offer/components/__tests__/OfferHero.test.tsx
+++ src/features/offer/components/__tests__/OfferHero.test.tsx
@@ -7,17 +7,20 @@ describe('OfferHero', () => {
   it('shows both placeholders when url is empty', () => {
     const { queryByTestId } = render(<OfferHero imageUrl={''} />)
     expect(queryByTestId('offerBackPlaceholder')).toBeTruthy()
+    expect(queryByTestId('categoryIcon')).toBeTruthy()
     expect(queryByTestId('offerPlaceholder')).toBeTruthy()
   })
   it('shows both placeholders when url is undefined', () => {
     // @ts-ignore : for test purpose
     const { queryByTestId } = render(<OfferHero imageUrl={undefined} />)
     expect(queryByTestId('offerBackPlaceholder')).toBeTruthy()
+    expect(queryByTestId('categoryIcon')).toBeTruthy()
     expect(queryByTestId('offerPlaceholder')).toBeTruthy()
   })
   it('does not show placeholders when an url is defined', () => {
     const { queryByTestId } = render(<OfferHero imageUrl={'some_url_to_some_resource'} />)
     expect(queryByTestId('offerBackPlaceholder')).toBeFalsy()
+    expect(queryByTestId('categoryIcon')).toBeFalsy()
     expect(queryByTestId('offerPlaceholder')).toBeFalsy()
   })
 })
