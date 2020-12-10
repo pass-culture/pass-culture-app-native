1a3a4a140e269d4d7edd55d7d628e2c16d7138d6
Gireg Maury <gireg.maury@gmail.com>
2020-12-10 11:20:59 +0100

    (PC-5652) feat : add dehumanizeId service

---
+++ src/features/offer/services/dehumanizeId.ts
@@ -0,0 +1,16 @@
+import { decode } from 'hi-base32'
+
+export const dehumanizeId = (humanId: string) => {
+  try {
+    return byteArrayToInt(decode.asBytes(humanId.replace('8', 'O').replace('9', 'I')))
+  } catch {
+    return null
+  }
+}
+
+const byteArrayToInt = (bytes: number[]) => {
+  return bytes.reduce(
+    (result, byte, index) => result + byte * Math.pow(256, bytes.length - 1 - index),
+    0
+  )
+}
