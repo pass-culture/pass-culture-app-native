c68050622fc0f2bc8cbfd4e8c06aced258044943
Gireg Maury <gireg.maury@gmail.com>
2020-12-10 11:20:59 +0100

    (PC-5652) feat : add dehumanizeId service

---
+++ src/features/offer/services/__tests__/dehumanizeId.test.ts
@@ -0,0 +1,22 @@
+import { dehumanizeId } from '../dehumanizeId'
+
+interface HumToDehumTest {
+  humanizedId: string
+  dehumanizedId: number
+}
+
+describe('dehumanizeId', () => {
+  it.each`
+    humanizedId | dehumanizedId
+    ${'AHEHK'}  | ${116853}
+    ${'AHD3Q'}  | ${116664}
+    ${'AHD3G'}  | ${116659}
+    ${'AHD3C'}  | ${116657}
+    ${'AGHYA'}  | ${102272}
+  `(
+    'a given humanizedId: $humanizedId becomes a dehumanizedId: $dehumanizedId',
+    ({ dehumanizedId, humanizedId }: HumToDehumTest) => {
+      expect(dehumanizeId(humanizedId)).toBe(dehumanizedId)
+    }
+  )
+})
