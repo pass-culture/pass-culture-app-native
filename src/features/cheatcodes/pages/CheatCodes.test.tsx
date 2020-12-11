ba068de195f31fa89056f83e479789ff8b746b85
Justine Mignot <justinem@bam.tech>
2020-12-11 10:25:58 +0100

    :white_check_mark: (PC-5737) add unit test

--- src/features/cheatcodes/pages/CheatCodes.test.tsx
+++ src/features/cheatcodes/pages/CheatCodes.test.tsx
@@ -2,6 +2,7 @@ import { act, render } from '@testing-library/react-native'
 import React from 'react'
 
 import { BatchUser } from '__mocks__/@bam.tech/react-native-batch'
+import { env } from 'libs/environment'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { flushAllPromises } from 'tests/utils'
 
@@ -31,6 +32,31 @@ describe('CheatCodes component', () => {
 
     expect(instance.toJSON()).toMatchSnapshot()
   })
+
+  it.each`
+    environment     | codePushManual | buttonIsdisplayed
+    ${'testing'}    | ${true}        | ${true}
+    ${'testing'}    | ${false}       | ${true}
+    ${'staging'}    | ${true}        | ${true}
+    ${'staging'}    | ${false}       | ${false}
+    ${'production'} | ${true}        | ${true}
+    ${'production'} | ${false}       | ${false}
+  `(
+    'should display/not display code push button',
+    async ({ environment, codePushManual, buttonIsdisplayed }) => {
+      env.ENV = environment
+      env.FEATURE_FLAG_CODE_PUSH_MANUAL = codePushManual
+      const instance = render(reactQueryProviderHOC(<CheatCodes navigation={navigation} />))
+
+      await act(async () => {
+        await flushAllPromises()
+      })
+      buttonIsdisplayed
+        ? expect(instance.queryByText('Check update')).toBeTruthy()
+        : expect(instance.queryByText('Check update')).toBeFalsy()
+      expect.assertions(1)
+    }
+  )
 
   it('should call installationID and display it', async () => {
     const { queryByText } = render(reactQueryProviderHOC(<CheatCodes navigation={navigation} />))
