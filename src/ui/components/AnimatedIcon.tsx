a6349b0c63b56a278af2ea024a0e52cb534f5af9
Gireg Maury <gireg.maury@gmail.com>
2020-12-11 16:53:30 +0100

    (PC-5629) feat: add AnimatedIcon component

---
+++ src/ui/components/AnimatedIcon.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Animated } from 'react-native'
+import styled from 'styled-components/native'
+
+import { IconInterface } from '../svg/icons/types'
+import { ColorsEnum } from '../theme'
+
+interface Props {
+  Icon: React.ElementType<IconInterface>
+  transition: Animated.AnimatedInterpolation
+  size: number
+  initialColor: ColorsEnum
+  finalColor: ColorsEnum
+  testID?: string
+}
+
+export const AnimatedIcon: React.FC<Props> = ({
+  Icon,
+  transition,
+  testID,
+  size,
+  initialColor,
+  finalColor,
+}) => {
+  const oppositeOpacity = transition.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+  })
+  return (
+    <Container size={size}>
+      <IconContainer testID="initial-icon-container" style={{ opacity: oppositeOpacity }}>
+        <Icon testID={testID} size={size} color={initialColor} />
+      </IconContainer>
+      <IconContainer testID="final-icon-container" style={{ opacity: transition }}>
+        <Icon size={size} color={finalColor} testID={testID ? `final-${testID}` : undefined} />
+      </IconContainer>
+    </Container>
+  )
+}
+const IconContainer = styled(Animated.View)({ position: 'absolute' })
+const Container = styled.View<{ size: number }>(({ size }) => ({
+  height: size,
+  width: size,
+}))
