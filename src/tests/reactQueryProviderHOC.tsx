d27a968e1d5b004f72c88f189cb75e8c5942016b
Gireg Maury <gireg.maury@gmail.com>
2020-12-14 16:36:09 +0100

    (PC-5740) feat: clear react-query cache after each test file

--- src/tests/reactQueryProviderHOC.tsx
+++ src/tests/reactQueryProviderHOC.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { QueryCache, QueryClient, QueryClientProvider } from 'react-query'
 
+export const queryCache = new QueryCache()
+
 export const reactQueryProviderHOC = (component: React.ReactNode) => {
-  const queryClient = new QueryClient({ cache: new QueryCache() })
+  const queryClient = new QueryClient({ cache: queryCache })
   return <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>
 }
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -2,6 +2,8 @@ import { toMatchDiffSnapshot } from 'snapshot-diff'
 
 import { server } from 'tests/server'
 
+import { queryCache } from './reactQueryProviderHOC'
+
 global.expect.extend({ toMatchDiffSnapshot })
 
 global.beforeAll(() => server.listen())
@@ -9,4 +11,5 @@ global.beforeAll(() => server.listen())
 global.afterAll(() => {
   server.resetHandlers()
   server.close()
+  queryCache.clear()
 })
