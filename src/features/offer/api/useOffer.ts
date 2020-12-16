520c31c109c32d8a5b2213fb1dd2ee3b0d3d78af
Justine Mignot <justinem@bam.tech>
2020-12-16 11:01:35 +0100

    :sparkles: (PC-5679) add api response adaptation (/offer/id) for address

--- src/features/offer/api/useOffer.ts
+++ src/features/offer/api/useOffer.ts
@@ -3,15 +3,36 @@ import { useQuery } from 'react-query'
 import { api } from 'api/api'
 import { OfferResponse } from 'api/gen'
 
+interface OfferAdaptedResponse extends OfferResponse {
+  fullAddress: string | null
+}
+
 interface UseOfferInterface {
   offerId: number | null
 }
 
+export const adaptOfferResponse = (offerApiResponse: OfferResponse): OfferAdaptedResponse => {
+  const locationName = offerApiResponse.venue.publicName || offerApiResponse.venue.name
+  const addressSecondPart = `${offerApiResponse.venue.postalCode} ${offerApiResponse.venue.city}`
+  const fullAddress = offerApiResponse.venue.address
+    ? `${locationName}, ${offerApiResponse.venue.address} ${addressSecondPart}`
+    : `${locationName}, ${addressSecondPart}`
+  return {
+    ...offerApiResponse,
+    fullAddress,
+  }
+}
+
+const getOfferById = async (offerId: string) => {
+  const offerApiResponse = await api.getnativev1offerofferId(offerId.toString())
+  return adaptOfferResponse(offerApiResponse)
+}
+
 export const useOffer = ({ offerId }: UseOfferInterface) => {
-  return useQuery<OfferResponse>(
+  return useQuery<OfferAdaptedResponse>(
     ['offer', offerId],
     //@ts-ignore: Query is enabled only if offerId is truthy
-    () => api.getnativev1offerofferId(offerId.toString()),
+    () => getOfferById(offerId.toString()),
     { enabled: !!offerId }
   )
 }
