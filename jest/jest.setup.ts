a96dba21639b089a352c5a648b49f0e8188f413a
blixit <celestinya@gmail.com>
2020-12-16 17:21:45 +0100

    move jest-setup file from js to ts to prevent typing errors

--- jest/jest.setup.ts
+++ jest/jest.setup.ts
@@ -27,7 +27,7 @@ jest.mock('react-native/Libraries/Linking/Linking', () => ({
 }))
 
 jest.mock('react-native-safe-area-context', () => ({
-  ...jest.requireActual('react-native-safe-area-context'),
+  ...(jest.requireActual('react-native-safe-area-context') as Record<string, unknown>),
   useSafeAreaInsets: () => ({ bottom: 16, right: 16, left: 16, top: 16 }),
 }))
 
