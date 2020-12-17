8006aac07a1343f497ef6bff2ec1b7ac632af7bb
Gireg Maury <gireg.maury@gmail.com>
2020-12-17 18:16:44 +0100

    (PC-5807) feat: add AccessibilityAtom

---
+++ src/features/offer/atoms/AccessibilityAtom.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import { LayoutChangeEvent } from 'react-native'
+import styled from 'styled-components/native'
+
+import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
+import { BorderRadiusEnum } from 'ui/theme/grid'
+
+import { _ } from '../../../libs/i18n'
+import { ValidationMark } from '../../../ui/components/ValidationMark'
+
+import { getIconAndWording } from './AccessibilityAtom.service'
+
+export enum HandicapCategory {
+  'VISUAL',
+  'MENTAL',
+  'MOTOR',
+  'AUDIO',
+}
+interface Props {
+  handicap: HandicapCategory
+  isAccessible: boolean
+  sideSpace: number
+}
+
+export const AccessibilityAtom: React.FC<Props> = ({ handicap, isAccessible, sideSpace }) => {
+  const { Icon, wording } = getIconAndWording(handicap)
+  const [width, setWidth] = useState<number | undefined>(undefined)
+  return (
+    <Container>
+      <Frame
+        testID="accessibilityFrame"
+        onLayout={(event: LayoutChangeEvent) => {
+          setWidth(event.nativeEvent.layout.width)
+        }}>
+        <Spacer.Flex />
+        <Icon size={getSpacing(12)} />
+        <Spacer.Flex />
+        <ValidationContainer>
+          <ValidationMark
+            isValid={isAccessible}
+            size={getSpacing(6)}
+            invalidTestID="invalidTestId"
+            validtestID="validTestId"
+          />
+        </ValidationContainer>
+      </Frame>
+      <Spacer.Column numberOfSpaces={4} />
+      <TextContainer testID="textContainer" width={(width ?? 0) + sideSpace}>
+        <Text numberOfLines={2}>{wording}</Text>
+      </TextContainer>
+    </Container>
+  )
+}
+
+const Frame = styled.View({
+  aspectRatio: '1',
+  alignItems: 'center',
+  borderColor: ColorsEnum.GREY_MEDIUM,
+  borderRadius: BorderRadiusEnum.BORDER_RADIUS,
+  borderWidth: 1,
+})
+
+const Container = styled.View({
+  flexDirection: 'column',
+  flexShrink: 1,
+})
+const Text = styled(Typo.Caption)({
+  textAlign: 'center',
+})
+const TextContainer = styled.View<{ width: number }>(({ width }) => ({
+  width,
+  alignSelf: 'center',
+}))
+const ValidationContainer = styled.View({
+  position: 'absolute',
+  bottom: -getSpacing(3),
+})
---
+++ src/features/offer/atoms/__tests__/AccessibilityAtom.test.tsx
@@ -0,0 +1,63 @@
+import { fireEvent, render } from '@testing-library/react-native'
+import React from 'react'
+
+import { HandicapAudio } from 'ui/svg/icons/HandicapAudio'
+import { HandicapMental } from 'ui/svg/icons/HandicapMental'
+import { HandicapMotor } from 'ui/svg/icons/HandicapMotor'
+import { HandicapVisual } from 'ui/svg/icons/HandicapVisual'
+
+import { AccessibilityAtom, HandicapCategory } from '../AccessibilityAtom'
+import { getIconAndWording } from '../AccessibilityAtom.service'
+
+describe('getIconAndWording', () => {
+  it.each`
+    handicapCategory           | expectedIcon      | expectedWording
+    ${HandicapCategory.MOTOR}  | ${HandicapMotor}  | ${'Handicap moteur'}
+    ${HandicapCategory.VISUAL} | ${HandicapVisual} | ${'Handicap visuel'}
+    ${HandicapCategory.AUDIO}  | ${HandicapAudio}  | ${'Handicap auditif'}
+    ${HandicapCategory.MENTAL} | ${HandicapMental} | ${'Handicap mental'}
+  `(
+    'should return $expectedIcon and $expectedWording when given $handicapCategory',
+    ({
+      handicapCategory,
+      expectedIcon,
+      expectedWording,
+    }: {
+      handicapCategory: HandicapCategory
+      expectedIcon: React.FC
+      expectedWording: string
+    }) => {
+      expect(getIconAndWording(handicapCategory)).toEqual({
+        Icon: expectedIcon,
+        wording: expectedWording,
+      })
+    }
+  )
+})
+describe('AccessibilityAtom', () => {
+  it('should display the validIcon when isValid is true', () => {
+    const { queryByTestId } = render(
+      <AccessibilityAtom sideSpace={0} handicap={HandicapCategory.MENTAL} isAccessible={true} />
+    )
+    expect(queryByTestId('invalidTestId')).toBeFalsy()
+    expect(queryByTestId('validTestId')).toBeTruthy()
+  })
+  it('should displat the invalidIcon when isValid is false', () => {
+    const { queryByTestId } = render(
+      <AccessibilityAtom sideSpace={0} handicap={HandicapCategory.MENTAL} isAccessible={false} />
+    )
+    expect(queryByTestId('invalidTestId')).toBeTruthy()
+    expect(queryByTestId('validTestId')).toBeFalsy()
+  })
+  it('uses sideSpace to overgrow textContainer', async () => {
+    const { getByTestId } = render(
+      <AccessibilityAtom sideSpace={10} handicap={HandicapCategory.MENTAL} isAccessible={false} />
+    )
+    const textContainer = getByTestId('textContainer')
+    expect(textContainer.props.style[0].width).toEqual(10)
+
+    const accessibilityFrame = getByTestId('accessibilityFrame')
+    fireEvent(accessibilityFrame, 'layout', { nativeEvent: { layout: { width: 25 } } })
+    expect(textContainer.props.style[0].width).toEqual(35)
+  })
+})
