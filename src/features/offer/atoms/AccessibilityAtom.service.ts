173c3ebaa52d7fdd93bc5603222b4573f9317001
Gireg Maury <gireg.maury@gmail.com>
2020-12-17 18:16:44 +0100

    (PC-5807) feat: add AccessibilityAtom

---
+++ src/features/offer/atoms/AccessibilityAtom.service.ts
@@ -0,0 +1,25 @@
+import { t } from '@lingui/macro'
+
+import { _ } from 'libs/i18n'
+import { HandicapAudio } from 'ui/svg/icons/HandicapAudio'
+import { HandicapMental } from 'ui/svg/icons/HandicapMental'
+import { HandicapMotor } from 'ui/svg/icons/HandicapMotor'
+import { HandicapVisual } from 'ui/svg/icons/HandicapVisual'
+import { IconInterface } from 'ui/svg/icons/types'
+
+import { HandicapCategory } from './AccessibilityAtom'
+
+export const getIconAndWording = (
+  handicap: HandicapCategory
+): { Icon: React.FC<IconInterface>; wording: string } => {
+  switch (handicap) {
+    case HandicapCategory.VISUAL:
+      return { Icon: HandicapVisual, wording: _(t`Handicap visuel`) }
+    case HandicapCategory.MENTAL:
+      return { Icon: HandicapMental, wording: _(t`Handicap mental`) }
+    case HandicapCategory.MOTOR:
+      return { Icon: HandicapMotor, wording: _(t`Handicap moteur`) }
+    case HandicapCategory.AUDIO:
+      return { Icon: HandicapAudio, wording: _(t`Handicap auditif`) }
+  }
+}
