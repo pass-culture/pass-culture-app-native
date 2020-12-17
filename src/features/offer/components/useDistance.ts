575eb33eb27806da8aa3bf6f59fc5184cd065435
Justine Mignot <justinem@bam.tech>
2020-12-17 11:38:47 +0100

    :recycle: (PC-5679) refacto, move to a component

--- src/features/offer/components/useDistance.ts
+++ src/features/offer/components/useDistance.ts
@@ -2,8 +2,8 @@ import { useGeolocation } from 'libs/geolocation'
 import { formatDistance } from 'libs/parsers'
 
 export const useDistance = (offerPosition: {
-  lat: number | undefined
-  lng: number | undefined
+  lat?: number
+  lng?: number
 }): string | undefined | null => {
   const userPosition = useGeolocation()
   if (!userPosition || !offerPosition) return null
