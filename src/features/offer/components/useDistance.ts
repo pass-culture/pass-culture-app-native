870ff7b2dadc30748abd05056fd90c389bef738e
Justine Mignot <justinem@bam.tech>
2020-12-17 14:23:00 +0100

    :recycle: (PC-5679) typing

--- src/features/offer/components/useDistance.ts
+++ src/features/offer/components/useDistance.ts
@@ -1,11 +1,8 @@
 import { useGeolocation } from 'libs/geolocation'
 import { formatDistance } from 'libs/parsers'
 
-export const useDistance = (offerPosition: {
-  lat?: number
-  lng?: number
-}): string | undefined | null => {
+export const useDistance = (offerPosition: { lat?: number; lng?: number }): string | undefined => {
   const userPosition = useGeolocation()
-  if (!userPosition || !offerPosition) return null
+  if (!userPosition || !offerPosition) return undefined
   return formatDistance(offerPosition, userPosition)
 }
