40737cd721b867d00ea1506bfddded948102bf09
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-12-18 18:13:48 +0100

    Add previous arrow on RetryBoundary if can go back

--- __mocks__/@react-navigation/native.ts
+++ __mocks__/@react-navigation/native.ts
@@ -1,13 +1,13 @@
-import { renderHook } from "@testing-library/react-hooks"
-
 export const navigate = jest.fn()
 export const reset = jest.fn()
 export const goBack = jest.fn()
+export const canGoBack = jest.fn()
 
 export const useNavigation = () => ({
   navigate,
   reset,
   goBack,
+  canGoBack,
 })
 
 export const useRoute = jest.fn()
