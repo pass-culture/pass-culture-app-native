12933b1eb7bb9e64c72845ab5bb0d3f46a51e1c1
Gireg Maury <gireg.maury@gmail.com>
2020-12-18 14:24:52 +0100

    (PC-5807) feat: add AccessibilityBlock

---
+++ src/features/offer/components/__tests__/AccessibilityBlock.test.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react-native'
+import React from 'react'
+
+import { AccessibilityBlock } from '../AccessibilityBlock'
+
+describe('AccessibilityBlock', () => {
+  it('renders all handicap information', () => {
+    const { queryByText } = render(
+      <AccessibilityBlock
+        audioDisability={true}
+        motorDisability={false}
+        mentalDisability={true}
+        visualDisability={false}
+      />
+    )
+    expect(queryByText('Handicap visuel')).toBeTruthy()
+    expect(queryByText('Handicap moteur')).toBeTruthy()
+    expect(queryByText('Handicap mental')).toBeTruthy()
+    expect(queryByText('Handicap auditif')).toBeTruthy()
+  })
+})
