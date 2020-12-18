02954e413bc6bddd14bab7f10c0c929d05ee1ce3
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-12-18 18:23:57 +0100

    write test for ErrorBoundary

--- src/features/errors/pages/tests/RetryBoundary.test.tsx
+++ src/features/errors/pages/tests/RetryBoundary.test.tsx
@@ -1,6 +1,8 @@
 import { render } from '@testing-library/react-native'
 import React from 'react'
 
+import { canGoBack } from '__mocks__/@react-navigation/native'
+
 import { RetryBoundary } from '../RetryBoundary'
 
 describe('RetryBoundary component', () => {
@@ -10,5 +12,20 @@ describe('RetryBoundary component', () => {
       <RetryBoundary error={new Error('error')} resetErrorBoundary={resetErrorBoundary} />
     )
     expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('should have back arrow if possible', () => {
+    canGoBack.mockImplementation(() => true)
+    const { queryByTestId } = render(
+      <RetryBoundary error={new Error('error')} resetErrorBoundary={jest.fn()} />
+    )
+    expect(queryByTestId('backArrow')).toBeTruthy()
+  })
+  it('should not have back arrow if impossible', () => {
+    canGoBack.mockImplementation(() => false)
+    const { queryByTestId } = render(
+      <RetryBoundary error={new Error('error')} resetErrorBoundary={jest.fn()} />
+    )
+    expect(queryByTestId('backArrow')).toBeFalsy()
   })
 })
