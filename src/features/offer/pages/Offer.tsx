405aa20f8959afb692cb225d1578008c51318437
Justine Mignot <justinem@bam.tech>
2020-12-21 11:02:23 +0100

    :sparkles: (PC-5943) highlight links in withdrawal

--- src/features/offer/pages/Offer.tsx
+++ src/features/offer/pages/Offer.tsx
@@ -11,6 +11,7 @@ import { UseRouteType } from 'features/navigation/RootNavigator'
 import { LocationCaption } from 'features/offer/atoms/LocationCaption'
 import { _ } from 'libs/i18n'
 import { formatDatePeriod } from 'libs/parsers'
+import { highlightLinks } from 'libs/parsers/highlightLinks'
 import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
 
 import { useOffer } from '../api/useOffer'
@@ -110,7 +111,9 @@ const OfferComponent: FunctionComponent = () => {
 
         <Section visible={!!offerResponse.withdrawalDetails}>
           <AccordionItem title={_(t`Modalités de retrait`)}>
-            <Typo.Body>{offerResponse.withdrawalDetails}</Typo.Body>
+            <Typo.Body>
+              {offerResponse.withdrawalDetails && highlightLinks(offerResponse.withdrawalDetails)}
+            </Typo.Body>
           </AccordionItem>
         </Section>
 
