8985c5df9ae0ebf455acd86afb26c609d4493424
Antoine Garcia <antoine.garcia@polyconseil.fr>
2020-12-22 18:56:23 +0100

    Offer detail: 0-loading time

--- src/features/home/atoms/OfferTile.tsx
+++ src/features/home/atoms/OfferTile.tsx
@@ -1,10 +1,13 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { View, PixelRatio } from 'react-native'
+import { useQueryClient } from 'react-query'
 import styled from 'styled-components/native'
 
+import { OfferResponse } from 'api/gen'
 import { Layout } from 'features/home/contentful'
 import { UseNavigationType } from 'features/navigation/RootNavigator'
+import { OfferAdaptedResponse } from 'features/offer/api/useOffer'
 import { analytics } from 'libs/analytics'
 import { MARGIN_DP, LENGTH_M, LENGTH_L, RATIO_ALGOLIA } from 'ui/theme'
 import { BorderRadiusEnum } from 'ui/theme/grid'
@@ -14,6 +17,7 @@ import { OfferCaption } from './OfferCaption'
 
 interface OfferTileProps {
   category: string
+  description?: string
   distance?: string
   date?: string
   name?: string
@@ -26,15 +30,40 @@ interface OfferTileProps {
   moduleName: string
 }
 
+type PartialOffer = Pick<
+  OfferTileProps,
+  'category' | 'description' | 'thumbUrl' | 'isDuo' | 'name' | 'offerId'
+>
+
+const mergeOfferData = (offer: PartialOffer) => (
+  prevData: OfferAdaptedResponse | undefined
+): OfferAdaptedResponse => ({
+  fullAddress: null,
+  description: offer.description,
+  imageUrl: offer.thumbUrl,
+  isDuo: offer.isDuo || false,
+  name: offer.name || '',
+  isDigital: false,
+  id: offer.offerId,
+  stocks: [],
+  accessibility: {},
+  category: { label: offer.category } as OfferResponse['category'],
+  venue: { coordinates: {} } as OfferResponse['venue'],
+  ...(prevData || {}),
+})
+
 export const OfferTile = (props: OfferTileProps) => {
   const navigation = useNavigation<UseNavigationType>()
   const { layout = 'one-item-medium', moduleName, isBeneficiary, ...offer } = props
   const imageHeight = layout === 'two-items' ? LENGTH_M : LENGTH_L
   const imageWidth = imageHeight * RATIO_ALGOLIA
+  const queryClient = useQueryClient()
 
   function handlePressOffer() {
-    navigation.navigate('Offer', { id: offer.offerId })
+    // We pre-populate the query-cache with the data from algolia for a smooth transition
+    queryClient.setQueryData(['offer', offer.offerId], mergeOfferData(offer))
     analytics.logConsultOffer(offer.offerId, moduleName)
+    navigation.navigate('Offer', { id: offer.offerId })
   }
 
   return (
