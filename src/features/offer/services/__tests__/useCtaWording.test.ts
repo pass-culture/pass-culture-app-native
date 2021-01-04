25ff55a5c335f554bafb86b7b234cacaff64bc5f
Justine Mignot <justinem@bam.tech>
2021-01-04 16:02:35 +0100

    :sparkles: (PC-6002) isOfferExpired function

--- src/features/offer/services/__tests__/useCtaWording.test.ts
+++ src/features/offer/services/__tests__/useCtaWording.test.ts
@@ -1,10 +1,13 @@
 import { renderHook } from '@testing-library/react-hooks'
+import mockdate from 'mockdate'
 
 import { CategoryType } from 'api/gen'
 
 import { useAuthContext } from '../../../auth/AuthContext'
 import { useUserProfileInfo } from '../../../home/api'
-import { useCtaWording } from '../useCtaWording'
+import { useCtaWording, isOfferExpired } from '../useCtaWording'
+
+mockdate.set(new Date('2021-01-04T00:00:00Z'))
 
 jest.mock('features/auth/AuthContext')
 jest.mock('features/home/api')
@@ -44,4 +47,69 @@ describe('useCtaWording', () => {
       unmount()
     }
   )
+})
+
+describe.only('isOfferExpired', () => {
+  const expiredOffer = {
+    stocks: [
+      {
+        id: 118929,
+        beginningDatetime: new Date('2021-01-01T13:30:00'),
+        bookingLimitDatetime: new Date('2021-01-03T13:30:00'),
+        price: 5,
+        isBookable: true,
+      },
+      {
+        id: 118928,
+        beginningDatetime: new Date('2021-01-02T18:00:00'),
+        bookingLimitDatetime: new Date('2021-01-03T13:30:00'),
+        price: 5,
+        isBookable: true,
+      },
+    ],
+  }
+  const notExpiredOffer1 = {
+    stocks: [
+      {
+        id: 118929,
+        beginningDatetime: new Date('2021-01-01T13:30:00'),
+        bookingLimitDatetime: new Date('2021-01-05T13:30:00'),
+        price: 5,
+        isBookable: true,
+      },
+      {
+        id: 118928,
+        beginningDatetime: new Date('2021-01-02T18:00:00'),
+        bookingLimitDatetime: new Date('2021-01-03T13:30:00'),
+        price: 5,
+        isBookable: true,
+      },
+    ],
+  }
+  const notExpiredOffer2 = {
+    stocks: [
+      {
+        id: 118929,
+        beginningDatetime: new Date('2021-01-01T13:30:00'),
+        bookingLimitDatetime: new Date('2021-01-05T13:30:00'),
+        price: 5,
+        isBookable: true,
+      },
+      {
+        id: 118928,
+        beginningDatetime: new Date('2021-01-02T18:00:00'),
+        price: 5,
+        isBookable: true,
+      },
+    ],
+  }
+
+  it.each`
+    offer               | isExpired
+    ${expiredOffer}     | ${true}
+    ${notExpiredOffer1} | ${false}
+    ${notExpiredOffer2} | ${false}
+  `('should check offer expiration correctlty', ({ offer, isExpired }) => {
+    expect(isOfferExpired(offer)).toEqual(isExpired)
+  })
 })
