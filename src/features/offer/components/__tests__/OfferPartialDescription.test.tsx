b3b0cfcbbee64c72d2acc29b5470628e184c99ae
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-07 14:22:27 +0100

    (PC-6100) Test only show SeeMore offer when there's something to show

--- src/features/offer/components/__tests__/OfferPartialDescription.test.tsx
+++ src/features/offer/components/__tests__/OfferPartialDescription.test.tsx
@@ -1,5 +1,9 @@
 import { render } from '@testing-library/react-native'
 import React from 'react'
+import { QueryClient } from 'react-query'
+
+import { offerResponseSnap } from 'features/offer/api/snaps/offerResponseSnap'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 
 import { OfferPartialDescription } from '../OfferPartialDescription'
 
@@ -7,22 +11,50 @@ const description =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua: https://google.com'
 
 describe('OfferPartialDescription', () => {
-  it('centers CTA when provided description is empty', () => {
-    const { getByTestId } = render(<OfferPartialDescription id={123} description={''} />)
+  it('centers CTA when provided description is empty', async () => {
+    const { getByTestId } = await renderOfferDescription('')
     const offerSeeMoreContainer = getByTestId('offerSeeMoreContainer')
     expect(offerSeeMoreContainer.props.style[0].alignSelf).toBe('center')
   })
-  it('places CTA on flex-end when provided a description', () => {
-    const { getByTestId } = render(<OfferPartialDescription id={123} description={description} />)
+  it('places CTA on flex-end when provided a description', async () => {
+    const { getByTestId } = await renderOfferDescription(description)
     const offerSeeMoreContainer = getByTestId('offerSeeMoreContainer')
     expect(offerSeeMoreContainer.props.style[0].alignSelf).toBe('flex-end')
   })
-  it('renders externalLinks if http(s) url are present in the description', () => {
-    const { queryByTestId } = render(<OfferPartialDescription id={123} description={description} />)
+  it('renders externalLinks if http(s) url are present in the description', async () => {
+    const { queryByTestId } = await renderOfferDescription(description)
     expect(queryByTestId('externalSiteIcon')).toBeTruthy()
   })
-  it("shouldn't render an empty line and a spacer when description is undefined", () => {
-    const { queryByTestId } = render(<OfferPartialDescription id={123} description={undefined} />)
+  it("shouldn't render an empty line and a spacer when description is undefined", async () => {
+    const { queryByTestId } = await renderOfferDescription(undefined)
     expect(queryByTestId('offerPartialDescriptionBody')).toBeFalsy()
   })
+  it("shouldn't render the SeeMore button if no content is on description page", async () => {
+    const { queryByTestId } = render(
+      reactQueryProviderHOC(
+        <OfferPartialDescription id={offerId} description={undefined} />,
+        (queryClient: QueryClient) => {
+          queryClient.setQueryData(['offer', offerId], {
+            ...offerResponseSnap,
+            image: {},
+            extraData: {},
+          })
+        }
+      )
+    )
+    expect(queryByTestId('offerSeeMoreContainer')).toBeFalsy()
+  })
 })
+
+const offerId = 123
+const renderOfferDescription = async (description?: string) => {
+  const setup = (queryClient: QueryClient) => {
+    queryClient.removeQueries()
+    queryClient.setQueryData(['offer', offerId], offerResponseSnap)
+  }
+
+  const wrapper = render(
+    reactQueryProviderHOC(<OfferPartialDescription id={offerId} description={description} />, setup)
+  )
+  return wrapper
+}
