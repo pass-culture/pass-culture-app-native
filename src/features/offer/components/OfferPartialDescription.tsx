92165e81cf610a92356b3f19ff0089292b7b0b83
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-07 14:10:53 +0100

    (PC-6100) Only show SeeMore offer when there's something to show

--- src/features/offer/components/OfferPartialDescription.tsx
+++ src/features/offer/components/OfferPartialDescription.tsx
@@ -4,28 +4,38 @@ import styled from 'styled-components/native'
 import { highlightLinks } from 'libs/parsers/highlightLinks'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 
+import { useOffer } from '../api/useOffer'
 import { OfferSeeMore } from '../atoms/OfferSeeMore'
+import { getContentFromOffer } from '../pages/OfferDescription'
 
 interface Props {
   id: number
   description?: string
 }
 
-export const OfferPartialDescription: React.FC<Props> = ({ id, description = '' }) => (
-  <DescriptionContainer>
-    {!!description && (
-      <React.Fragment>
-        <TypoDescription testID="offerPartialDescriptionBody" numberOfLines={8}>
-          {highlightLinks(description)}
-        </TypoDescription>
-        <Spacer.Column numberOfSpaces={2} />
-      </React.Fragment>
-    )}
-    <OfferSeeMoreContainer testID="offerSeeMoreContainer" description={description}>
-      <OfferSeeMore id={id} longWording={!description} />
-    </OfferSeeMoreContainer>
-  </DescriptionContainer>
-)
+export const OfferPartialDescription: React.FC<Props> = ({ id, description = '' }) => {
+  const { data: offerResponse } = useOffer({ offerId: id })
+  const { extraData = {}, image } = offerResponse || {}
+  const contentOfferDescription = getContentFromOffer(extraData, description, image?.credit)
+
+  return (
+    <DescriptionContainer>
+      {!!description && (
+        <React.Fragment>
+          <TypoDescription testID="offerPartialDescriptionBody" numberOfLines={8}>
+            {highlightLinks(description)}
+          </TypoDescription>
+          <Spacer.Column numberOfSpaces={2} />
+        </React.Fragment>
+      )}
+      {contentOfferDescription.length > 0 && (
+        <OfferSeeMoreContainer testID="offerSeeMoreContainer" description={description}>
+          <OfferSeeMore id={id} longWording={!description} />
+        </OfferSeeMoreContainer>
+      )}
+    </DescriptionContainer>
+  )
+}
 
 const OfferSeeMoreContainer = styled.View<{ description: string }>(({ description }) => ({
   alignSelf: description ? 'flex-end' : 'center',
