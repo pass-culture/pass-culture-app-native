4e8f25224265e56e304e2a6239a21be969cf4eb0
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-08 12:31:46 +0100

    Add number of results

--- src/features/search/pages/Search.tsx
+++ src/features/search/pages/Search.tsx
@@ -2,7 +2,7 @@ import { t } from '@lingui/macro'
 import algoliasearch from 'algoliasearch'
 import React from 'react'
 import styled from 'styled-components/native'
-import { InstantSearch } from 'react-instantsearch-native'
+import { InstantSearch, connectStats } from 'react-instantsearch-native'
 
 import { _ } from 'libs/i18n'
 import { env } from 'libs/environment'
@@ -15,9 +15,14 @@ export const Search: React.FC = () => (
       indexName={env.ALGOLIA_INDEX_NAME}>
       <Spacer.Flex />
       <Typo.Hero>{_(t`Search`)}</Typo.Hero>
+      <NumberOfResults />
       <Spacer.Flex />
     </InstantSearch>
   </Container>
 )
 
 const Container = styled.View({ flex: 1, alignItems: 'center' })
+
+const NumberOfResults = connectStats(({ nbHits }) => (
+  <Typo.Caption>{nbHits.toLocaleString()} results !</Typo.Caption>
+))
