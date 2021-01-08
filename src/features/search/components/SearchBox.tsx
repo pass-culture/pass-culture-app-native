2d7d22a410b32ec30a6cac96a1e105fe36dd4551
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-08 12:59:34 +0100

    Add SearchBox component

---
+++ src/features/search/components/SearchBox.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { connectSearchBox } from 'react-instantsearch-native'
+import styled from 'styled-components/native'
+
+import { TextInput } from 'ui/components/inputs/TextInput'
+import { getSpacing } from 'ui/theme'
+
+interface Props {
+  onChangeText: (text: string) => void
+  value: string
+}
+
+export const SearchBoxComponent: React.FC<Props> = ({ onChangeText, value }) => (
+  <StyledInput>
+    <TextInput
+      value={value}
+      onChangeText={onChangeText}
+      placeholder="Chercher par titre, artiste, ..."
+      autoFocus={true}
+    />
+  </StyledInput>
+)
+
+const StyledInput = styled.View({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  width: '100%',
+  maxWidth: getSpacing(125),
+  margin: getSpacing(4),
+})
+
+export const SearchBox = connectSearchBox(({ refine, currentRefinement }) => (
+  <SearchBoxComponent onChangeText={(text) => refine(text)} value={currentRefinement} />
+))
--- src/features/search/pages/Search.tsx
+++ src/features/search/pages/Search.tsx
@@ -1,27 +1,26 @@
-import { t } from '@lingui/macro'
 import algoliasearch from 'algoliasearch'
 import React from 'react'
-import styled from 'styled-components/native'
 import { InstantSearch, connectStats } from 'react-instantsearch-native'
+import styled from 'styled-components/native'
 
+import { SearchBox } from 'features/search/components/SearchBox'
 import { _ } from 'libs/i18n'
 import { env } from 'libs/environment'
-import { Spacer, Typo } from 'ui/theme'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
 
 export const Search: React.FC = () => (
   <Container>
     <InstantSearch
       searchClient={algoliasearch(env.ALGOLIA_APPLICATION_ID, env.ALGOLIA_SEARCH_API_KEY)}
       indexName={env.ALGOLIA_INDEX_NAME}>
-      <Spacer.Flex />
-      <Typo.Hero>{_(t`Search`)}</Typo.Hero>
+      <Spacer.TopScreen />
+      <SearchBox />
       <NumberOfResults />
-      <Spacer.Flex />
     </InstantSearch>
   </Container>
 )
 
-const Container = styled.View({ flex: 1, alignItems: 'center' })
+const Container = styled.View({ flex: 1, alignItems: 'center', marginHorizontal: getSpacing(6) })
 
 const NumberOfResults = connectStats(({ nbHits }) => (
   <Typo.Caption>{nbHits.toLocaleString()} results !</Typo.Caption>
