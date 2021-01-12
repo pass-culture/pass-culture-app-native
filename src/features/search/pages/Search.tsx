37ff3bb1ef8aa314f49e542f1785a6a190268744
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-12 15:16:20 +0100

    (PC-4837) Wrap Search page with InstantSearch

--- src/features/search/pages/Search.tsx
+++ src/features/search/pages/Search.tsx
@@ -1,15 +1,21 @@
+import algoliasearch from 'algoliasearch'
 import React from 'react'
+import { InstantSearch } from 'react-instantsearch-native'
 import styled from 'styled-components/native'
 
 import { InfiniteHits } from 'features/search/components/InfiniteHits'
 import { SearchHeader } from 'features/search/components/SearchHeader'
-import { _ } from 'libs/i18n'
+import { env } from 'libs/environment'
+
+const searchClient = algoliasearch(env.ALGOLIA_APPLICATION_ID, env.ALGOLIA_SEARCH_API_KEY)
 
 export const Search: React.FC = () => (
-  <Container>
-    <SearchHeader />
-    <InfiniteHits />
-  </Container>
+  <InstantSearch searchClient={searchClient} indexName={env.ALGOLIA_INDEX_NAME}>
+    <Container>
+      <SearchHeader />
+      <InfiniteHits />
+    </Container>
+  </InstantSearch>
 )
 
 const Container = styled.View({ flex: 1, alignItems: 'center' })
