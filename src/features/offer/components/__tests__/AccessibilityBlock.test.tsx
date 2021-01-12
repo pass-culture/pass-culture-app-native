25933582b03eef2cec3d764f06d27f9d242ee720
Emma Jamin <emmajamin@orange.fr>
2021-01-12 11:55:09 +0100

    feat(pc-6132): render accessibilityAtom only when available

--- src/features/offer/components/__tests__/AccessibilityBlock.test.tsx
+++ src/features/offer/components/__tests__/AccessibilityBlock.test.tsx
@@ -18,4 +18,16 @@ describe('AccessibilityBlock', () => {
     expect(queryByText('Handicap mental')).toBeTruthy()
     expect(queryByText('Handicap auditif')).toBeTruthy()
   })
+  it('renders only available handicap information', () => {
+    const { queryByText } = render(<AccessibilityBlock
+      audioDisability={undefined}
+      motorDisability={false}
+      mentalDisability={true}
+      visualDisability={false}
+    />)
+    expect(queryByText('Handicap visuel')).toBeTruthy()
+    expect(queryByText('Handicap moteur')).toBeTruthy()
+    expect(queryByText('Handicap mental')).toBeTruthy()
+    expect(queryByText('Handicap auditif')).toBeNull()
+  })
 })
