36524a994cb85fc6d3805c6d7757166ff4d39b05
BenStirrup <benjamin.stirrup@gmail.com>
2021-01-13 13:48:27 +0100

    (PC-5854) Add test to verify that all analytics event respect firebase nalming rules

---
+++ src/libs/analytics.test.ts
@@ -0,0 +1,42 @@
+export {}
+
+jest.mock('@react-native-firebase/analytics')
+
+const { AnalyticsEvent, validateAnalyticsEvent } = jest.requireActual('./analytics')
+
+describe('analytics', () => {
+  describe('AnalyticsEvent', () => {
+    it('actual members should respect Firebase naming rules', () => {
+      for (const member of Object.values(AnalyticsEvent)) {
+        const isValidMember = validateAnalyticsEvent(member as string)
+        if (!isValidMember) {
+          fail(`${member} is not valid`)
+        }
+      }
+    })
+    it('fake member should fail the test when it is too long', () => {
+      expect(validateAnalyticsEvent('a'.repeat(39))).toBeTruthy()
+      expect(validateAnalyticsEvent('a'.repeat(40))).toBeTruthy()
+      expect(validateAnalyticsEvent('a'.repeat(41))).toBeFalsy()
+    })
+    it('fake member should fail the test when its first letter is not alphabetic', () => {
+      expect(validateAnalyticsEvent('!test')).toBeFalsy()
+      expect(validateAnalyticsEvent('1test')).toBeFalsy()
+      expect(validateAnalyticsEvent('_test')).toBeFalsy()
+    })
+    it('fake member should fail the test when it contains a non alpha numeric char', () => {
+      expect(validateAnalyticsEvent('Event-Testé')).toBeFalsy()
+    })
+    it('fake member should fail the test when it includes a "-"', () => {
+      expect(validateAnalyticsEvent('Event-Test')).toBeFalsy()
+    })
+    it('fake member should fail the test when it starts a reserved prefix', () => {
+      expect(validateAnalyticsEvent('firebase_test')).toBeFalsy()
+      expect(validateAnalyticsEvent('google_test')).toBeFalsy()
+      expect(validateAnalyticsEvent('ga_test')).toBeFalsy()
+      expect(validateAnalyticsEvent('test_firebase_test')).toBeTruthy()
+      expect(validateAnalyticsEvent('test_googletest')).toBeTruthy()
+      expect(validateAnalyticsEvent('test_ga_test')).toBeTruthy()
+    })
+  })
+})
