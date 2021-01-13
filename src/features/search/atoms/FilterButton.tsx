6944b19a145f5f6ad330e98fb25cea9de901384a
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-13 15:57:17 +0100

    (PC-6097) Linear gradient on filter button

--- src/features/search/atoms/FilterButton.tsx
+++ src/features/search/atoms/FilterButton.tsx
@@ -1,11 +1,12 @@
 import { t } from '@lingui/macro'
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
+import LinearGradient from 'react-native-linear-gradient'
 import styled from 'styled-components/native'
 
 import { _ } from 'libs/i18n'
 import { Filter } from 'ui/svg/icons/Filter'
-import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
+import { ColorsEnum, UniqueColors, getSpacing, Spacer, Typo } from 'ui/theme'
 import { ACTIVE_OPACITY } from 'ui/theme/colors'
 import { BorderRadiusEnum } from 'ui/theme/grid'
 
@@ -13,22 +14,25 @@ export const FilterButton: React.FC = () => {
   const { navigate } = useNavigation()
   return (
     <Container onPress={() => navigate('SearchFilter')}>
-      <Filter color={ColorsEnum.WHITE} />
-      <Spacer.Row numberOfSpaces={1} />
-      <Title>{_(t`Filtrer`)}</Title>
-      <Spacer.Row numberOfSpaces={2} />
+      <StyledLinearGradient colors={[ColorsEnum.PRIMARY, UniqueColors.FILTER_BUTTON]} angle={106}>
+        <Filter color={ColorsEnum.WHITE} />
+        <Spacer.Row numberOfSpaces={1} />
+        <Title>{_(t`Filtrer`)}</Title>
+        <Spacer.Row numberOfSpaces={2} />
+      </StyledLinearGradient>
     </Container>
   )
 }
 
 const Container = styled.TouchableOpacity.attrs(() => ({
   activeOpacity: ACTIVE_OPACITY,
-}))({
+}))({ overflow: 'hidden' })
+
+const StyledLinearGradient = styled(LinearGradient)({
+  borderRadius: BorderRadiusEnum.BUTTON,
   alignItems: 'center',
   flexDirection: 'row',
-  borderRadius: BorderRadiusEnum.BUTTON,
   overflow: 'hidden',
-  backgroundColor: ColorsEnum.PRIMARY,
   paddingHorizontal: getSpacing(3),
   height: getSpacing(10),
 })
