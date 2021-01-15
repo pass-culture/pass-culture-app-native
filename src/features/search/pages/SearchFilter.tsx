d4fad3544d800d2375fb64106f5088a41d747a51
Justine Mignot <justinem@bam.tech>
2021-01-15 11:17:40 +0100

    :recycle: (PC-6250) move rightComponent width recuperation logic

--- src/features/search/pages/SearchFilter.tsx
+++ src/features/search/pages/SearchFilter.tsx
@@ -1,5 +1,5 @@
 import { t } from '@lingui/macro'
-import React, { useState } from 'react'
+import React from 'react'
 import { LayoutChangeEvent } from 'react-native'
 import styled from 'styled-components/native'
 
@@ -7,26 +7,18 @@ import { _ } from 'libs/i18n'
 import { PageHeader } from 'ui/components/headers/PageHeader'
 import { Spacer, Typo, ColorsEnum } from 'ui/theme'
 
+const rightButton = (onLayout: (event: LayoutChangeEvent) => void): JSX.Element => {
+  return (
+    <Typo.ButtonText onLayout={onLayout} color={ColorsEnum.WHITE}>
+      {_(t`Réinitialiser`)}
+    </Typo.ButtonText>
+  )
+}
+
 export const SearchFilter: React.FC = () => {
-  const [rightComponentWidth, setRightComponentWidth] = useState(0)
-
-  const onRightComponentLayout = (event: LayoutChangeEvent) => {
-    if (rightComponentWidth !== 0) return
-    const { width } = event.nativeEvent.layout
-    setRightComponentWidth(width)
-  }
-
   return (
     <Container>
-      <PageHeader
-        title={_(t`Filtrer`)}
-        rightComponentWidth={rightComponentWidth}
-        rightComponent={
-          <Typo.ButtonText color={ColorsEnum.WHITE} onLayout={onRightComponentLayout}>
-            {_(t`Réinitialiser`)}
-          </Typo.ButtonText>
-        }
-      />
+      <PageHeader title={_(t`Filtrer`)} rightComponent={rightButton} />
       <Spacer.Flex />
       <Typo.Hero>{_(t`SearchFilter`)}</Typo.Hero>
       <Spacer.Flex />
