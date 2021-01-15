2a3dabdb6ecbc37e12a8cd44a54430ad2d20463d
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-15 16:07:47 +0100

    (PC-6240) Move keyboard logic in a separate hook

--- src/features/search/pages/Search.tsx
+++ src/features/search/pages/Search.tsx
@@ -1,8 +1,6 @@
 import algoliasearch from 'algoliasearch'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Configure, InstantSearch } from 'react-instantsearch-native'
-import { Platform } from 'react-native'
-import AndroidKeyboardAdjust from 'react-native-android-keyboard-adjust'
 import styled from 'styled-components/native'
 
 import { TAB_BAR_COMP_HEIGHT } from 'features/navigation/TabBar/TabBarComponent'
@@ -10,17 +8,13 @@ import { FilterButton } from 'features/search/atoms/FilterButton'
 import { InfiniteHits } from 'features/search/components/InfiniteHits'
 import { SearchHeader } from 'features/search/components/SearchHeader'
 import { env } from 'libs/environment'
+import { useKeyboardAdjust } from 'ui/components/keyboard/useKeyboardAdjust'
 import { getSpacing, Spacer } from 'ui/theme'
 
 const searchClient = algoliasearch(env.ALGOLIA_APPLICATION_ID, env.ALGOLIA_SEARCH_API_KEY)
 
 export const Search: React.FC = () => {
-  useEffect(() => {
-    // This prevents the navbar and the filter button to 'jump' above the keyboard
-    // when we open the keyboard. Thus Android and iOS have the same behaviour
-    if (Platform.OS === 'android') AndroidKeyboardAdjust.setAdjustNothing()
-    return () => Platform.OS === 'android' && AndroidKeyboardAdjust.setAdjustResize()
-  }, [])
+  useKeyboardAdjust()
 
   return (
     <InstantSearch searchClient={searchClient} indexName={env.ALGOLIA_INDEX_NAME}>
