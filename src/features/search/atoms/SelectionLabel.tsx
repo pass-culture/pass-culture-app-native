7a4c3e0ced5c8635c3edae428a8a74ec88aece57
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-18 16:21:30 +0100

    (PC-6324) Selected labels become red

--- src/features/search/atoms/SelectionLabel.tsx
+++ src/features/search/atoms/SelectionLabel.tsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/native'
 
-import { ColorsEnum, getSpacing, Typo } from 'ui/theme'
+import { Validate } from 'ui/svg/icons/Validate'
+import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
 
-export const SelectionLabel: React.FC<{ label: string }> = ({ label }) => (
-  <TouchableOpacity>
-    <Label>{label}</Label>
-  </TouchableOpacity>
-)
+export const SelectionLabel: React.FC<{ label: string }> = ({ label }) => {
+  const [selected, setSelected] = useState<boolean>(false)
+  return (
+    <TouchableOpacity
+      selected={selected}
+      onPress={() => setSelected((prevSelected) => !prevSelected)}>
+      {selected ? (
+        <IconContainer>
+          <Validate color={ColorsEnum.WHITE} size={getSpacing(6)} />
+        </IconContainer>
+      ) : (
+        <Spacer.Row numberOfSpaces={5} />
+      )}
+      <Label color={selected ? ColorsEnum.WHITE : ColorsEnum.BLACK}>{label}</Label>
+      <Spacer.Row numberOfSpaces={selected ? 2 : 5} />
+    </TouchableOpacity>
+  )
+}
 
-const TouchableOpacity = styled.TouchableOpacity({
-  borderRadius: getSpacing(6),
+const IconContainer = styled.View({
+  width: getSpacing(8),
+  paddingHorizontal: getSpacing(1),
+})
+const TouchableOpacity = styled.TouchableOpacity<{ selected: boolean }>(({ selected }) => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  borderRadius: getSpacing(10),
   borderWidth: 2,
-  borderColor: ColorsEnum.GREY_MEDIUM,
+  borderColor: selected ? ColorsEnum.PRIMARY : ColorsEnum.GREY_MEDIUM,
   marginBottom: getSpacing(3),
   marginRight: getSpacing(3),
-})
+  backgroundColor: selected ? ColorsEnum.PRIMARY : ColorsEnum.WHITE,
+}))
 const Label = styled(Typo.ButtonText)({
   paddingVertical: getSpacing(2.5),
-  paddingHorizontal: getSpacing(4),
 })
