88669232e8e7e8e288f62940a64638c17ccca81c
BenStirrup <benjamin.stirrup@gmail.com>
2021-01-19 15:26:31 +0100

    (PC-6343) Make autoFocus work with inputs on Android

---
+++ src/ui/components/inputs/BaseTextInput.test.tsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react-native'
+import React from 'react'
+import { TextInput as RNTextInput } from 'react-native'
+
+import { BaseTextInput } from './BaseTextInput'
+
+describe('<BaseTextInput />', () => {
+  it('should render correctly', () => {
+    const renderAPI = render(<BaseTextInput placeholder={'placeholder'} value={'value'} />)
+    expect(renderAPI).toMatchSnapshot()
+  })
+
+  it('should render ref correctly', () => {
+    const ref = React.createRef<RNTextInput>()
+    render(<BaseTextInput placeholder="placeholder" value={'value'} ref={ref} />)
+
+    expect(ref.current).toBeTruthy()
+  })
+
+  it('should replicate autoFocus behaviour by calling focus() on mount when prop autoFocus is true', () => {
+    const ref = React.createRef<RNTextInput>()
+    const renderAPI = render(
+      <BaseTextInput autoFocus={true} placeholder={'placeholder'} value={'value'} ref={ref} />
+    )
+
+    const nativeTextInput = renderAPI.getByPlaceholderText('placeholder')
+
+    expect(nativeTextInput.props.autoFocus).toBe(undefined)
+    expect(ref.current?.focus).toBeCalledTimes(1)
+  })
+})
