23eeb0ea2cfbaa479f50a85865389150fce8ee67
Emma Jamin <emmajamin@orange.fr>
2021-01-19 15:12:28 +0100

    (PC-6261) Create new type of section

--- src/features/search/atoms/Sections.tsx
+++ src/features/search/atoms/Sections.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
-import { getSpacing, Typo } from 'ui/theme'
+import { ColorsEnum, getSpacing, Typo } from 'ui/theme'
 
 export const CenteredSection: React.FC<{ title: string; children: Element }> = ({
   title,
@@ -15,3 +15,32 @@ export const CenteredSection: React.FC<{ title: string; children: Element }> = (
 
 const Container = styled.View({ marginHorizontal: getSpacing(6) })
 const Center = styled.View({ alignItems: 'center', marginTop: getSpacing(4) })
+
+export const InlineSection: React.FC<{ title: string; subtitle?: string; children: Element }> = ({
+  title,
+  subtitle,
+  children,
+}) => (
+  <SectionContainer>
+    <InlineSectionTitleContainer>
+      <StyledTitle numberOfLines={2}>{title}</StyledTitle>
+      {children}
+    </InlineSectionTitleContainer>
+    <StyledSubtitle>{subtitle}</StyledSubtitle>
+  </SectionContainer>
+)
+
+const SectionContainer = styled.View({
+  marginTop: getSpacing(4),
+  marginHorizontal: getSpacing(6),
+})
+
+const InlineSectionTitleContainer = styled.View({
+  flexDirection: 'row',
+  alignItems: 'center',
+})
+
+const StyledTitle = styled(Typo.Title4)({ flex: 1, marginRight: getSpacing(7) })
+const StyledSubtitle = styled(Typo.Caption).attrs({
+  color: ColorsEnum.GREY_DARK,
+})({ marginTop: getSpacing(2) })
