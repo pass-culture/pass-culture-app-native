0fd752a39c1026853c898d6bdd246f1817d2ebd8
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-20 15:01:13 +0100

    (PC-6368) Make LeftIcon mandatory for SearchBox component

--- src/ui/components/inputs/SearchInput.tsx
+++ src/ui/components/inputs/SearchInput.tsx
@@ -1,11 +1,8 @@
-import React, { forwardRef, useState } from 'react'
+import React, { forwardRef } from 'react'
 import { TextInput as RNTextInput } from 'react-native'
 
-import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
 import { Invalidate } from 'ui/svg/icons/Invalidate'
-import { ColorsEnum } from 'ui/theme'
-
-import { Spacer } from '../spacer/Spacer'
+import { ColorsEnum, Spacer } from 'ui/theme'
 
 import { BaseTextInput } from './BaseTextInput'
 import { InputContainer } from './InputContainer'
@@ -19,30 +16,13 @@ const WithRefSearchInput: React.ForwardRefRenderFunction<RNTextInput, SearchInpu
   const customProps = getCustomSearchInputProps(props)
   const { LeftIcon, RightIcon = () => <Invalidate size={24} /> } = customProps
 
-  const [isFocus, setIsFocus] = useState(false)
-
-  function onFocus() {
-    setIsFocus(true)
-  }
-
-  function onBlur() {
-    setIsFocus(false)
-  }
-
   return (
-    <InputContainer isFocus={isFocus} inputHeight={props.inputHeight}>
-      {!!LeftIcon && (
-        <React.Fragment>
-          {/* TODO: show left icon depending on nb of results. This is a temporary way to show both icons */}
-          {isFocus ? <LeftIcon /> : <ArrowPrevious />}
-          <Spacer.Row numberOfSpaces={2} />
-        </React.Fragment>
-      )}
+    <InputContainer inputHeight={props.inputHeight}>
+      <LeftIcon />
+      <Spacer.Row numberOfSpaces={2} />
       <BaseTextInput
         {...nativeProps}
         ref={forwardedRef}
-        onFocus={onFocus}
-        onBlur={onBlur}
         autoCorrect={false}
         returnKeyType="search"
         selectionColor={ColorsEnum.GREY_DARK}
