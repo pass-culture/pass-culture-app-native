b146e5b76674fae05486bd7bd9d8b2f465fd65af
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-22 19:28:24 +0100

    test sections/Price

--- src/features/search/sections/tests/Price.test.tsx
+++ src/features/search/sections/tests/Price.test.tsx
@@ -1,21 +1,40 @@
 import { render } from '@testing-library/react-native'
 import React from 'react'
+import { ReactTestInstance } from 'react-test-renderer'
 
 import { initialSearchState } from 'features/search/pages/reducer'
 
 import { Price } from '../Price'
 
-const mockSearchState = initialSearchState
+let mockSearchState = initialSearchState
+const mockDispatch = jest.fn()
 jest.mock('features/search/pages/SearchWrapper', () => ({
   useSearch: () => ({
     searchState: mockSearchState,
-    dispatch: jest.fn(),
+    dispatch: mockDispatch,
   }),
 }))
 
 describe('Price component', () => {
   it('should render initial price range correctly', () => {
-    const { queryByText } = render(<Price />)
-    expect(queryByText('0 € - 300 €')).toBeTruthy()
+    expect(render(<Price />).queryByText('0 € - 300 €')).toBeTruthy()
+    mockSearchState = { ...initialSearchState, priceRange: [20, 200] }
+    expect(render(<Price />).queryByText('20 € - 200 €')).toBeTruthy()
+  })
+  it('should dispatch PRICE_RANGE onPress', () => {
+    const { getByTestId } = render(<Price />)
+    const slider = getByTestId('slider').children[0] as ReactTestInstance
+    slider.props.onValuesChange([20, 30])
+    expect(mockDispatch).toHaveBeenCalledWith({ type: 'PRICE_RANGE', payload: [20, 30] })
+  })
+
+  it('should have the indicator of the filters in the title', () => {
+    mockSearchState = initialSearchState
+    expect(render(<Price />).queryByText('Prix')).toBeTruthy()
+    expect(render(<Price />).queryByText('Prix\xa0(1)')).toBeFalsy()
+    mockSearchState = { ...initialSearchState, priceRange: [0, 200] }
+    expect(render(<Price />).queryByText('Prix\xa0(1)')).toBeTruthy()
+    mockSearchState = { ...initialSearchState, priceRange: [20, 200] }
+    expect(render(<Price />).queryByText('Prix\xa0(1)')).toBeTruthy()
   })
 })
