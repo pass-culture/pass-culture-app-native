3aaa4d84082586aeb28ac9dfcf1801d3de0db2eb
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-22 11:57:35 +0100

    (PC-6262) Conditionally show DateOffer section

--- src/features/search/pages/SearchFilter.tsx
+++ src/features/search/pages/SearchFilter.tsx
@@ -4,80 +4,88 @@ import styled from 'styled-components/native'
 
 import { ShowResults, ReinitializeFilters } from 'features/search/atoms/Buttons'
 import { LocationSection, SectionWithSwitch } from 'features/search/components'
+import { useSearch } from 'features/search/pages/SearchWrapper'
 import Section from 'features/search/sections'
 import { _ } from 'libs/i18n'
 import { PageHeader } from 'ui/components/headers/PageHeader'
 import { ColorsEnum, getSpacing, Spacer } from 'ui/theme'
 
-export const SearchFilter: React.FC = () => (
-  <React.Fragment>
+export const SearchFilter: React.FC = () => {
+  const { searchState } = useSearch()
+  return (
     <React.Fragment>
-      <Container>
-        <Spacer.TopScreen />
-        <Spacer.Column numberOfSpaces={16} />
+      <React.Fragment>
+        <Container>
+          <Spacer.TopScreen />
+          <Spacer.Column numberOfSpaces={16} />
 
-        {/* Localisation */}
-        <LocationSection />
-        <Separator marginVertical={getSpacing(6)} />
+          {/* Localisation */}
+          <LocationSection />
+          <Separator marginVertical={getSpacing(6)} />
 
-        {/* Rayon */}
-        <Section.Radius />
-        <Spacer.Column numberOfSpaces={6} />
-        <Separator />
+          {/* Rayon */}
+          <Section.Radius />
+          <Spacer.Column numberOfSpaces={6} />
+          <Separator />
 
-        {/* Catégories */}
-        <Section.Category />
-        <Spacer.Column numberOfSpaces={2} />
-        <Separator />
+          {/* Catégories */}
+          <Section.Category />
+          <Spacer.Column numberOfSpaces={2} />
+          <Separator />
 
-        {/* Type d'offre */}
-        <Section.OfferType />
-        <Spacer.Column numberOfSpaces={2} />
-        <Separator />
-        <Spacer.Column numberOfSpaces={6} />
+          {/* Type d'offre */}
+          <Section.OfferType />
+          <Spacer.Column numberOfSpaces={2} />
+          <Separator />
+          <Spacer.Column numberOfSpaces={6} />
 
-        {/* Prix */}
-        <Section.Price />
-        <Separator marginVertical={getSpacing(6)} />
+          {/* Prix */}
+          <Section.Price />
+          <Separator marginVertical={getSpacing(6)} />
 
-        {/* Uniquement les offres gratuites */}
-        <Section.FreeOffer />
-        <Separator marginVertical={getSpacing(6)} />
+          {/* Uniquement les offres gratuites */}
+          <Section.FreeOffer />
+          <Separator marginVertical={getSpacing(6)} />
 
-        {/* Uniquement les offres duo */}
-        <Section.DuoOffer />
-        <Separator marginVertical={getSpacing(6)} />
+          {/* Uniquement les offres duo */}
+          <Section.DuoOffer />
+          <Separator marginVertical={getSpacing(6)} />
 
-        {/* Uniquement les nouveautés */}
-        <Section.NewOffer />
-        <Separator marginVertical={getSpacing(6)} />
+          {/* Uniquement les nouveautés */}
+          <Section.NewOffer />
+          <Separator marginVertical={getSpacing(6)} />
 
-        {/* Date */}
-        <Section.Date />
-        <Separator marginVertical={getSpacing(6)} />
+          {/* Date */}
+          <Section.Date />
+          <Separator marginVertical={getSpacing(6)} />
 
-        {/* Date de l'offre */}
-        <Section.OfferDate />
-        <Separator marginVertical={getSpacing(6)} />
+          {/* Date de l'offre */}
+          {!!searchState.date && (
+            <React.Fragment>
+              <Section.OfferDate />
+              <Separator marginVertical={getSpacing(6)} />
+            </React.Fragment>
+          )}
 
-        {/* Heure */}
-        <SectionWithSwitch
-          title={_(t`Heure`)}
-          subtitle={_(t`Seules les offres Sorties seront affichées`)}
-        />
-        {/* TODO (#6420): Créneau horaire */}
-        <Spacer.Column numberOfSpaces={30} />
-      </Container>
+          {/* Heure */}
+          <SectionWithSwitch
+            title={_(t`Heure`)}
+            subtitle={_(t`Seules les offres Sorties seront affichées`)}
+          />
+          {/* TODO (#6420): Créneau horaire */}
+          <Spacer.Column numberOfSpaces={30} />
+        </Container>
+      </React.Fragment>
+
+      <PageHeader title={_(t`Filtrer`)} RightComponent={ReinitializeFilters} />
+
+      <ShowResultsContainer>
+        <ShowResults />
+        <Spacer.BottomScreen />
+      </ShowResultsContainer>
     </React.Fragment>
-
-    <PageHeader title={_(t`Filtrer`)} RightComponent={ReinitializeFilters} />
-
-    <ShowResultsContainer>
-      <ShowResults />
-      <Spacer.BottomScreen />
-    </ShowResultsContainer>
-  </React.Fragment>
-)
+  )
+}
 
 const Container = styled.ScrollView({ flex: 1 })
 const Separator = styled.View<{ marginVertical?: number }>(({ marginVertical = 0 }) => ({
