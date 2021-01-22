60acf1167cb7e84aa05500cec5354711bafee79e
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-22 20:08:33 +0100

    further test the search reducer

--- src/features/search/pages/__tests__/reducer.test.ts
+++ src/features/search/pages/__tests__/reducer.test.ts
@@ -1,7 +1,18 @@
+import mockdate from 'mockdate'
+
+import { DATE_FILTER_OPTIONS } from 'libs/algolia/enums'
+
 import { Action, initialSearchState, searchReducer, SearchState } from '../reducer'
 import { MAX_PRICE } from '../reducer.helpers'
 
+const Today = new Date(2020, 10, 1)
+const Tomorrow = new Date(2020, 10, 2)
+
 describe('Search reducer', () => {
+  beforeAll(() => {
+    mockdate.set(Today)
+  })
+
   const state = initialSearchState
   it('should handle INIT', () => {
     const action: Action = { type: 'INIT' }
@@ -79,5 +90,80 @@ describe('Search reducer', () => {
     // 3. Remove isDigital
     newState = searchReducer(newState, { type: 'OFFER_TYPE', payload: 'isDigital' })
     expect(newState.offerTypes).toStrictEqual({ isDigital: false, isThing: true, isEvent: false })
+  })
+
+  it('should handle SHOW_RESULTS', () => {
+    let newState = searchReducer(state, { type: 'SHOW_RESULTS', payload: true })
+    expect(newState).toStrictEqual({ ...state, showResults: true })
+    newState = searchReducer(newState, { type: 'SHOW_RESULTS', payload: false })
+    expect(newState).toStrictEqual({ ...state, showResults: false })
+  })
+  it('should handle TOGGLE_OFFER_FREE', () => {
+    let newState = searchReducer(state, { type: 'TOGGLE_OFFER_FREE' })
+    expect(newState).toStrictEqual({ ...state, offerIsFree: true })
+    newState = searchReducer(newState, { type: 'TOGGLE_OFFER_FREE' })
+    expect(newState).toStrictEqual({ ...state, offerIsFree: false })
+  })
+  it('should handle TOGGLE_OFFER_DUO', () => {
+    let newState = searchReducer(state, { type: 'TOGGLE_OFFER_DUO' })
+    expect(newState).toStrictEqual({ ...state, offerIsDuo: true })
+    newState = searchReducer(newState, { type: 'TOGGLE_OFFER_DUO' })
+    expect(newState).toStrictEqual({ ...state, offerIsDuo: false })
+  })
+  it('should handle TOGGLE_OFFER_NEW', () => {
+    let newState = searchReducer(state, { type: 'TOGGLE_OFFER_NEW' })
+    expect(newState).toStrictEqual({ ...state, offerIsNew: true })
+    newState = searchReducer(newState, { type: 'TOGGLE_OFFER_NEW' })
+    expect(newState).toStrictEqual({ ...state, offerIsNew: false })
+  })
+  it('should handle TOGGLE_DATE', () => {
+    let newState = searchReducer(state, { type: 'TOGGLE_DATE' })
+    expect(newState.date).toStrictEqual({ option: DATE_FILTER_OPTIONS.TODAY, selectedDate: Today })
+    newState = searchReducer(newState, { type: 'TOGGLE_DATE' })
+    expect(newState.date).toBeNull()
+  })
+  it('should handle SELECT_DATE_FILTER_OPTION', () => {
+    // 1. No effect if we haven't selected Date before
+    let newState = searchReducer(state, {
+      type: 'SELECT_DATE_FILTER_OPTION',
+      payload: DATE_FILTER_OPTIONS.TODAY,
+    })
+    expect(newState.date).toBeNull()
+
+    // 2. We enable Date, the section 'Date de l'offre' appears and the actions have an effect
+    newState = searchReducer(newState, { type: 'TOGGLE_DATE' })
+    expect(newState.date?.option).toStrictEqual(DATE_FILTER_OPTIONS.TODAY) // "Aujourd'hui" by default
+
+    // 3. We pick the 'Cette semaine'
+    newState = searchReducer(newState, {
+      type: 'SELECT_DATE_FILTER_OPTION',
+      payload: DATE_FILTER_OPTIONS.CURRENT_WEEK,
+    })
+    expect(newState.date?.option).toStrictEqual(DATE_FILTER_OPTIONS.CURRENT_WEEK)
+
+    // 4. We pick the 'Cette semaine'
+    newState = searchReducer(newState, {
+      type: 'SELECT_DATE_FILTER_OPTION',
+      payload: DATE_FILTER_OPTIONS.CURRENT_WEEK_END,
+    })
+    expect(newState.date?.option).toStrictEqual(DATE_FILTER_OPTIONS.CURRENT_WEEK_END)
+
+    // 5. We pick the 'Date précise'
+    newState = searchReducer(newState, {
+      type: 'SELECT_DATE_FILTER_OPTION',
+      payload: DATE_FILTER_OPTIONS.USER_PICK,
+    })
+    expect(newState.date?.option).toStrictEqual(DATE_FILTER_OPTIONS.USER_PICK)
+  })
+
+  it('should handle SELECT_DATE', () => {
+    // 1. No effect if we haven't selected Date before
+    let newState = searchReducer(state, { type: 'SELECT_DATE', payload: Today })
+    expect(newState.date).toBeNull()
+
+    // 2. We choose another date after enabling section Date de l'offre
+    newState = searchReducer(newState, { type: 'TOGGLE_DATE' })
+    newState = searchReducer(newState, { type: 'SELECT_DATE', payload: Tomorrow })
+    expect(newState.date?.selectedDate).toStrictEqual(Tomorrow)
   })
 })
