c9f9fb66ec1727ed1a8270e74754a5dd0cbe3ce1
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-25 11:35:35 +0100

    Add test for Hit position

--- src/features/search/atoms/__tests__/Hit.test.tsx
+++ src/features/search/atoms/__tests__/Hit.test.tsx
@@ -9,13 +9,30 @@ import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { Hit } from '../Hit'
 
 const hit = mockedAlgoliaResponse.hits[0]
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const offerId = dehumanizeId(hit.offer.id)!
+const offerId = dehumanizeId(hit.offer.id)
+
+let mockDistance: string | null = null
+jest.mock('features/offer/components/useDistance', () => ({
+  useDistance: () => mockDistance,
+}))
 
 describe('Hit component', () => {
-  it('should navigate to the offer when clicking on the hit', async () => {
+  it('should navigate to the offer when clicking on the hit', () => {
     const { getByTestId } = render(reactQueryProviderHOC(<Hit hit={hit} />))
     fireEvent.press(getByTestId('offerHit'))
     expect(navigate).toHaveBeenCalledWith('Offer', { id: offerId })
+  })
+  it('should show distance if geolocation enabled', async () => {
+    mockDistance = '10 km'
+    const { queryByText } = render(reactQueryProviderHOC(<Hit hit={hit} />))
+    expect(queryByText('10 km')).toBeTruthy()
+  })
+  it('offer name should take full space if no geolocation', async () => {
+    mockDistance = '10 km'
+    const withDistanceSnapshot = render(reactQueryProviderHOC(<Hit hit={hit} />)).toJSON()
+
+    mockDistance = null
+    const withoutDistanceSnapshot = render(reactQueryProviderHOC(<Hit hit={hit} />)).toJSON()
+    expect(withoutDistanceSnapshot).toMatchDiffSnapshot(withDistanceSnapshot)
   })
 })
