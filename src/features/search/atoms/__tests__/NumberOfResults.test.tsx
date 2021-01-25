a25068b9f04108efbfba7ca0c04c6ad2ec1fe788
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-25 11:44:57 +0100

    Test NumberOfResults atom

---
+++ src/features/search/atoms/__tests__/NumberOfResults.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react-native'
+import React from 'react'
+
+import { NumberOfResultsComponent as NumberOfResults } from '../NumberOfResults'
+
+describe('NumberOfResults component', () => {
+  it('should correctly format the number of hit', () => {
+    expect(render(<NumberOfResults nbHits={0} />).toJSON()).toBeNull()
+    expect(render(<NumberOfResults nbHits={1} />).getByText('1 résultat')).toBeTruthy()
+    expect(render(<NumberOfResults nbHits={2} />).getByText('2 résultats')).toBeTruthy()
+    expect(render(<NumberOfResults nbHits={1234} />).getByText('1234 résultats')).toBeTruthy()
+  })
+})
