c197cd2a745a4e400c4163459749b6ee2c82a1cc
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-26 10:33:23 +0100

    (PC-6465) Map category to Bicolor icon

--- src/libs/algolia/enums/criteriaEnums.ts
+++ src/libs/algolia/enums/criteriaEnums.ts
@@ -1,48 +1,17 @@
 import { CategoryNameEnum } from 'api/gen'
-
-const GEOLOCATION_CRITERIA = {
-  EVERYWHERE: {
-    label: 'Partout',
-    icon: 'ico-everywhere',
-    requiresGeolocation: false,
-  },
-  AROUND_ME: {
-    label: 'Autour de moi',
-    icon: 'ico-around-me',
-    requiresGeolocation: true,
-  },
-}
-
-const SORT_CRITERIA = {
-  RELEVANCE: {
-    label: 'Pertinence',
-    icon: 'ico-relevance',
-    index: '',
-    requiresGeolocation: false,
-  },
-  PRICE: {
-    label: 'Prix',
-    icon: 'ico-price',
-    index: '_by_price',
-    requiresGeolocation: false,
-  },
-}
-
-export const GEOLOCATED_CRITERIA = {
-  ...GEOLOCATION_CRITERIA,
-  ...SORT_CRITERIA,
-}
+import CategoryIcon from 'ui/svg/icons/categories/bicolor'
+import { BicolorIconInterface } from 'ui/svg/icons/types'
 
 type CategoryCriteria = {
   ALL: {
     label: string
-    icon: string
+    icon: React.FC<BicolorIconInterface>
     facetFilter: ''
   }
 } & {
   [category in CategoryNameEnum]: {
     label: string
-    icon: string
+    icon: React.FC<BicolorIconInterface>
     facetFilter: CategoryNameEnum
   }
 }
@@ -50,62 +19,62 @@ type CategoryCriteria = {
 export const CATEGORY_CRITERIA: CategoryCriteria = {
   ALL: {
     label: 'Toutes les catégories',
-    icon: 'ico-all',
+    icon: CategoryIcon.All,
     facetFilter: '',
   },
   [CategoryNameEnum.CINEMA]: {
     label: 'Cinéma',
-    icon: 'ico-cinema',
+    icon: CategoryIcon.Cinema,
     facetFilter: CategoryNameEnum.CINEMA,
   },
   [CategoryNameEnum.VISITE]: {
     label: 'Visites, expositions',
-    icon: 'ico-exposition',
+    icon: CategoryIcon.Exposition,
     facetFilter: CategoryNameEnum.VISITE,
   },
   [CategoryNameEnum.MUSIQUE]: {
     label: 'Musique',
-    icon: 'ico-music',
+    icon: CategoryIcon.Musique,
     facetFilter: CategoryNameEnum.MUSIQUE,
   },
   [CategoryNameEnum.SPECTACLE]: {
     label: 'Spectacles',
-    icon: 'ico-show',
+    icon: CategoryIcon.Spectacles,
     facetFilter: CategoryNameEnum.SPECTACLE,
   },
   [CategoryNameEnum.LECON]: {
     label: 'Cours, ateliers',
-    icon: 'ico-arts',
+    icon: CategoryIcon.Atelier,
     facetFilter: CategoryNameEnum.LECON,
   },
   [CategoryNameEnum.LIVRE]: {
     label: 'Livres',
-    icon: 'ico-books',
+    icon: CategoryIcon.Livres,
     facetFilter: CategoryNameEnum.LIVRE,
   },
   [CategoryNameEnum.FILM]: {
     label: 'Films, séries, podcasts',
-    icon: 'ico-movie',
+    icon: CategoryIcon.Streaming,
     facetFilter: CategoryNameEnum.FILM,
   },
   [CategoryNameEnum.PRESSE]: {
     label: 'Presse',
-    icon: 'ico-newspaper',
+    icon: CategoryIcon.Presse,
     facetFilter: CategoryNameEnum.PRESSE,
   },
   [CategoryNameEnum.JEUXVIDEO]: {
     label: 'Jeux vidéos',
-    icon: 'ico-video-game',
+    icon: CategoryIcon.JeuxVideo,
     facetFilter: CategoryNameEnum.JEUXVIDEO,
   },
   [CategoryNameEnum.CONFERENCE]: {
     label: 'Conférences, rencontres',
-    icon: 'ico-conference',
+    icon: CategoryIcon.Conference,
     facetFilter: CategoryNameEnum.CONFERENCE,
   },
   [CategoryNameEnum.INSTRUMENT]: {
     label: 'Instruments de musique',
-    icon: 'ico-instrument',
+    icon: CategoryIcon.Instrument,
     facetFilter: CategoryNameEnum.INSTRUMENT,
   },
 }
