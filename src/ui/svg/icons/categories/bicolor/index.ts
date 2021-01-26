3abfb9aae8a24ce80d7c7ec6e35950df3e38e0af
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-26 10:32:40 +0100

    (PC-6465) Import des catégories bicolores

---
+++ src/ui/svg/icons/categories/bicolor/index.ts
@@ -0,0 +1,27 @@
+import { All } from './All'
+import { Atelier } from './Atelier'
+import { Cinema } from './Cinema'
+import { Conference } from './Conference'
+import { Exposition } from './Exposition'
+import { Instrument } from './Instrument'
+import { JeuxVideo } from './JeuxVideo'
+import { Livres } from './Livres'
+import { Musique } from './Musique'
+import { Presse } from './Presse'
+import { Spectacles } from './Spectacles'
+import { Streaming } from './Streaming'
+
+export default {
+  All,
+  Atelier,
+  Cinema,
+  Conference,
+  Exposition,
+  Instrument,
+  JeuxVideo,
+  Livres,
+  Musique,
+  Presse,
+  Spectacles,
+  Streaming,
+}
