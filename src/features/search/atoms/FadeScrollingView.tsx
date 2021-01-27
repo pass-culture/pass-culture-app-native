94cf2ef9c57900dba01f74beac3661595e13e93a
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-01-27 09:54:42 +0100

    (PC-6285) Move the Fadein logic to a specific atom

---
+++ src/features/search/atoms/FadeScrollingView.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { Animated } from 'react-native'
+
+export const useDebouncedScrolling = () => {
+  const [isScrolling, setIsScrolling] = useState(false)
+  let timeoutID: NodeJS.Timeout
+  const handleIsScrolling = useCallback(
+    (scrolling: boolean) => () => {
+      if (scrolling) {
+        if (timeoutID) clearTimeout(timeoutID)
+        setIsScrolling(true)
+      } else {
+        timeoutID = setTimeout(() => setIsScrolling(false), 1000)
+      }
+    },
+    []
+  )
+  return { isScrolling, handleIsScrolling }
+}
+
+export const FadeScrollingView: React.FC<{ children: Element; isScrolling: boolean }> = ({
+  children,
+  isScrolling,
+}) => {
+  const [isFirstRender, setIsFirstRender] = useState(true)
+  const [visible, setVisible] = useState<boolean>(!isScrolling)
+  const fadeAnim = useRef(new Animated.Value(+isFirstRender)).current
+
+  useEffect(() => setIsFirstRender(false), [])
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: +visible,
+      duration: 300,
+      useNativeDriver: false,
+    }).start(() => setVisible(!visible))
+  }, [isScrolling])
+
+  return <Animated.View style={{ opacity: fadeAnim }}>{children}</Animated.View>
+}
