f7dae6028874ede6c8380f7a50f131bbd50207ee
Daniel Baudry <dbaudry@octo.com>
2021-01-28 18:04:33 +0100

    (PC-5959): Extract GenericTutorial component

---
+++ src/features/tutorials/components/GenericTutorial.tsx
@@ -0,0 +1,113 @@
+import { t } from '@lingui/macro'
+import { useNavigation } from '@react-navigation/native'
+import LottieView from 'lottie-react-native'
+import AnimatedLottieView from 'lottie-react-native'
+import React, { FunctionComponent, useEffect, useRef } from 'react'
+import { Alert } from 'react-native'
+import styled from 'styled-components/native'
+
+import { UseNavigationType } from 'features/navigation/RootNavigator'
+import { _ } from 'libs/i18n'
+import { AnimationObject } from 'ui/animations/type'
+import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { ButtonTertiaryGreyDark } from 'ui/components/buttons/ButtonTertiaryGreyDark'
+import { StepDots } from 'ui/components/StepDots'
+import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
+
+type Props = {
+  animation: AnimationObject
+  animationSize: number
+  endFrame?: number
+  title: string
+  subTitle: string
+  text: string
+}
+
+export const GenericTutorial: FunctionComponent<Props> = (props) => {
+  const { navigate } = useNavigation<UseNavigationType>()
+
+  const animationRef = useRef<AnimatedLottieView>(null)
+
+  useEffect(() => {
+    if (props.endFrame) {
+      animationRef.current?.play(0, 62)
+    }
+  }, [])
+
+  function goToHomeWithoutModal() {
+    navigate('Home', { shouldDisplayLoginModal: false })
+  }
+
+  function goToNextTutorial() {
+    Alert.alert('TODO: PC-5960')
+  }
+
+  return (
+    <Container>
+      <Spacer.Flex flex={1} />
+      <Container2>
+        <ButtonTertiaryGreyDark title={_(t`Tout passer`)} onPress={goToHomeWithoutModal} />
+      </Container2>
+      <Spacer.Flex flex={2} />
+      <StyledLottieView
+        ref={animationRef}
+        source={props.animation}
+        autoPlay
+        loop={false}
+        size={props.animationSize}
+      />
+      <Spacer.Flex flex={0.8} />
+      <StyledTitle>{props.title}</StyledTitle>
+      <StyledSubTitle>{props.subTitle}</StyledSubTitle>
+      <Spacer.Flex flex={0.8} />
+      <StyledBody>{props.text}</StyledBody>
+      <Spacer.Flex flex={2} />
+      <ButtonContainer>
+        <ButtonPrimary title={_(t`Continuer`)} onPress={goToNextTutorial} />
+      </ButtonContainer>
+      <Spacer.Flex flex={0.5} />
+      <StepDots numberOfSteps={4} currentStep={1} />
+      <Spacer.Flex flex={1} />
+    </Container>
+  )
+}
+
+const Container2 = styled.View({
+  alignSelf: 'flex-end',
+  width: '40%',
+})
+
+const ButtonContainer = styled.View({
+  paddingHorizontal: getSpacing(6),
+  width: '100%',
+})
+
+const StyledLottieView = styled(LottieView)((props: { size: number }) => ({
+  width: props.size,
+  height: props.size,
+}))
+
+const StyledBody = styled(Typo.Body).attrs({
+  color: ColorsEnum.BLACK,
+})({
+  width: '60%',
+  textAlign: 'center',
+  flexDirection: 'column',
+})
+
+const Container = styled.View({
+  alignItems: 'center',
+  flexGrow: 1,
+})
+
+const StyledTitle = styled(Typo.Title1).attrs({
+  color: ColorsEnum.BLACK,
+})({
+  display: 'flex',
+})
+
+const StyledSubTitle = styled(Typo.Title2).attrs({
+  color: ColorsEnum.BLACK,
+})({
+  textAlign: 'center',
+})
--- src/features/tutorials/pages/FirstTutorial.tsx
+++ src/features/tutorials/pages/FirstTutorial.tsx
@@ -1,101 +1,20 @@
 import { t } from '@lingui/macro'
-import { useNavigation } from '@react-navigation/native'
-import LottieView from 'lottie-react-native'
-import AnimatedLottieView from 'lottie-react-native'
-import React, { useEffect, useRef } from 'react'
-import styled from 'styled-components/native'
+import React from 'react'
 
-import { UseNavigationType } from 'features/navigation/RootNavigator'
+import { GenericTutorial } from 'features/tutorials/components/GenericTutorial'
 import { _ } from 'libs/i18n'
 import TutorialPassLogo from 'ui/animations/tutorial_pass_logo.json'
-import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
-import { ButtonTertiaryGreyDark } from 'ui/components/buttons/ButtonTertiaryGreyDark'
-import { Spacer } from 'ui/components/spacer/Spacer'
-import { StepDots } from 'ui/components/StepDots'
-import { ColorsEnum, getSpacing, Typo } from 'ui/theme'
+import { getSpacing } from 'ui/theme'
 
 export function FirstTutorial() {
-  const { navigate } = useNavigation<UseNavigationType>()
-
-  const animationRef = useRef<AnimatedLottieView>(null)
-
-  useEffect(() => {
-    animationRef.current?.play(0, 62)
-  }, [])
-
-  function goToHomeWithoutModal() {
-    navigate('Home', { shouldDisplayLoginModal: false })
-  }
-
-  function goToNextTutorial() {
-    navigate('Home', { shouldDisplayLoginModal: false })
-  }
-
   return (
-    <React.Fragment>
-      <Container2>
-        <ButtonTertiaryGreyDark title={_(t`Tout passer`)} onPress={goToHomeWithoutModal} />
-      </Container2>
-      <Container>
-        <React.Fragment>
-          <StyledLottieView
-            ref={animationRef}
-            source={TutorialPassLogo}
-            autoPlay
-            loop={false}
-            size={getSpacing(60)}
-          />
-          <Spacer.Column numberOfSpaces={9} />
-        </React.Fragment>
-        <StyledTitle>{_(t`Le pass Culture`)}</StyledTitle>
-        <StyledSubTitle>{_(t`c'est...`)}</StyledSubTitle>
-        <Spacer.Column numberOfSpaces={10} />
-        <StyledBody>
-          <Spacer.Flex />
-          {_(t`une initiative financée par le Ministère de la Culture.`)}
-          <Spacer.Flex />
-        </StyledBody>
-        <Spacer.Column numberOfSpaces={15} />
-        <ButtonPrimary title={_(t`Continuer`)} onPress={goToNextTutorial} />
-        <Spacer.Column numberOfSpaces={6} />
-        <StepDots numberOfSteps={4} currentStep={1} />
-      </Container>
-    </React.Fragment>
+    <GenericTutorial
+      title={_(t`Le pass Culture`)}
+      subTitle={_(t`c'est...`)}
+      text={_(t`une initiative financée par le Ministère de la Culture.`)}
+      animation={TutorialPassLogo}
+      animationSize={getSpacing(60)}
+      endFrame={62}
+    />
   )
 }
-
-const Container2 = styled.View({
-  paddingTop: getSpacing(6),
-  alignSelf: 'flex-end',
-  width: '40%',
-})
-
-const StyledLottieView = styled(LottieView)((props: { size: number }) => ({
-  width: props.size,
-  height: props.size,
-}))
-
-const StyledBody = styled(Typo.Body).attrs({
-  color: ColorsEnum.BLACK,
-})({
-  width: '60%',
-  textAlign: 'center',
-  flexDirection: 'column',
-})
-
-const Container = styled.View({
-  padding: getSpacing(6),
-  alignItems: 'center',
-})
-
-const StyledTitle = styled(Typo.Title1).attrs({
-  color: ColorsEnum.BLACK,
-})({
-  display: 'flex',
-})
-
-const StyledSubTitle = styled(Typo.Title2).attrs({
-  color: ColorsEnum.BLACK,
-})({
-  textAlign: 'center',
-})
