302e343251a52b8df7ed880a1413a87af76b62a0
BenStirrup <benjamin.stirrup@gmail.com>
2021-02-02 14:52:23 +0100

    (PC-5961) Generalize GenericTutorial to integrate other pages

--- src/features/firstLogin/tutorials/pages/FirstTutorial.tsx
+++ src/features/firstLogin/tutorials/pages/FirstTutorial.tsx
@@ -1,5 +1,6 @@
 import { t } from '@lingui/macro'
 import React from 'react'
+import { Alert } from 'react-native'
 
 import { GenericTutorial } from 'features/firstLogin/tutorials/components/GenericTutorial'
 import { _ } from 'libs/i18n'
@@ -7,14 +8,21 @@ import TutorialPassLogo from 'ui/animations/tutorial_pass_logo.json'
 import { getSpacing } from 'ui/theme'
 
 export function FirstTutorial() {
+  function goToSecondTutorial() {
+    Alert.alert('TODO: PC-5960')
+  }
+
   return (
     <GenericTutorial
-      title={_(t`Le pass Culture`)}
-      subTitle={_(t`c'est...`)}
-      text={_(t`une initiative financée par le Ministère de la Culture.`)}
       animation={TutorialPassLogo}
       animationSize={getSpacing(60)}
+      buttonCallback={goToSecondTutorial}
+      buttonText={_(t`Continuer`)}
       pauseAnimationOnRenderAtFrame={62}
+      step={1}
+      subTitle={_(t`c'est...`)}
+      text={_(t`une initiative financée par le Ministère de la Culture.`)}
+      title={_(t`Le pass Culture`)}
     />
   )
 }
