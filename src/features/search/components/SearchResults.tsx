258e8271359abc3c8e3d74596279465b7ee7fcf8
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-02-05 15:05:26 +0100

    (PC-6590) Reduce bottom margin on category page

--- src/features/search/components/SearchResults.tsx
+++ src/features/search/components/SearchResults.tsx
@@ -6,9 +6,5 @@ import { InfiniteHits } from 'features/search/components'
 export const SearchResults: React.FC = () => {
   const { isScrolling, handleIsScrolling } = useDebouncedScrolling()
 
-  return (
-    <React.Fragment>
-      <InfiniteHits handleIsScrolling={handleIsScrolling} isScrolling={isScrolling} />
-    </React.Fragment>
-  )
+  return <InfiniteHits handleIsScrolling={handleIsScrolling} isScrolling={isScrolling} />
 }
