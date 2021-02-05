bbf7de9ccc969daffb7d110ce1d9e2a7af3833e3
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-02-05 13:49:07 +0100

    (PC-6560) Show empty list on location picker

--- src/features/search/pages/LocationPicker.tsx
+++ src/features/search/pages/LocationPicker.tsx
@@ -1,18 +1,18 @@
 import { t } from '@lingui/macro'
-import { useNavigation } from '@react-navigation/native'
 import debounce from 'lodash.debounce'
 import React, { useEffect, useRef, useState } from 'react'
-import { FlatList, TouchableOpacity } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import styled from 'styled-components/native'
 
-import { useSearch } from 'features/search/pages/SearchWrapper'
 import { _ } from 'libs/i18n'
-import { fetchPlaces, REGEX_STARTING_WITH_NUMBERS, SuggestedPlace } from 'libs/place'
+import { fetchPlaces, SuggestedPlace } from 'libs/place'
 import { PageHeader } from 'ui/components/headers/PageHeader'
 import { SearchInput } from 'ui/components/inputs/SearchInput'
 import { Invalidate } from 'ui/svg/icons/Invalidate'
-import { getSpacing, Spacer, Typo } from 'ui/theme'
-import { ACTIVE_OPACITY, ColorsEnum } from 'ui/theme/colors'
+import { Spacer } from 'ui/theme'
+import { ACTIVE_OPACITY } from 'ui/theme/colors'
+
+import { SuggestedPlaces } from './SuggestedPlaces'
 
 const SEARCH_DEBOUNCE_MS = 500
 
@@ -23,31 +23,20 @@ const RightIcon: React.FC<{ value: string; onPress: () => void }> = (props) =>
     </TouchableOpacity>
   ) : null
 
-export const keyExtractor = ({ geolocation, name }: SuggestedPlace) => {
-  if (geolocation) return `${geolocation.latitude}-${geolocation.longitude}`
-  return `${name.short}-${name.long}`
-}
-
-const PlaceHit: React.FC<{ place: SuggestedPlace; onPress: () => void }> = ({ place, onPress }) => (
-  <ItemContainer onPress={onPress} testID={keyExtractor(place)}>
-    <Typo.ButtonText>{place.name.short}</Typo.ButtonText>
-    <Spacer.Row numberOfSpaces={1} />
-    <Typo.Body>
-      {REGEX_STARTING_WITH_NUMBERS.test(place.name.short)
-        ? place.extraData.city
-        : place.extraData.department}
-    </Typo.Body>
-  </ItemContainer>
-)
-
 export const LocationPicker: React.FC = () => {
   const [places, setPlaces] = useState<SuggestedPlace[]>([])
   const [value, setValue] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const [debouncedValue, setDebouncedValue] = useState<string>(value)
   const debouncedSetValue = useRef(debounce(setDebouncedValue, SEARCH_DEBOUNCE_MS)).current
 
   useEffect(() => {
-    if (debouncedValue.length > 0) fetchPlaces({ query: debouncedValue }).then(setPlaces)
+    if (debouncedValue.length > 0) {
+      setIsLoading(true)
+      fetchPlaces({ query: debouncedValue })
+        .then(setPlaces)
+        .finally(() => setIsLoading(false))
+    }
   }, [debouncedValue])
 
   const resetSearch = () => {
@@ -75,47 +64,10 @@ export const LocationPicker: React.FC = () => {
         />
       </StyledInput>
       <Spacer.Column numberOfSpaces={4} />
-      <SuggestedPlaces places={places} />
+      <SuggestedPlaces places={places} query={debouncedValue} isLoading={isLoading} />
       <PageHeader title={_(t`Choisir un lieu`)} />
     </React.Fragment>
   )
 }
 
-export const SuggestedPlaces: React.FC<{ places: SuggestedPlace[] }> = ({ places }) => {
-  const { goBack } = useNavigation()
-  const { dispatch } = useSearch()
-
-  const onPickPlace = (place: SuggestedPlace) => () => {
-    if (place.geolocation) {
-      dispatch({ type: 'LOCATION_PLACE', payload: place })
-    }
-    goBack()
-  }
-
-  return (
-    <FlatList
-      data={places}
-      keyExtractor={keyExtractor}
-      renderItem={({ item: place }) => <PlaceHit place={place} onPress={onPickPlace(place)} />}
-      ListEmptyComponent={React.Fragment}
-      ItemSeparatorComponent={Separator}
-      keyboardShouldPersistTaps="handled"
-      keyboardDismissMode="on-drag"
-    />
-  )
-}
-
 const StyledInput = styled.View({ alignItems: 'center' })
-const ItemContainer = styled.TouchableOpacity.attrs({
-  activeOpacity: ACTIVE_OPACITY,
-})({
-  flexDirection: 'row',
-  marginHorizontal: getSpacing(6),
-  paddingVertical: getSpacing(4),
-  alignItems: 'center',
-})
-const Separator = styled.View({
-  height: 2,
-  backgroundColor: ColorsEnum.GREY_LIGHT,
-  marginHorizontal: getSpacing(6),
-})
--- src/features/search/pages/__tests__/LocationPicker.test.tsx
+++ src/features/search/pages/__tests__/LocationPicker.test.tsx
@@ -2,8 +2,9 @@ import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
 
 import { goBack } from '__mocks__/@react-navigation/native'
-import { keyExtractor, LocationPicker, SuggestedPlaces } from 'features/search/pages/LocationPicker'
+import { LocationPicker } from 'features/search/pages/LocationPicker'
 import { initialSearchState } from 'features/search/pages/reducer'
+import { keyExtractor, SuggestedPlaces } from 'features/search/pages/SuggestedPlaces'
 import { buildSuggestedPlaces } from 'libs/place'
 import { mockedSuggestedPlaces } from 'libs/place/fixtures/mockedSuggestedPlaces'
 
@@ -25,9 +26,11 @@ describe('LocationPicker component', () => {
 })
 
 describe('SuggestedPlaces component', () => {
-  it('should dispatch LOCATION_PLACE on pick place', async () => {
+  it('should dispatch LOCATION_PLACE on pick place', () => {
     const places = buildSuggestedPlaces(mockedSuggestedPlaces)
-    const { getByTestId } = render(<SuggestedPlaces places={places} />)
+    const { getByTestId } = render(
+      <SuggestedPlaces places={places} query="paris" isLoading={false} />
+    )
 
     fireEvent.press(getByTestId(keyExtractor(places[1])))
     expect(mockDispatch).toHaveBeenCalledWith({
@@ -35,5 +38,18 @@ describe('SuggestedPlaces component', () => {
       payload: places[1],
     })
     expect(goBack).toHaveBeenCalledTimes(1)
+  })
+
+  it('should show empty component only when query is not empty and the results are not loading', () => {
+    const { getByText } = render(<SuggestedPlaces places={[]} query="paris" isLoading={false} />)
+    expect(getByText('Aucun lieu ne correspond à ta recherche')).toBeTruthy()
+  })
+  it('should not show empty component if the query is empty and the results are not loading', () => {
+    const { queryByText } = render(<SuggestedPlaces places={[]} query="" isLoading={false} />)
+    expect(queryByText('Aucun lieu ne correspond à ta recherche')).toBeFalsy()
+  })
+  it('should not show empty component if the results are still loading', () => {
+    const { queryByText } = render(<SuggestedPlaces places={[]} query="paris" isLoading={true} />)
+    expect(queryByText('Aucun lieu ne correspond à ta recherche')).toBeFalsy()
   })
 })
