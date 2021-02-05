8d8d2a460978638ca63e4a467325dc9b80bfb9ad
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-02-05 13:49:07 +0100

    (PC-6560) Show empty list on location picker

---
+++ src/features/search/pages/SuggestedPlaces.tsx
@@ -0,0 +1,89 @@
+import { t } from '@lingui/macro'
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { FlatList } from 'react-native'
+import styled from 'styled-components/native'
+
+import { useSearch } from 'features/search/pages/SearchWrapper'
+import { _ } from 'libs/i18n'
+import { REGEX_STARTING_WITH_NUMBERS, SuggestedPlace } from 'libs/place'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
+import { ACTIVE_OPACITY, ColorsEnum } from 'ui/theme/colors'
+
+export const keyExtractor = ({ geolocation, name }: SuggestedPlace) => {
+  if (geolocation) return `${geolocation.latitude}-${geolocation.longitude}`
+  return `${name.short}-${name.long}`
+}
+
+const PlaceHit: React.FC<{ place: SuggestedPlace; onPress: () => void }> = ({ place, onPress }) => (
+  <ItemContainer onPress={onPress} testID={keyExtractor(place)}>
+    <Typo.ButtonText>{place.name.short}</Typo.ButtonText>
+    <Spacer.Row numberOfSpaces={1} />
+    <Typo.Body>
+      {REGEX_STARTING_WITH_NUMBERS.test(place.name.short)
+        ? place.extraData.city
+        : place.extraData.department}
+    </Typo.Body>
+  </ItemContainer>
+)
+
+interface Props {
+  places: SuggestedPlace[]
+  query: string
+  isLoading: boolean
+}
+
+export const SuggestedPlaces: React.FC<Props> = ({ places, query, isLoading }) => {
+  const { goBack } = useNavigation()
+  const { dispatch } = useSearch()
+
+  const onPickPlace = (place: SuggestedPlace) => () => {
+    if (place.geolocation) {
+      dispatch({ type: 'LOCATION_PLACE', payload: place })
+    }
+    goBack()
+  }
+
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={keyExtractor}
+      renderItem={({ item: place }) => <PlaceHit place={place} onPress={onPickPlace(place)} />}
+      ListEmptyComponent={() => <NoSuggestedPlaces show={query.length > 0 && !isLoading} />}
+      ItemSeparatorComponent={Separator}
+      keyboardShouldPersistTaps="handled"
+      keyboardDismissMode="on-drag"
+    />
+  )
+}
+
+const NoSuggestedPlaces = ({ show }: { show: boolean }) =>
+  show ? (
+    <DescriptionErrorTextContainer>
+      <DescriptionErrorText>{_(t`Aucun lieu ne correspond à ta recherche`)}</DescriptionErrorText>
+    </DescriptionErrorTextContainer>
+  ) : (
+    <React.Fragment />
+  )
+
+const ItemContainer = styled.TouchableOpacity.attrs({
+  activeOpacity: ACTIVE_OPACITY,
+})({
+  flexDirection: 'row',
+  marginHorizontal: getSpacing(6),
+  paddingVertical: getSpacing(4),
+  alignItems: 'center',
+})
+
+const Separator = styled.View({
+  height: 2,
+  backgroundColor: ColorsEnum.GREY_LIGHT,
+  marginHorizontal: getSpacing(6),
+})
+
+const DescriptionErrorTextContainer = styled.Text({
+  marginTop: getSpacing(6.5),
+  textAlign: 'center',
+})
+
+const DescriptionErrorText = styled(Typo.Body)({ color: ColorsEnum.GREY_DARK })
