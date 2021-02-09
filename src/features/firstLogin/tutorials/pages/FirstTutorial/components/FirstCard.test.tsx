b81b35ee5d45a679f256b3f3c9dc6c3e51c5b84d
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-02-09 18:45:06 +0100

    (PC-6744): improve tests

--- src/features/firstLogin/tutorials/pages/FirstTutorial/components/FirstCard.test.tsx
+++ src/features/firstLogin/tutorials/pages/FirstTutorial/components/FirstCard.test.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-community/async-storage'
-import { fireEvent, render } from '@testing-library/react-native'
-import React, { createRef } from 'react'
+import { act, fireEvent, render } from '@testing-library/react-native'
+import React, { RefObject } from 'react'
 import Swiper from 'react-native-web-swiper'
 
 import { FirstCard } from './FirstCard'
@@ -11,19 +11,13 @@ describe('FirstCard', () => {
     expect(firstTutorial).toMatchSnapshot()
   })
   it('should swipe to next card on button press', async () => {
-    const ref = createRef<Swiper>()
-    const { getByText } = render(
-      <Swiper ref={ref}>
-        <FirstCard swiperRef={ref} />
-      </Swiper>
-    )
+    const ref = { current: { goToNext: jest.fn() } }
+    const { getByText } = render(<FirstCard swiperRef={(ref as unknown) as RefObject<Swiper>} />)
     const button = await getByText('Continuer')
-    const goToNext = ref?.current ?? false
-    if (goToNext) {
-      const spy = jest.spyOn(goToNext, 'goToNext')
-      fireEvent.press(button)
-      expect(AsyncStorage.setItem).toBeCalledWith('has_seen_tutorials', 'true')
-      expect(spy).toHaveBeenCalled()
-    }
+    fireEvent.press(button)
+    expect(AsyncStorage.setItem).toBeCalledWith('has_seen_tutorials', 'true')
+    await act(async () => {
+      expect(ref.current.goToNext).toHaveBeenCalled()
+    })
   })
 })
--- src/features/firstLogin/tutorials/pages/FirstTutorial/components/SecondCard.test.tsx
+++ src/features/firstLogin/tutorials/pages/FirstTutorial/components/SecondCard.test.tsx
@@ -1,5 +1,5 @@
-import { fireEvent, render } from '@testing-library/react-native'
-import React, { createRef } from 'react'
+import { act, fireEvent, render } from '@testing-library/react-native'
+import React, { RefObject } from 'react'
 import Swiper from 'react-native-web-swiper'
 
 import { SecondCard } from './SecondCard'
@@ -10,14 +10,12 @@ describe('SecondCard', () => {
     expect(firstTutorial).toMatchSnapshot()
   })
   it('should swipe to next card on button press', async () => {
-    const ref = createRef<Swiper>()
-    const { getByText } = render(<SecondCard swiperRef={ref} />)
+    const ref = { current: { goToNext: jest.fn() } }
+    const { getByText } = render(<SecondCard swiperRef={(ref as unknown) as RefObject<Swiper>} />)
     const button = await getByText('Continuer')
-    const goToNext = ref?.current ?? false
-    if (goToNext) {
-      const spy = jest.spyOn(goToNext, 'goToNext')
-      fireEvent.press(button)
-      expect(spy).toHaveBeenCalled()
-    }
+    fireEvent.press(button)
+    await act(async () => {
+      expect(ref.current.goToNext).toHaveBeenCalled()
+    })
   })
 })
