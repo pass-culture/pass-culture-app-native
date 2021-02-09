f25f265d58876057e205b33a0305d0660f355c0c
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-02-09 18:18:41 +0100

    (PC-6744): improve tests

--- src/features/firstLogin/tutorials/pages/FirstTutorial/components/ThirdCard.test.tsx
+++ src/features/firstLogin/tutorials/pages/FirstTutorial/components/ThirdCard.test.tsx
@@ -1,55 +1,57 @@
-import { fireEvent, render } from '@testing-library/react-native'
-import React, { createRef, RefObject } from 'react'
+import { fireEvent, render, waitFor } from '@testing-library/react-native'
+import React, { RefObject } from 'react'
+import Geolocation from 'react-native-geolocation-service'
 import Swiper from 'react-native-web-swiper'
 
 import { analytics } from 'libs/analytics'
+import { GeolocationWrapper } from 'libs/geolocation'
 
-import { ThirdCard, useRequestGeolocPermissionParameters } from './ThirdCard'
-
-let mockSubmitSpy: () => void
-let mockRootSpy: () => void
-let mockSpy: () => void
-jest.mock('libs/geolocation', () => {
-  mockSubmitSpy = jest.fn()
-  mockSpy = jest.fn().mockReturnValue({
-    onSubmit: mockSubmitSpy,
-  })
-  mockRootSpy = jest.fn().mockReturnValue({
-    requestGeolocPermission: mockSpy,
-  })
-
-  return {
-    useGeolocation: mockRootSpy,
-  }
-})
+import { ThirdCard } from './ThirdCard'
 
 describe('ThirdCard', () => {
   it('should render third card', () => {
     const firstTutorial = render(<ThirdCard />)
     expect(firstTutorial).toMatchSnapshot()
   })
+  it('should not trigger analytics on refusal', async () => {
+    jest.spyOn(Geolocation, 'requestAuthorization').mockResolvedValueOnce('denied')
+    const ref = { current: { goToNext: jest.fn() } }
+    const { getByText } = render(
+      <GeolocationWrapper>
+        <ThirdCard swiperRef={(ref as unknown) as RefObject<Swiper>} />
+      </GeolocationWrapper>
+    )
+    fireEvent.press(getByText('Activer la géolocalisation'))
+    await waitFor(() => {
+      expect(analytics.logHasActivateGeolocFromTutorial).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  it('should trigger analytics on acceptance', async () => {
+    jest.spyOn(Geolocation, 'requestAuthorization').mockResolvedValueOnce('granted')
+    const ref = { current: { goToNext: jest.fn() } }
+    const { getByText } = render(
+      <GeolocationWrapper>
+        <ThirdCard swiperRef={(ref as unknown) as RefObject<Swiper>} />
+      </GeolocationWrapper>
+    )
+    fireEvent.press(getByText('Activer la géolocalisation'))
+    await waitFor(() => {
+      expect(analytics.logHasActivateGeolocFromTutorial).toHaveBeenCalledTimes(1)
+    })
+  })
+
   it('should swipe to next card on button press', async () => {
-    const ref = createRef<Swiper>()
-    const { getByText } = render(<ThirdCard swiperRef={ref} />)
-    const button = await getByText('Activer la géolocalisation')
-    fireEvent.press(button)
-    expect(mockRootSpy).toHaveBeenCalled()
-    expect(mockSpy).toHaveBeenCalled()
-  })
-  it('should trigger analytics on acceptance', () => {
-    const swiperRef = createRef<Swiper>()
-    const action = useRequestGeolocPermissionParameters(swiperRef)
-    action.onAcceptance()
-    expect(analytics.logHasActivateGeolocFromTutorial).toHaveBeenCalledTimes(1)
-  })
-  it('should trigger slide change on submit', () => {
-    const ref = {
-      current: {
-        goToNext: jest.fn(),
-      },
-    }
-    const action = useRequestGeolocPermissionParameters((ref as unknown) as RefObject<Swiper>)
-    action.onSubmit()
-    expect(ref.current.goToNext).toHaveBeenCalledTimes(1)
+    jest.spyOn(Geolocation, 'requestAuthorization').mockResolvedValueOnce('granted')
+    const ref = { current: { goToNext: jest.fn() } }
+    const { getByText } = render(
+      <GeolocationWrapper>
+        <ThirdCard swiperRef={(ref as unknown) as RefObject<Swiper>} />
+      </GeolocationWrapper>
+    )
+    fireEvent.press(getByText('Activer la géolocalisation'))
+    await waitFor(() => {
+      expect(ref.current?.goToNext).toBeCalled()
+    })
   })
 })
--- src/features/firstLogin/tutorials/pages/FirstTutorial/components/ThirdCard.tsx
+++ src/features/firstLogin/tutorials/pages/FirstTutorial/components/ThirdCard.tsx
@@ -1,6 +1,5 @@
 import { t } from '@lingui/macro'
-import React, { RefObject } from 'react'
-import Swiper from 'react-native-web-swiper'
+import React from 'react'
 
 import { CardKey, GenericCard } from 'features/firstLogin/tutorials/components/GenericCard'
 import { analytics } from 'libs/analytics'
@@ -8,23 +7,18 @@ import { useGeolocation } from 'libs/geolocation'
 import { _ } from 'libs/i18n'
 import GeolocationAnimation from 'ui/animations/geolocalisation.json'
 
-export const useRequestGeolocPermissionParameters = (swiperRef?: RefObject<Swiper>) => {
-  return {
-    onSubmit: () => {
-      swiperRef?.current?.goToNext()
-    },
-    onAcceptance: () => {
-      analytics.logHasActivateGeolocFromTutorial()
-    },
-  }
-}
-
 export function ThirdCard(props: CardKey) {
   const { requestGeolocPermission } = useGeolocation()
-  const requestGeolocPermissionParameters = useRequestGeolocPermissionParameters(props.swiperRef)
 
   async function onGeolocationButtonPress() {
-    await requestGeolocPermission(requestGeolocPermissionParameters)
+    await requestGeolocPermission({
+      onSubmit: () => {
+        props.swiperRef?.current?.goToNext()
+      },
+      onAcceptance: () => {
+        analytics.logHasActivateGeolocFromTutorial()
+      },
+    })
   }
 
   return (
