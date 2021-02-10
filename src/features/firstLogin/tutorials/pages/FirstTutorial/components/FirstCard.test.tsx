1b5a24be1850bf92f98adbc1f6427bb33b8297ad
BenStirrup <benjamin.stirrup@gmail.com>
2021-02-10 12:11:59 +0100

    (PC-5979) Do not save has_seen_tutorials on first card of tutorials

--- src/features/firstLogin/tutorials/pages/FirstTutorial/components/FirstCard.test.tsx
+++ src/features/firstLogin/tutorials/pages/FirstTutorial/components/FirstCard.test.tsx
@@ -1,4 +1,3 @@
-import AsyncStorage from '@react-native-community/async-storage'
 import { act, fireEvent, render } from '@testing-library/react-native'
 import React, { RefObject } from 'react'
 import Swiper from 'react-native-web-swiper'
@@ -10,12 +9,13 @@ describe('FirstCard', () => {
     const firstTutorial = render(<FirstCard />)
     expect(firstTutorial).toMatchSnapshot()
   })
+
   it('should swipe to next card on button press', async () => {
     const ref = { current: { goToNext: jest.fn() } }
     const { getByText } = render(<FirstCard swiperRef={(ref as unknown) as RefObject<Swiper>} />)
-    const button = await getByText('Continuer')
-    fireEvent.press(button)
-    expect(AsyncStorage.setItem).toBeCalledWith('has_seen_tutorials', 'true')
+
+    fireEvent.press(getByText('Continuer'))
+
     await act(async () => {
       expect(ref.current.goToNext).toHaveBeenCalled()
     })
