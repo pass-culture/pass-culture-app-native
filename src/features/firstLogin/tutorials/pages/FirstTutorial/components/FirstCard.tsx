2644fdfd59b02c319dcee5784be35347711027d0
BenStirrup <benjamin.stirrup@gmail.com>
2021-02-10 12:11:59 +0100

    (PC-5979) Do not save has_seen_tutorials on first card of tutorials

--- src/features/firstLogin/tutorials/pages/FirstTutorial/components/FirstCard.tsx
+++ src/features/firstLogin/tutorials/pages/FirstTutorial/components/FirstCard.tsx
@@ -3,14 +3,13 @@ import React from 'react'
 
 import { CardKey, GenericCard } from 'features/firstLogin/tutorials/components/GenericCard'
 import { _ } from 'libs/i18n'
-import { storage } from 'libs/storage'
 import TutorialPassLogo from 'ui/animations/tutorial_pass_logo.json'
 
 export function FirstCard(props: CardKey) {
   function onButtonPress() {
-    storage.saveObject('has_seen_tutorials', true)
     props.swiperRef?.current?.goToNext()
   }
+
   return (
     <GenericCard
       animation={TutorialPassLogo}
