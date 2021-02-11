7c5546d790fbc3ff3e495a89ebdb57e90d7c3dd8
Anouk Hello <anouk.hello@polyconseil.fr>
2021-02-11 19:58:41 +0100

    (PC-6169) pricesConversion: define a method to convert cents to euros

--- src/libs/parsers/pricesConversion.ts
+++ src/libs/parsers/pricesConversion.ts
@@ -8,6 +8,8 @@ export const CENTS_IN_EURO = 100
 
 export const convertEuroToCents = (p: number): number => Math.floor(p * CENTS_IN_EURO)
 
+export const convertCentsToEuros = (p: number): number => Math.floor(p / CENTS_IN_EURO)
+
 export const convertAlgoliaHitToCents = (hit: AlgoliaHit): AlgoliaHit => {
   const { prices, priceMax, priceMin, ...offer } = hit.offer
   return {
