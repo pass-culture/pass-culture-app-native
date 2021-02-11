4b3d866a27efa8550bdb5f7b9440d72d3955afaf
Justine Mignot <justinem@bam.tech>
2021-02-11 10:15:01 +0100

    :sparkles: (PC-6628) open settings modal if never_ask_again

--- src/features/search/pages/LocationFilter.tsx
+++ src/features/search/pages/LocationFilter.tsx
@@ -2,7 +2,7 @@ import { t } from '@lingui/macro'
 import { useNavigation } from '@react-navigation/native'
 import debounce from 'lodash.debounce'
 import React, { useRef } from 'react'
-import { ScrollView, ViewStyle, Linking } from 'react-native'
+import { ScrollView, ViewStyle, Linking, TouchableOpacity, Text } from 'react-native'
 
 import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { useAppStateChange } from 'features/offer/pages/useAppStateChange'
@@ -11,6 +11,8 @@ import { useGeolocation, GeolocPermissionState } from 'libs/geolocation'
 import { _ } from 'libs/i18n'
 import { Banner, BannerType } from 'ui/components/Banner'
 import { PageHeader } from 'ui/components/headers/PageHeader'
+import { AppInformationModal } from 'ui/components/modals/AppInformationModal'
+import { useModal } from 'ui/components/modals/useModal'
 import { Spacer } from 'ui/theme'
 
 import { LocationChoice } from '../components/LocationChoice'
@@ -29,6 +31,11 @@ export const LocationFilter: React.FC = () => {
   } = useGeolocation()
   const { dispatch } = useSearch()
   const debouncedGoBack = useRef(debounce(goBack, DEBOUNCED_CALLBACK)).current
+  const {
+    visible: isGeolocPermissionModalVisible,
+    showModal: showGeolocPermissionModal,
+    hideModal: hideGeolocPermissionModal,
+  } = useModal(false)
 
   const onAppBecomeActive = async () => {
     await checkGeolocPermission()
@@ -47,21 +54,28 @@ export const LocationFilter: React.FC = () => {
   const onPressAroundMe = async () => {
     if (position === null) {
       if (permissionState === GeolocPermissionState.NEVER_ASK_AGAIN) {
-        Linking.openSettings()
+        showGeolocPermissionModal()
       } else {
         await requestGeolocPermission()
+        debouncedGoBack()
       }
     } else {
       dispatch({
         type: 'LOCATION_AROUND_ME',
         payload: { latitude: position.latitude, longitude: position.longitude },
       })
+      debouncedGoBack()
     }
-    debouncedGoBack()
   }
 
   const onPressEverywhere = () => {
     dispatch({ type: 'LOCATION_EVERYWHERE' })
+    debouncedGoBack()
+  }
+
+  const onPressGeolocPermissionModalButton = () => {
+    Linking.openSettings()
+    hideGeolocPermissionModal()
     debouncedGoBack()
   }
 
@@ -99,6 +113,18 @@ export const LocationFilter: React.FC = () => {
       </ScrollView>
 
       <PageHeader title={_(t`Localisation`)} />
+      <AppInformationModal
+        title="Geoloc"
+        visible={isGeolocPermissionModalVisible}
+        onCloseIconPress={hideGeolocPermissionModal}
+        testIdSuffix="geoloc-permission-modal">
+        <React.Fragment>
+          <Text>Je suis la modale</Text>
+          <TouchableOpacity onPress={onPressGeolocPermissionModalButton}>
+            <Text>Ouvrir settings</Text>
+          </TouchableOpacity>
+        </React.Fragment>
+      </AppInformationModal>
     </React.Fragment>
   )
 }
