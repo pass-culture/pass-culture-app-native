f3973e0cc7662acf6c6ffdb19e574511c7749abd
Justine Mignot <justinem@bam.tech>
2021-02-15 15:14:51 +0100

    :sparkles: (PC-6626) manage 'allow once' user response

--- src/libs/geolocation/requestGeolocPermission.ios.ts
+++ src/libs/geolocation/requestGeolocPermission.ios.ts
@@ -4,7 +4,11 @@ import { GeolocPermissionState } from './permissionState.d'
 
 export const requestGeolocPermissionSystem = async (): Promise<GeolocPermissionState> => {
   const permissionValue = await Geolocation.requestAuthorization('whenInUse')
+  // Corresponding user response: Allow while using app
   if (permissionValue === 'granted') return GeolocPermissionState.GRANTED
+  // Corresponding user response: Allow once
+  if (permissionValue === 'denied') return GeolocPermissionState.DENIED
+  // Corresponding user response: Never
   return GeolocPermissionState.NEVER_ASK_AGAIN
 }
 
