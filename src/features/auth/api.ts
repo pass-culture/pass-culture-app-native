cb086ccd51841002fb6b9977a4f3469c5aa5c531
BenStirrup <benjamin.stirrup@gmail.com>
2021-02-15 18:55:28 +0100

    (PC-6582) Redirect to Cultural Survey after signin if user has not filled it already

--- src/features/auth/api.ts
+++ src/features/auth/api.ts
@@ -2,20 +2,22 @@ import { useQuery } from 'react-query'
 
 import { api } from 'api/api'
 import { AccountRequest, GetIdCheckTokenResponse, SigninRequest } from 'api/gen'
+import { isApiError } from 'api/helpers'
 import { useAuthContext, useLoginRoutine } from 'features/auth/AuthContext'
 
-type SignInResponse =
-  | {
-      isSuccess: true
-      content: undefined
-    }
-  | {
-      isSuccess: false
-      content?: {
-        code: 'EMAIL_NOT_VALIDATED' | 'NETWORK_REQUEST_FAILED'
-        general: string[]
-      }
-    }
+export type SignInResponse = SignInResponseSuccess | SignInResponseFailure
+
+export type SignInResponseSuccess = {
+  isSuccess: true
+}
+
+export type SignInResponseFailure = {
+  isSuccess: false
+  content?: {
+    code: 'EMAIL_NOT_VALIDATED' | 'NETWORK_REQUEST_FAILED'
+    general: string[]
+  }
+}
 
 type SignUpResponse =
   | {
@@ -36,14 +38,17 @@ export function useSignIn(): (data: SigninRequest) => Promise<SignInResponse> {
   return async (body: SigninRequest) => {
     try {
       const response = await api.postnativev1signin(body, { credentials: 'omit' })
-      if (!response) return { isSuccess: false }
       await loginRoutine(response, 'fromLogin')
-      return { isSuccess: true }
+      const successResponse: SignInResponseSuccess = { isSuccess: true }
+      return successResponse
     } catch (error) {
-      return {
-        isSuccess: false,
-        content: error.statusCode ? error.content : { code: `NETWORK_REQUEST_FAILED`, general: [] },
+      const errorResponse: SignInResponseFailure = { isSuccess: false }
+      if (isApiError(error)) {
+        errorResponse.content = error.content
+      } else {
+        errorResponse.content = { code: 'NETWORK_REQUEST_FAILED', general: [] }
       }
+      return errorResponse
     }
   }
 }
