819a9bff2e72cd1012183eaf6c26e27a6d117cb0
BenStirrup <benjamin.stirrup@gmail.com>
2021-02-16 09:30:33 +0100

    Fix : replace typedoc on splashscreen.ts

--- src/libs/splashscreen.ts
+++ src/libs/splashscreen.ts
@@ -2,18 +2,18 @@ import { useEffect } from 'react'
 import SplashScreen from 'react-native-splash-screen'
 
 export const DEFAULT_SPLASHSCREEN_DELAY = 200
-/**
- * Hides the splash screen after some delay.
- *
- * @param delay delay in milliseconds after which the splash screen will hide.
- * @param shouldHideSplashScreen boolean used to "lock" the splashscreen until some condition is met.
- */
 
 type Params = {
   delay?: number
   shouldHideSplashScreen?: boolean
 }
 
+/**
+ * Hides the splash screen after some delay.
+ *
+ * @param delay delay in milliseconds after which the splash screen will hide.
+ * @param shouldHideSplashScreen boolean used to "lock" the splashscreen until some condition is met.
+ */
 export function useHideSplashScreen(params?: Params): void {
   const shouldHideSplashScreen =
     params?.shouldHideSplashScreen === undefined ? true : params.shouldHideSplashScreen
