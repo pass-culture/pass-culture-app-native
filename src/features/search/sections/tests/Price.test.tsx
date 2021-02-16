285897b96d05e8dde3cab828f9d3c54c841701ff
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-02-16 11:54:05 +0100

    (PC-6815) Add test: Connect sections price and free offer

--- src/features/search/sections/tests/Price.test.tsx
+++ src/features/search/sections/tests/Price.test.tsx
@@ -16,6 +16,9 @@ jest.mock('features/search/pages/SearchWrapper', () => ({
 }))
 
 describe('Price component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
   it('should render initial price range correctly', () => {
     expect(render(<Price />).queryByText('0 € - 300 €')).toBeTruthy()
     mockSearchState = { ...initialSearchState, priceRange: [20, 200] }
@@ -36,5 +39,43 @@ describe('Price component', () => {
     expect(render(<Price />).queryByText('Prix\xa0(1)')).toBeTruthy()
     mockSearchState = { ...initialSearchState, priceRange: [20, 200] }
     expect(render(<Price />).queryByText('Prix\xa0(1)')).toBeTruthy()
+  })
+
+  it('should be connected to section FreeOffer - FreeOffer is activated when priceRange is [Ø, 0]', () => {
+    mockSearchState.offerIsFree = false
+    const { getByTestId } = render(<Price />)
+    const slider = getByTestId('slider').children[0] as ReactTestInstance
+
+    // 1. Move it to [0, 30]: we should not toggle offerIsFree
+    let dispatchCalledTimes = 1
+    slider.props.onValuesChangeFinish([0, 30])
+    expect(mockStagedDispatch).toHaveBeenCalledTimes(dispatchCalledTimes)
+    expect(mockStagedDispatch).toHaveBeenCalledWith({ type: 'PRICE_RANGE', payload: [0, 30] })
+
+    // 2. Move it to [0, 0]: we should toggle offerIsFree
+    slider.props.onValuesChangeFinish([0, 0])
+    dispatchCalledTimes += 2
+    expect(mockStagedDispatch).toHaveBeenCalledTimes(dispatchCalledTimes)
+    expect(mockStagedDispatch).toHaveBeenCalledWith({ type: 'TOGGLE_OFFER_FREE' })
+    expect(mockStagedDispatch).toHaveBeenCalledWith({ type: 'PRICE_RANGE', payload: [0, 0] })
+  })
+
+  it('should be connected to section FreeOffer - FreeOffer is deactivated when priceRange is not [Ø, 0]', () => {
+    mockSearchState.offerIsFree = true
+    const { getByTestId } = render(<Price />)
+    const slider = getByTestId('slider').children[0] as ReactTestInstance
+
+    // 1. Move it to [0, 0]: we should not toggle offerIsFree, because it is already true
+    slider.props.onValuesChangeFinish([0, 0])
+    let dispatchCalledTimes = 1
+    expect(mockStagedDispatch).toHaveBeenCalledTimes(dispatchCalledTimes)
+    expect(mockStagedDispatch).toHaveBeenCalledWith({ type: 'PRICE_RANGE', payload: [0, 0] })
+
+    // 2. Move it back to [0, 300]: we should toggle offerIsFree
+    slider.props.onValuesChangeFinish([0, 300])
+    dispatchCalledTimes += 2
+    expect(mockStagedDispatch).toHaveBeenCalledTimes(dispatchCalledTimes)
+    expect(mockStagedDispatch).toHaveBeenCalledWith({ type: 'TOGGLE_OFFER_FREE' })
+    expect(mockStagedDispatch).toHaveBeenCalledWith({ type: 'PRICE_RANGE', payload: [0, 300] })
   })
 })
