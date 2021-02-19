f79c3338b6522dc41ef6c3f4bc67192ee23ea95d
BenStirrup <benjamin.stirrup@gmail.com>
2021-02-19 17:00:28 +0100

    (PC-6582) Make AuthContext render its children only when the logged in has been determined

--- src/features/auth/__mocks__/AuthContext.tsx
+++ src/features/auth/__mocks__/AuthContext.tsx
@@ -1,13 +1,11 @@
-import { IAuthContext } from '../AuthContext'
+import { useAuthContext as actualUseAuthContext } from '../AuthContext'
 
 export const setIsLoggedIn = jest.fn()
 
-export const useAuthContext = jest.fn(
-  (): IAuthContext => ({
-    setIsLoggedIn,
-    isLoggedIn: false,
-  })
-)
+export const useAuthContext = jest.fn().mockReturnValue({
+  setIsLoggedIn,
+  isLoggedIn: false,
+}) as jest.MockedFunction<typeof actualUseAuthContext>
 
 export const signOut = jest.fn()
 
