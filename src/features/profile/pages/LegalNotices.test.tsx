5fa756667f24948f4ef7a77f82048632dee4aec5
blixit <celestinya@gmail.com>
2021-02-19 10:40:31 +0100

    (PC-6206) Add call to support when deleting account

--- src/features/profile/pages/LegalNotices.test.tsx
+++ src/features/profile/pages/LegalNotices.test.tsx
@@ -1,10 +1,23 @@
 import { render, act, fireEvent } from '@testing-library/react-native'
 import React from 'react'
+import { UseQueryResult } from 'react-query'
 
+import { UserProfileResponse } from 'api/gen'
+import { contactSupport } from 'features/auth/support.services'
 import * as NavigationHelpers from 'features/navigation/helpers'
 import { flushAllPromises } from 'tests/utils'
 
 import { LegalNotices } from './LegalNotices'
+
+jest.mock('features/home/api', () => ({
+  useUserProfileInfo: jest.fn(
+    () =>
+      ({
+        isLoading: false,
+        data: { email: 'email2@domain.ext', firstName: 'Jean', isBeneficiary: false },
+      } as UseQueryResult<UserProfileResponse>)
+  ),
+}))
 
 async function renderProfile() {
   const wrapper = render(<LegalNotices />)
@@ -32,5 +45,13 @@ describe('LegalNotices', () => {
     fireEvent.press(row)
 
     expect(openExternalUrl).toBeCalledWith('https://pass.culture.fr/donnees-personnelles/')
+  })
+  it('should open email interface when the account-deletion row is clicked', async () => {
+    const { getByTestId } = await renderProfile()
+
+    const row = getByTestId('row-account-deletion')
+    fireEvent.press(row)
+
+    expect(contactSupport.forAccountDeletion).toBeCalledWith('email2@domain.ext')
   })
 })
--- src/features/profile/pages/LegalNotices.tsx
+++ src/features/profile/pages/LegalNotices.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import styled from 'styled-components/native'
 
+import { contactSupport } from 'features/auth/support.services'
+import { useUserProfileInfo } from 'features/home/api'
 import { openExternalUrl } from 'features/navigation/helpers'
 import { _ } from 'libs/i18n'
 import { ExternalSite } from 'ui/svg/icons/ExternalSite'
@@ -14,6 +16,7 @@ import { Separator } from '../components/reusables'
 import { SectionRow } from '../components/SectionRow'
 
 export function LegalNotices() {
+  const { data: user } = useUserProfileInfo()
   return (
     <View>
       <ProfileHeaderWithNavigation title={_(t`Mentions légales`)} />
@@ -35,17 +38,19 @@ export function LegalNotices() {
           style={styles.row}
           testID="row-data-privacy-chart"
         />
-        <Separator />
-        <Row
-          title={_(t`Suppression du compte`)}
-          type="clickable"
-          onPress={() => {
-            // TODO: 6206
-          }}
-          icon={ProfileDeletion}
-          style={styles.row}
-          testID="row-account-deletion"
-        />
+        {user && (
+          <React.Fragment>
+            <Separator />
+            <Row
+              title={_(t`Suppression du compte`)}
+              type="clickable"
+              onPress={() => contactSupport.forAccountDeletion(user.email)}
+              icon={ProfileDeletion}
+              style={styles.row}
+              testID="row-account-deletion"
+            />
+          </React.Fragment>
+        )}
       </Container>
     </View>
   )
@@ -59,7 +64,7 @@ const styles = StyleSheet.create({
 
 const Container = styled.View({
   flexDirection: 'column',
-  paddingHorizontal: getSpacing(5),
+  paddingHorizontal: getSpacing(4),
 })
 
 const Row = styled(SectionRow).attrs({
