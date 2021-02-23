9616dd3a9715f2a3388bdf0e12ed6d535e1486eb
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-02-23 11:41:29 +0100

    (PC-6717) Add designed component BookingImpossible

---
+++ src/features/bookOffer/components/BookingImpossible.tsx
@@ -0,0 +1,46 @@
+import { t } from '@lingui/macro'
+import React from 'react'
+import styled from 'styled-components/native'
+
+import { _ } from 'libs/i18n'
+import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { ButtonTertiary } from 'ui/components/buttons/ButtonTertiary'
+import { SadFace } from 'ui/svg/icons/SadFace'
+import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
+
+interface Props {
+  dismissModal: () => void
+}
+
+export const BookingImpossible: React.FC<Props> = ({ dismissModal }) => (
+  <Container>
+    <SadFace size={getSpacing(17)} color={ColorsEnum.GREY_DARK} />
+    <Spacer.Column numberOfSpaces={6} />
+
+    <Content>
+      {_(
+        t`Les conditions générales d'utilisation de l'App Store iOs ne permettent pas de réserver cette offre sur l'application.`
+      )}
+    </Content>
+    <Spacer.Column numberOfSpaces={6} />
+    <Content>
+      {_(
+        t`Ajoute cette offre à tes favoris et rends-toi vite sur le site pass Culture afin de la réserver.`
+      )}
+    </Content>
+
+    <Spacer.Column numberOfSpaces={6} />
+
+    <ButtonPrimary title={_(t`Mettre en favoris`)} />
+    <Spacer.Column numberOfSpaces={4} />
+    <ButtonTertiary title={_(t`Retourner à l'offre`)} onPress={dismissModal} />
+  </Container>
+)
+
+const Container = styled.View({
+  width: '100%',
+  justifyContent: 'center',
+  alignContent: 'center',
+  alignItems: 'center',
+})
+const Content = styled(Typo.Body)({ textAlign: 'center', paddingHorizontal: getSpacing(6) })
