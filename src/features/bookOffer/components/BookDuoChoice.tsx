f771833e8c97ca8a27a3c9067709ff8795b70cfc
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-02-23 16:39:10 +0100

    Add quantity to book offer reducer

--- src/features/bookOffer/components/BookDuoChoice.tsx
+++ src/features/bookOffer/components/BookDuoChoice.tsx
@@ -4,11 +4,21 @@ import React from 'react'
 import { _ } from 'libs/i18n'
 import { Typo } from 'ui/theme'
 
+import { useBooking } from '../pages/BookingOfferWrapper'
+
 export const BookDuoChoice: React.FC = () => {
+  const { bookingState, dispatch } = useBooking()
+
+  const toggleQuantity = () => {
+    dispatch({ type: 'SELECT_QUANTITY', payload: bookingState.quantity === 1 ? 2 : 1 })
+  }
+
   return (
     <React.Fragment>
       <Typo.Title4>{_(t`Nombre de place`)}</Typo.Title4>
-      <Typo.ButtonText>{_(t`Duo`)}</Typo.ButtonText>
+      <Typo.ButtonText onPress={toggleQuantity}>
+        {bookingState.quantity === 1 ? _(t`Solo`) : _(t`Duo`)}
+      </Typo.ButtonText>
     </React.Fragment>
   )
 }
--- src/features/bookOffer/pages/reducer.ts
+++ src/features/bookOffer/pages/reducer.ts
@@ -9,17 +9,20 @@ export type BookingState = {
   offerId: number | undefined
   stockId: number | undefined
   step: Step | undefined
+  quantity: 1 | 2
 }
 
 export const initialBookingState: BookingState = {
   offerId: undefined,
   stockId: undefined,
   step: undefined,
+  quantity: 1,
 }
 
 export type Action =
   | { type: 'INIT'; payload: Partial<BookingState> }
   | { type: 'VALIDATE_OPTIONS' }
+  | { type: 'SELECT_QUANTITY'; payload: 1 | 2 }
   | { type: 'MODIFY_OPTIONS' }
   | { type: 'SELECT_STOCK'; payload: number }
 
@@ -31,6 +34,8 @@ export const searchReducer = (state: BookingState, action: Action): BookingState
       return { ...state, step: Step.CONFIRMATION }
     case 'SELECT_STOCK':
       return { ...state, stockId: action.payload }
+    case 'SELECT_QUANTITY':
+      return { ...state, quantity: action.payload }
     case 'MODIFY_OPTIONS':
       return { ...state, step: Step.DUO }
     default:
