02849e2920bae4762c13473b636852bc061662c1
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-02-23 16:48:54 +0100

    Only enable quantity>1 for duo offers

--- src/features/bookOffer/components/BookDuoChoice.tsx
+++ src/features/bookOffer/components/BookDuoChoice.tsx
@@ -1,13 +1,17 @@
 import { t } from '@lingui/macro'
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 
+import { useOffer } from 'features/offer/api/useOffer'
 import { _ } from 'libs/i18n'
 import { Typo } from 'ui/theme'
+import { ACTIVE_OPACITY } from 'ui/theme/colors'
 
 import { useBooking } from '../pages/BookingOfferWrapper'
 
 export const BookDuoChoice: React.FC = () => {
   const { bookingState, dispatch } = useBooking()
+  const { data: offer } = useOffer({ offerId: bookingState.offerId || 0 })
 
   const toggleQuantity = () => {
     dispatch({ type: 'SELECT_QUANTITY', payload: bookingState.quantity === 1 ? 2 : 1 })
@@ -16,9 +20,12 @@ export const BookDuoChoice: React.FC = () => {
   return (
     <React.Fragment>
       <Typo.Title4>{_(t`Nombre de place`)}</Typo.Title4>
-      <Typo.ButtonText onPress={toggleQuantity}>
-        {bookingState.quantity === 1 ? _(t`Solo`) : _(t`Duo`)}
-      </Typo.ButtonText>
+      <TouchableOpacity
+        activeOpacity={ACTIVE_OPACITY}
+        onPress={toggleQuantity}
+        disabled={!offer?.isDuo}>
+        <Typo.ButtonText>{bookingState.quantity === 1 ? _(t`Solo`) : _(t`Duo`)}</Typo.ButtonText>
+      </TouchableOpacity>
     </React.Fragment>
   )
 }
