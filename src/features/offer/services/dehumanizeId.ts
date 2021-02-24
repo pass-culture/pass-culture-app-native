08a570ab4d215aa0f19692324bd96ba2f70653de
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-02-24 18:48:28 +0100

    (PC-7274) Add fonction to humanizeIds

--- src/features/offer/services/dehumanizeId.ts
+++ src/features/offer/services/dehumanizeId.ts
@@ -1,4 +1,4 @@
-import { decode } from 'hi-base32'
+import { decode, encode } from 'hi-base32'
 
 export const dehumanizeId = (humanId: string) => {
   try {
@@ -8,9 +8,18 @@ export const dehumanizeId = (humanId: string) => {
   }
 }
 
-const byteArrayToInt = (bytes: number[]) => {
-  return bytes.reduce(
-    (result, byte, index) => result + byte * Math.pow(256, bytes.length - 1 - index),
-    0
-  )
+const byteArrayToInt = (bytes: number[]) =>
+  bytes.reduce((result, byte, index) => result + byte * Math.pow(256, bytes.length - 1 - index), 0)
+
+const intToByteArray = (x: number): number[] => [x << 8, x << 16, x << 24].map((z) => z >>> 24)
+
+export const humanizeId = (dehumanizedId: number) => {
+  try {
+    return encode(intToByteArray(dehumanizedId))
+      .replace(/O/g, '8')
+      .replace(/I/g, '9')
+      .replace(/=/g, '')
+  } catch {
+    return null
+  }
 }
