2eca50525555a4894091916a93c6055a556bc356
Emma Jamin <emmajamin@orange.fr>
2021-02-25 14:49:36 +0100

    (PC-7091) Use functions in calendar to render correctly each date

--- src/features/bookOffer/components/Calendar/Calendar.tsx
+++ src/features/bookOffer/components/Calendar/Calendar.tsx
@@ -3,9 +3,12 @@ import { View } from 'react-native'
 import { Calendar as RNCalendar, LocaleConfig } from 'react-native-calendars'
 import styled from 'styled-components/native'
 
+import { OfferStockResponse } from 'api/gen'
 import { ArrowNext } from 'ui/svg/icons/ArrowNext'
 import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
 import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
+
+import { getStocksByDate, OfferStatus, getDateStatusAndPrice } from '../BookDateChoice'
 
 import { monthNames, monthNamesShort, dayNames, dayNamesShort } from './Calendar.utils'
 import { DiagonalStripe } from './DiagonalStripe'
@@ -19,15 +22,15 @@ LocaleConfig.locales['fr'] = {
 }
 LocaleConfig.defaultLocale = 'fr'
 
-const renderDay = (bookable: boolean, notBookable: boolean, selected: boolean, day: number) => {
-  if (bookable) return <Day color={ColorsEnum.PRIMARY}>{day}</Day>
+const renderDay = (status: OfferStatus, selected: boolean, day: number) => {
+  if (status === OfferStatus.BOOKABLE) return <Day color={ColorsEnum.PRIMARY}>{day}</Day>
   if (selected)
     return (
       <SelectedDay>
         <SelectedDayNumber color={ColorsEnum.WHITE}>{day}</SelectedDayNumber>
       </SelectedDay>
     )
-  if (notBookable)
+  if (status === OfferStatus.NOTBOOKABLE)
     return (
       <DiagonalStripe>
         <Day color={ColorsEnum.GREY_DARK}>{day}</Day>
@@ -42,42 +45,52 @@ const renderArrow = (direction: string) => {
   return <React.Fragment />
 }
 
-export const Calendar: React.FC = () => (
-  <RNCalendar
-    firstDay={1}
-    enableSwipeMonths={true}
-    renderHeader={(date) => <MonthHeader date={date} />}
-    hideExtraDays={true}
-    renderArrow={renderArrow}
-    dayComponent={({ date }) => {
-      const bookable = date.day === 21 || date.day === 3
-      // TODO: PC-6695 change hard coded for real data
-      const displayPrice =
-        date.day === 20 ||
-        date.day === 21 ||
-        date.day === 2 ||
-        date.day === 3 ||
-        (date.day === 11 && date.month === 2)
-      // TODO: PC-6716 change hard coded for real data
-      const notBookable = date.day === 20 || date.day === 2
-      // TODO: PC-6698 change hard coded for real data
-      const selected = date.day === 11 && date.month === 2
-      return (
-        <View>
-          {renderDay(bookable, notBookable, selected, date.day)}
-          {displayPrice ? (
-            // eslint-disable-next-line react-native/no-raw-text
-            <Typo.Caption color={notBookable ? ColorsEnum.GREY_DARK : ColorsEnum.PRIMARY}>
-              19,90€
-            </Typo.Caption>
-          ) : (
-            <Spacer.Column numberOfSpaces={getSpacing(1)} />
-          )}
-        </View>
-      )
-    }}
-  />
-)
+interface Props {
+  stocks: OfferStockResponse[]
+}
+
+export const Calendar: React.FC<Props> = ({ stocks }) => {
+  const stocksDate = getStocksByDate(stocks)
+  return (
+    <RNCalendar
+      firstDay={1}
+      enableSwipeMonths={true}
+      renderHeader={(date) => <MonthHeader date={date} />}
+      hideExtraDays={true}
+      renderArrow={renderArrow}
+      dayComponent={({ date }) => {
+        const dateStatusAndPrice = getDateStatusAndPrice(new Date(date.timestamp), stocksDate)
+        // TODO: PC-6695 change hard coded for real data
+        const displayPrice =
+          date.day === 20 ||
+          date.day === 21 ||
+          date.day === 2 ||
+          date.day === 3 ||
+          (date.day === 11 && date.month === 2)
+        // TODO: PC-6698 change hard coded for real data
+        const selected = date.day === 11 && date.month === 2
+        return (
+          <View>
+            {renderDay(dateStatusAndPrice.status, selected, date.day)}
+            {displayPrice ? (
+              <Typo.Caption
+                color={
+                  dateStatusAndPrice.status === OfferStatus.NOTBOOKABLE
+                    ? ColorsEnum.GREY_DARK
+                    : ColorsEnum.PRIMARY
+                  // eslint-disable-next-line react-native/no-raw-text
+                }>
+                19,90€
+              </Typo.Caption>
+            ) : (
+              <Spacer.Column numberOfSpaces={getSpacing(1)} />
+            )}
+          </View>
+        )
+      }}
+    />
+  )
+}
 
 const Day = styled(Typo.ButtonText)({
   textAlign: 'center',
