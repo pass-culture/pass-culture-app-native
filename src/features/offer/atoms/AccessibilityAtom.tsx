49040490cc53d2dd74418404ca0bef87d40ccb07
Justine Mignot <justinem@bam.tech>
2021-02-25 10:41:59 +0100

    :bug: (PC-6754) accessbility render fine on all devices (little, big)
    there was a bug on OnePlus 7 pro, texts was very little

--- src/features/offer/atoms/AccessibilityAtom.tsx
+++ src/features/offer/atoms/AccessibilityAtom.tsx
@@ -2,18 +2,15 @@ import React from 'react'
 import styled from 'styled-components/native'
 
 import { ValidationMark } from 'ui/components/ValidationMark'
-import { useElementWidth } from 'ui/hooks/useElementWidth'
 import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
 import { BorderRadiusEnum } from 'ui/theme/grid'
 
 import { getIconAndWording, HandicapCategory } from './AccessibilityAtom.service'
 
-const HandicapText: React.FC<{ wording: string; width: number }> = ({ wording, width }) => (
-  <TextContainer testID={wording} width={width}>
+const HandicapText: React.FC<{ wording: string }> = ({ wording }) => (
+  <TextContainer testID={wording}>
     {wording.split(' ').map((word) => (
-      <Text key={word} numberOfLines={1} adjustsFontSizeToFit={true} allowFontScaling={false}>
-        {word}
-      </Text>
+      <Text key={word}>{word}</Text>
     ))}
   </TextContainer>
 )
@@ -21,16 +18,19 @@ const HandicapText: React.FC<{ wording: string; width: number }> = ({ wording, w
 interface Props {
   handicap: HandicapCategory
   isAccessible: boolean
-  sideSpace: number
+  rightSpacingValue?: number
 }
 
-export const AccessibilityAtom: React.FC<Props> = ({ handicap, isAccessible, sideSpace }) => {
+export const AccessibilityAtom: React.FC<Props> = ({
+  handicap,
+  isAccessible,
+  rightSpacingValue = 0,
+}) => {
   const { Icon, wording } = getIconAndWording(handicap)
-  const { width, onLayout } = useElementWidth()
 
   return (
-    <Container>
-      <Frame testID="accessibilityFrame" onLayout={onLayout}>
+    <Container rightSpacingValue={rightSpacingValue} testID="accessibilityAtomContainer">
+      <Frame testID="accessibilityFrame">
         <Spacer.Flex />
         <Icon size={getSpacing(12)} />
         <Spacer.Flex />
@@ -44,7 +44,7 @@ export const AccessibilityAtom: React.FC<Props> = ({ handicap, isAccessible, sid
         </ValidationContainer>
       </Frame>
       <Spacer.Column numberOfSpaces={4} />
-      <HandicapText wording={wording} width={(width ?? 0) + sideSpace} />
+      <HandicapText wording={wording} />
     </Container>
   )
 }
@@ -57,19 +57,18 @@ const Frame = styled.View({
   borderWidth: 1,
 })
 
-const Container = styled.View({
+const Container = styled.View<{ rightSpacingValue: number }>(({ rightSpacingValue }) => ({
   flexDirection: 'column',
   flexShrink: 1,
-})
-const Text = styled(Typo.Caption)({ textAlign: 'center', paddingHorizontal: 1 })
-const TextContainer = styled.View<{ width: number }>(({ width }) => ({
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
-  width,
-  alignSelf: 'center',
-  marginHorizontal: getSpacing(2),
+  marginRight: rightSpacingValue,
 }))
+
+const TextContainer = styled.View({
+  marginHorizontal: -getSpacing(1),
+})
+
+const Text = styled(Typo.Caption)({ textAlign: 'center', paddingHorizontal: 1 })
+
 const ValidationContainer = styled.View({
   position: 'absolute',
   bottom: -getSpacing(4),
