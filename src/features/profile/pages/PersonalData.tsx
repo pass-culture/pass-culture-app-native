a0cb8af6b0ac2d6ba49c985628062b76b26aca8f
blixit <celestinya@gmail.com>
2021-02-26 17:18:32 +0100

    (PC-6157) Display real user data

--- src/features/profile/pages/PersonalData.tsx
+++ src/features/profile/pages/PersonalData.tsx
@@ -2,6 +2,7 @@ import { t } from '@lingui/macro'
 import React from 'react'
 import styled from 'styled-components/native'
 
+import { useUserProfileInfo } from 'features/home/api'
 import { _ } from 'libs/i18n'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 
@@ -9,28 +10,40 @@ import { ProfileHeaderWithNavigation } from '../components/ProfileHeaderWithNavi
 import { ProfileContainer, Separator } from '../components/reusables'
 
 export function PersonalData() {
+  const { data: user } = useUserProfileInfo()
+
+  const fullname = String(user?.firstName + ' ' + user?.lastName).trim()
+
   return (
     <React.Fragment>
       <ProfileHeaderWithNavigation title={_(t`Informations personnelles`)} />
       <Container>
-        <Row>
-          <Typo.Caption>{_(t`Prénom et nom`)}</Typo.Caption>
-          <Spacer.Column numberOfSpaces={2} />
-          <Typo.Body>{_(t`Rosa Bonheur`)}</Typo.Body>
-        </Row>
-        <Separator />
+        {user?.isBeneficiary && (
+          <React.Fragment>
+            <Row>
+              <Typo.Caption>{_(t`Prénom et nom`)}</Typo.Caption>
+              <Spacer.Column numberOfSpaces={2} />
+              <Typo.Body>{fullname}</Typo.Body>
+            </Row>
+            <Separator />
+          </React.Fragment>
+        )}
         <Row>
           <Typo.Caption>{_(t`E-mail`)}</Typo.Caption>
           <Spacer.Column numberOfSpaces={2} />
-          <Typo.Body>{_(t`Rosa.bonheur@gmail.com`)}</Typo.Body>
+          <Typo.Body>{user?.email}</Typo.Body>
         </Row>
         <Separator />
-        <Row>
-          <Typo.Caption>{_(t`Numéro de téléphone`)}</Typo.Caption>
-          <Spacer.Column numberOfSpaces={2} />
-          <Typo.Body>{_(t`+33 6 12 34 56 78`)}</Typo.Body>
-        </Row>
-        <Separator />
+        {user?.isBeneficiary && (
+          <React.Fragment>
+            <Row>
+              <Typo.Caption>{_(t`Numéro de téléphone`)}</Typo.Caption>
+              <Spacer.Column numberOfSpaces={2} />
+              <Typo.Body>{user?.phoneNumber}</Typo.Body>
+            </Row>
+            <Separator />
+          </React.Fragment>
+        )}
       </Container>
     </React.Fragment>
   )
@@ -38,6 +51,7 @@ export function PersonalData() {
 
 const Container = styled(ProfileContainer)({
   padding: getSpacing(4),
+  paddingTop: 0,
 })
 
 const Row = styled.View({
