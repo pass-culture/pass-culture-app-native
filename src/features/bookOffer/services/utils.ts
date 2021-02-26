64c3b19e8f29d70dc4885b2fca92a2a125d04782
Emma Jamin <emmajamin@orange.fr>
2021-02-26 10:37:57 +0100

    (PC-6695) Add price in getDateStatusAndPrice

--- src/features/bookOffer/services/utils.ts
+++ src/features/bookOffer/services/utils.ts
@@ -1,5 +1,8 @@
 import { OfferStockResponse } from 'api/gen'
 import { formatToSlashedFrenchDate } from 'libs/dates'
+import { CENTS_IN_EURO } from 'libs/parsers/pricesConversion'
+
+const EURO_SYMBOL = '€'
 
 export const getStocksByDate = (
   stocks: OfferStockResponse[]
@@ -24,13 +27,24 @@ export enum OfferStatus {
   NOT_OFFERED = 'NOTOFFERED',
 }
 
+export const formatToFrenchDecimalWithoutSpace = (cents: number) => {
+  const euros = cents / CENTS_IN_EURO
+  // we show 2 decimals if price is not round. Ex: 21,50€
+  const fixed = euros === Math.floor(euros) ? euros : euros.toFixed(2)
+  return `${fixed.toString().replace('.', ',')}${EURO_SYMBOL}`
+}
+
 export const getDateStatusAndPrice = (
   date: Date,
   stocksDates: { [date: string]: OfferStockResponse[] }
-): { status: OfferStatus } => {
+): { status: OfferStatus; price: string | null } => {
   const stocksByDate = stocksDates[formatToSlashedFrenchDate(date.toString())]
-  if (!stocksByDate) return { status: OfferStatus.NOT_OFFERED }
+  if (!stocksByDate) return { status: OfferStatus.NOT_OFFERED, price: null }
+
+  const prices = stocksByDate.map((stock) => stock.price)
+  const price = formatToFrenchDecimalWithoutSpace(Math.min(...prices))
   const offerIsBookable = stocksByDate.some((stock) => stock.isBookable)
-  if (offerIsBookable) return { status: OfferStatus.BOOKABLE }
-  return { status: OfferStatus.NOT_BOOKABLE }
+
+  if (offerIsBookable) return { status: OfferStatus.BOOKABLE, price }
+  return { status: OfferStatus.NOT_BOOKABLE, price }
 }
