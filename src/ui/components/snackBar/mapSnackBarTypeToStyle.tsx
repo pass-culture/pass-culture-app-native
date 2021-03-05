c11f0cc9020dacd3df1f8d8e63b3a47b49999ad0
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-03-05 17:10:34 +0100

    (PC-6407) cchange snackbar API and use types of snackbars

---
+++ src/ui/components/snackBar/mapSnackBarTypeToStyle.tsx
@@ -0,0 +1,33 @@
+import { Check } from 'ui/svg/icons/Check'
+import { Warning } from 'ui/svg/icons/Warning'
+import { ColorsEnum } from 'ui/theme'
+
+import { SnackBarSettings, SnackBarType } from './types'
+
+export const mapSnackBarTypeToStyle = (
+  type: SnackBarType
+): Pick<SnackBarSettings, 'icon' | 'color' | 'backgroundColor' | 'progressBarColor'> => {
+  switch (type) {
+    case SnackBarType.SUCCESS:
+      return {
+        icon: Check,
+        backgroundColor: ColorsEnum.GREEN_VALID,
+        progressBarColor: ColorsEnum.GREEN_LIGHT,
+        color: ColorsEnum.WHITE,
+      }
+    case SnackBarType.ERROR:
+      return {
+        icon: Warning,
+        backgroundColor: ColorsEnum.ERROR,
+        progressBarColor: ColorsEnum.PRIMARY_DISABLED,
+        color: ColorsEnum.WHITE,
+      }
+    default:
+      return {
+        icon: undefined,
+        backgroundColor: ColorsEnum.TRANSPARENT,
+        progressBarColor: ColorsEnum.TRANSPARENT,
+        color: ColorsEnum.WHITE,
+      }
+  }
+}
--- src/ui/components/snackBar/types.ts
+++ src/ui/components/snackBar/types.ts
@@ -1,7 +1,12 @@
 import { SnackBarProps } from './SnackBar'
 
+export enum SnackBarType {
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
+
 export type SnackBarSettings = Omit<SnackBarProps, 'visible' | 'refresher'>
 export type SnackBarHelperSettings = Omit<
   SnackBarSettings,
   'icon' | 'color' | 'backgroundColor' | 'progressBarColor'
->
+> & { type: SnackBarType }
