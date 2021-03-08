985ab593967efad54651e8dc0cfbb9e17c9949b7
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-03-08 12:13:38 +0100

    (PC-6721) API call to book an offer

--- src/features/bookOffer/components/BookingDetails.tsx
+++ src/features/bookOffer/components/BookingDetails.tsx
@@ -8,9 +8,11 @@ import { _ } from 'libs/i18n'
 import { formatToFrenchDecimal } from 'libs/parsers'
 import { Banner } from 'ui/components/Banner'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
 import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
 
 import { useBooking, useBookingStock } from '../pages/BookingOfferWrapper'
+import { useBookOfferMutation } from '../services/useBookOfferMutation'
 
 import { BookingInformations } from './BookingInformations'
 import { CancellationDetails } from './CancellationDetails'
@@ -27,11 +29,28 @@ export const BookingDetails: React.FC<Props> = ({ dismissModal }) => {
   const { navigate } = useNavigation<UseNavigationType>()
   const { bookingState } = useBooking()
   const stock = useBookingStock()
+  const { showErrorSnackBar } = useSnackBarContext()
+
+  const { mutate } = useBookOfferMutation({
+    onSuccess: () => {
+      dismissModal()
+      navigate('BookingConfirmation')
+    },
+    onError: () => {
+      showErrorSnackBar({
+        message: _(t`Erreur. L'offre n'a pas été réservée !`),
+        timeout: SNACK_BAR_TIME_OUT,
+      })
+    },
+  })
 
   if (!stock) return <React.Fragment />
 
   const price =
     stock.price > 0 ? formatToFrenchDecimal(bookingState.quantity * stock.price) : undefined
+
+  const onPressBookOffer = () =>
+    mutate({ quantity: bookingState.quantity, stockId: stock.id.toString() })
 
   return (
     <Container>
@@ -48,13 +67,7 @@ export const BookingDetails: React.FC<Props> = ({ dismissModal }) => {
 
       <Spacer.Column numberOfSpaces={6} />
 
-      <ButtonPrimary
-        title={_(t`Confirmer la réservation`)}
-        onPress={() => {
-          dismissModal()
-          navigate('BookingConfirmation')
-        }}
-      />
+      <ButtonPrimary title={_(t`Confirmer la réservation`)} onPress={onPressBookOffer} />
       {price ? (
         <Caption>{_(t`${price} seront déduits de ton crédit pass Culture`)}</Caption>
       ) : (
