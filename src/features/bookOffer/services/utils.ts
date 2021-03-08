6ce27e89609099c111b5d3143b03c8943cfd5c14
Justine Mignot <justinem@bam.tech>
2021-03-08 11:46:20 +0100

    :bug: (PC-7451) if equal, credit is enough

--- src/features/bookOffer/services/utils.ts
+++ src/features/bookOffer/services/utils.ts
@@ -36,7 +36,7 @@ export const getDateStatusAndPrice = (
 
   const price = getOfferPrice(stocksByDate)
   const offerIsBookable = stocksByDate.some((stock) => stock.isBookable)
-  const hasEnoughCredit = userRemainingCredit ? userRemainingCredit > price : false
+  const hasEnoughCredit = userRemainingCredit ? userRemainingCredit >= price : false
   const formattedPrice = formatToFrenchDecimal(price).replace(' ', '')
 
   if (offerIsBookable && hasEnoughCredit)
