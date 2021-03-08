010cee93c9519a9130d649d33f553d7bb59a46c8
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-03-08 12:58:20 +0100

    update bookingConfirmation snapshot

--- src/features/bookOffer/pages/__tests__/BookingConfirmation.test.tsx
+++ src/features/bookOffer/pages/__tests__/BookingConfirmation.test.tsx
@@ -1,11 +1,17 @@
 import { render } from '@testing-library/react-native'
 import React from 'react'
 
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+
 import { BookingConfirmation } from '../BookingConfirmation'
+
+jest.mock('features/home/services/useAvailableCredit', () => ({
+  useAvailableCredit: jest.fn(() => ({ isExpired: false, amount: 2000 })),
+}))
 
 describe('<BookingConfirmation />', () => {
   it('should render correctly', () => {
-    const page = render(<BookingConfirmation />)
+    const page = render(reactQueryProviderHOC(<BookingConfirmation />))
     expect(page).toMatchSnapshot()
   })
 })
