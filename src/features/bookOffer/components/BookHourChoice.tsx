6ea5f9f7721866728d9cb35c83654b99d3b0a3a9
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-03-08 20:08:16 +0100

    (PC-6701) Show available hours according to selected date

--- src/features/bookOffer/components/BookHourChoice.tsx
+++ src/features/bookOffer/components/BookHourChoice.tsx
@@ -1,31 +1,47 @@
 import { t } from '@lingui/macro'
-import React from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import React, { useEffect } from 'react'
+import { TouchableOpacity } from 'react-native'
 
 import { useBooking, useBookingOffer } from 'features/bookOffer/pages/BookingOfferWrapper'
+import { formatHour, formatToKeyDate } from 'features/bookOffer/services/utils'
 import { _ } from 'libs/i18n'
 import { Typo, Spacer } from 'ui/theme'
+import { ACTIVE_OPACITY } from 'ui/theme/colors'
 
 import { Step } from '../pages/reducer'
 
 export const BookHourChoice: React.FC = () => {
-  const { dispatch } = useBooking()
+  const { bookingState, dispatch } = useBooking()
   const offer = useBookingOffer()
 
-  const goToNextStep = () => {
-    //TODO 6705: add dispatch hour choice in state
+  const { stocks = [] } = useBookingOffer() || {}
+  const selectedDate = bookingState.date ? formatToKeyDate(bookingState.date) : undefined
+
+  const selectStock = (stockId: number) => {
+    dispatch({ type: 'SELECT_STOCK', payload: stockId })
     if (offer?.isDuo) dispatch({ type: 'CHANGE_STEP', payload: Step.DUO })
   }
 
+  const filteredStocks = stocks.filter(({ beginningDatetime }) =>
+    selectedDate && beginningDatetime ? formatToKeyDate(beginningDatetime) === selectedDate : false
+  )
+
+  useEffect(() => {
+    if (filteredStocks.length === 1) selectStock(filteredStocks[0].id)
+  }, [filteredStocks.length])
+
   return (
     <React.Fragment>
-      <Typo.Title4 testID="HourStep">{_(t`Heure`)}</Typo.Title4>
-      <Spacer.Column numberOfSpaces={2} />
-      <Typo.ButtonText>{_(t`21:00`)}</Typo.ButtonText>
-      <TouchableOpacity onPress={goToNextStep}>
-        {/* TODO 6705: Remove this line */}
-        <Text>Simuler choix horaire</Text>
-      </TouchableOpacity>
+      <Typo.Title4>{_(t`Heure`)}</Typo.Title4>
+      {filteredStocks.map((stock) => (
+        <TouchableOpacity
+          activeOpacity={ACTIVE_OPACITY}
+          key={stock.id}
+          onPress={() => selectStock(stock.id)}>
+          <Spacer.Column numberOfSpaces={2} />
+          <Typo.ButtonText>{formatHour(stock.beginningDatetime)}</Typo.ButtonText>
+        </TouchableOpacity>
+      ))}
     </React.Fragment>
   )
 }
