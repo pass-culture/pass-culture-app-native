984a4434d13b4259df35fc86bf5e233bce369ca7
Emma Jamin <emmajamin@orange.fr>
2021-03-08 11:43:03 +0100

    (PC-6698) add spacer in section

--- src/features/bookOffer/components/BookDateChoice.tsx
+++ src/features/bookOffer/components/BookDateChoice.tsx
@@ -30,6 +30,7 @@ export const BookDateChoice: React.FC<Props> = ({ stocks, userRemainingCredit })
         <TouchableOpacity
           activeOpacity={ACTIVE_OPACITY}
           onPress={() => dispatch({ type: 'CHANGE_STEP', payload: Step.DATE })}>
+          <Spacer.Column numberOfSpaces={2} />
           <Typo.ButtonText>
             {bookingState.date ? formatToCompleteFrenchDate(bookingState.date) : ''}
           </Typo.ButtonText>
--- src/features/bookOffer/components/BookHourChoice.tsx
+++ src/features/bookOffer/components/BookHourChoice.tsx
@@ -2,12 +2,13 @@ import { t } from '@lingui/macro'
 import React from 'react'
 
 import { _ } from 'libs/i18n'
-import { Typo } from 'ui/theme'
+import { Typo, Spacer } from 'ui/theme'
 
 export const BookHourChoice: React.FC = () => {
   return (
     <React.Fragment>
       <Typo.Title4>{_(t`Heure`)}</Typo.Title4>
+      <Spacer.Column numberOfSpaces={2} />
       <Typo.ButtonText>{_(t`21:00`)}</Typo.ButtonText>
     </React.Fragment>
   )
