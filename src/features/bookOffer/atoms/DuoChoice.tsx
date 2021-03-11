cfcaa7f13caf521fbbf680350718b5224a7f7dc4
Justine Mignot <justinem@bam.tech>
2021-03-11 14:45:10 +0100

    :recycle: (PC-6706) centralize quantity logic

--- src/features/bookOffer/atoms/DuoChoice.tsx
+++ src/features/bookOffer/atoms/DuoChoice.tsx
@@ -1,32 +1,25 @@
-import { t } from '@lingui/macro'
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 
-import { _ } from 'libs/i18n'
-import { DuoPerson } from 'ui/svg/icons/DuoPerson'
-import { Profile } from 'ui/svg/icons/Profile'
+import { IconInterface } from 'ui/svg/icons/types'
 import { ColorsEnum, getSpacing, Typo } from 'ui/theme'
 
 import { ChoiceBloc } from './ChoiceBloc'
 
 interface Props {
-  quantity: 1 | 2
+  title: string
   price: string
   selected: boolean
+  icon: FunctionComponent<IconInterface>
   onPress: () => void
 }
 
-export const DuoChoice: React.FC<Props> = ({ quantity, price, selected, onPress }) => {
+export const DuoChoice: React.FC<Props> = ({ title, price, selected, icon: Icon, onPress }) => {
   const textColor = selected ? ColorsEnum.WHITE : ColorsEnum.BLACK
-  const title = quantity === 1 ? _(t`Solo`) : _(t`Duo`)
   return (
     <ChoiceBloc onPress={onPress} selected={selected}>
       <Container>
-        {quantity === 1 ? (
-          <Profile color={textColor} size={28} />
-        ) : (
-          <DuoPerson color={textColor} size={28} />
-        )}
+        <Icon color={textColor} size={28} />
         <Typo.ButtonText color={textColor}>{title}</Typo.ButtonText>
 
         <Typo.Caption testID="price" color={textColor}>
