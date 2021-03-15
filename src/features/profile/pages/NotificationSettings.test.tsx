3db6da133ac5f745b6be2d80852b8f6e88fd5b81
blixit <celestinya@gmail.com>
2021-03-15 17:13:16 +0100

    (PC-7445) Hide the save button for unauthenticated users

--- src/features/profile/pages/NotificationSettings.test.tsx
+++ src/features/profile/pages/NotificationSettings.test.tsx
@@ -140,8 +140,8 @@ describe('NotificationSettings', () => {
     )
   })
   describe('The behavior of the save button', () => {
-    it('should be disabled when and grey for unauthenticated users', async () => {
-      const { getByTestId } = await renderNotificationSettings(
+    it('should not be displayed when for unauthenticated users', async () => {
+      const { queryByTestId } = await renderNotificationSettings(
         'granted',
         {
           subscriptions: {},
@@ -149,9 +149,10 @@ describe('NotificationSettings', () => {
         false
       )
       let saveButton: ReactTestInstance | null = null
+      saveButton = queryByTestId('button-container')
+
       await waitFor(() => {
-        saveButton = getByTestId('button-container')
-        expect(saveButton.props.style.backgroundColor).toEqual(ColorsEnum.GREY_LIGHT)
+        expect(saveButton).toBeFalsy()
       })
     })
     it('should enable the save button when the email switch changed', async () => {
