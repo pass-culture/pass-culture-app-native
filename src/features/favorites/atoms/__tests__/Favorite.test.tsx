6db9281b6a2f0bf7edacce6557ab28e6e97a499e
BenStirrup <benjamin.stirrup@gmail.com>
2021-03-15 14:34:59 +0100

    (PC-7023) Favorite booking button : hide OR book in app OR book externally according to user and offer states

--- src/features/favorites/atoms/__tests__/Favorite.test.tsx
+++ src/features/favorites/atoms/__tests__/Favorite.test.tsx
@@ -4,8 +4,10 @@ import React from 'react'
 import waitForExpect from 'wait-for-expect'
 
 import { navigate } from '__mocks__/@react-navigation/native'
-import { FavoriteCategoryResponse, FavoriteResponse } from 'api/gen'
+import { FavoriteCategoryResponse, FavoriteResponse, UserProfileResponse } from 'api/gen'
 import { initialFavoritesState } from 'features/favorites/pages/reducer'
+import { Credit } from 'features/home/services/useAvailableCredit'
+import * as NavigationHelpers from 'features/navigation/helpers'
 import { env } from 'libs/environment'
 import { EmptyResponse } from 'libs/fetch'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
@@ -27,6 +29,7 @@ jest.mock('ui/components/snackBar/SnackBarContext', () => ({
   }),
 }))
 
+const credit: Credit = { amount: 100, isExpired: false }
 const favorite: FavoriteResponse = {
   id: 393,
   offer: {
@@ -37,7 +40,7 @@ const favorite: FavoriteResponse = {
     } as FavoriteCategoryResponse,
     coordinates: { latitude: 48.9263, longitude: 2.49008 },
     date: null,
-    externalTicketOfficeUrl: null,
+    externalTicketOfficeUrl: 'https://externalbooking.test.com',
     id: 146105,
     image: {
       credit: null,
@@ -50,6 +53,10 @@ const favorite: FavoriteResponse = {
     startPrice: 2700,
   },
 }
+const user: UserProfileResponse = { isBeneficiary: true } as UserProfileResponse
+const setOfferToBook = jest.fn()
+
+const defaultProps = { credit, favorite, user, setOfferToBook }
 
 type Options = {
   id?: number
@@ -72,24 +79,27 @@ function simulateBackend(options: Options = defaultOptions) {
   )
 }
 
-const mockFavoritesState = initialFavoritesState
-
 let mockDistance: string | null = null
 jest.mock('features/offer/components/useDistance', () => ({
   useDistance: () => mockDistance,
 }))
 
+const mockFavoritesState = initialFavoritesState
 jest.mock('features/favorites/pages/FavoritesWrapper', () => ({
   useFavoritesState: () => ({
     favoritesState: mockFavoritesState,
   }),
 }))
 
-describe('Favorite component', () => {
+const openExternalUrl = jest
+  .spyOn(NavigationHelpers, 'openExternalUrl')
+  .mockImplementation(jest.fn())
+
+describe('<Favorite /> component', () => {
   afterEach(jest.clearAllMocks)
 
   it('should navigate to the offer when clicking on the favorite', () => {
-    const { getByTestId } = render(reactQueryProviderHOC(<Favorite favorite={favorite} />))
+    const { getByTestId } = render(reactQueryProviderHOC(<Favorite {...defaultProps} />))
     fireEvent.press(getByTestId('favorite'))
     expect(navigate).toHaveBeenCalledWith('Offer', {
       id: favorite.offer.id,
@@ -99,23 +109,23 @@ describe('Favorite component', () => {
 
   it('should show distance if geolocation enabled', () => {
     mockDistance = '10 km'
-    const { queryByText } = render(reactQueryProviderHOC(<Favorite favorite={favorite} />))
+    const { queryByText } = render(reactQueryProviderHOC(<Favorite {...defaultProps} />))
     expect(queryByText('10 km')).toBeTruthy()
   })
 
   it('offer name should take full space if no geolocation', () => {
     mockDistance = '10 km'
-    const withDistance = render(reactQueryProviderHOC(<Favorite favorite={favorite} />)).toJSON()
+    const withDistance = render(reactQueryProviderHOC(<Favorite {...defaultProps} />)).toJSON()
 
     mockDistance = null
-    const withoutDistance = render(reactQueryProviderHOC(<Favorite favorite={favorite} />)).toJSON()
+    const withoutDistance = render(reactQueryProviderHOC(<Favorite {...defaultProps} />)).toJSON()
     expect(withoutDistance).toMatchDiffSnapshot(withDistance)
   })
 
   it('should delete favorite on button click', async () => {
     simulateBackend()
     mockDistance = '10 km'
-    const { getByText } = render(reactQueryProviderHOC(<Favorite favorite={favorite} />))
+    const { getByText } = render(reactQueryProviderHOC(<Favorite {...defaultProps} />))
     await superFlushWithAct(15)
     fireEvent.press(getByText('Supprimer'))
     await superFlushWithAct(15)
@@ -136,7 +146,14 @@ describe('Favorite component', () => {
     mockDistance = '10 km'
     const { getByText } = render(
       reactQueryProviderHOC(
-        <Favorite favorite={{ ...favorite, id, offer: { ...favorite.offer, id } }} />
+        <Favorite
+          {...defaultProps}
+          favorite={{
+            ...defaultProps.favorite,
+            id,
+            offer: { ...defaultProps.favorite.offer, id },
+          }}
+        />
       )
     )
     await superFlushWithAct(20)
@@ -148,6 +165,114 @@ describe('Favorite component', () => {
         message: `L'offre n'a pas été retirée de tes favoris`,
         timeout: SNACK_BAR_TIME_OUT,
       })
+    })
+  })
+})
+
+describe('<Favorite /> component - Booking button', () => {
+  afterEach(jest.clearAllMocks)
+
+  describe('when user is beneficiary', () => {
+    it('they should be able to book in app', () => {
+      const renderAPI = render(reactQueryProviderHOC(<Favorite {...defaultProps} />))
+
+      fireEvent.press(renderAPI.getByText('Réserver'))
+
+      expect(renderAPI.queryByText('button-icon-SVG-Mock')).toBeFalsy()
+      expect(openExternalUrl).not.toBeCalled()
+      expect(defaultProps.setOfferToBook).toBeCalledWith(favorite.offer)
+    })
+  })
+
+  describe('when user is ex-beneficiary (i.e. beneficiary with expired credit)', () => {
+    it('they should be able to book in app when the offer is free', () => {
+      const renderAPI = render(
+        reactQueryProviderHOC(
+          <Favorite
+            {...defaultProps}
+            credit={{ ...defaultProps.credit, isExpired: true }}
+            favorite={{
+              ...defaultProps.favorite,
+              offer: { ...defaultProps.favorite.offer, price: 0 },
+            }}
+          />
+        )
+      )
+
+      fireEvent.press(renderAPI.getByText('Réserver'))
+
+      expect(renderAPI.queryByText('button-icon-SVG-Mock')).toBeFalsy()
+      expect(openExternalUrl).not.toBeCalled()
+      expect(defaultProps.setOfferToBook).toBeCalledWith({
+        ...defaultProps.favorite.offer,
+        price: 0,
+      })
+    })
+
+    it('they should be able to book externally when the offer is NOT free', () => {
+      const renderAPI = render(
+        reactQueryProviderHOC(
+          <Favorite {...defaultProps} credit={{ ...defaultProps.credit, isExpired: true }} />
+        )
+      )
+
+      fireEvent.press(renderAPI.getByText('Réserver'))
+
+      expect(renderAPI.queryByText('button-icon-SVG-Mock')).toBeTruthy()
+      expect(openExternalUrl).toBeCalledWith(defaultProps.favorite.offer.externalTicketOfficeUrl)
+      expect(defaultProps.setOfferToBook).not.toBeCalled()
+    })
+
+    it('they should NOT be able to book externally when the offer is NOT free and an external URL has not been defined', () => {
+      const renderAPI = render(
+        reactQueryProviderHOC(
+          <Favorite
+            {...defaultProps}
+            credit={{ ...defaultProps.credit, isExpired: true }}
+            favorite={{
+              ...defaultProps.favorite,
+              offer: { ...defaultProps.favorite.offer, externalTicketOfficeUrl: null },
+            }}
+          />
+        )
+      )
+
+      expect(renderAPI.queryByText('Réserver')).toBeFalsy()
+      expect(renderAPI.queryByText('button-icon-SVG-Mock')).toBeFalsy()
+    })
+  })
+
+  describe('when user is NOT a beneficiary', () => {
+    it('they should be able to book externally', () => {
+      const renderAPI = render(
+        reactQueryProviderHOC(
+          <Favorite {...defaultProps} user={{ ...defaultProps.user, isBeneficiary: false }} />
+        )
+      )
+
+      fireEvent.press(renderAPI.getByText('Réserver'))
+
+      expect(renderAPI.queryByText('button-icon-SVG-Mock')).toBeTruthy()
+      expect(openExternalUrl).toBeCalledWith(defaultProps.favorite.offer.externalTicketOfficeUrl)
+      expect(defaultProps.setOfferToBook).not.toBeCalled()
+    })
+
+    it('they should NOT be able to book externally when an external URL has not been defined', () => {
+      const renderAPI = render(
+        reactQueryProviderHOC(
+          <Favorite
+            {...defaultProps}
+            favorite={{
+              ...defaultProps.favorite,
+              offer: { ...defaultProps.favorite.offer, externalTicketOfficeUrl: null },
+            }}
+            user={{ ...defaultProps.user, isBeneficiary: false }}
+          />
+        )
+      )
+
+      expect(renderAPI.queryByText('Réserver')).toBeFalsy()
+      expect(renderAPI.queryByText('button-icon-SVG-Mock')).toBeFalsy()
     })
   })
 })
