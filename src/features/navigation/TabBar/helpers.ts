1ac9b04f3cf870026adcd392c2d4c18574815123
Justine Mignot <justinem@bam.tech>
2021-03-17 15:07:11 +0100

    :sparkles: (PC-7616) feature flip booking_tab for prod soumission

--- src/features/navigation/TabBar/helpers.ts
+++ src/features/navigation/TabBar/helpers.ts
@@ -1,5 +1,6 @@
 import { UserProfileResponse } from 'api/gen'
 import { IAuthContext } from 'features/auth/AuthContext'
+import { env } from 'libs/environment'
 
 import { TabRoute } from './types'
 
@@ -7,7 +8,10 @@ export const shouldDisplayTabIconPredicate = (
   authContext: IAuthContext,
   user?: UserProfileResponse
 ) => (route: TabRoute): boolean => {
-  if (route.name === 'Bookings' && (!authContext.isLoggedIn || !user?.isBeneficiary)) {
+  if (
+    route.name === 'Bookings' &&
+    (!authContext.isLoggedIn || !user?.isBeneficiary || !env.SHOULD_DISPLAY_BOOKINGS_TAB)
+  ) {
     return false
   }
   return true
