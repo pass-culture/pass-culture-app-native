10e30f5b8a7df100a5ffb4ae3735d6be7a6b06c4
blixit <celestinya@gmail.com>
2021-03-18 23:26:03 +0100

    (PC-7398) OnGoingTicket Component

---
+++ src/features/bookings/components/OnGoingTicket.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { ClippedImage } from 'ui/components/ClippedImage'
+
+interface OnGoingTicketProps {
+  image: string
+}
+
+export function OnGoingTicket(props: OnGoingTicketProps) {
+  const dPath =
+    'M26.838 0c0 5.308 4.326 9.612 9.662 9.612 5.336 0 9.662-4.304 9.662-9.612H69c2.21 0 4 1.79 4 4v102c0 2.21-1.79 4-4 4l-22.838.001V110c0-5.308-4.326-9.612-9.662-9.612-5.336 0-9.662 4.304-9.662 9.612H4c-2.21 0-4-1.79-4-4V4c0-2.21 1.79-4 4-4h22.838z'
+
+  return (
+    <ClippedImage clipId="onGoingClip" path={dPath} width={81} height={118} image={props.image} />
+  )
+}
