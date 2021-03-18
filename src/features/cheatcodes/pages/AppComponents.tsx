8a512134006de13c33cb3d6f54b5ffdb68669d81
blixit <celestinya@gmail.com>
2021-03-18 23:26:03 +0100

    (PC-7398) OnGoingTicket Component

--- src/features/cheatcodes/pages/AppComponents.tsx
+++ src/features/cheatcodes/pages/AppComponents.tsx
@@ -6,6 +6,7 @@ import { TouchableOpacity } from 'react-native-gesture-handler'
 import styled from 'styled-components/native'
 
 import { CategoryNameEnum, Expense, ExpenseDomain } from 'api/gen/api'
+import { OnGoingTicket } from 'features/bookings/components/OnGoingTicket'
 import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { AccordionItem, CallToAction } from 'features/offer/components'
 import { BeneficiaryCeilings } from 'features/profile/components/BeneficiaryCeilings'
@@ -652,6 +653,10 @@ export const AppComponents: FunctionComponent = () => {
             title="Next"
             onPress={() => setCurrentStep((step) => (step === NUMBER_OF_STEPS ? step : step + 1))}
           />
+        </AlignedText>
+        <AlignedText>
+          <OnGoingTicket image="https://img-19.ccm2.net/8vUCl8TXZfwTt7zAOkBkuDRHiT8=/1240x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg" />
+          <Text> - OnGoing Ticket </Text>
         </AlignedText>
       </AccordionItem>
       <Spacer.Column numberOfSpaces={5} />
