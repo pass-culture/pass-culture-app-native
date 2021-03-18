e8b509fa59214636d5d9ab1c6d419ce984974e05
blixit <celestinya@gmail.com>
2021-03-18 11:54:33 +0100

    (PC-7405) Redirect to the search from the no bookings view

--- src/features/bookings/components/NoBookingsView.tsx
+++ src/features/bookings/components/NoBookingsView.tsx
@@ -1,24 +1,27 @@
 import { t } from '@lingui/macro'
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import styled from 'styled-components/native'
 
+import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { _ } from 'libs/i18n'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { NoBookings } from 'ui/svg/icons/NoBookings'
 import { ColorsEnum, getSpacing, Spacer, TAB_BAR_COMP_HEIGHT, Typo } from 'ui/theme'
 
 export function NoBookingsView() {
+  const { navigate } = useNavigation<UseNavigationType>()
   return (
     <Container>
       <NoBookings size={197} color={ColorsEnum.GREY_MEDIUM} />
-      <Explaination color={ColorsEnum.GREY_DARK}>
+      <Explanation color={ColorsEnum.GREY_DARK}>
         {_(t`Tu n’as pas de réservations en cours.
       Découvre les offres disponibles 
       sans attendre !`)}
-      </Explaination>
+      </Explanation>
       <Spacer.Column numberOfSpaces={6} />
       <ButtonContainer>
-        <ButtonPrimary title={_(t`Explorer les offres`)} />
+        <ButtonPrimary title={_(t`Explorer les offres`)} onPress={() => navigate('Search')} />
       </ButtonContainer>
     </Container>
   )
@@ -37,6 +40,6 @@ const ButtonContainer = styled.View({
   width: '100%',
 })
 
-const Explaination = styled(Typo.Body)({
+const Explanation = styled(Typo.Body)({
   textAlign: 'center',
 })
