689db8e2f9971fe31ff1c55872b4bd2e7f646d46
Emma Jamin <emmajamin@orange.fr>
2021-03-18 14:46:42 +0100

    (PC-7469) update BookingEventChoices and add new test

--- src/features/bookOffer/components/tests/BookingEventChoices.test.tsx
+++ src/features/bookOffer/components/tests/BookingEventChoices.test.tsx
@@ -1,7 +1,7 @@
-import { render, act } from '@testing-library/react-native'
-import React from 'react'
+import { render, act, fireEvent } from '@testing-library/react-native'
+import * as React from 'react'
 
-import { useBooking } from 'features/bookOffer/pages/BookingOfferWrapper'
+import { useBooking, useBookingOffer } from 'features/bookOffer/pages/BookingOfferWrapper'
 import { Step } from 'features/bookOffer/pages/reducer'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { flushAllPromises } from 'tests/utils'
@@ -9,13 +9,17 @@ import { flushAllPromises } from 'tests/utils'
 import { BookingEventChoices } from '../BookingEventChoices'
 
 const mockUseBooking = useBooking as jest.Mock
+const mockUseBookingOffer = useBookingOffer as jest.Mock
+
+const mockDispatch = jest.fn()
+
 jest.mock('features/bookOffer/pages/BookingOfferWrapper', () => ({
   useBooking: jest.fn(() => ({
     bookingState: {
       offerId: undefined,
       stockId: undefined,
       step: undefined,
-      quantity: 1,
+      quantity: undefined,
       date: undefined,
     },
     dispatch: jest.fn(),
@@ -49,7 +53,7 @@ describe('<BookingEventChoices />', () => {
         offerId: 1,
         stockId: 1,
         step: Step.HOUR,
-        quantity: 1,
+        quantity: undefined,
         date: '01/02/2021',
       },
       dispatch: jest.fn(),
@@ -80,18 +84,61 @@ describe('<BookingEventChoices />', () => {
   })
 
   it('should display date step and hour step and duo step on a snapshot', async () => {
-    mockUseBooking.mockImplementationOnce(() => ({
+    mockUseBooking.mockImplementation(() => ({
       bookingState: {
         offerId: 1,
         stockId: 1,
         step: Step.DUO,
         quantity: 1,
-        date: '01/02/2021',
+        date: new Date('01/02/2021'),
       },
       dispatch: jest.fn(),
     }))
     const page = render(reactQueryProviderHOC(<BookingEventChoices stocks={[]} />))
     await act(flushAllPromises)
     expect(page.toJSON()).toMatchSnapshot()
+  })
+  it('should skip duo step if quantity is already selected', async () => {
+    mockUseBooking.mockImplementation(() => ({
+      bookingState: {
+        offerId: 1,
+        stockId: 1,
+        step: Step.HOUR,
+        quantity: 1,
+        date: new Date('02/01/2021'),
+      },
+      dispatch: mockDispatch,
+    }))
+    mockUseBookingOffer.mockImplementation(() => ({
+      id: 1,
+      isDuo: true,
+      stocks: [
+        {
+          id: 1,
+          beginningDatetime: new Date('2021-02-01T20:00:00'),
+          bookingLimitDatetime: new Date('2021-02-01T20:00:00'),
+          cancellationLimitDatetime: new Date('2021-02-01T12:14:57.081907'),
+          isBookable: true,
+          price: 2400,
+        },
+      ],
+    }))
+
+    const debouncedFunction = jest.fn()
+    jest.spyOn(React, 'useRef').mockReturnValue({
+      current: debouncedFunction,
+    })
+
+    const page = render(reactQueryProviderHOC(<BookingEventChoices stocks={[]} />))
+    await act(flushAllPromises)
+
+    const hourBloc = page.getByTestId('HourChoice1')
+    fireEvent.press(hourBloc)
+
+    expect(mockDispatch).toHaveBeenCalledWith({ type: 'SELECT_STOCK', payload: 1 })
+    expect(debouncedFunction).toHaveBeenCalledWith({
+      type: 'CHANGE_STEP',
+      payload: Step.PRE_VALIDATION,
+    })
   })
 })
