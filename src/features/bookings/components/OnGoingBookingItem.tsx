b071c408756e9dfc0fd109d6e24c290c3f76be27
blixit <celestinya@gmail.com>
2021-03-22 15:32:19 +0100

    (PC-7402) DAdd separator between items and css refacto

--- src/features/bookings/components/OnGoingBookingItem.tsx
+++ src/features/bookings/components/OnGoingBookingItem.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Dimensions, Text } from 'react-native'
 import styled from 'styled-components/native'
 
+import { Separator } from 'features/profile/components/reusables'
 import { getSpacing, Typo } from 'ui/theme'
 
+import { OnGoingTicket, onGoingTicketWidth } from './OnGoingTicket'
 import { Booking } from './types'
 
 interface OnGoingBookingItemProps {
@@ -12,19 +14,42 @@ interface OnGoingBookingItemProps {
 
 export const OnGoingBookingItem = ({ booking }: OnGoingBookingItemProps) => {
   return (
-    <ItemContainer>
-      <Typo.ButtonText>{booking.stock.offer.name}</Typo.ButtonText>
-      <Typo.ButtonText>{booking.stock.offer.image?.url}</Typo.ButtonText>
-      <Text>is Duo: {isDuo(booking).toString()}</Text>
-      <Text>Begin: {booking.stock.beginningDatetime}</Text>
-      <Text>Expire: {booking.expirationDate}</Text>
-      <Text>is Permanent: {booking.stock.offer.isPermanent.toString()}</Text>
-    </ItemContainer>
+    <React.Fragment>
+      <ItemContainer>
+        <OnGoingTicket image={booking.stock.offer.image?.url} />
+        <AttributesView>
+          <TitleContainer>
+            <Title numberOfLines={2}>{booking.stock.offer.name}</Title>
+          </TitleContainer>
+          <Text>is Duo: {isDuo(booking).toString()}</Text>
+          <Text>Begin: {booking.stock.beginningDatetime}</Text>
+          <Text>Expire: {booking.expirationDate}</Text>
+          <Text>is Permanent: {booking.stock.offer.isPermanent.toString()}</Text>
+        </AttributesView>
+      </ItemContainer>
+      <Separator />
+    </React.Fragment>
   )
 }
+const viewWidth = Dimensions.get('screen').width - onGoingTicketWidth - 40
 
 const ItemContainer = styled.View({
+  flexDirection: 'row',
   paddingVertical: getSpacing(4),
+})
+
+const AttributesView = styled.View({
+  paddingLeft: 8,
+  paddingRight: 4,
+})
+
+const TitleContainer = styled.View({
+  flexDirection: 'row',
+  width: viewWidth,
+})
+
+const Title = styled(Typo.ButtonText)({
+  flexShrink: 1,
 })
 
 function isDuo(booking: Booking) {
