8d0da672a510feccb978d1434467fbdd82a8ce41
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-03-23 14:53:48 +0100

    (PC-6718) add test for adding to favorites on BookingImpossible page

---
+++ src/features/bookOffer/components/__tests__/BookingImpossible.test.tsx
@@ -0,0 +1,73 @@
+import { fireEvent, render } from '@testing-library/react-native'
+import { rest } from 'msw'
+import * as React from 'react'
+import { QueryClient } from 'react-query'
+import waitForExpect from 'wait-for-expect'
+
+import { addFavoriteJsonResponseSnap } from 'features/favorites/api/snaps/favorisResponseSnap'
+import { analytics } from 'libs/analytics'
+import { env } from 'libs/environment'
+import { EmptyResponse } from 'libs/fetch'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { server } from 'tests/server'
+import { superFlushWithAct } from 'tests/utils'
+
+import { BookingImpossible } from '../BookingImpossible'
+
+const mockDismissModal = jest.fn()
+jest.mock('features/bookOffer/pages/BookingOfferWrapper', () => ({
+  useBooking: jest.fn(() => ({
+    bookingState: {
+      offerId: 20,
+      stockId: undefined,
+      step: undefined,
+      quantity: undefined,
+      date: undefined,
+    },
+    dismissModal: mockDismissModal,
+  })),
+}))
+
+jest.mock('features/auth/AuthContext', () => ({
+  useAuthContext: jest.fn(() => ({ isLoggedIn: true })),
+}))
+
+server.use(
+  rest.post<EmptyResponse>(`${env.API_BASE_URL}/native/v1/me/favorites`, (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(addFavoriteJsonResponseSnap))
+  )
+)
+
+describe('<BookingImpossible />', () => {
+  it('should render', () => {
+    const { toJSON } = render(reactQueryProviderHOC(<BookingImpossible />))
+    expect(toJSON()).toMatchSnapshot()
+  })
+
+  it("should dismiss modal when clicking on 'Retourner à l'offre'", () => {
+    const { getByText } = render(reactQueryProviderHOC(<BookingImpossible />))
+    fireEvent.press(getByText("Retourner à l'offre"))
+    expect(mockDismissModal).toHaveBeenCalledTimes(1)
+  })
+
+  it('should log analytics event when adding to favorites', async () => {
+    const setup = (queryClient: QueryClient) => {
+      queryClient.setQueryData('favorites', {
+        favorites: [],
+      })
+    }
+
+    const { getByText } = render(reactQueryProviderHOC(<BookingImpossible />, setup))
+
+    fireEvent.press(getByText('Mettre en favoris'))
+    await superFlushWithAct()
+
+    await waitForExpect(() => {
+      expect(analytics.logHasAddedOfferToFavorites).toHaveBeenCalledTimes(1)
+      expect(analytics.logHasAddedOfferToFavorites).toHaveBeenCalledWith({
+        from: 'offer',
+        offerId: 20,
+      })
+    })
+  })
+})
