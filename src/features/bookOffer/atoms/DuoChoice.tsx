53bce1c7119422e5d50720c7ae512486585a4728
Emma Jamin <emmajamin@orange.fr>
2021-03-23 12:29:08 +0100

    (PC-6703) add notEnoughCredit parameter

--- src/features/bookOffer/atoms/DuoChoice.tsx
+++ src/features/bookOffer/atoms/DuoChoice.tsx
@@ -2,9 +2,9 @@ import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 
 import { IconInterface } from 'ui/svg/icons/types'
-import { ColorsEnum, getSpacing, Typo } from 'ui/theme'
+import { getSpacing, Typo } from 'ui/theme'
 
-import { ChoiceBloc } from './ChoiceBloc'
+import { ChoiceBloc, getTextColor } from './ChoiceBloc'
 
 interface Props {
   title: string
@@ -12,25 +12,38 @@ interface Props {
   selected: boolean
   icon: FunctionComponent<IconInterface>
   onPress: () => void
+  hasEnoughCredit: boolean
 }
 
-export const DuoChoice: React.FC<Props> = ({ title, price, selected, icon: Icon, onPress }) => {
-  const textColor = selected ? ColorsEnum.WHITE : ColorsEnum.BLACK
+export const DuoChoice: React.FC<Props> = ({
+  title,
+  price,
+  selected,
+  icon: Icon,
+  onPress,
+  hasEnoughCredit,
+}) => {
+  const disabled = !hasEnoughCredit
+  const textColor = getTextColor(selected, disabled)
+
   return (
-    <ChoiceBloc onPress={onPress} selected={selected}>
+    <ChoiceBloc onPress={onPress} selected={selected} disabled={disabled}>
       <Container>
         <Icon color={textColor} size={28} />
         <Typo.ButtonText color={textColor}>{title}</Typo.ButtonText>
 
-        <Typo.Caption testID="price" color={textColor}>
+        <Caption testID="price" color={textColor}>
           {price}
-        </Typo.Caption>
+        </Caption>
       </Container>
     </ChoiceBloc>
   )
 }
 
 const Container = styled.View({
-  paddingVertical: getSpacing(2),
+  minHeight: getSpacing(20),
+  justifyContent: 'center',
   alignItems: 'center',
 })
+
+const Caption = styled(Typo.Caption)({ textAlign: 'center' })
