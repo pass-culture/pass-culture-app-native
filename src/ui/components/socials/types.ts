0d0515c5ca4f4cb57ba59996f7ec8620d0d2bb76
Emma Jamin <emmajamin@orange.fr>
2021-03-24 18:51:44 +0100

    (PC-7622) change url for facebook

--- src/ui/components/socials/types.ts
+++ src/ui/components/socials/types.ts
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { Platform } from 'react-native'
 
 import { Facebook } from 'ui/svg/icons/socialNetwork/Facebook'
 import { Instagram } from 'ui/svg/icons/socialNetwork/Instagram'
@@ -8,6 +9,9 @@ import { IconInterface } from 'ui/svg/icons/types'
 
 export type SocialNetwork = 'facebook' | 'instagram' | 'snapchat' | 'twitter'
 
+const facebookAndroidLink = 'fb://page/2202916773290436'
+const facebookIOSLink = 'fb://page/?id=2202916773290436'
+
 export const SocialNetworkIconsMap: Record<
   SocialNetwork,
   {
@@ -15,7 +19,11 @@ export const SocialNetworkIconsMap: Record<
     link: string
   }
 > = {
-  facebook: { icon: Facebook, link: 'https://www.facebook.com/passCultureofficiel/' },
+  facebook: {
+    icon: Facebook,
+    link: Platform.OS === 'ios' ? facebookIOSLink : facebookAndroidLink,
+  },
+
   instagram: { icon: Instagram, link: 'https://www.instagram.com/passcultureofficiel/?hl=fr' },
   snapchat: { icon: Snapchat, link: 'https://www.snapchat.com/add/pass.culture' },
   twitter: { icon: Twitter, link: 'https://twitter.com/pass_Culture' },
