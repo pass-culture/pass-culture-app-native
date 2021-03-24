bfef6f9d90eff45c8eac5d948a0fcc964e8df075
BenStirrup <benjamin.stirrup@gmail.com>
2021-03-24 11:23:40 +0100

    Add new fields to offer and stocks (without breaking changes)

--- src/features/offer/services/useCtaWordingAndAction.testsFixtures.ts
+++ src/features/offer/services/useCtaWordingAndAction.testsFixtures.ts
@@ -1,47 +1,61 @@
-export const notExpiredStock = {
+import { OfferStockResponse } from 'api/gen'
+
+export const notExpiredStock: OfferStockResponse = {
   id: 118929,
   beginningDatetime: new Date('2021-01-01T13:30:00'),
   bookingLimitDatetime: new Date('2021-01-05T13:30:00'),
   price: 500,
   isBookable: true,
+  isExpired: false,
+  isSoldOut: false,
 }
 
-const notExpiredStockNoLimitDate = {
+const notExpiredStockNoLimitDate: OfferStockResponse = {
   id: 118928,
   beginningDatetime: new Date('2021-01-02T18:00:00'),
   price: 500,
   isBookable: true,
+  isExpired: false,
+  isSoldOut: false,
 }
 
-export const expiredStock1 = {
+export const expiredStock1: OfferStockResponse = {
   id: 118929,
   beginningDatetime: new Date('2021-01-01T13:30:00'),
   bookingLimitDatetime: new Date('2021-01-03T13:30:00'),
   price: 500,
   isBookable: true,
+  isExpired: false,
+  isSoldOut: false,
 }
-export const expiredStock2 = {
+export const expiredStock2: OfferStockResponse = {
   id: 118928,
   beginningDatetime: new Date('2021-01-02T18:00:00'),
   bookingLimitDatetime: new Date('2021-01-03T13:30:00'),
   price: 500,
   isBookable: true,
+  isExpired: false,
+  isSoldOut: false,
 }
 
-export const soldOutStock = {
+export const soldOutStock: OfferStockResponse = {
   id: 118929,
   beginningDatetime: new Date('2021-01-01T13:30:00'),
   bookingLimitDatetime: new Date('2021-01-05T13:30:00'),
   price: 500,
   isBookable: false,
+  isExpired: false,
+  isSoldOut: false,
 }
 
-const notSoldOutStock = {
+const notSoldOutStock: OfferStockResponse = {
   id: 118929,
   beginningDatetime: new Date('2021-01-01T13:30:00'),
   bookingLimitDatetime: new Date('2021-01-05T13:30:00'),
   price: 500,
   isBookable: true,
+  isExpired: false,
+  isSoldOut: false,
 }
 
 export const expiredOffer = {
