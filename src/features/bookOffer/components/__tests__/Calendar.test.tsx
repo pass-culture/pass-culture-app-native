6871a09f8d7c030a10307f950854b6045b6c84c1
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-03-26 13:40:03 +0100

    (PC-7757) calendar to first available date

--- src/features/bookOffer/components/__tests__/Calendar.test.tsx
+++ src/features/bookOffer/components/__tests__/Calendar.test.tsx
@@ -1,6 +1,11 @@
-import { getMinDate } from '../Calendar/Calendar'
+import { OfferStatus } from 'features/bookOffer/services/utils'
 
-describe('getMinDate', () => {
+import { getMinAvailableDate } from '../Calendar/Calendar'
+import { Marking } from '../Calendar/useMarkedDates'
+
+const marking = { price: 2700, selected: false, status: OfferStatus.BOOKABLE }
+
+describe('getMinAvailableDate', () => {
   it.each`
     dates                                         | expected
     ${[]}                                         | ${undefined}
@@ -8,7 +13,23 @@ describe('getMinDate', () => {
     ${['2021-03-28', '2021-03-29']}               | ${'2021-03-28'}
     ${['2021-03-29', '2021-03-28']}               | ${'2021-03-28'}
     ${['2021-03-29', '2021-02-12', '2021-03-28']} | ${'2021-02-12'}
-  `('getMinDate($dates) \t= $expected', ({ dates, expected }) => {
-    expect(getMinDate(dates)).toBe(expected)
+  `(
+    'should return the first date from a set of available dates: $dates \t= $expected',
+    ({ dates, expected }: { dates: string[]; expected: string | undefined }) => {
+      const markedDates = dates.reduce((acc: Record<string, Marking>, curr) => {
+        acc[curr] = marking
+        return acc
+      }, {})
+
+      expect(getMinAvailableDate(markedDates)).toBe(expected)
+    }
+  )
+
+  it('should return the first available date', () => {
+    const minDate = getMinAvailableDate({
+      '2021-03-28': marking,
+      '2021-03-27': { ...marking, status: OfferStatus.NOT_BOOKABLE },
+    })
+    expect(minDate).toEqual('2021-03-28')
   })
 })
