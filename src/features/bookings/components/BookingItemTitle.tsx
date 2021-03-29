8f049e4333168e03ffd6f3cb58098bda3b3609f4
AnoukHello <anouk.hello@polyconseil.fr>
2021-03-29 17:31:29 +0200

    (PC-7430) Bookings: create BookingItemTitle component

---
+++ src/features/bookings/components/BookingItemTitle.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Dimensions } from 'react-native'
+import styled from 'styled-components/native'
+
+import { getSpacing, Typo } from 'ui/theme'
+
+type BookingItemTitleProps = {
+  ticketWidth: number
+  title: string
+}
+
+export function BookingItemTitle(props: BookingItemTitleProps) {
+  const containerWidth = Dimensions.get('screen').width - props.ticketWidth - getSpacing(10)
+
+  return (
+    <TitleContainer width={containerWidth}>
+      <Title numberOfLines={2}>{props.title}</Title>
+    </TitleContainer>
+  )
+}
+
+const TitleContainer = styled.View<{ width: number }>(({ width }) => ({
+  flexDirection: 'row',
+  width: width,
+  paddingBottom: getSpacing(1),
+}))
+
+export const Title = styled(Typo.ButtonText)({
+  flexShrink: 1,
+})
