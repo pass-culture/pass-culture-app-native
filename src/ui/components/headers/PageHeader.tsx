93aea498d060faf31db15ed44b2e47ea857191fb
BenStirrup <benjamin.stirrup@gmail.com>
2021-03-29 16:59:12 +0200

    (PC-7499) Make headers titles perfectly centered no matter the icons sizes

--- src/ui/components/headers/PageHeader.tsx
+++ src/ui/components/headers/PageHeader.tsx
@@ -6,7 +6,7 @@ import styled from 'styled-components/native'
 import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { useElementWidth } from 'ui/hooks/useElementWidth'
 import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
-import { ColorsEnum, Spacer, Typo } from 'ui/theme'
+import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
 import { ACTIVE_OPACITY } from 'ui/theme/colors'
 
 interface Props {
@@ -36,31 +36,29 @@ const HeaderIconBack: React.FC<HeaderIconProps> = ({ onGoBack }) => {
 
 export const PageHeader: React.FC<Props> = (props) => {
   const { title, RightComponent } = props
-  const { width: rightComponentWidth, onLayout } = useElementWidth()
-  const spaceToAddBeforeTitle = rightComponentWidth ? rightComponentWidth / 6 : 0
+  const { onLayout } = useElementWidth()
 
   return (
     <HeaderContainer>
       <Spacer.TopScreen />
       <Spacer.Column numberOfSpaces={2} />
+
       <Row>
-        <Spacer.Row numberOfSpaces={5} />
-        <HeaderIconBack onGoBack={props.onGoBack} />
-        <Spacer.Row numberOfSpaces={spaceToAddBeforeTitle} />
-        <Spacer.Flex />
+        <IconContainer positionInHeader="left">
+          <HeaderIconBack onGoBack={props.onGoBack} />
+        </IconContainer>
 
         <Title color={ColorsEnum.WHITE}>{title}</Title>
 
-        <Spacer.Flex />
-        {RightComponent ? (
-          <View onLayout={onLayout}>
-            <RightComponent />
-          </View>
-        ) : (
-          <Spacer.Row numberOfSpaces={10} />
-        )}
-        <Spacer.Row numberOfSpaces={6} />
+        <IconContainer positionInHeader="right">
+          {RightComponent && (
+            <View onLayout={onLayout}>
+              <RightComponent />
+            </View>
+          )}
+        </IconContainer>
       </Row>
+
       <Spacer.Column numberOfSpaces={2} />
     </HeaderContainer>
   )
@@ -74,7 +72,7 @@ const HeaderContainer = styled.View({
 })
 
 const Title = styled(Typo.Body).attrs({ numberOfLines: 1, color: ColorsEnum.WHITE })({
-  flexShrink: 1,
+  flex: 1,
   textAlign: 'center',
 })
 
@@ -83,3 +81,14 @@ const Row = styled.View({
   flexDirection: 'row',
   alignItems: 'center',
 })
+
+const IconContainer = styled.View<{ positionInHeader: 'left' | 'right' }>(
+  ({ positionInHeader = 'left' }) => ({
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: positionInHeader === 'left' ? 'flex-start' : 'flex-end',
+    width: getSpacing(15),
+    paddingLeft: getSpacing(3),
+    paddingRight: getSpacing(3),
+  })
+)
