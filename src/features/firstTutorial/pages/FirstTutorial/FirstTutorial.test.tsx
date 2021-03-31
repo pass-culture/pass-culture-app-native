4aabcf2cde449fd1e7b1d1558f1e10779999d9a9
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-03-31 20:05:35 +0200

    (PC-7877) extract translations

--- src/features/firstTutorial/pages/FirstTutorial/FirstTutorial.test.tsx
+++ src/features/firstTutorial/pages/FirstTutorial/FirstTutorial.test.tsx
@@ -2,16 +2,19 @@ import AsyncStorage from '@react-native-community/async-storage'
 import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
 
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+
 import { FirstTutorial } from './FirstTutorial'
 
 describe('FirstTutorial page', () => {
   it('should render first tutorial', () => {
-    const firstTutorial = render(<FirstTutorial />)
+    const firstTutorial = render(reactQueryProviderHOC(<FirstTutorial />))
     expect(firstTutorial).toMatchSnapshot()
   })
 
   it('should set has_seen_tutorials in storage on skip', () => {
-    const { getByText } = render(<FirstTutorial />)
+    const { getByText } = render(reactQueryProviderHOC(<FirstTutorial />))
+
     fireEvent.press(getByText('Tout passer'))
     expect(AsyncStorage.setItem).toBeCalledWith('has_seen_tutorials', 'true')
   })
