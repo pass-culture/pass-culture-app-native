6a32107cb84cf674e185049cc0bddbb4fedee54f
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-03-31 17:39:38 +0200

    (PC-7917) App Native - Amélioration tech - Mise en place de la traduction au pluriel (plural)

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -1,4 +1,6 @@
 import { MessageDescriptor, setupI18n } from '@lingui/core' //@translations
+/* PluralProps is provided by @types/lingui__core/select.d.ts therefore we can ignore no-unresolved error */
+import { PluralProps } from '@lingui/core/select' // eslint-disable-line import/no-unresolved
 import { findBestAvailableLanguage } from 'react-native-localize'
 
 import frenchCatalog from 'locales/fr/messages'
@@ -16,6 +18,8 @@ export const i18n = setupI18n({
   },
 })
 
-export function _(id: MessageDescriptor): string {
-  return i18n._(id)
+export function _(id: MessageDescriptor | PluralProps): string {
+  return !Object.prototype.hasOwnProperty.call(id, 'one')
+    ? i18n._(id as MessageDescriptor)
+    : i18n.plural(id as PluralProps)
 }
