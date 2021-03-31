547ee4c09383825aae26d8c8f2b709bedf7de8e8
blixit <celestinya@gmail.com>
2021-03-31 14:11:33 +0200

    (PC-7463) Create the 3 shapes ticket component

---
+++ src/features/bookings/components/ThreeShapesTicket.tsx
@@ -0,0 +1,57 @@
+import React, { PropsWithChildren, useState } from 'react'
+import { LayoutRectangle } from 'react-native'
+import styled from 'styled-components/native'
+
+import { TicketFooter } from 'ui/svg/TicketFooter'
+import { TicketHeader } from 'ui/svg/TicketHeader'
+import { ColorsEnum, getNativeShadow } from 'ui/theme'
+
+type ThreeShapesTicketProps = PropsWithChildren<{
+  color?: ColorsEnum
+  width: number
+}>
+
+export function ThreeShapesTicket(props: ThreeShapesTicketProps) {
+  const [headerDimensions, setHeaderDimensions] = useState<LayoutRectangle | undefined>()
+  return (
+    <Container customWitdh={props.width} style={shaddowStyle}>
+      <TicketHeader
+        width={props.width}
+        color={props.color}
+        onLayout={(e) => setHeaderDimensions(e.nativeEvent.layout)}
+      />
+      {headerDimensions && (
+        <CenterView customWitdh={headerDimensions?.width} color={props.color}>
+          {props.children}
+        </CenterView>
+      )}
+      <TicketFooter width={props.width} color={props.color} />
+    </Container>
+  )
+}
+
+const shaddowStyle = {
+  ...getNativeShadow({
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 3,
+    shadowColor: ColorsEnum.BLACK,
+    shadowOpacity: 0.1,
+  }),
+}
+
+const Container = styled.View<{ customWitdh: number; color?: ColorsEnum }>(({ customWitdh }) => ({
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  maxWidth: customWitdh,
+}))
+
+const CenterView = styled.View<{ customWitdh: number; color?: ColorsEnum }>(
+  ({ customWitdh, color }) => ({
+    width: customWitdh,
+    backgroundColor: color,
+  })
+)
---
+++ src/ui/svg/TicketFooter.tsx
@@ -0,0 +1,42 @@
+import React, { memo } from 'react'
+import Svg, { Path, G, Stop, LinearGradient, Use } from 'react-native-svg'
+
+import { ColorsEnum } from 'ui/theme'
+
+const ratio = 303 / 38
+const viewBox = '0 0 303 38'
+
+interface Props {
+  width: number
+  color?: ColorsEnum
+}
+
+function TicketFooterComponent(props: Props) {
+  const height = props.width / ratio
+  const color = props.color || ColorsEnum.WHITE
+  const linearGradientId = 'ticket-footer-gradient'
+  const pathId = 'ticket-footer-path'
+
+  return (
+    <Svg style={{ aspectRatio: ratio }} width={props.width} height={height} viewBox={viewBox}>
+      <LinearGradient x1="0" y1="1" x2="0" y2="0" id={linearGradientId}>
+        <Stop offset="0" stopColor={props.color} stopOpacity="1" />
+        <Stop offset="1" stopColor={'#F0F0F0'} stopOpacity="1" />
+      </LinearGradient>
+      <G fill="none" fillRule="evenodd">
+        <G fill={color}>
+          <G>
+            <Path
+              id={pathId}
+              d="M121.004 329c.27 16.616 13.82 30 30.496 30 16.678 0 30.229-13.386 30.496-30H283c11.046 0 20 8.954 20 20l-.001 18H0v-18c0-11.046 8.954-20 20-20h101.004z"
+              transform="translate(-36 -356) translate(36 27) matrix(1 0 0 -1 0 696)"
+            />
+            <Use fill={`url(#${linearGradientId})`} xlinkHref={`#${pathId}`} />
+          </G>
+        </G>
+      </G>
+    </Svg>
+  )
+}
+
+export const TicketFooter = memo(TicketFooterComponent)
