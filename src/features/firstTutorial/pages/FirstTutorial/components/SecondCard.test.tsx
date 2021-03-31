442f3ad4e9d9a128ee52fc11032b4c2b631b8292
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-03-31 20:03:46 +0200

    (PC-7877) Chang on second tuto card depending on deposit amount

--- src/features/firstTutorial/pages/FirstTutorial/components/SecondCard.test.tsx
+++ src/features/firstTutorial/pages/FirstTutorial/components/SecondCard.test.tsx
@@ -4,12 +4,26 @@ import Swiper from 'react-native-web-swiper'
 
 import { SecondCard } from './SecondCard'
 
+let mockDepositAmount = 30000
+jest.mock('features/auth/api', () => ({ useDepositAmount: () => mockDepositAmount }))
+
 describe('SecondCard', () => {
   beforeEach(jest.clearAllMocks)
   it('should render second card', () => {
     const firstTutorial = render(<SecondCard activeIndex={0} index={0} lastIndex={0} />)
     expect(firstTutorial).toMatchSnapshot()
   })
+
+  it('should show the correct deposit amount', async () => {
+    mockDepositAmount = 30000
+    let queryByText = render(<SecondCard activeIndex={0} index={0} lastIndex={0} />).queryByText
+    expect(queryByText(/un montant de 300€ à dépenser/)).toBeTruthy()
+
+    mockDepositAmount = 50000
+    queryByText = render(<SecondCard activeIndex={0} index={0} lastIndex={0} />).queryByText
+    expect(queryByText(/un montant de 500€ à dépenser/)).toBeTruthy()
+  })
+
   it('should swipe to next card on button press', () => {
     const ref = {
       current: {
