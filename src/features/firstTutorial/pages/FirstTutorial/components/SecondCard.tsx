b6cc7963400f077d1dc8239768e131e3f4465b7f
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-03-31 20:03:46 +0200

    (PC-7877) Chang on second tuto card depending on deposit amount

--- src/features/firstTutorial/pages/FirstTutorial/components/SecondCard.tsx
+++ src/features/firstTutorial/pages/FirstTutorial/components/SecondCard.tsx
@@ -1,7 +1,9 @@
 import { t } from '@lingui/macro'
 import React from 'react'
 
+import { useDepositAmount } from 'features/auth/api'
 import { _ } from 'libs/i18n'
+import { formatToFrenchDecimal } from 'libs/parsers'
 import TutorialOffers from 'ui/animations/tutorial_offers.json'
 import {
   AchievementCardKeyProps,
@@ -9,9 +11,13 @@ import {
 } from 'ui/components/achievements/components/GenericAchievementCard'
 
 export function SecondCard(props: AchievementCardKeyProps) {
+  const depositAmount = useDepositAmount()
+  const deposit = formatToFrenchDecimal(depositAmount).replace(' ', '')
+
   function onButtonPress() {
     props.swiperRef?.current?.goToNext()
   }
+
   return (
     <GenericAchievementCard
       animation={TutorialOffers}
@@ -20,7 +26,7 @@ export function SecondCard(props: AchievementCardKeyProps) {
       pauseAnimationOnRenderAtFrame={62}
       subTitle={_(t`et si tu es...`)}
       text={_(
-        t`dans l’année de tes 18 ans, obtiens l’aide financière pass Culture d’un montant de 300€ à dépenser dans l’application.`
+        t`dans l’année de tes 18 ans, obtiens l’aide financière pass Culture d’un montant de ${deposit} à dépenser dans l’application.`
       )}
       title={_(t`Des offres pour tous`)}
       swiperRef={props.swiperRef}
