fbe7b22d1d0d38a7fbe6b3973686e980955e566e
Justine Mignot <justinem@bam.tech>
2021-04-08 18:40:55 +0200

    :sparkles: (PC-7201) remove snackbar error on short dynamic links

--- src/features/deeplinks/listener.ios.ts
+++ src/features/deeplinks/listener.ios.ts
@@ -7,7 +7,11 @@ import { DeeplinkEvent } from './types'
 import { useDeeplinkUrlHandler } from './useDeeplinkUrlHandler'
 import { FIREBASE_DYNAMIC_LINK_DOMAIN, FIREBASE_DYNAMIC_LINK_PARAMS } from './utils'
 
-const isFirebaseDynamicLinks = (url: string) => url.startsWith(FIREBASE_DYNAMIC_LINK_DOMAIN)
+const isFirebaseDynamicLink = (url: string) => url.startsWith(FIREBASE_DYNAMIC_LINK_DOMAIN)
+const isFirebaseLongDynamicLink = (url: string): boolean | undefined => {
+  if (!isFirebaseDynamicLink(url)) return undefined
+  return url.includes('?link=')
+}
 
 /* For Firebase Dynamic Links with params (exemple /offer?id=234)
  * we must use long dynamic links, and there are not recognized by dynamicLinks().onLink
@@ -22,13 +26,18 @@ const convertLongDynamicLinkToUniversalLink = (event: DeeplinkEvent): string => 
 export function useListenDeepLinksEffect() {
   const handleDeeplinkUrl = useDeeplinkUrlHandler()
 
-  const handleUniversalLinks = (event: DeeplinkEvent) => {
-    // Firebase Dynamic Links are handled with dynamicLinks().onLink
-    if (isFirebaseDynamicLinks(event.url)) {
-      handleDeeplinkUrl({ url: convertLongDynamicLinkToUniversalLink(event) })
-    } else {
-      handleDeeplinkUrl(event)
+  const handleLinks = (event: DeeplinkEvent) => {
+    const isFirebaseLongLink = isFirebaseLongDynamicLink(event.url)
+    const isUniversalLink = isFirebaseLongLink === undefined
+    if (isUniversalLink) {
+      // Universal links: https://app.passculture-{env}.beta.gouv.fr/<routeName>
+      return handleDeeplinkUrl(event)
+    } else if (isFirebaseLongLink === true) {
+      // Long Firebase Dynamic Links: https://passculture{env}.page.link/?link=https://app.passculture-{env}.beta.gouv.fr/<routeName>?param=214906&apn=app.passculture.testing&isi=1557887412&ibi=app.passculture.test&efr=1
+      return handleDeeplinkUrl({ url: convertLongDynamicLinkToUniversalLink(event) })
     }
+    // Short Firebase Dynamic Links: https://passculture{env}.page.link/<routeName>
+    // => handled with dynamicLinks().onLink
   }
 
   const handleDynamicLink = (event: DeeplinkEvent) => {
@@ -37,7 +46,7 @@ export function useListenDeepLinksEffect() {
 
   useEffect(() => {
     // Universal links
-    Linking.addEventListener('url', handleUniversalLinks)
+    Linking.addEventListener('url', handleLinks)
     // Firebase Dynamic links
     const unsubscribe = dynamicLinks().onLink(handleDynamicLink)
     return () => {
