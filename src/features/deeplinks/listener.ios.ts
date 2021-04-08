602c7a7b9fc70b93058a82b250ffae1924fc7fb1
Justine Mignot <justinem@bam.tech>
2021-04-08 11:34:28 +0200

    :ok_hand: (PC-7201) take review into account

--- src/features/deeplinks/listener.ios.ts
+++ src/features/deeplinks/listener.ios.ts
@@ -2,14 +2,24 @@ import dynamicLinks from '@react-native-firebase/dynamic-links'
 import { useEffect } from 'react'
 import { Linking } from 'react-native'
 
+import { DeeplinkEvent } from './types'
 import { useDeeplinkUrlHandler } from './useDeeplinkUrlHandler'
+import { FIREBASE_DYNAMIC_LINK_DOMAIN } from './utils'
+
+const isFirebaseDynamicLinks = (url: string) => url.startsWith(FIREBASE_DYNAMIC_LINK_DOMAIN)
 
 export function useListenDeepLinksEffect() {
   const handleDeeplinkUrl = useDeeplinkUrlHandler()
 
+  const handleUniversalLinks = (event: DeeplinkEvent) => {
+    // Firebase Dynamic Links are handled with dynamicLinks().onLink
+    if (isFirebaseDynamicLinks(event.url)) return
+    handleDeeplinkUrl(event)
+  }
+
   useEffect(() => {
     // Universal links
-    Linking.addEventListener('url', handleDeeplinkUrl)
+    Linking.addEventListener('url', handleUniversalLinks)
     // Firebase Dynamic links
     const unsubscribe = dynamicLinks().onLink(handleDeeplinkUrl)
     return () => {
