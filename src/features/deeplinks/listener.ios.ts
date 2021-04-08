28f99a1292f87ea0abb35b1ce4dc31892b159d0e
Justine Mignot <justinem@bam.tech>
2021-04-08 18:19:49 +0200

    :sparkles: (PC-7201) handle long dynamic links on ios
    we need to do it to be able to handle links with params

--- src/features/deeplinks/listener.ios.ts
+++ src/features/deeplinks/listener.ios.ts
@@ -1,19 +1,37 @@
 import dynamicLinks from '@react-native-firebase/dynamic-links'
 import { useEffect } from 'react'
 import { Linking } from 'react-native'
+import { omit } from 'search-params'
 
 import { DeeplinkEvent } from './types'
 import { useDeeplinkUrlHandler } from './useDeeplinkUrlHandler'
-import { FIREBASE_DYNAMIC_LINK_DOMAIN } from './utils'
+import { FIREBASE_DYNAMIC_LINK_DOMAIN, FIREBASE_DYNAMIC_LINK_PARAMS } from './utils'
 
 const isFirebaseDynamicLinks = (url: string) => url.startsWith(FIREBASE_DYNAMIC_LINK_DOMAIN)
+
+/* For Firebase Dynamic Links with params (exemple /offer?id=234)
+ * we must use long dynamic links, and there are not recognized by dynamicLinks().onLink
+ * so we handle it manually
+ */
+const convertLongDynamicLinkToUniversalLink = (event: DeeplinkEvent): string => {
+  const searchParams = event.url.replace(`${FIREBASE_DYNAMIC_LINK_DOMAIN}?`, '')
+  const paramsString = omit(searchParams, FIREBASE_DYNAMIC_LINK_PARAMS).querystring
+  return paramsString.replace(/^link=/, '')
+}
 
 export function useListenDeepLinksEffect() {
   const handleDeeplinkUrl = useDeeplinkUrlHandler()
 
   const handleUniversalLinks = (event: DeeplinkEvent) => {
     // Firebase Dynamic Links are handled with dynamicLinks().onLink
-    if (isFirebaseDynamicLinks(event.url)) return
+    if (isFirebaseDynamicLinks(event.url)) {
+      handleDeeplinkUrl({ url: convertLongDynamicLinkToUniversalLink(event) })
+    } else {
+      handleDeeplinkUrl(event)
+    }
+  }
+
+  const handleDynamicLink = (event: DeeplinkEvent) => {
     handleDeeplinkUrl(event)
   }
 
@@ -21,7 +39,7 @@ export function useListenDeepLinksEffect() {
     // Universal links
     Linking.addEventListener('url', handleUniversalLinks)
     // Firebase Dynamic links
-    const unsubscribe = dynamicLinks().onLink(handleDeeplinkUrl)
+    const unsubscribe = dynamicLinks().onLink(handleDynamicLink)
     return () => {
       Linking.removeEventListener('url', handleDeeplinkUrl)
       unsubscribe()
