c9f4396c20341b243fc34ffa21bb4db229ce031a
Justine Mignot <justinem@bam.tech>
2021-04-08 18:19:49 +0200

    :sparkles: (PC-7201) handle long dynamic links on ios
    we need to do it to be able to handle links with params

--- src/features/deeplinks/utils.ts
+++ src/features/deeplinks/utils.ts
@@ -2,3 +2,25 @@ import { env } from 'libs/environment'
 
 export const DEEPLINK_DOMAIN = `https://${env.UNIVERSAL_LINK}/`
 export const FIREBASE_DYNAMIC_LINK_DOMAIN = `https://${env.FIREBASE_DYNAMIC_LINK}/`
+
+// See all params here: https://firebase.google.com/docs/dynamic-links/create-manually
+export const FIREBASE_DYNAMIC_LINK_PARAMS = [
+  'apn',
+  'afl',
+  'amv',
+  'ibi',
+  'ifl',
+  'ius',
+  'ipfl',
+  'ipbi',
+  'isi',
+  'imv',
+  'efr',
+  'ofl',
+  'utm_source',
+  'utm_medium',
+  'utm_campaign',
+  'utm_term',
+  'utm_content',
+  'gclid',
+]
