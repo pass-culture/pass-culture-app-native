a5c7201958baaeb57b4001f81eb5d49e173ff303
blixit <celestinya@gmail.com>
2021-04-09 14:44:52 +0200

    (PC-7477) Added the 'see itinerary button' to the booking details page

--- src/libs/itinerary/useOpenItinerary.test.ts
+++ src/libs/itinerary/useOpenItinerary.test.ts
@@ -6,46 +6,43 @@ import * as Itinerary from 'libs/itinerary/useItinerary'
 import useOpenItinerary from './useOpenItinerary'
 
 describe('useOpenItinerary', () => {
+  let useItinerary: jest.SpyInstance<ReturnType<typeof Itinerary.useItinerary>, []> | undefined
+
+  beforeEach(() => {
+    useItinerary = jest.spyOn(Itinerary, 'useItinerary')
+  })
+  afterEach(() => useItinerary?.mockRestore())
+
   describe('beforeNavigate', () => {
     it('should call beforeNavigate', () => {
-      const useItinerary = jest
-        .spyOn(Itinerary, 'useItinerary')
-        .mockReturnValue({ availableApps: [AppEnum.WAZE], navigateTo: jest.fn() })
+      useItinerary?.mockReturnValue({ availableApps: [AppEnum.WAZE], navigateTo: jest.fn() })
       const beforeNavigate = jest.fn()
 
       const { result } = renderHook(() => useOpenItinerary(0.45, 1.45, beforeNavigate))
       result.current.openItinerary()
 
       expect(beforeNavigate).toBeCalled()
-      useItinerary.mockRestore()
     })
     it.each([
       [0.45, undefined],
       [undefined, 0.45],
     ])('should not call beforeNavigate', (lat, lng) => {
-      const useItinerary = jest
-        .spyOn(Itinerary, 'useItinerary')
-        .mockReturnValue({ availableApps: [AppEnum.WAZE], navigateTo: jest.fn() })
+      useItinerary?.mockReturnValue({ availableApps: [AppEnum.WAZE], navigateTo: jest.fn() })
       const beforeNavigate = jest.fn()
 
       const { result } = renderHook(() => useOpenItinerary(lat, lng, beforeNavigate))
       result.current.openItinerary()
 
       expect(beforeNavigate).not.toBeCalled()
-      useItinerary.mockRestore()
     })
   })
   describe('canOpenItinerary', () => {
     it('should return true', () => {
-      const useItinerary = jest
-        .spyOn(Itinerary, 'useItinerary')
-        .mockReturnValue({ availableApps: [AppEnum.WAZE], navigateTo: jest.fn() })
+      useItinerary?.mockReturnValue({ availableApps: [AppEnum.WAZE], navigateTo: jest.fn() })
 
       const { result } = renderHook(() => useOpenItinerary(0.45, 1.45))
 
       expect(result.current.canOpenItinerary).toBe(true)
-
-      useItinerary.mockRestore()
     })
     it.each<[string, [number | undefined, number | undefined, AppEnum[] | undefined]]>([
       ['latitude not defined', [undefined, 0.456, [AppEnum.WAZE]]],
@@ -53,14 +50,11 @@ describe('useOpenItinerary', () => {
       ['availableApps not defined', [0.456, 0.321, undefined]],
     ])('should return false when %s', (_reason, context) => {
       const [lat, lng, availableApps] = context
-      const useItinerary = jest
-        .spyOn(Itinerary, 'useItinerary')
-        .mockReturnValue({ availableApps, navigateTo: jest.fn() })
+      useItinerary?.mockReturnValue({ availableApps, navigateTo: jest.fn() })
 
       const { result } = renderHook(() => useOpenItinerary(lat, lng))
 
       expect(result.current.canOpenItinerary).toBe(false)
-      useItinerary.mockRestore()
     })
   })
 })
