bfcc748afa6d9f1473174b64668ea6fa63d85823
Marion Barré <marionb@bam.tech>
2021-04-13 16:23:53 +0200

    :sparkles: (CancelBookingModal) handle on success cancel booking

--- src/features/bookings/components/CancelBookingModal.tsx
+++ src/features/bookings/components/CancelBookingModal.tsx
@@ -1,4 +1,5 @@
 import { t } from '@lingui/macro'
+import { useNavigation } from '@react-navigation/native'
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 
@@ -6,12 +7,14 @@ import { UserProfileResponse } from 'api/gen'
 import { Booking } from 'features/bookings/components/types'
 import { useUserProfileInfo } from 'features/home/api'
 import { Credit, useAvailableCredit } from 'features/home/services/useAvailableCredit'
+import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { isUserBeneficiary, isUserExBeneficiary } from 'features/profile/utils'
 import { analytics } from 'libs/analytics'
 import { convertCentsToEuros } from 'libs/parsers/pricesConversion'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
 import { AppModal } from 'ui/components/modals/AppModal'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
 import { Close } from 'ui/svg/icons/Close'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 
@@ -31,9 +34,15 @@ export const CancelBookingModal: FunctionComponent<Props> = ({
   const { data: user } = useUserProfileInfo()
   const credit = useAvailableCredit()
   const refundRule = getRefundRule(booking, user, credit)
+  const { navigate } = useNavigation<UseNavigationType>()
+  const { showSuccessSnackBar } = useSnackBarContext()
 
   function onSuccess() {
-    // TODO add sucess behaviour
+    navigate('Bookings')
+    showSuccessSnackBar({
+      message: t`La réservation a bien été annulée. Tu pourras la retrouver dans tes réservations terminées`,
+      timeout: SNACK_BAR_TIME_OUT,
+    })
   }
 
   function onError() {
