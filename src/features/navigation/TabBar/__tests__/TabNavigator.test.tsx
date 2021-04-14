01eeb413dbe8de870cb6905f2ef0616797037a53
Justine Mignot <justinem@bam.tech>
2021-04-14 16:20:48 +0200

    :triangular_flag_on_post: (PC-8109) removing feature flipping for campaigns

--- src/features/navigation/TabBar/__tests__/TabNavigator.test.tsx
+++ src/features/navigation/TabBar/__tests__/TabNavigator.test.tsx
@@ -1,6 +1,5 @@
 import { UserProfileResponse } from 'api/gen'
 import { IAuthContext } from 'features/auth/AuthContext'
-import { env } from 'libs/environment'
 
 import { shouldDisplayTabIconPredicate } from '../helpers'
 import { TabRoute } from '../types'
@@ -14,7 +13,6 @@ jest.mock('libs/environment', () => ({
 describe('TabNavigator', () => {
   describe('shouldDisplayTabIconPredicate', () => {
     it('should display "Bookings" icon for authenticated and beneficiary users', () => {
-      env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING = true
       const shouldDisplayFunction = shouldDisplayTabIconPredicate(
         { isLoggedIn: true } as IAuthContext,
         {
@@ -30,9 +28,7 @@ describe('TabNavigator', () => {
       [true, false],
       [false, true],
       [false, false],
-      [true, true],
     ])('should NOT display "Bookings" icon ', (isLoggedIn, isBeneficiary) => {
-      env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING = false
       const shouldDisplayFunction = shouldDisplayTabIconPredicate(
         { isLoggedIn } as IAuthContext,
         {
