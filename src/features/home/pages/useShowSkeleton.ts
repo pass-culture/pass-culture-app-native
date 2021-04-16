059ae30a6ceaa0d13d657c107847db82b0f84641
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-04-16 17:28:40 +0200

    Revert "(PC-8066) avoid setting state on unmounted Home in case of craash"
    This reverts commit d998e9718219b96f900bd8bbac0a14aed54b3995.

--- src/features/home/pages/useShowSkeleton.ts
+++ src/features/home/pages/useShowSkeleton.ts
@@ -1,5 +1,4 @@
-import debounce from 'lodash.debounce'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useIsFetching } from 'react-query'
 
 import { DEFAULT_SPLASHSCREEN_DELAY } from 'libs/splashscreen'
@@ -11,24 +10,23 @@ const DELAY = ANIMATION_DELAY + DEFAULT_SPLASHSCREEN_DELAY
 
 export const useShowSkeleton = function () {
   const [showSkeleton, setShowSkeleton] = useState(true)
-  const debouncedShowSkeleton = useRef(debounce(() => setShowSkeleton(false), DELAY)).current
-
   const isFetchingHomepageModules = useIsFetching({ queryKey: 'homepageModules' })
   const isFetchingAlgoliaModules = useIsFetching({ queryKey: 'algoliaModule' })
   const isFetchingOfferIds = useIsFetching({ queryKey: 'recommendationOfferIds' })
   const isFetchingRecommendedHits = useIsFetching({ queryKey: 'recommendationHits' })
 
   useEffect(() => {
+    let timeout: NodeJS.Timeout
     if (
       isFetchingAlgoliaModules === 0 &&
       isFetchingHomepageModules === 0 &&
       isFetchingOfferIds === 0 &&
       isFetchingRecommendedHits === 0
     ) {
-      debouncedShowSkeleton()
+      timeout = setTimeout(() => setShowSkeleton(false), DELAY)
     }
     return () => {
-      debouncedShowSkeleton.cancel()
+      if (timeout) clearTimeout(timeout)
     }
   }, [
     isFetchingAlgoliaModules,
