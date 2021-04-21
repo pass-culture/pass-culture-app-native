bc4c9c0c8566e8bb26d1ad0800a4bf2a6952e7a7
blixit <celestinya@gmail.com>
2021-04-21 08:52:32 +0200

    (PC-8219) CodeInput - make onChangeValue to be called with cthe las typed value and input position

--- src/ui/components/inputs/CodeInput.tsx
+++ src/ui/components/inputs/CodeInput.tsx
@@ -1,4 +1,4 @@
-import React, { RefObject, useEffect, useState } from 'react'
+import React, { RefObject, useRef, useState } from 'react'
 import { TextInput } from 'react-native'
 import styled from 'styled-components/native'
 
@@ -9,14 +9,33 @@ export interface CodeValidation {
   isValid: boolean
 }
 
+/**
+ * WARNING: Today there are no requirements to set the value of this component from outside.
+ * That's why it is not set to be controlled and you won't be able to set its value.
+ * You can just get its inner value using the props onChangeValue.
+ * If the requirement comes one day, do the following:
+ * - add a new props: 'value: string'
+ * - creates a function that converts a string to a map (index=>letter) and use it to initialise 'inputValues'
+ */
 export type CodeInputProps = {
   codeLength: number
   autoFocus?: boolean
+  placeholder: string
 } & (
   | {
       enableValidation: true
-      onChangeValue?: (value: string | null, codeValidation: CodeValidation) => void
+      onChangeValue?: (
+        value: string | null,
+        codeValidation: CodeValidation,
+        inputPosition: number | string,
+        inputValue: string
+      ) => void
+      /**
+       * Used to compute the validation state.
+       * WARNING: to perform state update based on the validation state, use onChangeValue
+       */
       isValid: (value: string | null, isComplete: boolean) => boolean
+      isInputValid: (value: string, position: number) => boolean
     }
   | {
       enableValidation?: false
@@ -25,31 +44,27 @@ export type CodeInputProps = {
 )
 
 export const CodeInput = (props: CodeInputProps) => {
-  const [inputsRef, setInputsRef] = useState<Record<string, React.RefObject<TextInput>>>({})
-  const [_, setInputValues] = useState<Record<string, string>>({})
+  const inputsRef = useRef(createMapOfRef<TextInput>({}, props.codeLength))
+  const [inputValues, setInputValues] = useState<Record<string, string>>({})
 
-  useEffect(() => {
-    setInputsRef(createMapOfRef<TextInput>({}, props.codeLength))
-    return () => setInputsRef({})
-  }, [props.codeLength])
-
-  const onChangeValue = (value: string, identifier: number) => {
-    setInputValues(inputUpdator(value, identifier, props))
+  const onChangeValue = (value: string, inputId: number) => {
+    setInputValues(inputUpdator(value, inputId, props))
   }
 
   return (
     <Container testID="code-input-container">
-      {Object.keys(inputsRef || {}).map((key) => {
+      {Object.keys(inputsRef.current || {}).map((key) => {
         const index = Number(key)
+        const inputValue = inputValues[index] || ''
         return (
           <ShortInput
             key={index} // using index is safe since inputs are ordered and refreshed atomically
             autoFocus={index === 0 ? props.autoFocus : false}
             identifier={index}
-            isValid={true}
+            isValid={props.enableValidation ? props.isInputValid(inputValue, index) : true}
             onChangeValue={onChangeValue}
-            placeholder="0"
-            ref={inputsRef[index]}
+            placeholder={props.placeholder}
+            ref={inputsRef.current[index]}
           />
         )
       })}
@@ -76,12 +91,15 @@ function createMapOfRef<U, T extends RefObject<U> = RefObject<U>>(
  * Generates a state action to update the current map of values with the digit
  * typed into the current input
  * @param newDigitValue the value typed into the current input
- * @param identifier the input identifier
+ * @param inputPosition the input identifier
  * @param props the CodeInput props
+ * @returns a react state action
  */
-export const inputUpdator = (newDigitValue: string, identifier: number, props: CodeInputProps) => (
-  currentDigitValues: Record<string, string>
-) => {
+export const inputUpdator = (
+  newDigitValue: string,
+  inputPosition: number,
+  props: CodeInputProps
+) => (currentDigitValues: Record<string, string>) => {
   let completion = 0
   let code = ''
 
@@ -92,7 +110,7 @@ export const inputUpdator = (newDigitValue: string, identifier: number, props: C
      * - for the current index: the value that will be stored into the state
      * - for the others indexes: the value already stored in the state
      */
-    const consideredValue = index === identifier ? newDigitValue : currentDigitValue
+    const consideredValue = index === inputPosition ? newDigitValue : currentDigitValue
 
     completion += Number(consideredValue.length > 0)
     code += consideredValue
@@ -101,13 +119,18 @@ export const inputUpdator = (newDigitValue: string, identifier: number, props: C
   const isComplete = completion === props.codeLength
 
   if (props.enableValidation) {
-    props.onChangeValue?.(code, {
-      isComplete,
-      isValid: props.isValid(code, isComplete),
-    })
+    props.onChangeValue?.(
+      code,
+      {
+        isComplete,
+        isValid: props.isValid(code, isComplete),
+      },
+      inputPosition,
+      newDigitValue
+    )
   } else {
     props.onChangeValue?.(code)
   }
 
-  return { ...currentDigitValues, [identifier]: newDigitValue }
+  return { ...currentDigitValues, [inputPosition]: newDigitValue }
 }
