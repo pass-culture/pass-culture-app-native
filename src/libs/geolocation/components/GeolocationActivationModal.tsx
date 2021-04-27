017d0a085ca7bcea9aebdbad4a67b31cbf920306
BenStirrup <benjamin.stirrup@gmail.com>
2021-04-27 18:02:31 +0200

    (PC-8372) Redirect user to settings when they want to deactivate geoloc from profile screen

--- src/libs/geolocation/components/GeolocationActivationModal.tsx
+++ src/libs/geolocation/components/GeolocationActivationModal.tsx
@@ -9,6 +9,9 @@ import { AppInformationModal } from 'ui/components/modals/AppInformationModal'
 import { BicolorLocationPointer } from 'ui/svg/icons/BicolorLocationPointer'
 import { ColorsEnum, Spacer, Typo } from 'ui/theme'
 
+import { useGeolocation } from '../GeolocationWrapper'
+import { GeolocPermissionState } from '../permissionState.d'
+
 type Props = {
   hideGeolocPermissionModal: () => void
   isGeolocPermissionModalVisible: boolean
@@ -19,40 +22,47 @@ export const GeolocationActivationModal: React.FC<Props> = ({
   isGeolocPermissionModalVisible,
   hideGeolocPermissionModal,
   onPressGeolocPermissionModalButton,
-}) => (
-  <AppInformationModal
-    title={t`Paramètres de localisation`}
-    visible={isGeolocPermissionModalVisible}
-    onCloseIconPress={hideGeolocPermissionModal}
-    testIdSuffix="geoloc-permission-modal">
-    <React.Fragment>
-      <BicolorLocationPointer
-        size={140}
-        color={ColorsEnum.GREY_DARK}
-        color2={ColorsEnum.GREY_DARK}
-      />
-      <Spacer.Column numberOfSpaces={2} />
-      <InformationText>
-        {t`Retrouve toutes les offres autour de chez toi en activant les données de localisation.`}
-      </InformationText>
-      <Spacer.Column numberOfSpaces={4} />
+}) => {
+  const { permissionState } = useGeolocation()
+  const callToActionMessage =
+    permissionState === GeolocPermissionState.GRANTED
+      ? t`Désactiver la géolocalisation`
+      : t`Activer la géolocalisation`
+  return (
+    <AppInformationModal
+      title={t`Paramètres de localisation`}
+      visible={isGeolocPermissionModalVisible}
+      onCloseIconPress={hideGeolocPermissionModal}
+      testIdSuffix="geoloc-permission-modal">
+      <React.Fragment>
+        <BicolorLocationPointer
+          size={140}
+          color={ColorsEnum.GREY_DARK}
+          color2={ColorsEnum.GREY_DARK}
+        />
+        <Spacer.Column numberOfSpaces={2} />
+        <InformationText>
+          {t`Retrouve toutes les offres autour de chez toi en activant les données de localisation.`}
+        </InformationText>
+        <Spacer.Column numberOfSpaces={4} />
 
-      <InformationText>
-        {Platform.OS === 'android'
-          ? t`Tu peux activer ou désactiver cette fonctionnalité dans Autorisations > Localisation.`
-          : t`Tu peux activer ou désactiver cette fonctionnalité dans les paramètres de localisation ton téléphone.`}
-      </InformationText>
-      <Spacer.Column numberOfSpaces={6} />
-      <ButtonPrimary
-        title={t`Activer la géolocalisation`}
-        onPress={() => {
-          analytics.logOpenLocationSettings()
-          onPressGeolocPermissionModalButton()
-        }}
-      />
-    </React.Fragment>
-  </AppInformationModal>
-)
+        <InformationText>
+          {Platform.OS === 'android'
+            ? t`Tu peux activer ou désactiver cette fonctionnalité dans Autorisations > Localisation.`
+            : t`Tu peux activer ou désactiver cette fonctionnalité dans les paramètres de localisation ton téléphone.`}
+        </InformationText>
+        <Spacer.Column numberOfSpaces={6} />
+        <ButtonPrimary
+          title={callToActionMessage}
+          onPress={() => {
+            analytics.logOpenLocationSettings()
+            onPressGeolocPermissionModalButton()
+          }}
+        />
+      </React.Fragment>
+    </AppInformationModal>
+  )
+}
 
 const InformationText = styled(Typo.Body)({
   textAlign: 'center',
