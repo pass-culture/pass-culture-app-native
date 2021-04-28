39b41fcc857d51c48b4487de66128415c031c3f8
Marion Barré <marionb@bam.tech>
2021-04-28 10:00:11 +0200

    :white_check_mark: (helpers) add tests

---
+++ src/api/__tests__/helpers.test.ts
@@ -0,0 +1,18 @@
+import { DefaultApi } from '../gen'
+import { safeFetch } from '../helpers'
+
+global.fetch = jest.fn()
+const api = new DefaultApi({})
+
+describe('[api] helpers', () => {
+  describe('[method] safeFetch', () => {
+    it('should call fetch with populated header', async () => {
+      await safeFetch('url', {}, api)
+      expect(global.fetch).toHaveBeenCalledWith('url', {
+        headers: {
+          'device-id': 'ad7b7b5a169641e27cadbdb35adad9c4ca23099a',
+        },
+      })
+    })
+  })
+})
