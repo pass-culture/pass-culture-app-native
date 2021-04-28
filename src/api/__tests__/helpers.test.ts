3d8cf099e1ca4b75db2a98e61750eb2a79739477
Marion Barré <marionb@bam.tech>
2021-04-28 14:42:21 +0200

    :white_check_mark: (helpers ) add tests

--- src/api/__tests__/helpers.test.ts
+++ src/api/__tests__/helpers.test.ts
@@ -1,19 +1,30 @@
 import { DefaultApi } from '../gen'
 import { safeFetch } from '../helpers'
 
-global.fetch = jest.fn()
+global.fetch = jest.fn().mockResolvedValue('apiResponse')
 const api = new DefaultApi({})
 
 describe('[api] helpers', () => {
   describe('[method] safeFetch', () => {
     it('should call fetch with populated header', async () => {
-      await safeFetch('url', {}, api)
+      const response = await safeFetch('url', {}, api)
       expect(global.fetch).toHaveBeenCalledWith('url', {
         headers: {
           'app-version': '1.10.5',
           'device-id': 'ad7b7b5a169641e27cadbdb35adad9c4ca23099a',
         },
       })
+      expect(response).toEqual('apiResponse')
+    })
+    it('should call fetch with populated header when route is in NotAuthenticatedCalls', async () => {
+      const response = await safeFetch('native/v1/account', {}, api)
+      expect(global.fetch).toHaveBeenCalledWith('native/v1/account', {
+        headers: {
+          'app-version': '1.10.5',
+          'device-id': 'ad7b7b5a169641e27cadbdb35adad9c4ca23099a',
+        },
+      })
+      expect(response).toEqual('apiResponse')
     })
   })
 })
