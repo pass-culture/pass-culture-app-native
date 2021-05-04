36def506ec666b1a6fcbb2a68cdd2166175d25b0
Anouk Hello <anouk.hello@polyconseil.fr>
2021-05-04 18:28:42 +0200

    (PC-8164) setPhoneNumberValidationCode: disable button if input is not valid

--- src/features/auth/signup/SetPhoneNumberValidationCode.test.tsx
+++ src/features/auth/signup/SetPhoneNumberValidationCode.test.tsx
@@ -5,6 +5,7 @@ import { SetPhoneNumberValidationCode } from 'features/auth/signup/SetPhoneNumbe
 import { contactSupport } from 'features/auth/support.services'
 import { fireEvent, render } from 'tests/utils'
 import * as ModalModule from 'ui/components/modals/useModal'
+import { ColorsEnum } from 'ui/theme'
 
 describe('SetPhoneNumberValidationCode', () => {
   describe('Quit process', () => {
@@ -44,6 +45,48 @@ describe('SetPhoneNumberValidationCode', () => {
 
       await waitForExpect(() => {
         expect(contactSupport.forPhoneNumberConfirmation).toHaveBeenCalled()
+      })
+    })
+  })
+
+  describe('Continue button', () => {
+    it('should enable continue button if input is valid and complete', async () => {
+      const { getByTestId, rerender } = render(
+        <SetPhoneNumberValidationCode dismissModal={jest.fn()} visible={true} />
+      )
+
+      const continueButton = getByTestId('button-container-continue')
+      expect(continueButton.props.style.backgroundColor).toEqual(ColorsEnum.GREY_LIGHT)
+
+      const codeInputContainer = getByTestId('code-input-container')
+      for (let i = 0; i < codeInputContainer.props.children.length; i++) {
+        fireEvent.changeText(getByTestId(`input-${i}`), '1')
+        rerender(<SetPhoneNumberValidationCode dismissModal={jest.fn()} visible={true} />)
+      }
+
+      await waitForExpect(() => {
+        expect(continueButton.props.style.backgroundColor).toEqual(ColorsEnum.PRIMARY)
+      })
+    })
+
+    it.each([
+      ['empty', ''],
+      ['includes string', 's09453'],
+      ['is too short', '54'],
+    ])('should not enable continue button when "%s"', async (_reason, codeTyped) => {
+      const { getByTestId, rerender } = render(
+        <SetPhoneNumberValidationCode dismissModal={jest.fn()} visible={true} />
+      )
+
+      const continueButton = getByTestId('button-container-continue')
+
+      for (let i = 0; i < codeTyped.length; i++) {
+        fireEvent.changeText(getByTestId(`input-${i}`), codeTyped[i])
+        rerender(<SetPhoneNumberValidationCode dismissModal={jest.fn()} visible={true} />)
+      }
+
+      await waitForExpect(() => {
+        expect(continueButton.props.style.backgroundColor).toEqual(ColorsEnum.GREY_LIGHT)
       })
     })
   })
