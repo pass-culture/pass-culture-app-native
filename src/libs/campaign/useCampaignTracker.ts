3940b6c73a9e6ae0310ba9ad88a96c3edced4fd6
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-05-05 19:57:20 +0200

    (PC-8527) Wait 60s before sending events to AppsFlyer

--- src/libs/campaign/useCampaignTracker.ts
+++ src/libs/campaign/useCampaignTracker.ts
@@ -5,18 +5,27 @@ import { analytics } from 'libs/analytics'
 import { env } from 'libs/environment'
 import { useTrackingConsent } from 'libs/trackingConsent'
 
+// We ask for the user's consent on app launch. We defer the sending of events to AppsFlyer,
+// depending on the user's response, by the following delay.
+// Reference:
+// https://support.appsflyer.com/hc/en-us/articles/207032066-iOS-SDK-V6-X-integration-guide-for-developers#integration-33-configuring-app-tracking-transparency-att-support
+const TIME_TO_WAIT_FOR_ATT_CONSENT = 60 // in seconds
+
 export const useCampaignTracker = () => {
-  const consentTracking = useTrackingConsent()
+  // First we ask for the user's consent to access their IDFA information
+  useTrackingConsent()
 
   useEffect(() => {
-    if (__DEV__ || !consentTracking) return
+    if (__DEV__) return
 
+    // Second we initialize the SDK.
     appsFlyer.initSdk(
       {
         devKey: env.APPS_FLYER_DEV_KEY,
-        isDebug: true, // set to true if you want to see data in the logs
+        isDebug: env.ENV === 'testing',
         appId: env.IOS_APP_STORE_ID,
         onInstallConversionDataListener: false,
+        timeToWaitForATTUserAuthorization: TIME_TO_WAIT_FOR_ATT_CONSENT,
       },
       () => {
         analytics.logCampaignTrackerEnabled()
@@ -25,5 +34,5 @@ export const useCampaignTracker = () => {
         console.error(error)
       }
     )
-  }, [consentTracking])
+  }, [])
 }
