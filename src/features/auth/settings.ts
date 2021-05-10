14825560da4fdb17a4d887c2360c9448f85d1163
blixit <celestinya@gmail.com>
2021-05-10 15:07:13 +0200

    (PC-7887) Make useAppSettings query optionnal

--- src/features/auth/settings.ts
+++ src/features/auth/settings.ts
@@ -4,6 +4,8 @@ import { api } from 'api/api'
 import { SettingsResponse } from 'api/gen'
 import { QueryKeys } from 'libs/queryKeys'
 
-export function useAppSettings() {
-  return useQuery<SettingsResponse>(QueryKeys.SETTINGS, () => api.getnativev1settings())
+export function useAppSettings(enabled = true) {
+  return useQuery<SettingsResponse>(QueryKeys.SETTINGS, () => api.getnativev1settings(), {
+    enabled,
+  })
 }
