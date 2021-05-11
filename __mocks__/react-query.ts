40585ec4a6b9e32b758160b5286ceed69691eae9
Marion Barré <marionb@bam.tech>
2021-05-11 17:43:06 +0200

    (PC-8207) add react-query mock

--- __mocks__/react-query.ts
+++ __mocks__/react-query.ts
@@ -1,8 +1,19 @@
 const invalidateQueries = jest.fn()
-export const useMutation = jest
-  .fn()
-  .mockImplementation((mutationFunction, mutationOptions) => ({
-    mutationFunction,
-    mutationOptions,
-  }))
-export const useQueryClient = jest.fn().mockReturnValue({ invalidateQueries })
+
+export class QueryCache {
+  clear() {}
+}
+export class QueryClient {}
+
+export const QueryClientProvider = (component: React.ReactNode) => {
+  return component
+}
+
+export const useMutation = jest.fn().mockImplementation((mutationFunction, mutationOptions) => ({
+  mutationFunction,
+  mutationOptions,
+  mutate: () => {},
+}))
+export const useQueryClient = jest.fn().mockReturnValue({ invalidateQueries, getQueryData: () => {}})
+
+export const useQuery = jest.fn().mockResolvedValue({})
