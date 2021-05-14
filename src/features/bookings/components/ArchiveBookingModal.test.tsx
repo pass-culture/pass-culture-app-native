5584cf291261dde9dee1e6a35c41ed16aeb0dc6d
blixit <celestinya@gmail.com>
2021-05-14 14:41:26 +0200

    (PC-8357) Add a mutation to archive a booking

--- src/features/bookings/components/ArchiveBookingModal.test.tsx
+++ src/features/bookings/components/ArchiveBookingModal.test.tsx
@@ -1,14 +1,42 @@
+import { rest } from 'msw'
 import React from 'react'
+import waitForExpect from 'wait-for-expect'
 
 import { ArchiveBookingModal } from 'features/bookings/components/ArchiveBookingModal'
-import { fireEvent, render } from 'tests/utils'
+import { env } from 'libs/environment'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { server } from 'tests/server'
+import { fireEvent, render, superFlushWithAct } from 'tests/utils'
 
 describe('<ArchiveBookingModal />', () => {
   it('should call on onDismiss', () => {
     const onDismiss = jest.fn()
-    const { getByTestId } = render(<ArchiveBookingModal visible onDismiss={onDismiss} />)
+    const { getByTestId } = renderArchiveDigitalBookingOfferModal(onDismiss)
     const button = getByTestId('button-container-back-to-booking')
     fireEvent.press(button)
     expect(onDismiss).toBeCalled()
   })
+  it('should call the mutation without failure', async () => {
+    server.use(
+      rest.post(env.API_BASE_URL + '/native/v1/bookings/232423/toggle_display', (req, res, ctx) =>
+        res.once(ctx.status(204))
+      )
+    )
+    const onDismiss = jest.fn()
+    const { getByText } = renderArchiveDigitalBookingOfferModal(onDismiss)
+
+    const cancelButton = getByText('Terminer ma réservation')
+
+    fireEvent.press(cancelButton)
+
+    await superFlushWithAct()
+
+    await waitForExpect(() => {
+      expect(onDismiss).toBeCalled()
+    })
+  })
 })
+
+function renderArchiveDigitalBookingOfferModal(onDismiss: () => void) {
+  return render(reactQueryProviderHOC(<ArchiveBookingModal visible onDismiss={onDismiss} />))
+}
