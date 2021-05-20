c4d703dd81ac4169639263d19f9f04431c11bae4
Anouk Hello <anouk.hello@polyconseil.fr>
2021-05-20 12:51:22 +0200

    (PC-8783) use settings to display phone validation modals

--- src/features/auth/signup/TemporaryIdCheck.tsx
+++ src/features/auth/signup/TemporaryIdCheck.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 
+import { useAppSettings } from 'features/auth/settings'
 import { SetPhoneNumberModal } from 'features/auth/signup/SetPhoneNumberModal'
 import { SetPhoneValidationCodeModal } from 'features/auth/signup/SetPhoneValidationCodeModal'
 import { useModal } from 'ui/components/modals/useModal'
 
 export const TemporaryIdCheck: React.FC = function () {
+  const { data: settings } = useAppSettings()
   const [phoneNumber, setPhoneNumber] = useState('')
   const {
     visible: phoneNumberModalVisible,
     showModal: showPhoneNumberModal,
     hideModal: hidePhoneNumberModal,
-  } = useModal(true)
+  } = useModal(settings?.enablePhoneValidation)
 
   const {
     visible: phoneValidationModalVisible,
