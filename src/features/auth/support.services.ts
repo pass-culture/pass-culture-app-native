37f80d61e1aafcd59173d082c8e02d48147083b0
Emma Jamin <emmajamin@orange.fr>
2021-05-21 11:38:03 +0200

    (PC-8860) Change wording + redirection to website

--- src/features/auth/support.services.ts
+++ src/features/auth/support.services.ts
@@ -8,9 +8,9 @@ export const contactSupport = {
       analytics.logMailTo('forGenericQuestion')
     )
   },
-  forSignupConfirmationEmailNotReceived(email: string) {
+  forSignupConfirmationEmailNotReceived() {
     openExternalUrl(
-      `mailto:${env.SUPPORT_EMAIL_ADDRESS}?subject=Non-r%C3%A9ception%20d'email%20de%20confirmation%20de%20compte&body=Bonjour%2C%0D%0A%0D%0AJe%20vous%20%C3%A9cris%20afin%20de%20vous%20informer%20de%20la%20non-r%C3%A9ception%20de%20l'email%20de%20confirmation%20de%20compte%20associ%C3%A9%20%C3%A0%20l%E2%80%99adresse%20e-mail%20${email}%2C%20y%20compris%20dans%20mes%20spams.%0D%0A%0D%0ABien%20cordialement%2C`,
+      'https://aide.passculture.app/fr/articles/5257121-je-n-ai-pas-recu-le-mail-de-confirmation-de-creation-de-compte',
       false
     ).then(() => analytics.logMailTo('forSignupConfirmationEmailNotReceived'))
   },
