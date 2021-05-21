f22fb649e026d31c7b9d8d24fb6f4e2800fd7105
blixit <celestinya@gmail.com>
2021-05-21 14:54:45 +0200

    (PC-8863) Resolve the deeplink when the clipboard is loaded

--- src/features/deeplinks/pages/DeeplinkImporter.test.tsx
+++ src/features/deeplinks/pages/DeeplinkImporter.test.tsx
@@ -1,8 +1,13 @@
+import Clipboard from '@react-native-clipboard/clipboard'
 import React from 'react'
 
 import { navigate } from '__mocks__/@react-navigation/native'
 import { DeeplinkImporter } from 'features/deeplinks/pages/DeeplinkImporter'
-import { fireEvent, render } from 'tests/utils'
+import { FIREBASE_DYNAMIC_LINK_DOMAIN } from 'features/deeplinks/utils'
+import * as DeeplinkUtils from 'features/deeplinks/utils'
+import { render, fireEvent } from 'tests/utils'
+
+jest.mock('react-query')
 
 describe('DeeplinkImporter', () => {
   it('should navigate to the home', () => {
@@ -12,5 +17,18 @@ describe('DeeplinkImporter', () => {
     fireEvent.press(goToOffersButton)
 
     expect(navigate).toBeCalledWith('Home')
+  })
+  it('should resolve the link', () => {
+    const url = FIREBASE_DYNAMIC_LINK_DOMAIN + '/home'
+    Clipboard.setString(url)
+
+    const resolveHandlerSpy = jest.spyOn(DeeplinkUtils, 'resolveHandler')
+
+    const { getByTestId } = render(<DeeplinkImporter />)
+
+    const importButton = getByTestId('button-container-import')
+    fireEvent.press(importButton)
+
+    resolveHandlerSpy.mockRestore()
   })
 })
--- src/features/deeplinks/pages/DeeplinkImporter.tsx
+++ src/features/deeplinks/pages/DeeplinkImporter.tsx
@@ -4,6 +4,8 @@ import { useNavigation } from '@react-navigation/native'
 import * as React from 'react'
 import styled from 'styled-components/native'
 
+import { useDeeplinkUrlHandler } from 'features/deeplinks/useDeeplinkUrlHandler'
+import { resolveHandler } from 'features/deeplinks/utils'
 import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
 import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
@@ -13,7 +15,14 @@ import { BrokenConnection } from 'ui/svg/BrokenConnection'
 import { Spacer, Typo } from 'ui/theme'
 
 export const DeeplinkImporter: React.FC = () => {
+  const [url, setUrl] = React.useState('')
   const { navigate } = useNavigation<UseNavigationType>()
+  const handleDeeplinkUrl = useDeeplinkUrlHandler()
+  const resolveLink = () => {
+    if (url.length > 0) {
+      resolveHandler(handleDeeplinkUrl, true)({ url })
+    }
+  }
 
   return (
     <GenericInfoPage icon={BrokenConnection} title="" spacingMatrix={{ afterIcon: 1 }}>
@@ -24,9 +33,9 @@ export const DeeplinkImporter: React.FC = () => {
           ColorsEnum.WHITE
         }>{t`Pour copier ton lien, ouvre l'e-mail que tu as reçu, appuie sur le bouton ou le lien pendant environ 3 secondes, sélectionne l'option "Copier le lien" et clique sur le bouton "Importer le lien"`}</Explanation>
       <Spacer.Column numberOfSpaces={12} />
-      <TextInput placeholder={t`Colle ton lien ici ...`} />
+      <TextInput placeholder={t`Colle ton lien ici ...`} value={url} onChangeText={setUrl} />
       <Spacer.Column numberOfSpaces={8} />
-      <ButtonPrimaryWhite title="Importer le lien" testIdSuffix="import" />
+      <ButtonPrimaryWhite title="Importer le lien" onPress={resolveLink} testIdSuffix="import" />
       <Spacer.Column numberOfSpaces={4} />
       <ButtonTertiaryWhite
         title={t`Accéder aux offres`}
