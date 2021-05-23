dbc6e9dbe23a1974d9e21a3cc73882f891e83c8e
blixit <celestinya@gmail.com>
2021-05-23 17:35:10 +0200

    (PC-8913) Notify backend when IDCheck completed

--- src/features/auth/signup/BeneficiaryRequestSent.test.tsx
+++ src/features/auth/signup/BeneficiaryRequestSent.test.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import { UseMutationResult } from 'react-query'
 
 import { navigate } from '__mocks__/@react-navigation/native'
-import { render, fireEvent } from 'tests/utils'
+import * as AuthApi from 'features/auth/api'
+import { EmptyResponse } from 'libs/fetch'
+import { render, fireEvent, waitFor } from 'tests/utils'
 
 import { BeneficiaryRequestSent } from './BeneficiaryRequestSent'
 
-beforeEach(() => {
-  jest.clearAllMocks()
-})
+jest.mock('react-query')
 
 describe('<BeneficiaryRequestSent />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  it('should call notifyIdCheckCompleted inconditonnally', () => {
+    const notifyIdCheckCompleted = jest.fn()
+    const useNotifyIdCheckCompletedMock = jest
+      .spyOn(AuthApi, 'useNotifyIdCheckCompleted')
+      .mockReturnValue(({
+        mutate: notifyIdCheckCompleted,
+      } as unknown) as UseMutationResult<EmptyResponse, unknown, void, unknown>)
+
+    waitFor(() => {
+      expect(notifyIdCheckCompleted).toBeCalled()
+    })
+    useNotifyIdCheckCompletedMock.mockRestore()
+  })
   it('should redirect to cultural survey page WHEN "On y va !" button is clicked', async () => {
     const { findByText } = render(<BeneficiaryRequestSent />)
 
