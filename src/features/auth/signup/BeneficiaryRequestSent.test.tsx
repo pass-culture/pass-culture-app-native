523e74e5d1a722bdd4a5c94000e496896d53131b
BenStirrup <benjamin.stirrup@gmail.com>
2021-05-25 11:06:32 +0200

    (PC-8941) Navigate to cultural survey only if needsToFillCulturalSurvey is true

--- src/features/auth/signup/BeneficiaryRequestSent.test.tsx
+++ src/features/auth/signup/BeneficiaryRequestSent.test.tsx
@@ -1,22 +1,61 @@
 import React from 'react'
+import { UseMutationResult, UseQueryResult } from 'react-query'
+import { mocked } from 'ts-jest/utils'
 
 import { navigate } from '__mocks__/@react-navigation/native'
-import { render, fireEvent } from 'tests/utils'
+import { UserProfileResponse } from 'api/gen'
+import * as AuthApi from 'features/auth/api'
+import { useUserProfileInfo } from 'features/home/api'
+import { navigateToHome } from 'features/navigation/helpers'
+import { EmptyResponse } from 'libs/fetch'
+import { render, fireEvent, waitFor } from 'tests/utils'
 
 import { BeneficiaryRequestSent } from './BeneficiaryRequestSent'
 
-beforeEach(() => {
-  jest.clearAllMocks()
-})
+const mockedUseUserProfileInfo = mocked(useUserProfileInfo)
+jest.mock('features/home/api')
+jest.mock('react-query')
+jest.mock('features/navigation/helpers')
 
 describe('<BeneficiaryRequestSent />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    mockedUseUserProfileInfo.mockReturnValue({
+      data: { isBeneficiary: true, needsToFillCulturalSurvey: true },
+    } as UseQueryResult<UserProfileResponse>)
+  })
+
   it('should redirect to cultural survey page WHEN "On y va !" button is clicked', async () => {
     const { findByText } = render(<BeneficiaryRequestSent />)
 
-    const button = await findByText('On y va !')
-    fireEvent.press(button)
+    fireEvent.press(await findByText('On y va !'))
 
+    expect(navigateToHome).not.toBeCalled()
     expect(navigate).toBeCalledTimes(1)
     expect(navigate).toBeCalledWith('CulturalSurvey')
+  })
+
+  it('should redirect to home page WHEN "On y va !" button is clicked and user is not beneficiary', async () => {
+    mockedUseUserProfileInfo.mockReturnValue({
+      data: { isBeneficiary: false, needsToFillCulturalSurvey: true },
+    } as UseQueryResult<UserProfileResponse>)
+    const { findByText } = render(<BeneficiaryRequestSent />)
+
+    fireEvent.press(await findByText('On y va !'))
+
+    expect(navigateToHome).toBeCalledTimes(1)
+    expect(navigate).not.toBeCalledWith('CulturalSurvey')
+  })
+
+  it('should redirect to home page WHEN "On y va !" button is clicked and user needs not to fill cultural survey', async () => {
+    mockedUseUserProfileInfo.mockReturnValue({
+      data: { isBeneficiary: true, needsToFillCulturalSurvey: false },
+    } as UseQueryResult<UserProfileResponse>)
+    const { findByText } = render(<BeneficiaryRequestSent />)
+
+    fireEvent.press(await findByText('On y va !'))
+
+    expect(navigateToHome).toBeCalledTimes(1)
+    expect(navigate).not.toBeCalledWith('CulturalSurvey')
   })
 })
--- src/features/auth/signup/BeneficiaryRequestSent.tsx
+++ src/features/auth/signup/BeneficiaryRequestSent.tsx
@@ -3,6 +3,8 @@ import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import styled from 'styled-components/native'
 
+import { useUserProfileInfo } from 'features/home/api'
+import { navigateToHome } from 'features/navigation/helpers'
 import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
 import { GenericInfoPage } from 'ui/components/GenericInfoPage'
@@ -11,19 +13,30 @@ import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
 
 export function BeneficiaryRequestSent() {
   const { navigate } = useNavigation<UseNavigationType>()
+  const { data: user } = useUserProfileInfo()
 
-  function goToCulturalSurvey() {
-    navigate('CulturalSurvey')
+  const shouldNavigateToCulturalSurvey = user?.isBeneficiary && user?.needsToFillCulturalSurvey
+
+  function onPress() {
+    if (shouldNavigateToCulturalSurvey) {
+      navigate('CulturalSurvey')
+    } else {
+      navigateToHome()
+    }
   }
 
   return (
     <GenericInfoPage title={t`Demande envoyée !`} icon={RequestSent} iconSize={getSpacing(42)}>
       <StyledBody>{t`Nous étudions ton dossier...`}</StyledBody>
-      <StyledBody>
-        {t`Tu recevras un e-mail lorsque ta demande sera validée. En attendant, aide-nous à en savoir plus sur tes pratiques culturelles !`}
-      </StyledBody>
+      {shouldNavigateToCulturalSurvey ? (
+        <StyledBody>
+          {t`Tu recevras un e-mail lorsque ta demande sera validée. En attendant, aide-nous à en savoir plus sur tes pratiques culturelles !`}
+        </StyledBody>
+      ) : (
+        <StyledBody>{t`Tu recevras un e-mail lorsque ta demande sera validée.`}</StyledBody>
+      )}
       <Spacer.Column numberOfSpaces={15} />
-      <ButtonPrimaryWhite title={t`On y va !`} onPress={goToCulturalSurvey} />
+      <ButtonPrimaryWhite title={t`On y va !`} onPress={onPress} />
     </GenericInfoPage>
   )
 }
