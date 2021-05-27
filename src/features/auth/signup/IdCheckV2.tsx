25151086a4cb014225e48773ef93c15c0d8b7945
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-05-27 13:00:53 +0200

    (PC-9003) delete hard cache

--- src/features/auth/signup/IdCheckV2.tsx
+++ src/features/auth/signup/IdCheckV2.tsx
@@ -1,18 +1,22 @@
 import { IdCheckHomePage, useIdCheckContext } from '@pass-culture/id-check'
 import { useNavigation } from '@react-navigation/native'
 import React, { useEffect } from 'react'
+import { useQueryClient } from 'react-query'
 
 import { useAppSettings } from 'features/auth/settings'
 import { useUserProfileInfo } from 'features/home/api'
 import { homeNavigateConfig } from 'features/navigation/helpers'
 import { ScreenNavigationProp, UseNavigationType } from 'features/navigation/RootNavigator'
 import { errorMonitoring } from 'libs/errorMonitoring'
+import { QueryKeys } from 'libs/queryKeys'
 
 export const IdCheckV2 = (props: ScreenNavigationProp<'IdCheckV2'>) => {
   const { setContextValue } = useIdCheckContext()
   const { replace } = useNavigation<UseNavigationType>()
   const { data: settings } = useAppSettings()
-  const { refetch, remove } = useUserProfileInfo({
+  const queryClient = useQueryClient()
+
+  const { refetch } = useUserProfileInfo({
     cacheTime: 0,
   })
 
@@ -25,7 +29,7 @@ export const IdCheckV2 = (props: ScreenNavigationProp<'IdCheckV2'>) => {
   }
 
   function onSuccess() {
-    remove()
+    queryClient.invalidateQueries(QueryKeys.USER_PROFILE)
     refetch()
       .then(goToBeneficiaryRequestSent)
       .catch((err) => {
