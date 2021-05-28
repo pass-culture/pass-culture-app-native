177c6917c9c096c0d8b6c09ac86ed789fdd59cb5
Marion Barré <marionb@bam.tech>
2021-05-28 14:05:42 +0200

    :wrench: (eslint) warn deev abou use of && on string

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -1,5 +1,7 @@
 const noAllowConsole = require('./eslint-custom-rules/no-allow-console')
+const noStringCheckBeforeComponent = require('./eslint-custom-rules/no-string-check-before-component')
 
 module.exports = {
   'no-allow-console': noAllowConsole,
+  'no-string-check-before-component': noStringCheckBeforeComponent,
 }
