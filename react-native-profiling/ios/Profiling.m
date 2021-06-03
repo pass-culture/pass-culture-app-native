03bdcd5dca33e03de45b94bc0ba6f7d39d6406a8
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-06-03 10:13:00 +0200

    (PC-9056) Make profiling work for iOS

--- react-native-profiling/ios/Profiling.m
+++ react-native-profiling/ios/Profiling.m
@@ -2,15 +2,44 @@
 
 #import "Profiling.h"
 
+#import "TMXProfiling.h"
+#import "TMXProfilingConnections.h"
 
 @implementation Profiling
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(profileDevice:(NSString *)strDevice callback: (RCTResponseSenderBlock)mycallback)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    TMXProfilingConnections *tcn = [[TMXProfilingConnections alloc] init];
+    tcn.connectionTimeout = 30;  // Default value is 10 seconds
+    tcn.connectionRetryCount = 2;  // Default value is 0 (no retry)
+
+    [[TMXProfiling sharedInstance] configure:@{
+        TMXOrgID:@"16khy574",
+        TMXFingerprintServer:@"h.online-metrix.net",
+        TMXProfilingConnectionsInstance:tcn
+    }];
+
+    NSArray *customAttributes = @[@"attribute 1", @"attribute 2"];
+
+    TMXProfileHandle *profileHandle = [[TMXProfiling sharedInstance] profileDeviceUsing:@{TMXCustomAttributes : customAttributes}                                                                           callbackBlock:^(NSDictionary * _Nullable result) {
+        TMXStatusCode statusCode = [[result valueForKey:TMXProfileStatus] integerValue];
+
+        NSLog(@"Profile completed with: %s and session ID: %@", statusCode == TMXStatusCodeOk ? "OK"
+              : statusCode == TMXStatusCodeNetworkTimeoutError ? "Timed out"
+              : statusCode == TMXStatusCodeConnectionError     ? "Connection Error"
+              : statusCode == TMXStatusCodeHostNotFoundError   ? "Host not found error"
+              : statusCode == TMXStatusCodeInternalError       ? "Internal Error"
+              : statusCode == TMXStatusCodeInterruptedError    ? "Interrupted"
+              : "other",
+              [result valueForKey:TMXSessionID]
+        );
+    }];
+
+    // Session id can be collected here (to use in API call (AKA session query))
+    NSLog(@"Session id is %@", profileHandle.sessionID);
+    mycallback(@[profileHandle.sessionID]);
 }
 
 @end
