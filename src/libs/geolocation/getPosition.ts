8b07267a1916727a28b94efba38d9f79dfecd7ae
BenStirrup <benjamin.stirrup@gmail.com>
2021-06-04 11:51:36 +0200

    (PC-9110) Modify custom error messages for position errors

--- src/libs/geolocation/getPosition.ts
+++ src/libs/geolocation/getPosition.ts
@@ -13,12 +13,12 @@ const GET_POSITION_SETTINGS = {
 }
 
 export const GEOLOCATION_USER_ERROR_MESSAGE: Record<PositionError, string> = {
-  [PositionError.PERMISSION_DENIED]: t`La géolocalisation est inutilisable sur ton téléphone car le réglage de l'application est désactivé`,
-  [PositionError.POSITION_UNAVAILABLE]: t`La géolocalisation est temporairement inutilisable sur ton téléphone`,
-  [PositionError.TIMEOUT]: t`La géolocalisation est temporairement inutilisable sur ton téléphone`,
-  [PositionError.PLAY_SERVICE_NOT_AVAILABLE]: t`La géolocalisation est possiblement inutilisable sur ton téléphone parce que les services Google Play ne sont pas disponibles`,
-  [PositionError.SETTINGS_NOT_SATISFIED]: t`La géolocalisation est possiblement inutilisable sur ton téléphone pour l'une des raisons suivantes: réglage du téléphone désactivé, réseau hors-ligne...`,
-  [PositionError.INTERNAL_ERROR]: t`La géolocalisation est temporairement inutilisable sur ton téléphone`,
+  [PositionError.PERMISSION_DENIED]: t`La géolocalisation est désactivée sur l'application`,
+  [PositionError.POSITION_UNAVAILABLE]: t`La géolocalisation est temporairement inutilisable`,
+  [PositionError.TIMEOUT]: t`La géolocalisation est temporairement inutilisable`,
+  [PositionError.PLAY_SERVICE_NOT_AVAILABLE]: t`L'absence des services Google Play bloquent possiblement la géolocalisation`,
+  [PositionError.SETTINGS_NOT_SATISFIED]: t`Les réglages de ton téléphone ne nous permettent pas d’utiliser la géolocalisation`,
+  [PositionError.INTERNAL_ERROR]: t`La géolocalisation est temporairement inutilisable`,
 }
 
 export type GeolocationError = {
