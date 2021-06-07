5068096d7d99e1f3bdfbbfc7dd41f19c35748193
Anouk Hello <anouk.hello@polyconseil.fr>
2021-06-07 10:36:59 +0200

    (PC-9026) Add navigation to next beneficiary validation step

---
+++ src/features/auth/signup/useBeneficiaryValidationNavigation.test.ts
@@ -0,0 +1,109 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import { navigate } from '__mocks__/@react-navigation/native'
+import { BeneficiaryValidationStep } from 'api/gen'
+import { useBeneficiaryValidationNavigation } from 'features/auth/signup/useBeneficiaryValidationNavigation'
+import { navigateToHome } from 'features/navigation/helpers'
+import { flushAllPromises } from 'tests/utils'
+
+jest.mock('features/navigation/helpers')
+jest.mock('features/auth/settings')
+
+let mockData: unknown = null
+jest.mock('features/home/api', () => ({
+  useUserProfileInfo: jest.fn(() => ({
+    refetch: jest.fn(() => Promise.resolve({ data: mockData })),
+  })),
+}))
+
+describe('useBeneficiaryValidationNavigation', () => {
+  beforeEach(jest.clearAllMocks)
+
+  it('should navigate to home if user is undefined', async () => {
+    const { result } = renderHook(useBeneficiaryValidationNavigation)
+    result.current.navigateToNextBeneficiaryValidationStep()
+
+    await flushAllPromises()
+
+    expect(navigateToHome).toBeCalled()
+  })
+
+  it('should navigate to home if user.nextBeneficiaryValidationStep is null', async () => {
+    mockData = { firstName: 'Christophe', lastName: 'Dupont' }
+    const { result } = renderHook(useBeneficiaryValidationNavigation)
+    result.current.navigateToNextBeneficiaryValidationStep()
+
+    await flushAllPromises()
+
+    expect(navigateToHome).toBeCalled()
+  })
+
+  it('should navigate to PhoneValidation if user.nextBeneficiaryValidationStep is phone-validation', async () => {
+    mockData = {
+      firstName: 'Christophe',
+      lastName: 'Dupont',
+      nextBeneficiaryValidationStep: 'phone-validation',
+    }
+    const { result } = renderHook(useBeneficiaryValidationNavigation)
+    result.current.navigateToNextBeneficiaryValidationStep()
+
+    await flushAllPromises()
+
+    expect(navigate).toBeCalledWith('PhoneValidation')
+  })
+
+  it('should navigate to IdCheck if user.nextBeneficiaryValidationStep is undefined', async () => {
+    mockData = {
+      firstName: 'Christophe',
+      lastName: 'Dupont',
+      nextBeneficiaryValidationStep: 'id-check',
+      email: 'christophe.dupont@gmail.com',
+    }
+
+    const { result } = renderHook(useBeneficiaryValidationNavigation)
+    result.current.navigateToNextBeneficiaryValidationStep()
+
+    await flushAllPromises()
+
+    expect(navigate).toBeCalledWith('IdCheck', {
+      email: 'christophe.dupont@gmail.com',
+    })
+  })
+
+  it('should navigate to IdCheck if user.nextBeneficiaryValidationStep is undefined', async () => {
+    mockData = {
+      firstName: 'Christophe',
+      lastName: 'Dupont',
+      nextBeneficiaryValidationStep: 'id-check',
+    }
+
+    const { result } = renderHook(useBeneficiaryValidationNavigation)
+    result.current.navigateToNextBeneficiaryValidationStep()
+
+    await flushAllPromises()
+
+    expect(navigate).toBeCalledWith('IdCheckUnavailable')
+  })
+
+  it('should navigate to IdCheck if prefetched next step is id-check and email is not null', () => {
+    const { result } = renderHook(useBeneficiaryValidationNavigation)
+    result.current.navigateToNextBeneficiaryValidationStep({
+      email: 'christophe.dupont@gmail.com',
+      nextBeneficiaryValidationStep: BeneficiaryValidationStep.IdCheck,
+    })
+
+    expect(navigate).toBeCalledWith('IdCheck', {
+      email: 'christophe.dupont@gmail.com',
+    })
+  })
+
+  it('should navigate to IdCheck if prefetched next step is id-check and email is not null', () => {
+    const { result } = renderHook(useBeneficiaryValidationNavigation)
+    result.current.navigateToNextBeneficiaryValidationStep({
+      email: 'christophe.dupont@gmail.com',
+      nextBeneficiaryValidationStep: BeneficiaryValidationStep.PhoneValidation,
+    })
+
+    expect(navigate).toBeCalledWith('PhoneValidation')
+  })
+})
---
+++ src/features/auth/signup/useBeneficiaryValidationNavigation.ts
@@ -0,0 +1,78 @@
+import { t } from '@lingui/macro'
+import { useNavigation } from '@react-navigation/native'
+import { useCallback, useMemo, useState } from 'react'
+
+import { BeneficiaryValidationStep } from 'api/gen'
+import { useAppSettings } from 'features/auth/settings'
+import { useNavigateToIdCheck } from 'features/auth/signup/idCheck/useNavigateToIdCheck'
+import { useUserProfileInfo } from 'features/home/api'
+import { navigateToHome } from 'features/navigation/helpers'
+import { UseNavigationType } from 'features/navigation/RootNavigator'
+import { analytics } from 'libs/analytics'
+import { errorMonitoring } from 'libs/errorMonitoring'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
+
+type PrefetchedInfo = {
+  email: string | null
+  nextBeneficiaryValidationStep: BeneficiaryValidationStep | null
+}
+
+export const useBeneficiaryValidationNavigation = () => {
+  const { data: settings } = useAppSettings()
+  const { navigate } = useNavigation<UseNavigationType>()
+  const { refetch } = useUserProfileInfo()
+  const navigateToIdCheck = useNavigateToIdCheck({
+    onIdCheckNavigationBlocked: () => navigate('IdCheckUnavailable'),
+  })
+  const { showErrorSnackBar } = useSnackBarContext()
+  const [error, setError] = useState<Error | null>(null)
+
+  const navigateToNextBeneficiaryValidationStep = useCallback(
+    (prefetchedInfo?: PrefetchedInfo) => {
+      if (!prefetchedInfo) {
+        refetch()
+          .then((value) => {
+            const user = value.data
+            const nextStep = user?.nextBeneficiaryValidationStep ?? null
+            const email = user?.email ?? null
+            navigateToNextStep(nextStep, email)
+          })
+          .catch(setError)
+      } else {
+        navigateToNextStep(prefetchedInfo.nextBeneficiaryValidationStep, prefetchedInfo.email)
+      }
+    },
+    [settings?.allowIdCheckRegistration, showErrorSnackBar, navigateToIdCheck, refetch]
+  )
+
+  function navigateToNextStep(
+    nextStep: PrefetchedInfo['nextBeneficiaryValidationStep'],
+    email: PrefetchedInfo['email']
+  ) {
+    if (nextStep === BeneficiaryValidationStep.PhoneValidation && settings?.enablePhoneValidation) {
+      navigate('PhoneValidation')
+    } else if (nextStep === BeneficiaryValidationStep.IdCheck) {
+      if (settings?.allowIdCheckRegistration && !!email) {
+        try {
+          analytics.logIdCheck('Profile')
+          navigateToIdCheck(email)
+        } catch (err) {
+          errorMonitoring.captureException(err, { isEligible: true })
+          showErrorSnackBar({
+            message: t`Désolé, tu as effectué trop de tentatives. Essaye de nouveau dans 12 heures.`,
+            timeout: SNACK_BAR_TIME_OUT,
+          })
+        }
+      } else {
+        navigate('IdCheckUnavailable')
+      }
+    } else {
+      navigateToHome()
+    }
+  }
+
+  return useMemo(() => ({ error, navigateToNextBeneficiaryValidationStep }), [
+    error,
+    navigateToNextBeneficiaryValidationStep,
+  ])
+}
