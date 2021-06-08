36a8eb91b96a75a2412e2677332c85800a4cb59e
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-08 11:19:59 +0200

    (PC-9265) App Native - ID Check - Bassin de rétention - appel à lendpoint pour uploader les pièces d'identité quand le FF est activé

--- src/libs/idCheckRetentionClient.ts
+++ src/libs/idCheckRetentionClient.ts
@@ -1,24 +1,71 @@
-import { UserProfile, IdCheckRetentionClient } from '@pass-culture/id-check'
+import {
+  ApiError as IdCheckApiError,
+  IdCheckErrors,
+  IdCheckRetentionClient,
+  UserProfile,
+  IdCheckFile,
+  LocalStorageService,
+} from '@pass-culture/id-check'
 
 import { api } from 'api/api'
 import { ActivityEnum, BeneficiaryInformationUpdateRequest } from 'api/gen'
-import { errorMonitoring } from 'libs/errorMonitoring'
+import { ApiError } from 'api/helpers'
 
 export const idCheckRetentionClient: IdCheckRetentionClient = {
-  confirmProfile: async (values?: Partial<UserProfile>) => {
+  confirmProfile(values?: Partial<UserProfile>) {
+    return api.patchnativev1beneficiaryInformation({
+      ...(values?.address ? { address: values?.address } : {}),
+      ...(values?.city ? { city: values?.city } : {}),
+      ...(values?.email ? { email: values?.email } : {}),
+      ...(values?.phone ? { phone: values?.phone } : {}),
+      ...(values?.postalCode ? { postalCode: values?.postalCode } : {}),
+      ...(values?.status ? { activity: values?.status as ActivityEnum } : {}),
+    } as BeneficiaryInformationUpdateRequest)
+  },
+  uploadDocument: async (file: IdCheckFile) => {
+    let error, token
     try {
-      const res = await api.patchnativev1beneficiaryInformation({
-        ...(values?.address ? { address: values?.address } : {}),
-        ...(values?.city ? { city: values?.city } : {}),
-        ...(values?.email ? { email: values?.email } : {}),
-        ...(values?.phone ? { phone: values?.phone } : {}),
-        ...(values?.postalCode ? { postalCode: values?.postalCode } : {}),
-        ...(values?.status ? { activity: values?.status as ActivityEnum } : {}),
-      } as BeneficiaryInformationUpdateRequest)
-      return res
+      token = await LocalStorageService.getLicenceToken()
+      if (!token) {
+        error = new IdCheckApiError('Auth required', 400, {
+          code: IdCheckErrors['auth-required'],
+        })
+      }
     } catch (err) {
-      errorMonitoring.captureException(err)
-      return Promise.reject(err)
+      error = err
+    }
+    if (error) {
+      return Promise.reject(error)
+    }
+    try {
+      const data = new FormData()
+      data.append('token', token)
+      data.append('identityDocumentFile', file as Blob)
+      return await api.postnativev1identityDocument({
+        body: data,
+      })
+    } catch (err) {
+      error = err
+      if (err instanceof ApiError) {
+        if (err.content.code === 'EXPIRED_TOKEN') {
+          error = new IdCheckApiError(err.content.message, err.statusCode, {
+            code: IdCheckErrors['auth-token-expired'],
+          })
+        } else if (err.content.code === 'INVALID_TOKEN') {
+          error = new IdCheckApiError(err.content.message, err.statusCode, {
+            code: IdCheckErrors['auth-required'],
+          })
+        } else if (err.content.code === 'FILE_SIZE_EXCEEDED') {
+          error = new IdCheckApiError(err.content.message, err.statusCode, {
+            code: IdCheckErrors['file-size-exceeded'],
+          })
+        } else if (err.content.code === 'SERVICE_UNAVAILABLE') {
+          error = new IdCheckApiError(err.content.message, err.statusCode, {
+            code: IdCheckErrors['validation-unavailable'],
+          })
+        }
+      }
+      return Promise.reject(error)
     }
   },
 }
