d0f79412891cebc43db2c9f5590eac4f6e205b90
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-09 09:12:04 +0200

    (PC-9220) App Native - ID Check - Bassin de rétention - Version alternative de la bannière dossier déposé sur la page profil

--- src/features/profile/components/IdCheckProcessingBadge.tsx
+++ src/features/profile/components/IdCheckProcessingBadge.tsx
@@ -8,7 +8,7 @@ export function IdCheckProcessingBadge() {
   return (
     <ProfileBadge
       icon={Clock}
-      message={t`Dossier déposé ! Nous avons bien reçu ton dossier et sommes en train de l'analyser.`}
+      message={t`Un peu de patience. En raison du grand nombre de demandes ton dossier doit être analysé manuellement par nos équipes.`}
     />
   )
 }
