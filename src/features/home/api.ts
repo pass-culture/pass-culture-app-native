96af7f0522fb1d2e2bcffc4199ccfd02c64568cc
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-06-10 09:55:10 +0200

    (PC-8969) Enable support to other entryId from contentful

--- src/features/home/api.ts
+++ src/features/home/api.ts
@@ -4,43 +4,43 @@ import { useQuery } from 'react-query'
 import { api } from 'api/api'
 import { UserProfileResponse } from 'api/gen'
 import { useAuthContext } from 'features/auth/AuthContext'
-import {
-  EntryCollection,
-  EntryFields,
-  HomepageEntries,
-  ProcessedModule,
-  processHomepageEntries,
-} from 'features/home/contentful'
+import { HomepageEntry } from 'features/home/contentful'
+import { EntryCollection, EntryFields, processHomepageEntry } from 'features/home/contentful'
 import { env } from 'libs/environment'
 import { getExternal } from 'libs/fetch'
 import { QueryKeys } from 'libs/queryKeys'
 
-export const CONTENTFUL_BASE_URL = 'https://cdn.contentful.com'
 const DEPTH_LEVEL = 2
+const STALE_TIME_CONTENTFUL = 5 * 60 * 1000
 
-const adaptHomepageEntries = (
-  homepageData: EntryCollection<EntryFields, string>
-): HomepageEntries => {
-  const formattedResponse = resolveResponse(homepageData)
+export const CONTENTFUL_BASE_URL = 'https://cdn.contentful.com'
+export const BASE_URL = `${CONTENTFUL_BASE_URL}/spaces/${env.CONTENTFUL_SPACE_ID}/environments/${env.CONTENTFUL_ENVIRONMENT}`
+export const PARAMS = `?include=${DEPTH_LEVEL}&content_type=homepageNatif&access_token=${env.CONTENTFUL_ACCESS_TOKEN}`
+
+const selectEntry = (entries?: HomepageEntry[], entryId?: string): HomepageEntry | undefined => {
+  if (!entries) return undefined
   /* Support good practice is to configure on Contentful dashboard only 1 contenttype homepage
     But there is not blocking on the dashboard, that's why we select first one here */
-  return formattedResponse[0]
+  const firstEntry = entries[0]
+  if (!entryId) return firstEntry
+
+  return entries.find(({ sys }) => sys.id === entryId) || firstEntry
 }
 
-export async function getHomepageEntries() {
-  const json = await getExternal<EntryCollection<EntryFields, string>>(
-    `${CONTENTFUL_BASE_URL}` +
-      `/spaces/${env.CONTENTFUL_SPACE_ID}` +
-      `/environments/${env.CONTENTFUL_ENVIRONMENT}` +
-      `/entries?include=${DEPTH_LEVEL}&content_type=homepageNatif&access_token=${env.CONTENTFUL_ACCESS_TOKEN}`
-  )
-  return adaptHomepageEntries(json)
+export async function getEntries() {
+  const url = `${BASE_URL}/entries${PARAMS}`
+  const json = await getExternal<EntryCollection<EntryFields, 'homepageNatif'>>(url)
+
+  return resolveResponse(json)
 }
 
-export function useHomepageModules() {
-  return useQuery<ProcessedModule[]>(QueryKeys.HOMEPAGE_MODULES, async () =>
-    processHomepageEntries(await getHomepageEntries())
-  )
+export function useHomepageModules(entryId?: string) {
+  const { data: entries } = useQuery<HomepageEntry[]>(QueryKeys.HOMEPAGE_MODULES, getEntries, {
+    staleTime: STALE_TIME_CONTENTFUL,
+  })
+
+  const entry = selectEntry(entries, entryId)
+  return { data: entry ? processHomepageEntry(entry) : [] }
 }
 
 export function useUserProfileInfo(options = {}) {
