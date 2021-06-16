603edbccf5a9bce893fa028f66c374b50533be37
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-16 09:40:09 +0200

    (PC-9434) App Native - ID check - Ecran session expirée à l'entrée de l id check

--- src/features/auth/signup/IdCheckV2.tsx
+++ src/features/auth/signup/IdCheckV2.tsx
@@ -6,8 +6,8 @@ import {
   StepConfig,
   useIdCheckContext,
 } from '@pass-culture/id-check'
-import { useNavigation } from '@react-navigation/native'
-import React, { useEffect } from 'react'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import React, { useCallback, useState } from 'react'
 import { useQueryClient } from 'react-query'
 
 import { useNotifyIdCheckCompleted } from 'features/auth/api'
@@ -17,8 +17,9 @@ import { homeNavigateConfig } from 'features/navigation/helpers'
 import { ScreenNavigationProp, UseNavigationType } from 'features/navigation/RootNavigator'
 import { errorMonitoring } from 'libs/errorMonitoring'
 import { QueryKeys } from 'libs/queryKeys'
+import { LoadingPage } from 'ui/components/LoadingPage'
 
-export const IdCheckV2 = (props: ScreenNavigationProp<'IdCheckV2'>) => {
+export const IdCheckV2 = function IdCheckV2(props: ScreenNavigationProp<'IdCheckV2'>) {
   const { setContextValue } = useIdCheckContext()
   const { navigate } = useNavigation<UseNavigationType>()
   const { data: settings, refetch: refetchSettings } = useAppSettings()
@@ -27,6 +28,7 @@ export const IdCheckV2 = (props: ScreenNavigationProp<'IdCheckV2'>) => {
     onSuccess: syncUserAndProceedToNextScreen,
     onError: syncUserAndProceedToNextScreen,
   })
+  const [isEnteringIdCheck, setIsEnteringIdCheck] = useState(true)
 
   const { refetch } = useUserProfileInfo({
     cacheTime: 0,
@@ -50,7 +52,8 @@ export const IdCheckV2 = (props: ScreenNavigationProp<'IdCheckV2'>) => {
     notifyIdCheckCompleted()
   }
 
-  useEffect(() => {
+  const resetContextValues = useCallback(() => {
+    setIsEnteringIdCheck(false)
     if (setContextValue) {
       refetchSettings()
         .then(({ data }) => {
@@ -82,8 +85,15 @@ export const IdCheckV2 = (props: ScreenNavigationProp<'IdCheckV2'>) => {
             }),
           })
         })
+        .finally(() => setIsEnteringIdCheck(true))
     }
   }, [setContextValue])
+
+  useFocusEffect(resetContextValues)
+
+  if (!isEnteringIdCheck) {
+    return <LoadingPage />
+  }
 
   // @ts-ignore : props typing issue with IdCheck screen from module.
   // Probably needs some change on the side of the module.
