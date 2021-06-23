26cf118fb2b67136eee1d740e33a74a952de5cdb
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-06-23 16:20:21 +0200

    (PC-9515) Mock the complete settings response

--- src/features/auth/__mocks__/settings.ts
+++ src/features/auth/__mocks__/settings.ts
@@ -2,19 +2,22 @@ import { SettingsResponse } from 'api/gen'
 import { useAppSettings as actualUseAppSettings } from 'features/auth/settings'
 import { UseQueryResult } from 'react-query'
 
+const data: SettingsResponse = {
+  depositAmount: 30000,
+  isRecaptchaEnabled: true,
+  allowIdCheckRegistration: true,
+  enableNativeIdCheckVersion: false,
+  wholeFranceOpening: false,
+  enablePhoneValidation: true,
+  displayDmsRedirection: true,
+  autoActivateDigitalBookings: true,
+  enableIdCheckRetention: false,
+  enableNativeIdCheckVerboseDebugging: false,
+  idCheckAddressAutocompletion: false,
+  objectStorageUrl: 'http://localhost-storage',
+  useAppSearch: false,
+}
+
 export const useAppSettings: typeof actualUseAppSettings = jest.fn(
-  () =>
-    ({
-      data: {
-        depositAmount: 30000,
-        isRecaptchaEnabled: true,
-        allowIdCheckRegistration: true,
-        enableNativeIdCheckVersion: false,
-        wholeFranceOpening: false,
-        enablePhoneValidation: true,
-        displayDmsRedirection: true,
-        autoActivateDigitalBookings: true,
-      },
-      isLoading: false,
-    } as UseQueryResult<SettingsResponse, unknown>)
+  () => ({ data, isLoading: false } as UseQueryResult<SettingsResponse, unknown>)
 )
