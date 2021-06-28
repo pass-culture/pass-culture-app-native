69aee2c98c5dbd8693dc6a37dc0dccde1ef12232
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-28 15:25:45 +0200

    (Tech) fix webpack configuration

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -336,7 +336,7 @@ module.exports = function (webpackEnv) {
               loader: require.resolve('eslint-loader'),
             },
           ],
-          include: [paths.appSrc, paths.packagesSrc],
+          include: [paths.appSrc],
         },
         {
           // "oneOf" will traverse all following loaders until one will
