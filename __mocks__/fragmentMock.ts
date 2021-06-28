d2cc2b192918a8bdfc5fd8c0a335c3e9c4e2ba2f
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-28 15:38:46 +0200

    (Tech) fix fragment Mock

--- __mocks__/fragmentMock.ts
+++ __mocks__/fragmentMock.ts
@@ -1,1 +1,4 @@
-export { Fragment as default } from 'react'
+import { createElement, Fragment, ReactNode } from 'react'
+
+
+export default ({ children }: { children: ReactNode }) => createElement(Fragment, { children })
