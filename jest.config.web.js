c7c64902251e6afefb2a5d945c5681fe1a98a2cb
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-28 11:34:23 +0200

    (Tech) mocking react-native-fast-image and react-native-dash with Fragment and allow more test to succeed

--- jest.config.web.js
+++ jest.config.web.js
@@ -16,6 +16,13 @@ module.exports = {
     '^react-native-svg$': 'react-native-svg-web',
     '^lottie-react-native$': 'react-native-web-lottie',
     '^react-native-linear-gradient$': 'react-native-web-linear-gradient',
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.ts',
+
+    // temporary mock things until implemented
+    'react-native-dash': '<rootDir>/__mocks__/fragmentMock.ts',
+    'react-native-fast-image': '<rootDir>/__mocks__/fragmentMock.ts',
   },
   collectCoverageFrom: [...base.collectCoverageFrom, '!**/*.(native|ios|android).*'],
   testPathIgnorePatterns: [
