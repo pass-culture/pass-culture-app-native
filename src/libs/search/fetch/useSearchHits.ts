60f104843d43bdf80ad3d97bd23399e225e3c262
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-06-28 14:08:58 +0200

    (PC-9515) migrate Recommendation to App Search

--- src/libs/search/fetch/useSearchHits.ts
+++ src/libs/search/fetch/useSearchHits.ts
@@ -1,16 +1,31 @@
-import {
-  fetchAlgoliaHits,
-  filterAlgoliaHit,
-  useTransformAlgoliaHits,
-} from 'libs/algolia/fetchAlgolia'
+import { SearchOptions } from '@elastic/app-search-javascript'
+
+import { useTransformAlgoliaHits } from 'libs/algolia/fetchAlgolia'
+import { SearchHit } from 'libs/search'
+import { client } from 'libs/search/client'
+import { AppSearchFields, result_fields } from 'libs/search/filters'
+import { buildAlgoliaHit } from 'libs/search/utils/buildAlgoliaHit'
+
+const fetchHits = (ids: string[]): Promise<{ results: { hits: SearchHit[] } }> => {
+  const options: SearchOptions<AppSearchFields> = {
+    result_fields,
+    filters: { any: ids.map((id) => ({ [AppSearchFields.object_id]: id })) },
+  }
+
+  return client.search<AppSearchFields>('', options).then((response) => ({
+    results: response.results.map(buildAlgoliaHit),
+  }))
+}
+
+// We don't want to display offers without image
+const filterHits = (hit: SearchHit): boolean => hit && hit.offer && !!hit.offer.thumbUrl
 
 export const useSearchHits = () => {
-  // TODO(antoinewg) use functions to connect to app search
-  const transformAlgoliaHit = useTransformAlgoliaHits()
+  const transformHits = useTransformAlgoliaHits()
 
   return {
-    fetchHits: fetchAlgoliaHits,
-    filterHits: filterAlgoliaHit,
-    transformHits: transformAlgoliaHit,
+    fetchHits,
+    filterHits,
+    transformHits,
   }
 }
