329d2efb796a297200085fee7e3faa534c397bb9
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-29 08:49:14 +0200

    (Tech) recycle some test for AppButton, OfferPartialDescription and SignupConfirmationMailSent

---
+++ src/features/offer/components/__tests__/OfferPartialDescription.web.test.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { QueryClient } from 'react-query'
+
+import { offerResponseSnap } from 'features/offer/api/snaps/offerResponseSnap'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render } from 'tests/utils/web'
+
+import { OfferPartialDescription } from '../OfferPartialDescription'
+
+const description =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua: https://google.com'
+
+describe('OfferPartialDescription', () => {
+  it('centers CTA when provided description is empty', async () => {
+    const { getByTestId } = await renderOfferDescription('')
+    const offerSeeMoreContainer = getByTestId('offerSeeMoreContainer')
+    expect(offerSeeMoreContainer.style._values['align-self']).toBe('center')
+  })
+  it('places CTA on flex-end when provided a description', async () => {
+    const { getByTestId } = await renderOfferDescription(description)
+    const offerSeeMoreContainer = getByTestId('offerSeeMoreContainer')
+    expect(offerSeeMoreContainer.style._values['align-self']).toBe('flex-end')
+  })
+  it('renders externalLinks if http(s) url are present in the description', async () => {
+    const { queryByTestId } = await renderOfferDescription(description)
+    expect(queryByTestId('externalSiteIcon')).toBeTruthy()
+  })
+  it("shouldn't render an empty line and a spacer when description is undefined", async () => {
+    const { queryByTestId } = await renderOfferDescription(undefined)
+    expect(queryByTestId('offerPartialDescriptionBody')).toBeFalsy()
+  })
+  it("shouldn't render the SeeMore button if no content is on description page", async () => {
+    const { queryByTestId } = render(
+      reactQueryProviderHOC(
+        <OfferPartialDescription id={offerId} description={undefined} />,
+        (queryClient: QueryClient) => {
+          queryClient.setQueryData(['offer', offerId], {
+            ...offerResponseSnap,
+            image: {},
+            extraData: {},
+          })
+        }
+      )
+    )
+    expect(queryByTestId('offerSeeMoreContainer')).toBeFalsy()
+  })
+})
+
+const offerId = 123
+const renderOfferDescription = async (description?: string) => {
+  const setup = (queryClient: QueryClient) => {
+    queryClient.removeQueries()
+    queryClient.setQueryData(['offer', offerId], offerResponseSnap)
+  }
+
+  const wrapper = render(
+    reactQueryProviderHOC(<OfferPartialDescription id={offerId} description={description} />, setup)
+  )
+  return wrapper
+}
