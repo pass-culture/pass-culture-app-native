fd9ec941f42c78b80be3500ca075f6c1c716c0c7
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-29 10:09:55 +0200

    (Tech) Recycle unit test for Web: OfferTile ClippedImage ShortInput

---
+++ src/ui/components/ClippedImage.web.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { render } from 'tests/utils/web'
+
+import { ClippedImage } from './ClippedImage'
+
+const props = {
+  clipId: 'clipId',
+  path: 'path',
+}
+
+describe('ClippedImage', () => {
+  it('should render the placeholder icon if no image', () => {
+    const { queryByTestId } = render(<ClippedImage {...props} />)
+    expect(queryByTestId('iconContainer')).not.toBeNull()
+  })
+
+  it('should not render the placeholder icon if image', () => {
+    const { queryByTestId } = render(<ClippedImage {...props} image="uri" />)
+    expect(queryByTestId('iconContainer')).toBeNull()
+  })
+})
