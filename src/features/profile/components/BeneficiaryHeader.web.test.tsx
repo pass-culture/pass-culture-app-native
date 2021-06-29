c77dd4e58cc94ff922906ce8dff19044fb4decef
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-29 16:03:38 +0200

    (Tech) Recycle test for Web: BeneficiaryHeader BusinessModule Search BusinessModule

---
+++ src/features/profile/components/BeneficiaryHeader.web.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { render } from 'tests/utils/web'
+
+import { BeneficiaryHeader } from './BeneficiaryHeader'
+
+const domains_credit_v1 = {
+  all: { initial: 50000, remaining: 40000 },
+  physical: { initial: 30000, remaining: 10000 },
+  digital: { initial: 30000, remaining: 20000 },
+}
+
+const domains_credit_v2 = {
+  all: { initial: 30000, remaining: 10000 },
+  digital: { initial: 20000, remaining: 5000 },
+}
+
+describe('BeneficiaryHeader', () => {
+  it('should render properly with deposit version 1', () => {
+    const { getAllByTestId } = render(
+      <BeneficiaryHeader
+        domainsCredit={domains_credit_v1}
+        firstName={'Rosa'}
+        lastName={'Bonheur'}
+      />
+    )
+
+    const progressBars = getAllByTestId('progress-bar')
+    expect(progressBars.length).toBe(3)
+  })
+
+  it('should render properly with deposit version 2', () => {
+    const { getAllByTestId } = render(
+      <BeneficiaryHeader
+        domainsCredit={domains_credit_v2}
+        firstName={'Rosa'}
+        lastName={'Bonheur'}
+      />
+    )
+
+    const progressBars = getAllByTestId('progress-bar')
+    expect(progressBars.length).toBe(2)
+  })
+})
