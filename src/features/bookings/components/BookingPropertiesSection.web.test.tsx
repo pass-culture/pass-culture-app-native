091fe7943d056932cc67d4391450e92346831124
Lucas Beneston <lucasbeneston@MacBook-Pro-de-Lucas.local>
2021-06-29 16:59:22 +0200

    (Tech) Recycle test for web : BicolorFavoriteCount, BookingPropretiesSection

---
+++ src/features/bookings/components/BookingPropertiesSection.web.test.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import waitForExpect from 'wait-for-expect'
+
+import { bookingsSnap } from 'features/bookings/api/bookingsSnap'
+import { BookingPropertiesSection } from 'features/bookings/components/BookingPropertiesSection'
+import { Booking } from 'features/bookings/components/types'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render } from 'tests/utils/web'
+
+jest.mock('features/home/api', () => ({
+  useUserProfileInfo: jest.fn(() => ({ data: { firstName: 'Christophe', lastName: 'Dupont' } })),
+}))
+
+describe('<BookingPropertiesSection />', () => {
+  const booking = bookingsSnap.ongoing_bookings[0]
+
+  it('should display user firstname and lastname', async () => {
+    const { getByText } = await renderBookingProperties(booking)
+
+    await waitForExpect(() => {
+      expect(getByText('Christophe Dupont')).toBeTruthy()
+    })
+  })
+
+  it('should display duo icon when offer is duo', async () => {
+    booking.quantity = 2
+    const { getByTestId } = await renderBookingProperties(booking)
+
+    await waitForExpect(() => {
+      expect(getByTestId('duo-icon')).toBeTruthy()
+    })
+  })
+
+  it('should display date label', async () => {
+    const { getByText } = await renderBookingProperties(booking)
+
+    await waitForExpect(() => {
+      expect(getByText('Le 15 mars 2021 à 20h00')).toBeTruthy()
+    })
+  })
+
+  // FIXME: web integration
+  it.skip('should display location label if offer is not permanent and not a digital event', async () => {
+    booking.stock.offer.isDigital = false
+    booking.stock.offer.isPermanent = false
+    const { getByText } = await renderBookingProperties(booking)
+    await waitForExpect(() => {
+      expect(getByText('Maison de la Brique, Drancy')).toBeTruthy()
+    })
+  })
+})
+
+async function renderBookingProperties(booking: Booking) {
+  const wrapper = render(
+    reactQueryProviderHOC(
+      <BookingPropertiesSection
+        booking={booking}
+        appSettings={{
+          allowIdCheckRegistration: false,
+          autoActivateDigitalBookings: false,
+          depositAmount: 5000,
+          enableNativeIdCheckVersion: false,
+          enableNativeIdCheckVerboseDebugging: false,
+          enableIdCheckRetention: false,
+          enablePhoneValidation: false,
+          isRecaptchaEnabled: false,
+          wholeFranceOpening: false,
+          objectStorageUrl: 'http://localhost',
+          displayDmsRedirection: true,
+          idCheckAddressAutocompletion: false,
+          useAppSearch: false,
+        }}
+      />
+    )
+  )
+  return wrapper
+}
