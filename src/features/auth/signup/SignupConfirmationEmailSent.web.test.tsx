6c821d0df6d962e4d0983c63e081fc34ff9e0fc9
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-29 08:49:14 +0200

    (Tech) recycle some test for AppButton, OfferPartialDescription and SignupConfirmationMailSent

---
+++ src/features/auth/signup/SignupConfirmationEmailSent.web.test.tsx
@@ -0,0 +1,91 @@
+import { StackScreenProps } from '@react-navigation/stack'
+import React from 'react'
+import { openInbox } from 'react-native-email-link'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import waitForExpect from 'wait-for-expect'
+
+import { goBack } from '__mocks__/@react-navigation/native'
+import { contactSupport } from 'features/auth/support.services'
+import { usePreviousRoute, navigateToHome } from 'features/navigation/helpers'
+import { RootStackParamList } from 'features/navigation/RootNavigator'
+import { analytics } from 'libs/analytics'
+import { fireEvent, render } from 'tests/utils/web'
+
+import { SignupConfirmationEmailSent } from './SignupConfirmationEmailSent'
+
+const mockUsePreviousRoute = usePreviousRoute as jest.MockedFunction<typeof usePreviousRoute>
+jest.mock('features/navigation/helpers')
+
+describe('<SignupConfirmationEmailSent />', () => {
+  beforeEach(() => {
+    mockUsePreviousRoute.mockReturnValue({ name: 'SomeScreen', key: 'key' })
+  })
+
+  it('should go back when clicking on left icon of modal header', async () => {
+    const { findByTestId } = renderPage()
+
+    const leftIconButton = await findByTestId('leftIcon')
+    fireEvent.click(leftIconButton)
+
+    await waitForExpect(() => {
+      expect(goBack).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  it('should NOT display back button when previous screen is AcceptCgu', async () => {
+    // eslint-disable-next-line local-rules/independant-mocks
+    mockUsePreviousRoute.mockReturnValue({ name: 'AcceptCgu', key: 'key' })
+    const { queryByTestId } = renderPage()
+
+    const leftIconButton = queryByTestId('leftIcon')
+
+    await waitForExpect(() => {
+      expect(leftIconButton).toBeFalsy()
+    })
+  })
+
+  it('should go to home page when clicking on right icon', async () => {
+    const { findByTestId } = renderPage()
+
+    const rightIconButton = await findByTestId('rightIconButton')
+    fireEvent.click(rightIconButton)
+
+    await waitForExpect(() => {
+      expect(navigateToHome).toBeCalledTimes(1)
+    })
+  })
+
+  it('should open faq webpage when clicking on consult help support', async () => {
+    const { findByText } = renderPage()
+
+    const consultHelpSupportButton = await findByText("Consulter notre centre d'aide")
+    fireEvent.click(consultHelpSupportButton)
+
+    await waitForExpect(() => {
+      expect(analytics.logHelpCenterContactSignupConfirmationEmailSent).toBeCalledTimes(1)
+      expect(contactSupport.forSignupConfirmationEmailNotReceived).toBeCalledTimes(1)
+    })
+  })
+
+  it('should open mail app when clicking on check email button', async () => {
+    const { findByText } = renderPage()
+
+    const checkEmailsButton = await findByText('Consulter mes e-mails')
+    fireEvent.click(checkEmailsButton)
+
+    await waitForExpect(() => {
+      expect(openInbox).toHaveBeenCalled()
+    })
+  })
+})
+
+function renderPage() {
+  const navigationProps = {
+    route: { params: { email: 'john.doe@gmail.com' } },
+  } as StackScreenProps<RootStackParamList, 'SignupConfirmationEmailSent'>
+  return render(
+    <SafeAreaProvider>
+      <SignupConfirmationEmailSent {...navigationProps} />
+    </SafeAreaProvider>
+  )
+}
---
+++ src/ui/components/buttons/AppButton.web.test.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+import { render } from 'tests/utils/web'
+import { Close } from 'ui/svg/icons/Close'
+import { ColorsEnum } from 'ui/theme'
+
+import { AppButton } from './AppButton'
+
+describe('AppButton Component', () => {
+  describe('* Icon property', () => {
+    it('should display icon when provided', () => {
+      const { getByTestId } = render(
+        <AppButton title="Testing Disabled" loadingIconColor={ColorsEnum.BLACK} icon={Close} />
+      )
+      getByTestId('button-icon')
+    })
+    it('should not display icon when not provided', () => {
+      const { queryByTestId } = render(
+        <AppButton title="Testing Disabled" loadingIconColor={ColorsEnum.BLACK} />
+      )
+      const icon = queryByTestId('button-icon')
+      expect(icon).toBeFalsy()
+    })
+  })
+  describe('* isLoading property', () => {
+    it('should display right elements when isLoading equals true', () => {
+      const { getByTestId, queryByTestId } = render(
+        <AppButton
+          title="Testing Disabled"
+          loadingIconColor={ColorsEnum.BLACK}
+          isLoading
+          icon={Close}
+        />
+      )
+      getByTestId('button-isloading-icon')
+      const icon = queryByTestId('button-icon')
+      expect(icon).toBeFalsy()
+    })
+    it('should display right elements when isLoading equals false', () => {
+      const { getByTestId, queryByTestId } = render(
+        <AppButton
+          title="Testing Disabled"
+          loadingIconColor={ColorsEnum.BLACK}
+          isLoading={false}
+          icon={Close}
+        />
+      )
+
+      getByTestId('button-icon')
+      const icon = queryByTestId('button-isloading-icon')
+      expect(icon).toBeFalsy()
+    })
+  })
+
+  // TODO: web integration for button and restore test
+  describe.skip('* Disabled property', () => {
+    it('should disable handlers when disabled equals true [TODO: web]', () => {
+      const { getByTestId } = render(
+        <AppButton
+          title="Testing Disabled"
+          loadingIconColor={ColorsEnum.BLACK}
+          icon={Close}
+          disabled
+        />
+      )
+
+      const container = getByTestId('Testing Disabled')
+      expect(container.props.onPress).toBeFalsy()
+      expect(container.props.onLongPress).toBeFalsy()
+    })
+  })
+  describe('* inline property', () => {
+    it('should use inline css style when true', () => {
+      const renderAPI = render(
+        <AppButton title="Testing inline" loadingIconColor={ColorsEnum.BLACK} icon={Close} inline />
+      )
+      expect(renderAPI).toMatchSnapshot()
+    })
+  })
+})
