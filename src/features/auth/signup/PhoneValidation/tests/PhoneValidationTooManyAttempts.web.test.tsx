b980094300867e19faa670da117542b95fb2f0f5
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-29 11:00:22 +0200

    (Tech) recycle test when possible: PhoneValidationTooManyAttempts, Login, ReCaptcha

---
+++ src/features/auth/signup/PhoneValidation/tests/PhoneValidationTooManyAttempts.web.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import waitForExpect from 'wait-for-expect'
+
+import { PhoneValidationTooManyAttempts } from 'features/auth/signup/PhoneValidation/PhoneValidationTooManyAttempts'
+import { contactSupport } from 'features/auth/support.services'
+import { navigateToHome } from 'features/navigation/helpers'
+import { fireEvent, render } from 'tests/utils/web'
+
+jest.mock('features/navigation/helpers')
+
+describe('Contact support button', () => {
+  it('should open mail app when clicking on contact support button', async () => {
+    const { getByText } = render(<PhoneValidationTooManyAttempts />)
+
+    const contactSupportButton = getByText('Contacter le support')
+    fireEvent.click(contactSupportButton)
+
+    await waitForExpect(() => {
+      expect(contactSupport.forPhoneNumberConfirmation).toHaveBeenCalled()
+    })
+  })
+})
+
+describe('Navigate to home button', () => {
+  it('should redirect to Home when clicking on homepage button', async () => {
+    const { getByText } = render(<PhoneValidationTooManyAttempts />)
+
+    fireEvent.click(getByText("Retourner à l'accueil"))
+
+    expect(navigateToHome).toBeCalled()
+  })
+})
