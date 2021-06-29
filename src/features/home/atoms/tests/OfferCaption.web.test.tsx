a900abb091d66703ca1684409f36bb3348730c0d
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-06-29 09:57:38 +0200

    (Tech) recycle test for Web: OfferCaption LoggedOutHeader ControlComponent

---
+++ src/features/home/atoms/tests/OfferCaption.web.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import { render } from 'tests/utils/web'
+
+import { OfferCaption } from '../OfferCaption'
+
+const props = {
+  name: 'Mensch ! Où sont les Hommes ?',
+  distance: '1,2km',
+  imageWidth: 50,
+  date: 'Dès le 2 mars 2020',
+  isDuo: true,
+  isBeneficiary: true,
+  price: 'Dès 5€',
+}
+
+describe('OfferCaption component', () => {
+  afterAll(() => jest.resetAllMocks())
+
+  it('should render correctly', () => {
+    const renderAPI = render(<OfferCaption {...props} />)
+    expect(renderAPI).toMatchSnapshot()
+  })
+
+  it('should have the isDuo text if user is Beneficiary', () => {
+    const renderAPI = render(<OfferCaption {...props} />)
+    expect(renderAPI.getByTestId('priceIsDuo').textContent).toBe('Dès 5€ - Duo')
+
+    renderAPI.rerender(<OfferCaption {...props} isDuo={false} />)
+    expect(renderAPI.getByTestId('priceIsDuo').textContent).toBe('Dès 5€')
+  })
+
+  it('should not have the isDuo text if user is not Beneficiary', () => {
+    const { getByTestId } = render(<OfferCaption {...props} isBeneficiary={false} />)
+    expect(getByTestId('priceIsDuo').textContent).toBe('Dès 5€')
+  })
+})
