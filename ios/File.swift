7de1162c9e07481363ff792bc6bb932db0da934f
Marion Barré <marionb@bam.tech>
2021-07-02 11:20:15 +0200

    :wrench: (reac-native) upgrade to last version

---
+++ ios/File.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  PassCulture
+//
+//  Created by Marion Barré on 02/07/2021.
+//  Copyright © 2021 PassCulture. All rights reserved.
+// Usefull to fix build error Could not find or use auto-linked library 'swiftWebKit'
+//
+
+import Foundation
