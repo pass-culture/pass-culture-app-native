27966b31b298ce9e36b69b1f9a5da9214edc87b7
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-07-02 12:20:37 +0200

    (PC-9514) simplify logic for Cancelling details

--- src/features/bookOffer/components/CancellationDetails.tsx
+++ src/features/bookOffer/components/CancellationDetails.tsx
@@ -35,13 +35,7 @@ export const CancellationDetails: React.FC = () => {
   }
   // if "autoActivateDigitalBookings" is set, any digital booking with activationCode will be
   // automatically activated. As a result, they are not cancellable.
-  // In the event where the offer cannot expire (press subscription or audio book), the user cannot cancel
-  if (
-    settings?.autoActivateDigitalBookings &&
-    offer.isDigital &&
-    !offer.canExpire &&
-    !!activationCode
-  ) {
+  if (settings?.autoActivateDigitalBookings && offer.isDigital && !!activationCode) {
     message = notCancellableMessage
   }
 
