1c692d2898300b76e86a005e2d1fafefdf43d822
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-07-06 17:12:30 +0200

    (PC-9894) App Native - Amélioration tech - CI - Réduire les build hard systématiques sur Circle

---
+++ scripts/check_diff.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+check_diff(){
+  base_sha=$(curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/pass-culture/pass-culture-app-native/pulls?state=closed" | jq --arg sha "$CIRCLE_SHA1" -r '.[]|select(.merge_commit_sha == $sha).base.sha')
+  filter='^(src|web|public)/'
+  echo "Diff $base_sha..$CIRCLE_SHA1"
+
+  if git diff --name-only "$base_sha..$CIRCLE_SHA1" | grep -E -v "$filter" ; then
+    echo "Skipping build hard"
+    exit 1
+  else
+    echo "Running build hard"
+    exit 0
+  fi
+}
+
+check_diff
