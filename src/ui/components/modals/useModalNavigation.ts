dc58196b1ce3d4238d1168af496c556c3f32e804
Anouk Hello <anouk.hello@polyconseil.fr>
2021-07-07 16:24:34 +0200

    (PC-9486) fix useModalTransition for android

--- src/ui/components/modals/useModalNavigation.ts
+++ src/ui/components/modals/useModalNavigation.ts
@@ -3,6 +3,6 @@ import { useCallback } from 'react'
 export function useModalNavigation(hideFirstModal: () => void, showNextModal: () => void) {
   return useCallback(() => {
     hideFirstModal()
-    setTimeout(() => showNextModal(), 500)
+    showNextModal()
   }, [hideFirstModal, showNextModal])
 }
