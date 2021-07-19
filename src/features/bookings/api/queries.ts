916c5a1fd39e83f5e4ab65cc5a34dfc410e3f4cd
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-07-19 17:25:07 +0200

    (PC-10050) See BookingDetails without crashing

--- src/features/bookings/api/queries.ts
+++ src/features/bookings/api/queries.ts
@@ -1,17 +1,21 @@
-import { useQuery, useQueryClient } from 'react-query'
+import { useQuery } from 'react-query'
 
 import { api } from 'api/api'
 import { BookingReponse, BookingsResponse } from 'api/gen'
 import { QueryKeys } from 'libs/queryKeys'
 
+// Arbitrary. Make sure the cache is invalidated after each booking
+const STALE_TIME_BOOKINGS = 5 * 60 * 1000
+
 export function useBookings() {
-  return useQuery<BookingsResponse>(QueryKeys.BOOKINGS, () => api.getnativev1bookings())
+  return useQuery<BookingsResponse>(QueryKeys.BOOKINGS, () => api.getnativev1bookings(), {
+    staleTime: STALE_TIME_BOOKINGS,
+  })
 }
 
 export function useOngoingOrEndedBooking(id: number): BookingReponse | undefined {
-  const client = useQueryClient()
-  const state = client.getQueryState<BookingsResponse>(QueryKeys.BOOKINGS)
-  const onGoingBooking = state?.data?.ongoing_bookings?.find((item) => item.id === id)
-  const endedBooking = state?.data?.ended_bookings?.find((item) => item.id === id)
+  const { data: bookings } = useBookings()
+  const onGoingBooking = bookings?.ongoing_bookings?.find((item) => item.id === id)
+  const endedBooking = bookings?.ended_bookings?.find((item) => item.id === id)
   return onGoingBooking || endedBooking
 }
