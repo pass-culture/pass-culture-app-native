6383bf2ba463612644a3aeae10f284d5492d2d5c
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-07-21 12:27:23 +0200

    (Tech) remove useless allowConsole for BookingDetails

--- src/features/bookOffer/components/__tests__/BookingDetails.native.test.tsx
+++ src/features/bookOffer/components/__tests__/BookingDetails.native.test.tsx
@@ -18,9 +18,6 @@ import { SnackBarHelperSettings } from 'ui/components/snackBar/types'
 
 import { BookingDetails } from '../BookingDetails'
 
-// eslint-disable-next-line local-rules/no-allow-console
-allowConsole({ error: true })
-
 const mockDismissModal = jest.fn()
 const mockDispatch = jest.fn()
 
