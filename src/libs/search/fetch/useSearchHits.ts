b2d391bf4db3c1a2499bf08987424bff9f0c9011
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-07-21 13:11:34 +0200

    (Search) move all functions to a single file

--- src/libs/search/fetch/useSearchHits.ts
+++ src/libs/search/fetch/useSearchHits.ts
@@ -1,21 +1,7 @@
-import { SearchOptions } from '@elastic/app-search-javascript'
-
 import { useTransformAlgoliaHits } from 'libs/algolia/fetchAlgolia'
 import { SearchHit } from 'libs/search'
-import { client } from 'libs/search/client'
-import { AppSearchFields, RESULT_FIELDS } from 'libs/search/filters'
-import { buildAlgoliaHit } from 'libs/search/utils/buildAlgoliaHit'
 
-const fetchHits = (ids: string[]): Promise<{ results: { hits: SearchHit[] } }> => {
-  const options: SearchOptions<AppSearchFields> = {
-    result_fields: RESULT_FIELDS,
-    filters: { any: ids.map((id) => ({ [AppSearchFields.id]: id })) },
-  }
-
-  return client.search<AppSearchFields>('', options).then((response) => ({
-    results: response.results.map(buildAlgoliaHit),
-  }))
-}
+import { fetchObjects } from './search'
 
 // We don't want to display offers without image
 const filterHits = (hit: SearchHit): boolean => hit && hit.offer && !!hit.offer.thumbUrl
@@ -24,7 +10,7 @@ export const useSearchHits = () => {
   const transformHits = useTransformAlgoliaHits()
 
   return {
-    fetchHits,
+    fetchHits: fetchObjects,
     filterHits,
     transformHits,
   }
--- src/libs/search/fetch/useSearchQuery.ts
+++ src/libs/search/fetch/useSearchQuery.ts
@@ -1,24 +1,6 @@
-import { Response } from 'features/search/pages/useSearchResults'
-import { SearchParametersQuery } from 'libs/algolia'
 import { useTransformAlgoliaHits } from 'libs/algolia/fetchAlgolia'
-import { client } from 'libs/search/client'
-import { AppSearchFields, buildQueryOptions } from 'libs/search/filters'
-import { buildAlgoliaHit } from 'libs/search/utils/buildAlgoliaHit'
 
-const fetchHits = (params: SearchParametersQuery): Promise<Response> => {
-  const options = buildQueryOptions(params, params.page)
-
-  return client.search<AppSearchFields>(params.query, options).then((response) => {
-    const { meta } = response.info
-
-    return {
-      hits: response.results.map(buildAlgoliaHit),
-      nbHits: meta.page.total_results,
-      page: meta.page.current,
-      nbPages: meta.page.total_pages,
-    }
-  })
-}
+import { fetchHits } from './search'
 
 export const useSearchQuery = () => {
   const transformAlgoliaHit = useTransformAlgoliaHits()
