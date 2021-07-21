d09c57bd00f0da3d9cbcae98f23fef58676898b6
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-07-21 12:16:07 +0200

    (Tech) unskip test suite Offer and run on native

--- src/features/offer/pages/tests/Offer.native.test.tsx
+++ src/features/offer/pages/tests/Offer.native.test.tsx
@@ -2,8 +2,12 @@ import { act, fireEvent } from 'tests/utils'
 
 import { renderOfferPage } from './renderOfferPageTestUtil'
 
-// FIXME: web integration
-describe.skip('<Offer />', () => {
+describe('<Offer />', () => {
+  // fake timers are needed to avoid warning (because we use useTrackOfferSeenDuration)
+  // See https://github.com/facebook/jest/issues/6434
+  beforeEach(() => jest.useFakeTimers())
+  afterEach(() => jest.useRealTimers())
+
   it('animates on scroll', async () => {
     const { getByTestId } = await renderOfferPage()
     expect(getByTestId('offerHeaderName').props.style.opacity).toBe(0)
