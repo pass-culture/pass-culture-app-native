9768952f5bf5a63d7442706edfcf6f4a624af9b3
Lucas Beneston <lucasbeneston@MacBook-Pro-de-Lucas.local>
2021-07-22 18:39:20 +0200

    (PC-10036) Add VenueHeader + update snapshot

---
+++ src/features/venue/atoms/HeaderIcon.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Animated, TouchableOpacity } from 'react-native'
+import styled from 'styled-components/native'
+
+import { testID } from 'tests/utils'
+import { AnimatedIcon } from 'ui/components/AnimatedIcon'
+import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
+import { Favorite } from 'ui/svg/icons/Favorite'
+import { FavoriteFilled } from 'ui/svg/icons/FavoriteFilled'
+import { Share } from 'ui/svg/icons/Share'
+import { IconInterface } from 'ui/svg/icons/types'
+import { ColorsEnum, getSpacing } from 'ui/theme'
+
+interface HeaderIconProps {
+  iconName: 'back' | 'share' | 'favorite' | 'favorite-filled'
+  initialColor?: ColorsEnum | undefined
+  onPress: () => void
+  scaleAnimatedValue?: Animated.Value
+  animationState: {
+    iconBackgroundColor: Animated.AnimatedInterpolation
+    iconBorderColor: Animated.AnimatedInterpolation
+    transition: Animated.AnimatedInterpolation
+  }
+  testID: string
+}
+
+const getIcon = (iconName: HeaderIconProps['iconName']): React.FC<IconInterface> => {
+  if (iconName === 'back') return ArrowPrevious
+  if (iconName === 'share') return Share
+  if (iconName === 'favorite-filled') return FavoriteFilled
+  return Favorite
+}
+
+export const HeaderIcon = (props: HeaderIconProps) => {
+  const Icon = getIcon(props.iconName)
+
+  return (
+    <TouchableOpacity activeOpacity={0.5} onPress={props.onPress} {...testID(props.testID)}>
+      <StyledAnimatedView
+        testID="headerIconRoundContainer"
+        style={{
+          borderColor: props.animationState.iconBorderColor,
+          backgroundColor: props.animationState.iconBackgroundColor,
+          transform: props.scaleAnimatedValue ? [{ scale: props.scaleAnimatedValue }] : undefined,
+        }}>
+        <AnimatedIcon
+          Icon={Icon}
+          size={getSpacing(8)}
+          initialColor={props.initialColor || ColorsEnum.BLACK}
+          testID={`icon-${props.iconName}`}
+          transition={props.animationState.transition}
+          finalColor={ColorsEnum.WHITE}
+        />
+      </StyledAnimatedView>
+    </TouchableOpacity>
+  )
+}
+
+const StyledAnimatedView = styled(Animated.View)({
+  width: getSpacing(10),
+  aspectRatio: '1',
+  borderRadius: getSpacing(10),
+  backgroundColor: ColorsEnum.WHITE,
+  border: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  overflow: 'hidden',
+  borderColor: ColorsEnum.GREY_LIGHT,
+})
--- src/features/venue/pages/Venue.tsx
+++ src/features/venue/pages/Venue.tsx
@@ -1,9 +1,12 @@
 import { useRoute } from '@react-navigation/native'
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useRef } from 'react'
+import { Animated } from 'react-native'
 import styled from 'styled-components/native'
 
 import { UseRouteType } from 'features/navigation/RootNavigator'
+import { VenueHeader } from 'features/venue/components/VenueHeader'
 import { testID } from 'tests/utils'
+import { interpolationConfig } from 'ui/components/headers/animationHelpers'
 import { Spacer, Typo } from 'ui/theme'
 
 import { useVenue } from '../api/useVenue'
@@ -11,15 +14,21 @@ import { useVenue } from '../api/useVenue'
 export const Venue: FunctionComponent = () => {
   const { params } = useRoute<UseRouteType<'Venue'>>()
   const { data: venue } = useVenue(params.id)
+  const headerScroll = useRef(new Animated.Value(0)).current
 
   if (!venue) return <React.Fragment></React.Fragment>
 
+  const headerTransition = headerScroll.interpolate(interpolationConfig)
+
   return (
-    <Container {...testID('Page de détail du lieu')}>
-      <Spacer.TopScreen />
-      <Typo.Caption>{venue.id}</Typo.Caption>
-      <Typo.Caption>{venue.name}</Typo.Caption>
-    </Container>
+    <React.Fragment>
+      <Container {...testID('Page de détail du lieu')}>
+        <Spacer.TopScreen />
+        <Typo.Caption>{venue.id}</Typo.Caption>
+        <Typo.Caption>{venue.name}</Typo.Caption>
+      </Container>
+      <VenueHeader headerTransition={headerTransition} />
+    </React.Fragment>
   )
 }
 
