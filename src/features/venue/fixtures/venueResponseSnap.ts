378a5938a7b3e3312ef12680a2386aa08eac43b4
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-07-22 12:10:23 +0200

    (PC-10035) Test page <Venue />

---
+++ src/features/venue/fixtures/venueResponseSnap.ts
@@ -0,0 +1,15 @@
+import { VenueResponse } from 'api/gen'
+
+export const venueResponseSnap: VenueResponse = {
+  id: 5543,
+  name: 'Le Petit Rintintin 1',
+  latitude: 48.87004,
+  longitude: 2.3785,
+  city: 'Paris',
+  publicName: 'Le Petit Rintintin 1',
+  isVirtual: false,
+  isPermanent: true,
+  withdrawalDetails: null,
+  address: '1 boulevard Poissonnière',
+  postalCode: '75000',
+}
