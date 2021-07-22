13fd7c8962912d151586bc10ba81a99522b40d5c
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-07-22 12:29:36 +0200

    (Tech) added todo with jira ticket

--- src/libs/environment/env.web.ts
+++ src/libs/environment/env.web.ts
@@ -4,7 +4,7 @@ import { parseBooleanVariables } from 'libs/environment/parseBooleanVariables'
 
 import { Environment } from '.'
 
-// TODO: FIXME: configure cors on backend and disable proxy in webpackDevServer.config.js#L110
+// TODO: FIXME: configure cors on backend and disable proxy in webpackDevServer.config.js#L110 when doing https://passculture.atlassian.net/browse/PC-9847
 export const env = (parseBooleanVariables(
   omit(process.env, __DEV__ ? ['API_BASE_URL'] : [])
 ) as unknown) as Environment
