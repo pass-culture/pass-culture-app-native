9eb9011a008f7730dbdeba2d998d8582df1584f4
BenStirrup <benjamin.stirrup@gmail.com>
2021-07-23 15:56:10 +0200

    (PC-10128) Add first version of web reCAPTCHA (not tested with server's token validation)

--- src/types.ts
+++ src/types.ts
@@ -28,4 +28,25 @@ declare global {
       setMustUpdateApp?: (input: boolean) => void
     }
   }
+
+  // Web only
+  interface Window {
+    // See usage of `window.grecaptcha` in ReCaptcha.web.tsx
+    grecaptcha?: {
+      execute?: () => void
+      ready?: (callback: () => void) => void
+      render?: (
+        containerId: string,
+        options: {
+          sitekey: string
+          callback: (token: string) => void
+          'expired-callback': () => void
+          'error-callback': () => void
+          size: string
+          theme: string
+        }
+      ) => void
+      reset?: () => void
+    }
+  }
 }
