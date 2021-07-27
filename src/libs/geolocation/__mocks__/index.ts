0a26ef56855a1518d9283cdd1e69fd47823333fd
Marion Barré <marionb@bam.tech>
2021-07-27 15:59:52 +0200

    :white_check_mark: (mock geoloc)

--- src/libs/geolocation/__mocks__/index.ts
+++ src/libs/geolocation/__mocks__/index.ts
@@ -1,11 +1,9 @@
-export const useGeolocation = jest.fn(() => ({
-  position: {
-    latitude: 90,
-    longitude: 90,
-    accuracy: 1,
-    altitude: null,
-    heading: null,
-    speed: null,
-    altitudeAccuracy: null,
-  },
-}))
+import { geolocalisationContext } from 'libs/geolocation/fixtures/geolocationContext'
+
+export const useGeolocation = jest.fn(() => geolocalisationContext)
+
+export enum GeolocPermissionState {
+  GRANTED = 'granted',
+  DENIED = 'denied',
+  NEVER_ASK_AGAIN = 'never_ask_again',
+}
