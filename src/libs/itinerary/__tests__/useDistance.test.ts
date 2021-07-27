32eb41c5c4a68399ce6430d023d64e67f4aa7e74
Marion Barré <marionb@bam.tech>
2021-07-27 16:00:02 +0200

    :white_check_mark: (useDistance) add test

---
+++ src/libs/itinerary/__tests__/useDistance.test.ts
@@ -0,0 +1,40 @@
+import { mocked } from 'ts-jest/utils'
+
+import { useDistance } from 'features/offer/components/useDistance'
+import { useGeolocation } from 'libs/geolocation'
+import {
+  emptyGeolocationContext,
+  geolocalisationContext,
+} from 'libs/geolocation/fixtures/geolocationContext'
+import { formatDistance } from 'libs/parsers'
+
+jest.mock('libs/parsers')
+jest.mock('libs/geolocation')
+const mockedUseGeolocation = mocked(useGeolocation, true)
+
+describe('useDistance', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  const offerPosition = { lat: 31, long: 56 }
+
+  it('should call useGeolocation and formatDistance when geolocation is on', () => {
+    useDistance(offerPosition)
+    expect(useGeolocation).toHaveBeenCalledWith()
+    expect(formatDistance).toHaveBeenCalledWith(offerPosition, geolocalisationContext.position)
+  })
+
+  it('should return undefined when no offerpositon given', () => {
+    // @ts-expect-error offer position should not be undefined or null
+    useDistance()
+    expect(useGeolocation).toHaveBeenCalledWith()
+    expect(formatDistance).not.toHaveBeenCalled()
+  })
+  it('should return undefined when geolocation is off', () => {
+    mockedUseGeolocation.mockReturnValueOnce(emptyGeolocationContext)
+    useDistance(offerPosition)
+    expect(useGeolocation).toHaveBeenCalledWith()
+    expect(formatDistance).not.toHaveBeenCalled()
+  })
+})
