d670288f989016ae7ba4f1ee1e5e8a7064e40e88
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-07-27 19:08:38 +0200

    (PC-9990) Add proximity boost for search results

--- src/libs/search/filters/index.ts
+++ src/libs/search/filters/index.ts
@@ -1,6 +1,7 @@
 import { SearchOptions } from '@elastic/app-search-javascript'
 
 import { SearchParameters } from 'features/search/types'
+import { buildBoosts } from 'libs/search/filters/buildBoosts'
 
 import { buildFacetFilters } from './buildFacetFilters'
 import { buildGeolocationFilter } from './buildGeolocationFilter'
@@ -10,36 +11,35 @@ import { AppSearchFields, RESULT_FIELDS, SORT_OPTIONS } from './constants'
 export const buildQueryOptions = (
   params: SearchParameters,
   page?: number
-): SearchOptions<AppSearchFields> => ({
-  result_fields: RESULT_FIELDS,
-  filters: {
-    all: [
-      ...buildFacetFilters(params),
-      ...buildNumericFilters(params),
-      ...buildGeolocationFilter(params),
-    ],
-  },
-  page: {
-    current: page || 1,
-    size: params.hitsPerPage || 20,
-  },
-  group: {
-    // This ensures that only one offer of each group is retrieved.
-    // Ex: when we look for a book, we only show one per isbn (one per visa for the movies).
-    // See https://www.elastic.co/fr/blog/advanced-search-queries-in-elastic-app-search
-    field: AppSearchFields.group,
-  },
-  sort: SORT_OPTIONS,
-  // TODO(antoinewg): use boosts to sort by proximity. Or use _.sortBy ? pagination ?
-  // Although doesn't seem to work for offers without position or negative factor
-  // boosts: {
-  //   [AppSearchFields.geoloc]: {
-  //     type: 'proximity',
-  //     function: 'exponential',
-  //     center: '48.8557, 2.3469',
-  //     factor: 3,
-  //   },
-  // },
-})
+): SearchOptions<AppSearchFields> => {
+  const queryOptions: SearchOptions<AppSearchFields> = {
+    result_fields: RESULT_FIELDS,
+    filters: {
+      all: [
+        ...buildFacetFilters(params),
+        ...buildNumericFilters(params),
+        ...buildGeolocationFilter(params),
+      ],
+    },
+    page: {
+      current: page || 1,
+      size: params.hitsPerPage || 20,
+    },
+    group: {
+      // This ensures that only one offer of each group is retrieved.
+      // Ex: when we look for a book, we only show one per isbn (one per visa for the movies).
+      // See https://www.elastic.co/fr/blog/advanced-search-queries-in-elastic-app-search
+      field: AppSearchFields.group,
+    },
+    sort: SORT_OPTIONS,
+  }
+
+  const boosts = buildBoosts(params.geolocation)
+  if (boosts) {
+    queryOptions['boosts'] = boosts
+  }
+
+  return queryOptions
+}
 
 export { AppSearchFields, RESULT_FIELDS }
