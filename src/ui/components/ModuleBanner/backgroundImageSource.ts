65beaa13034a58a2d8119d33b50346e9778a73f0
BenStirrup <benjamin.stirrup@gmail.com>
2021-07-28 16:34:45 +0200

    (PC-10179) Make a web version of ModuleBanner and move an asset to public/images

---
+++ src/ui/components/ModuleBanner/backgroundImageSource.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+export const BACKGROUND_IMAGE_SOURCE = require('../../../public/images/BannerRectangle.png')
---
+++ src/ui/components/ModuleBanner/backgroundImageSource.web.ts
@@ -0,0 +1,3 @@
+// TODO WEB : PC-9602 ==> we may want to optimize the display of images on the web
+// instead of just using the raw image from /public/images
+export const BACKGROUND_IMAGE_SOURCE = { uri: '/images/BannerRectangle.png' }
