24452ab129e3d109dc44727b74e18f7b3a002f49
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-07-28 11:53:17 +0200

    Remove outdated cheatcode to Venue

--- src/features/cheatcodes/pages/Navigation.tsx
+++ src/features/cheatcodes/pages/Navigation.tsx
@@ -193,12 +193,6 @@ export function Navigation(): JSX.Element {
             onPress={() => navigation.navigate('CulturalSurvey')}
           />
         </Row>
-        <Row half>
-          <NavigationButton
-            title="Venue"
-            onPress={() => navigation.navigate('Venue', { id: 5543 })}
-          />
-        </Row>
         <Row>
           <NavigationButton
             title={'Mauvais deeplink unknown'}
