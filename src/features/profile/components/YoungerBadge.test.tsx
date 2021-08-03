b6bb7b999407a85fd8212c0b1b0862844a0575f7
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-08-03 12:41:59 +0200

    (Tests) unskip tests about deposit

--- src/features/profile/components/YoungerBadge.test.tsx
+++ src/features/profile/components/YoungerBadge.test.tsx
@@ -3,19 +3,13 @@ import React from 'react'
 import { YoungerBadge } from 'features/profile/components/YoungerBadge'
 import { render } from 'tests/utils'
 
-let mockDepositAmount = '300 €'
-jest.mock('features/auth/api', () => ({ useDepositAmount: () => mockDepositAmount }))
+jest.mock('features/auth/api', () => ({ useDepositAmount: () => '300 €' }))
 
 describe('YoungerBadge', () => {
   it('should display correct depositAmount', () => {
-    mockDepositAmount = '300 €'
-    let { queryByText } = render(<YoungerBadge />)
-    expect(queryByText(/tu bénéficieras de/)).toBeTruthy()
-    expect(queryByText(/300€/)).toBeTruthy()
-
-    mockDepositAmount = '500 €'
-    queryByText = render(<YoungerBadge />).queryByText
-    expect(queryByText(/tu bénéficieras de/)).toBeTruthy()
-    expect(queryByText(/500€/)).toBeTruthy()
+    const { queryByText } = render(<YoungerBadge />)
+    expect(
+      queryByText(/tu bénéficieras de\u00a0300€\u00a0offerts à dépenser sur l’application./)
+    ).toBeTruthy()
   })
 })
