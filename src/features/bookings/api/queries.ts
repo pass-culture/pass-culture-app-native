667941c27bd187112f6dd70ea8d4e431479d9f9c
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-08-03 14:52:44 +0200

    (Tech) do not catch error boundary on both web and mobile for anonymous

--- src/features/bookings/api/queries.ts
+++ src/features/bookings/api/queries.ts
@@ -2,14 +2,18 @@ import { useQuery } from 'react-query'
 
 import { api } from 'api/api'
 import { BookingReponse, BookingsResponse } from 'api/gen'
+import { useAuthContext } from 'features/auth/AuthContext'
 import { QueryKeys } from 'libs/queryKeys'
 
 // Arbitrary. Make sure the cache is invalidated after each booking
 const STALE_TIME_BOOKINGS = 5 * 60 * 1000
 
 export function useBookings() {
+  const { isLoggedIn } = useAuthContext()
+
   return useQuery<BookingsResponse>(QueryKeys.BOOKINGS, () => api.getnativev1bookings(), {
     staleTime: STALE_TIME_BOOKINGS,
+    useErrorBoundary: isLoggedIn,
   })
 }
 
