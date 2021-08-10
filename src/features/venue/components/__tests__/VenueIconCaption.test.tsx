79a95c804f2d22d605921ff90ac57ab5e0fa0436
Lucas Beneston <lucasbeneston@MacBook-Pro-de-Lucas.local>
2021-08-10 18:56:47 +0200

    (PC-10065) Add localisation icon for VenueIconCaption

--- src/features/venue/components/__tests__/VenueIconCaption.test.tsx
+++ src/features/venue/components/__tests__/VenueIconCaption.test.tsx
@@ -7,20 +7,38 @@ import { render } from 'tests/utils'
 
 import { VenueIconCaptions } from '../VenueIconCaptions'
 
+const mockDistance: string | null = null
+jest.mock('libs/geolocation/hooks/useDistance', () => ({
+  useDistance: () => mockDistance,
+}))
+
 const typeLabel = parseType(VenueTypeCode.MOVIE)
 const typeLabelNull = parseType(null)
+const locationCoordinates = { latitude: 2, longitude: 4 }
 
 describe('<VenueIconCaptions />', () => {
   it('should match snapshot', async () => {
     const { toJSON } = render(
-      reactQueryProviderHOC(<VenueIconCaptions label={typeLabel} type={VenueTypeCode.MOVIE} />)
+      reactQueryProviderHOC(
+        <VenueIconCaptions
+          label={typeLabel}
+          type={VenueTypeCode.MOVIE}
+          locationCoordinates={locationCoordinates}
+        />
+      )
     )
     expect(toJSON()).toMatchSnapshot()
   })
 
   it('should display a default label "Autre" if type is null', async () => {
     const { getByText } = render(
-      reactQueryProviderHOC(<VenueIconCaptions type={null} label={typeLabelNull} />)
+      reactQueryProviderHOC(
+        <VenueIconCaptions
+          type={null}
+          label={typeLabelNull}
+          locationCoordinates={locationCoordinates}
+        />
+      )
     )
     expect(getByText('Autre')).toBeTruthy()
   })
