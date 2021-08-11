c7479a4b57df0623e26bf034f6590ee1ffdd58b1
Lucas Beneston <lucasbeneston@MacBook-Pro-de-Lucas.local>
2021-08-11 11:37:08 +0200

    (PC-10065) Add isEnabled condition for IconWithCaption

--- src/features/venue/atoms/IconWithCaption.tsx
+++ src/features/venue/atoms/IconWithCaption.tsx
@@ -8,16 +8,28 @@ interface IconWithCaptionProps {
   Icon: React.FC<IconInterface>
   caption: string
   testID?: string
+  isDisabled?: boolean | false
 }
 
-export const IconWithCaption = ({ Icon, caption, testID }: IconWithCaptionProps) => (
-  <Container>
-    <IconContainer>
-      <Icon size={getSpacing(10)} color={ColorsEnum.GREY_DARK} testID={testID} />
-    </IconContainer>
-    <Caption testID={`caption-${testID}`}>{caption}</Caption>
-  </Container>
-)
+export const IconWithCaption = ({ Icon, caption, testID, isDisabled }: IconWithCaptionProps) => {
+  const getTextColor = (isDisabled: boolean) => {
+    if (isDisabled) return ColorsEnum.GREY_MEDIUM
+    return ColorsEnum.GREY_DARK
+  }
+
+  const color = getTextColor(isDisabled as boolean)
+
+  return (
+    <Container>
+      <IconContainer>
+        <Icon size={getSpacing(10)} color={color} testID={testID} />
+      </IconContainer>
+      <Caption testID={`caption-${testID}`} color={color}>
+        {caption}
+      </Caption>
+    </Container>
+  )
+}
 
 const Container = styled.View({ flex: 1, alignItems: 'center' })
 const IconContainer = styled.View({ padding: getSpacing(1) })
