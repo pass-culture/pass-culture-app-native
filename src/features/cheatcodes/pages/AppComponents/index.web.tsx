022a1466c19498de53a8b28c93dbe2121debb56b
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-08-12 17:31:13 +0200

    [WIP] quick win

--- src/features/cheatcodes/pages/AppComponents/index.web.tsx
+++
@@ -1,9 +0,0 @@
-import loadable from '@loadable/component'
-import React from 'react'
-
-import { LoadingPage } from 'ui/components/LoadingPage'
-
-export const AppComponents = loadable(() => import('./AppComponents'), {
-  resolveComponent: (components) => components.AppComponents,
-  fallback: <LoadingPage />,
-})
---
+++ src/features/forceUpdate/ForceUpdate.web.tsx
@@ -0,0 +1,16 @@
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import React, { useCallback } from 'react'
+
+import { homeNavigateConfig } from 'features/navigation/helpers'
+import { UseNavigationType } from 'features/navigation/RootNavigator'
+import { LoadingPage } from 'ui/components/LoadingPage'
+
+export const ForceUpdate: React.FC = () => {
+  const { navigate } = useNavigation<UseNavigationType>()
+  useFocusEffect(
+    useCallback(() => {
+      navigate(homeNavigateConfig.screen, homeNavigateConfig.params)
+    }, [])
+  )
+  return <LoadingPage />
+}
