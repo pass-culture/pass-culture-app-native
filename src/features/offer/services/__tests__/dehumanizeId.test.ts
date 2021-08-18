d365c886943d04f8d513011b20ce7ab643b0b8a4
Emma Jamin <emmajamin@orange.fr>
2021-08-18 12:39:06 +0200

    (PC-10375) fix intToByteArray function

--- src/features/offer/services/__tests__/dehumanizeId.test.ts
+++ src/features/offer/services/__tests__/dehumanizeId.test.ts
@@ -7,36 +7,22 @@ interface HumToDehumTest {
 
 describe('dehumanizeId', () => {
   it.each`
-    humanizedId | dehumanizedId
-    ${'AHEHK'}  | ${116853}
-    ${'AHD3Q'}  | ${116664}
-    ${'AHD3G'}  | ${116659}
-    ${'AHD3C'}  | ${116657}
-    ${'AGHYA'}  | ${102272}
-    ${'A98K8'}  | ${138407}
+    humanizedId  | dehumanizedId
+    ${'8Q'}      | ${116}
+    ${'AHEHK'}   | ${116853}
+    ${'AHD3Q'}   | ${116664}
+    ${'AHD3G'}   | ${116659}
+    ${'AHD3C'}   | ${116657}
+    ${'AGHYA'}   | ${102272}
+    ${'A98K8'}   | ${138407}
+    ${'MAYGK'}   | ${6303845}
+    ${'AG8YRWQ'} | ${27101402}
+    ${'B4GQA'}   | ${986368}
   `(
     'a given humanizedId: $humanizedId becomes a dehumanizedId: $dehumanizedId',
     ({ dehumanizedId, humanizedId }: HumToDehumTest) => {
       expect(dehumanizeId(humanizedId)).toBe(dehumanizedId)
       expect(humanizeId(dehumanizedId)).toBe(humanizedId)
-    }
-  )
-
-  it.each`
-    dehumanizedId | humanizedId
-    ${27101402}   | ${'AG8YRWQ'}
-    ${116}        | ${'8Q'}
-    ${116853}     | ${'AHEHK'}
-    ${116664}     | ${'AHD3Q'}
-    ${116659}     | ${'AHD3G'}
-    ${116657}     | ${'AHD3C'}
-    ${6303845}    | ${'MAYGK'}
-    ${116659}     | ${'AHD3G'}
-  `(
-    'a given dehumanizedId: $dehumanizedId becomes a humanizedId: $humanizedId',
-    ({ dehumanizedId, humanizedId }: HumToDehumTest) => {
-      expect(humanizeId(dehumanizedId)).toBe(humanizedId)
-      expect(dehumanizeId(humanizedId)).toBe(dehumanizedId)
     }
   )
 })
