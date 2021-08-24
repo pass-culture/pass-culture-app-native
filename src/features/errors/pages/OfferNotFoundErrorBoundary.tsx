d4e579a1ab662efd2129983ccdc44f8c03e5cd01
BenStirrup <benjamin.stirrup@gmail.com>
2021-08-24 12:13:05 +0200

    (PC-10356) Fix error fallback not removed after navigating back to an offer page

--- src/features/errors/pages/OfferNotFoundErrorBoundary.tsx
+++ src/features/errors/pages/OfferNotFoundErrorBoundary.tsx
@@ -1,24 +1,32 @@
 import { t } from '@lingui/macro'
+import { useNavigation } from '@react-navigation/native'
 import React, { ComponentType } from 'react'
 import { FallbackProps, withErrorBoundary } from 'react-error-boundary'
 import styled from 'styled-components/native'
 
-import { navigateToHome } from 'features/navigation/helpers'
-import { AsyncError, OfferNotFoundError } from 'libs/errorMonitoring'
+import { homeNavigateConfig } from 'features/navigation/helpers'
+import { UseNavigationType } from 'features/navigation/RootNavigator'
+import { OfferNotFoundError } from 'libs/errorMonitoring'
 import { Helmet } from 'libs/react-helmet/Helmet'
 import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
 import { GenericInfoPage } from 'ui/components/GenericInfoPage'
 import { NoOffer } from 'ui/svg/icons/NoOffer'
 import { ColorsEnum, Spacer, Typo, getSpacing } from 'ui/theme'
 
-interface Props extends FallbackProps {
-  error: AsyncError | OfferNotFoundError
-}
+export const OfferNotFoundErrorFallback = (props: FallbackProps) => {
+  const navigation = useNavigation<UseNavigationType>()
 
-export const OfferNotFoundErrorBoundary = (props: Props) => {
+  // Error bubbling : wrap `withOfferNotFoundErrorBoundary` with another boundary
+  // to handle errors that are not `OfferNotFoundError`
   if (!(props.error instanceof OfferNotFoundError)) {
     throw props.error
   }
+
+  function onPress() {
+    props.resetErrorBoundary()
+    navigation.replace(homeNavigateConfig.screen, homeNavigateConfig.params)
+  }
+
   return (
     <React.Fragment>
       <Helmet>
@@ -28,7 +36,7 @@ export const OfferNotFoundErrorBoundary = (props: Props) => {
       <GenericInfoPage title={t`Offre introuvable !`} icon={NoOffer} iconSize={getSpacing(40)}>
         <StyledBody>{t`Il est possible que cette offre soit désactivée ou n'existe pas.`}</StyledBody>
         <Spacer.Column numberOfSpaces={12} />
-        <ButtonPrimaryWhite title={t`Retourner à l'accueil`} onPress={navigateToHome} />
+        <ButtonPrimaryWhite title={t`Retourner à l'accueil`} onPress={onPress} />
       </GenericInfoPage>
     </React.Fragment>
   )
@@ -37,7 +45,7 @@ export const OfferNotFoundErrorBoundary = (props: Props) => {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function withOfferNotFoundErrorBoundary(component: ComponentType<any>) {
   return withErrorBoundary(React.memo(component), {
-    FallbackComponent: OfferNotFoundErrorBoundary,
+    FallbackComponent: OfferNotFoundErrorFallback,
   })
 }
 
