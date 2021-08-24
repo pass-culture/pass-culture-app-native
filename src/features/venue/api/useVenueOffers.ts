1730f7a42b9e241354fdc5c8d8da634bb9d3b30c
Lucas Beneston <lucasbeneston@MacBook-Pro-de-Lucas.local>
2021-08-24 17:33:21 +0200

    (PC-10045) Add redirection to all offers search from the button 'Voir toutes les offres'

--- src/features/venue/api/useVenueOffers.ts
+++ src/features/venue/api/useVenueOffers.ts
@@ -8,9 +8,8 @@ import { useGeolocation } from 'libs/geolocation'
 import { QueryKeys } from 'libs/queryKeys'
 import { fetchVenueOffers } from 'libs/search'
 
-export const useVenueOffers = (venueId: number) => {
+export const useVenueSearchParameters = (venueId: number): SearchParameters => {
   const { position } = useGeolocation()
-  const transformHits = useTransformAlgoliaHits()
 
   const params: SearchParameters = {
     aroundRadius: position ? 100 : null,
@@ -30,6 +29,13 @@ export const useVenueOffers = (venueId: number) => {
     timeRange: null,
     venueId,
   }
+
+  return params
+}
+
+export const useVenueOffers = (venueId: number) => {
+  const transformHits = useTransformAlgoliaHits()
+  const params = useVenueSearchParameters(venueId)
 
   return useQuery([QueryKeys.VENUE_OFFERS, venueId], () => fetchVenueOffers(params), {
     select: ({ hits, nbHits }) => ({
