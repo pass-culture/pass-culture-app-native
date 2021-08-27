7b21f1e15f8785fae435475d44cabb3b9fbe4c79
Lucas Beneston <lucasbeneston@MacBook-Pro-de-Lucas.local>
2021-08-27 12:36:53 +0200

    (PC-10045) Add ClippedTag tests

---
+++ src/ui/components/__tests__/ClippedTag.web.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { fireEvent, render } from 'tests/utils/web'
+
+import { ClippedTag } from '../ClippedTag'
+
+const label = 'Musée du louvre'
+const removeVenueId = jest.fn()
+const testId = 'Enlever le lieu'
+
+const mockDispatchSearch = jest.fn()
+jest.mock('features/search/pages/SearchWrapper', () => ({
+  useSearch: () => ({
+    dispatch: mockDispatchSearch,
+  }),
+}))
+
+describe('<ClippedTag/>', () => {
+  it('should render correctly', () => {
+    const renderAPI = render(<ClippedTag label={label} onPress={removeVenueId} testId={testId} />)
+    expect(renderAPI).toMatchSnapshot()
+  })
+
+  it('should delete venueId and ClippedTag when clicking on Clear icon', () => {
+    const { getByTestId } = render(
+      <ClippedTag label={label} onPress={removeVenueId} testId={testId} />
+    )
+
+    const clearIcon = getByTestId('Enlever le lieu')
+    fireEvent.click(clearIcon)
+    expect(removeVenueId).toHaveBeenCalledTimes(1)
+  })
+})
