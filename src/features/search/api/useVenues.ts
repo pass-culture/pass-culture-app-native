619f4d253abcf8174750385ea2890fd19a7d80c0
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-09-02 10:41:11 +0200

    (PC-10075) FF searching on venues

--- src/features/search/api/useVenues.ts
+++ src/features/search/api/useVenues.ts
@@ -3,11 +3,16 @@ import { useQuery } from 'react-query'
 import { SuggestedPlace } from 'libs/place'
 import { QueryKeys } from 'libs/queryKeys'
 import { fetchVenues } from 'libs/search/fetch/search'
+import { useAppSearchBackend } from 'libs/search/fetch/useAppSearchBackend'
 
 const STALE_TIME_VENUES = 5 * 60 * 1000
 
-export const useVenues = (query: string) =>
-  useQuery<SuggestedPlace[]>([QueryKeys.VENUES, query], () => fetchVenues(query), {
+export const useVenues = (query: string) => {
+  const { enabled, isAppSearchBackend } = useAppSearchBackend()
+
+  return useQuery<SuggestedPlace[]>([QueryKeys.VENUES, query], () => fetchVenues(query), {
     staleTime: STALE_TIME_VENUES,
-    enabled: query.length > 0,
+    // TODO(antoinewg) remove condition once migration to AppSearch is complete
+    enabled: query.length > 0 && enabled && isAppSearchBackend,
   })
+}
