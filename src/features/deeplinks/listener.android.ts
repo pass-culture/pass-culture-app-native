db2fec151f0d41e9b178fcca9baf117e0db80b99
BenStirrup <benjamin.stirrup@gmail.com>
2021-09-14 19:15:58 +0200

    (PC-10796) Use custom listener for iOS deeplinks instead of R.Nav.

--- src/features/deeplinks/listener.android.ts
+++ src/features/deeplinks/listener.android.ts
@@ -1,29 +1,36 @@
-import { useEffect } from 'react'
-import { Linking } from 'react-native'
+// import { useEffect } from 'react'
+// import { Linking } from 'react-native'
 
-import { useDeeplinkUrlHandler, useOnDeeplinkError } from './useDeeplinkUrlHandler'
+// import { useDeeplinkUrlHandler, useOnDeeplinkError } from './useDeeplinkUrlHandler'
 
+// To handle the "deeplink" expperience of the app on android,
+// we rely only on the `linking` config object passed to NavigationContainer,
+// so this hook is useless on android.
 export function useListenDeepLinksEffect() {
-  const onError = useOnDeeplinkError()
-  const handleDeeplinkUrl = useDeeplinkUrlHandler()
-
-  useEffect(() => {
-    Linking.getInitialURL()
-      .catch(onError)
-      .then((url) => {
-        if (url === null) {
-          // Android/iOS can return null when opening the app from certains places
-          // in this case, it's not en error, just ignore it.
-          return
-        }
-
-        if (url) {
-          handleDeeplinkUrl({ url })
-        } else {
-          onError()
-        }
-      })
-    Linking.addEventListener('url', handleDeeplinkUrl)
-    return () => Linking.removeEventListener('url', handleDeeplinkUrl)
-  }, [])
+  // do nothing
 }
+
+// export function useListenDeepLinksEffect() {
+//   const onError = useOnDeeplinkError()
+//   const handleDeeplinkUrl = useDeeplinkUrlHandler()
+
+//   useEffect(() => {
+//     Linking.getInitialURL()
+//       .catch(onError)
+//       .then((url) => {
+//         if (url === null) {
+//           // Android/iOS can return null when opening the app from certains places
+//           // in this case, it's not en error, just ignore it.
+//           return
+//         }
+
+//         if (url) {
+//           handleDeeplinkUrl({ url })
+//         } else {
+//           onError()
+//         }
+//       })
+//     Linking.addEventListener('url', handleDeeplinkUrl)
+//     return () => Linking.removeEventListener('url', handleDeeplinkUrl)
+//   }, [])
+// }
--- src/libs/typesUtils/typeHelpers.ts
+++ src/libs/typesUtils/typeHelpers.ts
@@ -1,2 +1,3 @@
 export type NoNullProperties<T> = { [K in keyof T]: Exclude<T[K], null> }
 export type Range<T> = [T, T]
+export type RequireField<T, K extends keyof T> = T & Required<Pick<T, K>>
