4f86e76ddf7382185e643db236cba3f247c5923c
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-09-14 17:21:05 +0200

    :sparkles: (pc-10745) add new info page for phone validation error

---
+++ src/features/auth/signup/PhoneValidation/PhoneValidationTooManySMSSent/tests/PhoneValidationTooManySMSSent.native.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { PhoneValidationTooManySMSSent } from 'features/auth/signup/PhoneValidation/PhoneValidationTooManySMSSent/PhoneValidationTooManySMSSent'
+import { navigateToHome } from 'features/navigation/helpers'
+import { fireEvent, render } from 'tests/utils'
+
+jest.mock('features/navigation/helpers')
+
+describe('Navigate to home button', () => {
+  it('should redirect to Home when clicking on homepage button', async () => {
+    const { getByText } = render(<PhoneValidationTooManySMSSent />)
+
+    fireEvent.press(getByText("Retourner à l'accueil"))
+
+    expect(navigateToHome).toBeCalled()
+  })
+})
---
+++ src/features/auth/signup/PhoneValidation/PhoneValidationTooManySMSSent/tests/PhoneValidationTooManySMSSent.web.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { PhoneValidationTooManySMSSent } from 'features/auth/signup/PhoneValidation/PhoneValidationTooManySMSSent/PhoneValidationTooManySMSSent'
+import { navigateToHome } from 'features/navigation/helpers'
+import { fireEvent, render } from 'tests/utils/web'
+
+jest.mock('features/navigation/helpers')
+
+describe('Navigate to home button', () => {
+  it('should redirect to Home when clicking on homepage button', async () => {
+    const { getByText } = render(<PhoneValidationTooManySMSSent />)
+
+    fireEvent.click(getByText("Retourner à l'accueil"))
+
+    expect(navigateToHome).toBeCalled()
+  })
+})
