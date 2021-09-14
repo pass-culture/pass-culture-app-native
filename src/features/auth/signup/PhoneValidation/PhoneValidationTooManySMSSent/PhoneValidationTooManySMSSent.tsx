472107c128e755b4db5c133c6153a3f0bc1b90b9
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-09-14 17:21:05 +0200

    :sparkles: (pc-10745) add new info page for phone validation error

---
+++ src/features/auth/signup/PhoneValidation/PhoneValidationTooManySMSSent/PhoneValidationTooManySMSSent.tsx
@@ -0,0 +1,28 @@
+import { t } from '@lingui/macro'
+import React from 'react'
+import styled from 'styled-components/native'
+
+import { navigateToHome } from 'features/navigation/helpers'
+import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
+import { GenericInfoPage } from 'ui/components/GenericInfoPage'
+import { AccountLocked } from 'ui/svg/icons/AccountLocked'
+import { ColorsEnum, Spacer, Typo } from 'ui/theme'
+
+export function PhoneValidationTooManySMSSent() {
+  return (
+    <GenericInfoPage title={t`Trop de tentatives !`} icon={AccountLocked}>
+      <StyledBody>
+        {t`Tu as dépassé le nombre d’essais autorisés. Tu pourras réessayer dans 12 heures !`}
+      </StyledBody>
+
+      <Spacer.Column numberOfSpaces={22} />
+      <ButtonPrimaryWhite title={t`Retourner à l'accueil`} onPress={navigateToHome} />
+    </GenericInfoPage>
+  )
+}
+
+const StyledBody = styled(Typo.Body).attrs({
+  color: ColorsEnum.WHITE,
+})({
+  textAlign: 'center',
+})
