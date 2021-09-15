99674f39e17ea1669b0d9eec34fc3ce074faa84d
BenStirrup <benjamin.stirrup@gmail.com>
2021-09-15 11:14:53 +0200

    (PC-10796) Add support for legacy offer urls

--- src/features/navigation/RootNavigator/linking/getStateFromPath.ts
+++ src/features/navigation/RootNavigator/linking/getStateFromPath.ts
@@ -1,12 +1,16 @@
+import url from 'url'
+
 import { getStateFromPath } from '@react-navigation/native'
 
+import { DeeplinkPath, DeeplinkPathWithPathParams } from 'features/deeplinks/enums'
 import { ScreenNames } from 'features/navigation/RootNavigator/types'
 
 type Params = Parameters<typeof getStateFromPath>
 type Path = Params[0]
 type Config = Params[1]
 
-export function customGetStateFromPath(path: Path, config: Config) {
+export function customGetStateFromPath(originalPath: Path, config: Config) {
+  const path = addRedirectSupport(originalPath)
   const state = getStateFromPath(path, config)
   if (state && state.routes) {
     const screenName = state.routes[0].name as ScreenNames
@@ -20,4 +24,46 @@ export function customGetStateFromPath(path: Path, config: Config) {
     }
   }
   return state
+}
+
+function addRedirectSupport(path: Path): Path {
+  const { pathname, search } = url.parse(path, true)
+  if (!pathname) {
+    return path
+  }
+  if (search && matchPathname(pathname, ['offre', 'offer'])) {
+    const { id } = getUrlQueryParams(search)
+    if (id) {
+      return new DeeplinkPathWithPathParams(DeeplinkPath.OFFER, { id }).getFullPath()
+    }
+  }
+  return path
+}
+
+function matchPathname(pathname: string, pathnamesToCheck: string[]): boolean {
+  return pathnamesToCheck.some((p) => pathname.includes(p))
+}
+
+function getUrlQueryParams(encodedUri: string) {
+  const uri = decodeURI(encodedUri)
+  const sanitizedUri = uri
+    .trim()
+    .replace(/^\/+|\/+$/g, '') // removes external '/'
+    .replace(/^\?+/g, '') // removes initial '?'
+    .replace(/^&+|&+$/g, '') // removes external '&'
+  const parameterFields =
+    sanitizedUri.split('&').reduce((accumulator, field) => {
+      accumulator = accumulator || {}
+      const index = field.indexOf('=')
+      if (index === -1) {
+        accumulator[field] = '' // empty parameter
+      } else {
+        const key = field.slice(0, index)
+        const value = field.slice(index + 1)
+        accumulator[key] = value
+      }
+      return accumulator
+    }, <Record<string, string> | null>{}) || {}
+
+  return parameterFields
 }
