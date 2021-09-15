10e26ad991cc21597b38cc6de27525f671576c5d
Lucas Beneston <lucasbeneston@MacBook-Pro-de-Lucas.local>
2021-09-15 17:32:49 +0200

    (PC-10447) Add PhoneFilled icon and update ContactBlock component + tests

--- src/ui/components/contact/__tests__/ContactBlock.native.test.tsx
+++ src/ui/components/contact/__tests__/ContactBlock.native.test.tsx
@@ -12,7 +12,6 @@ const venueId = venueResponseSnap.id
 const email = 'contact@venue.com'
 const phoneNumber = '+33102030405'
 const website = 'https://my@website.com'
-const venueName = 'Le Petit Rintintin 1'
 
 jest.mock('features/venue/api/useVenue')
 const openMail = jest.spyOn(ContactHelpers, 'openMail')
@@ -24,50 +23,51 @@ describe('<ContactBlock/>', () => {
 
   it('should open external email when the email button is press', () => {
     const { getByText } = render(<ContactBlock venueId={venueId} />)
-    fireEvent.press(getByText(`Contacter ${venueName}`))
+    fireEvent.press(getByText('E-mail'))
     expect(openMail).toHaveBeenNthCalledWith(1, email)
   })
 
   it('should open external phone when the phone button is press', () => {
     const { getByText } = render(<ContactBlock venueId={venueId} />)
-    fireEvent.press(getByText(phoneNumber))
+    fireEvent.press(getByText('Téléphone'))
     expect(openPhoneNumber).toHaveBeenNthCalledWith(1, phoneNumber)
   })
 
   it('should open external website when the website button is press', () => {
     const { getByText } = render(<ContactBlock venueId={venueId} />)
-    fireEvent.press(getByText(website))
+    fireEvent.press(getByText('Site internet'))
     expect(openExternalUrl).toHaveBeenNthCalledWith(1, website)
   })
 
   it('should display the email, phoneNumber and website', () => {
     const { queryByText } = render(<ContactBlock venueId={venueId} />)
-    expect(queryByText(email))
-    expect(queryByText(phoneNumber))
-    expect(queryByText(website))
+    expect(queryByText('E-mail'))
+    expect(queryByText('Téléphone'))
+    expect(queryByText('Site internet'))
   })
 
-  it('should display 3 external icons if email, phoneNumber and website are enable', () => {
+  it('should display 3 different icons if email, phoneNumber and website are enable', () => {
     const { getAllByTestId } = render(<ContactBlock venueId={venueId} />)
-    const externalLinkSquare = getAllByTestId('ExternalLinkSquare')
-    expect(externalLinkSquare.length).toBe(3)
+    expect(getAllByTestId('Icon E-mail'))
+    expect(getAllByTestId('Icon Téléphone'))
+    expect(getAllByTestId('Icon Site internet'))
   })
 
   it('should log event VenueContact when opening email', () => {
     const { getByText } = render(<ContactBlock venueId={venueId} />)
-    fireEvent.press(getByText(`Contacter ${venueName}`))
+    fireEvent.press(getByText('E-mail'))
     expect(analytics.logVenueContact).toHaveBeenNthCalledWith(1, { type: 'email', venueId })
   })
 
   it('should log event VenueContact when opening phone number', () => {
     const { getByText } = render(<ContactBlock venueId={venueId} />)
-    fireEvent.press(getByText(phoneNumber))
+    fireEvent.press(getByText('Téléphone'))
     expect(analytics.logVenueContact).toHaveBeenNthCalledWith(1, { type: 'phoneNumber', venueId })
   })
 
   it('should log event VenueContact when opening website', () => {
     const { getByText } = render(<ContactBlock venueId={venueId} />)
-    fireEvent.press(getByText(website))
+    fireEvent.press(getByText('Site internet'))
     expect(analytics.logVenueContact).toHaveBeenNthCalledWith(1, { type: 'website', venueId })
   })
 })
