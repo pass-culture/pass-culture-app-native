731284d29e73cc08a8d50e25463eab486a724638
AnoukHello <anouk.hello@polyconseil.fr>
2021-09-16 16:03:54 +0200

    (PC-10666) add search util to filter out unavailable categories

---
+++ src/libs/search/utils/filterAvailableCategories.ts
@@ -0,0 +1,10 @@
+import { OptionalCategoryCriteria } from 'features/search/enums'
+
+export const filterAvailableCategories = (
+  selectedCategories: string[],
+  availableCategories: OptionalCategoryCriteria
+): string[] => {
+  return Object.values(availableCategories)
+    .filter((categoryCriterion) => selectedCategories.includes(categoryCriterion.label))
+    .map((categoryCriterion) => categoryCriterion.facetFilter)
+}
