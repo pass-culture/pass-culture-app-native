c18db49e0f77a261d71b977efba6b521d367b3e0
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-09-16 15:21:15 +0200

    (PC-10068) renamme VenuesPane => VenuesModule

--- src/features/home/contentful/index.ts
+++ src/features/home/contentful/index.ts
@@ -1,5 +1,5 @@
 import { ProcessedModule as ProcessedModuleType } from './moduleTypes'
 export * from './contentful'
-export { ExclusivityPane, BusinessPane, Offers, OffersWithCover } from './moduleTypes'
+export { ExclusivityPane, BusinessPane, Offers, OffersWithCover, VenuesModule } from './moduleTypes'
 export { processHomepageEntry } from './processHomepageEntry'
 export type ProcessedModule = ProcessedModuleType
