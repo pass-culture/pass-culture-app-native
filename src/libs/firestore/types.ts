1621882e451f16db614e0bf8663e13c78404f736
Marion Barré <marionb@bam.tech>
2021-09-17 15:18:43 +0200

    :label: (firestore) add new variables

---
+++ src/libs/firestore/types.ts
@@ -0,0 +1,11 @@
+export enum RemoteStoreCollections {
+  APP_SEARCH = 'appsearch',
+  MAINTENANCE = 'maintenance',
+  APPLICATION_VERSIONS = 'applicationVersions',
+}
+
+export enum RemoteStoreDocuments {
+  LOAD_PERCENT = 'load_percent',
+  MAINTENANCE_IS_ON = 'maintenanceIsOn',
+  MINIMAL_BUILD_NUMBER = 'minimalBuildNumber',
+}
