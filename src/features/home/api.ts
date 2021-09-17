6f3d6e85c27036dde309e8fa730dfb5cfec136ab
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-09-17 12:09:08 +0200

    (PC-10780) select by default playlist tagged master

--- src/features/home/api.ts
+++ src/features/home/api.ts
@@ -6,6 +6,7 @@ import { UserProfileResponse } from 'api/gen'
 import { useAuthContext } from 'features/auth/AuthContext'
 import { HomepageEntry } from 'features/home/contentful'
 import { EntryCollection, EntryFields, processHomepageEntry } from 'features/home/contentful'
+import { useSelectPlaylist } from 'features/home/selectPlaylist'
 import { env } from 'libs/environment'
 import { getExternal } from 'libs/fetch'
 import { QueryKeys } from 'libs/queryKeys'
@@ -17,16 +18,6 @@ export const CONTENTFUL_BASE_URL = 'https://cdn.contentful.com'
 export const BASE_URL = `${CONTENTFUL_BASE_URL}/spaces/${env.CONTENTFUL_SPACE_ID}/environments/${env.CONTENTFUL_ENVIRONMENT}`
 export const PARAMS = `?include=${DEPTH_LEVEL}&content_type=homepageNatif&access_token=${env.CONTENTFUL_ACCESS_TOKEN}`
 
-const selectEntry = (entries?: HomepageEntry[], entryId?: string): HomepageEntry | undefined => {
-  if (!entries) return undefined
-  /* Support good practice is to configure on Contentful dashboard only 1 contenttype homepage
-    But there is not blocking on the dashboard, that's why we select first one here */
-  const firstEntry = entries[0]
-  if (!entryId) return firstEntry
-
-  return entries.find(({ sys }) => sys.id === entryId) || firstEntry
-}
-
 export async function getEntries() {
   const url = `${BASE_URL}/entries${PARAMS}`
   const json = await getExternal<EntryCollection<EntryFields, 'homepageNatif'>>(url)
@@ -35,6 +26,7 @@ export async function getEntries() {
 }
 
 export function useHomepageModules(entryId?: string) {
+  const selectPlaylist = useSelectPlaylist(entryId)
   const { data: entries } = useQuery<HomepageEntry[]>(QueryKeys.HOMEPAGE_MODULES, getEntries, {
     staleTime: STALE_TIME_CONTENTFUL,
   })
