2b9f9bb0258b60a04407a4669185009bcf7c152f
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-09-21 16:38:35 +0200

    (Tech) remove warning for NextBeneficiaryStep

--- src/features/auth/signup/NextBeneficiaryStep/tests/NextBeneficiaryStep.native.test.tsx
+++ src/features/auth/signup/NextBeneficiaryStep/tests/NextBeneficiaryStep.native.test.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
 import { NextBeneficiaryStep } from 'features/auth/signup/NextBeneficiaryStep'
-import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { render, superFlushWithAct } from 'tests/utils'
+import { render } from 'tests/utils'
+
+jest.mock('features/auth/settings')
+
+const mockData: unknown = null
+jest.mock('features/home/api', () => ({
+  useUserProfileInfo: jest.fn(() => ({
+    refetch: jest.fn(() => Promise.resolve({ data: mockData })),
+  })),
+}))
 
 describe('<NextBeneficiaryStep />', () => {
-  it('should show a loading page on init', async () => {
-    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-    const renderAPI = render(reactQueryProviderHOC(<NextBeneficiaryStep />))
-    await superFlushWithAct()
+  it('should show a loading page on init', () => {
+    const renderAPI = render(<NextBeneficiaryStep />)
     expect(renderAPI.getByTestId('Loading-Animation')).toBeTruthy()
   })
 })
