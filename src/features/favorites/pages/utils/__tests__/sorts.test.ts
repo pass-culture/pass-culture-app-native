26fcb9c884b23dc8564c9662f8e6d4d50a799613
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-09-23 16:51:48 +0200

    (PC-8434) clarify test on favorites

--- src/features/favorites/pages/utils/__tests__/sorts.test.ts
+++ src/features/favorites/pages/utils/__tests__/sorts.test.ts
@@ -1,244 +1,42 @@
-import { ExpenseDomain, FavoriteResponse } from 'api/gen'
+import { FavoriteResponse } from 'api/gen'
 import { paginatedFavoritesResponseSnap } from 'features/favorites/api/snaps/favorisResponseSnap'
 import { GeoCoordinates } from 'libs/geolocation'
 
 import { sortByIdDesc, sortByAscendingPrice, sortByDistanceAroundMe } from '../sorts'
 
+const baseOffer: FavoriteResponse = paginatedFavoritesResponseSnap.favorites[0]
+
 describe('Favorites sorts', () => {
   it('should sort by RECENTLY_ADDED (API default sort)', () => {
-    const data: Array<FavoriteResponse> = paginatedFavoritesResponseSnap.favorites
+    const data: Array<FavoriteResponse> = [
+      { ...baseOffer, id: 1 },
+      { ...baseOffer, id: 3 },
+      { ...baseOffer, id: 2 },
+    ]
     data.sort(sortByIdDesc)
-    expect(data).toEqual(paginatedFavoritesResponseSnap.favorites)
+    expect(data.map(({ id }) => id)).toEqual([3, 2, 1])
   })
 
   it('should sort by ASCENDING_PRICE', () => {
-    const data: Array<FavoriteResponse> = paginatedFavoritesResponseSnap.favorites
+    const data: Array<FavoriteResponse> = [
+      { ...baseOffer, id: 1, offer: { ...baseOffer.offer, price: 10 } },
+      { ...baseOffer, id: 3, offer: { ...baseOffer.offer, price: 30 } },
+      { ...baseOffer, id: 2, offer: { ...baseOffer.offer, price: 20 } },
+    ]
     data.sort(sortByAscendingPrice)
-    expect(data).toEqual([
-      {
-        id: 380,
-        offer: {
-          id: 146193,
-          name: 'Spectacle une seule date',
-          category: {
-            categoryType: 'Event',
-            label: 'Spectacle',
-            name: 'SPECTACLE',
-          },
-          expenseDomains: [ExpenseDomain.All],
-          externalTicketOfficeUrl: null,
-          image: null,
-          coordinates: {
-            latitude: 48.12108,
-            longitude: -1.17896,
-          },
-          price: 0,
-          startPrice: null,
-          date: new Date('2021-03-28T15:45:00.000Z'),
-          startDate: null,
-          isReleased: true,
-        },
-      },
-      {
-        id: 381,
-        offer: {
-          id: 146112,
-          name: 'Je ne sais pas ce que je dis',
-          category: {
-            categoryType: 'Event',
-            label: 'Pratique artistique',
-            name: 'LECON',
-          },
-          expenseDomains: [ExpenseDomain.All],
-          externalTicketOfficeUrl: null,
-          image: {
-            credit: null,
-            url:
-              'https://storage.gra.cloud.ovh.net/v1/AUTH_688df1e25bd84a48a3804e7fa8938085/storage-pc-dev/thumbs/mediations/CW8Q',
-          },
-          coordinates: {
-            latitude: 5.15839,
-            longitude: -52.63741,
-          },
-          price: null,
-          startPrice: 2400,
-          date: null,
-          startDate: new Date('2021-03-02T20:00:00.000Z'),
-          isReleased: true,
-        },
-      },
-      {
-        id: 393,
-        offer: {
-          id: 146105,
-          name: 'Un lit sous une rivière',
-          category: {
-            categoryType: 'Event',
-            label: 'Pratique artistique',
-            name: 'LECON',
-          },
-          expenseDomains: [ExpenseDomain.All],
-          externalTicketOfficeUrl: null,
-          image: {
-            credit: null,
-            url:
-              'https://storage.gra.cloud.ovh.net/v1/AUTH_688df1e25bd84a48a3804e7fa8938085/storage-pc-dev/thumbs/mediations/CWMA',
-          },
-          coordinates: {
-            latitude: 48.9263,
-            longitude: 2.49008,
-          },
-          price: null,
-          startPrice: 2700,
-          date: null,
-          startDate: new Date('2021-03-04T20:00:00.000Z'),
-          isReleased: true,
-        },
-      },
-      {
-        id: 389,
-        offer: {
-          id: 146099,
-          name: 'Un lit sous une rivière',
-          category: {
-            categoryType: 'Event',
-            label: 'Spectacle',
-            name: 'SPECTACLE',
-          },
-          expenseDomains: [ExpenseDomain.All],
-          externalTicketOfficeUrl: null,
-          image: {
-            credit: null,
-            url:
-              'https://storage.gra.cloud.ovh.net/v1/AUTH_688df1e25bd84a48a3804e7fa8938085/storage-pc-dev/thumbs/mediations/CWJQ',
-          },
-          coordinates: {
-            latitude: 48.94538,
-            longitude: 2.5029,
-          },
-          price: null,
-          startPrice: 2700,
-          date: null,
-          startDate: new Date('2021-03-04T20:00:00.000Z'),
-          isReleased: true,
-        },
-      },
-    ])
+    expect(data.map(({ id }) => id)).toEqual([1, 2, 3])
   })
 
   it('should sort by AROUND_ME', () => {
-    const data: Array<FavoriteResponse> = paginatedFavoritesResponseSnap.favorites
     const position = { latitude: 48.8584, longitude: 2.2945 } as GeoCoordinates
+    const position2 = { latitude: 49.8584, longitude: 3.2945 } as GeoCoordinates
+    const position3 = { latitude: 50.8584, longitude: 4.2945 } as GeoCoordinates
+    const data: Array<FavoriteResponse> = [
+      { ...baseOffer, id: 1, offer: { ...baseOffer.offer, coordinates: position } },
+      { ...baseOffer, id: 3, offer: { ...baseOffer.offer, coordinates: position3 } },
+      { ...baseOffer, id: 2, offer: { ...baseOffer.offer, coordinates: position2 } },
+    ]
     data.sort(sortByDistanceAroundMe(position))
-    expect(data).toEqual([
-      {
-        id: 393,
-        offer: {
-          id: 146105,
-          name: 'Un lit sous une rivière',
-          category: {
-            categoryType: 'Event',
-            label: 'Pratique artistique',
-            name: 'LECON',
-          },
-          expenseDomains: [ExpenseDomain.All],
-          externalTicketOfficeUrl: null,
-          image: {
-            credit: null,
-            url:
-              'https://storage.gra.cloud.ovh.net/v1/AUTH_688df1e25bd84a48a3804e7fa8938085/storage-pc-dev/thumbs/mediations/CWMA',
-          },
-          coordinates: {
-            latitude: 48.9263,
-            longitude: 2.49008,
-          },
-          price: null,
-          startPrice: 2700,
-          date: null,
-          startDate: new Date('2021-03-04T20:00:00.000Z'),
-          isReleased: true,
-        },
-      },
-      {
-        id: 389,
-        offer: {
-          id: 146099,
-          name: 'Un lit sous une rivière',
-          category: {
-            categoryType: 'Event',
-            label: 'Spectacle',
-            name: 'SPECTACLE',
-          },
-          expenseDomains: [ExpenseDomain.All],
-          externalTicketOfficeUrl: null,
-          image: {
-            credit: null,
-            url:
-              'https://storage.gra.cloud.ovh.net/v1/AUTH_688df1e25bd84a48a3804e7fa8938085/storage-pc-dev/thumbs/mediations/CWJQ',
-          },
-          coordinates: {
-            latitude: 48.94538,
-            longitude: 2.5029,
-          },
-          price: null,
-          startPrice: 2700,
-          date: null,
-          startDate: new Date('2021-03-04T20:00:00.000Z'),
-          isReleased: true,
-        },
-      },
-      {
-        id: 380,
-        offer: {
-          id: 146193,
-          name: 'Spectacle une seule date',
-          category: {
-            categoryType: 'Event',
-            label: 'Spectacle',
-            name: 'SPECTACLE',
-          },
-          expenseDomains: [ExpenseDomain.All],
-          externalTicketOfficeUrl: null,
-          image: null,
-          coordinates: {
-            latitude: 48.12108,
-            longitude: -1.17896,
-          },
-          price: 0,
-          startPrice: null,
-          date: new Date('2021-03-28T15:45:00.000Z'),
-          startDate: null,
-          isReleased: true,
-        },
-      },
-      {
-        id: 381,
-        offer: {
-          id: 146112,
-          name: 'Je ne sais pas ce que je dis',
-          category: {
-            categoryType: 'Event',
-            label: 'Pratique artistique',
-            name: 'LECON',
-          },
-          expenseDomains: [ExpenseDomain.All],
-          externalTicketOfficeUrl: null,
-          image: {
-            credit: null,
-            url:
-              'https://storage.gra.cloud.ovh.net/v1/AUTH_688df1e25bd84a48a3804e7fa8938085/storage-pc-dev/thumbs/mediations/CW8Q',
-          },
-          coordinates: {
-            latitude: 5.15839,
-            longitude: -52.63741,
-          },
-          price: null,
-          startPrice: 2400,
-          date: null,
-          startDate: new Date('2021-03-02T20:00:00.000Z'),
-          isReleased: true,
-        },
-      },
-    ])
+    expect(data.map(({ id }) => id)).toEqual([1, 2, 3])
   })
 })
