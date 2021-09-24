2453c1f92186491060a2ab86016f8aa9eb9a7e78
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-09-24 17:30:27 +0200

    (PC-8434) regenerate API client following backend update

--- src/libs/subcategories/useSubcategories.ts
+++ src/libs/subcategories/useSubcategories.ts
@@ -4,12 +4,12 @@ import { api } from 'api/api'
 import { SubcategoriesResponseModel } from 'api/gen'
 import { QueryKeys } from 'libs/queryKeys'
 
-import subcategories from './subcategories.json'
+import { placeholderData } from './placeholderData'
 
 const STALE_TIME_SUBCATEGORIES = 5 * 60 * 1000
 
 export const useSubcategories = () =>
   useQuery<SubcategoriesResponseModel>(QueryKeys.SUBCATEGORIES, api.getnativev1subcategories, {
     staleTime: STALE_TIME_SUBCATEGORIES,
-    placeholderData: subcategories as SubcategoriesResponseModel,
+    placeholderData,
   })
