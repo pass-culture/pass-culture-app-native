dfdfa74ad29cbc009bd4ff4e2d575833d99b92fe
Lucas Beneston <lucas.beneston@passculture.app>
2021-09-28 10:55:26 +0200

    (PC-10877) useFormatFullAddress : Display fullAddress whitout publicName or Name if showVenueBanner

--- src/libs/address/useFormatFullAddress.ts
+++ src/libs/address/useFormatFullAddress.ts
@@ -1,3 +1,5 @@
+import { env } from 'libs/environment'
+
 const isNotEmpty = (text: string | undefined | null) => text !== undefined && text !== ''
 
 export const formatFullAddress = (
@@ -5,26 +7,22 @@ export const formatFullAddress = (
   name: string,
   address: string | undefined | null,
   postalCode: string | undefined | null,
-  city: string | undefined | null
+  city: string | undefined | null,
+  showVenueBanner?: boolean | false
 ) => {
   let fullAddress = `${publicName || name}`
   if (isNotEmpty(address)) fullAddress = fullAddress.concat(`, ${address}`)
   if (isNotEmpty(postalCode) || isNotEmpty(city)) fullAddress = fullAddress.concat(',')
   if (isNotEmpty(postalCode)) fullAddress = fullAddress.concat(` ${postalCode}`)
   if (isNotEmpty(city)) fullAddress = fullAddress.concat(` ${city}`)
+  if (
+    isNotEmpty(address) &&
+    isNotEmpty(postalCode) &&
+    isNotEmpty(city) &&
+    // TODO (Lucasbeneston): Remove testing condition when display the link to venue button
+    env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING &&
+    showVenueBanner
+  )
+    fullAddress = `${address}, ${postalCode} ${city}`
   return fullAddress
 }
-
-// TODO (Lucasbeneston) : Add addressWithoutVenueName to formatFullAddress
-// const addressWithoutVenueName = t`${venue.address}, ${venue.postalCode} ${venue.city}`
-// const adaptedAddress =
-//   address && !showVenueBanner
-//     ? address
-//     : venue.address &&
-//       venue.postalCode &&
-//       venue.city &&
-//       // TODO : Remove testing condition when display the link to venue button
-//       env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING &&
-//       showVenueBanner
-//     ? addressWithoutVenueName
-//     : null
