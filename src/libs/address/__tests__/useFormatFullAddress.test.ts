7cae680c79484e91679070a94bceb503aeb7ac12
Lucas Beneston <lucas.beneston@passculture.app>
2021-09-28 11:34:18 +0200

    (PC-10877) Update tests for useFormatFullAddress

--- src/libs/address/__tests__/useFormatFullAddress.test.ts
+++ src/libs/address/__tests__/useFormatFullAddress.test.ts
@@ -2,33 +2,39 @@ import { formatFullAddress } from 'libs/address/useFormatFullAddress'
 
 describe('useOffer', () => {
   it.each`
-    publicName              | name            | address               | postalCode   | city         | expectedResult
-    ${undefined}            | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${'Paris'}   | ${'Ciné Pathé, 2 rue des champs, 75015 Paris'}
-    ${undefined}            | ${'Ciné Pathé'} | ${undefined}          | ${'75015'}   | ${'Paris'}   | ${'Ciné Pathé, 75015 Paris'}
-    ${undefined}            | ${'Ciné Pathé'} | ${undefined}          | ${undefined} | ${'Paris'}   | ${'Ciné Pathé, Paris'}
-    ${undefined}            | ${'Ciné Pathé'} | ${undefined}          | ${undefined} | ${undefined} | ${'Ciné Pathé'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${undefined}          | ${'75015'}   | ${'Paris'}   | ${'Pathé beaugrenelle, 75015 Paris'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${undefined}          | ${'75015'}   | ${undefined} | ${'Pathé beaugrenelle, 75015'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${undefined}          | ${undefined} | ${'Paris'}   | ${'Pathé beaugrenelle, Paris'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${undefined} | ${'Paris'}   | ${'Pathé beaugrenelle, 2 rue des champs, Paris'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${undefined} | ${undefined} | ${'Pathé beaugrenelle, 2 rue des champs'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${undefined} | ${'Pathé beaugrenelle, 2 rue des champs, 75015'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${undefined} | ${'Paris'}   | ${'Pathé beaugrenelle, 2 rue des champs, Paris'}
-    ${''}                   | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${'Paris'}   | ${'Ciné Pathé, 2 rue des champs, 75015 Paris'}
-    ${''}                   | ${'Ciné Pathé'} | ${''}                 | ${'75015'}   | ${'Paris'}   | ${'Ciné Pathé, 75015 Paris'}
-    ${''}                   | ${'Ciné Pathé'} | ${''}                 | ${''}        | ${'Paris'}   | ${'Ciné Pathé, Paris'}
-    ${''}                   | ${'Ciné Pathé'} | ${''}                 | ${''}        | ${''}        | ${'Ciné Pathé'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${''}                 | ${'75015'}   | ${'Paris'}   | ${'Pathé beaugrenelle, 75015 Paris'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${''}                 | ${'75015'}   | ${''}        | ${'Pathé beaugrenelle, 75015'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${''}                 | ${''}        | ${'Paris'}   | ${'Pathé beaugrenelle, Paris'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${''}        | ${'Paris'}   | ${'Pathé beaugrenelle, 2 rue des champs, Paris'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${''}        | ${''}        | ${'Pathé beaugrenelle, 2 rue des champs'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${''}        | ${'Pathé beaugrenelle, 2 rue des champs, 75015'}
-    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${''}        | ${'Paris'}   | ${'Pathé beaugrenelle, 2 rue des champs, Paris'}
+    publicName              | name            | address               | postalCode   | city         | showVenueBanner | expectedResult
+    ${undefined}            | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${'Paris'}   | ${false}        | ${'Ciné Pathé, 2 rue des champs, 75015 Paris'}
+    ${undefined}            | ${'Ciné Pathé'} | ${undefined}          | ${'75015'}   | ${'Paris'}   | ${false}        | ${'Ciné Pathé, 75015 Paris'}
+    ${undefined}            | ${'Ciné Pathé'} | ${undefined}          | ${undefined} | ${'Paris'}   | ${false}        | ${'Ciné Pathé, Paris'}
+    ${undefined}            | ${'Ciné Pathé'} | ${undefined}          | ${undefined} | ${undefined} | ${false}        | ${'Ciné Pathé'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${undefined}          | ${'75015'}   | ${'Paris'}   | ${false}        | ${'Pathé beaugrenelle, 75015 Paris'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${undefined}          | ${'75015'}   | ${undefined} | ${false}        | ${'Pathé beaugrenelle, 75015'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${undefined}          | ${undefined} | ${'Paris'}   | ${false}        | ${'Pathé beaugrenelle, Paris'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${undefined} | ${'Paris'}   | ${false}        | ${'Pathé beaugrenelle, 2 rue des champs, Paris'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${undefined} | ${undefined} | ${false}        | ${'Pathé beaugrenelle, 2 rue des champs'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${undefined} | ${false}        | ${'Pathé beaugrenelle, 2 rue des champs, 75015'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${undefined} | ${'Paris'}   | ${false}        | ${'Pathé beaugrenelle, 2 rue des champs, Paris'}
+    ${''}                   | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${'Paris'}   | ${false}        | ${'Ciné Pathé, 2 rue des champs, 75015 Paris'}
+    ${''}                   | ${'Ciné Pathé'} | ${''}                 | ${'75015'}   | ${'Paris'}   | ${false}        | ${'Ciné Pathé, 75015 Paris'}
+    ${''}                   | ${'Ciné Pathé'} | ${''}                 | ${''}        | ${'Paris'}   | ${false}        | ${'Ciné Pathé, Paris'}
+    ${''}                   | ${'Ciné Pathé'} | ${''}                 | ${''}        | ${''}        | ${false}        | ${'Ciné Pathé'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${''}                 | ${'75015'}   | ${'Paris'}   | ${false}        | ${'Pathé beaugrenelle, 75015 Paris'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${''}                 | ${'75015'}   | ${''}        | ${false}        | ${'Pathé beaugrenelle, 75015'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${''}                 | ${''}        | ${'Paris'}   | ${false}        | ${'Pathé beaugrenelle, Paris'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${''}        | ${'Paris'}   | ${false}        | ${'Pathé beaugrenelle, 2 rue des champs, Paris'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${''}        | ${''}        | ${false}        | ${'Pathé beaugrenelle, 2 rue des champs'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${''}        | ${false}        | ${'Pathé beaugrenelle, 2 rue des champs, 75015'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${''}        | ${'Paris'}   | ${false}        | ${'Pathé beaugrenelle, 2 rue des champs, Paris'}
+    ${'Pathé beaugrenelle'} | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${'Paris'}   | ${true}         | ${'2 rue des champs, 75015 Paris'}
+    ${'Pathé beaugrenelle'} | ${undefined}    | ${'2 rue des champs'} | ${'75015'}   | ${'Paris'}   | ${true}         | ${'2 rue des champs, 75015 Paris'}
+    ${undefined}            | ${'Ciné Pathé'} | ${'2 rue des champs'} | ${'75015'}   | ${'Paris'}   | ${true}         | ${'2 rue des champs, 75015 Paris'}
+    ${undefined}            | ${undefined}    | ${'2 rue des champs'} | ${'75015'}   | ${'Paris'}   | ${true}         | ${'2 rue des champs, 75015 Paris'}
   `(
     'should format correctly full address',
-    ({ publicName, name, address, postalCode, city, expectedResult }) => {
-      expect(formatFullAddress(publicName, name, address, postalCode, city)).toEqual(expectedResult)
+    ({ publicName, name, address, postalCode, city, showVenueBanner, expectedResult }) => {
+      expect(
+        formatFullAddress(publicName, name, address, postalCode, city, showVenueBanner)
+      ).toEqual(expectedResult)
     }
   )
 })
