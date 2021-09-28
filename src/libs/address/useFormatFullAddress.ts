6b4733a4211b451c4ec7ce42b703be7ae1cbe641
Lucas Beneston <lucas.beneston@passculture.app>
2021-09-28 15:53:37 +0200

    (PC-10877) Update conditions and tests for useFormatFullAddress

--- src/libs/address/useFormatFullAddress.ts
+++ src/libs/address/useFormatFullAddress.ts
@@ -15,14 +15,12 @@ export const formatFullAddress = (
   if (isNotEmpty(postalCode) || isNotEmpty(city)) fullAddress = fullAddress.concat(',')
   if (isNotEmpty(postalCode)) fullAddress = fullAddress.concat(` ${postalCode}`)
   if (isNotEmpty(city)) fullAddress = fullAddress.concat(` ${city}`)
-  if (
-    isNotEmpty(address) &&
-    isNotEmpty(postalCode) &&
-    isNotEmpty(city) &&
-    // TODO (Lucasbeneston): Remove testing condition when display the link to venue button
-    env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING &&
-    showVenueBanner
-  )
-    fullAddress = `${address}, ${postalCode} ${city}`
+  // TODO (Lucasbeneston): Remove testing condition when display the link to venue button
+  if (showVenueBanner && isNotEmpty(address) && env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING) {
+    if (isNotEmpty(postalCode)) fullAddress = `${address}, ${postalCode}`
+    if (isNotEmpty(city)) fullAddress = `${address}, ${city}`
+    if (isNotEmpty(postalCode) && isNotEmpty(city))
+      fullAddress = `${address}, ${postalCode} ${city}`
+  }
   return fullAddress
 }
