09c65d408c02f8583026ff833dcf75bde1d40e9e
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-09-29 20:10:59 +0200

    (Tech) Cleaning web test (useless for this release)

--- src/features/navigation/__tests__/RootNavigator.web.test.tsx
+++ src/features/navigation/__tests__/RootNavigator.web.test.tsx
@@ -1,19 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { mocked } from 'ts-jest/utils'
 import waitForExpect from 'wait-for-expect'
 
 import { AcceptCgu } from 'features/auth/signup/AcceptCgu'
 import { AccountCreated } from 'features/auth/signup/AccountCreated/AccountCreated'
-import { useMustUpdateApp } from 'features/forceUpdate/useMustUpdateApp'
 import * as splashScreenModule from 'libs/splashscreen'
 import { storage } from 'libs/storage'
 import { render } from 'tests/utils/web'
 
 import { RootNavigator, Route, wrapRoute } from '../RootNavigator'
 
-jest.mock('features/forceUpdate/useMustUpdateApp')
-const mockedUseMustUpdateApp = mocked(useMustUpdateApp)
 jest.mock('@react-navigation/native', () => jest.requireActual('@react-navigation/native'))
 jest.mock('features/auth/AuthContext', () => ({
   useAuthContext: jest.fn(() => ({ isLoggedIn: true })),
@@ -95,16 +91,6 @@ describe('wrapRoute()', () => {
     }
     wrapRoute(routeWithoutHoc)
     expect(hoc).not.toBeCalledWith(AccountCreated)
-  })
-
-  it('should display force update page when global variable is set', async () => {
-    await storage.saveObject('has_accepted_cookie', false)
-    mockedUseMustUpdateApp.mockReturnValueOnce(true)
-
-    const rootNavigator = await renderRootNavigator()
-
-    expect(rootNavigator).toMatchSnapshot()
-    expect(rootNavigator.queryAllByText("Mise à jour de l'application")).toBeTruthy()
   })
 })
 
