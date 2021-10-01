8aaddf7000261b744d4d132a70403df56a31b038
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-10-01 19:32:25 +0200

    (PC-11140) adapt test to new interface VenueHit

--- src/features/home/atoms/tests/VenueTile.native.test.tsx
+++ src/features/home/atoms/tests/VenueTile.native.test.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
 
 import { navigate } from '__mocks__/@react-navigation/native'
-import { VenueTypeCode } from 'api/gen'
 import { analytics } from 'libs/analytics'
 import { mockedSearchResponse } from 'libs/search/fixtures/mockedSearchResponse'
 import { fireEvent, render } from 'tests/utils'
 
-import { VenueTile } from '../VenueTile'
+import { VenueTile, VenueTileProps } from '../VenueTile'
 
 jest.mock('react-query')
 
 const venue = mockedSearchResponse.hits[0]
 
-const props = {
-  name: venue.name,
-  venueType: VenueTypeCode.MUSEUM,
-  venueId: Number(venue.id),
+const props: VenueTileProps = {
+  venue,
+  userPosition: null,
 }
 
 describe('VenueTile component', () => {
@@ -27,16 +25,14 @@ describe('VenueTile component', () => {
   it('should navigate to the venue when clicking on the venue tile', () => {
     const { getByTestId } = render(<VenueTile {...props} />)
     fireEvent.press(getByTestId('venueTile'))
-    expect(navigate).toHaveBeenCalledWith('Venue', {
-      id: props.venueId,
-    })
+    expect(navigate).toHaveBeenCalledWith('Venue', { id: venue.id })
   })
 
   it('should log analytics event ConsultVenue when pressing on the venue tile', () => {
     const { getByTestId } = render(<VenueTile {...props} />)
     fireEvent.press(getByTestId('venueTile'))
     expect(analytics.logConsultVenue).toHaveBeenNthCalledWith(1, {
-      venueId: props.venueId,
+      venueId: venue.id,
       from: 'home',
     })
   })
