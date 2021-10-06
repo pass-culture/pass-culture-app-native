73368379fb088b82da6fd59d3855734a9c1086a6
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-10-06 16:07:49 +0200

    :sparkles: (pc-10973) match popOverIcon string to Icon svg

--- src/features/profile/utils.test.ts
+++ src/features/profile/utils.test.ts
@@ -1,4 +1,7 @@
-import { computeCredit } from './utils'
+import { Clock } from 'ui/svg/icons/Clock'
+import { Info } from 'ui/svg/icons/Info'
+
+import { computeCredit, matchSubscriptionMessagePopOverIconToSvg } from './utils'
 
 const domainsCredit = {
   all: { initial: 50000, remaining: 40000 },
@@ -7,10 +10,26 @@ const domainsCredit = {
 }
 
 describe('profile utils', () => {
-  it('should compute credit', () => {
-    expect(computeCredit(domainsCredit)).toEqual(40000)
+  describe('Compute credit', () => {
+    it('should compute credit', () => {
+      expect(computeCredit(domainsCredit)).toEqual(40000)
+    })
+    it('should compute credit equal to zero when no domainsCredit', () => {
+      expect(computeCredit(null)).toEqual(0)
+    })
   })
-  it('should compute credit equal to zero when no domainsCredit', () => {
-    expect(computeCredit(null)).toEqual(0)
+  describe('Match PopOverIcon string to SVG', () => {
+    it('should return no icon if undefined is passed', () => {
+      const returnedIcon = matchSubscriptionMessagePopOverIconToSvg(undefined)
+      expect(returnedIcon).toEqual(undefined)
+    })
+    it("should return Clock if 'Clock' is passed", () => {
+      const returnedIcon = matchSubscriptionMessagePopOverIconToSvg('Clock')
+      expect(returnedIcon).toEqual(Clock)
+    })
+    it('should return Info if unknown string is passed', () => {
+      const returnedIcon = matchSubscriptionMessagePopOverIconToSvg('I am an unknown string')
+      expect(returnedIcon).toEqual(Info)
+    })
   })
 })
