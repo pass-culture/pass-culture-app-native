086badd4820b515ecbabb5e74f780bc5e413b295
Marion Barré <marionb@bam.tech>
2021-10-06 11:40:23 +0200

    :sparkles: (availablecategories) enable some "JEU" subcate to underage

--- src/features/search/utils/useAvailableCategories.ts
+++ src/features/search/utils/useAvailableCategories.ts
@@ -1,12 +1,8 @@
 import { omit } from 'lodash'
 
 import { SearchGroupNameEnum } from 'api/gen'
-import { useIsUserUnderageBeneficiary } from 'features/profile/utils'
 import { CategoryCriteria, CATEGORY_CRITERIA } from 'features/search/enums'
 
 export const useAvailableCategories = (): Partial<CategoryCriteria> => {
-  const isUserUnderageBeneficiary = useIsUserUnderageBeneficiary()
-  return isUserUnderageBeneficiary
-    ? omit(CATEGORY_CRITERIA, [SearchGroupNameEnum.NONE, SearchGroupNameEnum.JEU])
-    : omit(CATEGORY_CRITERIA, [SearchGroupNameEnum.NONE])
+  return omit(CATEGORY_CRITERIA, [SearchGroupNameEnum.NONE])
 }
