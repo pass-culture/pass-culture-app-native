0d5bdb3768d9af245edab8af9a788001176c1b34
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-10-07 17:12:47 +0200

    (PC-11058) App Native - Decli Web - Template de pages informatives

--- src/features/navigation/RedirectWebToNative.tsx
+++ src/features/navigation/RedirectWebToNative.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import styled from 'styled-components/native'
 
 import { openExternalUrl } from 'features/navigation/helpers'
-import { Helmet } from 'libs/react-helmet/Helmet'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { ExternalLink } from 'ui/components/buttons/externalLink/ExternalLink'
 import { GenericInfoPage } from 'ui/components/GenericInfoPage'
@@ -23,47 +22,42 @@ export const RedirectWebToNative: React.FC = () => {
     openExternalUrl(url)
   }
   return (
-    <React.Fragment>
-      <Helmet>
-        <meta name="robots" content="noindex" />
-      </Helmet>
-      <GenericInfoPage
-        title={t`Ce lien fonctionne uniquement sur notre application mobile !`}
-        icon={PageNotFoundIcon}
-        iconSize={getSpacing(25)}>
-        <Spacer.Column numberOfSpaces={2} />
-        <StyledBody>{t`1. Tu peux installer l'application :`}</StyledBody>
-        <Spacer.Column numberOfSpaces={2} />
-        <Row>
-          <ExternalLink
-            color={ColorsEnum.WHITE}
-            text={t`PlayStore`}
-            url="https://play.google.com/store/apps/details?id=app.passculture.webapp&hl=fr"
-          />
-          <Spacer.Row numberOfSpaces={6} />
-          <ExternalLink
-            color={ColorsEnum.WHITE}
-            text={t`AppStore`}
-            url="https://apps.apple.com/fr/app/pass-culture/id1557887412"
-          />
-        </Row>
-        <Spacer.Column numberOfSpaces={6} />
-        <StyledBody>{t`2. Puis réessayer d'ouvrir ce lien sur le navigateur de ton téléphone :`}</StyledBody>
-        <Spacer.Column numberOfSpaces={4} />
-        <ButtonPrimary title={t`Ouvrir le lien`} onPress={onPressOpenLink} />
-        <Spacer.Column numberOfSpaces={8} />
-        <InlineTextGroup>
-          <StyledBody>{t`Si tu rencontres à nouveau des difficultés, consulte`}</StyledBody>
-          <Spacer.Row numberOfSpaces={1} />
-          <ExternalLink
-            color={ColorsEnum.WHITE}
-            text={t`notre centre d'aide`}
-            url="https://aide.passculture.app/fr/articles/5260446-j-ai-un-probleme-avec-les-liens-sur-mon-application"
-          />
-        </InlineTextGroup>
-        <LogoPassCulture width={getSpacing(35)} height={getSpacing(25)} />
-      </GenericInfoPage>
-    </React.Fragment>
+    <GenericInfoPage
+      title={t`Ce lien fonctionne uniquement sur notre application mobile !`}
+      icon={PageNotFoundIcon}
+      iconSize={getSpacing(25)}>
+      <Spacer.Column numberOfSpaces={2} />
+      <StyledBody>{t`1. Tu peux installer l'application :`}</StyledBody>
+      <Spacer.Column numberOfSpaces={2} />
+      <Row>
+        <ExternalLink
+          color={ColorsEnum.WHITE}
+          text={t`PlayStore`}
+          url="https://play.google.com/store/apps/details?id=app.passculture.webapp&hl=fr"
+        />
+        <Spacer.Row numberOfSpaces={6} />
+        <ExternalLink
+          color={ColorsEnum.WHITE}
+          text={t`AppStore`}
+          url="https://apps.apple.com/fr/app/pass-culture/id1557887412"
+        />
+      </Row>
+      <Spacer.Column numberOfSpaces={6} />
+      <StyledBody>{t`2. Puis réessayer d'ouvrir ce lien sur le navigateur de ton téléphone :`}</StyledBody>
+      <Spacer.Column numberOfSpaces={4} />
+      <ButtonPrimary title={t`Ouvrir le lien`} onPress={onPressOpenLink} />
+      <Spacer.Column numberOfSpaces={8} />
+      <InlineTextGroup>
+        <StyledBody>{t`Si tu rencontres à nouveau des difficultés, consulte`}</StyledBody>
+        <Spacer.Row numberOfSpaces={1} />
+        <ExternalLink
+          color={ColorsEnum.WHITE}
+          text={t`notre centre d'aide`}
+          url="https://aide.passculture.app/fr/articles/5260446-j-ai-un-probleme-avec-les-liens-sur-mon-application"
+        />
+      </InlineTextGroup>
+      <LogoPassCulture width={getSpacing(35)} height={getSpacing(25)} />
+    </GenericInfoPage>
   )
 }
 
