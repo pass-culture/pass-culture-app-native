ac5363c2a3713a6300d8aa01f5412d55c142c044
Emma Jamin <emmajamin@orange.fr>
2021-10-08 16:03:19 +0200

    (PC-10909) Ask review to user 3 seconds after render

--- src/features/bookOffer/pages/__tests__/BookingConfirmation.test.tsx
+++ src/features/bookOffer/pages/__tests__/BookingConfirmation.test.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import waitForExpect from 'wait-for-expect'
 
 import { reset, useRoute } from '__mocks__/@react-navigation/native'
+import reactNativeInAppReview from '__mocks__/react-native-in-app-review'
 import { analytics } from 'libs/analytics'
-import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { env } from 'libs/environment'
 import { fireEvent, render } from 'tests/utils'
 
 import { BookingConfirmation } from '../BookingConfirmation'
@@ -11,6 +12,10 @@ import { BookingConfirmation } from '../BookingConfirmation'
 jest.mock('features/home/services/useAvailableCredit', () => ({
   useAvailableCredit: jest.fn(() => ({ isExpired: false, amount: 2000 })),
 }))
+
+jest.mock('react-query')
+
+const isAvailable = jest.spyOn(reactNativeInAppReview, 'isAvailable').mockImplementation(() => true)
 
 describe('<BookingConfirmation />', () => {
   const mockOfferId = 1337
@@ -25,14 +30,12 @@ describe('<BookingConfirmation />', () => {
   afterEach(jest.clearAllMocks)
 
   it('should render correctly', () => {
-    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-    const page = render(reactQueryProviderHOC(<BookingConfirmation />))
+    const page = render(<BookingConfirmation />)
     expect(page).toMatchSnapshot()
   })
 
   it('should go to Bookings and log analytics event', async () => {
-    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-    const renderAPI = render(reactQueryProviderHOC(<BookingConfirmation />))
+    const renderAPI = render(<BookingConfirmation />)
     fireEvent.press(renderAPI.getByText('Voir ma réservation'))
     await waitForExpect(() => {
       expect(analytics.logSeeMyBooking).toBeCalledWith(mockOfferId)
@@ -55,5 +58,25 @@ describe('<BookingConfirmation />', () => {
         ],
       })
     })
+  })
+
+  it('should call InAppReview Modal after 3 seconds', async () => {
+    env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING = true
+    const requestInAppReview = jest.spyOn(reactNativeInAppReview, 'RequestInAppReview')
+    jest.useFakeTimers()
+
+    render(<BookingConfirmation />)
+    jest.advanceTimersByTime(3000)
+    expect(requestInAppReview).toHaveBeenCalled()
+  })
+  it('should not call InAppReview Modal if isAvailable is false', async () => {
+    env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING = true
+    isAvailable.mockImplementationOnce(() => false)
+    const requestInAppReview = jest.spyOn(reactNativeInAppReview, 'RequestInAppReview')
+    jest.useFakeTimers()
+
+    render(<BookingConfirmation />)
+    jest.advanceTimersByTime(3000)
+    expect(requestInAppReview).toHaveBeenCalledTimes(0)
   })
 })
