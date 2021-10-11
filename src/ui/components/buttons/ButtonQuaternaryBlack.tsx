62039ae881d5a9c9e847c596bd6696f5b6e2d791
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-10-11 16:41:42 +0200

    :lipstick: (pc-10974) add ButtonQuaternaryBlack

---
+++ src/ui/components/buttons/ButtonQuaternaryBlack.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent } from 'react'
+
+import { AppButton, BaseButtonProps } from 'ui/components/buttons/AppButton'
+import { ColorsEnum, getSpacing } from 'ui/theme'
+
+export const ButtonQuaternaryBlack: FunctionComponent<BaseButtonProps> = (props) => {
+  let textColor = ColorsEnum.BLACK
+  const backgroundColor = ColorsEnum.TRANSPARENT
+  const loadingIconColor = ColorsEnum.PRIMARY_DARK
+  let iconColor = ColorsEnum.BLACK
+
+  if (props.disabled) {
+    textColor = iconColor = ColorsEnum.GREY_DARK
+  }
+
+  return (
+    <AppButton
+      {...props}
+      loadingIconColor={loadingIconColor}
+      backgroundColor={backgroundColor}
+      iconColor={iconColor}
+      textColor={textColor}
+      iconSize={getSpacing(6)}
+      textSize={getSpacing(3)}
+    />
+  )
+}
