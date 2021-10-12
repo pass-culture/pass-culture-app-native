d9f4255a13bf77f8b35dd6227a43dda611ea8d92
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-10-12 10:46:51 +0200

    :sparkles: (pc-10974) add call to action button and icon (ui)

--- src/features/profile/components/ProfileBadge.native.test.tsx
+++ src/features/profile/components/ProfileBadge.native.test.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { ProfileBadge } from 'features/profile/components/ProfileBadge'
 import { render } from 'tests/utils'
 import { Clock } from 'ui/svg/icons/Clock'
+import { EmailFilled } from 'ui/svg/icons/EmailFilled'
 
 describe('ProfileBadge', () => {
   it('should render component correctly if no icon is provided', () => {
@@ -12,6 +13,27 @@ describe('ProfileBadge', () => {
   it('should render component correctly if icon is provided', () => {
     const myComponent = render(
       <ProfileBadge message={'Tu as déposé ton dossier. Bravo'} icon={Clock} />
+    )
+    expect(myComponent).toMatchSnapshot()
+  })
+  it('should show neither CTA Icon nor CTA message if no CTA message is provided', () => {
+    const myComponent = render(
+      <ProfileBadge
+        message={'Tu as déposé ton dossier. Bravo'}
+        icon={Clock}
+        callToActionIcon={EmailFilled}
+      />
+    )
+    expect(myComponent).toMatchSnapshot()
+  })
+  it('should show CTA Icon and CTA message if both are provided', () => {
+    const myComponent = render(
+      <ProfileBadge
+        message={'Tu as déposé ton dossier. Bravo'}
+        icon={Clock}
+        callToActionIcon={EmailFilled}
+        callToActionMessage={'Tu peux cliquer ici'}
+      />
     )
     expect(myComponent).toMatchSnapshot()
   })
--- src/features/profile/components/ProfileBadge.tsx
+++ src/features/profile/components/ProfileBadge.tsx
@@ -1,12 +1,15 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 
+import { ButtonQuaternaryBlack } from 'ui/components/buttons/ButtonQuaternaryBlack'
 import { IconInterface } from 'ui/svg/icons/types'
-import { ColorsEnum, getSpacing, Typo } from 'ui/theme'
+import { ColorsEnum, getSpacing, Typo, Spacer } from 'ui/theme'
 
 interface ProfileBadgeProps {
   message: string
   icon?: FunctionComponent<IconInterface>
+  callToActionIcon?: FunctionComponent<IconInterface>
+  callToActionMessage?: string
   testID?: string
 }
 
@@ -20,7 +23,23 @@ export function ProfileBadge(props: ProfileBadgeProps) {
         </IconContainer>
       ) : null}
       <TextContainer>
-        <Typo.Caption>{props.message}</Typo.Caption>
+        <Typo.Caption color={props.callToActionMessage ? ColorsEnum.GREY_DARK : ColorsEnum.BLACK}>
+          {props.message}
+        </Typo.Caption>
+        {!!props.callToActionMessage && (
+          <React.Fragment>
+            <Spacer.Column numberOfSpaces={4} />
+            <CallToActionContainer>
+              <ButtonQuaternaryBlack
+                inline
+                icon={props.callToActionIcon}
+                testId="call-to-action-button"
+                onPress={() => null}
+                title={props.callToActionMessage}
+              />
+            </CallToActionContainer>
+          </React.Fragment>
+        )}
       </TextContainer>
     </Container>
   )
@@ -41,4 +60,8 @@ const IconContainer = styled.View({
 
 const TextContainer = styled.View({
   flex: 1,
+})
+
+const CallToActionContainer = styled.View({
+  flexDirection: 'row',
 })
