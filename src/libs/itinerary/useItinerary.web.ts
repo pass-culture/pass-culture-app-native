1524adce1cf68812a4c9a21e691c36a835f7a661
BenStirrup <benjamin.stirrup@gmail.com>
2021-10-13 11:06:55 +0200

    (PC-9621) useItinerary() for web

--- src/libs/itinerary/useItinerary.web.ts
+++ src/libs/itinerary/useItinerary.web.ts
@@ -1,11 +1,23 @@
-import { useEffect, useState } from 'react'
+import { isIOS } from 'react-device-detect'
+
+import { Coordinates } from 'api/gen'
+import { openExternalUrl } from 'features/navigation/helpers'
 
 export const useItinerary = () => {
-  const [availableApps] = useState<undefined>(undefined)
-  const getApps = async () => true
-  const navigateTo = () => true
-  useEffect(() => {
-    getApps()
-  }, [])
-  return { availableApps, navigateTo }
+  // To accomodate the API of useOpenItinerary, we have to pass
+  // availableApps as a non-empty array.
+  // Indeed, in web, we know for sure we always have the option
+  // to open the web version of Google maps.
+  return { availableApps: ['google_maps'], navigateTo }
+}
+
+function navigateTo(coordinates: Required<Coordinates>) {
+  const { latitude: lat, longitude: lon } = coordinates
+  const googleMapsPath = `maps.google.com/maps?daddr=${lat},${lon}`
+  if (isIOS) {
+    /* on iOS, the device will try to open the link first in Apple Maps, then in Google Maps */
+    openExternalUrl('maps://' + googleMapsPath)
+  } else {
+    openExternalUrl('https://' + googleMapsPath)
+  }
 }
