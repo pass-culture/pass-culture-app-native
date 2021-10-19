17630b1180d5ab1a6770152ee7d95458c86c5e11
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-10-19 16:11:03 +0200

    (Tech) remove offer.category from algolia facets

--- src/libs/algolia/enums/facetsEnums.ts
+++ src/libs/algolia/enums/facetsEnums.ts
@@ -1,6 +1,5 @@
 // Taken from https://www.algolia.com/apps/E2IKXJ325N/explorer/configuration/PRODUCTION/facets
 export enum FACETS_ENUM {
-  OFFER_CATEGORY = 'offer.category',
   OFFER_DATES = 'offer.dates',
   OFFER_IS_DIGITAL = 'offer.isDigital',
   OFFER_IS_DUO = 'offer.isDuo',
