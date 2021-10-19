3d9d40085dba41217423ea5a74ccbc0580c58493
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-10-19 10:49:05 +0200

    :sparkles: (pc-11285) add handleCallToActionLink to ProfileBadge

--- src/features/profile/__mocks__/utils.ts
+++ src/features/profile/__mocks__/utils.ts
@@ -1,1 +1,3 @@
 export const isUserExBeneficiary = jest.fn().mockReturnValue(false)
+
+export const handleCallToActionLink = jest.fn()
--- src/features/profile/components/ProfileBadge.native.test.tsx
+++ src/features/profile/components/ProfileBadge.native.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 import { ProfileBadge } from 'features/profile/components/ProfileBadge'
-import { render } from 'tests/utils'
+import { handleCallToActionLink } from 'features/profile/utils'
+import { render, fireEvent } from 'tests/utils'
 import { Clock } from 'ui/svg/icons/Clock'
 import { EmailFilled } from 'ui/svg/icons/EmailFilled'
+
+jest.mock('features/profile/utils')
 
 describe('ProfileBadge', () => {
   it('should render component correctly if no icon is provided', () => {
@@ -26,7 +29,19 @@ describe('ProfileBadge', () => {
     )
     expect(myComponent).toMatchSnapshot()
   })
-  it('should show CTA Icon and CTA message if both are provided', () => {
+  it('should show CTA Icon and CTA message if Icon, Message and Link are provided', () => {
+    const myComponent = render(
+      <ProfileBadge
+        message={'Tu as déposé ton dossier. Bravo'}
+        icon={Clock}
+        callToActionIcon={EmailFilled}
+        callToActionMessage={'Tu peux cliquer ici'}
+        callToActionLink={'https://calltoaction.com'}
+      />
+    )
+    expect(myComponent).toMatchSnapshot()
+  })
+  it('should not show CTA if no link is provided', () => {
     const myComponent = render(
       <ProfileBadge
         message={'Tu as déposé ton dossier. Bravo'}
@@ -36,5 +51,19 @@ describe('ProfileBadge', () => {
       />
     )
     expect(myComponent).toMatchSnapshot()
+  })
+  it('should call handleCallToActionLink with url onPress', () => {
+    const { getByTestId } = render(
+      <ProfileBadge
+        message={'Tu as déposé ton dossier. Bravo'}
+        icon={Clock}
+        callToActionIcon={EmailFilled}
+        callToActionLink={'https://google.com'}
+        callToActionMessage={'Tu peux cliquer ici'}
+      />
+    )
+    const CTAButton = getByTestId('call-to-action-button')
+    fireEvent.press(CTAButton)
+    expect(handleCallToActionLink).toHaveBeenCalledWith('https://google.com')
   })
 })
