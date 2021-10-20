6ea5c7b84946e25f4b6fed935bb42e5af321dbd0
Lucas Beneston <lucas.beneston@passculture.app>
2021-10-20 17:04:12 +0200

    (PC-10893) Set first time review has been requested date and stock in storage

--- src/features/bookOffer/services/useReviewInAppInformation.ts
+++ src/features/bookOffer/services/useReviewInAppInformation.ts
@@ -5,29 +5,20 @@ import { storage } from 'libs/storage'
 export const useReviewInAppInformation = () => {
   const [timesReviewHasBeenRequested, setTimesReviewHasBeenRequested] = useState(0)
   const [shouldReviewBeRequested, setShouldReviewBeRequested] = useState(false)
-  const [firstTimeReviewHasBeenRequested, setFirstTimeReviewHasBeenRequested] = useState('')
-  console.log({ firstTimeReviewHasBeenRequested })
 
   useEffect(() => {
-    // Times review has been requested
-    storage.readObject('times_review_has_been_requested').then((timesReviewHasBeenRequested) => {
-      setTimesReviewHasBeenRequested(timesReviewHasBeenRequested as number)
-      setShouldReviewBeRequested((timesReviewHasBeenRequested as number) < 4)
-    })
-
-    // Firt time review has been requested date
-    storage.readObject('first_time_review_has_been_requested').then((value) => {
-      setFirstTimeReviewHasBeenRequested(value as string)
-      if (timesReviewHasBeenRequested === 1) {
-        setFirstTimeReviewHasBeenRequested('First time !')
-      }
-    })
+    storage
+      .readObject('times_review_has_been_requested')
+      .then((storageTimesReviewHasBeenRequested) => {
+        setTimesReviewHasBeenRequested(storageTimesReviewHasBeenRequested as number)
+        setShouldReviewBeRequested((storageTimesReviewHasBeenRequested as number) < 4)
+        if ((storageTimesReviewHasBeenRequested as number) === 1) {
+          storage.saveObject('first_time_review_has_been_requested', Date.now())
+        }
+      })
   }, [])
 
   const updateInformationWhenReviewHasBeenRequested = (hasFlowFinishedSuccessfully: boolean) => {
-    // TODO (LucasBeneston) : cheat for reset timesReviewHasBeenRequested in storage.
-    // storage.saveObject('times_review_has_been_requested', 0)
-
     if (hasFlowFinishedSuccessfully)
       storage.saveObject('times_review_has_been_requested', timesReviewHasBeenRequested + 1)
   }
