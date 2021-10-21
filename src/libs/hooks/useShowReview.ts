83a52f9a9ee4484f7b12e70442a5f953d7406da9
Lucas Beneston <lucas.beneston@passculture.app>
2021-10-21 16:56:40 +0200

    (PC-10893) Add useShowReview hook

---
+++ src/libs/hooks/useShowReview.ts
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import InAppReview from 'react-native-in-app-review'
+
+import { useReviewInAppInformation } from 'features/bookOffer/services/useReviewInAppInformation'
+import { eventMonitoring } from 'libs/monitoring'
+
+export const useShowReview = () => {
+  const {
+    shouldReviewBeRequested,
+    updateInformationWhenReviewHasBeenRequested,
+  } = useReviewInAppInformation()
+
+  useEffect(() => {
+    if (InAppReview.isAvailable() && shouldReviewBeRequested) {
+      setTimeout(
+        () =>
+          InAppReview.RequestInAppReview()
+            .then((hasFlowFinishedSuccessfully) => {
+              if (hasFlowFinishedSuccessfully) updateInformationWhenReviewHasBeenRequested()
+            })
+            .catch((error) => {
+              eventMonitoring.captureException(error)
+            }),
+        3000
+      )
+    }
+  }, [shouldReviewBeRequested])
+}
