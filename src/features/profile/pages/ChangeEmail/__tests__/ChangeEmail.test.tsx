dfce7dcdba68734442e216e9d7668ca5d07b0485
Lucas Beneston <lucas.beneston@passculture.app>
2021-10-25 15:42:14 +0200

    (PC-11361) ChangeEmail : Add test for submit button + password lenght >= 12

--- src/features/profile/pages/ChangeEmail/__tests__/ChangeEmail.test.tsx
+++ src/features/profile/pages/ChangeEmail/__tests__/ChangeEmail.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import waitForExpect from 'wait-for-expect'
 
-import { render } from 'tests/utils'
+import { fireEvent, render } from 'tests/utils'
+import { ColorsEnum } from 'ui/theme'
 
 import { ChangeEmail } from '../ChangeEmail'
 
@@ -8,5 +10,19 @@ describe('<ChangeEmail/>', () => {
   it('should render correctly', () => {
     const renderAPI = render(<ChangeEmail />)
     expect(renderAPI.toJSON()).toMatchSnapshot()
+  })
+
+  it('should enable the submit button when passwords length is more or equal than 12', async () => {
+    const { getByPlaceholderText, getByTestId } = await render(<ChangeEmail />)
+    const submitButton = getByTestId('Enregistrer')
+    const background = submitButton.props.style.backgroundColor
+    expect(background).toEqual(ColorsEnum.GREY_LIGHT)
+
+    const passwordInput = getByPlaceholderText('Ton mot de passe')
+    fireEvent.changeText(passwordInput, 'user@AZERTY123')
+    await waitForExpect(() => {
+      const background = submitButton.props.style.backgroundColor
+      expect(background).toEqual(ColorsEnum.PRIMARY)
+    })
   })
 })
