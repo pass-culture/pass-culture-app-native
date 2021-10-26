dc8d8c3fa9b67c6f91a4a1a3ebdd3135d738878d
Lucas Beneston <lucas.beneston@passculture.app>
2021-10-26 15:45:20 +0200

    (PC-11345) Update snapshots

--- src/features/profile/pages/ChangeEmail/__tests__/ChangeEmail.web.test.tsx
+++ src/features/profile/pages/ChangeEmail/__tests__/ChangeEmail.web.test.tsx
@@ -4,6 +4,8 @@ import { render } from 'tests/utils/web'
 
 import { ChangeEmail } from '../ChangeEmail'
 
+jest.mock('react-query')
+
 describe('<ChangeEmail/>', () => {
   it('should render correctly', () => {
     const renderAPI = render(<ChangeEmail />)
