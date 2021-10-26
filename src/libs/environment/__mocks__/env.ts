934dddadbbc789e65e04f781b19efcd2dfb5df63
BenStirrup <benjamin.stirrup@gmail.com>
2021-10-26 16:39:33 +0200

    (PC-10731) Add sendinblue email domain as associated domain

--- src/libs/environment/__mocks__/env.ts
+++ src/libs/environment/__mocks__/env.ts
@@ -1,1 +1,4 @@
-export { env } from './envFixtures'
+import { env as envFixtures } from './envFixtures'
+
+export const env = envFixtures
+export const EMAIL_PROVIDER_CUSTOM_URL = `https://${env.EMAIL_PROVIDER_CUSTOM_DOMAIN}`
