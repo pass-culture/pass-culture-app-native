9612ed6e7b11d904856583ba07ed81b8f61fdd7b
Lucas Beneston <lucas.beneston@passculture.app>
2021-10-26 18:38:33 +0200

    (PC-11345) useValidEmail : Add tests

---
+++ src/features/profile/pages/ChangeEmail/utils/useValidateEmail.test.ts
@@ -0,0 +1,36 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import { useValidateEmail } from './useValidateEmail'
+
+const currentUserEmail = 'current@gmail.com'
+const newUserEmail = 'new@gmail.com'
+const invalidNewUserEmail = 'new@invaild'
+
+jest.mock('features/home/api', () => ({
+  useUserProfileInfo: jest.fn(() => ({ data: { email: currentUserEmail } })),
+}))
+
+describe('useValidateEmail function', () => {
+  beforeEach(jest.clearAllMocks)
+
+  it('should not return an error message if the new email is valid', () => {
+    const { result } = renderHook(() => useValidateEmail(newUserEmail))
+    expect(result.current).toEqual({ hasError: false, message: '' })
+  })
+
+  it('should return an error message if the new email is invalid', () => {
+    const { result } = renderHook(() => useValidateEmail(invalidNewUserEmail))
+    expect(result.current).toEqual({
+      hasError: true,
+      message: "Format de l'e-mail incorrect",
+    })
+  })
+
+  it('should return an error message if the new email is the same than the new one ', () => {
+    const { result } = renderHook(() => useValidateEmail(currentUserEmail))
+    expect(result.current).toEqual({
+      hasError: true,
+      message: "L'e-mail saisi est identique a votre e-mail actuel",
+    })
+  })
+})
