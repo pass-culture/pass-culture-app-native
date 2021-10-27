7583ee16f84a9a61c66b8a27df76675f46aaed37
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-10-27 13:32:17 +0200

    (tech) rename file useAvailableCategories => availableCategories

--- src/features/search/utils/tests/availableCategories.test.ts
+++ src/features/search/utils/tests/availableCategories.test.ts
@@ -2,7 +2,7 @@ import { omit } from 'lodash'
 
 import { SearchGroupNameEnum } from 'api/gen'
 import { CATEGORY_CRITERIA } from 'features/search/enums'
-import { availableCategories } from 'features/search/utils/useAvailableCategories'
+import { availableCategories } from 'features/search/utils/availableCategories'
 
 describe('availableCategories', () => {
   it('should return all searchable categories from CATEGORY_CRITERIA - without NONE', () => {
