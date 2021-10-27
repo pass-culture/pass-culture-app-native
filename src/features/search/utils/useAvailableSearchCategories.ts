1661fb649a51ca7a5f3573749364edf72506dd6b
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-10-27 12:23:08 +0200

    (Tech) remove duplicate hook useAvailableSearchCategories

--- src/features/search/utils/useAvailableSearchCategories.ts
+++
@@ -1,9 +0,0 @@
-import { omit } from 'lodash'
-
-import { SearchGroupNameEnum } from 'api/gen'
-import { useAvailableCategories } from 'features/search/utils/useAvailableCategories'
-
-export const useAvailableSearchCategories = () => {
-  const availableCategories = useAvailableCategories()
-  return omit(availableCategories, SearchGroupNameEnum.NONE)
-}
