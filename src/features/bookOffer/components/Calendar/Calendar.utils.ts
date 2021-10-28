520a7c2f9d481ae391e9a6ac2d7ab5334e69073e
BenStirrup <benjamin.stirrup@gmail.com>
2021-10-28 13:39:37 +0200

    (PC-11558) Fix CalendarPicker on web not displaying correct list of dates

--- src/features/bookOffer/components/Calendar/Calendar.utils.ts
+++ src/features/bookOffer/components/Calendar/Calendar.utils.ts
@@ -1,3 +1,5 @@
+import range from 'lodash/range'
+
 export const monthNames = [
   'Janvier',
   'Février',
@@ -30,16 +32,22 @@ export const dayNames = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Ven
 export const dayNamesShort = ['D', 'L', 'M', 'M', 'J', 'V', 'S']
 export const today = "Aujourd'hui"
 
-export const isLeapYear = (year: number) => (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
-export const generateDays = (activeMonth: number, activeYear: number) => {
-  let limit = 31
-  if (activeMonth % 2 === 0) {
-    limit = activeMonth === 2 ? (isLeapYear(activeYear) ? 29 : 28) : 30
-  }
-  return Array.from(new Array(limit)).map((item, index) => index + 1)
-}
-
-export const CURRENT_YEAR = new Date().getFullYear()
+const EARLIEST_YEAR_AVAILABLE = new Date().getFullYear() - 50
 export const YEAR_LIST = Array.from(new Array(100)).map(
-  (item, index) => CURRENT_YEAR + (index - 50)
+  (item, index) => EARLIEST_YEAR_AVAILABLE + index
 )
+
+/**
+ * Returns a list of dates such as [1, 2, ..., 31].
+ * Example :
+ * ```ts
+ * getListOfDatesInMonth(1, 2009) // february 2009 --> [1, 2, 3, ..., 27, 28]
+ * ```
+ * @param monthIndex Integer value representing the month, beginning with 0 for January to 11 for December
+ * @param year Integer value representing the year
+ */
+export function getListOfDatesInMonth(monthIndex: number, year: number) {
+  const nextMonthIndex = monthIndex + 1
+  const nbOfDaysInMonth = new Date(year, nextMonthIndex, 0).getDate()
+  return range(1, nbOfDaysInMonth + 1)
+}
---
+++ src/features/bookOffer/components/__tests__/Calendar.utils.test.ts
@@ -0,0 +1,22 @@
+import range from 'lodash/range'
+import timezoneMock, { TimeZone } from 'timezone-mock'
+
+import { getListOfDatesInMonth } from 'features/bookOffer/components/Calendar/Calendar.utils'
+
+describe('getListOfDatesInMonth()', () => {
+  it.each`
+    timezone
+    ${'Australia/Adelaide'}
+    ${'Europe/London'}
+    ${'Brazil/East'}
+  `(
+    'should return a list [1, 2, ..., N] for some specified month and year with timezone $timezone',
+    ({ timezone }: { timezone: TimeZone }) => {
+      timezoneMock.register(timezone)
+      expect(getListOfDatesInMonth(9, 2021)).toEqual(range(1, 31 + 1)) // october 2021 has 31 days
+      expect(getListOfDatesInMonth(10, 2021)).toEqual(range(1, 30 + 1)) // november 2021 has 30 days
+      expect(getListOfDatesInMonth(1, 2009)).toEqual(range(1, 28 + 1)) // february 2009 has 28 days
+      expect(getListOfDatesInMonth(1, 2008)).toEqual(range(1, 29 + 1)) // february 2008 has 29 days
+    }
+  )
+})
