0e2e733510fbe1a6af24b8a17f67002149240e44
Lucas Beneston <lucas.beneston@passculture.app>
2021-10-29 18:02:41 +0200

    refacto(PC-11584) ResetPasswordExpiredLink : Use LayoutExpiredLink

--- src/features/auth/forgottenPassword/ResetPasswordExpiredLink/ResetPasswordExpiredLink.tsx
+++ src/features/auth/forgottenPassword/ResetPasswordExpiredLink/ResetPasswordExpiredLink.tsx
@@ -1,23 +1,14 @@
-import { t } from '@lingui/macro'
 import { useNavigation } from '@react-navigation/native'
 import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
 import { useQuery } from 'react-query'
-import styled from 'styled-components/native'
 
 import { api } from 'api/api'
-import { openUrl } from 'features/navigation/helpers'
-import { navigateToHome } from 'features/navigation/helpers'
 import { RootStackParamList, UseNavigationType } from 'features/navigation/RootNavigator'
+import { LayoutExpiredLink } from 'features/profile/pages/ChangeEmail/LayoutExpiredLink'
 import { analytics } from 'libs/analytics'
 import { AsyncError } from 'libs/monitoring'
 import { QueryKeys } from 'libs/queryKeys'
-import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
-import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
-import { GenericInfoPage } from 'ui/components/GenericInfoPage'
-import { ExternalSite } from 'ui/svg/icons/ExternalSite'
-import { SadFace } from 'ui/svg/icons/SadFace'
-import { ColorsEnum, Spacer, Typo } from 'ui/theme'
 
 type Props = StackScreenProps<RootStackParamList, 'ResetPasswordExpiredLink'>
 
@@ -44,35 +35,10 @@ export function ResetPasswordExpiredLink(props: Props) {
   }
 
   return (
-    <GenericInfoPage title={t`Oups`} icon={SadFace}>
-      <StyledBody>{t`Le lien est expiré !`}</StyledBody>
-      <StyledBody>{t`Clique sur « Renvoyer l’e-mail » pour recevoir un nouveau lien.`}</StyledBody>
-      <Spacer.Column numberOfSpaces={4} />
-      <StyledBody>{t`Si tu as besoin d’aide n’hésite pas à :`}</StyledBody>
-      <Spacer.Column numberOfSpaces={4} />
-      <ButtonTertiaryWhite
-        title={t`Consulter l'article d'aide`}
-        onPress={() =>
-          openUrl(
-            'https://aide.passculture.app/fr/articles/5261997-je-n-ai-pas-recu-le-mail-de-confirmation-de-changement-de-mot-de-passe'
-          )
-        }
-        icon={ExternalSite}
-      />
-      <Spacer.Column numberOfSpaces={4} />
-      <ButtonPrimaryWhite
-        title={t`Renvoyer l'email`}
-        onPress={() => resetPasswordEmailQuery()}
-        disabled={isFetching}
-      />
-      <Spacer.Column numberOfSpaces={4} />
-      <ButtonTertiaryWhite title={t`Retourner à l'accueil`} onPress={navigateToHome} />
-    </GenericInfoPage>
+    <LayoutExpiredLink
+      resetQuery={() => resetPasswordEmailQuery()}
+      isFetching={isFetching}
+      urlFAQ="https://aide.passculture.app/fr/articles/5261997-je-n-ai-pas-recu-le-mail-de-confirmation-de-changement-de-mot-de-passe"
+    />
   )
 }
-
-const StyledBody = styled(Typo.Body).attrs({
-  color: ColorsEnum.WHITE,
-})({
-  textAlign: 'center',
-})
