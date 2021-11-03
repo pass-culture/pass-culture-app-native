73b53fae5bc5db72a8bb73176ffe3546bcbd4042
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-11-03 16:31:44 +0100

    (Tech) fixing env web

--- src/libs/environment/env.web.ts
+++ src/libs/environment/env.web.ts
@@ -1,11 +1,8 @@
-import { omit } from 'lodash'
-
 import { parseBooleanVariables } from 'libs/environment/parseBooleanVariables'
 
 import { Environment } from './types'
 
-// TODO: FIXME: configure cors on backend and disable proxy in webpackDevServer.config.js#L110 when doing https://passculture.atlassian.net/browse/PC-9847
 export const env = (parseBooleanVariables({
-  ...omit(process.env, __DEV__ ? ['API_BASE_URL'] : []),
-  API_BASE_URL: '',
+  ...process.env,
+  API_BASE_URL: __DEV__ ? '' : process.env.API_BASE_URL,
 }) as unknown) as Environment
