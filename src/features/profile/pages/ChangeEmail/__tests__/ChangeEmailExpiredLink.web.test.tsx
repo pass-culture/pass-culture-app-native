123bc9d7dc48b15e8d9ae2927dee46f9345795c8
Lucas Beneston <lucas.beneston@passculture.app>
2021-11-03 17:28:04 +0100

    feat(PC-11602) ChangeEmailExpiredLink : Add tests

---
+++ src/features/profile/pages/ChangeEmail/__tests__/ChangeEmailExpiredLink.web.test.tsx
@@ -0,0 +1,52 @@
+import { StackScreenProps } from '@react-navigation/stack'
+import React from 'react'
+import waitForExpect from 'wait-for-expect'
+
+import { contactSupport } from 'features/auth/support.services'
+import { navigateToHome } from 'features/navigation/helpers'
+import { RootStackParamList } from 'features/navigation/RootNavigator'
+import { ChangeEmailExpiredLink } from 'features/profile/pages/ChangeEmail/ChangeEmailExpiredLink'
+import { fireEvent, render } from 'tests/utils/web'
+
+jest.mock('features/navigation/helpers')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+const userEmail = 'john@wick.com'
+
+describe('<ChangeEmailExpiredLink />', () => {
+  it('should render correctly', () => {
+    const renderAPI = renderChangeEmailExpiredLink()
+    expect(renderAPI).toMatchSnapshot()
+  })
+
+  it('should redirect to home page when go back to home button is clicked', async () => {
+    const { getByText } = await renderChangeEmailExpiredLink()
+
+    fireEvent.click(getByText(`Retourner à l'accueil`))
+
+    await waitForExpect(() => {
+      expect(navigateToHome).toBeCalledTimes(1)
+    })
+  })
+
+  it('should open mail app when clicking on contact support button', async () => {
+    const { getByText } = renderChangeEmailExpiredLink()
+
+    const contactSupportButton = getByText('Contacter le support')
+    fireEvent.click(contactSupportButton)
+
+    await waitForExpect(() => {
+      expect(contactSupport.forChangeEmailExpiredLink).toHaveBeenCalledWith(userEmail)
+    })
+  })
+})
+
+function renderChangeEmailExpiredLink() {
+  const navigationProps = {
+    route: { params: { email: userEmail } },
+  } as StackScreenProps<RootStackParamList, 'ChangeEmailExpiredLink'>
+  return render(<ChangeEmailExpiredLink {...navigationProps} />)
+}
