91806c8b8f276fb2716fef97f08aeefcaf3dcaa2
Lucas Beneston <lucas.beneston@passculture.app>
2021-11-03 11:40:05 +0100

    refacto(PC-11584) SignupConfirmationExpiredLink : use LayoutExpiredLink

--- src/ui/components/__tests__/LayoutExpiredLink.test.tsx
+++ src/ui/components/__tests__/LayoutExpiredLink.test.tsx
@@ -6,11 +6,17 @@ import { LayoutExpiredLink } from 'ui/components/LayoutExpiredLink'
 const changeEmailExpiredLink = jest.fn()
 const isFetching = false
 const url = 'https://aide.passculture.app/fr/'
+const contactSupport = jest.fn()
 
 describe('<LayoutExpiredLink/>', () => {
   it('should render correctly', () => {
     const renderAPI = render(
-      <LayoutExpiredLink resetQuery={changeEmailExpiredLink} isFetching={isFetching} urlFAQ={url} />
+      <LayoutExpiredLink
+        resetQuery={changeEmailExpiredLink}
+        isFetching={isFetching}
+        urlFAQ={url}
+        contactSupport={contactSupport}
+      />
     )
     expect(renderAPI).toMatchSnapshot()
   })
