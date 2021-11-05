08f96561944cda1cc957733255702a41af687ed3
Hugo Voisin <hugov@bam.tech>
2021-11-05 15:18:16 +0100

    feat(changeEmailMutation): switch error type at each call as a API mock for manual testing purposes

--- src/features/profile/mutations.ts
+++ src/features/profile/mutations.ts
@@ -1,19 +1,33 @@
 import { useMutation } from 'react-query'
 
+export enum CHANGE_EMAIL_ERROR_CODE {
+  TOKEN_EXISTS = 'TOKEN_EXISTS',
+  INVALID_PASSWORD = 'INVALID_PASSWORD',
+  INVALID_EMAIL = 'INVALID_EMAIL',
+  EMAIL_UPDATE_ATTEMPTS_LIMIT = 'EMAIL_UPDATE_ATTEMPTS_LIMIT',
+}
+let mockedErrorCodeIndex = 0
+
 interface ChangeEmailRequest {
   email: string
   password: string
 }
 export interface UseChangeEmailMutationProps {
   onSuccess: () => void
-  onError: (error: unknown) => void
+  onError: (error: { code: CHANGE_EMAIL_ERROR_CODE }) => void
 }
 
 export function useChangeEmailMutation({ onSuccess, onError }: UseChangeEmailMutationProps) {
   return useMutation(
-    // TODO (PC-11573): call the API once available
+    // TODO (PC-11573): call the API once available, and remove `mockedErrorCodeIndex`
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    (body: ChangeEmailRequest) => new Promise((resolve, reject) => setTimeout(reject, 2000)),
+    (body: ChangeEmailRequest) =>
+      new Promise((resolve, reject) =>
+        setTimeout(() => {
+          mockedErrorCodeIndex = (mockedErrorCodeIndex + 1) % 4
+          reject({ code: Object.values(CHANGE_EMAIL_ERROR_CODE)[mockedErrorCodeIndex] })
+        }, 2000)
+      ),
     {
       onSuccess,
       onError,
