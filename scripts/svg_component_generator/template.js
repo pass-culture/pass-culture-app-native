108563ba65d6dc6da1760730e01a14da330cef5c
Hugo Voisin <hugov@bam.tech>
2021-11-11 17:12:13 +0100

    chore(svg generator): handle svg with multiple linear gradients

--- scripts/svg_component_generator/template.js
+++ scripts/svg_component_generator/template.js
@@ -1,15 +1,29 @@
-const LINEAR_GRADIENT_ID_REGEX = /"openingElement":{[^[\]{}]*"name":{[^[\]{}]*"linearGradient[^[\]{}]*}[^[\]{}]*"attributes":\[{[^[\]{}]*"name":{[^[\]{}]*id"}[^[\]{}]*"value":{[^[\]{}]*"value":"([^[\]{}]*)"[^[\]{}]*/
+const LINEAR_GRADIENT_ID_REGEX = /"openingElement":{[^[\]{}]*"name":{[^[\]{}]*"linearGradient[^[\]{}]*}[^[\]{}]*"attributes":\[{[^[\]{}]*"name":{[^[\]{}]*id"}[^[\]{}]*"value":{[^[\]{}]*"value":"([^[\]{}]*)"[^[\]{}]*/g
 
 const BICOLOR_SVG_IMPORT = "import { svgIdentifier } from 'ui/svg/utils'"
-const BICOLOR_SVG_BODY = 'const { id: gradientId, fill: gradientFill } = svgIdentifier()'
 
-function updateStringJSXForBicolorSvg(initStringJsx, linearGradientId) {
+const getSvgWithIdsBody = (numberOfIds) =>
+  [...Array(numberOfIds).keys()]
+    .map(
+      (i) =>
+        `const { id: gradientId${i == 0 ? '' : `${i + 1}`}, fill: gradientFill${
+          i == 0 ? '' : `${i + 1}`
+        } } = svgIdentifier()`
+    )
+    .join('\n')
+
+function updateStringJSXForBicolorSvg(
+  initStringJsx,
+  linearGradientId,
+  gradientId = 'gradientId',
+  gradientFill = 'gradientFill'
+) {
   let stringJsx = initStringJsx
   const oldFillElement = `{"type":"JSXAttribute","name":{"type":"JSXIdentifier","name":"fill"},"value":{"type":"StringLiteral","value":"url(#${linearGradientId})"}}`
-  const newFillElement = `{"type":"JSXAttribute","name":{"type":"JSXIdentifier","name":"fill"},"value":{"type":"JSXExpressionContainer","expression":{"type":"Identifier","name":"gradientFill"}}}`
+  const newFillElement = `{"type":"JSXAttribute","name":{"type":"JSXIdentifier","name":"fill"},"value":{"type":"JSXExpressionContainer","expression":{"type":"Identifier","name":"${gradientFill}"}}}`
   stringJsx = stringJsx.replace(oldFillElement, newFillElement)
   const oldLinearGradientElement = `{"type":"JSXAttribute","name":{"type":"JSXIdentifier","name":"id"},"value":{"type":"StringLiteral","value":"${linearGradientId}"}}`
-  const newLinearGradientElement = `{"type":"JSXAttribute","name":{"type":"JSXIdentifier","name":"id"},"value":{"type":"JSXExpressionContainer","expression":{"type":"Identifier","name":"gradientId"}}}`
+  const newLinearGradientElement = `{"type":"JSXAttribute","name":{"type":"JSXIdentifier","name":"id"},"value":{"type":"JSXExpressionContainer","expression":{"type":"Identifier","name":"${gradientId}"}}}`
   return stringJsx.replace(oldLinearGradientElement, newLinearGradientElement)
 }
 
@@ -19,11 +33,20 @@ function template({ template }, opts, { imports, componentName, jsx: initJsx }) 
 
   let jsx = initJsx
   let stringJsx = JSON.stringify(jsx)
-  const regexMatch = stringJsx.match(LINEAR_GRADIENT_ID_REGEX)
-  const isBicolor = regexMatch && regexMatch.length > 1
+  const regexMatch = [...stringJsx.matchAll(LINEAR_GRADIENT_ID_REGEX)]
+  const isBicolor = regexMatch && regexMatch.length > 0
+  const numberOfIds = regexMatch.length
   if (isBicolor) {
-    const linearGradientId = regexMatch[1]
-    stringJsx = updateStringJSXForBicolorSvg(stringJsx, linearGradientId)
+    for (let i = 0; i < regexMatch.length; i++) {
+      const gradientId = `gradientId${i == 0 ? '' : `${i + 1}`}`
+      const gradientFill = `gradientFill${i == 0 ? '' : `${i + 1}`}`
+      stringJsx = updateStringJSXForBicolorSvg(
+        stringJsx,
+        regexMatch[i][1],
+        gradientId,
+        gradientFill
+      )
+    }
     jsx = JSON.parse(stringJsx)
   }
 
@@ -39,7 +62,7 @@ function template({ template }, opts, { imports, componentName, jsx: initJsx }) 
     color = ColorsEnum.BLACK,
     testID,
   }: IconInterface) => {
-    ${isBicolor ? BICOLOR_SVG_BODY : ''}
+    ${isBicolor ? getSvgWithIdsBody(numberOfIds) : ''}
   return ${jsx}
 }`
 }
