4c494c9ad524833d881d933cd1dabc10845c9698
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-11-11 15:53:12 +0100

    (PC-11766) Supprimer la gestion specifique web pour le fore update firestore

--- src/libs/firestore/applicationVersions.ts
+++ src/libs/firestore/applicationVersions.ts
@@ -1,29 +1,17 @@
-import { Platform } from 'react-native'
-
 import { env } from 'libs/environment'
 import { firestoreRemoteStore } from 'libs/firestore/client'
 import { RemoteStoreCollections, RemoteStoreDocuments } from 'libs/firestore/types'
 import { MonitoringError } from 'libs/monitoring'
 
-const minimalBuildNumberKey = Platform.select({
-  default: RemoteStoreDocuments.MINIMAL_BUILD_NUMBER,
-  web: RemoteStoreDocuments.MINIMAL_BUILD_NUMBER_WEB,
-})
-
-const applicationVersions = Platform.select({
-  default: RemoteStoreCollections.APPLICATION_VERSIONS,
-  web: RemoteStoreCollections.APPLICATION_VERSIONS_WEB,
-})
-
 export const minimalBuildNumberStatusListener = (
   onValueChange: (minimalBuildNumber: number) => void
 ) =>
   firestoreRemoteStore
-    .collection(applicationVersions)
+    .collection(RemoteStoreCollections.APPLICATION_VERSIONS)
     .doc(env.ENV)
     .onSnapshot(
       (docSnapshot) => {
-        onValueChange(docSnapshot.get(minimalBuildNumberKey))
+        onValueChange(docSnapshot.get(RemoteStoreDocuments.MINIMAL_BUILD_NUMBER))
       },
       (error) => {
         new MonitoringError(error.message, 'firestore_not_available')
