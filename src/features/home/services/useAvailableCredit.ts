237c76099b314884f8956a2204958e1131a9e49f
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-11-11 16:07:56 +0100

    :recycle: (pc-11313) extract Credit computation in separate function

--- src/features/home/services/useAvailableCredit.ts
+++ src/features/home/services/useAvailableCredit.ts
@@ -1,17 +1,22 @@
+import { UserProfileResponse } from 'api/gen/api'
 import { useUserProfileInfo } from 'features/home/api'
 import { computeCredit } from 'features/profile/utils'
 
 export type Credit = { amount: number; isExpired: boolean }
 
-export const useAvailableCredit = (): Credit | undefined => {
-  const { data: user } = useUserProfileInfo()
-
-  if (!user) return undefined
-
+export const getAvailableCredit = (user: UserProfileResponse): Credit => {
   return {
     amount: computeCredit(user.domainsCredit),
     isExpired: user.depositExpirationDate
       ? new Date(user.depositExpirationDate) < new Date()
       : false,
   }
+}
+
+export const useAvailableCredit = (): Credit | undefined => {
+  const { data: user } = useUserProfileInfo()
+
+  if (!user) return undefined
+
+  return getAvailableCredit(user)
 }
