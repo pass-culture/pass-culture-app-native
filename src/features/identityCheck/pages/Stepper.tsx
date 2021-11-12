c4aa1b82ba73336cebf33faf572028bc843c454b
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-11-12 10:26:28 +0100

    (PC-11741) review design of Stepper page

--- src/features/identityCheck/pages/Stepper.tsx
+++ src/features/identityCheck/pages/Stepper.tsx
@@ -1,36 +1,62 @@
 import { t } from '@lingui/macro'
 import React from 'react'
-import styled from 'styled-components/native'
+import styled, { useTheme } from 'styled-components/native'
 
 import { StepButton } from 'features/identityCheck/atoms/StepButton'
 import { useIdentityCheckSteps } from 'features/identityCheck/useIdentityCheckSteps'
 import { homeNavConfig } from 'features/navigation/TabBar/helpers'
 import { useGoBack } from 'features/navigation/useGoBack'
 import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
-import { GenericInfoPage } from 'ui/components/GenericInfoPage'
-import { Spacer, Typo, ColorsEnum } from 'ui/theme'
+import { Background } from 'ui/svg/Background'
+import { Spacer, Typo, ColorsEnum, getSpacing } from 'ui/theme'
 
 export const IdentityCheckStepper = () => {
+  const theme = useTheme()
   const { goBack } = useGoBack(...homeNavConfig)
   const steps = useIdentityCheckSteps()
 
   return (
-    <GenericInfoPage title={t`C’est très rapide !`}>
-      <StyledBody>{t`Voici les 3 étapes que tu vas devoir suivre.`}</StyledBody>
+    <CenteredContainer>
+      <Background />
+      <Container>
+        <Spacer.TopScreen />
+        {theme.isDesktopViewport ? <Spacer.Column numberOfSpaces={2} /> : <Spacer.Flex />}
 
-      <Spacer.Column numberOfSpaces={6} />
+        <Title>{t`C’est très rapide !`}</Title>
+        <Spacer.Column numberOfSpaces={2} />
 
-      {/* TODO(antoinewg) dehardcode state. This is temporary for design purposes */}
-      <StepButton key={steps[0].name} step={steps[0]} state="completed" />
-      <StepButton key={steps[1].name} step={steps[1]} state="current" />
-      <StepButton key={steps[2].name} step={steps[2]} state="disabled" />
+        <StyledBody>{t`Voici les 3 étapes que tu vas devoir suivre.`}</StyledBody>
+        {theme.isDesktopViewport ? <Spacer.Column numberOfSpaces={2} /> : <Spacer.Flex />}
 
-      <Spacer.Column numberOfSpaces={12} />
-      <ButtonTertiaryWhite title={t`Abandonner`} onPress={goBack} />
-    </GenericInfoPage>
+        {/* TODO(antoinewg) dehardcode state. This is temporary for design purposes */}
+        <StepButton key={steps[0].name} step={steps[0]} state="completed" />
+        <StepButton key={steps[1].name} step={steps[1]} state="current" />
+        <StepButton key={steps[2].name} step={steps[2]} state="disabled" />
+        {theme.isDesktopViewport ? <Spacer.Column numberOfSpaces={10} /> : <Spacer.Flex flex={2} />}
+
+        <ButtonTertiaryWhite title={t`Abandonner`} onPress={goBack} />
+      </Container>
+    </CenteredContainer>
   )
 }
 
+const Title = styled(Typo.Title3).attrs({ color: ColorsEnum.WHITE })({
+  textAlign: 'center',
+})
+
 const StyledBody = styled(Typo.Body).attrs({ color: ColorsEnum.WHITE })({
   textAlign: 'center',
+})
+
+const CenteredContainer = styled.View({
+  flex: 1,
+  alignItems: 'center',
+})
+
+const Container = styled.View({
+  flex: 1,
+  justifyContent: 'center',
+  padding: getSpacing(5),
+  width: '100%',
+  maxWidth: getSpacing(125),
 })
