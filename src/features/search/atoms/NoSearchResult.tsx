b922aa57fd04577df34c11048411cca68dbc42c5
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2021-11-15 18:27:36 +0100

    PC-11508 fix(Search): NoSearchResults now have a minHeight: 250

--- src/features/search/atoms/NoSearchResult.tsx
+++ src/features/search/atoms/NoSearchResult.tsx
@@ -49,6 +49,7 @@ export const NoSearchResult: React.FC = () => {
 
 const Container = styled.View({
   flex: 1,
+  minHeight: 250,
   height: '100%',
   alignItems: 'center',
   justifyContent: 'center',
