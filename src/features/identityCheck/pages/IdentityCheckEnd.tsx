d19552ff21c547cee885f1e13cc367b91707f7bd
Lucas Beneston <lucas.beneston@passculture.app>
2021-11-16 09:55:34 +0100

    PC-11744 feat(identityCheck) Add redirection to identity check stepper after 3s + extract translation

--- src/features/identityCheck/pages/IdentityCheckEnd.tsx
+++ src/features/identityCheck/pages/IdentityCheckEnd.tsx
@@ -1,14 +1,24 @@
 import { t } from '@lingui/macro'
-import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import React, { useEffect } from 'react'
 
+import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { GenericInfoPage } from 'ui/components/GenericInfoPage'
 import { EmailSent } from 'ui/svg/icons/EmailSent'
 import { getSpacing } from 'ui/theme'
 
-export const IdentityCheckEnd = () => (
-  <GenericInfoPage
-    title={t`Ta pièce d’identité a bien été transmise !`}
-    icon={EmailSent}
-    iconSize={getSpacing(42)}
-  />
-)
+export const IdentityCheckEnd = () => {
+  const navigation = useNavigation<UseNavigationType>()
+
+  useEffect(() => {
+    setTimeout(() => navigation.navigate('IdentityCheck'), 3000)
+  }, [])
+
+  return (
+    <GenericInfoPage
+      title={t`Ta pièce d’identité a bien été transmise !`}
+      icon={EmailSent}
+      iconSize={getSpacing(42)}
+    />
+  )
+}
