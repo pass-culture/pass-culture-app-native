73bb6a967be0d925a173e1d2fa0a9e37713d5ef2
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-11-19 16:55:24 +0100

    :sparkles: (pc-11654) create SlantTag component

---
+++ src/ui/components/SlantTag.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+import { getSpacing, Typo } from 'ui/theme'
+
+interface Props {
+  text: string
+  width?: number
+  height?: number
+  slantAngle?: number
+  testID?: string
+}
+
+export const SlantTag: React.FC<Props> = ({ text, width, height, testID, slantAngle }) => {
+  return (
+    <SlantTagContainer
+      testID={testID}
+      tagWidth={width ? width : undefined}
+      tagHeight={height ? height : undefined}
+      tagAngle={slantAngle ? slantAngle : undefined}>
+      <StyledPrice tagAngle={slantAngle ? slantAngle : undefined}>{text}</StyledPrice>
+    </SlantTagContainer>
+  )
+}
+
+const StyledPrice = styled(Typo.Caption)<{
+  tagAngle: number | undefined
+}>(({ tagAngle, theme }) => ({
+  color: theme.colors.white,
+  transform: tagAngle ? `rotate(${-tagAngle}deg)` : `rotate(4.34deg)`,
+  alignSelf: 'center',
+}))
+
+const SlantTagContainer = styled.View<{
+  tagWidth: number | undefined
+  tagHeight: number | undefined
+  tagAngle: number | undefined
+}>(({ tagWidth, tagHeight, tagAngle, theme }) => ({
+  backgroundColor: theme.colors.secondary,
+  justifyContent: 'center',
+  borderRadius: theme.borderRadius.checkbox,
+  width: tagWidth ? tagWidth : 'auto',
+  height: tagHeight ? tagHeight : 'auto',
+  paddingVertical: tagHeight ? 0 : getSpacing(0.5),
+  paddingHorizontal: tagWidth ? 0 : getSpacing(2.5),
+  transform: tagAngle ? `rotate(${tagAngle}deg)` : `rotate(-4.34deg)`,
+}))
