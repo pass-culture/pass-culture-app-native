fcb308430d41fa4ef9be84f5d3ca05e66e38224c
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2021-11-19 17:58:33 +0100

    (PC-11884) fix(LogoMinistere): suppression marge à gauche et correction page profil web mobile  (#2084)
    * (PC-11884) fix(LogoMinistere): suppression marge à gauche et correction page profil web mobile (PC-11889)
    
    * chore(snapshots): mise à jour des snapshots
    
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- src/ui/components/spacer/Spacer.tsx
+++ src/ui/components/spacer/Spacer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components/native'
+import styled, { useTheme } from 'styled-components/native'
 
 import { getSpacing } from 'ui/theme/spacing'
 import { useCustomSafeInsets } from 'ui/theme/useCustomSafeInsets'
@@ -11,29 +11,37 @@ interface SpacerProps {
 const RowSpacer = styled.View<SpacerProps>(({ numberOfSpaces }) => ({
   width: getSpacing(numberOfSpaces),
 }))
+
 const ColumnSpacer = styled.View<SpacerProps>(({ numberOfSpaces }) => ({
   height: getSpacing(numberOfSpaces),
 }))
 
 const TabBarSpacer: React.FC = () => {
   const { tabBarHeight } = useCustomSafeInsets()
-  return <CustomHeightSpacer customHeight={tabBarHeight} />
+  const { showTabBar } = useTheme()
+  return <CustomHeightSpacer customHeight={tabBarHeight} enable={showTabBar} />
 }
+
 const TopScreenSpacer: React.FC = () => {
   const { top } = useCustomSafeInsets()
-  return <CustomHeightSpacer customHeight={top} />
+  return <CustomHeightSpacer customHeight={top} enable />
 }
+
 const BottomScreenSpacer: React.FC = () => {
   const { bottom } = useCustomSafeInsets()
-  return <CustomHeightSpacer customHeight={bottom} />
+  return <CustomHeightSpacer customHeight={bottom} enable />
 }
-const CustomHeightSpacer = styled.View<{ customHeight: number }>(({ customHeight }) => ({
-  height: customHeight,
-}))
+
+const CustomHeightSpacer = styled.View<{ customHeight: number; enable?: boolean }>(
+  ({ customHeight, enable }) => ({
+    height: enable ? customHeight : 0,
+  })
+)
 
 interface FlexSpacerProps {
   flex?: number
 }
+
 const FlexSpacer = styled.View<FlexSpacerProps>(({ flex }) => ({
   flex: flex ? flex : 1,
 }))
