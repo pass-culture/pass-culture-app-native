5269dbe287bd725900daa8b20bf0bdad70fde7a8
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-11-20 10:34:24 +0100

    (PC-11746) refactor(DeeplinkImporter): remove unused component QuitSignupModal

--- src/features/deeplinks/pages/DeeplinkImporter.tsx
+++ src/features/deeplinks/pages/DeeplinkImporter.tsx
@@ -3,7 +3,6 @@ import React, { FunctionComponent, useRef, useState } from 'react'
 import { TextInput as RNTextInput } from 'react-native'
 import styled from 'styled-components/native'
 
-import { QuitSignupModal, SignupSteps } from 'features/auth/components/QuitSignupModal'
 import { resolveHandler } from 'features/deeplinks'
 import { useDeeplinkUrlHandler } from 'features/deeplinks/useDeeplinkUrlHandler'
 import { homeNavConfig } from 'features/navigation/TabBar/helpers'
@@ -14,7 +13,6 @@ import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { InputError } from 'ui/components/inputs/InputError'
 import { TextInput } from 'ui/components/inputs/TextInput'
 import { ModalHeader } from 'ui/components/modals/ModalHeader'
-import { useModal } from 'ui/components/modals/useModal'
 import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
 import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
 
@@ -35,53 +33,43 @@ export const DeeplinkImporter: FunctionComponent = () => {
   const [hasError, setHasError] = useState(false)
   const urlInput = useRef<RNTextInput | null>(null)
 
-  const { visible: fullPageModalVisible, hideModal: hideFullPageModal } = useModal(false)
-
   return (
-    <React.Fragment>
-      <BottomContentPage>
-        <ModalHeader
-          title={t`Accès aux liens`}
-          leftIconAccessibilityLabel={t`Revenir en arrière`}
-          leftIcon={ArrowPrevious}
-          onLeftIconPress={goBack}
-          rightIconAccessibilityLabel={undefined}
-          rightIcon={undefined}
-          onRightIconPress={undefined}
-        />
-        <ModalContent>
-          <StyledInput>
-            <Typo.Body>{t`Un lien ne fonctionne pas ?`}</Typo.Body>
-            <Spacer.Column numberOfSpaces={2} />
-            <TextInput
-              autoFocus={true}
-              onChangeText={setUrl}
-              placeholder={t`Colle ton lien ici ...`}
-              ref={urlInput}
-              value={url}
-            />
-            <InputError
-              visible={hasError}
-              messageId={t`Format du lien incorrect`}
-              numberOfSpacesTop={1}
-            />
-          </StyledInput>
-          <Spacer.Column numberOfSpaces={4} />
-          <Typo.Caption color={ColorsEnum.GREY_DARK}>
-            {t`Copie ici le lien qui t'a été envoyé par email et clique sur le bouton "Importer le lien"`}
-          </Typo.Caption>
-          <Spacer.Column numberOfSpaces={6} />
-          <ButtonPrimary title={t`Importer le lien`} onPress={resolveLink} isLoading={false} />
-          <Spacer.Column numberOfSpaces={3} />
-        </ModalContent>
-      </BottomContentPage>
-      <QuitSignupModal
-        visible={fullPageModalVisible}
-        resume={hideFullPageModal}
-        testIdSuffix="email-quit-signup"
-        signupStep={SignupSteps.Email}
+    <BottomContentPage>
+      <ModalHeader
+        title={t`Accès aux liens`}
+        leftIconAccessibilityLabel={t`Revenir en arrière`}
+        leftIcon={ArrowPrevious}
+        onLeftIconPress={goBack}
+        rightIconAccessibilityLabel={undefined}
+        rightIcon={undefined}
+        onRightIconPress={undefined}
       />
-    </React.Fragment>
+      <ModalContent>
+        <StyledInput>
+          <Typo.Body>{t`Un lien ne fonctionne pas ?`}</Typo.Body>
+          <Spacer.Column numberOfSpaces={2} />
+          <TextInput
+            autoFocus={true}
+            onChangeText={setUrl}
+            placeholder={t`Colle ton lien ici ...`}
+            ref={urlInput}
+            value={url}
+          />
+          <InputError
+            visible={hasError}
+            messageId={t`Format du lien incorrect`}
+            numberOfSpacesTop={1}
+          />
+        </StyledInput>
+        <Spacer.Column numberOfSpaces={4} />
+        <Typo.Caption color={ColorsEnum.GREY_DARK}>
+          {t`Copie ici le lien qui t'a été envoyé par email et clique sur le bouton "Importer le lien"`}
+        </Typo.Caption>
+        <Spacer.Column numberOfSpaces={6} />
+        <ButtonPrimary title={t`Importer le lien`} onPress={resolveLink} isLoading={false} />
+        <Spacer.Column numberOfSpaces={3} />
+      </ModalContent>
+    </BottomContentPage>
   )
 }
 
