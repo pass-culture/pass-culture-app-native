c9b0cadf8523eb90214587d7cabee3496461043c
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-11-24 16:01:35 +0100

    :sparkles: (pc-11899) add collégien status and feature flag it

--- src/features/identityCheck/pages/profile/__test__/Status.test.tsx
+++ src/features/identityCheck/pages/profile/__test__/Status.test.tsx
@@ -3,9 +3,28 @@ import React from 'react'
 import { Status } from 'features/identityCheck/pages/profile/Status'
 import { render } from 'tests/utils'
 
+jest.mock('features/auth/api')
+
+const mockSettings = {
+  enableUnderageGeneralisation: false,
+  enableNativeEacIndividual: false,
+}
+jest.mock('features/auth/settings', () => ({
+  useAppSettings: jest.fn(() => ({
+    data: mockSettings,
+  })),
+}))
+
 describe('<Status/>', () => {
-  it('should render correctly', () => {
+  it('should render correctly with no Collégien status', () => {
     const renderAPI = render(<Status />)
     expect(renderAPI).toMatchSnapshot()
+  })
+  it('should show Collégien if enabledGeneralisation is true', () => {
+    mockSettings.enableNativeEacIndividual = true
+    mockSettings.enableUnderageGeneralisation = true
+    const { getByText } = render(<Status />)
+
+    expect(getByText('Collégien')).toBeTruthy()
   })
 })
