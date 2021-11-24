4917423a86bd564334a5298397b9ab6704ec4ee0
Marion Barré <marionb@bam.tech>
2021-11-24 10:58:57 +0100

    :sparkles: (idCheckUnavailable) update wording to fit with underage

--- src/features/auth/IdcheckUnavailable/IdCheckUnavailable.tsx
+++ src/features/auth/IdcheckUnavailable/IdCheckUnavailable.tsx
@@ -11,21 +11,23 @@ import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
 import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
 import { GenericInfoPage } from 'ui/components/GenericInfoPage'
 import { ExternalLinkSite } from 'ui/svg/icons/ExternalLinkSite'
-import { HappyFaceStars } from 'ui/svg/icons/HappyFaceStars'
-import { ColorsEnum, Spacer, Typo } from 'ui/theme'
+import { HappyFace } from 'ui/svg/icons/HappyFace'
+import { PlainArrowPrevious } from 'ui/svg/icons/PlainArrowPrevious'
+import { ColorsEnum, getSpacing, Spacer, Typo } from 'ui/theme'
 
 export function IdCheckUnavailable() {
   const { goBack } = useGoBack(...homeNavConfig)
   const { data: settings } = useAppSettings()
   return (
     <GenericInfoPage
-      title={t`Victime de notre succès`}
-      icon={({ color }) => <HappyFaceStars size={220} color={color} />}>
-      <StyledBody>{t`Vous êtes actuellement très nombreux à demander les 300€ et notre service rencontre quelques difficultés.`}</StyledBody>
+      title={t`Victime de notre succès !`}
+      icon={({ color }) => <HappyFace size={getSpacing(30)} color={color} />}>
+      <StyledBody>{t`Vous êtes actuellement très nombreux à vouloir créer un compte, notre service rencontre quelques difficultés.`}</StyledBody>
       <Spacer.Column numberOfSpaces={6} />
       {!!settings?.displayDmsRedirection && (
         <React.Fragment>
-          <StyledBody>{t`Cependant, tu peux nous transmettre ton dossier via la plateforme Démarches Simplifiées, et on reviendra vers toi d'ici quelques jours au plus tard :`}</StyledBody>
+          <StyledBody>{t`Tu peux nous transmettre ton dossier via la plateforme Démarches Simplifiées.
+Nous reviendrons vers toi d’ici quelques jours.`}</StyledBody>
           <Spacer.Column numberOfSpaces={8} />
           <ButtonPrimaryWhite
             title={t`Transmettre un dossier`}
@@ -35,7 +37,11 @@ export function IdCheckUnavailable() {
         </React.Fragment>
       )}
       <Spacer.Column numberOfSpaces={4} />
-      <ButtonTertiaryWhite title={t`Retour`} onPress={goBack} />
+      <ButtonTertiaryWhite
+        title={t`Retourner à l'accueil`}
+        onPress={goBack}
+        icon={PlainArrowPrevious}
+      />
     </GenericInfoPage>
   )
 }
