88587087ba0bb23575e312d65c0dd0dc1162773a
Yorick Vinesse <yorick.vinesse@gmail.com>
2021-11-25 16:27:02 +0100

    :sparkles: (pc-11984) add new BicolorLocationPointer icon

--- src/features/cheatcodes/pages/AppComponents/Icons.tsx
+++ src/features/cheatcodes/pages/AppComponents/Icons.tsx
@@ -21,6 +21,7 @@ import { BicolorConfidentiality } from 'ui/svg/icons/BicolorConfidentiality'
 import { BicolorEverywhere } from 'ui/svg/icons/BicolorEverywhere'
 import { BicolorFavorite } from 'ui/svg/icons/BicolorFavorite'
 import { BicolorLocationBuilding } from 'ui/svg/icons/BicolorLocationBuilding'
+import { BicolorLocationPointer } from 'ui/svg/icons/BicolorLocationPointer'
 import { BicolorLocationPointerDeprecated } from 'ui/svg/icons/BicolorLocationPointer_deprecated'
 import { BicolorLock } from 'ui/svg/icons/BicolorLock'
 import { BicolorLogo } from 'ui/svg/icons/BicolorLogo'
@@ -166,6 +167,7 @@ export const Icons: FunctionComponent = () => {
       <Icon name="BicolorAroundMe" component={BicolorAroundMe} isNew />
       <Icon name="EverywhereDeprecated" component={EverywhereDeprecated} />
       <Icon name="BicolorEverywhere" component={BicolorEverywhere} isNew />
+      <Icon name="BicolorLocationPointer" component={BicolorLocationPointer} isNew />
       <Icon name="BicolorLocationPointerDeprecated" component={BicolorLocationPointerDeprecated} />
       <Icon name="BicolorLocationBuilding" component={BicolorLocationBuilding} />
       <Icon name="NoOffer" component={NoOffer} />
