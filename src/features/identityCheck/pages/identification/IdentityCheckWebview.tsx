24ddbcf74ffe098113ae78615a782c1878fc4d8a
Antoine Garcia <a.garcia.walecha@gmail.com>
2021-11-25 10:14:45 +0100

    (PC-11727) fix(ubble): allow inline media playback for Ubble webview (#2121)

--- src/features/identityCheck/pages/identification/IdentityCheckWebview.tsx
+++ src/features/identityCheck/pages/identification/IdentityCheckWebview.tsx
@@ -25,6 +25,7 @@ export const IdentityCheckWebview: React.FC = () => {
         source={{ uri: params.identificationUrl }}
         onNavigationStateChange={onNavigationStateChange}
         originWhitelist={ORIGIN_WHITELIST}
+        allowsInlineMediaPlayback
         testID="identity-check-webview"
       />
       <Spacer.BottomScreen />
