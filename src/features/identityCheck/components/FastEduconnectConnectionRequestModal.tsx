faf49b153b95fee833ec5d320786933720f6ce48
Wendy Jean-Louis <jeanlouis.wendy@gmail.com>
2021-11-26 14:53:55 +0100

    (PC-11756) create InfoPlain icon

--- src/features/identityCheck/components/FastEduconnectConnectionRequestModal.tsx
+++ src/features/identityCheck/components/FastEduconnectConnectionRequestModal.tsx
@@ -1,67 +1,67 @@
-import { t } from "@lingui/macro";
-import React from "react";
-import { Text } from "react-native";
-import styled, { useTheme } from "styled-components/native";
+import { t } from '@lingui/macro'
+import React from 'react'
+import styled, { useTheme } from 'styled-components/native'
 
-import { ButtonPrimary } from "ui/components/buttons/ButtonPrimary";
-import { ButtonTertiaryBlack } from "ui/components/buttons/ButtonTertiaryBlack";
-import { AppModal } from "ui/components/modals/AppModal";
-import { OrSeparator } from "ui/components/OrSeparator";
-import { ArrowPrevious } from "ui/svg/icons/ArrowPrevious";
-import { Close } from "ui/svg/icons/Close";
-import { EditPen } from "ui/svg/icons/EditPen";
-import { Typo, getSpacing } from "ui/theme";
+import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
+import { AppModal } from 'ui/components/modals/AppModal'
+import { OrSeparator } from 'ui/components/OrSeparator'
+import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
+import { Close } from 'ui/svg/icons/Close'
+import { EditPen } from 'ui/svg/icons/EditPen'
+import { InfoPlain } from 'ui/svg/icons/InfoPlain'
+import { Typo, getSpacing } from 'ui/theme'
 
 export interface FastEduconnectConnectionRequestModalProps {
-  visible: boolean;
-  hideModal: () => void;
+  visible: boolean
+  hideModal: () => void
 }
 
-export const FastEduconnectConnectionRequestModal: React.FC<FastEduconnectConnectionRequestModalProps> = ({
-                                                                                                            visible,
-                                                                                                            hideModal
-                                                                                                          }) => {
-  const { colors } = useTheme();
+export const FastEduconnectConnectionRequestModal: React.FC<FastEduconnectConnectionRequestModalProps> =
+  ({ visible, hideModal }) => {
+    const { colors } = useTheme()
 
-  return (
-    <AppModal
-      title={t`Identifie-toi en 2 minutes`}
-      visible={visible}
-      leftIconAccessibilityLabel={"leftIconButton"}
-      leftIcon={ArrowPrevious}
-      onLeftIconPress={hideModal}
-      rightIconAccessibilityLabel={"rightIconButton"}
-      rightIcon={Close}
-      onRightIconPress={hideModal}>
-      <MainContent color={colors.greyDark}>
-        {t`Tu peux vérifier ton identité en moins de 2 minutes en utilisant ton compte ÉduConnect. Si tu n'as pas d'identifiants ÉduConnect rapproche toi de ton établissement. `}
-      </MainContent>
+    return (
+      <AppModal
+        title={t`Identifie-toi en 2 minutes`}
+        visible={visible}
+        leftIconAccessibilityLabel={'leftIconButton'}
+        leftIcon={ArrowPrevious}
+        onLeftIconPress={hideModal}
+        rightIconAccessibilityLabel={'rightIconButton'}
+        rightIcon={Close}
+        onRightIconPress={hideModal}>
+        <MainContent color={colors.greyDark}>
+          {t`Tu peux vérifier ton identité en moins de 2 minutes en utilisant ton compte ÉduConnect. Si tu n'as pas d'identifiants ÉduConnect rapproche toi de ton établissement. `}
+        </MainContent>
 
-      <TextQuestion title={t`C’est quoi ÉduConnect ?`} />
+        <TextQuestion icon={InfoPlain} title={t`C’est quoi ÉduConnect ?`} />
 
-      <ButtonPrimary
-        title={"Identification avec ÉduConnect  "}
-        onPress={() => {
-        }}
-      />
+        <ButtonPrimary
+          title={'Identification avec ÉduConnect  '}
+          onPress={() => {
+            /*
+            TODO: navigate to educonnect identification
+           */
+          }}
+        />
 
-      <OrSeparator />
+        <OrSeparator />
 
-      <ButtonTertiaryBlack icon={EditPen} title={t`Identification manuelle`} />
-      <DurationInfoText color={colors.greyDark}>{t`Environ 3 heures`}</DurationInfoText>
-    </AppModal>
-  );
-};
+        <ButtonTertiaryBlack icon={EditPen} title={t`Identification manuelle`} />
+        <DurationInfoText color={colors.greyDark}>{t`Environ 3 heures`}</DurationInfoText>
+      </AppModal>
+    )
+  }
 
 const MainContent = styled(Typo.Body)({
-  textAlign: "center"
-
-});
+  textAlign: 'center',
+})
 
 const DurationInfoText = styled(Typo.Body)({
-  textAlign: "center"
-});
+  textAlign: 'center',
+})
 
 const TextQuestion = styled(ButtonTertiaryBlack)({
-  marginBottom: getSpacing(4)
-});
+  marginBottom: getSpacing(4),
+})
