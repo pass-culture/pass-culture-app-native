49e63f2450db708f17d2e4553e4213ddb26a18ab
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-11-29 19:57:21 +0100

    (PC-11746) feat(IdentityCheck): add SetName as first screen of profile step

--- src/features/identityCheck/useIdentityCheckSteps.tsx
+++ src/features/identityCheck/useIdentityCheckSteps.tsx
@@ -14,7 +14,7 @@ export const useIdentityCheckSteps = (): StepConfig[] => {
       name: IdentityCheckStep.PROFILE,
       icon: ProfileIcon,
       label: t`Profil`,
-      screens: ['IdentityCheckCity', 'IdentityCheckAddress', 'IdentityCheckStatus'],
+      screens: ['SetName', 'IdentityCheckCity', 'IdentityCheckAddress', 'IdentityCheckStatus'],
     },
     {
       name: IdentityCheckStep.IDENTIFICATION,
