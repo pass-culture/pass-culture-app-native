7649bc12ba7b8bb3fb6af5a73282d12ea59d9235
Antoine Garcia <a.garcia.walecha@gmail.com>
2021-11-29 11:08:29 +0100

    (PC-11993) feat(ubble): enable redirection to ubble on web (#2139)

--- src/features/identityCheck/pages/identification/IdentityCheckWebview.web.tsx
+++ src/features/identityCheck/pages/identification/IdentityCheckWebview.web.tsx
@@ -1,16 +1,66 @@
-import { t } from '@lingui/macro'
-import React from 'react'
+import { useRoute, useNavigation } from '@react-navigation/native'
+import React, { useEffect } from 'react'
 
-import { GenericInfoPage } from 'ui/components/GenericInfoPage'
-import { Error } from 'ui/svg/icons/Error'
-import { getSpacing } from 'ui/theme'
+import { REDIRECT_URL_UBBLE } from 'features/identityCheck/api'
+import { UseNavigationType, UseRouteType } from 'features/navigation/RootNavigator'
+import { analytics } from 'libs/analytics'
+import { Helmet } from 'libs/react-helmet/Helmet'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const Ubble: any
+
+const ubbleIframeId = 'ubble-container'
+
+interface CompleteEvent {
+  status: 'processing'
+  redirectUrl: string
+}
+interface AbortEvent {
+  status: 'aborted'
+  returnReason: string
+  redirectUrl: string
+}
+
+// https://ubbleai.github.io/developer-documentation/#webview-integration
 export const IdentityCheckWebview: React.FC = () => {
+  const { params } = useRoute<UseRouteType<'IdentityCheckWebview'>>()
+  const { navigate } = useNavigation<UseNavigationType>()
+
+  useEffect(() => {
+    window.onUbbleReady = () => {
+      const ubbleIDV = new Ubble.IDV(document.getElementById(ubbleIframeId), {
+        width: '100%',
+        height: '100%',
+        allowCamera: true,
+        identificationUrl: params.identificationUrl,
+        events: {
+          onComplete({ redirectUrl, status }: CompleteEvent) {
+            analytics.logIdentityCheckComplete({ status })
+            ubbleIDV.destroy()
+            if (redirectUrl.includes(REDIRECT_URL_UBBLE)) navigate('IdentityCheckEnd')
+          },
+          onAbort({ redirectUrl, status, returnReason }: AbortEvent) {
+            analytics.logIdentityCheckAbort({ status, returnReason })
+            ubbleIDV.destroy()
+            // TODO(antoinewg): navigate to an error page.
+            if (redirectUrl.includes(REDIRECT_URL_UBBLE)) navigate('IdentityCheckEnd')
+          },
+        },
+      })
+    }
+  }, [])
+
   return (
-    <GenericInfoPage
-      title={t`Cet écran n'est pas accessible depuis le web`}
-      icon={Error}
-      iconSize={getSpacing(42)}
-    />
+    <React.Fragment>
+      <Helmet>
+        <script
+          src="https://oos.eu-west-2.outscale.com/public-ubble-ai/iframe-sdk-0.0.1.js"
+          type="application/javascript"
+        />
+      </Helmet>
+      <div id={ubbleIframeId} style={style} />
+    </React.Fragment>
   )
 }
+
+const style = { flex: 1 }
