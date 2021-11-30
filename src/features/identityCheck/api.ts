7bb0a6360384a09733cdc74b8deb5992ec2421fd
Lucas Beneston <lucas.beneston@passculture.app>
2021-11-30 18:50:37 +0100

    (PC-1208) feat(identityCheck): add usePatchProfile

--- src/features/identityCheck/api.ts
+++ src/features/identityCheck/api.ts
@@ -1,26 +1,52 @@
+import { t } from '@lingui/macro'
 import { useEffect, useState } from 'react'
 import { useMutation } from 'react-query'
 
 import { api } from 'api/api'
-import { IdentificationSessionResponse } from 'api/gen'
+import { ActivityEnum, IdentificationSessionResponse } from 'api/gen'
+import { useIdentityCheckContext } from 'features/identityCheck/context/IdentityCheckContextProvider'
 import { WEBAPP_V2_URL } from 'libs/environment'
 import { MutationKeys } from 'libs/queryKeys'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
 
 export const REDIRECT_URL_UBBLE = `${WEBAPP_V2_URL}/verification-identite/fin`
 
 export function useIdentificationUrl() {
   const [identificationUrl, setIdentificationUrl] = useState<string | undefined>()
-
   const { mutate: requestIdentificationUrl } = useMutation<IdentificationSessionResponse, void>(
     MutationKeys.IDENTIFICATION_URL,
     () => api.postnativev1ubbleIdentification({ redirectUrl: REDIRECT_URL_UBBLE }),
     { onSuccess: (data) => setIdentificationUrl(data.identificationUrl) }
   )
-
   useEffect(() => {
     if (identificationUrl) return
     requestIdentificationUrl()
   }, [])
 
   return identificationUrl
+}
+
+export function usePatchProfile() {
+  const { profile } = useIdentityCheckContext()
+  const { showErrorSnackBar } = useSnackBarContext()
+
+  return useMutation(
+    MutationKeys.PATCH_PROFILE,
+    () =>
+      api.patchnativev1beneficiaryInformation({
+        activity: profile.status as ActivityEnum,
+        address: profile.address,
+        city: profile.city?.name || '',
+        firstName: profile.name?.firstName,
+        lastName: profile.name?.lastName,
+        postalCode: profile.city?.postalCode || '',
+      }),
+    {
+      onError: () =>
+        showErrorSnackBar({
+          message: t`Une erreur est survenue lors de la mise à jour de votre profil`,
+          timeout: SNACK_BAR_TIME_OUT,
+        }),
+    }
+  )
 }
--- src/features/identityCheck/useIdentityCheckNavigation.ts
+++ src/features/identityCheck/useIdentityCheckNavigation.ts
@@ -1,55 +1,83 @@
 import { useNavigation, useRoute } from '@react-navigation/native'
 
+import { usePatchProfile } from 'features/identityCheck/api'
 import { useIdentityCheckContext } from 'features/identityCheck/context/IdentityCheckContextProvider'
 import { IdentityCheckScreen, IdentityCheckStep, StepConfig } from 'features/identityCheck/types'
 import { useIdentityCheckSteps } from 'features/identityCheck/useIdentityCheckSteps'
-import { UseNavigationType } from 'features/navigation/RootNavigator'
+import {
+  IdentityCheckRootStackParamList,
+  UseNavigationType,
+} from 'features/navigation/RootNavigator'
 import { identityCheckRoutes } from 'features/navigation/RootNavigator/identityCheckRoutes'
+
+type NextScreenOrStep = { screen: IdentityCheckScreen } | { step: IdentityCheckStep } | null
 
 const isIdentityCheckRoute = (name: string): name is IdentityCheckScreen =>
   identityCheckRoutes.map((route) => route.name).includes(name as IdentityCheckScreen)
 
+const getCurrentStep = (steps: StepConfig[], currentRoute: keyof IdentityCheckRootStackParamList) =>
+  steps.find((step) => step.screens.includes(currentRoute)) || null
+
+export const useCurrentIdentityCheckStep = (): IdentityCheckStep | null => {
+  const { name } = useRoute()
+  const steps = useIdentityCheckSteps()
+  const currentRoute = isIdentityCheckRoute(name) ? name : null
+  const currentStep = currentRoute ? getCurrentStep(steps, currentRoute) : null
+  return currentStep ? currentStep.name : null
+}
+
+export const useNextScreenOrStep = (): NextScreenOrStep => {
+  const { name } = useRoute()
+  const steps = useIdentityCheckSteps()
+  const currentRoute = isIdentityCheckRoute(name) ? name : null
+  return getNextScreenOrStep(steps, currentRoute)
+}
+
 export const useIdentityCheckNavigation = (): { navigateToNextScreen: () => void } => {
   const { dispatch } = useIdentityCheckContext()
-  const steps = useIdentityCheckSteps()
   const { navigate } = useNavigation<UseNavigationType>()
-  const { name } = useRoute()
+  const currentStep = useCurrentIdentityCheckStep()
+  const nextScreenOrStep = useNextScreenOrStep()
 
-  const currentRoute = isIdentityCheckRoute(name) ? name : null
-  const nextScreenOrStep = getNextScreenOrStep(steps, currentRoute)
+  const { mutateAsync: patchProfile } = usePatchProfile()
+
+  const saveCheckpoint = async (nextStep: IdentityCheckStep) => {
+    try {
+      if (currentStep === IdentityCheckStep.PROFILE) await patchProfile()
+      dispatch({ type: 'SET_STEP', payload: nextStep })
+    } catch (e) {
+      // do nothing
+    }
+  }
 
   return {
-    navigateToNextScreen: () => {
+    navigateToNextScreen: async () => {
       if (!nextScreenOrStep) return
       if ('screen' in nextScreenOrStep) {
         navigate(nextScreenOrStep.screen)
       } else if ('step' in nextScreenOrStep) {
-        // In this case, we just redirect to the stepper screen
-        dispatch({ type: 'SET_STEP', payload: nextScreenOrStep.step })
+        await saveCheckpoint(nextScreenOrStep.step)
         navigate('IdentityCheck')
       }
     },
   }
 }
-
 export const getNextScreenOrStep = (
   steps: StepConfig[],
   currentRoute: IdentityCheckScreen | null
-): { screen: IdentityCheckScreen } | { step: IdentityCheckStep } | null => {
+): NextScreenOrStep => {
   if (!currentRoute) return null
-  const currentStep = steps.find((step) => step.screens.includes(currentRoute))
+  const currentStep = getCurrentStep(steps, currentRoute)
   if (!currentStep) return null
 
   // Step is not completed
   const currentScreenIndex = currentStep.screens.indexOf(currentRoute)
   const isLastScreen = currentScreenIndex === currentStep.screens.length - 1
   if (!isLastScreen) return { screen: currentStep.screens[currentScreenIndex + 1] }
-
   // Step is completed => find next step
   const currentStepIndex = steps.indexOf(currentStep)
   const isLastStep = currentStepIndex === steps.length - 1
   if (!isLastStep) return { step: steps[currentStepIndex + 1].name }
-
   // Step is complete and is last step
   return { step: IdentityCheckStep.END }
 }
