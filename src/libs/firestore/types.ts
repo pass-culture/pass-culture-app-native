71a9568782f758f4061f0a020c71d57fdc4216e3
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-11-30 17:53:22 +0100

    (PC-11977) refactor(ubbleLoad): use skeleton code useSearchLoad to get ubble load

--- src/libs/firestore/types.ts
+++ src/libs/firestore/types.ts
@@ -1,8 +1,10 @@
 export enum RemoteStoreCollections {
+  // TODO(antoinewg): delete this once we have no more application using it
   APP_SEARCH = 'appsearch',
   MAINTENANCE = 'maintenance',
   APPLICATION_VERSIONS = 'applicationVersions',
   APPLICATION_VERSIONS_WEB = 'applicationVersionsWeb',
+  UBBLE = 'ubble',
 }
 
 export enum RemoteStoreDocuments {
--- src/libs/firestore/ubbleLoad.ts
+++ src/libs/firestore/ubbleLoad.ts
@@ -6,13 +6,13 @@ import { RemoteStoreCollections, RemoteStoreDocuments } from 'libs/firestore/typ
 import { QueryKeys } from 'libs/queryKeys'
 
 // To avoid firing requests firestore on every request
-const STALE_TIME_FIRESTORE_SEARCH_LOAD = 5 * 60 * 1000
+const STALE_TIME_FIRESTORE_UBBLE_LOAD = 5 * 60 * 1000
 
 // TODO(antoinewg): RemoteStoreDocuments.LOAD_PERCENT is not used anymore
 // However, we can reuse this code for sending to ubble based on firestore param.
-export const getSearchLoad = () =>
+export const getUbbleLoad = () =>
   firestoreRemoteStore
-    .collection(RemoteStoreCollections.APP_SEARCH)
+    .collection(RemoteStoreCollections.UBBLE)
     .doc(env.ENV)
     .get()
     .then((collection) => collection.data())
@@ -22,7 +22,7 @@ export const getSearchLoad = () =>
         : 0
     )
 
-export const useSearchLoad = () =>
-  useQuery<number>(QueryKeys.FIRESTORE_APP_SEARCH, getSearchLoad, {
-    staleTime: STALE_TIME_FIRESTORE_SEARCH_LOAD,
+export const useUbbleLoad = () =>
+  useQuery<number>(QueryKeys.FIRESTORE_UBBLE_LOAD, getUbbleLoad, {
+    staleTime: STALE_TIME_FIRESTORE_UBBLE_LOAD,
   })
