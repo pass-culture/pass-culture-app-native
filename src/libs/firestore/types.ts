a3cb90fd1ef90084000721d3e01c95040e0b2b74
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-11-30 18:08:10 +0100

    refactor(applicationVersionsWeb): remove unused enum key

--- src/libs/firestore/types.ts
+++ src/libs/firestore/types.ts
@@ -3,7 +3,6 @@ export enum RemoteStoreCollections {
   APP_SEARCH = 'appsearch',
   MAINTENANCE = 'maintenance',
   APPLICATION_VERSIONS = 'applicationVersions',
-  APPLICATION_VERSIONS_WEB = 'applicationVersionsWeb',
   UBBLE = 'ubble',
 }
 
@@ -11,5 +10,4 @@ export enum RemoteStoreDocuments {
   LOAD_PERCENT = 'load_percent',
   MAINTENANCE_IS_ON = 'maintenanceIsOn',
   MINIMAL_BUILD_NUMBER = 'minimalBuildNumber',
-  MINIMAL_BUILD_NUMBER_WEB = 'minimalBuildNumberWeb',
 }
