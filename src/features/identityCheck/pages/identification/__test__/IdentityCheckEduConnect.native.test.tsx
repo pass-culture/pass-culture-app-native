5e294023f13d157df1b3914c47e4ab222a3bfce1
Wendy Jean-Louis <jeanlouis.wendy@gmail.com>
2021-12-01 09:54:43 +0100

    (PC-11988) reload translations

--- src/features/identityCheck/pages/identification/__test__/IdentityCheckEduConnect.native.test.tsx
+++ src/features/identityCheck/pages/identification/__test__/IdentityCheckEduConnect.native.test.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 import { IdentityCheckEduConnect } from 'features/identityCheck/pages/identification/IdentityCheckEduConnect'
 import { render } from 'tests/utils'
 
-jest.mock('react-query')
-
 describe('<IdentityCheckEduConnect />', () => {
   it('should render correctly', () => {
     const renderAPI = render(<IdentityCheckEduConnect />)
