ca09b283e0e27f4a62ef4ba543ddee3172217369
Lucas Beneston <lucas.beneston@passculture.app>
2021-12-03 11:27:47 +0100

    (PC-12138) feat(identityCheck) Update design for DMSModal

--- src/features/identityCheck/components/DMSModal.tsx
+++ src/features/identityCheck/components/DMSModal.tsx
@@ -1,9 +1,14 @@
 import { t } from '@lingui/macro'
 import React, { FunctionComponent } from 'react'
+import styled from 'styled-components/native'
 
+import { openUrl } from 'features/navigation/helpers'
+import { env } from 'libs/environment'
+import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
 import { AppModal } from 'ui/components/modals/AppModal'
 import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
-import { Typo } from 'ui/theme'
+import { ExternalSite } from 'ui/svg/icons/ExternalSite'
+import { ColorsEnum, Spacer, Typo } from 'ui/theme'
 
 interface Props {
   visible: boolean
@@ -20,6 +25,31 @@ export const DMSModal: FunctionComponent<Props> = ({ visible, hideModal }) => (
     rightIconAccessibilityLabel={undefined}
     rightIcon={undefined}
     onRightIconPress={undefined}>
-    <Typo.Body>{t`DMS modale`}</Typo.Body>
+    <Typo.Body color={ColorsEnum.GREY_DARK}>
+      {t`Tu peux aussi compléter ton dossier sur Démarches simplifiées. Attention le traitement sera plus long !`}
+    </Typo.Body>
+    <Spacer.Column numberOfSpaces={8} />
+    <CustomButtonTertiaryBlack
+      title={t`Je suis de nationalité française`}
+      onPress={() => openUrl(env.DSM_URL)}
+      icon={ExternalSite}
+    />
+    <Typo.Caption color={ColorsEnum.GREY_DARK}>{t`Carte d’identité ou passeport.`}</Typo.Caption>
+    <Spacer.Column numberOfSpaces={8} />
+    <CustomButtonTertiaryBlack
+      title={t`Je suis de nationalité étrangère`}
+      onPress={() => openUrl(env.DSM_URL)}
+      icon={ExternalSite}
+    />
+    <Typo.Caption color={ColorsEnum.GREY_DARK}>
+      {t`Titre de séjour, carte d'identité, ou passeport.`}
+    </Typo.Caption>
+    <Spacer.Column numberOfSpaces={4} />
   </AppModal>
 )
+
+const CustomButtonTertiaryBlack = styled(ButtonTertiaryBlack)({
+  justifyContent: 'flex-start',
+  paddingLeft: 0,
+  paddingRight: 0,
+})
