10e27b1bc11e412387211d4525411e1e6d8dccac
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-12-06 12:51:30 +0100

    (PC-12158) fix(recommendations): fix algolia call crashing

--- src/libs/algolia/enums/facetsEnums.ts
+++ src/libs/algolia/enums/facetsEnums.ts
@@ -1,5 +1,6 @@
 // Taken from https://www.algolia.com/apps/E2IKXJ325N/explorer/configuration/PRODUCTION/facets
 export enum FACETS_ENUM {
+  OBJECT_ID = 'objectID',
   OFFER_DATES = 'offer.dates',
   OFFER_IS_DIGITAL = 'offer.isDigital',
   OFFER_IS_DUO = 'offer.isDuo',
