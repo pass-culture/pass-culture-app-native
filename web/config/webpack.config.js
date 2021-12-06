a18eb000a7b2b2d09b0b6404b4b35b20174dedd0
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2021-12-06 10:22:28 +0100

    build(webpack): re-enable postcss (#2206)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -25,6 +25,7 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin')
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin')
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter')
 const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+const postcssNormalize = require('postcss-normalize');
 
 const appPackageJson = require(paths.appPackageJson)
 
@@ -92,20 +93,20 @@ module.exports = function (webpackEnv) {
         options: {
           // Necessary for external CSS imports to work
           // https://github.com/facebook/create-react-app/issues/2677
-          // ident: 'postcss',
-          // plugins: () => [
-          //   require('postcss-flexbugs-fixes'),
-          //   require('postcss-preset-env')({
-          //     autoprefixer: {
-          //       flexbox: 'no-2009',
-          //     },
-          //     stage: 3,
-          //   }),
-          //   // Adds PostCSS Normalize as the reset css with default options,
-          //   // so that it honors browserslist config in package.json
-          //   // which in turn let's users customize the target behavior as per their needs.
-          //   postcssNormalize(),
-          // ],
+          ident: 'postcss',
+          plugins: () => [
+            require('postcss-flexbugs-fixes'),
+            require('postcss-preset-env')({
+              autoprefixer: {
+                flexbox: 'no-2009',
+              },
+              stage: 3,
+            }),
+            // Adds PostCSS Normalize as the reset css with default options,
+            // so that it honors browserslist config in package.json
+            // which in turn let's users customize the target behavior as per their needs.
+            postcssNormalize(),
+          ],
           sourceMap: isEnvProduction && shouldUseSourceMap,
         },
       },
