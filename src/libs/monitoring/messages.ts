c660fee52b0760da5ca0e134513e1d73d7a01cc6
Antoine Garcia <a.garcia.walecha@gmail.com>
2021-12-06 15:43:26 +0100

    feat(sentry): silence Error Messages (#2214)

--- src/libs/monitoring/messages.ts
+++
@@ -1,9 +0,0 @@
-import { Severity } from '@sentry/react-native'
-
-import { eventMonitoring } from 'libs/monitoring/services'
-
-export class MonitoringMessage {
-  constructor(message: string) {
-    eventMonitoring.captureMessage(message, { level: Severity.Info })
-  }
-}
