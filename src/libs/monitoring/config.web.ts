acb65b50353dfbf965f6edae99beedec507bfb51
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2021-12-13 11:38:44 +0100

    (PC-10087) fix(Sentry): remove webpack resolve alias (#2256)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- src/libs/monitoring/config.web.ts
+++ src/libs/monitoring/config.web.ts
@@ -6,7 +6,7 @@ import { version } from '../../../package.json'
 
 export const SENTRY_CONFIG = {
   dsn: env.SENTRY_DSN,
-  environment: env.ENV,
+  environment: __DEV__ ? 'development' : env.ENV,
   release: version,
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 0.01,
