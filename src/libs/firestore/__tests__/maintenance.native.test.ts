197ee3c48546961b7ae8a1193378d89abf2bf03d
bruno ebstein <bruno.ebstein@polyconseil.fr>
2021-12-13 12:33:08 +0100

    refactor: isolate tests

--- src/libs/firestore/__tests__/maintenance.native.test.ts
+++ src/libs/firestore/__tests__/maintenance.native.test.ts
@@ -7,17 +7,21 @@ jest.mock('@react-native-firebase/firestore')
 describe('[method] maintenanceStatus', () => {
   let onNext: (input: FirebaseFirestoreTypes.DocumentSnapshot) => void
 
-  firestore()
-    .collection('maintenance')
-    .doc('testing')
-    // @ts-expect-error is a mock
-    .onSnapshot.mockImplementationOnce((localOnNext) => {
-      onNext = localOnNext
-    })
   const mockedCallBack = jest.fn()
+
+  beforeEach(() => {
+    firestore()
+      .collection('maintenance')
+      .doc('testing')
+      // @ts-expect-error is a mock
+      .onSnapshot.mockImplementationOnce((localOnNext) => {
+        onNext = localOnNext
+      })
+  })
 
   it('should set up listener to the maintenance feature flag', () => {
     maintenanceStatusListener(mockedCallBack)
+
     expect(firestore().collection).toHaveBeenCalledWith('maintenance')
     expect(firestore().collection('maintenance').doc).toHaveBeenCalledWith('testing')
     expect(firestore().collection('maintenance').doc('testing').onSnapshot).toHaveBeenCalledWith(
@@ -27,6 +31,8 @@ describe('[method] maintenanceStatus', () => {
   })
 
   it('should set up listener with given callback', () => {
+    maintenanceStatusListener(mockedCallBack)
+
     const get = jest.fn().mockReturnValue('getReturn')
     // @ts-expect-error is an incomplete mock
     const docSnapshot: FirebaseFirestoreTypes.DocumentSnapshot = { get }
