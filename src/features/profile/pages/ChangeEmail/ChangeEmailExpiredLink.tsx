b469fc8fcb598ae58ce5fdfb9912c5acfd2c2d66
Hugo Voisin <hugov@bam.tech>
2021-12-13 16:17:21 +0100

    revert(changeEmailError): send analytics when pressing resend cta

--- src/features/profile/pages/ChangeEmail/ChangeEmailExpiredLink.tsx
+++ src/features/profile/pages/ChangeEmail/ChangeEmailExpiredLink.tsx
@@ -4,20 +4,20 @@ import React from 'react'
 
 import { contactSupport } from 'features/auth/support.services'
 import { RootStackParamList } from 'features/navigation/RootNavigator'
-// import { analytics } from 'libs/analytics'
+import { analytics } from 'libs/analytics'
 import { LayoutExpiredLink } from 'ui/components/LayoutExpiredLink'
 
 type Props = StackScreenProps<RootStackParamList, 'ChangeEmailExpiredLink'>
 
 export function ChangeEmailExpiredLink(props: Props) {
-  // let resendEmailNumberOfHits = 0
+  let resendEmailNumberOfHits = 0
   const { email } = props.route.params
 
-  // const changeEmailExpiredLink = () => {
-  //   resendEmailNumberOfHits++
-  //   analytics.logSendActivationMailAgain(resendEmailNumberOfHits)
-  // TODO (PC-11697): try / catch api call like api.postnativev1requestPasswordReset({ email })
-  // }
+  const changeEmailExpiredLink = () => {
+    resendEmailNumberOfHits++
+    analytics.logSendActivationMailAgain(resendEmailNumberOfHits)
+    // TODO (PC-11697): try / catch api call like api.postnativev1requestPasswordReset({ email })
+  }
 
   const bodyText = !email
     ? t`Tu peux te connecter sur ton profil pour recommencer le parcours de changement d’e-mail.` +
@@ -26,14 +26,9 @@ export function ChangeEmailExpiredLink(props: Props) {
       t`Si tu as besoin d’aide, n’hésite pas à contacter le support.`
     : undefined
 
-  // TODO (LucasBeneston): use isFetching from useQuery()
-  const isFetching = false
-
   return (
     <LayoutExpiredLink
-      // TODO (PC-11697): uncomment when the resend email feature is available
-      // onResendEmail={changeEmailExpiredLink}
-      disabledResendEmailButton={isFetching}
+      onResendEmail={changeEmailExpiredLink}
       customBodyText={bodyText}
       contactSupport={email ? () => contactSupport.forChangeEmailExpiredLink(email) : undefined}
     />
