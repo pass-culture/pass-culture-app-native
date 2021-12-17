330e106082968b99b7e0dbda34e94e3454202fec
Hugo Voisin <hugov@bam.tech>
2021-12-17 16:32:33 +0100

    chore(eslint): add rule to autofix space before ! or ? the other characters will be added progressively

---
+++ eslint-custom-rules/nbsp-in-french-translations.js
@@ -0,0 +1,45 @@
+/**
+ * This rule aims to spot miss-use of a space instead of a non-breaking space (nbsp) with code \u00a0
+ * before some characters in French translations, such as ! ? : ; € » or after «
+ *
+ * Ex: "Bienvenue !" should be "Bienvenue\u00a0!"
+ */
+
+module.exports = {
+  name: 'nbsp-in-french-translations',
+  meta: {
+    docs: {
+      description:
+        'force the use of non-breaking space before some characters in French translations',
+    },
+    fixable: 'code',
+  },
+  create(context) {
+    return {
+      'TaggedTemplateExpression[tag.name="t"] > TemplateLiteral > TemplateElement[value.raw=/ !/]':
+        (node) => {
+          context.report({
+            node,
+            message:
+              'Please use \\u00a0 (nbsp) instead of whitespace before !, ?, :, ;, €, » and after «',
+            fix: function (fixer) {
+              const textToReplace = node.value.raw.replace(/ !/g, '\\u00a0!')
+              return fixer.replaceText(node, `\`${textToReplace}\``)
+            },
+          })
+        },
+      'TaggedTemplateExpression[tag.name="t"] > TemplateLiteral > TemplateElement[value.raw=/ \\?/]':
+        (node) => {
+          context.report({
+            node,
+            message:
+              'Please use \\u00a0 (nbsp) instead of whitespace before !, ?, :, ;, €, » and after «',
+            fix: function (fixer) {
+              const textToReplace = node.value.raw.replace(/ \?/g, '\\u00a0?')
+              return fixer.replaceText(node, `\`${textToReplace}\``)
+            },
+          })
+        },
+    }
+  },
+}
