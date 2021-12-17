9ac6fe664f684a3d00ad9cbe445aafc78eab20ba
AnoukHello <anouk.hello@polyconseil.fr>
2021-12-17 16:53:53 +0100

    (PC-12378) Stepper: navigate to maintenance page if nextStep = maintenance

--- src/features/identityCheck/useSetCurrentSubscriptionStep.ts
+++ src/features/identityCheck/useSetCurrentSubscriptionStep.ts
@@ -1,32 +1,40 @@
 import { useFocusEffect } from '@react-navigation/native'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 
-import { SubscriptionStep } from 'api/gen'
+import { NextSubscriptionStepResponse, SubscriptionStep } from 'api/gen'
 import { useNextSubscriptionStep } from 'features/auth/signup/nextSubscriptionStep'
 import { useIdentityCheckContext } from 'features/identityCheck/context/IdentityCheckContextProvider'
 import { IdentityCheckStep } from 'features/identityCheck/types'
 import { eventMonitoring } from 'libs/monitoring'
 
-const getIdentityCheckStep = (subscriptionStep: SubscriptionStep | null): IdentityCheckStep => {
+const getIdentityCheckStep = (
+  subscriptionStep: SubscriptionStep | null
+): IdentityCheckStep | null => {
   if (subscriptionStep === SubscriptionStep.ProfileCompletion) return IdentityCheckStep.PROFILE
   if (subscriptionStep === SubscriptionStep.IdentityCheck) return IdentityCheckStep.IDENTIFICATION
-  return IdentityCheckStep.CONFIRMATION
+  if (subscriptionStep === SubscriptionStep.HonorStatement) return IdentityCheckStep.CONFIRMATION
+  return null
 }
 
 export const useSetCurrentSubscriptionStep = () => {
   const context = useIdentityCheckContext()
   const { refetch } = useNextSubscriptionStep()
+  const [subscription, setSubscription] = useState<NextSubscriptionStepResponse | undefined>()
 
   useFocusEffect(
     useCallback(() => {
       refetch()
         .then(({ data: subscription }) => {
-          const step = getIdentityCheckStep(subscription?.nextSubscriptionStep || null)
-          context.dispatch({ type: 'SET_STEP', payload: step })
+          setSubscription(subscription)
+          const nextStep = subscription?.nextSubscriptionStep
+          const step = getIdentityCheckStep(nextStep || null)
+          if (step) context.dispatch({ type: 'SET_STEP', payload: step })
         })
         .catch(() => {
           eventMonitoring.captureException(new Error('Error fetching subscription'))
         })
     }, [])
   )
+
+  return { subscription }
 }
