a7db14ff2f39cc800cb0569f9385f577bd747523
BenStirrup <benjamin.stirrup@gmail.com>
2021-12-17 14:35:20 +0100

    (PC-12044) fix(mobileDatePicker) Disable choice for past dates

--- src/features/bookOffer/components/__tests__/Calendar.utils.test.ts
+++ src/features/bookOffer/components/__tests__/Calendar.utils.test.ts
@@ -1,9 +1,9 @@
 import range from 'lodash/range'
 import timezoneMock, { TimeZone } from 'timezone-mock'
 
-import { getListOfDatesInMonth } from 'features/bookOffer/components/Calendar/Calendar.utils'
+import { getDatesInMonth } from 'features/bookOffer/components/Calendar/Calendar.utils'
 
-describe('getListOfDatesInMonth()', () => {
+describe('getDatesInMonth()', () => {
   it.each`
     timezone
     ${'Australia/Adelaide'}
@@ -13,10 +13,10 @@ describe('getListOfDatesInMonth()', () => {
     'should return a list [1, 2, ..., N] for some specified month and year with timezone $timezone',
     ({ timezone }: { timezone: TimeZone }) => {
       timezoneMock.register(timezone)
-      expect(getListOfDatesInMonth(9, 2021)).toEqual(range(1, 31 + 1)) // october 2021 has 31 days
-      expect(getListOfDatesInMonth(10, 2021)).toEqual(range(1, 30 + 1)) // november 2021 has 30 days
-      expect(getListOfDatesInMonth(1, 2009)).toEqual(range(1, 28 + 1)) // february 2009 has 28 days
-      expect(getListOfDatesInMonth(1, 2008)).toEqual(range(1, 29 + 1)) // february 2008 has 29 days
+      expect(getDatesInMonth(9, 2021)).toEqual(range(1, 31 + 1)) // october 2021 has 31 days
+      expect(getDatesInMonth(10, 2021)).toEqual(range(1, 30 + 1)) // november 2021 has 30 days
+      expect(getDatesInMonth(1, 2009)).toEqual(range(1, 28 + 1)) // february 2009 has 28 days
+      expect(getDatesInMonth(1, 2008)).toEqual(range(1, 29 + 1)) // february 2008 has 29 days
     }
   )
 })
