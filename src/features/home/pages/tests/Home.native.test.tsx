ef938551ccc8d38af946311037e179060774cea3
Antoine Garcia <a.garcia.walecha@gmail.com>
2021-12-17 18:33:07 +0100

    test(home): simplify home test suite (#2313)

--- src/features/home/pages/tests/Home.native.test.tsx
+++ src/features/home/pages/tests/Home.native.test.tsx
@@ -1,15 +1,10 @@
-import { rest } from 'msw'
 import React from 'react'
-import waitForExpect from 'wait-for-expect'
 
 import { useRoute } from '__mocks__/@react-navigation/native'
 import { UserProfileResponse } from 'api/gen'
-import { AuthContext } from 'features/auth/AuthContext'
 import { analytics } from 'libs/analytics'
 import { env } from 'libs/environment'
-import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { server } from 'tests/server'
-import { flushAllPromises, superFlushWithAct, render, act } from 'tests/utils'
+import { flushAllPromises, render } from 'tests/utils'
 
 import { Home } from '../Home'
 
@@ -19,92 +14,87 @@ jest.mock('features/home/pages/useShowSkeleton', () => ({
 
 useRoute.mockImplementation(() => ({ params: { entryId: 'specific_entry_id' } }))
 
-function simulateAuthenticatedUser(partialUser?: Partial<UserProfileResponse>) {
-  const user = {
-    email: 'email@domain.ext',
-    firstName: 'Jean',
-    isBeneficiary: true,
-    depositExpirationDate: '2023-02-16T17:16:04.735235',
-    domainsCredit: {
-      all: { initial: 50000, remaining: 49600 },
-      physical: { initial: 20000, remaining: 0 },
-      digital: { initial: 20000, remaining: 19600 },
-    },
-    ...(partialUser || {}),
-  }
-  server.use(
-    rest.get<UserProfileResponse>(env.API_BASE_URL + '/native/v1/me', (req, res, ctx) =>
-      res(ctx.status(200), ctx.json(user))
-    )
-  )
-}
+let mockUserProfileInfo: Partial<UserProfileResponse> | undefined = undefined
+jest.mock('features/home/api', () => ({
+  useUserProfileInfo: jest.fn(() => ({ data: mockUserProfileInfo })),
+  useHomepageModules: () => [],
+}))
 
 describe('Home component', () => {
+  afterEach(jest.clearAllMocks)
+  beforeEach(() => {
+    mockUserProfileInfo = {
+      email: 'email@domain.ext',
+      firstName: 'Jean',
+      isBeneficiary: true,
+      depositExpirationDate: new Date('2023-02-16T17:16:04.735235'),
+      domainsCredit: {
+        all: { initial: 50000, remaining: 49600 },
+        physical: { initial: 20000, remaining: 0 },
+        digital: { initial: 20000, remaining: 19600 },
+      },
+    }
+  })
+
   it('should render correctly without login modal', async () => {
+    mockUserProfileInfo = undefined
     env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING = false
-    const home = await homeRenderer({ isLoggedIn: false })
-    expect(home).toMatchSnapshot()
+    const { toJSON } = render(<Home />)
+    expect(toJSON()).toMatchSnapshot()
   })
 
-  it('should have a welcome message', async () => {
-    const { getByText } = await homeRenderer({ isLoggedIn: false })
-    const welcomeText = getByText('Bienvenue\u00a0!')
-    expect(welcomeText.props.children).toBe('Bienvenue\u00a0!')
+  it('should have a welcome message', () => {
+    mockUserProfileInfo = undefined
+    const { getByText } = render(<Home />)
+    getByText('Bienvenue\u00a0!')
   })
 
-  it('should have a personalized welcome message when user is logged in', async () => {
-    const { getByText } = await homeRenderer({ isLoggedIn: true })
-    await waitForExpect(() => {
-      expect(getByText('Bonjour Jean')).toBeTruthy()
-    })
+  it('should have a personalized welcome message when user is logged in', () => {
+    const { getByText } = render(<Home />)
+    getByText('Bonjour Jean')
   })
 
-  it('should show the available credit to the user - remaining', async () => {
-    const { getByText } = await homeRenderer({ isLoggedIn: true })
-    await waitForExpect(() => {
-      expect(getByText('Tu as 496 € sur ton pass')).toBeTruthy()
-    })
+  it('should show the available credit to the user - remaining', () => {
+    const { getByText } = render(<Home />)
+    getByText('Tu as 496 € sur ton pass')
   })
 
-  it('should show the available credit to the user - expired', async () => {
-    const { queryByText, getByText } = await homeRenderer({
-      isLoggedIn: true,
-      partialUser: { depositExpirationDate: new Date('2020-02-16T17:16:04.735235') },
-    })
-    await waitForExpect(() => {
-      expect(getByText('Ton crédit est expiré')).toBeTruthy()
-      expect(queryByText('Tu as 496 € sur ton pass')).toBeFalsy()
-    })
+  it('should show the available credit to the user - expired', () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    mockUserProfileInfo!.depositExpirationDate = new Date('2020-02-16T17:16:04.735235')
+    const { queryByText, getByText } = render(<Home />)
+    expect(getByText('Ton crédit est expiré')).toBeTruthy()
+    expect(queryByText('Tu as 496 € sur ton pass')).toBeFalsy()
   })
 
-  it('should show the available credit to the user - not logged in', async () => {
-    const { queryByText } = await homeRenderer({ isLoggedIn: false })
+  it('should show the available credit to the user - not logged in', () => {
+    mockUserProfileInfo = undefined
+    const { queryByText } = render(<Home />)
     expect(queryByText('Toute la culture à portée de main')).toBeTruthy()
   })
 
-  it('should show the available credit to the user - not beneficiary', async () => {
-    const { queryByText } = await homeRenderer({
-      isLoggedIn: true,
-      partialUser: { isBeneficiary: false },
-    })
+  it('should show the available credit to the user - not beneficiary', () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    mockUserProfileInfo!.isBeneficiary = false
+    const { queryByText } = render(<Home />)
     expect(queryByText('Toute la culture à portée de main')).toBeTruthy()
   })
 
   it('should not have code push button', async () => {
-    const home = await homeRenderer({})
-    expect(home.queryByText('Check update')).toBeFalsy()
+    const { queryByText } = render(<Home />)
+    expect(queryByText('Check update')).toBeFalsy()
   })
 
   it('should have CheatMenu button when FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING=true', async () => {
     env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING = true
-    const home = await homeRenderer({ isLoggedIn: false })
-    expect(home.queryByText('CheatMenu')).toBeTruthy()
+    const { queryByText } = render(<Home />)
+    expect(queryByText('CheatMenu')).toBeTruthy()
   })
 
   it('should NOT have CheatMenu button when NOT FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING=false', async () => {
     env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING = false
-    const home = await homeRenderer({ isLoggedIn: false })
-    expect(home.queryByText('CheatMenu')).toBeFalsy()
+    const { queryByText } = render(<Home />)
+    expect(queryByText('CheatMenu')).toBeFalsy()
   })
 })
 
@@ -120,75 +110,31 @@ describe('Home component - Analytics', () => {
     contentSize: { height: 1600 },
   }
 
-  it('should trigger logEvent "AllModulesSeen" when reaching the end', async () => {
-    const home = await homeRenderer({ isLoggedIn: false })
-    const scrollView = home.getByTestId('homeBodyScrollView')
-    await act(async () => {
-      await flushAllPromises()
-    })
+  it('should trigger logEvent "AllModulesSeen" when reaching the end', () => {
+    const { getByTestId } = render(<Home />)
+    const scrollView = getByTestId('homeBodyScrollView')
 
-    await act(async () => {
-      await scrollView.props.onScroll({ nativeEvent: nativeEventMiddle })
-    })
+    scrollView.props.onScroll({ nativeEvent: nativeEventMiddle })
     expect(analytics.logAllModulesSeen).not.toHaveBeenCalled()
 
-    await act(async () => {
-      await scrollView.props.onScroll({ nativeEvent: nativeEventBottom })
-    })
-
+    scrollView.props.onScroll({ nativeEvent: nativeEventBottom })
     expect(analytics.logAllModulesSeen).toHaveBeenCalledWith(0)
   })
 
-  it('should trigger logEvent "AllModulesSeen" only once', async () => {
-    const home = await homeRenderer({ isLoggedIn: false })
-    const scrollView = home.getByTestId('homeBodyScrollView')
+  it('should trigger logEvent "AllModulesSeen" only once', () => {
+    const { getByTestId } = render(<Home />)
+    const scrollView = getByTestId('homeBodyScrollView')
 
-    await act(async () => {
-      await flushAllPromises()
-    })
-
-    await act(async () => {
-      // 1st scroll to bottom => trigger
-      await scrollView.props.onScroll({ nativeEvent: nativeEventBottom })
-      await flushAllPromises()
-    })
+    // 1st scroll to bottom => trigger
+    scrollView.props.onScroll({ nativeEvent: nativeEventBottom })
     expect(analytics.logAllModulesSeen).toHaveBeenCalledWith(0)
 
-    // @ts-expect-error: logAllModulesSeen is the mock function but is seen as the real function
-    analytics.logAllModulesSeen.mockClear()
+    jest.clearAllMocks()
 
-    await act(async () => {
-      // 2nd scroll to bottom => NOT trigger
-      await scrollView.props.onScroll({ nativeEvent: nativeEventMiddle })
-      await scrollView.props.onScroll({ nativeEvent: nativeEventBottom })
-      await flushAllPromises()
-    })
-
+    // 2nd scroll to bottom => NOT trigger
+    scrollView.props.onScroll({ nativeEvent: nativeEventMiddle })
+    scrollView.props.onScroll({ nativeEvent: nativeEventBottom })
+    flushAllPromises()
     expect(analytics.logAllModulesSeen).not.toHaveBeenCalled()
   })
 })
-
-interface Params {
-  isLoggedIn?: boolean | undefined
-  partialUser?: Partial<UserProfileResponse>
-}
-
-const defaultParams: Params = {
-  isLoggedIn: false,
-  partialUser: {},
-}
-
-async function homeRenderer({ isLoggedIn, partialUser }: Params = defaultParams) {
-  if (isLoggedIn) simulateAuthenticatedUser(partialUser)
-  const renderAPI = render(<Home />, {
-    wrapper: ({ children }) =>
-      // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-      reactQueryProviderHOC(
-        <AuthContext.Provider value={{ isLoggedIn: !!isLoggedIn, setIsLoggedIn: jest.fn() }}>
-          {children}
-        </AuthContext.Provider>
-      ),
-  })
-  await superFlushWithAct(50)
-  return renderAPI
-}
