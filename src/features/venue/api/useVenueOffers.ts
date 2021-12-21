48efa1c54542197722efef8cd119fb7e964c322a
Yorick Vinesse <44037911+yorickeando@users.noreply.github.com>
2021-12-21 11:30:40 +0100

    :bug: (pc-11811) filter offers based on underage (#2325)

--- src/features/venue/api/useVenueOffers.ts
+++ src/features/venue/api/useVenueOffers.ts
@@ -1,7 +1,7 @@
 import uniqBy from 'lodash.uniqby'
 import { useQuery } from 'react-query'
 
-import { useIsUserUnderageBeneficiary } from 'features/profile/utils'
+import { useIsUserUnderage } from 'features/profile/utils'
 import { useVenueSearchParameters } from 'features/venue/api/useVenueSearchParameters'
 import { fetchAlgolia, filterAlgoliaHit, useTransformAlgoliaHits } from 'libs/algolia/fetchAlgolia'
 import { QueryKeys } from 'libs/queryKeys'
@@ -10,11 +10,11 @@ import { SearchHit } from 'libs/search'
 export const useVenueOffers = (venueId: number) => {
   const transformHits = useTransformAlgoliaHits()
   const params = useVenueSearchParameters(venueId)
-  const isUserUnderageBeneficiary = useIsUserUnderageBeneficiary()
+  const isUserUnderage = useIsUserUnderage()
 
   return useQuery(
     [QueryKeys.VENUE_OFFERS, venueId],
-    () => fetchAlgolia({ ...params, page: 0 }, null, isUserUnderageBeneficiary),
+    () => fetchAlgolia({ ...params, page: 0 }, null, isUserUnderage),
     {
       select: ({ hits, nbHits }) => ({
         hits: uniqBy(hits.filter(filterAlgoliaHit).map(transformHits), 'objectID') as SearchHit[],
