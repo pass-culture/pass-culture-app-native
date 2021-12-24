d9c35e4ccf67e7d13a3745b956cc7befbc83b22f
bruno ebstein <bruno.ebstein@passculture.app>
2021-12-24 17:51:35 +0100

    test(AppModal): retro test AppModal component

---
+++ src/ui/components/modals/__tests__/fixture.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
+import { Close } from 'ui/svg/icons/Close'
+
+import { AppModal } from '../AppModal'
+
+export type AppModalProps = Parameters<typeof AppModal>[0]
+
+export const defaultProps: AppModalProps = {
+  title: 'Some title',
+  children: <Text>Hello World</Text>,
+
+  visible: true,
+
+  leftIconAccessibilityLabel: undefined,
+  leftIcon: undefined,
+  onLeftIconPress: undefined,
+
+  rightIconAccessibilityLabel: undefined,
+  rightIcon: undefined,
+  onRightIconPress: undefined,
+}
+
+export const leftIconCallbackMock = jest.fn()
+export const leftIconProps = {
+  leftIconAccessibilityLabel: "Revenir à l'étape précédente",
+  leftIcon: ArrowPrevious,
+  onLeftIconPress: leftIconCallbackMock,
+}
+
+export const rightIconCallbackMock = jest.fn()
+export const rightIconProps = {
+  rightIconAccessibilityLabel: 'Fermer la modale et annuler',
+  rightIcon: Close,
+  onRightIconPress: rightIconCallbackMock,
+}
