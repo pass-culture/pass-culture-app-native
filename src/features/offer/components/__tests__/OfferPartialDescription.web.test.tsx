b4107b55e55d0c70c77f4477643ed923fd14efa0
bruno ebstein <bruno.ebstein@passculture.app>
2021-12-28 18:06:11 +0100

    test(offer): refactor and add tests

--- src/features/offer/components/__tests__/OfferPartialDescription.web.test.tsx
+++ src/features/offer/components/__tests__/OfferPartialDescription.web.test.tsx
@@ -7,57 +7,116 @@ import { render } from 'tests/utils/web'
 
 import { OfferPartialDescription } from '../OfferPartialDescription'
 
-const description =
+const defaultDescription =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua: https://google.com'
+const offerId = 123
+type Setup = (queryClient: QueryClient) => void
+const defaultSetup: Setup = (queryClient) => {
+  queryClient.removeQueries()
+  queryClient.setQueryData(['offer', offerId], offerResponseSnap)
+}
+type Params = {
+  description?: string
+  setup: Setup
+}
+const defaultParams: Params = {
+  description: defaultDescription,
+  setup: defaultSetup,
+}
 
 describe('OfferPartialDescription', () => {
-  it('centers CTA when provided description is empty', async () => {
-    const { getByTestId } = await renderOfferDescription('')
+  it('centers CTA when provided description is empty', () => {
+    const { getByTestId } = renderOfferDescription({
+      ...defaultParams,
+      description: '',
+    })
     const offerSeeMoreContainer = getByTestId('offerSeeMoreContainer')
     // @ts-expect-error FIXME see how to fix typing
     const { 'align-self': alignSelf } = offerSeeMoreContainer.style
     expect(alignSelf).toBe('center')
   })
-  it('places CTA on flex-end when provided a description', async () => {
-    const { getByTestId } = await renderOfferDescription(description)
+  it('places CTA on flex-end when provided a description', () => {
+    const { getByTestId } = renderOfferDescription(defaultParams)
     const offerSeeMoreContainer = getByTestId('offerSeeMoreContainer')
     // @ts-expect-error FIXME see how to fix typing
     const { 'align-self': alignSelf } = offerSeeMoreContainer.style
     expect(alignSelf).toBe('flex-end')
   })
-  it('renders externalLinks if http(s) url are present in the description', async () => {
-    const { queryByTestId } = await renderOfferDescription(description)
+  it('renders externalLinks if http(s) url are present in the description', () => {
+    const { queryByTestId } = renderOfferDescription(defaultParams)
     expect(queryByTestId('externalSiteIcon')).toBeTruthy()
   })
-  it("shouldn't render an empty line and a spacer when description is undefined", async () => {
-    const { queryByTestId } = await renderOfferDescription(undefined)
+  it("shouldn't render an empty line and a spacer when description is empty", () => {
+    const { queryByTestId } = renderOfferDescription({
+      ...defaultParams,
+      description: undefined,
+    })
     expect(queryByTestId('offerPartialDescriptionBody')).toBeFalsy()
   })
-  it("shouldn't render the SeeMore button if no content is on description page", async () => {
-    const { queryByTestId } = render(
-      // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-      reactQueryProviderHOC(
-        <OfferPartialDescription id={offerId} description={undefined} />,
-        (queryClient: QueryClient) => {
-          queryClient.setQueryData(['offer', offerId], {
-            ...offerResponseSnap,
-            image: {},
-            extraData: {},
-          })
-        }
-      )
-    )
-    expect(queryByTestId('offerSeeMoreContainer')).toBeFalsy()
+  describe('SeeMore button', () => {
+    const setupWithNoDataInDescriptionPage: Setup = (queryClient) => {
+      queryClient.setQueryData(['offer', offerId], {
+        ...offerResponseSnap,
+        image: {},
+        extraData: {},
+      })
+    }
+
+    it('should be rendered when there is some content on the description page', () => {
+      const { queryByTestId } = renderOfferDescription(defaultParams)
+      expect(queryByTestId('offerSeeMoreContainer')).toBeTruthy()
+    })
+    describe('should be rendered', () => {
+      it('when there is description on the description page', () => {
+        const { queryByTestId } = renderOfferDescription({
+          ...defaultParams,
+          setup: (queryClient) => {
+            queryClient.setQueryData(['offer', offerId], {
+              image: {},
+              extraData: {},
+            })
+          },
+        })
+        expect(queryByTestId('offerSeeMoreContainer')).toBeTruthy()
+      })
+      it('when there is image on the description page', () => {
+        const { queryByTestId } = renderOfferDescription({
+          ...defaultParams,
+          description: undefined,
+          setup: (queryClient) => {
+            queryClient.setQueryData(['offer', offerId], {
+              image: offerResponseSnap.image,
+              extraData: {},
+            })
+          },
+        })
+        expect(queryByTestId('offerSeeMoreContainer')).toBeTruthy()
+      })
+      it('when there is extraData on the description page', () => {
+        const { queryByTestId } = renderOfferDescription({
+          ...defaultParams,
+          description: undefined,
+          setup: (queryClient) => {
+            queryClient.setQueryData(['offer', offerId], {
+              image: {},
+              extraData: { author: 'John Lang' },
+            })
+          },
+        })
+        expect(queryByTestId('offerSeeMoreContainer')).toBeTruthy()
+      })
+    })
+    it("shouldn't be rendered when there is no content on the description page", () => {
+      const { queryByTestId } = renderOfferDescription({
+        description: undefined,
+        setup: setupWithNoDataInDescriptionPage,
+      })
+      expect(queryByTestId('offerSeeMoreContainer')).toBeFalsy()
+    })
   })
 })
 
-const offerId = 123
-const renderOfferDescription = async (description?: string) => {
-  const setup = (queryClient: QueryClient) => {
-    queryClient.removeQueries()
-    queryClient.setQueryData(['offer', offerId], offerResponseSnap)
-  }
-
+const renderOfferDescription = ({ description, setup }: Params) => {
   const wrapper = render(
     // eslint-disable-next-line local-rules/no-react-query-provider-hoc
     reactQueryProviderHOC(<OfferPartialDescription id={offerId} description={description} />, setup)
