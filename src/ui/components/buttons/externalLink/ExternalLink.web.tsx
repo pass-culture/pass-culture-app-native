9dab3da7b77c70629682d72f5f1482c7c037c2fd
Marion Barré <marionb@bam.tech>
2022-01-03 16:32:21 +0100

    :sparkles: (externalLink) make icon stick to text

---
+++ src/ui/components/buttons/externalLink/ExternalLink.web.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+import { openUrl } from 'features/navigation/helpers'
+import { extractExternalLinkParts } from 'ui/components/buttons/externalLink/ExternalLink.service'
+import { ExternalSite } from 'ui/svg/icons/ExternalSite'
+import { ColorsEnum, Spacer, Typo } from 'ui/theme'
+
+interface Props {
+  url: string
+  text?: string
+  color?: ColorsEnum
+  testID?: string
+}
+
+export const ExternalLink: React.FC<Props> = ({ url, text, color, testID }) => {
+  text = text || url
+
+  const [firstWord, remainingWords] = extractExternalLinkParts(text)
+
+  return (
+    <Typo.ButtonText color={color} onPress={() => openUrl(url)} testID={testID}>
+      <Spacer.Row numberOfSpaces={1} />
+      <Text>
+        <ExternalSite size={14} color={color} testID="externalSiteIcon" />
+        {firstWord}
+      </Text>
+      {remainingWords}
+    </Typo.ButtonText>
+  )
+}
+
+const Text = styled.Text({
+  whiteSpace: 'nowrap',
+})
