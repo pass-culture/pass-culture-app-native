3007b9e5e25bb9b93634cf0346763ef267defa69
Antoine Garcia <antoine.garcia@polyconseil.fr>
2022-01-04 16:39:47 +0100

    refactor(lodash): prefer specific import of lodash libs

--- src/features/bookOffer/components/BookHourChoice.tsx
+++ src/features/bookOffer/components/BookHourChoice.tsx
@@ -1,5 +1,5 @@
 import { t } from '@lingui/macro'
-import { debounce } from 'lodash'
+import debounce from 'lodash/debounce'
 import React, { useMemo, useRef } from 'react'
 import { TouchableOpacity } from 'react-native'
 import styled from 'styled-components/native'
