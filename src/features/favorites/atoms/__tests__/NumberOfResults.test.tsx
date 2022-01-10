243a3fd919f7ad0bb99e59e10372bfecab90b51a
bruno ebstein <bruno.ebstein@passculture.app>
2022-01-10 15:14:18 +0100

    [PC-12698] chore(node): bump NodeJS from 12 to 16
    There is a breaking change that replace number formatting from `1,234` to `1 234` with a small non breaking space character https://unicode-table.com/fr/202F/

--- src/features/favorites/atoms/__tests__/NumberOfResults.test.tsx
+++ src/features/favorites/atoms/__tests__/NumberOfResults.test.tsx
@@ -9,6 +9,6 @@ describe('NumberOfResults component', () => {
     expect(render(<NumberOfResults nbFavorites={0} />).toJSON()).toBeNull()
     expect(render(<NumberOfResults nbFavorites={1} />).getByText('1 favori')).toBeTruthy()
     expect(render(<NumberOfResults nbFavorites={2} />).getByText('2 favoris')).toBeTruthy()
-    expect(render(<NumberOfResults nbFavorites={1234} />).getByText('1,234 favoris')).toBeTruthy()
+    expect(render(<NumberOfResults nbFavorites={1234} />).getByText('1 234 favoris')).toBeTruthy()
   })
 })
