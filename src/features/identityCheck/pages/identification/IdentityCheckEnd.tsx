2eb0ab18f2fe5f210f4a8e64176d178bf8cdf610
Lucas Beneston <lucas.beneston@passculture.app>
2022-01-11 12:59:30 +0100

    (PC-12724) feat(identityCheck): add navigateToHome if for IdentityCheckEnd

--- src/features/identityCheck/pages/identification/IdentityCheckEnd.tsx
+++ src/features/identityCheck/pages/identification/IdentityCheckEnd.tsx
@@ -4,14 +4,20 @@ import React, { useEffect } from 'react'
 import { useIdentityCheckNavigation } from 'features/identityCheck/useIdentityCheckNavigation'
 import { GenericInfoPage } from 'ui/components/GenericInfoPage'
 import { EmailSent } from 'ui/svg/icons/EmailSent'
+import { useNextSubscriptionStep } from 'features/auth/signup/nextSubscriptionStep'
+import { navigateToHome } from 'features/navigation/helpers'
 
 export const IdentityCheckEnd = () => {
+  const { data: subscription } = useNextSubscriptionStep()
   const { navigateToNextScreen } = useIdentityCheckNavigation()
 
   useEffect(() => {
-    const timeout = setTimeout(navigateToNextScreen, 3000)
+    const timeout = setTimeout(
+      !subscription?.nextSubscriptionStep ? navigateToHome : navigateToNextScreen,
+      3000
+    )
     return () => clearTimeout(timeout)
-  }, [])
+  }, [subscription?.nextSubscriptionStep])
 
   return (
     <GenericInfoPage title={t`Ta pièce d’identité a bien été transmise\u00a0!`} icon={EmailSent} />
--- src/features/identityCheck/pages/identification/__test__/IdentityCheckEnd.test.tsx
+++ src/features/identityCheck/pages/identification/__test__/IdentityCheckEnd.test.tsx
@@ -2,12 +2,25 @@ import React from 'react'
 
 import { IdentityCheckEnd } from 'features/identityCheck/pages/identification/IdentityCheckEnd'
 import { render } from 'tests/utils'
+import { IdentityCheckMethod, NextSubscriptionStepResponse, SubscriptionStep } from 'api/gen'
 
 const mockNavigateToNextScreen = jest.fn()
 jest.mock('features/identityCheck/useIdentityCheckNavigation', () => ({
   useIdentityCheckNavigation: () => ({
     navigateToNextScreen: mockNavigateToNextScreen,
   }),
+}))
+
+let mockNextSubscriptionStep: NextSubscriptionStepResponse = {
+  allowedIdentityCheckMethods: [IdentityCheckMethod.ubble],
+  nextSubscriptionStep: SubscriptionStep['honor-statement'],
+  hasIdentityCheckPending: false,
+}
+
+jest.mock('features/auth/signup/nextSubscriptionStep', () => ({
+  useNextSubscriptionStep: jest.fn(() => ({
+    data: mockNextSubscriptionStep,
+  })),
 }))
 
 describe('<IdentityCheckEnd/>', () => {
