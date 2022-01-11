c9c5ca8431223eeff18ce91e281d1a4e836704c9
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2022-01-11 14:52:08 +0100

    (PC-12724) feat(identityCheck): add navigateToHome if !nextSubcriptionStep for IdentityCheck (#2463)

--- src/features/identityCheck/pages/identification/IdentityCheckEnd.tsx
+++ src/features/identityCheck/pages/identification/IdentityCheckEnd.tsx
@@ -1,17 +1,23 @@
 import { t } from '@lingui/macro'
 import React, { useEffect } from 'react'
 
+import { useNextSubscriptionStep } from 'features/auth/signup/nextSubscriptionStep'
 import { useIdentityCheckNavigation } from 'features/identityCheck/useIdentityCheckNavigation'
+import { navigateToHome } from 'features/navigation/helpers'
 import { GenericInfoPage } from 'ui/components/GenericInfoPage'
 import { EmailSent } from 'ui/svg/icons/EmailSent'
 
 export const IdentityCheckEnd = () => {
+  const { data: subscription } = useNextSubscriptionStep()
   const { navigateToNextScreen } = useIdentityCheckNavigation()
 
   useEffect(() => {
-    const timeout = setTimeout(navigateToNextScreen, 3000)
+    const timeout = setTimeout(
+      !subscription?.nextSubscriptionStep ? navigateToHome : navigateToNextScreen,
+      3000
+    )
     return () => clearTimeout(timeout)
-  }, [])
+  }, [subscription?.nextSubscriptionStep])
 
   return (
     <GenericInfoPage title={t`Ta pièce d’identité a bien été transmise\u00a0!`} icon={EmailSent} />
