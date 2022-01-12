7ef501cef351934fd8d8d6118a99508ebceb06bf
Marion Valentin <43779561+marionnousvalentin@users.noreply.github.com>
2022-01-12 15:33:19 +0100

    feat(LandscapeMode): do not set landscape mode when keybord is open (#2469)

--- src/features/landscapePosition/useIsLandscapePosition.web.tsx
+++ src/features/landscapePosition/useIsLandscapePosition.web.tsx
@@ -4,8 +4,11 @@ import { useWindowDimensions } from 'react-native'
 // eslint-disable-next-line no-restricted-imports
 import { isDesktopDeviceDetectOnWeb } from 'libs/react-device-detect'
 
+// is usefull to avoid detecting landscape mode while keyboard is open on portrait mode
+const landscapeModeMargin = 30
+
 export const useIsLandscapePosition = (): boolean => {
   const { width, height } = useWindowDimensions()
   if (isDesktopDeviceDetectOnWeb) return false
-  return width >= height
+  return width > height + landscapeModeMargin
 }
