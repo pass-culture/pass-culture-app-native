e5f816664f051c63ff5375642132c3cc06d22e53
Yorick Vinesse <yorick.vinesse@gmail.com>
2022-01-25 11:46:34 +0100

    :recycle: (pc-12881) add UserError illu

--- src/features/cheatcodes/pages/AppComponents/Illustrations.tsx
+++ src/features/cheatcodes/pages/AppComponents/Illustrations.tsx
@@ -34,6 +34,7 @@ import { SadFaceDeprecated } from 'ui/svg/icons/SadFace_deprecated'
 import { TicketBooked } from 'ui/svg/icons/TicketBooked'
 import { TicketBookedDeprecated } from 'ui/svg/icons/TicketBooked_deprecated'
 import { IconInterface } from 'ui/svg/icons/types'
+import { UserError } from 'ui/svg/icons/UserError'
 import { UserErrorDeprecated } from 'ui/svg/icons/UserError_deprecated'
 // eslint-disable-next-line no-restricted-imports
 import { ColorsEnum } from 'ui/theme/colors'
@@ -98,6 +99,7 @@ export const Illustrations: FunctionComponent = () => {
       </AlignedText>
       <Illustration name="TicketBooked" component={TicketBooked} isNew />
       <Illustration name="UserErrorDeprecated" component={UserErrorDeprecated} />
+      <Illustration name="UserError" component={UserError} isNew />
     </React.Fragment>
   )
 }
