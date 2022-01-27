80305889749cdc0d4e14b6532d32389eba3fb484
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-01-27 15:23:58 +0100

    (PC-12840) feat(accessibility): Make web TabBar the first accessible item with the Tab key

---
+++ src/features/navigation/RootNavigator/Header/AccessibleTabBar.web.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { navigateFromRef } from 'features/navigation/navigationRef'
+import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
+import { mapTabRouteToBicolorIcon } from 'features/navigation/TabBar/mapTabRouteToBicolorIcon'
+import { TabBarComponent } from 'features/navigation/TabBar/TabBarComponent'
+import { TabBarContainer } from 'features/navigation/TabBar/TabBarContainer'
+import { useTabNavigationContext } from 'features/navigation/TabBar/TabNavigationStateContext'
+import { TabParamList, TabRouteName } from 'features/navigation/TabBar/types'
+import { Link } from 'ui/web/link/Link'
+
+export const AccessibleTabBar = () => {
+  const { tabRoutes } = useTabNavigationContext()
+
+  const onPress = (name: keyof TabParamList) => {
+    navigateFromRef(...getTabNavConfig(name, undefined))
+  }
+
+  return (
+    <AccessibleTabBarContainer>
+      <TabBarContainer>
+        {tabRoutes.map((route) => {
+          return (
+            <LinkContainer key={route.name}>
+              <Link to={{ screen: route.name, params: undefined }}>
+                <TabBarComponent
+                  tabName={route.name}
+                  isSelected={route.isSelected}
+                  BicolorIcon={mapTabRouteToBicolorIcon(route.name as TabRouteName)}
+                  onPress={() => onPress(route.name)}
+                />
+              </Link>
+            </LinkContainer>
+          )
+        })}
+      </TabBarContainer>
+    </AccessibleTabBarContainer>
+  )
+}
+
+const AccessibleTabBarContainer = styled.nav(({ theme }) => ({
+  zIndex: theme.zIndex.tabBar,
+}))
+
+const LinkContainer = styled.div({
+  display: 'flex',
+  flex: 1,
+  '> *': {
+    display: 'flex',
+    flex: 1,
+  },
+})
