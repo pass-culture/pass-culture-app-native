6f04a6373b92bbc9b6831f73144dfbe7954d30ba
Yorick Vinesse <yorick.vinesse@gmail.com>
2022-01-28 11:11:36 +0100

    :sparkles: (pc-13100) replace button wih Linear CTA and improve readability

--- src/features/bookings/components/BookingDetailsTicketContent.tsx
+++ src/features/bookings/components/BookingDetailsTicketContent.tsx
@@ -12,7 +12,7 @@ import {
 import { getBookingProperties } from 'features/bookings/helpers'
 import { openUrl } from 'features/navigation/helpers'
 import { useCategoryId, useSubcategory } from 'libs/subcategories'
-import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { ButtonWithLinearGradient } from 'ui/components/buttons/ButtonWithLinearGradient'
 import { getSpacing, Typo } from 'ui/theme'
 // eslint-disable-next-line no-restricted-imports
 import { ColorsEnum } from 'ui/theme/colors'
@@ -32,18 +32,23 @@ export const BookingDetailsTicketContent = (props: BookingDetailsTicketContentPr
       openUrl(offer.url, { analyticsData: { offerId: offer.id } })
     }
   }
+  const categoryId = useCategoryId(offer.subcategoryId)
+  const shouldDisplayEAN = offer.extraData?.isbn && categoryId === CategoryIdEnum.LIVRE
 
   const accessOfferButton = (
-    <InnerButtonContainer>
-      <ButtonPrimary wording={t`Accéder à l'offre`} onPress={accessExternalOffer} />
-    </InnerButtonContainer>
+    <ButtonWithLinearGradient
+      wording={t`Accéder à l'offre`}
+      isExternal
+      isDisabled={false}
+      onPress={accessExternalOffer}
+    />
   )
 
   const isDigitalAndActivationCodeEnabled =
     activationCodeFeatureEnabled && properties.hasActivationCode
 
   return (
-    <TicketContent>
+    <View>
       <Title>{offer.name}</Title>
       <TicketInnerContent>
         {isDigitalAndActivationCodeEnabled ? (
@@ -59,49 +64,39 @@ export const BookingDetailsTicketContent = (props: BookingDetailsTicketContentPr
             ) : booking.qrCodeData ? (
               <QrCodeView qrCodeData={booking.qrCodeData} />
             ) : null}
-            <Ean offer={offer} />
           </React.Fragment>
         )}
       </TicketInnerContent>
-    </TicketContent>
+      {!!shouldDisplayEAN && <Ean offer={offer} />}
+    </View>
   )
 }
 
 type EanProps = {
   offer: BookingOfferResponse
 }
-const Ean = ({ offer }: EanProps) => {
-  const categoryId = useCategoryId(offer.subcategoryId)
-  const shouldDisplayEAN = offer.extraData?.isbn && categoryId === CategoryIdEnum.LIVRE
-  return shouldDisplayEAN ? (
-    <EANContainer>
-      <Typo.Caption>{t`EAN` + '\u00a0'}</Typo.Caption>
-      <Typo.Body color={ColorsEnum.GREY_DARK}>{offer.extraData?.isbn}</Typo.Body>
-    </EANContainer>
-  ) : null
-}
+const Ean = ({ offer }: EanProps) => (
+  <EANContainer>
+    <Typo.Caption>{t`EAN` + '\u00a0'}</Typo.Caption>
+    <DarkGreyCaption>{offer.extraData?.isbn}</DarkGreyCaption>
+  </EANContainer>
+)
 
 type QrCodeViewProps = {
   qrCodeData: string
 }
-const QrCodeView = ({ qrCodeData }: QrCodeViewProps) => {
-  return (
-    <View testID="qr-code">
-      <QRCode value={qrCodeData} size={QR_CODE_SIZE} />
-    </View>
-  )
-}
+const QrCodeView = ({ qrCodeData }: QrCodeViewProps) => (
+  <QrCodeContainer testID="qr-code">
+    <QRCode value={qrCodeData} size={QR_CODE_SIZE} />
+  </QrCodeContainer>
+)
 
-const TicketContent = styled.View({
-  paddingHorizontal: getSpacing(7),
-  paddingVertical: getSpacing(2),
-  alignItems: 'center',
-  minHeight: TICKET_MIN_HEIGHT,
-  // Improve Web spacings using non default View boxSizing
-  ...(Platform.OS === 'web' ? { boxSizing: 'content-box' } : {}),
-})
+const DarkGreyCaption = styled(Typo.Caption)(({ theme }) => ({
+  color: theme.colors.greyDark,
+}))
 
 const Title = styled(Typo.Title3)({
+  marginTop: getSpacing(2),
   paddingHorizontal: getSpacing(1),
   textAlign: 'center',
   maxWidth: '100%',
@@ -114,17 +109,23 @@ const Token = styled(Typo.Title4)({
 })
 
 const TicketInnerContent = styled.View({
-  alignItems: 'center',
   justifyContent: 'center',
-  flexGrow: 1,
+  paddingHorizontal: getSpacing(7),
+  paddingTop: getSpacing(2),
+  minHeight: TICKET_MIN_HEIGHT,
+  // Improve Web spacings using non default View boxSizing
+  ...(Platform.OS === 'web' ? { boxSizing: 'content-box' } : {}),
 })
 
-const InnerButtonContainer = styled.View({
-  flexDirection: 'row',
+const QrCodeContainer = styled.View({
+  alignItems: 'center',
+  marginBottom: getSpacing(3),
 })
 
 const EANContainer = styled.View({
-  paddingTop: getSpacing(2.5),
   flexDirection: 'row',
+  marginBottom: getSpacing(2),
+  marginTop: getSpacing(3),
   alignItems: 'center',
+  justifyContent: 'center',
 })
