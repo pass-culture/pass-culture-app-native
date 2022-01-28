c53ca04208c4be99228f77437e5412ada3aa9134
bruno ebstein <bruno.ebstein@passculture.app>
2022-01-28 17:54:22 +0100

    (PC-13138) fix(bookings): crash with new subcategories (#2554)
    * test(bookings): retro test
    
    * (PC-13138) fix(bookings): crash with new subcategories

--- src/features/bookings/components/OnGoingBookingsList.tsx
+++ src/features/bookings/components/OnGoingBookingsList.tsx
@@ -13,6 +13,7 @@ import {
 import { analytics, isCloseToBottom } from 'libs/analytics'
 import useFunctionOnce from 'libs/hooks/useFunctionOnce'
 import { useIsFalseWithDelay } from 'libs/hooks/useIsFalseWithDelay'
+import { useSubcategories } from 'libs/subcategories/useSubcategories'
 import { Separator } from 'ui/components/Separator'
 import { getSpacing, Typo } from 'ui/theme'
 // eslint-disable-next-line no-restricted-imports
@@ -30,7 +31,8 @@ const ANIMATION_DURATION = 700
 export function OnGoingBookingsList() {
   const { data: bookings, isLoading, isFetching, refetch } = useBookings()
   const { bottom } = useSafeAreaInsets()
-  const showSkeleton = useIsFalseWithDelay(isLoading, ANIMATION_DURATION)
+  const { isLoading: subcategoriesIsLoading } = useSubcategories()
+  const showSkeleton = useIsFalseWithDelay(isLoading || subcategoriesIsLoading, ANIMATION_DURATION)
   const isRefreshing = useIsFalseWithDelay(isFetching, ANIMATION_DURATION)
 
   const {
@@ -130,7 +132,7 @@ function BookingsPlaceholder() {
   const ListFooterComponent = useMemo(() => <Footer />, [])
 
   return (
-    <LoadingContainer>
+    <LoadingContainer testID="BookingsPlaceholder">
       <FlatList
         data={BOOKINGS_LIST_PLACEHOLDER}
         renderItem={renderItem}
