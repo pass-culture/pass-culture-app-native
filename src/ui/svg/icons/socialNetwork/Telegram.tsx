2c6fa6400894a5b7271f9cda0c3603c66460e99a
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-02-02 17:24:01 +0400

    (PC-13246) fix(SVG): Fix error in JS console for fill-color, fill-rule, clip-rule and theme usage

--- src/ui/svg/icons/socialNetwork/Telegram.tsx
+++ src/ui/svg/icons/socialNetwork/Telegram.tsx
@@ -1,17 +1,12 @@
-import * as React from 'react'
+import React from 'react'
 import Svg, { Defs, LinearGradient, Path, Stop } from 'react-native-svg'
+import styled from 'styled-components/native'
 
 import { svgIdentifier } from 'ui/svg/utils'
-// eslint-disable-next-line no-restricted-imports
-import { ColorsEnum } from 'ui/theme/colors'
 
 import { IconInterface } from '../types'
 
-export const Telegram: React.FunctionComponent<IconInterface> = ({
-  size = 32,
-  color: _color = ColorsEnum.BLACK,
-  testID,
-}) => {
+const TelegramSvg: React.FunctionComponent<IconInterface> = ({ size, testID }) => {
   const { id: gradientId, fill: gradientFill } = svgIdentifier()
 
   return (
@@ -21,8 +16,8 @@ export const Telegram: React.FunctionComponent<IconInterface> = ({
         fill={gradientFill}
       />
       <Path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M10.8509 23.7402C17.8451 20.7027 22.5012 18.6844 24.8393 17.7052C31.4937 14.9276 32.8926 14.448 33.7918 14.428C33.9916 14.428 34.4313 14.4679 34.731 14.7077C34.9708 14.9076 35.0308 15.1674 35.0707 15.3672C35.1107 15.567 35.1507 15.9867 35.1107 16.3064C34.751 20.1032 33.1923 29.3156 32.393 33.552C32.0533 35.3505 31.3938 35.95 30.7543 36.01C29.3555 36.1299 28.2964 35.0908 26.9575 34.2115C24.8393 32.8326 23.6603 31.9734 21.602 30.6145C19.224 29.0558 20.7627 28.1965 22.1215 26.7977C22.4812 26.438 28.6161 20.8426 28.736 20.343C28.756 20.2831 28.756 20.0433 28.6161 19.9234C28.4762 19.8035 28.2764 19.8435 28.1165 19.8834C27.8967 19.9234 24.5395 22.1615 18.005 26.5779C17.0458 27.2373 16.1865 27.557 15.4071 27.5371C14.5478 27.5171 12.9092 27.0574 11.6702 26.6578C10.1715 26.1782 8.9725 25.9184 9.07241 25.0791C9.13236 24.6395 9.73186 24.1998 10.8509 23.7402Z"
         fill="white"
       />
@@ -34,10 +29,14 @@ export const Telegram: React.FunctionComponent<IconInterface> = ({
           x2="23.98"
           y2="47.6203"
           gradientUnits="userSpaceOnUse">
-          <Stop stop-color="#2AABEE" />
-          <Stop offset="1" stop-color="#229ED9" />
+          <Stop stopColor="#2AABEE" />
+          <Stop offset="1" stopColor="#229ED9" />
         </LinearGradient>
       </Defs>
     </Svg>
   )
 }
+
+export const Telegram = styled(TelegramSvg).attrs(({ size, theme }) => ({
+  size: size ?? theme.icons.sizes.standard,
+}))``
