aa3075aa6d440ce4e571c74671895cc6541536a3
Marion Barré <marionb@bam.tech>
2022-02-02 11:23:57 +0100

    :recycle: (ScrollButtonForNotTouchDevice) use themee

--- src/ui/components/buttons/ScrollButtonForNotTouchDevice.tsx
+++ src/ui/components/buttons/ScrollButtonForNotTouchDevice.tsx
@@ -2,8 +2,6 @@ import styled from 'styled-components/native'
 
 import { getSpacing } from 'ui/theme'
 
-const BUTTON_SIZE = getSpacing(10)
-const HALF_BUTTON_SIZE = BUTTON_SIZE / 2
 export const ScrollButtonForNotTouchDevice = styled.TouchableOpacity<{
   horizontalAlign: 'left' | 'right'
   top?: number
@@ -12,14 +10,14 @@ export const ScrollButtonForNotTouchDevice = styled.TouchableOpacity<{
   justifyContent: 'center',
   alignItems: 'center',
   margin: 'auto',
-  height: BUTTON_SIZE,
-  width: BUTTON_SIZE,
+  height: theme.buttons.scrollButton.size,
+  width: theme.buttons.scrollButton.size,
   right: horizontalAlign === 'right' ? getSpacing(2) : 'auto',
   left: horizontalAlign === 'left' ? getSpacing(2) : 'auto',
-  top: top ? top - HALF_BUTTON_SIZE : 0,
+  top: top ? top - theme.buttons.scrollButton.size / 2 : 0,
   bottom: top ? 'auto' : 0,
   borderWidth: 1,
-  borderRadius: HALF_BUTTON_SIZE,
+  borderRadius: theme.buttons.scrollButton.size / 2,
   borderColor: theme.colors.greyMedium,
   backgroundColor: theme.colors.white,
   zIndex: theme.zIndex.playlistsButton,
