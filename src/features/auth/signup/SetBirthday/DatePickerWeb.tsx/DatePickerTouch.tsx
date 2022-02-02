ee2ccf064921ea811d633da3759813fb866ca588
Lucas Beneston <lucas.beneston@passculture.app>
2022-02-02 19:02:29 +0100

    (PC-12635) feat(SetBirthday): add error for DatePickerTouch

--- src/features/auth/signup/SetBirthday/DatePickerWeb.tsx/DatePickerTouch.tsx
+++ src/features/auth/signup/SetBirthday/DatePickerWeb.tsx/DatePickerTouch.tsx
@@ -1,8 +1,9 @@
 import { t } from '@lingui/macro'
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Picker from 'react-mobile-picker'
 import styled from 'styled-components/native'
 
+import { useAppSettings } from 'features/auth/settings'
 import { SignupData } from 'features/auth/signup/types'
 import {
   getDatesInMonth,
@@ -10,58 +11,74 @@ import {
   getYears,
   monthNamesShort,
 } from 'features/bookOffer/components/Calendar/Calendar.utils'
+import { analytics } from 'libs/analytics'
+import { dateDiffInFullYears } from 'libs/dates'
+import { formatDateToISOStringWithoutTime, pad } from 'libs/parsers'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { InputError } from 'ui/components/inputs/InputError'
 import { Spacer } from 'ui/theme'
-
 interface Props {
   accessibilityLabelForNextStep?: string
   goToNextStep: (signupData: Partial<SignupData>) => void
 }
 
+const MINIMUM_DATE = 1900
+const DEFAULT_YOUNGEST_AGE = 15
+
 export function DatePickerTouch(props: Props) {
   const CURRENT_DATE = new Date()
+  const CURRENT_DATE_WITHOUT_TIME = formatDateToISOStringWithoutTime(CURRENT_DATE)
 
   const [date, setDate] = useState({
     day: CURRENT_DATE.getDate(),
     month: monthNamesShort[CURRENT_DATE.getMonth()],
     year: CURRENT_DATE.getFullYear(),
   })
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
-  const { isDateInvalid, optionGroups } = useMemo(() => {
-    const { day: selectedDay, month: selectedMonth, year: selectedYear } = date
+  const dateMonth = monthNamesShort.indexOf(date.month) + 1
+  const SELECTED_DATE_WITHOUT_TIME = `${date.year}-${pad(dateMonth)}-${pad(date.day)}`
+
+  const isDisabled = SELECTED_DATE_WITHOUT_TIME === CURRENT_DATE_WITHOUT_TIME
+
+  const { optionGroups } = useMemo(() => {
+    const { month: selectedMonth, year: selectedYear } = date
     const selectedMonthIndex = monthNamesShort.indexOf(selectedMonth)
     const currentYear = CURRENT_DATE.getFullYear()
-    const currentMonth = CURRENT_DATE.getMonth()
-    const currentDay = CURRENT_DATE.getDate()
-
-    let invalid = false
-    if (
-      selectedDay >= currentDay &&
-      currentMonth >= selectedMonthIndex &&
-      currentYear === selectedYear
-    ) {
-      invalid = true
-    }
     return {
-      isDateInvalid: invalid,
       optionGroups: {
         day: getDatesInMonth(selectedMonthIndex, selectedYear),
         month: monthNamesShort,
-        year: getPastYears(1900, currentYear),
+        year: getPastYears(MINIMUM_DATE, currentYear),
       },
     }
   }, [date, monthNamesShort, getYears])
+
+  const { data: settings } = useAppSettings()
+  const youngestAge = settings?.accountCreationMinimumAge ?? DEFAULT_YOUNGEST_AGE
+
+  useEffect(() => {
+    const age = dateDiffInFullYears(new Date(SELECTED_DATE_WITHOUT_TIME), CURRENT_DATE)
+    if (isDisabled) {
+      return setErrorMessage(null)
+    } else if (age < 0) {
+      return setErrorMessage(t`Tu ne peux pas choisir une date dans le futur`)
+    } else if (age < 15) {
+      analytics.logSignUpTooYoung(age)
+      return setErrorMessage(
+        t`Tu dois avoir au moins\u00a0${youngestAge}\u00a0ans pour t’inscrire au pass Culture`
+      )
+    }
+    return setErrorMessage(null)
+  }, [date, isDisabled])
 
   function onDateChange(name: string, value: number | string) {
     setDate((prevDateValues) => ({ ...prevDateValues, [name]: value }))
   }
 
   function goToNextStep() {
-    const dateMonth = monthNamesShort.indexOf(date.month) + 1
-    const DateWithoutTime = `${date.year}-${dateMonth}-${date.day}`
     if (date) {
-      const birthday = DateWithoutTime
-      props.goToNextStep({ birthdate: birthday })
+      props.goToNextStep({ birthdate: SELECTED_DATE_WITHOUT_TIME })
     }
   }
 
@@ -71,11 +88,12 @@ export function DatePickerTouch(props: Props) {
       <CalendarPickerWrapper>
         <Picker valueGroups={date} optionGroups={optionGroups} onChange={onDateChange} />
       </CalendarPickerWrapper>
+      {!!errorMessage && <InputError visible messageId={errorMessage} numberOfSpacesTop={2} />}
       <Spacer.Column numberOfSpaces={2} />
       <ButtonPrimary
         wording={t`Continuer`}
         accessibilityLabel={props.accessibilityLabelForNextStep}
-        disabled={isDateInvalid}
+        disabled={isDisabled || !!errorMessage}
         onPress={goToNextStep}
       />
       <Spacer.Column numberOfSpaces={2} />
