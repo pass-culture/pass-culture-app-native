8b49d0b8aca7f729047a03ecd93c7ce756792ee9
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2022-02-02 17:55:56 +0400

    (PC-13246) fix(SVG): Fix error in JS console for fill-color, fill-rule, clip-rule and theme usage (#2570)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- src/ui/svg/icons/SMSFilled.tsx
+++ src/ui/svg/icons/SMSFilled.tsx
@@ -1,15 +1,13 @@
-import * as React from 'react'
+import React from 'react'
 import { Circle, Path } from 'react-native-svg'
+import styled from 'styled-components/native'
 
 import { AccessibleSvg } from 'ui/svg/AccessibleSvg'
 import { AccessibleIcon } from 'ui/svg/icons/types'
-// eslint-disable-next-line no-restricted-imports
-import { ColorsEnum } from 'ui/theme/colors'
-import { SMALLER_ICON_SIZE } from 'ui/theme/constants'
 
-export const SMSFilled: React.FunctionComponent<AccessibleIcon> = ({
-  size = SMALLER_ICON_SIZE,
-  color = ColorsEnum.BLACK,
+const SMSFilledSvg: React.FunctionComponent<AccessibleIcon> = ({
+  size,
+  color,
   testID,
   accessibilityLabel,
 }) => (
@@ -20,8 +18,8 @@ export const SMSFilled: React.FunctionComponent<AccessibleIcon> = ({
     testID={testID}
     accessibilityLabel={accessibilityLabel ?? 'sms'}>
     <Path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M24 40.2371C34.4934 40.2371 43 33.2444 43 24.6186C43 15.9927 34.4934 9 24 9C13.5066 9 5 15.9927 5 24.6186C5 28.3843 6.62126 31.8388 9.32132 34.5361C9.02727 38.3967 7.26559 40.9014 6.0843 42.1706C5.80631 42.4693 6.06112 43.0485 6.46524 42.9922C9.18941 42.6126 14.9065 41.5307 16.5495 38.9906C18.8373 39.793 21.3555 40.2371 24 40.2371Z"
       fill={color}
     />
@@ -30,3 +28,8 @@ export const SMSFilled: React.FunctionComponent<AccessibleIcon> = ({
     <Circle cx="31.5" cy="25.5" r="2.5" fill="white" />
   </AccessibleSvg>
 )
+
+export const SMSFilled = styled(SMSFilledSvg).attrs(({ color, size, theme }) => ({
+  color: color ?? theme.colors.black,
+  size: size ?? theme.icons.sizes.smaller,
+}))``
