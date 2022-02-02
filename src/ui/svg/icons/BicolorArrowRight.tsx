9ef96065f5052393c612090d1e2e4e018437516e
Marion Barré <marionb@bam.tech>
2022-02-02 10:57:59 +0100

    :sparkles: (BiclorArrowRightAndLeft) do not display svg as element

--- src/ui/svg/icons/BicolorArrowRight.tsx
+++ src/ui/svg/icons/BicolorArrowRight.tsx
@@ -13,7 +13,14 @@ const NotMemoizedBicolorArrowRight: React.FC<BicolorIconInterface> = (props) => 
   const primaryColor = color || ColorsEnum.PRIMARY
   const secondaryColor = color2 || color || ColorsEnum.SECONDARY
   return (
-    <Svg width={size} height={size} style={style} testID={testID} viewBox="0 0 22 18" fill="none">
+    <Svg
+      width={size}
+      height={size}
+      style={style}
+      testID={testID}
+      viewBox="0 0 22 18"
+      fill="none"
+      aria-hidden>
       <Path
         d="M11.6134 15.2066C10.96 15.8456 10.96 16.8817 11.6134 17.5207C12.2669 18.1598 13.3264 18.1598 13.9799 17.5207L21.5099 10.1571C22.1634 9.51804 22.1634 8.48196 21.5099 7.84292L13.9799 0.479281C13.3264 -0.159761 12.2669 -0.159761 11.6134 0.479281C10.96 1.11832 10.96 2.15441 11.6134 2.79345L16.2717 7.34876L1.67333 7.34876C0.749177 7.34876 -8.67113e-07 8.08138 -7.88106e-07 8.98512C-7.09099e-07 9.88886 0.749177 10.6215 1.67333 10.6215L16.3021 10.6215L11.6134 15.2066Z"
         fill={gradientFill}
