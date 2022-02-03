9a2c5d4c35e0b273903c40454ebf30982cbe57b6
Yorick Vinesse <yorick.vinesse@gmail.com>
2022-02-03 11:52:29 +0100

    :sparkles: (pc-13202) align address validation rule with backend

---
+++ src/ui/components/inputs/addressCheck.test.ts
@@ -0,0 +1,19 @@
+import { isAddressValid } from './addressCheck'
+
+describe('isAddressValid function', () => {
+  it.each(['1 ', '1', '1 rue', '1 rue de Loïc', 'àâçéèêîôœùûÀÂÇÉÈÊÎÔŒÙÛ'])(
+    'should accept a well-formatted address: %s',
+    (address) => {
+      const isValid = isAddressValid(address)
+      expect(isValid).toEqual(true)
+    }
+  )
+
+  it.each(['J@hn', 'ჯონ', '𝘑𝘦𝘢𝘯 𝘩𝘦𝘯𝘳𝘺', ' ', '  '])(
+    'should reject a unwell-formatted address: %s',
+    (address) => {
+      const isValid = isAddressValid(address)
+      expect(isValid).toEqual(false)
+    }
+  )
+})
