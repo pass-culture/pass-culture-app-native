1eac80abca844a1d3954890deb5bc72b5d168693
bruno ebstein <bruno.ebstein@passculture.app>
2022-02-03 10:17:43 +0100

    (PC-13229) feat(booking): display duo selector only for not event offer (#2567)
    * test(booking): refactor wrap tests
    
    * (PC-13229) feat(booking): display duo selector only for not event offer

--- src/features/bookOffer/components/__tests__/BookingDetails.native.test.tsx
+++ src/features/bookOffer/components/__tests__/BookingDetails.native.test.tsx
@@ -62,6 +62,14 @@ const mockDigitalStocks = mockDigitalOffer.stocks
 jest.mock('features/profile/utils')
 const mockedUseIsUserUnderage = mocked(useIsUserUnderage)
 
+const mockUseSubcategoriesMapping = jest.fn()
+jest.mock('libs/subcategories', () => ({
+  useSubcategoriesMapping: jest.fn(() => mockUseSubcategoriesMapping()),
+}))
+mockUseSubcategoriesMapping.mockReturnValue({
+  EVENEMENT_PATRIMOINE: { isEvent: true },
+})
+
 describe('<BookingDetails />', () => {
   it('should initialize correctly state when offer isDigital', async () => {
     mockBookingState = {
@@ -192,31 +200,56 @@ describe('<BookingDetails />', () => {
     }
   )
 
-  it('should not display the Duo selector when the offer is not duo', async () => {
-    mockBookingState = {
-      bookingState: mockInitialBookingState,
-      dismissModal: mockDismissModal,
-      dispatch: mockDispatch,
-    }
+  describe('duo selector', () => {
+    it('should not display the Duo selector when the offer is not duo', async () => {
+      mockBookingState = {
+        bookingState: mockInitialBookingState,
+        dismissModal: mockDismissModal,
+        dispatch: mockDispatch,
+      }
 
-    const { queryByTestId } = await renderBookingDetails(mockDigitalStocks)
+      const { queryByTestId } = await renderBookingDetails(mockDigitalStocks)
 
-    expect(queryByTestId('DuoChoiceSelector')).toBeFalsy()
-  })
+      expect(queryByTestId('DuoChoiceSelector')).toBeFalsy()
+    })
 
-  it('should display the Duo selector when the offer is duo', async () => {
-    mockUseBookingOffer.mockReturnValueOnce({ ...mockOffer, isDuo: true })
+    it('should not display the Duo selector when the offer is duo but is an event', async () => {
+      mockUseBookingOffer.mockReturnValueOnce({ ...mockOffer, isDuo: true })
 
-    const duoBookingState: BookingState = { ...mockInitialBookingState, quantity: 2 }
-    mockBookingState = {
-      bookingState: duoBookingState,
-      dismissModal: mockDismissModal,
-      dispatch: mockDispatch,
-    }
+      const duoBookingState: BookingState = { ...mockInitialBookingState, quantity: 2 }
+      mockBookingState = {
+        bookingState: duoBookingState,
+        dismissModal: mockDismissModal,
+        dispatch: mockDispatch,
+      }
 
-    const { queryByTestId } = await renderBookingDetails(mockDigitalStocks)
+      mockUseSubcategoriesMapping.mockReturnValueOnce({
+        EVENEMENT_PATRIMOINE: { isEvent: true },
+      })
 
-    expect(queryByTestId('DuoChoiceSelector')).toBeTruthy()
+      const { queryByTestId } = await renderBookingDetails(mockDigitalStocks)
+
+      expect(queryByTestId('DuoChoiceSelector')).toBeFalsy()
+    })
+
+    it('should display the Duo selector when the offer is duo and not an event', async () => {
+      mockUseBookingOffer.mockReturnValueOnce({ ...mockOffer, isDuo: true })
+
+      const duoBookingState: BookingState = { ...mockInitialBookingState, quantity: 2 }
+      mockBookingState = {
+        bookingState: duoBookingState,
+        dismissModal: mockDismissModal,
+        dispatch: mockDispatch,
+      }
+
+      mockUseSubcategoriesMapping.mockReturnValueOnce({
+        EVENEMENT_PATRIMOINE: { isEvent: false },
+      })
+
+      const { queryByTestId } = await renderBookingDetails(mockDigitalStocks)
+
+      expect(queryByTestId('DuoChoiceSelector')).toBeTruthy()
+    })
   })
 })
 
