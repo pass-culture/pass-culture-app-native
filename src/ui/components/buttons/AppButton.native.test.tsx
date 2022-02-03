46bfa4dbbcc2b8d6800d12c97b1df908aefbc038
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-02-03 11:29:21 +0100

    (Tech) test(snapshots): Update app snapshots

--- src/ui/components/buttons/AppButton.native.test.tsx
+++ src/ui/components/buttons/AppButton.native.test.tsx
@@ -2,17 +2,32 @@ import React from 'react'
 
 import { render } from 'tests/utils'
 import { Close } from 'ui/svg/icons/Close'
+import { Logo as InitialLoadingIndicator } from 'ui/svg/icons/Logo'
+import { Typo } from 'ui/theme'
 
 import { AppButton } from './AppButton'
 
 describe('AppButton Component', () => {
   describe('* Icon property', () => {
     it('should display icon when provided', () => {
-      const { getByTestId } = render(<AppButton wording="Testing Disabled" icon={Close} />)
+      const { getByTestId } = render(
+        <AppButton
+          title={Typo.ButtonText}
+          loadingIndicator={InitialLoadingIndicator}
+          wording="Testing Disabled"
+          icon={Close}
+        />
+      )
       getByTestId('button-icon')
     })
     it('should not display icon when not provided', () => {
-      const { queryByTestId } = render(<AppButton wording="Testing Disabled" />)
+      const { queryByTestId } = render(
+        <AppButton
+          title={Typo.ButtonText}
+          loadingIndicator={InitialLoadingIndicator}
+          wording="Testing Disabled"
+        />
+      )
       const icon = queryByTestId('button-icon')
       expect(icon).toBeFalsy()
     })
@@ -20,7 +35,13 @@ describe('AppButton Component', () => {
   describe('* isLoading property', () => {
     it('should display right elements when isLoading equals true', () => {
       const { getByTestId, queryByTestId } = render(
-        <AppButton wording="Testing Disabled" isLoading icon={Close} />
+        <AppButton
+          title={Typo.ButtonText}
+          loadingIndicator={InitialLoadingIndicator}
+          wording="Testing Disabled"
+          isLoading
+          icon={Close}
+        />
       )
       getByTestId('button-isloading-icon')
       const icon = queryByTestId('button-icon')
@@ -28,7 +49,13 @@ describe('AppButton Component', () => {
     })
     it('should display right elements when isLoading equals false', () => {
       const { getByTestId, queryByTestId } = render(
-        <AppButton wording="Testing Disabled" isLoading={false} icon={Close} />
+        <AppButton
+          title={Typo.ButtonText}
+          loadingIndicator={InitialLoadingIndicator}
+          wording="Testing Disabled"
+          isLoading={false}
+          icon={Close}
+        />
       )
 
       getByTestId('button-icon')
@@ -38,7 +65,15 @@ describe('AppButton Component', () => {
   })
   describe('* Disabled property', () => {
     it('should disable handlers when disabled equals true', () => {
-      const { getByTestId } = render(<AppButton wording="Testing Disabled" icon={Close} disabled />)
+      const { getByTestId } = render(
+        <AppButton
+          title={Typo.ButtonText}
+          loadingIndicator={InitialLoadingIndicator}
+          wording="Testing Disabled"
+          icon={Close}
+          disabled
+        />
+      )
 
       const container = getByTestId('Testing Disabled')
 
@@ -48,7 +83,15 @@ describe('AppButton Component', () => {
   })
   describe('* inline property', () => {
     it('should use inline css style when true', () => {
-      const renderAPI = render(<AppButton wording="Testing inline" icon={Close} inline />)
+      const renderAPI = render(
+        <AppButton
+          title={Typo.ButtonText}
+          loadingIndicator={InitialLoadingIndicator}
+          wording="Testing inline"
+          icon={Close}
+          inline
+        />
+      )
       expect(renderAPI).toMatchSnapshot()
     })
   })
