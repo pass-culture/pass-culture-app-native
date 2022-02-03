ca4f866422698da20abad5a47d82c61fada2462a
bruno ebstein <bruno.ebstein@passculture.app>
2022-02-03 19:05:42 +0100

    add theme provider

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,26 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-react-native-web',
+      options: {
+        modulesToAlias: {
+          api: './src/api',
+          features: './src/features',
+          fixtures: './src/fixtures',
+          libs: './src/libs',
+          theme: './src/theme',
+          locales: './src/locales',
+          types: './src/types',
+          tests: './src/tests',
+          ui: './src/ui',
+          web: './src/web',
+          __mocks__: './__mocks__',
+        },
+      },
+    },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    '@storybook/addon-react-native-web'
-  ],
-  "framework": "@storybook/react"
+  framework: '@storybook/react',
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import { ThemeProvider } from 'libs/styled'
+import { theme } from 'theme'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +10,11 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+]
