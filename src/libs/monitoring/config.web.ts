7c9fb4315949730338ccb435bc38609d859ac656
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2022-02-16 17:46:06 +0100

    (PC-10087) build(Sentry): add dist to web sdk init (#2636)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- src/libs/monitoring/config.web.ts
+++ src/libs/monitoring/config.web.ts
@@ -2,12 +2,13 @@ import { Integrations } from '@sentry/tracing'
 
 import { env } from 'libs/environment'
 
-import { version } from '../../../package.json'
+import { version, build } from '../../../package.json'
 
 export const SENTRY_CONFIG = {
   dsn: env.SENTRY_DSN,
   environment: __DEV__ ? 'development' : env.ENV,
   release: version,
+  dist: `${build}`,
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 0.01,
 }
