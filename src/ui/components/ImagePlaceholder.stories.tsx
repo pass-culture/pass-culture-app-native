a02dc1a6c48532100351d6a699de150156bedf95
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-02-16 15:11:48 +0100

    fix(storybook): update size

--- src/ui/components/ImagePlaceholder.stories.tsx
+++ src/ui/components/ImagePlaceholder.stories.tsx
@@ -2,7 +2,7 @@ import { ComponentStory } from '@storybook/react'
 import React from 'react'
 
 import { MAP_CATEGORY_ID_TO_ICON } from 'libs/parsers'
-import { theme } from 'theme'
+import { getSpacing } from 'ui/theme'
 
 import { ImagePlaceholder } from './ImagePlaceholder'
 
@@ -19,7 +19,7 @@ export const Default: ComponentStory<typeof ImagePlaceholder> = (props) => (
   <ImagePlaceholder {...props} />
 )
 Default.args = {
-  size: theme.icons.sizes.standard,
+  size: getSpacing(24),
   borderRadius: 4,
   Icon: MAP_CATEGORY_ID_TO_ICON.FILM,
 }
