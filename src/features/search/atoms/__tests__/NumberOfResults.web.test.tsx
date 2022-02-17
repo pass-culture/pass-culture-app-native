453dbefb83a6c1c72594c29d9045b9842367af5c
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-02-17 11:38:44 +0100

    (PC-13502) fix(Search): allow removal of locationFilter in current and staged search

--- src/features/search/atoms/__tests__/NumberOfResults.web.test.tsx
+++ src/features/search/atoms/__tests__/NumberOfResults.web.test.tsx
@@ -20,8 +20,14 @@ let mockLocationFilter: LocationFilter = {
   locationType: LocationType.VENUE,
   venue: mockedSuggestedVenues[0],
 }
+
+const mockStagedSearchDispatch = jest.fn()
+
 jest.mock('features/search/pages/SearchWrapper', () => ({
   useSearch: () => ({ searchState: { locationFilter: mockLocationFilter } }),
+  useStagedSearch: () => ({
+    dispatch: mockStagedSearchDispatch,
+  }),
 }))
 
 describe('NumberOfResults component', () => {
@@ -63,9 +69,15 @@ describe('NumberOfResults component', () => {
 
       fireEvent.click(getByTestId('Enlever le lieu'))
 
+      const params = { locationFilter: { locationType: LocationType.AROUND_ME, aroundRadius: 100 } }
+      expect(mockStagedSearchDispatch).toBeCalledWith({
+        type: 'SET_STATE_FROM_NAVIGATE',
+        payload: params,
+      })
+
       expect(navigate).toBeCalledWith('TabNavigator', {
         screen: 'Search',
-        params: { locationFilter: { locationType: LocationType.AROUND_ME, aroundRadius: 100 } },
+        params,
       })
     })
   })
