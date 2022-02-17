21a354f71f5ca612657b01b5b7d56ba804f68b43
Wendy Jean-louis <jeanlouis.wendy@gmail.com>
2022-02-17 17:28:08 +0100

    Revert "(PC-13023) fix(shadows): Update shadows for offer images, booking images and main header (#2606)"
    This reverts commit 4f477ed1ea67b4705a50ce075264ed7064fef6bb.

--- src/features/navigation/RootNavigator/Header/tests/Header.web.test.tsx
+++ src/features/navigation/RootNavigator/Header/tests/Header.web.test.tsx
@@ -1,6 +1,5 @@
 import { render } from '@testing-library/react'
 import React from 'react'
-import { ThemeProvider as WebThemeProvider } from 'styled-components'
 import { ThemeProvider } from 'styled-components/native'
 
 import { useAuthContext } from 'features/auth/AuthContext'
@@ -59,13 +58,11 @@ function renderHeader({
   return render(
     // eslint-disable-next-line local-rules/no-react-query-provider-hoc
     reactQueryProviderHOC(
-      <WebThemeProvider theme={{ ...theme, showTabBar: false }}>
-        <ThemeProvider theme={{ ...theme, showTabBar: false }}>
-          <TabNavigationStateProvider>
-            <Header />
-          </TabNavigationStateProvider>
-        </ThemeProvider>
-      </WebThemeProvider>
+      <ThemeProvider theme={{ ...theme, showTabBar: false }}>
+        <TabNavigationStateProvider>
+          <Header />
+        </TabNavigationStateProvider>
+      </ThemeProvider>
     )
   )
 }
