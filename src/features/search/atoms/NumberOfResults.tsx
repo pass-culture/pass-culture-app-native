69d9f0dca5866f48bf1d47fffb495eaad36c8633
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2022-02-17 12:15:13 +0100

    (PC-13502) fix(Search): allow removal of locationFilter in current and staged search (#2640)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- src/features/search/atoms/NumberOfResults.tsx
+++ src/features/search/atoms/NumberOfResults.tsx
@@ -6,7 +6,7 @@ import styled from 'styled-components/native'
 import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
 import { LocationType } from 'features/search/enums'
-import { useSearch } from 'features/search/pages/SearchWrapper'
+import { useSearch, useStagedSearch } from 'features/search/pages/SearchWrapper'
 import { LocationFilter } from 'features/search/types'
 import { useGeolocation } from 'libs/geolocation'
 import { ClippedTag } from 'ui/components/ClippedTag'
@@ -19,6 +19,7 @@ interface Props {
 export const NumberOfResults: React.FC<Props> = ({ nbHits }) => {
   const { navigate } = useNavigation<UseNavigationType>()
   const { searchState } = useSearch()
+  const { dispatch: dispatchStagedSearch } = useStagedSearch()
   const { position } = useGeolocation()
 
   const venueLabel =
@@ -31,6 +32,10 @@ export const NumberOfResults: React.FC<Props> = ({ nbHits }) => {
       ? { locationType: LocationType.AROUND_ME, aroundRadius: 100 }
       : { locationType: LocationType.EVERYWHERE }
 
+    // this reset staged in case of new search
+    dispatchStagedSearch({ type: 'SET_STATE_FROM_NAVIGATE', payload: { locationFilter } })
+
+    // this reset current search results
     navigate(...getTabNavConfig('Search', { locationFilter }))
   }, [!position])
 
