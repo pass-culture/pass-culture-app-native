6d414638d45a00f6e1f7031c8436e20d10649cf2
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-02-21 13:23:04 +0100

    (PC-13509) fix(Sentry): configure sourcesmaps for iOS/Android

--- src/libs/monitoring/config.ts
+++ src/libs/monitoring/config.ts
@@ -1,11 +1,21 @@
+import { Platform } from 'react-native'
+import CodePush from 'react-native-code-push'
+
 import { env } from 'libs/environment'
 
 import { version, build } from '../../../package.json'
 
-export const SENTRY_CONFIG = {
-  dsn: env.SENTRY_DSN,
-  environment: __DEV__ ? 'development' : env.ENV,
-  release: version,
-  dist: `${build}`,
-  tracesSampleRate: 0.01,
+export async function getSentryConfig() {
+  const update = await CodePush.getUpdateMetadata()
+  let dist = `${build}-${Platform.OS}`
+  if (update) {
+    dist += `-${update.appVersion}+codepush:${update.label}`
+  }
+  return {
+    dsn: env.SENTRY_DSN,
+    environment: __DEV__ ? 'development' : env.ENV,
+    release: version,
+    dist,
+    tracesSampleRate: 0.01,
+  }
 }
--- src/libs/monitoring/config.web.ts
+++ src/libs/monitoring/config.web.ts
@@ -4,11 +4,13 @@ import { env } from 'libs/environment'
 
 import { version, build } from '../../../package.json'
 
-export const SENTRY_CONFIG = {
-  dsn: env.SENTRY_DSN,
-  environment: __DEV__ ? 'development' : env.ENV,
-  release: version,
-  dist: env.ENV === 'testing' ? `${build}-web-${env.COMMIT_HASH}` : `${build}-web`,
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 0.01,
+export async function getSentryConfig() {
+  return {
+    dsn: env.SENTRY_DSN,
+    environment: __DEV__ ? 'development' : env.ENV,
+    release: version,
+    dist: env.ENV === 'testing' ? `${build}-web-${env.COMMIT_HASH}` : `${build}-web`,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 0.01,
+  }
 }
