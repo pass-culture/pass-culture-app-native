6e865d96ce07e78bd2001544346c4534269cfb22
Antoine Garcia <antoine.garcia@polyconseil.fr>
2022-02-22 13:02:50 +0100

    (PC-13582) feat(cache): consider requests as stale by default

--- src/libs/react-query/queryClient.ts
+++ src/libs/react-query/queryClient.ts
@@ -2,10 +2,14 @@ import { QueryCache, QueryClient } from 'react-query'
 
 import { saveQueryClient } from 'libs/react-query/persistor'
 
-// Read https://tkdodo.eu/blog/placeholder-and-initial-data-in-react-query
-export const GLOBAL_STALE_TIME = 1000 * 60 * 60 * 24 // 24 hours
+// every query is considered stale once it's triggered. As a result,
+// every time we consult an offer, the request will be made to the server
+// this allows us to have a direct feedback when changing home playlist for instance
+// the tradeoff is that more requests are made to the server
+export const GLOBAL_STALE_TIME = 0
 
 const queryCache = new QueryCache()
+// Read https://tkdodo.eu/blog/placeholder-and-initial-data-in-react-query
 export const queryClient = new QueryClient({
   queryCache,
   defaultOptions: {
