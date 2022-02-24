fe48c4776771eba016e0f948e6e3778c231dcab1
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-02-24 14:29:56 +0100

    Revert "(PC-13582) feat(cache): consider requests as stale by default (#2661)"
    This reverts commit f135ca754a1e391aeff475b4faf0a0748a80ab5b.

--- src/libs/react-query/queryClient.ts
+++ src/libs/react-query/queryClient.ts
@@ -2,14 +2,10 @@ import { QueryCache, QueryClient } from 'react-query'
 
 import { saveQueryClient } from 'libs/react-query/persistor'
 
-// every query is considered stale once it's triggered. As a result,
-// every time we consult an offer, the request will be made to the server
-// this allows us to have a direct feedback when changing home playlist for instance
-// the tradeoff is that more requests are made to the server
-export const GLOBAL_STALE_TIME = 0
+// Read https://tkdodo.eu/blog/placeholder-and-initial-data-in-react-query
+export const GLOBAL_STALE_TIME = 1000 * 60 * 60 * 24 // 24 hours
 
 const queryCache = new QueryCache()
-// Read https://tkdodo.eu/blog/placeholder-and-initial-data-in-react-query
 export const queryClient = new QueryClient({
   queryCache,
   defaultOptions: {
