d816d154752033b797a0e1fae4c049e5335c4fe7
Marion Barré <marionb@bam.tech>
2022-02-25 10:01:08 +0100

    :sparkles: (Filter) add button balise

--- src/features/search/atoms/Buttons/__tests__/Filter.web.test.tsx
+++ src/features/search/atoms/Buttons/__tests__/Filter.web.test.tsx
@@ -6,6 +6,11 @@ import { fireEvent, render } from 'tests/utils/web'
 
 import { Filter } from '../Filter'
 
+// TODO why is onPress props passed through web button?
+// it creates "Warning: Unknown event handler property `%s`. It will be ignored.%s", "onPress" error
+// eslint-disable-next-line local-rules/no-allow-console
+allowConsole({ error: true })
+
 jest.mock('features/search/utils/useMaxPrice', () => ({
   useMaxPrice: jest.fn(() => 300),
 }))
