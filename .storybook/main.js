603ac91c209996c9641bce3e5c65c5c2d8998c79
Lucas Beneston <lucas.beneston@passculture.app>
2022-02-28 15:13:41 +0100

    (PC-13645) feat(storybook): add a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
     {
       name: '@storybook/addon-react-native-web',
       options: {
