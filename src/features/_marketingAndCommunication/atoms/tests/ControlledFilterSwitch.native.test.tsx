ec23030f400065eab2fd93a7aaff365e61b4360d
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-03-03 15:17:32 +0100

    added tests

---
+++ src/features/_marketingAndCommunication/atoms/tests/ControlledFilterSwitch.native.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { ControlledFilterSwitch } from 'features/_marketingAndCommunication/atoms/ControlledFilterSwitch'
+import { render, fireEvent } from 'tests/utils'
+
+describe('<ControlledFilterSwitch />', () => {
+  it('should call onChange with new value on toggle', () => {
+    const onChange = jest.fn()
+    const renderAPI = render(<ControlledFilterSwitch onChange={onChange} name="testUnit" />)
+    fireEvent.press(renderAPI.getByTestId('testUnit'))
+    expect(onChange).toBeCalledWith(true)
+    fireEvent.press(renderAPI.getByTestId('testUnit'))
+    expect(onChange).toBeCalledWith(false)
+  })
+})
