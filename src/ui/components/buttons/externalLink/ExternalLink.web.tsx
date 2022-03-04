6da47215de60cb306df31bc95765e1a759261ce1
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2022-03-04 10:47:26 +0100

    (PC-13581) fix(accessibility): Fix keyboard enter on ExternalLink (#2744)

--- src/ui/components/buttons/externalLink/ExternalLink.web.tsx
+++ src/ui/components/buttons/externalLink/ExternalLink.web.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components/native'
 
 import { openUrl } from 'features/navigation/helpers'
 import { extractExternalLinkParts } from 'ui/components/buttons/externalLink/ExternalLink.service'
+import { TouchableOpacity } from 'ui/components/TouchableOpacity'
 import { ExternalSite as DefaultExternalSite } from 'ui/svg/icons/ExternalSite'
 import { Spacer, Typo } from 'ui/theme'
 import { A } from 'ui/web/link/A'
@@ -19,20 +20,26 @@ export const ExternalLink: React.FC<Props> = ({ url, text, primary, testID }) =>
 
   return (
     <A href={url}>
-      <ButtonText primary={primary} onPress={() => openUrl(url)} testID={testID} accessible={true}>
-        <Spacer.Row numberOfSpaces={1} />
-        <Text>
-          <ExternalSite primary={primary} testID="externalSiteIcon" />
-          {firstWord}
-        </Text>
-        {remainingWords}
-      </ButtonText>
+      <StyledTouchableOpacity onPress={() => openUrl(url)} testID={testID}>
+        <ButtonText primary={primary}>
+          <Spacer.Row numberOfSpaces={1} />
+          <Text>
+            <ExternalSite primary={primary} testID="externalSiteIcon" />
+            {firstWord}
+          </Text>
+          {remainingWords}
+        </ButtonText>
+      </StyledTouchableOpacity>
     </A>
   )
 }
 
 const Text = styled.Text({
   whiteSpace: 'nowrap',
+})
+
+const StyledTouchableOpacity = styled(TouchableOpacity)({
+  display: 'inline',
 })
 
 const ButtonText = styled(Typo.ButtonText)<{ primary?: boolean }>(({ primary, theme }) => ({
