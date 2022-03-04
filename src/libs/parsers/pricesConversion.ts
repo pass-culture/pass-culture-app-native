1cef776e80a0e866884cfe0337c5bf5b0e2b2439
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2022-03-04 17:58:57 +0100

    (PC-13805) fix(PriceConversion): Fix number of decimals in price conversion from euro to cents (#2754)

--- src/libs/parsers/pricesConversion.ts
+++ src/libs/parsers/pricesConversion.ts
@@ -1,5 +1,7 @@
 export const CENTS_IN_EURO = 100
 
-export const convertEuroToCents = (p: number): number => Math.floor(p * CENTS_IN_EURO)
+export const convertEuroToCents = (p: number): number => {
+  return Math.floor(Number((p * CENTS_IN_EURO).toFixed(2)))
+}
 
 export const convertCentsToEuros = (p: number): number => Math.floor(p / CENTS_IN_EURO)
