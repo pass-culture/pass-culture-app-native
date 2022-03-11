94713ef8a55831d8ebc8656841a9e2289569308c
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2022-03-11 09:59:20 +0100

    (PC-13841) feat(accessibility): Fix focus and component order in booking details (#2782)

--- src/features/bookings/atoms/TicketCodeTitle.tsx
+++ src/features/bookings/atoms/TicketCodeTitle.tsx
@@ -1,8 +1,22 @@
+import React, { ReactNode } from 'react'
 import styled from 'styled-components/native'
 
+import { TouchableOpacity } from 'ui/components/TouchableOpacity'
 import { getSpacing, Typo } from 'ui/theme'
 
-export const TicketCodeTitle = styled(Typo.Title4)(({ theme }) => ({
+export const TicketCodeTitle = ({
+  onPress,
+  children,
+}: {
+  onPress?: () => void
+  children: ReactNode
+}) => (
+  <TouchableOpacity onPress={onPress} disabled={!onPress}>
+    <StyledTitle>{children}</StyledTitle>
+  </TouchableOpacity>
+)
+
+const StyledTitle = styled(Typo.Title4)(({ theme }) => ({
   color: theme.colors.primary,
   textAlign: 'center',
   padding: getSpacing(2.5),
