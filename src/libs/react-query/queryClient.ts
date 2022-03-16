ec1b543b6b055bfdb31f69c0805cea9fe908450b
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-03-16 12:29:34 +0100

    fix(queryClient): restore global stale time to 24h

--- src/libs/react-query/queryClient.ts
+++ src/libs/react-query/queryClient.ts
@@ -3,7 +3,7 @@ import { QueryCache, QueryClient } from 'react-query'
 // every time we consult an offer, the request will be made to the server
 // this allows us to have a direct feedback when changing home playlist for instance
 // the tradeoff is that more requests are made to the server
-export const GLOBAL_STALE_TIME = 0
+export const GLOBAL_STALE_TIME = 1000 * 60 * 60 * 24 // 24 hours
 
 const queryCache = new QueryCache()
 // Read https://tkdodo.eu/blog/placeholder-and-initial-data-in-react-query
