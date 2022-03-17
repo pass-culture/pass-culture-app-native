2ddf2881a823b601ba35a3221597adca55e16cba
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-03-17 13:35:05 +0100

    (PC-10585) feat(meta): added custom meta on venue and offer endpoints

---
+++ server/services/entities/venue.ts
@@ -0,0 +1,66 @@
+import { env } from '../../libs/environment/env'
+
+import { EntityType, TwitterCard } from './types'
+
+const { DEEPLINK_PROTOCOL } = env
+
+export const VENUE: EntityType = {
+  NAME: 'venue',
+  METAS_CONFIG: {
+    title(entity: Record<string, unknown>) {
+      return (entity.publicName || entity.name) as string
+    },
+    description(entity: Record<string, unknown>) {
+      return entity.description as string
+    },
+    ['og:url'](entity: Record<string, unknown>, href: string, subPath: string) {
+      return `${href}${subPath}`
+    },
+    ['og:title'](entity: Record<string, unknown>) {
+      return (entity.publicName || entity.name) as string
+    },
+    ['og:description'](entity: Record<string, unknown>) {
+      return entity.description as string
+    },
+    ['og:image'](entity: Record<string, unknown>) {
+      return entity.bannerUrl as string
+    },
+    ['og:image:alt'](entity: Record<string, unknown>) {
+      return entity.description as string
+    },
+    ['twitter:card']() {
+      return TwitterCard.summaryLargeImage
+    },
+    ['twitter:url'](entity: Record<string, unknown>, href: string, subPath: string) {
+      return `${href}${subPath}`
+    },
+    ['twitter:title'](entity: Record<string, unknown>) {
+      return (entity.publicName || entity.name) as string
+    },
+    ['twitter:description'](entity: Record<string, unknown>) {
+      return entity.description as string
+    },
+    ['twitter:image'](entity: Record<string, unknown>) {
+      return entity.bannerUrl as string
+    },
+    ['twitter:image:alt'](entity: Record<string, unknown>) {
+      return ((entity.bannerMeta as Record<string, string>)?.image_credit ||
+        entity.description) as string
+    },
+    ['twitter:app:url:iphone'](entity: Record<string, unknown>, href: string, subPath: string) {
+      return `${DEEPLINK_PROTOCOL}${href}${subPath}`
+    },
+    ['twitter:app:url:ipad'](entity: Record<string, unknown>, href: string, subPath: string) {
+      return `${DEEPLINK_PROTOCOL}${href}${subPath}`
+    },
+    ['twitter:app:url:googleplay'](entity: Record<string, unknown>, href: string, subPath: string) {
+      return `${DEEPLINK_PROTOCOL}${href}${subPath}`
+    },
+    ['al:ios:url'](entity: Record<string, unknown>, href: string, subPath: string) {
+      return `${DEEPLINK_PROTOCOL}${href}${subPath}`
+    },
+    ['al:android:url'](entity: Record<string, unknown>, href: string, subPath: string) {
+      return `${DEEPLINK_PROTOCOL}${href}${subPath}`
+    },
+  },
+}
