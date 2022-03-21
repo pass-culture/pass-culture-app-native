63b0783744d8cab3fc1ba37a32d9ff9dfe47d9ee
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2022-03-21 16:22:12 +0100

    (PC-13962) feat(storybook): add Illustrations stories (#2814)

--- src/features/cheatcodes/pages/AppComponents/Illustrations.tsx
+++ src/features/cheatcodes/pages/AppComponents/Illustrations.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react'
-import { Text, View } from 'react-native'
-import styled from 'styled-components/native'
+import { Text } from 'react-native'
 
+import { Illustration } from 'ui/storybook/Illustration'
 import { BrokenConnection } from 'ui/svg/BrokenConnection'
 import { BicolorIdCardWithMagnifyingGlass } from 'ui/svg/icons/BicolorIdCardWithMagnifyingGlass'
 import { BicolorPhonePending } from 'ui/svg/icons/BicolorPhonePending'
@@ -24,7 +24,6 @@ import { RequestSent } from 'ui/svg/icons/RequestSent'
 import { SadFace } from 'ui/svg/icons/SadFace'
 import { Star } from 'ui/svg/icons/Star'
 import { TicketBooked } from 'ui/svg/icons/TicketBooked'
-import { IconInterface } from 'ui/svg/icons/types'
 import { UserBlocked } from 'ui/svg/icons/UserBlocked'
 import { UserError } from 'ui/svg/icons/UserError'
 import { UserFavorite } from 'ui/svg/icons/UserFavorite'
@@ -33,60 +32,34 @@ export const Illustrations: FunctionComponent = () => {
   return (
     <React.Fragment>
       <Text>{'Illustration icons should have a standard size of 140'}</Text>
-      <Illustration name="BicolorPhonePending" component={BicolorPhonePending} isNew />
+      <Illustration name="BicolorPhonePending" component={BicolorPhonePending} />
       <Illustration
         name="BicolorIdCardWithMagnifyingGlass"
         component={BicolorIdCardWithMagnifyingGlass}
-        isNew
       />
-      <Illustration name="BirthdayCake" component={BirthdayCake} isNew />
-      <Illustration name="BrokenConnection" component={BrokenConnection} isNew />
-      <Illustration name="CalendarIllustration" component={CalendarIllustration} isNew />
-      <Illustration name="ErrorIllustration" component={ErrorIllustration} isNew />
-      <Illustration name="EmailSent" component={EmailSent} isNew />
-      <Illustration name="EmptyFavorites" component={EmptyFavorites} isNew />
-      <Illustration name="HappyFace" component={HappyFace} isNew />
-      <Illustration name="IdCardError" component={IdCardError} isNew />
-      <Illustration name="LocationIllustration" component={LocationIllustration} isNew />
-      <Illustration name="MaintenanceCone" component={MaintenanceCone} isNew />
-      <Illustration name="NoBookings" component={NoBookings} isNew />
-      <Illustration name="NoOffer" component={NoOffer} isNew />
-      <Illustration name="Notification" component={Notification} isNew />
-      <Illustration name="PageNotFound" component={PageNotFound} isNew />
-      <Illustration name="PhoneFlip" component={PhoneFlip} isNew />
-      <Illustration
-        name="ProfileDeletionIllustration"
-        component={ProfileDeletionIllustration}
-        isNew
-      />
-      <Illustration name="RequestSent" component={RequestSent} isNew />
-      <Illustration name="SadFace" component={SadFace} isNew />
-      <Illustration name="Star" component={Star} isNew />
-      <Illustration name="TicketBooked" component={TicketBooked} isNew />
-      <Illustration name="UserBlocked" component={UserBlocked} isNew />
-      <Illustration name="UserError" component={UserError} isNew />
-      <Illustration name="UserFavorite" component={UserFavorite} isNew />
+      <Illustration name="BirthdayCake" component={BirthdayCake} />
+      <Illustration name="BrokenConnection" component={BrokenConnection} />
+      <Illustration name="CalendarIllustration" component={CalendarIllustration} />
+      <Illustration name="ErrorIllustration" component={ErrorIllustration} />
+      <Illustration name="EmailSent" component={EmailSent} />
+      <Illustration name="EmptyFavorites" component={EmptyFavorites} />
+      <Illustration name="HappyFace" component={HappyFace} />
+      <Illustration name="IdCardError" component={IdCardError} />
+      <Illustration name="LocationIllustration" component={LocationIllustration} />
+      <Illustration name="MaintenanceCone" component={MaintenanceCone} />
+      <Illustration name="NoBookings" component={NoBookings} />
+      <Illustration name="NoOffer" component={NoOffer} />
+      <Illustration name="Notification" component={Notification} />
+      <Illustration name="PageNotFound" component={PageNotFound} />
+      <Illustration name="PhoneFlip" component={PhoneFlip} />
+      <Illustration name="ProfileDeletionIllustration" component={ProfileDeletionIllustration} />
+      <Illustration name="RequestSent" component={RequestSent} />
+      <Illustration name="SadFace" component={SadFace} />
+      <Illustration name="Star" component={Star} />
+      <Illustration name="TicketBooked" component={TicketBooked} />
+      <Illustration name="UserBlocked" component={UserBlocked} />
+      <Illustration name="UserError" component={UserError} />
+      <Illustration name="UserFavorite" component={UserFavorite} />
     </React.Fragment>
   )
 }
-
-interface IllustrationsProps {
-  name: string
-  component: React.ComponentType<IconInterface>
-  isNew?: boolean
-}
-
-const Illustration = ({ name, component: IconComponent, isNew = false }: IllustrationsProps) => (
-  <AlignedText>
-    <IconComponent />
-    <StyledText isNew={isNew}>{` - ${name} ${isNew ? '' : '(deprecated)'}`}</StyledText>
-  </AlignedText>
-)
-const AlignedText = styled(View)({
-  flexDirection: 'row',
-  alignItems: 'center',
-})
-
-const StyledText = styled(Text)<{ isNew: boolean }>(({ theme, isNew }) => ({
-  color: isNew ? theme.colors.black : theme.colors.greyDark,
-}))
