f6461396d9a376dc8f9d95be8091dd4d05d89c6a
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-03-22 15:32:00 +0100

    (PC-14107) fix(mustUpdateApp): added sentry logging when munimnalBuildNumber is not set

--- src/features/forceUpdate/useMustUpdateApp.ts
+++ src/features/forceUpdate/useMustUpdateApp.ts
@@ -1,8 +1,21 @@
 import { useMinimalBuildNumber } from 'features/forceUpdate/useMinimalBuildNumber'
+import { eventMonitoring } from 'libs/monitoring'
 
 import { build } from '../../../package.json'
 
 export const useMustUpdateApp = () => {
   const minimalBuildNumber = useMinimalBuildNumber()
-  return !!minimalBuildNumber && build < minimalBuildNumber
+  const mustUpdateApp = !!minimalBuildNumber && build < minimalBuildNumber
+
+  if (mustUpdateApp && !build) {
+    eventMonitoring.captureException(new Error('MustUpdateAppError'), {
+      extra: {
+        mustUpdateApp,
+        minimalBuildNumber,
+        build,
+      },
+    })
+  }
+
+  return mustUpdateApp
 }
