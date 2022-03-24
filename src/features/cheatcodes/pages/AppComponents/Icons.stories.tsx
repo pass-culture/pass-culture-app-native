022498bd35624945644f23efd58fef926ad59343
Lucas Beneston <lucas.beneston@passculture.app>
2022-03-24 16:00:21 +0100

    (PC-13963) feat(storybook): add social SecondaryAndBigger icons

--- src/features/cheatcodes/pages/AppComponents/Icons.stories.tsx
+++ src/features/cheatcodes/pages/AppComponents/Icons.stories.tsx
@@ -14,6 +14,60 @@ import { WhatsApp } from 'ui/svg/icons/socialNetwork/WhatsApp'
 import { Profile } from 'ui/svg/icons/Profile'
 import { IdCard } from 'ui/svg/icons/IdCard'
 import { Confirmation } from 'ui/svg/icons/Confirmation'
+import { BicolorAroundMe } from 'ui/svg/icons/BicolorAroundMe'
+import { BicolorBookings } from 'ui/svg/icons/BicolorBookings'
+import { BicolorConfidentiality } from 'ui/svg/icons/BicolorConfidentiality'
+import { BicolorEverywhere } from 'ui/svg/icons/BicolorEverywhere'
+import { BicolorFavorite } from 'ui/svg/icons/BicolorFavorite'
+import { BicolorLocationBuilding } from 'ui/svg/icons/BicolorLocationBuilding'
+import { BicolorLocationPointer } from 'ui/svg/icons/BicolorLocationPointer'
+import { BicolorLock } from 'ui/svg/icons/BicolorLock'
+import { BicolorLogo } from 'ui/svg/icons/BicolorLogo'
+import { BicolorProfile } from 'ui/svg/icons/BicolorProfile'
+import { BicolorSearch } from 'ui/svg/icons/BicolorSearch'
+// import { BicolorSelector } from 'ui/svg/icons/BicolorSelector'
+import { ArrowNext } from 'ui/svg/icons/ArrowNext'
+import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
+import { Bell } from 'ui/svg/icons/Bell'
+import { Booking } from 'ui/svg/icons/Booking'
+import { Calendar } from 'ui/svg/icons/Calendar'
+import { Check } from 'ui/svg/icons/Check'
+import { Clock } from 'ui/svg/icons/Clock'
+import { Close } from 'ui/svg/icons/Close'
+import { Confidentiality } from 'ui/svg/icons/Confidentiality'
+import { Duo } from 'ui/svg/icons/Duo'
+import { Email } from 'ui/svg/icons/Email'
+import { Error } from 'ui/svg/icons/Error'
+import { ExternalSite } from 'ui/svg/icons/ExternalSite'
+import { Eye } from 'ui/svg/icons/Eye'
+import { EyeSlash } from 'ui/svg/icons/EyeSlash'
+import { Favorite } from 'ui/svg/icons/Favorite'
+import { FavoriteFilled } from 'ui/svg/icons/FavoriteFilled'
+import { Flash } from 'ui/svg/icons/Flash'
+import { HandicapAudio } from 'ui/svg/icons/HandicapAudio'
+import { HandicapMental } from 'ui/svg/icons/HandicapMental'
+import { HandicapMotor } from 'ui/svg/icons/HandicapMotor'
+import { HandicapVisual } from 'ui/svg/icons/HandicapVisual'
+import { Idea } from 'ui/svg/icons/Idea'
+import { Info } from 'ui/svg/icons/Info'
+import { LegalNotices } from 'ui/svg/icons/LegalNotices'
+import { LifeBuoy } from 'ui/svg/icons/LifeBuoy'
+import { LocationBuilding } from 'ui/svg/icons/LocationBuilding'
+import { LocationPointerNotFilled } from 'ui/svg/icons/LocationPointerNotFilled'
+import { Lock } from 'ui/svg/icons/Lock'
+import { Logo } from 'ui/svg/icons/Logo'
+import { MagnifyingGlass } from 'ui/svg/icons/MagnifyingGlass'
+import { OfferDigital } from 'ui/svg/icons/OfferDigital'
+import { OfferEvent } from 'ui/svg/icons/OfferEvent'
+import { OfferPhysical } from 'ui/svg/icons/OfferPhysical'
+import { Offers } from 'ui/svg/icons/Offers'
+import { OrderPrice } from 'ui/svg/icons/OrderPrice'
+import { ProfileDeletion } from 'ui/svg/icons/ProfileDeletion'
+import { Quote } from 'ui/svg/icons/Quote'
+import { Share } from 'ui/svg/icons/Share'
+import { SignOut } from 'ui/svg/icons/SignOut'
+import { Sun } from 'ui/svg/icons/Sun'
+import { Warning } from 'ui/svg/icons/Warning'
 
 export default {
   title: 'ui/icons',
@@ -52,5 +106,65 @@ IdentityCheck.args = {
     Profile,
     IdCard,
     Confirmation,
+  },
+}
+
+export const SecondaryAndBigger = Icons.bind({})
+SecondaryAndBigger.args = {
+  icon: {
+    BicolorAroundMe,
+    BicolorBookings,
+    BicolorConfidentiality,
+    BicolorEverywhere,
+    BicolorFavorite,
+    BicolorLock,
+    BicolorLocationPointer,
+    BicolorLocationBuilding,
+    BicolorLogo,
+    BicolorProfile,
+    BicolorSearch,
+    // BicolorSelector,
+    ArrowNext,
+    ArrowPrevious,
+    Bell,
+    Booking,
+    Calendar,
+    Check,
+    Clock,
+    Close,
+    Confidentiality,
+    Duo,
+    Email,
+    Error,
+    ExternalSite,
+    Eye,
+    EyeSlash,
+    Favorite,
+    FavoriteFilled,
+    Flash,
+    HandicapVisual,
+    HandicapMental,
+    HandicapMotor,
+    HandicapAudio,
+    Idea,
+    Info,
+    LegalNotices,
+    LifeBuoy,
+    LocationBuilding,
+    LocationPointerNotFilled,
+    Lock,
+    Logo,
+    MagnifyingGlass,
+    OfferDigital,
+    OfferEvent,
+    OfferPhysical,
+    Offers,
+    OrderPrice,
+    ProfileDeletion,
+    Quote,
+    SignOut,
+    Share,
+    Sun,
+    Warning,
   },
 }
