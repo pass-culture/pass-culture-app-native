1afa7f9104b785f54561307aa80db24c9ede5240
cedricls1 <78556024+cedricls1@users.noreply.github.com>
2022-03-25 09:26:33 +0100

    feat(storybook): add multiple RadioButton story (#2838)
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

--- src/ui/components/RadioButton.stories.tsx
+++ src/ui/components/RadioButton.stories.tsx
@@ -1,10 +1,10 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import React from 'react'
+import React, { useState } from 'react'
 
 import { RadioButton } from './RadioButton'
 
 export default {
-  title: 'ui/RadioButton',
+  title: 'ui/inputs/RadioButton',
   component: RadioButton,
 } as ComponentMeta<typeof RadioButton>
 
@@ -37,3 +37,21 @@ WithSelectedValueAndDescription.args = {
   selectedValue: '1',
   description: 'description item 1',
 }
+
+const ListTemplate = () => {
+  const [selectedValue, setSelectedRadioButton] = useState('')
+  const items = [
+    { id: '1', title: 'item 1', onSelect: setSelectedRadioButton, selectedValue },
+    { id: '2', title: 'item 2', onSelect: setSelectedRadioButton, selectedValue },
+    { id: '3', title: 'item 3', onSelect: setSelectedRadioButton, selectedValue },
+  ]
+  return (
+    <React.Fragment>
+      {items.map((item) => (
+        <RadioButton key={item.id} {...item} />
+      ))}
+    </React.Fragment>
+  )
+}
+
+export const WithMultipleRadioButton = ListTemplate.bind({})
