6efab73e338074924f02eff4216dded43885c739
Yorick Vinesse <44037911+yorickeando@users.noreply.github.com>
2022-04-04 13:53:13 +0200

    (PC-13344) feat(QPI): create cultural survey answer page (#2860)
    * :bug: (pc-13344) fix CulturalSurveyProgressBar usability in other component
    
    * :sparkles: (pc-13344) create CulturalSurveyQuestions route and page
    
    * :sparkles: (pc-13344) create CulturalSurveyPageHeader
    
    * :sparkles: (pc-13344) create page with mock answers
    
    * :sparkles: (pc-13344) update CulturalSurveyCheckbox with modular icons
    
    * :sparkles: (pc-13344) finish QPI page design and use mocked back data
    
    * :globe_with_meridians: (pc-13344) update translation file

--- src/features/culturalSurvey/components/__tests__/CulturalSurveyCheckbox.native.test.tsx
+++ src/features/culturalSurvey/components/__tests__/CulturalSurveyCheckbox.native.test.tsx
@@ -2,14 +2,25 @@ import * as React from 'react'
 
 import { CulturalSurveyCheckbox } from 'features/culturalSurvey/components/CulturalSurveyCheckbox'
 import { render, fireEvent } from 'tests/utils'
+import CulturalSurveyIcons from 'ui/svg/icons/culturalSurvey'
 
 describe('CulturalSurveyCheckbox', () => {
   it('should render correctly', () => {
-    const CulturalSurveyCheckboxComponent = render(<CulturalSurveyCheckbox />)
+    const CulturalSurveyCheckboxComponent = render(
+      <CulturalSurveyCheckbox
+        icon={CulturalSurveyIcons.MuseumIcon}
+        subtitle={'un monument, une exposition...'}
+      />
+    )
     expect(CulturalSurveyCheckboxComponent).toMatchSnapshot()
   })
   it('should render correctly when pressed', () => {
-    const CulturalSurveyCheckboxComponent = render(<CulturalSurveyCheckbox />)
+    const CulturalSurveyCheckboxComponent = render(
+      <CulturalSurveyCheckbox
+        icon={CulturalSurveyIcons.MuseumIcon}
+        subtitle={'un monument, une exposition...'}
+      />
+    )
     const Button = CulturalSurveyCheckboxComponent.getByTestId('CulturalSurveyAnswer')
     fireEvent.press(Button)
     expect(CulturalSurveyCheckboxComponent).toMatchSnapshot()
