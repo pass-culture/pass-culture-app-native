8ba56a0c252f6f7aaf5cd04f174ae029439ebb82
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-04-05 17:04:28 +0200

    (PC-14142) feat(bookings): new display by email

--- src/features/bookings/components/BookingDetailsTicketContent.web.test.tsx
+++ src/features/bookings/components/BookingDetailsTicketContent.web.test.tsx
@@ -1,6 +1,11 @@
+import { addDays, formatISO } from 'date-fns'
 import React from 'react'
 
-import { BookingDetailsTicketContent } from 'features/bookings/components/BookingDetailsTicketContent'
+import {
+  BookingDetailsTicketContent,
+  formatCollectDelayString,
+} from 'features/bookings/components/BookingDetailsTicketContent'
+import { OFFER_WITHDRAWAL_TYPE_OPTIONS } from 'features/bookings/components/types'
 import { render } from 'tests/utils/web'
 
 import { bookingsSnap } from '../api/bookingsSnap'
@@ -57,31 +62,157 @@ describe('BookingDetailsTicketContent', () => {
       expect(queryByTestId('collect-info')).toBeFalsy()
     })
 
-    it('should display on site collect wording', () => {
-      const booking = bookingsSnap.ongoing_bookings[1]
-      booking.stock.offer.isDigital = false
-      const { getByTestId } = render(
-        <BookingDetailsTicketContent booking={booking} proDisableEventsQrcode={true} />
-      )
-      getByTestId('collect-info')
+    describe('should display collect wording', () => {
+      const initialBooking = bookingsSnap.ongoing_bookings[0]
+
+      it('On site with delay', () => {
+        const booking = {
+          ...initialBooking,
+          stock: {
+            ...initialBooking.stock,
+            offer: {
+              ...initialBooking.stock.offer,
+              withdrawalType: OFFER_WITHDRAWAL_TYPE_OPTIONS.ON_SITE,
+              withdrawalDelay: 60 * 60 * 2,
+            },
+          },
+        }
+
+        const { getByTestId } = render(
+          <BookingDetailsTicketContent booking={booking} proDisableEventsQrcode={true} />
+        )
+        getByTestId('collect-info-delay')
+      })
+
+      it('On site without delay', () => {
+        const booking = {
+          ...initialBooking,
+          stock: {
+            ...initialBooking.stock,
+            offer: {
+              ...initialBooking.stock.offer,
+              withdrawalType: OFFER_WITHDRAWAL_TYPE_OPTIONS.ON_SITE,
+              withdrawalDelay: 0,
+            },
+          },
+        }
+        const { queryByTestId } = render(
+          <BookingDetailsTicketContent booking={booking} proDisableEventsQrcode={true} />
+        )
+        expect(queryByTestId('collect-info-delay')).toBeFalsy()
+      })
+
+      it('By email with delay and email not received yet', () => {
+        const booking = {
+          ...initialBooking,
+          confirmationDate: formatISO(new Date()).slice(0, -1),
+          stock: {
+            ...initialBooking.stock,
+            beginningDatetime: formatISO(addDays(new Date(), 3)).slice(0, -1),
+            offer: {
+              ...initialBooking.stock.offer,
+              withdrawalType: OFFER_WITHDRAWAL_TYPE_OPTIONS.BY_EMAIL,
+              withdrawalDelay: 60 * 60 * 24 * 2,
+            },
+          },
+        }
+        const { getByTestId } = render(
+          <BookingDetailsTicketContent booking={booking} proDisableEventsQrcode={true} />
+        )
+        getByTestId('collect-info-delay')
+      })
+
+      it('By email with delay and email normally received', () => {
+        const booking = {
+          ...initialBooking,
+          confirmationDate: formatISO(new Date()).slice(0, -1),
+          stock: {
+            ...initialBooking.stock,
+            beginningDatetime: formatISO(addDays(new Date(), 2)).slice(0, -1),
+            offer: {
+              ...initialBooking.stock.offer,
+              withdrawalType: OFFER_WITHDRAWAL_TYPE_OPTIONS.BY_EMAIL,
+              withdrawalDelay: 60 * 60 * 24 * 2,
+            },
+          },
+        }
+
+        const { getByTestId } = render(
+          <BookingDetailsTicketContent booking={booking} proDisableEventsQrcode={true} />
+        )
+        getByTestId('collect-info-email')
+      })
+
+      it('By email without delay and email not received yet', () => {
+        const booking = {
+          ...initialBooking,
+          confirmationDate: formatISO(new Date()).slice(0, -1),
+          stock: {
+            ...initialBooking.stock,
+            beginningDatetime: formatISO(addDays(new Date(), 3)).slice(0, -1),
+            offer: {
+              ...initialBooking.stock.offer,
+              withdrawalType: OFFER_WITHDRAWAL_TYPE_OPTIONS.BY_EMAIL,
+              withdrawalDelay: 0,
+            },
+          },
+        }
+
+        const { queryByTestId } = render(
+          <BookingDetailsTicketContent booking={booking} proDisableEventsQrcode={true} />
+        )
+        expect(queryByTestId('collect-info-delay')).toBeFalsy()
+      })
+
+      it('By email, email normally received and same day event', () => {
+        const booking = {
+          ...initialBooking,
+          confirmationDate: formatISO(addDays(new Date(), -2)).slice(0, -1),
+          stock: {
+            ...initialBooking.stock,
+            beginningDatetime: formatISO(new Date()).slice(0, -1),
+            offer: {
+              ...initialBooking.stock.offer,
+              withdrawalType: OFFER_WITHDRAWAL_TYPE_OPTIONS.BY_EMAIL,
+              withdrawalDelay: 0,
+            },
+          },
+        }
+
+        const { getByTestId } = render(
+          <BookingDetailsTicketContent booking={booking} proDisableEventsQrcode={true} />
+        )
+        getByTestId('collect-info-email')
+      })
     })
 
-    it('should display on site collect wording with delay', () => {
-      const booking = bookingsSnap.ongoing_bookings[2]
-      booking.stock.offer.isDigital = false
-      const { getByTestId } = render(
-        <BookingDetailsTicketContent booking={booking} proDisableEventsQrcode={true} />
-      )
-      getByTestId('collect-info-delay')
-    })
+    describe('formatCollectDelayString', () => {
+      describe('should display collect wording', () => {
+        it('In minutes', () => {
+          const message = formatCollectDelayString(1800)
+          expect(message).toEqual('30 minutes')
+        })
 
-    it('should display on site collect wording without delay', () => {
-      const booking = bookingsSnap.ongoing_bookings[1]
-      booking.stock.offer.isDigital = false
-      const { queryByTestId } = render(
-        <BookingDetailsTicketContent booking={booking} proDisableEventsQrcode={true} />
-      )
-      expect(queryByTestId('collect-info-delay')).toBeFalsy()
+        it('In hour', () => {
+          const message = formatCollectDelayString(3600)
+          expect(message).toEqual('1 heure')
+        })
+
+        it('In hours', () => {
+          const message = formatCollectDelayString(7200)
+          expect(message).toEqual('2 heures')
+        })
+
+        it('In days', () => {
+          const message = formatCollectDelayString(518400)
+          expect(message).toEqual('6 jours')
+        })
+
+        it('In week', () => {
+          const message = formatCollectDelayString(604800)
+          expect(message).toEqual('1 semaine')
+        })
+      })
     })
   })
 })
--- src/features/bookings/components/types.ts
+++ src/features/bookings/components/types.ts
@@ -5,3 +5,9 @@ export type Booking = BookingReponse
 export interface BookingItemProps {
   booking: Booking
 }
+
+export enum OFFER_WITHDRAWAL_TYPE_OPTIONS {
+  ON_SITE = 'on_site',
+  NO_TICKET = 'no_ticket',
+  BY_EMAIL = 'by_email',
+}
