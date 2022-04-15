2155b8e14e6301aa35d8ea7e58bb348bfb931eb7
Wendy <90036171+WendyPassculture@users.noreply.github.com>
2022-04-15 09:20:53 +0200

    (PC-13341) feat(CulturalSurvey): Create mapCulturalSurveyTypeToIcon (#2872)
    Co-authored-by: Wendy Jean-louis <jeanlouis.wendy@gmail.com>

--- src/libs/parsers/tests/culturalSurveyType.test.ts
+++ src/libs/parsers/tests/culturalSurveyType.test.ts
@@ -1,10 +1,10 @@
-import { CulturalSurveyTypeCodeKey } from 'api/gen'
+import { CulturalSurveyAnswerEnum } from 'api/gen'
 import { mapCulturalSurveyTypeToIcon } from 'libs/parsers/culturalSurveyType'
 import CulturalSurveyIcons from 'ui/svg/icons/culturalSurvey'
 
 describe('culturalSurveyType', () => {
   it('should have default icon as FestivalIcon', () => {
-    const result = mapCulturalSurveyTypeToIcon('__unknown_key__' as CulturalSurveyTypeCodeKey)
+    const result = mapCulturalSurveyTypeToIcon('__unknown_key__' as CulturalSurveyAnswerEnum)
     expect(result).toBe(CulturalSurveyIcons.FestivalIcon)
   })
 })
