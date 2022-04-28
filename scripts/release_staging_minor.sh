e59e655056f17cd8a35bce7edd4b642691564e86
Yorick Vinesse <yorick.vinesse@gmail.com>
2022-04-28 16:12:01 +0200

    :sparkles: (pc-14340) release staging version with a minor # diff of 1

---
+++ scripts/release_staging_minor.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+# When releasing a staging minor, all we do is getting the version from 
+# the package.json and pushing the corresponding tag to trigger the CI.
+
+
+./scripts/check_branch.sh
+
+./scripts/create_and_push_tag_from_package_json_version.sh "v"
+
