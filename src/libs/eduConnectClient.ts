7c4c89702e8d48005b599541208f36c4107ec360
AnoukHello <anoukhello22@gmail.com>
2022-05-03 10:49:31 +0200

    (PC-14256) useEduConnectLogin: better manage errors

--- src/libs/eduConnectClient.ts
+++ src/libs/eduConnectClient.ts
@@ -2,7 +2,6 @@ import { api } from 'api/api'
 import { refreshAccessToken } from 'api/apiHelpers'
 import { env } from 'libs/environment'
 import { getAccessTokenStatus } from 'libs/jwt'
-import { eventMonitoring } from 'libs/monitoring'
 import { storage } from 'libs/storage'
 
 export const eduConnectClient = {
@@ -13,14 +12,12 @@ export const eduConnectClient = {
     const accessToken = await storage.readString('access_token')
     const accessTokenStatus = getAccessTokenStatus(accessToken)
     if (accessTokenStatus === 'unknown') {
-      eventMonitoring.captureMessage('eduConnectClient failed to decodeAccessToken')
-      return Promise.reject(new Error())
+      return Promise.reject(new Error('eduConnectClient failed to decodeAccessToken'))
     }
     if (accessTokenStatus === 'expired') {
       const { result, error } = await refreshAccessToken(api)
 
       if (error) {
-        eventMonitoring.captureException(new Error(`eduConnectClient ${error}`))
         return Promise.reject(new Error(`eduConnectClient ${error}`))
       }
 
