b93ebaa8374015be62a0aea1e947d50b828c69f7
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-05-04 11:50:20 +0200

    (PC-14855) test(apiHelpers): Add tests for handleGeneratedApiResponse

--- src/api/apiHelpers.ts
+++ src/api/apiHelpers.ts
@@ -175,8 +175,11 @@ export async function handleGeneratedApiResponse(response: Response): Promise<an
 
   // We are not suppose to have side-effects in this function but this is a special case
   // where the access token is corrupted and we need to recreate it by logging-in again
-  if (response.status === 401 && response.statusText === 'NeedsAuthenticationResponse') {
-    eventMonitoring.captureException(new Error('NeedsAuthenticationResponse'))
+  if (
+    response.status === NeedsAuthenticationResponse.status &&
+    response.statusText === NeedsAuthenticationResponse.statusText
+  ) {
+    eventMonitoring.captureException(new Error(NeedsAuthenticationResponse.statusText))
     navigateToLogin()
     return {}
   }
