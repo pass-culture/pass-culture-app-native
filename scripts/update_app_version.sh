e1b88c4c0505ca2511cac220c0c2ea1048cb7f05
Yorick Vinesse <44037911+yorickeando@users.noreply.github.com>
2022-05-05 16:01:44 +0200

    :wrench: ((no-ticket)) remove now useless warning for patches (#2968)

--- scripts/update_app_version.sh
+++ scripts/update_app_version.sh
@@ -46,25 +46,7 @@ update_app_version(){
   fi
 }
 
-check_hard_hotfix_integrity(){
-  CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-  echo $CURRENT_BRANCH
-  if [ "$CURRENT_BRANCH" != "master" ];
-  then
-    read -p "Do not forget to cherry-pick your bump version commit to master" -n 1 -r
-    echo    # step a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-       echo "Thanks"
-    else
-     echo "Why not? :("
-     exit 1
-    fi
-  fi
-}
 
 # $1 is the tag used to trigger the ci deployment (see .circleci/config.yml)
 # $2 is upgrade level (major, minor or patch)
 update_app_version "$1" "$2"
-
-check_hard_hotfix_integrity
