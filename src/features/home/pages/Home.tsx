c837b45ba75c1ec9ed6f2b26f58585cac6760486
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2022-05-11 14:32:39 +0200

    (PC-14945) fix(tabBar): Fix tabBar lag when playlists are loading in native (#2980)

--- src/features/home/pages/Home.tsx
+++ src/features/home/pages/Home.tsx
@@ -1,6 +1,12 @@
 import { useRoute } from '@react-navigation/native'
-import React, { useCallback, FunctionComponent } from 'react'
-import { FlatList, ScrollView, NativeScrollEvent, NativeSyntheticEvent } from 'react-native'
+import React, { useCallback, useEffect, useState, FunctionComponent } from 'react'
+import {
+  FlatList,
+  ScrollView,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Platform,
+} from 'react-native'
 import styled from 'styled-components/native'
 
 import { useHomepageModules } from 'features/home/api'
@@ -21,6 +27,7 @@ import { UseRouteType } from 'features/navigation/RootNavigator'
 import { useABTestingContext } from 'libs/ABTesting'
 import { analytics, isCloseToBottom } from 'libs/analytics'
 import useFunctionOnce from 'libs/hooks/useFunctionOnce'
+import { Spinner } from 'ui/components/Spinner'
 import { Spacer } from 'ui/theme'
 
 const keyExtractor = (item: ProcessedModule, index: number) =>
@@ -61,20 +68,61 @@ const renderModule = ({ item, index }: { item: ProcessedModule; index: number })
   return <React.Fragment></React.Fragment>
 }
 
+const FooterComponent = ({ isLoading }: { isLoading: boolean }) => {
+  return (
+    <React.Fragment>
+      {!!isLoading && (
+        <React.Fragment>
+          <Spinner />
+          <Spacer.Column numberOfSpaces={6} />
+        </React.Fragment>
+      )}
+      <Spacer.TabBar />
+    </React.Fragment>
+  )
+}
+
 export const Home: FunctionComponent = () => {
   const { params } = useRoute<UseRouteType<'Home'>>()
   const { homeEntryId } = useABTestingContext()
   const modules = useHomepageModules(params?.entryId ?? homeEntryId) || []
   const logHasSeenAllModules = useFunctionOnce(() => analytics.logAllModulesSeen(modules.length))
   const showSkeleton = useShowSkeleton()
+  const initialNumToRender = 5
+  const maxToRenderPerBatch = 5
+  const [maxIndex, setMaxIndex] = useState(initialNumToRender)
+  const [isLoading, setIsLoading] = useState(false)
+  const modulesToDisplay = Platform.OS === 'web' ? modules : modules.slice(0, maxIndex)
 
   const onScroll = useCallback(
     ({ nativeEvent }: NativeSyntheticEvent<NativeScrollEvent>) => {
-      if (isCloseToBottom(nativeEvent)) logHasSeenAllModules()
+      if (isCloseToBottom(nativeEvent) && modulesToDisplay.length === modules.length)
+        logHasSeenAllModules()
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [modules.length]
+    [modules.length, modulesToDisplay.length]
   )
+
+  const onEndReached = useCallback(() => {
+    if (Platform.OS === 'web') return
+    if (maxIndex < modules.length) {
+      setIsLoading(true)
+      setMaxIndex(maxIndex + maxToRenderPerBatch)
+    }
+  }, [modules.length, maxIndex])
+
+  useEffect(() => {
+    // We use this to load more modules, in case the content size doesn't change after the load triggered by onEndReached (i.e. no new modules were shown).
+    const loadMore = setInterval(() => {
+      if (maxIndex < modules.length && isLoading) {
+        setMaxIndex(maxIndex + maxToRenderPerBatch)
+      } else {
+        setIsLoading(false)
+      }
+    }, 3000)
+
+    return () => clearInterval(loadMore)
+  }, [modules.length, isLoading, maxIndex])
 
   return (
     <Container>
@@ -98,14 +146,16 @@ export const Home: FunctionComponent = () => {
           scrollEventThrottle={400}
           bounces={false}
           onScroll={onScroll}
-          data={modules}
+          data={modulesToDisplay}
           renderItem={renderModule}
           keyExtractor={keyExtractor}
-          ListFooterComponent={<Spacer.TabBar />}
+          ListFooterComponent={<FooterComponent isLoading={isLoading} />}
           ListHeaderComponent={ListHeaderComponent}
-          initialNumToRender={5}
+          initialNumToRender={initialNumToRender}
           onEndReachedThreshold={0.5}
           removeClippedSubviews={false}
+          onEndReached={onEndReached}
+          onContentSizeChange={() => setIsLoading(false)}
         />
       </HomeBodyLoadingContainer>
       <Spacer.Column numberOfSpaces={6} />
