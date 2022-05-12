b3dd54693e860cd4191cd35eb12d53074635d4ac
Yorick Vinesse <44037911+yorickeando@users.noreply.github.com>
2022-05-12 17:37:41 +0200

    (PC-14340) build(versions): update versioning system (#2910)
    * :recycle: (pc-14340) extract shell scripts for readability
    
    * :green_heart: (pc-14340) update deploy regex
    
    * :sparkles: (pc-14340) add specific minor deploy staging script
    
    * :sparkles: (pc-14340) release staging version with a minor # diff of 1
    
    * :sparkles: (pc-14340) extract get_version in shell script

--- scripts/update_app_version.sh
+++ scripts/update_app_version.sh
@@ -5,41 +5,14 @@ set -e
 update_app_version(){
   yarn version --"$2" --no-git-tag-version
 
-  VERSION=`json -f package.json version`
-
-  # We have to increment the build number because it is used as the versionCode
-  # in app/build.gradle: versionCode (packageJson.build as Integer) and the
-  # Play Store requires this value to be strictly increasing. Otherwise, we get this error:
-
-  # ERROR Google Api Error: forbidden: You cannot rollout this release because it does not allow
-  # any existing users to upgrade to the newly added APKs.
-
-  # As such, since the current version is 10136027 = 1 * 10_000_000 + 136 * 1_000 + 27, we build each new version
-  # as the sum of:
-  #  + 10_000_000 * MAJOR,
-  #  + 1_000 * MINOR,
-  #  + PATCH
-  # where <VERSION> = <MAJOR>.<MINOR>.<PATCH> (ex: 1.137.3) as we use semantic versioning.
-
-  # Examples:
-  #   1.136.27 => 10 000 000 + 136 000 + 027 => 10136027
-  #   1.137.1  => 10 000 000 + 137 000 + 001 => 10137001
-
-
-  SEMVER=( ${VERSION//./ } )
-  MAJOR=${SEMVER[0]}
-  MINOR=${SEMVER[1]}
-  PATCH=${SEMVER[2]}
-  BUILD_NUMBER="$((10000000 * MAJOR + 1000 * MINOR + PATCH))"
-  CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-
-  json -I -f package.json -e "this.build=$BUILD_NUMBER"
-
+  source ./scripts/get_version.sh
+  ./scripts/update_build_number_from_package_json_version.sh
   git add package.json
-  git commit -m "v${VERSION}"
-  git tag -a "$1${VERSION}" -m "v${VERSION}"
-  git push origin "$1${VERSION}"
 
+  git commit -m "v${VERSION}"
+  ./scripts/create_and_push_tag_from_package_json_version.sh "$1"
+
+  CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
   if [ "$CURRENT_BRANCH" = "master" ]
   then
     git push
