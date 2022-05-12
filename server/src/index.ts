81fae3992da5d402b4c4de264b80fdc4051d0585
Michael MACHADO <michael.machado@passculture.app>
2022-05-12 08:40:56 +0200

    Add app engine configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 // FIXME: for some reason, this is scanned by root project and cause a linting error
 // eslint-disable-next-line import/no-unresolved
-import { region } from 'firebase-functions'
 
 import { app } from './app'
 import { env } from './libs/environment/env'
@@ -8,21 +7,9 @@ import { logger } from './utils/logging'
 
 let appServer
 
-if (env.__DEV__) {
-  const PORT = Number(process.env.PORT) || 8080
-  appServer = app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    /* istanbul ignore next */
-    process.env.NODE_ENV !== 'test' && logger.info(`[${env.ENV}] Listening on port ${PORT}`)
-  })
-} else {
-  // TODO: see if unit test of next line is possible using https://www.npmjs.com/package/firebase-functions-test
-  appServer = region(env.REGION).https.onRequest(app)
-}
-
-const functionName =
-  process.env.NODE_ENV === 'test'
-    ? 'server'
-    : `server${env.ENV[0].toUpperCase()}${env.ENV.slice(1)}`
-
-module.exports[functionName] = appServer
+const PORT = Number(process.env.PORT) || 8080
+appServer = app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  /* istanbul ignore next */
+  process.env.NODE_ENV !== 'test' && logger.info(`[${env.ENV}] Listening on port ${PORT}`)
+})
