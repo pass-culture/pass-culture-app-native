9b5fd7d8fede73b8aa479c94560aed2f05a36efd
AnoukHello <anoukhello22@gmail.com>
2022-05-16 09:43:42 +0200

    (PC-14857) recommendation: update url

--- src/features/home/pages/useHomeRecommendedHits.ts
+++ src/features/home/pages/useHomeRecommendedHits.ts
@@ -27,7 +27,7 @@ export const getRecommendationEndpoint = (
   position: GeoCoordinates | null
 ): string | undefined => {
   if (!userId) return undefined
-  const endpoint = `${env.RECOMMENDATION_ENDPOINT}/recommendation/${userId}?token=${env.RECOMMENDATION_TOKEN}`
+  const endpoint = `${env.RECOMMENDATION_ENDPOINT}/playlist_recommendation/${userId}?token=${env.RECOMMENDATION_TOKEN}`
 
   if (position) return `${endpoint}&longitude=${position.longitude}&latitude=${position.latitude}`
   return endpoint
