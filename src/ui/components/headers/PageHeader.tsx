0c121ec2268331458b502ecb9afe84d5df82b62b
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2022-05-24 11:51:14 +0200

    (PC-15078) feat(accessibility): add HiddenText for previous button and toggle (#3014)

--- src/ui/components/headers/PageHeader.tsx
+++ src/ui/components/headers/PageHeader.tsx
@@ -6,6 +6,7 @@ import styled from 'styled-components/native'
 import { homeNavConfig } from 'features/navigation/TabBar/helpers'
 import { useGoBack } from 'features/navigation/useGoBack'
 import { accessibilityAndTestId } from 'libs/accessibilityAndTestId'
+import { HiddenText } from 'ui/components/HiddenText'
 import { Touchable } from 'ui/components/touchable/Touchable'
 import { useElementWidth } from 'ui/hooks/useElementWidth'
 import { ArrowPrevious as DefaultArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
@@ -29,6 +30,7 @@ const HeaderIconBack: React.FC<HeaderIconProps> = ({ onGoBack }) => {
   return (
     <Touchable onPress={onGoBack || goBack} {...accessibilityAndTestId(t`Revenir en arrière`)}>
       <ArrowPrevious testID="icon-back" />
+      <HiddenText>{t`Retour`}</HiddenText>
     </Touchable>
   )
 }
