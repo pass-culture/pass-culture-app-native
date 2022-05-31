525f11957a1066666df58c1c5ffb471546e28f8e
Hugo Voisin <hugo.voisin@ensta-paristech.fr>
2022-05-31 14:36:06 +0200

    (PC-14994) test(search): add category component to storybook (#3042)
    * (PC-14994) test(search): add category component to storybook
    
    * (PC-14994) test: add snapshot

---
+++ src/features/search/components/__tests__/CategoryButton.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { CategoryButton } from 'features/search/components/CategoryButton'
+import { render } from 'tests/utils'
+import { Bookstore } from 'ui/svg/icons/bicolor/Bookstore'
+
+describe('CategoryButton', () => {
+  it('should render correctly', () => {
+    const categoryButton = render(
+      <CategoryButton label="Bibliothèques & Médiathèques" Icon={Bookstore} />
+    )
+    expect(categoryButton).toMatchSnapshot()
+  })
+})
