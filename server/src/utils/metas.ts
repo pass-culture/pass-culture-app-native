b96a69fd9526d0baaa39edb20775074ea06b2aa3
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-06-02 15:21:11 +0200

    improve regexp

--- server/src/utils/metas.ts
+++ server/src/utils/metas.ts
@@ -7,25 +7,26 @@ const { APP_PUBLIC_URL } = env
 const { href } = new URL(APP_PUBLIC_URL)
 
 const REGEX = {
-  title: /<meta\s(name)="(title)"\scontent="([^"]*)"\s*\/?>/g,
-  description: /<meta\s(name)="(description)"\scontent="([^"]*)"\s*\/?>/g,
-  ['og:url']: /<meta\s(property)="(og:url)"\scontent="([^"]*)"\s*\/?>/g,
-  ['og:title']: /<meta\s(property)="(og:title)"\scontent="([^"]*)"\s*\/?>/g,
-  ['og:description']: /<meta\s(property)="(og:description)"\scontent="([^"]*)"\s*\/?>/g,
-  ['og:image']: /<meta\s(property)="(og:image)"\scontent="([^"]*)"\s*\/?>/g,
-  ['og:image:alt']: /<meta\s(property)="(og:image:alt)"\scontent="([^"]*)"\s*\/?>/g,
-  ['twitter:card']: /<meta\s(name)="(twitter:card)"\scontent="([^"]*)"\s*\/?>/g,
-  ['twitter:url']: /<meta\s(name)="(twitter:url)"\scontent="([^"]*)"\s*\/?>/g,
-  ['twitter:title']: /<meta\s(name)="(twitter:title)"\scontent="([^"]*)"\s*\/?>/g,
-  ['twitter:description']: /<meta\s(name)="(twitter:description)"\scontent="([^"]*)"\s*\/?>/g,
-  ['twitter:image']: /<meta\s(name)="(twitter:image)"\scontent="([^"]*)"\s*\/?>/g,
-  ['twitter:image:alt']: /<meta\s(name)="(twitter:image:alt)"\scontent="([^"]*)"\s*\/?>/g,
-  ['twitter:app:url:iphone']: /<meta\s(name)="(twitter:app:url:iphone)"\scontent="([^"]*)"\s*\/?>/g,
-  ['twitter:app:url:ipad']: /<meta\s(name)="(twitter:app:url:ipad)"\scontent="([^"]*)"\s*\/?>/g,
+  title: /<meta\s+(name)="(title)"\s+content="([^"]*)"\s*\/?>/g,
+  description: /<meta\s+(name)="(description)"\s+content="([^"]*)"\s*\/?>/g,
+  ['og:url']: /<meta\s+(property)="(og:url)"\s+content="([^"]*)"\s*\/?>/g,
+  ['og:title']: /<meta\s+(property)="(og:title)"\s+content="([^"]*)"\s*\/?>/g,
+  ['og:description']: /<meta\s+(property)="(og:description)"\s+content="([^"]*)"\s*\/?>/g,
+  ['og:image']: /<meta\s+(property)="(og:image)"\s+content="([^"]*)"\s*\/?>/g,
+  ['og:image:alt']: /<meta\s+(property)="(og:image:alt)"\s+content="([^"]*)"\s*\/?>/g,
+  ['twitter:card']: /<meta\s+(name)="(twitter:card)"\s+content="([^"]*)"\s*\/?>/g,
+  ['twitter:url']: /<meta\s+(name)="(twitter:url)"\s+content="([^"]*)"\s*\/?>/g,
+  ['twitter:title']: /<meta\s+(name)="(twitter:title)"\s+content="([^"]*)"\s*\/?>/g,
+  ['twitter:description']: /<meta\s+(name)="(twitter:description)"\s+content="([^"]*)"\s*\/?>/g,
+  ['twitter:image']: /<meta\s+(name)="(twitter:image)"\s+content="([^"]*)"\s*\/?>/g,
+  ['twitter:image:alt']: /<meta\s+(name)="(twitter:image:alt)"\s+content="([^"]*)"\s*\/?>/g,
+  ['twitter:app:url:iphone']:
+    /<meta\s+(name)="(twitter:app:url:iphone)"\s+content="([^"]*)"\s*\/?>/g,
+  ['twitter:app:url:ipad']: /<meta\s+(name)="(twitter:app:url:ipad)"\s+content="([^"]*)"\s*\/?>/g,
   ['twitter:app:url:googleplay']:
-    /<meta\s(name)="(twitter:app:url:googleplay)"\scontent="([^"]*)"\s*\/?>/g,
-  ['al:ios:url']: /<meta\s(name)="(al:ios:url)"\scontent="([^"]*)"\s*\/?>/g,
-  ['al:android:url']: /<meta\s(name)="(al:android:url)"\scontent="([^"]*)"\s*\/?>/g,
+    /<meta\s+(name)="(twitter:app:url:googleplay)"\s+content="([^"]*)"\s*\/?>/g,
+  ['al:ios:url']: /<meta\s+(name)="(al:ios:url)"\s+content="([^"]*)"\s*\/?>/g,
+  ['al:android:url']: /<meta\s+(name)="(al:android:url)"\s+content="([^"]*)"\s*\/?>/g,
 }
 
 export async function replaceHtmlMetas(
