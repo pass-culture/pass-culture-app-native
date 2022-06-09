901a987d62eeefc71e2399084bbd578186840f53
bruno ebstein <bruno.ebstein@passculture.app>
2022-06-09 15:00:44 +0200

    (PC-14996) refactor(search): split legacy and reworked search (#3092)
    * refactor(search): split legacy and reworked search
    
    * reworked is the new version behind a feature flag
    * legacy will be deleted after the feature flag has been enabled
    
    * refactor: comment french -> english
    
    * refactor: use `getSpacing` instead of hardcoding value
    
    * feat(search): update snapshots
    
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

---
+++ src/features/search/pages/SearchRework.tsx
@@ -0,0 +1,52 @@
+import { useRoute } from '@react-navigation/native'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components/native'
+import { v4 as uuidv4 } from 'uuid'
+
+import { UseRouteType } from 'features/navigation/RootNavigator'
+import { SearchLandingPage } from 'features/search/components'
+import { SearchDetails } from 'features/search/components/SearchDetails'
+import { SearchHeaderRework } from 'features/search/components/SearchHeaderRework'
+import { useSearch } from 'features/search/pages/SearchWrapper'
+import { useShowResults } from 'features/search/pages/useShowResults'
+import { Form } from 'ui/web/form/Form'
+
+export function SearchRework() {
+  const { params } = useRoute<UseRouteType<'Search'>>()
+  const { dispatch } = useSearch()
+  const showResults = useShowResults()
+  const searchInputID = uuidv4()
+  const [isFocus, setIsFocus] = useState(false)
+
+  useEffect(() => {
+    if (params) {
+      dispatch({ type: 'SET_STATE_FROM_NAVIGATE', payload: params })
+      dispatch({ type: 'SHOW_RESULTS', payload: true })
+    }
+  }, [dispatch, params])
+
+  const bodySearch = () => {
+    // SearchDetails will integrate recent searches and suggestions
+    if (showResults || isFocus) return <SearchDetails />
+    return <SearchLandingPage />
+  }
+
+  const onFocusState = (focus: boolean) => {
+    setIsFocus(focus)
+  }
+
+  return (
+    <Container>
+      <Form.Flex>
+        <SearchHeaderRework
+          searchInputID={searchInputID}
+          onFocusState={onFocusState}
+          isFocus={isFocus}
+        />
+        {bodySearch()}
+      </Form.Flex>
+    </Container>
+  )
+}
+
+const Container = styled.View({ flex: 1 })
