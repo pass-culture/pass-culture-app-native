3c2eb4fa15aa0d74f09ccd478ef3b2bd659cd096
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-06-10 14:50:26 +0200

    fix(firestore): set cacheSizeBytes to 0 and persistence false

--- src/libs/firebase/firestore/client.ts
+++ src/libs/firebase/firestore/client.ts
@@ -1,3 +1,8 @@
 import firestore from 'libs/firebase/shims/firestore'
 
 export const firestoreRemoteStore = firestore()
+
+firestoreRemoteStore.settings({
+  cacheSizeBytes: 0,
+  persistence: false,
+})
