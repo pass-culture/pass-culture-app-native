41ead88baed3f9167640d179adf5448c50320886
Lucas Beneston <lucas.beneston@passculture.app>
2022-06-10 16:59:11 +0200

    (PC-15303) fix(Sonar): resolve 'This assertion is unnecessary since it does not change the type of the expression' issue

--- src/libs/environment/env.ts
+++ src/libs/environment/env.ts
@@ -2,6 +2,4 @@ import Config from '@bam.tech/react-native-config'
 
 import { parseBooleanVariables } from 'libs/environment/parseBooleanVariables'
 
-import { Environment } from './types'
-
-export const env = parseBooleanVariables(Config) as Environment
+export const env = parseBooleanVariables(Config)
