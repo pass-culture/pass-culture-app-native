b9e642f9cbb9e91412064648b2ca2314211877b0
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2022-06-20 18:29:55 +0200

    (PC-15707) feat(OfflineMode): use not connected template on favorite (#3145)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- src/features/favorites/pages/Favorites.tsx
+++ src/features/favorites/pages/Favorites.tsx
@@ -6,11 +6,18 @@ import { useAuthContext } from 'features/auth/AuthContext'
 import { FavoritesResults } from 'features/favorites/components/FavoritesResults'
 import { NotConnectedFavorites } from 'features/favorites/components/NotConnectedFavorites'
 import { SvgPageHeader } from 'ui/components/headers/SvgPageHeader'
+import { OfflinePage } from 'libs/network/OfflinePage'
+import { useNetInfo } from 'libs/network/useNetInfo'
 
 export const Favorites: React.FC = () => {
+  const netInfo = useNetInfo()
   const { isLoggedIn } = useAuthContext()
 
-  if (!isLoggedIn) return <NotConnectedFavorites />
+  if (!netInfo.isConnected) {
+    return <OfflinePage />
+  } else if (!isLoggedIn) {
+    return <NotConnectedFavorites />
+  }
 
   return (
     <Container>
