4170cdab0ce4552b6a195f24fc4d5947c1730ae6
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-06-23 18:19:06 +0200

    test(search): add navigate on autocomplete item

--- src/features/search/components/__tests__/SearchAutocompleteItem.native.test.tsx
+++ src/features/search/components/__tests__/SearchAutocompleteItem.native.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
+import { navigate } from '__mocks__/@react-navigation/native'
 import { SearchGroupNameEnum } from 'api/gen'
 import { SearchAutocompleteItem } from 'features/search/components/SearchAutocompleteItem'
-import { render } from 'tests/utils'
+import { render, fireEvent } from 'tests/utils'
 
 describe('SearchAutocompleteItem component', () => {
   const hit = {
@@ -15,7 +16,7 @@ describe('SearchAutocompleteItem component', () => {
     expect(render(<SearchAutocompleteItem index={0} hit={hit} />)).toMatchSnapshot()
   })
 
-  it('should display the search group name if the item is in the top three', () => {
+  it('should display the search group name if the hit is in the top three', () => {
     const { queryByText } = render(<SearchAutocompleteItem index={0} hit={hit} />)
 
     const text = 'Test1 dans Cinéma'
@@ -23,11 +24,18 @@ describe('SearchAutocompleteItem component', () => {
     expect(queryByText(text)).toBeTruthy()
   })
 
-  it('should not display the search group name if the item is not in the top three', () => {
+  it('should not display the search group name if the hit is not in the top three', () => {
     const { queryByText } = render(<SearchAutocompleteItem index={3} hit={hit} />)
 
     const text = 'Test1'
 
     expect(queryByText(text)).toBeTruthy()
+  })
+
+  it('should navigate on selected offer on hit click', async () => {
+    const { getByText } = render(<SearchAutocompleteItem index={0} hit={hit} />)
+    await fireEvent.press(getByText('Test1 dans Cinéma'))
+    expect(navigate).toBeCalledTimes(1)
+    expect(navigate).toBeCalledWith('Offer', { id: +hit.objectID, from: 'search' })
   })
 })
