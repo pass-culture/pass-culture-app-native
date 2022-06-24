b1930df7db7ae2b5118538e223a601baddc314dd
AnoukHello <anoukhello22@gmail.com>
2022-06-24 12:53:57 +0200

    (PC-14869) SetPhoneNumber: fix warning in tests

--- src/features/identityCheck/pages/phoneValidation/__tests__/SetPhoneNumber.native.test.tsx
+++ src/features/identityCheck/pages/phoneValidation/__tests__/SetPhoneNumber.native.test.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 
 import { SetPhoneNumber } from 'features/identityCheck/pages/phoneValidation/SetPhoneNumber'
-import { render } from 'tests/utils'
+import { render, waitFor } from 'tests/utils'
+import * as useModalAPI from 'ui/components/modals/useModal'
 
 describe('SetPhoneNumber', () => {
-  it('should match snapshot', () => {
+  // FIXME(anoukhello) find a way to get snapshot with modal animation
+  // when keeping visible to true, snapshot is different on every test run on modal animation props opacity and translateY
+  it('should match snapshot without modal appearance', async () => {
+    jest.spyOn(useModalAPI, 'useModal').mockReturnValueOnce({
+      visible: false,
+      showModal: jest.fn(),
+      hideModal: jest.fn(),
+      toggleModal: jest.fn(),
+    })
     const SetPhoneNumberPage = render(<SetPhoneNumber />)
-    expect(SetPhoneNumberPage).toMatchSnapshot()
+    await waitFor(() => expect(SetPhoneNumberPage).toMatchSnapshot())
   })
 
-  it('should show modal on first render', () => {
-    const { queryByText } = render(<SetPhoneNumber />)
-    expect(queryByText("J'ai compris")).toBeTruthy()
+  it('should show modal on first render', async () => {
+    const { getByText } = render(<SetPhoneNumber />)
+    await waitFor(() => expect(getByText("J'ai compris")).toBeTruthy())
   })
 
   // TODO PC-14869 : implement the check that modal is visible when going to SetPhoneValiditationCode then using GoBack
