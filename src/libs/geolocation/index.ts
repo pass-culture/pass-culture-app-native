e16015f45cac15929faf0da01e8a635426f9876c
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-06-27 10:31:02 +0200

    (PC-13359) refactor(ColorsEnum): Remove temporary component

--- src/libs/geolocation/index.ts
+++ src/libs/geolocation/index.ts
@@ -1,12 +1,10 @@
 import { checkGeolocPermission } from './checkGeolocPermission'
-import CoordinatesView from './components/CoordinatesView'
 import { GEOLOCATION_USER_ERROR_MESSAGE, GeolocPermissionState, GeolocPositionError } from './enums'
 import { useGeolocation, GeolocationWrapper } from './GeolocationWrapper'
 import { requestGeolocPermission } from './requestGeolocPermission'
 import { GeoCoordinates, GeolocationError, IGeolocationContext } from './types'
 
 export {
-  CoordinatesView,
   useGeolocation,
   GeolocationWrapper,
   requestGeolocPermission,
