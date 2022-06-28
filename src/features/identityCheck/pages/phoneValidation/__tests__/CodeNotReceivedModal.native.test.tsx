7f103d8da197afe6585205ae50238b61c62889eb
AnoukHello <anoukhello22@gmail.com>
2022-06-28 15:48:47 +0200

    (PC-14461) phoneValidation: request new code on CodeNotReceivedModal

--- src/features/identityCheck/pages/phoneValidation/__tests__/CodeNotReceivedModal.native.test.tsx
+++ src/features/identityCheck/pages/phoneValidation/__tests__/CodeNotReceivedModal.native.test.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
-import { CodeNotReceivedModal } from 'features/identityCheck/pages/phoneValidation/CodeNotReceivedModal'
-import { fireEvent, render } from 'tests/utils'
+import { navigate } from '__mocks__/@react-navigation/native'
+import { ApiError } from 'api/apiHelpers'
+import {
+  CodeNotReceivedModal,
+  CodeNotReceivedModalProps,
+} from 'features/identityCheck/pages/phoneValidation/CodeNotReceivedModal'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { fireEvent, render, waitFor } from 'tests/utils'
+import { SNACK_BAR_TIME_OUT } from 'ui/components/snackBar/SnackBarContext'
+import { SnackBarHelperSettings } from 'ui/components/snackBar/types'
+
+const mockShowErrorSnackBar = jest.fn()
+jest.mock('ui/components/snackBar/SnackBarContext', () => ({
+  useSnackBarContext: () => ({
+    showErrorSnackBar: jest.fn((props: SnackBarHelperSettings) => mockShowErrorSnackBar(props)),
+  }),
+}))
 
 describe('<CodeNotReceivedModal />', () => {
+  const mockFetch = jest.spyOn(global, 'fetch')
+
   it('should match snapshot', () => {
-    const renderAPI = render(<CodeNotReceivedModal isVisible dismissModal={jest.fn()} />)
+    const renderAPI = renderCodeNotReceivedModal()
     expect(renderAPI).toMatchSnapshot()
   })
 
@@ -13,21 +30,95 @@ describe('<CodeNotReceivedModal />', () => {
 
   it('should call dismissModal upon pressing on Close', () => {
     const dismissModalMock = jest.fn()
-    const { getByTestId } = render(
-      <CodeNotReceivedModal isVisible dismissModal={dismissModalMock} />
-    )
+    const { getByTestId } = renderCodeNotReceivedModal({ dismissModal: dismissModalMock })
     const closeButton = getByTestId('Fermer la modale')
 
     fireEvent.press(closeButton)
     expect(dismissModalMock).toBeCalled()
   })
 
-  it('should call dismissModal upon pressing on "Demander un autre code"', () => {
+  it('should dismiss modal on /send_phone_validation_code request success', async () => {
+    mockFetch.mockResolvedValueOnce(
+      new Response(JSON.stringify({}), {
+        headers: {
+          'content-type': 'application/json',
+        },
+        status: 200,
+      })
+    )
     const dismissModalMock = jest.fn()
-    const { getByText } = render(<CodeNotReceivedModal isVisible dismissModal={dismissModalMock} />)
-    const closeButton = getByText('Demander un autre code')
+    const { getByTestId } = renderCodeNotReceivedModal({ dismissModal: dismissModalMock })
 
-    fireEvent.press(closeButton)
-    expect(dismissModalMock).toBeCalled()
+    const requestNewCodeButton = getByTestId('Demander un autre code')
+    fireEvent.press(requestNewCodeButton)
+
+    await waitFor(() => expect(dismissModalMock).toHaveBeenCalled())
+  })
+
+  it('should dismiss modal if request fails', async () => {
+    mockFetch.mockRejectedValueOnce(
+      new ApiError(400, {
+        code: 'SOME_CODE',
+        message: 'some message',
+      })
+    )
+    const dismissModalMock = jest.fn()
+    const { getByTestId } = renderCodeNotReceivedModal({ dismissModal: dismissModalMock })
+
+    const requestNewCodeButton = getByTestId('Demander un autre code')
+    fireEvent.press(requestNewCodeButton)
+
+    await waitFor(() => expect(dismissModalMock).toHaveBeenCalled())
+  })
+
+  it('should show toaster with error message if request fails', async () => {
+    mockFetch.mockRejectedValueOnce(
+      new ApiError(400, {
+        code: 'SOME_CODE',
+        message: 'some message',
+      })
+    )
+    const { getByTestId } = renderCodeNotReceivedModal()
+
+    const requestNewCodeButton = getByTestId('Demander un autre code')
+    fireEvent.press(requestNewCodeButton)
+
+    await waitFor(() =>
+      expect(mockShowErrorSnackBar).toHaveBeenCalledWith({
+        message: 'some message',
+        timeout: SNACK_BAR_TIME_OUT,
+      })
+    )
+  })
+
+  it('should navigate to SetPhoneNumberTooManySMSSent page if request fails with TOO_MANY_SMS_SENT code', async () => {
+    mockFetch.mockRejectedValueOnce(
+      new ApiError(400, {
+        code: 'TOO_MANY_SMS_SENT',
+        message: 'Le nombre de tentatives maximal est dépassé',
+      })
+    )
+
+    const { getByTestId } = renderCodeNotReceivedModal()
+
+    const requestNewCodeButton = getByTestId('Demander un autre code')
+    fireEvent.press(requestNewCodeButton)
+
+    await waitFor(() => expect(navigate).toHaveBeenCalledWith('PhoneValidationTooManySMSSent'))
   })
 })
+
+function renderCodeNotReceivedModal(props?: Partial<CodeNotReceivedModalProps>) {
+  return render(
+    <CodeNotReceivedModal
+      isVisible
+      dismissModal={jest.fn()}
+      phoneNumber={'+3612345678'}
+      {...props}
+    />,
+    {
+      // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+      wrapper: ({ children }) => reactQueryProviderHOC(children),
+    }
+  )
+}
