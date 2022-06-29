448f95bb0cc64badd7205a9cd91b9e2b14ca3513
bruno ebstein <bruno.ebstein@passculture.app>
2022-06-29 15:56:16 +0200

    (PC-15130) fix(search): categories buttons must keep search context

--- src/features/search/pages/useShowResultsForCategory.ts
+++ src/features/search/pages/useShowResultsForCategory.ts
@@ -3,16 +3,19 @@ import { useCallback } from 'react'
 import { OnPressCategory } from 'features/search/components/CategoriesButtons'
 import { useSearch, useStagedSearch } from 'features/search/pages/SearchWrapper'
 
+import { useShowResultsWithStagedSearch } from './useShowResultsWithStagedSearch'
+
 export const useShowResultsForCategory = (): OnPressCategory => {
-  const { dispatch } = useSearch()
   const { dispatch: stagedDispatch } = useStagedSearch()
+  const showResultsWithStagedSearch = useShowResultsWithStagedSearch()
+  const { dispatch } = useSearch()
 
   return useCallback(
     (pressedCategory) => {
       stagedDispatch({ type: 'SET_CATEGORY', payload: [pressedCategory] })
+      showResultsWithStagedSearch()
       dispatch({ type: 'SET_CATEGORY', payload: [pressedCategory] })
-      dispatch({ type: 'SHOW_RESULTS', payload: true })
     },
-    [dispatch, stagedDispatch]
+    [stagedDispatch, dispatch, showResultsWithStagedSearch]
   )
 }
