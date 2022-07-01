566cb1c8bfb9a79b84e3bc2f944c0038b9415937
anoukhello <anoukhello22@gmail.com>
2022-07-01 09:47:39 +0200

    (PC-14461) phoneValidation: request new code on CodeNotReceivedModal (#3188)

--- src/features/identityCheck/pages/phoneValidation/CodeNotReceivedModal.tsx
+++ src/features/identityCheck/pages/phoneValidation/CodeNotReceivedModal.tsx
@@ -1,25 +1,50 @@
 import { t, plural } from '@lingui/macro'
+import { useNavigation } from '@react-navigation/native'
 import React, { FunctionComponent, useState } from 'react'
 import styled from 'styled-components/native'
 
+import { ApiError, extractApiErrorMessage } from 'api/apiHelpers'
+import { useSendPhoneValidationMutation } from 'features/identityCheck/api'
+import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { AppModal } from 'ui/components/modals/AppModal'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
 import { Close } from 'ui/svg/icons/Close'
 import { Spacer, Typo } from 'ui/theme'
 
-interface Props {
+export interface CodeNotReceivedModalProps {
   isVisible: boolean
   dismissModal: () => void
+  phoneNumber: string
 }
 
-export const CodeNotReceivedModal: FunctionComponent<Props> = (props) => {
+export const CodeNotReceivedModal: FunctionComponent<CodeNotReceivedModalProps> = (props) => {
   // TODO : PC-14462 implement attempts remaining fetch and delete this mock initialization
   const [requestsRemaining, setRequestsRemaining] = useState(5)
+  const { navigate } = useNavigation<UseNavigationType>()
+  const { showErrorSnackBar } = useSnackBarContext()
   const hasOneRequestRemaining = requestsRemaining === 1
 
   const requestsWording = plural(requestsRemaining, {
     one: '# demande',
     other: '# demandes',
+  })
+
+  const { mutate: sendPhoneValidationCode, isLoading } = useSendPhoneValidationMutation({
+    onSuccess: () => props.dismissModal(),
+    onError: (error: ApiError | unknown) => {
+      props.dismissModal()
+      const { content } = error as ApiError
+      if (content.code === 'TOO_MANY_SMS_SENT') {
+        navigate('PhoneValidationTooManySMSSent')
+      } else {
+        const message = extractApiErrorMessage(error)
+        showErrorSnackBar({
+          message,
+          timeout: SNACK_BAR_TIME_OUT,
+        })
+      }
+    },
   })
 
   return (
@@ -46,7 +71,7 @@ export const CodeNotReceivedModal: FunctionComponent<Props> = (props) => {
           <ButtonPrimary
             type="submit"
             onPress={() => {
-              props.dismissModal()
+              sendPhoneValidationCode(props.phoneNumber)
               // TODO : PC-14461 implement code resend and delete this mock behavior
               if (requestsRemaining > 0) {
                 setRequestsRemaining(requestsRemaining - 1)
@@ -55,6 +80,7 @@ export const CodeNotReceivedModal: FunctionComponent<Props> = (props) => {
               }
             }}
             wording={t`Demander un autre code`}
+            isLoading={isLoading}
           />
         </BottomContentContainer>
       </React.Fragment>
