f19afdca4aeacac6d4003cc0d6faafc2a232434b
Hugo Voisin <hugov@bam.tech>
2022-07-04 18:43:55 +0200

    feat: add log conversion event which is similar to click on offer event in the structure

---
+++ src/libs/algolia/analytics/__tests__/logOfferConversion.test.ts
@@ -0,0 +1,43 @@
+import AlgoliaSearchInsights from 'search-insights'
+
+import { logOfferConversion } from 'libs/algolia/analytics/logOfferConversion'
+import { captureMonitoringError } from 'libs/monitoring'
+import { getCookiesConsent } from 'libs/trackingConsent/consent'
+
+jest.mock('search-insights')
+const mockAlgoliaSearchInsights = AlgoliaSearchInsights as jest.Mock
+
+jest.mock('libs/monitoring')
+const mockCaptureMonitoringError = captureMonitoringError as jest.Mock
+
+jest.mock('libs/trackingConsent/consent')
+const mockGetConsentToCookies = getCookiesConsent as jest.Mock
+mockGetConsentToCookies.mockResolvedValue(true)
+
+describe('logOfferConversion', () => {
+  it('should send the corresponding Algolia conversion event when called', async () => {
+    await logOfferConversion('abc123')('object123')
+
+    expect(mockAlgoliaSearchInsights).toHaveBeenCalledWith('convertedObjectIDsAfterSearch', {
+      eventName: 'Offer reserved',
+      index: 'algoliaOffersIndexName',
+      objectIDs: ['object123'],
+      queryID: 'abc123',
+    })
+  })
+  it('should raise a warning instead of sending an event when called without a queryID set', async () => {
+    await logOfferConversion()('object123')
+
+    expect(mockAlgoliaSearchInsights).not.toHaveBeenCalled()
+    expect(mockCaptureMonitoringError).toHaveBeenCalledWith(
+      'Algolia Analytics: useLogOfferConversion called without any QueryID set'
+    )
+  })
+  it("should not send an Algolia conversion event when user didn't accept cookies", async () => {
+    mockGetConsentToCookies.mockResolvedValueOnce(false)
+
+    await logOfferConversion('abc123')('object123')
+
+    expect(mockAlgoliaSearchInsights).not.toHaveBeenCalled()
+  })
+})
