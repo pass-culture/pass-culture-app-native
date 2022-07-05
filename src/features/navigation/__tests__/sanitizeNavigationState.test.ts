02748723a5f45cdacbc9e4c2c87a469241d8265d
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-07-05 17:33:48 +0200

    (PC-16045) fix(Search): do not modify original navigation state

--- src/features/navigation/__tests__/sanitizeNavigationState.test.ts
+++ src/features/navigation/__tests__/sanitizeNavigationState.test.ts
@@ -25,29 +25,39 @@ describe('sanitizeNavigationState', () => {
       },
     ],
   }
-  it('should clean search params from navigation state', () => {
-    expect(sanitizeNavigationState(state)).toEqual({
-      index: 1,
-      routes: [
-        {
-          name: 'TabNavigator',
-          params: { screen: 'Search' },
-          state: {
-            routeNames: ['Home', 'Search'],
-            history: [
-              { type: 'route', key: 'Home-XHWcHfi6Qpstz4JqwXxK_' },
-              { type: 'route', key: 'Search-xufH8CYsq0efty7Gl1gA2' },
-            ],
-            routes: [
-              { name: 'Home', key: 'Home-XHWcHfi6Qpstz4JqwXxK_' },
-              {
-                name: 'Search',
-                key: 'Search-xufH8CYsq0efty7Gl1gA2',
-              },
-            ],
-          },
+
+  const cleanedState = {
+    index: 1,
+    routes: [
+      {
+        name: 'TabNavigator',
+        params: { screen: 'Search' },
+        state: {
+          routeNames: ['Home', 'Search'],
+          history: [
+            { type: 'route', key: 'Home-XHWcHfi6Qpstz4JqwXxK_' },
+            { type: 'route', key: 'Search-xufH8CYsq0efty7Gl1gA2' },
+          ],
+          routes: [
+            { name: 'Home', key: 'Home-XHWcHfi6Qpstz4JqwXxK_' },
+            {
+              name: 'Search',
+              key: 'Search-xufH8CYsq0efty7Gl1gA2',
+            },
+          ],
         },
-      ],
-    })
+      },
+    ],
+  }
+
+  it('should clean search params from navigation state', () => {
+    const newState = sanitizeNavigationState(state)
+    expect(newState).toEqual(cleanedState)
+  })
+
+  it('should not mutate original navigation state', () => {
+    const defaultStateCopy = JSON.parse(JSON.stringify(state))
+    sanitizeNavigationState(state)
+    expect(state).toEqual(defaultStateCopy)
   })
 })
