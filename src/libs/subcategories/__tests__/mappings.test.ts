b181d8fd16e23dcdfe6374f54eabcff087181047
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2022-07-06 10:01:28 +0200

    (PC-15976) feat(home): add tests for categories mapping (#3207)

---
+++ src/libs/subcategories/__tests__/mappings.test.ts
@@ -0,0 +1,50 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import {
+  categoryIdMappingSnap,
+  subcategoriesMappingSnap,
+  useCategoryHomeLabelMappingSnap,
+  useSearchGroupLabelMappingSnap,
+} from 'libs/subcategories/fixtures/mappings'
+import {
+  useCategoryHomeLabelMapping,
+  useCategoryIdMapping,
+  useSearchGroupLabelMapping,
+  useSubcategoriesMapping,
+} from 'libs/subcategories/mappings'
+import { placeholderData } from 'libs/subcategories/placeholderData'
+
+const mockSubcategories = placeholderData.subcategories
+jest.mock('libs/subcategories/useSubcategories', () => ({
+  useSubcategories: () => ({
+    data: { subcategories: mockSubcategories },
+  }),
+}))
+
+describe('useCategoryIdMapping', () => {
+  it('should match category id mapping', () => {
+    const { result } = renderHook(useCategoryIdMapping)
+    expect(result.current).toEqual(categoryIdMappingSnap)
+  })
+})
+
+describe('useSubcategoriesMapping', () => {
+  it('should match subcategories mapping', () => {
+    const { result } = renderHook(useSubcategoriesMapping)
+    expect(result.current).toEqual(subcategoriesMappingSnap)
+  })
+})
+
+describe('useSearchGroupLabelMapping', () => {
+  it('should match search group label mapping', () => {
+    const { result } = renderHook(useSearchGroupLabelMapping)
+    expect(result.current).toEqual(useSearchGroupLabelMappingSnap)
+  })
+})
+
+describe('useCategoryHomeLabelMapping', () => {
+  it('should match category Home label mapping', () => {
+    const { result } = renderHook(useCategoryHomeLabelMapping)
+    expect(result.current).toEqual(useCategoryHomeLabelMappingSnap)
+  })
+})
