3ee44069147e070f80829c01859bf9e7d65f89b6
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2022-07-07 18:25:35 +0200

    (PC-15976) test(hooks): Add tests for hooks (#3208)
    * (PC-15976) test(hooks): Add test for use itinerary web
    
    * (PC-15976) test(hooks): Add test for useIsLandscapePosition in web

---
+++ src/libs/itinerary/useItinerary.web.test.ts
@@ -0,0 +1,30 @@
+import { renderHook } from '@testing-library/react-hooks'
+import waitForExpect from 'wait-for-expect'
+
+import { openGoogleMapsItinerary } from 'libs/itinerary/openGoogleMapsItinerary'
+import { useItinerary } from 'libs/itinerary/useItinerary'
+import { superFlushWithAct } from 'tests/utils'
+import { SnackBarHelperSettings } from 'ui/components/snackBar/types'
+
+jest.mock('libs/itinerary/openGoogleMapsItinerary')
+jest.mock('libs/itinerary/useItinerary', () => jest.requireActual('libs/itinerary/useItinerary'))
+
+const mockShowInfoSnackBar = jest.fn()
+jest.mock('ui/components/snackBar/SnackBarContext', () => ({
+  useSnackBarContext: () => ({
+    showInfoSnackBar: jest.fn((props: SnackBarHelperSettings) => mockShowInfoSnackBar(props)),
+  }),
+}))
+
+const address = '48.85837, 2.294481'
+
+describe('useItinerary', () => {
+  it('should open itinerary in google maps web', async () => {
+    const { result } = renderHook(useItinerary)
+    await superFlushWithAct()
+    result.current.navigateTo(address)
+    await waitForExpect(() => {
+      expect(openGoogleMapsItinerary).toBeCalledWith(address)
+    })
+  })
+})
