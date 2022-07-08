c563b883eb7e868e6de92b5960ff01c7e0953bae
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2022-07-08 14:06:01 +0200

    (PC-16116) fix(Search): limit state size used in localStorage for navigation (#3222)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- src/features/navigation/__tests__/sanitizeNavigationState.test.ts
+++ src/features/navigation/__tests__/sanitizeNavigationState.test.ts
@@ -23,11 +23,33 @@ describe('sanitizeNavigationState', () => {
           ],
         },
       },
+      {
+        name: 'TabNavigator',
+        params: {
+          screen: 'Search',
+          params: { locationFilter: { locationType: 'EVERYWHERE' }, showResults: true },
+        },
+        state: {
+          routeNames: ['Home', 'Search'],
+          history: [
+            { type: 'route', key: 'Home-XHWcHfi6Qpstz4JqwXxK_' },
+            { type: 'route', key: 'Search-xufH8CYsq0efty7Gl1gA2' },
+          ],
+          routes: [
+            { name: 'Home', key: 'Home-XHWcHfi6Qpstz4JqwXxK_' },
+            {
+              name: 'Search',
+              key: 'Search-xufH8CYsq0efty7Gl1gA2',
+              params: { locationFilter: { locationType: 'EVERYWHERE' } },
+            },
+          ],
+        },
+      },
     ],
   }
 
   const cleanedState = {
-    index: 1,
+    index: 0,
     routes: [
       {
         name: 'TabNavigator',
@@ -59,5 +81,11 @@ describe('sanitizeNavigationState', () => {
     const defaultStateCopy = JSON.parse(JSON.stringify(state))
     sanitizeNavigationState(state)
     expect(state).toEqual(defaultStateCopy)
+  })
+
+  it('should save 1 entry in history with index 0', () => {
+    const newState = sanitizeNavigationState(state)
+    expect(newState.index).toEqual(0)
+    expect((newState.routes as []).length).toEqual(1)
   })
 })
