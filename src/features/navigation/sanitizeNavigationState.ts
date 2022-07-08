3460f2fb6671f40061d8b3ab27dd551e2ee9f1fc
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-07-08 12:10:15 +0200

    (PC-16116) fix(Search): limit state size used in localStorage for navigation

--- src/features/navigation/sanitizeNavigationState.ts
+++ src/features/navigation/sanitizeNavigationState.ts
@@ -1,10 +1,9 @@
 import cloneDeep from 'lodash.clonedeep'
 
-export function sanitizeNavigationState(state: Record<string, unknown>) {
-  const newState = cloneDeep(state)
+function clearSearchParams(newState: Record<string, unknown>) {
   // @ts-ignore NavigationState is normally readonly, we voluntary don't use this type
   newState.routes = newState?.routes?.map((route) => {
-    if (route.params?.screen === 'Search' && route.params?.params) {
+    if (route?.params?.screen === 'Search' && route?.params?.params) {
       delete route.params.params
       // @ts-ignore NavigationState is normally readonly, we voluntary don't use this type
       route.state.routes = route.state?.routes?.map((r) => {
@@ -16,5 +15,20 @@ export function sanitizeNavigationState(state: Record<string, unknown>) {
     }
     return route
   })
+  return newState
+}
+
+function limitStateSize(newState: Record<string, unknown>) {
+  if (newState.index !== undefined && newState.routes) {
+    newState.routes = [(newState.routes as [])[newState.index as number]]
+    newState.index = 0
+  }
+  return newState
+}
+
+export function sanitizeNavigationState(state: Record<string, unknown>) {
+  const clonedState = cloneDeep(state)
+  let newState = limitStateSize(clonedState)
+  newState = clearSearchParams(newState)
   return newState
 }
