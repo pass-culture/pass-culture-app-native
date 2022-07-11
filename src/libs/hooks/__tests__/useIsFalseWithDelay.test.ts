e2624df85d4913d7d5357950fcbf37c86a171cfb
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-07-11 16:58:26 +0200

    (PC-16149) feat(tests): Add tests for useIsFalseWithDelay

---
+++ src/libs/hooks/__tests__/useIsFalseWithDelay.test.ts
@@ -0,0 +1,50 @@
+import { act } from 'react-test-renderer'
+
+import { useIsFalseWithDelay } from 'libs/hooks/useIsFalseWithDelay'
+import { renderHook } from 'tests/utils'
+
+let condition = false
+const DELAY = 1000
+
+jest.useFakeTimers()
+describe('useIsFalseWithDelay()', () => {
+  it("should always return true if condition is true and doesn't change", () => {
+    condition = true
+    const { result } = renderUseIsFalseWithDelay(condition)
+    expect(result.current).toBeTruthy()
+
+    act(() => {
+      jest.advanceTimersByTime(DELAY)
+    })
+    expect(result.current).toBeTruthy()
+  })
+
+  it("should always return false if condition is false and doesn't change", () => {
+    const condition = false
+    const { result } = renderUseIsFalseWithDelay(condition)
+    expect(result.current).toBeFalsy()
+
+    act(() => {
+      jest.advanceTimersByTime(DELAY)
+    })
+    expect(result.current).toBeFalsy()
+  })
+
+  it('should always return false after delay if condition is true at first then false', () => {
+    const condition = true
+    const { result, rerender } = renderUseIsFalseWithDelay(condition)
+
+    rerender({ condition: false })
+    expect(result.current).toBeTruthy()
+
+    act(() => {
+      jest.advanceTimersByTime(DELAY)
+    })
+    expect(result.current).toBeFalsy()
+  })
+})
+
+const renderUseIsFalseWithDelay = (condition: boolean) =>
+  renderHook((props?: { condition: boolean }) =>
+    useIsFalseWithDelay(props?.condition ?? condition, DELAY)
+  )
