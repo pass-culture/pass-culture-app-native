376b518ea868c6c4455cba4a4b0181e5a9a45fdf
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-07-11 13:08:20 +0200

    (PC-16136) feat(tests): Add tests for useGoBack

---
+++ src/features/navigation/__tests__/useGoBack.native.test.ts
@@ -0,0 +1,67 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import { homeNavConfig } from 'features/navigation/TabBar/helpers'
+
+import { useGoBack } from '../useGoBack'
+
+let mockCanGoBack = false
+
+jest.unmock('features/navigation/useGoBack')
+
+const mockNavigate = jest.fn()
+const mockGoBack = jest.fn()
+const mockPreviousRoute: { name?: string } = { name: undefined }
+jest.mock('@react-navigation/native', () => ({
+  ...(jest.requireActual('@react-navigation/native') as Record<string, unknown>),
+  useNavigation: () => ({
+    navigate: mockNavigate,
+    goBack: mockGoBack,
+    canGoBack: jest.fn(() => mockCanGoBack),
+  }),
+}))
+
+jest.mock('features/navigation/helpers/usePreviousRoute', () => ({
+  usePreviousRoute: jest.fn(() => mockPreviousRoute),
+}))
+
+describe('useGoBack()', () => {
+  describe('customCanGoBack()', () => {
+    it.each([true, false])(
+      'should always return same value as original function: "%s"',
+      (canGoBackValue) => {
+        mockCanGoBack = canGoBackValue
+        const { result } = renderUseGoBack()
+        expect(result.current.canGoBack()).toBe(canGoBackValue)
+      }
+    )
+  })
+
+  describe('customGoBack()', () => {
+    it('should use navigate if canGoBack = false', () => {
+      mockCanGoBack = false
+      const { result } = renderUseGoBack()
+      result.current.goBack()
+      expect(mockNavigate).toBeCalledWith(...homeNavConfig)
+    })
+
+    it("should use navigate if previous route doesn't exist", () => {
+      mockCanGoBack = true
+      mockPreviousRoute.name = undefined
+      const { result } = renderUseGoBack()
+      result.current.goBack()
+      expect(mockNavigate).toBeCalledWith(...homeNavConfig)
+    })
+
+    it('should call goBack if previous route exists and canGoBack = true', () => {
+      mockCanGoBack = true
+      mockPreviousRoute.name = 'Login'
+      const { result } = renderUseGoBack()
+      result.current.goBack()
+      expect(mockGoBack).toBeCalled()
+    })
+  })
+})
+
+function renderUseGoBack() {
+  return renderHook(() => useGoBack(...homeNavConfig))
+}
