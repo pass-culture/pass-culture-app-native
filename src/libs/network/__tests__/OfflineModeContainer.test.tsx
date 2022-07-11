470de4f6c96c87114aae5075ce21065cef0ee0dc
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-07-11 12:47:17 +0200

    (PC-16119) fix(Offline): show banner when isInternetReachable stay false for more than 3secs

--- src/libs/network/__tests__/OfflineModeContainer.test.tsx
+++ src/libs/network/__tests__/OfflineModeContainer.test.tsx
@@ -12,18 +12,47 @@ const mockUseNetInfo = useNetInfoDefault as jest.Mock
 describe('<OfflineModeContainer />', () => {
   mockUseNetInfo.mockImplementation(() => ({ isConnected: false }))
 
-  it('should render children and show banner when offline', () => {
+  it('should render children and show banner when offline at init when isConnected is false', () => {
     const renderAPI = renderOfflineModeContainer()
     expect(renderAPI.queryByText('Hello World')).toBeTruthy()
     expect(renderAPI.queryByText('aucune connexion internet.')).toBeTruthy()
   })
-  it('should render children and not show banner when offline', () => {
+
+  it('should render children and not show banner when offline at init when isConnected is true', () => {
     mockUseNetInfo.mockImplementationOnce(() => ({ isConnected: true }))
     const renderAPI = renderOfflineModeContainer()
     expect(renderAPI.queryByText('aucune connexion internet.')).toBeFalsy()
     expect(renderAPI.queryByText('Hello World')).toBeTruthy()
   })
+
+  it('should not show "aucune connexion internet." at init, then show when isConnected is false, then hide when isConnected switch back to true', () => {
+    mockUseNetInfo.mockImplementationOnce(() => ({ isConnected: true, isInternetReachable: true }))
+    const renderAPI = render(jsx)
+    expect(renderAPI.queryByText('aucune connexion internet.')).toBeFalsy()
+
+    mockUseNetInfo.mockImplementationOnce(() => ({
+      isConnected: false,
+      isInternetReachable: false,
+    }))
+    renderAPI.rerender(
+      <OfflineModeContainer>
+        <View>
+          <Text>Hello World</Text>
+        </View>
+      </OfflineModeContainer>
+    )
+
+    expect(renderAPI.queryByText('aucune connexion internet.')).toBeTruthy()
+  })
 })
+
+const jsx = (
+  <OfflineModeContainer>
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  </OfflineModeContainer>
+)
 
 function renderOfflineModeContainer() {
   return render(
