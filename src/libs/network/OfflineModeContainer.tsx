03298402d1da931b1b70a52861d2256aa18d739c
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2022-07-12 12:47:04 +0200

    (PC-16119) fix(Offline): show banner when isInternetReachable stay false for more than 3secs (#3229)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- src/libs/network/OfflineModeContainer.tsx
+++ src/libs/network/OfflineModeContainer.tsx
@@ -1,21 +1,49 @@
 import { t } from '@lingui/macro'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState, useEffect, useRef } from 'react'
 import styled from 'styled-components/native'
 
 import { useNetInfo } from 'libs/network/useNetInfo'
 import { getSpacing, Typo, Spacer } from 'ui/theme'
 
+const THIRTY_SECONDS = 30000
+
 export function OfflineModeContainer({ children }: { children: ReactNode }) {
   const netInfo = useNetInfo()
+  const [show, setShow] = useState(!netInfo.isConnected)
+  const isInternetReachable = useRef(netInfo.isInternetReachable)
+
+  useEffect(() => {
+    setShow(!netInfo.isConnected)
+  }, [netInfo.isConnected])
+
+  useEffect(() => {
+    isInternetReachable.current = netInfo.isInternetReachable
+    let timer: number | undefined
+    if (!isInternetReachable.current) {
+      timer = globalThis.setTimeout(() => {
+        if (!isInternetReachable.current) {
+          setShow(true)
+        }
+      }, THIRTY_SECONDS)
+    } else {
+      setShow(false)
+    }
+    return () => {
+      if (timer) {
+        globalThis.clearInterval(timer)
+      }
+    }
+  }, [netInfo.isInternetReachable])
+
   return (
     <FlexView>
       <FlexView>{children}</FlexView>
-      {!netInfo.isConnected && (
+      {show ? (
         <OfflineModeBanner>
           <OfflineModeBannerText>{t`aucune connexion internet.`}</OfflineModeBannerText>
           <Spacer.BottomScreen />
         </OfflineModeBanner>
-      )}
+      ) : null}
     </FlexView>
   )
 }
