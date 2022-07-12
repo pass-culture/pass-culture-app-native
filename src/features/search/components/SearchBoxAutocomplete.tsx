8eb0230b848e41c0e62f8730006d2bb2aaf73b93
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-07-12 17:36:28 +0200

    feat(search): add autocomplete search box

---
+++ src/features/search/components/SearchBoxAutocomplete.tsx
@@ -0,0 +1,216 @@
+import { t } from '@lingui/macro'
+import { useNavigation } from '@react-navigation/native'
+import debounce from 'lodash.debounce'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { useSearchBox, UseSearchBoxProps } from 'react-instantsearch-hooks'
+import {
+  Keyboard,
+  NativeSyntheticEvent,
+  Platform,
+  TextInputSubmitEditingEventData,
+  View,
+} from 'react-native'
+import { TextInput as RNTextInput } from 'react-native'
+import styled from 'styled-components/native'
+import { v4 as uuidv4 } from 'uuid'
+
+import { UseNavigationType } from 'features/navigation/RootNavigator'
+import { useLocationChoice } from 'features/search/components/locationChoice.utils'
+import { LocationType } from 'features/search/enums'
+import { useSearch, useStagedSearch } from 'features/search/pages/SearchWrapper'
+import { usePushWithStagedSearch } from 'features/search/pages/usePushWithStagedSearch'
+import { useShowResults } from 'features/search/pages/useShowResults'
+import { analytics } from 'libs/firebase/analytics'
+import { HiddenAccessibleText } from 'ui/components/HiddenAccessibleText'
+import { SearchInput } from 'ui/components/inputs/SearchInput'
+import { TouchableOpacity } from 'ui/components/TouchableOpacity'
+import { ArrowPrevious as DefaultArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
+import { MagnifyingGlass as DefaultMagnifyingGlass } from 'ui/svg/icons/MagnifyingGlass'
+import { getSpacing } from 'ui/theme'
+import { getHeadingAttrs } from 'ui/theme/typographyAttrs/getHeadingAttrs'
+
+type Props = UseSearchBoxProps & {
+  searchInputID: string
+  showLocationButton?: boolean
+  isFocus: boolean
+  shouldAutocomplete: boolean
+  accessibleHiddenTitle?: string
+  setShouldAutocomplete: (shouldAutocomplete: boolean) => void
+  setAutocompleteValue: (query: string) => void
+}
+
+const SEARCH_DEBOUNCE_MS = 500
+
+export const SearchBoxAutocomplete: React.FC<Props> = ({
+  searchInputID,
+  showLocationButton,
+  isFocus,
+  accessibleHiddenTitle,
+  setShouldAutocomplete,
+  shouldAutocomplete,
+  setAutocompleteValue,
+  ...props
+}) => {
+  const { navigate } = useNavigation<UseNavigationType>()
+  const { searchState } = useSearch()
+  const { searchState: stagedSearchState, dispatch: stagedDispatch } = useStagedSearch()
+  const accessibilityDescribedBy = uuidv4()
+  const { locationFilter } = stagedSearchState
+  const { locationType } = locationFilter
+  // PLACE and VENUE belong to the same section
+  const section = locationType === LocationType.VENUE ? LocationType.PLACE : locationType
+  const { label: locationLabel } = useLocationChoice(section)
+  const inputRef = useRef<RNTextInput | null>(null)
+  const { query, refine } = useSearchBox(props)
+  const [value, setValue] = useState<string>(query)
+  const [debouncedValue, setDebouncedValue] = useState<string>(value)
+  const debouncedSetValue = useRef(debounce(setDebouncedValue, SEARCH_DEBOUNCE_MS)).current
+  const showResults = useShowResults()
+  const pushWithStagedSearch = usePushWithStagedSearch()
+
+  // Track when the value coming from the React state changes to synchronize
+  // it with InstantSearch.
+  useEffect(() => {
+    if (query !== debouncedValue) {
+      refine(debouncedValue)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue, refine])
+
+  // Track when the InstantSearch query changes to synchronize it with
+  // the React state.
+  useEffect(() => {
+    // We bypass the state update if the input is focused to avoid concurrent
+    // updates when typing.
+    if (!inputRef.current?.isFocused() && query !== debouncedValue && searchState.query === '') {
+      setValue(query)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query])
+
+  useEffect(() => {
+    // If the user select a value in autocomplete list it must be display in search input
+    onChangeText(searchState.query)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchState.query])
+
+  const resetQuery = useCallback(() => {
+    setAutocompleteValue('')
+    pushWithStagedSearch({ query: '' })
+    setValue('')
+    setShouldAutocomplete(false)
+    // To force remove focus on search input
+    Keyboard.dismiss()
+  }, [setAutocompleteValue, setShouldAutocomplete, pushWithStagedSearch])
+
+  const onPressArrowBack = () => {
+    // Only close autocomplete list if open
+    if (shouldAutocomplete) {
+      setShouldAutocomplete(false)
+      // To force remove focus on search input
+      Keyboard.dismiss()
+      return
+    }
+
+    stagedDispatch({ type: 'SET_QUERY', payload: '' })
+    pushWithStagedSearch({
+      query: '',
+      showResults: false,
+    })
+    setShouldAutocomplete(false)
+    setValue('')
+    setAutocompleteValue('')
+  }
+
+  const onPressLocationButton = useCallback(() => {
+    navigate('LocationFilter')
+  }, [navigate])
+
+  const onSubmitQuery = (event: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
+    const queryText = event.nativeEvent.text
+    if (queryText.length < 1 && Platform.OS !== 'android') return
+    setShouldAutocomplete(false)
+    // When we hit enter, we may have selected a category or a venue on the search landing page
+    // these are the two potentially 'staged' filters that we want to commit to the global search state.
+    // We also want to commit the price filter, as beneficiary users may have access to different offer
+    // price range depending on their available credit.
+    const { offerCategories, priceRange } = stagedSearchState
+    pushWithStagedSearch({
+      showResults: true,
+      query: queryText,
+      locationFilter,
+      offerCategories,
+      priceRange,
+    })
+    analytics.logSearchQuery(queryText)
+  }
+
+  const onChangeText = (value: string) => {
+    setValue(value)
+    debouncedSetValue(value)
+    setAutocompleteValue(value)
+  }
+
+  return (
+    <View testID="searchBoxWithAutocomplete">
+      {!!accessibleHiddenTitle && (
+        <HiddenAccessibleText {...getHeadingAttrs(1)}>{accessibleHiddenTitle}</HiddenAccessibleText>
+      )}
+      <SearchInputContainer
+        marginHorizontal={shouldAutocomplete || showResults ? getSpacing(6) : 0}>
+        {shouldAutocomplete || showResults ? (
+          <StyledTouchableOpacity testID="previousButton" onPress={onPressArrowBack}>
+            <ArrowPrevious />
+          </StyledTouchableOpacity>
+        ) : null}
+        <StyledSearchInput
+          searchInputID={searchInputID}
+          value={value}
+          onChangeText={onChangeText}
+          placeholder={t`Offre, artiste...`}
+          autoFocus={isFocus}
+          inputHeight="regular"
+          LeftIcon={() => <MagnifyingGlassIcon />}
+          onSubmitEditing={onSubmitQuery}
+          onPressRightIcon={resetQuery}
+          setShouldAutocomplete={setShouldAutocomplete}
+          testID="searchInput"
+          onPressLocationButton={showLocationButton ? onPressLocationButton : undefined}
+          locationLabel={locationLabel}
+          ref={inputRef}
+        />
+      </SearchInputContainer>
+      <HiddenAccessibleText nativeID={accessibilityDescribedBy}>
+        {t`Indique le nom d'une offre ou d'un lieu puis lance la recherche à l'aide de la touche
+          "Entrée"`}
+      </HiddenAccessibleText>
+    </View>
+  )
+}
+
+const StyledSearchInput = styled(SearchInput)({
+  outlineOffset: 0,
+})
+
+const ArrowPrevious = styled(DefaultArrowPrevious).attrs(({ theme }) => ({
+  size: theme.icons.sizes.small,
+}))``
+
+const MagnifyingGlass = styled(DefaultMagnifyingGlass).attrs(({ theme }) => ({
+  size: theme.icons.sizes.smaller,
+}))``
+
+const MagnifyingGlassIcon = styled(MagnifyingGlass).attrs(({ theme }) => ({
+  size: theme.icons.sizes.smaller,
+}))``
+
+const SearchInputContainer = styled.View<{ marginHorizontal: number }>(({ marginHorizontal }) => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginHorizontal,
+}))
+
+const StyledTouchableOpacity = styled(TouchableOpacity)({
+  marginRight: getSpacing(4),
+})
---
+++ src/features/search/components/__tests__/SearchBoxAutocomplete.native.test.tsx
@@ -0,0 +1,239 @@
+import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+import { navigate, push } from '__mocks__/@react-navigation/native'
+import { SearchGroupNameEnum } from 'api/gen'
+import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
+import { SearchBoxAutocomplete } from 'features/search/components/SearchBoxAutocomplete'
+import { LocationType } from 'features/search/enums'
+import { initialSearchState } from 'features/search/pages/reducer'
+import { SearchState } from 'features/search/types'
+import { analytics } from 'libs/firebase/analytics'
+import { SuggestedVenue } from 'libs/venue'
+import { mockedSuggestedVenues } from 'libs/venue/fixtures/mockedSuggestedVenues'
+import { fireEvent, render } from 'tests/utils'
+
+const venue: SuggestedVenue = mockedSuggestedVenues[0]
+
+const mockSearchState = initialSearchState
+const mockStagedSearchState: SearchState = {
+  ...initialSearchState,
+  offerCategories: [SearchGroupNameEnum.CINEMA],
+  locationFilter: { locationType: LocationType.VENUE, venue },
+  priceRange: [0, 20],
+}
+
+const mockDispatch = jest.fn()
+const mockStagedDispatch = jest.fn()
+jest.mock('features/search/pages/SearchWrapper', () => ({
+  useSearch: () => ({ searchState: mockSearchState, dispatch: mockDispatch }),
+  useStagedSearch: () => ({ searchState: mockStagedSearchState, dispatch: mockStagedDispatch }),
+}))
+jest.mock('libs/firebase/analytics')
+
+jest.mock('features/auth/settings')
+
+jest.mock('features/profile/api', () => ({
+  useUserProfileInfo: jest.fn(() => ({ data: { isBeneficiary: true } })),
+}))
+
+const mockData = { pages: [{ nbHits: 0, hits: [], page: 0 }] }
+const mockHasNextPage = true
+const mockFetchNextPage = jest.fn()
+jest.mock('features/search/pages/useSearchResults', () => ({
+  useSearchResults: () => ({
+    data: mockData,
+    hits: [],
+    nbHits: 0,
+    isFetching: false,
+    isLoading: false,
+    hasNextPage: mockHasNextPage,
+    fetchNextPage: mockFetchNextPage,
+    isFetchingNextPage: false,
+  }),
+}))
+jest.mock('react-instantsearch-hooks', () => ({
+  useSearchBox: () => ({
+    query: '',
+    refine: jest.fn,
+  }),
+}))
+
+describe('SearchBoxAutocomplete component', () => {
+  const searchInputID = uuidv4()
+  const setShouldAutocomplete = jest.fn
+  const setAutocompleteValue = jest.fn
+
+  it('should call logSearchQuery on submit', () => {
+    const { getByPlaceholderText } = render(
+      <SearchBoxAutocomplete
+        searchInputID={searchInputID}
+        isFocus={true}
+        shouldAutocomplete={true}
+        setShouldAutocomplete={setShouldAutocomplete}
+        setAutocompleteValue={setAutocompleteValue}
+      />
+    )
+    const searchInput = getByPlaceholderText('Offre, artiste...')
+
+    fireEvent(searchInput, 'onSubmitEditing', { nativeEvent: { text: 'jazzaza' } })
+
+    expect(analytics.logSearchQuery).toBeCalledWith('jazzaza')
+    expect(push).toBeCalledWith(
+      ...getTabNavConfig('Search', {
+        ...initialSearchState,
+        query: 'jazzaza',
+        showResults: true,
+        offerCategories: mockStagedSearchState.offerCategories,
+        locationFilter: mockStagedSearchState.locationFilter,
+        priceRange: mockStagedSearchState.priceRange,
+      })
+    )
+  })
+
+  it('should not show previous if no search executed and no autocomplete list not visible', () => {
+    const { queryByTestId } = render(
+      <SearchBoxAutocomplete
+        searchInputID={searchInputID}
+        isFocus={false}
+        shouldAutocomplete={false}
+        setShouldAutocomplete={setShouldAutocomplete}
+        setAutocompleteValue={setAutocompleteValue}
+      />
+    )
+    const previousButton = queryByTestId('previousButton')
+
+    expect(previousButton).toBeFalsy()
+  })
+
+  it('should show previous button if search executed', () => {
+    mockSearchState.showResults = true
+    const { getByTestId } = render(
+      <SearchBoxAutocomplete
+        searchInputID={searchInputID}
+        isFocus={false}
+        shouldAutocomplete={false}
+        setShouldAutocomplete={setShouldAutocomplete}
+        setAutocompleteValue={setAutocompleteValue}
+      />
+    )
+    const previousButton = getByTestId('previousButton')
+
+    expect(previousButton).toBeTruthy()
+  })
+
+  it('should show previous button if autocomplete list visible', () => {
+    const { getByTestId } = render(
+      <SearchBoxAutocomplete
+        searchInputID={searchInputID}
+        isFocus={false}
+        shouldAutocomplete={true}
+        setShouldAutocomplete={setShouldAutocomplete}
+        setAutocompleteValue={setAutocompleteValue}
+      />
+    )
+    const previousButton = getByTestId('previousButton')
+
+    expect(previousButton).toBeTruthy()
+  })
+
+  it('should show the text type by the user', async () => {
+    const { getByPlaceholderText } = render(
+      <SearchBoxAutocomplete
+        searchInputID={searchInputID}
+        isFocus={true}
+        shouldAutocomplete={true}
+        setShouldAutocomplete={setShouldAutocomplete}
+        setAutocompleteValue={setAutocompleteValue}
+      />
+    )
+
+    const searchInput = getByPlaceholderText('Offre, artiste...')
+    await fireEvent(searchInput, 'onChangeText', 'Some text')
+
+    expect(searchInput.props.value).toBe('Some text')
+  })
+
+  it('should reset input when user click on reset icon', async () => {
+    const { getByTestId, getByPlaceholderText } = render(
+      <SearchBoxAutocomplete
+        searchInputID={searchInputID}
+        isFocus={false}
+        shouldAutocomplete={false}
+        setShouldAutocomplete={setShouldAutocomplete}
+        setAutocompleteValue={setAutocompleteValue}
+      />
+    )
+
+    const searchInput = getByPlaceholderText('Offre, artiste...')
+    await fireEvent(searchInput, 'onChangeText', 'Some text')
+
+    const resetIcon = getByTestId('resetSearchInput')
+    await fireEvent.press(resetIcon)
+
+    expect(searchInput.props.value).toBe('')
+  })
+
+  it('should reset input when user click on previous button', async () => {
+    const { getByTestId, getByPlaceholderText } = render(
+      <SearchBoxAutocomplete
+        searchInputID={searchInputID}
+        isFocus={false}
+        shouldAutocomplete={false}
+        setShouldAutocomplete={setShouldAutocomplete}
+        setAutocompleteValue={setAutocompleteValue}
+      />
+    )
+    const previousButton = getByTestId('previousButton')
+
+    const searchInput = getByPlaceholderText('Offre, artiste...')
+    await fireEvent(searchInput, 'onChangeText', 'Some text')
+
+    await fireEvent.press(previousButton)
+
+    expect(searchInput.props.value).toBe('')
+  })
+
+  it('should not execute a search if input is empty', () => {
+    const { getByPlaceholderText } = render(
+      <SearchBoxAutocomplete
+        searchInputID={searchInputID}
+        isFocus={true}
+        shouldAutocomplete={true}
+        setShouldAutocomplete={setShouldAutocomplete}
+        setAutocompleteValue={setAutocompleteValue}
+      />
+    )
+    const searchInput = getByPlaceholderText('Offre, artiste...')
+
+    fireEvent(searchInput, 'onSubmitEditing', { nativeEvent: { text: '' } })
+
+    expect(navigate).not.toBeCalled()
+  })
+
+  it('should execute a search if input is not empty', () => {
+    const { getByPlaceholderText } = render(
+      <SearchBoxAutocomplete
+        searchInputID={searchInputID}
+        isFocus={true}
+        shouldAutocomplete={true}
+        setShouldAutocomplete={setShouldAutocomplete}
+        setAutocompleteValue={setAutocompleteValue}
+      />
+    )
+    const searchInput = getByPlaceholderText('Offre, artiste...')
+
+    fireEvent(searchInput, 'onSubmitEditing', { nativeEvent: { text: 'jazzaza' } })
+
+    expect(push).toBeCalledWith(
+      ...getTabNavConfig('Search', {
+        ...initialSearchState,
+        query: 'jazzaza',
+        showResults: true,
+        offerCategories: mockStagedSearchState.offerCategories,
+        locationFilter: mockStagedSearchState.locationFilter,
+        priceRange: mockStagedSearchState.priceRange,
+      })
+    )
+  })
+})
