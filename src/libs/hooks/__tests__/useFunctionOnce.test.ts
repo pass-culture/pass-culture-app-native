5902411c0f811dcdcbffec31e0a67e2bdfd58d9f
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2022-07-12 11:27:33 +0200

    (PC-16149) feat(tests): Add tests for various hooks (#3231)
    * (PC-16149) feat(tests): Add tests for useFunctionOnce
    
    * (PC-16149) feat(tests): Add tests for useIsFalseWithDelay
    
    * (PC-16149) feat(tests): Add test for useIsMounted

---
+++ src/libs/hooks/__tests__/useFunctionOnce.test.ts
@@ -0,0 +1,13 @@
+import useFunctionOnce from 'libs/hooks/useFunctionOnce'
+import { renderHook } from 'tests/utils'
+
+const callBackFn = jest.fn()
+describe('useFunctionOnce()', () => {
+  it('should only call callback function once', () => {
+    const { result } = renderHook(() => useFunctionOnce(callBackFn))
+    result.current()
+    expect(callBackFn).toBeCalledTimes(1)
+    result.current()
+    expect(callBackFn).toBeCalledTimes(1)
+  })
+})
---
+++ src/libs/hooks/__tests__/useIsMounted.test.ts
@@ -0,0 +1,11 @@
+import useIsMounted from 'libs/hooks/useIsMounted'
+import { renderHook } from 'tests/utils'
+
+describe('useIsMounted()', () => {
+  it('should return true if mounted and false if not', () => {
+    const { result, unmount } = renderHook(useIsMounted)
+    expect(result.current.current).toBeTruthy()
+    unmount()
+    expect(result.current.current).toBeFalsy()
+  })
+})
