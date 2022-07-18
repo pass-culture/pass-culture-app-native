fabddfc85f8411016d0e7736876645dfa1e2dd6a
bruno ebstein <bruno.ebstein@passculture.app>
2022-07-18 17:28:58 +0200

    (PC-15523) refactor(search): split generic search input and specific location button into a new search main input (#3250)
    * refactor: add specific search input component
    
    * refactor: move location button from generic search input to specific

---
+++ src/features/search/components/SearchMainInput/SearchMainInput.tsx
@@ -0,0 +1,83 @@
+import React, { FunctionComponent } from 'react'
+import { NativeSyntheticEvent, TextInputSubmitEditingEventData } from 'react-native'
+import styled from 'styled-components/native'
+
+import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { styledButton } from 'ui/components/buttons/styledButton'
+import { SearchInput } from 'ui/components/inputs/SearchInput'
+import { LocationPointer } from 'ui/svg/icons/LocationPointer'
+import { MagnifyingGlass } from 'ui/svg/icons/MagnifyingGlass'
+import { getSpacing } from 'ui/theme'
+
+type QueryProps = {
+  query?: string
+  setQuery: (text: string) => void
+  onSubmitQuery: (event: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => void
+  resetQuery: () => void
+}
+
+type FocusProps = {
+  isFocus?: boolean
+  onFocusState?: (focus: boolean) => void
+}
+
+type LocationProps = {
+  showLocationButton?: boolean
+  locationLabel?: string
+  onPressLocationButton: () => void
+}
+
+type Props = QueryProps &
+  FocusProps &
+  LocationProps & {
+    searchInputID?: string
+    children?: never
+  }
+
+export const SearchMainInput: FunctionComponent<Props> = ({
+  query,
+  setQuery,
+  onSubmitQuery,
+  resetQuery,
+  isFocus = false,
+  onFocusState,
+  showLocationButton = false,
+  locationLabel,
+  onPressLocationButton,
+  ...props
+}) => (
+  <SearchInput
+    placeholder="Offre, artiste..."
+    value={query}
+    onChangeText={setQuery}
+    onSubmitEditing={onSubmitQuery}
+    onPressRightIcon={resetQuery}
+    autoFocus={isFocus}
+    onFocusState={onFocusState}
+    nativeAutoFocus
+    LeftIcon={MagnifyingGlassIcon}
+    inputHeight="regular"
+    testID="searchInput"
+    {...props}>
+    {!!showLocationButton && (
+      <LocationButton
+        testID="locationButton"
+        wording={locationLabel || ''}
+        onPress={onPressLocationButton}
+        icon={LocationPointer}
+        buttonHeight="extraSmall"
+        ellipsizeMode="tail"
+      />
+    )}
+  </SearchInput>
+)
+
+const MagnifyingGlassIcon = styled(MagnifyingGlass).attrs(({ theme }) => ({
+  size: theme.icons.sizes.smaller,
+}))``
+
+const LocationButton = styledButton(ButtonPrimary)({
+  maxWidth: 142, // max width corresponds to the size of "Autour de moi" state.
+  marginLeft: getSpacing(2),
+  marginRight: -getSpacing(2),
+})
--- src/ui/components/inputs/SearchInput.stories.tsx
+++ src/ui/components/inputs/SearchInput.stories.tsx
@@ -15,48 +15,34 @@ const Template: ComponentStory<typeof SearchInput> = (args) => <SearchInput {...
 export const Default = Template.bind({})
 Default.args = {
   placeholder: 'Placeholder...',
-  onPressLocationButton: undefined,
 }
 
 export const Disabled = Template.bind({})
 Disabled.args = {
   placeholder: 'Placeholder...',
   disabled: true,
-  onPressLocationButton: undefined,
 }
 
 export const WithTallHeight = Template.bind({})
 WithTallHeight.args = {
   placeholder: 'Placeholder...',
   inputHeight: 'tall',
-  onPressLocationButton: undefined,
 }
 
 export const WithLeftIcon = Template.bind({})
 WithLeftIcon.args = {
   placeholder: 'Placeholder...',
   LeftIcon: () => <MagnifyingGlass />,
-  onPressLocationButton: undefined,
 }
 
 export const WithLabel = Template.bind({})
 WithLabel.args = {
   placeholder: 'Placeholder...',
   label: 'Label',
-  onPressLocationButton: undefined,
 }
 
 export const WithValue = Template.bind({})
 WithValue.args = {
   placeholder: 'Placeholder...',
   value: 'Value',
-  onPressLocationButton: undefined,
-}
-
-export const WithLocationButton = Template.bind({})
-WithLocationButton.args = {
-  placeholder: 'Placeholder...',
-  value: '',
-  locationLabel: 'Autour de moi',
-  inputHeight: 'regular',
 }
