1699718e24583bb55c28a7254ae5d4e32f42d2f3
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-07-19 14:16:11 +0200

    (PC-16256) build(metro): set blacklistRE in rn-cli.config.js

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,4 +1,12 @@
+const blacklist = require('react-native/packager/blacklist')
+
 module.exports = {
+  getBlacklistRE(platform) {
+    return blacklist(platform, [
+      /build\/.*/,
+      /server\/.*/,
+    ]);
+  },
   getTransformModulePath() {
     return require.resolve('react-native-typescript-transformer');
   },
