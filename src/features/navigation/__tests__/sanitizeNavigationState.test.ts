bf4e6bfed7625f499026b02875a7e940c82c5567
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-07-19 18:23:05 +0200

    (PC-16268) fix(Search): do not clearSearchParams

--- src/features/navigation/__tests__/sanitizeNavigationState.test.ts
+++ src/features/navigation/__tests__/sanitizeNavigationState.test.ts
@@ -48,35 +48,6 @@ describe('sanitizeNavigationState', () => {
     ],
   }
 
-  const cleanedState = {
-    index: 0,
-    routes: [
-      {
-        name: 'TabNavigator',
-        params: { screen: 'Search' },
-        state: {
-          routeNames: ['Home', 'Search'],
-          history: [
-            { type: 'route', key: 'Home-XHWcHfi6Qpstz4JqwXxK_' },
-            { type: 'route', key: 'Search-xufH8CYsq0efty7Gl1gA2' },
-          ],
-          routes: [
-            { name: 'Home', key: 'Home-XHWcHfi6Qpstz4JqwXxK_' },
-            {
-              name: 'Search',
-              key: 'Search-xufH8CYsq0efty7Gl1gA2',
-            },
-          ],
-        },
-      },
-    ],
-  }
-
-  it('should clean search params from navigation state', () => {
-    const newState = sanitizeNavigationState(state)
-    expect(newState).toEqual(cleanedState)
-  })
-
   it('should not mutate original navigation state', () => {
     const defaultStateCopy = JSON.parse(JSON.stringify(state))
     sanitizeNavigationState(state)
