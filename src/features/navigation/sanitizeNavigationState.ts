316d76764527171d5914fb9405de90859249704a
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2022-07-20 08:38:59 +0100

    (PC-16268) fix(Search): do not clearSearchParams (#3274)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- src/features/navigation/sanitizeNavigationState.ts
+++ src/features/navigation/sanitizeNavigationState.ts
@@ -1,23 +1,5 @@
 import cloneDeep from 'lodash.clonedeep'
 
-function clearSearchParams(newState: Record<string, unknown>) {
-  // @ts-ignore NavigationState is normally readonly, we voluntary don't use this type
-  newState.routes = newState?.routes?.map((route) => {
-    if (route?.params?.screen === 'Search' && route?.params?.params) {
-      delete route.params.params
-      // @ts-ignore NavigationState is normally readonly, we voluntary don't use this type
-      route.state.routes = route.state?.routes?.map((r) => {
-        if (r.name === 'Search') {
-          delete r.params
-        }
-        return r
-      })
-    }
-    return route
-  })
-  return newState
-}
-
 function limitStateSize(newState: Record<string, unknown>) {
   if (newState.index !== undefined && newState.routes) {
     newState.routes = [(newState.routes as [])[newState.index as number]]
@@ -27,8 +9,5 @@ function limitStateSize(newState: Record<string, unknown>) {
 }
 
 export function sanitizeNavigationState(state: Record<string, unknown>) {
-  const clonedState = cloneDeep(state)
-  let newState = limitStateSize(clonedState)
-  newState = clearSearchParams(newState)
-  return newState
+  return limitStateSize(cloneDeep(state))
 }
