cc01ef4b903b9b9b176f128ee5b56dfe44032474
Lucas Beneston <lucas.beneston@passculture.app>
2022-07-21 16:44:50 +0200

    (PC-16230) feat(accessibility): add test useSpacerBarAction for FilterSwitch and ControlledFilterSwitch

--- src/features/_marketingAndCommunication/atoms/__tests__/ControlledFilterSwitch.web.test.tsx
+++ src/features/_marketingAndCommunication/atoms/__tests__/ControlledFilterSwitch.web.test.tsx
@@ -3,13 +3,17 @@ import React from 'react'
 import { ControlledFilterSwitch } from 'features/_marketingAndCommunication/atoms/ControlledFilterSwitch'
 import { render, fireEvent } from 'tests/utils/web'
 
+const onChange = jest.fn()
+const name = 'testUnit'
+
 describe('<ControlledFilterSwitch />', () => {
-  it('should call onChange with new value on toggle', () => {
-    const onChange = jest.fn()
-    const renderAPI = render(<ControlledFilterSwitch onChange={onChange} name="testUnit" />)
-    fireEvent.click(renderAPI.getByTestId('Interrupteur'))
-    expect(onChange).toBeCalledWith(true)
-    fireEvent.click(renderAPI.getByTestId('Interrupteur'))
-    expect(onChange).toBeCalledWith(false)
+  it('should call onChange when pressing Spacebar on focused switch', () => {
+    const { getByTestId } = render(<ControlledFilterSwitch onChange={onChange} name={name} />)
+
+    const Switch = getByTestId('Interrupteur')
+    fireEvent.focus(Switch)
+    fireEvent.keyDown(Switch, { key: 'Spacebar' })
+
+    expect(onChange).toHaveBeenNthCalledWith(1, true)
   })
 })
