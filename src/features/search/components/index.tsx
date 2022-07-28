2f55085b710c66abbe65d0419ae3ee79481d753a
Hugo Voisin <hugov@bam.tech>
2022-07-28 16:11:43 +0200

    refactor(search): remove dead code
    Co-authored-by: bruno ebstein <bruno.ebstein@passculture.app>

--- src/features/search/components/index.tsx
+++ src/features/search/components/index.tsx
@@ -1,5 +1,4 @@
 export { CalendarPicker } from './CalendarPicker'
 export * from './SearchHeader'
-export * from './SearchLandingPage'
 export * from './SearchResults'
 export * from './SectionWithSwitch'
