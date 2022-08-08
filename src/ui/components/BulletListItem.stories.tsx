c844b78d7d1dff014e20edbea63cf88fa3ffabd5
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-08-08 09:35:55 +0200

    (PC-16475) fix(Profile): Add BulletListItem to storybook

---
+++ src/ui/components/BulletListItem.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import React from 'react'
+
+import { VerticalUl } from 'ui/web/list/Ul'
+
+import { BulletListItem } from './BulletListItem'
+
+export default {
+  title: 'ui/BulletListItem',
+  component: BulletListItem,
+} as ComponentMeta<typeof BulletListItem>
+
+const Template: ComponentStory<typeof BulletListItem> = (props) => (
+  <VerticalUl>
+    <BulletListItem {...props} />
+    <BulletListItem {...props} />
+    <BulletListItem {...props} />
+  </VerticalUl>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  text: 'List item',
+}
+
+export const ListItemWithSpacing = Template.bind({})
+ListItemWithSpacing.args = {
+  text: 'List item with spacing',
+  spacing: 3,
+}
