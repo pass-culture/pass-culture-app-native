496d528351cb7aaabeb8a40ed7b2e13175e389a0
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2022-08-09 18:07:58 +0200

    (PC-16586) fix(batch): remove unused import batch sdk service worker (#3386)

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -76,6 +76,3 @@ self.addEventListener('message', (event) => {
     self.skipWaiting()
   }
 })
-
-// This will add batch sdk service worker
-self.importScripts(process.env.PUBLIC_URL + '/batchsdk-shared-worker.js')
