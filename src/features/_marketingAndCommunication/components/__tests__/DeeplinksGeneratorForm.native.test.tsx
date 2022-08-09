45d85fd35de9dd52c0cc33cb2806881a955c7eff
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-08-09 11:25:01 +0200

    feat(deeplinksGenerator): force showResults
    Co-authored-by: bruno ebstein <bruno.ebstein@passculture.app>

--- src/features/_marketingAndCommunication/components/__tests__/DeeplinksGeneratorForm.native.test.tsx
+++ src/features/_marketingAndCommunication/components/__tests__/DeeplinksGeneratorForm.native.test.tsx
@@ -1,7 +1,10 @@
+import mockdate from 'mockdate'
 import React from 'react'
 
 import { DeeplinksGeneratorForm } from 'features/_marketingAndCommunication/components/DeeplinksGeneratorForm'
 import { render, fireEvent } from 'tests/utils'
+
+mockdate.set(new Date('2022-08-09T00:00:00Z'))
 
 describe('<DeeplinksGeneratorForm />', () => {
   it('should render deeplink generator form', () => {
@@ -28,6 +31,24 @@ describe('<DeeplinksGeneratorForm />', () => {
       firebaseLink:
         'https://passcultureapptesting.page.link/?link=https%3A%2F%2Fwebapp-v2.example.com%2Fprofil&apn=app.android&isi=1557887412&ibi=app.ios&efr=1',
       universalLink: 'https://webapp-v2.example.com/profil',
+    })
+  })
+
+  it('should add showResults param when the user generate a search link', () => {
+    const onCreate = jest.fn()
+    const renderAPI = render(<DeeplinksGeneratorForm onCreate={onCreate} />)
+    const generateButton = renderAPI.getByTestId('Générer le lien')
+    const search = renderAPI.getByText('Search')
+
+    fireEvent.press(search)
+
+    fireEvent.press(generateButton)
+
+    expect(onCreate).toHaveBeenNthCalledWith(1, {
+      firebaseLink:
+        'https://passcultureapptesting.page.link/?link=https%3A%2F%2Fwebapp-v2.example.com%2Frecherche%3Fview%3D%2522Results%2522%26showResults%3Dtrue%26beginningDatetime%3D%25222022-08-09T00%253A00%253A00.000Z%2522%26endingDatetime%3D%25222022-08-09T00%253A00%253A00.000Z%2522&apn=app.android&isi=1557887412&ibi=app.ios&efr=1',
+      universalLink:
+        'https://webapp-v2.example.com/recherche?view=%22Results%22&showResults=true&beginningDatetime=%222022-08-09T00%3A00%3A00.000Z%22&endingDatetime=%222022-08-09T00%3A00%3A00.000Z%22',
     })
   })
 })
