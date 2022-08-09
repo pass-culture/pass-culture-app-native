809df1da916a53879de930b12bc5590801a11acc
Lucas Beneston <lucas.beneston@passculture.app>
2022-08-09 10:26:11 +0200

    (PC-16586) refator(batch): remove unused code

--- src/libs/react-native-batch/index.web.ts
+++ src/libs/react-native-batch/index.web.ts
@@ -1,92 +1,21 @@
-import { getBatchSDK } from 'libs/batch/batch-sdk'
-import { env } from 'libs/environment'
-import { eventMonitoring } from 'libs/monitoring'
-
-/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-empty-function */
 export const Batch = {
-  start() {
-    /* Initiate Batch SDK opt-in UI configuration (native prompt) */
-    let batchSDKUIConfig
-
-    /* Use a specific configuration for the Firefox and Safari web browser (custom prompt) */
-    if (
-      navigator.userAgent.indexOf('Firefox') !== -1 ||
-      navigator.userAgent.indexOf('Safari') !== -1
-    ) {
-      batchSDKUIConfig = {
-        alert: {
-          attach: 'top center',
-          autoShow: false,
-          btnWidth: '200',
-          positiveSubBtnLabel: 'Activer les notifications',
-          negativeBtnLabel: 'Plus tard',
-          positiveBtnStyle: { backgroundColor: '#eb0055', hoverBackgroundColor: '#c10046' },
-          icon: env.PUBLIC_URL + '/images/ic_launcher_xxxhdpi.png',
-          text: 'Découvre les nouvelles offres en exclusivité sur ton pass en activant les notifications !',
-        },
-      }
-    } else {
-      batchSDKUIConfig = {
-        native: {},
-      }
-    }
-
-    const ENV = env.ENV === 'production' ? '' : `.${env.ENV}`
-    /* Finalize the Batch SDK setup */
-    /* eslint-disable-next-line */
-    const options = {
-      apiKey: env.BATCH_API_KEY_WEB,
-      subdomain: env.BATCH_SUBDOMAIN,
-      authKey: env.BATCH_AUTH_KEY,
-      vapidPublicKey: env.BATCH_VAPID_PUBLIC_KEY,
-      ui: batchSDKUIConfig,
-      defaultIcon: env.PUBLIC_URL + '/images/ic_launcher_xxxhdpi.png',
-      smallIcon: env.PUBLIC_URL + '/images/app-icon-android-notif.png', // for Chrome Android
-      useExistingServiceWorker: true,
-      dev: __DEV__,
-      safari: {
-        [env.PUBLIC_URL]: `web.passculture${env.ENV === 'production' ? '' : ENV}`,
-      },
-    }
-
-    try {
-      window.batchSDK('setup', options)
-      window.batchSDK((api) => api.ui.show('alert'))
-    } catch (error) {
-      eventMonitoring.captureException(error)
-    }
-  },
+  start() {},
 }
 
 export const BatchUser = {
   getInstallationID() {
-    return new Promise((resolve, reject) => {
-      try {
-        window.batchSDK((api) => api.getInstallationID().then(resolve).catch(reject))
-      } catch (error) {
-        eventMonitoring.captureException(error)
-      }
-    })
+    return Promise.resolve()
   },
   editor() {
     return this
   },
-  setIdentifier(id: string) {
-    try {
-      window.batchSDK((api) => api.setCustomUserID(id))
-    } catch (error) {
-      eventMonitoring.captureException(error)
-    }
+  setIdentifier() {
     return this
   },
-  save() {
-    return
-  },
+  save() {},
 }
 
 export const BatchPush = {
-  registerForRemoteNotifications() {
-    getBatchSDK()
-    Batch.start()
-  },
+  registerForRemoteNotifications() {},
 }
