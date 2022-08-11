4508dfedb7c66f857279ee7e1cfeab6c3c9d7c6e
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-08-11 12:09:14 +0200

    (PC-16682): Remove LandscapePage from app

--- src/features/landscapePosition/LandscapePositionPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-import { LandscapePositionPageProps } from 'features/landscapePosition/types'
-
-export const LandscapePositionPage: React.FC<LandscapePositionPageProps> = () => null
