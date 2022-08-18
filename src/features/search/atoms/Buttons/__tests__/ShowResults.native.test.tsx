002352a33e3e915a7dd68abdf0cc80b8b5d7b7f3
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-08-18 10:27:34 +0200

    add test

--- src/features/search/atoms/Buttons/__tests__/ShowResults.native.test.tsx
+++ src/features/search/atoms/Buttons/__tests__/ShowResults.native.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { render } from 'tests/utils'
+import { navigate } from '__mocks__/@react-navigation/native'
+import { SearchWrapper } from 'features/search/pages/SearchWrapper'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render, fireEvent } from 'tests/utils'
 
 import { ShowResults } from '../ShowResults'
 
 let mockData = { pages: [{ nbHits: 0 }] }
+
+jest.unmock('features/search/pages/SearchWrapper')
+
 jest.mock('features/search/pages/useSearchResults', () => ({
   useStagedSearchResults: () => ({
     data: mockData,
@@ -22,12 +28,53 @@ describe('<ShowResults />', () => {
     ${1200} | ${'Afficher les 999+ résultats'} | ${false}
   `(
     'should display the correct translation ($expected) and be disabled=$disabled',
-    ({ nbHits, expected, disabled }) => {
+    async ({ nbHits, expected, disabled }) => {
       mockData = { pages: [{ nbHits }] }
-      const { getByText, getByTestId } = render(<ShowResults />)
-      getByText(expected)
+      const { getByText } = render(
+        // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+        reactQueryProviderHOC(
+          <SearchWrapper>
+            <ShowResults />
+          </SearchWrapper>
+        )
+      )
 
-      expect(getByTestId(expected))[disabled ? 'toBeDisabled' : 'toBeEnabled']()
+      expect(await getByText(expected))[disabled ? 'toBeDisabled' : 'toBeEnabled']()
     }
   )
+
+  it('should call navigate with searchState.view = "Results"', async () => {
+    mockData = { pages: [{ nbHits: 1 }] }
+    const { findByTestId } = render(
+      // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+      reactQueryProviderHOC(
+        <SearchWrapper>
+          <ShowResults />
+        </SearchWrapper>
+      )
+    )
+    const button = await findByTestId('Afficher 1 résultat')
+    await fireEvent.press(button)
+    expect(navigate).toBeCalledWith('TabNavigator', {
+      params: {
+        beginningDatetime: null,
+        date: null,
+        endingDatetime: null,
+        hitsPerPage: 20,
+        locationFilter: { locationType: 'EVERYWHERE' },
+        offerCategories: [],
+        offerIsDuo: false,
+        offerIsFree: false,
+        offerIsNew: false,
+        offerSubcategories: [],
+        offerTypes: { isDigital: false, isEvent: false, isThing: false },
+        priceRange: [0, 300],
+        query: '',
+        tags: [],
+        timeRange: null,
+        view: 'Results',
+      },
+      screen: 'Search',
+    })
+  })
 })
