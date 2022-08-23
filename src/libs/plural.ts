5d7fbc35d5ea562424b2828378db57b6eeacd232
Lucas Beneston <lucas.beneston@passculture.app>
2022-08-23 08:59:22 +0200

    (PC-16806) refactor(lingui): update plural with formated string

--- src/libs/plural.ts
+++ src/libs/plural.ts
@@ -5,6 +5,6 @@ type ChoiceOptions = {
 
 export const plural = (value: number | string, options: ChoiceOptions): string => {
   if (options.one && value === 1) return options.one.replace('#', '1')
-  if (options.other && value > 1) return options.other.replace('#', value.toString())
+  if (options.other && value > 1) return options.other.replace('#', value.toLocaleString('fr'))
   return ''
 }
