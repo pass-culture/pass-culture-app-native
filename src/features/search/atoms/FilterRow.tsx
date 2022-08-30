f356ea4a9953cf3bf6040a988f7242e62527d208
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-08-30 12:05:04 +0200

    (PC-17061) fix(Search): added FilterRow on storybook

---
+++ src/features/search/atoms/FilterRow.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+import { TouchableOpacity } from 'ui/components/TouchableOpacity'
+import { ArrowNext as DefaultArrowNext } from 'ui/svg/icons/ArrowNext'
+import { AccessibleIcon } from 'ui/svg/icons/types'
+import { Typo, Spacer, getSpacing } from 'ui/theme'
+
+interface Props {
+  icon: React.FunctionComponent<AccessibleIcon>
+  title: string
+  description?: string
+  captionId?: string
+  onPress: () => void
+}
+
+export const FilterRow = ({ icon: Icon, title, description, captionId, onPress }: Props) => {
+  const StyledIcon = styled(Icon).attrs(({ theme }) => ({
+    color: theme.colors.black,
+    color2: theme.colors.black,
+    size: theme.icons.sizes.small,
+  }))``
+  return (
+    <MarginHorizontalContainer>
+      <LocationContentContainer testID="FilterRow" onPress={onPress} aria-describedby={captionId}>
+        <StyledIcon />
+        <Spacer.Row numberOfSpaces={2} />
+        <TextContainer>
+          <Title numberOfLines={1}>{title}</Title>
+          <Description numberOfLines={1}>{description}</Description>
+        </TextContainer>
+        <Spacer.Flex />
+        <ArrowNext />
+      </LocationContentContainer>
+    </MarginHorizontalContainer>
+  )
+}
+
+const MarginHorizontalContainer = styled.View({ marginHorizontal: getSpacing(6) })
+
+const LocationContentContainer = styled(TouchableOpacity)({
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
+
+const TextContainer = styled.View({
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+})
+
+const Title = styled(Typo.ButtonText)({
+  flexShrink: 1,
+})
+
+const Description = styled(Typo.Caption)(({ theme }) => ({
+  flexShrink: 1,
+  color: theme.colors.greyDark,
+}))
+
+const ArrowNext = styled(DefaultArrowNext).attrs(({ theme }) => ({
+  size: theme.icons.sizes.smaller,
+}))``
--- src/ui/svg/icons/Sort.tsx
+++ src/ui/svg/icons/Sort.tsx
@@ -16,7 +16,7 @@ function SortSvg({ size, color, accessibilityLabel, testID }: AccessibleIcon) {
       accessibilityLabel={accessibilityLabel}
       testID={testID}>
       <G fill="none" fillRule="evenodd">
-        <G fill="#FFF">
+        <G fill={color}>
           <Path
             d="M16 21.333c.368 0 .667.299.667.667 0 .368-.299.667-.667.667H8c-.368 0-.667-.299-.667-.667 0-.368.299-.667.667-.667h8zm4-6c.368 0 .667.299.667.667 0 .368-.299.667-.667.667H8c-.368 0-.667-.299-.667-.667 0-.368.299-.667.667-.667h12zm4-6c.368 0 .667.299.667.667 0 .368-.299.667-.667.667H8c-.368 0-.667-.299-.667-.667 0-.368.299-.667.667-.667h16z"
             transform="translate(-146 -1065) translate(134 1061) translate(12 4)"
