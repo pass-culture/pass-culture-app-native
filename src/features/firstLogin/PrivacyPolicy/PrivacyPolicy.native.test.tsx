986ceac19b6aaabf748da6fc311e80ba96d04720
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2022-09-02 10:45:57 +0200

    (PC-17117) feat(cookies): show CookiesConsent modale according to FF appEnableCookiesV2 (#3491)
    * (PC-17117) feat(cookies): show CookiesConsent modale according to FF appEnableCookiesV2
    
    * (PC-17117) feat(cookies): add tests relative to FF appEnableCookiesV2 for PrivacyPolicy
    
    * (PC-17177) fix(tests): update web snapshots
    
    * (PC-17117) feat(cookies): update condition to display cookies modale

--- src/features/firstLogin/PrivacyPolicy/PrivacyPolicy.native.test.tsx
+++ src/features/firstLogin/PrivacyPolicy/PrivacyPolicy.native.test.tsx
@@ -6,80 +6,98 @@ import { flushAllPromisesWithAct, fireEvent, render } from 'tests/utils'
 
 import { PrivacyPolicy } from './PrivacyPolicy'
 
+const mockSettings = jest.fn().mockReturnValue({ data: { appEnableCookiesV2: false } })
+jest.mock('features/auth/settings', () => ({
+  useAppSettings: jest.fn(() => mockSettings()),
+}))
+
 describe('<PrivacyPolicy />', () => {
-  afterEach(() => {
-    storage.clear('has_accepted_cookie')
+  describe('Cookies Modal V1', () => {
+    afterEach(() => {
+      storage.clear('has_accepted_cookie')
+    })
+
+    it('should not show cookies modal V2 when appEnableCookiesV2 feature flag is desactivated', async () => {
+      const renderAPI = render(<PrivacyPolicy />)
+      await flushAllPromisesWithAct()
+
+      const title = renderAPI.queryByText('Choisir les cookies')
+      expect(title).toBeFalsy()
+    })
+
+    it('should show modal when has_accepted_cookie is null', async () => {
+      const renderAPI = render(<PrivacyPolicy />)
+      await flushAllPromisesWithAct()
+
+      const title = renderAPI.queryByText('Respect de ta vie privée')
+      expect(title).toBeTruthy()
+    })
+
+    it('should NOT show modal when has_accepted_cookie is false', async () => {
+      await storage.saveObject('has_accepted_cookie', false)
+      const renderAPI = render(<PrivacyPolicy />)
+      await flushAllPromisesWithAct()
+
+      const title = renderAPI.queryByText('Respect de ta vie privée')
+      expect(title).toBeFalsy()
+    })
+
+    it('should NOT show modal when has_accepted_cookie is true', async () => {
+      await storage.saveObject('has_accepted_cookie', true)
+      const renderAPI = render(<PrivacyPolicy />)
+      await flushAllPromisesWithAct()
+
+      const title = renderAPI.queryByText('Respect de ta vie privée')
+      expect(title).toBeFalsy()
+    })
+
+    it('should close modal and set has_accepted_cookie to true in storage on approval', async () => {
+      expect(await storage.readObject('has_accepted_cookie')).toBeNull()
+      const renderAPI = render(<PrivacyPolicy />)
+      await flushAllPromisesWithAct()
+
+      expect(await storage.readObject('has_accepted_cookie')).toBeNull()
+      const title = renderAPI.queryByText('Respect de ta vie privée')
+      expect(title).toBeTruthy()
+
+      fireEvent.press(renderAPI.getByText('Autoriser'))
+      await flushAllPromisesWithAct()
+
+      expect(await storage.readObject('has_accepted_cookie')).toBe(true)
+      expect(renderAPI.queryByText('Autoriser')).toBeNull()
+    })
+
+    it('should close modal and set has_accepted_cookie to false in storage on refusal', async () => {
+      expect(await storage.readObject('has_accepted_cookie')).toBeNull()
+      const renderAPI = render(<PrivacyPolicy />)
+      await flushAllPromisesWithAct()
+
+      expect(await storage.readObject('has_accepted_cookie')).toBeNull()
+      const title = renderAPI.queryByText('Respect de ta vie privée')
+      expect(title).toBeTruthy()
+
+      fireEvent.press(renderAPI.getByTestId('rightIcon'))
+      await flushAllPromisesWithAct()
+
+      expect(await storage.readObject('has_accepted_cookie')).toBe(false)
+      expect(renderAPI.queryByText('Autoriser')).toBeNull()
+      expect(analytics.logHasRefusedCookie).toHaveBeenCalled()
+      await flushAllPromisesWithAct()
+      expect(analytics.disableCollection).toHaveBeenCalled()
+    })
   })
 
-  it('should render privacy policy', async () => {
-    jest.useFakeTimers()
-    const renderAPI = render(<PrivacyPolicy />)
-    await flushAllPromisesWithAct()
+  describe('Cookies Modal V2', () => {
+    beforeAll(() => {
+      mockSettings.mockReturnValue({ data: { appEnableCookiesV2: true } })
+    })
 
-    jest.advanceTimersByTime(1000)
+    it('should show cookies modal V2 when appEnableCookiesV2 feature flag is activated', async () => {
+      const renderAPI = render(<PrivacyPolicy />)
+      await flushAllPromisesWithAct()
 
-    expect(renderAPI).toMatchSnapshot()
-    jest.useRealTimers()
-  })
-
-  it('should show modal when has_accepted_cookie is null', async () => {
-    const renderAPI = render(<PrivacyPolicy />)
-    await flushAllPromisesWithAct()
-
-    const title = renderAPI.queryByText('Respect de ta vie privée')
-    expect(title).toBeTruthy()
-  })
-
-  it('should NOT show modal when has_accepted_cookie is false', async () => {
-    await storage.saveObject('has_accepted_cookie', false)
-    const renderAPI = render(<PrivacyPolicy />)
-    await flushAllPromisesWithAct()
-
-    const title = renderAPI.queryByText('Respect de ta vie privée')
-    expect(title).toBeFalsy()
-  })
-
-  it('should NOT show modal when has_accepted_cookie is true', async () => {
-    await storage.saveObject('has_accepted_cookie', true)
-    const renderAPI = render(<PrivacyPolicy />)
-    await flushAllPromisesWithAct()
-
-    const title = renderAPI.queryByText('Respect de ta vie privée')
-    expect(title).toBeFalsy()
-  })
-
-  it('should close modal and set has_accepted_cookie to true in storage on approval', async () => {
-    expect(await storage.readObject('has_accepted_cookie')).toBeNull()
-    const renderAPI = render(<PrivacyPolicy />)
-    await flushAllPromisesWithAct()
-
-    expect(await storage.readObject('has_accepted_cookie')).toBeNull()
-    const title = renderAPI.queryByText('Respect de ta vie privée')
-    expect(title).toBeTruthy()
-
-    fireEvent.press(renderAPI.getByText('Autoriser'))
-    await flushAllPromisesWithAct()
-
-    expect(await storage.readObject('has_accepted_cookie')).toBe(true)
-    expect(renderAPI.queryByText('Autoriser')).toBeNull()
-  })
-
-  it('should close modal and set has_accepted_cookie to false in storage on refusal', async () => {
-    expect(await storage.readObject('has_accepted_cookie')).toBeNull()
-    const renderAPI = render(<PrivacyPolicy />)
-    await flushAllPromisesWithAct()
-
-    expect(await storage.readObject('has_accepted_cookie')).toBeNull()
-    const title = renderAPI.queryByText('Respect de ta vie privée')
-    expect(title).toBeTruthy()
-
-    fireEvent.press(renderAPI.getByTestId('rightIcon'))
-    await flushAllPromisesWithAct()
-
-    expect(await storage.readObject('has_accepted_cookie')).toBe(false)
-    expect(renderAPI.queryByText('Autoriser')).toBeNull()
-    expect(analytics.logHasRefusedCookie).toHaveBeenCalled()
-    await flushAllPromisesWithAct()
-    expect(analytics.disableCollection).toHaveBeenCalled()
+      const title = renderAPI.queryByText('Choisir les cookies')
+      expect(title).toBeTruthy()
+    })
   })
 })
