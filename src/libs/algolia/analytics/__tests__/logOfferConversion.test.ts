e5f577544257d532d1379928fe73104e561f6ae9
Lucas Beneston <lucas.beneston@passculture.app>
2022-09-05 15:42:43 +0200

    (PC-17157) feat(cookies): add tests for logOfferConversion

--- src/libs/algolia/analytics/__tests__/logOfferConversion.test.ts
+++ src/libs/algolia/analytics/__tests__/logOfferConversion.test.ts
@@ -1,5 +1,6 @@
 import AlgoliaSearchInsights from 'search-insights'
 
+import { getAcceptedCookieConsent } from 'features/cookies/getAcceptedCookieConsent'
 import { logOfferConversion } from 'libs/algolia/analytics/logOfferConversion'
 import { captureMonitoringError } from 'libs/monitoring'
 import { getCookiesConsent } from 'libs/trackingConsent/consent'
@@ -18,30 +19,73 @@ jest.mock('libs/trackingConsent/consent')
 const mockGetConsentToCookies = getCookiesConsent as jest.MockedFunction<typeof getCookiesConsent>
 mockGetConsentToCookies.mockResolvedValue(true)
 
-describe('logOfferConversion', () => {
-  it('should send the corresponding Algolia conversion event when called', async () => {
-    await logOfferConversion('abc123')('object123')
+jest.mock('features/cookies/getAcceptedCookieConsent')
+const mockGetAcceptedCookieConsent = getAcceptedCookieConsent as jest.Mock
+mockGetAcceptedCookieConsent.mockResolvedValue(true)
 
-    expect(mockAlgoliaSearchInsights).toBeCalledWith('convertedObjectIDsAfterSearch', {
-      eventName: 'Offer reserved',
-      index: 'algoliaOffersIndexName',
-      objectIDs: ['object123'],
-      queryID: 'abc123',
+let appEnableCookiesV2 = false
+
+describe('logOfferConversion', () => {
+  // TODO(PC-17175): remove this describe
+  describe('cookies consent V1', () => {
+    it('should send the corresponding Algolia conversion event when called', async () => {
+      await logOfferConversion(appEnableCookiesV2, 'abc123')('object123')
+
+      expect(mockAlgoliaSearchInsights).toBeCalledWith('convertedObjectIDsAfterSearch', {
+        eventName: 'Offer reserved',
+        index: 'algoliaOffersIndexName',
+        objectIDs: ['object123'],
+        queryID: 'abc123',
+      })
+    })
+
+    it('should raise a warning instead of sending an event when called without a queryID set', async () => {
+      await logOfferConversion(appEnableCookiesV2)('object123')
+
+      expect(mockAlgoliaSearchInsights).not.toBeCalled()
+      expect(mockCaptureMonitoringError).toBeCalledWith(
+        'Algolia Analytics: useLogOfferConversion called without any QueryID set'
+      )
+    })
+
+    it("should not send an Algolia conversion event when user didn't accept cookies", async () => {
+      mockGetConsentToCookies.mockResolvedValueOnce(false)
+
+      await logOfferConversion(appEnableCookiesV2, 'abc123')('object123')
+
+      expect(mockAlgoliaSearchInsights).not.toBeCalled()
     })
   })
-  it('should raise a warning instead of sending an event when called without a queryID set', async () => {
-    await logOfferConversion()('object123')
 
-    expect(mockAlgoliaSearchInsights).not.toBeCalled()
-    expect(mockCaptureMonitoringError).toBeCalledWith(
-      'Algolia Analytics: useLogOfferConversion called without any QueryID set'
-    )
-  })
-  it("should not send an Algolia conversion event when user didn't accept cookies", async () => {
-    mockGetConsentToCookies.mockResolvedValueOnce(false)
+  describe('cookies consent V2', () => {
+    beforeEach(() => (appEnableCookiesV2 = true))
 
-    await logOfferConversion('abc123')('object123')
+    it('should send the corresponding Algolia conversion event when called', async () => {
+      await logOfferConversion(appEnableCookiesV2, 'abc123')('object123')
 
-    expect(mockAlgoliaSearchInsights).not.toBeCalled()
+      expect(mockAlgoliaSearchInsights).toBeCalledWith('convertedObjectIDsAfterSearch', {
+        eventName: 'Offer reserved',
+        index: 'algoliaOffersIndexName',
+        objectIDs: ['object123'],
+        queryID: 'abc123',
+      })
+    })
+
+    it('should raise a warning instead of sending an event when called without a queryID set', async () => {
+      await logOfferConversion(appEnableCookiesV2)('object123')
+
+      expect(mockAlgoliaSearchInsights).not.toBeCalled()
+      expect(mockCaptureMonitoringError).toBeCalledWith(
+        'Algolia Analytics: useLogOfferConversion called without any QueryID set'
+      )
+    })
+
+    it("should not send an Algolia conversion event when user didn't accept cookies", async () => {
+      mockGetAcceptedCookieConsent.mockResolvedValueOnce(false)
+
+      await logOfferConversion(appEnableCookiesV2, 'abc123')('object123')
+
+      expect(mockAlgoliaSearchInsights).not.toBeCalled()
+    })
   })
 })
