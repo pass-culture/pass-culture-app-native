57cd5b32e946b7bd9e61f9f0b50487fe4ece54e6
Eve Julliard <evej@bam.tech>
2022-09-06 15:09:21 +0200

    (PC-16826) feat(HeroButtonList): extract the description into a DescriptionContent prop

--- src/features/identityCheck/components/HeroButtonList.stories.tsx
+++ src/features/identityCheck/components/HeroButtonList.stories.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable react-native/no-raw-text */
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import React from 'react'
+import { Text } from 'react-native'
 
 import { HeroButtonList } from 'features/identityCheck/components/HeroButtonList'
 import { BicolorSmartphone } from 'ui/svg/icons/BicolorSmartphone'
+import { Typo } from 'ui/theme'
 
 export default {
   title: 'Features/HeroButtonList',
@@ -11,8 +14,16 @@ export default {
 
 const Template: ComponentStory<typeof HeroButtonList> = (props) => <HeroButtonList {...props} />
 
+const description = (
+  <Text>
+    <Typo.Body>J’ai une carte d’identité, un passeport </Typo.Body>
+    <Typo.ButtonText>étranger</Typo.ButtonText>
+    <Typo.Body> ou un titre séjour français</Typo.Body>
+  </Text>
+)
+
 export const Default = Template.bind({})
 Default.args = {
-  title: "J'ai une carte d'identité ou un passeport français",
+  DescriptionContent: description,
   icon: BicolorSmartphone,
 }
