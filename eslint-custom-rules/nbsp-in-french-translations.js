16526a923aa38811dbf52719eca2df0dda95d117
Hugo Voisin <hugov@bam.tech>
2022-09-15 18:43:37 +0200

    feat(eslint nbsp): update rule for template string

--- eslint-custom-rules/nbsp-in-french-translations.js
+++ eslint-custom-rules/nbsp-in-french-translations.js
@@ -16,60 +16,31 @@ module.exports = {
   },
   create(context) {
     return {
-      // For t`textToTranslate !`
-      'TaggedTemplateExpression[tag.name="t"] > TemplateLiteral > TemplateElement[value.raw=/\\s+[!?:»€]/]':
-        (node) => {
-          context.report({
-            node,
-            message: 'Please use \\u00a0 (nbsp) instead of whitespace before !, ?, :, », €',
-            fix: function (fixer) {
-              const textToReplace = node.value.raw.replace(/\s+([!?:»€])/g, '\\u00a0$1')
-              const range = getReplaceRange(node)
-
-              return fixer.replaceTextRange(range, textToReplace)
-            },
-          })
-        },
-      'TaggedTemplateExpression[tag.name="t"] > TemplateLiteral > TemplateElement[value.raw=/«\\s+/]':
-        (node) => {
-          context.report({
-            node,
-            message: 'Please use \\u00a0 (nbsp) instead of whitespace after «',
-            fix: function (fixer) {
-              const textToReplace = node.value.raw.replace(/(«)\s+/g, '$1\\u00a0')
-              const range = getReplaceRange(node)
-
-              return fixer.replaceTextRange(range, textToReplace)
-            },
-          })
-        },
-
-      // For t({ message: 'textToTranslate !'})
-      'CallExpression[callee.name="t"] > ObjectExpression > Property[key.name="message"][value.raw=/\\s+[!?:»€]/]':
-        (node) => {
-          context.report({
-            node,
-            message: 'Please use \\u00a0 (nbsp) instead of whitespace before !, ?, :, », €',
-            fix: function (fixer) {
-              const textToReplace =
-                'message: ' + node.value.raw.replace(/\s+([!?:»€])/g, '\\u00a0$1')
-
-              return fixer.replaceText(node, textToReplace)
-            },
-          })
-        },
-      'CallExpression[callee.name="t"] > ObjectExpression > Property[key.name="message"][value.raw=/«\\s+/]':
-        (node) => {
-          context.report({
-            node,
-            message: 'Please use \\u00a0 (nbsp) instead of whitespace after «',
-            fix: function (fixer) {
-              const textToReplace = 'message: ' + node.value.raw.replace(/(«)\s+/g, '$1\\u00a0')
-
-              return fixer.replaceText(node, textToReplace)
-            },
-          })
-        },
+      // For `textToTranslate !` with characters !, ?, :, » and €
+      'TemplateLiteral > TemplateElement[value.raw=/\\s+[!?:»€]/]': (node) => {
+        context.report({
+          node,
+          message:
+            'Please use unicode non-breaking space \\u00a0 instead of whitespace before !, ?, :, », €',
+          fix: function (fixer) {
+            const textToReplace = node.value.raw.replace(/\s+([!?:»€])/g, '\\u00a0$1')
+            const range = getReplaceRange(node)
+            return fixer.replaceTextRange(range, textToReplace)
+          },
+        })
+      },
+      // For `« textToTranslate`
+      'TemplateLiteral > TemplateElement[value.raw=/«\\s+/]': (node) => {
+        context.report({
+          node,
+          message: 'Please use unicode non-breaking space \\u00a0 instead of whitespace after «',
+          fix: function (fixer) {
+            const textToReplace = node.value.raw.replace(/(«)\s+/g, '$1\\u00a0')
+            const range = getReplaceRange(node)
+            return fixer.replaceTextRange(range, textToReplace)
+          },
+        })
+      },
     }
   },
 }
