f34eb6f08a1f9d009b492c4f7132e06287fc38fa
bruno ebstein <bruno.ebstein@passculture.app>
2022-09-15 17:06:03 +0200

    (PC-16988) refactor(cookies): simpler API for better tests (#3542)
    * refactor(test): stabilize tests
    
    Co-authored-by: Hugo Voisin <hugov@bam.tech>
    Co-authored-by: Ali RAIKI <ali.raiki@polyconseil.fr>
    
    * (PC-16988) refactor(cookies): simpler API for better tests
    
    Co-authored-by: Hugo Voisin <hugov@bam.tech>
    Co-authored-by: Ali RAIKI <ali.raiki@polyconseil.fr>

--- src/features/cookies/api/usePostCookiesConsent.ts
+++ src/features/cookies/api/usePostCookiesConsent.ts
@@ -3,12 +3,14 @@ import { useMutation } from 'react-query'
 import { api } from 'api/api'
 import { getCookiesChoice } from 'features/cookies/helpers/useCookies'
 
+const wait = async (time: number) => new Promise((resolve) => setTimeout(resolve, time))
+
 export function usePostCookiesConsent() {
   return useMutation(async () => {
-    setTimeout(
-      () =>
-        getCookiesChoice().then((consent) => consent && api.postnativev1cookiesConsent(consent)),
-      200 // To make the localStorage is up to date before reading it
-    )
+    await wait(0) // To make the localStorage is up to date before reading it
+    const consent = await getCookiesChoice()
+    if (consent) {
+      await api.postnativev1cookiesConsent(consent)
+    }
   })
 }
