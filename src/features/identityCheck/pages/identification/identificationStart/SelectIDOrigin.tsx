9f24c58851c53249ce687b3a909f74e97185b02a
EveJulliard <89008469+EveJulliard@users.noreply.github.com>
2022-09-16 16:16:19 +0200

    (PC-16827) feat(selectIDOrigin): add forgotten accessibility tag on the SelectIDOrigin screen (#3562)
    * (PC-16827) feat(selectIdOrigin) add forgotten accessibitly tags
    
    * (PC-16827) chore(yarn.lock) update yarn.lock

--- src/features/identityCheck/pages/identification/identificationStart/SelectIDOrigin.tsx
+++ src/features/identityCheck/pages/identification/identificationStart/SelectIDOrigin.tsx
@@ -5,6 +5,8 @@ import styled from 'styled-components/native'
 
 import { HeroButtonList } from 'features/identityCheck/components/HeroButtonList'
 import { PageWithHeader } from 'features/identityCheck/components/layout/PageWithHeader'
+import { Li } from 'ui/components/Li'
+import { VerticalUl } from 'ui/components/Ul'
 import { BicolorEarth } from 'ui/svg/icons/BicolorEarth'
 import { BicolorFrance } from 'ui/svg/icons/BicolorFrance'
 import { BicolorIdCardWithMagnifyingGlass } from 'ui/svg/icons/BicolorIdCardWithMagnifyingGlass'
@@ -23,32 +25,38 @@ const SelectIDOriginContent: FunctionComponent = () => {
       <Spacer.Column numberOfSpaces={4} />
       <StyledSubtitle>{t`Pour cela, nous avons besoin de vérifier ton identité.`}</StyledSubtitle>
       <Spacer.Column numberOfSpaces={8} />
-      <HeroButtonList
-        Title={
-          <Text>
-            <Typo.Body>{t`J’ai une carte d’identité ou un passeport` + ' '}</Typo.Body>
-            <Typo.ButtonText>{t` français`}</Typo.ButtonText>
-          </Text>
-        }
-        icon={BicolorFrance}
-        onPress={() => {
-          return
-        }}
-      />
-      <Spacer.Column numberOfSpaces={6} />
-      <HeroButtonList
-        Title={
-          <Text>
-            <Typo.Body>{t`J’ai une carte d’identité, un passeport` + ' '}</Typo.Body>
-            <Typo.ButtonText>{t`étranger` + ' '}</Typo.ButtonText>
-            <Typo.Body>{t`ou un titre de séjour français`}</Typo.Body>
-          </Text>
-        }
-        icon={BicolorEarth}
-        onPress={() => {
-          return
-        }}
-      />
+      <StyledVerticalUl>
+        <Li>
+          <HeroButtonList
+            Title={
+              <Text>
+                <Typo.Body>{t`J’ai une carte d’identité ou un passeport` + ' '}</Typo.Body>
+                <Typo.ButtonText>{t` français`}</Typo.ButtonText>
+              </Text>
+            }
+            icon={BicolorFrance}
+            onPress={() => {
+              return
+            }}
+          />
+        </Li>
+        <Spacer.Column numberOfSpaces={6} />
+        <Li>
+          <HeroButtonList
+            Title={
+              <Text>
+                <Typo.Body>{t`J’ai une carte d’identité, un passeport` + ' '}</Typo.Body>
+                <Typo.ButtonText>{t`étranger` + ' '}</Typo.ButtonText>
+                <Typo.Body>{t`ou un titre de séjour français`}</Typo.Body>
+              </Text>
+            }
+            icon={BicolorEarth}
+            onPress={() => {
+              return
+            }}
+          />
+        </Li>
+      </StyledVerticalUl>
     </Container>
   )
 }
@@ -71,4 +79,8 @@ const StyledTitle = styled(Typo.Title4)({
 })
 const StyledSubtitle = styled(Typo.Body)({
   textAlign: 'center',
+})
+
+const StyledVerticalUl = styled(VerticalUl)({
+  width: '100%',
 })
