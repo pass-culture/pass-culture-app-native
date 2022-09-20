5e46e3f1b8bc70f5b5ed9579c8bc5167ee797824
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-09-20 17:59:22 +0200

    fix(search): remove venue badge search results

--- src/features/search/atoms/__tests__/NumberOfResults.native.test.tsx
+++ src/features/search/atoms/__tests__/NumberOfResults.native.test.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 
-import { navigate } from '__mocks__/@react-navigation/native'
 import { LocationType } from 'features/search/enums'
 import { LocationFilter } from 'features/search/types'
 import { GeoCoordinates } from 'libs/geolocation'
 import { mockedSuggestedVenues } from 'libs/venue/fixtures/mockedSuggestedVenues'
-import { fireEvent, render } from 'tests/utils'
+import { render } from 'tests/utils'
 
 import { NumberOfResults } from '../NumberOfResults'
 
 jest.mock('react-query')
 
-let mockPosition: GeoCoordinates | null = null
+const mockPosition: GeoCoordinates | null = null
 jest.mock('libs/geolocation', () => ({
   useGeolocation: jest.fn(() => ({ position: mockPosition })),
 }))
@@ -37,48 +36,5 @@ describe('NumberOfResults component', () => {
     render(<NumberOfResults nbHits={1} />).getByText('1 résultat')
     render(<NumberOfResults nbHits={2} />).getByText('2 résultats')
     render(<NumberOfResults nbHits={1234} />).getByText('1 234 résultats')
-  })
-
-  describe('with venue', () => {
-    beforeEach(() => {
-      mockLocationFilter = { locationType: LocationType.VENUE, venue: mockedSuggestedVenues[0] }
-    })
-
-    it('should correctly format the number of hit', () => {
-      expect(render(<NumberOfResults nbHits={0} />).toJSON()).toBeNull()
-      render(<NumberOfResults nbHits={1} />).getByText('1 résultat pour ')
-      render(<NumberOfResults nbHits={2} />).getByText('2 résultats pour ')
-      render(<NumberOfResults nbHits={1234} />).getByText('1 234 résultats pour ')
-    })
-
-    it('should returns to locationFilter - Everywhere if geolocation not activated', () => {
-      mockPosition = null
-      const { getByTestId } = render(<NumberOfResults nbHits={2} />)
-
-      fireEvent.press(getByTestId('Enlever le lieu'))
-
-      const params = { locationFilter: { locationType: LocationType.EVERYWHERE } }
-      expect(mockStagedSearchDispatch).toBeCalledWith({
-        type: 'SET_STATE_FROM_NAVIGATE',
-        payload: params,
-      })
-
-      expect(navigate).toBeCalledWith('TabNavigator', {
-        screen: 'Search',
-        params,
-      })
-    })
-
-    it('should returns to locationFilter - Everywhere if geolocation not activated', () => {
-      mockPosition = { latitude: 20, longitude: 23 }
-      const { getByTestId } = render(<NumberOfResults nbHits={2} />)
-
-      fireEvent.press(getByTestId('Enlever le lieu'))
-
-      expect(navigate).toBeCalledWith('TabNavigator', {
-        screen: 'Search',
-        params: { locationFilter: { locationType: LocationType.AROUND_ME, aroundRadius: 100 } },
-      })
-    })
   })
 })
