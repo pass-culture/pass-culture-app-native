45d793da889f80dc71d1fedd28e484678de635d0
Lucas Beneston <lucas.beneston@passculture.app>
2022-09-20 12:43:14 +0200

    (PC-16806) refactor(plural): udpate plural props

--- src/libs/plural.ts
+++ src/libs/plural.ts
@@ -3,7 +3,7 @@ type ChoiceOptions = {
   other?: string
 }
 
-export const plural = (value: number | string, options: ChoiceOptions): string => {
+export const plural = (value: number, options: ChoiceOptions): string => {
   if (options.one && value === 1) return options.one.replace('#', '1')
   if (options.other && value > 1) return options.other.replace('#', value.toLocaleString('fr'))
   return ''
