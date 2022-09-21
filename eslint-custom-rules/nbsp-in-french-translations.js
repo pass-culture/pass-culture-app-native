19324d9a9a267c297fe835ef8452d7e8a2aac026
Lucas Beneston <lucas.beneston@passculture.app>
2022-09-21 09:11:18 +0200

    (PC-16806) refactor(eslint): update nbsp rule

--- eslint-custom-rules/nbsp-in-french-translations.js
+++ eslint-custom-rules/nbsp-in-french-translations.js
@@ -1,8 +1,12 @@
 /**
- * This rule aims to spot misuses of a space instead of a non-breaking space (nbsp) with code \u00a0
+ * This rule aims to spot misuses of a space instead of a non-breaking space with code \u00a0 or &nbsp
  * before some characters in French translations, such as ! ? : ; € » or after «
  *
  * Ex: "Bienvenue !" should be "Bienvenue\u00a0!"
+ *
+ * The work here is based on ESLint documentation: https://eslint.org/docs/latest/developer-guide/working-with-rules
+ * and especially for the selectors: https://eslint.org/docs/latest/developer-guide/selectors
+ * You can visualize the "node" structure with: https://astexplorer.net/
  */
 
 module.exports = {
@@ -16,45 +20,125 @@ module.exports = {
   },
   create(context) {
     return {
-      // For t`textToTranslate !`
-      'TaggedTemplateExpression[tag.name="t"] > TemplateLiteral > TemplateElement[value.raw=/\\s+[!?:»€]/]':
-        (node) => {
-          context.report({
-            node,
-            message: 'Please use \\u00a0 (nbsp) instead of whitespace before !, ?, :, », €',
-            fix: function (fixer) {
-              const textToReplace = node.value.raw.replace(/\s+([!?:»€])/g, '\\u00a0$1')
-              const range = getReplaceRange(node)
+      // \u00a0 for 'textToTranslate !' with characters !, ?, :, » and €
+      "Literal[raw=/^'.*\\s+[!?:»€].*'$/]": (node) => {
+        if (node.value.raw && node.value.raw.includes('!important')) return
 
-              return fixer.replaceTextRange(range, textToReplace)
-            },
-          })
-        },
-      'TaggedTemplateExpression[tag.name="t"] > TemplateLiteral > TemplateElement[value.raw=/«\\s+/]':
-        (node) => {
-          context.report({
-            node,
-            message: 'Please use \\u00a0 (nbsp) instead of whitespace after «',
-            fix: function (fixer) {
-              const textToReplace = node.value.raw.replace(/(«)\s+/g, '$1\\u00a0')
-              const range = getReplaceRange(node)
+        context.report({
+          node,
+          message:
+            'Please use unicode non-breaking space \\u00a0 instead of whitespace before !, ?, :, », €',
+          fix: function (fixer) {
+            const textToReplace = node.raw.replace(/\s+([!?:»€])/g, '\\u00a0$1')
+            return fixer.replaceText(node, textToReplace)
+          },
+        })
+      },
 
-              return fixer.replaceTextRange(range, textToReplace)
-            },
-          })
-        },
-      'CallExpression[callee.name="t"] > ObjectExpression > Property[key.name="message"][value.raw=/«\\s+/]':
-        (node) => {
-          context.report({
-            node,
-            message: 'Please use \\u00a0 (nbsp) instead of whitespace after «',
-            fix: function (fixer) {
-              const textToReplace = 'message: ' + node.value.raw.replace(/(«)\s+/g, '$1\\u00a0')
+      // &nbsp; for "textToTranslate !" with characters !, ?, :, » and €
+      'Literal[raw=/^\\".*\\s+[!?:»€].*\\"$/]': (node) => {
+        context.report({
+          node,
+          message:
+            'Please use unicode non-breaking space &nbsp; instead of whitespace before !, ?, :, », €',
+          fix: function (fixer) {
+            const textToReplace = node.raw.replace(/\s+([!?:»€])/g, '&nbsp;$1')
+            return fixer.replaceText(node, textToReplace)
+          },
+        })
+      },
 
-              return fixer.replaceText(node, textToReplace)
-            },
-          })
-        },
+      // \u00a0 for '« textToTranslate'
+      "Literal[raw=/^'.*«\\s+.*'$/]": (node) => {
+        context.report({
+          node,
+          message: 'Please use unicode non-breaking space \\u00a0 instead of whitespace after «',
+          fix: function (fixer) {
+            const textToReplace = node.raw.replace(/(«)\s+/g, '$1\\u00a0')
+            return fixer.replaceText(node, textToReplace)
+          },
+        })
+      },
+
+      // &nbsp; for "« textToTranslate"
+      'Literal[raw=/^\\".*«\\s+.*\\"$/]': (node) => {
+        context.report({
+          node,
+          message: 'Please use unicode non-breaking space &nbsp; instead of whitespace after «',
+          fix: function (fixer) {
+            const textToReplace = node.raw.replace(/(«)\s+/g, '$1&nbsp;')
+            return fixer.replaceText(node, textToReplace)
+          },
+        })
+      },
+
+      // &nbsp; for "« textToTranslate"
+      'Literal[raw=/^\\".*«\\s+.*\\"$/]': (node) => {
+        context.report({
+          node,
+          message: 'Please use unicode non-breaking space &nbsp; instead of whitespace after «',
+          fix: function (fixer) {
+            const textToReplace = node.raw.replace(/(«)\s+/g, '$1&nbsp;')
+            return fixer.replaceText(node, textToReplace)
+          },
+        })
+      },
+
+      // For `textToTranslate !` with characters !, ?, :, » and €
+      'TemplateLiteral > TemplateElement[value.raw=/\\s+[!?:»€]/]': (node) => {
+        if (node.value.raw.includes('!important')) return
+
+        context.report({
+          node,
+          message:
+            'Please use unicode non-breaking space \\u00a0 instead of whitespace before !, ?, :, », €',
+          fix: function (fixer) {
+            const textToReplace = node.value.raw.replace(/\s+([!?:»€])/g, '\\u00a0$1')
+            const range = getReplaceRange(node)
+            return fixer.replaceTextRange(range, textToReplace)
+          },
+        })
+      },
+
+      // For `« textToTranslate`
+      'TemplateLiteral > TemplateElement[value.raw=/«\\s+/]': (node) => {
+        context.report({
+          node,
+          message: 'Please use unicode non-breaking space \\u00a0 instead of whitespace after «',
+          fix: function (fixer) {
+            const textToReplace = node.value.raw.replace(/(«)\s+/g, '$1\\u00a0')
+            const range = getReplaceRange(node)
+            return fixer.replaceTextRange(range, textToReplace)
+          },
+        })
+      },
+
+      // For <Text>textToTranslate !</Text> with characters !, ?, :, » and €
+      'JSXText[value=/\\s+[!?:»€]/]': (node) => {
+        context.report({
+          node,
+          message:
+            'Please use &nbsp; (non-breaking space) instead of whitespace before !, ?, :, », €',
+          fix: function (fixer) {
+            const textToReplace = node.value.replace(/\s+([!?:»€])/g, '&nbsp;$1')
+
+            return fixer.replaceText(node, textToReplace)
+          },
+        })
+      },
+
+      // For <Text>« textToTranslate</Text>
+      'JSXText[value=/«\\s+/]': (node) => {
+        context.report({
+          node,
+          message: 'Please use &nbsp; (non-breaking space) instead of whitespace after «',
+          fix: function (fixer) {
+            const textToReplace = node.value.replace(/(«)\s+/g, '$1&nbsp;')
+
+            return fixer.replaceText(node, textToReplace)
+          },
+        })
+      },
     }
   },
 }
