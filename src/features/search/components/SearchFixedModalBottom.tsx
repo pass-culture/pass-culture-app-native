4905509a59be943c743e977516fbc9a7c8a25018
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-09-23 17:34:22 +0200

    (PC-17578) fix(search): improve displaying search price form

--- src/features/search/components/SearchFixedModalBottom.tsx
+++ src/features/search/components/SearchFixedModalBottom.tsx
@@ -1,6 +1,10 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
+import { Platform } from 'react-native'
+import styled from 'styled-components/native'
 
 import { FilterPageButtons } from 'features/search/components/FilterPageButtons/FilterPageButtons'
+import { useForHeightKeyboardEvents } from 'ui/components/keyboard/useKeyboardEvents'
+import { ModalSpacing } from 'ui/components/modals/enum'
 
 type Props = {
   onResetPress: () => void
@@ -13,12 +17,23 @@ export const SearchFixedModalBottom = memo(function SearchFixedModalBottom({
   onSearchPress,
   isSearchDisabled,
 }: Props) {
+  const [keyboardHeight, setKeyboardHeight] = useState(0)
+  useForHeightKeyboardEvents(setKeyboardHeight)
+
+  const modalSpacing = Platform.OS === 'ios' ? ModalSpacing.LG : ModalSpacing.SM
+
   return (
-    <FilterPageButtons
-      onResetPress={onResetPress}
-      onSearchPress={onSearchPress}
-      isModal={true}
-      isSearchDisabled={isSearchDisabled}
-    />
+    <FilterPageButtonsContainer paddingBottom={keyboardHeight ? keyboardHeight - modalSpacing : 0}>
+      <FilterPageButtons
+        onResetPress={onResetPress}
+        onSearchPress={onSearchPress}
+        isModal={true}
+        isSearchDisabled={isSearchDisabled}
+      />
+    </FilterPageButtonsContainer>
   )
 })
+
+const FilterPageButtonsContainer = styled.View<{ paddingBottom: number }>(({ paddingBottom }) => ({
+  ...(paddingBottom ? { paddingBottom } : {}),
+}))
