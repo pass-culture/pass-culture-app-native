9da48fcc05b74f62d4060a0477ea4ccfc3db0ecf
Lucas Beneston <lucas.beneston@passculture.app>
2022-09-28 15:40:50 +0200

    (PC-17514) feat(reviewApp): add FF disabledStoreReview to useShowReview

--- src/libs/hooks/useShowReview.ts
+++ src/libs/hooks/useShowReview.ts
@@ -1,15 +1,19 @@
 import { useEffect } from 'react'
 import InAppReview from 'react-native-in-app-review'
 
+import { useAppSettings } from 'features/auth/settings'
 import { useReviewInAppInformation } from 'features/bookOffer/services/useReviewInAppInformation'
 import { eventMonitoring } from 'libs/monitoring'
 
 export const useShowReview = () => {
+  const { data: settings } = useAppSettings()
+  const disableStoreReview = settings?.disableStoreReview
+
   const { shouldReviewBeRequested, updateInformationWhenReviewHasBeenRequested } =
     useReviewInAppInformation()
 
   useEffect(() => {
-    if (InAppReview.isAvailable() && shouldReviewBeRequested) {
+    if (InAppReview.isAvailable() && shouldReviewBeRequested && !disableStoreReview) {
       setTimeout(
         () =>
           InAppReview.RequestInAppReview()
