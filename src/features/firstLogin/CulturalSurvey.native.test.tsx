99d8a86e2fa490fbf0a45a75544e39642a84e46d
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2022-09-29 18:26:44 +0200

    Revert "(PC-17231) feat(android): Target Android SDK 33 (#3629)" (#3664)

--- src/features/firstLogin/CulturalSurvey.native.test.tsx
+++ src/features/firstLogin/CulturalSurvey.native.test.tsx
@@ -95,10 +95,7 @@ describe('<CulturalSurvey />', () => {
     const renderAPI = await renderCulturalSurveyWithNavigation()
     act(() => {
       const webview = renderAPI.getByTestId('cultural-survey-webview')
-      // onNavigationStateChange is triggered when the WebView loading starts or ends
-      webview.props.onLoadingStart({
-        nativeEvent: { url: 'passculture.typeform.com' },
-      })
+      webview.props.onNavigationStateChange({ url: 'passculture.typeform.com' })
     })
     await superFlushWithAct()
     expect(renderAPI.queryByTestId('cultural-survey-webview')).toBeTruthy()
@@ -109,10 +106,7 @@ describe('<CulturalSurvey />', () => {
     const renderAPI = await renderCulturalSurveyWithNavigation()
     act(() => {
       const webview = renderAPI.getByTestId('cultural-survey-webview')
-      // onNavigationStateChange is triggered when the WebView loading starts or ends
-      webview.props.onLoadingStart({
-        nativeEvent: { url: 'app.passculture' },
-      })
+      webview.props.onNavigationStateChange({ url: 'app.passculture' })
     })
     await superFlushWithAct()
     await waitForExpect(() => {
