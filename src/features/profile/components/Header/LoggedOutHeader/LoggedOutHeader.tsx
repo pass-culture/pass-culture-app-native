9d8f190e7435d9007cdb03550e53c29b930236ef
Lucas Beneston <lucas.beneston@passculture.app>
2022-10-04 16:57:45 +0200

    (PC-16966) feat(profile): update design for LoggedOutHeader

--- src/features/profile/components/Header/LoggedOutHeader/LoggedOutHeader.tsx
+++ src/features/profile/components/Header/LoggedOutHeader/LoggedOutHeader.tsx
@@ -1,58 +1,60 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
-import { accessibilityAndTestId } from 'libs/accessibilityAndTestId'
 import { analytics } from 'libs/firebase/analytics'
 import { ButtonInsideText } from 'ui/components/buttons/buttonInsideText/ButtonInsideText'
-import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { ButtonWithLinearGradient } from 'ui/components/buttons/ButtonWithLinearGradient'
 import { PageHeader } from 'ui/components/headers/PageHeader'
 import { TouchableLink } from 'ui/components/touchableLink/TouchableLink'
 import { Connect } from 'ui/svg/icons/Connect'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 
 export function LoggedOutHeader() {
-  const loginText = 'Tu as déjà un compte\u00a0?\u00a0'
   return (
     <React.Fragment>
       <PageHeader title="Mon profil" size="medium" />
-      <Spacer.Column numberOfSpaces={2} />
-      <ContentContainer>
-        <StyledBody>
-          Tu as entre 15 et 18 ans&nbsp;? Crée-toi un compte pour bénéficier de ton crédit pass
-          Culture
-        </StyledBody>
-        <Spacer.Column numberOfSpaces={8} />
-        <TouchableLink
-          as={ButtonPrimary}
-          testID="S’inscrire"
-          wording="S’inscrire"
-          navigateTo={{ screen: 'SignupForm', params: { preventCancellation: true } }}
-          onPress={() => {
-            analytics.logProfilSignUp()
-          }}
-        />
+      <Container>
+        <Typo.Body>Tu as entre 15 et 18 ans&nbsp;?</Typo.Body>
         <Spacer.Column numberOfSpaces={5} />
-        <StyledBody>
-          {loginText}
+        <GreyContainer>
+          <Typo.Body>Crée-toi un compte pour bénéficier de ton crédit pass Culture</Typo.Body>
+          <Spacer.Column numberOfSpaces={5} />
           <TouchableLink
-            as={ButtonInsideText}
-            navigateTo={{ screen: 'Login', params: { preventCancellation: true } }}
-            wording="Connecte-toi"
-            icon={Connect}
-            {...accessibilityAndTestId('Connecte-toi')}
+            as={ButtonWithLinearGradient}
+            wording="Créer un compte"
+            navigateTo={{ screen: 'SignupForm', params: { preventCancellation: true } }}
+            onPress={() => analytics.logProfilSignUp()}
           />
-        </StyledBody>
-      </ContentContainer>
-      <Spacer.Column numberOfSpaces={6} />
+          <Spacer.Column numberOfSpaces={5} />
+          <StyledBody>
+            Déjà un compte&nbsp;?&nbsp;
+            <TouchableLink
+              as={StyledButtonInsideText}
+              navigateTo={{ screen: 'Login', params: { preventCancellation: true } }}
+              wording="Se connecter"
+              icon={Connect}
+            />
+          </StyledBody>
+        </GreyContainer>
+      </Container>
+      <Spacer.Column numberOfSpaces={2} />
     </React.Fragment>
   )
 }
 
-const ContentContainer = styled.View({
-  alignItems: 'center',
-  paddingHorizontal: getSpacing(6),
-  width: '100%',
+const Container = styled.View({
+  marginHorizontal: getSpacing(6),
 })
+
+const GreyContainer = styled.View(({ theme }) => ({
+  padding: getSpacing(6),
+  borderRadius: getSpacing(2),
+  backgroundColor: theme.colors.greyLight,
+}))
+
+const StyledButtonInsideText = styled(ButtonInsideText).attrs(({ theme }) => ({
+  color: theme.colors.secondary,
+}))``
 
 const StyledBody = styled(Typo.Body)({
   textAlign: 'center',
