3c70ad614da5ec053bcddc1fd0c961b264463be3
EveJulliard <89008469+EveJulliard@users.noreply.github.com>
2022-10-05 17:37:39 +0200

    (PC-17863) feat(a11y): more accessible search filter er button (#3697)
    Co-authored-by: marineshaw <marines@bam.tech>

--- src/features/search/atoms/Buttons/__tests__/SingleFilterButton.native.test.tsx
+++ src/features/search/atoms/Buttons/__tests__/SingleFilterButton.native.test.tsx
@@ -1,53 +1,23 @@
 import React from 'react'
 
 import { SingleFilterButton } from 'features/search/atoms/Buttons/FilterButton/SingleFilterButton'
-import { render } from 'tests/utils'
-import { theme } from 'theme'
-import { Check } from 'ui/svg/icons/Check'
+import { fireEvent, render } from 'tests/utils'
 
 describe('SingleFilterButton', () => {
-  it('should render correctly', () => {
-    const singleFilterButton = render(
-      <SingleFilterButton
-        label="CD, vinyles, musique en ligne"
-        Icon={Check}
-        color={theme.colors.primary}
-        onPress={jest.fn()}
-      />
-    )
-    expect(singleFilterButton).toMatchSnapshot()
-  })
-
-  it('should display an icon when specified', () => {
-    const { getByTestId } = render(
-      <SingleFilterButton label="CD, vinyles, musique en ligne" Icon={Check} onPress={jest.fn()} />
-    )
-
-    expect(getByTestId('filterButtonIcon')).toBeTruthy()
-  })
-
-  it('should use the color in the label when specified', () => {
+  it('should execute onPress function when the button is clicked', () => {
+    const mockedOnPress = jest.fn()
     const { getByTestId } = render(
       <SingleFilterButton
         label="CD, vinyles, musique en ligne"
-        color={theme.colors.primary}
-        onPress={jest.fn()}
-      />
-    )
-
-    expect(getByTestId('filterButtonLabel')).toHaveStyle({ color: theme.colors.primary })
-  })
-
-  it('should use the color in the border of the button when specified', () => {
-    const { getByTestId } = render(
-      <SingleFilterButton
-        label="CD, vinyles, musique en ligne"
-        color={theme.colors.primary}
-        onPress={jest.fn()}
+        onPress={mockedOnPress}
         testID="filterButton"
+        isSelected
       />
     )
 
-    expect(getByTestId('filterButton')).toHaveStyle({ borderColor: theme.colors.primary })
+    const button = getByTestId('filterButton')
+    fireEvent.press(button)
+
+    expect(mockedOnPress).toHaveBeenCalledTimes(1)
   })
 })
