990ab70e6ff525a11b1303ce57d78ba764705458
Lucas Beneston <lucas.beneston@passculture.app>
2022-10-06 15:57:03 +0200

    (PC-16973) feat(profile): update web design for LoggedOutHeader

--- src/features/profile/components/Header/LoggedOutHeader/LoggedOutHeader.tsx
+++ src/features/profile/components/Header/LoggedOutHeader/LoggedOutHeader.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import styled from 'styled-components/native'
+import styled, { useTheme } from 'styled-components/native'
 
 import { HeaderWithGreyContainer } from 'features/profile/components/Header/HeaderWithGreyContainer/HeaderWithGreyContainer'
 import { analytics } from 'libs/firebase/analytics'
 import { ButtonInsideText } from 'ui/components/buttons/buttonInsideText/ButtonInsideText'
-import { ButtonWithLinearGradient } from 'ui/components/buttons/ButtonWithLinearGradient'
+import { ButtonWithLinearGradient } from 'ui/components/buttons/buttonWithLinearGradient/ButtonWithLinearGradient'
 import { TouchableLink } from 'ui/components/touchableLink/TouchableLink'
 import { Connect } from 'ui/svg/icons/Connect'
-import { Spacer, Typo } from 'ui/theme'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
 
 export function LoggedOutHeader() {
+  const { isDesktopViewport } = useTheme()
   const loginText = 'Déjà un compte\u00a0?\u00a0'
   return (
     <HeaderWithGreyContainer
@@ -19,27 +20,48 @@ export function LoggedOutHeader() {
         <React.Fragment>
           <Typo.Body>Identifie-toi pour bénéficier de ton crédit pass Culture</Typo.Body>
           <Spacer.Column numberOfSpaces={5} />
-          <TouchableLink
-            as={ButtonWithLinearGradient}
-            wording="Créer un compte"
-            navigateTo={{ screen: 'SignupForm', params: { preventCancellation: true } }}
-            onPress={() => analytics.logProfilSignUp()}
-          />
-          <Spacer.Column numberOfSpaces={5} />
-          <StyledBody>
-            {loginText}
+          <Container>
             <TouchableLink
-              as={StyledButtonInsideText}
-              navigateTo={{ screen: 'Login', params: { preventCancellation: true } }}
-              wording="Se connecter"
-              icon={Connect}
+              as={ButtonWithLinearGradient}
+              wording="Créer un compte"
+              navigateTo={{ screen: 'SignupForm', params: { preventCancellation: true } }}
+              onPress={() => analytics.logProfilSignUp()}
+              fitContentWidth={isDesktopViewport}
             />
-          </StyledBody>
+
+            {isDesktopViewport ? <VerticalSeparator /> : <Spacer.Column numberOfSpaces={5} />}
+
+            <LoginContainer>
+              <StyledBody>
+                {loginText}
+                <TouchableLink
+                  as={StyledButtonInsideText}
+                  navigateTo={{ screen: 'Login', params: { preventCancellation: true } }}
+                  wording="Se connecter"
+                  icon={Connect}
+                />
+              </StyledBody>
+            </LoginContainer>
+          </Container>
         </React.Fragment>
       }
     />
   )
 }
+
+const Container = styled.View(({ theme }) => ({
+  flexDirection: theme.isDesktopViewport ? 'row' : 'column',
+}))
+
+const VerticalSeparator = styled.View(({ theme }) => ({
+  borderRightWidth: getSpacing(0.25),
+  marginHorizontal: getSpacing(6),
+  borderRightColor: theme.colors.greyMedium,
+}))
+
+const LoginContainer = styled.View({
+  justifyContent: 'center',
+})
 
 const StyledButtonInsideText = styled(ButtonInsideText).attrs(({ theme }) => ({
   color: theme.colors.secondary,
