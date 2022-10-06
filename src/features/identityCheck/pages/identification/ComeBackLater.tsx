41417991ec8a4b33f7f74f3ca74db8376fb9a7c1
Eve Julliard <evej@bam.tech>
2022-10-06 16:32:07 +0200

    (PC-16836) create ComeBackLater screen

---
+++ src/features/identityCheck/pages/identification/ComeBackLater.tsx
@@ -0,0 +1,42 @@
+import React, { FunctionComponent } from 'react'
+import { View } from 'react-native'
+import styled from 'styled-components/native'
+
+import { navigateToHomeConfig } from 'features/navigation/helpers'
+import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { TouchableLink } from 'ui/components/touchableLink/TouchableLink'
+import { GenericInfoPageWhite } from 'ui/pages/GenericInfoPageWhite'
+import { BicolorIdCardInvalid } from 'ui/svg/icons/BicolorIdCardInvalid'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
+
+export const ComeBackLater: FunctionComponent = () => {
+  return (
+    <GenericInfoPageWhite
+      icon={BicolorIdCardInvalid}
+      titleComponent={Typo.Title2}
+      title="Reviens plus tard"
+      separateIconFromTitle={false}
+      headerGoBack>
+      <StyledText>
+        <Typo.Body>Pour profiter du pass Culture, tu dois avoir </Typo.Body>
+        <Typo.ButtonText>
+          ta pièce d’identité originale et en cours de validité avec toi.
+        </Typo.ButtonText>
+      </StyledText>
+      <StyledText>N’hésite pas à revenir t’identifier quand tu l’auras avec toi&nbsp;!</StyledText>
+      <Spacer.Flex flex={1} />
+      <View>
+        <TouchableLink
+          as={ButtonPrimary}
+          wording="M’identifier plus tard"
+          navigateTo={navigateToHomeConfig}
+        />
+      </View>
+    </GenericInfoPageWhite>
+  )
+}
+
+const StyledText = styled(Typo.Body)({
+  textAlign: 'center',
+  marginBottom: getSpacing(6),
+})
