6987c27df424544bf3a639fe3b032710ba241e5b
Hugo Voisin <hugov@bam.tech>
2022-10-07 14:58:09 +0200

    feat: navigate to ubble when having his ID card

--- src/features/identityCheck/pages/identification/identificationStart/SelectIDStatus.test.tsx
+++ src/features/identityCheck/pages/identification/identificationStart/SelectIDStatus.test.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
+import { navigate } from '__mocks__/@react-navigation/native'
 import { SelectIDStatus } from 'features/identityCheck/pages/identification/identificationStart/SelectIDStatus'
-import { render } from 'tests/utils'
+import { fireEvent, render } from 'tests/utils'
 
 describe('SelectIDStatus', () => {
   it('should render SelectIDStatus page correctly', () => {
     const renderAPI = render(<SelectIDStatus />)
     expect(renderAPI).toMatchSnapshot()
+  })
+
+  it('should navigate to ubble webview when pressing "J’ai ma pièce d’identité" button', () => {
+    const { getByText } = render(<SelectIDStatus />)
+
+    const button = getByText('J’ai ma pièce d’identité')
+    fireEvent.press(button)
+
+    expect(navigate).toHaveBeenCalledWith('UbbleWebview', undefined)
   })
 })
