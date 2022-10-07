ad9ef628825942bfb410475146524f00a7b3ca72
AnoukHello <anoukhello22@gmail.com>
2022-10-07 16:38:10 +0200

    (PC-16840) expiredID: open ants url on 'Demander un renouvellement' press

--- src/features/identityCheck/pages/identification/identificationStart/ExpiredOrLostID.native.test.tsx
+++ src/features/identityCheck/pages/identification/identificationStart/ExpiredOrLostID.native.test.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import { ExpiredOrLostID } from 'features/identityCheck/pages/identification/identificationStart/ExpiredOrLostID'
 import { navigateToHomeConfig } from 'features/navigation/helpers'
+import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
 import { navigateFromRef } from 'features/navigation/navigationRef'
 import { fireEvent, render } from 'tests/utils'
 
 jest.mock('features/navigation/navigationRef')
+const openUrl = jest.spyOn(NavigationHelpers, 'openUrl')
 
 describe('ExpiredOrLostID', () => {
   it('should render correctly', () => {
@@ -19,5 +21,13 @@ describe('ExpiredOrLostID', () => {
     const button = getByText('M’identifier plus tard')
     fireEvent.press(button)
     expect(navigateFromRef).toBeCalledWith(navigateToHomeConfig.screen, navigateToHomeConfig.params)
+  })
+
+  it('should open ants url on press "Demander un renouvellement"', async () => {
+    const { getByText } = render(<ExpiredOrLostID />)
+
+    fireEvent.press(getByText('Demander un renouvellement'))
+
+    expect(openUrl).toHaveBeenCalledWith('https://ants.gouv.fr/', undefined)
   })
 })
