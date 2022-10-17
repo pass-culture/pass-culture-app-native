f129b8ebcb9abc1c1206d2fdb4e4bf969d70e5e9
anoukhello <anoukhello22@gmail.com>
2022-10-17 10:01:42 +0200

    (PC-17895) fix(ubbleFlow): navigate explicitly to stepper after identitycheck end (#3770)
    * (PC-17895) fix(ubbleFlow): fix navigation from ubble webview to identity check end
    
    * (PC-17895) fix(ubbleFlow): navigate explicitly to stepper after identitycheck end

--- src/features/identityCheck/pages/identification/ubble/IdentityCheckEnd.tsx
+++ src/features/identityCheck/pages/identification/ubble/IdentityCheckEnd.tsx
@@ -1,18 +1,21 @@
+import { useNavigation } from '@react-navigation/native'
 import React, { useEffect } from 'react'
 
 import { useNextSubscriptionStep } from 'features/auth/signup/useNextSubscriptionStep'
-import { useIdentityCheckNavigation } from 'features/identityCheck/useIdentityCheckNavigation'
 import { navigateToHome } from 'features/navigation/helpers'
+import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { GenericInfoPage } from 'ui/pages/GenericInfoPage'
 import { EmailSent } from 'ui/svg/icons/EmailSent'
 
 export const IdentityCheckEnd = () => {
   const { data: subscription } = useNextSubscriptionStep()
-  const { navigateToNextScreen } = useIdentityCheckNavigation()
+  const { navigate } = useNavigation<UseNavigationType>()
+
+  const navigateToStepper = () => navigate('IdentityCheckStepper')
 
   useEffect(() => {
     const timeout = setTimeout(
-      !subscription?.nextSubscriptionStep ? navigateToHome : navigateToNextScreen,
+      !subscription?.nextSubscriptionStep ? navigateToHome : navigateToStepper,
       3000
     )
     return () => clearTimeout(timeout)
