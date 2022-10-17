e62b4452b8124b9bbe25912a7f05d64658c160a9
marineshaw <marines@bam.tech>
2022-10-17 15:19:28 +0200

    :sparkles: (TouchableLink) Separate apis

---
+++ src/ui/components/touchableLink/InternalTouchableLink.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react'
+
+import { TouchableLink } from 'ui/components/touchableLink/TouchableLink'
+import { TouchableLinkProps } from 'ui/components/touchableLink/types'
+
+type Props = TouchableLinkProps
+
+export const InternalTouchableLink: FunctionComponent<Props> = (props) => {
+  return <TouchableLink {...props} />
+}
