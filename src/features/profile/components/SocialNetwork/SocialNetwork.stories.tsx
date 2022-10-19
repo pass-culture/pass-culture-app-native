86514e4cb0330c0915a72495a0e27b6c78b457f8
Gabriela Bertoli <gabrielab@bam.tech>
2022-10-19 11:31:40 +0200

    feat(SocialNetwork): create associated storybook

---
+++ src/features/profile/components/SocialNetwork/SocialNetwork.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import React from 'react'
+
+import { SocialNetwork } from 'features/profile/components/SocialNetwork/SocialNetwork'
+import { theme } from 'theme'
+
+export default {
+  title: 'Features/Profile/SocialNetwork',
+  component: SocialNetwork,
+} as ComponentMeta<typeof SocialNetwork>
+
+// TODO(PC-17931): Fix those stories
+const Default: ComponentStory<typeof SocialNetwork> = () => <SocialNetwork />
+
+Default.parameters = {
+  chromatic: {
+    viewports: [theme.breakpoints.lg],
+  },
+}
