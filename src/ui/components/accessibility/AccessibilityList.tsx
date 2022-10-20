1cfdeb635f8b79b5806946b57e330bf0b23a7c13
Eve Julliard <evej@bam.tech>
2022-10-20 10:29:06 +0200

    (PC-17655) feat(AccessiblityList): create a AccessibilyList component

---
+++ src/ui/components/accessibility/AccessibilityList.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent, ReactElement, ReactNode } from 'react'
+import styled from 'styled-components/native'
+
+import { Li } from 'ui/components/Li'
+import { VerticalUl } from 'ui/components/Ul'
+
+interface AccessibilityListProps {
+  items: ReactNode[]
+  Separator: ReactElement
+}
+
+export const AccessibilityList: FunctionComponent<AccessibilityListProps> = ({
+  items,
+  Separator,
+}) => {
+  const itemListLength = items.length
+  return (
+    <StyledVerticalUl>
+      {items.map((item, index) => {
+        return (
+          <React.Fragment key={index}>
+            <Li>{item}</Li>
+            {index < itemListLength - 1 && Separator}
+          </React.Fragment>
+        )
+      })}
+    </StyledVerticalUl>
+  )
+}
+
+const StyledVerticalUl = styled(VerticalUl)({
+  width: '100%',
+})
