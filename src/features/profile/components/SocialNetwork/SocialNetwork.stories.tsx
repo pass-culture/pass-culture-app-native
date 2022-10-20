048da3ca7a1422ec9b6dc73bcf3e2db1b9c6fad2
Gabriela Bertoli <gabrielab@bam.tech>
2022-10-20 10:39:11 +0200

    :recycle: (Storybook) add breakpoints

--- src/features/profile/components/SocialNetwork/SocialNetwork.stories.tsx
+++ src/features/profile/components/SocialNetwork/SocialNetwork.stories.tsx
@@ -14,6 +14,6 @@ const Default: ComponentStory<typeof SocialNetwork> = () => <SocialNetwork />
 
 Default.parameters = {
   chromatic: {
-    viewports: [theme.breakpoints.lg],
+    viewports: [theme.breakpoints.md, theme.breakpoints.lg, theme.breakpoints.xl],
   },
 }
