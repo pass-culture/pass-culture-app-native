0321be11d560ac6d29a17c38919bf4ec6acc67ee
AnoukHello <anoukhello22@gmail.com>
2022-10-24 19:18:04 +0200

    (BSR) fix(tests): fix OfferHeader web tests

--- src/features/offer/components/__tests__/OfferHeader.web.test.tsx
+++ src/features/offer/components/__tests__/OfferHeader.web.test.tsx
@@ -9,14 +9,13 @@ import { OfferHeader } from '../OfferHeader'
 
 jest.mock('features/auth/AuthContext')
 const mockUseAuthContext = useAuthContext as jest.MockedFunction<typeof useAuthContext>
+mockUseAuthContext.mockImplementation(() => ({
+  isLoggedIn: true,
+  setIsLoggedIn: jest.fn(),
+}))
 
 describe('<OfferHeader />', () => {
-  it('should fully display the title at the end of the animation', () => {
-    mockUseAuthContext.mockImplementationOnce(() => ({
-      isLoggedIn: true,
-      setIsLoggedIn: jest.fn(),
-    }))
-
+  it('should fully display the title at the end of the animation', async () => {
     const animatedValue = new Animated.Value(0)
     const offerId = 116656
 
@@ -31,10 +30,14 @@ describe('<OfferHeader />', () => {
       )
     )
 
-    expect(getByTestId('offerHeaderName').style.opacity).toBe('0')
+    const offerHeaderName = getByTestId('offerHeaderName')
+
+    expect(offerHeaderName.style.opacity).toBe('0')
     act(() => {
       Animated.timing(animatedValue, { duration: 100, toValue: 1, useNativeDriver: false }).start()
     })
-    waitFor(() => expect(getByTestId('offerHeaderName').style.opacity).toBe('1'))
+    await waitFor(() => {
+      expect(offerHeaderName.style.opacity).toBe('1')
+    })
   })
 })
