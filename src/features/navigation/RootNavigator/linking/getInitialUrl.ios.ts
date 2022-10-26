801b24b56edf41d34e94196b03fe1b6c94ec2d39
cedricls1 <78556024+cedricls1@users.noreply.github.com>
2022-10-26 12:24:04 +0200

    (PC-18020) fix(navigation): good redirect (#3818)
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

--- src/features/navigation/RootNavigator/linking/getInitialUrl.ios.ts
+++ src/features/navigation/RootNavigator/linking/getInitialUrl.ios.ts
@@ -6,14 +6,16 @@ import { BatchPush } from 'libs/react-native-batch'
 // firebase dynamic links work on iOS :
 // See: https://medium.com/tribalscale/working-with-react-navigation-v5-firebase-cloud-messaging-and-firebase-dynamic-links-f8580c3b83ad
 export async function getInitialURL(): Promise<string> {
-  const dynamicLinkUrl = await dynamicLinks().getInitialLink()
-  if (dynamicLinkUrl) {
-    return dynamicLinkUrl.url
-  }
   // Little break for react-navigation loading
   // Issue opened to remove it : https://github.com/react-navigation/react-navigation/issues/10752
   // TODO(PC-16812): https://passculture.atlassian.net/browse/PC-16812
   await new Promise((r) => setTimeout(r, 500))
+
+  const dynamicLinkUrl = await dynamicLinks().getInitialLink()
+  if (dynamicLinkUrl) {
+    return dynamicLinkUrl.url
+  }
+
   const url = await BatchPush.getInitialURL()
   if (url) {
     return url
