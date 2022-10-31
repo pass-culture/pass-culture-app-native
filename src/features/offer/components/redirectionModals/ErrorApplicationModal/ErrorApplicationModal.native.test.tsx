ddfb1f1a339a89e271ba9a604f2e4c5e9ce359e4
marineshaw <marines@bam.tech>
2022-10-31 11:41:59 +0100

    :white_check_mark: (ErrorApplicationModal) Create navigation test

--- src/features/offer/components/redirectionModals/ErrorApplicationModal/ErrorApplicationModal.native.test.tsx
+++ src/features/offer/components/redirectionModals/ErrorApplicationModal/ErrorApplicationModal.native.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
+import { navigate } from '__mocks__/@react-navigation/native'
 import { ErrorApplicationModal } from 'features/offer/components/redirectionModals/ErrorApplicationModal/ErrorApplicationModal'
-import { render } from 'tests/utils'
+import { render, fireEvent } from 'tests/utils'
 
 const hideModal = jest.fn()
 
@@ -9,5 +10,13 @@ describe('<AuthenticationModal />', () => {
   it('should match previous snapshot', () => {
     const modal = render(<ErrorApplicationModal visible hideModal={hideModal} />)
     expect(modal).toMatchSnapshot()
+  })
+
+  it('should close modal and navigate to profile when pressing "Aller sur mon profil" button', () => {
+    const { getByLabelText } = render(<ErrorApplicationModal visible hideModal={hideModal} />)
+
+    fireEvent.press(getByLabelText('Aller vers la section profil'))
+    expect(hideModal).toBeCalledTimes(1)
+    expect(navigate).toBeCalledWith('TabNavigator', { screen: 'Profile' })
   })
 })
