a51a6d01dfada04085cfaaadcc820b7854333f02
EveJulliard <89008469+EveJulliard@users.noreply.github.com>
2022-11-01 14:05:55 +0100

    (PC-17986) feat(ThematicHomeHeader): add missing subtilte to the header (#3847)

--- src/features/home/components/headers/ThematicHomeHeader.tsx
+++ src/features/home/components/headers/ThematicHomeHeader.tsx
@@ -6,14 +6,24 @@ import { Spacer, Typo } from 'ui/theme'
 
 export interface ThematicHomeHeaderProps {
   headerTitle: string
+  headerSubtitle?: string
 }
-export const ThematicHomeHeader: FunctionComponent<ThematicHomeHeaderProps> = ({ headerTitle }) => {
+export const ThematicHomeHeader: FunctionComponent<ThematicHomeHeaderProps> = ({
+  headerTitle,
+  headerSubtitle,
+}) => {
   return (
     <View>
       <Spacer.TopScreen />
       <BackButton />
       <Spacer.Column numberOfSpaces={6} />
       <Typo.Title1 numberOfLines={1}>{headerTitle}</Typo.Title1>
+      {headerSubtitle ? (
+        <React.Fragment>
+          <Spacer.Column numberOfSpaces={2} />
+          <Typo.Body numberOfLines={1}>{headerSubtitle}</Typo.Body>
+        </React.Fragment>
+      ) : null}
     </View>
   )
 }
