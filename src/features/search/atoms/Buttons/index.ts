f0bacab36061516abff1f30adcb739f9686b773e
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-11-03 10:41:37 +0100

    (PC-18344) feat(search): remove counter button general filters page

--- src/features/search/atoms/Buttons/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './ReinitializeFilters'
-export * from './ShowResults'
--- src/features/search/pages/SearchFilter.tsx
+++ src/features/search/pages/SearchFilter.tsx
@@ -1,69 +1,63 @@
 import { useNavigation } from '@react-navigation/native'
-import React, { useCallback, useEffect, useRef } from 'react'
-import { LayoutChangeEvent, ScrollView, useWindowDimensions } from 'react-native'
+import React, { useCallback } from 'react'
+import { ScrollView } from 'react-native'
 import styled from 'styled-components/native'
 
 import { UseNavigationType } from 'features/navigation/RootNavigator'
 import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
-import { ShowResults, ReinitializeFilters } from 'features/search/atoms/Buttons'
-import { useStagedSearch } from 'features/search/pages/SearchWrapper'
+import { FilterPageButtons } from 'features/search/components/FilterPageButtons/FilterPageButtons'
+import { LocationType } from 'features/search/enums'
+import { initialSearchState } from 'features/search/pages/reducer'
+import { MAX_RADIUS } from 'features/search/pages/reducer.helpers'
+import { useSearch } from 'features/search/pages/SearchWrapper'
 import Section from 'features/search/sections'
+import { SearchView } from 'features/search/types'
+import { analytics } from 'libs/firebase/analytics'
+import { useGeolocation } from 'libs/geolocation'
+import { useFunctionOnce } from 'libs/hooks'
 import { PageHeader } from 'ui/components/headers/PageHeader'
 import { Li } from 'ui/components/Li'
 import { VerticalUl } from 'ui/components/Ul'
 import { getSpacing, Spacer } from 'ui/theme'
 
-const useScrollToEndOnTimeOrDateActivation = () => {
-  const windowHeight = useWindowDimensions().height
-  const { searchState } = useStagedSearch()
-  const scrollViewRef = useRef<ScrollView | null>(null)
-  const shouldScrollRef = useRef<boolean>(false)
-
-  useEffect(() => {
-    shouldScrollRef.current = true
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [!searchState.date])
-
-  useEffect(() => {
-    shouldScrollRef.current = true
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [!searchState.timeRange])
-
-  const scrollToEnd = (event: LayoutChangeEvent) => {
-    const { y } = event.nativeEvent.layout
-    if (scrollViewRef.current !== null && shouldScrollRef.current) {
-      scrollViewRef.current.scrollTo({ y: y - (4 * windowHeight) / 5 })
-      shouldScrollRef.current = false
-    }
-  }
-
-  return { scrollViewRef, scrollToEnd }
-}
-
 export const SearchFilter: React.FC = () => {
-  const { searchState } = useStagedSearch()
-  const { scrollViewRef } = useScrollToEndOnTimeOrDateActivation()
+  const { searchState, dispatch } = useSearch()
   const { navigate } = useNavigation<UseNavigationType>()
+  const logReinitializeFilters = useFunctionOnce(() => {
+    analytics.logReinitializeFilters()
+  })
+  const { position } = useGeolocation()
 
   const onGoBack = useCallback(() => {
     navigate(
       ...getTabNavConfig('Search', {
         ...searchState,
+        view: SearchView.Results,
       })
     )
   }, [navigate, searchState])
 
+  const onResetPress = useCallback(() => {
+    dispatch({
+      type: 'SET_STATE',
+      payload: {
+        ...initialSearchState,
+        locationFilter:
+          position !== null
+            ? { locationType: LocationType.AROUND_ME, aroundRadius: MAX_RADIUS }
+            : { locationType: LocationType.EVERYWHERE },
+        minPrice: undefined,
+        maxPrice: undefined,
+      },
+    })
+    logReinitializeFilters()
+  }, [dispatch, logReinitializeFilters, position])
+
   return (
     <Container>
-      <PageHeader
-        title="Filtrer"
-        RightComponent={ReinitializeFilters}
-        background="primary"
-        withGoBackButton
-        onGoBack={onGoBack}
-      />
+      <PageHeader title="Filtrer" background="primary" withGoBackButton onGoBack={onGoBack} />
       <React.Fragment>
-        <StyledScrollView ref={scrollViewRef} scrollEnabled>
+        <StyledScrollView scrollEnabled>
           {/* Localisation */}
           <VerticalUl>
             <StyledLi>
@@ -104,10 +98,8 @@ export const SearchFilter: React.FC = () => {
         </StyledScrollView>
       </React.Fragment>
 
-      <ShowResultsContainer>
-        <ShowResults />
-        <Spacer.BottomScreen />
-      </ShowResultsContainer>
+      <FilterPageButtons onResetPress={onResetPress} onSearchPress={onGoBack} isModal={false} />
+      <Spacer.BottomScreen />
     </Container>
   )
 }
@@ -128,14 +120,6 @@ const Separator = styled.View<{ marginVertical?: number }>(({ theme, marginVerti
   alignSelf: 'center',
   marginVertical: marginVertical,
 }))
-
-const ShowResultsContainer = styled.View({
-  width: '100%',
-  position: 'absolute',
-  bottom: getSpacing(6),
-  paddingHorizontal: getSpacing(6),
-  alignItems: 'center',
-})
 
 const StyledLi = styled(Li)({
   display: 'flex',
