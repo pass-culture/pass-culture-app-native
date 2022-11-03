e3a03eb616edaa78f9c62b248bda7fb86e6f1523
cedricls1 <78556024+cedricls1@users.noreply.github.com>
2022-11-03 12:07:38 +0100

    (PC-18349) docs(navigation): improve comment (#3857)
    timeout
    
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

--- src/features/navigation/RootNavigator/linking/getInitialUrl.ios.ts
+++ src/features/navigation/RootNavigator/linking/getInitialUrl.ios.ts
@@ -6,7 +6,9 @@ import { BatchPush } from 'libs/react-native-batch'
 // firebase dynamic links work on iOS :
 // See: https://medium.com/tribalscale/working-with-react-navigation-v5-firebase-cloud-messaging-and-firebase-dynamic-links-f8580c3b83ad
 export async function getInitialURL(): Promise<string> {
-  // Little break for react-navigation loading
+  // When opening the app through an external link (without the app being in the background),
+  // the routes are not ready and the user is systematically redirected to the homepage.
+  // The setTimeout allows react-navigation to load properly and redirect the user to the right page
   // Issue opened to remove it : https://github.com/react-navigation/react-navigation/issues/10752
   // TODO(PC-16812): https://passculture.atlassian.net/browse/PC-16812
   await new Promise((r) => setTimeout(r, 500))
