da63ec2e8a48599feb5012a6d2f5ef9e0356a163
marineshaw <marines@bam.tech>
2022-11-04 14:45:55 +0100

    :white_check_mark: (AddToFavoriteButton) add test on add favorite behaviour

---
+++ src/features/offer/components/redirectionModals/components/AddToFavoritesButton.native.test.tsx
@@ -0,0 +1,121 @@
+import { rest } from 'msw'
+import React from 'react'
+
+import { FavoriteResponse } from 'api/gen'
+import { useAuthContext } from 'features/auth/AuthContext'
+import {
+  paginatedFavoritesResponseSnap,
+  addFavoriteJsonResponseSnap,
+} from 'features/favorites/fixtures/favoritesResponse'
+import { AddToFavoritesButton } from 'features/offer/components/redirectionModals/components/AddToFavoritesButton'
+import { env } from 'libs/environment'
+import { EmptyResponse } from 'libs/fetch'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { server } from 'tests/server'
+import { fireEvent, waitFor, render } from 'tests/utils'
+import {
+  showSuccessSnackBar,
+  showErrorSnackBar,
+  hideSnackBar,
+  showInfoSnackBar,
+} from 'ui/components/snackBar/__mocks__/SnackBarContext'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
+
+jest.mock('features/auth/AuthContext')
+const mockUseAuthContext = useAuthContext as jest.MockedFunction<typeof useAuthContext>
+mockUseAuthContext.mockReturnValue({
+  isLoggedIn: true,
+  setIsLoggedIn: jest.fn(),
+})
+
+jest.mock('ui/components/snackBar/SnackBarContext', () => ({
+  useSnackBarContext: jest.fn(() => ({})),
+}))
+
+const mockPostFavorite = jest.fn()
+
+const mockedUseSnackBarContext = useSnackBarContext as jest.MockedFunction<
+  typeof useSnackBarContext
+>
+
+mockedUseSnackBarContext.mockReturnValue({
+  hideSnackBar,
+  showInfoSnackBar,
+  showSuccessSnackBar,
+  showErrorSnackBar,
+})
+
+describe('<AddToFavoriteButton />', () => {
+  it('should render nothing when offer already in favorite', async () => {
+    const favoriteOfferId = 146193
+    const { queryByText } = renderButton({ offerId: favoriteOfferId })
+
+    await waitFor(() => {
+      expect(queryByText('Mettre en favori')).toBeNull()
+    })
+  })
+
+  it('should add favorite', async () => {
+    const { getByText } = renderButton({
+      offerId: addFavoriteJsonResponseSnap.offer.id,
+    })
+
+    fireEvent.press(getByText('Mettre en favori'))
+
+    await waitFor(() => {
+      expect(mockPostFavorite).toHaveBeenCalled()
+    })
+  })
+
+  it('should show error snackbar when add favorite fails', async () => {
+    const { getByText } = renderButton({
+      offerId: addFavoriteJsonResponseSnap.offer.id,
+      hasAddFavoriteError: true,
+    })
+
+    fireEvent.press(getByText('Mettre en favori'))
+
+    await waitFor(() => {
+      expect(showErrorSnackBar).toHaveBeenCalledWith({
+        message: 'L’offre n’a pas été ajoutée à tes favoris',
+        timeout: SNACK_BAR_TIME_OUT,
+      })
+    })
+  })
+})
+
+type Options = {
+  offerId?: number
+  hasAddFavoriteError?: boolean
+  hasTooManyFavorites?: boolean
+}
+
+const defaultOptions = {
+  offerId: 116656,
+  hasAddFavoriteError: false,
+  hasTooManyFavorites: false,
+}
+
+const renderButton = (options?: Options) => {
+  const { offerId, hasAddFavoriteError } = {
+    ...defaultOptions,
+    ...options,
+  }
+
+  server.use(
+    rest.get<Array<FavoriteResponse>>(
+      `${env.API_BASE_URL}/native/v1/me/favorites`,
+      (_req, res, ctx) => res(ctx.status(200), ctx.json(paginatedFavoritesResponseSnap))
+    ),
+    rest.post<EmptyResponse>(`${env.API_BASE_URL}/native/v1/me/favorites`, (_req, res, ctx) => {
+      if (hasAddFavoriteError) {
+        return res(ctx.status(415), ctx.json({}))
+      } else {
+        mockPostFavorite()
+        return res(ctx.status(200), ctx.json(addFavoriteJsonResponseSnap))
+      }
+    })
+  )
+  // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+  return render(reactQueryProviderHOC(<AddToFavoritesButton offerId={offerId} />))
+}
