3a034f9567277a4b1bfcdecf445475ee51815a72
AnoukHello <anoukhello22@gmail.com>
2022-11-04 16:02:55 +0100

    (PC-17989) ThematicHomeHeader: navigate explicitly to home page on go back press

--- src/features/home/components/headers/ThematicHomeHeader.tsx
+++ src/features/home/components/headers/ThematicHomeHeader.tsx
@@ -1,6 +1,9 @@
-import React, { FunctionComponent } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import React, { FunctionComponent, useCallback } from 'react'
 import styled from 'styled-components/native'
 
+import { UseNavigationType } from 'features/navigation/RootNavigator/types'
+import { homeNavConfig } from 'features/navigation/TabBar/helpers'
 import { BackButton } from 'ui/components/headers/BackButton'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 
@@ -12,10 +15,13 @@ export const ThematicHomeHeader: FunctionComponent<ThematicHomeHeaderProps> = ({
   headerTitle,
   headerSubtitle,
 }) => {
+  const { navigate } = useNavigation<UseNavigationType>()
+  const onGoBack = useCallback(() => navigate(...homeNavConfig), [navigate])
+
   return (
     <Container>
       <Spacer.TopScreen />
-      <BackButton />
+      <BackButton onGoBack={onGoBack} />
       <Spacer.Column numberOfSpaces={6} />
       <Typo.Title1 numberOfLines={1}>{headerTitle}</Typo.Title1>
       {headerSubtitle ? (
