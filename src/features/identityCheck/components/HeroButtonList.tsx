5467966429faa4024c1bc4bd92ad0e292fd22bea
Hugo Voisin <hugov@bam.tech>
2022-11-07 12:08:37 +0100

    refactor: use generic component

--- src/features/identityCheck/components/HeroButtonList.tsx
+++ src/features/identityCheck/components/HeroButtonList.tsx
@@ -1,10 +1,9 @@
 import React, { FunctionComponent, ReactElement } from 'react'
-import { View } from 'react-native'
 import styled from 'styled-components/native'
 
+import { GenericBanner } from 'ui/components/ModuleBanner/GenericBanner'
 import { TouchableLink } from 'ui/components/touchableLink/TouchableLink'
 import { TouchableLinkProps } from 'ui/components/touchableLink/types'
-import { ArrowNext } from 'ui/svg/icons/ArrowNext'
 import { IconInterface } from 'ui/svg/icons/types'
 import { getSpacing } from 'ui/theme'
 
@@ -22,45 +21,22 @@ export const HeroButtonList: FunctionComponent<HeroButtonListProps> = (props) =>
   }))``
 
   return (
-    <ChoiceContainer navigateTo={props.navigateTo} testID={`HeroButtonList`}>
-      <IconContainer>
-        <Icon />
-      </IconContainer>
-      <DescriptionContainer>
-        {props.Title}
-        {!!props.Subtitle && <SubtitleContainer>{props.Subtitle}</SubtitleContainer>}
-      </DescriptionContainer>
-      <View>
-        <StyledArrowNextIcon />
-      </View>
-    </ChoiceContainer>
+    <TouchableLink navigateTo={props.navigateTo} testID={`HeroButtonList`}>
+      <GenericBanner LeftIcon={Icon}>
+        <TextWrapper>
+          {props.Title}
+          {!!props.Subtitle && <SubtitleContainer>{props.Subtitle}</SubtitleContainer>}
+        </TextWrapper>
+      </GenericBanner>
+    </TouchableLink>
   )
 }
 
-const StyledArrowNextIcon = styled(ArrowNext).attrs(({ theme }) => ({
-  size: theme.icons.sizes.smaller,
-}))``
-
-const ChoiceContainer = styled(TouchableLink)(({ theme }) => ({
-  flexDirection: 'row',
-  justifyContent: 'flex-start',
-  alignItems: 'center',
-  border: getSpacing(0.25),
-  borderRadius: getSpacing(1.8),
-  borderColor: theme.colors.greySemiDark,
-  minHeight: getSpacing(23),
-  paddingHorizontal: getSpacing(4),
-  padding: getSpacing(4),
-}))
-
-const DescriptionContainer = styled.View({
-  flex: 1,
-  marginHorizontal: getSpacing(4),
+const TextWrapper = styled.View({
+  minHeight: getSpacing(14.5),
+  justifyContent: 'center',
 })
+
 const SubtitleContainer = styled.View({
   marginTop: getSpacing(1),
-})
-
-const IconContainer = styled.View({
-  alignContent: 'center',
 })
