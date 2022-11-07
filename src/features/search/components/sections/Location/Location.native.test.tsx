66e28f551dabae1da83068eb36b45ab7f9dbf941
AnoukHello <anoukhello22@gmail.com>
2022-11-07 10:33:50 +0100

    (BSR) location test: replace superflushwithact with await waitFor

--- src/features/search/components/sections/Location/Location.native.test.tsx
+++ src/features/search/components/sections/Location/Location.native.test.tsx
@@ -4,7 +4,7 @@ import { Location } from 'features/search/components/sections/Location/Location'
 import { initialSearchState } from 'features/search/context/reducer'
 import { LocationType, RadioButtonLocation } from 'features/search/enums'
 import { GeoCoordinates } from 'libs/geolocation'
-import { render, fireEvent, superFlushWithAct } from 'tests/utils'
+import { render, fireEvent, waitFor } from 'tests/utils'
 import * as useModalAPI from 'ui/components/modals/useModal'
 
 const mockSearchState = jest.fn().mockReturnValue({
@@ -31,18 +31,24 @@ describe('Location component', () => {
   })
 
   it(`should have EVERYWHERE description's by default`, async () => {
-    const { getByText, queryByText } = await renderLocation()
-    expect(getByText(RadioButtonLocation.EVERYWHERE)).toBeTruthy()
-    expect(queryByText(RadioButtonLocation.AROUND_ME)).toBeFalsy()
-    expect(queryByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeFalsy()
+    const { getByText, queryByText } = render(<Location />)
+
+    await waitFor(() => {
+      expect(getByText(RadioButtonLocation.EVERYWHERE)).toBeTruthy()
+      expect(queryByText(RadioButtonLocation.AROUND_ME)).toBeFalsy()
+      expect(queryByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeFalsy()
+    })
   })
 
   it(`should not have description when EVERYWHERE selected and position is null`, async () => {
     mockPosition = null
-    const { queryByText } = await renderLocation()
-    expect(queryByText(RadioButtonLocation.EVERYWHERE)).toBeFalsy()
-    expect(queryByText(RadioButtonLocation.AROUND_ME)).toBeFalsy()
-    expect(queryByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeFalsy()
+    const { queryByText } = render(<Location />)
+
+    await waitFor(() => {
+      expect(queryByText(RadioButtonLocation.EVERYWHERE)).toBeFalsy()
+      expect(queryByText(RadioButtonLocation.AROUND_ME)).toBeFalsy()
+      expect(queryByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeFalsy()
+    })
   })
 
   it(`should have AROUND_ME description's when selected`, async () => {
@@ -51,10 +57,13 @@ describe('Location component', () => {
         locationFilter: { locationType: LocationType.AROUND_ME, aroundRadius: null },
       },
     })
-    const { getByText, queryByText } = await renderLocation()
-    expect(queryByText(RadioButtonLocation.EVERYWHERE)).toBeFalsy()
-    expect(getByText(RadioButtonLocation.AROUND_ME)).toBeTruthy()
-    expect(queryByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeFalsy()
+    const { getByText, queryByText } = render(<Location />)
+
+    await waitFor(() => {
+      expect(queryByText(RadioButtonLocation.EVERYWHERE)).toBeFalsy()
+      expect(getByText(RadioButtonLocation.AROUND_ME)).toBeTruthy()
+      expect(queryByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeFalsy()
+    })
   })
 
   it(`should have VENUE description's when selected`, async () => {
@@ -63,10 +72,13 @@ describe('Location component', () => {
         locationFilter: { locationType: LocationType.VENUE },
       },
     })
-    const { getByText, queryByText } = await renderLocation()
-    expect(queryByText(RadioButtonLocation.EVERYWHERE)).toBeFalsy()
-    expect(queryByText(RadioButtonLocation.AROUND_ME)).toBeFalsy()
-    expect(getByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeTruthy()
+    const { getByText, queryByText } = render(<Location />)
+
+    await waitFor(() => {
+      expect(queryByText(RadioButtonLocation.EVERYWHERE)).toBeFalsy()
+      expect(queryByText(RadioButtonLocation.AROUND_ME)).toBeFalsy()
+      expect(getByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeTruthy()
+    })
   })
 
   it(`should have PLACE description's when selected`, async () => {
@@ -75,10 +87,13 @@ describe('Location component', () => {
         locationFilter: { locationType: LocationType.PLACE },
       },
     })
-    const { getByText, queryByText } = await renderLocation()
-    expect(queryByText(RadioButtonLocation.EVERYWHERE)).toBeFalsy()
-    expect(queryByText(RadioButtonLocation.AROUND_ME)).toBeFalsy()
-    expect(getByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeTruthy()
+    const { getByText, queryByText } = render(<Location />)
+
+    await waitFor(() => {
+      expect(queryByText(RadioButtonLocation.EVERYWHERE)).toBeFalsy()
+      expect(queryByText(RadioButtonLocation.AROUND_ME)).toBeFalsy()
+      expect(getByText(RadioButtonLocation.CHOOSE_PLACE_OR_VENUE)).toBeTruthy()
+    })
   })
 
   it('should open modal when clicked', async () => {
@@ -89,14 +104,12 @@ describe('Location component', () => {
       hideModal: jest.fn(),
       toggleModal: jest.fn(),
     })
-    const { getAllByText } = await renderLocation()
+    const { getAllByText } = render(<Location />)
+
     fireEvent.press(getAllByText(RadioButtonLocation.EVERYWHERE)[0])
-    expect(mockShowModal).toBeCalledTimes(1)
+
+    await waitFor(() => {
+      expect(mockShowModal).toBeCalledTimes(1)
+    })
   })
 })
-
-async function renderLocation() {
-  const renderAPI = render(<Location />)
-  await superFlushWithAct()
-  return renderAPI
-}
