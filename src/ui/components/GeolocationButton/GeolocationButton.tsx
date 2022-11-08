50e3f606d1a63e3809d24c478bf1d5bd6878c25d
Hugo Voisin <hugo.voisin@ensta-paristech.fr>
2022-11-08 14:25:43 +0100

    (PC-18275) refactor(banners): extract design in a generic component (#3873)
    * refactor: extract design in a generic component
    
    * refactor: use generic component
    
    * types: fix typing error
    due to issues in TouchableLink typing
    
    * test: update snapshots

--- src/ui/components/GeolocationButton/GeolocationButton.tsx
+++ src/ui/components/GeolocationButton/GeolocationButton.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
-import { styledButton } from 'ui/components/buttons/styledButton'
+import { GenericBanner } from 'ui/components/ModuleBanner/GenericBanner'
 import { Touchable } from 'ui/components/touchable/Touchable'
-import { ArrowNext } from 'ui/svg/icons/ArrowNext'
 import { BicolorEverywhere as Everywhere } from 'ui/svg/icons/BicolorEverywhere'
-import { getSpacing, Spacer, Typo } from 'ui/theme'
+import { Spacer, Typo } from 'ui/theme'
 
 type Props = {
   onPress: () => void
@@ -13,51 +12,15 @@ type Props = {
 
 export const GeolocationButton = ({ onPress }: Props) => {
   return (
-    <StyledTouchable onPress={onPress}>
-      <SubContainer>
-        <IconWrapper>
-          <LocationIcon />
-        </IconWrapper>
-        <Spacer.Row numberOfSpaces={2} />
-        <TextWrapper>
-          <TitleText>Géolocalise toi</TitleText>
-          <Spacer.Column numberOfSpaces={1} />
-          <DescriptionText numberOfLines={2}>
-            Pour trouver des offres autour de toi.
-          </DescriptionText>
-        </TextWrapper>
-      </SubContainer>
-      <Spacer.Row numberOfSpaces={2} />
-      <IconWrapper>
-        <ArrowNextIcon />
-      </IconWrapper>
-    </StyledTouchable>
+    <Touchable onPress={onPress} accessibilityLabel="Active ta géolocalisation">
+      <GenericBanner LeftIcon={LocationIcon}>
+        <TitleText>Géolocalise toi</TitleText>
+        <Spacer.Column numberOfSpaces={1} />
+        <DescriptionText numberOfLines={2}>Pour trouver des offres autour de toi.</DescriptionText>
+      </GenericBanner>
+    </Touchable>
   )
 }
-
-const StyledTouchable = styledButton(Touchable)(({ theme }) => ({
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  padding: getSpacing(4),
-  border: `1px solid ${theme.colors.greySemiDark}`,
-  borderRadius: theme.borderRadius.radius,
-  backgroundColor: theme.colors.white,
-}))
-
-const SubContainer = styled.View({
-  flex: 1,
-  flexDirection: 'row',
-  alignItems: 'center',
-})
-
-const IconWrapper = styled.View({
-  flexShrink: 0,
-})
-
-const TextWrapper = styled.View({
-  flexShrink: 1,
-})
 
 const TitleText = styled(Typo.ButtonText)({
   textAlign: 'left',
@@ -69,8 +32,4 @@ const DescriptionText = styled(Typo.Caption)({
 
 const LocationIcon = styled(Everywhere).attrs(({ theme }) => ({
   size: theme.icons.sizes.standard,
-}))``
-
-const ArrowNextIcon = styled(ArrowNext).attrs(({ theme }) => ({
-  size: theme.icons.sizes.small,
 }))``
