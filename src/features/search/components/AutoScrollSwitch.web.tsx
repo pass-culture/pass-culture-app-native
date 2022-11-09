d6104648eb8ea0c4a1c74473746f033c3dd6abec
Lucas Beneston <lucas.beneston@passculture.app>
2022-11-09 09:43:23 +0100

    (PC-18576) feat(accessibility): move toggle before title for AutoScrollSwitch

--- src/features/search/components/AutoScrollSwitch.web.tsx
+++ src/features/search/components/AutoScrollSwitch.web.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import { v4 as uuidv4 } from 'uuid'
 
-import { SectionWithSwitch } from 'features/profile/components/SectionWithSwitch/SectionWithSwitch'
-import { getSpacing } from 'ui/theme'
+import FilterSwitch from 'ui/components/FilterSwitch'
+import { InputLabel } from 'ui/components/InputLabel/InputLabel'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
 
 export const AutoScrollSwitch = ({
   title,
@@ -12,20 +14,38 @@ export const AutoScrollSwitch = ({
   title: string
   active: boolean
   toggle: () => void
-}) => (
-  <Container>
-    <SectionWithSwitch title={title} active={active} toggle={toggle} />
-  </Container>
-)
+}) => {
+  const checkboxID = uuidv4()
+
+  return (
+    <Container>
+      <FlexContainer>
+        <FilterSwitch active={active} toggle={toggle} checkboxID={checkboxID} />
+        <Spacer.Row numberOfSpaces={5} />
+        <InputLabel htmlFor={checkboxID}>
+          <Typo.ButtonText>{title}</Typo.ButtonText>
+        </InputLabel>
+        <Spacer.Column numberOfSpaces={2} />
+      </FlexContainer>
+    </Container>
+  )
+}
 
 const Container = styled.div({
-  width: '1px',
-  height: '1px',
   overflow: 'hidden',
   marginLeft: getSpacing(6),
   marginRight: getSpacing(6),
+  width: '1px',
+  height: '1px',
   '&:focus-within': {
     width: 'auto',
     height: 'auto',
   },
+})
+
+const FlexContainer = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginBottom: getSpacing(3),
 })
