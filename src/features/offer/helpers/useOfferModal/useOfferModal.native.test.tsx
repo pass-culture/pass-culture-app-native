a8ef575c7b1f5269bec06318a476be9fa3ef73ac
marineshaw <marines@bam.tech>
2022-11-22 11:03:32 +0100

    :sparkles: (Offer) Create hook useOfferModal to get right modal in offer

---
+++ src/features/offer/helpers/useOfferModal/useOfferModal.native.test.tsx
@@ -0,0 +1,78 @@
+import React, { FunctionComponent } from 'react'
+import { Text } from 'react-native'
+
+import { OfferModal } from 'features/offer/enums'
+import { OfferModalProps, useOfferModal } from 'features/offer/helpers/useOfferModal/useOfferModal'
+import { renderHook, render } from 'tests/utils'
+import { LINE_BREAK } from 'ui/theme/constants'
+
+jest.mock('react-query')
+
+const mockBookingOfferModal = <Text>BookingOfferModal</Text>
+jest.mock('features/bookOffer/pages/BookingOfferModal', () => ({
+  BookingOfferModal: () => mockBookingOfferModal,
+}))
+
+const mockHideModal = jest.fn()
+jest.mock('ui/components/modals/useModal.ts', () => ({
+  useModal: jest.fn().mockReturnValue({
+    visible: true,
+    showModal: jest.fn(),
+    hideModal: mockHideModal,
+  }),
+}))
+
+const TestOfferModal: FunctionComponent<OfferModalProps> = ({
+  modalToDisplay,
+  offerId,
+  isEndedUsedBooking,
+}) => {
+  const { OfferModal } = useOfferModal({
+    offerId,
+    isEndedUsedBooking,
+    modalToDisplay,
+  })
+  return OfferModal
+}
+
+describe('useOfferModal', () => {
+  it('do not display anything when there is no modal to display', () => {
+    const { result } = renderHook(() => useOfferModal({ offerId: 1000 }))
+    expect(result.current.OfferModal).toBeNull()
+  })
+
+  it('should return application processing modal when asked', () => {
+    const { getByText } = render(
+      <TestOfferModal modalToDisplay={OfferModal.APPLICATION_PROCESSING} offerId={1000} />
+    )
+    expect(getByText("C'est pour bientôt\u00a0!")).toBeTruthy()
+  })
+
+  it('should return authentication modal when asked', () => {
+    const { getByText } = render(
+      <TestOfferModal modalToDisplay={OfferModal.AUTHENTICATION} offerId={1000} />
+    )
+    expect(getByText('Identifie-toi' + LINE_BREAK + 'pour réserver l’offre')).toBeTruthy()
+  })
+
+  it('should return booking modal when asked', () => {
+    const { getByText } = render(
+      <TestOfferModal modalToDisplay={OfferModal.BOOKING} offerId={1000} />
+    )
+    expect(getByText('BookingOfferModal')).toBeTruthy()
+  })
+
+  it('should return error application modal when asked', () => {
+    const { getByText } = render(
+      <TestOfferModal modalToDisplay={OfferModal.ERROR_APPLICATION} offerId={1000} />
+    )
+    expect(getByText('Tu n’as pas encore obtenu' + LINE_BREAK + 'ton crédit')).toBeTruthy()
+  })
+
+  it('should return finish subscription modal when asked', () => {
+    const { getByText } = render(
+      <TestOfferModal modalToDisplay={OfferModal.FINISH_SUBSCRIPTION} offerId={1000} />
+    )
+    expect(getByText('Débloque ton crédit' + LINE_BREAK + 'pour réserver cette offre')).toBeTruthy()
+  })
+})
---
+++ src/features/offer/helpers/useOfferModal/useOfferModal.tsx
@@ -0,0 +1,78 @@
+import React, { ReactElement } from 'react'
+
+import { BookingOfferModal } from 'features/bookOffer/pages/BookingOfferModal'
+import { ApplicationProcessingModal } from 'features/offer/components/ApplicationProcessingModal/ApplicationProcessingModal'
+import { AuthenticationModal } from 'features/offer/components/AuthenticationModal/AuthenticationModal'
+import { ErrorApplicationModal } from 'features/offer/components/ErrorApplicationModal/ErrorApplicationModal'
+import { FinishSubscriptionModal } from 'features/offer/components/FinishSubscriptionModal/FinishSubscriptionModal'
+import { OfferModal } from 'features/offer/enums'
+import { useModal } from 'ui/components/modals/useModal'
+
+export type OfferModalProps = {
+  modalToDisplay?: OfferModal
+  offerId: number
+  isEndedUsedBooking?: boolean
+}
+
+type Output = {
+  OfferModal: ReactElement | null
+  showModal?: () => void
+  dismissBookingOfferModal?: () => void
+}
+
+export const useOfferModal = ({
+  modalToDisplay,
+  offerId,
+  isEndedUsedBooking,
+}: OfferModalProps): Output => {
+  const { visible, showModal, hideModal } = useModal(false)
+
+  switch (modalToDisplay) {
+    case OfferModal.APPLICATION_PROCESSING:
+      return {
+        OfferModal: (
+          <ApplicationProcessingModal visible={visible} hideModal={hideModal} offerId={offerId} />
+        ),
+        showModal,
+      }
+
+    case OfferModal.AUTHENTICATION:
+      return {
+        OfferModal: (
+          <AuthenticationModal visible={visible} hideModal={hideModal} offerId={offerId} />
+        ),
+        showModal,
+      }
+
+    case OfferModal.BOOKING:
+      return {
+        OfferModal: (
+          <BookingOfferModal
+            visible={visible}
+            dismissModal={hideModal}
+            offerId={offerId}
+            isEndedUsedBooking={isEndedUsedBooking}
+          />
+        ),
+        showModal,
+        dismissBookingOfferModal: hideModal,
+      }
+
+    case OfferModal.ERROR_APPLICATION:
+      return {
+        OfferModal: (
+          <ErrorApplicationModal visible={visible} hideModal={hideModal} offerId={offerId} />
+        ),
+        showModal,
+      }
+
+    case OfferModal.FINISH_SUBSCRIPTION:
+      return {
+        OfferModal: <FinishSubscriptionModal visible={visible} hideModal={hideModal} />,
+        showModal,
+      }
+
+    default:
+      return { OfferModal: null }
+  }
+}
