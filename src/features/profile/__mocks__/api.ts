2e2b70afcc25675a281aa80a102b6ef99a8440ae
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-11-23 12:21:44 +0100

    (PC-17103) Remove useUserProfileInfo query from profile utils

--- src/features/profile/__mocks__/api.ts
+++ src/features/profile/__mocks__/api.ts
@@ -1,11 +1,5 @@
-import { beneficiaryUser } from 'fixtures/user'
-
 export enum CHANGE_EMAIL_ERROR_CODE {
   TOKEN_EXISTS = 'TOKEN_EXISTS',
   INVALID_PASSWORD = 'INVALID_PASSWORD',
   EMAIL_UPDATE_ATTEMPTS_LIMIT = 'EMAIL_UPDATE_ATTEMPTS_LIMIT',
 }
-
-export const useUserProfileInfo = jest.fn().mockReturnValue({
-  data: beneficiaryUser,
-})
