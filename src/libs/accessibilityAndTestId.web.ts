be1947c72cf85dd519825b25c48089d526f1c738
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-11-23 22:29:27 +0200

    add comment on accessbilityAndTestId

--- src/libs/accessibilityAndTestId.web.ts
+++ src/libs/accessibilityAndTestId.web.ts
@@ -1,3 +1,7 @@
+/** RN is having a bug that make e2e testing complicated, this is why we use accessibilityLabel as testID first, otherwise wording
+ * Don't change that
+ * https://stackoverflow.com/questions/64706072/how-to-set-the-testid-and-the-accessibilitylabel-together-with-react-native
+ */
 export function accessibilityAndTestId(accessibilityLabel?: string, testID?: string) {
   return {
     accessible: true,
