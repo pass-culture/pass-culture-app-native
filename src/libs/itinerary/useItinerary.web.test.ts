b2e0beb3f3960352b65224e2bf0e8afe6a4d3895
AnoukHello <anoukhello22@gmail.com>
2022-11-24 18:21:33 +0100

    (BSR) itinerary test: replace superflushwithact with await waitFor

--- src/libs/itinerary/useItinerary.web.test.ts
+++ src/libs/itinerary/useItinerary.web.test.ts
@@ -1,8 +1,6 @@
-import waitForExpect from 'wait-for-expect'
-
 import { openGoogleMapsItinerary } from 'libs/itinerary/openGoogleMapsItinerary'
 import { useItinerary } from 'libs/itinerary/useItinerary'
-import { renderHook, superFlushWithAct } from 'tests/utils'
+import { renderHook, waitFor } from 'tests/utils'
 import { SnackBarHelperSettings } from 'ui/components/snackBar/types'
 
 jest.mock('libs/itinerary/openGoogleMapsItinerary')
@@ -20,9 +18,8 @@ const address = '48.85837, 2.294481'
 describe('useItinerary', () => {
   it('should open itinerary in google maps web', async () => {
     const { result } = renderHook(useItinerary)
-    await superFlushWithAct()
     result.current.navigateTo(address)
-    await waitForExpect(() => {
+    await waitFor(() => {
       expect(openGoogleMapsItinerary).toBeCalledWith(address)
     })
   })
