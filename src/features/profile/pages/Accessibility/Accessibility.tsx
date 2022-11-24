ce675a2e798fe9e28992adb5ecdd130d60af359e
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2022-11-24 11:46:07 +0100

    (PC-18726) feat(onboarding): Add credit status tag component (#3953)
    * (PC-18726) feat(onboarding): Add credit status tag component
    
    * (PC-18787) fix(accessibility): Use list for sections

--- src/features/profile/pages/Accessibility/Accessibility.tsx
+++ src/features/profile/pages/Accessibility/Accessibility.tsx
@@ -2,36 +2,42 @@ import React from 'react'
 import styled from 'styled-components/native'
 
 import { PageProfileSection } from 'features/profile/pages/PageProfileSection/PageProfileSection'
+import { AccessibilityList } from 'ui/components/accessibility/AccessibilityList'
 import { SectionRow } from 'ui/components/SectionRow'
 import { Separator } from 'ui/components/Separator'
 import { getSpacing } from 'ui/theme'
 
+const sections = [
+  <SectionRow
+    key={1}
+    title="Les engagements du pass Culture"
+    type="navigable"
+    navigateTo={{ screen: 'AccessibilityEngagement' }}
+  />,
+  <SectionRow
+    key={2}
+    title="Parcours recommandés"
+    type="navigable"
+    navigateTo={{ screen: 'RecommendedPaths' }}
+  />,
+  <SectionRow
+    key={3}
+    title="Déclaration d’accessibilité"
+    type="navigable"
+    navigateTo={{ screen: 'AccessibilityDeclaration' }}
+  />,
+  <SectionRow
+    key={4}
+    title="Schéma pluriannuel"
+    type="navigable"
+    navigateTo={{ screen: 'AccessibilityActionPlan' }}
+  />,
+]
+
 export function Accessibility() {
   return (
     <PageProfileSection title="Accessibilité">
-      <SectionRow
-        title="Les engagements du pass Culture"
-        type="navigable"
-        navigateTo={{ screen: 'AccessibilityEngagement' }}
-      />
-      <StyledSeparator />
-      <SectionRow
-        title="Parcours recommandés"
-        type="navigable"
-        navigateTo={{ screen: 'RecommendedPaths' }}
-      />
-      <StyledSeparator />
-      <SectionRow
-        title="Déclaration d’accessibilité"
-        type="navigable"
-        navigateTo={{ screen: 'AccessibilityDeclaration' }}
-      />
-      <StyledSeparator />
-      <SectionRow
-        title="Schéma pluriannuel"
-        type="navigable"
-        navigateTo={{ screen: 'AccessibilityActionPlan' }}
-      />
+      <AccessibilityList items={sections} Separator={<StyledSeparator />} />
     </PageProfileSection>
   )
 }
