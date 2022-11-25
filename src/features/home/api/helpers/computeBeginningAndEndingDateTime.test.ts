acd77d3721654e16fea96f6c3c075cddd6863495
Eve Julliard <evej@bam.tech>
2022-11-25 15:37:17 +0100

    (PC-18592) feat(computeBeginningAndEndingDatetime): extract dates into constants to increase test readability

--- src/features/home/api/helpers/computeBeginningAndEndingDateTime.test.ts
+++ src/features/home/api/helpers/computeBeginningAndEndingDateTime.test.ts
@@ -2,20 +2,24 @@ import mockdate from 'mockdate'
 
 import { computeBeginningAndEndingDateTime } from 'features/home/api/helpers/computeBeginningAndEndingDateTime'
 
-mockdate.set(new Date('2022-11-30T00:00+00:00'))
-describe('computeBeginningAndEndingDateTime', () => {
-  const mockedTodayDate = new Date()
+const mockedToday = '2022-11-30T00:00+00:00'
+mockdate.set(new Date(mockedToday))
 
+const mockedTodayDate = new Date()
+const mockedInTwoDays = '2022-12-02T00:00+00:00'
+const mockedNextSunday = '2022-12-04T23:59+00:00'
+const mockedNextMonth = '2022-12-30T00:00+00:00'
+describe('computeBeginningAndEndingDateTime', () => {
   it.each`
-    beginningDateTime  | endingDateTime                        | eventDuringNextXDays | currentNextWeek | expectedResult
-    ${mockedTodayDate} | ${undefined}                          | ${2}                 | ${false}        | ${{ beginningDateTime: '2022-11-30T00:00+00:00', endingDateTime: undefined }}
-    ${mockedTodayDate} | ${new Date('2022-12-30T00:00+00:00')} | ${2}                 | ${false}        | ${{ beginningDateTime: '2022-11-30T00:00+00:00', endingDateTime: '2022-12-30T00:00+00:00' }}
-    ${undefined}       | ${new Date('2022-12-30T00:00+00:00')} | ${2}                 | ${false}        | ${{ beginningDateTime: undefined, endingDateTime: '2022-12-30T00:00+00:00' }}
-    ${undefined}       | ${undefined}                          | ${2}                 | ${false}        | ${{ beginningDateTime: '2022-11-30T00:00+00:00', endingDateTime: '2022-12-02T00:00+00:00' }}
-    ${undefined}       | ${undefined}                          | ${undefined}         | ${true}         | ${{ beginningDateTime: '2022-11-30T00:00+00:00', endingDateTime: '2022-12-04T23:59+00:00' }}
-    ${undefined}       | ${undefined}                          | ${2}                 | ${true}         | ${{ beginningDateTime: '2022-11-30T00:00+00:00', endingDateTime: '2022-12-02T00:00+00:00' }}
-    ${mockedTodayDate} | ${undefined}                          | ${2}                 | ${true}         | ${{ beginningDateTime: '2022-11-30T00:00+00:00', endingDateTime: undefined }}
-    ${mockedTodayDate} | ${new Date('2022-12-30T00:00+00:00')} | ${2}                 | ${true}         | ${{ beginningDateTime: '2022-11-30T00:00+00:00', endingDateTime: '2022-12-30T00:00+00:00' }}
+    beginningDateTime  | endingDateTime               | eventDuringNextXDays | currentNextWeek | expectedResult
+    ${mockedTodayDate} | ${undefined}                 | ${2}                 | ${false}        | ${{ beginningDateTime: mockedToday, endingDateTime: undefined }}
+    ${mockedTodayDate} | ${new Date(mockedNextMonth)} | ${2}                 | ${false}        | ${{ beginningDateTime: mockedToday, endingDateTime: mockedNextMonth }}
+    ${undefined}       | ${new Date(mockedNextMonth)} | ${2}                 | ${false}        | ${{ beginningDateTime: undefined, endingDateTime: mockedNextMonth }}
+    ${undefined}       | ${undefined}                 | ${2}                 | ${false}        | ${{ beginningDateTime: mockedToday, endingDateTime: mockedInTwoDays }}
+    ${undefined}       | ${undefined}                 | ${undefined}         | ${true}         | ${{ beginningDateTime: mockedToday, endingDateTime: mockedNextSunday }}
+    ${undefined}       | ${undefined}                 | ${2}                 | ${true}         | ${{ beginningDateTime: mockedToday, endingDateTime: mockedInTwoDays }}
+    ${mockedTodayDate} | ${undefined}                 | ${2}                 | ${true}         | ${{ beginningDateTime: mockedToday, endingDateTime: undefined }}
+    ${mockedTodayDate} | ${new Date(mockedNextMonth)} | ${2}                 | ${true}         | ${{ beginningDateTime: mockedToday, endingDateTime: mockedNextMonth }}
   `(
     'should return $expectedResult when beginningDateTime: $beginningDateTime, endingDateTime: $endingDateTime, eventDuringNextXDays: $eventDuringNextXDays, currentNextWeek: $currentNextWeek',
     ({
