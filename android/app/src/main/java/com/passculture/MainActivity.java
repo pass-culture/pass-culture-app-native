056b9ff4c33604b1533fc3437831abbeb9ea5966
Hugo Voisin <hugov@bam.tech>
2022-11-29 18:33:07 +0100

    fix(android): avoid crash when re-entering the app

--- android/app/src/main/java/com/passculture/MainActivity.java
+++ android/app/src/main/java/com/passculture/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends ReactActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this, R.id.lottie);
         SplashScreen.setAnimationFinished(true); // We want the animation dialog to be forced to close when hide is called, so we use this
-        super.onCreate(savedInstanceState);
+        super.onCreate(null); // known fix for react-native-screens: https://github.com/software-mansion/react-native-screens#android
     }
 
     // @batch.com/react-native-plugin (https://doc.batch.com/react-native/sdk-integration#configure-onnewintent)
