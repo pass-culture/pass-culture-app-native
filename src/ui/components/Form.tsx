e56dc1280015387d22a72b42e2e9d970e25c68c1
Cédric Le Sausse <cedric.lesausse@passculture.app>
2022-11-29 15:39:30 +0100

    (PC-18889) feat(form): separate native & web component

--- src/ui/components/Form.tsx
+++ src/ui/components/Form.tsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
-import { AccessibilityRole } from 'libs/accessibilityRole/accessibilityRole'
+const MaxWidth: React.FC<{ flex?: number }> = styled.View<{ flex?: number }>(({ theme, flex }) => ({
+  width: '100%',
+  maxWidth: theme.forms.maxWidth,
+  flex,
+}))
 
-const FormView: React.FC = styled.View.attrs({
-  accessibilityRole: AccessibilityRole.FORM,
-})``
-
-const MaxWidth: React.FC<{ flex?: number }> = styled(FormView)<{ flex?: number }>(
-  ({ theme, flex }) => ({
-    width: '100%',
-    maxWidth: theme.forms.maxWidth,
-    flex,
-  })
-)
-
-const Flex: React.FC = styled(FormView)({
+const Flex: React.FC = styled.View({
   flex: 1,
 })
 
