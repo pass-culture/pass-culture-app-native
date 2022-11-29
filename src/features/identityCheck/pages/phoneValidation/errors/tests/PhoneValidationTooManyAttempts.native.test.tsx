7212cccef9ac93b6387335ee5b1af7d61a028291
AnoukHello <anoukhello22@gmail.com>
2022-11-29 19:50:48 +0100

    (BSR) identitycheck: remove superflushwithact

--- src/features/identityCheck/pages/phoneValidation/errors/tests/PhoneValidationTooManyAttempts.native.test.tsx
+++ src/features/identityCheck/pages/phoneValidation/errors/tests/PhoneValidationTooManyAttempts.native.test.tsx
@@ -1,24 +1,22 @@
 import React from 'react'
-import waitForExpect from 'wait-for-expect'
 
 import { navigate } from '__mocks__/@react-navigation/native'
 import { contactSupport } from 'features/auth/support.services'
 import { PhoneValidationTooManyAttempts } from 'features/identityCheck/pages/phoneValidation/errors/PhoneValidationTooManyAttempts'
 import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
 import { homeNavConfig } from 'features/navigation/TabBar/helpers'
-import { fireEvent, render, superFlushWithAct } from 'tests/utils'
+import { fireEvent, render, waitFor } from 'tests/utils'
 
 const openUrl = jest.spyOn(NavigationHelpers, 'openUrl')
 
 describe('Contact support button', () => {
   it('should open mail app when clicking on contact support button', async () => {
     const { getByText } = render(<PhoneValidationTooManyAttempts />)
-    await superFlushWithAct()
 
     const contactSupportButton = getByText('Contacter le support')
     fireEvent.press(contactSupportButton)
 
-    await waitForExpect(() => {
+    await waitFor(() => {
       expect(openUrl).toBeCalledWith(
         contactSupport.forPhoneNumberConfirmation.url,
         contactSupport.forPhoneNumberConfirmation.params
@@ -30,7 +28,6 @@ describe('Contact support button', () => {
 describe('Navigate to home button', () => {
   it('should redirect to Home when clicking on homepage button', async () => {
     const { getByText } = render(<PhoneValidationTooManyAttempts />)
-    await superFlushWithAct()
     fireEvent.press(getByText('Retourner à l’accueil'))
 
     expect(navigate).toBeCalledWith(homeNavConfig[0], homeNavConfig[1])
