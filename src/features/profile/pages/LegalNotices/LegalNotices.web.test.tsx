42dd608259bc3d595e922ee87ac01aec60295104
marineshaw <marines@bam.tech>
2022-12-02 11:29:19 +0100

    :recycle: (LegalNotices) Clean tests

--- src/features/profile/pages/LegalNotices/LegalNotices.web.test.tsx
+++ src/features/profile/pages/LegalNotices/LegalNotices.web.test.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 
-import { flushAllPromisesWithAct, render, checkAccessibilityFor } from 'tests/utils/web'
+import { render, checkAccessibilityFor } from 'tests/utils/web'
 
 import { LegalNotices } from './LegalNotices'
 
-async function renderProfile() {
-  const wrapper = render(<LegalNotices />)
-  await flushAllPromisesWithAct()
-  return wrapper
-}
-
 describe('LegalNotices', () => {
   it('should not have basic accessibility issues', async () => {
-    const { container } = await renderProfile()
+    const { container } = render(<LegalNotices />)
     const results = await checkAccessibilityFor(container)
 
     expect(results).toHaveNoViolations()
