11e33663ad701be43e952a3c70fee4ad7d3b5740
marineshaw <marines@bam.tech>
2022-12-02 18:12:15 +0100

    :sparkles: (LegalNotices) Add mail to support link

--- src/features/profile/pages/LegalNotices/LegalNotices.tsx
+++ src/features/profile/pages/LegalNotices/LegalNotices.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
+import { contactSupport } from 'features/auth/support.services'
 import { PageProfileSection } from 'features/profile/pages/PageProfileSection/PageProfileSection'
 import { env } from 'libs/environment'
 import { ButtonInsideText } from 'ui/components/buttons/buttonInsideText/ButtonInsideText'
 import { SectionRow } from 'ui/components/SectionRow'
 import { Separator } from 'ui/components/Separator'
 import { TouchableLink } from 'ui/components/touchableLink/TouchableLink'
+import { EmailFilled } from 'ui/svg/icons/EmailFilled'
 import { ExternalSite } from 'ui/svg/icons/ExternalSite'
 import { ExternalSiteFilled } from 'ui/svg/icons/ExternalSiteFilled'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
@@ -21,7 +23,7 @@ export function LegalNotices() {
       <Typo.Body>
         ÉDITEUR SAS pass Culture
         {LINE_BREAK}
-        <Typo.ButtonText>éditeur du site&nbsp;:</Typo.ButtonText>
+        éditeur du site&nbsp;:
         {LINE_BREAK}
         <TouchableLink
           as={ButtonInsideText}
@@ -30,19 +32,25 @@ export function LegalNotices() {
           icon={ExternalSiteFilled}
         />
         {LINE_BREAK}
-        <Typo.ButtonText>Société par action simplifiée</Typo.ButtonText> au capital de
-        1&nbsp;000&nbsp;000&nbsp;€
+        Société par action simplifiée au capital de 1&nbsp;000&nbsp;000&nbsp;€
         {LINE_BREAK}
-        <Typo.ButtonText>Siège social&nbsp;:</Typo.ButtonText> 12 rue Duhesme 75018 Paris
+        Siège social&nbsp;: 12 rue Duhesme 75018 Paris
         {LINE_BREAK}
         Immatriculée au RCS de Paris sous le numéro&nbsp;: 853&nbsp;318&nbsp;459
         {LINE_BREAK}
-        <Typo.ButtonText>Directeur de la publication&nbsp;:</Typo.ButtonText> Sébastien Cavalier
+        Directeur de la publication&nbsp;: Sébastien Cavalier
         {LINE_BREAK}
-        <Typo.ButtonText>Hébergeur&nbsp;:</Typo.ButtonText> Google Cloud Platform
+        Hébergeur&nbsp;: Google Cloud Platform
         {LINE_BREAK}8 Rue de Londres - 75009 Paris - France
         {LINE_BREAK}
-        <Typo.ButtonText>Nous contacter&nbsp;:</Typo.ButtonText> support@passculture.fr
+        Nous contacter&nbsp;:&nbsp;
+        <TouchableLink
+          as={ButtonInsideTextBlack}
+          wording="support@passculture.fr"
+          accessibilityLabel="Ouvrir le gestionnaire mail pour contacter le support"
+          externalNav={contactSupport.forGenericQuestion}
+          icon={EmailFilled}
+        />
       </Typo.Body>
 
       <StyledSeparator />
@@ -69,6 +77,10 @@ export function LegalNotices() {
 }
 
 const TitleText = styled(Typo.Title4).attrs(getHeadingAttrs(2))``
+
+const ButtonInsideTextBlack = styled(ButtonInsideText).attrs(({ theme }) => ({
+  color: theme.colors.black,
+}))``
 
 const StyledSeparator = styled(Separator)({
   marginVertical: getSpacing(4),
