119915d76e0498360d12b96c904c90b15de42767
marineshaw <marines@bam.tech>
2022-12-05 14:35:41 +0100

    :recycle: (LegalNotices) Make test file shorter

--- src/features/profile/pages/LegalNotices/LegalNotices.native.test.tsx
+++ src/features/profile/pages/LegalNotices/LegalNotices.native.test.tsx
@@ -2,38 +2,32 @@ import React from 'react'
 
 import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
 import { env } from 'libs/environment'
-import { flushAllPromisesWithAct, render, fireEvent } from 'tests/utils'
+import { render, fireEvent } from 'tests/utils'
 
 import { LegalNotices } from './LegalNotices'
 
-async function renderProfile() {
-  const wrapper = render(<LegalNotices />)
-  await flushAllPromisesWithAct()
-  return wrapper
-}
+const openUrl = jest.spyOn(NavigationHelpers, 'openUrl')
 
 describe('LegalNotices', () => {
   it('should render correctly', async () => {
-    const renderAPI = await renderProfile()
+    const renderAPI = render(<LegalNotices />)
     expect(renderAPI).toMatchSnapshot()
   })
 
   it('should navigate when the cgu row is clicked', async () => {
-    const openUrl = jest.spyOn(NavigationHelpers, 'openUrl')
-    const { getByText } = await renderProfile()
+    const { getByText } = render(<LegalNotices />)
 
     const row = getByText('Conditions Générales d’Utilisation')
     fireEvent.press(row)
 
-    expect(openUrl).toBeCalledWith(env.CGU_LINK, undefined)
+    expect(openUrl).toHaveBeenCalledWith(env.CGU_LINK, undefined)
   })
   it('should navigate when the data-privacy-chart row is clicked', async () => {
-    const openUrl = jest.spyOn(NavigationHelpers, 'openUrl')
-    const { getByText } = await renderProfile()
+    const { getByText } = render(<LegalNotices />)
 
     const row = getByText('Charte de protection des données personnelles')
     fireEvent.press(row)
 
-    expect(openUrl).toBeCalledWith(env.DATA_PRIVACY_CHART_LINK, undefined)
+    expect(openUrl).toHaveBeenCalledWith(env.DATA_PRIVACY_CHART_LINK, undefined)
   })
 })
