d6c2297706693ec65e213bc91f7e5cec531818e3
Lucas Beneston <lucas.beneston@passculture.app>
2022-12-07 11:26:49 +0100

    (PC-18668) feat(profil): fix PasswordSecurityRules visual bug and FilterSwitch accessibility error

--- src/ui/components/FilterSwitch.stories.tsx
+++ src/ui/components/FilterSwitch.stories.tsx
@@ -1,15 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import React from 'react'
+import styled from 'styled-components/native'
+
+import { Spacer } from 'ui/components/spacer/Spacer'
+import { Typo } from 'ui/theme'
 
 import FilterSwitch from './FilterSwitch'
 
 export default {
   title: 'ui/FilterSwitch',
   component: FilterSwitch,
-  parameters: {
-    axe: { disabledRules: ['aria-toggle-field-name'] },
-  },
   decorators: [
     (Story) => (
       <NavigationContainer>
@@ -19,7 +20,13 @@ export default {
   ],
 } as ComponentMeta<typeof FilterSwitch>
 
-const Template: ComponentStory<typeof FilterSwitch> = (props) => <FilterSwitch {...props} />
+const Template: ComponentStory<typeof FilterSwitch> = (props) => (
+  <StyledView>
+    <Typo.Body nativeID="filter-switch-id">{'Switch label'}</Typo.Body>
+    <Spacer.Row numberOfSpaces={2} />
+    <FilterSwitch {...props} accessibilityLabelledBy="filter-switch-id" />
+  </StyledView>
+)
 
 export const Inactive = Template.bind({})
 Inactive.args = {
@@ -42,3 +49,8 @@ ActiveDisabled.args = {
   active: true,
   disabled: true,
 }
+
+const StyledView = styled.View({
+  flexDirection: 'row',
+  alignItems: 'center',
+})
