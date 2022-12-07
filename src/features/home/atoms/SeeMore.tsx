67b3d60f9430c96f3a696d779ab53267a7347271
marineshaw <marines@bam.tech>
2022-12-07 09:38:24 +0100

    :sparkles: (SeeMore) Use internal touchableLink only when navigateTo is defined

--- src/features/home/atoms/SeeMore.tsx
+++ src/features/home/atoms/SeeMore.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
-import { TouchableLink } from 'ui/components/touchableLink/TouchableLink'
+import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
 import { InternalNavigationProps } from 'ui/components/touchableLink/types'
+import { TouchableOpacity } from 'ui/components/TouchableOpacity'
 import { ArrowNext as DefaultArrowNext } from 'ui/svg/icons/ArrowNext'
 import { getSpacing, Spacer, Typo, getShadow } from 'ui/theme'
 
@@ -13,24 +14,48 @@ interface SeeMoreProps {
   onPress: () => void
 }
 
-export const SeeMore: React.FC<SeeMoreProps> = ({ height, width, navigateTo, onPress }) => (
-  <Container height={height} width={width}>
-    <Spacer.Column numberOfSpaces={2} />
-    <ClickableArea activeOpacity={1} navigateTo={navigateTo} onBeforeNavigate={onPress}>
-      <Row>
-        <Spacer.Row numberOfSpaces={16} />
-        <RoundContainer>
-          <ArrowNext testID={'arrow-next'} />
-        </RoundContainer>
-        <Spacer.Row numberOfSpaces={16} />
-      </Row>
+export const SeeMore: React.FC<SeeMoreProps> = ({ height, width, navigateTo, onPress }) => {
+  return (
+    <Container height={height} width={width}>
       <Spacer.Column numberOfSpaces={2} />
-      <Row>
-        <Typo.ButtonTextPrimary>En voir plus</Typo.ButtonTextPrimary>
-      </Row>
-    </ClickableArea>
-  </Container>
-)
+      <ClickableArea activeOpacity={1} navigateTo={navigateTo} onPress={onPress}>
+        <Row>
+          <Spacer.Row numberOfSpaces={16} />
+          <RoundContainer>
+            <ArrowNext testID={'arrow-next'} />
+          </RoundContainer>
+          <Spacer.Row numberOfSpaces={16} />
+        </Row>
+        <Spacer.Column numberOfSpaces={2} />
+        <Row>
+          <Typo.ButtonTextPrimary>En voir plus</Typo.ButtonTextPrimary>
+        </Row>
+      </ClickableArea>
+    </Container>
+  )
+}
+
+const ClickableArea: React.FC<{
+  activeOpacity: number
+  navigateTo?: InternalNavigationProps['navigateTo']
+  onPress: () => void
+}> = ({ activeOpacity, children, onPress, navigateTo }) => {
+  if (navigateTo)
+    return (
+      <StyledTouchableLink
+        activeOpacity={activeOpacity}
+        navigateTo={navigateTo}
+        onBeforeNavigate={onPress}>
+        {children}
+      </StyledTouchableLink>
+    )
+  else
+    return (
+      <StyledTouchableOpacity activeOpacity={activeOpacity} onPress={onPress}>
+        {children}
+      </StyledTouchableOpacity>
+    )
+}
 
 const Row = styled.View({ flexDirection: 'row' })
 
@@ -41,7 +66,11 @@ const Container = styled.View<{ height: number; width: number }>(({ height, widt
   justifyContent: 'center',
 }))
 
-const ClickableArea = styled(TouchableLink)({
+const StyledTouchableLink = styled(InternalTouchableLink)({
+  alignItems: 'center',
+})
+
+const StyledTouchableOpacity = styled(TouchableOpacity)({
   alignItems: 'center',
 })
 
