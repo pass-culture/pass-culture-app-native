4d44d54bc4e9b7024279add33b946f2eb1d0926d
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-12-07 17:00:52 +0200

    Upgrade react/react-native types and follow react18 upgrade guide

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import 'react-app-polyfill/ie9'
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root')
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!)
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
