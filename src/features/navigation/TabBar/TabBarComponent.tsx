ea42b5e788284387d768721cc3ea7055c392754b
marineshaw <marines@bam.tech>
2022-12-09 14:52:01 +0100

    (PC-17958) fix(TabBar): fix tab bar after use of internal touchable link

--- src/features/navigation/TabBar/TabBarComponent.tsx
+++ src/features/navigation/TabBar/TabBarComponent.tsx
@@ -4,8 +4,10 @@ import styled from 'styled-components/native'
 
 import { menu } from 'features/navigation/TabBar/routes'
 import { TabRouteName } from 'features/navigation/TabBar/types'
+import { AccessibilityRole } from 'libs/accessibilityRole/accessibilityRole'
 import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
 import { InternalNavigationProps } from 'ui/components/touchableLink/types'
+import { TouchableOpacity } from 'ui/components/TouchableOpacity'
 import { BicolorLogo } from 'ui/svg/icons/BicolorLogo'
 import { BicolorSelector } from 'ui/svg/icons/BicolorSelector'
 import { BicolorIconInterface } from 'ui/svg/icons/types'
@@ -30,29 +32,50 @@ export const TabBarComponent: React.FC<Props> = ({
   navigateTo,
   onPress,
   tabName,
-}) => (
-  <TabComponentContainer
-    navigateTo={navigateTo}
-    onBeforeNavigate={onPress}
-    activeOpacity={1}
-    selected={isSelected}
-    accessibilityLabel={menu[tabName].accessibilityLabel}
-    testID={menu[tabName].accessibilityLabel || menu[tabName].displayName}
-    aria-current={isSelected ? 'page' : undefined}>
-    {!!isSelected && (
-      <BicolorSelector
-        width={SELECTOR_WIDTH}
-        height={SELECTOR_HEIGHT}
-        testID={`${menu[tabName].accessibilityLabel} sélectionné`}
-      />
-    )}
-    <Spacer.Flex />
-    <StyledIcon as={BicolorIcon} selected={isSelected} />
-    <Title selected={isSelected}>{menu[tabName].displayName}</Title>
-    <Spacer.Flex />
-    {!!isSelected && <BicolorSelectorPlaceholder />}
-  </TabComponentContainer>
-)
+}) => {
+  const commonProps = {
+    activeOpacity: 1,
+    selected: isSelected,
+    accessibilityLabel: menu[tabName].accessibilityLabel,
+    testID: menu[tabName].accessibilityLabel || menu[tabName].displayName,
+    accessibilityRole: AccessibilityRole.LINK,
+  }
+  const Content = (
+    <React.Fragment>
+      {!!isSelected && (
+        <BicolorSelector
+          width={SELECTOR_WIDTH}
+          height={SELECTOR_HEIGHT}
+          testID={`${menu[tabName].accessibilityLabel} sélectionné`}
+        />
+      )}
+      <Spacer.Flex />
+      <StyledIcon as={BicolorIcon} selected={isSelected} />
+      <Title selected={isSelected}>{menu[tabName].displayName}</Title>
+      <Spacer.Flex />
+      {!!isSelected && <BicolorSelectorPlaceholder />}
+    </React.Fragment>
+  )
+  if (navigateTo) {
+    return (
+      <TabComponentContainerLink
+        navigateTo={navigateTo}
+        onBeforeNavigate={onPress}
+        {...commonProps}
+        aria-current={isSelected ? 'page' : undefined}>
+        {Content}
+      </TabComponentContainerLink>
+    )
+  } else
+    return (
+      <TabComponentContainer
+        onPress={onPress}
+        {...commonProps}
+        aria-current={isSelected ? 'page' : undefined}>
+        {Content}
+      </TabComponentContainer>
+    )
+}
 
 const StyledIcon = styled(BicolorLogo).attrs<{ selected?: boolean }>(({ theme, selected }) => ({
   color: selected ? undefined : theme.colors.greyDark,
@@ -62,7 +85,18 @@ const StyledIcon = styled(BicolorLogo).attrs<{ selected?: boolean }>(({ theme, s
 
 const BicolorSelectorPlaceholder = styled.View({ height: SELECTOR_HEIGHT })
 
-const TabComponentContainer = styled(InternalTouchableLink).attrs(
+const TabComponentContainerLink = styled(InternalTouchableLink).attrs(
+  ({ theme, accessibilityLabel, selected }) => ({
+    accessibilityLabel: theme.tabBar.showLabels && isWeb ? undefined : accessibilityLabel,
+    hoverUnderlineColor: selected ? theme.colors.black : theme.colors.greyDark,
+  })
+)(({ theme }) => ({
+  alignItems: 'center',
+  height: theme.tabBar.height,
+  flex: 1,
+}))
+
+const TabComponentContainer = styled(TouchableOpacity).attrs<{ selected: boolean }>(
   ({ theme, accessibilityLabel, selected }) => ({
     accessibilityLabel: theme.tabBar.showLabels && isWeb ? undefined : accessibilityLabel,
     hoverUnderlineColor: selected ? theme.colors.black : theme.colors.greyDark,
