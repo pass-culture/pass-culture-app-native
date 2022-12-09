cffc6f1befcd51155270d6487dbe54c89b41164f
Hugo Voisin <hugov@bam.tech>
2022-12-09 16:25:37 +0100

    fixme

--- src/features/home/api.ts
+++ src/features/home/api.ts
@@ -30,6 +30,7 @@ const getHomepageNatifContent = async () => {
     return await fetchHomepageNatifContent()
   } catch (e) {
     const error = e as Error
+    // FIXME(voisinhugo): do we need this optional chaining? If yes, ScreenError should accept undefined message
     throw new ScreenError(error?.message, NoContentError)
   }
 }
