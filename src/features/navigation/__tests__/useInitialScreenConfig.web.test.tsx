4bef541250d3d3bc4aa7ee8bafca4da14e956e9c
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-12-09 11:06:03 +0100

    (PC-19246) feat(onboarding): Redirect to new onboarding instead of tutorials

---
+++ src/features/navigation/__tests__/useInitialScreenConfig.web.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import { analytics } from 'libs/firebase/analytics'
+import { SplashScreenProvider } from 'libs/splashscreen'
+import { storage } from 'libs/storage'
+import { renderHook, waitFor } from 'tests/utils'
+
+import { useInitialScreen } from '../RootNavigator/useInitialScreenConfig'
+
+jest.mock('features/auth/settings', () => ({
+  useAppSettings: jest.fn(() => ({
+    data: {
+      enableNativeCulturalSurvey: false,
+    },
+  })),
+}))
+
+const wrapper = (props: { children: unknown }) => (
+  <SplashScreenProvider>{props.children as JSX.Element}</SplashScreenProvider>
+)
+
+describe('useInitialScreen()', () => {
+  afterAll(async () => {
+    await storage.clear('has_seen_tutorials')
+    await storage.clear('has_seen_eligible_card')
+  })
+
+  it('should redirect to Home if user has not seen tutorials', async () => {
+    const { result } = renderHook(useInitialScreen, { wrapper })
+
+    await waitFor(() => {
+      expect(result.current).toEqual('TabNavigator')
+      expect(analytics.logScreenView).toBeCalledTimes(1)
+      expect(analytics.logScreenView).toBeCalledWith('Home')
+    })
+  })
+})
