7d769c8b6f199192ef80b9600afb027a4e1d919a
bruno ebstein <bruno.ebstein@passculture.app>
2022-12-15 16:54:03 +0100

    WIP

--- src/ui/components/inputs/EmailInputWithSpellingHelp/EmailInputWithSpellingHelp.stories.tsx
+++ src/ui/components/inputs/EmailInputWithSpellingHelp/EmailInputWithSpellingHelp.stories.tsx
@@ -5,7 +5,7 @@ import { Controller, useForm } from 'react-hook-form'
 import { EmailInputWithSpellingHelp } from './EmailInputWithSpellingHelp'
 
 export default {
-  title: 'ui/inputs/emailInputWithSpellingHelp',
+  title: 'ui/inputs/EmailInputWithSpellingHelp',
   component: EmailInputWithSpellingHelp,
 } as ComponentMeta<typeof EmailInputWithSpellingHelp>
 
