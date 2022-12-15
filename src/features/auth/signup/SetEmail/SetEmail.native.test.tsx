8c8036b3fa15a9ea7dd95d66a4c83db91d9980c7
bruno ebstein <bruno.ebstein@passculture.app>
2022-12-15 11:12:26 +0100

    refactor: simplify tests

--- src/features/auth/signup/SetEmail/SetEmail.native.test.tsx
+++ src/features/auth/signup/SetEmail/SetEmail.native.test.tsx
@@ -4,7 +4,10 @@ import { SetEmail } from 'features/auth/signup/SetEmail'
 import { analytics } from 'libs/firebase/analytics'
 import { act, fireEvent, render } from 'tests/utils'
 
-const props = { goToNextStep: jest.fn(), signUp: jest.fn() }
+const props = {
+  goToNextStep: jest.fn(),
+  signUp: jest.fn(),
+}
 
 describe('<SetEmail />', () => {
   it('should display disabled validate button when email input is not filled', () => {
@@ -17,8 +20,10 @@ describe('<SetEmail />', () => {
   it('should enable validate button when email input is filled', async () => {
     const { getByText, getByPlaceholderText } = render(<SetEmail {...props} />)
 
-    const emailInput = getByPlaceholderText('tonadresse@email.com')
-    await act(async () => await fireEvent.changeText(emailInput, 'john.doe@gmail.com'))
+    await act(async () => {
+      const emailInput = getByPlaceholderText('tonadresse@email.com')
+      fireEvent.changeText(emailInput, 'john.doe@gmail.com')
+    })
 
     const button = getByText('Continuer')
     expect(button).toBeEnabled()
@@ -27,11 +32,15 @@ describe('<SetEmail />', () => {
   it('should call goToNextStep() on valid email with email and newsletter params', async () => {
     const { getByText, getByPlaceholderText } = render(<SetEmail {...props} />)
 
-    const emailInput = getByPlaceholderText('tonadresse@email.com')
-    await act(async () => await fireEvent.changeText(emailInput, 'john.doe@gmail.com'))
+    await act(async () => {
+      const emailInput = getByPlaceholderText('tonadresse@email.com')
+      fireEvent.changeText(emailInput, 'john.doe@gmail.com')
+    })
 
-    const continueButton = getByText('Continuer')
-    await act(async () => await fireEvent.press(continueButton))
+    await act(async () => {
+      const continueButton = getByText('Continuer')
+      fireEvent.press(continueButton)
+    })
 
     expect(props.goToNextStep).toBeCalledWith({
       email: 'john.doe@gmail.com',
@@ -42,11 +51,13 @@ describe('<SetEmail />', () => {
   it('should not display email help message by default', async () => {
     const { getByText, getByPlaceholderText, queryByText } = render(<SetEmail {...props} />)
 
-    const emailInput = getByPlaceholderText('tonadresse@email.com')
-    await act(async () => await fireEvent.changeText(emailInput, 'john.doe@gmail.com'))
+    await act(async () => {
+      const emailInput = getByPlaceholderText('tonadresse@email.com')
+      fireEvent.changeText(emailInput, 'john.doe@gmail.com')
 
-    const continueButton = getByText('Continuer')
-    await act(async () => await fireEvent.press(continueButton))
+      const continueButton = getByText('Continuer')
+      fireEvent.press(continueButton)
+    })
 
     expect(
       queryByText(
@@ -58,11 +69,15 @@ describe('<SetEmail />', () => {
   it('should reject email', async () => {
     const { getByText, getByPlaceholderText, queryByText } = render(<SetEmail {...props} />)
 
-    const emailInput = getByPlaceholderText('tonadresse@email.com')
-    await act(async () => await fireEvent.changeText(emailInput, 'john.doe'))
+    await act(async () => {
+      const emailInput = getByPlaceholderText('tonadresse@email.com')
+      fireEvent.changeText(emailInput, 'john.doe')
+    })
 
-    const continueButton = getByText('Continuer')
-    await act(async () => await fireEvent.press(continueButton))
+    await act(async () => {
+      const continueButton = getByText('Continuer')
+      fireEvent.press(continueButton)
+    })
 
     expect(
       queryByText(
@@ -74,8 +89,10 @@ describe('<SetEmail />', () => {
   it('should log analytics when clicking on "Se connecter" button', async () => {
     const { getByText } = render(<SetEmail {...props} />)
 
-    const loginButton = getByText('Se connecter')
-    await act(async () => await fireEvent.press(loginButton))
+    await act(async () => {
+      const loginButton = getByText('Se connecter')
+      fireEvent.press(loginButton)
+    })
 
     expect(analytics.logLogin).toHaveBeenNthCalledWith(1, { method: 'fromSetEmail' })
   })
