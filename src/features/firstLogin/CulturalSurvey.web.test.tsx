2a039d175477c11d5e5dd2dc2a48100838fe6dc9
Hugo Voisin <hugov@bam.tech>
2022-12-16 18:02:19 +0100

    test: add a11y test to cultural survey

---
+++ src/features/firstLogin/CulturalSurvey.web.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { useCurrentRoute } from 'features/navigation/helpers'
+import { render, checkAccessibilityFor } from 'tests/utils/web'
+
+import { CulturalSurvey } from './CulturalSurvey'
+
+jest.mock('react-query')
+jest.mock('features/auth/settings')
+jest.mock('features/auth/AuthContext')
+
+jest.mock('features/navigation/helpers')
+const mockedUseCurrentRoute = useCurrentRoute as jest.MockedFunction<typeof useCurrentRoute>
+mockedUseCurrentRoute.mockReturnValue({ name: 'CulturalSurvey', key: 'key' })
+
+describe('<CulturalSurvey/>', () => {
+  describe('Accessibility', () => {
+    it('should not have basic accessibility issues', async () => {
+      const { container } = render(<CulturalSurvey />)
+      const results = await checkAccessibilityFor(container)
+      expect(results).toHaveNoViolations()
+    })
+  })
+})
