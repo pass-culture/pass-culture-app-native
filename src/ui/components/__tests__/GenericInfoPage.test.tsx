5cd5ecf5815eff5913052387ebe9de54a1d979ed
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2022-12-16 10:07:00 +0100

    (PC-19279) fix(StatusBar): Fix crash (#4083)

--- src/ui/components/__tests__/GenericInfoPage.test.tsx
+++ src/ui/components/__tests__/GenericInfoPage.test.tsx
@@ -4,6 +4,14 @@ import { render } from 'tests/utils'
 import { GenericInfoPage } from 'ui/pages/GenericInfoPage'
 import { BicolorPhonePending } from 'ui/svg/icons/BicolorPhonePending'
 
+// We unmock these modules to make sure they are not used because
+// navigation with @react-navigation is not always defined in GenericInfoPage
+jest.unmock('@react-navigation/native')
+jest.unmock('@react-navigation/stack')
+jest.unmock('@react-navigation/bottom-tabs')
+jest.unmock('features/navigation/useGoBack')
+jest.unmock('libs/hooks/useWhiteStatusBar')
+
 describe('<GenericInfoPage />', () => {
   it('should render correctly', () => {
     const renderAPI = render(<GenericInfoPage title="GenericInfoPage" icon={BicolorPhonePending} />)
