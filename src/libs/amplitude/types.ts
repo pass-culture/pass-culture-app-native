f046537ee7915ffdead6095f8cce16a08475b204
Hugo Voisin <hugov@bam.tech>
2022-12-21 11:49:09 +0100

    refactor: remove false return type it is the return type from the original Amplitude function, but not the return type from our interface

--- src/libs/amplitude/types.ts
+++ src/libs/amplitude/types.ts
@@ -1,5 +1,5 @@
 export interface AmplitudeClient {
-  logEvent(eventType: string, eventProperties?: Record<string, unknown>): Promise<void> | void
-  enableCollection(): Promise<void> | void
-  disableCollection(): Promise<void> | void
+  logEvent(eventType: string, eventProperties?: Record<string, unknown>): void
+  enableCollection(): void
+  disableCollection(): void
 }
