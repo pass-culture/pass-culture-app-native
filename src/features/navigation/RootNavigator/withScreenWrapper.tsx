3f7cb60c0a806d83d377e5cb2c6bc9976ee6effb
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2022-12-22 09:45:00 +0100

    remove any

--- src/features/navigation/RootNavigator/withScreenWrapper.tsx
+++ src/features/navigation/RootNavigator/withScreenWrapper.tsx
@@ -1,8 +1,7 @@
 import React, { ComponentType, memo } from 'react'
 import styled from 'styled-components/native'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const withScreenWrapper = (Component: ComponentType<any>, name: string) => {
+export const withScreenWrapper = (Component: ComponentType, name: string) => {
   return memo(function ComponentWithScreenWrapper(props = {}) {
     return (
       <View testID={name}>
