6499cb97ea97d88caf4d1b28e0462523a852f675
Lucas Beneston <lucas.beneston@passculture.app>
2022-12-26 17:14:35 +0100

    (PC-6360) fix(test): remove allowConsole and no-allow-console eslint rule

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -1,6 +1,5 @@
 const independentMocks = require('./eslint-custom-rules/independent-mocks')
 const nbspInText = require('./eslint-custom-rules/nbsp-in-text')
-const noAllowConsole = require('./eslint-custom-rules/no-allow-console')
 const noRawText = require('./eslint-custom-rules/no-raw-text')
 const noReactQueryProviderHOC = require('./eslint-custom-rules/no-react-query-provider-hoc')
 const noStringCheckBeforeComponent = require('./eslint-custom-rules/no-string-check-before-component')
@@ -9,7 +8,6 @@ const todoFormat = require('./eslint-custom-rules/todo-format')
 module.exports = {
   'independent-mocks': independentMocks,
   'nbsp-in-text': nbspInText,
-  'no-allow-console': noAllowConsole,
   'no-raw-text': noRawText,
   'no-react-query-provider-hoc': noReactQueryProviderHOC,
   'no-string-check-before-component': noStringCheckBeforeComponent,
