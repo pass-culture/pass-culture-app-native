c8bab6a96f17840fd5825c0f119b0c6c16c67dba
Eve Julliard <evej@bam.tech>
2022-12-30 11:29:26 +0100

    (PC-19704) feat(adaptHomepageModules): make the tests constants reusable to use them in other tests later

--- src/libs/contentful/adapters/adaptHomepageModules.native.test.ts
+++ src/libs/contentful/adapters/adaptHomepageModules.native.test.ts
@@ -32,21 +32,87 @@ import {
 } from 'libs/contentful/types'
 
 describe('adaptHomepageModules', () => {
+  const formattedBusinessModule: BusinessModule = {
+    id: '20SId61p6EFTG7kgBTFrOa',
+    analyticsTitle:
+      'Crée un compte\u00a0! 15-18 [A MAINTENIR EN BLOC 2 et paramétré pour être visible seulement pour les non connectés]',
+    title: 'Débloque ton crédit\u00a0! ',
+    subtitle: 'Termine ton inscription',
+    shouldTargetNotConnectedUsers: true,
+    url: 'https://passculture.app/creation-compte',
+    image:
+      'https://images.ctfassets.net/2bg01iqy0isv/1uTePwMo6qxJo7bMM7VLeX/fdea7eb6fd7ab2003a5f1eeaba2565e9/17-insta-1080x1350_560x800.jpg',
+    leftIcon: undefined,
+  }
+  const formattedVenuesModule: VenuesModule = {
+    id: '105MMz59tftcxXJICXt7ja',
+    venuesSearchParameters: [
+      {
+        title: 'Exemple de playlist de lieux',
+        isGeolocated: false,
+        venueTypes: [
+          'Bibliothèque ou médiathèque',
+          'Arts visuels, arts plastiques et galeries',
+          'Centre culturel',
+          'Cinéma - Salle de projections',
+          'Cours et pratique artistiques',
+          'Culture scientifique',
+          'Festival',
+          'Jeux / Jeux vidéos',
+          'Librairie',
+          'Magasin arts créatifs',
+          'Musique - Disquaire',
+          'Musique - Magasin d’instruments',
+          'Musique - Salle de concerts',
+          'Musée',
+          'Offre numérique',
+          'Patrimoine et tourisme',
+          'Spectacle vivant',
+          'Cinéma itinérant',
+          'Autre type de lieu',
+        ],
+        hitsPerPage: 15,
+      },
+    ],
+    displayParameters: {
+      title: 'Exemple de playlist de lieux',
+      subtitle: "Ceci n'est pas une playlist de lieux",
+      layout: 'one-item-medium',
+      minOffers: 1,
+    },
+  }
+  const formattedRecommendedOffersModule: RecommendedOffersModule = {
+    id: '3sAqNrRMXUOES7tFyRFFO8',
+    displayParameters: { title: 'Nos recos', layout: 'two-items', minOffers: 1 },
+    recommendationParameters: undefined,
+  }
+  const formattedExclusivityModule: ExclusivityModule = {
+    id: 'AEYnm9QjIo2rZKoCfSvMD',
+    title: 'WE FRAC CAEN',
+    alt: 'Week-end FRAC',
+    image:
+      'https://images.ctfassets.net/2bg01iqy0isv/1uTePwMo6qxJo7bMM7VLeX/fdea7eb6fd7ab2003a5f1eeaba2565e9/17-insta-1080x1350_560x800.jpg',
+    url: undefined,
+    offerId: 123456789,
+    displayParameters: undefined,
+  }
+  const formattedOffersModule: OffersModule = {
+    id: '2DYuR6KoSLElDuiMMjxx8g',
+    title: 'Fais le plein de lecture',
+    displayParameters: {
+      title: 'Fais le plein de lecture avec notre partenaire ',
+      subtitle: 'Tout plein de livres pour encore plus de fun sans que pour autant on en sache ',
+      layout: 'two-items',
+      minOffers: 1,
+    },
+    offersModuleParameters: [
+      { title: 'Livre', isGeolocated: false, categories: ['Livres'], hitsPerPage: 10 },
+    ],
+    cover: undefined,
+  }
+
   it('should adapt a business module', () => {
     const rawBusinessModule = businessNatifModuleFixture
-
-    const formattedBusinessModule: BusinessModule = {
-      id: '20SId61p6EFTG7kgBTFrOa',
-      analyticsTitle:
-        'Crée un compte\u00a0! 15-18 [A MAINTENIR EN BLOC 2 et paramétré pour être visible seulement pour les non connectés]',
-      title: 'Débloque ton crédit\u00a0! ',
-      subtitle: 'Termine ton inscription',
-      shouldTargetNotConnectedUsers: true,
-      url: 'https://passculture.app/creation-compte',
-      image:
-        'https://images.ctfassets.net/2bg01iqy0isv/1uTePwMo6qxJo7bMM7VLeX/fdea7eb6fd7ab2003a5f1eeaba2565e9/17-insta-1080x1350_560x800.jpg',
-      leftIcon: undefined,
-    }
 
     expect(isBusinessNatifModule(rawBusinessModule)).toBeTruthy()
     expect(adaptBusinessModule(rawBusinessModule)).toEqual(formattedBusinessModule)
@@ -54,55 +120,12 @@ describe('adaptHomepageModules', () => {
   it('should adapt a venues module', () => {
     const rawVenuesModule = venuesNatifModuleFixture
 
-    const formattedVenuesModule: VenuesModule = {
-      id: '105MMz59tftcxXJICXt7ja',
-      venuesSearchParameters: [
-        {
-          title: 'Exemple de playlist de lieux',
-          isGeolocated: false,
-          venueTypes: [
-            'Bibliothèque ou médiathèque',
-            'Arts visuels, arts plastiques et galeries',
-            'Centre culturel',
-            'Cinéma - Salle de projections',
-            'Cours et pratique artistiques',
-            'Culture scientifique',
-            'Festival',
-            'Jeux / Jeux vidéos',
-            'Librairie',
-            'Magasin arts créatifs',
-            'Musique - Disquaire',
-            'Musique - Magasin d’instruments',
-            'Musique - Salle de concerts',
-            'Musée',
-            'Offre numérique',
-            'Patrimoine et tourisme',
-            'Spectacle vivant',
-            'Cinéma itinérant',
-            'Autre type de lieu',
-          ],
-          hitsPerPage: 15,
-        },
-      ],
-      displayParameters: {
-        title: 'Exemple de playlist de lieux',
-        subtitle: "Ceci n'est pas une playlist de lieux",
-        layout: 'one-item-medium',
-        minOffers: 1,
-      },
-    }
-
     expect(isVenuesNatifModule(rawVenuesModule)).toBeTruthy()
     expect(adaptVenuesModule(rawVenuesModule)).toEqual(formattedVenuesModule)
   })
 
   it('should adapt a recommendedOffers module', () => {
     const rawRecommendationModule = recommendationNatifModuleFixture
-
-    const formattedRecommendedOffersModule: RecommendedOffersModule = {
-      id: '3sAqNrRMXUOES7tFyRFFO8',
-      displayParameters: { title: 'Nos recos', layout: 'two-items', minOffers: 1 },
-    }
 
     expect(isRecommendationNatifModule(rawRecommendationModule)).toBeTruthy()
     expect(adaptRecommendationModule(rawRecommendationModule)).toEqual(
@@ -113,35 +136,12 @@ describe('adaptHomepageModules', () => {
   it('should adapt an exclusivity module', () => {
     const rawExclusivityNatifModule = exclusivityNatifModuleFixture
 
-    const formattedExclusivityModule: ExclusivityModule = {
-      id: 'AEYnm9QjIo2rZKoCfSvMD',
-      title: 'WE FRAC CAEN',
-      alt: 'Week-end FRAC',
-      image:
-        'https://images.ctfassets.net/2bg01iqy0isv/1uTePwMo6qxJo7bMM7VLeX/fdea7eb6fd7ab2003a5f1eeaba2565e9/17-insta-1080x1350_560x800.jpg',
-      url: undefined,
-      offerId: 123456789,
-    }
     expect(isExclusivityNatifModule(rawExclusivityNatifModule)).toBeTruthy()
     expect(adaptExclusivityModule(rawExclusivityNatifModule)).toEqual(formattedExclusivityModule)
   })
 
   it('should adapt an offers module without additional offers', () => {
     const rawAlgoliaNatifModule = algoliaNatifModuleFixture
-
-    const formattedOffersModule: OffersModule = {
-      id: '2DYuR6KoSLElDuiMMjxx8g',
-      title: 'Fais le plein de lecture',
-      displayParameters: {
-        title: 'Fais le plein de lecture avec notre partenaire ',
-        subtitle: 'Tout plein de livres pour encore plus de fun sans que pour autant on en sache ',
-        layout: 'two-items',
-        minOffers: 1,
-      },
-      offersModuleParameters: [
-        { title: 'Livre', isGeolocated: false, categories: ['Livres'], hitsPerPage: 10 },
-      ],
-    }
 
     expect(isAlgoliaNatifModule(rawAlgoliaNatifModule)).toBeTruthy()
     expect(adaptOffersModule(rawAlgoliaNatifModule)).toEqual(formattedOffersModule)
