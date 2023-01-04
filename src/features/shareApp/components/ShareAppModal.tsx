31f7de8fe01c6858bed076f672f7bb70799f98ed
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-01-04 12:03:29 +0100

    (PC-19413) feat(shareApp): Update ShareApp modal

--- src/features/shareApp/components/ShareAppModal.tsx
+++ src/features/shareApp/components/ShareAppModal.tsx
@@ -1,15 +1,18 @@
 import React, { FunctionComponent, useCallback } from 'react'
 import styled from 'styled-components/native'
 
+import { SHARE_APP_IMAGE_SOURCE } from 'features/shareApp/components/shareAppImage'
 import { shareApp } from 'features/shareApp/helpers/shareApp'
 import {
   shareAppModalInformations,
   ShareAppModalType,
 } from 'features/shareApp/helpers/shareAppModalInformations'
 import { analytics } from 'libs/firebase/analytics'
-import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
-import { AppInformationModal } from 'ui/components/modals/AppInformationModal'
-import { BicolorShout } from 'ui/svg/icons/BicolorShout'
+import { ButtonQuaternaryBlack } from 'ui/components/buttons/ButtonQuaternaryBlack'
+import { ButtonWithLinearGradient } from 'ui/components/buttons/buttonWithLinearGradient/ButtonWithLinearGradient'
+import { MarketingModal } from 'ui/components/modals/MarketingModal'
+import { Invalidate } from 'ui/svg/icons/Invalidate'
+import { Share } from 'ui/svg/icons/Share'
 import { Spacer, Typo } from 'ui/theme'
 
 type Props = {
@@ -25,7 +28,7 @@ export const ShareAppModal: FunctionComponent<Props> = ({ visible, hideModal, mo
     setTimeout(shareApp, 0)
   }, [modalType, hideModal])
 
-  const onCloseIconPress = useCallback(() => {
+  const closeModal = useCallback(() => {
     analytics.logDismissShareApp(modalType)
     hideModal()
   }, [modalType, hideModal])
@@ -33,17 +36,28 @@ export const ShareAppModal: FunctionComponent<Props> = ({ visible, hideModal, mo
   const { title, explanation } = shareAppModalInformations(modalType)
 
   return (
-    <AppInformationModal visible={visible} title={title} onCloseIconPress={onCloseIconPress}>
-      <ShoutIcon />
-      <Spacer.Column numberOfSpaces={6} />
+    <MarketingModal
+      visible={visible}
+      title={title}
+      imageSource={SHARE_APP_IMAGE_SOURCE}
+      onBackdropPress={closeModal}>
       <StyledBody>{explanation}</StyledBody>
       <Spacer.Column numberOfSpaces={6} />
-      <ButtonPrimary
-        wording="Partager"
-        accessibilityLabel="Partager l'application"
-        onPress={openShareAppModal}
-      />
-    </AppInformationModal>
+      <ButtonContainer>
+        <ButtonWithLinearGradient
+          wording="Partager l’appli"
+          icon={Share}
+          onPress={openShareAppModal}
+        />
+        <Spacer.Column numberOfSpaces={4} />
+        <ButtonQuaternaryBlack
+          wording="Non merci"
+          accessibilityLabel="Fermer la modale"
+          icon={Invalidate}
+          onPress={closeModal}
+        />
+      </ButtonContainer>
+    </MarketingModal>
   )
 }
 
@@ -51,6 +65,4 @@ const StyledBody = styled(Typo.Body)({
   textAlign: 'center',
 })
 
-const ShoutIcon = styled(BicolorShout).attrs(({ theme }) => ({
-  size: theme.illustrations.sizes.fullPage,
-}))``
+const ButtonContainer = styled.View({ width: '100%' })
