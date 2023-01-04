06291f88d82b61bdccae99fe57107f57d3330605
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-01-04 09:57:00 +0100

    (PC-19465) fix(deploy): Revert changes made to deploy (#4159)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -61,15 +61,15 @@ check_environment() {
   # The CI jobs are based on a tag (not a branch)
   HARD_DEPLOY_TESTING_TAG_REGEX="(testing\/)?v[0-9]+(\.[0-9]+){2}"
 
-  # if [[ "$APP_ENV" == "testing" && "$CURRENT_BRANCH" != "master" ]]; then
-  #   if [[ $CURRENT_TAG =~ $HARD_DEPLOY_TESTING_TAG_REGEX ]]; then
-  #     success "Not on master but tag found. Deploying to $APP_ENV."
-  #   else
-  #     warn "Wrong branch, checkout master or create tag to deploy to $APP_ENV."
-  #   fi
-  # else
-  #   success "Deploying to $APP_ENV."
-  # fi
+  if [[ "$APP_ENV" == "testing" && "$CURRENT_BRANCH" != "master" ]]; then
+    if [[ $CURRENT_TAG =~ $HARD_DEPLOY_TESTING_TAG_REGEX ]]; then
+      success "Not on master but tag found. Deploying to $APP_ENV."
+    else
+      warn "Wrong branch, checkout master or create tag to deploy to $APP_ENV."
+    fi
+  else
+    success "Deploying to $APP_ENV."
+  fi
 }
 
 check_dependency() {
