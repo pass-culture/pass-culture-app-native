94b262bd82795757af9e2db7c0d23167a60fe3e9
Lucas Beneston <lucas.beneston@passculture.app>
2023-01-05 17:56:03 +0100

    (PC-19411) refactor(profile): update matchSubscriptionMessageIconToSvg logic and icons

--- src/features/profile/helpers/matchSubscriptionMessageIconToSvg.test.ts
+++ src/features/profile/helpers/matchSubscriptionMessageIconToSvg.test.ts
@@ -1,27 +1,67 @@
-import { BicolorClock } from 'ui/svg/icons/BicolorClock'
+import { CallToActionIcon, PopOverIcon } from 'api/gen'
+import { Again } from 'ui/svg/icons/Again'
+import { Clock } from 'ui/svg/icons/BicolorClock'
+import { Warning } from 'ui/svg/icons/BicolorWarning'
+import { EmailFilled } from 'ui/svg/icons/EmailFilled'
+import { Error as ErrorIcon } from 'ui/svg/icons/Error'
+import { ExternalSiteFilled } from 'ui/svg/icons/ExternalSiteFilled'
 import { Info } from 'ui/svg/icons/Info'
+import { LegalNotices } from 'ui/svg/icons/LegalNotices'
 
 import { matchSubscriptionMessageIconToSvg } from './matchSubscriptionMessageIconToSvg'
 
 describe('matchSubscriptionMessageIconToSvg', () => {
-  it('should return no icon if undefined is passed', () => {
-    const returnedIcon = matchSubscriptionMessageIconToSvg(undefined)
-    expect(returnedIcon).toEqual(undefined)
+  describe('PopOverIcon', () => {
+    it("should return Clock icon if 'CLOCK' is passed", () => {
+      const returnedIcon = matchSubscriptionMessageIconToSvg(PopOverIcon.CLOCK, Info)
+      expect(returnedIcon).toEqual(Clock)
+    })
+
+    it("should return LegalNotices icon if 'FILE' is passed", () => {
+      const returnedIcon = matchSubscriptionMessageIconToSvg(PopOverIcon.FILE, Info)
+      expect(returnedIcon).toEqual(LegalNotices)
+    })
+
+    it("should return Error icon if 'ERROR' is passed", () => {
+      const returnedIcon = matchSubscriptionMessageIconToSvg(PopOverIcon.ERROR, Info)
+      expect(returnedIcon).toEqual(ErrorIcon)
+    })
+
+    it("should return Warning icon if 'WARNING' is passed", () => {
+      const returnedIcon = matchSubscriptionMessageIconToSvg(PopOverIcon.WARNING, Info)
+      expect(returnedIcon).toEqual(Warning)
+    })
+
+    it("should return Info icon if 'INFO' is passed", () => {
+      const returnedIcon = matchSubscriptionMessageIconToSvg(PopOverIcon.INFO, Info)
+      expect(returnedIcon).toEqual(Info)
+    })
   })
-  it('should return no icon if undefined is passed and fallback is true', () => {
-    const returnedIcon = matchSubscriptionMessageIconToSvg(undefined, true)
-    expect(returnedIcon).toEqual(undefined)
+
+  describe('CallToActionIcon', () => {
+    it("should return EmailFiled icon if 'EMAIL' is passed", () => {
+      const returnedIcon = matchSubscriptionMessageIconToSvg(CallToActionIcon.EMAIL, Info)
+      expect(returnedIcon).toEqual(EmailFilled)
+    })
+
+    it("should return ExternalSiteFilled icon if 'EXTERNAL' is passed", () => {
+      const returnedIcon = matchSubscriptionMessageIconToSvg(CallToActionIcon.EXTERNAL, Info)
+      expect(returnedIcon).toEqual(ExternalSiteFilled)
+    })
+
+    it("should return Again icon if 'RETRY' is passed", () => {
+      const returnedIcon = matchSubscriptionMessageIconToSvg(CallToActionIcon.RETRY, Info)
+      expect(returnedIcon).toEqual(Again)
+    })
   })
-  it("should return Clock if 'CLOCK' is passed", () => {
-    const returnedIcon = matchSubscriptionMessageIconToSvg('CLOCK')
-    expect(returnedIcon).toEqual(BicolorClock)
-  })
-  it('should return Info if unknown string is passed and fallbackIcon is true', () => {
-    const returnedIcon = matchSubscriptionMessageIconToSvg('I am an unknown string', true)
+
+  it.each([undefined, null])('should give the fallback given no icon', (iconName) => {
+    const returnedIcon = matchSubscriptionMessageIconToSvg(iconName, Info)
     expect(returnedIcon).toEqual(Info)
   })
-  it('should return no icon if unknown string is passed and fallback is false', () => {
-    const returnedIcon = matchSubscriptionMessageIconToSvg('I am an unknown string')
+
+  it('should no return icon when given no icon and no fallback icon', () => {
+    const returnedIcon = matchSubscriptionMessageIconToSvg(undefined, undefined)
     expect(returnedIcon).toEqual(undefined)
   })
 })
--- src/features/profile/helpers/matchSubscriptionMessageIconToSvg.ts
+++ src/features/profile/helpers/matchSubscriptionMessageIconToSvg.ts
@@ -1,5 +1,8 @@
+import { FunctionComponent } from 'react'
+
+import { CallToActionIcon, PopOverIcon } from 'api/gen'
 import { Again } from 'ui/svg/icons/Again'
-import { BicolorClock } from 'ui/svg/icons/BicolorClock'
+import { Clock } from 'ui/svg/icons/BicolorClock'
 import { Warning } from 'ui/svg/icons/BicolorWarning'
 import { EmailFilled } from 'ui/svg/icons/EmailFilled'
 import { Error } from 'ui/svg/icons/Error'
@@ -7,32 +10,35 @@ import { ExternalSiteFilled } from 'ui/svg/icons/ExternalSiteFilled'
 import { Info } from 'ui/svg/icons/Info'
 import { LegalNotices } from 'ui/svg/icons/LegalNotices'
 import { MagnifyingGlass } from 'ui/svg/icons/MagnifyingGlass'
+import { IconInterface } from 'ui/svg/icons/types'
 
 export const matchSubscriptionMessageIconToSvg = (
-  iconName: string | undefined | null,
-  useFallbackIcon?: boolean | undefined
+  iconName: PopOverIcon | CallToActionIcon | undefined | null,
+  fallbackIcon?: FunctionComponent<IconInterface>
 ) => {
   switch (iconName) {
-    case undefined:
-    case null:
-      return undefined
-    case 'CLOCK':
-      return BicolorClock
-    case 'EXTERNAL':
-      return ExternalSiteFilled
-    case 'MAGNIFYING_GLASS':
+    case PopOverIcon.CLOCK:
+      return Clock
+    case PopOverIcon.MAGNIFYING_GLASS:
       return MagnifyingGlass
-    case 'ERROR':
+    case PopOverIcon.ERROR:
       return Error
-    case 'WARNING':
+    case PopOverIcon.WARNING:
       return Warning
-    case 'EMAIL':
-      return EmailFilled
-    case 'FILE':
+    case PopOverIcon.FILE:
       return LegalNotices
-    case 'RETRY':
+    case PopOverIcon.INFO:
+      return Info
+
+    case CallToActionIcon.EMAIL:
+      return EmailFilled
+    case CallToActionIcon.EXTERNAL:
+      return ExternalSiteFilled
+    case CallToActionIcon.RETRY:
       return Again
-    default:
-      return useFallbackIcon ? Info : undefined
+
+    case null:
+    case undefined:
+      return fallbackIcon
   }
 }
