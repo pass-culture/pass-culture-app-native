7b33a681ffcd89eca0122a6f39479713f6a87be9
Lucas Beneston <lucas.beneston@passculture.app>
2023-01-05 18:45:48 +0100

    (PC-19411) feat(SpellingHelp): add max with for SpellingHelp

--- src/ui/components/inputs/EmailInputWithSpellingHelp/SpellingHelp.tsx
+++ src/ui/components/inputs/EmailInputWithSpellingHelp/SpellingHelp.tsx
@@ -1,5 +1,6 @@
 import { MailSuggestion } from '@zootools/email-spell-checker/dist/lib/types'
 import React from 'react'
+import styled from 'styled-components/native'
 
 import { Banner } from 'ui/components/Banner'
 import { ButtonQuaternarySecondary } from 'ui/components/buttons/ButtonQuarternarySecondary'
@@ -17,16 +18,22 @@ export const SpellingHelp = ({ suggestedEmail, onEmailChange }: Props) => {
   }
 
   return (
-    <Banner message={`Veux-tu plutôt dire ${suggestedEmail.address}@${suggestedEmail.domain}`}>
-      <Spacer.Column numberOfSpaces={2} />
-      <ButtonQuaternarySecondary
-        numberOfLines={2}
-        justifyContent="flex-start"
-        onPress={replaceEmail}
-        icon={PlainArrowNext}
-        wording="Appliquer la modification"
-        inline
-      />
-    </Banner>
+    <Container>
+      <Banner message={`Veux-tu plutôt dire ${suggestedEmail.address}@${suggestedEmail.domain}`}>
+        <Spacer.Column numberOfSpaces={2} />
+        <ButtonQuaternarySecondary
+          numberOfLines={2}
+          justifyContent="flex-start"
+          onPress={replaceEmail}
+          icon={PlainArrowNext}
+          wording="Appliquer la modification"
+          inline
+        />
+      </Banner>
+    </Container>
   )
 }
+
+const Container = styled.View(({ theme }) => ({
+  maxWidth: theme.forms.maxWidth,
+}))
