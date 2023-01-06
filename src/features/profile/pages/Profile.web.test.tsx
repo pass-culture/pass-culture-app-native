2a1a27f7775bfc76e99d70f3a73d20384c28d5c5
Lucas Beneston <lucas.beneston@passculture.app>
2023-01-06 17:41:26 +0100

    (PC-19063) feat(profile): add accessibility tests for Profile

---
+++ src/features/profile/pages/Profile.web.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { render, checkAccessibilityFor } from 'tests/utils/web'
+
+import { Profile } from './Profile'
+
+jest.mock('react-query')
+
+describe('<Profile/>', () => {
+  describe('Accessibility', () => {
+    it('should not have basic accessibility issues', async () => {
+      const { container } = render(<Profile />)
+      const results = await checkAccessibilityFor(container, {
+        // TODO(LucasBeneston): Fix geolocation toggle accessibility errors
+        rules: {
+          'aria-toggle-field-name': { enabled: false },
+          'duplicate-id-aria': { enabled: false },
+        },
+      })
+      expect(results).toHaveNoViolations()
+    })
+  })
+})
