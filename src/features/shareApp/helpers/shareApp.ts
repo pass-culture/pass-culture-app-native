0761758f4848559d6431b29ee4084f385cc348db
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-01-09 17:31:45 +0100

    (PC-19508) feat(shareApp): Use specific url for testing (#4176)

--- src/features/shareApp/helpers/shareApp.ts
+++ src/features/shareApp/helpers/shareApp.ts
@@ -1,6 +1,12 @@
+import { env } from 'libs/environment'
 import { share } from 'libs/share'
 
-const url = 'https://passculture.onelink.me/SmRf/xthbokjg'
+const testUrl =
+  'https://app.testing.passculture.team/accueil?utm_campaign=test_growth&utm_source=in-app&utm_medium=in-app'
+const url = env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING
+  ? testUrl
+  : 'https://passculture.onelink.me/SmRf/xthbokjg'
+
 const message = `Profite toi aussi de tous les bons plans du pass Culture\u00a0: \n${url}`
 
 const shareAppContent = {
