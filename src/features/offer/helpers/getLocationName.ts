c10d6d30335487ae3685fd26a18073fc015ea3b1
Lucas Beneston <lucas.beneston@passculture.app>
2023-01-12 18:29:59 +0100

    (PC-19757) feat(offer): remove unused getLocationName

--- src/features/offer/helpers/getLocationName.ts
+++
@@ -1,4 +0,0 @@
-import { OfferVenueResponse } from 'api/gen'
-
-export const getLocationName = (venue: OfferVenueResponse, isDigital: boolean): string =>
-  isDigital ? venue.offerer.name : venue.publicName || venue.name
