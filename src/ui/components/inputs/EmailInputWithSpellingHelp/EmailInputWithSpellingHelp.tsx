1b180eb18bf30bf60e626311fc66b2e75c52fba2
Lucas Beneston <lucas.beneston@passculture.app>
2023-01-13 10:56:44 +0100

    (PC-19733) feat(signup): hide spellingHelp on click

--- src/ui/components/inputs/EmailInputWithSpellingHelp/EmailInputWithSpellingHelp.tsx
+++ src/ui/components/inputs/EmailInputWithSpellingHelp/EmailInputWithSpellingHelp.tsx
@@ -2,7 +2,6 @@ import React, { ComponentProps } from 'react'
 
 import { EmailInput } from 'ui/components/inputs/EmailInput/EmailInput'
 import { useEmailSpellingHelp } from 'ui/components/inputs/EmailInputWithSpellingHelp/useEmailSpellingHelp'
-import { Spacer } from 'ui/theme'
 
 import { SpellingHelp } from './SpellingHelp'
 
@@ -17,12 +16,7 @@ export const EmailInputWithSpellingHelp = ({
   return (
     <React.Fragment>
       <EmailInput {...props} onEmailChange={onEmailChange} />
-      {!!suggestedEmail && (
-        <React.Fragment>
-          <Spacer.Column numberOfSpaces={2} />
-          <SpellingHelp suggestedEmail={suggestedEmail} onEmailChange={onEmailChange} />
-        </React.Fragment>
-      )}
+      <SpellingHelp suggestedEmail={suggestedEmail} onEmailChange={onEmailChange} />
     </React.Fragment>
   )
 }
--- src/ui/components/inputs/EmailInputWithSpellingHelp/SpellingHelp.tsx
+++ src/ui/components/inputs/EmailInputWithSpellingHelp/SpellingHelp.tsx
@@ -1,5 +1,5 @@
 import { MailSuggestion } from '@zootools/email-spell-checker/dist/lib/types'
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components/native'
 
 import { Banner } from 'ui/components/Banner'
@@ -8,31 +8,43 @@ import { PlainArrowNext } from 'ui/svg/icons/PlainArrowNext'
 import { Spacer } from 'ui/theme'
 
 type Props = {
-  suggestedEmail: MailSuggestion
+  suggestedEmail?: MailSuggestion
   onEmailChange: (email: string) => void
 }
 
 export const SpellingHelp = ({ suggestedEmail, onEmailChange }: Props) => {
-  const replaceEmail = useCallback(() => {
-    onEmailChange(suggestedEmail.full)
-  }, [onEmailChange, suggestedEmail.full])
+  const [showBanner, setShowBanner] = useState(false)
 
-  return (
-    <Container>
-      <Banner
-        message={`Veux-tu plutôt dire ${suggestedEmail.address}@${suggestedEmail.domain}\u00a0?`}>
+  useEffect(() => {
+    if (suggestedEmail) setShowBanner(true)
+    return () => setShowBanner(false)
+  }, [suggestedEmail])
+
+  const replaceEmail = useCallback(() => {
+    setShowBanner(false)
+    if (suggestedEmail) onEmailChange(suggestedEmail.full)
+  }, [onEmailChange, suggestedEmail])
+
+  if (showBanner)
+    return (
+      <Container>
         <Spacer.Column numberOfSpaces={2} />
-        <ButtonQuaternarySecondary
-          numberOfLines={2}
-          justifyContent="flex-start"
-          onPress={replaceEmail}
-          icon={PlainArrowNext}
-          wording="Appliquer la modification"
-          inline
-        />
-      </Banner>
-    </Container>
-  )
+        <Banner
+          message={`Veux-tu plutôt dire ${suggestedEmail?.address}@${suggestedEmail?.domain}\u00a0?`}>
+          <Spacer.Column numberOfSpaces={2} />
+          <ButtonQuaternarySecondary
+            numberOfLines={2}
+            justifyContent="flex-start"
+            onPress={replaceEmail}
+            icon={PlainArrowNext}
+            wording="Appliquer la modification"
+            inline
+          />
+        </Banner>
+      </Container>
+    )
+
+  return <React.Fragment />
 }
 
 const Container = styled.View(({ theme }) => ({
