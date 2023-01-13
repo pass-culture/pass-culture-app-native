508ba2ff66ef4f961b3c62fa28fbdab624b58460
AnoukHello <anoukhello22@gmail.com>
2023-01-13 10:49:10 +0100

    (PC-18451) home: create GenericHome page

---
+++ src/features/home/pages/GenericHome.native.test.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+
+import * as showSkeletonAPI from 'features/home/api/useShowSkeleton'
+import { formattedVenuesModule } from 'features/home/fixtures/homepage.fixture'
+import { GenericHome } from 'features/home/pages/GenericHome'
+import { analytics } from 'libs/firebase/analytics'
+import { useNetInfoContext as useNetInfoContextDefault } from 'libs/network/NetInfoWrapper'
+import { BatchUser } from 'libs/react-native-batch'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render } from 'tests/utils'
+import { Typo } from 'ui/theme'
+
+const useShowSkeletonSpy = jest.spyOn(showSkeletonAPI, 'useShowSkeleton')
+
+jest.mock('libs/network/useNetInfo', () => jest.requireMock('@react-native-community/netinfo'))
+const mockUseNetInfoContext = useNetInfoContextDefault as jest.Mock
+
+jest.mock('features/auth/AuthContext', () => ({
+  useAuthContext: jest.fn(() => ({ isLoggedIn: true })),
+}))
+
+const modules = [formattedVenuesModule]
+const homeId = 'fake-id'
+const Header = <Typo.Title1>Header</Typo.Title1>
+
+describe('GenericHome', () => {
+  mockUseNetInfoContext.mockReturnValue({ isConnected: true })
+  useShowSkeletonSpy.mockReturnValue(false)
+
+  it('should render skeleton when useShowSkeleton is true', () => {
+    useShowSkeletonSpy.mockReturnValueOnce(true)
+    const home = renderGenericHome()
+    expect(home).toMatchSnapshot()
+  })
+
+  it('should render modules when useShowSkeleton is false', () => {
+    const home = renderGenericHome()
+    expect(home).toMatchSnapshot()
+  })
+
+  it('should render offline page when not connected', () => {
+    mockUseNetInfoContext.mockReturnValueOnce({ isConnected: false })
+    const { getByText } = renderGenericHome()
+    expect(getByText('Pas de réseau internet')).toBeTruthy()
+  })
+})
+
+describe('GenericHome page - Analytics', () => {
+  const nativeEventMiddle = {
+    layoutMeasurement: { height: 1000 },
+    contentOffset: { y: 400 }, // how far did we scroll
+    contentSize: { height: 1600 },
+  }
+  const nativeEventBottom = {
+    layoutMeasurement: { height: 1000 },
+    contentOffset: { y: 900 },
+    contentSize: { height: 1600 },
+  }
+
+  it('should trigger logEvent "AllModulesSeen" when reaching the end', () => {
+    const { getByTestId } = renderGenericHome()
+    const scrollView = getByTestId('homeBodyScrollView')
+
+    scrollView.props.onScroll({ nativeEvent: nativeEventMiddle })
+    expect(analytics.logAllModulesSeen).not.toHaveBeenCalled()
+    expect(BatchUser.trackEvent).not.toHaveBeenCalled()
+
+    scrollView.props.onScroll({ nativeEvent: nativeEventBottom })
+    expect(analytics.logAllModulesSeen).toHaveBeenCalledWith(1)
+    expect(BatchUser.trackEvent).toHaveBeenCalledWith('has_seen_all_the_homepage')
+  })
+
+  it('should trigger logEvent "AllModulesSeen" only once', () => {
+    const { getByTestId } = renderGenericHome()
+    const scrollView = getByTestId('homeBodyScrollView')
+
+    // 1st scroll to bottom => trigger
+    scrollView.props.onScroll({ nativeEvent: nativeEventBottom })
+    expect(analytics.logAllModulesSeen).toHaveBeenCalledWith(1)
+
+    jest.clearAllMocks()
+
+    // 2nd scroll to bottom => NOT trigger
+    scrollView.props.onScroll({ nativeEvent: nativeEventMiddle })
+    scrollView.props.onScroll({ nativeEvent: nativeEventBottom })
+
+    expect(analytics.logAllModulesSeen).not.toHaveBeenCalled()
+  })
+})
+
+function renderGenericHome() {
+  return render(
+    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+    reactQueryProviderHOC(<GenericHome modules={modules} Header={Header} homeId={homeId} />)
+  )
+}
---
+++ src/features/home/pages/GenericHome.tsx
@@ -0,0 +1,155 @@
+import { useScrollToTop } from '@react-navigation/native'
+import React, { FunctionComponent, useCallback, useEffect, useRef, useState } from 'react'
+import {
+  FlatList,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Platform,
+  ScrollView,
+} from 'react-native'
+import styled from 'styled-components/native'
+
+import { useShowSkeleton } from 'features/home/api/useShowSkeleton'
+import { HomeBodyPlaceholder } from 'features/home/components/HomeBodyPlaceholder'
+import { HomeModule } from 'features/home/components/modules/HomeModule'
+import { HomepageModule } from 'features/home/types'
+import { analytics, isCloseToBottom } from 'libs/firebase/analytics'
+import useFunctionOnce from 'libs/hooks/useFunctionOnce'
+import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
+import { OfflinePage } from 'libs/network/OfflinePage'
+import { BatchEvent, BatchUser } from 'libs/react-native-batch'
+import { theme } from 'theme'
+import { Spinner } from 'ui/components/Spinner'
+import { getSpacing, Spacer } from 'ui/theme'
+
+type GenericHomeProps = {
+  Header: JSX.Element
+  modules: HomepageModule[]
+  homeId?: string
+}
+const keyExtractor = (item: HomepageModule) => item.id
+
+const renderModule = (
+  { item, index }: { item: HomepageModule; index: number },
+  homeId: string | undefined
+) => <HomeModule item={item} index={index} homeEntryId={homeId} />
+
+const FooterComponent = ({ isLoading }: { isLoading: boolean }) => {
+  return (
+    <React.Fragment>
+      {!!isLoading && (
+        <FooterContainer>
+          <Spinner />
+        </FooterContainer>
+      )}
+      <Spacer.TabBar />
+    </React.Fragment>
+  )
+}
+
+export const OnlineHome: FunctionComponent<GenericHomeProps> = ({ Header, modules, homeId }) => {
+  const logHasSeenAllModules = useFunctionOnce(() => analytics.logAllModulesSeen(modules.length))
+  const trackEventHasSeenAllModules = useFunctionOnce(() =>
+    BatchUser.trackEvent(BatchEvent.hasSeenAllTheHomepage)
+  )
+  const showSkeleton = useShowSkeleton()
+  const initialNumToRender = 10
+  const maxToRenderPerBatch = 5
+  const [maxIndex, setMaxIndex] = useState(initialNumToRender)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const modulesToDisplay = Platform.OS === 'web' ? modules : modules.slice(0, maxIndex)
+
+  const scrollRef = useRef<FlatList>(null)
+  useScrollToTop(scrollRef)
+
+  const onScroll = useCallback(
+    ({ nativeEvent }: NativeSyntheticEvent<NativeScrollEvent>) => {
+      if (isCloseToBottom({ ...nativeEvent, padding: theme.minScreenHeight * 2 })) {
+        if (Platform.OS !== 'web' && maxIndex < modules.length) {
+          setIsLoading(true)
+          setMaxIndex(maxIndex + maxToRenderPerBatch)
+        }
+      }
+      if (isCloseToBottom(nativeEvent) && modulesToDisplay.length === modules.length) {
+        trackEventHasSeenAllModules()
+        logHasSeenAllModules()
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [modules.length, modulesToDisplay.length]
+  )
+
+  useEffect(() => {
+    // We use this to load more modules, in case the content size doesn't change after the load triggered by onEndReached (i.e. no new modules were shown).
+    const loadMore = setInterval(() => {
+      if (maxIndex < modules.length && isLoading) {
+        setMaxIndex(maxIndex + maxToRenderPerBatch)
+      } else {
+        setIsLoading(false)
+      }
+    }, 3000)
+
+    return () => clearInterval(loadMore)
+  }, [modules.length, isLoading, maxIndex])
+
+  return (
+    <Container>
+      {showSkeleton ? (
+        <ScrollView
+          testID="homeScrollView"
+          scrollEventThrottle={400}
+          bounces={false}
+          scrollEnabled={false}>
+          {Header}
+          <HomeBodyPlaceholder />
+          <Spacer.TabBar />
+        </ScrollView>
+      ) : (
+        <React.Fragment />
+      )}
+      <HomeBodyLoadingContainer hide={showSkeleton}>
+        <FlatList
+          ref={scrollRef}
+          testID="homeBodyScrollView"
+          scrollEventThrottle={1000}
+          onScroll={onScroll}
+          data={modulesToDisplay}
+          renderItem={({ item, index }) => renderModule({ item, index }, homeId)}
+          keyExtractor={keyExtractor}
+          ListFooterComponent={<FooterComponent isLoading={isLoading} />}
+          ListHeaderComponent={Header}
+          initialNumToRender={initialNumToRender}
+          removeClippedSubviews={false}
+          onContentSizeChange={() => setTimeout(() => setIsLoading(false), 1000)}
+          bounces
+        />
+      </HomeBodyLoadingContainer>
+      <Spacer.Column numberOfSpaces={6} />
+    </Container>
+  )
+}
+
+export const GenericHome: FunctionComponent<GenericHomeProps> = (props) => {
+  const netInfo = useNetInfoContext()
+  if (netInfo.isConnected) {
+    return <OnlineHome {...props} />
+  }
+  return <OfflinePage />
+}
+
+const HomeBodyLoadingContainer = styled.View<{ hide: boolean }>(({ hide }) => ({
+  height: hide ? 0 : '100%',
+  overflow: 'hidden',
+}))
+
+const Container = styled.View({
+  flexBasis: 1,
+  flexGrow: 1,
+  flexShrink: 0,
+})
+
+const FooterContainer = styled.View({
+  paddingTop: getSpacing(2),
+  paddingBottom: getSpacing(10),
+})
