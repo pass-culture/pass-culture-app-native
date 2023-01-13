7fa7c65f9df180846e1d37b1ba2f9094bbfe66c2
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2023-01-13 10:38:50 +0100

    (PC-19743) text(search): fix import so it fixes tests

--- src/features/search/components/Buttons/FilterButton/FilterButton.tsx
+++ src/features/search/components/Buttons/FilterButton/FilterButton.tsx
@@ -1,4 +1,4 @@
-import { useRoute } from '@react-navigation/core'
+import { useRoute } from '@react-navigation/native'
 import React, { FunctionComponent, useCallback } from 'react'
 import styled from 'styled-components/native'
 
