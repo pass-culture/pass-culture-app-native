2781302990b4c10bc83a647f839118e5d5345438
« YassinL » <yassin.leclercq1@gmail.com>
2023-01-16 11:48:23 +0100

    dropme

--- scripts/check_branch.sh
+++ scripts/check_branch.sh
@@ -5,18 +5,18 @@ set -e
 check_branch() {
   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
   echo $CURRENT_BRANCH
-  if [ "$CURRENT_BRANCH" != "master" ]; then
-    read -p "Are you sure you want to deploy this branch? (Yy/Nn)" -n 1 -r
-    echo # step a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-      echo "YOU ARE DEPLOYING $CURRENT_BRANCH"
-    else
-      echo "Deployment aborted, checkout master to deploy"
-      exit 1
-    fi
-  else
-    git pull
-  fi
+  # if [ "$CURRENT_BRANCH" != "master" ]; then
+  #   read -p "Are you sure you want to deploy this branch? (Yy/Nn)" -n 1 -r
+  #   echo # step a new line
+  #   if [[ $REPLY =~ ^[Yy]$ ]]; then
+  #     echo "YOU ARE DEPLOYING $CURRENT_BRANCH"
+  #   else
+  #     echo "Deployment aborted, checkout master to deploy"
+  #     exit 1
+  #   fi
+  # else
+  #   git pull
+  # fi
 }
 
 check_branch
