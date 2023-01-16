8ba510337a2de44f74337f82a26b90c2de15b2ec
Lucas Beneston <lucas.beneston@passculture.app>
2023-01-16 10:13:13 +0100

    (PC-17806) feat(auth): remove duplicate useNextSubscriptionStep and useDepositAmountsByAge

--- src/features/auth/helpers/__mocks__/useDepositAmountsByAge.ts
+++
@@ -1,6 +0,0 @@
-export const useDepositAmountsByAge = jest.fn().mockReturnValue({
-  fifteenYearsOldDeposit: '20\u00a0€',
-  sixteenYearsOldDeposit: '30\u00a0€',
-  seventeenYearsOldDeposit: '30\u00a0€',
-  eighteenYearsOldDeposit: '300\u00a0€',
-})
