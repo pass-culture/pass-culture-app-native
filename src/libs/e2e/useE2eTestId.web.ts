18c264b750028c0725615fcb0c1c9157a9fd5090
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2023-01-17 18:00:33 +0100

    (PC-19912) fix(e2e): added web utils useE2eTestId

--- src/libs/e2e/useE2eTestId.web.ts
+++ src/libs/e2e/useE2eTestId.web.ts
@@ -6,10 +6,11 @@ import { useIsE2e } from 'libs/e2e/E2eContextProvider'
 // otherwise you can use the accessibilityAndTestId, which will keep the accessibilityLabel set outside of e2e execution
 export function useE2eTestId(frenchText: string) {
   const isE2e = useIsE2e()
-  return useMemo<{ testID: string; accessibilityLabel?: string }>(
+  return useMemo<{ testID: string; accessibilityLabel?: string; 'data-testid': string }>(
     () => ({
       testID: frenchText,
       accessibilityLabel: isE2e ? frenchText : undefined,
+      ['data-testid']: frenchText,
     }),
     [frenchText, isE2e]
   )
