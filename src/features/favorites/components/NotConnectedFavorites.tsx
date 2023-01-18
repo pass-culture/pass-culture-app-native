f1285b5aaaa28a6d09940e967af806a480857099
matthias-prevost <matthias.prevost@gmail.com>
2023-01-18 14:24:42 +0100

    Using GenericInfoPageWhite

--- src/features/favorites/components/NotConnectedFavorites.tsx
+++ src/features/favorites/components/NotConnectedFavorites.tsx
@@ -5,75 +5,47 @@ import { AuthenticationButton } from 'features/auth/components/AuthenticationBut
 import { analytics } from 'libs/firebase/analytics'
 import { ButtonWithLinearGradient } from 'ui/components/buttons/buttonWithLinearGradient/ButtonWithLinearGradient'
 import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
+import { GenericInfoPageWhite } from 'ui/pages/GenericInfoPageWhite'
 import { BicolorUserFavorite } from 'ui/svg/icons/BicolorUserFavorite'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 
-export const NotConnectedFavorites = () => {
-  return (
-    <Container>
-      <Spacer.TopScreen />
-      <Spacer.Flex />
-      <StyledUserFavorite />
-      <CenteredContainer>
-        <StyledTitle4>Identifie-toi pour retrouver tes favoris</StyledTitle4>
+export const NotConnectedFavorites = () => (
+  <GenericInfoPageWhite
+    title="Identifie-toi pour retrouver tes favoris"
+    subtitle="Ton compte te permettra de retrouver tous tes bons plans en un clin d’oeil&nbsp;!"
+    titleComponent={StyledTitle4}
+    subtitleComponent={CenteredText}
+    icon={BicolorUserFavorite}
+    separateIconFromTitle={false}>
+    <Row>
+      <ButtonContainer>
+        <InternalTouchableLink
+          as={ButtonWithLinearGradient}
+          wording="Créer un compte"
+          navigateTo={{ screen: 'SignupForm' }}
+          onBeforeNavigate={analytics.logSignUpFromFavorite}
+          buttonHeight="tall"
+        />
         <Spacer.Column numberOfSpaces={4} />
-
-        <TextContainer>
-          <CenteredText>
-            Ton compte te permettra de retrouver tous tes bons plans en un clin d’oeil&nbsp;!
-          </CenteredText>
-        </TextContainer>
-      </CenteredContainer>
-
-      <Row>
-        <ButtonContainer>
-          <InternalTouchableLink
-            as={ButtonWithLinearGradient}
-            wording="Créer un compte"
-            navigateTo={{ screen: 'SignupForm' }}
-            onBeforeNavigate={analytics.logSignUpFromFavorite}
-            buttonHeight="tall"
-          />
-          <Spacer.Column numberOfSpaces={4} />
-          <StyledAuthenticationButton
-            type="login"
-            onAdditionalPress={analytics.logSignInFromFavorite}
-          />
-        </ButtonContainer>
-      </Row>
-      <Spacer.Column numberOfSpaces={12} />
-      <Spacer.Flex />
-      <Spacer.BottomScreen />
-    </Container>
-  )
-}
-
-const StyledUserFavorite = styled(BicolorUserFavorite).attrs(({ theme }) => ({
-  size: theme.illustrations.sizes.fullPage,
-}))``
-
-const Container = styled.View(({ theme }) => ({
-  flex: 1,
-  alignItems: 'center',
-  marginHorizontal: theme.contentPage.marginHorizontal,
-}))
+        <StyledAuthenticationButton
+          type="login"
+          onAdditionalPress={analytics.logSignInFromFavorite}
+        />
+      </ButtonContainer>
+    </Row>
+    <Spacer.Column numberOfSpaces={12} />
+    <Spacer.BottomScreen />
+  </GenericInfoPageWhite>
+)
 
 const Row = styled.View({ flexDirection: 'row' })
 
 const StyledTitle4 = styled(Typo.Title4)({
   textAlign: 'center',
-})
-
-const CenteredContainer = styled.View({
-  flexGrow: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
-  textAlign: 'center',
+  marginBottom: getSpacing(4),
 })
 
 const ButtonContainer = styled.View({ flex: 1 })
-
-const TextContainer = styled.View({ maxWidth: getSpacing(88) })
 
 const CenteredText = styled(Typo.Body)({
   textAlign: 'center',
