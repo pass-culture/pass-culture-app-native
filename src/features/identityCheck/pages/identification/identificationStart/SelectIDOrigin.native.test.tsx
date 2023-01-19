c9aafd976bcf69897ac6ff4e244d54321594d3df
AnoukHello <anoukhello22@gmail.com>
2023-01-19 16:23:01 +0100

    (PC-19747) SelectIDOrigin: add amplitude click events

--- src/features/identityCheck/pages/identification/identificationStart/SelectIDOrigin.native.test.tsx
+++ src/features/identityCheck/pages/identification/identificationStart/SelectIDOrigin.native.test.tsx
@@ -18,16 +18,16 @@ describe('SelectIDOrigin', () => {
     expect(renderAPI).toMatchSnapshot()
   })
 
-  it('should navigate to SelectIDStatus on press french HeroButtonList', () => {
+  it('should navigate to SelectIDStatus on press french HeroButtonList', async () => {
     const { getByTestId } = render(<SelectIDOrigin />)
 
     const HeroButtonListFrench = getByTestId('J’ai une carte d’identité ou un passeport français')
     fireEvent.press(HeroButtonListFrench)
 
-    expect(navigate).toHaveBeenCalledWith('SelectIDStatus', undefined)
+    await waitFor(() => expect(navigate).toHaveBeenCalledWith('SelectIDStatus', undefined))
   })
 
-  it('should navigate to DMSIntroduction with foreign parameter on press foreign button', () => {
+  it('should navigate to DMSIntroduction with foreign parameter on press foreign HeroButtonList', async () => {
     const { getByText } = render(<SelectIDOrigin />)
 
     const ButtonForeign = getByText(
@@ -35,7 +35,9 @@ describe('SelectIDOrigin', () => {
     )
     fireEvent.press(ButtonForeign)
 
-    expect(navigate).toHaveBeenCalledWith('DMSIntroduction', { isForeignDMSInformation: true })
+    await waitFor(() =>
+      expect(navigate).toHaveBeenCalledWith('DMSIntroduction', { isForeignDMSInformation: true })
+    )
   })
 
   it('should send a amplitude event when the screen is mounted', async () => {
@@ -43,6 +45,36 @@ describe('SelectIDOrigin', () => {
 
     await waitFor(() =>
       expect(amplitude.logEvent).toHaveBeenNthCalledWith(1, 'screen_view_select_id_origin')
+    )
+  })
+
+  it('should send an amplitude event set_id_origin_clicked with french type on press french HeroButtonList', async () => {
+    const { getByTestId } = render(<SelectIDOrigin />)
+
+    const HeroButtonListFrench = getByTestId('J’ai une carte d’identité ou un passeport français')
+    fireEvent.press(HeroButtonListFrench)
+
+    await waitFor(() =>
+      // first call will be the event screen_view_select_id_origin on mount
+      expect(amplitude.logEvent).toHaveBeenNthCalledWith(2, 'set_id_origin_clicked', {
+        type: 'France',
+      })
+    )
+  })
+
+  it('should send an amplitude event set_id_origin_clicked with foregin type on press foreign HeroButtonList', async () => {
+    const { getByText } = render(<SelectIDOrigin />)
+
+    const ButtonForeign = getByText(
+      'J’ai un titre de séjour, une carte d’identité ou un passeport étranger.'
+    )
+    fireEvent.press(ButtonForeign)
+
+    await waitFor(() =>
+      // first call will be the event screen_view_select_id_origin on mount
+      expect(amplitude.logEvent).toHaveBeenNthCalledWith(2, 'set_id_origin_clicked', {
+        type: 'Foreign',
+      })
     )
   })
 })
