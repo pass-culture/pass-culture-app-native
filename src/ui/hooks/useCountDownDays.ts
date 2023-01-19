fb0f74352ba0c53f6fe0ea0f9f1a3453f55d0d26
« YassinL » <yassin.leclercq1@gmail.com>
2023-01-19 09:41:19 +0100

    (PC-19626) fix(useCountDownDays): rewording

--- src/ui/hooks/useCountDownDays.ts
+++ src/ui/hooks/useCountDownDays.ts
@@ -5,16 +5,16 @@ import { clearLocalInterval } from 'libs/timer'
 export const useCountdownDays = (dateCreated: string) => {
   const countDownDate = new Date(dateCreated).getTime()
 
-  const milliseconds = 1000
-  const seconds = 60
-  const minutes = 60
-  const hours = 24
-  const days = 30
+  const MILLISECONDS = 1000
+  const SECONDS = 60
+  const MINUTES = 60
+  const HOURS = 24
+  const DAYS = 30
 
   const daysUntilExpiration =
     Math.floor(
-      (countDownDate - new Date().getTime()) / (milliseconds * seconds * minutes * hours)
-    ) + days
+      (countDownDate - new Date().getTime()) / (MILLISECONDS * SECONDS * MINUTES * HOURS)
+    ) + DAYS
 
   const [daysLeft, setDaysLeft] = useState(daysUntilExpiration)
   const intervalRef = useRef<NodeJS.Timeout | null>(null)
