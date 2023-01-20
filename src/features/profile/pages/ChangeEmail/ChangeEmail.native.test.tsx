67f3f356b072d2d302b172aa1520c860b149dc01
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-01-20 15:32:00 +0100

    (PC-19543) test(ChangeEmail): Refactor and improve tests (#4262)

--- src/features/profile/pages/ChangeEmail/ChangeEmail.native.test.tsx
+++ src/features/profile/pages/ChangeEmail/ChangeEmail.native.test.tsx
@@ -1,40 +1,20 @@
+import { rest } from 'msw'
 import React from 'react'
-import { useMutation, useQuery } from 'react-query'
-import waitForExpect from 'wait-for-expect'
 
 import { navigate } from '__mocks__/@react-navigation/native'
 import { CHANGE_EMAIL_ERROR_CODE } from 'features/profile/enums'
+import { env } from 'libs/environment'
 import { analytics } from 'libs/firebase/analytics'
-import { act, fireEvent, render } from 'tests/utils'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { server } from 'tests/server'
+import { act, fireEvent, render, screen, superFlushWithAct, waitFor } from 'tests/utils'
+import { SNACK_BAR_TIME_OUT } from 'ui/components/snackBar/SnackBarContext'
 import { SnackBarHelperSettings } from 'ui/components/snackBar/types'
 
 import { ChangeEmail } from './ChangeEmail'
 
-jest.mock('react-query')
 jest.mock('features/auth/context/AuthContext')
 jest.useFakeTimers()
-const mockedUseMutation = useMutation as jest.Mock
-const mockedUseQuery = useQuery as jest.Mock
-
-const mockUseMutationSuccess = () => {
-  // @ts-ignore we don't use the other properties of UseMutationResult (such as failureCount)
-  mockedUseMutation.mockImplementation((mutationFunction, { onSuccess }) => ({
-    mutationFunction,
-    mutationOptions: { onSuccess },
-    mutate: () => onSuccess(),
-  }))
-}
-
-const mockUseMutationError = (code: CHANGE_EMAIL_ERROR_CODE) => {
-  // @ts-ignore we don't use the other properties of UseMutationResult (such as failureCount)
-  mockedUseMutation.mockImplementation((mutationFunction, { onError }) => ({
-    mutationFunction,
-    mutationOptions: { onError },
-    mutate: () => {
-      onError({ content: { code } })
-    },
-  }))
-}
 
 const mockShowSuccessSnackBar = jest.fn()
 const mockShowErrorSnackBar = jest.fn()
@@ -43,155 +23,238 @@ jest.mock('ui/components/snackBar/SnackBarContext', () => ({
     showSuccessSnackBar: jest.fn((props: SnackBarHelperSettings) => mockShowSuccessSnackBar(props)),
     showErrorSnackBar: jest.fn((props: SnackBarHelperSettings) => mockShowErrorSnackBar(props)),
   }),
-  SNACK_BAR_TIME_OUT: 5000,
 }))
 
 describe('<ChangeEmail/>', () => {
-  beforeEach(() => {
-    mockedUseQuery.mockImplementation(() => ({}))
-    mockUseMutationSuccess()
-  })
+  beforeEach(simulateUpdateEmailSuccess)
+
   it('should render correctly', () => {
-    const renderAPI = render(<ChangeEmail />)
-    expect(renderAPI.toJSON()).toMatchSnapshot()
+    const renderAPI = renderChangeEmail()
+    expect(renderAPI).toMatchSnapshot()
   })
 
-  it('should render and log correctly when an email change is already in progress', async () => {
-    mockedUseQuery.mockImplementationOnce(() => ({
-      data: { expiration: '2021-12-07T13:45:05.812190' },
-    }))
-    const renderAPI = render(<ChangeEmail />)
-    expect(renderAPI.toJSON()).toMatchSnapshot()
+  describe('email change already in progress', () => {
+    it('should render correctly', async () => {
+      simulateCurrentEmailChange()
+      const renderAPI = renderChangeEmail()
 
-    await waitForExpect(() => {
-      expect(analytics.logConsultDisclaimerValidationMail).toHaveBeenCalledTimes(1)
+      await superFlushWithAct()
+      expect(renderAPI).toMatchSnapshot()
+    })
+
+    it('should log analytics', async () => {
+      simulateCurrentEmailChange()
+      renderChangeEmail()
+
+      await waitFor(() => {
+        expect(analytics.logConsultDisclaimerValidationMail).toHaveBeenCalledTimes(1)
+      })
     })
   })
 
-  it.each`
-    password          | email                | isDisabled
-    ${'password>=12'} | ${'valid@email.com'} | ${false}
-    ${'password>=12'} | ${'invalid@email'}   | ${true}
-    ${'password<12'}  | ${'valid@email.com'} | ${true}
-    ${'password>=12'} | ${''}                | ${true}
-  `(
-    'CTA "Enregistrer les modifications" (disabled=$isDisabled) with background color = $backgroundColor if password = "$password" and email = $email',
-    async ({ password, email, isDisabled }) => {
-      const { getByPlaceholderText, getByLabelText } = render(<ChangeEmail />)
+  describe('submit button', () => {
+    it('should be disabled by default', () => {
+      const { getByLabelText } = renderChangeEmail()
       const submitButton = getByLabelText('Enregistrer les modifications')
-      expect(submitButton).toBeDisabled()
 
-      await act(async () => {
-        const passwordInput = getByPlaceholderText('Ton mot de passe')
-        fireEvent.changeText(passwordInput, password)
+      expect(submitButton).toBeDisabled()
+    })
+
+    it('should be enabled when form is valid', async () => {
+      const { getByLabelText } = renderChangeEmail()
+      const submitButton = getByLabelText('Enregistrer les modifications')
+
+      await fillInputs({ email: 'valid@email.com', password: 'password>=12' })
+
+      expect(submitButton).toBeEnabled()
+    })
+
+    it.each`
+      password          | email
+      ${'password>=12'} | ${'invalid@email'}
+      ${'password<12'}  | ${'valid@email.com'}
+      ${'password>=12'} | ${''}
+    `('should be disabled when form is invalid', async ({ password, email }) => {
+      const { getByLabelText } = renderChangeEmail()
+      const submitButton = getByLabelText('Enregistrer les modifications')
+
+      await fillInputs({ email, password })
+
+      expect(submitButton).toBeDisabled()
+    })
+  })
+
+  describe('When email change succeeds', () => {
+    it('should navigate to Profile ', async () => {
+      renderChangeEmail()
+
+      await fillInputs({})
+
+      await submitForm()
+
+      expect(navigate).toBeCalledWith('TabNavigator', { screen: 'Profile' })
+    })
+
+    it('should show success snackbar', async () => {
+      renderChangeEmail()
+
+      await fillInputs({})
+
+      await submitForm()
+
+      expect(mockShowSuccessSnackBar).toBeCalledWith({
+        message:
+          'E-mail envoyé\u00a0! Tu as 24h pour activer ta nouvelle adresse. Si tu ne le trouves pas, pense à vérifier tes spams.',
+        timeout: SNACK_BAR_TIME_OUT,
       })
-      await act(async () => {
-        const emailInput = getByPlaceholderText('tonadresse@email.com')
-        fireEvent.changeText(emailInput, email)
-      })
-      expect(submitButton)[isDisabled ? 'toBeDisabled' : 'toBeEnabled']()
-    }
-  )
+    })
+
+    it('should log analytics', async () => {
+      renderChangeEmail()
+
+      await fillInputs({})
+
+      await submitForm()
+
+      expect(analytics.logSaveNewMail).toHaveBeenCalledTimes(1)
+    })
+  })
 
   it('should display "same email" error if I entered the same email (case insensitive)', async () => {
-    const { getByPlaceholderText, getByLabelText, queryByText } = render(<ChangeEmail />)
+    const { getByLabelText, queryByText } = renderChangeEmail()
     const submitButton = getByLabelText('Enregistrer les modifications')
+
+    await fillInputs({ email: 'EMAIL@domain.ext' })
+
     expect(submitButton).toBeDisabled()
 
-    await act(async () => {
-      const passwordInput = getByPlaceholderText('Ton mot de passe')
-      fireEvent.changeText(passwordInput, 'password>=12')
+    act(() => {
+      jest.advanceTimersByTime(600)
     })
-    await act(async () => {
-      const emailInput = getByPlaceholderText('tonadresse@email.com')
-      fireEvent.changeText(emailInput, 'EMAIL@domain.ext')
-    })
-    expect(submitButton).toBeDisabled()
 
-    await act(async () => {
-      await jest.advanceTimersByTime(600)
-    })
     const errorMessage = queryByText('L’e-mail saisi est identique à ton e-mail actuel')
     expect(errorMessage).toBeTruthy()
   })
 
-  it('should navigate to Profile and log event if the API call is ok', async () => {
-    const { getByPlaceholderText, getByLabelText } = render(<ChangeEmail />)
-    const submitButton = getByLabelText('Enregistrer les modifications')
-    await act(async () => {
-      const emailInput = getByPlaceholderText('tonadresse@email.com')
-      fireEvent.changeText(emailInput, 'tonadresse@email.com')
-    })
-    await act(async () => {
-      const passwordInput = getByPlaceholderText('Ton mot de passe')
-      fireEvent.changeText(passwordInput, 'password>=12')
+  describe('When user gives wrong password', () => {
+    beforeEach(() => simulateUpdateEmailError(CHANGE_EMAIL_ERROR_CODE.INVALID_PASSWORD))
+
+    it('should not navigate', async () => {
+      renderChangeEmail()
+
+      await fillInputs({})
+
+      await submitForm()
+
+      expect(navigate).not.toBeCalled()
     })
 
-    await act(async () => {
-      fireEvent.press(submitButton)
+    it('should show error message', async () => {
+      const { queryByText } = renderChangeEmail()
+
+      await fillInputs({})
+
+      await submitForm()
+
+      const errorMessage = queryByText('Mot de passe incorrect')
+      expect(errorMessage).toBeTruthy()
     })
 
-    expect(navigate).toBeCalledWith('TabNavigator', { screen: 'Profile' })
-    expect(mockShowSuccessSnackBar).toBeCalledWith({
-      message:
-        'E-mail envoyé\u00a0! Tu as 24h pour activer ta nouvelle adresse. Si tu ne le trouves pas, pense à vérifier tes spams.',
-      timeout: 5000,
+    it('should log analytics', async () => {
+      renderChangeEmail()
+
+      await fillInputs({})
+
+      await submitForm()
+
+      expect(analytics.logErrorSavingNewEmail).toHaveBeenCalledWith(
+        CHANGE_EMAIL_ERROR_CODE.INVALID_PASSWORD
+      )
     })
-    expect(analytics.logSaveNewMail).toHaveBeenCalledTimes(1)
   })
 
-  it('should show error message if the user gave a wrong password', async () => {
-    mockUseMutationError(CHANGE_EMAIL_ERROR_CODE.INVALID_PASSWORD)
+  describe('When user has reached attempts limit', () => {
+    beforeEach(() => simulateUpdateEmailError(CHANGE_EMAIL_ERROR_CODE.EMAIL_UPDATE_ATTEMPTS_LIMIT))
 
-    const { getByPlaceholderText, getByLabelText, queryByText } = render(<ChangeEmail />)
-    await act(async () => {
-      const emailInput = getByPlaceholderText('tonadresse@email.com')
-      fireEvent.changeText(emailInput, 'tonadresse@email.com')
+    it('should not navigate', async () => {
+      renderChangeEmail()
+
+      await fillInputs({})
+
+      await submitForm()
+
+      expect(navigate).not.toBeCalled()
     })
 
-    await act(async () => {
-      const passwordInput = getByPlaceholderText('Ton mot de passe')
-      fireEvent.changeText(passwordInput, 'password>=12')
+    it('should show the generic error message', async () => {
+      renderChangeEmail()
+
+      await fillInputs({})
+
+      await submitForm()
+
+      expect(mockShowErrorSnackBar).toBeCalledWith({
+        message: `Une erreur s’est produite pendant la modification de ton e-mail. Réessaie plus tard.`,
+        timeout: SNACK_BAR_TIME_OUT,
+      })
     })
 
-    await act(async () => {
-      const submitButton = getByLabelText('Enregistrer les modifications')
-      fireEvent.press(submitButton)
-    })
+    it('should log analytics', async () => {
+      renderChangeEmail()
 
-    expect(navigate).not.toBeCalled()
-    const errorMessage = queryByText('Mot de passe incorrect')
-    expect(errorMessage).toBeTruthy()
-    expect(analytics.logErrorSavingNewEmail).toHaveBeenCalledWith(
-      CHANGE_EMAIL_ERROR_CODE.INVALID_PASSWORD
-    )
-  })
+      await fillInputs({})
 
-  it('should show the generic error message if the API call returns an attempts limit error', async () => {
-    mockUseMutationError(CHANGE_EMAIL_ERROR_CODE.EMAIL_UPDATE_ATTEMPTS_LIMIT)
+      await submitForm()
 
-    const { getByPlaceholderText, getByLabelText } = render(<ChangeEmail />)
-    const submitButton = getByLabelText('Enregistrer les modifications')
-    await act(async () => {
-      const emailInput = getByPlaceholderText('tonadresse@email.com')
-      fireEvent.changeText(emailInput, 'tonadresse@email.com')
+      expect(analytics.logErrorSavingNewEmail).toHaveBeenCalledWith(
+        CHANGE_EMAIL_ERROR_CODE.EMAIL_UPDATE_ATTEMPTS_LIMIT
+      )
     })
-    await act(async () => {
-      const passwordInput = getByPlaceholderText('Ton mot de passe')
-      fireEvent.changeText(passwordInput, 'password>=12')
-    })
-
-    await act(async () => {
-      fireEvent.press(submitButton)
-    })
-
-    expect(navigate).not.toBeCalled()
-    expect(mockShowErrorSnackBar).toBeCalledWith({
-      message: `Une erreur s’est produite pendant la modification de ton e-mail. Réessaie plus tard.`,
-      timeout: 5000,
-    })
-    expect(analytics.logErrorSavingNewEmail).toHaveBeenCalledWith(
-      CHANGE_EMAIL_ERROR_CODE.EMAIL_UPDATE_ATTEMPTS_LIMIT
-    )
   })
 })
+
+// eslint-disable-next-line local-rules/no-react-query-provider-hoc
+const renderChangeEmail = () => render(reactQueryProviderHOC(<ChangeEmail />))
+
+const fillInputs = async ({ email, password }: { email?: string; password?: string }) => {
+  const passwordInput = screen.getByPlaceholderText('Ton mot de passe')
+  fireEvent.changeText(passwordInput, password ?? 'password>=12')
+
+  const emailInput = screen.getByPlaceholderText('tonadresse@email.com')
+  fireEvent.changeText(emailInput, email ?? 'tonadresse@email.com')
+
+  // To avoid CI flakiness
+  await superFlushWithAct()
+}
+
+const submitForm = async () => {
+  const submitButton = screen.getByLabelText('Enregistrer les modifications')
+  fireEvent.press(submitButton)
+
+  // To avoid CI flakiness
+  await superFlushWithAct()
+}
+
+function simulateUpdateEmailSuccess() {
+  server.use(
+    rest.post(env.API_BASE_URL + '/native/v1/profile/update_email', async (_, res, ctx) =>
+      res.once(ctx.status(200))
+    )
+  )
+}
+
+function simulateUpdateEmailError(code: CHANGE_EMAIL_ERROR_CODE) {
+  server.use(
+    rest.post(env.API_BASE_URL + '/native/v1/profile/update_email', async (_, res, ctx) =>
+      res.once(ctx.status(400), ctx.json({ code }))
+    )
+  )
+}
+
+function simulateCurrentEmailChange() {
+  server.use(
+    rest.get(env.API_BASE_URL + '/native/v1/profile/token_expiration', async (_, res, ctx) =>
+      res.once(ctx.status(200), ctx.json({ expiration: '2021-12-07T13:45:05.812190' }))
+    )
+  )
+}
