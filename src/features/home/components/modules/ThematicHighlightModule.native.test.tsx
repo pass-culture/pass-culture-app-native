c073f40c41ccf1f78cb2ebd0fec443bf1e2dc44d
Hugo Voisin <hugov@bam.tech>
2023-01-23 11:06:39 +0100

    refactor: use screen from RNTL

--- src/features/home/components/modules/ThematicHighlightModule.native.test.tsx
+++ src/features/home/components/modules/ThematicHighlightModule.native.test.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import { ThematicHighlightModule } from 'features/home/components/modules/ThematicHighlightModule'
 import { formattedThematicHighlightModule } from 'features/home/fixtures/homepage.fixture'
-import { render } from 'tests/utils'
+import { render, screen } from 'tests/utils'
 
 const CURRENT_DATE = new Date('2020-12-01T00:00:00.000Z')
 const PASSED_DATE = new Date('2020-11-30T00:00:00.000Z')
@@ -11,24 +11,24 @@ mockDate.set(CURRENT_DATE)
 
 describe('ThematicHighlightModule', () => {
   it('should render if the ending date is not passed', () => {
-    const { queryAllByText } = render(
+    render(
       <ThematicHighlightModule
         {...formattedThematicHighlightModule}
         beginningDate={CURRENT_DATE}
         endingDate={CURRENT_DATE}
       />
     )
-    expect(queryAllByText(formattedThematicHighlightModule.title)).toBeTruthy()
+    expect(screen.queryAllByText(formattedThematicHighlightModule.title)).toBeTruthy()
   })
 
   it('should not render if the ending date is passed', () => {
-    const { queryByText } = render(
+    render(
       <ThematicHighlightModule
         {...formattedThematicHighlightModule}
         beginningDate={PASSED_DATE}
         endingDate={PASSED_DATE}
       />
     )
-    expect(queryByText(formattedThematicHighlightModule.title)).toBeNull()
+    expect(screen.queryByText(formattedThematicHighlightModule.title)).toBeNull()
   })
 })
