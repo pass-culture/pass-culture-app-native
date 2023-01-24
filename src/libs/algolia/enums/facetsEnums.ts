dee804d2616c9d1680463b180a4b0fb6eb9f1ef5
Eve Julliard <evej@bam.tech>
2023-01-24 14:56:57 +0100

    (PC-19437) refactor(FACET_ENUM): rename FACET_ENUM into FACET_FILTERS_ENUM to match with NUMERIC_FILTERS_ENUM

--- src/libs/algolia/enums/facetsEnums.ts
+++ src/libs/algolia/enums/facetsEnums.ts
@@ -1,5 +1,5 @@
 // Taken from https://www.algolia.com/apps/E2IKXJ325N/explorer/configuration/PRODUCTION/facets
-export enum FACETS_ENUM {
+export enum FACETS_FILTERS_ENUM {
   OBJECT_ID = 'objectID',
   OFFER_IS_DIGITAL = 'offer.isDigital',
   OFFER_IS_DUO = 'offer.isDuo',
