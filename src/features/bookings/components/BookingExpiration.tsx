38a2f0be1d2841f91e5b03aaa26a6494c908b33a
YassinL <62058919+YassinL@users.noreply.github.com>
2023-01-25 12:13:49 +0100

    (PC-19509) feat(BookingDetailsCancel): display an expiration date message if the booking is digital (#4271)
    * (PC-19509) feat(BookingDetailsCancel): display an expiration date message if the booking is digital
    
    * (PC-19509) test(BookingDetailsCancel): add some test to check if we displayed the expiration date message
    
    * (PC-19509) fix(snapshot)
    
    * (PC-19509) refacto(BookingDetailsCancelButton): After review refactoring to make the code more readable
    
    * (PC-19509) fix(expirationDateUtils): manage empty string case and some rewording
    
    * (PC-19509) refacto(BookingDetailsCancel): refacto code & clean missing code
    
    * (PC-19509) refacto(BookingDetailsCancel): fix condition for expirationDate et some rewording
    
    * (PC-19509) fix(test): rewording
    
    Co-authored-by: « YassinL » <« yassin.leclercq1@gmail.com»>

---
+++ src/features/bookings/components/BookingExpiration.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from 'react'
+import styled from 'styled-components/native'
+
+import { Spacer, Typo } from 'ui/theme'
+
+export interface BookingExpirationProps {
+  children: ReactNode
+  expirationDate: string
+}
+
+export const BookingExpiration = ({ children, expirationDate }: BookingExpirationProps) => {
+  if (!expirationDate) {
+    return null
+  }
+  const expirationDateMessage = `Ta réservation expirera le ${expirationDate}`
+  return (
+    <React.Fragment>
+      {children}
+      <Spacer.Column numberOfSpaces={4} />
+      <StyledCaption>{expirationDateMessage}</StyledCaption>
+    </React.Fragment>
+  )
+}
+
+const StyledCaption = styled(Typo.CaptionNeutralInfo)({
+  textAlign: 'center',
+})
