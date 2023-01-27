e16112b115e02867cf416b93e4928133778c24cd
Lixxday <corentin.normant@gmail.com>
2023-01-27 14:12:25 +0100

    (PC-18797) feat(ExpiredOrLostID): send batch event when landing on page

--- src/libs/react-native-batch/enums.ts
+++ src/libs/react-native-batch/enums.ts
@@ -4,5 +4,6 @@ export enum BatchEvent {
   hasSeenAllTheHomepage = 'has_seen_all_the_homepage',
   hasSeenOffer = 'has_seen_offer',
   hasValidatedAccount = 'has_validated_account',
+  screenViewExpiredOrLostId = 'screen_view_expired_or_lost_id',
   screenViewComeBackLater = 'screen_view_come_back_later',
 }
