2cb2917b25721c9fee6bc97b216b377542213b52
matthias-prevost <matthias.prevost@gmail.com>
2023-01-30 16:04:42 +0100

    (PC-20071) feat(navBar): fix navBar in desktop view (#4309)
    * modify breakpoint
    
    * update value of md breakpoint
    
    * Review PR

--- e2e/tests/pc/helpers/utils/theme.ts
+++ e2e/tests/pc/helpers/utils/theme.ts
@@ -27,7 +27,7 @@ export function getTheme({
 }): DefaultTheme {
   const isTouch = driver.isAndroid || driver.isIOS
   const breakpoints = {
-    md: 768,
+    md: 960,
     lg: 1024,
   }
   const tabletMinWidth = breakpoints.md
