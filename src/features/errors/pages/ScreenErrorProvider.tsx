4aad5933793c30e4b71d9eb1142514992c7f1c79
Lucas Beneston <lucas.beneston@passculture.app>
2023-01-31 12:03:00 -0400

    (PC-19954) fix(ForceUpdate): fix ScreenErrorProvider import after rebase

--- src/features/errors/pages/ScreenErrorProvider.tsx
+++ src/features/errors/pages/ScreenErrorProvider.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 
-<<<<<<< PC-19954
 import { useMustUpdateApp } from 'features/forceUpdate/helpers/useMustUpdateApp'
 import { ForceUpdate } from 'features/forceUpdate/pages/ForceUpdate'
-import { MaintenanceErrorPage } from 'features/maintenance/MaintenanceErrorPage'
-import { useMaintenance } from 'features/maintenance/useMaintenance'
-=======
-import { ForceUpdate } from 'features/forceUpdate/ForceUpdate'
-import { useMustUpdateApp } from 'features/forceUpdate/useMustUpdateApp'
 import { useMaintenance } from 'features/maintenance/helpers/useMaintenance'
 import { MaintenanceErrorPage } from 'features/maintenance/pages/MaintenanceErrorPage'
->>>>>>> master
 import { MAINTENANCE } from 'libs/firebase/firestore/maintenance'
 import { ScreenError } from 'libs/monitoring/errors'
 
