7bdf7736c9c8ca12725afc5169fe25ca6620e0ac
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2023-01-31 19:01:15 +0100

    (PC-20101) ci(e2e): fix FeatureClient API_BASE_URL (#4333)
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>

--- e2e/tests/pc/helpers/FeatureClient.ts
+++ e2e/tests/pc/helpers/FeatureClient.ts
@@ -11,21 +11,25 @@ const reset = '\x1b[0m'
 const bright = '\x1b[1m'
 
 class FeatureClient {
-  private static API_BASE_URL = 'http://localhost:6001'
+  private static API_BASE_URL = process.env.API_BASE_URL
   private static ENDPOINT = 'native/v1/features'
 
   private async patchFeatures(features: Features) {
-    const response = await fetch(`${FeatureClient.API_BASE_URL}/${FeatureClient.ENDPOINT}`, {
-      method: 'PATCH',
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      }),
-      body: JSON.stringify({
-        features,
-      }),
-    })
-    if (response.ok) {
+    try {
+      const response = await fetch(`${FeatureClient.API_BASE_URL}/${FeatureClient.ENDPOINT}`, {
+        method: 'PATCH',
+        mode: 'cors',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        }),
+        body: JSON.stringify({
+          features,
+        }),
+      })
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
       console.log(
         new Date().toISOString(),
         `${fgCyan}INFO`,
@@ -33,14 +37,15 @@ class FeatureClient {
         `${reset}${features.map(({ name, isActive }) => `${name} set to ${isActive}`).join('\n')}`
       )
       return response
+    } catch (error) {
+      console.log(
+        new Date().toISOString(),
+        `${fgCyan}INFO`,
+        `${bright}FeatureClient`,
+        `${reset}Failed to PATCH ${FeatureClient.ENDPOINT} for ${features.length} features: ${error}`
+      )
+      return Promise.reject(error)
     }
-    console.log(
-      new Date().toISOString(),
-      `${fgCyan}INFO`,
-      `${bright}FeatureClient`,
-      `${reset}Failed to PATCH ${FeatureClient.ENDPOINT} for ${features.length} features: ${response.statusText}`
-    )
-    return Promise.reject(response)
   }
 
   setEnableNativeAppRecaptcha = async (isActive: boolean) => {
