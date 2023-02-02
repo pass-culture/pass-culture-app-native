8d77d6146b6264b6d4831d072a2a6be2600d624e
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-02-02 18:36:58 +0100

    (PC-20096) feat(MessagingApps): Add helper to check if apps are installed

---
+++ src/features/offer/helpers/checkInstalledApps.ts
@@ -0,0 +1,17 @@
+import { Platform, Linking } from 'react-native'
+
+import { Network } from 'ui/components/ShareMessagingApp'
+
+export const checkInstalledApps: () => Promise<Record<Network, boolean>> = async () => {
+  // ORDERED BY PRIORITY
+  const networks = {
+    [Network.snapchat]: Platform.OS === 'android' && (await Linking.canOpenURL('snapchat://')), // Only supported on Android
+    [Network.instagram]: Platform.OS === 'ios' && (await Linking.canOpenURL('instagram://user/')), // Only supported on iOS
+    [Network.whatsapp]: await Linking.canOpenURL('whatsapp://send/'),
+    [Network.imessage]: Platform.OS === 'ios' && (await Linking.canOpenURL('sms://')), // Native to iOS
+    [Network.messenger]: await Linking.canOpenURL('fb-messenger://'),
+    [Network.telegram]: await Linking.canOpenURL('tg://'),
+    [Network.viber]: await Linking.canOpenURL('viber://forward'),
+  }
+  return networks
+}
