e2901294f73feda7a89608bf223c07358fb42ba1
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-02-02 18:38:34 +0100

    (PC-20096) feat(MessagingApps): Add react-native-share to open messaging apps

--- src/features/internal/cheatcodes/pages/GetDynamicSocials/GetDynamicSocials.tsx
+++ src/features/internal/cheatcodes/pages/GetDynamicSocials/GetDynamicSocials.tsx
@@ -1,6 +1,9 @@
 import React, { FunctionComponent } from 'react'
+import styled from 'styled-components/native'
 
+import { InstalledMessagingApps } from 'features/offer/components/shareMessagingOffer/InstalledMessagingApps'
 import { PageHeaderSecondary } from 'ui/components/headers/PageHeaderSecondary'
+import { getSpacing } from 'ui/theme'
 
 interface Props {
   children?: never
@@ -10,6 +13,17 @@ export const GetDynamicSocials: FunctionComponent<Props> = () => {
   return (
     <React.Fragment>
       <PageHeaderSecondary title="Spike réseaux sociaux dynamiques" />
+      <Container>
+        <InstalledMessagingApps />
+      </Container>
     </React.Fragment>
   )
 }
+
+const Container = styled.View({
+  padding: getSpacing(2),
+  paddingTop: getSpacing(6),
+  justifyContent: 'space-between',
+  flexDirection: 'row',
+  flex: 1,
+})
