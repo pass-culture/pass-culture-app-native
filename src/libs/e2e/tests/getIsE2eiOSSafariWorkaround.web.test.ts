7873394769e8167fe8f46861eaf4200a49ced6f5
Dimitri Kopriwa <dimitri.kopriwa@yeutech.com>
2023-02-03 11:48:38 +0100

    fix test

--- src/libs/e2e/tests/getIsE2eiOSSafariWorkaround.web.test.ts
+++ src/libs/e2e/tests/getIsE2eiOSSafariWorkaround.web.test.ts
@@ -2,6 +2,7 @@ import { getIsE2e } from '../getIsE2e'
 
 describe('getIsE2e web ios safari workaround', () => {
   it('should return true when navigator.webdriver is false but window.webdriver is set (iOS Safari workaround)', async () => {
+    jest.setTimeout(6000)
     process.env.NODE_ENV = 'development'
     // @ts-expect-error : `webdriver` is a read-only property
     globalThis.navigator.webdriver = false
