dc1cf4f467fd48bec4774461f6cc5e68ac62af4c
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2023-02-03 12:16:04 +0100

    (BSR) ci(e2e): improve safari ios e2e detection (#4350)
    * (BSR) ci(e2e): improve safari ios e2e detection
    
    * lint
    
    ---------
    
    Co-authored-by: Dimitri Kopriwa <dimitri.kopriwa@yeutech.com>

--- src/libs/e2e/tests/getIsE2eiOSSafariWorkaround.web.test.ts
+++ src/libs/e2e/tests/getIsE2eiOSSafariWorkaround.web.test.ts
@@ -1,12 +1,13 @@
 import { getIsE2e } from '../getIsE2e'
 
 describe('getIsE2e web ios safari workaround', () => {
-  it('should return true when navigator.webdriver is false but window.webdriver is set (iOS Safari workaround)', async () => {
-    jest.setTimeout(6000)
+  it('should return true when navigator.webdriver is false but appium server is up (iOS Safari workaround)', async () => {
     process.env.NODE_ENV = 'development'
     // @ts-expect-error : `webdriver` is a read-only property
     globalThis.navigator.webdriver = false
-    globalThis.window.webdriver = true
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      ok: true,
+    } as Response)
     expect(await getIsE2e()).toBeTruthy()
     process.env.NODE_ENV = 'test'
   })
