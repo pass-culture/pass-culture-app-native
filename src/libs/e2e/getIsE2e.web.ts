3f89d3c6bf6bf94fc9a1a97a17ba6b0db9c34600
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2023-02-03 11:53:26 +0100

    (BSR) ci(e2e): increase getE2e safari.ios fix (#4347)

--- src/libs/e2e/getIsE2e.web.ts
+++ src/libs/e2e/getIsE2e.web.ts
@@ -15,7 +15,7 @@ export async function getIsE2e() {
   } else {
     // This alternative is specific for iOS browser as safaridriver ios have a bug and do not set navigator.webdriver property during automation
     // Also, we will wait a bit more, because we inject window.webdriver and it can take time, this is not a problem as we already wait 5sec before starting a suite.
-    await new Promise((resolve) => setTimeout(resolve, 5000))
+    await new Promise((resolve) => setTimeout(resolve, 6000))
     isE2e = !!globalThis.window.webdriver
   }
   return isE2e
