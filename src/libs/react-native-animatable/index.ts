2243c0f6b03a9dab1873ce9bae762a3a1440a631
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-02-05 18:41:43 +0100

    (PC-20317) feat(onboarding): Animate unlocked credit block

--- src/libs/react-native-animatable/index.ts
+++ src/libs/react-native-animatable/index.ts
@@ -3,3 +3,5 @@ export * from 'react-native-animatable'
 
 // To differentiate from RN View and Text
 export { Text as AnimatedText, View as AnimatedView } from 'react-native-animatable'
+
+export const NAV_DELAY_IN_MS = 200 // Standard delay to wait before triggering animation, to avoid animation component during navigation
