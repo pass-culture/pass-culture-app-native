7352b01287b65a6cf17f947559349eb726f3a73a
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2023-02-07 16:02:03 +0100

    improve e2e/generate-env.sh

--- e2e/generate-env.sh
+++ e2e/generate-env.sh
@@ -2,12 +2,12 @@
 # .env.local.secret generator
 #
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(dirname -- "$0")
 
 ANDROID=${ANDROID:-"false"}
 END_TO_END_TESTS_EMAIL_ADDRESS=${END_TO_END_TESTS_EMAIL_ADDRESS}
 SENDINBLUE_API_KEY=${SENDINBLUE_API_KEY}
 
 # shellcheck disable=SC2002
-eval "$(cat "${SCRIPT_DIR}/.env.local.secret.tpl" | sed 's/"/+++/g'|sed  's/^\(.*\)$/echo "\1"/')" |sed 's/+++/"/g'|sed 's;\\";";g' > "${SCRIPT_DIR}/.env.local.secret"
+eval "$(cat "${SCRIPT_DIR}/.env.local.secret.tpl" | sed 's/^\(.*\)$/echo "\1"/')" > "${SCRIPT_DIR}/.env.local.secret"
 
