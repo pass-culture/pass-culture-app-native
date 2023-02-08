7f1368ea8af479e6bf35f054b26b8b54d901f938
matthias-prevost <matthias.prevost@gmail.com>
2023-02-08 10:39:31 +0100

    create banner and place it on top of favorties page (inside the scroll)

---
+++ src/features/FavoriteList/FakeDoor/FavoriteListBanner.tsx
@@ -0,0 +1,41 @@
+import React, { FunctionComponent } from 'react'
+import styled from 'styled-components/native'
+
+import { FavoriteListSurveyModal } from 'features/FavoriteList/FakeDoor/FavoriteListSurveyModal'
+import { useModal } from 'ui/components/modals/useModal'
+import { GenericBanner } from 'ui/components/ModuleBanner/GenericBanner'
+import { TouchableOpacity } from 'ui/components/TouchableOpacity'
+import { BicolorListFav } from 'ui/svg/icons/BicolorListFav'
+import { getSpacing, Typo } from 'ui/theme'
+
+export const FavoriteListBanner: FunctionComponent = () => {
+  const {
+    visible: fakeDoorListFavoritesVisible,
+    showModal: showFakeDoorListFavoritesVisible,
+    hideModal: hideFakeDoorListFavoritesVisible,
+  } = useModal(false)
+
+  return (
+    <React.Fragment>
+      <StyledTouchableWrapper onPress={showFakeDoorListFavoritesVisible}>
+        <StyledBanner dense LeftIcon={BicolorListFav}>
+          <Typo.ButtonText>Crée une liste de favoris</Typo.ButtonText>
+          <Typo.Body>Trie tes favoris à ta façon et partage-les&nbsp;!</Typo.Body>
+        </StyledBanner>
+      </StyledTouchableWrapper>
+      <FavoriteListSurveyModal
+        visible={fakeDoorListFavoritesVisible}
+        hideModal={hideFakeDoorListFavoritesVisible}
+      />
+    </React.Fragment>
+  )
+}
+
+const StyledBanner = styled(GenericBanner)<{ dense?: boolean }>(({ dense }) => ({
+  marginVertical: getSpacing(4),
+  paddingVertical: getSpacing(dense ? 4 : 6),
+}))
+
+const StyledTouchableWrapper = styled(TouchableOpacity)({
+  marginHorizontal: getSpacing(6),
+})
