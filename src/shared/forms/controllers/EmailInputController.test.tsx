0c830e1da0fc1df7984b137c8f210be2a3294772
Marine Shaw <68000368+marineshaw@users.noreply.github.com>
2023-02-08 10:08:51 +0100

    (PC-19620) test(Form): add test for generic controller (#4329)
    * :white_check_mark: Add test for email input
    
    * :white_check_mark: Add test for password input
    
    * :recycle: (EmailInputController) put fake timers on top of test file
    
    * :pencil2: (EmailInputController) remove && syntax
    
    * :white_check_mark: (EmailInputController) test that the component does not show the spellingHelp without being asked rather than testing spelling help behaviour
    
    * :pencil2: (PasswordInputController) rename test
    
    * :pencil2: (EmailInputController) correct test name

---
+++ src/shared/forms/controllers/EmailInputController.test.tsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { useForm, ErrorOption } from 'react-hook-form'
+
+import { EmailInputController } from 'shared/forms/controllers/EmailInputController'
+import { act, fireEvent, render, screen } from 'tests/utils'
+import { SUGGESTION_DELAY_IN_MS } from 'ui/components/inputs/EmailInputWithSpellingHelp/useEmailSpellingHelp'
+
+type EmailForm = {
+  email: string
+}
+
+jest.useFakeTimers()
+
+describe('<EmailInputController />', () => {
+  it('should show error when form input is invalid', () => {
+    renderEmailInputController({
+      error: { type: 'custom', message: 'Email has an incorrect format' },
+    })
+
+    expect(screen.getByText('Email has an incorrect format')).toBeTruthy()
+  })
+
+  it('should not show error when form input is valid', () => {
+    renderEmailInputController({})
+
+    expect(screen.queryByText('error')).toBeFalsy()
+  })
+
+  it('should not show spelling help when not asked', async () => {
+    renderEmailInputController({})
+
+    const input = screen.getByPlaceholderText('tonadresse@email.com')
+    fireEvent.changeText(input, 'firstname.lastname@gmal.com')
+
+    await act(async () => {
+      jest.advanceTimersByTime(SUGGESTION_DELAY_IN_MS)
+    })
+
+    expect(screen.queryByText('Veux-tu plutôt dire firstname.lastname@gmail.com ?')).toBeFalsy()
+  })
+
+  it('should show spelling help when asked', async () => {
+    renderEmailInputController({ withSpellingHelp: true })
+
+    const input = screen.getByPlaceholderText('tonadresse@email.com')
+    fireEvent.changeText(input, 'firstname.lastname@gmal.com')
+
+    await act(async () => {
+      jest.advanceTimersByTime(SUGGESTION_DELAY_IN_MS)
+    })
+    expect(screen.getByText('Veux-tu plutôt dire firstname.lastname@gmail.com ?')).toBeTruthy()
+  })
+
+  it('should perform action on spelling help press when given', async () => {
+    const mockOnSpellingHelpPress = jest.fn()
+    renderEmailInputController({
+      withSpellingHelp: true,
+      onSpellingHelpPress: mockOnSpellingHelpPress,
+    })
+
+    const input = screen.getByPlaceholderText('tonadresse@email.com')
+    fireEvent.changeText(input, 'firstname.lastname@gmal.com')
+
+    await screen.findByText('Veux-tu plutôt dire firstname.lastname@gmail.com ?')
+
+    const suggestionButton = screen.getByText('Appliquer la modification')
+    fireEvent.press(suggestionButton)
+    expect(mockOnSpellingHelpPress).toHaveBeenCalledTimes(1)
+  })
+})
+
+const renderEmailInputController = ({
+  withSpellingHelp,
+  error,
+  onSpellingHelpPress,
+}: {
+  withSpellingHelp?: boolean
+  error?: ErrorOption
+  onSpellingHelpPress?: () => void
+}) => {
+  const EmailForm = () => {
+    const { control, setError } = useForm<EmailForm>({
+      defaultValues: { email: '' },
+    })
+
+    if (error) setError('email', error)
+    return (
+      <EmailInputController
+        control={control}
+        name="email"
+        label="ton adresse email"
+        withSpellingHelp={withSpellingHelp}
+        onSpellingHelpPress={onSpellingHelpPress}
+      />
+    )
+  }
+  render(<EmailForm />)
+}
---
+++ src/shared/forms/controllers/PasswordInputController.test.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { useForm, ErrorOption } from 'react-hook-form'
+
+import { PasswordInputController } from 'shared/forms/controllers/PasswordInputController'
+import { fireEvent, render, screen } from 'tests/utils'
+
+type PasswordForm = {
+  password: string
+}
+
+describe('<PasswordInputController />', () => {
+  it('should not show error when password is invalid but not given', async () => {
+    renderPasswordInputController({
+      error: { type: 'custom', message: 'error' },
+    })
+
+    expect(screen.queryByText('error')).toBeNull()
+  })
+
+  it('should show error when form is invalid and password is not empty', async () => {
+    renderPasswordInputController({
+      error: { type: 'custom', message: 'error' },
+    })
+
+    const input = screen.getByPlaceholderText('Ton mot de passe')
+    fireEvent.changeText(input, 'pass')
+
+    expect(screen.getByText('error')).toBeTruthy()
+  })
+
+  it('should show that password is mandatory', async () => {
+    renderPasswordInputController({})
+
+    expect(screen.getByText('Obligatoire')).toBeTruthy()
+  })
+
+  it.each([
+    '12 Caractères',
+    '1 Majuscule',
+    '1 Minuscule',
+    '1 Chiffre',
+    '1 Caractère spécial (!@#$%^&*...)',
+  ])('should show password validation rules when asked', (rules) => {
+    renderPasswordInputController({ withSecurityRules: true })
+
+    expect(screen.getByText(rules)).toBeTruthy()
+  })
+
+  it('should not show password validation rules by default', () => {
+    renderPasswordInputController({})
+
+    expect(screen.queryByText('12 Caractères')).toBeFalsy()
+  })
+})
+
+const renderPasswordInputController = ({
+  withSecurityRules,
+  error,
+}: {
+  withSecurityRules?: boolean
+  error?: ErrorOption
+}) => {
+  const PasswordForm = () => {
+    const { control, setError } = useForm<PasswordForm>({
+      defaultValues: { password: '' },
+    })
+
+    error && setError('password', error)
+    return (
+      <PasswordInputController
+        control={control}
+        name="password"
+        withSecurityRules={withSecurityRules}
+      />
+    )
+  }
+  render(<PasswordForm />)
+}
