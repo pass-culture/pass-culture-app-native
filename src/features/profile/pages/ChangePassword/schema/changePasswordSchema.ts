cc945ab307afd6fcb1c83678ce84253bb2f08faa
matthias-prevost <matthias.prevost@gmail.com>
2023-02-09 14:46:31 +0100

    reuse generic password schema

--- src/features/profile/pages/ChangePassword/schema/changePasswordSchema.ts
+++ src/features/profile/pages/ChangePassword/schema/changePasswordSchema.ts
@@ -1,28 +1,10 @@
 import { object, ref, string } from 'yup'
 
-import {
-  CAPITAL_REGEX,
-  LOWERCASE_REGEX,
-  NUMBER_REGEX,
-  PASSWORD_MIN_LENGTH,
-  SPECIAL_CHARACTER_REGEX,
-} from 'features/auth/components/PasswordSecurityRules'
+import { passwordSchema } from 'shared/forms/schemas/passwordSchema'
 
 export const changePasswordSchema = object().shape({
-  currentPassword: string()
-    .min(PASSWORD_MIN_LENGTH, '12 Caractères')
-    .required()
-    .matches(CAPITAL_REGEX, '1 Majuscule')
-    .matches(LOWERCASE_REGEX, '1 Minuscule')
-    .matches(NUMBER_REGEX, '1 Chiffre')
-    .matches(SPECIAL_CHARACTER_REGEX, '1 Caractère spécial (!@#$%^&*...)'),
-  newPassword: string()
-    .min(PASSWORD_MIN_LENGTH, '12 Caractères')
-    .required('required')
-    .matches(CAPITAL_REGEX, '1 Majuscule')
-    .matches(LOWERCASE_REGEX, '1 Minuscule')
-    .matches(NUMBER_REGEX, '1 Chiffre')
-    .matches(SPECIAL_CHARACTER_REGEX, '1 Caractère spécial (!@#$%^&*...)'),
+  currentPassword: passwordSchema,
+  newPassword: passwordSchema,
   confirmedPassword: string()
     .oneOf([ref('newPassword'), null], 'Les mots de passe ne concordent pas')
     .required(),
