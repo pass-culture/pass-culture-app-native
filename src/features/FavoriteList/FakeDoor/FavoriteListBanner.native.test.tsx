d35fe624f89250209ac57ef3788d1cb0706108c3
matthias-prevost <matthias.prevost@gmail.com>
2023-02-09 18:48:18 +0100

    create tests for favoriteListBanner component

---
+++ src/features/FavoriteList/FakeDoor/FavoriteListBanner.native.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { FavoriteListBanner } from 'features/FavoriteList/FakeDoor/FavoriteListBanner'
+import { fireEvent, render, screen } from 'tests/utils'
+
+describe('FavoriteListBanner', () => {
+  it('should render correctly', () => {
+    const renderAPI = render(<FavoriteListBanner />)
+    expect(renderAPI).toMatchSnapshot()
+  })
+
+  it('should open modal when clicking the banner', () => {
+    render(<FavoriteListBanner />)
+
+    const banner = screen.getByText('Crée une liste de favoris')
+    fireEvent.press(banner)
+    expect(screen.getByText('Encore un peu de patience...')).toBeTruthy()
+  })
+})
