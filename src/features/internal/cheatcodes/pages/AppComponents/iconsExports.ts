2bd3fec70477d21239fc0aecc38e1f8a2833f255
matthias-prevost <matthias.prevost@gmail.com>
2023-02-09 18:04:54 +0100

    remove unused icon screenPlay

--- src/features/internal/cheatcodes/pages/AppComponents/iconsExports.ts
+++ src/features/internal/cheatcodes/pages/AppComponents/iconsExports.ts
@@ -83,7 +83,6 @@ import { Profile } from 'ui/svg/icons/Profile'
 import { ProfileDeletion } from 'ui/svg/icons/ProfileDeletion'
 import { Question } from 'ui/svg/icons/Question'
 import { Quote } from 'ui/svg/icons/Quote'
-import { ScreenPlay } from 'ui/svg/icons/ScreenPlay'
 import { ScrollToTop } from 'ui/svg/icons/ScrollToTop'
 import { SignOut } from 'ui/svg/icons/SignOut'
 import { SMSFilled } from 'ui/svg/icons/SMSFilled'
@@ -217,7 +216,6 @@ export const TertiaryAndSmallerIcons = {
   Plus,
   Profile,
   Question,
-  ScreenPlay,
   SMSFilled,
   Validate,
   ValidateOff,
