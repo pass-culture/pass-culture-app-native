973b0309ccfb294a5291d51b0a9db253a5361864
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-02-13 11:47:29 +0100

    (PC-20361) feat(appModal): add prop to not display spacer between header and content

--- src/ui/components/modals/AppModal.tsx
+++ src/ui/components/modals/AppModal.tsx
@@ -41,6 +41,7 @@ type Props = {
   maxHeight?: number
   shouldScrollToEnd?: boolean
   keyboardShouldPersistTaps?: ScrollViewProps['keyboardShouldPersistTaps']
+  shouldRemoveSpacerBetweenHeaderAndContent?: boolean
 } & ModalIconProps
 
 // Without this, the margin is recomputed with arbitrary values
@@ -72,6 +73,7 @@ export const AppModal: FunctionComponent<Props> = ({
   maxHeight,
   shouldScrollToEnd,
   keyboardShouldPersistTaps,
+  shouldRemoveSpacerBetweenHeaderAndContent,
 }) => {
   const iconProps = {
     rightIconAccessibilityLabel,
@@ -201,7 +203,9 @@ export const AppModal: FunctionComponent<Props> = ({
           </StyledScrollView>
         ) : (
           <React.Fragment>
-            <SpacerBetweenHeaderAndContent />
+            {!shouldRemoveSpacerBetweenHeaderAndContent && (
+              <SpacerBetweenHeaderAndContent testID="spacerBetweenHeaderAndContent" />
+            )}
             <ScrollViewContainer
               paddingBottom={scrollViewPaddingBottom}
               modalSpacing={modalSpacing}>
