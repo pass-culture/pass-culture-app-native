a8b8b56c977990c140cad7f48dba17eea0453678
marineshaw <marines@bam.tech>
2023-02-16 11:16:59 +0100

    :sparkles: (FavList) Add tracker in favorite list offer modal

--- src/features/FavoriteList/FakeDoor/FavoriteListOfferModal.native.test.tsx
+++ src/features/FavoriteList/FakeDoor/FavoriteListOfferModal.native.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { FavoriteListOfferModal } from 'features/FavoriteList/FakeDoor/FavoriteListOfferModal'
+import { analytics } from 'libs/firebase/analytics'
 import { fireEvent, render, screen } from 'tests/utils'
 
 describe('<FavoriteListOfferModal />', () => {
@@ -25,5 +26,13 @@ describe('<FavoriteListOfferModal />', () => {
     fireEvent.press(screen.getByText('Créer une liste de favoris'))
 
     expect(hideModalMock).toHaveBeenCalledTimes(1)
+  })
+
+  it('should track when the user wants to create a favorite list', () => {
+    render(<FavoriteListOfferModal visible hideModal={jest.fn()} showSurveyModal={jest.fn()} />)
+
+    fireEvent.press(screen.getByText('Créer une liste de favoris'))
+
+    expect(analytics.logFavoriteListButtonClicked).toHaveBeenNthCalledWith(1, 'offer')
   })
 })
--- src/features/FavoriteList/FakeDoor/FavoriteListOfferModal.tsx
+++ src/features/FavoriteList/FakeDoor/FavoriteListOfferModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components/native'
 
+import { analytics } from 'libs/firebase/analytics'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
 import { AppModalWithIllustration } from 'ui/components/modals/AppModalWithIllustration'
@@ -22,6 +23,7 @@ export const FavoriteListOfferModal: React.FC<Props> = ({
 }) => {
   const [shouldSeeSurvey, setShouldSeeSurvey] = useState(false)
   const onCreateFavListPress = () => {
+    analytics.logFavoriteListButtonClicked('offer')
     setShouldSeeSurvey(true)
     hideModal()
   }
