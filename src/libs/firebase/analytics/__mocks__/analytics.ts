69ff1da4c437b43b3bb4eea4dfa6c21f6d20b4db
marineshaw <marines@bam.tech>
2023-02-16 11:03:14 +0100

    :sparkles: (Analytics) Add new events and logs for favorite list fake door

--- src/libs/firebase/analytics/__mocks__/analytics.ts
+++ src/libs/firebase/analytics/__mocks__/analytics.ts
@@ -61,6 +61,8 @@ export const analytics: typeof actualAnalytics = {
   logDismissShareApp: jest.fn(),
   logErrorSavingNewEmail: jest.fn(),
   logExclusivityBlockClicked: jest.fn(),
+  logFavoriteListDisplayed: jest.fn(),
+  logFavoriteListButtonClicked: jest.fn(),
   logGoToParentsFAQ: jest.fn(),
   logGoToProfil: jest.fn(),
   logHasAcceptedAllCookies: jest.fn(),
--- src/libs/firebase/analytics/analytics.ts
+++ src/libs/firebase/analytics/analytics.ts
@@ -158,6 +158,10 @@ const logEventAnalytics = {
     moduleId: string
     homeEntryId?: string
   }) => analyticsProvider.logEvent(AnalyticsEvent.EXCLUSIVITY_BLOCK_CLICKED, params),
+  logFavoriteListDisplayed: (from: Referrals) =>
+    analyticsProvider.logEvent(AnalyticsEvent.FAVORITE_LIST_DISPLAYED, { from }),
+  logFavoriteListButtonClicked: (from: Referrals) =>
+    analyticsProvider.logEvent(AnalyticsEvent.FAVORITE_LIST_BUTTON_CLICKED, { from }),
   logGoToParentsFAQ: (from: Referrals) =>
     analyticsProvider.logEvent(AnalyticsEvent.GO_TO_PARENTS_FAQ, { from }),
   logGoToProfil: ({ from, offerId }: { from: string; offerId: number }) =>
