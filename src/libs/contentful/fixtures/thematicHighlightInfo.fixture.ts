05a3d196355d32306b4be2fdb835a5a1a0bc2400
Hugo Voisin <hugo.voisin@ensta-paristech.fr>
2023-02-20 12:01:05 +0100

    (PC-20029) feat(contentful): use data from thematic highlight info (#4418)
    * feat: use data from thematic highlight info
    so that it can be reused in thematic highlight homes in a future ticket
    
    * prune: remove unnecessary optional chaining
    as image is a mandatory field, not optional

---
+++ src/libs/contentful/fixtures/thematicHighlightInfo.fixture.ts
@@ -0,0 +1,79 @@
+import { ContentTypes, ThematicHighlightInfo } from 'libs/contentful/types'
+
+export const thematicHighlightInfoFixture: ThematicHighlightInfo = {
+  sys: {
+    space: {
+      sys: {
+        type: 'Link',
+        linkType: 'Space',
+        id: '2bg01iqy0isv',
+      },
+    },
+    id: '3IJm1DTZ0wBEmZl6jR2Z4K',
+    type: 'Entry',
+    createdAt: '2023-02-17T15:00:14.663Z',
+    updatedAt: '2023-02-17T15:00:20.329Z',
+    environment: {
+      sys: {
+        id: 'testing',
+        type: 'Link',
+        linkType: 'Environment',
+      },
+    },
+    revision: 2,
+    contentType: {
+      sys: {
+        type: 'Link',
+        linkType: 'ContentType',
+        id: ContentTypes.THEMATIC_HIGHLIGHT_INFO,
+      },
+    },
+    locale: 'en-US',
+  },
+  fields: {
+    title: 'Temps fort',
+    displayedTitle: 'Temps très fort',
+    displayedSubtitle: 'Avec son sous-titre',
+    image: {
+      sys: {
+        space: {
+          sys: {
+            type: 'Link',
+            linkType: 'Space',
+            id: '2bg01iqy0isv',
+          },
+        },
+        id: '6kYYW8Uwad2ZlLUmw1k4ax',
+        type: 'Asset',
+        createdAt: '2020-11-12T12:51:58.409Z',
+        updatedAt: '2020-11-12T12:51:58.409Z',
+        environment: {
+          sys: {
+            id: 'testing',
+            type: 'Link',
+            linkType: 'Environment',
+          },
+        },
+        revision: 1,
+        locale: 'en-US',
+      },
+      fields: {
+        title: 'Découvre Versailles',
+        file: {
+          url: '//images.ctfassets.net/2bg01iqy0isv/6kYYW8Uwad2ZlLUmw1k4ax/9e1261e7010f4419506dc821b2d0bea8/be697ba0-3439-42fa-8f54-b917e988db66.jpeg',
+          details: {
+            size: 321635,
+            image: {
+              width: 1600,
+              height: 1200,
+            },
+          },
+          fileName: 'be697ba0-3439-42fa-8f54-b917e988db66.jpeg',
+          contentType: 'image/jpeg',
+        },
+      },
+    },
+    beginningDatetime: '2022-12-22T00:00+01:00',
+    endingDatetime: '2023-01-15T00:00+01:00',
+  },
+}
--- src/libs/contentful/fixtures/thematicHighlightModule.fixture.ts
+++ src/libs/contentful/fixtures/thematicHighlightModule.fixture.ts
@@ -1,3 +1,4 @@
+import { thematicHighlightInfoFixture } from 'libs/contentful/fixtures/thematicHighlightInfo.fixture'
 import { ContentTypes, ThematicHighlightContentModel } from 'libs/contentful/types'
 
 export const thematicHighlightModuleFixture: ThematicHighlightContentModel = {
@@ -32,49 +33,7 @@ export const thematicHighlightModuleFixture: ThematicHighlightContentModel = {
   },
   fields: {
     title: 'Temps fort Hugo',
-    displayedTitle: 'Temps très fort',
-    displayedSubtitle: 'Avec son sous-titre',
-    image: {
-      sys: {
-        space: {
-          sys: {
-            type: 'Link',
-            linkType: 'Space',
-            id: '2bg01iqy0isv',
-          },
-        },
-        id: '6kYYW8Uwad2ZlLUmw1k4ax',
-        type: 'Asset',
-        createdAt: '2020-11-12T12:51:58.409Z',
-        updatedAt: '2020-11-12T12:51:58.409Z',
-        environment: {
-          sys: {
-            id: 'testing',
-            type: 'Link',
-            linkType: 'Environment',
-          },
-        },
-        revision: 1,
-        locale: 'en-US',
-      },
-      fields: {
-        title: 'Découvre Versailles',
-        file: {
-          url: '//images.ctfassets.net/2bg01iqy0isv/6kYYW8Uwad2ZlLUmw1k4ax/9e1261e7010f4419506dc821b2d0bea8/be697ba0-3439-42fa-8f54-b917e988db66.jpeg',
-          details: {
-            size: 321635,
-            image: {
-              width: 1600,
-              height: 1200,
-            },
-          },
-          fileName: 'be697ba0-3439-42fa-8f54-b917e988db66.jpeg',
-          contentType: 'image/jpeg',
-        },
-      },
-    },
-    beginningDatetime: '2022-12-22T00:00+01:00',
-    endingDatetime: '2023-01-15T00:00+01:00',
     thematicHomeEntryId: '6DCThxvbPFKAo04SVRZtwY',
+    thematicHighlightInfo: thematicHighlightInfoFixture,
   },
 }
