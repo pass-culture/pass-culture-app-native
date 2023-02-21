c8daca217a3201b75716c436fe2a29e73e3a5eb8
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-02-21 15:46:06 +0100

    (PC-20600) feat(MessagingApps): Support socials in web (#4431)

--- src/libs/react-native-share/index.web.tsx
+++ src/libs/react-native-share/index.web.tsx
@@ -4,6 +4,15 @@ const Share = {
   openSingle: () => null,
 }
 
-export enum Social {}
+export enum Social {
+  Twitter = 'twitter',
+  Whatsapp = 'whatsapp',
+  Instagram = 'instagram',
+  Sms = 'sms',
+  Telegram = 'telegram',
+  Snapchat = 'snapchat',
+  Messenger = 'messenger',
+  Viber = 'viber',
+}
 
 export default Share
