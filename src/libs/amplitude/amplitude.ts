811df720781f7d347021157ea318eb74813d5bf2
Hugo Voisin <hugov@bam.tech>
2023-02-21 16:38:38 +0100

    refactor: update amplitude adapter to the new lib

--- src/libs/amplitude/amplitude.ts
+++ src/libs/amplitude/amplitude.ts
@@ -1,28 +1,33 @@
-import { Amplitude } from '@amplitude/react-native'
+import * as amplitudeRN from '@amplitude/analytics-react-native'
 
 import { env } from 'libs/environment'
 
 import { AmplitudeClient } from './types'
 
-const ampInstance = Amplitude.getInstance()
-
 if (env.AMPLITUDE_API_KEY) {
-  ampInstance.init(env.AMPLITUDE_API_KEY)
-  ampInstance.setServerZone('EU')
-  ampInstance.setOptOut(true)
+  amplitudeRN.init(env.AMPLITUDE_API_KEY, undefined, {
+    serverZone: amplitudeRN.Types.ServerZone.EU,
+    optOut: true,
+  })
 }
 
 export const amplitude: AmplitudeClient = {
   logEvent: (eventType, eventProperties) => {
-    ampInstance.logEvent(eventType, eventProperties)
+    amplitudeRN.track(eventType, eventProperties)
   },
   enableCollection: () => {
-    ampInstance.setOptOut(false)
+    amplitudeRN.setOptOut(false)
   },
   disableCollection: () => {
-    ampInstance.setOptOut(true)
+    amplitudeRN.setOptOut(true)
   },
   setUserProperties: (properties) => {
-    ampInstance.setUserProperties(properties)
+    const identifyProperties = new amplitudeRN.Identify()
+
+    Object.keys(properties).forEach((key) => {
+      identifyProperties.set(key, properties[key])
+    })
+
+    amplitudeRN.identify(identifyProperties)
   },
 }
--- src/libs/amplitude/types.ts
+++ src/libs/amplitude/types.ts
@@ -1,6 +1,8 @@
+import { Types } from '@amplitude/analytics-react-native'
+
 export interface AmplitudeClient {
   logEvent(eventType: string, eventProperties?: Record<string, unknown>): void
   enableCollection(): void
   disableCollection(): void
-  setUserProperties(properties: Record<string, unknown>): void
+  setUserProperties(properties: Record<string, Types.ValidPropertyType>): void
 }
