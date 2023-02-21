a955aceedc3b9c14bc67108be04038e87b1c8a89
anoukhello <anoukhello22@gmail.com>
2023-02-21 15:09:11 +0100

    (PC-19952) chore(features): reorder birthday notifications pages to fit code structure standard  (#4436)
    * (PC-19952) put eighteenBirthday and recreditBirthdayNotification in same feature
    
    * (PC-19952) birthdayNotifications: put eighteenBirtday components and pages in /birthdayNofitication
    
    * (PC-19952) birthdayNotifications: put recreditNotification pages in /birthdayNofitication
    
    * (PC-19952) birthdayNotifications: remove __test__ folder
    
    * (PC-19952) birthdayNotifications: replace EighteenBirthdayAchievement page by EighteenBirthday page
    
    * (PC-19952) birthdayNotifications: update snapshots

--- src/features/birthdayNotifications/pages/EighteenBirthday.tsx
+++ src/features/birthdayNotifications/pages/EighteenBirthday.tsx
@@ -3,9 +3,9 @@ import React, { useEffect } from 'react'
 import { storage } from 'libs/storage'
 import { GenericAchievement } from 'ui/components/achievements'
 
-import { EighteenBirthdayCard } from './EighteenBirthdayCard'
+import { EighteenBirthdayCard } from '../components/EighteenBirthdayCard'
 
-export function EighteenBirthdayAchievement() {
+export function EighteenBirthday() {
   useEffect(() => {
     storage.saveObject('has_seen_eligible_card', true)
   }, [])
