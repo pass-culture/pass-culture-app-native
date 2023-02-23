eb84f9c0ec70db2941de0cfe84dab466737e405b
AnoukHello <anoukhello22@gmail.com>
2023-02-23 18:09:47 +0100

    (PC-20534) contentful: rename adaptHomepageNatifEntries

--- src/libs/contentful/adapters/adaptHomepageEntries.ts
+++ src/libs/contentful/adapters/adaptHomepageEntries.ts
@@ -2,9 +2,7 @@ import { Homepage } from 'features/home/types'
 import { adaptHomepageNatifModules } from 'libs/contentful/adapters/adaptHomepageModules'
 import { HomepageNatifEntry } from 'libs/contentful/types'
 
-export const adaptHomepageNatifEntries = (
-  homepageNatifEntries: HomepageNatifEntry[]
-): Homepage[] => {
+export const adaptHomepageEntries = (homepageNatifEntries: HomepageNatifEntry[]): Homepage[] => {
   return homepageNatifEntries.map((entry) => ({
     tags: entry.metadata.tags.map((tag) => tag.sys.id),
     id: entry.sys.id,
