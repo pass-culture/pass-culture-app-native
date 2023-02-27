57f617bc31f5fcd70b698363ad913699c21c31c9
« YassinL » <yassin.leclercq1@gmail.com>
2023-02-27 14:12:29 +0100

    Test-ci
    Co-authored-by: Ali RAIKI <aliraiki@users.noreply.github.com>

--- scripts/reassure-test.sh
+++ scripts/reassure-test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-BASELINE_BRANCH=${BASELINE_BRANCH:="master"}
+# Test on branch PC-20631-dup
+BASELINE_BRANCH=${BASELINE_BRANCH:="PC-20631-dup"}
 
 # Required for `git switch` on CI
 git fetch origin
 
 # Gather baseline perf measurements
 git switch "$BASELINE_BRANCH"
+yarn add reassure --ignore-scripts
 yarn install --force
 yarn reassure --baseline
 
