7978ada00da9097b24b2f5d9fca59a7f8e490c3e
AnoukHello <anoukhello22@gmail.com>
2023-02-28 10:17:25 +0100

    (PC-20534) ThematicHome: show appropriate header

--- src/features/home/pages/ThematicHome.tsx
+++ src/features/home/pages/ThematicHome.tsx
@@ -4,17 +4,34 @@ import styled from 'styled-components/native'
 
 import { useHomepageData } from 'features/home/api/useHomepageData'
 import { DefaultThematicHomeHeader } from 'features/home/components/headers/DefaultThematicHomeHeader'
+import { HighlightThematicHomeHeader } from 'features/home/components/headers/HighlightThematicHomeHeader'
 import { GenericHome } from 'features/home/pages/GenericHome'
+import {
+  DefaultThematicHeader,
+  HighligthThematicHeader,
+  ThematicHeaderType,
+} from 'features/home/types'
 import { UseRouteType } from 'features/navigation/RootNavigator/types'
 
-const Header = ({ thematicHeader }: { thematicHeader?: { title?: string; subtitle?: string } }) => (
-  <ListHeaderContainer>
-    <DefaultThematicHomeHeader
-      headerTitle={thematicHeader?.title}
-      headerSubtitle={thematicHeader?.subtitle}
-    />
-  </ListHeaderContainer>
-)
+const Header = ({
+  thematicHeader,
+}: {
+  thematicHeader?: DefaultThematicHeader | HighligthThematicHeader
+}) => {
+  if (thematicHeader?.type === ThematicHeaderType.HighligthThematicHeader) {
+    const header = thematicHeader as HighligthThematicHeader
+    return <HighlightThematicHomeHeader {...header} />
+  }
+
+  return (
+    <ListHeaderContainer>
+      <DefaultThematicHomeHeader
+        headerTitle={thematicHeader?.title}
+        headerSubtitle={thematicHeader?.subtitle}
+      />
+    </ListHeaderContainer>
+  )
+}
 
 export const ThematicHome: FunctionComponent = () => {
   const { params } = useRoute<UseRouteType<'ThematicHome'>>()
