b07fe17d8f968fbc5c5576f422bdd63d17f6830e
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-03-02 17:36:23 +0100

    (PC-20828) chore(test): Improve tests (PC-20833)  (#4473)

--- src/features/internal/marketingAndCommunication/pages/UTMParameters.native.test.tsx
+++ src/features/internal/marketingAndCommunication/pages/UTMParameters.native.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import { UTMParameters } from 'features/internal/marketingAndCommunication/pages/UTMParameters'
-import { render } from 'tests/utils'
+import { render, screen } from 'tests/utils'
 
 describe('<UTMParameters />', () => {
-  it('should render correctly', () => {
-    expect(render(<UTMParameters />)).toMatchSnapshot()
+  it('should render correctly', async () => {
+    render(<UTMParameters />)
+    await screen.findByText('UTM parameters')
+    expect(screen).toMatchSnapshot()
   })
 })
