b8b4f3eb51cf3dbd836be8eed1f99369b87b801c
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2023-03-03 10:11:30 +0100

    (BSR) feat(identityCheck): fix flaky accessibility test set phone number (#4479)
    * (BSR) chore(deps): update axe-storybook-testing from 6.2.0 to 6.3.0
    
    * (BSR) feat(identityCheck): fix SetPhoneNumber flaky tests

--- src/features/identityCheck/pages/phoneValidation/SetPhoneNumber.web.test.tsx
+++ src/features/identityCheck/pages/phoneValidation/SetPhoneNumber.web.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { render, checkAccessibilityFor, waitFor } from 'tests/utils/web'
+import { render, checkAccessibilityFor, act } from 'tests/utils/web'
 
 import { SetPhoneNumber } from './SetPhoneNumber'
 
@@ -38,7 +38,8 @@ describe('<SetPhoneNumber/>', () => {
     it('should not have basic accessibility issues', async () => {
       // eslint-disable-next-line local-rules/no-react-query-provider-hoc
       const { container } = render(reactQueryProviderHOC(<SetPhoneNumber />))
-      await waitFor(async () => {
+
+      await act(async () => {
         const results = await checkAccessibilityFor(container)
         expect(results).toHaveNoViolations()
       })
