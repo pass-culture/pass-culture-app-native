3adaa04b1a4551779d7d1c8b685448c77994b384
bruno ebstein <bruno.ebstein@passculture.app>
2023-03-06 10:47:15 +0100

    (PC-20821) refactor(test): better test (#4477)

--- src/features/auth/pages/forgottenPassword/ResetPasswordExpiredLink/ResetPasswordExpiredLink.native.test.tsx
+++ src/features/auth/pages/forgottenPassword/ResetPasswordExpiredLink/ResetPasswordExpiredLink.native.test.tsx
@@ -1,8 +1,10 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import { rest } from 'msw'
 import React from 'react'
+import * as ReactQueryAPI from 'react-query'
 
 import { navigate } from '__mocks__/@react-navigation/native'
+import { withAsyncErrorBoundary } from 'features/errors/hocs/withAsyncErrorBoundary'
 import { navigateToHomeConfig } from 'features/navigation/helpers'
 import { navigateFromRef } from 'features/navigation/navigationRef'
 import { RootStackParamList } from 'features/navigation/RootNavigator/types'
@@ -10,9 +12,11 @@ import { env } from 'libs/environment'
 import { analytics } from 'libs/firebase/analytics'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { server } from 'tests/server'
-import { render, fireEvent, waitFor } from 'tests/utils'
+import { render, fireEvent, screen, waitFor, flushAllPromisesWithAct } from 'tests/utils'
 
 import { ResetPasswordExpiredLink } from './ResetPasswordExpiredLink'
+
+const useQuerySpy = jest.spyOn(ReactQueryAPI, 'useQuery')
 
 jest.mock('features/navigation/helpers')
 jest.mock('features/navigation/navigationRef')
@@ -47,17 +51,21 @@ describe('<ResetPasswordExpiredLink/>', () => {
 
   it('should NOT redirect to reset password link sent page WHEN clicking on resend email and response is failure', async () => {
     server.use(
-      rest.post(env.API_BASE_URL + '/native/v1/request_password_reset', async (req, res, ctx) =>
-        res.once(ctx.status(401))
+      rest.post(env.API_BASE_URL + '/native/v1/request_password_reset', async (_req, res, ctx) =>
+        res(ctx.status(401))
       )
     )
-    const { getByText } = renderResetPasswordExpiredLink()
 
-    fireEvent.press(getByText(`Renvoyer l’email`))
+    const ResetPasswordExpiredLinkWithBoundary = withAsyncErrorBoundary(ResetPasswordExpiredLink)
+    render(
+      // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+      reactQueryProviderHOC(<ResetPasswordExpiredLinkWithBoundary {...navigationProps} />)
+    )
 
-    await waitFor(() => {
-      expect(navigate).not.toHaveBeenCalled()
-    })
+    fireEvent.press(screen.getByText(`Renvoyer l’email`))
+    await flushAllPromisesWithAct()
+
+    expect(useQuerySpy).toThrowError()
   })
 })
 
