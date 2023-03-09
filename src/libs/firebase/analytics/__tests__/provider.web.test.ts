820c634ae80a174c426da68ce26f34293e39ac05
marineshaw <marines@bam.tech>
2023-03-09 16:23:45 +0100

    :bug: (Tests) use msw in provider web

--- src/libs/firebase/analytics/__tests__/provider.web.test.ts
+++ src/libs/firebase/analytics/__tests__/provider.web.test.ts
@@ -1,7 +1,18 @@
+import { rest } from 'msw'
+
+import { env } from 'libs/environment'
 import { analytics } from 'libs/firebase/analytics'
+import { server } from 'tests/server'
 
 jest.unmock('libs/firebase/analytics/analytics')
 jest.unmock('libs/firebase/analytics/provider')
+
+server.use(
+  rest.get(
+    `https://firebase.googleapis.com/v1alpha/projects/-/apps/${env.FIREBASE_APPID}/webConfig`,
+    (_req, res, ctx) => res(ctx.status(200), ctx.json({}))
+  )
+)
 
 describe('analytics - getAppInstanceId', () => {
   it('should be null', async () => {
