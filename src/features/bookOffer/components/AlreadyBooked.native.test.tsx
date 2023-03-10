1bac6569358c6464e70770983f3f7a7491cb98ac
cedricls1 <78556024+cedricls1@users.noreply.github.com>
2023-03-10 15:04:27 +0100

    (PC-21013) fix(bookOffer): remove step & footer on already booked modal (#4525)
    * (PC-21013) fix(bookOffer): remove step & footer on booking impossible modal
    
    * (PC-21013) fix(bookOffer): remove step & footer on already booked modal
    
    ---------
    
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

--- src/features/bookOffer/components/AlreadyBooked.native.test.tsx
+++ src/features/bookOffer/components/AlreadyBooked.native.test.tsx
@@ -1,19 +1,35 @@
 import React from 'react'
 
 import { OfferResponse } from 'api/gen'
-import { fireEvent, render } from 'tests/utils'
+import { initialBookingState, Step } from 'features/bookOffer/context/reducer'
+import { fireEvent, render, screen } from 'tests/utils'
 
 import { AlreadyBooked } from './AlreadyBooked'
 
+const mockInitialBookingState = initialBookingState
+
 const mockDismissModal = jest.fn()
+const mockDispatch = jest.fn()
 jest.mock('features/bookOffer/context/useBookingContext', () => ({
-  useBookingContext: () => ({ dismissModal: mockDismissModal }),
+  useBookingContext: () => ({
+    bookingState: mockInitialBookingState,
+    dismissModal: mockDismissModal,
+    dispatch: mockDispatch,
+  }),
 }))
 
 describe('<AlreadyBooked />', () => {
   it('should dismiss modal when clicking on cta', () => {
-    const renderAPI = render(<AlreadyBooked offer={{ name: 'hello' } as OfferResponse} />)
-    fireEvent.press(renderAPI.getByText('Mes réservations terminées'))
+    render(<AlreadyBooked offer={{ name: 'hello' } as OfferResponse} />)
+    fireEvent.press(screen.getByText('Mes réservations terminées'))
     expect(mockDismissModal).toHaveBeenCalledTimes(1)
+  })
+
+  it('should change booking step from date to confirmation', () => {
+    render(<AlreadyBooked offer={{ name: 'hello' } as OfferResponse} />)
+    expect(mockDispatch).toHaveBeenNthCalledWith(1, {
+      type: 'CHANGE_STEP',
+      payload: Step.CONFIRMATION,
+    })
   })
 })
