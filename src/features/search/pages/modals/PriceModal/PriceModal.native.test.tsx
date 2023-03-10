639560432a4e235e60e8fd58e26efc03c91c663f
marineshaw <marines@bam.tech>
2023-03-10 16:50:48 +0100

    :bug: Mock animation where it is necessary

--- src/features/search/pages/modals/PriceModal/PriceModal.native.test.tsx
+++ src/features/search/pages/modals/PriceModal/PriceModal.native.test.tsx
@@ -18,6 +18,11 @@ const searchState: SearchState = { ...initialSearchState, searchId }
 let mockSearchState = searchState
 const mockDispatch = jest.fn()
 
+/* TODO(LucasBeneston): Remove this mock when update to Jest 28
+  In jest version 28, I don't bring that error :
+  TypeError: requestAnimationFrame is not a function */
+jest.mock('react-native/Libraries/Animated/animations/TimingAnimation')
+
 jest.mock('features/search/context/SearchWrapper', () => ({
   useSearch: () => ({
     searchState: mockSearchState,
