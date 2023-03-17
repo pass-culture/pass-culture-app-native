6272b03f40445c24ee3217d7832883f5b0fa0a74
Lucas Beneston <lucas.beneston@passculture.app>
2023-03-17 11:34:56 +0100

    (BSR) feat(culturalSurvey): fix TODO for CulturalSurveyQuestions

--- src/features/culturalSurvey/pages/CulturalSurveyIntro.web.test.tsx
+++ src/features/culturalSurvey/pages/CulturalSurveyIntro.web.test.tsx
@@ -8,16 +8,20 @@ import { render, fireEvent, screen } from 'tests/utils/web'
 jest.mock('features/culturalSurvey/helpers/useGetNextQuestion')
 jest.mock('features/navigation/helpers')
 jest.mock('features/culturalSurvey/context/CulturalSurveyContextProvider')
+
 describe('CulturalSurveyIntro page', () => {
   it('should render the page with correct layout', () => {
     const CulturalSurveyIntroPage = render(<CulturalSurveyIntro />)
+
     expect(CulturalSurveyIntroPage).toMatchSnapshot()
   })
 
   it('should open FAQ url when pressing En savoir plus', () => {
     render(<CulturalSurveyIntro />)
+
     const FAQButton = screen.getByText('En savoir plus')
     fireEvent.click(FAQButton)
+
     expect(openUrl).toHaveBeenCalledWith(FAQ_LINK_USER_DATA, undefined, true)
   })
 })
