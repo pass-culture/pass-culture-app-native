0bdce9b97b3f230b80258e5e1b7a6183fdd8fea0
Lucas Beneston <lucas.beneston@passculture.app>
2023-03-17 16:34:01 +0100

    (BSR) feat(profile): remove waitForExpect for ConfirmDeleteProfile tests

--- src/features/profile/pages/DeleteProfile/ConfirmDeleteProfile.native.test.tsx
+++ src/features/profile/pages/DeleteProfile/ConfirmDeleteProfile.native.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useMutation } from 'react-query'
-import waitForExpect from 'wait-for-expect'
 
 import { navigate } from '__mocks__/@react-navigation/native'
 import { mockGoBack } from 'features/navigation/__mocks__/useGoBack'
@@ -8,7 +7,7 @@ import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
 import { env } from 'libs/environment/env'
 import { analytics } from 'libs/firebase/analytics'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { fireEvent, render, useMutationFactory } from 'tests/utils'
+import { fireEvent, render, screen, waitFor, useMutationFactory } from 'tests/utils'
 import { SNACK_BAR_TIME_OUT } from 'ui/components/snackBar/SnackBarContext'
 import { SnackBarHelperSettings } from 'ui/components/snackBar/types'
 
@@ -38,69 +37,76 @@ const openUrl = jest.spyOn(NavigationHelpers, 'openUrl')
 
 describe('ConfirmDeleteProfile component', () => {
   it('should render confirm delete profile', () => {
-    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-    const renderAPI = render(reactQueryProviderHOC(<ConfirmDeleteProfile />))
-    expect(renderAPI.toJSON()).toMatchSnapshot()
+    renderConfirmDeleteProfile()
+
+    expect(screen).toMatchSnapshot()
   })
 
-  it('should redirect to DeleteProfileSuccess when clicking on "Supprimer mon compte" button', async () => {
+  it('should redirect to DeleteProfileSuccess when clicking on "Supprimer mon compte" button', () => {
     const useMutationCallbacks: { onSuccess: () => void } = {
       onSuccess: () => {},
     }
     // @ts-expect-error ts(2345)
     mockedUseMutation.mockImplementationOnce(useMutationFactory(useMutationCallbacks))
-    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-    const renderAPI = render(reactQueryProviderHOC(<ConfirmDeleteProfile />))
-    fireEvent.press(renderAPI.getByText('Supprimer mon compte'))
+    renderConfirmDeleteProfile()
+
+    fireEvent.press(screen.getByText('Supprimer mon compte'))
+
     useMutationCallbacks.onSuccess()
 
-    await waitForExpect(() => {
-      expect(navigate).toBeCalledTimes(1)
-      expect(navigate).toHaveBeenCalledWith('DeleteProfileSuccess')
-      expect(mockSignOut).toHaveBeenCalledTimes(1)
-    })
+    expect(navigate).toHaveBeenNthCalledWith(1, 'DeleteProfileSuccess')
+    expect(mockSignOut).toHaveBeenCalledTimes(1)
   })
 
-  it('should show error snackbar if suspend account request fails when clicking on "Supprimer mon compte" button', async () => {
+  it('should show error snackbar if suspend account request fails when clicking on "Supprimer mon compte" button', () => {
     const useMutationCallbacks: { onError: (error: unknown) => void } = {
       onError: () => {},
     }
     // @ts-expect-error ts(2345)
     mockedUseMutation.mockImplementationOnce(useMutationFactory(useMutationCallbacks))
-    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-    const renderAPI = render(reactQueryProviderHOC(<ConfirmDeleteProfile />))
-    fireEvent.press(renderAPI.getByText('Supprimer mon compte'))
+    renderConfirmDeleteProfile()
+
+    fireEvent.press(screen.getByText('Supprimer mon compte'))
+
     useMutationCallbacks.onError({ error: undefined })
 
-    await waitForExpect(() => {
-      expect(mockShowErrorSnackBar).toHaveBeenCalledWith({
-        message: 'Une erreur s’est produite pendant le chargement.',
-        timeout: SNACK_BAR_TIME_OUT,
-      })
+    expect(mockShowErrorSnackBar).toHaveBeenCalledWith({
+      message: 'Une erreur s’est produite pendant le chargement.',
+      timeout: SNACK_BAR_TIME_OUT,
     })
   })
 
   it('should log analytics and redirect to FAQ when clicking on FAQ link', async () => {
-    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-    const renderAPI = render(reactQueryProviderHOC(<ConfirmDeleteProfile />))
-    fireEvent.press(renderAPI.getByText('Consulter l’article d’aide'))
-    await waitForExpect(() => {
+    renderConfirmDeleteProfile()
+
+    fireEvent.press(screen.getByText('Consulter l’article d’aide'))
+
+    await waitFor(() => {
       expect(analytics.logConsultArticleAccountDeletion).toHaveBeenCalledTimes(1)
       expect(openUrl).toBeCalledWith(env.FAQ_LINK_DELETE_ACCOUNT, undefined, true)
     })
   })
 
   it('should go back when clicking on go back button', () => {
-    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-    const renderAPI = render(reactQueryProviderHOC(<ConfirmDeleteProfile />))
-    fireEvent.press(renderAPI.getByTestId('Revenir en arrière'))
+    renderConfirmDeleteProfile()
+
+    fireEvent.press(screen.getByTestId('Revenir en arrière'))
+
     expect(mockGoBack).toBeCalledTimes(1)
   })
 
   it('should open CGU when clicking on "conditions générales d’utilisation"', () => {
-    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
-    const { getByLabelText } = render(reactQueryProviderHOC(<ConfirmDeleteProfile />))
-    fireEvent.press(getByLabelText('conditions générales d’utilisation'))
+    renderConfirmDeleteProfile()
+
+    fireEvent.press(screen.getByLabelText('conditions générales d’utilisation'))
+
     expect(openUrl).toHaveBeenNthCalledWith(1, env.CGU_LINK, undefined, true)
   })
 })
+
+function renderConfirmDeleteProfile() {
+  return render(
+    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+    reactQueryProviderHOC(<ConfirmDeleteProfile />)
+  )
+}
