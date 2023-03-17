bd02d2d7a1a9a1dbd2a43e3b8de77be19306e636
Lucas Beneston <lucas.beneston@passculture.app>
2023-03-17 15:50:39 +0100

    (BSR) feat(offer): remove waitForExpect for ApplicationProcessingModal tests

--- src/features/offer/components/ApplicationProcessingModal/ApplicationProcessingModal.native.test.tsx
+++ src/features/offer/components/ApplicationProcessingModal/ApplicationProcessingModal.native.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import waitForExpect from 'wait-for-expect'
 
 import { navigate } from '__mocks__/@react-navigation/native'
 import { analytics } from 'libs/firebase/analytics'
-import { fireEvent, render } from 'tests/utils'
+import { fireEvent, render, screen, waitFor } from 'tests/utils'
 
 import { ApplicationProcessingModal } from './ApplicationProcessingModal'
 
@@ -14,32 +13,26 @@ const offerId = 1
 
 describe('<ApplicationProcessingModal />', () => {
   it('should match previous snapshot', () => {
-    const modal = render(
-      <ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />
-    )
+    render(<ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />)
 
-    expect(modal).toMatchSnapshot()
+    expect(screen).toMatchSnapshot()
   })
 
   it('should navigate to profile when clicking on button "Aller sur mon profil"', async () => {
-    const { getByTestId } = render(
-      <ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />
-    )
-    const button = getByTestId('Aller sur mon profil')
+    render(<ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />)
 
+    const button = screen.getByTestId('Aller sur mon profil')
     fireEvent.press(button)
 
-    await waitForExpect(() => {
+    await waitFor(() => {
       expect(navigate).toHaveBeenCalledWith('TabNavigator', { screen: 'Profile' })
     })
   })
 
-  it('should log analytics when clicking on button "Aller sur mon profil', async () => {
-    const { getByTestId } = render(
-      <ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />
-    )
+  it('should log analytics when clicking on button "Aller sur mon profil', () => {
+    render(<ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />)
 
-    fireEvent.press(getByTestId('Aller sur mon profil'))
+    fireEvent.press(screen.getByTestId('Aller sur mon profil'))
 
     expect(analytics.logGoToProfil).toHaveBeenCalledWith({
       from: 'ApplicationProcessingModal',
@@ -47,22 +40,18 @@ describe('<ApplicationProcessingModal />', () => {
     })
   })
 
-  it('should close modal when clicking on button "Mettre en favori', async () => {
-    const { getByText } = render(
-      <ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />
-    )
+  it('should close modal when clicking on button "Mettre en favori', () => {
+    render(<ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />)
 
-    fireEvent.press(getByText('Mettre en favori'))
+    fireEvent.press(screen.getByText('Mettre en favori'))
 
     expect(hideModal).toHaveBeenCalledTimes(1)
   })
 
-  it('should log analytics when clicking on button "Mettre en favori', async () => {
-    const { getByText } = render(
-      <ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />
-    )
+  it('should log analytics when clicking on button "Mettre en favori', () => {
+    render(<ApplicationProcessingModal visible hideModal={hideModal} offerId={offerId} />)
 
-    fireEvent.press(getByText('Mettre en favori'))
+    fireEvent.press(screen.getByText('Mettre en favori'))
 
     expect(analytics.logHasAddedOfferToFavorites).toHaveBeenCalledWith({
       from: 'ApplicationProcessingModal',
