5d6b98c6739366944cf02b916bb633bbb88ad4db
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-03-21 09:42:13 +0100

    Add test:perf command

--- scripts/reassure-test.sh
+++ scripts/reassure-test.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-# Test on branch PC-20631-dup
-BASELINE_BRANCH=${BASELINE_BRANCH:="PC-20631-dup"}
+# Test on branch PC-20631-duplicate
+BASELINE_BRANCH=${BASELINE_BRANCH:="PC-20631-duplicate"}
 
 # Required for `git switch` on CI
 git fetch origin
 
 # Gather baseline perf measurements
 git switch "$BASELINE_BRANCH"
-yarn add reassure --ignore-scripts
-yarn install --force
-yarn reassure --baseline
+# yarn add reassure --ignore-scripts
+# yarn install --force
+yarn test:perf --baseline
 
 # Gather current perf measurements & compare results
 git switch --detach -
-yarn install --force
-yarn reassure
+# yarn install --force
+yarn test:perf
