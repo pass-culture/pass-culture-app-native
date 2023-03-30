c8ead52686b8ee68bbb12ad4a0cf8666793be217
bruno ebstein <bruno.ebstein@passculture.app>
2023-03-30 18:10:18 +0200

    (PC-20788) feat(SEO): add top 50 offer for referencing (#4576)
    Co-authored-by: bruno ebstein <bruno.ebstein@passculture.app>
    Co-authored-by: Lucas BENESTON <Lucasbeneston@users.noreply.github.com>
    Co-authored-by: Hugo Voisin <hugov@bam.tech>
    Co-authored-by: marineshaw <marines@bam.tech>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,19 @@
+import path from 'path'
+
 import express from 'express'
 
 import { webAppProxyMiddleware } from './middlewares/webAppProxyMiddleware'
 
 export const app = express()
+
+const options = {
+  // We set the index option at false because `public/index.html` is displayed instead of the homepage
+  index: false,
+}
+
+const rootFolder = path.dirname(path.dirname(__dirname))
+// We want to serve static files from the root folder /public
+// https://expressjs.com/en/4x/api.html#express.static
+app.use(express.static(path.join(rootFolder, 'public'), options))
 
 app.use('*', webAppProxyMiddleware)
