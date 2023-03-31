853d0a2f079ee52583a27f15b8d45f8da48bde4e
YassinL <yassin.leclercq1@gmail.com>
2023-03-31 18:09:15 +0200

    (PC-21040) update(Pick): update test to fit with string types for Picker

--- src/features/bookOffer/components/Calendar/Calendar.utils.test.ts
+++ src/features/bookOffer/components/Calendar/Calendar.utils.test.ts
@@ -1,4 +1,3 @@
-import range from 'lodash/range'
 import timezoneMock, { TimeZone } from 'timezone-mock'
 
 import { getDatesInMonth } from 'features/bookOffer/components/Calendar/Calendar.utils'
@@ -13,10 +12,15 @@ describe('getDatesInMonth()', () => {
     'should return a list [1, 2, ..., N] for some specified month and year with timezone $timezone',
     ({ timezone }: { timezone: TimeZone }) => {
       timezoneMock.register(timezone)
-      expect(getDatesInMonth(9, 2021)).toEqual(range(1, 31 + 1)) // october 2021 has 31 days
-      expect(getDatesInMonth(10, 2021)).toEqual(range(1, 30 + 1)) // november 2021 has 30 days
-      expect(getDatesInMonth(1, 2009)).toEqual(range(1, 28 + 1)) // february 2009 has 28 days
-      expect(getDatesInMonth(1, 2008)).toEqual(range(1, 29 + 1)) // february 2008 has 29 days
+
+      expect(getDatesInMonth(9, '2021')).toEqual(getArrayOfStrings(31)) // october 2021 has 31 days
+      expect(getDatesInMonth(10, '2021')).toEqual(getArrayOfStrings(30)) // november 2021 has 30 days
+      expect(getDatesInMonth(1, '2009')).toEqual(getArrayOfStrings(28)) // february 2009 has 28 days
+      expect(getDatesInMonth(1, '2008')).toEqual(getArrayOfStrings(29)) // february 2008 has 29 days
     }
   )
 })
+
+function getArrayOfStrings(day: number): string[] {
+  return Array.from({ length: day }, (_, i) => (i + 1).toString())
+}
