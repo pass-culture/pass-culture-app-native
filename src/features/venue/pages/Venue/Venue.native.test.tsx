515731398baddd219b90aec4f2edaf5591e00666
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-04-07 15:10:07 +0200

    (PC-21405) feat(venue): log consult venue when venue consulted from deeplink

--- src/features/venue/pages/Venue/Venue.native.test.tsx
+++ src/features/venue/pages/Venue/Venue.native.test.tsx
@@ -2,8 +2,10 @@ import mockdate from 'mockdate'
 import React from 'react'
 
 import { useRoute } from '__mocks__/@react-navigation/native'
+import { Referrals } from 'features/navigation/RootNavigator/types'
 import { venueResponseSnap } from 'features/venue/fixtures/venueResponseSnap'
 import { Venue } from 'features/venue/pages/Venue/Venue'
+import { analytics } from 'libs/firebase/analytics'
 import { placeholderData } from 'libs/subcategories/placeholderData'
 import { render, waitFor } from 'tests/utils'
 
@@ -30,10 +32,28 @@ describe('<Venue />', () => {
     const venue = await renderVenue(venueId)
     expect(venue).toMatchSnapshot()
   })
+
+  it('should log consult venue when URL has from param with deeplink', async () => {
+    await renderVenue(venueId, 'deeplink')
+    expect(analytics.logConsultVenue).toHaveBeenNthCalledWith(1, {
+      venueId,
+      from: 'deeplink',
+    })
+  })
+
+  it('should not log consult venue when URL has "from" param with something other than deeplink', async () => {
+    await renderVenue(venueId, 'search')
+    expect(analytics.logConsultVenue).not.toHaveBeenCalled()
+  })
+
+  it('should not log consult venue when URL has not "from" param', async () => {
+    await renderVenue(venueId)
+    expect(analytics.logConsultVenue).not.toHaveBeenCalled()
+  })
 })
 
-async function renderVenue(id: number) {
-  useRoute.mockImplementation(() => ({ params: { id } }))
+async function renderVenue(id: number, from?: Referrals) {
+  useRoute.mockImplementation(() => ({ params: { id, from } }))
   const wrapper = render(<Venue />)
   await waitFor(() => wrapper)
   return wrapper
