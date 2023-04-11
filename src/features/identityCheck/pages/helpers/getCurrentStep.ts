58324e1929447590a64097222f5512cf4ed69c35
Eve Julliard <evej@bam.tech>
2023-04-11 11:26:19 +0200

    (PC-20931) chore(useCurrentSubscritptionStep): get steps from new useStepperInfo and duplicate logic to also temporarily support both type of steps

--- src/features/identityCheck/pages/helpers/getCurrentStep.ts
+++ src/features/identityCheck/pages/helpers/getCurrentStep.ts
@@ -1,7 +1,12 @@
-import { DeprecatedStepConfig } from 'features/identityCheck/types'
+import { DeprecatedStepConfig, StepDetails } from 'features/identityCheck/types'
 import { SubscriptionRootStackParamList } from 'features/navigation/RootNavigator/types'
 
 export const deprecatedGetCurrentStep = (
   steps: DeprecatedStepConfig[],
+  currentRoute: keyof SubscriptionRootStackParamList
+) => steps.find((step) => step.screens.includes(currentRoute)) || null
+
+export const getCurrentStep = (
+  steps: StepDetails[],
   currentRoute: keyof SubscriptionRootStackParamList
 ) => steps.find((step) => step.screens.includes(currentRoute)) || null
--- src/features/identityCheck/pages/helpers/useCurrentSubscriptionStep.ts
+++ src/features/identityCheck/pages/helpers/useCurrentSubscriptionStep.ts
@@ -1,14 +1,31 @@
 import { useRoute } from '@react-navigation/native'
 
-import { deprecatedGetCurrentStep } from 'features/identityCheck/pages/helpers/getCurrentStep'
+import {
+  deprecatedGetCurrentStep,
+  getCurrentStep,
+} from 'features/identityCheck/pages/helpers/getCurrentStep'
 import { isSubscriptionRoute } from 'features/identityCheck/pages/helpers/isSubscriptionRoute'
+import { useStepperInfo } from 'features/identityCheck/pages/helpers/useStepperInfo'
 import { useSubscriptionSteps } from 'features/identityCheck/pages/helpers/useSubscriptionSteps'
-import { DeprecatedIdentityCheckStep } from 'features/identityCheck/types'
+import { DeprecatedIdentityCheckStep, IdentityCheckStep } from 'features/identityCheck/types'
+import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 
-export const useCurrentSubscriptionStep = (): DeprecatedIdentityCheckStep | null => {
+export const useCurrentSubscriptionStep = ():
+  | DeprecatedIdentityCheckStep
+  | IdentityCheckStep
+  | null => {
   const { name } = useRoute()
   const deprecatedSteps = useSubscriptionSteps()
+  const { stepsDetails } = useStepperInfo()
+  const wipStepperRetryUbble = useFeatureFlag(RemoteStoreFeatureFlags.WIP_STEPPER_RETRY_UBBLE)
   const currentRoute = isSubscriptionRoute(name) ? name : null
+
+  if (wipStepperRetryUbble) {
+    const currentStep = currentRoute ? getCurrentStep(stepsDetails, currentRoute) : null
+    return currentStep ? currentStep.name : null
+  }
+
   const currentStep = currentRoute ? deprecatedGetCurrentStep(deprecatedSteps, currentRoute) : null
   return currentStep ? currentStep.name : null
 }
