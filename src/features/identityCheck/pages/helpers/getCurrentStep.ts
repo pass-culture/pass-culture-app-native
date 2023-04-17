44942029447d47ef7762c1be3ec63e2cd55d10af
Eve Julliard <evej@bam.tech>
2023-04-17 11:32:19 +0200

    (PC-20931) chore(deprecatedGetCurrentStep): remove deprecated function because there is no use of it anymore

--- src/features/identityCheck/pages/helpers/getCurrentStep.ts
+++ src/features/identityCheck/pages/helpers/getCurrentStep.ts
@@ -1,10 +1,5 @@
-import { DeprecatedStepConfig, StepDetails } from 'features/identityCheck/types'
+import { StepDetails } from 'features/identityCheck/types'
 import { SubscriptionRootStackParamList } from 'features/navigation/RootNavigator/types'
-
-export const deprecatedGetCurrentStep = (
-  steps: DeprecatedStepConfig[],
-  currentRoute: keyof SubscriptionRootStackParamList
-) => steps.find((step) => step.screens.includes(currentRoute)) || null
 
 export const getCurrentStep = (
   steps: StepDetails[],
