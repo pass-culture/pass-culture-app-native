8af16089cbffd26e4b0b1e19757f29dc00d24bae
dbenfouzari-pass <114916654+dbenfouzari-pass@users.noreply.github.com>
2023-04-20 14:04:50 +0200

    (PC-21625) refactor(search): export bodysearch component (#4681)
    * (PC-21625) refactor(search): export bodysearch component
    
    * (PC-21625) test(search): add tests to new componetn
    
    * (PC-21625) test(search): regenerate failing snapshots

---
+++ src/features/search/components/BodySearch/BodySearch.native.test.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import { mockSuggestionHits } from 'features/search/fixtures/algolia'
+import { SearchView } from 'features/search/types'
+import { placeholderData as mockData } from 'libs/subcategories/placeholderData'
+import { render, screen } from 'tests/utils'
+
+import { BodySearch } from './BodySearch'
+
+jest.mock('react-query')
+
+jest.mock('react-instantsearch-hooks', () => ({
+  useInfiniteHits: () => ({
+    hits: mockSuggestionHits,
+  }),
+}))
+
+jest.mock('libs/subcategories/useSubcategories', () => ({
+  useSubcategories: () => ({
+    data: mockData,
+  }),
+}))
+
+jest.mock('features/search/api/useSearchResults/useSearchResults', () => ({
+  useSearchResults: () => ({
+    data: mockData,
+    hits: [],
+    nbHits: 0,
+    isFetching: false,
+    isLoading: false,
+    hasNextPage: true,
+    fetchNextPage: jest.fn(),
+    isFetchingNextPage: false,
+    userData: [],
+  }),
+}))
+
+describe('<BodySearch />', () => {
+  it('should render categories buttons by default', () => {
+    render(<BodySearch />)
+
+    expect(screen.getByTestId('categoriesButtons')).toBeTruthy()
+    expect(screen.queryByTestId('autocompleteList')).toBeNull()
+    expect(screen.queryByTestId('searchResults')).toBeNull()
+  })
+
+  it('should render suggestions when asked', () => {
+    render(<BodySearch view={SearchView.Suggestions} />)
+
+    expect(screen.queryByTestId('categoriesButtons')).toBeNull()
+    expect(screen.getByTestId('autocompleteList')).toBeTruthy()
+    expect(screen.queryByTestId('searchResults')).toBeNull()
+  })
+
+  it('should render search results when asked', () => {
+    render(<BodySearch view={SearchView.Results} />)
+
+    expect(screen.queryByTestId('categoriesButtons')).toBeNull()
+    expect(screen.queryByTestId('autocompleteList')).toBeNull()
+    expect(screen.findByTestId('searchResults')).toBeTruthy()
+  })
+})
--- src/features/search/pages/Search/Search.tsx
+++ src/features/search/pages/Search/Search.tsx
@@ -1,20 +1,16 @@
 import { useRoute } from '@react-navigation/native'
 import { SearchClient } from 'algoliasearch'
 import { SendEventForHits } from 'instantsearch.js/es/lib/utils'
-import React, { memo, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Configure, InstantSearch } from 'react-instantsearch-hooks'
 import { StatusBar } from 'react-native'
-import styled from 'styled-components/native'
 import { v4 as uuidv4 } from 'uuid'
 
 import { UseRouteType } from 'features/navigation/RootNavigator/types'
-import { CategoriesButtons } from 'features/search/components/CategoriesButtons/CategoriesButtons'
-import { SearchAutocomplete } from 'features/search/components/SearchAutocomplete/SearchAutocomplete'
+import { BodySearch } from 'features/search/components/BodySearch/BodySearch'
 import { SearchAutocompleteItem } from 'features/search/components/SearchAutocompleteItem/SearchAutocompleteItem'
 import { SearchHeader } from 'features/search/components/SearchHeader/SearchHeader'
-import { SearchResults } from 'features/search/components/SearchResults/SearchResults'
 import { useSearch } from 'features/search/context/SearchWrapper'
-import { useShowResultsForCategory } from 'features/search/helpers/useShowResultsForCategory/useShowResultsForCategory'
 import { SearchView } from 'features/search/types'
 import { AlgoliaSuggestionHit } from 'libs/algolia'
 import { InsightsMiddleware } from 'libs/algolia/analytics/InsightsMiddleware'
@@ -23,7 +19,6 @@ import { env } from 'libs/environment'
 import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
 import { OfflinePage } from 'libs/network/OfflinePage'
 import { Form } from 'ui/components/Form'
-import { Spacer } from 'ui/theme'
 
 const searchInputID = uuidv4()
 const searchClient: SearchClient = {
@@ -48,30 +43,6 @@ const searchClient: SearchClient = {
   },
 }
 const suggestionsIndex = env.ALGOLIA_SUGGESTIONS_INDEX_NAME
-
-type BodySearchProps = {
-  view?: SearchView
-}
-
-const BodySearch = memo(function BodySearch({ view }: BodySearchProps) {
-  const showResultsForCategory = useShowResultsForCategory()
-
-  if (view === SearchView.Suggestions) {
-    return (
-      <React.Fragment>
-        <SearchAutocomplete hitComponent={Hit} />
-      </React.Fragment>
-    )
-  } else if (view === SearchView.Results) {
-    return <SearchResults />
-  }
-  return (
-    <Container>
-      <CategoriesButtons onPressCategory={showResultsForCategory} />
-      <Spacer.TabBar />
-    </Container>
-  )
-})
 
 export function Search() {
   const netInfo = useNetInfoContext()
@@ -100,11 +71,6 @@ export function Search() {
     </React.Fragment>
   )
 }
-
-const Container = styled.View({
-  flex: 1,
-  overflowY: 'auto',
-})
 
 export type HitProps = {
   hit: AlgoliaSuggestionHit
