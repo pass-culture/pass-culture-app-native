6d060136066a441464bce1ceea7f4641680dbd66
marineshaw <marines@bam.tech>
2023-04-21 10:33:56 +0200

    :recycle: (BookingImpossible) use msw instead of react query mock in tests

--- src/features/bookOffer/components/BookingImpossible.native.test.tsx
+++ src/features/bookOffer/components/BookingImpossible.native.test.tsx
@@ -1,44 +1,62 @@
+import { rest } from 'msw'
 import * as React from 'react'
-import { useMutation } from 'react-query'
 
+import { api } from 'api/api'
+import { FavoriteResponse } from 'api/gen'
 import { initialBookingState, Step } from 'features/bookOffer/context/reducer'
-import { fireEvent, render, screen, useMutationFactory } from 'tests/utils'
+import { favoriteResponseSnap } from 'features/favorites/fixtures/favoriteResponseSnap'
+import { env } from 'libs/environment'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { server } from 'tests/server'
+import { act, fireEvent, render, screen } from 'tests/utils'
 
 import { BookingImpossible } from './BookingImpossible'
-
-jest.mock('react-query')
-const mockedUseMutation = jest.mocked(useMutation)
 
 jest.mock('features/auth/context/AuthContext', () => ({
   useAuthContext: jest.fn(() => ({ isLoggedIn: true })),
 }))
 
-const mockInitialBookingState = initialBookingState
+const mockOfferId = favoriteResponseSnap.offer.id
 
+const mockInitialBookingState = initialBookingState
 const mockDismissModal = jest.fn()
 const mockDispatch = jest.fn()
 jest.mock('features/bookOffer/context/useBookingContext', () => ({
   useBookingContext: jest.fn(() => ({
-    bookingState: { ...mockInitialBookingState, offerId: 20 },
+    bookingState: { ...mockInitialBookingState, offerId: mockOfferId },
     dismissModal: mockDismissModal,
     dispatch: mockDispatch,
   })),
 }))
 
-describe('<BookingImpossible />', () => {
-  it('should call notifyWebappLinkSent when adding to favorites', async () => {
-    const useMutationAddFavoriteCallbacks: Parameters<typeof useMutationFactory>[0] = {
-      onSuccess: () => {},
-      onError: () => {},
-    }
-    const useMutationNotifyWebappLinkSentCallbacks: Parameters<typeof useMutationFactory>[0] = {
-      onSuccess: () => {},
-      onError: () => {},
-    }
+const mockPostnativev1sendOfferWebappLinkByEmailofferId = jest.spyOn(
+  api,
+  'postnativev1sendOfferWebappLinkByEmailofferId'
+)
 
-    const mockUseMutationNotifyWebappLinkSent = useMutationFactory(
-      useMutationNotifyWebappLinkSentCallbacks
+const mockPostnativev1sendOfferLinkByPushofferId = jest.spyOn(
+  api,
+  'postnativev1sendOfferLinkByPushofferId'
+)
+
+describe('<BookingImpossible />', () => {
+  beforeAll(() => {
+    const favoriteResponse: FavoriteResponse = favoriteResponseSnap
+
+    server.use(
+      rest.post(`${env.API_BASE_URL}/native/v1/me/favorites`, (_req, res, ctx) =>
+        res(ctx.status(200), ctx.json(favoriteResponse))
+      ),
+      rest.post(
+        `${env.API_BASE_URL}/native/v1/send_offer_link_by_push/${mockOfferId}`,
+        (_req, res, ctx) => res(ctx.status(200), ctx.json({}))
+      ),
+      rest.post(
+        `${env.API_BASE_URL}/native/v1/send_offer_webapp_link_by_email/${mockOfferId}`,
+        (_req, res, ctx) => res(ctx.status(200), ctx.json({}))
+      )
     )
+  })
 
     mockedUseMutation
       // @ts-expect-error ts(2345)
@@ -46,23 +64,26 @@ describe('<BookingImpossible />', () => {
       // @ts-expect-error ts(2345)
       .mockImplementationOnce(useMutationFactory(useMutationAddFavoriteCallbacks))
 
-    render(<BookingImpossible />)
+  it('should send email/push notification when adding to favorites', async () => {
+    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+    render(reactQueryProviderHOC(<BookingImpossible />))
 
-    const addToFavoriteButton = screen.getByText('Mettre en favoris')
+    await act(async () => {
+      const addToFavoriteButton = await screen.findByLabelText('Mettre en favoris')
 
-    fireEvent.press(addToFavoriteButton)
+      fireEvent.press(addToFavoriteButton)
+    })
 
-    useMutationAddFavoriteCallbacks.onSuccess()
-    expect(
-      mockUseMutationNotifyWebappLinkSent(() => {}, {
-        onSuccess: () => {},
-        onError: () => {},
-      }).mutate
-    ).toHaveBeenCalledTimes(1)
+    expect(mockPostnativev1sendOfferWebappLinkByEmailofferId).toHaveBeenCalledWith(mockOfferId)
+    expect(mockPostnativev1sendOfferLinkByPushofferId).toHaveBeenCalledWith(mockOfferId)
   })
 
-  it('should change booking step from date to confirmation', () => {
-    render(<BookingImpossible />)
+  it('should change booking step from date to confirmation', async () => {
+    // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+    render(reactQueryProviderHOC(<BookingImpossible />))
+
+    await screen.findByLabelText('Mettre en favoris')
+
     expect(mockDispatch).toHaveBeenNthCalledWith(1, {
       type: 'CHANGE_STEP',
       payload: Step.CONFIRMATION,
