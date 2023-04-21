88bdb4efa204f3b7b83e702bc81f131700168bea
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-04-21 15:36:07 +0200

    (PC-21909) refactor(search): externalize helpers for buildFacetFilters

---
+++ src/libs/algolia/fetchAlgolia/buildAlgoliaParameters/helpers/buildFacetFiltersHelpers/buildFacetFiltersHelpers.test.ts
@@ -0,0 +1,220 @@
+import {
+  GenreType,
+  NativeCategoryIdEnumv2,
+  SearchGroupNameEnumv2,
+  SubcategoryIdEnumv2,
+} from 'api/gen'
+import {
+  buildObjectIdsPredicate,
+  buildOfferCategoriesPredicate,
+  buildOfferGenreTypesPredicate,
+  buildOfferIsDuoPredicate,
+  buildOfferNativeCategoriesPredicate,
+  buildOfferSubcategoriesPredicate,
+  buildOfferTypesPredicate,
+  buildTagsPredicate,
+} from 'libs/algolia/fetchAlgolia/buildAlgoliaParameters/helpers/buildFacetFiltersHelpers/buildFacetFiltersHelpers'
+import { eventMonitoring } from 'libs/monitoring'
+
+describe('buildFacetFilters', () => {})
+
+describe('buildOfferCategoriesPredicate', () => {
+  it('should return an offer categories predicate formatted for Algolia API', () => {
+    const offerCategoriesPredicate = buildOfferCategoriesPredicate([
+      SearchGroupNameEnumv2.ARTS_LOISIRS_CREATIFS,
+      SearchGroupNameEnumv2.BIBLIOTHEQUES_MEDIATHEQUE,
+    ])
+    expect(offerCategoriesPredicate).toEqual([
+      'offer.searchGroupNamev2:ARTS_LOISIRS_CREATIFS',
+      'offer.searchGroupNamev2:BIBLIOTHEQUES_MEDIATHEQUE',
+    ])
+  })
+})
+
+describe('buildOfferSubcategoriesPredicate', () => {
+  it('should return an offer subcategories predicate formatted for Algolia API', () => {
+    const offerSubcategoriesPredicate = buildOfferSubcategoriesPredicate([
+      SubcategoryIdEnumv2.CONCERT,
+      SubcategoryIdEnumv2.SEANCE_CINE,
+    ])
+    expect(offerSubcategoriesPredicate).toEqual([
+      'offer.subcategoryId:CONCERT',
+      'offer.subcategoryId:SEANCE_CINE',
+    ])
+  })
+})
+
+describe('buildOfferNativeCategoriesPredicate', () => {
+  it('should return an offer native categories predicate formatted for Algolia API', () => {
+    const offerSubcategoriesPredicate = buildOfferNativeCategoriesPredicate([
+      NativeCategoryIdEnumv2.ARTS_VISUELS,
+      NativeCategoryIdEnumv2.DVD_BLU_RAY,
+    ])
+    expect(offerSubcategoriesPredicate).toEqual([
+      'offer.nativeCategoryId:ARTS_VISUELS',
+      'offer.nativeCategoryId:DVD_BLU_RAY',
+    ])
+  })
+})
+
+describe('buildOfferGenreTypesPredicate', () => {
+  it('should return an offer genre types predicate with an empty string when no API correspondence', () => {
+    const offerGenreTypePredicate = buildOfferGenreTypesPredicate([
+      { key: 'DRAMA' as GenreType, name: 'Comédie', value: 'Comédie' },
+    ])
+    expect(offerGenreTypePredicate).toEqual([''])
+  })
+
+  it('should return a movie offer genre types predicate formatted for Algolia API', () => {
+    const offerGenreTypePredicate = buildOfferGenreTypesPredicate([
+      { key: GenreType.MOVIE, name: 'DETECTIVE', value: 'Policier' },
+      { key: GenreType.MOVIE, name: 'ROMANCE', value: 'Romance' },
+    ])
+    expect(offerGenreTypePredicate).toEqual([
+      'offer.movieGenres:DETECTIVE',
+      'offer.movieGenres:ROMANCE',
+    ])
+  })
+
+  it('should return a book offer genre types predicate formatted for Algolia API', () => {
+    const offerGenreTypePredicate = buildOfferGenreTypesPredicate([
+      { key: GenreType.BOOK, name: 'Policier', value: 'Policier' },
+      { key: GenreType.BOOK, name: 'Romance', value: 'Romance' },
+    ])
+    expect(offerGenreTypePredicate).toEqual([
+      'offer.bookMacroSection:Policier',
+      'offer.bookMacroSection:Romance',
+    ])
+  })
+
+  it('should return a music offer genre types predicate formatted for Algolia API', () => {
+    const offerGenreTypePredicate = buildOfferGenreTypesPredicate([
+      { key: GenreType.MUSIC, name: 'Pop', value: 'Pop' },
+      { key: GenreType.MUSIC, name: 'Rap', value: 'Rap' },
+    ])
+    expect(offerGenreTypePredicate).toEqual(['offer.musicType:Pop', 'offer.musicType:Rap'])
+  })
+
+  it('should return a show offer genre types predicate formatted for Algolia API', () => {
+    const offerGenreTypePredicate = buildOfferGenreTypesPredicate([
+      { key: GenreType.SHOW, name: 'Comédie musicale', value: 'Comédie musicale' },
+      { key: GenreType.SHOW, name: 'Pièce de théâtre', value: 'Pièce de théâtre' },
+    ])
+    expect(offerGenreTypePredicate).toEqual([
+      'offer.showType:Comédie musicale',
+      'offer.showType:Pièce de théâtre',
+    ])
+  })
+})
+
+describe('buildObjectIdsPredicate', () => {
+  it('should return an object ids predicate formatted for Algolia API', () => {
+    const objectIdsPredicate = buildObjectIdsPredicate(['15000', '150001'])
+    expect(objectIdsPredicate).toEqual(['objectID:15000', 'objectID:150001'])
+  })
+
+  it('should catch an error Sentry when object ids param not correcty passed and return an empty array', () => {
+    const error = new TypeError('objectIds.map is not a function')
+    const objectIdsPredicate = buildObjectIdsPredicate('15000' as unknown as string[])
+    expect(eventMonitoring.captureException).toBeCalledWith(error, {
+      level: 'error',
+      objectIds: '15000',
+    })
+    expect(objectIdsPredicate).toEqual([])
+  })
+})
+
+describe('buildOfferIsDuoPredicate', () => {
+  it('should return an offer is duo predicate formatted for Algolia API when offerIsDuo param is true', () => {
+    const offerIsDuoPredicate = buildOfferIsDuoPredicate(true)
+    expect(offerIsDuoPredicate).toEqual(['offer.isDuo:true'])
+  })
+
+  it('should return undefined when offerIsDuo param is false', () => {
+    const offerIsDuoPredicate = buildOfferIsDuoPredicate(false)
+    expect(offerIsDuoPredicate).toEqual(undefined)
+  })
+})
+
+describe('buildOfferTypesPredicate', () => {
+  it('should return undefined when all offer types are false', () => {
+    const offerTypesPredicate = buildOfferTypesPredicate({
+      isDigital: false,
+      isEvent: false,
+      isThing: false,
+    })
+    expect(offerTypesPredicate).toEqual(undefined)
+  })
+
+  it('should return an offer types predicate formatted for Algolia API with isDigital = true when offer is digital, is not an event and a thing', () => {
+    const offerTypesPredicate = buildOfferTypesPredicate({
+      isDigital: true,
+      isEvent: false,
+      isThing: false,
+    })
+    expect(offerTypesPredicate).toEqual([['offer.isDigital:true']])
+  })
+
+  it('should return an offer types predicate formatted for Algolia API with isThing = true when offer is digital, is a thing and is not an event', () => {
+    const offerTypesPredicate = buildOfferTypesPredicate({
+      isDigital: true,
+      isEvent: false,
+      isThing: true,
+    })
+    expect(offerTypesPredicate).toEqual([['offer.isThing:true']])
+  })
+
+  it('should return an offer types predicate formatted for Algolia API with isDigital = true and isEvent = true when offer is digital, is an event and is not a thing', () => {
+    const offerTypesPredicate = buildOfferTypesPredicate({
+      isDigital: true,
+      isEvent: true,
+      isThing: false,
+    })
+    expect(offerTypesPredicate).toEqual([['offer.isDigital:true', 'offer.isEvent:true']])
+  })
+
+  it('should return an offer types predicate formatted for Algolia API with isDigital = false and isThing = true when offer is not digital, is not an event and is a thing', () => {
+    const offerTypesPredicate = buildOfferTypesPredicate({
+      isDigital: false,
+      isEvent: false,
+      isThing: true,
+    })
+    expect(offerTypesPredicate).toEqual([['offer.isDigital:false'], ['offer.isThing:true']])
+  })
+
+  it('should return an offer types predicate formatted for Algolia API with isEvent = true when offer is not digital, is not a thing and is an event', () => {
+    const offerTypesPredicate = buildOfferTypesPredicate({
+      isDigital: false,
+      isEvent: true,
+      isThing: false,
+    })
+    expect(offerTypesPredicate).toEqual([['offer.isEvent:true']])
+  })
+
+  it('should return an offer types predicate formatted for Algolia API with isDigital = false when offer is not digital, is a thing and is an event', () => {
+    const offerTypesPredicate = buildOfferTypesPredicate({
+      isDigital: false,
+      isEvent: true,
+      isThing: true,
+    })
+    expect(offerTypesPredicate).toEqual([['offer.isDigital:false']])
+  })
+})
+
+describe('buildTagsPredicate', () => {
+  it('should return undefined when no tags associated', () => {
+    const tagsPredicate = buildTagsPredicate([])
+    expect(tagsPredicate).toEqual(undefined)
+  })
+
+  it('should return a tags predicate for formatted for Algolia API', () => {
+    const tagsPredicate = buildTagsPredicate([
+      'Semaine du 14 juillet',
+      'Offre cinema spéciale pass culture',
+    ])
+    expect(tagsPredicate).toEqual([
+      'offer.tags:Semaine du 14 juillet',
+      'offer.tags:Offre cinema spéciale pass culture',
+    ])
+  })
+})
---
+++ src/libs/algolia/fetchAlgolia/buildAlgoliaParameters/helpers/buildFacetFiltersHelpers/buildFacetFiltersHelpers.ts
@@ -0,0 +1,79 @@
+import {
+  GenreType,
+  NativeCategoryIdEnumv2,
+  SearchGroupNameEnumv2,
+  SubcategoryIdEnumv2,
+} from 'api/gen'
+import { OfferGenreType } from 'features/search/types'
+import { FACETS_FILTERS_ENUM } from 'libs/algolia/enums'
+import { FiltersArray, SearchParametersQuery } from 'libs/algolia/types'
+import { eventMonitoring } from 'libs/monitoring'
+
+export const buildOfferCategoriesPredicate = (searchGroups: SearchGroupNameEnumv2[]): string[] =>
+  searchGroups.map((searchGroup) => `${FACETS_FILTERS_ENUM.OFFER_SEARCH_GROUP_NAME}:${searchGroup}`)
+
+export const buildOfferSubcategoriesPredicate = (subcategoryIds: SubcategoryIdEnumv2[]): string[] =>
+  subcategoryIds.map(
+    (subcategoryId) => `${FACETS_FILTERS_ENUM.OFFER_SUB_CATEGORY}:${subcategoryId}`
+  )
+
+export const buildOfferNativeCategoriesPredicate = (nativeCategories: NativeCategoryIdEnumv2[]) =>
+  nativeCategories.map(
+    (nativeCategory) => `${FACETS_FILTERS_ENUM.OFFER_NATIVE_CATEGORY}:${nativeCategory}`
+  )
+
+const offerGenreTypesPredicate = {
+  [GenreType.MOVIE]: FACETS_FILTERS_ENUM.OFFER_MOVIE_GENRES,
+  [GenreType.BOOK]: FACETS_FILTERS_ENUM.OFFER_BOOK_TYPE,
+  [GenreType.MUSIC]: FACETS_FILTERS_ENUM.OFFER_MUSIC_TYPE,
+  [GenreType.SHOW]: FACETS_FILTERS_ENUM.OFFER_SHOW_TYPE,
+}
+
+export const buildOfferGenreTypesPredicate = (offerGenreTypes: OfferGenreType[]) =>
+  offerGenreTypes.map((offerGenreType) =>
+    offerGenreTypesPredicate[offerGenreType.key]
+      ? `${offerGenreTypesPredicate[offerGenreType.key]}:${[offerGenreType.name]}`
+      : ''
+  )
+
+export const buildObjectIdsPredicate = (objectIds: string[]) => {
+  try {
+    return objectIds.map((objectId) => `${FACETS_FILTERS_ENUM.OBJECT_ID}:${objectId}`)
+  } catch (error) {
+    eventMonitoring.captureException(error, {
+      level: 'error',
+      objectIds,
+    })
+    return []
+  }
+}
+
+export const buildOfferIsDuoPredicate = (offerIsDuo: boolean): string[] | undefined =>
+  offerIsDuo ? [`${FACETS_FILTERS_ENUM.OFFER_IS_DUO}:${offerIsDuo}`] : undefined
+
+export const buildOfferTypesPredicate = (
+  offerTypes: SearchParametersQuery['offerTypes']
+): FiltersArray | undefined => {
+  const { isDigital, isEvent, isThing } = offerTypes
+  const DIGITAL = `${FACETS_FILTERS_ENUM.OFFER_IS_DIGITAL}:${isDigital}`
+  const EVENT = `${FACETS_FILTERS_ENUM.OFFER_IS_EVENT}:${isEvent}`
+  const THING = `${FACETS_FILTERS_ENUM.OFFER_IS_THING}:${isThing}`
+
+  if (isDigital) {
+    if (!isEvent && !isThing) return [[DIGITAL]]
+    if (!isEvent && isThing) return [[THING]]
+    if (isEvent && !isThing) return [[DIGITAL, EVENT]]
+  } else {
+    if (!isEvent && isThing) return [[DIGITAL], [THING]]
+    if (isEvent && !isThing) return [[EVENT]]
+    if (isEvent && isThing) return [[DIGITAL]]
+  }
+  return undefined
+}
+
+export const buildTagsPredicate = (
+  tags: SearchParametersQuery['tags']
+): FiltersArray[0] | undefined => {
+  if (tags.length > 0) return tags.map((tag: string) => `${FACETS_FILTERS_ENUM.OFFER_TAGS}:${tag}`)
+  return undefined
+}
