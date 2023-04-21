d7c516b75fafb0eeb62a6903c58996798e3c1849
tanguyprache <47600889+tanguyprache@users.noreply.github.com>
2023-04-21 14:34:37 +0200

    (PC-21562) feat(identification): add amplitude tracking for the fork screens (#4694)
    * (PC-21562) feat(identification): add amplitude trackers to the fork screens
    
    * (PC-21562) test(identification): update tests for fork screens

--- src/features/identityCheck/pages/identification/IdentificationForkEduconnect.native.test.tsx
+++ src/features/identityCheck/pages/identification/IdentificationForkEduconnect.native.test.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
 import { navigate } from '__mocks__/@react-navigation/native'
+import { IdentityCheckMethod } from 'api/gen'
 import { IdentificationForkEduconnect } from 'features/identityCheck/pages/identification/IdentificationForkEduconnect'
-import { fireEvent, render, waitFor } from 'tests/utils'
+import { amplitude } from 'libs/amplitude'
+import { fireEvent, render, waitFor, screen } from 'tests/utils'
 
 describe('<IdentificationForkEduconnect />', () => {
   it('should render correctly', () => {
-    const renderForkUbble = render(<IdentificationForkEduconnect />)
-    expect(renderForkUbble).toMatchSnapshot()
+    render(<IdentificationForkEduconnect />)
+    expect(screen).toMatchSnapshot()
   })
 
   it('should navigate to next screen "SelectIDOrigin" on press "Ma pièce d’identité"', async () => {
-    const { getByText } = render(<IdentificationForkEduconnect />)
-    const button = getByText('Ma pièce d’identité')
+    render(<IdentificationForkEduconnect />)
+    const button = screen.getByText('Ma pièce d’identité')
 
     fireEvent.press(button)
 
@@ -22,13 +24,47 @@ describe('<IdentificationForkEduconnect />', () => {
   })
 
   it('should navigate to next screen "IdentityCheckEduConnect" on press "Mes codes ÉduConnect"', async () => {
-    const { getByText } = render(<IdentificationForkEduconnect />)
-    const button = getByText('Mes codes ÉduConnect')
+    render(<IdentificationForkEduconnect />)
+    const button = screen.getByText('Mes codes ÉduConnect')
 
     fireEvent.press(button)
 
     await waitFor(() => {
       expect(navigate).toHaveBeenCalledWith('IdentityCheckEduConnect', undefined)
+    })
+  })
+
+  it('should send a amplitude event when the screen is mounted', async () => {
+    render(<IdentificationForkEduconnect />)
+
+    await waitFor(() =>
+      expect(amplitude.logEvent).toHaveBeenCalledWith('screen_view_fork_educonnect')
+    )
+  })
+
+  it('should send a amplitude event choose_method_ubble with fork_origin on press "Ma pièce d’identité"', async () => {
+    render(<IdentificationForkEduconnect />)
+    const button = screen.getByText('Ma pièce d’identité')
+
+    fireEvent.press(button)
+
+    await waitFor(() => {
+      expect(amplitude.logEvent).toHaveBeenCalledWith('choose_method_ubble', {
+        fork_origin: IdentityCheckMethod.educonnect,
+      })
+    })
+  })
+
+  it('should send a amplitude event choose_method_ubble with fork_origin on press "Mes codes ÉduConnect"', async () => {
+    render(<IdentificationForkEduconnect />)
+    const button = screen.getByText('Mes codes ÉduConnect')
+
+    fireEvent.press(button)
+
+    await waitFor(() => {
+      expect(amplitude.logEvent).toHaveBeenCalledWith('choose_method_educonnect', {
+        fork_origin: IdentityCheckMethod.educonnect,
+      })
     })
   })
 })
--- src/features/identityCheck/pages/identification/IdentificationForkUbble.native.test.tsx
+++ src/features/identityCheck/pages/identification/IdentificationForkUbble.native.test.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
 import { navigate } from '__mocks__/@react-navigation/native'
+import { IdentityCheckMethod } from 'api/gen'
 import { IdentificationForkUbble } from 'features/identityCheck/pages/identification/IdentificationForkUbble'
-import { fireEvent, render, waitFor } from 'tests/utils'
+import { amplitude } from 'libs/amplitude'
+import { fireEvent, render, waitFor, screen } from 'tests/utils'
 
 describe('<IdentificationForkUbble />', () => {
   it('should render correctly', () => {
-    const renderForkUbble = render(<IdentificationForkUbble />)
-    expect(renderForkUbble).toMatchSnapshot()
+    render(<IdentificationForkUbble />)
+    expect(screen).toMatchSnapshot()
   })
 
   it('should navigate to next screen "SelectIDOrigin" on press "Ma pièce d’identité"', async () => {
-    const { getByText } = render(<IdentificationForkUbble />)
-    const button = getByText('Ma pièce d’identité')
+    render(<IdentificationForkUbble />)
+    const button = screen.getByText('Ma pièce d’identité')
 
     fireEvent.press(button)
 
@@ -22,13 +24,45 @@ describe('<IdentificationForkUbble />', () => {
   })
 
   it('should navigate to next screen "IdentityCheckEduConnect" on press "Mes codes ÉduConnect"', async () => {
-    const { getByText } = render(<IdentificationForkUbble />)
-    const button = getByText('Mes codes ÉduConnect')
+    render(<IdentificationForkUbble />)
+    const button = screen.getByText('Mes codes ÉduConnect')
 
     fireEvent.press(button)
 
     await waitFor(() => {
       expect(navigate).toHaveBeenCalledWith('IdentityCheckEduConnect', undefined)
+    })
+  })
+
+  it('should send a amplitude event when the screen is mounted', async () => {
+    render(<IdentificationForkUbble />)
+
+    await waitFor(() => expect(amplitude.logEvent).toHaveBeenCalledWith('screen_view_fork_ubble'))
+  })
+
+  it('should send a amplitude event choose_method_ubble with fork_origin on press "Ma pièce d’identité"', async () => {
+    render(<IdentificationForkUbble />)
+    const button = screen.getByText('Ma pièce d’identité')
+
+    fireEvent.press(button)
+
+    await waitFor(() => {
+      expect(amplitude.logEvent).toHaveBeenCalledWith('choose_method_ubble', {
+        fork_origin: IdentityCheckMethod.ubble,
+      })
+    })
+  })
+
+  it('should send a amplitude event choose_method_ubble with fork_origin on press "Mes codes ÉduConnect"', async () => {
+    render(<IdentificationForkUbble />)
+    const button = screen.getByText('Mes codes ÉduConnect')
+
+    fireEvent.press(button)
+
+    await waitFor(() => {
+      expect(amplitude.logEvent).toHaveBeenCalledWith('choose_method_educonnect', {
+        fork_origin: IdentityCheckMethod.ubble,
+      })
     })
   })
 })
