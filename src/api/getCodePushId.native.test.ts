b6e123346a937890e26eff4410d9f49ef88d6744
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-04-24 14:50:34 +0200

    (BSR) feat(getCodePushId): Fetch metadata once (#4702)

--- src/api/getCodePushId.native.test.ts
+++ src/api/getCodePushId.native.test.ts
@@ -1,10 +1,14 @@
 import CodePush, { LocalPackage } from 'react-native-code-push'
 
-import { getCodePushId } from './getCodePushId'
+import { getCodePushId, metadataRef } from './getCodePushId'
 
 const codePushSpy = jest.spyOn(CodePush, 'getUpdateMetadata')
 
 describe('getCodePushId', () => {
+  beforeEach(() => {
+    metadataRef.hasFetchedMetadata = false
+  })
+
   it('should return empty string when there is no code push information', async () => {
     codePushSpy.mockResolvedValueOnce(null)
     const codePushId = await getCodePushId()
@@ -17,6 +21,14 @@ describe('getCodePushId', () => {
     const codePushId = await getCodePushId()
 
     expect(codePushId).toEqual('4')
+  })
+
+  it('should fetch metadata once', async () => {
+    codePushSpy.mockResolvedValueOnce({ label: 'V4' } as LocalPackage)
+    await getCodePushId()
+    await getCodePushId()
+
+    expect(codePushSpy).toHaveBeenCalledTimes(1)
   })
 
   it('should return empty string when there is an error', async () => {
