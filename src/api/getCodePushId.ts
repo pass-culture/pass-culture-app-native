c8f6682a13f2c3cfdcd9523ec4604a50b1e23cd7
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-04-24 11:55:29 +0200

    (BSR) feat(getCodePushId): Fetch metadata once

--- src/api/getCodePushId.ts
+++ src/api/getCodePushId.ts
@@ -1,11 +1,19 @@
 import CodePush from 'react-native-code-push'
 
+// Exported for tests only
+export const metadataRef = { hasFetchedMetadata: false }
+let codePushLabel = ''
+
 export const getCodePushId = async () => {
   try {
-    const metadata = await CodePush.getUpdateMetadata()
+    if (!metadataRef.hasFetchedMetadata) {
+      const metadata = await CodePush.getUpdateMetadata()
+      metadataRef.hasFetchedMetadata = true
 
-    // We want to remove the letter 'v' from the  code push label : 'v4' => '4'
-    const codePushLabel = metadata ? metadata.label.slice(1) : ''
+      // We want to remove the letter 'v' from the  code push label : 'v4' => '4'
+      codePushLabel = metadata ? metadata.label.slice(1) : ''
+      return codePushLabel
+    }
 
     return codePushLabel
   } catch {
