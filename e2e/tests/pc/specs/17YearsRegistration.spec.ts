644f310ba8232f42a2f1a9a603efce15582eb159
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-04-25 14:56:46 +0200

    Init 17yo test with account creation

---
+++ e2e/tests/pc/specs/17YearsRegistration.spec.ts
@@ -0,0 +1,115 @@
+import FirstLaunch from '../helpers/FirstLaunch'
+import { TabBar } from '../features/navigation/TabBar'
+import { didFirstLaunch } from '../helpers/utils/error'
+import { DefaultTheme, getTheme } from '../helpers/utils/theme'
+import ProfileScreen from '../features/profile/ProfileScreen'
+import SignupScreens from '../features/auth/SignupScreens'
+import { getRandomInt } from '../helpers/utils/number'
+import GmailClient, { Email } from '../helpers/GmailClient'
+import { openDeepLinkUrl } from '../helpers/utils/deeplink'
+import { timeout } from '../helpers/utils/time'
+
+type RegistrationConfirmationEmail = Omit<Email, 'params'> & {
+  params: {
+    CONFIRMATION_LINK: string
+  }
+}
+
+describe('18YearsRegistration', () => {
+  let ok = false
+  let tabBar: TabBar
+  let theme: DefaultTheme
+
+  before(async () => {
+    theme = getTheme(await browser.getWindowSize())
+    tabBar = new TabBar(theme)
+  })
+
+  it('should first launch app', async () => {
+    ok = await FirstLaunch.init(tabBar)
+  })
+
+  it('should click on profile', async () => {
+    didFirstLaunch(ok)
+    await tabBar.profil.click()
+  })
+
+  describe('profile registration', () => {
+    const email = GmailClient.randomUniqueUsername()
+    const password = 'user@AZERTY123'
+    const gmailClient = new GmailClient()
+    let registrationDate = new Date()
+    let registrationConfirmationEmail: RegistrationConfirmationEmail
+
+    beforeEach(() => {
+      didFirstLaunch(ok)
+    })
+
+    it('should click on "Créer un compte"', async () => {
+      await ProfileScreen.waitForIsShown()
+      await ProfileScreen.createAccount.click()
+      await ProfileScreen.waitForIsHidden()
+    })
+
+    it('should set email and randomly accept newsletter checkbox', async () => {
+      await SignupScreens.waitForIsShown()
+      await SignupScreens.emailScreen.waitForIsShown()
+      await SignupScreens.emailScreen.email.setValue(email)
+
+      await SignupScreens.emailScreen.newsletterCheckbox.click()
+
+      await timeout(2000)
+      await SignupScreens.emailScreen.submit.click()
+      await SignupScreens.emailScreen.waitForIsHidden()
+    })
+
+    it('should set password', async () => {
+      await SignupScreens.waitForIsShown()
+      await SignupScreens.passwordScreen.waitForIsShown()
+      await SignupScreens.passwordScreen.password.waitForEnabled()
+      await SignupScreens.passwordScreen.password.setValue(password)
+      await SignupScreens.passwordScreen.submit.click()
+      await SignupScreens.passwordScreen.waitForIsHidden()
+    })
+
+    it('should set birthdate to 18 years old', async () => {
+      const birthDate = new Date(
+        new Date().getFullYear() - 17, // year (17 years old)
+        getRandomInt(0, new Date().getMonth()), // monthIndex
+        getRandomInt(1, new Date().getDay()) // day
+      )
+
+      await SignupScreens.birthDateScreen.waitForIsShown()
+      await SignupScreens.birthDateScreen.setBirthDate(birthDate, theme)
+      await SignupScreens.birthDateScreen.submit.click()
+      await SignupScreens.birthDateScreen.waitForIsHidden()
+    })
+
+    it('should pass recaptcha and post account registration to pcapi', async () => {
+      await SignupScreens.acceptCguScreen.waitForIsShown()
+      registrationDate = new Date()
+      await SignupScreens.acceptCguScreen.submit.click()
+      await SignupScreens.acceptCguScreen.waitForIsHidden()
+      try {
+        await SignupScreens.signupConfirmationEmailSentScreen.waitForIsShown()
+      } catch {
+        throw new Error('Re-CAPTCHA challenge not disabled')
+      }
+    })
+
+    it('should receive registration confirmation email', async () => {
+      registrationConfirmationEmail = (await gmailClient.getRegistrationConfirmationEmail({
+        dateFrom: registrationDate,
+        username: email,
+      })) as RegistrationConfirmationEmail
+      expect(registrationConfirmationEmail).toBeDefined()
+      await SignupScreens.signupConfirmationEmailSentScreen.waitForIsShown()
+    })
+
+    it('should open registration confirmation email link and perform log in', async () => {
+      const url = new URL(registrationConfirmationEmail.params.CONFIRMATION_LINK)
+      await openDeepLinkUrl(url.href)
+      await SignupScreens.signupConfirmationEmailSentScreen.waitForIsHidden()
+    })
+  })
+})
