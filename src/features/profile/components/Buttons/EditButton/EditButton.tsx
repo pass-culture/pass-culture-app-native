cdb700c13304dae5a7afef0971227a7a54c183f9
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-04-25 16:52:35 +0200

    (PC-21717) feat(editButton): possible to use it without navigateTo

--- src/features/profile/components/Buttons/EditButton/EditButton.tsx
+++ src/features/profile/components/Buttons/EditButton/EditButton.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import { ButtonQuaternaryPrimary } from 'ui/components/buttons/ButtonQuaternaryPrimary'
 import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
+import { TouchableLink } from 'ui/components/touchableLink/TouchableLink'
 import { InternalNavigationProps } from 'ui/components/touchableLink/types'
 import { EditPen } from 'ui/svg/icons/EditPen'
 
 type EditButtonProps = {
-  navigateTo: InternalNavigationProps['navigateTo']
   wording: string
   accessibilityLabel?: string
+  navigateTo?: InternalNavigationProps['navigateTo']
   onPress?: () => void
 }
 
@@ -17,14 +18,25 @@ export const EditButton = ({
   onPress,
   wording,
   accessibilityLabel,
-}: EditButtonProps) => (
-  <InternalTouchableLink
-    as={ButtonQuaternaryPrimary}
-    navigateTo={navigateTo}
-    onBeforeNavigate={onPress}
-    wording={wording}
-    icon={EditPen}
-    accessibilityLabel={accessibilityLabel}
-    inline
-  />
-)
+}: EditButtonProps) => {
+  return navigateTo ? (
+    <InternalTouchableLink
+      as={ButtonQuaternaryPrimary}
+      navigateTo={navigateTo}
+      onBeforeNavigate={onPress}
+      wording={wording}
+      icon={EditPen}
+      accessibilityLabel={accessibilityLabel}
+      inline
+    />
+  ) : (
+    <TouchableLink
+      as={ButtonQuaternaryPrimary}
+      wording={wording}
+      icon={EditPen}
+      inline
+      accessibilityLabel={accessibilityLabel}
+      handleNavigation={onPress !== undefined ? onPress : () => undefined}
+    />
+  )
+}
