1eb87371771ce323463706d68e6f2ece04c6ff59
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-04-26 17:58:38 +0200

    (PC-21961) fix(proxy): Fix proxy tests

--- server/src/index.test.ts
+++ server/src/index.test.ts
@@ -25,12 +25,13 @@ describe('express server', () => {
     expect(addressInfo.port).toBe(8080)
   })
 
-  it(`should return same index.html from proxy ${env.APP_PUBLIC_URL} as from bucket ${env.APP_BUCKET_URL} (minus the chunk hash)`, async () => {
+  it(`should return same index.html from proxy ${env.APP_PUBLIC_URL} as from bucket ${env.APP_BUCKET_URL} (minus the chunk and commit hash)`, async () => {
     const regExp = /\/(\d|main)\.[0-9a-f]+(\.chunk\.js)/gm
+    const commitHashRegExp = /<meta name="commit-hash".*?>/gm
     const response = await fetch(env.APP_PUBLIC_URL)
-    const html = (await response.text()).replace(regExp, '/$1$3')
+    const html = (await response.text()).replace(regExp, '/$1$3').replace(commitHashRegExp, '')
     const responseProxy = await fetch(env.APP_BUCKET_URL)
-    const htmlProxy = (await responseProxy.text()).replace(regExp, '/$1$3')
+    const htmlProxy = (await responseProxy.text()).replace(regExp, '/$1$3').replace(commitHashRegExp, '')
     expect(html).toEqual(htmlProxy)
   })
 })
