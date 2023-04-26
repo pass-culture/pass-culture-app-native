5e70055d8638a6f08de6a9484427b675c56c4321
marineshaw <marines@bam.tech>
2023-04-26 11:36:03 +0200

    :recycle: (Tests) Use waitFor instead of flush promises

--- src/features/cookies/helpers/useIsCookiesListUpToDate.native.test.ts
+++ src/features/cookies/helpers/useIsCookiesListUpToDate.native.test.ts
@@ -6,7 +6,7 @@ import {
 } from 'features/cookies/helpers/useIsCookiesListUpToDate'
 import * as Firestore from 'libs/firebase/firestore/getCookiesLastUpdate'
 import { storage } from 'libs/storage'
-import { flushAllPromisesWithAct, renderHook } from 'tests/utils'
+import { renderHook, waitFor } from 'tests/utils'
 
 import Package from '../../../../package.json'
 
@@ -32,39 +32,42 @@ describe('isCookiesListUpToDate', () => {
   beforeEach(() => storage.clear(COOKIES_CONSENT_KEY))
 
   it('should be false if no consent date', async () => {
-    await storage.saveObject(COOKIES_CONSENT_KEY, {
+    storage.saveObject(COOKIES_CONSENT_KEY, {
       buildVersion: Package.build,
     })
 
     const { result } = renderHook(useIsCookiesListUpToDate)
-    await flushAllPromisesWithAct()
 
-    expect(result.current).toEqual(false)
+    await waitFor(() => {
+      expect(result.current).toEqual(false)
+    })
   })
 
   it('should be false if no consent build version', async () => {
-    await storage.saveObject(COOKIES_CONSENT_KEY, {
+    storage.saveObject(COOKIES_CONSENT_KEY, {
       choiceDatetime: TODAY,
     })
 
     const { result } = renderHook(useIsCookiesListUpToDate)
-    await flushAllPromisesWithAct()
 
-    expect(result.current).toEqual(false)
+    await waitFor(() => {
+      expect(result.current).toEqual(false)
+    })
   })
 
   it.each([A_SECOND_AGO, LAST_WEEK, THREE_MONTHS_AGO])(
     'should be false if user has made choice before last list update',
     async (choiceDatetime) => {
-      await storage.saveObject(COOKIES_CONSENT_KEY, {
+      storage.saveObject(COOKIES_CONSENT_KEY, {
         buildVersion: Package.build - 1,
         choiceDatetime: choiceDatetime.toISOString(),
       })
 
       const { result } = renderHook(useIsCookiesListUpToDate)
-      await flushAllPromisesWithAct()
 
-      expect(result.current).toEqual(false)
+      await waitFor(() => {
+        expect(result.current).toEqual(false)
+      })
     }
   )
 
@@ -72,21 +75,22 @@ describe('isCookiesListUpToDate', () => {
     mockFirestore.mockResolvedValueOnce(undefined)
 
     const { result } = renderHook(useIsCookiesListUpToDate)
-    await flushAllPromisesWithAct()
 
-    expect(result.current).toEqual(true)
+    await waitFor(() => {
+      expect(result.current).toEqual(true)
+    })
   })
 
   it('should be true if user has made choice after last list update', async () => {
-    await storage.saveObject(COOKIES_CONSENT_KEY, {
+    storage.saveObject(COOKIES_CONSENT_KEY, {
       buildVersion: Package.build,
       choiceDatetime: TODAY,
     })
 
     const { result } = renderHook(useIsCookiesListUpToDate)
-    await flushAllPromisesWithAct()
-
-    expect(result.current).toEqual(true)
+    await waitFor(() => {
+      expect(result.current).toEqual(true)
+    })
   })
 
   it('should be true if current app version does not contain cookies update', async () => {
@@ -95,28 +99,28 @@ describe('isCookiesListUpToDate', () => {
       lastUpdateBuildVersion: Package.build + 1,
     })
 
-    await storage.saveObject(COOKIES_CONSENT_KEY, {
+    storage.saveObject(COOKIES_CONSENT_KEY, {
       buildVersion: Package.build,
       choiceDatetime: TODAY,
     })
 
     const { result } = renderHook(useIsCookiesListUpToDate)
-    await flushAllPromisesWithAct()
-
-    expect(result.current).toEqual(true)
+    await waitFor(() => {
+      expect(result.current).toEqual(true)
+    })
   })
 
   it('should be true if cookies list update is in the future', async () => {
     mockFirestore.mockResolvedValueOnce({ ...defaultMockFirestore, lastUpdated: TOMORROW })
 
-    await storage.saveObject(COOKIES_CONSENT_KEY, {
+    storage.saveObject(COOKIES_CONSENT_KEY, {
       buildVersion: Package.build,
       choiceDatetime: TODAY,
     })
 
     const { result } = renderHook(useIsCookiesListUpToDate)
-    await flushAllPromisesWithAct()
-
-    expect(result.current).toEqual(true)
+    await waitFor(() => {
+      expect(result.current).toEqual(true)
+    })
   })
 })
