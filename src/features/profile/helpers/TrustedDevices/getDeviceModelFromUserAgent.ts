cfdfe0c0ab539ff54c346e28641c6e327878948a
Lucas Beneston <lucas.beneston@passculture.app>
2023-04-28 13:44:07 +0200

    (PC-21835) feat(trustedDevice): use multiple regex instead a long device model regex

--- src/features/profile/helpers/TrustedDevices/getDeviceModelFromUserAgent.ts
+++ src/features/profile/helpers/TrustedDevices/getDeviceModelFromUserAgent.ts
@@ -1,8 +1,21 @@
 import { Platform } from 'react-native'
 
 const NO_DEVICE_MODEL_FOUND = 'Model inconnu'
-const DEVICE_MODEL_REGEX =
-  /(iPhone|iPad|iPod|Macintosh|Android|Pixel|Samsung|Google|LG|Motorola|OnePlus|HTC|Sony Xperia|Huawei|Xiaomi|Asus Zenfone|Microsoft Surface|Amazon Kindle|BlackBerry|Nokia Lumia|Windows Phone|Microsoft Lumia|Lenovo Yoga|Dell XPS|HP Spectre|Acer Aspire|Asus VivoBook|Chromebook)/gi
+
+const APPLE_DEVICES = /iPhone|iPad|iPod|Macintosh/
+
+const ANDROID_DEVICES =
+  /Android|Pixel|Samsung|LG|Motorola|OnePlus|HTC|Sony Xperia|Huawei|Xiaomi|Asus Zenfone/
+
+const WINDOWS_DEVICES =
+  /Windows Phone|Microsoft Surface|Dell XPS|HP Spectre|Acer Aspire|Asus VivoBook|Nokia Lumia|Microsoft Lumia/
+
+const OTHER_DEVICES = /Amazon Kindle|BlackBerry|Lenovo Yoga|Chromebook/
+
+const DEVICE_MODEL_REGEX = new RegExp(
+  `${APPLE_DEVICES.source}|${ANDROID_DEVICES.source}|${WINDOWS_DEVICES.source}|${OTHER_DEVICES.source}`,
+  'gi'
+)
 
 export const getDeviceModelFromUserAgent = (): string => {
   const isWeb = Platform.OS === 'web'
