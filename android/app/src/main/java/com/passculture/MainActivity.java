1b9bce86b90a5eca746a698f7b997ef136f5a26f
AnoukHello <anoukhello22@gmail.com>
2023-05-10 17:15:02 +0200

    (PC-22176) bump rn version to 0.69.0 and react to 0.18.0 following the rn upgrade helper

--- android/app/src/main/java/com/passculture/MainActivity.java
+++ android/app/src/main/java/com/passculture/MainActivity.java
@@ -36,7 +36,8 @@ public class MainActivity extends ReactActivity {
 
     /**
      * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
-     * you can specify the rendered you wish to use (Fabric or the older renderer).
+     * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
+     * (Paper).
     */
     // @react-navigation (https://reactnavigation.org/docs/en/next/getting-started.html)
     @Override
@@ -51,14 +52,22 @@ public class MainActivity extends ReactActivity {
 
     public static class MainActivityDelegate extends ReactActivityDelegate {
         public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
-        super(activity, mainComponentName);
+            super(activity, mainComponentName);
         }
+
         @Override
         protected ReactRootView createRootView() {
-        ReactRootView reactRootView = new ReactRootView(getContext());
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
-        return reactRootView;
+            ReactRootView reactRootView = new ReactRootView(getContext());
+            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+            reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+            return reactRootView;
+        }
+
+        @Override
+        protected boolean isConcurrentRootEnabled() {
+            // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+            // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
         }
     }
 }
