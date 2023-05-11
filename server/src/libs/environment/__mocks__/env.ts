48dff741e738afa2e4f40d054bf208c4de702a9c
bruno ebstein <bruno.ebstein@passculture.app>
2023-05-11 14:42:50 +0200

    (PC-22130) tests(proxy): add tests between proxy and backend
    Co-authored-by: Ali RAIKI <aliraiki@users.noreply.github.com>

--- server/src/libs/environment/__mocks__/env.ts
+++ server/src/libs/environment/__mocks__/env.ts
@@ -6,7 +6,7 @@ export const env: Environment = {
   ENV: 'test',
   APP_PUBLIC_URL: 'http://localhost:8080',
   APP_BUCKET_URL: 'https://app-bucket.testing.passculture.team',
-  API_BASE_URL: 'https://api.testing.passculture.team',
+  API_BASE_URL: 'https://backend.testing.passculture.team',
   API_BASE_PATH_NATIVE_V1: 'native/v1',
   DEEPLINK_PROTOCOL: 'passculture://',
   PROXY_CACHE_CONTROL: 'public,max-age=3600',
