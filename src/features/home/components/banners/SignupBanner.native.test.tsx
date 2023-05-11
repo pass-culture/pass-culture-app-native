30f009723d964f33696f5b89fdac73425d170bbf
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-05-11 17:53:25 +0200

    Add tracker for home banner signup

--- src/features/home/components/banners/SignupBanner.native.test.tsx
+++ src/features/home/components/banners/SignupBanner.native.test.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 import { navigate } from '__mocks__/@react-navigation/native'
 import { SignupBanner } from 'features/home/components/banners/SignupBanner'
-import { fireEvent, render, waitFor } from 'tests/utils'
+import { analytics } from 'libs/analytics'
+import { fireEvent, render, screen, waitFor } from 'tests/utils'
 
 describe('SignupBanner', () => {
   it('should redirect to signup form on press', async () => {
@@ -11,5 +12,15 @@ describe('SignupBanner', () => {
     fireEvent.press(getByText('Débloque ton crédit'))
 
     await waitFor(() => expect(navigate).toHaveBeenCalledWith('SignupForm', undefined))
+  })
+
+  it('should log analytics on press', async () => {
+    render(<SignupBanner />)
+
+    fireEvent.press(screen.getByText('Débloque ton crédit'))
+
+    await waitFor(() =>
+      expect(analytics.logSignUpClicked).toHaveBeenNthCalledWith(1, { from: 'home' })
+    )
   })
 })
--- src/features/home/components/banners/SignupBanner.tsx
+++ src/features/home/components/banners/SignupBanner.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
+import { analytics } from 'libs/analytics'
 import { BannerWithBackground } from 'ui/components/ModuleBanner/BannerWithBackground'
 import { BicolorUnlock } from 'ui/svg/icons/BicolorUnlock'
 import { Typo } from 'ui/theme'
 
+const onBeforeNavigate = () => analytics.logSignUpClicked({ from: 'home' })
+
 export const SignupBanner = () => {
   return (
-    <BannerWithBackground leftIcon={StyledBicolorUnlock} navigateTo={{ screen: 'SignupForm' }}>
+    <BannerWithBackground
+      leftIcon={StyledBicolorUnlock}
+      navigateTo={{ screen: 'SignupForm' }}
+      onBeforeNavigate={onBeforeNavigate}>
       <StyledButtonText>Débloque ton crédit</StyledButtonText>
       <StyledBodyText>Crée ton compte si tu as entre 15 et 18 ans&nbsp;!</StyledBodyText>
     </BannerWithBackground>
