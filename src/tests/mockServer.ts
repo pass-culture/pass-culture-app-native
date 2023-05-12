0a7634b137534c5c369f5dddaa13c3095445bd29
Hugo Voisin <hugov@bam.tech>
2023-05-12 11:05:41 +0200

    :sparkles: Add new msw server and utils

---
+++ src/tests/mockServer.ts
@@ -0,0 +1,59 @@
+export interface MockServerInterface<BodyType, HeadersType, RequestBody> {
+  baseUrl: string
+  mode: MockServerMode
+  delay: number
+  get<TResponse extends BodyType>(
+    url: string,
+    options: TResponse | MockOptions<HeadersType, TResponse, RequestBody>
+  ): MockReturnType
+  post<TResponse extends BodyType>(
+    url: string,
+    options: TResponse | MockOptions<HeadersType, TResponse, RequestBody>
+  ): MockReturnType
+  delete<TResponse extends BodyType>(
+    url: string,
+    options: TResponse | MockOptions<HeadersType, TResponse, RequestBody>
+  ): MockReturnType
+  put<TResponse extends BodyType>(
+    url: string,
+    options: TResponse | MockOptions<HeadersType, TResponse, RequestBody>
+  ): MockReturnType
+}
+
+export type Mocks<HeadersType, TResponse, RequestBody> = {
+  method: SupportedMethod
+  url: string
+  options: MockOptions<HeadersType, TResponse, RequestBody>
+}
+
+export type MockServerConstructorOptions<HeadersType, TResponse, RequestBody> = {
+  mode?: MockServerMode
+  delay?: number
+  defaultRequests?: Mocks<HeadersType, TResponse, RequestBody>[]
+}
+
+export type MockServerMode = 'SERVER' | 'TEST'
+
+export type RequestOptions<HeadersType, RequestBody> = {
+  headers?: Record<string, HeadersType> | undefined
+  matchHeader?: [string, string]
+  matchData?: RequestBody | undefined
+  persist?: boolean
+}
+
+export type ResponseOptions<TResponse> = {
+  statusCode?: number | 'network-error'
+  delay?: number
+  data?: TResponse
+}
+
+export type MockOptions<HeadersType, TResponse, RequestBody> = {
+  requestOptions?: RequestOptions<HeadersType, RequestBody>
+  responseOptions?: ResponseOptions<TResponse>
+}
+
+export type MockReturnType = {
+  requestData: () => void
+} | void
+
+export type SupportedMethod = 'GET' | 'POST' | 'PUT' | 'DELETE'
