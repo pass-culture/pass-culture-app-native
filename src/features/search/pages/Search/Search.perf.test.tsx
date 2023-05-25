d5d200907ca65bde01691674238355c41471b673
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-05-25 11:54:14 +0200

    Add performance test for search suggestions page

--- src/features/search/pages/Search/Search.perf.test.tsx
+++ src/features/search/pages/Search/Search.perf.test.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useRoute } from '__mocks__/@react-navigation/native'
 import algoliasearch from '__mocks__/algoliasearch'
 import { SearchWrapper } from 'features/search/context/SearchWrapper'
+import { mockSuggestionHits } from 'features/search/fixtures/algolia'
 import { Search } from 'features/search/pages/Search/Search'
 import { SearchView } from 'features/search/types'
 import { mockedAlgoliaResponse } from 'libs/algolia/__mocks__/mockedAlgoliaResponse'
@@ -17,6 +18,8 @@ jest.mock('react-instantsearch-hooks', () => ({
   useInstantSearch: () => ({
     use: jest.fn(),
   }),
+  // We mock this hook to simulate autocomplete suggestions
+  useInfiniteHits: () => ({ hits: mockSuggestionHits }),
 }))
 
 describe('<Search />', () => {
@@ -44,6 +47,20 @@ describe('<Search />', () => {
       await measurePerformance(<SearchPage />, {
         scenario: async () => {
           await screen.findByText('4 résultats')
+        },
+      })
+    })
+  })
+
+  describe('Search Suggestions', () => {
+    beforeAll(() => {
+      useRoute.mockReturnValue({ params: { view: SearchView.Suggestions, query: 'ciné' } })
+    })
+
+    it('Performance test for Search Suggestions page', async () => {
+      await measurePerformance(<SearchPage />, {
+        scenario: async () => {
+          await screen.findAllByText('Séances de cinéma')
         },
       })
     })
