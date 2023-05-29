17d886e9c5a5825cc5c5df882f5c5410f3908b95
Lucas Beneston <lucas.beneston@passculture.app>
2023-05-29 17:53:24 +0200

    (PC-22092) feat(TrustedDevice): catch error for saving string in storage in getDeviceId

--- src/libs/react-native-device-info/getDeviceId.web.test.tsx
+++ src/libs/react-native-device-info/getDeviceId.web.test.tsx
@@ -1,18 +1,20 @@
 import { v4 as uuidv4 } from 'uuid'
 
+import { eventMonitoring } from 'libs/monitoring'
 import { getDeviceId } from 'libs/react-native-device-info/getDeviceId.web'
 import { storage } from 'libs/storage'
 
 jest.unmock('libs/react-native-device-info/getDeviceId')
 
+const DEVICE_ID_KEY = 'device_id'
 const DEVICE_ID_FROM_UUID = uuidv4()
 const DEVICE_ID_FROM_STORAGE = '1234-abcd-5678-efgh'
 
 describe('getDeviceId', () => {
-  beforeEach(() => storage.clear('device_id'))
+  beforeEach(() => storage.clear(DEVICE_ID_KEY))
 
   it('should return the device ID from local storage if present', async () => {
-    storage.saveString('device_id', DEVICE_ID_FROM_STORAGE)
+    storage.saveString(DEVICE_ID_KEY, DEVICE_ID_FROM_STORAGE)
     const deviceId = await getDeviceId()
 
     expect(deviceId).toEqual(DEVICE_ID_FROM_STORAGE)
@@ -27,15 +29,34 @@ describe('getDeviceId', () => {
   it('should store the new device ID in the storage if not present', async () => {
     await getDeviceId()
 
-    const deviceIdStorageAfter = await storage.readString('device_id')
+    const deviceIdStorageAfter = await storage.readString(DEVICE_ID_KEY)
     expect(deviceIdStorageAfter).toEqual(DEVICE_ID_FROM_UUID)
   })
 
-  it('should handle errors and return an empty string as a default device ID', async () => {
+  it('should return an empty string when an error appear on saving device ID in storage', async () => {
+    const error = new Error('SAVING_REJECTED')
+    jest.spyOn(storage, 'saveString').mockRejectedValueOnce(error)
+    const deviceId = await getDeviceId()
+
+    expect(deviceId).toEqual('')
+  })
+
+  it('should capture an exception when an error appear on saving device ID in storage', async () => {
+    const error = new Error('SAVING_REJECTED')
+    jest.spyOn(storage, 'saveString').mockRejectedValueOnce(error)
+    await getDeviceId()
+
+    expect(eventMonitoring.captureException).toHaveBeenNthCalledWith(
+      1,
+      `Error when save device ID in storage: ${error}`
+    )
+  })
+
+  it('should return a random device ID when an error appear on read device ID in storage', async () => {
     const error = new Error('READING_REJECTED')
     jest.spyOn(storage, 'readString').mockRejectedValueOnce(error)
     const deviceId = await getDeviceId()
 
-    expect(deviceId).toEqual('')
+    expect(deviceId).toEqual(DEVICE_ID_FROM_UUID)
   })
 })
--- src/libs/react-native-device-info/getDeviceId.web.ts
+++ src/libs/react-native-device-info/getDeviceId.web.ts
@@ -1,16 +1,24 @@
 import { v4 as uuidv4 } from 'uuid'
 
+import { eventMonitoring } from 'libs/monitoring'
 import { storage } from 'libs/storage'
+
+const DEVICE_ID_KEY = 'device_id'
+const NEW_DEVICE_ID = uuidv4()
 
 export async function getDeviceId() {
   try {
-    const deviceIdFromStorage = await storage.readString('device_id')
+    const deviceIdFromStorage = await storage.readString(DEVICE_ID_KEY)
     if (deviceIdFromStorage) return deviceIdFromStorage
 
-    const newDeviceId = uuidv4()
-    await storage.saveString('device_id', newDeviceId)
-    return newDeviceId
+    try {
+      await storage.saveString(DEVICE_ID_KEY, NEW_DEVICE_ID)
+    } catch (error) {
+      eventMonitoring.captureException(`Error when save device ID in storage: ${error}`)
+      return ''
+    }
+    return NEW_DEVICE_ID
   } catch (error) {
-    return ''
+    return NEW_DEVICE_ID
   }
 }
