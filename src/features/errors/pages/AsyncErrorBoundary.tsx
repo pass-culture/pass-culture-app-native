d22d04f0d0d9e9d93fe78e98ddabbe99c53ce25d
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-05-29 09:30:50 +0200

    (PC-22086) feat(errors): capture info for some API errors

--- src/features/errors/pages/AsyncErrorBoundary.tsx
+++ src/features/errors/pages/AsyncErrorBoundary.tsx
@@ -3,7 +3,7 @@ import { FallbackProps } from 'react-error-boundary'
 import { useQueryErrorResetBoundary } from 'react-query'
 import styled from 'styled-components/native'
 
-import { ApiError, isOnlyCapturedByAPIException } from 'api/apiHelpers'
+import { ApiError, isAPIExceptionCapturedAsInfo } from 'api/apiHelpers'
 import { homeNavConfig } from 'features/navigation/TabBar/helpers'
 import { useGoBack } from 'features/navigation/useGoBack'
 import { AsyncError, MonitoringError, eventMonitoring } from 'libs/monitoring'
@@ -32,16 +32,19 @@ export const AsyncErrorBoundaryWithoutNavigation = ({
   const { reset } = useQueryErrorResetBoundary()
 
   useEffect(() => {
-    const shouldNotCapturedApiError = Boolean(
-      error instanceof ApiError && isOnlyCapturedByAPIException(error.statusCode)
+    const shouldCapturedApiErrorAsInfo = Boolean(
+      error instanceof ApiError && isAPIExceptionCapturedAsInfo(error.statusCode)
     )
+    if (shouldCapturedApiErrorAsInfo) {
+      eventMonitoring.captureMessage(error.message, 'info')
+    }
     // we already captures MonitoringError exceptions (in AsyncError constructor)
     // we don't need to capture those errors
     // we don't capture API errors 5xx
     const shouldCaptureError =
       !(error instanceof MonitoringError) &&
       !(error instanceof ScreenError) &&
-      !shouldNotCapturedApiError
+      !shouldCapturedApiErrorAsInfo
 
     if (shouldCaptureError) {
       eventMonitoring.captureException(error)
