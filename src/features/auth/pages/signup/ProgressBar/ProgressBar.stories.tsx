9a17c407b7b94efb60e9606aba2adcf7cb5fb90f
Lucas Beneston <lucas.beneston@passculture.app>
2023-05-29 14:17:27 +0200

    (BSR) fix(storybook): udpate ProgressBar animation duration

--- src/features/auth/pages/signup/ProgressBar/ProgressBar.stories.tsx
+++ src/features/auth/pages/signup/ProgressBar/ProgressBar.stories.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 
 import { ProgressBar } from './ProgressBar'
 
-const ANIMATION_DURATION_IN_S = 0.8
+const ANIMATION_DURATION_IN_S = 1
 
 export default {
   title: 'features/auth/ProgressBar',
   component: ProgressBar,
   parameters: {
+    // Wait for animation to finish before snapshot
     chromatic: { delay: ANIMATION_DURATION_IN_S },
   },
 } as ComponentMeta<typeof ProgressBar>
@@ -19,11 +20,13 @@ FirstStep.args = {
   totalStep: 5,
   currentStep: 0,
 }
+
 export const SecondStep = Template.bind({})
 SecondStep.args = {
   totalStep: 5,
   currentStep: 1,
 }
+
 export const LastStep = Template.bind({})
 LastStep.args = {
   totalStep: 5,
