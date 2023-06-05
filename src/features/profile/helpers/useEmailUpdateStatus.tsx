b278341d011227c478dfbf7e4a7e4681809a4860
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-06-05 09:37:41 +0200

    (PC-22208) feat(profile): add api call email update status

---
+++ src/features/profile/helpers/useEmailUpdateStatus.tsx
@@ -0,0 +1,19 @@
+import { useQuery } from 'react-query'
+
+import { api } from 'api/api'
+import { useAuthContext } from 'features/auth/context/AuthContext'
+import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
+import { QueryKeys } from 'libs/queryKeys'
+
+export const useEmailUpdateStatus = () => {
+  const netInfo = useNetInfoContext()
+  const { isLoggedIn } = useAuthContext()
+
+  const { data } = useQuery(
+    [QueryKeys.EMAIL_UPDATE_STATUS],
+    () => api.getnativev1profileemailUpdatestatus(),
+    { enabled: !!netInfo.isConnected && isLoggedIn }
+  )
+
+  return { data }
+}
