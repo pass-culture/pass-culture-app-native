9e1ebe63b4f0b6f286524d56c74768486ab67d2e
AnoukHello <anoukhello22@gmail.com>
2023-06-07 21:28:44 +0200

    (PC-22176) deps: bump @types/styled-components-react-native

--- src/ui/components/Li.tsx
+++ src/ui/components/Li.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components/native'
 
 import { AccessibilityRole } from 'libs/accessibilityRole/accessibilityRole'
 
-export const Li = styled.View.attrs({
+export const Li = styled.View.attrs<{ key?: string | number | null }>({
   accessibilityRole: AccessibilityRole.LISTITEM,
 })({
   display: Platform.OS === 'web' ? 'list-item' : 'flex',
