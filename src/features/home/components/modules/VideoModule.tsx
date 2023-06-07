ddcf6ceadfc7e38861dd5290459e97cefacf1635
Sandro <sandro.sgro@passculture.app>
2023-06-07 12:09:51 +0200

    (PC-22172) feat(VideoModule): Start putting the space necessarry for offer and backgroundColor

--- src/features/home/components/modules/VideoModule.tsx
+++ src/features/home/components/modules/VideoModule.tsx
@@ -1,5 +1,6 @@
 import colorAlpha from 'color-alpha'
 import React, { FunctionComponent } from 'react'
+import LinearGradient from 'react-native-linear-gradient'
 import styled from 'styled-components/native'
 
 import { BlackGradient } from 'features/home/components/BlackGradient'
@@ -16,8 +17,10 @@ const THUMBNAIL_HEIGHT = getSpacing(45)
 // We do not center the player icon, because when the title is 2-line long,
 // the title is to close to the player. So the player is closer to the top.
 const PLAYER_TOP_MARGIN = getSpacing(12.5)
-
 const PLAYER_SIZE = getSpacing(14.5)
+
+const OFFER_INSERT_HEIGHT = getSpacing(35)
+const COLOR_CATEGORY_BACKGROUND_HEIGHT = getSpacing(76)
 
 export const VideoModule: FunctionComponent<VideoModuleProps> = (props) => {
   const {
@@ -29,32 +32,47 @@ export const VideoModule: FunctionComponent<VideoModuleProps> = (props) => {
 
   return (
     <Container>
-      <Typo.Title3>{props.title}</Typo.Title3>
-      <Spacer.Column numberOfSpaces={5} />
-      <StyledTouchable onPress={showVideoModal} testID={'video-thumbnail'}>
-        <Thumbnail source={{ uri: props.videoThumbnail }}>
-          <DurationCaptionContainer>
-            <DurationCaption>{videoDuration}</DurationCaption>
-          </DurationCaptionContainer>
-          <TextContainer>
-            <BlackGradient />
-            <BlackBackground>
-              <VideoTitle numberOfLines={2}>{props.videoTitle}</VideoTitle>
-            </BlackBackground>
-          </TextContainer>
-          <PlayerContainer>
-            <Player />
-          </PlayerContainer>
-        </Thumbnail>
-      </StyledTouchable>
-      <VideoModal visible={videoModalVisible} hideModal={hideVideoModal} {...props} />
+      <ColorCategoryBackground
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+        colors={['#27DCA8FF', '#0E8474FF']}
+      />
+      <VideoOfferContainer>
+        <Typo.Title3>{props.title}</Typo.Title3>
+        <Spacer.Column numberOfSpaces={5} />
+        <StyledTouchable onPress={showVideoModal} testID={'video-thumbnail'}>
+          <Thumbnail source={{ uri: props.videoThumbnail }}>
+            <DurationCaptionContainer>
+              <DurationCaption>{videoDuration}</DurationCaption>
+            </DurationCaptionContainer>
+            <TextContainer>
+              <BlackGradient />
+              <BlackBackground>
+                <VideoTitle numberOfLines={2}>{props.videoTitle}</VideoTitle>
+              </BlackBackground>
+            </TextContainer>
+            <PlayerContainer>
+              <Player />
+            </PlayerContainer>
+          </Thumbnail>
+        </StyledTouchable>
+        <VideoModal visible={videoModalVisible} hideModal={hideVideoModal} {...props} />
+      </VideoOfferContainer>
+      <Spacer.Column numberOfSpaces={2} />
+      <VideoOfferContainer>
+        <OfferInsert />
+      </VideoOfferContainer>
+      <Spacer.Column numberOfSpaces={2} />
     </Container>
   )
 }
 
 const Container = styled.View(({ theme }) => ({
-  marginHorizontal: theme.contentPage.marginHorizontal,
   paddingBottom: theme.home.spaceBetweenModules,
+}))
+
+const VideoOfferContainer = styled.View(({ theme }) => ({
+  marginHorizontal: theme.contentPage.marginHorizontal,
 }))
 
 const Thumbnail = styled.ImageBackground(({ theme }) => ({
@@ -64,6 +82,15 @@ const Thumbnail = styled.ImageBackground(({ theme }) => ({
   borderRadius: theme.borderRadius.radius,
   height: THUMBNAIL_HEIGHT,
   width: '100%',
+}))
+
+const OfferInsert = styled.View(({ theme }) => ({
+  //the overflow: hidden allow to add border radius to the image
+  //https://stackoverflow.com/questions/49442165/how-do-you-add-borderradius-to-imagebackground/57616397
+  overflow: 'hidden',
+  borderRadius: theme.borderRadius.radius,
+  height: OFFER_INSERT_HEIGHT,
+  backgroundColor: theme.colors.white,
 }))
 
 const DurationCaptionContainer = styled.View(({ theme }) => ({
@@ -85,6 +112,14 @@ const PlayerContainer = styled.View({
   left: 0,
   right: 0,
   alignItems: 'center',
+})
+
+const ColorCategoryBackground = styled(LinearGradient)({
+  position: 'absolute',
+  top: getSpacing(20),
+  right: 0,
+  left: 0,
+  height: COLOR_CATEGORY_BACKGROUND_HEIGHT,
 })
 
 const Player = styled(Play).attrs({ size: PLAYER_SIZE })({})
