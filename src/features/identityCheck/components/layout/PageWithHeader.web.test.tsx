e97a2824bda1760c3c6b24eb6f75db27f7e4b009
Hugo Voisin <hugov@bam.tech>
2023-06-12 19:24:22 +0200

    test: move web test to its own file

---
+++ src/features/identityCheck/components/layout/PageWithHeader.web.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { PageWithHeader } from 'features/identityCheck/components/layout/PageWithHeader'
+import { mockGoBack } from 'features/navigation/__mocks__/useGoBack'
+import { render } from 'tests/utils'
+
+describe('<PageWithHeader/>', () => {
+  it('should render correctly', () => {
+    const renderAPI = render(
+      <PageWithHeader
+        title="Page with header title"
+        scrollChildren={<Text>scroll children</Text>}
+        fixedBottomChildren={<Text>fixed bottom children</Text>}
+        onGoBack={mockGoBack}
+      />
+    )
+    expect(renderAPI).toMatchSnapshot()
+  })
+})
