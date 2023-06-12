85a439c97d20311f36ea729a8533a1fa0f403484
Hugo Voisin <hugov@bam.tech>
2023-06-12 19:13:51 +0200

    test: add snapshot for android

--- src/features/identityCheck/components/layout/PageWithHeader.test.tsx
+++ src/features/identityCheck/components/layout/PageWithHeader.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, Platform } from 'react-native'
 
 import { PageWithHeader } from 'features/identityCheck/components/layout/PageWithHeader'
 import { mockGoBack } from 'features/navigation/__mocks__/useGoBack'
@@ -7,6 +7,21 @@ import { render } from 'tests/utils'
 
 describe('<PageWithHeader/>', () => {
   it('should render correctly', () => {
+    Platform.OS = 'ios'
+
+    const renderAPI = render(
+      <PageWithHeader
+        title="Page with header title"
+        scrollChildren={<Text>scroll children</Text>}
+        fixedBottomChildren={<Text>fixed bottom children</Text>}
+        onGoBack={mockGoBack}
+      />
+    )
+    expect(renderAPI).toMatchSnapshot()
+  })
+  it('should render correctly on Android, with white header', () => {
+    Platform.OS = 'android'
+
     const renderAPI = render(
       <PageWithHeader
         title="Page with header title"
