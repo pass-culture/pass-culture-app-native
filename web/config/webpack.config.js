0a52de684cf0d331c81d26cb287612e6582abaaa
tanguyprache <47600889+tanguyprache@users.noreply.github.com>
2023-06-12 10:37:18 +0200

    (PC-22712) fix(video): fix the build for web and remove react-native-web-webview (#4879)
    * (PC-22712) fix(video): remove the react-native-web-webview package
    
    * (PC-22712) fix(video): remove the react-native-web-webview webpack config
    
    * (PC-22712) fix(video): create an empty component for video web view

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -310,7 +310,6 @@ module.exports = function (webpackEnv) {
         'react-native': 'react-native-web',
         'react-native-svg': 'react-native-svg-web',
         'react-native-linear-gradient': 'react-native-web-linear-gradient',
-        'react-native-webview': 'react-native-web-webview',
 
         // Those libs are mocked until we implement web specific solutions
         'react-native-email-link': path.join(paths.appSrc, 'libs/react-native-email-link'),
@@ -530,17 +529,6 @@ module.exports = function (webpackEnv) {
                 },
                 'sass-loader'
               ),
-            },
-            // Adds support for the React Native Webview in web
-            // using the react-native-web-webview package
-            {
-              test: /postMock.html$/,
-              use: {
-                loader: require.resolve('file-loader'),
-                options: {
-                  name: '[name].[ext]',
-                },
-              },
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
