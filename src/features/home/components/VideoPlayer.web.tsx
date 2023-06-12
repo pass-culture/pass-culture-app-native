2792d7a381ecbf88f6435a090baaee867044001d
Tanguy <tanguy.prache@passculture.app>
2023-06-12 09:51:35 +0200

    (PC-22712) fix(video): create an empty component for video web view

---
+++ src/features/home/components/VideoPlayer.web.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+//TODO(PC-22712): Tempory fix before imlpementing a solution for youtube web player
+export const VideoPlayer: React.FC = () => {
+  return <React.Fragment />
+}
