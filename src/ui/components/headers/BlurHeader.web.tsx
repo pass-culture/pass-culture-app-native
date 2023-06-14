62159cfa2133e75746e85a7e5bb3185fa78dceb4
Hugo Voisin <hugov@bam.tech>
2023-06-14 18:23:24 +0200

    prune: remove unused component

--- src/ui/components/headers/BlurHeader.web.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-// We need to define this unused empty component in order to compile web app correctly. Without it,
-// the native component BlurHeader.tsx makes web compilation fail.
-export const BlurHeader: React.FC = () => {
-  return <React.Fragment />
-}
