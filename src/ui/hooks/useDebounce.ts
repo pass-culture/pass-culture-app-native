c0538bbe2cba8cd57d9e79260d29653e5607a6df
Hugo Voisin <hugo.voisin@passculture.app>
2023-06-14 15:33:59 +0200

    (PC-22788) fix(home): fix long loading time on home when scrolling down (#4905)
    * fix: use throttle instead of debounce
    debounce was misused, it should have been throttle from the beginning
    
    * refactor: rename variables
    
    * refactor: rename variable
    to match the behavior
    
    * docs: add comment about option given to throttle
    
    * feat: can set debounce options for useDebounce
    
    * docs: improve JSdoc for useDebounce

--- src/ui/hooks/useDebounce.ts
+++ src/ui/hooks/useDebounce.ts
@@ -1,17 +1,27 @@
-import debounce from 'lodash/debounce'
+import { debounce, DebounceSettings } from 'lodash'
 import { useEffect, useMemo, useRef } from 'react'
 
 /**
  * This hook is used to debounce, using `lodash.debounce` more safely.
  * You can use it in a React component, even setting some state (which can be usually tricky).
  *
- * Inspired from https://www.developerway.com/posts/debouncing-in-react
+ * @param callback - The function to debounce
+ * @param delay - The number of milliseconds to delay.
+ * @param options - Same options as `lodash.debounce`. See https://lodash.com/docs#debounce for more details.
  *
  * @example
+ * ```js
  * const debouncedFunc = useDebounce(func, 500)
  * debouncedFunc() // debouncedFunc will be called once in 500ms
+ * ```
+ *
+ * Inspired from https://www.developerway.com/posts/debouncing-in-react
  */
-export function useDebounce<T, U>(callback: (props: T) => U, delay: number) {
+export function useDebounce<T, U>(
+  callback: (props: T) => U,
+  delay: number,
+  options?: DebounceSettings
+) {
   const ref = useRef<(props: T) => U>()
 
   useEffect(() => {
@@ -21,8 +31,9 @@ export function useDebounce<T, U>(callback: (props: T) => U, delay: number) {
   const debouncedCallback = useMemo(() => {
     const func = (props: T) => ref.current?.(props)
 
-    return debounce(func, delay)
-  }, [delay])
+    return debounce(func, delay, options)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return debouncedCallback
 }
