911fe46f9394af10b9da99e53615ead37769c8ad
Hugo Voisin <hugov@bam.tech>
2023-06-14 15:59:52 +0200

    wip

--- src/features/home/components/HomeBodyPlaceholder.stories.tsx
+++ src/features/home/components/HomeBodyPlaceholder.stories.tsx
@@ -10,3 +10,7 @@ export default {
 
 export const HomeBody: ComponentStory<typeof HomeBodyPlaceholder> = () => <HomeBodyPlaceholder />
 HomeBody.storyName = 'HomeBodyPlaceholder'
+HomeBody.parameters = {
+  // Notifies Chromatic to pause the animations when they finish for the specific story.
+  chromatic: { pauseAnimationAtEnd: true },
+}
