a23b7f297c73e87c6311b177456a34da3325107f
Hugo Voisin <hugov@bam.tech>
2023-06-14 11:19:54 +0200

    refactor: rename variables

--- src/features/home/pages/helpers/useOnScroll.ts
+++ src/features/home/pages/helpers/useOnScroll.ts
@@ -4,10 +4,10 @@ import { useOpacityTransition } from 'ui/animations/helpers/useOpacityTransition
 import { useThrottle } from 'ui/hooks/useThrottle'
 
 export const useOnScroll = (
-  scrollListenerToDebounce: (e: NativeSyntheticEvent<NativeScrollEvent>) => void,
+  scrollListenerToThrottle: (e: NativeSyntheticEvent<NativeScrollEvent>) => void,
   scrollListener: (e: NativeSyntheticEvent<NativeScrollEvent>) => void
 ) => {
-  const debouncedAnalyticsListener = useThrottle(scrollListenerToDebounce, 100, { leading: false })
+  const throttledListener = useThrottle(scrollListenerToThrottle, 100, { leading: false })
 
   const listeners = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
     // In React-Native only, the event is not usable several milliseconds after its throw
@@ -15,7 +15,7 @@ export const useOnScroll = (
     // https://reactjs.org/docs/legacy-event-pooling.html
     if (Platform.OS !== 'web') e.persist()
 
-    debouncedAnalyticsListener(e)
+    throttledListener(e)
     scrollListener(e)
   }
 
