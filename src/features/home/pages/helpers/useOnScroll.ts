191d144e0502dc2e41ed3e524d742e855f7d1adf
Hugo Voisin <hugov@bam.tech>
2023-06-14 11:11:43 +0200

    fix: use throttle instead of debounce debounce was misused, it should have been throttle from the beginning

--- src/features/home/pages/helpers/useOnScroll.ts
+++ src/features/home/pages/helpers/useOnScroll.ts
@@ -1,13 +1,14 @@
 import { NativeScrollEvent, NativeSyntheticEvent, Platform } from 'react-native'
 
 import { useOpacityTransition } from 'ui/animations/helpers/useOpacityTransition'
-import { useDebounce } from 'ui/hooks/useDebounce'
+import { useThrottle } from 'ui/hooks/useThrottle'
 
 export const useOnScroll = (
   scrollListenerToDebounce: (e: NativeSyntheticEvent<NativeScrollEvent>) => void,
   scrollListener: (e: NativeSyntheticEvent<NativeScrollEvent>) => void
 ) => {
-  const debouncedAnalyticsListener = useDebounce(scrollListenerToDebounce, 100)
+  const debouncedAnalyticsListener = useThrottle(scrollListenerToDebounce, 100, { leading: false })
+
   const listeners = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
     // In React-Native only, the event is not usable several milliseconds after its throw
     // so we need to persist it, if we want to access its value in a debounced function.
