7271f0bf0a37be9c0760e353d466813df51497bf
bruno ebstein <bruno.ebstein@passculture.app>
2023-06-19 11:49:32 +0200

    (BSR) fix(global): Prefer using an optional chain expression instead

--- src/features/cookies/helpers/getAcceptedCookieConsent.ts
+++ src/features/cookies/helpers/getAcceptedCookieConsent.ts
@@ -3,6 +3,5 @@ import { getCookiesChoice as getCookiesConsentV2 } from 'features/cookies/helper
 
 export const getAcceptedCookieConsent = async (cookie: CookieNameEnum): Promise<boolean | null> => {
   const cookiesChoiceV2 = await getCookiesConsentV2()
-  if (cookiesChoiceV2?.consent) return cookiesChoiceV2.consent.accepted.includes(cookie)
-  return false
+  return cookiesChoiceV2?.consent?.accepted?.includes(cookie) ?? false
 }
