1f5941a1dcb869141a518eef7fb05694f43786b2
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2023-06-19 10:56:41 +0200

    (PC-22517) feat(forceUpdate): udpate design for ForceUpdate screen (#4917)

--- src/features/forceUpdate/pages/ForceUpdate.web.test.tsx
+++ src/features/forceUpdate/pages/ForceUpdate.web.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-import { render, checkAccessibilityFor } from 'tests/utils/web'
+import { render, checkAccessibilityFor, screen } from 'tests/utils/web'
 
 import { ForceUpdate } from './ForceUpdate'
 
 describe('<ForceUpdate/>', () => {
+  it('should not display the web app button', () => {
+    render(<ForceUpdate resetErrorBoundary={() => null} />)
+
+    const goToWebappButton = screen.queryByText('Utiliser la version web')
+    expect(goToWebappButton).toBeNull()
+  })
+
   describe('Accessibility', () => {
     it('should not have basic accessibility issues', async () => {
       const { container } = render(<ForceUpdate resetErrorBoundary={() => null} />)
