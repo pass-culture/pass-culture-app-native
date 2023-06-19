cbeb85b68c7411cd9cad37c119dc954e238d6c3c
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2023-06-19 10:56:41 +0200

    (PC-22517) feat(forceUpdate): udpate design for ForceUpdate screen (#4917)

--- src/features/forceUpdate/pages/ForceUpdate.tsx
+++ src/features/forceUpdate/pages/ForceUpdate.tsx
@@ -5,9 +5,13 @@ import styled from 'styled-components/native'
 import { STORE_LINK, TITLE, BUTTON_TEXT, DESCRIPTION } from 'features/forceUpdate/constants'
 import { useMinimalBuildNumber } from 'features/forceUpdate/helpers/useMinimalBuildNumber'
 import { openUrl } from 'features/navigation/helpers'
+import { env } from 'libs/environment'
 import { Helmet } from 'libs/react-helmet/Helmet'
 import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
+import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
+import { ExternalTouchableLink } from 'ui/components/touchableLink/ExternalTouchableLink'
 import { GenericInfoPage } from 'ui/pages/GenericInfoPage'
+import { ExternalSiteFilled } from 'ui/svg/icons/ExternalSiteFilled'
 import { Star } from 'ui/svg/icons/Star'
 import { Typo } from 'ui/theme'
 
@@ -48,6 +52,7 @@ export const ForceUpdate = ({ resetErrorBoundary }: ForceUpdateProps) => {
         icon={Star}
         buttons={[
           <ButtonPrimaryWhite key={BUTTON_TEXT} wording={BUTTON_TEXT} onPress={onPressStoreLink} />,
+          <WebAppButton key={2} />,
         ]}>
         <StyledBody>{DESCRIPTION}</StyledBody>
       </GenericInfoPage>
@@ -59,3 +64,16 @@ const StyledBody = styled(Typo.Body)(({ theme }) => ({
   textAlign: 'center',
   color: theme.colors.white,
 }))
+
+const isWeb = Platform.OS === 'web'
+const WebAppButton = () => {
+  if (isWeb) return <React.Fragment />
+  return (
+    <ExternalTouchableLink
+      as={ButtonTertiaryWhite}
+      wording="Utiliser la version web"
+      externalNav={{ url: `https://${env.WEBAPP_V2_DOMAIN}` }}
+      icon={ExternalSiteFilled}
+    />
+  )
+}
