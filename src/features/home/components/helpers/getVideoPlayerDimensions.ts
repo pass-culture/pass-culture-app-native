2dd15d9c2bd21e67935a1633a57804ee31a32d5e
Tanguy <tanguy.prache@passculture.app>
2023-06-22 15:39:12 +0200

    (PC-21766) VideoPlayer: adjust player size

--- src/features/home/components/helpers/getVideoPlayerDimensions.ts
+++ src/features/home/components/helpers/getVideoPlayerDimensions.ts
@@ -6,13 +6,13 @@ export const getVideoPlayerDimensions = (
   isDesktopViewport: boolean | undefined,
   windowWidth: number
 ) => {
-  const playerHeight = Math.ceil(
+  const playerHeight =
     ((isDesktopViewport
       ? Math.min(windowWidth, theme.modal.desktopMaxWidth) * PixelRatio.get()
       : windowWidth * PixelRatio.get()) *
       (9 / 16)) /
-      PixelRatio.get()
-  )
+    PixelRatio.get()
+
   const playerWidth = isDesktopViewport
     ? Math.min(windowWidth, theme.modal.desktopMaxWidth)
     : windowWidth
