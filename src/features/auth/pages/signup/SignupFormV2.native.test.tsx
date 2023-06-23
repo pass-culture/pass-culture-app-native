7a51732422a92fcdf41b71c26fc5f50f350213e2
Marine Shaw <68000368+marineshaw@users.noreply.github.com>
2023-06-23 17:03:00 +0200

    (PC-22325) feat(signup): new email confirmation (#4944)
    * :sparkles: (Signup) Create new confirmation email sent component
    
    * :sparkles: (Signup) Add signupConfirmationEmailSent in signupform
    
    * :bug: (Signup) Fix act warning
    
    * :sparkles: (Signup) Remove backButton on confirmation email sent
    
    * :sparkles: (Signup) Change close button wording when is last step
    
    * :bug: (SignupConfirmationEmailSent) fix wording
    
    * :bug: (Signup) Fix act warning

--- src/features/auth/pages/signup/SignupFormV2.native.test.tsx
+++ src/features/auth/pages/signup/SignupFormV2.native.test.tsx
@@ -1,9 +1,15 @@
+import { rest } from 'msw'
 import React from 'react'
 
+import { navigate } from '__mocks__/@react-navigation/native'
+import { ELIGIBLE_AGE_DATE } from 'features/auth/fixtures/fixtures'
 import { SignupForm } from 'features/auth/pages/signup/SignupFormV2'
 import { mockGoBack } from 'features/navigation/__mocks__/useGoBack'
+import { navigateToHomeConfig } from 'features/navigation/helpers'
 import { analytics } from 'libs/analytics'
-import { fireEvent, render, screen } from 'tests/utils'
+import { env } from 'libs/environment'
+import { server } from 'tests/server'
+import { act, fireEvent, render, screen } from 'tests/utils'
 
 describe('Signup Form', () => {
   it('should not display quit button on firstStep', async () => {
@@ -76,4 +82,83 @@ describe('Signup Form', () => {
     const passwordInput = await screen.findAllByText('Mot de passe')
     expect(passwordInput).toBeTruthy()
   })
+
+  it('should show email sent confirmation on signup success', async () => {
+    simulateSignupSuccess()
+    render(<SignupForm />)
+
+    const emailInput = screen.getByPlaceholderText('tonadresse@email.com')
+    fireEvent.changeText(emailInput, 'email@gmail.com')
+    await act(() => fireEvent.press(screen.getByText('Continuer')))
+
+    const passwordInput = screen.getByPlaceholderText('Ton mot de passe')
+    await act(async () => fireEvent.changeText(passwordInput, 'user@AZERTY123'))
+    await act(async () => fireEvent.press(screen.getByText('Continuer')))
+
+    const datePicker = screen.getByTestId('date-picker-spinner-native')
+    await act(async () =>
+      fireEvent(datePicker, 'onChange', { nativeEvent: { timestamp: ELIGIBLE_AGE_DATE } })
+    )
+    await act(async () => fireEvent.press(screen.getByText('Continuer')))
+
+    await act(async () => fireEvent.press(screen.getByText('Accepter et s’inscrire')))
+
+    expect(screen.getByText('Confirme ton adresse e-mail')).toBeTruthy()
+  })
+
+  it('should not display backButton on confirmation email sent page', async () => {
+    simulateSignupSuccess()
+    render(<SignupForm />)
+
+    const emailInput = screen.getByPlaceholderText('tonadresse@email.com')
+    fireEvent.changeText(emailInput, 'email@gmail.com')
+    await act(() => fireEvent.press(screen.getByText('Continuer')))
+
+    const passwordInput = screen.getByPlaceholderText('Ton mot de passe')
+    await act(async () => fireEvent.changeText(passwordInput, 'user@AZERTY123'))
+    await act(async () => fireEvent.press(screen.getByText('Continuer')))
+
+    const datePicker = screen.getByTestId('date-picker-spinner-native')
+    await act(async () =>
+      fireEvent(datePicker, 'onChange', { nativeEvent: { timestamp: ELIGIBLE_AGE_DATE } })
+    )
+    await act(async () => fireEvent.press(screen.getByText('Continuer')))
+
+    await act(async () => fireEvent.press(screen.getByText('Accepter et s’inscrire')))
+
+    expect(screen.queryByLabelText('Revenir en arrière')).toBeNull()
+  })
+
+  it('should go back to home when pressing close button on email confirmation sent', async () => {
+    simulateSignupSuccess()
+    render(<SignupForm />)
+
+    const emailInput = screen.getByPlaceholderText('tonadresse@email.com')
+    fireEvent.changeText(emailInput, 'email@gmail.com')
+    await act(() => fireEvent.press(screen.getByText('Continuer')))
+
+    const passwordInput = screen.getByPlaceholderText('Ton mot de passe')
+    await act(async () => fireEvent.changeText(passwordInput, 'user@AZERTY123'))
+    await act(async () => fireEvent.press(screen.getByText('Continuer')))
+
+    const datePicker = screen.getByTestId('date-picker-spinner-native')
+    await act(async () =>
+      fireEvent(datePicker, 'onChange', { nativeEvent: { timestamp: ELIGIBLE_AGE_DATE } })
+    )
+    await act(async () => fireEvent.press(screen.getByText('Continuer')))
+
+    await act(async () => fireEvent.press(screen.getByText('Accepter et s’inscrire')))
+
+    const closeButton = screen.getByText('Fermer')
+    fireEvent.press(closeButton)
+
+    expect(navigate).toHaveBeenCalledWith(navigateToHomeConfig.screen, navigateToHomeConfig.params)
+  })
 })
+
+const simulateSignupSuccess = () =>
+  server.use(
+    rest.post(env.API_BASE_URL + '/native/v1/account', (_req, res, ctx) => {
+      return res.once(ctx.status(200), ctx.json({}))
+    })
+  )
