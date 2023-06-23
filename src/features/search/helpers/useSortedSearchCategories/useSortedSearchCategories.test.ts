a27539fb51fbba315eb6eba8e3212e6356e441e5
YassinL <62058919+YassinL@users.noreply.github.com>
2023-06-23 16:02:30 +0200

    (PC-22552) feat(useSortedSearchCategories): sorted categories by desired order (#4931)
    * (PC-22552) feat(useSortedSearchCategories): sorted categories by desired order
    
    * (PC-22552) fix(Search): upgrade snapshot
    
    * (PC-22552) review(useSortedSearchCategories): after review add a new key position to sort by position
    
    * (PC-22552) review(useSortedSearchCategories): handle the case if position is undefined
    
    * (PC-22552) test(search): add some tests
    
    * (PC-22552) test(search): fixed tests
    
    * (PC-22552) test(search): fixed lint
    
    ---------
    
    Co-authored-by: dbenfouzari-pass <donovan.benfouzari@passculture.app>

---
+++ src/features/search/helpers/useSortedSearchCategories/useSortedSearchCategories.test.ts
@@ -0,0 +1,78 @@
+import { SearchCategoriesIllustrations } from 'features/internal/cheatcodes/pages/AppComponents/illustrationsExports'
+import { placeholderData } from 'libs/subcategories/placeholderData'
+import { renderHook } from 'tests/utils'
+
+import {
+  categoriesSortPredicate,
+  MappingOutput,
+  useSortedSearchCategories,
+} from './useSortedSearchCategories'
+
+const mockData = placeholderData
+jest.mock('libs/subcategories/useSubcategories', () => ({
+  useSubcategories: () => ({
+    data: mockData,
+  }),
+}))
+
+describe('useSortedSearchCategories', () => {
+  const options = { initialProps: jest.fn() }
+
+  it('should return all categories', () => {
+    const { result } = renderHook(useSortedSearchCategories, options)
+
+    expect(result.current.length).toEqual(14)
+  })
+
+  it("should format category's label", () => {
+    const { result } = renderHook(useSortedSearchCategories, options)
+
+    expect(result.current[11].label).toEqual('Cartes jeunes')
+  })
+
+  it('should set illustration for category', () => {
+    const { result } = renderHook(useSortedSearchCategories, options)
+
+    expect(result.current[11].Illustration).toEqual(SearchCategoriesIllustrations.YouthCards)
+  })
+
+  it('should sort search group names by the key position', () => {
+    const { result } = renderHook(useSortedSearchCategories, options)
+
+    const actualCategoriesLabels = result.current.map((category) => category.label)
+    expect(actualCategoriesLabels).toEqual([
+      'Concerts & festivals',
+      'Films, séries, cinéma',
+      'Livres',
+      'CD, vinyles, musique en ligne',
+      'Arts & loisirs créatifs',
+      'Spectacles',
+      'Musées & visites culturelles',
+      'Jeux & jeux vidéos',
+      'Instruments de musique',
+      'Médias & presse',
+      'Bibliothèques, Médiathèques',
+      'Cartes jeunes',
+      'Conférences & rencontres',
+      'Évènements en ligne',
+    ])
+  })
+})
+
+describe('categoriesSortPredicate', () => {
+  it('should sort when example is simple', () => {
+    const itemA = { position: 2, label: 'Example 1' } as MappingOutput
+    const itemB = { position: 1, label: 'Example 2' } as MappingOutput
+
+    expect(categoriesSortPredicate(itemA, itemB)).toEqual(1)
+    expect(categoriesSortPredicate(itemB, itemA)).toEqual(-1)
+  })
+
+  it('should sort when undefined', () => {
+    const itemA = { position: undefined, label: 'Example 1' } as MappingOutput
+    const itemB = { position: 1, label: 'Example 2' } as MappingOutput
+
+    expect(categoriesSortPredicate(itemA, itemB)).toEqual(0)
+    expect(categoriesSortPredicate(itemB, itemA)).toEqual(0)
+  })
+})
--- src/features/search/helpers/useSortedSearchCategories/useSortedSearchCategories.ts
+++ src/features/search/helpers/useSortedSearchCategories/useSortedSearchCategories.ts
@@ -1,9 +1,18 @@
 import { SearchGroupNameEnumv2 } from 'api/gen'
 import { ListCategoryButtonProps } from 'features/search/components/CategoriesButtonsDisplay/CategoriesButtonsDisplay'
+import { CategoryButtonProps } from 'features/search/components/CategoryButton/CategoryButton'
 import { useAvailableCategories } from 'features/search/helpers/useAvailableCategories/useAvailableCategories'
 import { useSearchGroupLabelMapping } from 'libs/subcategories/mappings'
 
 export type OnPressCategory = (pressedCategory: SearchGroupNameEnumv2) => void
+
+export type MappingOutput = CategoryButtonProps & { position: number | undefined }
+
+export function categoriesSortPredicate(a: MappingOutput, b: MappingOutput): number {
+  if (a.position === undefined) return 0
+  if (b.position === undefined) return 0
+  return a.position - b.position
+}
 
 export const useSortedSearchCategories = (
   onPressCategory: OnPressCategory
@@ -12,7 +21,7 @@ export const useSortedSearchCategories = (
   const categories = useAvailableCategories()
 
   return categories
-    .map((category) => ({
+    .map<MappingOutput>((category) => ({
       label: searchGroupLabelMapping?.[category.facetFilter] || '',
       Icon: category.icon,
       Illustration: category.illustration,
@@ -21,6 +30,7 @@ export const useSortedSearchCategories = (
       },
       baseColor: category.baseColor,
       gradients: category.gradients,
+      position: category.position,
     }))
-    .sort((a, b) => a.label.localeCompare(b.label))
+    .sort(categoriesSortPredicate)
 }
