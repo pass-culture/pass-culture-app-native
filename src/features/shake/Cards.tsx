eff885e21471a47b5c6dcb99a0de3f2a69ca78e4
AnoukHello <anoukhello22@gmail.com>
2023-06-27 18:24:03 +0200

    WIP

--- src/features/shake/Cards.tsx
+++ src/features/shake/Cards.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 import { PixelRatio } from 'react-native'
 import styled from 'styled-components/native'
 
@@ -6,8 +6,8 @@ import { ImageCaption } from 'ui/components/ImageCaption'
 import { ImageTile } from 'ui/components/ImageTile'
 import { getSpacing, MARGIN_DP } from 'ui/theme'
 
-const OFFER_HEIGHT_1 = getSpacing(56)
-const OFFER_WIDTH_1 = getSpacing(40)
+const OFFER_HEIGHT_1 = getSpacing(70)
+const OFFER_WIDTH_1 = OFFER_HEIGHT_1 * 0.75
 
 const OFFER_HEIGHT_2 = OFFER_HEIGHT_1 * 0.9
 const OFFER_WIDTH_2 = OFFER_WIDTH_1 * 0.9
@@ -17,49 +17,46 @@ const OFFER_WIDTH_3 = OFFER_WIDTH_1 * 0.8
 
 const IMAGE_CAPTION_HEIGHT = PixelRatio.roundToNearestPixel(MARGIN_DP)
 
-export const Cards = () => {
+const OFFER_POSITION_X_2 = 7.5
+const OFFER_POSITION_X_3 = OFFER_POSITION_X_2 * 2
+
+const OFFER_POSITION_Y_2 = 27.5
+const OFFER_POSITION_Y_3 = OFFER_POSITION_Y_2 * 2
+
+type Props = {
+  uri: string
+  distance: string
+  categoryLabel: string
+}
+
+export const Cards: FunctionComponent<Props> = ({ uri, distance, categoryLabel }) => {
   return (
     <Container>
-      <Card strikeLineAngle={15} positionX={43} positionY={32}>
-        <ImageTile
-          onlyTopBorderRadius
-          width={OFFER_WIDTH_3}
-          height={OFFER_HEIGHT_3}
-          uri="https://storage.googleapis.com/passculture-metier-ehp-testing-assets-fine-grained/thumbs/mediations/test_image_2.png"
-        />
+      <Card strikeLineAngle={OFFER_POSITION_X_3} positionX={OFFER_POSITION_Y_3} positionY={30}>
+        <ImageTile onlyTopBorderRadius width={OFFER_WIDTH_3} height={OFFER_HEIGHT_3} uri={uri} />
         <ImageCaption
           height={IMAGE_CAPTION_HEIGHT}
           width={OFFER_WIDTH_3}
-          categoryLabel="Théâtre"
-          distance="100km"
+          categoryLabel={categoryLabel}
+          distance={distance}
         />
       </Card>
-      <Card strikeLineAngle={7.5} positionX={22} positionY={15}>
-        <ImageTile
-          onlyTopBorderRadius
-          width={OFFER_WIDTH_2}
-          height={OFFER_HEIGHT_2}
-          uri="https://storage.googleapis.com/passculture-metier-ehp-testing-assets-fine-grained/thumbs/mediations/test_image_1_bis.jpg"
-        />
+      <Card strikeLineAngle={OFFER_POSITION_X_2} positionX={OFFER_POSITION_Y_2} positionY={15}>
+        <ImageTile onlyTopBorderRadius width={OFFER_WIDTH_2} height={OFFER_HEIGHT_2} uri={uri} />
         <ImageCaption
           height={IMAGE_CAPTION_HEIGHT}
           width={OFFER_WIDTH_2}
-          categoryLabel="Théâtre"
-          distance="100km"
+          categoryLabel={categoryLabel}
+          distance={distance}
         />
       </Card>
       <Card strikeLineAngle={0} positionX={0} positionY={0}>
-        <ImageTile
-          onlyTopBorderRadius
-          width={OFFER_WIDTH_1}
-          height={OFFER_HEIGHT_1}
-          uri="https://storage.googleapis.com/passculture-metier-ehp-testing-assets-fine-grained/thumbs/mediations/test_image_2.png"
-        />
+        <ImageTile onlyTopBorderRadius width={OFFER_WIDTH_1} height={OFFER_HEIGHT_1} uri={uri} />
         <ImageCaption
           height={IMAGE_CAPTION_HEIGHT}
           width={OFFER_WIDTH_1}
-          categoryLabel="Théâtre"
-          distance="100km"
+          categoryLabel={categoryLabel}
+          distance={distance}
         />
       </Card>
     </Container>
@@ -67,8 +64,8 @@ export const Cards = () => {
 }
 
 const Container = styled.View({
-  padding: getSpacing(5),
   alignItems: 'center',
+  height: OFFER_HEIGHT_1 + IMAGE_CAPTION_HEIGHT,
 })
 
 const Card = styled.View<{ strikeLineAngle: number; positionX: number; positionY: number }>(
