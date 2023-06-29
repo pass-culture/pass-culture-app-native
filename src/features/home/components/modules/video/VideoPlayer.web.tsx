92c6787dfcbf009cdd1ca753e3ca9aa4e3913cde
Sandro <sandro.sgro@passculture.app>
2023-06-29 15:22:46 +0200

    (PC-22106) feat(VideoModal): use react-youtube for the web to autoplay and display end layer

--- src/features/home/components/modules/video/VideoPlayer.web.tsx
+++ src/features/home/components/modules/video/VideoPlayer.web.tsx
@@ -1,6 +1,99 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useWindowDimensions } from 'react-native'
+import YouTube, { YouTubeProps } from 'react-youtube'
+import styled, { useTheme } from 'styled-components/native'
 
-//TODO(PC-22712): Tempory fix before imlpementing a solution for youtube web player
-export const VideoPlayer: React.FC = () => {
-  return <React.Fragment />
+import { getVideoPlayerDimensions } from 'features/home/components/helpers/getVideoPlayerDimensions'
+import { VideoEndView } from 'features/home/components/modules/video/VideoEndView'
+import { VideoErrorView } from 'features/home/components/modules/video/VideoErrorView'
+import { analytics } from 'libs/analytics'
+import { Offer } from 'shared/offer/types'
+import { theme } from 'theme'
+import { getSpacing } from 'ui/theme'
+
+interface VideoPlayerProps {
+  youtubeVideoId: string
+  offer: Offer
+  onPressSeeOffer: () => void
+  moduleId: string
+  moduleName: string
+  homeEntryId: string
 }
+
+export const VideoPlayer: React.FC<VideoPlayerProps> = ({
+  youtubeVideoId,
+  offer,
+  onPressSeeOffer,
+  moduleId,
+  moduleName,
+  homeEntryId,
+}) => {
+  const [hasFinishPlaying, setHasFinishPlaying] = useState(false)
+  const [showErrorView, setShowErrorView] = React.useState(false)
+  const { isDesktopViewport } = useTheme()
+  const { width: windowWidth } = useWindowDimensions()
+  const { playerHeight, playerWidth } = getVideoPlayerDimensions(isDesktopViewport, windowWidth)
+
+  const logConsultVideo = () => {
+    analytics.logConsultVideo({ from: 'home', moduleId })
+  }
+
+  const replayVideo = () => {
+    // YouTube.playerRef.current?.seekTo(0, true)
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    height: playerHeight,
+    width: playerWidth,
+    playerVars: {
+      autoplay: true,
+      modestbranding: true,
+      rel: false,
+    },
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function onPlayerStateChange(event: any) {
+    if (event.data === YouTube.PlayerState.ENDED) {
+      setHasFinishPlaying(true)
+      analytics.logHasSeenAllVideo(moduleId)
+    }
+    if (event.data !== YouTube.PlayerState.ENDED && hasFinishPlaying === true)
+      setHasFinishPlaying(false)
+  }
+
+  return (
+    <React.Fragment>
+      <StyledVideoPlayerContainer>
+        <YouTube
+          videoId={youtubeVideoId}
+          opts={opts}
+          onReady={logConsultVideo}
+          onStateChange={onPlayerStateChange}
+          onError={() => {
+            setShowErrorView(true)
+          }}
+        />
+      </StyledVideoPlayerContainer>
+      {!!hasFinishPlaying && (
+        <VideoEndView
+          onPressReplay={replayVideo}
+          offer={offer}
+          onPressSeeOffer={onPressSeeOffer}
+          style={{ height: playerHeight, width: playerWidth }}
+          moduleId={moduleId}
+          moduleName={moduleName}
+          homeEntryId={homeEntryId}
+        />
+      )}
+      {!!showErrorView && <VideoErrorView style={{ height: playerHeight, width: playerWidth }} />}
+    </React.Fragment>
+  )
+}
+
+const StyledVideoPlayerContainer = styled.View({
+  backgroundColor: theme.colors.black,
+  borderTopLeftRadius: getSpacing(4),
+  borderTopRightRadius: getSpacing(4),
+  overflow: 'hidden',
+})
