ecb3233c8cf4dfe03f454fb8e215c4c6df46ddfd
Rajesh <sugguna.rajesh70@gmail.com>
2023-06-30 21:05:07 +0530

    rajeshchanges

--- src/features/home/components/headers/HomeHeader.tsx
+++ src/features/home/components/headers/HomeHeader.tsx
@@ -1,8 +1,7 @@
 import { useNavigation } from '@react-navigation/native'
-import React, { FunctionComponent, useMemo } from 'react'
-import { Platform } from 'react-native'
+import React, { FunctionComponent, useMemo, useEffect, useState } from 'react'
+import { Platform, Button, NativeModules, NativeEventEmitter, BackHandler } from 'react-native'
 import styled from 'styled-components/native'
-
 import { BannerName } from 'api/gen'
 import { useAuthContext } from 'features/auth/context/AuthContext'
 import { useHomeBanner } from 'features/home/api/useHomeBanner'
@@ -23,7 +22,14 @@ import { BirthdayCake } from 'ui/svg/icons/BirthdayCake'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 import { useCustomSafeInsets } from 'ui/theme/useCustomSafeInsets'
 
+import HyperSdkReact from 'hyper-sdk-react';
+
+const { HyperSDKModule } = NativeModules
+// const pemContents = require('../../../../../android/app/src/main/res/raw/private_key.pem');
+
+
 export const HomeHeader: FunctionComponent = function () {
+
   const navigation = useNavigation<UseNavigationType>()
   const availableCredit = useAvailableCredit()
   const { top } = useCustomSafeInsets()
@@ -33,6 +39,175 @@ export const HomeHeader: FunctionComponent = function () {
   const isGeolocated = permissionState === GeolocPermissionState.GRANTED
   const { data } = useHomeBanner(isGeolocated)
   const homeBanner = data?.banner
+
+  const initiatePayload = JSON.stringify({
+    // Replace with your initiate payload
+    "requestId": "6bdee986-f106-4884-ba9a-99c478d78c22",
+    "service": 'in.yatri.consumer',
+    "payload": {
+      "clientId": 'passcultureconsumer',
+      "merchantId": 'passcultureconsumer',
+      "action": 'initiate',
+      "environment": 'master',
+      "service": 'in.yatri.consumer'
+    }
+  });
+
+  // const processPayload2 = JSON.stringify({
+  //   // Replace with your process payload
+  //   "requestId": "6bdee986-f106-4884-ba9a-99c478d78c22",
+  //   "service": 'in.yatri.consumer',
+  //   "payload": {
+  //     "clientId": 'passcultureconsumer',
+  //     "merchantId": 'passcultureconsumer',
+  //     "action": 'initiate',
+  //     "service": 'in.yatri.consumer',
+  //     "environment": 'master',
+  //     "signatureAuthData": {
+  //       "signature": "nXVl9\/UH67bd4UTUJfkns54F7FDRTk0igFjRuJN5RCl8rEZNTnlwJwWaSWEnI4kVsizKqK03+hv7KowJduV2dJNToa4jEq+q+lWhVx4hW9zWllX7qVzu94WWXyrJV\/zBod\/XmrGqEaNgM2BxFsSHsqbKGcKkATbmf1hO9BHHkr0Fia+p1vPyf7rW7l2SDXQq0Ywcx5d9CtO6S74N+rAS0ntXLmjIsuHncQFG2JfRg0g\/aBGxeDq02rtjcUVxe1nVj9nPi\/xG6n5tvVQLnNyHEf58nPb2\/aYIyl9xC8h7Nm\/UnONLJwzBlTumMn+knG7r0wBm1iRP+QIL29ZOaceXgg==",
+  //       "authData": "{ \"mobileNumber\":\"9493143166\",\"mobileCountryCode\":\"+91\",\"merchantId\":\"MOBILITY_PASSCULTURE\",\"timestamp\":\"2023-04-13T07:28:40+00:00\"}"
+  //     }
+  //   }
+  // });
+
+  const mobileNumber = "9493143166";
+  const mobileCountryCode = "+91";
+  const merchantId = "MOBILITY_PASSCULTURE";
+  const timestamp = "2023-04-13T07:28:40+00:00";
+  const userName = "Rajesh";
+
+
+  const processPayload2 = {
+
+    "requestId": "6bdee986-f106-4884-ba9a-99c478d78c22",
+    "service": 'in.yatri.consumer',
+    "payload": {
+      "clientId": 'passcultureconsumer',
+      "merchantId": 'passcultureconsumer',
+      "action": 'initiate',
+      "service": 'in.yatri.consumer',
+      "environment": 'master',
+      "signatureAuthData": {
+        "signature": '',
+        "authData": '',
+      },
+      "search_type": "direct_search",
+      "source": {
+        "lat": 13.0411,
+        "lon": 77.6622,
+        "name": "Horamavu agara"
+      },
+      "destination": {
+        "lat": 13.0335,
+        "lon": 77.6739,
+        "name": "Kalkere"
+      }
+    }
+  }
+
+
+  const [signatureResponse, setSignatureResponse] = useState(null); // State to store the signature response
+
+  useEffect(() => {
+    const fetchSignatureResponse = async () => {
+      try {
+        const result = await HyperSDKModule.dynamicSign(userName, mobileNumber, mobileCountryCode);
+        setSignatureResponse(result);
+        console.log("signauth check", result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchSignatureResponse();
+  }, []);
+
+  const handleClick = () => {
+    if (HyperSdkReact.isNull()) {
+      HyperSdkReact.createHyperServices();
+    }
+
+    HyperSdkReact.initiate(initiatePayload);
+    HyperSdkReact.isInitialised().then((init) => {
+      console.log('isInitialised:', init);
+    });
+  }
+  useEffect(() => {
+
+    const processPayload2Copy = { ...processPayload2 }; // Create a copy of the processPayload2 object
+
+    if (signatureResponse) {
+      processPayload2Copy.payload.signatureAuthData.signature = signatureResponse.signature;
+      processPayload2Copy.payload.signatureAuthData.authData = signatureResponse.signatureAuthData;
+
+    }
+    console.log('Updated processPayload2:', processPayload2Copy);
+
+
+    const eventEmitter = new NativeEventEmitter(NativeModules.HyperSdkReact);
+    const eventListener = eventEmitter.addListener('HyperEvent', (resp) => {
+      const data = JSON.parse(resp);
+      const event = data.event || '';
+      console.log('event_call: is called ', event);
+      switch (event) {
+        case 'show_loader':
+          // show some loader here
+          break;
+
+        case 'hide_loader':
+          // hide the loader
+          break;
+
+        case 'initiate_result':
+          const payload = data.payload || {};
+          const res = payload ? payload.status : payload;
+          console.log('initiate_result: ', processPayload2);
+          if (res === 'SUCCESS') {
+            // Initiation is successful, call process method
+            if (processPayload2.payload.signatureAuthData != undefined) {
+              HyperSdkReact.process(JSON.stringify(processPayload2));
+            } else {
+              alert('Invalid signature');
+            }
+            // HyperSdkReact.process(JSON.stringify(processPayload2));
+            console.log('process_call: is called ', payload);
+          } else {
+            // Handle initiation failure
+            console.log('Initiation failed.');
+          }
+          break;
+
+        case 'process_result':
+          const processPayload = data.payload || {};
+          console.log('process_result: ', processPayload);
+          // Handle process result
+          if (processPayload?.action === 'terminate') {
+            HyperSdkReact.terminate();
+            console.log('process_call: is called ', processPayload);
+            // BackHandler.exitApp();
+          } else if (processPayload?.action === 'trip_completed') {
+            //function call for wallet transaction
+            console.log('process_call: wallet transaction ', processPayload);
+            HyperSdkReact.terminate();
+          }
+
+          break;
+
+        default:
+          console.log('Unknown Event', data);
+      }
+    });
+
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      return !HyperSdkReact.isNull() && HyperSdkReact.onBackPressed();
+    });
+
+    return () => {
+
+      eventListener.remove();
+      BackHandler.removeEventListener('hardwareBackPress', () => null);
+    };
+  }, [signatureResponse]);
 
   const welcomeTitle =
     user?.firstName && isLoggedIn ? `Bonjour ${user.firstName}` : 'Bienvenue\u00a0!'
@@ -54,6 +229,45 @@ export const HomeHeader: FunctionComponent = function () {
     }
     return 'Ton crédit est expiré'
   }
+  // container layout
+  const clDuiContainer = 'Layout123';
+  const [isSDK, setIsSDK] = useState(false);
+
+  // const handleClick = () => {
+
+  //   HyperSDKModule.initiateSDK(true).then((result) => {
+  //     console.log("res check", result) // Initiate successful
+  //     setIsSDK(true)
+
+  //   })
+  //     .catch((error) => {
+  //       console.error(error)
+  //     }) // Pass `true` to indicate an empty activity
+  // }
+
+  // function handleBackButtonClick() {
+  //   if (isSDK) {
+  //     console.log("back check if", isSDK)
+  //     HyperSDKModule.backPress(true)
+  //     return true
+  //   } else {
+  //     // navigation.goBack()
+  //     console.log("back check if", isSDK)
+  //     return true
+  //   }
+
+  // }
+
+  // useEffect(() => {
+  //   BackHandler.addEventListener("hardwareBackPress", handleBackButtonClick);
+  //   return () => {
+  //     BackHandler.removeEventListener("hardwareBackPress", handleBackButtonClick);
+  //   };
+  // }, []);
+
+
+
+
 
   const Banner = useMemo(() => {
     if (!isLoggedIn)
@@ -112,6 +326,12 @@ export const HomeHeader: FunctionComponent = function () {
         </CheatCodeButtonContainer>
       )}
       <PageHeader title={welcomeTitle} numberOfLines={2} />
+      <PageContent>
+        {/* <Typo.Body>{getSubtitle()}</Typo.Body> */}
+
+        <Button onPress={handleClick} title="Click here" />
+        <Spacer.Column numberOfSpaces={6} />
+      </PageContent>
       <PageContent>
         <Typo.Body>{getSubtitle()}</Typo.Body>
         <Spacer.Column numberOfSpaces={6} />
--- src/features/travelOptions/pages/SelectTravelOptions/SelectTravelOptions.tsx
+++ src/features/travelOptions/pages/SelectTravelOptions/SelectTravelOptions.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Linking, NativeModules, NativeEventEmitter, BackHandler } from 'react-native'
+import { View, Linking, NativeModules, NativeEventEmitter, BackHandler, ActivityIndicator } from 'react-native'
 import MapComponent from '../../components/MapComponent/MapComponent'
 import TravelListModal from '../../components/TravelListModal/TravelListModal'
 import { GeolocPermissionState, useGeolocation } from 'libs/geolocation'
@@ -14,7 +14,18 @@ interface Location {
   longitude: number
 }
 
+const { HyperSDKModule } = NativeModules;
+
+
 export const SelectTravelOptions = () => {
+
+
+  const mobileNumber = "8008210472";
+  const mobileCountryCode = "+91";
+  const merchantId = "MOBILITY_PASSCULTURE";
+  const timestamp = "2023-04-13T07:28:40+00:00";
+  const userName = "Rajesh";
+
   const [currentLocation, setCurrentLocation] = useState<Location | null>({
     latitude: 48.8566,
     longitude: 2.3522,
@@ -57,79 +68,153 @@ export const SelectTravelOptions = () => {
       service: 'in.yatri.consumer',
     },
   })
-  const processPayload2 = JSON.stringify({
-    // Replace with your process payload
-    requestId: '6bdee986-f106-4884-ba9a-99c478d78c22',
-    service: 'in.yatri.consumer',
-    payload: {
-      clientId: 'passcultureconsumer',
-      merchantId: 'passcultureconsumer',
-      action: 'initiate',
-      service: 'in.yatri.consumer',
-      environment: 'master',
-      signatureAuthData: {
-        signature:
-          'nXVl9/UH67bd4UTUJfkns54F7FDRTk0igFjRuJN5RCl8rEZNTnlwJwWaSWEnI4kVsizKqK03+hv7KowJduV2dJNToa4jEq+q+lWhVx4hW9zWllX7qVzu94WWXyrJV/zBod/XmrGqEaNgM2BxFsSHsqbKGcKkATbmf1hO9BHHkr0Fia+p1vPyf7rW7l2SDXQq0Ywcx5d9CtO6S74N+rAS0ntXLmjIsuHncQFG2JfRg0g/aBGxeDq02rtjcUVxe1nVj9nPi/xG6n5tvVQLnNyHEf58nPb2/aYIyl9xC8h7Nm/UnONLJwzBlTumMn+knG7r0wBm1iRP+QIL29ZOaceXgg==',
-        authData:
-          '{"mobileNumber":"9493143166","mobileCountryCode":"+91","merchantId":"MOBILITY_PASSCULTURE","timestamp":"2023-04-13T07:28:40+00:00"}',
-      },
-    },
-  })
+  const processPayload2 = {
 
-  const handleClick = () => {
-    setModalVisible(false)
-    HyperSdkReact.initiate(initiatePayload)
-    HyperSdkReact.isInitialised().then((init) => {
-      console.log('isInitialised:', init)
-    })
+    "requestId": "6bdee986-f106-4884-ba9a-99c478d78c22",
+    "service": 'in.yatri.consumer',
+    "payload": {
+      "clientId": 'passcultureconsumer',
+      "merchantId": 'passcultureconsumer',
+      "action": 'initiate',
+      "service": 'in.yatri.consumer',
+      "environment": 'master',
+      "signatureAuthData": {
+        "signature": '',
+        "authData": '',
+      },
+      "search_type": "direct_search",
+      "source": {
+        "lat": currentLocation?.latitude,
+        "lon": currentLocation?.longitude,
+        "name": "Horamavu agara"
+      },
+      "destination": {
+        "lat": 13.0335,
+        "lon": 77.6739,
+        "name": "Kalkere"
+      }
+    }
   }
 
+
+  // const handleClick = () => {
+  //   setModalVisible(false)
+  //   HyperSdkReact.initiate(initiatePayload)
+  //   HyperSdkReact.isInitialised().then((init) => {
+  //     console.log('isInitialised:', init)
+  //   })
+  // }
+  const [loader, setLoader] = useState(false);
+  const handleClick = () => {
+    setModalVisible(false)
+    setLoader(true);
+    if (HyperSdkReact.isNull()) {
+      HyperSdkReact.createHyperServices();
+    }
+
+    HyperSdkReact.initiate(initiatePayload);
+    HyperSdkReact.isInitialised().then((init) => {
+      console.log('isInitialised:', init);
+    });
+  }
+
+  const [signatureResponse, setSignatureResponse] = useState(null); // State to store the signature response
+
   useEffect(() => {
-    const eventEmitter = new NativeEventEmitter(NativeModules.HyperSdkReact)
+    const fetchSignatureResponse = async () => {
+      try {
+        const result = await HyperSDKModule.dynamicSign(userName, mobileNumber, mobileCountryCode);
+        setSignatureResponse(result);
+        console.log("signauth check", result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchSignatureResponse();
+  }, []);
+
+
+  useEffect(() => {
+
+    const processPayload2Copy = { ...processPayload2 }; // Create a copy of the processPayload2 object
+
+    if (signatureResponse) {
+      processPayload2Copy.payload.signatureAuthData.signature = signatureResponse.signature;
+      processPayload2Copy.payload.signatureAuthData.authData = signatureResponse.signatureAuthData;
+
+    }
+    console.log('Updated processPayload2:', processPayload2Copy);
+
+
+    const eventEmitter = new NativeEventEmitter(NativeModules.HyperSdkReact);
     const eventListener = eventEmitter.addListener('HyperEvent', (resp) => {
-      const data = JSON.parse(resp)
-      const event = data.event || ''
+      const data = JSON.parse(resp);
+      const event = data.event || '';
+      console.log('event_call: is called ', event);
+      console.log('data_call: is called ', data);
       switch (event) {
         case 'show_loader':
           // show some loader here
-          break
+          setLoader(true);
+          <ActivityIndicator />
+          break;
+
         case 'hide_loader':
           // hide the loader
-          break
+          setLoader(false);
+          break;
+
         case 'initiate_result':
-          const payload = data.payload || {}
-          const res = payload ? payload.status : payload
-          console.log('initiate_result: ', payload)
+          const payload = data.payload || {};
+          const res = payload ? payload.status : payload;
+          console.log('initiate_result: ', processPayload2);
           if (res === 'SUCCESS') {
             // Initiation is successful, call process method
-            HyperSdkReact.process(processPayload2)
-            console.log('process_call: is called ', payload)
+            if (processPayload2.payload.signatureAuthData != undefined) {
+              HyperSdkReact.process(JSON.stringify(processPayload2));
+            } else {
+              alert('Invalid signature');
+            }
+            // HyperSdkReact.process(JSON.stringify(processPayload2));
+            console.log('process_call: is called ', payload);
           } else {
             // Handle initiation failure
-            console.log('Initiation failed.')
+            console.log('Initiation failed.');
           }
-          break
+          break;
+
         case 'process_result':
-          const processPayload = data.payload || {}
-          console.log('process_result: ', processPayload)
+          const processPayload = data.payload || {};
+          console.log('process_result: ', processPayload);
           // Handle process result
           if (processPayload?.action === 'terminate') {
-            HyperSdkReact.terminate()
-            console.log('process_call: is called ', processPayload)
+            HyperSdkReact.terminate();
+            console.log('process_call: is called ', processPayload);
+            // BackHandler.exitApp();
+          } else if (processPayload?.action === 'trip_completed') {
+            //function call for wallet transaction
+            console.log('process_call: wallet transaction ', processPayload);
+            HyperSdkReact.terminate();
           }
-          break
+
+          break;
+
         default:
-          console.log('Unknown Event', data)
+          console.log('Unknown Event', data);
       }
-    })
+    });
+
     BackHandler.addEventListener('hardwareBackPress', () => {
-      return !HyperSdkReact.isNull() && HyperSdkReact.onBackPressed()
-    })
+      return !HyperSdkReact.isNull() && HyperSdkReact.onBackPressed();
+    });
+
     return () => {
-      eventListener.remove()
-      BackHandler.removeEventListener('hardwareBackPress', () => null)
-    }
-  }, [])
+
+      eventListener.remove();
+      BackHandler.removeEventListener('hardwareBackPress', () => null);
+    };
+  }, [signatureResponse]);
 
   return (
     <View style={{ flex: 1 }}>
