54418f424d0c48dd404a2b831480cef95706a979
tanguyprache <47600889+tanguyprache@users.noreply.github.com>
2023-06-30 09:37:18 +0200

    (PC-22937) feat(video): add error view on video player (#4946)
    * (PC-22937) add error view on player
    
    * (PC-22937) add unit test

---
+++ src/features/home/components/modules/video/VideoErrorView.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { StyleProp, ViewStyle } from 'react-native'
+import styled from 'styled-components/native'
+
+import { HeaderBackground } from 'ui/svg/HeaderBackground'
+import { Spacer, Typo, getSpacing } from 'ui/theme'
+
+export const VideoErrorView: React.FC<{
+  style: StyleProp<ViewStyle>
+}> = ({ style }) => {
+  const errorTitle = 'Oups\u00a0!'
+  const errorMessage = 'Une erreur s’est produite pendant le chargement de la vidéo.'
+
+  return (
+    <VideoPlayerErrorViewContainer style={style}>
+      <HeaderBackground />
+      <ErrorMessageContainer>
+        <StyledTitle>{errorTitle}</StyledTitle>
+        <Spacer.Column numberOfSpaces={4} />
+        <StyledBody>{errorMessage}</StyledBody>
+      </ErrorMessageContainer>
+    </VideoPlayerErrorViewContainer>
+  )
+}
+
+const VideoPlayerErrorViewContainer = styled.View({
+  position: 'absolute',
+  borderTopLeftRadius: getSpacing(4),
+  borderTopRightRadius: getSpacing(4),
+  overflow: 'hidden',
+})
+
+const ErrorMessageContainer = styled.View({
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: getSpacing(6),
+})
+
+const StyledTitle = styled(Typo.Title2)(({ theme }) => ({
+  color: theme.colors.white,
+  textAlign: 'center',
+}))
+
+const StyledBody = styled(Typo.Body)(({ theme }) => ({
+  color: theme.colors.white,
+  textAlign: 'center',
+}))
