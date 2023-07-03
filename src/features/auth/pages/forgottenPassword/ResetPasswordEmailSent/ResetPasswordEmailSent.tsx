2c19853800b1ea2cfe5642775658fa8ee6ce6756
Lucas Beneston <lucas.beneston@passculture.app>
2023-07-03 12:20:46 +0200

    (PC-22847) feat(signup): udpate design for ResetPasswordEmailSent

--- src/features/auth/pages/forgottenPassword/ResetPasswordEmailSent/ResetPasswordEmailSent.tsx
+++ src/features/auth/pages/forgottenPassword/ResetPasswordEmailSent/ResetPasswordEmailSent.tsx
@@ -1,73 +1,32 @@
+import { useNavigation } from '@react-navigation/native'
 import { StackScreenProps } from '@react-navigation/stack'
 import React, { FunctionComponent } from 'react'
-import { Platform } from 'react-native'
-import styled from 'styled-components/native'
 
-import { OpenInboxButton } from 'features/auth/components/OpenInboxButton'
-import { navigateToHome } from 'features/navigation/helpers'
-import { RootStackParamList } from 'features/navigation/RootNavigator/types'
-import { useGoBack } from 'features/navigation/useGoBack'
+import { EmailSentGeneric } from 'features/auth/components/EmailSentGeneric'
+import { RootStackParamList, UseNavigationType } from 'features/navigation/RootNavigator/types'
 import { env } from 'libs/environment'
-import { BottomContentPage } from 'ui/components/BottomContentPage'
-import { ButtonTertiaryPrimary } from 'ui/components/buttons/ButtonTertiaryPrimary'
-import { ModalHeader } from 'ui/components/modals/ModalHeader'
-import { ExternalTouchableLink } from 'ui/components/touchableLink/ExternalTouchableLink'
-import { ArrowPrevious } from 'ui/svg/icons/ArrowPrevious'
-import { Close } from 'ui/svg/icons/Close'
-import { ExternalSite } from 'ui/svg/icons/ExternalSite'
-import { padding, Spacer, Typo } from 'ui/theme'
+import { RightButtonText } from 'ui/components/headers/RightButtonText'
+import { SecondaryPageWithBlurHeader } from 'ui/pages/SecondaryPageWithBlurHeader'
 
 type Props = Pick<StackScreenProps<RootStackParamList, 'ResetPasswordEmailSent'>, 'route'>
 
 export const ResetPasswordEmailSent: FunctionComponent<Props> = ({ route }) => {
-  const { goBack } = useGoBack('Login', undefined)
+  const { navigate } = useNavigation<UseNavigationType>()
+
+  const onClose = () => {
+    navigate('Login', undefined)
+  }
+
   return (
-    <BottomContentPage>
-      <ModalHeader
-        title="E-mail de réinitialisation de mot de passe envoyé&nbsp;!"
-        leftIconAccessibilityLabel="Revenir en arrière"
-        leftIcon={ArrowPrevious}
-        onLeftIconPress={goBack}
-        rightIconAccessibilityLabel="Revenir à l’accueil"
-        rightIcon={Close}
-        onRightIconPress={navigateToHome}
+    <SecondaryPageWithBlurHeader
+      headerTitle="Oubli de mot de passe"
+      shouldDisplayBackButton={false}
+      RightButton={<RightButtonText onClose={onClose} wording="Quitter" />}>
+      <EmailSentGeneric
+        title="Clique sur le lien de réinitialisation reçu par e-mail"
+        consultFaq={{ url: env.FAQ_LINK_RESET_PASSORD_EMAIL_NOT_RECEIVED }}
+        email={route.params.email}
       />
-      <ModalContent>
-        <Description>
-          <Typo.Body>Clique sur le lien reçu à l’adresse&nbsp;:</Typo.Body>
-          <Typo.Body>{route.params.email}</Typo.Body>
-          <Spacer.Column numberOfSpaces={5} />
-          <CenteredText>
-            <Typo.Body>
-              L’e-mail peut prendre quelques minutes à arriver. Pense à vérifier tes spams&nbsp;! Si
-              l’e-mail n’arrive pas, tu peux&nbsp;:
-            </Typo.Body>
-          </CenteredText>
-          <Spacer.Column numberOfSpaces={5} />
-          <ExternalTouchableLink
-            as={ButtonTertiaryPrimary}
-            wording="Consulter l’article d’aide"
-            externalNav={{ url: env.FAQ_LINK_RESET_PASSORD_EMAIL_NOT_RECEIVED }}
-            icon={ExternalSite}
-          />
-        </Description>
-        {Platform.OS !== 'web' && <Spacer.Column numberOfSpaces={5} />}
-        <OpenInboxButton />
-      </ModalContent>
-    </BottomContentPage>
+    </SecondaryPageWithBlurHeader>
   )
 }
-
-const ModalContent = styled.View({
-  ...padding(4, 1),
-  alignItems: 'center',
-  width: '100%',
-})
-
-const Description = styled.View({
-  alignItems: 'center',
-})
-
-const CenteredText = styled(Typo.Body)({
-  textAlign: 'center',
-})
