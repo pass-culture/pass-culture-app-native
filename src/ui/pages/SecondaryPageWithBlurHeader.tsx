4eb10ba43d63403c32ae4ede5ad4dbf34befc2d5
Lucas Beneston <lucas.beneston@passculture.app>
2023-07-03 12:19:47 +0200

    (PC-22847) feat(ui): add RightButton props to SecondaryPageWithBlurHeader

--- src/ui/pages/SecondaryPageWithBlurHeader.tsx
+++ src/ui/pages/SecondaryPageWithBlurHeader.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren, ReactNode } from 'react'
 import styled from 'styled-components/native'
 
 import { BlurHeader } from 'ui/components/headers/BlurHeader'
@@ -12,12 +12,14 @@ type Props = PropsWithChildren<{
   headerTitle: string
   shouldDisplayBackButton?: boolean
   onGoBack?: () => void
+  RightButton?: ReactNode
 }>
 
 export const SecondaryPageWithBlurHeader = ({
   headerTitle,
   shouldDisplayBackButton,
   onGoBack,
+  RightButton,
   children,
 }: Props) => {
   const headerHeight = useGetHeaderHeight()
@@ -28,6 +30,7 @@ export const SecondaryPageWithBlurHeader = ({
         title={headerTitle}
         shouldDisplayBackButton={shouldDisplayBackButton}
         onGoBack={onGoBack}
+        RightButton={RightButton}
       />
       <StyledScrollView>
         <Placeholder height={headerHeight} />
