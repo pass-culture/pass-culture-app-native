6688972074a38bd19d3a454317c81b017d5fcbf3
Lucas Beneston <lucas.beneston@passculture.app>
2023-07-04 12:52:46 +0200

    (PC-22755) feat(trustedDevice): create DeviceInformationsBanner component

---
+++ src/features/trustedDevice/components/DeviceInformationsBanner.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+import { InfoBanner } from 'ui/components/InfoBanner'
+import { Info } from 'ui/svg/icons/Info'
+import { Typo } from 'ui/theme'
+import { LINE_BREAK } from 'ui/theme/constants'
+
+type DeviceInformationsBanner = {
+  location: string
+  osAndSource: string
+  loginDate: string
+}
+
+export const DeviceInformationsBanner: React.FC<DeviceInformationsBanner> = ({
+  osAndSource,
+  location,
+  loginDate,
+}) => (
+  <InfoBanner
+    message={
+      <CaptionRegular>
+        <Typo.Caption>Appareil utilisé&nbsp;: </Typo.Caption> {osAndSource}
+        {LINE_BREAK}
+        <Typo.Caption>Lieu&nbsp;: </Typo.Caption> {location}
+        {LINE_BREAK}
+        <Typo.Caption>Date et heure&nbsp;: </Typo.Caption> {loginDate}
+      </CaptionRegular>
+    }
+    icon={Info}
+  />
+)
+
+const CaptionRegular = styled(Typo.Caption)(({ theme }) => ({
+  fontFamily: theme.fontFamily.regular,
+}))
