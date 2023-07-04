dfa60e13b8e64f76ca696d155eb866646c2c5067
Hugo Voisin <hugov@bam.tech>
2023-07-04 15:22:18 +0200

    test: rename test file

--- eslint-custom-rules/__tests__/apostrophe-in-text.test.js
+++ eslint-custom-rules/__tests__/apostrophe-in-text.test.js
@@ -6,14 +6,8 @@ import rule from '../apostrophe-in-text'
 const ruleTester = new RuleTester()
 
 const tests = {
-    valid: [
-        { code: "const str = 'I’m working'" },
-        { code: "<Text>I’m working</Text>" },
-    ],
-    invalid: [
-        { code: `const str = "I'm failing"` },
-        { code: "<Text>I'm failing</Text>" },
-    ]
+  valid: [{ code: "const str = 'I’m working'" }, { code: '<Text>I’m working</Text>' }],
+  invalid: [{ code: `const str = "I'm failing"` }, { code: "<Text>I'm failing</Text>" }],
 }
 
 tests.valid.forEach((t) => Object.assign(t, config))
