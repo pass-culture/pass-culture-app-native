7214eaad6d2160d7487c09a8cd7799d7a4c38223
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2023-07-05 11:57:03 +0200

    (BSR) feat(prune): fix some prune issues (#4995)
    * (BSR) feat(prune): add .ts-prunerc config
    
    * (BSR) feat(prune): fix some prune issues

--- src/features/bookings/components/ActivationCodeButton.tsx
+++ src/features/bookings/components/ActivationCodeButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { ButtonSecondary } from 'ui/components/buttons/ButtonSecondary'
 
-export interface ActivationCodeButtonProps {
+interface ActivationCodeButtonProps {
   onTerminate?: () => void
   fullWidth?: boolean
 }
