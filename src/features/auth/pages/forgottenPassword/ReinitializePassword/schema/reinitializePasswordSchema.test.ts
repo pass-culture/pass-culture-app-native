42c5b2f24c9408951c66807127c9f8229983c377
marineshaw <marines@bam.tech>
2023-07-07 12:30:12 +0200

    :recycle: (ReinitializePassword) delete describe from schema test

--- src/features/auth/pages/forgottenPassword/ReinitializePassword/schema/reinitializePasswordSchema.test.ts
+++ src/features/auth/pages/forgottenPassword/ReinitializePassword/schema/reinitializePasswordSchema.test.ts
@@ -3,34 +3,28 @@ import { ValidationError } from 'yup'
 import { reinitializePasswordSchema } from './reinitializePasswordSchema'
 
 describe('reinitializePasswordSchema', () => {
-  describe('should validate', () => {
-    it('validate a form with all required values', async () => {
-      const values = {
-        newPassword: 'user@AZERTY123',
-        confirmedPassword: 'user@AZERTY123',
-      }
-      const result = await reinitializePasswordSchema.validate(values)
-      expect(result).toEqual(values)
-    })
+  it('should validate a form with all required values', async () => {
+    const values = {
+      newPassword: 'user@AZERTY123',
+      confirmedPassword: 'user@AZERTY123',
+    }
+    const result = await reinitializePasswordSchema.validate(values)
+    expect(result).toEqual(values)
   })
 
-  describe('should invalidate', () => {
-    it('should fail due to invalid new password', async () => {
-      const result = reinitializePasswordSchema.validate({
-        newPassword: 'userAZERTY123',
-        confirmedPassword: 'userAZERTY123',
-      })
-      await expect(result).rejects.toEqual(new ValidationError('1 Caractère spécial (!@#$%^&*...)'))
+  it('should invalidate due to invalid new password', async () => {
+    const result = reinitializePasswordSchema.validate({
+      newPassword: 'userAZERTY123',
+      confirmedPassword: 'userAZERTY123',
     })
+    await expect(result).rejects.toEqual(new ValidationError('1 Caractère spécial (!@#$%^&*...)'))
+  })
 
-    it('should fail due to non identical newPassword and confirmedPassword', async () => {
-      const result = reinitializePasswordSchema.validate({
-        newPassword: 'user@AZERTY123',
-        confirmedPassword: 'user@AZERTY12',
-      })
-      await expect(result).rejects.toEqual(
-        new ValidationError('Les mots de passe ne concordent pas')
-      )
+  it('should invalidate due to non identical newPassword and confirmedPassword', async () => {
+    const result = reinitializePasswordSchema.validate({
+      newPassword: 'user@AZERTY123',
+      confirmedPassword: 'user@AZERTY12',
     })
+    await expect(result).rejects.toEqual(new ValidationError('Les mots de passe ne concordent pas'))
   })
 })
