b5d534f9fe47b21de6a1beacdb09cd410638456d
cedricls1 <78556024+cedricls1@users.noreply.github.com>
2023-07-11 15:52:02 +0200

    (PC-22580) feat(profile): add API call to confirm change email (#5019)
    * (PC-22580) feat(profile): add API call to confirm change email
    
    * (PC-22580) feat(profile): fix accessibility test
    
    * (PC-22580) refactor(profile): simplify routing
    
    * (PC-22580) refactor(profile): add missing useEffect
    
    * (PC-22580) refactor(profile): improve tests
    
    ---------
    
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

--- src/features/profile/pages/ConfirmChangeEmail/ConfirmChangeEmail.tsx
+++ src/features/profile/pages/ConfirmChangeEmail/ConfirmChangeEmail.tsx
@@ -1,29 +1,54 @@
-import { useNavigation } from '@react-navigation/native'
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { NativeStackScreenProps } from 'react-native-screens/native-stack'
 
+import { api } from 'api/api'
 import { navigateToHome, navigateToHomeConfig } from 'features/navigation/helpers'
-import { UseNavigationType } from 'features/navigation/RootNavigator/types'
-import { useCheckHasCurrentEmailChange } from 'features/profile/helpers/useCheckHasCurrentEmailChange'
+import { RootStackParamList } from 'features/navigation/RootNavigator/types'
+import { useEmailUpdateStatus } from 'features/profile/helpers/useEmailUpdateStatus'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
 import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
 import { GenericInfoPageWhite } from 'ui/pages/GenericInfoPageWhite'
 import { BicolorPhonePending } from 'ui/svg/icons/BicolorPhonePending'
 import { Invalidate } from 'ui/svg/icons/Invalidate'
 import { Spacer, Typo } from 'ui/theme'
 
-export function ConfirmChangeEmail() {
-  const { hasCurrentEmailChange } = useCheckHasCurrentEmailChange()
-  const { navigate } = useNavigation<UseNavigationType>()
+type ConfirmChangeEmailProps = NativeStackScreenProps<RootStackParamList, 'ConfirmChangeEmail'>
 
-  if (!hasCurrentEmailChange) {
-    navigateToHome()
-  }
+export function ConfirmChangeEmail({ route: { params }, navigation }: ConfirmChangeEmailProps) {
+  const emailUpdateStatus = useEmailUpdateStatus()
+  const { showErrorSnackBar } = useSnackBarContext()
 
-  const onConfirmEmail = useCallback(() => {
-    // TODO(yassinL) complete with back-end route
-    navigate('TrackEmailChange')
-  }, [navigate])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    if (emailUpdateStatus?.data?.expired) {
+      navigateToHome()
+    }
+  }, [emailUpdateStatus.data?.expired])
+
+  const mutate = useCallback(async () => {
+    return api.postnativev1profileemailUpdateconfirm({
+      token: params.token,
+    })
+  }, [params.token])
+
+  const onConfirmEmail = useCallback(async () => {
+    setIsLoading(true)
+    try {
+      await mutate()
+      navigation.navigate('TrackEmailChange')
+    } catch (err) {
+      showErrorSnackBar({
+        message: 'Désolé, une erreur technique s’est produite. Veuillez réessayer plus tard.',
+        timeout: SNACK_BAR_TIME_OUT,
+      })
+      navigateToHome()
+    } finally {
+      setIsLoading(false)
+    }
+  }, [mutate, navigation, showErrorSnackBar])
 
   return (
     <React.Fragment>
@@ -38,6 +63,7 @@ export function ConfirmChangeEmail() {
           wording="Confirmer la demande"
           accessibilityLabel="Confirmer la demande"
           onPress={onConfirmEmail}
+          disabled={isLoading}
         />
         <Spacer.Column numberOfSpaces={4} />
         <InternalTouchableLink
@@ -45,6 +71,7 @@ export function ConfirmChangeEmail() {
           wording="Fermer"
           navigateTo={navigateToHomeConfig}
           icon={Invalidate}
+          disabled={isLoading}
         />
       </GenericInfoPageWhite>
     </React.Fragment>
--- src/features/profile/pages/ConfirmChangeEmail/ConfirmChangeEmail.web.test.tsx
+++ src/features/profile/pages/ConfirmChangeEmail/ConfirmChangeEmail.web.test.tsx
@@ -1,17 +1,40 @@
+import { RouteProp } from '@react-navigation/native'
 import React from 'react'
+import { NativeStackNavigationProp } from 'react-native-screens/native-stack'
 
-import * as useCheckHasCurrentEmailChange from 'features/profile/helpers/useCheckHasCurrentEmailChange'
+import { EmailHistoryEventTypeEnum } from 'api/gen'
+import { RootStackParamList } from 'features/navigation/RootNavigator/types'
+import * as useEmailUpdateStatus from 'features/profile/helpers/useEmailUpdateStatus'
 import { ConfirmChangeEmail } from 'features/profile/pages/ConfirmChangeEmail/ConfirmChangeEmail'
 import { checkAccessibilityFor, render } from 'tests/utils/web'
 
-jest.spyOn(useCheckHasCurrentEmailChange, 'useCheckHasCurrentEmailChange').mockReturnValue({
-  hasCurrentEmailChange: true,
-})
+type UseEmailUpdateStatusMock = ReturnType<typeof useEmailUpdateStatus['useEmailUpdateStatus']>
+
+jest.spyOn(useEmailUpdateStatus, 'useEmailUpdateStatus').mockReturnValue({
+  data: {
+    expired: false,
+    newEmail: '',
+    status: EmailHistoryEventTypeEnum.UPDATE_REQUEST,
+  },
+  isLoading: false,
+} as UseEmailUpdateStatusMock)
+jest.mock('react-query')
 
 describe('<ConfirmChangeEmail />', () => {
+  const navigation = {
+    navigate: jest.fn(),
+  } as unknown as NativeStackNavigationProp<RootStackParamList, 'ConfirmChangeEmail'>
+
+  const route = {
+    params: {
+      token: 'example',
+    },
+  } as unknown as RouteProp<RootStackParamList, 'ConfirmChangeEmail'>
+
   describe('Accessibility', () => {
     it('should not have basic accessibility issues', async () => {
-      const { container } = render(<ConfirmChangeEmail />)
+      const { container } = render(<ConfirmChangeEmail navigation={navigation} route={route} />)
+
       const results = await checkAccessibilityFor(container)
       expect(results).toHaveNoViolations()
     })
