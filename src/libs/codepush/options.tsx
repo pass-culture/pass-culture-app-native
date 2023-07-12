f59cda60b344a4984532a4aaf583e112842ddec1
Hugo Voisin <hugov@bam.tech>
2023-07-12 15:51:35 +0200

    chore(codepush): check update and install on next resume to improve install speed and rate
    Co-authored-by: Ali RAIKI <aliraiki@users.noreply.github.com>
    Co-authored-by: anoukhello <anoukhello@users.noreply.github.com>

--- src/libs/codepush/options.tsx
+++ src/libs/codepush/options.tsx
@@ -8,10 +8,12 @@ export const AutoImmediate: CodePushOptions = {
 }
 
 // for production environment, we want a more discreet update
-// On each app start, we check for update, that we install on
-// next app start. This means the app doesn't flicker and the update
+// On each app start/resume, we check for update, that we install on
+// next app start/resume. This means the app doesn't flicker and the update
 // is not visible to the user.
-export const NextRestart: CodePushOptions = {
-  installMode: CodePush.InstallMode.ON_NEXT_RESTART,
-  checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+export const NextResume: CodePushOptions = {
+  installMode: CodePush.InstallMode.ON_NEXT_RESUME,
+  // Warning : CodePush API usage has a rate limitation of 8 requests per 5 minutes applied.
+  // https://learn.microsoft.com/en-us/appcenter/distribution/codepush/
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
 }
