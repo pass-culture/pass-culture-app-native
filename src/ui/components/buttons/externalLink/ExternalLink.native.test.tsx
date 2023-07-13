0f2c18859d5573883418164136d3a751440825bf
marineshaw <marines@bam.tech>
2023-07-13 15:25:34 +0200

    :bug: (ExternalLink) Remove useless tests and fix act warning

--- src/ui/components/buttons/externalLink/ExternalLink.native.test.tsx
+++ src/ui/components/buttons/externalLink/ExternalLink.native.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Linking } from 'react-native'
 
-import { fireEvent, render, screen, waitFor } from 'tests/utils'
+import { act, fireEvent, render, screen } from 'tests/utils'
 
 import { ExternalLink } from './ExternalLink'
 
@@ -9,24 +9,20 @@ const openURLSpy = jest.spyOn(Linking, 'openURL')
 const someUrl = 'https://domain-that-does-not-exist.fr'
 
 describe('ExternalLink', () => {
-  afterEach(() => {
-    openURLSpy.mockClear()
-  })
-
   it('should open given url when text clicked', async () => {
     render(<ExternalLink url={someUrl} />)
 
-    fireEvent.press(screen.getByText('\u00a0' + someUrl))
+    await act(async () =>
+      fireEvent.press(screen.getByLabelText('Nouvelle fenêtre\u00a0: ' + someUrl))
+    )
 
-    await waitFor(() => {
-      expect(openURLSpy).toHaveBeenNthCalledWith(1, someUrl)
-    })
+    expect(openURLSpy).toHaveBeenNthCalledWith(1, someUrl)
   })
 
   it('should open given url when text clicked and text not matching url', () => {
     render(<ExternalLink text="anchor text" url={someUrl} />)
 
-    fireEvent.press(screen.getByText('\u00a0anchor'))
+    fireEvent.press(screen.getByLabelText('Nouvelle fenêtre\u00a0: anchor text'))
 
     expect(openURLSpy).toHaveBeenNthCalledWith(1, someUrl)
   })
@@ -37,18 +33,5 @@ describe('ExternalLink', () => {
     fireEvent.press(screen.getByTestId('externalSiteIcon'))
 
     expect(openURLSpy).toHaveBeenNthCalledWith(1, someUrl)
-  })
-
-  it('should display url with non breaking space when no text provided', () => {
-    render(<ExternalLink url={someUrl} />)
-
-    expect(screen.queryByText('\u00a0' + someUrl)).toBeTruthy()
-  })
-
-  it('should display splitted text with non breaking space on first word', () => {
-    render(<ExternalLink text="some text with several words" url={someUrl} />)
-
-    expect(screen.queryByText('\u00a0some')).toBeTruthy()
-    expect(screen.queryByText(' text with several words')).toBeTruthy()
   })
 })
