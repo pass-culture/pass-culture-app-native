1c5d55a453c64787b325bb938e340c0d800d603d
Hugo Voisin <hugov@bam.tech>
2023-07-17 10:19:30 +0200

    chore: add a 10mn limit before installing CodePush to avoid the app to restart when checking a SMS or a mail

--- src/libs/codepush/options.tsx
+++ src/libs/codepush/options.tsx
@@ -7,12 +7,15 @@ export const AutoImmediate: CodePushOptions = {
   checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
 }
 
+const TEN_MINUTES = 60 * 10
+
 // for production environment, we want a more discreet update
 // On each app start/resume, we check for update, that we install on
 // next app start/resume. This means the app doesn't flicker and the update
 // is not visible to the user.
 export const NextResume: CodePushOptions = {
   installMode: CodePush.InstallMode.ON_NEXT_RESUME,
+  minimumBackgroundDuration: TEN_MINUTES,
   // Warning : CodePush API usage has a rate limitation of 8 requests per 5 minutes applied.
   // https://learn.microsoft.com/en-us/appcenter/distribution/codepush/
   checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
