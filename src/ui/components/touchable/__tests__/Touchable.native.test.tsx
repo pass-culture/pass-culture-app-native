4422219f59255f3711f7b39cab2c832c77eea727
marineshaw <marines@bam.tech>
2023-07-17 10:42:00 +0200

    :fire: (Tests) Remove snapshot test for component to match test strategy
    :construction:

--- src/ui/components/touchable/__tests__/Touchable.native.test.tsx
+++ src/ui/components/touchable/__tests__/Touchable.native.test.tsx
@@ -6,26 +6,6 @@ import { fireEvent, render } from 'tests/utils'
 import { Touchable } from '../Touchable'
 
 describe('<Touchable />', () => {
-  it('should render correctly', () => {
-    const handleClick = jest.fn()
-    const renderTouchable = render(
-      <Touchable onPress={handleClick}>
-        <Text>Touchable content</Text>
-      </Touchable>
-    )
-    expect(renderTouchable).toMatchSnapshot()
-  })
-
-  it('should render correctly on custom type', () => {
-    const handleClick = jest.fn()
-    const renderTouchable = render(
-      <Touchable type="reset" onPress={handleClick}>
-        <Text>Touchable content</Text>
-      </Touchable>
-    )
-    expect(renderTouchable).toMatchSnapshot()
-  })
-
   it('should execute callback on press', () => {
     const handleClick = jest.fn()
     const { getByRole } = render(
