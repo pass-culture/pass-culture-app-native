12990a857350e0f16884bf9df52c3156716ba07a
Hugo Voisin <hugov@bam.tech>
2023-07-18 15:36:22 +0200

    refactor: contract condition to improve readability

--- src/libs/algolia/fetchAlgolia/buildAlgoliaParameters/buildFacetFilters.ts
+++ src/libs/algolia/fetchAlgolia/buildAlgoliaParameters/buildFacetFilters.ts
@@ -80,8 +80,7 @@ export const buildFacetFilters = ({
   if (tagsPredicate) facetFilters.push(tagsPredicate)
 
   if (
-    locationFilter &&
-    locationFilter.locationType === LocationType.VENUE &&
+    locationFilter?.locationType === LocationType.VENUE &&
     typeof locationFilter.venue.venueId === 'number'
   )
     facetFilters.push([`${FACETS_FILTERS_ENUM.VENUE_ID}:${locationFilter.venue.venueId}`])
