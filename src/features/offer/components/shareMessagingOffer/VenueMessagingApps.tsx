7afee94fafcc1b4527927bc09d7bc1c553053886
marineshaw <marines@bam.tech>
2023-07-19 11:15:52 +0200

    :sparkles: (MessagingApps) Create VenueMessagingApps component

---
+++ src/features/offer/components/shareMessagingOffer/VenueMessagingApps.tsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from 'react'
+import { Social } from 'react-native-share'
+
+import { MessagingApps } from 'features/offer/components/shareMessagingOffer/MessagingApps'
+import { useShareVenue } from 'features/share/helpers/useShareVenue'
+import { analytics } from 'libs/analytics'
+
+type MessagingAppsProps = {
+  venueId: number
+}
+
+export const VenueMessagingApps = ({ venueId }: MessagingAppsProps) => {
+  const { share, shareContent } = useShareVenue(venueId)
+
+  const messagingAppAnalytics = useCallback(
+    (social: Social | 'Other') => {
+      analytics.logShare({ type: 'Venue', id: venueId, from: 'venue', social })
+    },
+    [venueId]
+  )
+
+  if (!shareContent || !shareContent.url) return null
+
+  return (
+    <MessagingApps
+      title="Vas-y en bande organisée&nbsp;!"
+      shareContent={shareContent}
+      share={share}
+      messagingAppAnalytics={messagingAppAnalytics}
+    />
+  )
+}
