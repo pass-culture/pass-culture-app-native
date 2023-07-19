fa262e5f812d3fd33c0c0059919e5df66f23815d
Tanguy <tanguy.prache@passculture.app>
2023-07-19 16:59:15 +0200

    ui: add illustration size
    Co-authored-by: thconte <thconte@users.noreply.github.com>
    Co-authored-by: S4andro <S4andro@users.noreply.github.com>
    Co-authored-by: Hugo Voisin <hugov@bam.tech>
    Co-authored-by: anoukhello <anoukhello@users.noreply.github.com>

--- src/ui/theme/illustrationSizes.ts
+++ src/ui/theme/illustrationSizes.ts
@@ -1,11 +1,17 @@
-import { FULLPAGE_ILLUSTRATION_ICON_SIZE, ILLUSTRATION_ICON_SIZE } from './constants'
+import {
+  FULLPAGE_ILLUSTRATION_ICON_SIZE,
+  ILLUSTRATION_ICON_SIZE,
+  ILLUSTRATION_SMALL_SIZE,
+} from './constants'
 
 export type IllustrationSizesType = {
   fullPage: number
   medium: number
+  small: number
 }
 
 export const illustrationSizes: IllustrationSizesType = {
   fullPage: FULLPAGE_ILLUSTRATION_ICON_SIZE,
   medium: ILLUSTRATION_ICON_SIZE,
+  small: ILLUSTRATION_SMALL_SIZE,
 }
