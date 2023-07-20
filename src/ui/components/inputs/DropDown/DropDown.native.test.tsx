51dd288ac4684b7b99b5b41f6f86c365338025f4
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2023-07-20 17:09:50 +0200

    (PC-23208) feat(accessibility): change placeholder for DateInputDesktop month (#5061)

--- src/ui/components/inputs/DropDown/DropDown.native.test.tsx
+++ src/ui/components/inputs/DropDown/DropDown.native.test.tsx
@@ -7,7 +7,7 @@ import { DropDown } from 'ui/components/inputs/DropDown/DropDown'
 describe('<DropDown />', () => {
   it('should not render component when is native', () => {
     const renderAPI = render(
-      <DropDown label="Mois" placeholder="MM" options={monthNames} onChange={jest.fn()} />
+      <DropDown label="Mois" placeholder="Mois" options={monthNames} onChange={jest.fn()} />
     )
     expect(renderAPI.toJSON()).toBeNull()
   })
