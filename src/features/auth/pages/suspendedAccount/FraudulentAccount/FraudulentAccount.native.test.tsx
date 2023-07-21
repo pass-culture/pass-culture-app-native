ac0056259604daecf67f38f7265f7f5ef4c9b7bf
marineshaw <marines@bam.tech>
2023-07-21 11:41:20 +0200

    :recycle: (SuspendedAccount) Use generic component for fraudulentAccount and SuspensionConfirmation

--- src/features/auth/pages/suspendedAccount/FraudulentAccount/FraudulentAccount.native.test.tsx
+++ src/features/auth/pages/suspendedAccount/FraudulentAccount/FraudulentAccount.native.test.tsx
@@ -1,55 +1,15 @@
 import React from 'react'
 
-import { navigate } from '__mocks__/@react-navigation/native'
-import { navigateToHomeConfig, openUrl } from 'features/navigation/helpers'
-import { env } from 'libs/environment'
-import { fireEvent, render, screen, waitFor } from 'tests/utils'
+import { render, screen } from 'tests/utils'
 
 import { FraudulentAccount } from './FraudulentAccount'
 
-jest.mock('features/navigation/helpers')
-
-const mockSignOut = jest.fn()
-jest.mock('features/auth/helpers/useLogoutRoutine', () => ({
-  useLogoutRoutine: jest.fn(() => mockSignOut.mockResolvedValueOnce(jest.fn())),
-}))
-
-const mockedOpenUrl = openUrl as jest.MockedFunction<typeof openUrl>
+jest.mock('features/auth/helpers/useLogoutRoutine')
 
 describe('<FraudulentAccount />', () => {
   it('should match snapshot', () => {
     render(<FraudulentAccount />)
 
     expect(screen).toMatchSnapshot()
-  })
-
-  it('should open mail app when clicking on contact service button', () => {
-    render(<FraudulentAccount />)
-
-    const contactFraudButton = screen.getByText('Contacter le service')
-    fireEvent.press(contactFraudButton)
-
-    expect(mockedOpenUrl).toHaveBeenNthCalledWith(
-      1,
-      `mailto:${env.FRAUD_EMAIL_ADDRESS}`,
-      undefined,
-      true
-    )
-  })
-
-  it('should go to home page when clicking on go to home button', async () => {
-    render(<FraudulentAccount />)
-
-    const homeButton = screen.getByText('Retourner à l’accueil')
-    fireEvent.press(homeButton)
-
-    await waitFor(() => {
-      expect(navigate).toHaveBeenNthCalledWith(
-        1,
-        navigateToHomeConfig.screen,
-        navigateToHomeConfig.params
-      )
-      expect(mockSignOut).toBeCalledTimes(1)
-    })
   })
 })
--- src/features/trustedDevice/pages/SuspensionConfirmation.tsx
+++ src/features/trustedDevice/pages/SuspensionConfirmation.tsx
@@ -1,57 +1,18 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
-import { useLogoutRoutine } from 'features/auth/helpers/useLogoutRoutine'
-import { navigateToHomeConfig } from 'features/navigation/helpers'
-import { env } from 'libs/environment'
-import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
-import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
-import { ExternalTouchableLink } from 'ui/components/touchableLink/ExternalTouchableLink'
-import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
-import { GenericInfoPage } from 'ui/pages/GenericInfoPage'
-import { Email } from 'ui/svg/icons/Email'
-import { PlainArrowPrevious } from 'ui/svg/icons/PlainArrowPrevious'
-import { UserBlocked } from 'ui/svg/icons/UserBlocked'
+import { GenericSuspendedAccount } from 'features/auth/pages/suspendedAccount/GenericSuspendedAccount/GenericSuspendedAccount'
 import { Spacer, Typo } from 'ui/theme'
 
-export const SuspensionConfirmation = () => {
-  const signOut = useLogoutRoutine()
-
-  return (
-    <GenericInfoPage
-      title="Ton compte a été suspendu"
-      icon={UserBlocked}
-      buttons={[
-        <ExternalTouchableLink
-          key={1}
-          as={ButtonPrimaryWhite}
-          wording="Contacter le service fraude"
-          accessibilityLabel="Ouvrir le gestionnaire mail pour contacter le service fraude"
-          externalNav={{ url: `mailto:${env.FRAUD_EMAIL_ADDRESS}` }}
-          icon={Email}
-        />,
-        <InternalTouchableLink
-          key={2}
-          as={ButtonTertiaryWhite}
-          wording="Retourner à l’accueil"
-          navigateTo={{ ...navigateToHomeConfig, params: { ...navigateToHomeConfig.params } }}
-          onBeforeNavigate={signOut}
-          icon={PlainArrowPrevious}
-        />,
-      ]}>
-      <TextContainer>
-        <StyledBody>En raison d’une activité suspicieuse, ton compte a été suspendu.</StyledBody>
-        <Spacer.Column numberOfSpaces={5} />
-        <StyledBody>
-          Si tu souhaites revoir cette décision, tu peux contacter le service fraude.
-        </StyledBody>
-      </TextContainer>
-    </GenericInfoPage>
-  )
-}
-const TextContainer = styled.View({
-  width: '100%',
-})
+export const SuspensionConfirmation = () => (
+  <GenericSuspendedAccount>
+    <StyledBody>En raison d’une activité suspicieuse, ton compte a été suspendu.</StyledBody>
+    <Spacer.Column numberOfSpaces={5} />
+    <StyledBody>
+      Si tu souhaites revoir cette décision, tu peux contacter le service fraude.
+    </StyledBody>
+  </GenericSuspendedAccount>
+)
 
 const StyledBody = styled(Typo.Body)(({ theme }) => ({
   color: theme.colors.white,
