8dd49c146c4e9624e60e2b27d1b187c7be8de4b0
Hugo Voisin <hugo.voisin@passculture.app>
2023-07-26 14:19:30 +0200

    (PC-23218) chore(codepush): check update and install on next resume (#5028)
    * chore(codepush): check update and install on next resume
    to improve install speed and rate
    
    Co-authored-by: Ali RAIKI <aliraiki@users.noreply.github.com>
    Co-authored-by: anoukhello <anoukhello@users.noreply.github.com>
    
    * chore: add a 10mn limit before installing CodePush
    to avoid the app to restart when checking a SMS or a mail
    
    ---------
    
    Co-authored-by: Ali RAIKI <aliraiki@users.noreply.github.com>
    Co-authored-by: anoukhello <anoukhello@users.noreply.github.com>

--- src/libs/codepush/options.tsx
+++ src/libs/codepush/options.tsx
@@ -7,11 +7,16 @@ export const AutoImmediate: CodePushOptions = {
   checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
 }
 
+const TEN_MINUTES = 60 * 10
+
 // for production environment, we want a more discreet update
-// On each app start, we check for update, that we install on
-// next app start. This means the app doesn't flicker and the update
+// On each app start/resume, we check for update, that we install on
+// next app start/resume. This means the app doesn't flicker and the update
 // is not visible to the user.
-export const NextRestart: CodePushOptions = {
-  installMode: CodePush.InstallMode.ON_NEXT_RESTART,
-  checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+export const NextResume: CodePushOptions = {
+  installMode: CodePush.InstallMode.ON_NEXT_RESUME,
+  minimumBackgroundDuration: TEN_MINUTES,
+  // Warning : CodePush API usage has a rate limitation of 8 requests per 5 minutes applied.
+  // https://learn.microsoft.com/en-us/appcenter/distribution/codepush/
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
 }
