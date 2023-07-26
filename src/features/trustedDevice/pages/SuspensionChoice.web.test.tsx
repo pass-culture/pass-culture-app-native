1b80414f9cf20efcd5ce4dae9a212b747d08054e
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2023-07-26 10:33:36 +0200

    (PC-22834) feat(trustedDevice): add account suspension for Suspension… (#5069)
    * (PC-22834) feat(trustedDevice): add account suspension for SuspensionChoice
    
    * (PC-22834) feat(TrustedDevice): add log sentry on error for SuspensionChoice
    
    * (PC-22834) feat(TrustedDevice): add useCallback for onPressContinue in SuspensionChoice

--- src/features/trustedDevice/pages/SuspensionChoice.web.test.tsx
+++ src/features/trustedDevice/pages/SuspensionChoice.web.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { render, checkAccessibilityFor } from 'tests/utils/web'
 
 import { SuspensionChoice } from './SuspensionChoice'
@@ -7,7 +8,10 @@ import { SuspensionChoice } from './SuspensionChoice'
 describe('<SuspensionChoice/>', () => {
   describe('Accessibility', () => {
     it('should not have basic accessibility issues', async () => {
-      const { container } = render(<SuspensionChoice />)
+      const { container } = render(<SuspensionChoice />, {
+        // eslint-disable-next-line local-rules/no-react-query-provider-hoc
+        wrapper: ({ children }) => reactQueryProviderHOC(children),
+      })
 
       const results = await checkAccessibilityFor(container)
 
