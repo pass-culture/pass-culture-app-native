9bae4c54faf15536d6ffc4321cd7424bd4427b7a
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2023-07-28 10:35:03 +0200

    (PC-23546) feat(prune): update check_dead_code_introduced script (#5086)

--- scripts/check_dead_code_introduced.sh
+++ scripts/check_dead_code_introduced.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
-current_dead_code_count=$(yarn --silent prune:count)
+last_common_commit=$(git merge-base origin/master @)
+git switch --detach $last_common_commit
+yarn --silent prune:scan > scripts/dead_code_master.txt
+git switch -
+yarn --silent prune:scan > scripts/dead_code_current.txt
 
-git fetch origin
-git switch master
-master_dead_code_count=$(yarn --silent prune:count)
+current_dead_code_count=$(wc -l < scripts/dead_code_current.txt)
 
-echo "$current_dead_code_count lines of dead code, $master_dead_code_count on master"
+master_dead_code_count=$(wc -l < scripts/dead_code_master.txt)
 
-if [ "$current_dead_code_count" -gt "$master_dead_code_count" ];
-then
-echo "Error, you added dead code, please remove some."
-exit 1
+diff_dead_code_count=$((current_dead_code_count - master_dead_code_count))
+
+function display_differences_between_dead_code() {
+    diff --color=auto --unified scripts/dead_code_master.txt scripts/dead_code_current.txt
+}
+
+if [ "$diff_dead_code_count" -lt 0 ]; then
+    echo "✅ Le code mort a diminué :"
+    display_differences_between_dead_code
+elif [ "$diff_dead_code_count" -gt 0 ]; then
+    echo "❌ Le code mort a augmenté :"
+    display_differences_between_dead_code
+    exit 1
+else
+    echo "⬜ Le code mort semble inchangé."
+    display_differences_between_dead_code
 fi
