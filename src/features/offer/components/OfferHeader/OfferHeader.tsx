fe6059e2011e6b190b1318c7f32dfcfd52621916
Tanguy <tanguy.prache@passculture.app>
2023-08-02 15:14:05 +0200

    (pc-23136) refacto: extract FavoriteButton

--- src/features/offer/components/OfferHeader/OfferHeader.tsx
+++ src/features/offer/components/OfferHeader/OfferHeader.tsx
@@ -1,29 +1,18 @@
-import { useRoute } from '@react-navigation/native'
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Animated, Platform } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import styled, { useTheme } from 'styled-components/native'
 
-import { useAuthContext } from 'features/auth/context/AuthContext'
-import { useAddFavorite, useFavorite, useRemoveFavorite } from 'features/favorites/api'
-import { FavoriteListOfferModal } from 'features/favorites/favoriteList/FakeDoor/FavoriteListOfferModal'
-import { FavoriteListSurveyModal } from 'features/favorites/favoriteList/FakeDoor/FavoriteListSurveyModal'
-import { UseRouteType } from 'features/navigation/RootNavigator/types'
 import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
 import { useGoBack } from 'features/navigation/useGoBack'
-import { SignUpSignInChoiceOfferModal } from 'features/offer/components/SignUpSignInChoiceOfferModal/SignUpSignInChoiceOfferModal'
 import { useShareOffer } from 'features/share/helpers/useShareOffer'
 import { WebShareModal } from 'features/share/pages/WebShareModal'
 import { analytics } from 'libs/analytics'
-import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
-import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
-import { storage } from 'libs/storage'
-import { accessibleCheckboxProps } from 'shared/accessibilityProps/accessibleCheckboxProps'
 import { getAnimationState } from 'ui/animations/helpers/getAnimationState'
+import { FavoriteButton } from 'ui/components/buttons/FavoriteButton'
 import { RoundedButton } from 'ui/components/buttons/RoundedButton'
 import { AnimatedBlurHeader } from 'ui/components/headers/AnimatedBlurHeader'
 import { useModal } from 'ui/components/modals/useModal'
-import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
 import { Spacer, Typo } from 'ui/theme'
 
 interface Props {
@@ -38,91 +27,26 @@ interface Props {
  */
 export const OfferHeader: React.FC<Props> = (props) => {
   const { headerTransition, offerId, title } = props
-  const { isLoggedIn } = useAuthContext()
   const theme = useTheme()
-  const {
-    visible: signInModalVisible,
-    showModal: showSignInModal,
-    hideModal: hideSignInModal,
-  } = useModal(false)
+
   const {
     visible: shareOfferModalVisible,
     showModal: showShareOfferModal,
     hideModal: hideShareOfferModal,
   } = useModal(false)
-  const {
-    visible: FavoriteListOfferModalVisible,
-    showModal: showFavoriteListOfferModal,
-    hideModal: hideFavoriteListOfferModal,
-  } = useModal(false)
-  const {
-    visible: isFavoriteListSurveyModalVisible,
-    showModal: showFavoriteListSurveyModal,
-    hideModal: hideFavoriteListSurveyModal,
-  } = useModal()
 
   const { goBack } = useGoBack(...getTabNavConfig('Search'))
   const { share: shareOffer, shareContent } = useShareOffer(offerId)
-  const { params } = useRoute<UseRouteType<'Offer'>>()
-  const favorite = useFavorite({ offerId })
-  const { showErrorSnackBar } = useSnackBarContext()
   const { top } = useSafeAreaInsets()
   const headerHeight = theme.appBarHeight + top
-  const isFavListFakeDoorEnabled = useFeatureFlag(RemoteStoreFeatureFlags.FAV_LIST_FAKE_DOOR)
 
   const [ariaHiddenTitle, setAriaHiddenTitle] = useState(true)
   headerTransition.addListener((opacity) => setAriaHiddenTitle(opacity.value !== 1))
-
-  const { mutate: addFavorite, isLoading: addFavoriteIsLoading } = useAddFavorite({
-    onSuccess: () => {
-      if (typeof offerId === 'number') {
-        const { from, moduleName, moduleId, searchId } = params
-        analytics.logHasAddedOfferToFavorites({ from, offerId, moduleName, moduleId, searchId })
-      }
-    },
-  })
-
-  const { mutate: removeFavorite, isLoading: removeFavoriteIsLoading } = useRemoveFavorite({
-    onError: () => {
-      showErrorSnackBar({
-        message: 'L’offre n’a pas été retirée de tes favoris',
-        timeout: SNACK_BAR_TIME_OUT,
-      })
-    },
-  })
 
   const { animationState, containerStyle, blurContainerNative } = getAnimationState(
     theme,
     headerTransition
   )
-  const scaleFavoriteIconAnimatedValueRef = useRef(new Animated.Value(1))
-
-  const pressFavorite = useCallback(async () => {
-    if (!isLoggedIn) {
-      showSignInModal()
-    } else if (favorite) {
-      removeFavorite(favorite.id)
-    } else {
-      animateIcon(scaleFavoriteIconAnimatedValueRef.current)
-      addFavorite({ offerId })
-      if (isFavListFakeDoorEnabled) {
-        const hasSeenFavListFakeDoor = await storage.readObject('has_seen_fav_list_fake_door')
-        if (!hasSeenFavListFakeDoor) {
-          analytics.logFavoriteListDisplayed('offer')
-          showFavoriteListOfferModal()
-        }
-      }
-    }
-  }, [
-    addFavorite,
-    favorite,
-    isFavListFakeDoorEnabled,
-    isLoggedIn,
-    offerId,
-    removeFavorite,
-    showFavoriteListOfferModal,
-    showSignInModal,
-  ])
 
   const pressShareOffer = useCallback(() => {
     analytics.logShare({ type: 'Offer', from: 'offer', id: offerId })
@@ -165,16 +89,7 @@ export const OfferHeader: React.FC<Props> = (props) => {
             finalColor={theme.colors.black}
           />
           <Spacer.Row numberOfSpaces={3} />
-          <RoundedButton
-            animationState={animationState}
-            scaleAnimatedValue={scaleFavoriteIconAnimatedValueRef.current}
-            initialColor={favorite ? theme.colors.primary : undefined}
-            finalColor={favorite ? theme.colors.primary : theme.colors.black}
-            iconName={favorite ? 'favorite-filled' : 'favorite'}
-            onPress={pressFavorite}
-            disabled={removeFavoriteIsLoading || addFavoriteIsLoading}
-            {...accessibleCheckboxProps({ checked: !!favorite, label: 'Mettre en favoris' })}
-          />
+          <FavoriteButton animationState={animationState} offerId={offerId} />
           <Spacer.Row numberOfSpaces={6} />
         </Row>
         <Spacer.Column numberOfSpaces={2} />
@@ -187,37 +102,8 @@ export const OfferHeader: React.FC<Props> = (props) => {
           dismissModal={hideShareOfferModal}
         />
       ) : null}
-      <SignUpSignInChoiceOfferModal
-        visible={signInModalVisible}
-        offerId={offerId}
-        dismissModal={hideSignInModal}
-      />
-      <FavoriteListOfferModal
-        visible={FavoriteListOfferModalVisible}
-        hideModal={hideFavoriteListOfferModal}
-        showSurveyModal={showFavoriteListSurveyModal}
-      />
-      <FavoriteListSurveyModal
-        visible={isFavoriteListSurveyModalVisible}
-        hideModal={hideFavoriteListSurveyModal}
-      />
     </React.Fragment>
   )
-}
-
-function animateIcon(animatedValue: Animated.Value): void {
-  Animated.sequence([
-    Animated.timing(animatedValue, {
-      toValue: 1.3,
-      duration: 200,
-      useNativeDriver: false,
-    }),
-    Animated.timing(animatedValue, {
-      toValue: 1,
-      duration: 200,
-      useNativeDriver: false,
-    }),
-  ]).start()
 }
 
 const HeaderContainer = styled(Animated.View)<{ height: number }>(({ theme, height }) => ({
---
+++ src/ui/components/buttons/FavoriteButton.tsx
@@ -0,0 +1,146 @@
+import { useRoute } from '@react-navigation/native'
+import React, { useCallback, useRef } from 'react'
+import { Animated } from 'react-native'
+
+import { useAuthContext } from 'features/auth/context/AuthContext'
+import { useAddFavorite, useFavorite, useRemoveFavorite } from 'features/favorites/api'
+import { FavoriteListOfferModal } from 'features/favorites/favoriteList/FakeDoor/FavoriteListOfferModal'
+import { FavoriteListSurveyModal } from 'features/favorites/favoriteList/FakeDoor/FavoriteListSurveyModal'
+import { UseRouteType } from 'features/navigation/RootNavigator/types'
+import { SignUpSignInChoiceOfferModal } from 'features/offer/components/SignUpSignInChoiceOfferModal/SignUpSignInChoiceOfferModal'
+import { analytics } from 'libs/analytics'
+import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
+import { storage } from 'libs/storage'
+import { accessibleCheckboxProps } from 'shared/accessibilityProps/accessibleCheckboxProps'
+import { theme } from 'theme'
+import { RoundedButton } from 'ui/components/buttons/RoundedButton'
+import { useModal } from 'ui/components/modals/useModal'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
+
+interface Props {
+  offerId: number
+  animationState?: {
+    iconBackgroundColor: Animated.AnimatedInterpolation
+    iconBorderColor: Animated.AnimatedInterpolation
+    transition: Animated.AnimatedInterpolation
+  }
+}
+
+export const FavoriteButton: React.FC<Props> = (props) => {
+  const { animationState, offerId } = props
+
+  const {
+    visible: signInModalVisible,
+    showModal: showSignInModal,
+    hideModal: hideSignInModal,
+  } = useModal(false)
+  const {
+    visible: FavoriteListOfferModalVisible,
+    showModal: showFavoriteListOfferModal,
+    hideModal: hideFavoriteListOfferModal,
+  } = useModal(false)
+  const {
+    visible: isFavoriteListSurveyModalVisible,
+    showModal: showFavoriteListSurveyModal,
+    hideModal: hideFavoriteListSurveyModal,
+  } = useModal()
+
+  const { isLoggedIn } = useAuthContext()
+  const favorite = useFavorite({ offerId })
+  const { showErrorSnackBar } = useSnackBarContext()
+  const { params } = useRoute<UseRouteType<'Offer'>>()
+  const isFavListFakeDoorEnabled = useFeatureFlag(RemoteStoreFeatureFlags.FAV_LIST_FAKE_DOOR)
+
+  const scaleFavoriteIconAnimatedValueRef = useRef(new Animated.Value(1))
+
+  const { mutate: addFavorite, isLoading: addFavoriteIsLoading } = useAddFavorite({
+    onSuccess: () => {
+      if (typeof offerId === 'number') {
+        if (params) {
+          const { from, moduleName, moduleId, searchId } = params
+          analytics.logHasAddedOfferToFavorites({ from, offerId, moduleName, moduleId, searchId })
+        }
+      }
+    },
+  })
+
+  const { mutate: removeFavorite, isLoading: removeFavoriteIsLoading } = useRemoveFavorite({
+    onError: () => {
+      showErrorSnackBar({
+        message: 'L’offre n’a pas été retirée de tes favoris',
+        timeout: SNACK_BAR_TIME_OUT,
+      })
+    },
+  })
+
+  const pressFavorite = useCallback(async () => {
+    if (!isLoggedIn) {
+      showSignInModal()
+    } else if (favorite) {
+      removeFavorite(favorite.id)
+    } else {
+      animateIcon(scaleFavoriteIconAnimatedValueRef.current)
+      addFavorite({ offerId })
+      if (isFavListFakeDoorEnabled) {
+        const hasSeenFavListFakeDoor = await storage.readObject('has_seen_fav_list_fake_door')
+        if (!hasSeenFavListFakeDoor) {
+          analytics.logFavoriteListDisplayed('offer')
+          showFavoriteListOfferModal()
+        }
+      }
+    }
+  }, [
+    addFavorite,
+    favorite,
+    isFavListFakeDoorEnabled,
+    isLoggedIn,
+    offerId,
+    removeFavorite,
+    showFavoriteListOfferModal,
+    showSignInModal,
+  ])
+  return (
+    <React.Fragment>
+      <RoundedButton
+        animationState={animationState}
+        scaleAnimatedValue={scaleFavoriteIconAnimatedValueRef.current}
+        initialColor={favorite ? theme.colors.primary : undefined}
+        finalColor={favorite ? theme.colors.primary : theme.colors.black}
+        iconName={favorite ? 'favorite-filled' : 'favorite'}
+        onPress={pressFavorite}
+        disabled={removeFavoriteIsLoading || addFavoriteIsLoading}
+        {...accessibleCheckboxProps({ checked: !!favorite, label: 'Mettre en favoris' })}
+      />
+      <SignUpSignInChoiceOfferModal
+        visible={signInModalVisible}
+        offerId={offerId}
+        dismissModal={hideSignInModal}
+      />
+      <FavoriteListOfferModal
+        visible={FavoriteListOfferModalVisible}
+        hideModal={hideFavoriteListOfferModal}
+        showSurveyModal={showFavoriteListSurveyModal}
+      />
+      <FavoriteListSurveyModal
+        visible={isFavoriteListSurveyModalVisible}
+        hideModal={hideFavoriteListSurveyModal}
+      />
+    </React.Fragment>
+  )
+}
+
+function animateIcon(animatedValue: Animated.Value): void {
+  Animated.sequence([
+    Animated.timing(animatedValue, {
+      toValue: 1.3,
+      duration: 200,
+      useNativeDriver: false,
+    }),
+    Animated.timing(animatedValue, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: false,
+    }),
+  ]).start()
+}
