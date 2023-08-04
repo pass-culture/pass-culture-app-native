9fd4cbadc03e8d21c87ca628ac0147c3e0755b11
Lucas Beneston <lucas.beneston@passculture.app>
2023-08-04 11:18:15 +0200

    (PC-22272) feat(ui): add cmd + click for ButtonWithLinearGradient

--- src/ui/components/buttons/buttonWithLinearGradient/types.ts
+++ src/ui/components/buttons/buttonWithLinearGradient/types.ts
@@ -1,12 +1,13 @@
 import { FunctionComponent } from 'react'
 import { AccessibilityRole } from 'react-native'
+import { GestureResponderEvent } from 'react-native'
 
 import { IconInterface } from 'ui/svg/icons/types'
 
 export interface ButtonWithLinearGradientProps {
   children?: never
   wording: string
-  onPress?: (() => void) | (() => Promise<void>)
+  onPress?: (event: GestureResponderEvent | MouseEvent) => void | Promise<void>
   isDisabled?: boolean
   type?: 'button' | 'reset' | 'submit'
   className?: string
