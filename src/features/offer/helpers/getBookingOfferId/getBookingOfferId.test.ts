47d9c8da7909c9844c882342884b303acd960d14
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-08-04 09:28:57 +0200

    (PC-23426) test(offer): improve coverage

--- src/features/offer/helpers/getBookingOfferId/getBookingOfferId.test.ts
+++ src/features/offer/helpers/getBookingOfferId/getBookingOfferId.test.ts
@@ -8,6 +8,11 @@ describe('getBookingOfferId', () => {
     expect(bookingOfferId).toEqual(undefined)
   })
 
+  it('should return undefined when booked offer is an empty object', () => {
+    const bookingOfferId = getBookingOfferId(1500)
+    expect(bookingOfferId).toEqual(undefined)
+  })
+
   it('should return booking id when offer is booked', () => {
     const bookingOfferId = getBookingOfferId(1090, bookedOffer)
     expect(bookingOfferId).toEqual(32927191)
