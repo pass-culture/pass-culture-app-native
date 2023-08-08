013967a86ebf24dd3b9aabab05763182b68579d8
AnoukHello <anoukhello22@gmail.com>
2023-08-08 10:31:35 +0200

    (PC-18783) HighlightModule: fetch offer by id or tag or ean

--- src/features/home/api/useHighlightOffer.ts
+++ src/features/home/api/useHighlightOffer.ts
@@ -1,16 +1,32 @@
 import { useQuery } from 'react-query'
 
+import { useHomePosition } from 'features/home/helpers/useHomePosition'
 import { useIsUserUnderage } from 'features/profile/helpers/useIsUserUnderage'
+import { fetchOffersByEan } from 'libs/algolia/fetchAlgolia/fetchOffersByEan'
 import { fetchOffersByIds } from 'libs/algolia/fetchAlgolia/fetchOffersByIds'
+import { fetchOffersByTags } from 'libs/algolia/fetchAlgolia/fetchOffersByTags'
 import { useTransformOfferHits } from 'libs/algolia/fetchAlgolia/transformOfferHit'
 import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
 import { QueryKeys } from 'libs/queryKeys'
 import { Offer } from 'shared/offer/types'
 
-export const useHighlightOffer = (id: string, offerId?: string) => {
+type UseHightlightOfferParams = {
+  id: string
+  offerId?: string
+  offerEan?: string
+  offerTag?: string
+}
+
+export const useHighlightOffer = ({
+  id,
+  offerId,
+  offerEan,
+  offerTag,
+}: UseHightlightOfferParams) => {
   const isUserUnderage = useIsUserUnderage()
   const netInfo = useNetInfoContext()
   const transformOfferHits = useTransformOfferHits()
+  const { position } = useHomePosition()
 
   const offerByIdQuery = async () => {
     if (!offerId) return undefined
@@ -23,12 +39,33 @@ export const useHighlightOffer = (id: string, offerId?: string) => {
     return result
   }
 
+  const offerByTagQuery = async () => {
+    if (!offerTag) return undefined
+
+    const result = await fetchOffersByTags({
+      tags: [offerTag],
+      isUserUnderage,
+      userLocation: position,
+    })
+
+    return result
+  }
+
+  const offerByEanQuery = async () => {
+    if (!offerEan) return undefined
+
+    return await fetchOffersByEan({
+      eanList: [offerEan],
+      userLocation: position,
+      isUserUnderage,
+    })
+  }
+
   const { data } = useQuery({
     queryKey: [QueryKeys.HIGHLIGHT_OFFER, id],
-    queryFn: offerByIdQuery,
+    queryFn: offerEan ? offerByEanQuery : offerTag ? offerByTagQuery : offerByIdQuery,
     enabled: !!netInfo.isConnected,
   })
-
   const offers = (data?.map(transformOfferHits) as Offer[]) ?? []
   return offers[0]
 }
