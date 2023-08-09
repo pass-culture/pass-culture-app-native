82d815a338c558485f7e4d89145fe64dd4697836
marineshaw <marines@bam.tech>
2023-08-09 17:32:31 +0200

    (PC-23363) test(Touchable): make test accessible

--- src/ui/components/touchable/Touchable.native.test.tsx
+++ src/ui/components/touchable/Touchable.native.test.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-import { fireEvent, render, screen } from 'tests/utils/web'
+import { fireEvent, render, screen } from 'tests/utils'
 
-import { Touchable } from '../Touchable'
+import { Touchable } from './Touchable'
 
 describe('<Touchable />', () => {
-  it('should execute callback on click', () => {
+  it('should execute callback on press', () => {
     const handleClick = jest.fn()
     render(
-      <Touchable onPress={handleClick}>
+      <Touchable onPress={handleClick} accessibilityLabel="accessibility label">
         <Text>Touchable content</Text>
       </Touchable>
     )
 
     const button = screen.getByRole('button')
-    fireEvent.click(button)
+    fireEvent.press(button)
 
     expect(handleClick).toHaveBeenCalledTimes(1)
   })
