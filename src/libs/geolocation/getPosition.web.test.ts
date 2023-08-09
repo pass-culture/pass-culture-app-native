45d4e53a46acb9731f42daa08397db2826944240
marineshaw <marines@bam.tech>
2023-08-09 11:05:50 +0200

    (PC-23360) tests(mocks): remove restoreAllMocks in native tests

--- src/libs/geolocation/getPosition.web.test.ts
+++ src/libs/geolocation/getPosition.web.test.ts
@@ -4,8 +4,6 @@ import { EiffelTourCoordinates } from './tests.utils'
 jest.mock('libs/geolocation/getPosition', () => jest.requireActual('./getPosition'))
 
 describe('getPosition()', () => {
-  afterEach(jest.restoreAllMocks)
-
   it('should resolve with the geolocation', async () => {
     const position = await getPosition()
     expect(global.navigator.geolocation.getCurrentPosition).toHaveBeenCalledTimes(1)
