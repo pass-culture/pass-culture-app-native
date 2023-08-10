80d39a6f375035c9357e0d6170980f76fe83c4a5
marineshaw <marines@bam.tech>
2023-08-10 16:56:55 +0200

    :white_check_mark: (EmailInputController) unskip test as it does not fail

--- src/shared/forms/controllers/EmailInputController.native.test.tsx
+++ src/shared/forms/controllers/EmailInputController.native.test.tsx
@@ -51,9 +51,7 @@ describe('<EmailInputController />', () => {
     expect(screen.getByText('Veux-tu plutôt dire firstname.lastname@gmail.com ?')).toBeTruthy()
   })
 
-  // FIXME(anoukhello) find a way to fix this test that fails after react upgrade
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('should perform action on spelling help press when given', async () => {
+  it('should perform action on spelling help press when given', async () => {
     const mockOnSpellingHelpPress = jest.fn()
     renderEmailInputController({
       withSpellingHelp: true,
