f51c4f5193bd5e23c51b7509221bee8c76934998
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-08-10 16:28:19 +0200

    (PC-23773) test(errors): improve coverage

--- src/libs/monitoring/__tests__/errors.test.ts
+++ src/libs/monitoring/__tests__/errors.test.ts
@@ -1,8 +1,9 @@
 import { CaptureContext, Extras } from '@sentry/types'
 
+import { PageNotFound } from 'features/navigation/pages/PageNotFound'
 import { eventMonitoring } from 'libs/monitoring'
 
-import { MonitoringError } from '../errors'
+import { MonitoringError, OfferNotFoundError, VenueNotFoundError } from '../errors'
 
 describe('MonitoringError', () => {
   it('should call eventMonitoring.captureException() on new MonitoringError instance', () => {
@@ -35,5 +36,29 @@ describe('MonitoringError', () => {
     const error = new MonitoringError('error', { name: 'RenamedError', captureContext })
     expect(eventMonitoring.captureException).toBeCalledWith(error, captureContext)
     expect(error.name).toBe('RenamedError')
+  })
+})
+
+describe('OfferNotFoundError', () => {
+  it('should return message with offer id when it defined', () => {
+    const error = new OfferNotFoundError(10000, { Screen: PageNotFound })
+    expect(error.message).toBe('Offer 10000 could not be retrieved')
+  })
+
+  it('should return message without offer id when it undefined', () => {
+    const error = new OfferNotFoundError(undefined, { Screen: PageNotFound })
+    expect(error.message).toBe('offerId is undefined')
+  })
+})
+
+describe('VenueNotFoundError', () => {
+  it('should return message with offer id when it defined', () => {
+    const error = new VenueNotFoundError(5000, { Screen: PageNotFound })
+    expect(error.message).toBe('Venue 5000 could not be retrieved')
+  })
+
+  it('should return message without offer id when it undefined', () => {
+    const error = new VenueNotFoundError(undefined, { Screen: PageNotFound })
+    expect(error.message).toBe('venueId is undefined')
   })
 })
