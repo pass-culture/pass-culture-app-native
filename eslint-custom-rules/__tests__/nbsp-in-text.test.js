6a5efec52f5c7884be75da5a1407f7528e4ca298
Hugo Voisin <hugo.voisin@passculture.app>
2023-08-11 10:24:18 +0200

    (BSR) docs(eslint): add autofix to custom eslint rule doc (#5131)
    * docs: add autofix chapter to eslint doc
    
    * test: add test for fixer for nbsp rule
    
    * test: improve test cases
    adding diversity

--- eslint-custom-rules/__tests__/nbsp-in-text.test.js
+++ eslint-custom-rules/__tests__/nbsp-in-text.test.js
@@ -12,20 +12,28 @@ const tests = {
     // \u00a0 for '« myText'
     { code: `'«\\u00a0myText'` },
     // \u00a0 for `myText !` with characters !, ?, :, » and €
-    { code: `\`myText\\u00a0!\`` },
+    { code: `\`myText\\u00a0:\`` },
     // \u00a0 for `« myText`
     { code: `\`«\\u00a0myText\`` },
     // &nbsp; for "myText !" with characters !, ?, :, » and €
-    { code: `"myText&nbsp;!"` },
+    { code: `"myText&nbsp;?"` },
     // &nbsp; for "« myText"
     { code: `"«&nbsp;myText"` },
     // &nbsp; for <Text>myText !</Text> with characters !, ?, :, » and €
-    { code: '<Text>myText&nbsp;!</Text>' },
+    { code: '<Text>myText&nbsp;€</Text>' },
     // &nbsp; for <Text>« myText</Text>
     { code: '<Text>«&nbsp;myText</Text>' },
   ],
   invalid: [
-    // TODO(PC-21172): Add invalid tests (https://passculture.atlassian.net/browse/PC-21172)
+    { code: `'myText !'`, output: `'myText\\u00a0!'`, errors: 1 },
+    { code: `'« myText'`, output: `'«\\u00a0myText'`, errors: 1 },
+    { code: `\`myText :\`` ,output: `\`myText\\u00a0:\``, errors: 1 },
+    { code: `\`« myText\`` ,output: `\`«\\u00a0myText\``, errors: 1 },
+    { code: `"myText ?"` ,output: `"myText&nbsp;?"`, errors: 1 },
+    { code: `"« myText"` ,output: `"«&nbsp;myText"`, errors: 1 },
+    { code: '<Text>myText €</Text>' ,output: '<Text>myText&nbsp;€</Text>', errors: 1 },
+    { code: '<Text>« myText</Text>' ,output: '<Text>«&nbsp;myText</Text>', errors: 1 },
+
   ],
 }
 
