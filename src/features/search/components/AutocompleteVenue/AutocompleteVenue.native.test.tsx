911221c25b6fe2618b7fd76c312d2d5c332514fd
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-08-14 17:30:12 +0200

    (PC-23702) test(search): improve coverage

--- src/features/search/components/AutocompleteVenue/AutocompleteVenue.native.test.tsx
+++ src/features/search/components/AutocompleteVenue/AutocompleteVenue.native.test.tsx
@@ -1,17 +1,42 @@
+import { Hit } from '@algolia/client-search'
+import { BaseHit } from 'instantsearch.js'
 import React from 'react'
 
 import { AutocompleteVenue } from 'features/search/components/AutocompleteVenue/AutocompleteVenue'
 import { mockVenueHits } from 'features/search/fixtures/algolia'
-import { render } from 'tests/utils'
+import { render, screen } from 'tests/utils'
 
+let mockHits: Hit<BaseHit>[] = []
 jest.mock('react-instantsearch-hooks', () => ({
   useInfiniteHits: () => ({
-    hits: mockVenueHits,
+    hits: mockHits,
   }),
 }))
 
 describe('AutocompleteVenue component', () => {
-  it('should render AutocompleteVenue', () => {
-    expect(render(<AutocompleteVenue />)).toMatchSnapshot()
+  describe('With venue hits', () => {
+    beforeEach(() => {
+      mockHits = mockVenueHits
+    })
+
+    it('should render AutocompleteVenue', () => {
+      expect(render(<AutocompleteVenue />)).toMatchSnapshot()
+    })
+
+    it('should display "Points de vente"', () => {
+      render(<AutocompleteVenue />)
+      expect(screen.getByText('Points de vente')).toBeTruthy()
+    })
+  })
+
+  describe('Without venue hits', () => {
+    beforeEach(() => {
+      mockHits = []
+    })
+
+    it('should not display "Points de vente"', () => {
+      render(<AutocompleteVenue />)
+      expect(screen.queryByText('Points de vente')).toBeFalsy()
+    })
   })
 })
