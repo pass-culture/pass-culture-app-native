9bfef74091c5ac9d2007341768a44ac616acba88
AnoukHello <anoukhello22@gmail.com>
2023-08-16 17:17:24 +0200

    VerifyEligibility: navigate explicitly to Stepper page

--- src/features/auth/pages/signup/VerifyEligiblity/VerifyEligibility.native.test.tsx
+++ src/features/auth/pages/signup/VerifyEligiblity/VerifyEligibility.native.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { navigate } from '__mocks__/@react-navigation/native'
-import { useBeneficiaryValidationNavigation } from 'features/auth/helpers/useBeneficiaryValidationNavigation'
 import { navigateToHomeConfig } from 'features/navigation/helpers'
 import { navigateFromRef } from 'features/navigation/navigationRef'
 import { render, fireEvent, screen } from 'tests/utils'
@@ -11,7 +10,6 @@ import { VerifyEligibility } from './VerifyEligibility'
 jest.mock('features/navigation/helpers')
 jest.mock('features/navigation/navigationRef')
 jest.mock('react-query')
-jest.mock('features/auth/helpers/useBeneficiaryValidationNavigation')
 
 describe('<VerifyEligibility />', () => {
   it('should show the correct deposit amount', () => {
@@ -25,20 +23,18 @@ describe('<VerifyEligibility />', () => {
     const checkButton = screen.getByText('Vérifier mon identité plus tard')
     fireEvent.press(checkButton)
 
-    expect(navigateFromRef).toBeCalledWith(navigateToHomeConfig.screen, navigateToHomeConfig.params)
+    expect(navigateFromRef).toHaveBeenCalledWith(
+      navigateToHomeConfig.screen,
+      navigateToHomeConfig.params
+    )
   })
 
-  it('should navigate to nextBeneficiaryValidationStep WHEN clicking on "Commencer la vérification" button', () => {
-    const setError = jest.fn()
-    const { nextBeneficiaryValidationStepNavConfig } = useBeneficiaryValidationNavigation(setError)
+  it('should navigate to Stepper WHEN clicking on "Commencer la vérification" button', () => {
     render(<VerifyEligibility />)
 
     const startButton = screen.getByText('Commencer la vérification')
     fireEvent.press(startButton)
 
-    expect(navigate).toBeCalledWith(
-      nextBeneficiaryValidationStepNavConfig?.screen,
-      nextBeneficiaryValidationStepNavConfig?.params
-    )
+    expect(navigate).toHaveBeenCalledWith('Stepper', undefined)
   })
 })
