893d95b4ef6348a70299a96c3e09b16bec3fd620
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2023-08-18 10:27:05 +0200

    (PC-23211) feat(bookings): add share button for booking offers (#5160)
    * (PC-23211) feat(bookings): add share button for booking offers
    
    * (PC-23211) feat(bookings): add share button for ended booking offer
    
    * (PC-23211) feat(bookings): add snapshot for Bookingd

--- src/features/bookings/components/EndedBookingItem.tsx
+++ src/features/bookings/components/EndedBookingItem.tsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import styled from 'styled-components/native'
 
 import { BookingCancellationReasons } from 'api/gen'
+import { BookingItemTitle } from 'features/bookings/components/BookingItemTitle'
 import { isEligibleBookingsForArchive } from 'features/bookings/helpers/expirationDateUtils'
 import { BookingItemProps } from 'features/bookings/types'
+import { getShareOffer } from 'features/share/helpers/useShareOffer'
+import { WebShareModal } from 'features/share/pages/WebShareModal'
 import { analytics } from 'libs/analytics'
 import { formatToSlashedFrenchDate } from 'libs/dates'
 import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
 import { useCategoryId } from 'libs/subcategories'
 import { tileAccessibilityLabel, TileContentType } from 'libs/tileAccessibilityLabel'
 import { usePrePopulateOffer } from 'shared/offer/usePrePopulateOffer'
+import { RoundedButton } from 'ui/components/buttons/RoundedButton'
 import { InputRule } from 'ui/components/inputs/rules/InputRule'
+import { useModal } from 'ui/components/modals/useModal'
 import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
 import { OfferImage } from 'ui/components/tiles/OfferImage'
 import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
 import { Valid } from 'ui/svg/icons/Valid'
 import { Wrong } from 'ui/svg/icons/Wrong'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
-
-import { BookingItemTitle } from './BookingItemTitle'
 
 export const EndedBookingItem = ({ booking }: BookingItemProps) => {
   const { cancellationDate, cancellationReason, dateUsed, stock } = booking
@@ -68,17 +71,35 @@ export const EndedBookingItem = ({ booking }: BookingItemProps) => {
     }
   }
 
+  const {
+    visible: shareOfferModalVisible,
+    showModal: showShareOfferModal,
+    hideModal: hideShareOfferModal,
+  } = useModal(false)
+
+  const { share: shareOffer, shareContent } = getShareOffer({
+    offerId: stock.offer.id,
+    offerName: stock.offer.name,
+    venueName: stock.offer.venue.name,
+  })
+
+  const pressShareOffer = useCallback(() => {
+    analytics.logShare({ type: 'Offer', from: 'endedbookings', id: stock.offer.id })
+    shareOffer()
+    showShareOfferModal()
+  }, [stock.offer.id, shareOffer, showShareOfferModal])
+
   return (
-    <InternalTouchableLink
-      enableNavigate={!!netInfo.isConnected}
-      navigateTo={
-        shouldRedirectToBooking
-          ? { screen: 'BookingDetails', params: { id: booking.id } }
-          : { screen: 'Offer', params: { id: stock.offer.id, from: 'endedbookings' } }
-      }
-      onBeforeNavigate={handlePressOffer}
-      accessibilityLabel={accessibilityLabel}>
-      <ItemContainer>
+    <React.Fragment>
+      <ContentContainer
+        enableNavigate={!!netInfo.isConnected}
+        navigateTo={
+          shouldRedirectToBooking
+            ? { screen: 'BookingDetails', params: { id: booking.id } }
+            : { screen: 'Offer', params: { id: stock.offer.id, from: 'endedbookings' } }
+        }
+        onBeforeNavigate={handlePressOffer}
+        accessibilityLabel={accessibilityLabel}>
         <OfferImage imageUrl={stock.offer.image?.url} categoryId={categoryId} />
         <Spacer.Row numberOfSpaces={4} />
         <AttributesView>
@@ -89,18 +110,34 @@ export const EndedBookingItem = ({ booking }: BookingItemProps) => {
             <Typo.CaptionNeutralInfo>{endedBookingDateLabel}</Typo.CaptionNeutralInfo>
           </EndedReasonAndDate>
         </AttributesView>
-      </ItemContainer>
-    </InternalTouchableLink>
+      </ContentContainer>
+      <ShareContainer>
+        <RoundedButton
+          iconName="share"
+          onPress={pressShareOffer}
+          accessibilityLabel={`Partager l’offre ${stock.offer.name}`}
+        />
+      </ShareContainer>
+      {!!shareContent && (
+        <WebShareModal
+          visible={shareOfferModalVisible}
+          headerTitle="Partager l’offre"
+          shareContent={shareContent}
+          dismissModal={hideShareOfferModal}
+        />
+      )}
+    </React.Fragment>
   )
 }
+
+const ContentContainer = styled(InternalTouchableLink)(({ theme }) => ({
+  flexDirection: 'row',
+  paddingRight: theme.buttons.roundedButton.size,
+}))
 
 const AttributesView = styled.View({
   flex: 1,
   paddingRight: getSpacing(1),
-})
-
-const ItemContainer = styled.View({
-  flexDirection: 'row',
 })
 
 const EndedReasonAndDate = styled.View({
@@ -138,3 +175,10 @@ function getEndedBookingDateLabel(cancellationDate?: string | null, dateUsed?: s
 const StyledInputRule = styled(InputRule).attrs(({ theme }) => ({
   iconSize: theme.icons.sizes.smaller,
 }))``
+
+const ShareContainer = styled.View(({ theme }) => ({
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  borderRadius: theme.buttons.roundedButton.size,
+}))
