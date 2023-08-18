ebe5d8728eee6bcef15c60481ea7f5e4b2c02e85
Lucas Beneston <lucas.beneston@passculture.app>
2023-08-18 17:40:32 +0200

    (PC-21183) feat(auth): make maximum year unchangeable on DatePickerSpinner

--- src/ui/components/inputs/DateInput/DatePicker/DatePickerSpinner.tsx
+++ src/ui/components/inputs/DateInput/DatePicker/DatePickerSpinner.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import DatePicker from 'react-native-date-picker'
 import styled from 'styled-components/native'
 import { v4 as uuidv4 } from 'uuid'
@@ -8,33 +8,39 @@ import { DatePickerProps } from 'ui/components/inputs/DateInput/DatePicker/types
 import { InputError } from 'ui/components/inputs/InputError'
 import { getSpacing, Spacer } from 'ui/theme'
 
-export function DatePickerSpinner(props: DatePickerProps) {
+export const DatePickerSpinner: FunctionComponent<DatePickerProps> = ({
+  onChange,
+  defaultSelectedDate,
+  errorMessage,
+  maximumDate,
+  minimumDate,
+}) => {
   const [date, setDate] = useState<Date>()
   const birthdateInputErrorId = uuidv4()
 
   useEffect(() => {
-    props.onChange(date || props.defaultSelectedDate)
+    onChange(date || defaultSelectedDate)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [date])
 
   return (
     <React.Fragment>
-      <DateInputDisplay date={date || props.defaultSelectedDate} isError={!!props.errorMessage} />
+      <DateInputDisplay date={date || defaultSelectedDate} isError={!!errorMessage} />
       <InputError
-        visible={!!props.errorMessage}
-        messageId={props.errorMessage}
+        visible={!!errorMessage}
+        messageId={errorMessage}
         numberOfSpacesTop={2}
         relatedInputId={birthdateInputErrorId}
       />
       <Spacer.Column numberOfSpaces={5} />
       <SpinnerDatePicker
         testID="date-picker-spinner-native"
-        date={date || props.defaultSelectedDate}
+        date={date || defaultSelectedDate}
         onDateChange={setDate}
         mode="date"
         locale="fr-FR"
-        maximumDate={props.maximumDate}
-        minimumDate={props.minimumDate}
+        maximumDate={maximumDate}
+        minimumDate={minimumDate}
         androidVariant="nativeAndroid"
         accessibilityDescribedBy={birthdateInputErrorId}
       />
--- src/ui/components/inputs/DateInput/DatePicker/DatePickerSpinner.web.test.tsx
+++ src/ui/components/inputs/DateInput/DatePicker/DatePickerSpinner.web.test.tsx
@@ -1,7 +1,12 @@
 import mockdate from 'mockdate'
 import React from 'react'
 
-import { MINIMUM_DATE, CURRENT_DATE, DEFAULT_SELECTED_DATE } from 'features/auth/fixtures/fixtures'
+import {
+  MINIMUM_DATE,
+  CURRENT_DATE,
+  DEFAULT_SELECTED_DATE,
+  MAXIMUM_DATE,
+} from 'features/auth/fixtures/fixtures'
 import { act, fireEvent, render, screen } from 'tests/utils/web'
 import { DatePickerSpinner } from 'ui/components/inputs/DateInput/DatePicker/DatePickerSpinner.web'
 
@@ -9,6 +14,7 @@ const props = {
   onChange: jest.fn(),
   defaultSelectedDate: DEFAULT_SELECTED_DATE,
   minimumDate: MINIMUM_DATE,
+  maximumDate: MAXIMUM_DATE,
 }
 
 describe('<DatePickerSpinner />', () => {
@@ -24,17 +30,17 @@ describe('<DatePickerSpinner />', () => {
     const day = container.getElementsByClassName('picker-scroller')[0].childNodes[1] // 02
     fireEvent.click(day)
 
-    const month = container.getElementsByClassName('picker-scroller')[1].childNodes[6] // Juillet
+    const month = container.getElementsByClassName('picker-scroller')[1].childNodes[6] // 07
     fireEvent.click(month)
 
-    const year = container.getElementsByClassName('picker-scroller')[2].childNodes[2] // 2004
+    const year = container.getElementsByClassName('picker-scroller')[2].childNodes[1] // 2005
     fireEvent.click(year)
 
     expect(props.onChange).toHaveBeenNthCalledWith(1, DEFAULT_SELECTED_DATE) // first render trigger useEffect
     expect(props.onChange).toHaveBeenNthCalledWith(2, DEFAULT_SELECTED_DATE)
     expect(props.onChange).toHaveBeenNthCalledWith(3, new Date('2006-12-02T00:00:00.000Z'))
     expect(props.onChange).toHaveBeenNthCalledWith(4, new Date('2006-07-02T00:00:00.000Z'))
-    expect(props.onChange).toHaveBeenNthCalledWith(5, new Date('2004-07-02T00:00:00.000Z'))
+    expect(props.onChange).toHaveBeenNthCalledWith(5, new Date('2005-07-02T00:00:00.000Z'))
   })
 
   it('should trigger hidden value change when value is valid iso date string', () => {
