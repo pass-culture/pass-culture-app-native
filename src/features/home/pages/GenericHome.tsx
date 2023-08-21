495f5d8ff264f71102bf611eae6e2084a4e43556
Tanguy <tanguy.prache@passculture.app>
2023-08-21 15:56:12 +0200

    (pc-22484) feat: add scrollEventThrottle value

--- src/features/home/pages/GenericHome.tsx
+++ src/features/home/pages/GenericHome.tsx
@@ -186,6 +186,7 @@ const OnlineHome: FunctionComponent<GenericHomeProps> = ({
           initialNumToRender={initialNumToRender}
           removeClippedSubviews={false}
           onContentSizeChange={onContentSizeChange}
+          scrollEventThrottle={16}
           bounces
         />
         {shouldDisplayScrollToTop ? (
