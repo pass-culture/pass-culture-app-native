16cb361c30c839f2e1fa46424d56138d841241e4
Lucas Beneston <lucas.beneston@passculture.app>
2023-08-21 12:03:22 +0200

    (PC-21183) refactor(date): use map for dayNamesShort in days.ts

--- src/shared/date/days.ts
+++ src/shared/date/days.ts
@@ -2,4 +2,4 @@ import range from 'lodash/range'
 
 export const dayNumbers = range(1, 31 + 1).map((num) => String(num))
 export const dayNames = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
-export const dayNamesShort = ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+export const dayNamesShort = dayNames.map((dayName) => dayName.charAt(0))
