11c75a5dd7d0b6404e88448e7c9cd63fd080e671
marineshaw <marines@bam.tech>
2023-08-21 11:44:25 +0200

    :recycle: (CreditBlockTitle) simplifies switch case

--- src/features/onboarding/helpers/CreditBlockTitle.tsx
+++ src/features/onboarding/helpers/CreditBlockTitle.tsx
@@ -14,20 +14,10 @@ export const CreditBlockTitle = ({ age, userAge, deposit }: Props): React.ReactE
   const TitleText: React.JSXElementConstructor<{ children: string }> =
     age === userAge ? TitleSecondary : Typo.ButtonText
 
-  switch (age) {
-    case 15:
-      return <TitleText>{deposit}</TitleText>
-    case 16:
-    case 17:
-      if (age > userAge) {
-        return <TitleText>{`+ ${deposit}`}</TitleText>
-      } else {
-        return <TitleText>{deposit}</TitleText>
-      }
-    case 18:
-    default:
-      return <TitleText>{deposit}</TitleText>
+  if (age > userAge) {
+    return <TitleText>{`+ ${deposit}`}</TitleText>
   }
+  return <TitleText>{deposit}</TitleText>
 }
 
 const Title = styled(Typo.Title3).attrs(getNoHeadingAttrs)``
