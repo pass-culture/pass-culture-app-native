616a8b78e4b0adbaa53e1f67bdaf585f3c34acba
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-08-22 09:28:45 +0200

    (PC-23350) feat(analytics): add searchId param to AllTilesSeen log

--- src/libs/analytics/logEventAnalytics.ts
+++ src/libs/analytics/logEventAnalytics.ts
@@ -65,7 +65,7 @@ export const logEventAnalytics = {
     analytics.logEvent({ firebase: AnalyticsEvent.ACTIVATE_GEOLOC_FROM_SEARCH_RESULTS }),
   logAllModulesSeen: (numberOfModules: number) =>
     analytics.logEvent({ firebase: AnalyticsEvent.ALL_MODULES_SEEN }, { numberOfModules }),
-  logAllTilesSeen: (params: { moduleName?: string; numberOfTiles?: number }) =>
+  logAllTilesSeen: (params: { moduleName?: string; numberOfTiles?: number; searchId?: string }) =>
     analytics.logEvent({ firebase: AnalyticsEvent.ALL_TILES_SEEN }, params),
   logBackToHomeFromEduconnectError: (params: { fromError: string }) =>
     analytics.logEvent({ firebase: AnalyticsEvent.BACK_TO_HOME_FROM_EDUCONNECT_ERROR }, params),
