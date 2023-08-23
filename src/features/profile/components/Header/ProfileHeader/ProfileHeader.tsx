45e2d69bf946d37683d74aa1c6f783919c960073
AnoukHello <anoukhello22@gmail.com>
2023-08-23 11:18:26 +0200

    (PC-22949) move cheat menu button to its own component

--- src/features/profile/components/Header/ProfileHeader/ProfileHeader.tsx
+++ src/features/profile/components/Header/ProfileHeader/ProfileHeader.tsx
@@ -1,17 +1,11 @@
-import { useNavigation } from '@react-navigation/native'
 import React, { useMemo } from 'react'
-import { Platform, TouchableOpacity } from 'react-native'
-import styled from 'styled-components/native'
 
 import { UserProfileResponse } from 'api/gen'
 import { useAuthContext } from 'features/auth/context/AuthContext'
-import { UseNavigationType } from 'features/navigation/RootNavigator/types'
+import { CheatMenuButton } from 'features/internal/cheatcodes/components/CheatMenuButton'
 import { CreditHeader } from 'features/profile/components/Header/CreditHeader/CreditHeader'
 import { LoggedOutHeader } from 'features/profile/components/Header/LoggedOutHeader/LoggedOutHeader'
 import { NonBeneficiaryHeader } from 'features/profile/components/Header/NonBeneficiaryHeader/NonBeneficiaryHeader'
-import { env } from 'libs/environment'
-import { getSpacing, Typo } from 'ui/theme'
-import { useCustomSafeInsets } from 'ui/theme/useCustomSafeInsets'
 
 type ProfileHeaderProps = {
   user?: UserProfileResponse
@@ -20,8 +14,6 @@ type ProfileHeaderProps = {
 export function ProfileHeader(props: ProfileHeaderProps) {
   const { user } = props
   const { isLoggedIn } = useAuthContext()
-  const { navigate } = useNavigation<UseNavigationType>()
-  const { top } = useCustomSafeInsets()
 
   const ProfileHeader = useMemo(() => {
     if (!isLoggedIn || !user) {
@@ -49,22 +41,8 @@ export function ProfileHeader(props: ProfileHeaderProps) {
 
   return (
     <React.Fragment>
-      {!!env.FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING && (
-        <CheatCodeButtonContainer
-          onPress={() => navigate(Platform.OS === 'web' ? 'Navigation' : 'CheatMenu')}
-          style={{ top: getSpacing(3) + top }}>
-          <Typo.Body>CheatMenu</Typo.Body>
-        </CheatCodeButtonContainer>
-      )}
+      <CheatMenuButton />
       {ProfileHeader}
     </React.Fragment>
   )
 }
-
-const CheatCodeButtonContainer = styled(TouchableOpacity)(({ theme }) => ({
-  position: 'absolute',
-  right: getSpacing(2),
-  zIndex: theme.zIndex.cheatCodeButton,
-  border: 1,
-  padding: getSpacing(1),
-}))
