096e6344e966cfea30c7ea5305e78d4cac6a7ec9
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-08-24 16:51:09 +0200

    (PC-23399) feat(search): use venues playlist search index when location filter is everywhere and user shares his position

--- src/libs/algolia/fetchAlgolia/helpers/getCurrentVenuesIndex.ts
+++ src/libs/algolia/fetchAlgolia/helpers/getCurrentVenuesIndex.ts
@@ -1,11 +1,12 @@
 import { LocationType } from 'features/search/enums'
 import { env } from 'libs/environment'
+import { Position } from 'libs/geolocation'
 
-export function getCurrentVenuesIndex(locationType?: LocationType) {
+export function getCurrentVenuesIndex(locationType?: LocationType, userPosition?: Position) {
   const venuesIndexSearch = env.ALGOLIA_VENUES_INDEX_PLAYLIST_SEARCH
   const venuesIndexSearchNewest = env.ALGOLIA_VENUES_INDEX_PLAYLIST_SEARCH_NEWEST
 
-  if (!locationType || locationType === LocationType.EVERYWHERE) {
+  if (!locationType || (locationType === LocationType.EVERYWHERE && !userPosition)) {
     return venuesIndexSearchNewest
   }
 
