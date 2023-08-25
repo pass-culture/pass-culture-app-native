fc9a54bdbe4504633dce955fd88a7a72e4be20c4
Tanguy <tanguy.prache@passculture.app>
2023-08-25 12:10:10 +0200

    (pc-23523) feat: localize ubble instruction date

--- src/features/profile/helpers/formatDateToLastUpdatedAtMessage.ts
+++ src/features/profile/helpers/formatDateToLastUpdatedAtMessage.ts
@@ -3,7 +3,10 @@ import { formatToHour } from 'libs/parsers/formatDates'
 
 export const formatDateToLastUpdatedAtMessage = (lastUpdatedDate: string | undefined) => {
   if (!lastUpdatedDate) return
-  const day = formatToSlashedFrenchDate(new Date(lastUpdatedDate).toISOString())
-  const hour = formatToHour(new Date(lastUpdatedDate))
+  const utcDate = new Date(lastUpdatedDate)
+  const timeZoneOffest = new Date(lastUpdatedDate).getTimezoneOffset()
+  const localDate = utcDate.setMinutes(utcDate.getMinutes() - timeZoneOffest)
+  const day = formatToSlashedFrenchDate(new Date(localDate).toISOString())
+  const hour = formatToHour(new Date(localDate))
   return `${day} à ${hour}`
 }
