1dd5b826ea2ede0a81cfff9b372a6e9b0d9eca13
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-08-28 12:09:09 +0200

    (PC-23797) feat(batch): Trigger event on venue page

--- src/features/venue/pages/Venue/Venue.native.test.tsx
+++ src/features/venue/pages/Venue/Venue.native.test.tsx
@@ -6,8 +6,16 @@ import { Referrals } from 'features/navigation/RootNavigator/types'
 import { venueResponseSnap } from 'features/venue/fixtures/venueResponseSnap'
 import { Venue } from 'features/venue/pages/Venue/Venue'
 import { analytics } from 'libs/analytics'
+import { BatchEvent, BatchUser } from 'libs/react-native-batch'
 import { placeholderData } from 'libs/subcategories/placeholderData'
-import { render, waitFor } from 'tests/utils'
+import {
+  render,
+  screen,
+  bottomScrollEvent,
+  waitFor,
+  fireEvent,
+  middleScrollEvent,
+} from 'tests/utils'
 
 mockdate.set(new Date('2021-08-15T00:00:00Z'))
 
@@ -26,6 +34,10 @@ jest.mock('libs/subcategories/useSubcategories', () => ({
   }),
 }))
 const venueId = venueResponseSnap.id
+
+const BATCH_TRIGGER_DELAY_IN_MS = 5000
+
+jest.useFakeTimers({ legacyFakeTimers: true })
 
 describe('<Venue />', () => {
   it('should match snapshot', async () => {
@@ -49,6 +61,50 @@ describe('<Venue />', () => {
   it('should not log consult venue when URL has not "from" param', async () => {
     await renderVenue(venueId)
     expect(analytics.logConsultVenue).not.toHaveBeenCalled()
+  })
+
+  describe('Batch trigger', () => {
+    it('should trigger event after 5 seconds', async () => {
+      await renderVenue(venueId)
+
+      jest.advanceTimersByTime(BATCH_TRIGGER_DELAY_IN_MS)
+
+      expect(BatchUser.trackEvent).toHaveBeenCalledWith(BatchEvent.hasSeenVenueForSurvey)
+    })
+
+    it('should not trigger event before 5 seconds have elapsed', async () => {
+      await renderVenue(venueId)
+
+      jest.advanceTimersByTime(BATCH_TRIGGER_DELAY_IN_MS - 1)
+
+      expect(BatchUser.trackEvent).not.toHaveBeenCalled()
+    })
+
+    it('should trigger event on scroll to bottom', async () => {
+      await renderVenue(venueId)
+
+      fireEvent.scroll(screen.getByTestId('venue-container'), bottomScrollEvent)
+
+      expect(BatchUser.trackEvent).toHaveBeenCalledWith(BatchEvent.hasSeenVenueForSurvey)
+    })
+
+    it('should not trigger event on scroll to middle', async () => {
+      await renderVenue(venueId)
+
+      fireEvent.scroll(screen.getByTestId('venue-container'), middleScrollEvent)
+
+      expect(BatchUser.trackEvent).not.toHaveBeenCalled()
+    })
+
+    it('should trigger event once on scroll to bottom and after 5 seconds', async () => {
+      await renderVenue(venueId)
+
+      fireEvent.scroll(screen.getByTestId('venue-container'), bottomScrollEvent)
+      jest.advanceTimersByTime(BATCH_TRIGGER_DELAY_IN_MS)
+
+      expect(BatchUser.trackEvent).toHaveBeenCalledTimes(1)
+      expect(BatchUser.trackEvent).toHaveBeenCalledWith(BatchEvent.hasSeenVenueForSurvey)
+    })
   })
 })
 
--- src/libs/react-native-batch/enums.ts
+++ src/libs/react-native-batch/enums.ts
@@ -3,6 +3,7 @@ export enum BatchEvent {
   hasValidatedSubscription = 'has_validated_subscription',
   hasSeenAllTheHomepage = 'has_seen_all_the_homepage',
   hasSeenOffer = 'has_seen_offer',
+  hasSeenVenueForSurvey = 'has_seen_venue_for_survey',
   hasValidatedAccount = 'has_validated_account',
   screenViewExpiredOrLostId = 'screen_view_expired_or_lost_id',
   screenViewComeBackLater = 'screen_view_come_back_later',
