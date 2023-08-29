de489879fc5e6dc52fce63bc5fbd3d642e7e6f01
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2023-08-29 16:23:36 +0200

    (BSR) feat(sonar): fix somes sonar issues (#5229)
    * (BSR) fix(sonar): Fix 'Destructuring assignment userPosition unnecessarily renamed' issue for useDistance
    
    * (BSR) fix(sonar): Fix 'If statement should not be the only statement in else block' issues

--- src/libs/geolocation/hooks/useDistance.ts
+++ src/libs/geolocation/hooks/useDistance.ts
@@ -5,7 +5,7 @@ export const useDistance = (offerPosition: {
   lat?: number | null
   lng?: number | null
 }): string | undefined => {
-  const { userPosition: userPosition } = useGeolocation()
+  const { userPosition } = useGeolocation()
   // TODO(voisinhugo): check if !offerPosition check is useful - add data validation from API if needed
   if (!userPosition || !offerPosition) return undefined
   return formatDistance(offerPosition, userPosition)
