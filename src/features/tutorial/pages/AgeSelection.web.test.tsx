779ed352927311ca16a6f618a1db7ab9eac5c51c
Lucas Beneston <lucas.beneston@passculture.app>
2023-09-01 14:19:52 +0200

    (PC-23923) feat(tutorial): enable AgeSelection page for web and add accessibility tests

--- src/features/tutorial/pages/AgeSelection.web.test.tsx
+++ src/features/tutorial/pages/AgeSelection.web.test.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
-import { render } from 'tests/utils/web'
+import { render, checkAccessibilityFor } from 'tests/utils/web'
 
 import { AgeSelection } from './AgeSelection'
 
-describe('AgeSelection', () => {
-  it('should render null in web', () => {
-    const { container } = render(<AgeSelection type="onboarding" />)
-    expect(container).toBeEmptyDOMElement()
+describe('<AgeSelection/>', () => {
+  describe('Accessibility', () => {
+    it('should not have basic accessibility issues for onboarding tutorial', async () => {
+      const { container } = render(<AgeSelection type="onboarding" />)
+
+      const results = await checkAccessibilityFor(container)
+
+      expect(results).toHaveNoViolations()
+    })
+
+    it('should not have basic accessibility issues for profile tutorial', async () => {
+      const { container } = render(<AgeSelection type="profileTutorial" />)
+
+      const results = await checkAccessibilityFor(container)
+
+      expect(results).toHaveNoViolations()
+    })
   })
 })
