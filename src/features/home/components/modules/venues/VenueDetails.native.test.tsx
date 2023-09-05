db50186208362254168a95ecc5eb7dd6203afbfe
dbenfouzari-pass <donovan.benfouzari@passculture.app>
2023-09-05 16:37:24 +0200

    (PC-23850) test(home): add missing test

--- src/features/home/components/modules/venues/VenueDetails.native.test.tsx
+++ src/features/home/components/modules/venues/VenueDetails.native.test.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
 
-import { VenueTypeCodeKey } from 'api/gen'
-import { VenueTypeCode } from 'libs/parsers'
-import { render } from 'tests/utils'
+import { render, screen } from 'tests/utils'
 
 import { VenueDetails } from './VenueDetails'
-
-const props = {
-  name: 'Musée du Louvre',
-  width: 100,
-  venueType: VenueTypeCodeKey.MUSEUM as VenueTypeCode,
-}
 
 describe('VenueDetails component', () => {
   afterAll(() => jest.resetAllMocks())
 
   it('should render correctly', () => {
-    const { toJSON } = render(<VenueDetails {...props} />)
+    const { toJSON } = render(
+      <VenueDetails name="Musée du Louvre" width={100} postalCode="75000" city="Paris" />
+    )
     expect(toJSON()).toMatchSnapshot()
+  })
+
+  it('should show formatted city and postalCode', () => {
+    render(<VenueDetails name="Musée du Louvre" width={100} postalCode="75000" city="Paris" />)
+    expect(screen.getByText('Paris, 75000')).toBeOnTheScreen()
   })
 })
