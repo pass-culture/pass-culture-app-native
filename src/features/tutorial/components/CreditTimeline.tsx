1810b8a0a1fa1478420838e05887247036a3f512
marineshaw <marines@bam.tech>
2023-09-11 14:38:59 +0200

    :art: (CreditTimeline) Fix stepper icon alignment

--- src/features/tutorial/components/CreditTimeline.tsx
+++ src/features/tutorial/components/CreditTimeline.tsx
@@ -4,17 +4,17 @@ import styled from 'styled-components/native'
 import { InternalStep } from 'features/profile/components/InternalStep/InternalStep'
 import { StepVariant } from 'features/profile/components/VerticalStepper/types'
 import { AgeCreditBlock } from 'features/tutorial/components/AgeCreditBlock'
+import { CreditBlock } from 'features/tutorial/components/CreditBlock'
 import { OnboardingCreditBlockTitle } from 'features/tutorial/components/onboarding/OnboardingCreditBlockTitle'
 import { ProfileTutorialCreditBlockTitle } from 'features/tutorial/components/profileTutorial/ProfileTutorialCreditBlockTitle'
 import { getCreditStatusFromAge } from 'features/tutorial/helpers/getCreditStatusFromAge'
 import { getStepperIconFromCreditStatus } from 'features/tutorial/helpers/getStepperIconFromCreditStatus'
 import { getStepperVariantFromCreditStatus } from 'features/tutorial/helpers/getStepperVariantFromCreditStatus'
+import { CreditStatus } from 'features/tutorial/types'
 import { analytics } from 'libs/analytics'
 import { useDepositAmountsByAge } from 'shared/user/useDepositAmountsByAge'
 import { Warning } from 'ui/svg/icons/BicolorWarning'
 import { Spacer, getSpacing } from 'ui/theme'
-import { CreditStatus } from 'features/tutorial/types'
-import { CreditBlock } from 'features/tutorial/components/CreditBlock'
 
 type Age = 15 | 16 | 17 | 18
 
@@ -46,7 +46,7 @@ export const CreditTimeline = ({ stepperProps, age, type }: Props) => {
     [17, seventeenYearsOldDeposit],
     [18, eighteenYearsOldDeposit],
   ])
-  const SpaceBetweenBlock = type === 'onboarding' ? 2 : 6
+  const SpaceBetweenBlock = type === 'onboarding' ? 1 : 3
 
   const CreditBlockTitle =
     type === 'onboarding' ? OnboardingCreditBlockTitle : ProfileTutorialCreditBlockTitle
@@ -54,14 +54,17 @@ export const CreditTimeline = ({ stepperProps, age, type }: Props) => {
   return (
     <Container>
       {stepperProps.map((props, index) => {
+        const isLast = index === stepperProps.length - 1
+        const isFirst = index === 0
         if (props.creditStep === 'information') {
           const iconComponent = props.iconComponent ?? <GreyWarning />
           return (
             <InternalStep
               key={'information ' + index}
               variant={StepVariant.future}
-              isLast={index === stepperProps.length - 1}
+              isLast={isLast}
               iconComponent={iconComponent}>
+              <Spacer.Column numberOfSpaces={SpaceBetweenBlock} />
               {props.children}
               <Spacer.Column numberOfSpaces={SpaceBetweenBlock} />
             </InternalStep>
@@ -75,8 +78,9 @@ export const CreditTimeline = ({ stepperProps, age, type }: Props) => {
             <InternalStep
               key={'pastStep ' + index}
               variant={StepVariant.complete}
-              isFirst={index === 0}
+              isFirst={isFirst}
               iconComponent={iconComponent}>
+              <Spacer.Column numberOfSpaces={SpaceBetweenBlock} />
               <CreditBlock creditStatus={CreditStatus.GONE}>{props.children}</CreditBlock>
               <Spacer.Column numberOfSpaces={SpaceBetweenBlock} />
             </InternalStep>
@@ -90,8 +94,9 @@ export const CreditTimeline = ({ stepperProps, age, type }: Props) => {
             key={index}
             variant={stepVariant}
             iconComponent={getStepperIconFromCreditStatus(creditStatus)}
-            isFirst={index === 0}
-            isLast={index === stepperProps.length - 1}>
+            isFirst={isFirst}
+            isLast={isLast}>
+            <Spacer.Column numberOfSpaces={SpaceBetweenBlock} />
             <AgeCreditBlock
               creditStatus={creditStatus}
               age={props.creditStep}
