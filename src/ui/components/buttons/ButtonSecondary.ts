53e7d3ecd7505820ff0fd3029c3bad8a8fc76d30
Hugo Voisin <hugov@bam.tech>
2023-09-12 15:47:49 +0200

    refactor: use ternary instead of let + if

--- src/ui/components/buttons/ButtonSecondary.ts
+++ src/ui/components/buttons/ButtonSecondary.ts
@@ -50,14 +50,14 @@ export const ButtonSecondary = styledButton(AppButton).attrs<BaseButtonProps>(
     borderColor = theme.buttons.disabled.secondary.borderColor
   }
 
-  let webOnly = {}
-  if (Platform.OS === 'web') {
-    webOnly = {
-      ['&:disabled']: {
-        borderColor: theme.buttons.disabled.secondary.borderColor,
-      },
-    }
-  }
+  const webOnly =
+    Platform.OS === 'web'
+      ? {
+          ['&:disabled']: {
+            borderColor: theme.buttons.disabled.secondary.borderColor,
+          },
+        }
+      : {}
 
   return {
     borderStyle: 'solid',
