4c7612b982623356762b88d677cdd56f91e3dbea
dbenfouzari-pass <donovan.benfouzari@passculture.app>
2023-09-12 15:04:13 +0200

    (PC-24293) fix(search): rollback useless code

--- src/shared/getNextPageParam/getNextPageParam.ts
+++ src/shared/getNextPageParam/getNextPageParam.ts
@@ -1,3 +1,3 @@
 // first page is 0
-export const getNextPageParam = ({ nbPages, page }: { nbPages: number; page: number }) =>
+export const getNextPageParam = ({ page, nbPages }: { page: number; nbPages: number }) =>
   page + 1 < nbPages ? page + 1 : undefined
