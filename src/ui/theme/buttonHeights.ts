e081254d282140b88d479bba9ff8e60af25565a5
Hugo Voisin <hugov@bam.tech>
2023-09-14 10:54:12 +0200

    type: remove unused types

--- src/ui/theme/buttonHeights.ts
+++ src/ui/theme/buttonHeights.ts
@@ -5,16 +5,9 @@ import {
   EXTRA_SMALL_BUTTON_HEIGHT,
 } from 'ui/theme/constants'
 
-export type ButtonHeightsType = {
-  tall: number
-  small: number
-  inline: number
-  extraSmall: number
-}
-
-export const buttonHeights: ButtonHeightsType = {
+export const buttonHeights = {
   extraSmall: EXTRA_SMALL_BUTTON_HEIGHT,
   tall: TALL_BUTTON_HEIGHT,
   small: SMALL_BUTTON_HEIGHT,
   inline: DEFAULT_INLINE_BUTTON_HEIGHT,
-}
+} as const
--- src/ui/theme/iconSizes.ts
+++ src/ui/theme/iconSizes.ts
@@ -5,16 +5,9 @@ import {
   EXTRA_SMALL_ICON_SIZE,
 } from './constants'
 
-export type IconSizesType = {
-  standard: number
-  small: number
-  smaller: number
-  extraSmall: number
-}
-
-export const iconSizes: IconSizesType = {
+export const iconSizes = {
   standard: STANDARD_ICON_SIZE,
   small: SMALL_ICON_SIZE,
   smaller: SMALLER_ICON_SIZE,
   extraSmall: EXTRA_SMALL_ICON_SIZE,
-}
+} as const
