f503d7f3e0667377b2f703e3ad39613b45aaf497
Hugo Voisin <hugov@bam.tech>
2023-09-15 16:25:06 +0200

    feat: use new api route to retrieve activity types

--- src/features/identityCheck/api/useActivityTypes.ts
+++ src/features/identityCheck/api/useActivityTypes.ts
@@ -1,22 +1,21 @@
 import { useQuery } from 'react-query'
 
 import { api } from 'api/api'
-import { ProfileOptionsResponse } from 'api/gen'
+import { ActivityTypesResponse } from 'api/gen'
 import { QueryKeys } from 'libs/queryKeys'
 
 const STALE_TIME_PROFILE_OPTIONS = 5 * 60 * 1000
 
 function useActivityTypesResponse() {
-  return useQuery<ProfileOptionsResponse>(
+  return useQuery<ActivityTypesResponse>(
     [QueryKeys.SCHOOL_TYPES],
-    () => api.getNativeV1SubscriptionProfileOptions(),
+    () => api.getNativeV1SubscriptionActivityTypes(),
     { staleTime: STALE_TIME_PROFILE_OPTIONS }
   )
 }
 
 export const useActivityTypes = () => {
   const { data } = useActivityTypesResponse()
-  const schoolTypes = data?.school_types
   const activities = data?.activities
-  return { schoolTypes, activities }
+  return { activities }
 }
