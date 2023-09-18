65917f4a013faf205d4de7dbc2a29e06da5ffc2e
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-09-18 15:09:07 +0200

    (PC-24418) feat(search): add item in search history from suggestion

--- src/features/search/components/AutocompleteOffer/AutocompleteOffer.tsx
+++ src/features/search/components/AutocompleteOffer/AutocompleteOffer.tsx
@@ -3,15 +3,18 @@ import { useInfiniteHits, UseInfiniteHitsProps } from 'react-instantsearch-hooks
 import styled from 'styled-components/native'
 
 import { AutocompleteOfferItem } from 'features/search/components/AutocompleteOfferItem/AutocompleteOfferItem'
+import { HistoryItem } from 'features/search/types'
 import { AlgoliaSuggestionHit } from 'libs/algolia'
 import { Li } from 'ui/components/Li'
 import { VerticalUl } from 'ui/components/Ul'
 import { getSpacing, Typo } from 'ui/theme'
 import { getHeadingAttrs } from 'ui/theme/typographyAttrs/getHeadingAttrs'
 
-type AutocompleteOfferProps = UseInfiniteHitsProps
+type AutocompleteOfferProps = UseInfiniteHitsProps & {
+  addSearchHistory: (item: HistoryItem) => void
+}
 
-export function AutocompleteOffer({ ...props }: AutocompleteOfferProps) {
+export function AutocompleteOffer({ addSearchHistory, ...props }: AutocompleteOfferProps) {
   const { hits, sendEvent } = useInfiniteHits(props)
 
   return hits.length > 0 ? (
@@ -24,6 +27,7 @@ export function AutocompleteOffer({ ...props }: AutocompleteOfferProps) {
             <AutocompleteOfferItem
               hit={item as unknown as AlgoliaSuggestionHit}
               sendEvent={sendEvent}
+              addSearchHistory={addSearchHistory}
               shouldShowCategory={index < 3}
             />
           </Li>
--- src/features/search/components/AutocompleteOfferItem/AutocompleteOfferItem.tsx
+++ src/features/search/components/AutocompleteOfferItem/AutocompleteOfferItem.tsx
@@ -15,7 +15,7 @@ import {
   getSearchGroupsEnumArrayFromNativeCategoryEnum,
   isNativeCategoryOfCategory,
 } from 'features/search/helpers/categoriesHelpers/categoriesHelpers'
-import { SearchState, SearchView } from 'features/search/types'
+import { HistoryItem, SearchState, SearchView } from 'features/search/types'
 import { AlgoliaSuggestionHit } from 'libs/algolia'
 import { env } from 'libs/environment'
 import { useSearchGroupLabel } from 'libs/subcategories'
@@ -26,12 +26,14 @@ import { getSpacing, Typo } from 'ui/theme'
 type AutocompleteOfferItemProps = {
   hit: AlgoliaSuggestionHit
   sendEvent: SendEventForHits
+  addSearchHistory: (item: HistoryItem) => void
   shouldShowCategory?: boolean
 }
 
 export function AutocompleteOfferItem({
   hit,
   sendEvent,
+  addSearchHistory,
   shouldShowCategory,
 }: AutocompleteOfferItemProps) {
   const { query, [env.ALGOLIA_OFFERS_INDEX_NAME]: indexInfos } = hit
@@ -100,6 +102,12 @@ export function AutocompleteOfferItem({
         : undefined,
       offerCategories: shouldShowCategory ? mostPopularCategory : [],
     }
+    addSearchHistory({
+      query,
+      addedDate: new Date().getTime(),
+      nativeCategory: shouldFilteredOnNativeCategory ? nativeCategories[0].value : undefined,
+      category: shouldShowCategory ? mostPopularCategory[0] : undefined,
+    })
 
     navigate(...getTabNavConfig('Search', newSearchState))
   }
