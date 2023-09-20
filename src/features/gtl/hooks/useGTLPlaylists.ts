b5080395a7056695825a5ce03a5a2e909a33f600
dbenfouzari-pass <donovan.benfouzari@passculture.app>
2023-09-20 10:15:40 +0200

    (PC-23615) feat(gtl): add gtl hook

---
+++ src/features/gtl/hooks/useGTLPlaylists.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+
+import { fetchGTLPlaylists, GTLPlaylistResponse } from 'features/gtl/api/gtl-playlist-api'
+import { useHomePosition } from 'features/home/helpers/useHomePosition'
+import { useIsUserUnderage } from 'features/profile/helpers/useIsUserUnderage'
+
+export function useGTLPlaylists() {
+  const { position } = useHomePosition()
+  const isUserUnderage = useIsUserUnderage()
+  const [gtlPlaylists, setGtlPlaylists] = useState<GTLPlaylistResponse>([])
+
+  useEffect(() => {
+    fetchGTLPlaylists({ position, isUserUnderage }).then((response) => {
+      setGtlPlaylists(response)
+    })
+  }, [isUserUnderage, position])
+
+  return gtlPlaylists
+}
