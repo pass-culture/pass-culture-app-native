6d3eeb3da9e787eb09cc33df2f9e39ba292772e1
Hugo Voisin <hugov@bam.tech>
2023-09-22 11:56:36 +0200

    test: fix tests

--- src/libs/geolocation/__mocks__/index.ts
+++ src/libs/geolocation/__mocks__/index.ts
@@ -12,6 +12,12 @@ export const triggerPositionUpdate = jest.fn()
 export const showGeolocPermissionModal = jest.fn()
 export const onPressGeolocPermissionModalButton = jest.fn()
 export const setCustomPosition = jest.fn()
+const initialize = jest.fn()
+const isCurrentLocationMode = jest.fn().mockReturnValue(true)
+const runGeolocationDialogs = jest.fn()
+const saveAllPositionChanges = jest.fn()
+const setPlace = jest.fn()
+const setSelectedOption = jest.fn()
 
 const locationContext: ILocationContext = {
   userPosition: { longitude: 90, latitude: 90 },
@@ -23,6 +29,16 @@ const locationContext: ILocationContext = {
   triggerPositionUpdate,
   showGeolocPermissionModal,
   onPressGeolocPermissionModalButton,
+  getLocationTitle: () => 'Ma position',
+  initialize,
+  isCurrentLocationMode,
+  isGeolocated: true,
+  noPlace: false,
+  runGeolocationDialogs,
+  saveAllPositionChanges,
+  onModalHideRef: { current: undefined },
+  setPlace,
+  setSelectedOption,
 }
 
 export const useLocation = jest.fn().mockReturnValue(locationContext)
