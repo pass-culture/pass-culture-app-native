a42de2caa9f2129d8e572b4eb1aa0035d2f6b904
Hugo Voisin <hugov@bam.tech>
2023-09-27 18:26:31 +0200

    style: fix typo in comment

--- src/ui/theme/useCustomSafeInsets.tsx
+++ src/ui/theme/useCustomSafeInsets.tsx
@@ -5,7 +5,7 @@ import { TAB_BAR_COMP_HEIGHT } from './constants'
 /**
  * Hook used to adapt useSafeAreaInsets to the applications needs
  * We only want to use 50% of what the libs calls a safe bottom inset
- * We compute the total height of the navbar here aswell
+ * We compute the total height of the navbar here as well
  */
 export const useCustomSafeInsets = () => {
   const { bottom, top } = useSafeAreaInsets()
