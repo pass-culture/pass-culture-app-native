dcf60a035c13fa3d7f0589f3e6b7c08b79c47694
marineshaw <marines@bam.tech>
2023-10-02 17:29:26 +0200

    :recycle: (ContentHeader) use new component where it is necessary

--- src/features/bookings/components/BookingDetailsHeader.tsx
+++ src/features/bookings/components/BookingDetailsHeader.tsx
@@ -1,96 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Animated } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import styled, { useTheme } from 'styled-components/native'
 
 import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
 import { useGoBack } from 'features/navigation/useGoBack'
-import { getAnimationState } from 'ui/animations/helpers/getAnimationState'
-import { RoundedButton } from 'ui/components/buttons/RoundedButton'
-import { AnimatedBlurHeader } from 'ui/components/headers/AnimatedBlurHeader'
-import { getSpacing, Spacer, Typo } from 'ui/theme'
+import { ContentHeader } from 'ui/components/headers/ContentHeader'
 
 interface Props {
   headerTransition: Animated.AnimatedInterpolation<string | number>
   title: string
 }
-/**
- * @param props.headerTransition should be between animated between 0 and 1
- */
 
 export const BookingDetailsHeader: React.FC<Props> = (props) => {
-  const theme = useTheme()
   const { headerTransition, title } = props
-  const { top } = useSafeAreaInsets()
-  const headerHeight = theme.appBarHeight + top
 
   const { goBack } = useGoBack(...getTabNavConfig('Bookings'))
 
-  const [accessibilityHiddenTitle, setAccessibilityHiddenTitle] = useState(true)
-  headerTransition.addListener((opacity) => setAccessibilityHiddenTitle(opacity.value !== 1))
-
-  const { animationState, containerStyle, blurContainerNative } = getAnimationState(
-    theme,
-    headerTransition
-  )
-
   return (
-    <React.Fragment>
-      <HeaderContainer style={containerStyle}>
-        <Spacer.TopScreen />
-        <AnimatedBlurHeader height={headerHeight} style={blurContainerNative} />
-        <Spacer.Column numberOfSpaces={2} />
-        <Row>
-          <IconContainer>
-            <RoundedButton
-              animationState={animationState}
-              iconName="back"
-              onPress={goBack}
-              accessibilityLabel="Revenir en arrière"
-            />
-          </IconContainer>
-          <Spacer.Flex />
-          <Title
-            style={{ opacity: headerTransition }}
-            accessibilityHidden={accessibilityHiddenTitle}>
-            <StyledBody>{title}</StyledBody>
-          </Title>
-          <Spacer.Flex />
-          <IconContainer />
-        </Row>
-        <Spacer.Column numberOfSpaces={2} />
-      </HeaderContainer>
-    </React.Fragment>
+    <ContentHeader headerTitle={title} headerTransition={headerTransition} onBackPress={goBack} />
   )
 }
-
-const HeaderContainer = styled(Animated.View)(({ theme }) => ({
-  position: 'absolute',
-  top: 0,
-  width: '100%',
-  zIndex: theme.zIndex.header,
-  borderBottomColor: theme.colors.greyLight,
-  borderBottomWidth: 1,
-}))
-
-const Row = styled.View({
-  flexDirection: 'row',
-  alignItems: 'center',
-})
-
-const IconContainer = styled.View(({ theme }) => ({
-  marginLeft: getSpacing(3),
-  minWidth: theme.icons.sizes.standard,
-}))
-
-const Title = styled(Animated.Text).attrs({
-  numberOfLines: 2,
-})({
-  textAlign: 'center',
-  flex: 5,
-  marginHorizontal: getSpacing(3),
-})
-
-const StyledBody = styled(Typo.Body)(({ theme }) => ({
-  color: theme.colors.black,
-}))
--- src/features/venue/components/VenueHeader/VenueHeader.tsx
+++ src/features/venue/components/VenueHeader/VenueHeader.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Animated } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import styled, { useTheme } from 'styled-components/native'
+import { useTheme } from 'styled-components/native'
 
 import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
 import { useGoBack } from 'features/navigation/useGoBack'
@@ -10,9 +9,8 @@ import { WebShareModal } from 'features/share/pages/WebShareModal'
 import { analytics } from 'libs/analytics'
 import { getAnimationState } from 'ui/animations/helpers/getAnimationState'
 import { RoundedButton } from 'ui/components/buttons/RoundedButton'
-import { AnimatedBlurHeader } from 'ui/components/headers/AnimatedBlurHeader'
+import { ContentHeader } from 'ui/components/headers/ContentHeader'
 import { useModal } from 'ui/components/modals/useModal'
-import { Spacer, Typo } from 'ui/theme'
 
 interface Props {
   headerTransition: Animated.AnimatedInterpolation<string | number>
@@ -41,50 +39,24 @@ export const VenueHeader: React.FC<Props> = (props) => {
     showShareVenueModal()
   }
 
-  const [ariaHiddenTitle, setAriaHiddenTitle] = useState(true)
-  headerTransition.addListener((opacity) => setAriaHiddenTitle(opacity.value !== 1))
-
-  const { animationState, containerStyle, blurContainerNative } = getAnimationState(
-    theme,
-    headerTransition
-  )
-  const { top } = useSafeAreaInsets()
-  const headerHeight = theme.appBarHeight + top
+  const { animationState } = getAnimationState(theme, headerTransition)
 
   return (
     <React.Fragment>
-      <HeaderContainer style={containerStyle} height={headerHeight}>
-        <Spacer.TopScreen />
-        <AnimatedBlurHeader height={headerHeight} style={blurContainerNative} />
-        <Spacer.Column numberOfSpaces={2} />
-        <Row>
-          <Spacer.Row numberOfSpaces={6} />
-          <RoundedButton
-            animationState={animationState}
-            iconName="back"
-            onPress={goBack}
-            accessibilityLabel="Revenir en arrière"
-          />
-          <Spacer.Flex />
-
-          <Title
-            testID="venueHeaderName"
-            style={{ opacity: headerTransition }}
-            accessibilityHidden={ariaHiddenTitle}>
-            <Body>{title}</Body>
-          </Title>
-
-          <Spacer.Flex />
+      <ContentHeader
+        headerTitle={title}
+        headerTransition={headerTransition}
+        onBackPress={goBack}
+        titleTestID="venueHeaderName"
+        RightElement={
           <RoundedButton
             animationState={animationState}
             iconName="share"
             onPress={onSharePress}
             accessibilityLabel="Partager"
           />
-          <Spacer.Row numberOfSpaces={6} />
-        </Row>
-        <Spacer.Column numberOfSpaces={2} />
-      </HeaderContainer>
+        }
+      />
       {shareContent ? (
         <WebShareModal
           visible={shareVenueModalVisible}
@@ -96,31 +68,3 @@ export const VenueHeader: React.FC<Props> = (props) => {
     </React.Fragment>
   )
 }
-
-const Body = styled(Typo.Body)(({ theme }) => ({
-  color: theme.colors.black,
-}))
-
-const HeaderContainer = styled(Animated.View)<{ height: number }>(({ theme, height }) => ({
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  right: 0,
-  height,
-  zIndex: theme.zIndex.header,
-  borderBottomColor: theme.colors.greyLight,
-  borderBottomWidth: 1,
-}))
-
-const Row = styled.View({
-  flex: 1,
-  flexDirection: 'row',
-  alignItems: 'center',
-})
-
-const Title = styled(Animated.Text).attrs({
-  numberOfLines: 2,
-})({
-  flexShrink: 1,
-  textAlign: 'center',
-})
