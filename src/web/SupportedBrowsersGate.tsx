116c1adc0b0defd3ef5b218231c6e81a015a9ba6
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-10-04 10:30:16 +0200

    (PC-21434) refactor(SupportedBrowsers): Place constants inside component to be able to mock them

--- src/web/SupportedBrowsersGate.tsx
+++ src/web/SupportedBrowsersGate.tsx
@@ -9,7 +9,11 @@ import { Li } from 'ui/components/Li'
 import { VerticalUl } from 'ui/components/Ul'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 
-const browserVersion = Number(DeviceDetect.browserVersion)
+type SupportedBrowsers = Array<{
+  browser: string
+  active: boolean
+  version: number
+}>
 
 // To check if browser is in-app Facebook Messenger browser, we check if FBAV or FBAN is in user agent
 // https://stackoverflow.com/a/32348687
@@ -17,37 +21,42 @@ const messengerBrowserRegex = new RegExp(/FBA[VN]/i)
 const isFacebookMessenger = !!messengerBrowserRegex.exec(navigator?.userAgent)
 
 export const SupportedBrowsersGate: React.FC<PropsWithChildren> = ({ children }) => {
-  const [shouldDisplayApp, setShouldDisplayApp] = React.useState(() => isBrowserSupported())
+  const browserVersion = Number(DeviceDetect.browserVersion)
+  const supportedBrowsers: SupportedBrowsers = [
+    { browser: 'Facebook Messenger', active: isFacebookMessenger, version: 1 },
+    { browser: 'Chrome', active: DeviceDetect.isChrome, version: 50 },
+    { browser: 'Safari sur iOS', active: DeviceDetect.isMobileSafari, version: 12 },
+    { browser: 'Safari sur macOS', active: DeviceDetect.isSafari, version: 10 },
+    { browser: 'Firefox', active: DeviceDetect.isFirefox, version: 55 },
+    { browser: 'Edge sur Windows', active: DeviceDetect.isEdge, version: 79 },
+    { browser: 'Opera', active: DeviceDetect.isOpera && DeviceDetect.isDesktop, version: 80 },
+    { browser: 'Samsung', active: DeviceDetect.isSamsungBrowser, version: 5 },
+    { browser: 'Instagram', active: DeviceDetect.browserName === 'Instagram', version: 200 },
+    { browser: 'Brave', active: DeviceDetect.browserName === 'Brave', version: 98 },
+    {
+      browser: 'Les navigateurs basés sur Chromium (autre que Chrome et Brave)',
+      active: DeviceDetect.isChromium,
+      version: 0,
+    },
+  ]
+
+  const [shouldDisplayApp, setShouldDisplayApp] = React.useState(() =>
+    isBrowserSupported(browserVersion, supportedBrowsers)
+  )
 
   if (!shouldDisplayApp) {
-    return <BrowserNotSupportedPage onPress={() => setShouldDisplayApp(true)} />
+    return (
+      <BrowserNotSupportedPage
+        browserVersion={browserVersion}
+        supportedBrowsers={supportedBrowsers}
+        onPress={() => setShouldDisplayApp(true)}
+      />
+    )
   }
   return <React.Fragment>{children}</React.Fragment>
 }
 
-const supportedBrowsers: Array<{
-  browser: string
-  active: boolean
-  version: number
-}> = [
-  { browser: 'Facebook Messenger', active: isFacebookMessenger, version: 1 },
-  { browser: 'Chrome', active: DeviceDetect.isChrome, version: 50 },
-  { browser: 'Safari sur iOS', active: DeviceDetect.isMobileSafari, version: 12 },
-  { browser: 'Safari sur macOS', active: DeviceDetect.isSafari, version: 10 },
-  { browser: 'Firefox', active: DeviceDetect.isFirefox, version: 55 },
-  { browser: 'Edge sur Windows', active: DeviceDetect.isEdge, version: 79 },
-  { browser: 'Opera', active: DeviceDetect.isOpera && DeviceDetect.isDesktop, version: 80 },
-  { browser: 'Samsung', active: DeviceDetect.isSamsungBrowser, version: 5 },
-  { browser: 'Instagram', active: DeviceDetect.browserName === 'Instagram', version: 200 },
-  { browser: 'Brave', active: DeviceDetect.browserName === 'Brave', version: 98 },
-  {
-    browser: 'Les navigateurs basés sur Chromium (autre que Chrome et Brave)',
-    active: DeviceDetect.isChromium,
-    version: 0,
-  },
-]
-
-function isBrowserSupported() {
+function isBrowserSupported(browserVersion: number, supportedBrowsers: SupportedBrowsers) {
   for (const supportedBrowser of supportedBrowsers) {
     if (supportedBrowser.active && browserVersion >= supportedBrowser.version) {
       return true
@@ -56,7 +65,11 @@ function isBrowserSupported() {
   return false
 }
 
-export const BrowserNotSupportedPage: React.FC<{ onPress: () => void }> = ({ onPress }) => {
+export const BrowserNotSupportedPage: React.FC<{
+  browserVersion: number
+  supportedBrowsers: SupportedBrowsers
+  onPress: () => void
+}> = ({ browserVersion, supportedBrowsers, onPress }) => {
   const title = `Oups\u00a0! Nous ne pouvons afficher correctement l’application car ton navigateur (${DeviceDetect.browserName} v${browserVersion}) n’est pas à jour`
   return (
     <ScrollView contentContainerStyle={contentContainerStyle}>
