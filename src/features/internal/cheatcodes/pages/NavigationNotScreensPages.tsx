fb98d409c553ecbed97b41fe85dd23d71e31575c
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-10-04 13:00:03 +0200

    (PC-21434) fix(web): Fix webapp on Brave (#5373)
    * (PC-21434) fix(theme): Nest supported browser page inside theme provider since it uses styled components
    
    * (PC-21434) feat(web): Add Brave to supported browsers
    
    * (PC-21434) refactor(SupportedBrowsers): Place constants inside component to be able to mock them
    
    * (PC-21434) test(BrowsersGate): Add test for supported browsers

--- src/features/internal/cheatcodes/pages/NavigationNotScreensPages.tsx
+++ src/features/internal/cheatcodes/pages/NavigationNotScreensPages.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+// eslint-disable-next-line no-restricted-imports
+import * as DeviceDetect from 'react-device-detect'
 import { ScrollView } from 'react-native'
 import styled from 'styled-components/native'
 
@@ -16,8 +18,34 @@ const doNothing = () => {
   /* do nothing */
 }
 
+const messengerBrowserRegex = new RegExp(/FBA[VN]/i)
+const isFacebookMessenger = !!messengerBrowserRegex.exec(navigator?.userAgent)
+const browserVersion = Number(DeviceDetect.browserVersion)
+const supportedBrowsers = [
+  { browser: 'Facebook Messenger', active: isFacebookMessenger, version: 1 },
+  { browser: 'Chrome', active: DeviceDetect.isChrome, version: 50 },
+  { browser: 'Safari sur iOS', active: DeviceDetect.isMobileSafari, version: 12 },
+  { browser: 'Safari sur macOS', active: DeviceDetect.isSafari, version: 10 },
+  { browser: 'Firefox', active: DeviceDetect.isFirefox, version: 55 },
+  { browser: 'Edge sur Windows', active: DeviceDetect.isEdge, version: 79 },
+  { browser: 'Opera', active: DeviceDetect.isOpera && DeviceDetect.isDesktop, version: 80 },
+  { browser: 'Samsung', active: DeviceDetect.isSamsungBrowser, version: 5 },
+  { browser: 'Instagram', active: DeviceDetect.browserName === 'Instagram', version: 200 },
+  { browser: 'Brave', active: DeviceDetect.browserName === 'Brave', version: 98 },
+  {
+    browser: 'Les navigateurs basés sur Chromium (autre que Chrome et Brave)',
+    active: DeviceDetect.isChromium,
+    version: 0,
+  },
+]
 const mapPageToComponent: Record<Page, React.JSX.Element> = {
-  [Page.BrowserNotSupportedPage]: <BrowserNotSupportedPage onPress={doNothing} />,
+  [Page.BrowserNotSupportedPage]: (
+    <BrowserNotSupportedPage
+      onPress={doNothing}
+      browserVersion={browserVersion}
+      supportedBrowsers={supportedBrowsers}
+    />
+  ),
 }
 
 export function NavigationNotScreensPages(): React.JSX.Element {
--- src/web/SupportedBrowsersGate.native.test.tsx
+++ src/web/SupportedBrowsersGate.native.test.tsx
@@ -1,11 +1,72 @@
 import React from 'react'
 
-import { render } from 'tests/utils'
+import { render, screen } from 'tests/utils'
 import { SupportedBrowsersGate } from 'web/SupportedBrowsersGate'
+
+const defaultDeviceMock: Record<string, unknown> = {
+  __esModule: true,
+  isChrome: false,
+  isMobileSafari: false,
+  isSafari: false,
+  isFirefox: false,
+  isEdge: false,
+  isSamsungBrowser: false,
+  browserName: 'none',
+}
+jest.mock('react-device-detect', () => defaultDeviceMock)
 
 describe('SupportedBrowsersGate', () => {
   it('render correctly', () => {
     const renderAPI = render(<SupportedBrowsersGate />)
     expect(renderAPI).toMatchSnapshot()
+  })
+
+  describe.each`
+    browserProperty       | browserName        | minimalSupportedVersion
+    ${'isChrome'}         | ${'Chrome'}        | ${50}
+    ${'isMobileSafari'}   | ${'Mobile Safari'} | ${12}
+    ${'isSafari'}         | ${'Safari'}        | ${10}
+    ${'isFirefox'}        | ${'Firefox'}       | ${55}
+    ${'isEdge'}           | ${'Edge'}          | ${79}
+    ${'isSamsungBrowser'} | ${'Samsung'}       | ${5}
+    ${'isInstagram'}      | ${'Instagram'}     | ${200}
+    ${'isBrave'}          | ${'Brave'}         | ${98}
+    ${'isChromium'}       | ${'Chromium'}      | ${0}
+  `('', ({ browserProperty, browserName, minimalSupportedVersion }) => {
+    beforeAll(() => {
+      defaultDeviceMock[browserProperty] = true
+      defaultDeviceMock.browserName = browserName
+    })
+
+    afterAll(() => {
+      defaultDeviceMock[browserProperty] = false
+      defaultDeviceMock.browserName = 'none'
+      defaultDeviceMock.browserVersion = undefined
+    })
+
+    it(`should support ${browserName} for versions ${minimalSupportedVersion} and above`, () => {
+      defaultDeviceMock.browserVersion = minimalSupportedVersion
+
+      render(<SupportedBrowsersGate />)
+
+      expect(
+        screen.queryByText(
+          `Oups ! Nous ne pouvons afficher correctement l’application car ton navigateur (${browserName} v${minimalSupportedVersion}) n’est pas à jour`
+        )
+      ).not.toBeOnTheScreen()
+    })
+
+    it(`should not support ${browserName} for versions below ${minimalSupportedVersion}`, () => {
+      const unsupportedVersion = minimalSupportedVersion - 1
+      defaultDeviceMock.browserVersion = unsupportedVersion
+
+      render(<SupportedBrowsersGate />)
+
+      expect(
+        screen.getByText(
+          `Oups ! Nous ne pouvons afficher correctement l’application car ton navigateur (${browserName} v${unsupportedVersion}) n’est pas à jour`
+        )
+      ).toBeOnTheScreen()
+    })
   })
 })
