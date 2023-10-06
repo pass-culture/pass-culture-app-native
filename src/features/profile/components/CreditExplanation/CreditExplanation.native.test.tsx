e00f22394f70ac093d60efeebd229609ec5aeaff
marineshaw <marines@bam.tech>
2023-10-06 10:23:24 +0200

    :recycle: (CreditExplanation) Use screen in test

--- src/features/profile/components/CreditExplanation/CreditExplanation.native.test.tsx
+++ src/features/profile/components/CreditExplanation/CreditExplanation.native.test.tsx
@@ -7,67 +7,61 @@ import {
   domains_exhausted_credit_v1,
 } from 'features/profile/fixtures/domainsCredit'
 import { analytics } from 'libs/analytics'
-import { fireEvent, render } from 'tests/utils'
+import { fireEvent, render, screen } from 'tests/utils'
 
 describe('<CreditExplanation/>', () => {
   it('should render correctly for expired deposit', () => {
-    const renderAPI = render(
-      <CreditExplanation isDepositExpired domainsCredit={domains_credit_v1} age={18} />
-    )
-    expect(renderAPI).toMatchSnapshot()
+    render(<CreditExplanation isDepositExpired domainsCredit={domains_credit_v1} age={18} />)
+    expect(screen).toMatchSnapshot()
   })
 
   it('should render correctly for exhausted credit', () => {
-    const renderAPI = render(
+    render(
       <CreditExplanation
         isDepositExpired={false}
         domainsCredit={domains_exhausted_credit_v1}
         age={18}
       />
     )
-    expect(renderAPI).toMatchSnapshot()
+    expect(screen).toMatchSnapshot()
   })
 
   it('should render correctly for valid credit', () => {
-    const renderAPI = render(
+    render(
       <CreditExplanation isDepositExpired={false} domainsCredit={domains_credit_v1} age={18} />
     )
-    expect(renderAPI).toMatchSnapshot()
+    expect(screen).toMatchSnapshot()
   })
 
   describe('With modal', () => {
     it('should not display modal if button is not triggered', () => {
-      const { queryByTestId } = render(
-        <CreditExplanation isDepositExpired domainsCredit={domains_credit_v1} age={18} />
-      )
-      expect(queryByTestId('modalHeader')).not.toBeOnTheScreen()
+      render(<CreditExplanation isDepositExpired domainsCredit={domains_credit_v1} age={18} />)
+      expect(screen.queryByTestId('modalHeader')).not.toBeOnTheScreen()
     })
 
     it('should display modal when button is triggered', () => {
-      const { getByTestId, queryByTestId } = render(
-        <CreditExplanation isDepositExpired domainsCredit={domains_credit_v1} age={18} />
-      )
-      const explanationButton = getByTestId('Mon crédit est expiré, que\u00a0faire\u00a0?')
+      render(<CreditExplanation isDepositExpired domainsCredit={domains_credit_v1} age={18} />)
+      const explanationButton = screen.getByTestId('Mon crédit est expiré, que\u00a0faire\u00a0?')
       fireEvent.press(explanationButton)
-      expect(queryByTestId('modalHeader')).toBeOnTheScreen()
+      expect(screen.queryByTestId('modalHeader')).toBeOnTheScreen()
     })
   })
 
   describe('With redirection to tutorial', () => {
     it('should navigate to tutorial when button is triggered', () => {
-      const { getByTestId } = render(
+      render(
         <CreditExplanation isDepositExpired={false} domainsCredit={domains_credit_v1} age={18} />
       )
-      const explanationButton = getByTestId('Comment ça marche\u00a0?')
+      const explanationButton = screen.getByTestId('Comment ça marche\u00a0?')
       fireEvent.press(explanationButton)
       expect(navigate).toHaveBeenCalledWith('ProfileTutorialAgeInformation', { age: 18 })
     })
 
     it('should navigate to 17 years old tutorial when button is triggered and user is 17', () => {
-      const { getByTestId } = render(
+      render(
         <CreditExplanation isDepositExpired={false} domainsCredit={domains_credit_v1} age={17} />
       )
-      const explanationButton = getByTestId('Comment ça marche\u00a0?')
+      const explanationButton = screen.getByTestId('Comment ça marche\u00a0?')
       fireEvent.press(explanationButton)
       expect(navigate).toHaveBeenCalledWith('ProfileTutorialAgeInformation', { age: 17 })
     })
@@ -75,18 +69,16 @@ describe('<CreditExplanation/>', () => {
 
   describe('Analytics', () => {
     it('should log logConsultModalExpiredGrant analytics when expired deposit and press the button "Mon crédit est expiré, que faire ?"', () => {
-      const { getByText } = render(
-        <CreditExplanation isDepositExpired domainsCredit={domains_credit_v1} age={18} />
-      )
+      render(<CreditExplanation isDepositExpired domainsCredit={domains_credit_v1} age={18} />)
 
-      const explanationButton = getByText('Mon crédit est expiré, que faire ?')
+      const explanationButton = screen.getByText('Mon crédit est expiré, que faire ?')
       fireEvent.press(explanationButton)
 
       expect(analytics.logConsultModalExpiredGrant).toHaveBeenCalledTimes(1)
     })
 
     it('should log logConsultModalNoMoreCredit analytics when exhausted credit and press the button "J’ai dépensé tout mon crédit, que faire ?"', () => {
-      const { getByText } = render(
+      render(
         <CreditExplanation
           isDepositExpired={false}
           domainsCredit={domains_exhausted_credit_v1}
@@ -94,7 +86,7 @@ describe('<CreditExplanation/>', () => {
         />
       )
 
-      const explanationButton = getByText('J’ai dépensé tout mon crédit, que faire ?')
+      const explanationButton = screen.getByText('J’ai dépensé tout mon crédit, que faire ?')
       fireEvent.press(explanationButton)
 
       expect(analytics.logConsultModalNoMoreCredit).toHaveBeenCalledTimes(1)
