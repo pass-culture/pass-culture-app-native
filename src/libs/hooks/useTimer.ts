56c244ae0a09e6a4a36786d699556cdb3ec46b1a
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-10-06 17:36:25 +0200

    (PC-24892) feat(timer): Update timer on app resume (#5397)

--- src/libs/hooks/useTimer.ts
+++ src/libs/hooks/useTimer.ts
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react'
 
+import { useAppStateChange } from 'libs/appState'
+
+let appInBackgroundTime: null | number = null
+
 export const useTimer = (initialValueInSeconds: number) => {
   const [timeLeft, setTimeLeft] = useState(initialValueInSeconds)
 
@@ -13,6 +17,18 @@ export const useTimer = (initialValueInSeconds: number) => {
 
     return () => clearInterval(timerId)
   })
+
+  useAppStateChange(
+    () => {
+      if (appInBackgroundTime) {
+        const elapsedTimeInSeconds = Math.floor((Date.now() - appInBackgroundTime) / 1000)
+        setTimeLeft((timeLeft) => Math.max(timeLeft - elapsedTimeInSeconds, 0))
+      }
+    },
+    () => {
+      appInBackgroundTime = Date.now()
+    }
+  )
 
   return { timeLeft, setTimeLeft }
 }
