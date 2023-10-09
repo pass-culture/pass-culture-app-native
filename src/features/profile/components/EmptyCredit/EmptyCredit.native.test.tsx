a62aea1453de4b08849be848b8fbcbff22965c42
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-10-09 09:53:21 +0200

    (PC-24798) feat(credit): Redirect to thematic home with free offers

--- src/features/profile/components/EmptyCredit/EmptyCredit.native.test.tsx
+++ src/features/profile/components/EmptyCredit/EmptyCredit.native.test.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
+import { navigate } from '__mocks__/@react-navigation/native'
 import { EmptyCredit } from 'features/profile/components/EmptyCredit/EmptyCredit'
-import { render, screen } from 'tests/utils'
+import { fireEvent, render, screen } from 'tests/utils'
+
+jest.mock('libs/firebase/remoteConfig/RemoteConfigProvider', () => ({
+  useRemoteConfigContext: jest
+    .fn()
+    .mockReturnValue({ homeEntryIdFreeOffers: 'homeEntryIdFreeOffers' }),
+}))
 
 describe('<EmptyCredit />', () => {
   it.each([15, 16, 17, 18])('should render correctly for %s yo', (age) => {
     render(<EmptyCredit age={age} />)
 
     expect(screen).toMatchSnapshot()
+  })
+
+  it('should navigate to thematic home with remote config homeId on button press', () => {
+    render(<EmptyCredit age={16} />)
+
+    fireEvent.press(screen.getByText('Profite d’offres gratuites'))
+
+    expect(navigate).toHaveBeenCalledWith('ThematicHome', {
+      homeId: 'homeEntryIdFreeOffers',
+      from: 'EmptyCredit',
+    })
   })
 })
--- src/features/profile/components/EmptyCredit/EmptyCredit.tsx
+++ src/features/profile/components/EmptyCredit/EmptyCredit.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
+import { useRemoteConfigContext } from 'libs/firebase/remoteConfig'
 import { useDepositAmountsByAge } from 'shared/user/useDepositAmountsByAge'
-import { Typo } from 'ui/theme'
+import { ButtonWithLinearGradient } from 'ui/components/buttons/buttonWithLinearGradient/ButtonWithLinearGradient'
+import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
+import { Offers } from 'ui/svg/icons/Offers'
+import { Spacer, Typo } from 'ui/theme'
 
 export const EmptyCredit = ({ age }: { age: number }) => {
+  const { homeEntryIdFreeOffers } = useRemoteConfigContext()
   const { sixteenYearsOldDeposit, seventeenYearsOldDeposit, eighteenYearsOldDeposit } =
     useDepositAmountsByAge()
 
@@ -17,13 +22,29 @@ export const EmptyCredit = ({ age }: { age: number }) => {
   if (!incomingCreditMap[age]) return null
 
   return (
-    <Typo.Body>
-      Ton prochain crédit de <HighlightedBody>{incomingCreditMap[age]}</HighlightedBody> sera
-      débloqué à {age + 1} ans. En attendant…
-    </Typo.Body>
+    <React.Fragment>
+      <Typo.Body>
+        Ton prochain crédit de <HighlightedBody>{incomingCreditMap[age]}</HighlightedBody> sera
+        débloqué à {age + 1} ans. En attendant…
+      </Typo.Body>
+      <Spacer.Column numberOfSpaces={4} />
+      <InternalTouchableLink
+        as={ButtonWithLinearGradient}
+        wording="Profite d’offres gratuites"
+        navigateTo={{
+          screen: 'ThematicHome',
+          params: { homeId: homeEntryIdFreeOffers, from: 'EmptyCredit' },
+        }}
+        icon={WhiteOffers}
+      />
+    </React.Fragment>
   )
 }
 
 const HighlightedBody = styled(Typo.Body)(({ theme }) => ({
   color: theme.colors.secondary,
 }))
+
+const WhiteOffers = styled(Offers).attrs(({ theme }) => ({
+  color: theme.colors.white,
+}))``
