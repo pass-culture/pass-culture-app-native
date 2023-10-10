2b74570b10b02d3c543ef7ff56363a8141941129
Dimitri Kopriwa <kopx@free.fr>
2023-10-10 11:41:45 +0200

    (PC-24938) feat(proxy): expose proxy version (#5406) [revert]
    This reverts commit 81d47c7b7a5da1937d4d6011b48963f919fb74d7.

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,6 @@ import { dirname, join } from 'path'
 import express from 'express'
 
 import { webAppProxyMiddleware } from './middlewares/webAppProxyMiddleware'
-import { getVersion } from './routes/version'
 
 export const app = express()
 
@@ -12,11 +11,10 @@ const options = {
   index: false,
 }
 
-const rootFolder = dirname(__dirname)
+const rootFolder = dirname(dirname(__dirname))
 
 // We want to serve static files from the root folder /public
 // https://expressjs.com/en/4x/api.html#express.static
 app.use(express.static(join(rootFolder, 'public'), options))
 
-app.get('/version.txt', getVersion)
 app.use('*', webAppProxyMiddleware)
