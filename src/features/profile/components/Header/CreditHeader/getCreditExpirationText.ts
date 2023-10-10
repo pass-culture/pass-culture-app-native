2f4df955598f51c2e8aaab4c6a3449793bea957e
marineshaw <marines@bam.tech>
2023-10-10 11:31:06 +0200

    :sparkles: (CreditHeader) Create helper to get credit expiration text

---
+++ src/features/profile/components/Header/CreditHeader/getCreditExpirationText.ts
@@ -0,0 +1,20 @@
+import { differenceInDays } from 'date-fns'
+
+import { plural } from 'libs/plural'
+
+export const getCreditExpirationText = (
+  depositExpirationDate: Date,
+  baseDate: Date = new Date()
+): string | undefined => {
+  const daysLeft = differenceInDays(depositExpirationDate, baseDate)
+
+  if (daysLeft > 7 || daysLeft < 0) return
+
+  if (daysLeft === 0)
+    return 'Ton crédit sera remis à 0 aujourd’hui. Profite de ton crédit restant\u00a0!'
+
+  return `Ton crédit sera remis à 0 dans ${daysLeft} ${plural(daysLeft, {
+    one: 'jour',
+    other: 'jours',
+  })}. Profite de ton crédit restant\u00a0!`
+}
