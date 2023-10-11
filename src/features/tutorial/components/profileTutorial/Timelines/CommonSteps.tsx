5d65160c645b94c24f840ed72032d44f45a7de19
Marine Shaw <68000368+marineshaw@users.noreply.github.com>
2023-10-11 15:12:56 +0200

    (PC-25039) feat(ProfileTutorial): add spacers around eighteen separator Step (#5433)
    * (PC-25039) feat(ProfileTutorial): add spacers around eighteen separator step
    
    * :sparkles: (ProfileTutorial) Add spacer on last step too

--- src/features/tutorial/components/profileTutorial/Timelines/CommonSteps.tsx
+++ src/features/tutorial/components/profileTutorial/Timelines/CommonSteps.tsx
@@ -5,6 +5,7 @@ import { CreditComponentProps } from 'features/tutorial/components/CreditTimelin
 import { InformationStepContent } from 'features/tutorial/components/profileTutorial/InformationStepContent'
 import { Warning } from 'ui/svg/icons/BicolorWarning'
 import { Offers } from 'ui/svg/icons/Offers'
+import { Spacer } from 'ui/theme'
 
 const GreyOffers = styled(Offers).attrs(({ theme }) => ({
   color: theme.colors.greySemiDark,
@@ -18,7 +19,11 @@ export const eighteenSeparatorStep: CreditComponentProps = {
   creditStep: 'information',
   iconComponent: <GreyWarning />,
   children: (
-    <InformationStepContent title="La veille de tes 18 ans" subtitle="Ton crédit est remis à 0" />
+    <React.Fragment>
+      <Spacer.Column numberOfSpaces={6} />
+      <InformationStepContent title="La veille de tes 18 ans" subtitle="Ton crédit est remis à 0" />
+      <Spacer.Column numberOfSpaces={6} />
+    </React.Fragment>
   ),
 }
 
@@ -26,9 +31,12 @@ export const lastStep: CreditComponentProps = {
   creditStep: 'information',
   iconComponent: <GreyOffers />,
   children: (
-    <InformationStepContent
-      title="Au bout de 2 ans, ton crédit expire… Mais l’aventure continue&nbsp;!"
-      subtitle="Tu peux continuer à réserver des offres gratuites autour de chez toi."
-    />
+    <React.Fragment>
+      <Spacer.Column numberOfSpaces={6} />
+      <InformationStepContent
+        title="Au bout de 2 ans, ton crédit expire… Mais l’aventure continue&nbsp;!"
+        subtitle="Tu peux continuer à réserver des offres gratuites autour de chez toi."
+      />
+    </React.Fragment>
   ),
 }
