77eff5fdf3d261b56148f4188d5851f4e402a257
Tristan Maurier <tristan.maurier.pro@gmail.com>
2023-10-12 15:07:39 +0200

    create venue section

---
+++ src/features/search/components/sections/Venue/Venue.native.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import { Venue } from 'features/search/components/sections/Venue/Venue'
+import { initialSearchState } from 'features/search/context/reducer'
+import { LocationType } from 'features/search/enums'
+import { act, fireEvent, render, screen } from 'tests/utils'
+
+let mockSearchState = initialSearchState
+
+jest.mock('features/search/context/SearchWrapper', () => ({
+  useSearch: () => ({
+    searchState: mockSearchState,
+    dispatch: jest.fn(),
+  }),
+}))
+
+describe('Venue component', () => {
+  it('should display the venue label when a venue is selected', async () => {
+    mockSearchState = {
+      ...initialSearchState,
+      locationFilter: {
+        locationType: LocationType.VENUE,
+        venue: { label: 'Venue label', info: 'info', venueId: 123 },
+      },
+    }
+    render(<Venue />)
+
+    await act(async () => {})
+
+    expect(await screen.findByText('Venue label')).toBeOnTheScreen()
+  })
+
+  it('should open venue modal when clicking on the venue category button', async () => {
+    render(<Venue />, {
+      theme: { isDesktopViewport: false, isMobileViewport: true },
+    })
+    const searchVenueButton = screen.getByTestId('FilterRow')
+
+    await act(async () => {
+      fireEvent.press(searchVenueButton)
+    })
+
+    const modalButton = screen.getByText('Valider le point de vente')
+
+    expect(modalButton).toBeOnTheScreen()
+  })
+})
--- src/features/search/components/sections/index.ts
+++ src/features/search/components/sections/index.ts
@@ -2,10 +2,12 @@ import { Category } from './Category/Category'
 import { DateHour } from './DateHour/DateHour'
 import { OfferDuo } from './OfferDuo/OfferDuo'
 import { Price } from './Price/Price'
+import { Venue } from './Venue/Venue'
 
 export default {
   Category,
   DateHour,
   OfferDuo,
   Price,
+  Venue,
 }
