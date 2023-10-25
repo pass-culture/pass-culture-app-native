ce653cd23c7a29885ac116b54e5e53d74f17355c
Tanguy <tanguy.prache@passculture.app>
2023-10-25 10:32:53 +0200

    test: add and update tests

--- src/libs/geolocation/hooks/useDistance.test.ts
+++ src/libs/geolocation/hooks/useDistance.test.ts
@@ -20,29 +20,38 @@ describe('useDistance()', () => {
     mockUseGeolocation.mockReturnValue({ userPosition: position } as ILocationContext)
     useDistance(offerPosition)
 
-    expect(useLocation).toHaveBeenCalledWith()
+    expect(formatDistance).toHaveBeenCalledWith(offerPosition, position)
+  })
+
+  it('should call useLocation and formatDistance when geolocation is off but user set a custom position', () => {
+    // eslint-disable-next-line local-rules/independent-mocks
+    mockUseGeolocation.mockReturnValue({
+      userPosition: null,
+      customPosition: position,
+      isCustomPosition: true,
+    } as ILocationContext)
+    useDistance(offerPosition)
+
     expect(formatDistance).toHaveBeenCalledWith(offerPosition, position)
   })
 
   it('should return undefined when no offerPosition given', () => {
-    // @ts-expect-error offer position should not be undefined or null
-    useDistance(undefined)
+    // eslint-disable-next-line local-rules/independent-mocks
+    mockUseGeolocation.mockReturnValue({
+      userPosition: position,
+      customPosition: position,
+    } as ILocationContext)
 
-    expect(useLocation).toHaveBeenCalledWith()
-    expect(formatDistance).not.toHaveBeenCalled()
-
-    useDistance(undefinedOfferPosition)
-
-    expect(useLocation).toHaveBeenCalledWith()
-    expect(formatDistance(undefinedOfferPosition, position)).toEqual(undefined)
+    expect(useDistance(undefinedOfferPosition)).toEqual(undefined)
   })
 
-  it('should return undefined when position is null', () => {
+  it('should return undefined when user position and custom position are null', () => {
     // eslint-disable-next-line local-rules/independent-mocks
-    mockUseGeolocation.mockReturnValue({ userPosition: null } as ILocationContext)
-    useDistance(offerPosition)
+    mockUseGeolocation.mockReturnValue({
+      userPosition: null,
+      customPosition: null,
+    } as ILocationContext)
 
-    expect(useLocation).toHaveBeenCalledWith()
-    expect(formatDistance).not.toHaveBeenCalled()
+    expect(useDistance(offerPosition)).toEqual(undefined)
   })
 })
