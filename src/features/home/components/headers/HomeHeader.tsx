9288b66b9d7c63bef99e6615dcd33663c6b3a249
tanguyprache <47600889+tanguyprache@users.noreply.github.com>
2023-10-30 18:28:03 +0100

    (PC-23472) fix(locationWidget): fix design for desktop location widget (#5523)
    * feat: add height props for vertical separator
    
    * fix: limit vertical separator height
    
    * fix: add space between title and subtitle

--- src/features/home/components/headers/HomeHeader.tsx
+++ src/features/home/components/headers/HomeHeader.tsx
@@ -121,10 +121,11 @@ export const HomeHeader: FunctionComponent = function () {
               <Title testID="web-location-widget">
                 <TitleLabel numberOfLines={1}>{welcomeTitle}</TitleLabel>
                 <Spacer.Row numberOfSpaces={6} />
-                <Separator.Vertical />
+                <Separator.Vertical height={getSpacing(6)} />
                 <Spacer.Row numberOfSpaces={4} />
                 <LocationWidgetDesktop />
               </Title>
+              <Spacer.Column numberOfSpaces={1} />
               <Subtitle>{getSubtitle()}</Subtitle>
             </TitleContainer>
           </HeaderContainer>
@@ -169,6 +170,7 @@ const TitleLabel = styled(Typo.Title1)({
 
 const Title = styled.View({
   flexDirection: 'row',
+  alignItems: 'center',
 })
 
 const PageContent = styled.View({
