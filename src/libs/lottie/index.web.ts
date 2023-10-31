5e9b7b411d4e8d540c0685051ac26541b10d67a7
Hugo Voisin <hugov@bam.tech>
2023-10-31 10:05:20 +0100

    lib: add support of a11y props for external lib

--- src/libs/lottie/index.web.ts
+++ src/libs/lottie/index.web.ts
@@ -1,2 +1,4 @@
 // @ts-ignore @types/react-native-web-lottie is not existing and not necessary
 export { default } from 'react-native-web-lottie'
+
+// N.-B.: The props accessibilityLabel and accessibilityHint are supported, on the contrary of the react-native implementation.
