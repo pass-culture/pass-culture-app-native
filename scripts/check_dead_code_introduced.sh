4b27f49810fb45a0e504eabd4ce2c012e4ebcd24
LucasBeneston <benestonlucas@hotmail.fr>
2023-11-03 14:13:42 +0100

    (PC-25537) feat(deadcode): fix deadcode script

--- scripts/check_dead_code_introduced.sh
+++ scripts/check_dead_code_introduced.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
-last_common_commit=$(git merge-base origin/master @)
-git switch --detach $last_common_commit
-yarn --silent prune:scan > scripts/dead_code_master.txt
-git switch -
-yarn --silent prune:scan > scripts/dead_code_current.txt
+set -o errexit
+set -o nounset
+set -o pipefail
+
+yarn --silent test:deadcode > scripts/dead_code_current.txt
 
 current_dead_code_count=$(wc -l < scripts/dead_code_current.txt)
-
-master_dead_code_count=$(wc -l < scripts/dead_code_master.txt)
-
-diff_dead_code_count=$((current_dead_code_count - master_dead_code_count))
+snapshot_dead_code_count=$(wc -l < scripts/dead_code_snapshot.txt)
+diff_dead_code_count=$((current_dead_code_count - snapshot_dead_code_count))
 
 function display_differences_between_dead_code() {
-    diff --color=auto --unified scripts/dead_code_master.txt scripts/dead_code_current.txt
+    diff --color=auto --unified scripts/dead_code_snapshot.txt scripts/dead_code_current.txt
 }
 
 if [ "$diff_dead_code_count" -lt 0 ]; then
-    echo "✅ Le code mort a diminué :"
+    echo "✅ The dead code has decreased:"
     display_differences_between_dead_code
 elif [ "$diff_dead_code_count" -gt 0 ]; then
-    echo "❌ Le code mort a augmenté :"
+    echo "❌ The dead code has increased:"
     display_differences_between_dead_code
+    echo '👀 If the increase of the dead code is legit and you tried to update the configuration of ts-prune to ignore it but it does not work, please run `yarn test:deadcode:update`'
     exit 1
 else
-    echo "⬜ Le code mort semble inchangé."
+    echo "⬜ The dead code seems unchanged."
     display_differences_between_dead_code
 fi
