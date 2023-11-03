15fab629b5a4b98f9795c903cf48b1da0a1b19c0
marineshaw <marines@bam.tech>
2023-11-03 11:13:39 +0100

    :sparkles: (Venue) Add new address block

--- src/features/venue/pages/VenueBodyNew/VenueBodyNew.tsx
+++ src/features/venue/pages/VenueBodyNew/VenueBodyNew.tsx
@@ -2,9 +2,12 @@ import React, { FunctionComponent } from 'react'
 import styled, { useTheme } from 'styled-components/native'
 
 import { useVenue } from 'features/venue/api/useVenue'
+import { formatFullAddress } from 'libs/address/useFormatFullAddress'
 import { Image } from 'libs/resizing-image-on-demand/Image'
 import { useHeroDimensions } from 'ui/components/hero/useHeroDimensions'
 import { VenueHeaderBackground } from 'ui/svg/VenueHeaderBackground'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
+import { getHeadingAttrs } from 'ui/theme/typographyAttrs/getHeadingAttrs'
 
 interface Props {
   venueId: number
@@ -17,8 +20,10 @@ export const VenueBodyNew: FunctionComponent<Props> = ({ venueId }) => {
 
   if (!venue) return <React.Fragment />
 
-  const { bannerUrl } = venue
+  const { bannerUrl, publicName, name, address, postalCode, city } = venue
   const imageStyle = { height: backgroundHeight, width: appContentWidth }
+
+  const venueFullAddress = venue.address ? formatFullAddress(address, postalCode, city) : undefined
 
   return (
     <Container>
@@ -31,6 +36,20 @@ export const VenueBodyNew: FunctionComponent<Props> = ({ venueId }) => {
           ))}
         </BackgroundContainer>
       )}
+      <Spacer.Column numberOfSpaces={6} />
+      <MarginContainer>
+        <VenueTitle
+          accessibilityLabel={`Nom du lieu\u00a0: ${publicName || name}`}
+          numberOfLines={2}
+          adjustsFontSizeToFit
+          allowFontScaling={false}>
+          {publicName || name}
+        </VenueTitle>
+        <Spacer.Column numberOfSpaces={2} />
+        <Typo.Caption>Adresse</Typo.Caption>
+        <Spacer.Column numberOfSpaces={1} />
+        <Typo.Body>{venueFullAddress}</Typo.Body>
+      </MarginContainer>
     </Container>
   )
 }
@@ -39,4 +58,10 @@ const Container = styled.ScrollView({ overflow: 'visible' })
 
 const BackgroundContainer = styled.View({
   flexDirection: 'row',
+})
+
+const VenueTitle = styled(Typo.Title3).attrs(getHeadingAttrs(1))``
+
+const MarginContainer = styled.View({
+  marginHorizontal: getSpacing(6),
 })
