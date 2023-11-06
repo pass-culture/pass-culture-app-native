ce814c032f20de32abfa31ae960806525ea355df
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-11-06 18:03:59 +0100

    Cleaning and TS type enforcement

--- src/features/location/components/LocationWidgetDesktop.tsx
+++ src/features/location/components/LocationWidgetDesktop.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { LocationModal as HomeLocationModal } from 'features/location/components/LocationModal'
 import { LocationWidgetWrapperDesktop } from 'features/location/components/LocationWidgetWrapperDesktop'
 
+/**
+ * One widget for the home
+ */
 export const LocationWidgetDesktop = () => (
   <LocationWidgetWrapperDesktop>
     {({ visible, dismissModal }) => (
