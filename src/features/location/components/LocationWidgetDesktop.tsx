0cbba7f22cb039b7a25b81789d015bd2ef839dce
Tristan Maurier <tristan.maurier.pro@gmail.com>
2023-11-08 10:10:32 +0100

    add tooltip to LocationWidgetWrapperDesktop

--- src/features/location/components/LocationWidgetDesktop.tsx
+++ src/features/location/components/LocationWidgetDesktop.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 import { LocationModal as HomeLocationModal } from 'features/location/components/LocationModal'
 import { LocationWidgetWrapperDesktop } from 'features/location/components/LocationWidgetWrapperDesktop'
+import { ScreenOrigin } from 'features/location/enums'
 
 export const LocationWidgetDesktop = () => (
-  <LocationWidgetWrapperDesktop>
+  <LocationWidgetWrapperDesktop screenOrigin={ScreenOrigin.HOME}>
     {({ visible, dismissModal }) => (
       <HomeLocationModal visible={visible} dismissModal={dismissModal} />
     )}
