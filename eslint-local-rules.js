3bffdd31454877070282f5dd040374cb4fe8ff7a
marineshaw <marines@bam.tech>
2023-11-08 11:10:15 +0100

    :fire: (Eslint) remove local rules no-react-query-provider-hoc

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -1,7 +1,6 @@
 const independentMocks = require('./eslint-custom-rules/independent-mocks')
 const nbspInText = require('./eslint-custom-rules/nbsp-in-text')
 const noRawText = require('./eslint-custom-rules/no-raw-text')
-const noReactQueryProviderHOC = require('./eslint-custom-rules/no-react-query-provider-hoc')
 const noStringCheckBeforeComponent = require('./eslint-custom-rules/no-string-check-before-component')
 const todoFormat = require('./eslint-custom-rules/todo-format')
 const apostropheInText = require('./eslint-custom-rules/apostrophe-in-text')
@@ -15,7 +14,6 @@ module.exports = {
   'nbsp-in-text': nbspInText,
   'no-hardcoded-id-in-svg': noHardcodeIdInSvg,
   'no-raw-text': noRawText,
-  'no-react-query-provider-hoc': noReactQueryProviderHOC,
   'no-string-check-before-component': noStringCheckBeforeComponent,
   'todo-format': todoFormat,
   'apostrophe-in-text': apostropheInText,
