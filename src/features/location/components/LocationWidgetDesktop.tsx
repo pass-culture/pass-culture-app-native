87024547a7acb6ebf5c9eb67e635028a6bc7c26d
tanguyprache <47600889+tanguyprache@users.noreply.github.com>
2023-11-08 17:32:06 +0100

    (PC-25109) fix(location): fix location design issues on modal slider + scroll (#5569)
    * fix: fix slider width on desktop
    
    * fix: call right modal from home desktop widget
    
    * refacto: remove unused LocationModal

--- src/features/location/components/LocationWidgetDesktop.tsx
+++ src/features/location/components/LocationWidgetDesktop.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { LocationModal as HomeLocationModal } from 'features/location/components/LocationModal'
+import { HomeLocationModal } from 'features/location/components/HomeLocationModal'
 import { LocationWidgetWrapperDesktop } from 'features/location/components/LocationWidgetWrapperDesktop'
 import { ScreenOrigin } from 'features/location/enums'
 
--- src/features/search/helpers/useGetFullscreenModalSliderLength.ts
+++ src/features/search/helpers/useGetFullscreenModalSliderLength.ts
@@ -1,17 +1,23 @@
 import { useTheme } from 'styled-components'
 
-export function useGetFullscreenModalSliderLength() {
+export function useGetFullscreenModalSliderLength(hasFormWidthLimit = true) {
   const { isDesktopViewport, appContentWidth, forms, slider, modal } = useTheme()
   const baseSliderContainerWidth = isDesktopViewport ? modal.desktopMaxWidth : appContentWidth
   /**
    * This hack is used to avoid the slider to be cropped.
    * FIXME(PC-17652): We should create a slider that automatically scales ? Without defining any width
    */
+  const modalHorizontalSpacing = modal.spacing.MD * 2
+
+  const nonDesktopWidth = hasFormWidthLimit
+    ? Math.min(appContentWidth, forms.maxWidth + modalHorizontalSpacing)
+    : appContentWidth
+
   const fixedBaseSliderContainerWidth = isDesktopViewport
     ? baseSliderContainerWidth
-    : Math.min(appContentWidth, forms.maxWidth + modal.spacing.MD * 2)
+    : nonDesktopWidth
 
-  const sliderLength = fixedBaseSliderContainerWidth - modal.spacing.MD * 2 - slider.markerSize
+  const sliderLength = fixedBaseSliderContainerWidth - modalHorizontalSpacing - slider.markerSize
 
   return { sliderLength }
 }
