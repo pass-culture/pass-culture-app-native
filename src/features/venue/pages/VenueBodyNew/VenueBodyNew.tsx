5061cfa92c1f84444760a1d59c5ee3dec2114bb7
marineshaw <marines@bam.tech>
2023-11-08 16:41:02 +0100

    :sparkles: (Venue) Change layout if isDesktopViewport

--- src/features/venue/pages/VenueBodyNew/VenueBodyNew.tsx
+++ src/features/venue/pages/VenueBodyNew/VenueBodyNew.tsx
@@ -1,14 +1,16 @@
 import React, { FunctionComponent } from 'react'
-import styled from 'styled-components/native'
+import styled, { useTheme } from 'styled-components/native'
 
 import { VenueResponse } from 'api/gen'
 import { TabLayout } from 'features/venue/components/TabLayout/TabLayout'
+import { useVenueBackgroundStyle } from 'features/venue/helpers/useVenueBackgroundStyle'
 import { formatFullAddress } from 'libs/address/useFormatFullAddress'
 import { SeeItineraryButton } from 'libs/itinerary/components/SeeItineraryButton'
 import { getGoogleMapsItineraryUrl } from 'libs/itinerary/openGoogleMapsItinerary'
 import { Image } from 'libs/resizing-image-on-demand/Image'
 import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
 import { styledButton } from 'ui/components/buttons/styledButton'
+import { useGetHeaderHeight } from 'ui/components/headers/PageHeaderWithoutPlaceholder'
 import { SectionWithDivider } from 'ui/components/SectionWithDivider'
 import { Separator } from 'ui/components/Separator'
 import { Duplicate } from 'ui/svg/icons/Duplicate'
@@ -24,49 +26,55 @@ interface Props {
 export const VenueBodyNew: FunctionComponent<Props> = ({ venue, onScroll }) => {
   const { bannerUrl, publicName, name, address, postalCode, city } = venue
   const backgroundStyle = useVenueBackgroundStyle()
+  const { isDesktopViewport } = useTheme()
+  const headerHeight = useGetHeaderHeight()
 
   const venueFullAddress = formatFullAddress(address, postalCode, city)
   const venueName = publicName || name
 
   return (
     <Container onScroll={onScroll} scrollEventThrottle={16} bounces={false}>
-      <HeaderContainer>
-        {bannerUrl ? (
-          <Image style={backgroundStyle} resizeMode="cover" url={bannerUrl} />
-        ) : (
-          //TODO(PC-25598) Check if we want that behaviour when bannerUrl is missing
-          <BackgroundContainer>
-            {Array.from({ length: 9 }).map((_, index) => (
-              <VenueHeaderBackground key={index} />
-            ))}
-          </BackgroundContainer>
-        )}
-      </HeaderContainer>
-      <Spacer.Column numberOfSpaces={6} />
-      <MarginContainer>
-        <VenueTitle
-          accessibilityLabel={`Nom du lieu\u00a0: ${venueName}`}
-          numberOfLines={2}
-          adjustsFontSizeToFit
-          allowFontScaling={false}>
-          {venueName}
-        </VenueTitle>
-        <Spacer.Column numberOfSpaces={2} />
-        <Typo.Caption>Adresse</Typo.Caption>
-        <Spacer.Column numberOfSpaces={1} />
-        <Typo.Body>{venueFullAddress}</Typo.Body>
-        <Spacer.Column numberOfSpaces={3} />
-        <Separator.Horizontal />
-        <Spacer.Column numberOfSpaces={3} />
-        <StyledButtonTertiary icon={Duplicate} wording="Copier l’adresse" />
-        <Spacer.Column numberOfSpaces={3} />
-        <SeeItineraryButton
-          externalNav={{
-            url: getGoogleMapsItineraryUrl(venueFullAddress),
-            address: venueFullAddress,
-          }}
-        />
-      </MarginContainer>
+      {isDesktopViewport ? <Placeholder height={headerHeight} /> : null}
+      <TopContainer>
+        <HeaderContainer>
+          {bannerUrl ? (
+            <Image style={backgroundStyle} resizeMode="cover" url={bannerUrl} />
+          ) : (
+            //TODO(PC-25598) Check if we want that behaviour when bannerUrl is missing
+            <BackgroundContainer>
+              {Array.from({ length: 9 }).map((_, index) => (
+                <VenueHeaderBackground key={index} />
+              ))}
+            </BackgroundContainer>
+          )}
+        </HeaderContainer>
+        <Spacer.Column numberOfSpaces={6} />
+        <MarginContainer>
+          <VenueTitle
+            accessibilityLabel={`Nom du lieu\u00a0: ${venueName}`}
+            numberOfLines={2}
+            adjustsFontSizeToFit
+            allowFontScaling={false}>
+            {venueName}
+          </VenueTitle>
+          <Spacer.Column numberOfSpaces={2} />
+          <Typo.Caption>Adresse</Typo.Caption>
+          <Spacer.Column numberOfSpaces={1} />
+          <Typo.Body>{venueFullAddress}</Typo.Body>
+          <Spacer.Column numberOfSpaces={3} />
+          <Separator.Horizontal />
+          <Spacer.Column numberOfSpaces={3} />
+          <StyledButtonTertiary icon={Duplicate} wording="Copier l’adresse" />
+          <Spacer.Column numberOfSpaces={3} />
+          <SeeItineraryButton
+            externalNav={{
+              url: getGoogleMapsItineraryUrl(venueFullAddress),
+              address: venueFullAddress,
+            }}
+          />
+        </MarginContainer>
+      </TopContainer>
+
       <Spacer.Column numberOfSpaces={6} />
 
       <SectionWithDivider visible>
@@ -79,6 +87,12 @@ export const VenueBodyNew: FunctionComponent<Props> = ({ venue, onScroll }) => {
 }
 
 const Container = styled.ScrollView({ overflow: 'visible' })
+
+const TopContainer = styled.View(({ theme }) => ({
+  flexDirection: theme.isDesktopViewport ? 'row' : 'column',
+  marginTop: theme.isDesktopViewport ? getSpacing(8) : 0,
+  marginHorizontal: theme.isDesktopViewport ? getSpacing(18) : 0,
+}))
 
 const BackgroundContainer = styled.View({
   flexDirection: 'row',
@@ -97,3 +111,7 @@ const VenueTitle = styled(Typo.Title3).attrs(getHeadingAttrs(1))``
 const MarginContainer = styled.View({
   marginHorizontal: getSpacing(6),
 })
+
+const Placeholder = styled.View<{ height: number }>(({ height }) => ({
+  height,
+}))
