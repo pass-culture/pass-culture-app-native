65825c669119a34466772252ec0a6ed5d848fbd9
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-11-09 09:30:47 +0100

    (PC-25116) feat(eslint): Add rule to restrict import of @react-oauth/google

--- src/libs/react-native-google-sso/GoogleOAuthProvider.web.tsx
+++ src/libs/react-native-google-sso/GoogleOAuthProvider.web.tsx
@@ -1,1 +1,2 @@
+// eslint-disable-next-line no-restricted-imports
 export { GoogleOAuthProvider } from '@react-oauth/google'
--- src/libs/react-native-google-sso/useGoogleLogin.ts
+++ src/libs/react-native-google-sso/useGoogleLogin.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-restricted-imports
 import { UseGoogleLoginOptionsAuthCodeFlow } from '@react-oauth/google'
 
 export const useGoogleLogin = (_: UseGoogleLoginOptionsAuthCodeFlow) => () => {
