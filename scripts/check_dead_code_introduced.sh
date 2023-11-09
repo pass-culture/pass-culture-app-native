1a7bf2935658336645e3beb43f2b375764d86c4c
bruno ebstein <bruno.ebstein@passculture.app>
2023-11-09 17:48:06 +0100

    (BSR) chore(deadcode): explain what to do when the deadcode decreased (#5576)
    * (BSR) chore(deadcode): explain what to do when the deadcode decreased
    
    * (BSR) chore(deadcode): update snapshot

--- scripts/check_dead_code_introduced.sh
+++ scripts/check_dead_code_introduced.sh
@@ -17,10 +17,17 @@ function display_differences_between_dead_code() {
 if [ "$diff_dead_code_count" -lt 0 ]; then
     echo "✅ The dead code has decreased:"
     display_differences_between_dead_code
+    echo '👏 This means that you have deleted more dead code than you potentially introduced'
+    echo '👀 Check that you have not introduced a new dead code'
+    echo '⏺️ Please run `yarn test:deadcode:update`'
+    echo 'More informations https://en.wikipedia.org/wiki/Software_testing#Output_comparison_testing'
+    exit 1
 elif [ "$diff_dead_code_count" -gt 0 ]; then
     echo "❌ The dead code has increased:"
     display_differences_between_dead_code
-    echo '👀 If the increase of the dead code is legit and you tried to update the configuration of ts-prune to ignore it but it does not work, please run `yarn test:deadcode:update`'
+    echo '👀 If the increase of the dead code is legit and you tried to update the configuration of ts-prune to ignore it but it does not work 😣'
+    echo '⏺️😥 Please run `yarn test:deadcode:update`'
+    echo 'More informations https://en.wikipedia.org/wiki/Software_testing#Output_comparison_testing'
     exit 1
 else
     echo "⬜ The dead code seems unchanged."
