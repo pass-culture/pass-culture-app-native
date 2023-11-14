d7061f4b30601ef49414024afa23f8c79c7ffdee
Hugo Voisin <hugov@bam.tech>
2023-11-14 17:05:16 +0100

    feat: create practical information block
    Co-authored-by: vikingprank <vikingprank@users.noreply.github.com>
    Co-authored-by: Marine Shaw <marineshaw@users.noreply.github.com>

---
+++ src/features/venue/components/PracticalInformation.native.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { PracticalInformation } from 'features/venue/components/PracticalInformation'
+import { venueResponseSnap } from 'features/venue/fixtures/venueResponseSnap'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render, screen } from 'tests/utils'
+
+describe('PracticalInformation', () => {
+  it('should display withdrawal information', async () => {
+    render(reactQueryProviderHOC(<PracticalInformation venue={venueResponseSnap} />))
+
+    expect(await screen.findByText('How to withdraw, https://test.com')).toBeOnTheScreen()
+  })
+
+  it('should display description information', async () => {
+    render(reactQueryProviderHOC(<PracticalInformation venue={venueResponseSnap} />))
+
+    expect(
+      await screen.findByText(
+        ' https://pass.culture.fr/ lorem ipsum consectetur adipisicing elit. Debitis officiis maiores quia unde, hic quisquam odit ea quo ipsam possimus, labore nesciunt numquam. Id itaque in sed sapiente blanditiis necessitatibus. consectetur adipisicing elit. Debitis officiis maiores quia unde, hic quisquam odit ea quo ipsam possimus, consectetur adipisicing elit. Debitis officiis maiores quia unde, hic quisquam odit ea quo ipsam possimus,'
+      )
+    ).toBeOnTheScreen()
+  })
+})
---
+++ src/features/venue/components/PracticalInformation.tsx
@@ -0,0 +1,62 @@
+import React, { Fragment, FunctionComponent } from 'react'
+import { FlatList } from 'react-native'
+import styled from 'styled-components/native'
+
+import { VenueResponse } from 'api/gen'
+import { AccessibilityBlock } from 'ui/components/accessibility/AccessibilityBlock'
+import { ContactBlock } from 'ui/components/contact/ContactBlock'
+import { Separator } from 'ui/components/Separator'
+import { Spacer, Typo } from 'ui/theme'
+
+type Props = { venue: VenueResponse }
+type Section = { title: string; body: JSX.Element }
+
+export const PracticalInformation: FunctionComponent<Props> = ({ venue }) => {
+  const sections: Section[] = [
+    {
+      title: 'Modalités de retrait',
+      body: <Typo.Body>{venue.withdrawalDetails}</Typo.Body>,
+    },
+    {
+      title: 'Description',
+      body: <Typo.Body>{venue.description}</Typo.Body>,
+    },
+    {
+      title: 'Contact',
+      body: <ContactBlock venueId={venue.id} />,
+    },
+    {
+      title: 'Accessibilité',
+      body: <AccessibilityBlock {...venue.accessibility} />,
+    },
+  ]
+
+  return (
+    <Container>
+      <Spacer.Column numberOfSpaces={2} />
+      <FlatList
+        data={sections}
+        renderItem={({ item: section }) => <Section title={section.title}>{section.body}</Section>}
+        ItemSeparatorComponent={Separator.Horizontal}
+      />
+      <Spacer.Column numberOfSpaces={2} />
+    </Container>
+  )
+}
+
+const Container = styled.View(({ theme }) => ({
+  marginHorizontal: theme.contentPage.marginHorizontal,
+}))
+
+const Section: FunctionComponent<{ title: string; children?: JSX.Element }> = ({
+  title,
+  children,
+}) => (
+  <Fragment>
+    <Spacer.Column numberOfSpaces={4} />
+    <Typo.Caption>{title}</Typo.Caption>
+    <Spacer.Column numberOfSpaces={4} />
+    {children}
+    <Spacer.Column numberOfSpaces={4} />
+  </Fragment>
+)
