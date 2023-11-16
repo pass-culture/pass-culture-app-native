05f3d19a54590478414dd9acaf81766a75871d92
Marine Shaw <68000368+marineshaw@users.noreply.github.com>
2023-11-16 10:07:19 +0100

    (PC-24735) fix(E2E): fix maestro staging and testing web (#5599)
    * :bug: (E2E) Fix first launch onboarding geoloc skipped
    
    * :bug: (E2E) Remove webdriver check for Appium as it breaks maestro

--- src/libs/e2e/getIsE2e.web.ts
+++ src/libs/e2e/getIsE2e.web.ts
@@ -11,23 +11,18 @@ export async function getIsE2e() {
   if (isE2e === true || isE2e === false) {
     return isE2e
   }
-  // Read more: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/webdriver
-  if (globalThis.navigator.webdriver) {
-    isE2e = globalThis.navigator.webdriver
-  } else {
-    // This alternative is specific for iOS browser as safaridriver ios have a bug and do not set navigator.webdriver property during automation
-    try {
-      const url = `${APPIUM_ADDRESS}/status`
-      const response = await fetchWithTimeout(url, {
-        mode: 'cors',
-        headers: new Headers({
-          accept: 'application/json',
-        }),
-      })
-      isE2e = response.ok
-    } catch {
-      isE2e = false
-    }
+  // This alternative is specific for iOS browser as safaridriver ios have a bug and do not set navigator.webdriver property during automation
+  try {
+    const url = `${APPIUM_ADDRESS}/status`
+    const response = await fetchWithTimeout(url, {
+      mode: 'cors',
+      headers: new Headers({
+        accept: 'application/json',
+      }),
+    })
+    isE2e = response.ok
+  } catch {
+    isE2e = false
   }
   return isE2e
 }
