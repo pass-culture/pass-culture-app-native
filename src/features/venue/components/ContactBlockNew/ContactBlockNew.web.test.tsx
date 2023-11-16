6444eecdd36393eda262a4d6b690cb9279c5d403
Hugo Voisin <hugov@bam.tech>
2023-11-16 16:02:19 +0100

    test: snapshot only the container param

--- src/features/venue/components/ContactBlockNew/ContactBlockNew.web.test.tsx
+++ src/features/venue/components/ContactBlockNew/ContactBlockNew.web.test.tsx
@@ -7,8 +7,8 @@ import { ContactBlock } from './ContactBlockNew'
 
 describe('<ContactBlock/>', () => {
   it('should match snapshot', () => {
-    const renderAPI = render(<ContactBlock venue={venueResponseSnap} />)
+    const { container } = render(<ContactBlock venue={venueResponseSnap} />)
 
-    expect(renderAPI).toMatchSnapshot()
+    expect(container).toMatchSnapshot()
   })
 })
