461c3f523d7631d881f9c6c87f56850af8c74696
Christopher Gerrard <christopher.gerrard@passculture.app>
2023-11-22 11:54:03 +0100

    Reproducible example

--- src/features/auth/components/OpenInboxButton.tsx
+++ src/features/auth/components/OpenInboxButton.tsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { openInbox } from 'react-native-email-link'
 
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
-import { Email } from 'ui/svg/icons/Email'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
+import { EmailFilled } from 'ui/svg/icons/EmailFilled'
 
 export const OpenInboxButton = ({ onAdditionalPress }: { onAdditionalPress?: () => void }) => {
+  const { showErrorSnackBar } = useSnackBarContext()
+
   const onPress = async () => {
-    await openInbox()
-    onAdditionalPress?.()
+    try {
+      await openInbox({ app: 'apple-mail' })
+      onAdditionalPress?.()
+    } catch (error) {
+      showErrorSnackBar({
+        message: `${error}`,
+        // 'Nous n’avons pas réussi à ouvrir votre boîte e-mail. Veuillez l’ouvrir manuellement.',
+        timeout: SNACK_BAR_TIME_OUT,
+      })
+    }
   }
 
-  return <ButtonPrimary wording="Consulter mes e-mails" onPress={onPress} icon={Email} />
+  return <ButtonPrimary wording="Consulter mes e-mails" onPress={onPress} icon={EmailFilled} />
 }
