99d067ad5988f027c67f194a99ff9f9c1ff05692
tanguyprache <47600889+tanguyprache@users.noreply.github.com>
2023-11-22 14:16:30 +0100

    (PC-25949) fix(location): fix search results with location from deeplink or url  (#5628)
    * fix import order
    
    * fix web url params setup
    
    ---------
    
    Co-authored-by: Tristan Maurier <tristan.maurier.pro@gmail.com>

--- src/features/search/pages/Search/Search.native.test.tsx
+++ src/features/search/pages/Search/Search.native.test.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Keyboard } from 'react-native'
 import { v4 as uuidv4 } from 'uuid'
 
-import { useRoute, navigate } from '__mocks__/@react-navigation/native'
+import { navigate, useRoute } from '__mocks__/@react-navigation/native'
 import { NativeCategoryIdEnumv2, SearchGroupNameEnumv2 } from 'api/gen'
 import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
 import { initialSearchState } from 'features/search/context/reducer'
@@ -19,9 +19,10 @@ import { analytics } from 'libs/analytics'
 import { env } from 'libs/environment'
 import * as useFeatureFlag from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { useNetInfoContext as useNetInfoContextDefault } from 'libs/network/NetInfoWrapper'
+import { SuggestedPlace } from 'libs/place'
 import { placeholderData } from 'libs/subcategories/placeholderData'
 import { mockedSuggestedVenues } from 'libs/venue/fixtures/mockedSuggestedVenues'
-import { render, fireEvent, waitFor, act, screen } from 'tests/utils'
+import { act, fireEvent, render, screen, waitFor } from 'tests/utils'
 
 const venue: Venue = mockedSuggestedVenues[0]
 
@@ -186,6 +187,21 @@ mockUseSearchHistory.mockReturnValue({
 
 const searchId = uuidv4()
 
+const mockedPlace: SuggestedPlace = {
+  label: 'Kourou',
+  info: 'Guyane',
+  geolocation: { longitude: -52.669736, latitude: 5.16186 },
+}
+
+const mockSetPlace = jest.fn()
+jest.mock('libs/geolocation/LocationWrapper', () => ({
+  useLocation: () => ({
+    setPlace: mockSetPlace,
+    place: mockedPlace,
+    onModalHideRef: jest.fn(),
+  }),
+}))
+
 describe('<Search/>', () => {
   mockUseNetInfoContext.mockReturnValue({ isConnected: true })
 
@@ -205,6 +221,23 @@ describe('<Search/>', () => {
       type: 'SET_STATE',
       payload: {},
     })
+  })
+
+  it('should setPlace in location context when there is a place in the uri params', async () => {
+    useRoute.mockReturnValueOnce({
+      params: {
+        locationFilter: {
+          locationType: LocationType.PLACE,
+          place: mockedPlace,
+        },
+      },
+    })
+
+    render(<Search />)
+
+    await act(async () => {})
+
+    expect(mockSetPlace).toHaveBeenCalledWith(mockedPlace)
   })
 
   describe('When search view is suggestions', () => {
