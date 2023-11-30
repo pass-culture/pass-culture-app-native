1f8bb50371ecbffe08885b1cf7364d7eeb0bb926
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-11-30 14:15:55 +0100

    (PC-26096) fix(AccountSecurity): Handle new token format (#5654)

--- src/features/trustedDevice/fixtures/fixtures.ts
+++ src/features/trustedDevice/fixtures/fixtures.ts
@@ -1,6 +1,6 @@
 export const ROUTE_PARAMS = {
   token:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxIiwibG9jYXRpb24iOiJQYXJpcyIsImRhdGVDcmVhdGVkIjoiMjAyMy0wNi0wOVQxMDowMDowMFoiLCJvcyI6ImlPUyIsInNvdXJjZSI6ImlQaG9uZSAxMyJ9.0x9m4wEh0QKefPSsCOJDVrA-xVRGnUcoJR_vEbjNtaE',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJkYXRhIjp7ImRhdGVDcmVhdGVkIjoiMjAyMy0wNi0wOVQxMDowMDowMFoiLCJsb2NhdGlvbiI6IlBhcmlzIiwib3MiOiJpT1MiLCJzb3VyY2UiOiJpUGhvbmUgMTMifSwiZXhwIjoxNzAxOTM4MDE4fQ.quizgw61p4gX_B2Liz-WdoJlWadTx9kHMPKoEV_5tHI',
   email: 'john@example.com',
   reset_password_token: 'reerereskjlmkdlsf',
   reset_token_expiration_timestamp: 45465546445,
