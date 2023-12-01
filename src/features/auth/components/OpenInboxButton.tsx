7d7deaa8aa87765910527707de616c98617b81f7
Christopher Gerrard <christopher.gerrard@passculture.app>
2023-12-01 15:48:16 +0100

    removed snackbar on openIndoxButton

--- src/features/auth/components/OpenInboxButton.tsx
+++ src/features/auth/components/OpenInboxButton.tsx
@@ -2,23 +2,12 @@ import React from 'react'
 import { openInbox } from 'react-native-email-link'
 
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
-import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
 import { EmailFilled } from 'ui/svg/icons/EmailFilled'
 
 export const OpenInboxButton = ({ onAdditionalPress }: { onAdditionalPress?: () => void }) => {
-  const { showErrorSnackBar } = useSnackBarContext()
-
   const onPress = async () => {
-    try {
-      await openInbox()
-      onAdditionalPress?.()
-    } catch (error) {
-      showErrorSnackBar({
-        message:
-          'Nous n’avons pas réussi à ouvrir votre boîte e-mail. Veuillez l’ouvrir manuellement.',
-        timeout: SNACK_BAR_TIME_OUT,
-      })
-    }
+    await openInbox()
+    onAdditionalPress?.()
   }
 
   return <ButtonPrimary wording="Consulter mes e-mails" onPress={onPress} icon={EmailFilled} />
