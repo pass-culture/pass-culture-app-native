d2708063a375364cc3b3400a3bd3d7be515c4cfe
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-12-04 14:35:46 +0100

    (BSR) refactor(appsflyer): refactor method for web (#5667)

--- src/libs/campaign/campaignTracker.web.ts
+++ src/libs/campaign/campaignTracker.web.ts
@@ -1,26 +1,14 @@
 import { CampaignEvents } from './events'
 import { CampaignTracker } from './types'
 
-function useInit() {
-  // nothing
-}
-
 async function logEvent(event: CampaignEvents, params: Record<string, unknown>): Promise<void> {
   // eslint-disable-next-line no-console
   console.log('campaignTracker event', event, 'params', params)
 }
 
-async function getUserId(): Promise<string | undefined> {
-  return undefined
-}
-
-function startAppsFlyer() {
-  return
-}
-
 export const campaignTracker: CampaignTracker = {
   logEvent,
-  getUserId,
-  init: useInit,
-  startAppsFlyer,
+  getUserId: async () => undefined,
+  init: () => undefined,
+  startAppsFlyer: () => undefined,
 }
