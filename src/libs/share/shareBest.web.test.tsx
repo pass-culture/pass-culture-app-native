528ee2be56bd38dd2a3b46eb0cb9f31180d601f3
marineshaw <marines@bam.tech>
2023-12-05 17:10:37 +0100

    :pencil2: (Tests) rename shareBest web test

--- src/libs/share/shareBest.web.test.tsx
+++ src/libs/share/shareBest.web.test.tsx
@@ -7,7 +7,7 @@ const mockOpenUrl = jest.spyOn(Linking, 'openURL').mockResolvedValue(undefined)
 const defaultContent = { body: 'Message', url: 'https://www.toto.com' }
 
 describe('share()', () => {
-  it('should share with native dialog when default mode', () => {
+  it('should share with whatsapp url if on web and whatsapp mode', () => {
     share({ content: defaultContent, mode: 'WhatsApp' })
 
     expect(mockOpenUrl).toHaveBeenCalledWith(
