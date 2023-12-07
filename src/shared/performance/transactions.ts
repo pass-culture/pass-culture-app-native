499133334e8936a07aa8deb652871c2803adac6d
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-12-07 17:32:14 +0100

    Modification of transactions calls

---
+++ src/shared/performance/transactions.ts
@@ -0,0 +1,24 @@
+import * as Sentry from '@sentry/react-native'
+import '@sentry/tracing'
+import { Span, Transaction } from '@sentry/types'
+
+import { eventMonitoring } from 'libs/monitoring'
+
+const transactionsRef: { [key: string]: { transaction: Transaction; span: Span } | undefined } = {}
+
+export function startTransaction(name: string) {
+  if (transactionsRef?.[name] !== undefined) {
+    finishTransaction(name)
+  }
+  const transaction = eventMonitoring.startTransaction({ name })
+  Sentry.getCurrentHub().configureScope((scope) => scope.setSpan(transaction))
+  const span = transaction.startChild({ op: `performance` })
+  transactionsRef[name] = { transaction, span }
+}
+
+export function finishTransaction(name: string) {
+  if (!transactionsRef[name]) return
+  transactionsRef[name]?.span?.finish()
+  transactionsRef[name]?.transaction?.finish()
+  transactionsRef[name] = undefined
+}
