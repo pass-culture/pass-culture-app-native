f067acbc4414e2fd617900054ff2787c21507e60
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-12-07 17:58:09 +0100

    (PC-25494) feat(ui): see button only when text height is equal or greater than max possible text height

--- src/ui/components/CollapsibleText/CollapsibleText.stories.tsx
+++ src/ui/components/CollapsibleText/CollapsibleText.stories.tsx
@@ -21,3 +21,8 @@ Expanded.args = {
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec tellus in magna convallis egestas eget id justo. Donec lorem ante, tempor eu diam quis, laoreet rhoncus tortor. Sed posuere quis sapien sit amet rutrum. Nam arcu dui, blandit vitae massa ac, pulvinar rutrum tellus. Mauris molestie, sapien quis elementum interdum, ipsum turpis varius lorem, quis luctus tellus est et velit. Curabitur accumsan, enim ac tincidunt varius, lectus ligula elementum elit, a porta velit libero quis nunc. Maecenas semper augue justo, ac dapibus erat porttitor quis. Cras porttitor pharetra quam, et suscipit felis fringilla in. Aliquam ultricies mauris at vehicula finibus. Donec sed justo turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc dictum tempus velit, nec volutpat dolor fermentum non. Nullam efficitur diam nec orci aliquam, ut accumsan turpis convallis. Duis erat diam, ultricies non dolor a, elementum sagittis nibh. Curabitur dapibus ipsum eget quam scelerisque, eget venenatis urna laoreet.',
   isExpanded: true,
 }
+
+export const WithoutButton = Template.bind({})
+WithoutButton.args = {
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+}
--- src/ui/components/CollapsibleText/CollapsibleText.tsx
+++ src/ui/components/CollapsibleText/CollapsibleText.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
+import { LayoutChangeEvent } from 'react-native'
 import styled from 'styled-components/native'
 
 import { highlightLinks } from 'libs/parsers/highlightLinks'
@@ -11,13 +12,33 @@ import { getSpacing, Typo } from 'ui/theme'
 type Props = {
   text: string
   numberOfLines?: number
+  lineHeight?: number
   isExpanded?: boolean
 }
 
-export function CollapsibleText({ text, numberOfLines = 5, isExpanded = false }: Readonly<Props>) {
+const DEFAULT_NUMBER_OF_LINES = 5
+const DEFAULT_LINE_HEIGHT = 20
+
+export function CollapsibleText({
+  text,
+  numberOfLines = DEFAULT_NUMBER_OF_LINES,
+  lineHeight = DEFAULT_LINE_HEIGHT,
+  isExpanded = false,
+}: Readonly<Props>) {
   const [expanded, setExpanded] = useState(isExpanded)
+  const [shouldDisplayButton, setShouldDisplayButton] = useState(false)
 
   const onPress = () => setExpanded((prevExpanded) => !prevExpanded)
+
+  const onLayout = useCallback(
+    (event: LayoutChangeEvent) => {
+      const textHeight = event.nativeEvent.layout.height
+      const maxTextHeight = lineHeight * numberOfLines
+
+      setShouldDisplayButton(textHeight >= maxTextHeight)
+    },
+    [lineHeight, numberOfLines]
+  )
 
   const buttonText = expanded ? 'Voir moins' : 'Voir plus'
   const accessibilityLabel = expanded ? 'Réduire le texte' : 'Étendre le texte'
@@ -25,18 +46,20 @@ export function CollapsibleText({ text, numberOfLines = 5, isExpanded = false }:
 
   return (
     <React.Fragment>
-      <Typo.Body numberOfLines={expanded ? undefined : numberOfLines}>
+      <Typo.Body numberOfLines={expanded ? undefined : numberOfLines} onLayout={onLayout}>
         {highlightLinks(text)}
       </Typo.Body>
-      <ButtonContainer>
-        <LocationButton
-          wording={buttonText}
-          onPress={onPress}
-          accessibilityLabel={accessibilityLabel}
-          icon={icon}
-          buttonHeight="extraSmall"
-        />
-      </ButtonContainer>
+      {shouldDisplayButton ? (
+        <ButtonContainer>
+          <LocationButton
+            wording={buttonText}
+            onPress={onPress}
+            accessibilityLabel={accessibilityLabel}
+            icon={icon}
+            buttonHeight="extraSmall"
+          />
+        </ButtonContainer>
+      ) : null}
     </React.Fragment>
   )
 }
