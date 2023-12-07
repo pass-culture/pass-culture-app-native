30e818e8da0bd333fe9d0b8a42eea577429e947b
Hugo Voisin <hugov@bam.tech>
2023-12-07 15:49:54 +0100

    ci: KO perf ci, only if number of renders increase stays OK if the number of renders shrinks

--- scripts/reassure_test_check.sh
+++ scripts/reassure_test_check.sh
@@ -6,10 +6,10 @@ set -o pipefail
 
 REASSURE_FILE=.reassure/output.json
 
-render_count_changes=$(jq '.countChanged | length' $REASSURE_FILE)
+render_count_changes=$(jq '[.countChanged[] | select(.countDiff > 0)] | length' $REASSURE_FILE)
 
 if [ "$render_count_changes" -gt 0 ]; then
   echo "Error: number of renders increased for $render_count_changes test(s). Please check the following test(s):"
-  jq '.countChanged[].name' $REASSURE_FILE
+  jq '[.countChanged[] | select(.countDiff > 0)] | .[].name' $REASSURE_FILE
   exit 1
 fi
