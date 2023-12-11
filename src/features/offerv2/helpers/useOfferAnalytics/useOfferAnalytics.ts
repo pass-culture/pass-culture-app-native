bb46236a22fe8986bc7559329d31377b624a20b9
Cédric Le Sausse <cedric.lesausse@passculture.app>
2023-12-11 17:29:20 +0100

    (PC-25795) feat(offer): specific hook for analytics

---
+++ src/features/offerv2/helpers/useOfferAnalytics/useOfferAnalytics.ts
@@ -0,0 +1,79 @@
+import { useCallback } from 'react'
+
+import { PlaylistType } from 'features/offer/enums'
+import { analytics } from 'libs/analytics'
+import { useFunctionOnce } from 'libs/hooks'
+import { RecommendationApiParams } from 'shared/offer/types'
+
+type Props = {
+  offerId: number
+  nbSameArtistPlaylist: number
+  nbSameCategorySimilarOffers: number
+  nbOtherCategoriesSimilarOffers: number
+  apiRecoParamsSameCategory?: RecommendationApiParams
+  apiRecoParamsOtherCategories?: RecommendationApiParams
+  fromOfferId?: number
+}
+
+export type UseOfferAnalyticsType = {
+  logSameCategoryPlaylistVerticalScroll: VoidFunction
+  logOtherCategoriesPlaylistVerticalScroll: VoidFunction
+  logSameArtistPlaylistVerticalScroll: VoidFunction
+  logConsultWholeOffer: VoidFunction
+  logPlaylistHorizontalScroll: VoidFunction
+}
+
+export const useOfferAnalytics = ({
+  offerId,
+  nbSameArtistPlaylist,
+  apiRecoParamsSameCategory,
+  nbSameCategorySimilarOffers,
+  apiRecoParamsOtherCategories,
+  nbOtherCategoriesSimilarOffers,
+  fromOfferId,
+}: Props): UseOfferAnalyticsType => {
+  const logSameCategoryPlaylistVerticalScroll = useFunctionOnce(() => {
+    return analytics.logPlaylistVerticalScroll({
+      ...apiRecoParamsSameCategory,
+      fromOfferId,
+      offerId,
+      playlistType: PlaylistType.SAME_CATEGORY_SIMILAR_OFFERS,
+      nbResults: nbSameCategorySimilarOffers,
+    })
+  })
+
+  const logOtherCategoriesPlaylistVerticalScroll = useFunctionOnce(() => {
+    return analytics.logPlaylistVerticalScroll({
+      ...apiRecoParamsOtherCategories,
+      fromOfferId,
+      offerId,
+      playlistType: PlaylistType.OTHER_CATEGORIES_SIMILAR_OFFERS,
+      nbResults: nbOtherCategoriesSimilarOffers,
+    })
+  })
+
+  const logSameArtistPlaylistVerticalScroll = useFunctionOnce(() => {
+    return analytics.logPlaylistVerticalScroll({
+      fromOfferId,
+      offerId,
+      playlistType: PlaylistType.SAME_ARTIST_PLAYLIST,
+      nbResults: nbSameArtistPlaylist,
+    })
+  })
+
+  const logConsultWholeOffer = useFunctionOnce(() => {
+    analytics.logConsultWholeOffer(offerId)
+  })
+
+  const logPlaylistHorizontalScroll = useCallback(() => {
+    return analytics.logPlaylistHorizontalScroll(fromOfferId)
+  }, [fromOfferId])
+
+  return {
+    logSameCategoryPlaylistVerticalScroll,
+    logOtherCategoriesPlaylistVerticalScroll,
+    logSameArtistPlaylistVerticalScroll,
+    logConsultWholeOffer,
+    logPlaylistHorizontalScroll,
+  }
+}
