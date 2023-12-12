5117ee8bf78bfafecb1653112e54e1ff7ec56a3d
Pierre Canthelou <pierre.canthelou@passculture.app>
2023-12-12 23:10:19 +0100

    Fix the signature of OfferName component

--- src/ui/components/tiles/OfferName.tsx
+++ src/ui/components/tiles/OfferName.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components/native'
 
 import { Typo } from 'ui/theme/typography'
 
-const OfferName = ({ title }: { title: string }) => {
-  const cleanedTitle = title.trim()
+const OfferName = ({ title }: { title?: string }) => {
+  const cleanedTitle = title?.trim() ?? ''
   const uiTitle = cleanedTitle[0].toUpperCase() + cleanedTitle.slice(1)
   return <Name numberOfLines={2}>{uiTitle}</Name>
 }
