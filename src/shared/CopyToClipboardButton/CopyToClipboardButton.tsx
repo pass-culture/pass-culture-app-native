0e5f10e1b27de487a5a2cc5b648b3ca717e73042
Ali RAIKI <ali.raiki@polyconseil.fr>
2023-12-14 15:26:11 +0100

    (PC-26161) feat(clipboard): Use navigator instead of library in web

--- src/shared/CopyToClipboardButton/CopyToClipboardButton.tsx
+++ src/shared/CopyToClipboardButton/CopyToClipboardButton.tsx
@@ -1,10 +1,9 @@
-import Clipboard from '@react-native-clipboard/clipboard'
 import { FunctionComponent } from 'react'
 import React from 'react-native'
 
+import { useCopyToClipboard } from 'libs/useCopyToClipboard/useCopyToClipboard'
 import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
 import { styledButton } from 'ui/components/buttons/styledButton'
-import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
 import { Duplicate } from 'ui/svg/icons/Duplicate'
 
 interface Props {
@@ -20,22 +19,7 @@ export const CopyToClipboardButton: FunctionComponent<Props> = ({
   onCopy,
   snackBarMessage,
 }) => {
-  const copyToClipboard = async () => {
-    Clipboard.setString(textToCopy)
-    if ((await Clipboard.getString()) === textToCopy) {
-      onCopy?.()
-      showSuccessSnackBar({
-        message: snackBarMessage || 'Copié\u00a0!',
-        timeout: SNACK_BAR_TIME_OUT,
-      })
-    } else {
-      showErrorSnackBar({
-        message: 'Une erreur est survenue, veuillez réessayer',
-        timeout: SNACK_BAR_TIME_OUT,
-      })
-    }
-  }
-  const { showSuccessSnackBar, showErrorSnackBar } = useSnackBarContext()
+  const copyToClipboard = useCopyToClipboard({ textToCopy, snackBarMessage, onCopy })
 
   return <StyledButtonTertiary icon={Duplicate} wording={wording} onPress={copyToClipboard} />
 }
