5df8f1622cb0bfb19a3b98cb11a2451dcdcd544b
NolwenMajorFrances <144016890+NolwenMajorFrances@users.noreply.github.com>
2023-12-21 16:23:02 +0100

    PC-26477 deleteuseHomePosition

--- src/features/home/helpers/useHomePosition.ts
+++
@@ -1,9 +0,0 @@
-import { Position, useLocation } from 'libs/location'
-
-export const useHomePosition = () => {
-  const { geolocPosition, place } = useLocation()
-
-  const placeCoordinates: Position = place?.geolocation
-
-  return { position: placeCoordinates || geolocPosition }
-}
