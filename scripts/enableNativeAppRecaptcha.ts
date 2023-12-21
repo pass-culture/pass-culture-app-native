c08ff1b8ef9e927a9f5acd0f3384dec5025bab66
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2023-12-21 12:00:04 +0100

    (PC-25011) feat(maestro): Disable recaptcha before tests (#5730)

---
+++ scripts/enableNativeAppRecaptcha.ts
@@ -0,0 +1,55 @@
+/// <reference types="node" />
+
+type Feature = { name: string; isActive: boolean }
+type Features = Array<Feature>
+
+const CYAN = '\x1b[36m'
+const RED = '\x1b[31m'
+const RESET_COLOR = '\x1b[0m'
+const API_BASE_URL = `https://backend.${process.argv[2]}.passculture.team`
+
+async function patchFeatures(features: Features) {
+  try {
+    const response = await fetch(`${API_BASE_URL}/testing/features`, {
+      method: 'PATCH',
+      mode: 'cors',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      }),
+      body: JSON.stringify({
+        features,
+      }),
+    })
+
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+
+    // eslint-disable-next-line no-console
+    console.info(
+      `${CYAN}INFO`,
+      `${RESET_COLOR}${features
+        .map(({ name, isActive }) => `${name} set to ${isActive}`)
+        .join('\n')}`
+    )
+  } catch (error) {
+    console.error(
+      `${RED}ERROR`,
+      `${RESET_COLOR}Failed to patch ${features.length} feature(s): ${error}`
+    )
+  }
+}
+
+const enableNativeAppRecaptcha = async (isActive: boolean) => {
+  return patchFeatures([
+    {
+      name: 'ENABLE_NATIVE_APP_RECAPTCHA',
+      isActive,
+    },
+  ])
+}
+
+enableNativeAppRecaptcha(process.argv[3] as unknown as boolean)
+
+export {}
