c13bfdf27f8655a330f0ada1778fbb5672d49099
Tristan Maurier <tristan.maurier.pro@gmail.com>
2023-12-28 17:11:19 +0100

    update navigate tosearch...

--- src/features/search/helpers/useNavigateToSearch/useNavigateToSearch.ts
+++ src/features/search/helpers/useNavigateToSearch/useNavigateToSearch.ts
@@ -2,19 +2,13 @@ import { useNavigation } from '@react-navigation/native'
 
 import { UseNavigationType } from 'features/navigation/RootNavigator/types'
 import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
-import { useSearch } from 'features/search/context/SearchWrapper'
 import { SearchState } from 'features/search/types'
 
 export const useNavigateToSearch = () => {
-  const { dispatch } = useSearch()
   const { navigate } = useNavigation<UseNavigationType>()
 
   const navigateToSearch = (newSearchState: SearchState): void => {
-    dispatch({
-      type: 'SET_STATE',
-      payload: newSearchState,
-    })
-    navigate(...getTabNavConfig('Search'))
+    navigate(...getTabNavConfig('Search', newSearchState))
   }
   return { navigateToSearch }
 }
--- src/features/search/helpers/useNavigateToSearchFilter/useNavigateToSearchFilter.ts
+++ src/features/search/helpers/useNavigateToSearchFilter/useNavigateToSearchFilter.ts
@@ -1,18 +1,12 @@
 import { useNavigation } from '@react-navigation/native'
 
 import { UseNavigationType } from 'features/navigation/RootNavigator/types'
-import { useSearch } from 'features/search/context/SearchWrapper'
 import { SearchState } from 'features/search/types'
 
 export const useNavigateToSearchFilter = () => {
-  const { dispatch } = useSearch()
   const { navigate } = useNavigation<UseNavigationType>()
 
   const navigateToSearchFilter = (newSearchState: SearchState): void => {
-    dispatch({
-      type: 'SET_STATE',
-      payload: newSearchState,
-    })
     navigate('SearchFilter', newSearchState)
   }
   return { navigateToSearchFilter }
