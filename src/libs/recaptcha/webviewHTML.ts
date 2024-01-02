d0ba826d59fe05940781e0b4a8448aa496b4ad0b
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-01-02 14:34:50 +0100

    fixed lint error

--- src/libs/recaptcha/webviewHTML.ts
+++ src/libs/recaptcha/webviewHTML.ts
@@ -12,7 +12,7 @@ export const reCaptchaWebviewHTML = `
                 let numberOfRetryRender = 0;
                 const userAgent = navigator.userAgent.toLowerCase();
                 const ios = /iphone|ipod|ipad/.test(userAgent);
-                const isOnline = ios ? navigator.onLine : navigator.connection?.type !== "none";
+                const isOnline = ios\u00a0? navigator.onLine\u00a0: navigator.connection?.type\u00a0!== "none";
 
                 function sendMessagePayload(payload) { window.ReactNativeWebView.postMessage(JSON.stringify(payload)); }
                 function onClose() { sendMessagePayload({ "message": "close" }); }
