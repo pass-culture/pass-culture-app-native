bf7213815642f71f064d383104ce4b320f286480
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2024-01-03 14:36:45 +0100

    (BSR) fix(sso): Fetch auth state when FF is active (#5765)

--- src/features/auth/api/useOAuthState.ts
+++ src/features/auth/api/useOAuthState.ts
@@ -1,16 +1,19 @@
 import { useQuery } from 'react-query'
 
 import { api } from 'api/api'
+import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
 import { QueryKeys } from 'libs/queryKeys'
 
 const OAUTH_STATE_STALE_TIME = 60 * 60 * 1000
 
 export function useOAuthState() {
+  const enableGoogleSSO = useFeatureFlag(RemoteStoreFeatureFlags.WIP_ENABLE_GOOGLE_SSO)
   const netInfo = useNetInfoContext()
 
   return useQuery([QueryKeys.OAUTH_STATE], () => api.getNativeV1OauthState(), {
-    enabled: !!netInfo.isConnected,
+    enabled: !!(netInfo.isConnected && enableGoogleSSO),
     staleTime: OAUTH_STATE_STALE_TIME,
   })
 }
