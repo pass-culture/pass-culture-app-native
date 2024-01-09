62317567b146685723653aafd6dcd6153317a598
Tristan Maurier <tristan.maurier.pro@gmail.com>
2024-01-09 14:43:52 +0100

    rm action that are not used

--- src/features/search/context/reducer.ts
+++ src/features/search/context/reducer.ts
@@ -1,11 +1,6 @@
-import { Hit } from '@algolia/client-search'
-
-import { SearchGroupNameEnumv2 } from 'api/gen'
-import { DATE_FILTER_OPTIONS } from 'features/search/enums'
-import { addOrRemove, MAX_RADIUS, sortCategories } from 'features/search/helpers/reducer.helpers'
+import { MAX_RADIUS } from 'features/search/helpers/reducer.helpers'
 import { SearchState, SearchView } from 'features/search/types'
 import { Venue } from 'features/venue/types'
-import { AlgoliaVenue } from 'libs/algolia'
 import { LocationMode } from 'libs/location/types'
 import { SuggestedPlace } from 'libs/place'
 
@@ -31,29 +26,13 @@ export const initialSearchState: SearchState = {
 
 export type Action =
   | { type: 'INIT' }
-  | { type: 'SET_STATE_FROM_DEFAULT'; payload: Partial<SearchState> }
   | { type: 'SET_STATE'; payload: SearchState }
   | { type: 'PRICE_RANGE'; payload: SearchState['priceRange'] }
-  | { type: 'SET_MIN_PRICE'; payload: string }
-  | { type: 'SET_MAX_PRICE'; payload: string }
-  | { type: 'RADIUS'; payload: number }
-  | { type: 'TIME_RANGE'; payload: SearchState['timeRange'] }
-  | { type: 'TOGGLE_IS_DIGITAL'; payload: SearchState['isDigital'] }
-  | { type: 'SET_CATEGORY'; payload: SearchGroupNameEnumv2[] }
-  | { type: 'TOGGLE_CATEGORY'; payload: SearchGroupNameEnumv2 }
-  | { type: 'TOGGLE_OFFER_FREE' }
-  | { type: 'TOGGLE_OFFER_DUO' }
-  | { type: 'TOGGLE_OFFER_NEW' }
-  | { type: 'TOGGLE_DATE' }
-  | { type: 'TOGGLE_HOUR' }
-  | { type: 'SELECT_DATE_FILTER_OPTION'; payload: DATE_FILTER_OPTIONS }
   | { type: 'SELECT_DATE'; payload: Date }
   | { type: 'SET_LOCATION_EVERYWHERE' }
   | { type: 'SET_LOCATION_AROUND_ME'; payload?: number }
   | { type: 'SET_LOCATION_PLACE'; payload: { aroundRadius?: number; place: SuggestedPlace } }
   | { type: 'SET_VENUE'; payload?: Venue }
-  | { type: 'SET_QUERY'; payload: string }
-  | { type: 'SET_VENUES'; payload: Hit<AlgoliaVenue>[] }
   | {
       type: 'SET_LOCATION_FILTERS'
       payload: { locationFilter: SearchState['locationFilter'] }
@@ -63,63 +42,10 @@ export const searchReducer = (state: SearchState, action: Action): SearchState =
   switch (action.type) {
     case 'INIT':
       return { ...initialSearchState, view: state.view }
-    case 'SET_STATE_FROM_DEFAULT':
-      return { ...initialSearchState, ...action.payload }
     case 'SET_STATE':
       return action.payload
     case 'PRICE_RANGE':
       return { ...state, priceRange: action.payload }
-    case 'SET_MIN_PRICE':
-      return { ...state, minPrice: action.payload }
-    case 'SET_MAX_PRICE':
-      return { ...state, maxPrice: action.payload }
-    case 'RADIUS':
-      if ('aroundRadius' in state.locationFilter) {
-        return {
-          ...state,
-          locationFilter: { ...state.locationFilter, aroundRadius: action.payload },
-        }
-      } else {
-        return state
-      }
-    case 'TIME_RANGE':
-      return { ...state, timeRange: action.payload }
-    case 'TOGGLE_CATEGORY':
-      return {
-        ...state,
-        offerCategories: addOrRemove(state.offerCategories, action.payload).sort(sortCategories),
-      }
-    case 'SET_CATEGORY':
-      return { ...state, offerCategories: [...action.payload].sort(sortCategories) }
-    case 'TOGGLE_IS_DIGITAL':
-      return {
-        ...state,
-        isDigital: action.payload,
-      }
-    case 'TOGGLE_OFFER_FREE':
-      return { ...state, offerIsFree: !state.offerIsFree }
-    case 'TOGGLE_OFFER_DUO':
-      return { ...state, offerIsDuo: !state.offerIsDuo }
-    case 'TOGGLE_OFFER_NEW':
-      return { ...state, offerIsNew: !state.offerIsNew }
-    case 'TOGGLE_DATE':
-      if (state.date) return { ...state, date: null }
-      return {
-        ...state,
-        date: {
-          option: DATE_FILTER_OPTIONS.TODAY,
-          selectedDate: new Date().toISOString(),
-        },
-      }
-    case 'TOGGLE_HOUR':
-      if (state.timeRange) return { ...state, timeRange: null }
-      return {
-        ...state,
-        timeRange: [8, 24],
-      }
-    case 'SELECT_DATE_FILTER_OPTION':
-      if (!state.date) return state
-      return { ...state, date: { ...state.date, option: action.payload } }
     case 'SELECT_DATE':
       if (!state.date) return state
       return { ...state, date: { ...state.date, selectedDate: action.payload.toISOString() } }
@@ -147,8 +73,6 @@ export const searchReducer = (state: SearchState, action: Action): SearchState =
         ...state,
         venue: action.payload,
       }
-    case 'SET_QUERY':
-      return { ...state, query: action.payload }
     case 'SET_LOCATION_FILTERS':
       return {
         ...state,
