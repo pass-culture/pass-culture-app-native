3c806bb275c61a351144b654903b13ce7fd47bf3
Tristan Maurier <tristan.maurier.pro@gmail.com>
2024-01-11 16:35:09 +0100

    hide venue with no offer in venueModal

--- src/libs/algolia/fetchAlgolia/fetchVenues/buildFetchVenuesQueryParameters.ts
+++ src/libs/algolia/fetchAlgolia/fetchVenues/buildFetchVenuesQueryParameters.ts
@@ -1,3 +1,4 @@
+import { VenuesFacets } from 'libs/algolia/enums'
 import { AlgoliaQueryParameters, FetchVenuesParameters } from 'libs/algolia/types'
 
 export const buildFetchVenuesQueryParameters = ({
@@ -5,5 +6,8 @@ export const buildFetchVenuesQueryParameters = ({
   attributesToHighlight = [],
 }: FetchVenuesParameters): AlgoliaQueryParameters => ({
   query: query || '',
-  requestOptions: { attributesToHighlight }, // By default We disable highlighting because we don't need it
+  requestOptions: {
+    attributesToHighlight,
+    facetFilters: [[`${VenuesFacets.has_at_least_one_bookable_offer}:true`]],
+  }, // By default We disable highlighting because we don't need it
 })
