9e02e2d49dddcede6bf53124cbf5818d89851ca4
NolwenMajorFrances <144016890+NolwenMajorFrances@users.noreply.github.com>
2024-01-11 17:38:16 +0100

    BSR import of a component that could be a native component and might be a misimport

--- src/features/home/components/modules/video/VideoMultiOfferList.tsx
+++ src/features/home/components/modules/video/VideoMultiOfferList.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FlatList } from 'react-native-gesture-handler'
+import { FlatList } from 'react-native'
 import styled from 'styled-components/native'
 
 import { OfferAnalyticsParams } from 'libs/analytics/types'
