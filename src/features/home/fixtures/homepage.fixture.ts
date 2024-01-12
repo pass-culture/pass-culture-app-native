0d9c54c94988d324d4389cb99e876a3ea86b0e1e
Tristan Maurier <tristan.maurier.pro@gmail.com>
2024-01-12 14:35:49 +0100

    remove unused params from VenuesModuleParameters types

--- src/features/home/fixtures/homepage.fixture.ts
+++ src/features/home/fixtures/homepage.fixture.ts
@@ -38,7 +38,6 @@ export const formattedVenuesModule: VenuesModule = {
   id: '105MMz59tftcxXJICXt7ja',
   venuesParameters: {
     title: 'Exemple de playlist de lieux',
-    isGeolocated: false,
     venueTypes: [
       'Bibliothèque ou médiathèque',
       'Arts visuels, arts plastiques et galeries',
