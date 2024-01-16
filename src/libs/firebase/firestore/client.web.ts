c2aba6ed7afb73345f3df12a39151030491fab59
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-01-16 09:17:42 +0100

    (PC-26799) fix(firestore): Fix Could not reach Cloud Firestore backend. Backend didn't respond within 10 seconds

---
+++ src/libs/firebase/firestore/client.web.ts
@@ -0,0 +1,4 @@
+import firestore from 'libs/firebase/shims/firestore/index.web'
+
+export const firestoreRemoteStore = firestore()
+firestoreRemoteStore.settings({ experimentalAutoDetectLongPolling: true, merge: true })
