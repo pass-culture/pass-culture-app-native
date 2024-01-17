a7b35c7892ec0311c98f8bc70fc8cb25eb41b37c
NolwenMajorFrances <144016890+NolwenMajorFrances@users.noreply.github.com>
2024-01-17 16:36:19 +0100

    PC-27022 remove GeolocationActivationModal from VenueSelectionModal because it's already in the LocationWrapper

--- src/features/offer/components/VenueSelectionModal/VenueSelectionModal.tsx
+++ src/features/offer/components/VenueSelectionModal/VenueSelectionModal.tsx
@@ -8,12 +8,9 @@ import {
   VenueSelectionListProps,
 } from 'features/offer/components/VenueSelectionList/VenueSelectionList'
 import { AutoScrollSwitch } from 'features/search/components/AutoScrollSwitch/AutoScrollSwitch'
-import { GeolocPermissionState, useLocation } from 'libs/location'
-import { GeolocationActivationModal } from 'libs/location/geolocation/components/GeolocationActivationModal'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { AppModal } from 'ui/components/modals/AppModal'
 import { ModalHeader } from 'ui/components/modals/ModalHeader'
-import { useModal } from 'ui/components/modals/useModal'
 import { Close } from 'ui/svg/icons/Close'
 import { Spacer, getSpacing } from 'ui/theme'
 import { useCustomSafeInsets } from 'ui/theme/useCustomSafeInsets'
@@ -57,17 +54,6 @@ export function VenueSelectionModal({
   const [autoScrollEnabled, setAutoScrollEnabled] = useState(true)
   const venueListRef = useRef<FlatList<VenueListItem>>(null)
   const { top } = useCustomSafeInsets()
-  const {
-    permissionState,
-    requestGeolocPermission,
-    onPressGeolocPermissionModalButton: onPressGeolocPermissionModalButtonDefault,
-  } = useLocation()
-
-  const {
-    showModal: showGeolocPermissionModal,
-    hideModal: hideGeolocPermissionModal,
-    visible: isGeolocPermissionModalVisible,
-  } = useModal(false)
 
   const handleSubmit = useCallback(() => {
     /**
@@ -76,21 +62,6 @@ export function VenueSelectionModal({
      */
     onSubmit(selectedOffer as number)
   }, [onSubmit, selectedOffer])
-
-  const onPressGeolocPermissionModalButton = useCallback(() => {
-    hideGeolocPermissionModal()
-    onPressGeolocPermissionModalButtonDefault()
-  }, [hideGeolocPermissionModal, onPressGeolocPermissionModalButtonDefault])
-
-  const onPressGeolocationBanner = useCallback(() => {
-    void (async () => {
-      if (permissionState === GeolocPermissionState.NEVER_ASK_AGAIN) {
-        showGeolocPermissionModal()
-      } else {
-        await requestGeolocPermission()
-      }
-    })()
-  }, [permissionState, requestGeolocPermission, showGeolocPermissionModal])
 
   const customHeader = useMemo(() => {
     return (
@@ -164,12 +135,6 @@ export function VenueSelectionModal({
         isFetchingNextPage={isFetchingNextPage}
         isSharingLocation={isSharingLocation}
         venueName={venueName}
-        onPressGeolocPermissionModalButton={onPressGeolocationBanner}
-      />
-      <GeolocationActivationModal
-        isGeolocPermissionModalVisible={isGeolocPermissionModalVisible}
-        hideGeolocPermissionModal={hideGeolocPermissionModal}
-        onPressGeolocPermissionModalButton={onPressGeolocPermissionModalButton}
       />
     </AppModal>
   )
