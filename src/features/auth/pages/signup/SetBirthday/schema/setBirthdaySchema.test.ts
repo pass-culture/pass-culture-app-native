9947d6619eee4d3e20c7aa631b2e209e1a654034
marineshaw <marines@bam.tech>
2024-01-19 17:28:24 +0100

    :sparkles: (SetBirthday) add age validation in schema

--- src/features/auth/pages/signup/SetBirthday/schema/setBirthdaySchema.test.ts
+++ src/features/auth/pages/signup/SetBirthday/schema/setBirthdaySchema.test.ts
@@ -1,6 +1,7 @@
 import { ValidationError } from 'yup'
 
 import { setBirthdaySchema } from 'features/auth/pages/signup/SetBirthday/schema/setBirthdaySchema'
+import { analytics } from 'libs/analytics'
 
 describe('setBirthdaySchema', () => {
   it('should validate when valid birthdate is provided', async () => {
@@ -17,5 +18,22 @@ describe('setBirthdaySchema', () => {
     await expect(result).rejects.toEqual(
       new ValidationError('La date de naissance est obligatoire')
     )
+  })
+
+  it('should reject when user is under 15 years old', async () => {
+    const input = { birthdate: new Date('2010-01-01') }
+    const result = setBirthdaySchema.validate(input)
+
+    await expect(result).rejects.toEqual(
+      new ValidationError('Tu dois avoir au moins 15\u00a0ans pour t’inscrire au pass Culture')
+    )
+  })
+
+  it('should log analytics if user is under 15 years old', async () => {
+    const input = { birthdate: new Date('2010-01-01') }
+    const result = setBirthdaySchema.validate(input)
+
+    await expect(result).rejects.toBeTruthy()
+    expect(analytics.logSignUpTooYoung).toHaveBeenCalledTimes(1)
   })
 })
--- src/features/auth/pages/signup/SetBirthday/schema/setBirthdaySchema.ts
+++ src/features/auth/pages/signup/SetBirthday/schema/setBirthdaySchema.ts
@@ -1,5 +1,22 @@
 import { date, object } from 'yup'
 
+import { analytics } from 'libs/analytics'
+import { dateDiffInFullYears } from 'libs/dates'
+
 export const setBirthdaySchema = object().shape({
-  birthdate: date().required('La date de naissance est obligatoire'),
+  birthdate: date()
+    .required('La date de naissance est obligatoire')
+    .test(
+      'is-old-enough',
+      'Tu dois avoir au moins 15\u00a0ans pour t’inscrire au pass Culture',
+      (date) => {
+        if (!date) return false
+        const age = dateDiffInFullYears(date, new Date())
+        if (age < 15) {
+          analytics.logSignUpTooYoung(age)
+          return false
+        }
+        return true
+      }
+    ),
 })
