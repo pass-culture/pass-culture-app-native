4860a443113735beef9370bf3e383841bed442a4
Pierre Canthelou <pierre.canthelou@passculture.app>
2024-01-24 14:36:46 +0100

    Fix remontée de l'écran sur la home quand on retape sur l'icone Home

--- src/features/navigation/TabBar/TabBar.tsx
+++ src/features/navigation/TabBar/TabBar.tsx
@@ -1,9 +1,11 @@
-import { BottomTabBarProps } from '@react-navigation/bottom-tabs'
+import { BottomTabBarProps, BottomTabNavigationEventMap } from '@react-navigation/bottom-tabs'
+import { NavigationHelpers, ParamListBase } from '@react-navigation/native'
 import React from 'react'
 
 import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
 import { TabBarContainer } from 'features/navigation/TabBar/TabBarContainer'
 import { useTabNavigationContext } from 'features/navigation/TabBar/TabNavigationStateContext'
+import { TabStateRoute } from 'features/navigation/TabBar/types'
 import { initialSearchState } from 'features/search/context/reducer'
 import { useSearch } from 'features/search/context/SearchWrapper'
 
@@ -11,6 +13,19 @@ import { mapTabRouteToBicolorIcon } from './mapTabRouteToBicolorIcon'
 import { TabBarComponent } from './TabBarComponent'
 
 type Props = Pick<BottomTabBarProps, 'navigation'>
+
+const scrollTopOfHomeIfOnHome = (
+  navigation: NavigationHelpers<ParamListBase, BottomTabNavigationEventMap>,
+  route: TabStateRoute
+) => {
+  if (route.name === 'Home' && route.isSelected) {
+    navigation.emit({
+      type: 'tabPress',
+      target: route.key,
+      canPreventDefault: true,
+    })
+  }
+}
 
 export const TabBar: React.FC<Props> = ({ navigation }) => {
   const { tabRoutes } = useTabNavigationContext()
@@ -33,6 +48,7 @@ export const TabBar: React.FC<Props> = ({ navigation }) => {
             }
           } else {
             const params = route.name === 'Home' ? undefined : route.params
+            scrollTopOfHomeIfOnHome(navigation, route)
             navigation.navigate('TabNavigator', { screen: route.name, params })
           }
         }
