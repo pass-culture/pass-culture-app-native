5aa368baa0d85c57be2bcbb1568d7b2cd9ebd757
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-01-25 12:07:02 +0100

    ideas to fix error

--- src/features/offer/pages/Offer/Offer.tsx
+++ src/features/offer/pages/Offer/Offer.tsx
@@ -1,14 +1,17 @@
 import { useRoute } from '@react-navigation/native'
-import React from 'react'
+import React, { useState } from 'react'
 
+import { useQueryClient } from '__mocks__/react-query'
 import { UseRouteType } from 'features/navigation/RootNavigator/types'
 import { useOffer } from 'features/offer/api/useOffer'
 import { OfferContentOld } from 'features/offer/components/OfferContentOld/OfferContentOld'
-import { getSearchGroupAndNativeCategoryFromSubcategoryId } from 'features/offer/helpers/getSearchGroupAndNativeCategoryFromSubcategoryId/getSearchGroupAndNativeCategoryFromSubcategoryId'
 import { OfferContent } from 'features/offerv2/components/OfferContent/OfferContent'
 import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
-import { useSubcategoriesMapping } from 'libs/subcategories/mappings'
+import { eventMonitoring } from 'libs/monitoring'
+import { QueryKeys } from 'libs/queryKeys'
+import { useSubcategoriesMapping } from 'libs/subcategories'
+import { Subcategory } from 'libs/subcategories/types'
 import { useSubcategories } from 'libs/subcategories/useSubcategories'
 
 export function Offer() {
@@ -20,13 +23,34 @@ export function Offer() {
   const subcategoriesMapping = useSubcategoriesMapping()
 
   const shouldDisplayOfferV2 = useFeatureFlag(RemoteStoreFeatureFlags.WIP_OFFER_V2)
+  const queryClient = useQueryClient()
+
+  const [refetchDone, setRefetchDone] = useState(false)
+
+  const fetchData = () => {
+    console.log('je passe')
+    queryClient.invalidateQueries(QueryKeys.SUBCATEGORIES)
+    setRefetchDone(true)
+  }
 
   if (!offer || !subcategories) return null
 
-  const { searchGroupName, nativeCategory } = getSearchGroupAndNativeCategoryFromSubcategoryId(
-    subcategories,
-    offer.subcategoryId
-  )
+  const subcategory: Subcategory = subcategoriesMapping[offer.subcategoryId]
+
+  if (!subcategoriesMapping[offer.subcategoryId] && !refetchDone) {
+    fetchData()
+  }
+
+  if (!subcategory) {
+    eventMonitoring.captureException('Subcategory not found', {
+      extra: {
+        subcategoryId: offer.subcategoryId,
+        subcategories: subcategories.subcategories,
+      },
+    })
+
+    throw new Error('Subcategory not found')
+  }
 
   return shouldDisplayOfferV2 ? (
     <OfferContent
@@ -37,10 +61,59 @@ export function Offer() {
   ) : (
     <OfferContentOld
       offer={offer}
-      offerNativeCategory={nativeCategory}
-      offerSearchGroup={searchGroupName}
+      offerNativeCategory={subcategory.nativeCategoryId}
+      offerSearchGroup={subcategory.searchGroupName}
       searchGroupList={subcategories.searchGroups}
       subcategory={subcategoriesMapping[offer.subcategoryId]}
     />
   )
 }
+
+// export function Offer() {
+//   const route = useRoute<UseRouteType<'Offer'>>()
+//   const offerId = route.params?.id
+
+//   const { data: offer } = useOffer({ offerId })
+//   const { data: subcategories, refetch } = useSubcategories()
+//   const subcategoriesMapping = useSubcategoriesMapping()
+
+//   const [refetchDone, setRefetchDone] = useState(false)
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       if (!subcategoriesMapping[offer.subcategoryId]) {
+//         console.log('je passe')
+//         await refetch()
+//         setRefetchDone(true)
+//       }
+//     }
+
+//     fetchData()
+//   }, [subcategoriesMapping, refetch, offer.subcategoryId])
+
+//   if (!offer || !subcategories) return null
+
+//   const subcategory: Subcategory = subcategoriesMapping[offer.subcategoryId]
+
+//   if (refetchDone) {
+//     eventMonitoring.captureException('Subcategory not found', {
+//       extra: {
+//         subcategoryId: offer.subcategoryId,
+//         subcategories: subcategories.subcategories,
+//       },
+//     })
+
+//     throw new Error('Subcategory not found')
+//   }
+
+//   return (
+//     <OfferContent
+//       offer={offer}
+//       offerNativeCategory={subcategory.nativeCategoryId}
+//       offerSearchGroup={subcategory.searchGroupName}
+//       searchGroupList={subcategories.searchGroups}
+//     />
+//   )
+// }
+
+// L'idée était d'avoir une seule invalidation de cache si jamais on ne trouve pas de concordance avec la sous-catégorie de l'offre qu'on souhaite afficher
