d7b5839ec682f76e64131cf04a1d9bd878120993
Tristan Maurier <tristan.maurier.pro@gmail.com>
2024-01-31 17:39:26 +0100

    remove serveruse from simulatebackend

--- src/features/favorites/helpers/simulateBackend.ts
+++ src/features/favorites/helpers/simulateBackend.ts
@@ -1,13 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { rest } from 'msw'
 
-import { OfferResponse } from 'api/gen'
+import {
+  FavoriteResponse,
+  OfferResponse,
+  PaginatedFavoritesResponse,
+  UserProfileResponse,
+  SubcategoriesResponseModelv2,
+} from 'api/gen'
 import { favoriteResponseSnap } from 'features/favorites/fixtures/favoriteResponseSnap'
 import { paginatedFavoritesResponseSnap } from 'features/favorites/fixtures/paginatedFavoritesResponseSnap'
 import { offerResponseSnap } from 'features/offer/fixtures/offerResponse'
-import { env } from 'libs/environment'
+import { beneficiaryUser } from 'fixtures/user'
 import { EmptyResponse } from 'libs/fetch'
-import { server } from 'tests/server'
+import { placeholderData } from 'libs/subcategories/placeholderData'
+import { mockServer } from 'tests/mswServer'
 
 const offerId = 146193
 
@@ -30,25 +36,40 @@ export function simulateBackend(options: Options = defaultOptions) {
     ...defaultOptions,
     ...options,
   }
-  server.use(
-    rest.get<OfferResponse>(`${env.API_BASE_URL}/native/v1/offer/${id}`, (req, res, ctx) =>
-      res(ctx.status(200), ctx.json(offerResponseSnap))
-    ),
-    rest.post<EmptyResponse>(`${env.API_BASE_URL}/native/v1/me/favorites`, (req, res, ctx) => {
-      if (hasTooManyFavorites) {
-        return res(ctx.status(400), ctx.json({ code: 'MAX_FAVORITES_REACHED' }))
-      } else if (hasAddFavoriteError) {
-        return res(ctx.status(422), ctx.json({}))
-      } else {
-        return res(ctx.status(200), ctx.json(favoriteResponseSnap))
-      }
-    }),
-    rest.delete<EmptyResponse>(
-      `${env.API_BASE_URL}/native/v1/me/favorites/${
-        paginatedFavoritesResponseSnap.favorites.find((f) => f.offer.id === id)?.id
-      }`,
-      (req, res, ctx) =>
-        !hasRemoveFavoriteError ? res(ctx.status(204)) : res(ctx.status(422), ctx.json({}))
-    )
-  )
+
+  mockServer.getApiV1<OfferResponse>(`/offer/${id}`, offerResponseSnap)
+  mockServer.getApiV1<UserProfileResponse>(`/me`, beneficiaryUser)
+  mockServer.getApiV1<PaginatedFavoritesResponse>(`/me/favorites`, paginatedFavoritesResponseSnap)
+  mockServer.getApiV1<SubcategoriesResponseModelv2>(`/subcategories/v2`, { ...placeholderData })
+  if (hasAddFavoriteError) {
+    mockServer.postApiV1<EmptyResponse>(`/me/favorites`, {
+      responseOptions: { statusCode: 422, data: {} },
+    })
+  } else if (hasTooManyFavorites) {
+    mockServer.postApiV1(`/me/favorites`, {
+      responseOptions: { statusCode: 400, data: { code: 'MAX_FAVORITES_REACHED' } },
+    })
+  } else {
+    mockServer.postApiV1<FavoriteResponse>(`/me/favorites`, {
+      responseOptions: { statusCode: 200, data: favoriteResponseSnap },
+    })
+  }
+
+  !hasRemoveFavoriteError
+    ? mockServer.deleteApiV1<EmptyResponse>(
+        `/me/favorites/${
+          paginatedFavoritesResponseSnap.favorites.find((f) => f.offer.id === id)?.id
+        }`,
+        {
+          responseOptions: { statusCode: 204 },
+        }
+      )
+    : mockServer.deleteApiV1<EmptyResponse>(
+        `/me/favorites/${
+          paginatedFavoritesResponseSnap.favorites.find((f) => f.offer.id === id)?.id
+        }`,
+        {
+          responseOptions: { statusCode: 422, data: {} },
+        }
+      )
 }
