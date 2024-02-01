04509e97f1f7e25926ccd7ced797ac3d4c6e7e23
Hugo Voisin <hugo.voisin@passculture.app>
2024-02-01 14:40:03 +0100

    (PC-27721) feat(config): add wipCinemaOfferVenueBlock feature flag (#5904)

--- src/libs/firebase/firestore/featureFlags/getFeatureFlag.native.test.ts
+++ src/libs/firebase/firestore/featureFlags/getFeatureFlag.native.test.ts
@@ -10,6 +10,7 @@ const { collection } = firestore()
 describe('getFeatureFlag', () => {
   it.each([
     RemoteStoreFeatureFlags.FAV_LIST_FAKE_DOOR,
+    RemoteStoreFeatureFlags.WIP_CINEMA_OFFER_VENUE_BLOCK,
     RemoteStoreFeatureFlags.WIP_DISABLE_STORE_REVIEW,
     RemoteStoreFeatureFlags.WIP_DISPLAY_SEARCH_NB_FACET_RESULTS,
     RemoteStoreFeatureFlags.WIP_ENABLE_APP_LOCATION,
