4aa95e5a0f46ce3060c85b093efbe94489a42d5d
Hugo Voisin <hugov@bam.tech>
2024-02-02 17:03:15 +0100

    feat: add icon from figma

--- src/features/internal/cheatcodes/pages/AppComponents/iconsExports.ts
+++ src/features/internal/cheatcodes/pages/AppComponents/iconsExports.ts
@@ -96,6 +96,7 @@ import { Profile } from 'ui/svg/icons/Profile'
 import { ProfileDeletion } from 'ui/svg/icons/ProfileDeletion'
 import { Question } from 'ui/svg/icons/Question'
 import { Quote } from 'ui/svg/icons/Quote'
+import { RightFilled } from 'ui/svg/icons/RightFilled'
 import { ScrollToTop } from 'ui/svg/icons/ScrollToTop'
 import { Show } from 'ui/svg/icons/Show'
 import { SignOut } from 'ui/svg/icons/SignOut'
@@ -257,6 +258,7 @@ export const TertiaryAndSmallerIcons = {
   PlusLight,
   Profile,
   Question,
+  RightFilled,
   Show,
   SMSFilled,
   Stock,
