c6165302e20640b66eff952cf1ad68d1d072d8a6
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-02-06 14:21:33 +0100

    (PC-26771) chore(server): Remove default activity types mock

--- src/tests/server.ts
+++ src/tests/server.ts
@@ -1,20 +1,3 @@
-import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 
-import { ActivityTypesResponse } from 'api/gen'
-import { ActivityTypesSnap } from 'features/identityCheck/pages/profile/fixtures/mockedActivityTypes'
-import { env } from 'libs/environment'
-
-export const server = setupServer(
-  rest.get<ActivityTypesResponse>(
-    env.API_BASE_URL + '/native/v1/subscription/activity_types',
-    (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          activities: ActivityTypesSnap.activities,
-        })
-      )
-    }
-  )
-)
+export const server = setupServer()
