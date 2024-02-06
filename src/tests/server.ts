56853bc6f5ffc1bfb952df0b0c1d32b6633f94a1
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-02-06 12:13:33 +0100

    (PC-26771) chore(server): Remove default offer reports mock

--- src/tests/server.ts
+++ src/tests/server.ts
@@ -1,28 +1,11 @@
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 
-import {
-  ActivityTypesResponse,
-  PhoneValidationRemainingAttemptsRequest,
-  Reason,
-  UserReportedOffersResponse,
-} from 'api/gen'
+import { ActivityTypesResponse, PhoneValidationRemainingAttemptsRequest } from 'api/gen'
 import { ActivityTypesSnap } from 'features/identityCheck/pages/profile/fixtures/mockedActivityTypes'
 import { env } from 'libs/environment'
 
 export const server = setupServer(
-  rest.get<UserReportedOffersResponse>(
-    env.API_BASE_URL + '/native/v1/offers/reports',
-    (_req, res, ctx) =>
-      res(
-        ctx.status(200),
-        ctx.json({
-          reportedOffers: [
-            { offerId: 1234, reason: Reason.INAPPROPRIATE, reportedAt: '123456789' },
-          ],
-        })
-      )
-  ),
   rest.get<PhoneValidationRemainingAttemptsRequest>(
     env.API_BASE_URL + '/native/v1/phone_validation/remaining_attempts',
     (req, res, ctx) => {
