fcb7e26bf8b84a021b5f000fa7fca261c397a685
Hugo Voisin <hugov@bam.tech>
2024-02-06 16:40:59 +0100

    feat: add design to new offer venue block

--- src/features/offerv2/components/OfferVenueBlock/OfferVenueBlock.tsx
+++ src/features/offerv2/components/OfferVenueBlock/OfferVenueBlock.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { ComponentProps, FunctionComponent, useMemo } from 'react'
+import { View } from 'react-native'
 import styled from 'styled-components/native'
 
 import { OfferVenueResponse } from 'api/gen'
@@ -6,14 +7,19 @@ import { useVenueBlock } from 'features/offerv2/components/OfferVenueBlock/useVe
 import { formatFullAddressStartsWithPostalCode } from 'libs/address/useFormatFullAddress'
 import { SeeItineraryButton } from 'libs/itinerary/components/SeeItineraryButton'
 import { getGoogleMapsItineraryUrl } from 'libs/itinerary/openGoogleMapsItinerary'
+import { Image } from 'libs/resizing-image-on-demand/Image'
 import { ButtonSecondaryBlack } from 'ui/components/buttons/ButtonSecondaryBlack'
 import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
 import { Separator } from 'ui/components/Separator'
 import { Tag } from 'ui/components/Tag/Tag'
+import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
 import { Duplicate } from 'ui/svg/icons/Duplicate'
 import { EditPen } from 'ui/svg/icons/EditPen'
-import { Spacer, Typo } from 'ui/theme'
+import { RightFilled } from 'ui/svg/icons/RightFilled'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
 import { getHeadingAttrs } from 'ui/theme/typographyAttrs/getHeadingAttrs'
+
+const VENUE_THUMBNAIL_SIZE = getSpacing(14)
 
 type Props = {
   distance?: string
@@ -38,25 +44,51 @@ export function OfferVenueBlock({
     venue.postalCode,
     venue.city
   )
+  const hasVenuePage = !!onSeeVenuePress
+  const TouchableContainer: FunctionComponent<ComponentProps<typeof InternalTouchableLink>> =
+    useMemo(
+      () =>
+        styled(hasVenuePage ? InternalTouchableLink : View)({
+          flexDirection: 'row',
+        }),
+      [hasVenuePage]
+    )
 
   return (
     <Container>
       <Typo.Title3 {...getHeadingAttrs(2)}>{title}</Typo.Title3>
-
       <Spacer.Column numberOfSpaces={4} />
-      <StyledSeparator />
-      <Spacer.Column numberOfSpaces={6} />
-
-      <Typo.ButtonText>{venueName}</Typo.ButtonText>
-      <Spacer.Column numberOfSpaces={1} />
-      <Address>{address}</Address>
 
       {distance ? (
         <React.Fragment>
-          <Spacer.Column numberOfSpaces={4} />
           <Tag label={`à ${distance}`} />
+          <Spacer.Column numberOfSpaces={4} />
         </React.Fragment>
       ) : null}
+
+      <TouchableContainer
+        navigateTo={{ screen: 'Venue', params: { id: venue.id } }}
+        onBeforeNavigate={onSeeVenuePress}>
+        <VenueThumbnail
+          height={VENUE_THUMBNAIL_SIZE}
+          width={VENUE_THUMBNAIL_SIZE}
+          url="https://storage.googleapis.com/passculture-metier-ehp-staging-assets-fine-grained/assets/venue_default_images/becca-tapert-GnY_mW1Q6Xc-unsplash.png"
+        />
+        <Spacer.Row numberOfSpaces={2} />
+        <View>
+          <VenueTitleContainer>
+            <Typo.ButtonText>{venueName}</Typo.ButtonText>
+            {hasVenuePage ? (
+              <React.Fragment>
+                <Spacer.Row numberOfSpaces={1} />
+                <RightFilled size={16} />
+              </React.Fragment>
+            ) : null}
+          </VenueTitleContainer>
+          <Spacer.Column numberOfSpaces={1} />
+          <Address>{address}</Address>
+        </View>
+      </TouchableContainer>
 
       {onChangeVenuePress ? (
         <React.Fragment>
@@ -105,6 +137,17 @@ const Container = styled.View({
 
 const StyledSeparator = styled(Separator.Horizontal)(({ theme }) => ({
   backgroundColor: theme.colors.greyMedium,
+}))
+
+const VenueTitleContainer = styled.View({
+  flexDirection: 'row',
+  alignItems: 'center',
+})
+
+const VenueThumbnail = styled(Image)<{ height: number; width: number }>(({ height, width }) => ({
+  borderRadius: 4,
+  height,
+  width,
 }))
 
 const TertiaryButtonWrapper = styled.View({
