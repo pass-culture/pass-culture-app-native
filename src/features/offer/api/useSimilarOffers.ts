7160d0f75232a7b143768d3947de778359368589
YassinL <yassin.leclercq1@gmail.com>
2024-02-07 15:43:43 +0100

    (PC-26922) refactor(useSimilarOffers): use useQuery instead of useState/useEffect method

--- src/features/offer/api/useSimilarOffers.ts
+++ src/features/offer/api/useSimilarOffers.ts
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
+import { useQuery } from 'react-query'
 
 import { SearchGroupNameEnumv2, SearchGroupResponseModelv2 } from 'api/gen'
 import { useAuthContext } from 'features/auth/context/AuthContext'
@@ -8,6 +9,7 @@ import { usePrevious } from 'features/search/helpers/usePrevious'
 import { env } from 'libs/environment'
 import { Position } from 'libs/location'
 import { eventMonitoring } from 'libs/monitoring'
+import { QueryKeys } from 'libs/queryKeys'
 
 type WithIncludeCategoryProps = {
   categoryIncluded: SearchGroupNameEnumv2
@@ -47,7 +49,9 @@ export const getSimilarOffersEndpoint = (
   return endpoint + urlParams.toString()
 }
 
-export const getApiRecoSimilarOffers = async (similarOffersEndpoint: string) => {
+export const getApiRecoSimilarOffers = async (
+  similarOffersEndpoint: string
+): Promise<SimilarOffersResponse | undefined> => {
   try {
     const similarOffers = await fetch(similarOffersEndpoint)
     const json: SimilarOffersResponse = await similarOffers.json()
@@ -93,8 +97,9 @@ export const useSimilarOffers = ({
   )
 
   const { user: profile } = useAuthContext()
-  const similarOffersEndpoint = getSimilarOffersEndpoint(offerId, profile?.id, position, categories)
-  const [apiRecoResponse, setApiRecoResponse] = useState<SimilarOffersResponse>()
+  const similarOffersEndpoint =
+    getSimilarOffersEndpoint(offerId, profile?.id, position, categories) ?? ''
+
   const previousPosition = usePrevious(position)
   const previousCategoryExcluded = usePrevious(categoryExcluded)
   const previousCategoryIncluded = usePrevious(categoryIncluded)
@@ -102,30 +107,16 @@ export const useSimilarOffers = ({
   const hasSameCategoryIncluded = categoryIncluded === previousCategoryIncluded
   const hasSamePosition = JSON.stringify(previousPosition) === JSON.stringify(position)
 
-  const fetchApiReco = useCallback(async () => {
-    if (
-      !similarOffersEndpoint ||
-      // These conditions help avoid multiple unnecessary API calls
-      // So we have a call only when the excluded or included category changes as well as the position
-      (categoryExcluded && hasSameCategoryExcluded && hasSamePosition) ||
-      (categoryIncluded && hasSameCategoryIncluded && hasSamePosition)
-    ) {
-      return
+  const { data: apiRecoResponse } = useQuery(
+    [QueryKeys.SIMILAR_OFFERS, offerId, position, categories],
+    () => getApiRecoSimilarOffers(similarOffersEndpoint),
+    {
+      enabled:
+        !!similarOffersEndpoint ||
+        (categoryExcluded && hasSameCategoryExcluded && hasSamePosition) ||
+        (categoryIncluded && hasSameCategoryIncluded && hasSamePosition),
     }
-
-    setApiRecoResponse(await getApiRecoSimilarOffers(similarOffersEndpoint))
-  }, [
-    categoryExcluded,
-    categoryIncluded,
-    hasSameCategoryExcluded,
-    hasSameCategoryIncluded,
-    hasSamePosition,
-    similarOffersEndpoint,
-  ])
-
-  useEffect(() => {
-    fetchApiReco()
-  }, [fetchApiReco])
+  )
 
   return {
     similarOffers: useAlgoliaSimilarOffers(apiRecoResponse?.results ?? [], true),
