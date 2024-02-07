aa93f908d6e2a32f98c2ae303f1a46017c194c5c
Tristan Maurier <tristan.maurier.pro@gmail.com>
2024-02-07 15:46:20 +0100

    fix getCurrentVenuesIndex

--- src/libs/algolia/fetchAlgolia/helpers/getCurrentVenuesIndex.ts
+++ src/libs/algolia/fetchAlgolia/helpers/getCurrentVenuesIndex.ts
@@ -1,21 +1,18 @@
-import { Venue } from 'features/venue/types'
 import { LocationMode } from 'libs/algolia'
 import { env } from 'libs/environment'
 import { Position } from 'libs/location'
 
 export function getCurrentVenuesIndex({
-  locationType,
+  selectedLocationMode,
   userLocation,
-  venue,
 }: {
-  locationType?: LocationMode
-  userLocation?: Position
-  venue?: Venue
+  selectedLocationMode: LocationMode
+  userLocation: Position
 }) {
   const venuesIndexSearch = env.ALGOLIA_VENUES_INDEX_PLAYLIST_SEARCH
   const venuesIndexSearchNewest = env.ALGOLIA_VENUES_INDEX_PLAYLIST_SEARCH_NEWEST
 
-  if ((!locationType && !venue) || (locationType === LocationMode.EVERYWHERE && !userLocation)) {
+  if (selectedLocationMode === LocationMode.EVERYWHERE && !userLocation) {
     return venuesIndexSearchNewest
   }
 
