7aa66e7024b963b94e00e879e6dfd2170f5892a6
Hugo Voisin <hugov@bam.tech>
2024-02-09 12:35:19 +0100

    docs: explain why we don't use onTextLayout on web

--- src/ui/components/CollapsibleText/useIsTextEllipsis.ts
+++ src/ui/components/CollapsibleText/useIsTextEllipsis.ts
@@ -3,6 +3,8 @@ import { useTheme } from 'styled-components/native'
 
 import { IsTextEllipsisOutput } from './types'
 
+// React Native Web doesn't support onTextLayout
+// https://github.com/necolas/react-native-web/issues/1685
 export const useIsTextEllipsis = (numberOfLines: number): IsTextEllipsisOutput => {
   const [isTextEllipsis, setIsTextEllipsis] = useState(false)
   const theme = useTheme()
