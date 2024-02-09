87e59565fe0410680fde047246ae7f12952d3aab
Hugo Voisin <hugov@bam.tech>
2024-02-09 12:37:05 +0100

    feat: improve ellipsis detection on iOS explaining the onTextLayout difference between Android and iOS

--- src/ui/components/CollapsibleText/useIsTextEllipsis.ios.ts
+++ src/ui/components/CollapsibleText/useIsTextEllipsis.ios.ts
@@ -1,21 +1,28 @@
-import { useCallback, useState } from 'react'
-import { useTheme } from 'styled-components/native'
+import { useCallback, useRef, useState } from 'react'
 
 import { IsTextEllipsisOutput } from './types'
 
+const ELLIPSIS_LENGTH = 10
+
+// On iOS, onTextLayout doesn't return more text lines than numberOfLines.
+// So we check if the last line is long enough, compared to the whole text width.
 export const useIsTextEllipsis = (numberOfLines: number): IsTextEllipsisOutput => {
+  const textWidth = useRef<number | undefined>(undefined)
   const [isTextEllipsis, setIsTextEllipsis] = useState(false)
-  const theme = useTheme()
-  const lineHeight = Number(theme.typography.body.lineHeight.slice(0, -2))
 
-  const onLayout: Required<IsTextEllipsisOutput>['onLayout'] = useCallback(
+  const onTextLayout: Required<IsTextEllipsisOutput>['onTextLayout'] = useCallback(
     (event) => {
-      const textHeight = event.nativeEvent.layout.height
-      const maxTextHeight = lineHeight * numberOfLines
-
-      setIsTextEllipsis(textHeight >= maxTextHeight)
+      const linesWidth = event.nativeEvent.lines.map((line) => line.width)
+      if (linesWidth.length === numberOfLines && textWidth.current !== undefined) {
+        setIsTextEllipsis(linesWidth[linesWidth.length - 1] >= textWidth.current - ELLIPSIS_LENGTH)
+      }
     },
-    [lineHeight, numberOfLines]
+    [numberOfLines]
   )
-  return { isTextEllipsis, onLayout }
+
+  const onLayout: Required<IsTextEllipsisOutput>['onLayout'] = useCallback((event) => {
+    textWidth.current = event.nativeEvent.layout.width
+  }, [])
+
+  return { isTextEllipsis, onTextLayout, onLayout }
 }
