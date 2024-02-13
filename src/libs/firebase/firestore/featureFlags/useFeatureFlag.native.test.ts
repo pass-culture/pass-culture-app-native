2d43b464ffc763014ac616dbc3644af1b7a95e99
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-02-13 14:46:07 +0100

    (PC-27868) test(FF): Refactor tests

--- src/libs/firebase/firestore/featureFlags/useFeatureFlag.native.test.ts
+++ src/libs/firebase/firestore/featureFlags/useFeatureFlag.native.test.ts
@@ -13,23 +13,9 @@ const { collection } = firestore()
 
 const mockGet = jest.fn()
 
-describe.each([
-  RemoteStoreFeatureFlags.DISABLE_OLD_CHANGE_EMAIL,
-  RemoteStoreFeatureFlags.FAV_LIST_FAKE_DOOR,
-  RemoteStoreFeatureFlags.WIP_CINEMA_OFFER_VENUE_BLOCK,
-  RemoteStoreFeatureFlags.WIP_DISABLE_STORE_REVIEW,
-  RemoteStoreFeatureFlags.WIP_DISPLAY_SEARCH_NB_FACET_RESULTS,
-  RemoteStoreFeatureFlags.WIP_ENABLE_EMAIL_VALIDATION_RESEND,
-  RemoteStoreFeatureFlags.WIP_ENABLE_GOOGLE_SSO,
-  RemoteStoreFeatureFlags.WIP_ENABLE_GTL_PLAYLISTS_IN_BOOKSTORE_VENUES,
-  RemoteStoreFeatureFlags.WIP_OFFER_V2,
-  RemoteStoreFeatureFlags.WIP_SAME_ARTIST_PLAYLIST,
-  RemoteStoreFeatureFlags.WIP_STEPPER_RETRY_UBBLE,
-  RemoteStoreFeatureFlags.WIP_NEW_MAPPING_BOOKS,
-  RemoteStoreFeatureFlags.WIP_VENUE_MAP,
-  RemoteStoreFeatureFlags.WIP_VENUE_MAP_WITHOUT_PREVIEW,
-  RemoteStoreFeatureFlags.WIP_SEARCH_ACCESSIBILITY_FILTER,
-])('useFeatureFlag %s', (featureFlag: RemoteStoreFeatureFlags) => {
+const featureFlag = RemoteStoreFeatureFlags.WIP_DISABLE_STORE_REVIEW
+
+describe('useFeatureFlag', () => {
   beforeAll(() =>
     collection(featureFlag)
       .doc('testing')
@@ -37,34 +23,6 @@ describe.each([
       .get.mockResolvedValue({
         get: mockGet,
       })
-  )
-
-  it.each`
-    firebaseFeatureFlag | minimalBuildNumber  | expected
-    ${false}            | ${undefined}        | ${'disabled when firestore minimalBuildNumber is undefined'}
-    ${false}            | ${buildVersion + 1} | ${'disabled when build number is below firestore minimalBuildNumber'}
-    ${true}             | ${buildVersion}     | ${'enabled when build number is equal to firestore minimalBuildNumber'}
-    ${true}             | ${buildVersion - 1} | ${'enabled when build number is greater than firestore minimalBuildNumber'}
-  `(
-    `should be $expected`,
-    async ({
-      firebaseFeatureFlag,
-      minimalBuildNumber,
-    }: {
-      firebaseFeatureFlag: boolean | undefined
-      minimalBuildNumber: number | undefined
-    }) => {
-      const firestoreData = {
-        minimalBuildNumber,
-      }
-      mockGet.mockReturnValueOnce(firestoreData)
-
-      const { result } = renderHook(() => useFeatureFlag(featureFlag))
-
-      await act(async () => {})
-
-      expect(result.current).toBe(firebaseFeatureFlag)
-    }
   )
 
   it('should deactivate FF when no build number is given', async () => {
@@ -78,36 +36,65 @@ describe.each([
     expect(result.current).toBeFalsy()
   })
 
-  it('should activate FF when version is below maximalBuildNumber', async () => {
-    const firestoreData = { maximalBuildNumber: buildVersion + 1 }
-    mockGet.mockReturnValueOnce(firestoreData)
+  describe('minimalBuildNumber', () => {
+    it.each`
+      firebaseFeatureFlag | minimalBuildNumber  | expected
+      ${false}            | ${buildVersion + 1} | ${'disabled when build number is below firestore minimalBuildNumber'}
+      ${true}             | ${buildVersion}     | ${'enabled when build number is equal to firestore minimalBuildNumber'}
+      ${true}             | ${buildVersion - 1} | ${'enabled when build number is greater than firestore minimalBuildNumber'}
+    `(
+      `should be $expected`,
+      async ({
+        firebaseFeatureFlag,
+        minimalBuildNumber,
+      }: {
+        firebaseFeatureFlag?: boolean
+        minimalBuildNumber?: number
+      }) => {
+        const firestoreData = { minimalBuildNumber }
+        mockGet.mockReturnValueOnce(firestoreData)
 
-    const { result } = renderHook(() => useFeatureFlag(featureFlag))
+        const { result } = renderHook(() => useFeatureFlag(featureFlag))
 
-    await act(async () => {})
+        await act(async () => {})
 
-    expect(result.current).toBeTruthy()
+        expect(result.current).toBe(firebaseFeatureFlag)
+      }
+    )
   })
 
-  it('should activate FF when version is equal to maximalBuildNumber', async () => {
-    const firestoreData = { maximalBuildNumber: buildVersion }
-    mockGet.mockReturnValueOnce(firestoreData)
+  describe('maximalBuildNumber', () => {
+    it('should activate FF when version is below maximalBuildNumber', async () => {
+      const firestoreData = { maximalBuildNumber: buildVersion + 1 }
+      mockGet.mockReturnValueOnce(firestoreData)
 
-    const { result } = renderHook(() => useFeatureFlag(featureFlag))
+      const { result } = renderHook(() => useFeatureFlag(featureFlag))
 
-    await act(async () => {})
+      await act(async () => {})
 
-    expect(result.current).toBeTruthy()
-  })
+      expect(result.current).toBeTruthy()
+    })
 
-  it('should deactivate FF when version is greater than maximalBuildNumber', async () => {
-    const firestoreData = { maximalBuildNumber: buildVersion - 1 }
-    mockGet.mockReturnValueOnce(firestoreData)
+    it('should activate FF when version is equal to maximalBuildNumber', async () => {
+      const firestoreData = { maximalBuildNumber: buildVersion }
+      mockGet.mockReturnValueOnce(firestoreData)
 
-    const { result } = renderHook(() => useFeatureFlag(featureFlag))
+      const { result } = renderHook(() => useFeatureFlag(featureFlag))
 
-    await act(async () => {})
+      await act(async () => {})
 
-    expect(result.current).toBeFalsy()
+      expect(result.current).toBeTruthy()
+    })
+
+    it('should deactivate FF when version is greater than maximalBuildNumber', async () => {
+      const firestoreData = { maximalBuildNumber: buildVersion - 1 }
+      mockGet.mockReturnValueOnce(firestoreData)
+
+      const { result } = renderHook(() => useFeatureFlag(featureFlag))
+
+      await act(async () => {})
+
+      expect(result.current).toBeFalsy()
+    })
   })
 })
