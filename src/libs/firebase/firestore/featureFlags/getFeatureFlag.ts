fb4ced3ec660332eac57d293e73f0bee667fd343
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-02-13 14:45:42 +0100

    (PC-27868) fix(firestore): Fix FF typing

--- src/libs/firebase/firestore/featureFlags/getFeatureFlag.ts
+++ src/libs/firebase/firestore/featureFlags/getFeatureFlag.ts
@@ -3,16 +3,17 @@ import { firestoreRemoteStore } from 'libs/firebase/firestore/client'
 import { RemoteStoreCollections, RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 import { captureMonitoringError } from 'libs/monitoring'
 
+type FeatureFlagConfig = { minimalBuildNumber?: number; maximalBuildNumber?: number }
+
 export const getFeatureFlag = (
   featureFlag: RemoteStoreFeatureFlags
-): Promise<void | { minimalBuildNumber?: number; maximalBuildNumber?: number } | null> =>
+): Promise<void | FeatureFlagConfig | null> =>
   firestoreRemoteStore
     .collection(RemoteStoreCollections.FEATURE_FLAGS)
     .doc(env.ENV)
     .get()
     .then((docSnapshot) => {
-      const firebaseFeatureFlag =
-        docSnapshot.get<{ minimalBuildNumber: number; maximalBuildNumber?: number }>(featureFlag)
+      const firebaseFeatureFlag = docSnapshot.get<FeatureFlagConfig>(featureFlag)
 
       if (firebaseFeatureFlag === undefined) return null
       return firebaseFeatureFlag
