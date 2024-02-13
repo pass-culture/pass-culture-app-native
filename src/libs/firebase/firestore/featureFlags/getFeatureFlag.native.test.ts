8a8d9947531a301117c2d399470041250b8365c5
Inès Mouandjo Lobé <158567429+inesmouandjolobe@users.noreply.github.com>
2024-02-13 16:40:54 +0100

    (PC-27933) feat(Search): add Feature Flag WIP_SEARCH_ACCESSIBILITY_FILTERS (#5953)
    * (PC-27933): Create feature flag for accessibility filters
    
    * (PC-27933): fix wording
    
    ---------
    
    Co-authored-by: Inès ML <ines.mouandjolobe@passculture.app>

--- src/libs/firebase/firestore/featureFlags/getFeatureFlag.native.test.ts
+++ src/libs/firebase/firestore/featureFlags/getFeatureFlag.native.test.ts
@@ -22,6 +22,7 @@ describe('getFeatureFlag', () => {
     RemoteStoreFeatureFlags.WIP_NEW_MAPPING_BOOKS,
     RemoteStoreFeatureFlags.WIP_VENUE_MAP,
     RemoteStoreFeatureFlags.WIP_VENUE_MAP_WITHOUT_PREVIEW,
+    RemoteStoreFeatureFlags.WIP_SEARCH_ACCESSIBILITY_FILTER,
   ])(
     'should call the right firestore collection: featureFlags',
     (featureFlag: RemoteStoreFeatureFlags) => {
