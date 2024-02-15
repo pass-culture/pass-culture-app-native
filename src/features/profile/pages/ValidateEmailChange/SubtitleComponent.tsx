52cbe0ed0bfc9c6772c09cedb79449ed4859a072
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-02-15 16:33:34 +0100

    (PC-27167) feat(EmailChange): Change wording for email validation

--- src/features/profile/pages/ValidateEmailChange/SubtitleComponent.tsx
+++ src/features/profile/pages/ValidateEmailChange/SubtitleComponent.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
-import { getSpacing, Typo } from 'ui/theme'
+import { Separator } from 'ui/components/Separator'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
 import { TextProps } from 'ui/theme/typography'
 
 export function ValidateEmailChangeSubtitleComponent(props: TextProps) {
   return (
     <Wrapper>
-      <Typo.ButtonText>Nouvelle adresse e-mail&nbsp;:</Typo.ButtonText>
-      <Typo.Body {...props} />
+      <Typo.Body>Nouvelle adresse e-mail&nbsp;:</Typo.Body>
+      <Typo.ButtonText {...props} />
+      <Spacer.Column numberOfSpaces={4} />
+      <Separator.Horizontal />
+      <Spacer.Column numberOfSpaces={4} />
+      <StyledCaption>
+        En cliquant sur valider, tu seras déconnecté.e. Tu devras te reconnecter avec ta nouvelle
+        adresse e-mail.
+      </StyledCaption>
     </Wrapper>
   )
 }
@@ -16,4 +24,8 @@ export function ValidateEmailChangeSubtitleComponent(props: TextProps) {
 const Wrapper = styled.View({
   marginTop: getSpacing(4),
   alignItems: 'center',
+})
+
+const StyledCaption = styled(Typo.CaptionNeutralInfo)({
+  textAlign: 'center',
 })
