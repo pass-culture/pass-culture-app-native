4f8f4c38c99709499765257d942c533a1633c02a
Marine Shaw <68000368+marineshaw@users.noreply.github.com>
2024-02-16 15:27:04 +0100

    Revert ":fire: (VenueOffersNew) remove useless unseCallbacks (#5976)"
    This reverts commit ce3748ce16c28767bf4403ffdbf85e16cdae1324.

--- src/features/venue/components/VenueOffers/VenueOffersNew.tsx
+++ src/features/venue/components/VenueOffers/VenueOffersNew.tsx
@@ -1,5 +1,5 @@
 import { useRoute } from '@react-navigation/native'
-import React from 'react'
+import React, { useCallback } from 'react'
 import styled from 'styled-components/native'
 
 import { VenueResponse, VenueTypeCodeKey } from 'api/gen'
@@ -12,6 +12,7 @@ import { NoOfferPlaceholder } from 'features/venue/components/VenueOffers/NoOffe
 import { VenueOfferTile } from 'features/venue/components/VenueOfferTile/VenueOfferTile'
 import { useNavigateToSearchWithVenueOffers } from 'features/venue/helpers/useNavigateToSearchWithVenueOffers'
 import { analytics } from 'libs/analytics'
+import { useLocation } from 'libs/location'
 import { formatDates, getDisplayPrice, VenueTypeCode } from 'libs/parsers'
 import { useCategoryIdMapping, useCategoryHomeLabelMapping } from 'libs/subcategories'
 import { Offer } from 'shared/offer/types'
@@ -31,6 +32,7 @@ interface Props {
 const keyExtractor = (item: Offer) => item.objectID
 
 export function VenueOffersNew({ venue, venueOffers, playlists }: Readonly<Props>) {
+  const { geolocPosition } = useLocation()
   const { params: routeParams } = useRoute<UseRouteType<'Offer'>>()
   const searchNavConfig = useNavigateToSearchWithVenueOffers(venue)
   const { isLoading: areVenueOffersLoading } = useVenueOffers(venue)
@@ -41,26 +43,30 @@ export function VenueOffersNew({ venue, venueOffers, playlists }: Readonly<Props
   const mapping = useCategoryIdMapping()
   const labelMapping = useCategoryHomeLabelMapping()
 
-  const renderItem: CustomListRenderItem<Offer> = ({ item, width, height }) => {
-    const timestampsInMillis = item.offer.dates?.map((timestampInSec) => timestampInSec * 1000)
-    return (
-      <VenueOfferTile
-        categoryLabel={labelMapping[item.offer.subcategoryId]}
-        categoryId={mapping[item.offer.subcategoryId]}
-        subcategoryId={item.offer.subcategoryId}
-        offerId={+item.objectID}
-        name={item.offer.name}
-        date={formatDates(timestampsInMillis)}
-        isDuo={item.offer.isDuo}
-        thumbUrl={item.offer.thumbUrl}
-        price={getDisplayPrice(item.offer.prices)}
-        venueId={venue?.id}
-        width={width}
-        height={height}
-        searchId={routeParams?.searchId}
-      />
-    )
-  }
+  const renderItem: CustomListRenderItem<Offer> = useCallback(
+    ({ item, width, height }) => {
+      const timestampsInMillis = item.offer.dates?.map((timestampInSec) => timestampInSec * 1000)
+      return (
+        <VenueOfferTile
+          categoryLabel={labelMapping[item.offer.subcategoryId]}
+          categoryId={mapping[item.offer.subcategoryId]}
+          subcategoryId={item.offer.subcategoryId}
+          offerId={+item.objectID}
+          name={item.offer.name}
+          date={formatDates(timestampsInMillis)}
+          isDuo={item.offer.isDuo}
+          thumbUrl={item.offer.thumbUrl}
+          price={getDisplayPrice(item.offer.prices)}
+          venueId={venue?.id}
+          width={width}
+          height={height}
+          searchId={routeParams?.searchId}
+        />
+      )
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [geolocPosition]
+  )
 
   const shouldDisplayGtlPlaylist =
     [VenueTypeCodeKey.DISTRIBUTION_STORE, VenueTypeCodeKey.BOOKSTORE].includes(
@@ -70,13 +76,17 @@ export function VenueOffersNew({ venue, venueOffers, playlists }: Readonly<Props
   const showSeeMore = nbHits > hits.length && !shouldDisplayGtlPlaylist
   const onPressSeeMore = showSeeMore ? () => analytics.logVenueSeeMoreClicked(venue.id) : undefined
 
-  const renderFooter: RenderFooterItem = ({ width, height }: { width: number; height: number }) => (
-    <SeeMore
-      width={width}
-      height={height}
-      navigateTo={showSeeMore ? searchNavConfig : undefined}
-      onPress={() => analytics.logVenueSeeMoreClicked(venue.id)}
-    />
+  const renderFooter: RenderFooterItem = useCallback(
+    ({ width, height }: { width: number; height: number }) => (
+      <SeeMore
+        width={width}
+        height={height}
+        navigateTo={showSeeMore ? searchNavConfig : undefined}
+        onPress={() => analytics.logVenueSeeMoreClicked(venue.id)}
+      />
+    ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [onPressSeeMore]
   )
 
   if (areVenueOffersLoading || arePlaylistsLoading)
