fc1c24b9118d036d7a39106f6074a06dc2a1e9b2
NolwenMajorFrances <144016890+NolwenMajorFrances@users.noreply.github.com>
2024-02-19 15:25:18 +0100

    (PC-27840) feat(OfferCinemaSeance): fix UI (#5972)
    * add shaddowbox
    
    * fix size and focus
    
    * fix content justification

--- src/ui/components/eventCard/EventCard.tsx
+++ src/ui/components/eventCard/EventCard.tsx
@@ -3,11 +3,11 @@ import styled from 'styled-components/native'
 
 import { styledButton } from 'ui/components/buttons/styledButton'
 import { Touchable } from 'ui/components/touchable/Touchable'
-import { getSpacing, Spacer, Typo } from 'ui/theme'
-import { customFocusOutline } from 'ui/theme/customFocusOutline/customFocusOutline'
+import { getShadow, getSpacing, Spacer, Typo } from 'ui/theme'
 
+const BORDER_WIDTH = getSpacing(0.25)
 const CARD_HEIGHT = getSpacing(17)
-const CARD_WIDTH = getSpacing(28)
+const CARD_WIDTH = getSpacing(30)
 
 interface Props {
   onPress: () => void
@@ -23,15 +23,20 @@ export const EventCard: React.FC<Props> = ({
   subtitleLeft,
   subtitleRight,
 }) => {
+  const hasSubtitleRight = !subtitleRight
   return (
-    <StyledPressable onPress={onPress} disabled={isDisabled}>
+    <StyledTouchableOpacity onPress={isDisabled ? undefined : onPress} isDisabled={isDisabled}>
       <Container isDisabled={isDisabled}>
         <Title accessibilityLabel={title} numberOfLines={1} isDisabled={isDisabled}>
           {title}
         </Title>
         <Spacer.Column numberOfSpaces={1} />
         <SubtitleContainer>
-          <SubtitleLeft accessibilityLabel={subtitleLeft} numberOfLines={1} isDisabled={isDisabled}>
+          <SubtitleLeft
+            accessibilityLabel={subtitleLeft}
+            numberOfLines={1}
+            isDisabled={isDisabled}
+            hasSubtitleRight={hasSubtitleRight}>
             {subtitleLeft}
           </SubtitleLeft>
           <SubtitleRight
@@ -42,33 +47,42 @@ export const EventCard: React.FC<Props> = ({
           </SubtitleRight>
         </SubtitleContainer>
       </Container>
-    </StyledPressable>
+    </StyledTouchableOpacity>
   )
 }
-const StyledPressable = styledButton(Touchable)<{ isFocus?: boolean }>(({ theme, isFocus }) => ({
-  width: CARD_WIDTH,
-  ...customFocusOutline({ isFocus, color: theme.colors.black }),
-  '&:focus': {
-    outlineOffset: getSpacing(0.75),
-    outlineWidth: getSpacing(0.25),
+const StyledTouchableOpacity = styledButton(Touchable)<{ isDisabled: boolean }>(
+  ({ theme, isDisabled }) => ({
+    width: 'fit-content',
     borderRadius: theme.borderRadius.radius,
-  },
-}))
+    '&:hover': { textDecoration: isDisabled ? 'none' : 'underline' },
+    '&:focus': { outline: 'none' },
+    '&:focus-visible': { outline: 'auto' },
+  })
+)
 
 const Container = styled.View<{ isDisabled: boolean }>(({ theme, isDisabled }) => ({
-  display: 'inline-flex',
-  backgroundColor: isDisabled ? theme.colors.greyLight : theme.colors.white,
-  alignItems: 'flex-start',
-  heigth: CARD_HEIGHT,
-  width: '100%',
-  padding: getSpacing(3),
+  width: CARD_WIDTH,
+  height: CARD_HEIGHT,
+  borderColor: isDisabled ? theme.colors.greyLight : theme.colors.black,
+  borderWidth: BORDER_WIDTH,
   borderRadius: theme.borderRadius.radius,
-  borderWidth: isDisabled ? 0 : getSpacing(0.25),
-  borderColor: theme.colors.black,
+  boxSizing: 'border-box',
+  padding: getSpacing(3),
+  justifyContent: 'flex-start',
+  backgroundColor: isDisabled ? theme.colors.greyLight : theme.colors.white,
+  ...(!isDisabled
+    ? getShadow({
+        shadowOffset: { width: 0, height: getSpacing(1) },
+        shadowRadius: getSpacing(1),
+        shadowColor: theme.colors.greyDark,
+        shadowOpacity: 0.2,
+      })
+    : {}),
 }))
 
 const Title = styled(Typo.ButtonText)<{ isDisabled: boolean }>(({ theme, isDisabled }) => ({
   color: isDisabled ? theme.colors.greyDark : theme.colors.black,
+  textAlign: 'left',
 }))
 
 const SubtitleContainer = styled.View({
@@ -76,15 +90,23 @@ const SubtitleContainer = styled.View({
   flexDirection: 'row',
   width: '100%',
   alignItems: 'center',
+  justifyContent: 'space-between',
+  textOverflow: 'ellipsis',
 })
 
-const SubtitleLeft = styled(Typo.Caption)<{ isDisabled: boolean }>(({ theme, isDisabled }) => ({
-  color: isDisabled ? theme.colors.greySemiDark : theme.colors.greyDark,
-  lineHeight: getSpacing(5),
-}))
+const SubtitleLeft = styled(Typo.Caption)<{ isDisabled: boolean; hasSubtitleRight: boolean }>(
+  ({ theme, isDisabled, hasSubtitleRight }) => ({
+    color: isDisabled ? theme.colors.greySemiDark : theme.colors.greyDark,
+    lineHeight: getSpacing(5),
+    textAlign: 'left',
+    flex: hasSubtitleRight ? 'auto' : 1,
+  })
+)
 
 const SubtitleRight = styled(Typo.Body)<{ isDisabled: boolean }>(({ theme, isDisabled }) => ({
   color: isDisabled ? theme.colors.greySemiDark : theme.colors.black,
+  textAlign: 'right',
   flexShrink: 0,
   paddingLeft: getSpacing(1),
+  flex: 1,
 }))
