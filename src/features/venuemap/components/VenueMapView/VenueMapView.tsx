1925f838bf805917f3c7dcc74a4baccb45edf8c1
Hugo Voisin <hugov@bam.tech>
2024-02-19 12:54:22 +0100

    feat: display venues on the map

--- src/features/venuemap/components/VenueMapView/VenueMapView.tsx
+++ src/features/venuemap/components/VenueMapView/VenueMapView.tsx
@@ -1,14 +1,32 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 
-import MapView, { EdgePadding } from 'libs/maps/maps'
+import { useGetAllVenues } from 'features/venuemap/useGetAllVenues'
+import MapView, { EdgePadding, Marker } from 'libs/maps/maps'
 
 type Props = {
   padding: EdgePadding
 }
 
 export const VenueMapView: FunctionComponent<Props> = ({ padding }) => {
-  return <StyledMapView showsUserLocation mapPadding={padding} />
+  const { data: venues = [] } = useGetAllVenues()
+
+  return (
+    <StyledMapView showsUserLocation mapPadding={padding}>
+      {venues.map((venue) => (
+        <React.Fragment key={venue.venueId}>
+          {venue._geoloc?.lat && venue._geoloc.lng ? (
+            <Marker
+              coordinate={{
+                latitude: venue._geoloc.lat,
+                longitude: venue._geoloc.lng,
+              }}
+            />
+          ) : null}
+        </React.Fragment>
+      ))}
+    </StyledMapView>
+  )
 }
 
 const StyledMapView = styled(MapView)({ height: '100%', width: '100%' })
