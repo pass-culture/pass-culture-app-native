c2259705284b050237d4ff5483874036a1346d29
tanguyprache <47600889+tanguyprache@users.noreply.github.com>
2024-02-20 12:15:54 +0100

    Revert "(PC-26486) fix(sonar): fix a todo about slider width (#5846)" (#5987)
    This reverts commit bce20c46e8dd1d6295c0d13c014204bb332eb5b2.

--- src/features/search/helpers/useGetFullscreenModalSliderLength.ts
+++ src/features/search/helpers/useGetFullscreenModalSliderLength.ts
@@ -1,10 +1,23 @@
 import { useTheme } from 'styled-components'
 
 export function useGetFullscreenModalSliderLength(hasFormWidthLimit = true) {
-  const { isDesktopViewport, forms, modal, appContentWidth } = useTheme()
-  const deviceMaxWidth = isDesktopViewport ? modal.desktopMaxWidth : appContentWidth
-  const whiteAroundScreenWidth = modal.spacing.MD * 2
-  return {
-    sliderLength: (hasFormWidthLimit ? forms.maxWidth : deviceMaxWidth) - whiteAroundScreenWidth,
-  }
+  const { isDesktopViewport, appContentWidth, forms, slider, modal } = useTheme()
+  const baseSliderContainerWidth = isDesktopViewport ? modal.desktopMaxWidth : appContentWidth
+  /**
+   * This hack is used to avoid the slider to be cropped.
+   * FIXME(PC-17652): We should create a slider that automatically scales ? Without defining any width
+   */
+  const modalHorizontalSpacing = modal.spacing.MD * 2
+
+  const nonDesktopWidth = hasFormWidthLimit
+    ? Math.min(appContentWidth, forms.maxWidth + modalHorizontalSpacing)
+    : appContentWidth
+
+  const fixedBaseSliderContainerWidth = isDesktopViewport
+    ? baseSliderContainerWidth
+    : nonDesktopWidth
+
+  const sliderLength = fixedBaseSliderContainerWidth - modalHorizontalSpacing - slider.markerSize
+
+  return { sliderLength }
 }
