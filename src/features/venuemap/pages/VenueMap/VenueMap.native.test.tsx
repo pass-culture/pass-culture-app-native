ef8a1d5cf58c96a384075669ec2c7872b5e3a4b0
Hugo Voisin <hugov@bam.tech>
2024-02-20 10:44:31 +0100

    test: add react-query provider

--- src/features/venuemap/pages/VenueMap/VenueMap.native.test.tsx
+++ src/features/venuemap/pages/VenueMap/VenueMap.native.test.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import { VenueMap } from 'features/venuemap/pages/VenueMap/VenueMap'
-import { render, screen } from 'tests/utils'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render, screen, waitFor } from 'tests/utils'
 
 describe('<VenueMap />', () => {
-  it('Should display venue map header', () => {
-    render(<VenueMap />)
+  it('Should display venue map header', async () => {
+    render(reactQueryProviderHOC(<VenueMap />))
 
-    expect(screen.getByText('Carte des lieux')).toBeOnTheScreen()
+    await waitFor(() => {
+      expect(screen.getByText('Carte des lieux')).toBeOnTheScreen()
+    })
   })
 })
