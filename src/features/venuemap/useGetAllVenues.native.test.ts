74c06a9fe8ef31657d918ae55f938a4f4f1b4b30
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-02-23 10:24:41 +0100

    (PC-27600) feat(search): add type to pass region and radius in parameters of useGetAllVenues

--- src/features/venuemap/useGetAllVenues.native.test.ts
+++ src/features/venuemap/useGetAllVenues.native.test.ts
@@ -1,5 +1,6 @@
 import algoliasearch from 'algoliasearch'
 
+import { Region } from 'libs/maps/maps'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { renderHook, waitFor } from 'tests/utils'
 
@@ -13,16 +14,23 @@ jest.mock('libs/algolia/fetchAlgolia/AlgoliaError', () => ({
   captureAlgoliaError: jest.fn(),
 }))
 
+const region: Region = {
+  latitude: 48.866667,
+  latitudeDelta: 0.08157178378529253,
+  longitude: 2.333333,
+  longitudeDelta: 0.04760990854064799,
+}
+
 describe('useGetAllVenues', () => {
   it('should fetch all venues', async () => {
-    renderHook(() => useGetAllVenues(), {
+    renderHook(() => useGetAllVenues({ region, radius: 10 }), {
       wrapper: ({ children }) => reactQueryProviderHOC(children),
     })
 
     await waitFor(() => {
       expect(search).toHaveBeenCalledWith('', {
-        aroundLatLng: '0, 0',
-        aroundRadius: 'all',
+        aroundLatLng: '48.866667, 2.333333',
+        aroundRadius: 10000,
         attributesToHighlight: [],
         facetFilters: [['has_at_least_one_bookable_offer:true']],
       })
