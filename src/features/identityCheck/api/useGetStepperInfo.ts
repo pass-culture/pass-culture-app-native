d8b5268d749ba33d98ceb37d9d459382d0bbd13d
Marine Shaw <68000368+marineshaw@users.noreply.github.com>
2024-02-26 09:42:04 +0100

    (PC-26801) feat(useGetStepperInfo): fix sentry error cannot read property of undefined (#6007)
    * :bug: (useGetStepperInfo) fix returned data so that we never have undefined values
    
    * :white_check_mark: (useGetStepperInfo) Add test to check error case
    
    * :fire: (useGetStepperInfo) Remove now useless sentry extras

--- src/features/identityCheck/api/useGetStepperInfo.ts
+++ src/features/identityCheck/api/useGetStepperInfo.ts
@@ -2,7 +2,6 @@ import { useQuery } from 'react-query'
 
 import { api } from 'api/api'
 import { SubscriptionStepperResponse } from 'api/gen'
-import { eventMonitoring } from 'libs/monitoring'
 import { QueryKeys } from 'libs/queryKeys'
 
 export const useGetStepperInfo = (): {
@@ -14,18 +13,11 @@ export const useGetStepperInfo = (): {
 } => {
   const { data } = useQuery([QueryKeys.STEPPER_INFO], () => api.getNativeV1SubscriptionStepper())
 
-  // TODO(PC-26801): remove this when the bug is fixed
-  eventMonitoring.setExtras({ useGetStepperInfoResponse: JSON.stringify(data) })
-
-  if (data === undefined) {
-    return { stepToDisplay: [], title: '' }
-  }
-
   return {
-    stepToDisplay: data.subscriptionStepsToDisplay,
-    title: data.title,
-    subtitle: data.subtitle,
-    errorMessage: data.errorMessage,
-    identificationMethods: data.allowedIdentityCheckMethods,
+    stepToDisplay: data?.subscriptionStepsToDisplay || [],
+    title: data?.title || '',
+    subtitle: data?.subtitle,
+    errorMessage: data?.errorMessage,
+    identificationMethods: data?.allowedIdentityCheckMethods,
   }
 }
