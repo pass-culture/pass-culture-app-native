d726c1fa0235797ae03333a47d208c60901e4aa0
marineshaw <marines@bam.tech>
2024-02-28 16:42:52 +0100

    :white_check_mark: (Stepper) fix accessibility test by mocking everything (but not react query directly)

--- src/features/identityCheck/pages/Stepper.web.test.tsx
+++ src/features/identityCheck/pages/Stepper.web.test.tsx
@@ -1,29 +1,28 @@
 import mockdate from 'mockdate'
 import React from 'react'
 
+import { NextSubscriptionStepResponse } from 'api/gen'
 import { stepsDetailsFixture } from 'features/identityCheck/pages/helpers/stepDetails.fixture'
+import { useRehydrateProfile } from 'features/identityCheck/pages/helpers/useRehydrateProfile'
 import { useStepperInfo } from 'features/identityCheck/pages/helpers/useStepperInfo'
-import * as useFeatureFlag from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
-import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { checkAccessibilityFor, render } from 'tests/utils/web'
 
 import { Stepper } from './Stepper'
 
 mockdate.set(new Date('2020-12-01T00:00:00.000Z'))
 
+const subscriptionStep: NextSubscriptionStepResponse = {
+  allowedIdentityCheckMethods: [],
+  hasIdentityCheckPending: false,
+  stepperIncludesPhoneValidation: true,
+}
 jest.mock('features/identityCheck/pages/helpers/useSetCurrentSubscriptionStep', () => ({
   useSetSubscriptionStepAndMethod: jest.fn(() => ({
-    subscription: jest.fn(),
+    subscription: subscriptionStep,
   })),
 }))
 
 jest.mock('features/auth/context/AuthContext')
-
-jest.mock('features/identityCheck/context/SubscriptionContextProvider')
-
-jest.spyOn(useFeatureFlag, 'useFeatureFlag').mockReturnValue(true)
-
-jest.mock('react-query')
 
 jest.mock('features/identityCheck/pages/helpers/useStepperInfo')
 const mockUseStepperInfo = useStepperInfo as jest.Mock
@@ -34,10 +33,15 @@ mockUseStepperInfo.mockReturnValue({
   subtitle: 'Débloque ton crédit',
 })
 
+jest.mock('features/identityCheck/pages/helpers/useRehydrateProfile')
+const mockUseRehydrateProfile = useRehydrateProfile as jest.Mock
+mockUseRehydrateProfile.mockImplementation(jest.fn())
+
 describe('<Stepper/>', () => {
   describe('Accessibility', () => {
     it('should not have basic accessibility issues', async () => {
-      const { container } = render(reactQueryProviderHOC(<Stepper />))
+      const { container } = render(<Stepper />)
+
       const results = await checkAccessibilityFor(container)
 
       expect(results).toHaveNoViolations()
