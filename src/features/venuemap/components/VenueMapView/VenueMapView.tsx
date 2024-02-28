e63d6a9ccd3a918fdb545f10660ab3004916c906
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-02-28 17:19:09 +0100

    (PC-27709) feat(search): navigate to venue page when pressing venue pin on map

--- src/features/venuemap/components/VenueMapView/VenueMapView.tsx
+++ src/features/venuemap/components/VenueMapView/VenueMapView.tsx
@@ -1,7 +1,9 @@
+import { useNavigation } from '@react-navigation/native'
 import React, { FunctionComponent, useState } from 'react'
 import { useWindowDimensions } from 'react-native'
 import styled from 'styled-components/native'
 
+import { UseNavigationType } from 'features/navigation/RootNavigator/types'
 import { Venue } from 'features/venue/types'
 import { VenueMapCluster } from 'features/venuemap/components/VenueMapCluster/VenueMapCluster'
 import {
@@ -32,6 +34,7 @@ type GeolocatedVenue = Omit<Venue, 'venueId'> & {
 
 export const VenueMapView: FunctionComponent<Props> = ({ padding }) => {
   const { userLocation } = useLocation()
+  const { navigate } = useNavigation<UseNavigationType>()
 
   const { height, width } = useWindowDimensions()
   const screenRatio = height / width
@@ -74,8 +77,10 @@ export const VenueMapView: FunctionComponent<Props> = ({ padding }) => {
     setShowSearchButton(false)
   }
 
-  const handleMarkerPress = () => {
+  const handleMarkerPress = (venueId: number) => {
     setShowSearchButton(false)
+
+    navigate('Venue', { id: venueId })
   }
 
   return (
@@ -96,7 +101,7 @@ export const VenueMapView: FunctionComponent<Props> = ({ padding }) => {
               latitude: venue._geoloc.lat,
               longitude: venue._geoloc.lng,
             }}
-            onPress={handleMarkerPress}>
+            onPress={() => handleMarkerPress(venue.venueId)}>
             <MapPin />
           </Marker>
         ))}
