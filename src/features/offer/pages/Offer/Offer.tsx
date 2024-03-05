5b815c6c991444d2ad93fcfafec86434ecf0056b
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-03-05 15:13:24 +0100

    (PC-27884) feat(offer): display placeholder during offer loading

--- src/features/offer/pages/Offer/Offer.tsx
+++ src/features/offer/pages/Offer/Offer.tsx
@@ -6,16 +6,21 @@ import { useOffer } from 'features/offer/api/useOffer'
 import { OfferContentOld } from 'features/offer/components/OfferContentOld/OfferContentOld'
 import { getSearchGroupAndNativeCategoryFromSubcategoryId } from 'features/offer/helpers/getSearchGroupAndNativeCategoryFromSubcategoryId/getSearchGroupAndNativeCategoryFromSubcategoryId'
 import { OfferContent } from 'features/offerv2/components/OfferContent/OfferContent'
+import { OfferContentPlaceholder } from 'features/offerv2/components/OfferContentPlaceholder/OfferContentPlaceholder'
 import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
+import { useIsFalseWithDelay } from 'libs/hooks/useIsFalseWithDelay'
 import { useSubcategoriesMapping } from 'libs/subcategories/mappings'
 import { useSubcategories } from 'libs/subcategories/useSubcategories'
+
+const ANIMATION_DURATION = 700
 
 export function Offer() {
   const route = useRoute<UseRouteType<'Offer'>>()
   const offerId = route.params?.id
 
-  const { data: offer } = useOffer({ offerId })
+  const { data: offer, isLoading } = useOffer({ offerId })
+  const showSkeleton = useIsFalseWithDelay(isLoading, ANIMATION_DURATION)
   const { data: subcategories } = useSubcategories()
   const subcategoriesMapping = useSubcategoriesMapping()
 
@@ -27,6 +32,8 @@ export function Offer() {
     subcategories,
     offer.subcategoryId
   )
+
+  if (shouldDisplayOfferV2 && showSkeleton) return <OfferContentPlaceholder />
 
   return shouldDisplayOfferV2 ? (
     <OfferContent
