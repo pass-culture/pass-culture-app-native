57815c59c2cbf34d735b2cbc35b87c98ddabb691
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-03-05 15:12:17 +0100

    (PC-27884) feat(ui): add functions to construct OfferContentPlaceholder

--- src/ui/components/placeholders/Placeholders.tsx
+++ src/ui/components/placeholders/Placeholders.tsx
@@ -3,7 +3,9 @@ import { View } from 'react-native'
 import styled from 'styled-components/native'
 
 import { FavoriteButtonsContainer } from 'features/favorites/components/Favorite'
+import { theme } from 'theme'
 import { SkeletonTile } from 'ui/components/placeholders/SkeletonTile'
+import { SectionWithDivider } from 'ui/components/SectionWithDivider'
 import { getSpacing, Spacer } from 'ui/theme'
 
 const borderRadius = 4
@@ -32,6 +34,67 @@ function BasePlaceholder(props: {
 
 function TextPlaceholder({ width, height }: { width: number; height?: number }) {
   return <SkeletonTile borderRadius={2} height={height ?? getSpacing(3)} width={width} />
+}
+
+export function OfferButtonPlaceholder() {
+  return (
+    <SkeletonTile
+      borderRadius={theme.borderRadius.button}
+      width={getSpacing(82)}
+      height={getSpacing(10)}
+      fullWidth
+    />
+  )
+}
+
+export function OfferImagePlaceholder() {
+  return <TextPlaceholder height={getSpacing(95)} width={getSpacing(60)} />
+}
+
+export function OfferContentBodyPlaceholder() {
+  return (
+    <React.Fragment>
+      <Container>
+        <Row>
+          <TextPlaceholder height={getSpacing(6)} width={getSpacing(22)} />
+          <Spacer.Row numberOfSpaces={2} />
+          <TextPlaceholder height={getSpacing(6)} width={getSpacing(22)} />
+        </Row>
+        <Spacer.Column numberOfSpaces={5} />
+        <TextPlaceholder height={getSpacing(4)} width={getSpacing(74)} />
+        <Spacer.Column numberOfSpaces={2} />
+        <TextPlaceholder height={getSpacing(4)} width={getSpacing(40)} />
+        <Spacer.Column numberOfSpaces={10} />
+        <TextPlaceholder height={getSpacing(5)} width={getSpacing(22)} />
+        <Spacer.Column numberOfSpaces={10} />
+        <TextPlaceholder height={getSpacing(4)} width={getSpacing(33)} />
+        <Spacer.Column numberOfSpaces={3.5} />
+        <TextPlaceholder height={getSpacing(2)} width={getSpacing(82)} />
+        <Spacer.Column numberOfSpaces={2} />
+        <TextPlaceholder height={getSpacing(2)} width={getSpacing(67)} />
+        <Spacer.Column numberOfSpaces={2} />
+        <TextPlaceholder height={getSpacing(2)} width={getSpacing(82)} />
+        <Spacer.Column numberOfSpaces={2} />
+        <TextPlaceholder height={getSpacing(2)} width={getSpacing(67)} />
+        <Spacer.Column numberOfSpaces={2} />
+        <TextPlaceholder height={getSpacing(2)} width={getSpacing(82)} />
+        <Spacer.Column numberOfSpaces={2} />
+        <TextPlaceholder height={getSpacing(2)} width={getSpacing(82)} />
+        <Spacer.Column numberOfSpaces={8} />
+      </Container>
+      <SectionWithDivider visible margin>
+        <Spacer.Column numberOfSpaces={8} />
+        <TextPlaceholder height={getSpacing(5)} width={getSpacing(63)} />
+        <Spacer.Column numberOfSpaces={8} />
+        <TextPlaceholder height={getSpacing(4)} width={getSpacing(33)} />
+        <Spacer.Column numberOfSpaces={2} />
+        <TextPlaceholder height={getSpacing(2)} width={getSpacing(58)} />
+        <Spacer.Column numberOfSpaces={6} />
+        <TextPlaceholder height={getSpacing(6)} width={getSpacing(16)} />
+        <Spacer.Column numberOfSpaces={4} />
+      </SectionWithDivider>
+    </React.Fragment>
+  )
 }
 
 export function NumberOfResultsPlaceholder() {
