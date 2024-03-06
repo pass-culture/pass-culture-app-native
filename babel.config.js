4abdd769e283ee5ce838017a0df8bdfc0382abad
Hugo Voisin <hugo.voisin@passculture.app>
2024-03-06 17:05:32 +0100

    (PC-28405) chore(babel): add numeric separator plugin (#6060)

--- babel.config.js
+++ babel.config.js
@@ -23,6 +23,7 @@ module.exports = {
         },
       },
     ],
+    "@babel/plugin-transform-numeric-separator",
     '@babel/plugin-proposal-unicode-property-regex',
     '@babel/plugin-proposal-export-namespace-from',
     'react-native-reanimated/plugin',
