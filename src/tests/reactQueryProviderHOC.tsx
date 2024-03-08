ba890b1237decc7277487b8d449d90cce12eeac0
Hugo Voisin <hugov@bam.tech>
2024-03-08 18:36:37 +0100

    refactor: remove setLogger as it has been removed

--- src/tests/reactQueryProviderHOC.tsx
+++ src/tests/reactQueryProviderHOC.tsx
@@ -1,18 +1,19 @@
-import { QueryCache, QueryClient, QueryClientProvider, setLogger } from '@tanstack/react-query'
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 
 export const queryCache = new QueryCache()
+
+const logger = {
+  log: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+}
 
 export const reactQueryProviderHOC = (
   component: React.ReactNode,
   setup?: (queryClient: QueryClient) => void
 ) => {
-  const queryClient = new QueryClient({ queryCache })
+  const queryClient = new QueryClient({ queryCache, logger })
   if (setup) setup(queryClient)
-  setLogger({
-    log: jest.fn(),
-    warn: jest.fn(),
-    error: jest.fn(),
-  })
   return <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>
 }
