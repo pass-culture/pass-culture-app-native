164ac680abbc33f9d9daf2ce4e20d954723b2834
Hugo Voisin <hugov@bam.tech>
2024-03-11 15:37:35 +0100

    feat: do not display integer geoloc

---
+++ src/features/venuemap/helpers/isGeolocValid.ts
@@ -0,0 +1,5 @@
+import { Geoloc } from 'libs/algolia'
+
+export const isGeolocValid = (geoloc: Geoloc | undefined): boolean =>
+  // Integer values are suspicious, we avoid them so that we don't display venues with wrong geoloc
+  !!(geoloc?.lat && geoloc?.lng && !Number.isInteger(geoloc.lat) && !Number.isInteger(geoloc.lng))
