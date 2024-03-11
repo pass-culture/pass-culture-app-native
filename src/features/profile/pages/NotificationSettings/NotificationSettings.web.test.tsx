6ff13c0b7277a2ae807b780f8c6ca903b9d4e48b
Christopher Gerrard <75068235+vikingprank@users.noreply.github.com>
2024-03-11 10:02:35 +0100

    (PC-28334) feat(NotificationSettings): Make notification switch available on ios & android (#6069)
    * Make notification switch available on ios & android
    
    * Removed unused mock (web) and renamed test
    
    ---------
    
    Co-authored-by: Christopher Gerrard <christopher.gerrard@passculture.app>

--- src/features/profile/pages/NotificationSettings/NotificationSettings.web.test.tsx
+++ src/features/profile/pages/NotificationSettings/NotificationSettings.web.test.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
+import { useRoute } from '__mocks__/@react-navigation/native'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { render, checkAccessibilityFor, act } from 'tests/utils/web'
+import { render, checkAccessibilityFor, act, screen } from 'tests/utils/web'
 
 import { NotificationSettings } from './NotificationSettings'
 
+jest.mock('features/auth/context/AuthContext')
+
+useRoute.mockReturnValue({ key: 'ksdqldkmqdmqdq' })
+
 describe('<NotificationSettings/>', () => {
+  it('should only display switch to authorize emails', async () => {
+    render(reactQueryProviderHOC(<NotificationSettings />))
+
+    expect(await screen.findByText('Autoriser l’envoi d’e-mails')).toBeTruthy()
+    expect(screen.queryByText('Autoriser les notifications marketing')).not.toBeOnTheScreen()
+  })
+
   describe('Accessibility', () => {
     it('should not have basic accessibility issues', async () => {
       const { container } = render(reactQueryProviderHOC(<NotificationSettings />))
