e9c0b803ca451e55abab24d2e3bb4468010a3b1c
YassinL <62058919+YassinL@users.noreply.github.com>
2024-03-12 16:36:55 +0100

    (PC-28460) fix(OfferPreview): fix both header offer preview and venue map (use opacity header with blur (#6083)

--- src/features/offer/pages/OfferPreview/OfferPreview.tsx
+++ src/features/offer/pages/OfferPreview/OfferPreview.tsx
@@ -1,4 +1,5 @@
 import { useRoute } from '@react-navigation/native'
+import colorAlpha from 'color-alpha'
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 
@@ -6,19 +7,25 @@ import { UseRouteType } from 'features/navigation/RootNavigator/types'
 import { useGoBack } from 'features/navigation/useGoBack'
 import { useOffer } from 'features/offer/api/useOffer'
 import { PinchableBox } from 'features/offer/components/PinchableBox/PinchableBox'
-import { PageHeaderWithoutPlaceholder } from 'ui/components/headers/PageHeaderWithoutPlaceholder'
+import { BlurHeader } from 'ui/components/headers/BlurHeader'
+import {
+  PageHeaderWithoutPlaceholder,
+  useGetHeaderHeight,
+} from 'ui/components/headers/PageHeaderWithoutPlaceholder'
 
 export const OfferPreview: FunctionComponent = () => {
   const { params } = useRoute<UseRouteType<'OfferPreview'>>()
   const { goBack } = useGoBack('Offer', params)
   const { data: offer } = useOffer({ offerId: params.id })
+  const headerHeight = useGetHeaderHeight()
 
   if (!offer?.image) return null
 
   return (
     <Container>
-      <PageHeaderWithoutPlaceholder title="1/1" onGoBack={goBack} />
+      <StyledHeader title="1/1" onGoBack={goBack} />
       <PinchableBox imageUrl={offer.image.url} />
+      <BlurHeader height={headerHeight} />
     </Container>
   )
 }
@@ -26,4 +33,8 @@ export const OfferPreview: FunctionComponent = () => {
 const Container = styled.View(({ theme }) => ({
   flex: 1,
   backgroundColor: theme.colors.white,
+}))
+
+const StyledHeader = styled(PageHeaderWithoutPlaceholder)(({ theme }) => ({
+  backgroundColor: colorAlpha(theme.colors.white, 0.6),
 }))
