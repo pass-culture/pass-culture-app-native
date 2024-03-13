514029332b344ee9e158fe10ebb9a099c1b1d162
Tanguy <tanguy.prache@passculture.app>
2024-03-13 08:55:03 +0100

    feat: update navigate to search with accessibility params

--- src/features/search/helpers/useNavigateToSearch/useNavigateToSearch.ts
+++ src/features/search/helpers/useNavigateToSearch/useNavigateToSearch.ts
@@ -1,5 +1,6 @@
 import { useNavigation } from '@react-navigation/native'
 
+import { DisabilitiesProperties } from 'features/accessibility/types'
 import { UseNavigationType } from 'features/navigation/RootNavigator/types'
 import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
 import { SearchState } from 'features/search/types'
@@ -7,8 +8,16 @@ import { SearchState } from 'features/search/types'
 export const useNavigateToSearch = () => {
   const { navigate } = useNavigation<UseNavigationType>()
 
-  const navigateToSearch = (newSearchState: SearchState): void => {
-    navigate(...getTabNavConfig('Search', newSearchState))
+  const navigateToSearch = (
+    newSearchState: SearchState,
+    newAccessibilityFilter: DisabilitiesProperties
+  ): void => {
+    navigate(
+      ...getTabNavConfig('Search', {
+        ...newSearchState,
+        accessibilityFilter: newAccessibilityFilter,
+      })
+    )
   }
   return { navigateToSearch }
 }
