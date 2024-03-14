967af1028d2d425b143bf711483d74bef0e71d39
Inès Mouandjo Lobé <ines.mouandjolobe@passculture.app>
2024-03-14 11:58:40 +0100

    sonarcloud code smell

--- src/features/offer/components/MovieScreeningCalendar/utils.ts
+++ src/features/offer/components/MovieScreeningCalendar/utils.ts
@@ -1,7 +1,7 @@
 import { OfferStockResponse } from 'api/gen'
 
 export const sortScreeningDates = (screeningDates: OfferStockResponse[]) =>
-  screeningDates.sort((a, b) => {
+  [...screeningDates].sort((a, b) => {
     return a.beginningDatetime && b.beginningDatetime
       ? new Date(a.beginningDatetime).getTime() - new Date(b.beginningDatetime).getTime()
       : 0
