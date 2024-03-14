85bad26750676f3056baa332b4abf82d49597a3b
Inès Mouandjo Lobé <ines.mouandjolobe@passculture.app>
2024-03-14 11:24:37 +0100

    Use eventCardData to render eventCardList

--- src/features/offer/components/MovieScreeningCalendar/MovieScreeningCalendar.native.test.tsx
+++ src/features/offer/components/MovieScreeningCalendar/MovieScreeningCalendar.native.test.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
-import { OfferStockResponse } from 'api/gen'
+import { OfferResponse, OfferStockResponse, SubcategoryIdEnum } from 'api/gen'
 import { MovieScreeningCalendar } from 'features/offer/components/MovieScreeningCalendar/MovieScreeningCalendar'
+import { mockSubcategory } from 'features/offer/fixtures/mockSubcategory'
+import { offerResponseSnap } from 'features/offer/fixtures/offerResponse'
+import { Subcategory } from 'libs/subcategories/types'
+import { mockServer } from 'tests/mswServer'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { screen, render } from 'tests/utils'
 
 const defaultOfferStockResponse: OfferStockResponse = {
@@ -15,13 +20,26 @@ const defaultOfferStockResponse: OfferStockResponse = {
   price: 570,
 }
 
+const defaultOfferResponse: OfferResponse = {
+  ...offerResponseSnap,
+  subcategoryId: SubcategoryIdEnum.SEANCE_CINE,
+  stocks: [defaultOfferStockResponse],
+}
+
 describe('Movie screening calendar', () => {
+  beforeEach(() => {
+    mockServer.getApiV1<OfferResponse>(`/offer/${offerResponseSnap.id}`, offerResponseSnap)
+  })
+
   it('should render <MovieScreeningCalendar /> without duplicated screening dates', async () => {
     renderMovieScreeningCalendar({
-      stocks: [
-        defaultOfferStockResponse,
-        { ...defaultOfferStockResponse, beginningDatetime: '2024-02-27T11:20:00Z' },
-      ],
+      offer: {
+        ...defaultOfferResponse,
+        stocks: [
+          defaultOfferStockResponse,
+          { ...defaultOfferStockResponse, beginningDatetime: '2024-02-27T11:20:00Z' },
+        ],
+      },
     })
 
     expect(await screen.findByLabelText('Mardi 27 février')).toBeOnTheScreen()
@@ -29,14 +47,17 @@ describe('Movie screening calendar', () => {
 
   it('should render <MovieScreeningCalendar /> without expired screenings', async () => {
     renderMovieScreeningCalendar({
-      stocks: [
-        defaultOfferStockResponse,
-        {
-          ...defaultOfferStockResponse,
-          beginningDatetime: '2024-02-29T13:30:00Z',
-          isExpired: true,
-        },
-      ],
+      offer: {
+        ...defaultOfferResponse,
+        stocks: [
+          defaultOfferStockResponse,
+          {
+            ...defaultOfferStockResponse,
+            beginningDatetime: '2024-02-29T13:30:00Z',
+            isExpired: true,
+          },
+        ],
+      },
     })
 
     await screen.findByLabelText('Mardi 27 février')
@@ -46,14 +67,17 @@ describe('Movie screening calendar', () => {
 
   it('should render <MovieScreeningCalendar /> without forbidden to underage screenings', async () => {
     renderMovieScreeningCalendar({
-      stocks: [
-        defaultOfferStockResponse,
-        {
-          ...defaultOfferStockResponse,
-          beginningDatetime: '2024-02-19T11:10:00Z',
-          isForbiddenToUnderage: true,
-        },
-      ],
+      offer: {
+        ...defaultOfferResponse,
+        stocks: [
+          defaultOfferStockResponse,
+          {
+            ...defaultOfferStockResponse,
+            beginningDatetime: '2024-02-19T11:10:00Z',
+            isForbiddenToUnderage: true,
+          },
+        ],
+      },
     })
 
     await screen.findByLabelText('Mardi 27 février')
@@ -63,9 +87,11 @@ describe('Movie screening calendar', () => {
 })
 
 const renderMovieScreeningCalendar = ({
-  stocks = [defaultOfferStockResponse],
+  offer = offerResponseSnap,
+  subcategory = mockSubcategory,
 }: {
-  stocks: OfferStockResponse[]
+  offer: OfferResponse
+  subcategory?: Subcategory
 }) => {
-  render(<MovieScreeningCalendar stocks={stocks} />)
+  render(reactQueryProviderHOC(<MovieScreeningCalendar offer={offer} subcategory={subcategory} />))
 }
--- src/features/offer/components/MovieScreeningCalendar/MovieScreeningCalendar.tsx
+++ src/features/offer/components/MovieScreeningCalendar/MovieScreeningCalendar.tsx
@@ -1,19 +1,58 @@
 import React, { FunctionComponent, useEffect, useRef } from 'react'
 import { ScrollView } from 'react-native'
 
-import { OfferStockResponse } from 'api/gen'
+import { OfferResponse } from 'api/gen'
+import { useAuthContext } from 'features/auth/context/AuthContext'
+import { useOngoingOrEndedBooking } from 'features/bookings/api'
 import { MovieCalendar } from 'features/offer/components/MovieCalendar/MovieCalendar'
 import { useMovieScreeningCalendar } from 'features/offer/components/MovieScreeningCalendar/useMovieScreeningCalendar'
+import { useOfferCTAButton } from 'features/offer/components/OfferCTAButton/useOfferCTAButton'
+import { getBookingOfferId } from 'features/offer/helpers/getBookingOfferId/getBookingOfferId'
+import { getIsBookedOffer } from 'features/offer/helpers/useCtaWordingAndAction/useCtaWordingAndAction'
+import { useCreditForOffer } from 'features/offer/helpers/useHasEnoughCredit/useHasEnoughCredit'
+import { Subcategory } from 'libs/subcategories/types'
+import { EventCardList } from 'ui/components/eventCard/EventCardList'
 import { Spacer } from 'ui/theme'
 
 type Props = {
-  stocks: OfferStockResponse[]
-  offerId?: number
+  offer: OfferResponse
+  subcategory: Subcategory
 }
-export const MovieScreeningCalendar: FunctionComponent<Props> = ({ stocks, offerId }) => {
-  const { movieScreeningDates, selectedDate, setSelectedDate } = useMovieScreeningCalendar(stocks)
+export const MovieScreeningCalendar: FunctionComponent<Props> = ({ offer, subcategory }) => {
+  const { stocks, id: offerId } = offer
+  const offerVenueId = offer.venue.id
+
+  const {
+    movieScreeningDates,
+    selectedDate,
+    setSelectedDate,
+    getSelectedDateScreenings,
+    bookingData,
+  } = useMovieScreeningCalendar(stocks)
+
+  const { onPress: onPressOfferCTA, CTAOfferModal } = useOfferCTAButton(
+    offer,
+    subcategory,
+    bookingData
+  )
 
   const scrollViewRef = useRef<ScrollView | null>(null)
+
+  const { user, isLoggedIn } = useAuthContext()
+  const userCredit = useCreditForOffer(offerId)
+  const hasBookedOffer = offerId && user ? getIsBookedOffer(offerId, user.bookedOffers) : false
+  const { data: userBooking } = useOngoingOrEndedBooking(
+    getBookingOfferId(offerId, user?.bookedOffers) ?? 0
+  )
+
+  const eventCardData = getSelectedDateScreenings(
+    offerVenueId,
+    hasBookedOffer,
+    userCredit,
+    isLoggedIn,
+    onPressOfferCTA,
+    userBooking
+  )
 
   useEffect(() => {
     if (scrollViewRef?.current) {
@@ -30,6 +69,9 @@ export const MovieScreeningCalendar: FunctionComponent<Props> = ({ stocks, offer
         scrollViewRef={scrollViewRef}
       />
       <Spacer.Column numberOfSpaces={6} />
+      {eventCardData != undefined && <EventCardList data={eventCardData} />}
+      <Spacer.Column numberOfSpaces={6} />
+      {CTAOfferModal}
     </React.Fragment>
   )
 }
