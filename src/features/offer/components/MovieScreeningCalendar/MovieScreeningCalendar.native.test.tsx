e63fb1056a6a6922901cb49dbb6e49f3607d0cc9
Inès Mouandjo Lobé <ines.mouandjolobe@passculture.app>
2024-03-14 13:55:47 +0100

    add some tests

--- src/features/offer/components/MovieScreeningCalendar/MovieScreeningCalendar.native.test.tsx
+++ src/features/offer/components/MovieScreeningCalendar/MovieScreeningCalendar.native.test.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import { OfferResponse, OfferStockResponse, SubcategoryIdEnum } from 'api/gen'
+import { useAuthContext } from 'features/auth/context/AuthContext'
 import { MovieScreeningCalendar } from 'features/offer/components/MovieScreeningCalendar/MovieScreeningCalendar'
 import { mockSubcategory } from 'features/offer/fixtures/mockSubcategory'
 import { offerResponseSnap } from 'features/offer/fixtures/offerResponse'
 import { Subcategory } from 'libs/subcategories/types'
 import { mockServer } from 'tests/mswServer'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { screen, render } from 'tests/utils'
+import { screen, render, act, fireEvent } from 'tests/utils'
 
 const defaultOfferStockResponse: OfferStockResponse = {
   beginningDatetime: '2024-02-27T11:10:00Z',
@@ -26,9 +27,19 @@ const defaultOfferResponse: OfferResponse = {
   stocks: [defaultOfferStockResponse],
 }
 
+jest.mock('features/auth/context/AuthContext')
+const mockUseAuthContext = useAuthContext as jest.MockedFunction<typeof useAuthContext>
+
 describe('Movie screening calendar', () => {
   beforeEach(() => {
     mockServer.getApiV1<OfferResponse>(`/offer/${offerResponseSnap.id}`, offerResponseSnap)
+
+    mockUseAuthContext.mockReturnValue({
+      isLoggedIn: false,
+      setIsLoggedIn: jest.fn(),
+      refetchUser: jest.fn(),
+      isUserLoading: false,
+    })
   })
 
   it('should render <MovieScreeningCalendar /> without duplicated screening dates', async () => {
@@ -83,6 +94,29 @@ describe('Movie screening calendar', () => {
     await screen.findByLabelText('Mardi 27 février')
 
     expect(screen.queryByLabelText('Lundi 19 février')).not.toBeOnTheScreen()
+  })
+
+  it('should open authentication modal when an event card is pressed and user is not logged in', async () => {
+    renderMovieScreeningCalendar({
+      offer: {
+        ...defaultOfferResponse,
+        stocks: [
+          defaultOfferStockResponse,
+          {
+            ...defaultOfferStockResponse,
+            isSoldOut: false,
+          },
+        ],
+      },
+    })
+
+    await screen.findByLabelText('Mardi 27 février')
+
+    await act(async () => {
+      fireEvent.press(await screen.findByText('VO'))
+    })
+
+    expect(screen.queryByText('Identifie-toi pour réserver l’offre')).toBeOnTheScreen()
   })
 })
 
