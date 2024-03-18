372c733478cdd845dd0a3338948e56fdcd305f99
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-03-18 09:16:33 +0100

    (PC-25665) fix(offer): spacing between banner and button for not sticky button

--- src/features/offer/components/BookingButton/BookingButton.tsx
+++ src/features/offer/components/BookingButton/BookingButton.tsx
@@ -1,8 +1,10 @@
 import React, { FunctionComponent } from 'react'
+import styled from 'styled-components/native'
 
 import { BottomBanner } from 'features/offer/components/BottomBanner/BottomBanner'
 import { CTAButton } from 'features/offer/components/CTAButton/CTAButton'
 import { ICTAWordingAndAction } from 'features/offer/helpers/useCtaWordingAndAction/useCtaWordingAndAction'
+import { getSpacing } from 'ui/theme'
 
 type Props = {
   ctaWordingAndAction: ICTAWordingAndAction
@@ -24,17 +26,23 @@ export const BookingButton: FunctionComponent<Props> = ({
 
   return (
     <React.Fragment>
-      <CTAButton
-        wording={wording}
-        onPress={onPress}
-        navigateTo={navigateTo}
-        externalNav={externalNav}
-        isDisabled={isDisabled}
-        isFreeDigitalOffer={isFreeDigitalOffer}
-        isLoggedIn={isLoggedIn}
-      />
+      <CallToActionContainer>
+        <CTAButton
+          wording={wording}
+          onPress={onPress}
+          navigateTo={navigateTo}
+          externalNav={externalNav}
+          isDisabled={isDisabled}
+          isFreeDigitalOffer={isFreeDigitalOffer}
+          isLoggedIn={isLoggedIn}
+        />
+      </CallToActionContainer>
 
       {bottomBannerText ? <BottomBanner text={bottomBannerText} /> : null}
     </React.Fragment>
   )
 }
+
+const CallToActionContainer = styled.View({
+  marginBottom: getSpacing(8),
+})
