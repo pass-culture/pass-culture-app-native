bedfe0776b05c433382b4037631c05524563cc94
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2024-03-20 16:51:43 +0100

    (BSR) fix(sourcemaps): Fix sourcemaps for android (#6147)
    * (BSR) fix(sentry): Add missing config from doc
    https://docs.sentry.io/platforms/react-native/manual-setup/manual-setup/#enable-gradle-integration
    
    * (BSR) chore(scripts): Remove unnecessary condition since RN upgrade
    
    * (BSR) fix(sourcemaps): Add missing command from sentry docs
    https://docs.sentry.io/platforms/react-native/sourcemaps/uploading/hermes/#manual-upload
    
    * (BSR) chore(scripts): Remove unused variable
    
    * (BSR) chore(scripts): Remove unnecessary fix

--- scripts/upload_sourcemaps_to_sentry.sh
+++ scripts/upload_sourcemaps_to_sentry.sh
@@ -18,13 +18,8 @@ create_sourcemaps() {
   else
     HERMES_OS_BIN="osx-bin"
   fi
-  if [[ -f "node_modules/hermes-engine/${HERMES_OS_BIN}/hermesc" ]]; then
-    # react-native v0.68 (current)
-    HERMES_BIN="node_modules/hermes-engine/${HERMES_OS_BIN}/hermesc"
-  else
-    # react-native v0.69 or higher (keep only this condition when version is reached)
-    HERMES_BIN="node_modules/react-native/sdks/hermesc/${HERMES_OS_BIN}/hermesc"
-  fi
+
+  HERMES_BIN="node_modules/react-native/sdks/hermesc/${HERMES_OS_BIN}/hermesc"
 
   if [[ "${APP_OS}" = "android" ]]; then
     npx react-native bundle \
@@ -42,11 +37,14 @@ create_sourcemaps() {
       -output-source-map \
       -out="${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}.hbc" \
       "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}"
+    rm -f "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}"
+    mv "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}.hbc" "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}"
 
     node node_modules/react-native/scripts/compose-source-maps.js \
       "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}.packager.map" \
       "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}.hbc.map" \
       -o "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}.map"
+
   else
     npx react-native bundle \
       --platform "${APP_OS}" \
@@ -62,16 +60,12 @@ create_sourcemaps() {
 upload_sourcemaps() {
   APP_OS="$1"
   APP_ENV="$2"
-  CODE_PUSH_LABEL="${3:-}"
   VERSION=$(jq -r .version package.json)
   BUILD=$(jq -r .build package.json)
   BUNDLE_FILE_NAME="${BUNDLE_FILE_NAME:-}"
 
   echo "APP_OS: ${APP_OS}"
   echo "APP_ENV: ${APP_ENV}"
-  if [[ -n "$CODE_PUSH_LABEL" ]]; then
-    echo "CODE_PUSH_LABEL: ${CODE_PUSH_LABEL}"
-  fi
   echo "VERSION: $VERSION"
   echo "BUILD: $BUILD"
 
@@ -87,20 +81,7 @@ upload_sourcemaps() {
 
   echo "Uploading ${APP_OS} source maps... "
 
-  if [[ -n "${CODE_PUSH_LABEL}" ]]; then
-    RELEASE="${VERSION}-${APP_OS}+codepush:${CODE_PUSH_LABEL}"
-  else
-    RELEASE="${VERSION}-${APP_OS}"
-
-    # Fix android using wrong source maps (See: https://github.com/getsentry/sentry-react-native/issues/2087)
-    if [[ "${APP_OS}" = "android" ]] && [[ -n "${BUNDLE_FILE_NAME}" ]]; then
-      echo "Move: ${SOURCEMAPS_DIR}/${BUNDLE_FILE_NAME} -> ${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}.original"
-      mv "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}" "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}.original"
-      echo "Move: ${SOURCEMAPS_DIR}/${BUNDLE_FILE_NAME}.hbc -> ${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}"
-      mv "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}.hbc" "${SOURCEMAPS_DIR}/${SOURCEMAPS_NAME}"
-    fi
-  fi
-
+  RELEASE="${VERSION}-${APP_OS}"
   DIST="${BUILD}-${APP_OS}"
   echo "RELEASE: ${RELEASE}"
   echo "DIST: ${DIST}"
