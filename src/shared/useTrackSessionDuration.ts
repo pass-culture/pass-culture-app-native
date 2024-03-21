0e5bbd4418f18002944363698723fe4bf6d5ba12
cedricls1 <78556024+cedricls1@users.noreply.github.com>
2024-03-21 18:08:07 +0100

    (PC-28451) feat(search): use SessionDurationFromVenueMap log on venue map (#6154)
    * (PC-28451) refactor(app): rename useTrackOfferSeenDuration to useTrackSessionDuration and extract it in shared folder
    
    * (PC-28451) feat(search): add SessionDurationFromVenueMap log
    
    * (PC-28451) feat(search): use SessionDurationFromVenueMap log on venue map
    
    ---------
    
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

--- src/shared/useTrackSessionDuration.ts
+++ src/shared/useTrackSessionDuration.ts
@@ -1,9 +1,8 @@
 import { useRef, useEffect } from 'react'
 
-import { analytics } from 'libs/analytics'
 import { useAppStateChange } from 'libs/appState'
 
-export const useTrackOfferSeenDuration = (offerId: number) => {
+export const useTrackSessionDuration = (callback: (durationInSeconds: number) => void) => {
   const timeInBackground = useRef(0)
   const startTimeBackground = useRef<Date | null>(null)
   const onAppBecomeActive = () => {
@@ -22,11 +21,9 @@ export const useTrackOfferSeenDuration = (offerId: number) => {
     const startTime = new Date()
     return () => {
       const endTime = new Date()
-      const totalDurationOnPageInSeconds = (endTime.getTime() - startTime.getTime()) / 1000
-      const durationWithoutBackgroundTimeInSec =
-        totalDurationOnPageInSeconds - timeInBackground.current
-      analytics.logOfferSeenDuration(offerId, +durationWithoutBackgroundTimeInSec.toFixed(3))
+      const totalDurationOnPage = (endTime.getTime() - startTime.getTime()) / 1000
+      const durationWithoutBackgroundTime = totalDurationOnPage - timeInBackground.current
+      callback(Number(durationWithoutBackgroundTime.toFixed(3)))
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [callback])
 }
