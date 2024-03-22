b1ef366c0310e64c5b0e4d4bf3364f5a23779701
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2024-03-22 13:51:08 +0000

    (PC-28157) fix(CulturalSurvey): Simplify condition in helper (#6163)

--- src/shared/culturalSurvey/shouldShowCulturalSurvey.ts
+++ src/shared/culturalSurvey/shouldShowCulturalSurvey.ts
@@ -1,6 +1,5 @@
 import { UserProfileResponse } from 'api/gen'
 
 export function shouldShowCulturalSurvey(user?: UserProfileResponse) {
-  if (!user) return false
-  return user.needsToFillCulturalSurvey
+  return !!user?.needsToFillCulturalSurvey
 }
