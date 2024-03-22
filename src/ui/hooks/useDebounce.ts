ea4fa4d5f15b500076441cd3facbe5cfb4e40d3f
Hugo Voisin <hugov@bam.tech>
2024-03-22 19:00:54 +0100

    type: improve useDebounce typing

--- src/ui/hooks/useDebounce.ts
+++ src/ui/hooks/useDebounce.ts
@@ -1,6 +1,21 @@
 import { debounce, DebounceSettings } from 'lodash'
 import { useEffect, useMemo, useRef } from 'react'
 
+type WithParamCallback<T, U> = (props: T) => U
+type NoParamCallback<U> = () => U
+type Callback<T, U> = WithParamCallback<T, U> | NoParamCallback<U>
+const hasNoParams = <T, U>(func?: Callback<T, U>): func is NoParamCallback<U> => func?.length === 0
+
+export function useDebounce<U>(
+  callback: NoParamCallback<U>,
+  delay: number,
+  options?: DebounceSettings
+): NoParamCallback<U>
+export function useDebounce<T, U>(
+  callback: WithParamCallback<T, U>,
+  delay: number,
+  options?: DebounceSettings
+): WithParamCallback<T, U>
 /**
  * This hook is used to debounce, using `lodash.debounce` more safely.
  * You can use it in a React component, even setting some state (which can be usually tricky).
@@ -18,18 +33,18 @@ import { useEffect, useMemo, useRef } from 'react'
  * Inspired from https://www.developerway.com/posts/debouncing-in-react
  */
 export function useDebounce<T, U>(
-  callback: (props: T) => U,
+  callback: Callback<T, U>,
   delay: number,
   options?: DebounceSettings
-) {
-  const ref = useRef<(props: T) => U>()
+): Callback<T, U | undefined> {
+  const ref = useRef<Callback<T, U>>()
 
   useEffect(() => {
     ref.current = callback
   }, [callback])
 
   const debouncedCallback = useMemo(() => {
-    const func = (props: T) => ref.current?.(props)
+    const func = (props: T) => (hasNoParams(ref.current) ? ref.current() : ref.current?.(props))
 
     return debounce(func, delay, options)
     // eslint-disable-next-line react-hooks/exhaustive-deps
