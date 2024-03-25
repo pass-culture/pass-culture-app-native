8c59cee0e3c20da51d1923600f9118ebd260db7f
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-03-25 15:44:15 +0100

    (BSR) fix(NotifSettings): fix padding modal unsavedChanges

--- src/features/profile/pages/NotificationSettings/components/UnsavedSettingsModal.tsx
+++ src/features/profile/pages/NotificationSettings/components/UnsavedSettingsModal.tsx
@@ -8,7 +8,7 @@ import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
 import { AppModal } from 'ui/components/modals/AppModal'
 import { Close } from 'ui/svg/icons/Close'
 import { Invalidate } from 'ui/svg/icons/Invalidate'
-import { getSpacing, Spacer, Typo } from 'ui/theme'
+import { getSpacing, Typo } from 'ui/theme'
 
 interface Props {
   visible: boolean
@@ -32,9 +32,7 @@ export const UnsavedSettingsModal: FunctionComponent<Props> = ({
       rightIcon={Close}
       onRightIconPress={dismissModal}>
       <ModalContent>
-        <Spacer.Column numberOfSpaces={2} />
         <InformationText>Tes modifications ne seront pas prises en compte.</InformationText>
-        <Spacer.Column numberOfSpaces={8} />
         <ButtonPrimary
           wording="Enregistrer mes modifications"
           onPress={() => {
@@ -43,7 +41,6 @@ export const UnsavedSettingsModal: FunctionComponent<Props> = ({
             goBackAndLeaveNotificationSettings()
           }}
         />
-        <Spacer.Column numberOfSpaces={5} />
         <ButtonTertiaryBlack
           wording="Quitter sans enregistrer"
           onPress={() => {
@@ -52,7 +49,6 @@ export const UnsavedSettingsModal: FunctionComponent<Props> = ({
           }}
           icon={Invalidate}
         />
-        <Spacer.Column numberOfSpaces={1} />
       </ModalContent>
     </AppModal>
   )
@@ -63,6 +59,6 @@ const InformationText = styled(Typo.Body)({
 })
 
 const ModalContent = styled.View({
-  paddingHorizontal: getSpacing(5.5),
+  gap: getSpacing(6),
   width: '100%',
 })
