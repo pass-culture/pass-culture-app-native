19f572ac78d040a85c7609388f0a1bb5f82de62e
marineshaw <marines@bam.tech>
2024-03-25 11:53:07 +0100

    :sparkles: (Subscription) Create modal for unsuscribing confirmation

---
+++ src/features/subscription/components/UnsubscribingConfirmationModal.native.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { SubscriptionTheme } from 'features/subscription/types'
+import { render, screen } from 'tests/utils'
+
+import { UnsubscribingConfirmationModal } from './UnsubscribingConfirmationModal'
+
+describe('<UnsubscribingConfirmationModal />', () => {
+  it('should render correctly', () => {
+    render(
+      <UnsubscribingConfirmationModal
+        visible
+        theme={SubscriptionTheme.MUSIQUE}
+        dismissModal={jest.fn()}
+      />
+    )
+
+    expect(screen).toMatchSnapshot()
+  })
+})
