b0fb7d21b7a3f052a2edde8c53842ed57788744b
YassinL <yassin.leclercq1@gmail.com>
2024-03-28 17:17:36 +0100

    fix script on deployment

--- scripts/create_and_push_tag_from_package_json_version.sh
+++ scripts/create_and_push_tag_from_package_json_version.sh
@@ -8,7 +8,7 @@ create_and_push_tag_from_package_json_version() {
   TAG_PREFIX="$1"
   source ./scripts/get_version.sh
   git tag --annotate "${TAG_PREFIX}${VERSION}" --message "v${VERSION}"
-  git push origin "${TAG_PREFIX}${VERSION}"
+  git push origin "${TAG_PREFIX}${VERSION}" --no-verify
 }
 
 # $1 is the tag used to trigger the ci deployment (see .circleci/config.yml)
