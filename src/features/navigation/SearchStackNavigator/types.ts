6555ea529c74716a5aa0515064b864afcf8f0381
Tanguy <tanguy.prache@passculture.app>
2024-03-28 15:56:57 +0100

    separate search routes in two

--- src/features/navigation/SearchStackNavigator/types.ts
+++ src/features/navigation/SearchStackNavigator/types.ts
@@ -5,7 +5,8 @@ import { SearchState } from 'features/search/types'
 export type SearchStackRouteName = keyof SearchStackParamList
 
 export type SearchStackParamList = {
-  Search?: Partial<SearchState & { accessibilityFilter: Partial<DisabilitiesProperties> }>
+  SearchLanding?: Partial<SearchState & { accessibilityFilter: Partial<DisabilitiesProperties> }>
+  SearchResults?: Partial<SearchState & { accessibilityFilter: Partial<DisabilitiesProperties> }>
   SearchN1Books?: Partial<SearchState & { accessibilityFilter: Partial<DisabilitiesProperties> }>
 }
 
