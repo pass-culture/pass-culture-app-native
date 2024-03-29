42da792922cd0cc7b915e4f3f77eebdd70330f23
marineshaw <marines@bam.tech>
2024-03-29 12:00:04 +0100

    :recycle: (useMapSubscriptionHomeIdsToTheme) reduce duplicated code by mocking globally

--- src/features/subscription/helpers/useMapSubscriptionHomeIdsToTheme.native.test.ts
+++ src/features/subscription/helpers/useMapSubscriptionHomeIdsToTheme.native.test.ts
@@ -4,37 +4,26 @@ import { DEFAULT_REMOTE_CONFIG } from 'libs/firebase/remoteConfig/remoteConfig.c
 import * as useRemoteConfigContext from 'libs/firebase/remoteConfig/RemoteConfigProvider'
 
 const useRemoteConfigContextSpy = jest.spyOn(useRemoteConfigContext, 'useRemoteConfigContext')
+useRemoteConfigContextSpy.mockReturnValue({
+  ...DEFAULT_REMOTE_CONFIG,
+  subscriptionHomeEntryIds: {
+    [SubscriptionTheme.CINEMA]: 'cinemaId',
+    [SubscriptionTheme.MUSIQUE]: 'musiqueId',
+    [SubscriptionTheme.LECTURE]: 'lectureId',
+    [SubscriptionTheme.SPECTACLES]: 'spectaclesId',
+    [SubscriptionTheme.VISITES]: 'visiteId',
+    [SubscriptionTheme.ACTIVITES]: 'activitesId',
+  },
+})
 
 describe('useMapSubscriptionHomeIdsToTheme', () => {
   it('should return correct theme when homeId is found in remote config', () => {
-    useRemoteConfigContextSpy.mockReturnValueOnce({
-      ...DEFAULT_REMOTE_CONFIG,
-      subscriptionHomeEntryIds: {
-        [SubscriptionTheme.CINEMA]: 'cinemaId',
-        [SubscriptionTheme.MUSIQUE]: 'musiqueId',
-        [SubscriptionTheme.LECTURE]: 'lectureId',
-        [SubscriptionTheme.SPECTACLES]: 'spectaclesId',
-        [SubscriptionTheme.VISITES]: 'visiteId',
-        [SubscriptionTheme.ACTIVITES]: 'activitesId',
-      },
-    })
     const result = useMapSubscriptionHomeIdsToTheme('spectaclesId')
 
     expect(result).toEqual(SubscriptionTheme.SPECTACLES)
   })
 
   it('should return null when homeId is not found in remote config', () => {
-    useRemoteConfigContextSpy.mockReturnValueOnce({
-      ...DEFAULT_REMOTE_CONFIG,
-      subscriptionHomeEntryIds: {
-        [SubscriptionTheme.CINEMA]: 'cinemaId',
-        [SubscriptionTheme.MUSIQUE]: 'musiqueId',
-        [SubscriptionTheme.LECTURE]: 'lectureId',
-        [SubscriptionTheme.SPECTACLES]: 'spectaclesId',
-        [SubscriptionTheme.VISITES]: 'visiteId',
-        [SubscriptionTheme.ACTIVITES]: 'activitesId',
-      },
-    })
     const result = useMapSubscriptionHomeIdsToTheme('otherId')
 
     expect(result).toBeNull()
