83628b7e48d12b1468ce45e220a4e8196659e65d
NolwenMajorFrances <144016890+NolwenMajorFrances@users.noreply.github.com>
2024-04-02 15:34:55 +0200

    add params necessary to select filter pills and count them

--- src/features/search/helpers/useFilterCount/useFilterCount.ts
+++ src/features/search/helpers/useFilterCount/useFilterCount.ts
@@ -1,34 +1,48 @@
 import { useAccessibilityFiltersContext } from 'features/accessibility/context/AccessibilityFiltersWrapper'
+import { getPriceAsNumber } from 'features/search/helpers/getPriceAsNumber/getPriceAsNumber'
 import { SearchState } from 'features/search/types'
 
 export const useFilterCount = (searchState: SearchState): number => {
-  const { offerCategories, minPrice, maxPrice, offerIsFree, offerIsDuo, date, timeRange, venue } =
-    searchState
+  const {
+    offerCategories,
+    minPrice,
+    maxPrice,
+    offerIsFree,
+    offerIsDuo,
+    date,
+    timeRange,
+    venue,
+    beginningDatetime,
+    endingDatetime,
+    priceRange,
+  } = searchState
   const { disabilities } = useAccessibilityFiltersContext()
-  const hasCategories = offerCategories.length > 0
-  const hasPrices = ((!!minPrice && Number(minPrice) > 0) || !!maxPrice) && !offerIsFree
-  const hasActivatedFreeOffer = offerIsFree ?? false
+  const hasCategories = offerCategories?.length > 0
+  const minPriceAsNumber = getPriceAsNumber(minPrice)
+  const maxPriceAsNumber = getPriceAsNumber(maxPrice)
+  const hasPricesOrFree =
+    (!!minPriceAsNumber && minPriceAsNumber > 0) ||
+    !!maxPriceAsNumber ||
+    !!priceRange ||
+    !!offerIsFree
   const hasActivatedAccessibility =
     !!disabilities.isMentalDisabilityCompliant ||
     !!disabilities.isMotorDisabilityCompliant ||
     !!disabilities.isVisualDisabilityCompliant ||
     !!disabilities.isAudioDisabilityCompliant
+  const hasDatesHours = Boolean(date ?? timeRange ?? beginningDatetime ?? endingDatetime)
 
   return (
     // Lieux culturels
     +!!venue +
     // Catégories
     +hasCategories +
-    // Prix
-    +hasPrices +
-    // Uniquement les offres gratuites
-    +hasActivatedFreeOffer +
+    // Prix ou gratuit
+    +hasPricesOrFree +
     // Uniquement les offres duo
     +offerIsDuo +
-    // Date
-    +!!date +
-    // Heure
-    +!!timeRange +
+    // Date & heure
+    +hasDatesHours +
     //
     +hasActivatedAccessibility
   )
