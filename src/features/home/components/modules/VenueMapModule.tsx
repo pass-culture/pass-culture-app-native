8618fcd602be7ee3e815871b1596c5e1854a1372
Hugo Voisin <hugov@bam.tech>
2024-04-04 18:36:12 +0200

    refactor: extract in hook venue map display condition

--- src/features/home/components/modules/VenueMapModule.tsx
+++ src/features/home/components/modules/VenueMapModule.tsx
@@ -1,24 +1,11 @@
 import React from 'react'
-import { Platform } from 'react-native'
 import styled from 'styled-components/native'
 
 import { VenueMapBlock } from 'features/venueMap/components/VenueMapBlock/VenueMapBlock'
-import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
-import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
-import { useLocation } from 'libs/location'
-import { LocationMode } from 'libs/location/types'
-
-const isWeb = Platform.OS === 'web'
+import { useShouldDisplayVenueMap } from 'features/venueMap/hook/useShouldDisplayVenueMap'
 
 export const VenueMapModule = () => {
-  const enabledVenueMap = useFeatureFlag(RemoteStoreFeatureFlags.WIP_VENUE_MAP)
-
-  const { hasGeolocPosition, selectedLocationMode } = useLocation()
-  const isLocated =
-    selectedLocationMode === LocationMode.AROUND_PLACE ||
-    (selectedLocationMode === LocationMode.AROUND_ME && hasGeolocPosition)
-
-  const shouldDisplayVenueMap = enabledVenueMap && isLocated && !isWeb
+  const shouldDisplayVenueMap = useShouldDisplayVenueMap()
 
   return shouldDisplayVenueMap ? <StyledVenueMapBlock /> : null
 }
---
+++ src/features/venueMap/hook/useShouldDisplayVenueMap.web.test.ts
@@ -0,0 +1,29 @@
+import { useShouldDisplayVenueMap } from 'features/venueMap/hook/useShouldDisplayVenueMap'
+import * as useFeatureFlagAPI from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { LocationMode } from 'libs/location/types'
+import { SuggestedPlace } from 'libs/place/types'
+import { renderHook } from 'tests/utils/web'
+
+jest.spyOn(useFeatureFlagAPI, 'useFeatureFlag').mockReturnValue(true)
+
+const mockedPlace: SuggestedPlace = {
+  label: 'Kourou',
+  info: 'Guyane',
+  geolocation: { longitude: -52.669736, latitude: 5.16186 },
+}
+const mockUseLocation = jest.fn(() => ({
+  hasGeolocPosition: true,
+  selectedLocationMode: LocationMode.AROUND_ME,
+  place: mockedPlace,
+}))
+jest.mock('libs/location', () => ({
+  useLocation: () => mockUseLocation(),
+}))
+
+describe('useShouldDisplayVenueMap', () => {
+  it('should not render venue map on web', () => {
+    const { result } = renderHook(useShouldDisplayVenueMap)
+
+    expect(result.current).toBe(false)
+  })
+})
