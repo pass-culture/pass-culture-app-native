b205d55af8c2dc2752d9d7ad07e50c6beef9bacf
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2024-04-04 17:10:28 +0200

    (PC-19496) feat(accessibility): Update accessibility action plan for 2023 and 2024 (#6242)

---
+++ src/features/profile/pages/Accessibility/AccessibilityActionPlan2024.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+
+import {
+  ActionPlanStatus,
+  ActionPlanTag,
+} from 'features/profile/components/Buttons/ActionPlanTag/ActionPlanTag'
+import { BulletListItem } from 'ui/components/BulletListItem'
+import { ButtonInsideText } from 'ui/components/buttons/buttonInsideText/ButtonInsideText'
+import { ExternalTouchableLink } from 'ui/components/touchableLink/ExternalTouchableLink'
+import { VerticalUl } from 'ui/components/Ul'
+import { ExternalSiteFilled } from 'ui/svg/icons/ExternalSiteFilled'
+import { Spacer, Typo } from 'ui/theme'
+import { getHeadingAttrs } from 'ui/theme/typographyAttrs/getHeadingAttrs'
+
+export const AccessibilityActionPlan2024 = () => (
+  <React.Fragment>
+    <Typo.Title4 {...getHeadingAttrs(2)}>Plan annuel 2024</Typo.Title4>
+    <Spacer.Column numberOfSpaces={6} />
+    <VerticalUl>
+      <BulletListItem>
+        <Typo.Body>
+          Publier la déclaration d’accessibilité du site{' '}
+          <ExternalTouchableLink
+            as={ButtonInsideText}
+            typography="Caption"
+            wording="https://passculture.pro/"
+            icon={ExternalSiteFilled}
+            externalNav={{ url: 'https://passculture.pro/' }}
+          />
+          <ActionPlanTag status={ActionPlanStatus.ONGOING} />
+        </Typo.Body>
+      </BulletListItem>
+
+      <BulletListItem text="Intégrer les données d’Acceslibre pour améliorer l’information sur l’accessibilité des lieux">
+        <ActionPlanTag status={ActionPlanStatus.ONGOING} />
+      </BulletListItem>
+
+      <BulletListItem text="Mener des actions de sensibilisation des partenaires culturels sur l’information d’accessibilité de leurs lieux">
+        <ActionPlanTag status={ActionPlanStatus.TODO} />
+      </BulletListItem>
+
+      <BulletListItem text="Mise en place d’une solution permettant aux jeunes en situation de handicap de retrouver facilement les offres accessibles">
+        <ActionPlanTag status={ActionPlanStatus.DONE} />
+      </BulletListItem>
+
+      <BulletListItem>
+        <Typo.Body>
+          Publier la déclaration d’accessibilité du site{' '}
+          <ExternalTouchableLink
+            as={ButtonInsideText}
+            typography="Caption"
+            wording="https://pass.culture.fr/"
+            icon={ExternalSiteFilled}
+            externalNav={{ url: 'https://pass.culture.fr/' }}
+          />
+          <ActionPlanTag status={ActionPlanStatus.TODO} />
+        </Typo.Body>
+      </BulletListItem>
+
+      <BulletListItem>
+        <Typo.Body>
+          Formation des Product Manager du site{' '}
+          <ExternalTouchableLink
+            as={ButtonInsideText}
+            typography="Caption"
+            wording="https://passculture.pro/"
+            icon={ExternalSiteFilled}
+            externalNav={{ url: 'https://passculture.pro/' }}
+          />
+          <ActionPlanTag status={ActionPlanStatus.TODO} />
+        </Typo.Body>
+      </BulletListItem>
+
+      <BulletListItem text="Mettre en place une charte graphique 100% accessible">
+        <ActionPlanTag status={ActionPlanStatus.TODO} />
+      </BulletListItem>
+
+      <BulletListItem>
+        <Typo.Body>
+          Rendre le site{' '}
+          <ExternalTouchableLink
+            as={ButtonInsideText}
+            typography="Caption"
+            wording="https://passculture.app/"
+            icon={ExternalSiteFilled}
+            externalNav={{ url: 'https://passculture.app/' }}
+          />{' '}
+          100% accessible
+          <ActionPlanTag status={ActionPlanStatus.TODO} />
+        </Typo.Body>
+      </BulletListItem>
+
+      <BulletListItem text="Améliorer la lisibilité de l’accessibilité des offres avec la mise en place des modalités d’accessibilité par offre">
+        <ActionPlanTag status={ActionPlanStatus.TODO} />
+      </BulletListItem>
+
+      <BulletListItem text="Sensibilisation des équipes internes aux enjeux d’accessibilité">
+        <ActionPlanTag status={ActionPlanStatus.TODO} />
+      </BulletListItem>
+    </VerticalUl>
+  </React.Fragment>
+)
