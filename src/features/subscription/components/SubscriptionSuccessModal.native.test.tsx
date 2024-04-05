c409dc2c6f615d73fe9df9e7e1a63763f72c7d17
marineshaw <marines@bam.tech>
2024-04-05 16:54:05 +0200

    :white_check_mark: (SubscriptionSucessModal) fix and complete tests

--- src/features/subscription/components/SubscriptionSuccessModal.native.test.tsx
+++ src/features/subscription/components/SubscriptionSuccessModal.native.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
+import { navigate } from '__mocks__/@react-navigation/native'
 import { SubscriptionTheme } from 'features/subscription/types'
-import { fireEvent, render, screen } from 'tests/utils'
+import { fireEvent, render, screen, waitFor } from 'tests/utils'
 
 import { SubscriptionSuccessModal } from './SubscriptionSuccessModal'
 
@@ -10,6 +11,33 @@ describe('<SubscriptionSuccessModal />', () => {
     render(<SubscriptionSuccessModal visible theme={theme} dismissModal={jest.fn()} />)
 
     expect(screen).toMatchSnapshot()
+  })
+
+  it('should dismiss modal when user press "Continuer sur l’app"', () => {
+    const dismissModal = jest.fn()
+    render(
+      <SubscriptionSuccessModal
+        visible
+        theme={SubscriptionTheme.CINEMA}
+        dismissModal={dismissModal}
+      />
+    )
+
+    fireEvent.press(screen.getByText('Continuer sur l’app'))
+
+    expect(dismissModal).toHaveBeenCalledTimes(1)
+  })
+
+  it('should navigate to notifications settings when user press "Voir mes paramètres"', async () => {
+    render(
+      <SubscriptionSuccessModal visible theme={SubscriptionTheme.CINEMA} dismissModal={jest.fn()} />
+    )
+
+    fireEvent.press(screen.getByText('Voir mes préférences'))
+
+    await waitFor(() => {
+      expect(navigate).toHaveBeenCalledWith('NotificationsSettingsWIP', undefined)
+    })
   })
 
   it('should dismiss modal when navigating to notifications settings', () => {
--- src/features/subscription/components/SubscriptionSuccessModal.web.test.tsx
+++ src/features/subscription/components/SubscriptionSuccessModal.web.test.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 import { SubscriptionTheme } from 'features/subscription/types'
 import { render, checkAccessibilityFor } from 'tests/utils/web'
 
-import { UnsubscribingConfirmationModal } from './UnsubscribingConfirmationModal'
+import { SubscriptionSuccessModal } from './SubscriptionSuccessModal'
 
-describe('<UnsubscribingConfirmationModal />', () => {
+describe('<SubscriptionSuccessModal />', () => {
   describe('Accessibility', () => {
     it.each(Object.values(SubscriptionTheme))(
       'should not have basic accessibility issues for %s',
       async (theme) => {
         const { container } = render(
-          <UnsubscribingConfirmationModal visible theme={theme} dismissModal={jest.fn()} />
+          <SubscriptionSuccessModal visible theme={theme} dismissModal={jest.fn()} />
         )
 
         const results = await checkAccessibilityFor(container)
