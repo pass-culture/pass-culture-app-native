9643915ba11beabbb5231e17978ad7c00d464458
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-04-06 14:18:52 +0200

    (PC-28570) feat(SubscribeTooltip): Show after 1 second and hide after 8 seconds

--- src/features/home/components/SubscribeButtonWithTooltip.native.test.tsx
+++ src/features/home/components/SubscribeButtonWithTooltip.native.test.tsx
@@ -1,22 +1,44 @@
 import React from 'react'
 
 import { SubscribeButtonWithTooltip } from 'features/home/components/SubscribeButtonWithTooltip'
-import { render, screen } from 'tests/utils'
+import { act, render, screen } from 'tests/utils'
+
+const DISPLAY_START_OFFSET_IN_MS = 1000
+const TOOLTIP_TEXT = 'Suis ce thème pour recevoir de l’actualité sur ce sujet !'
+
+jest.useFakeTimers({ legacyFakeTimers: true })
 
 describe('<SubscribeButtonWithTooltip />', () => {
-  it('should render tooltip when user is not subscribed', () => {
+  it('should not show tooltip before 1 second has elapsed', () => {
     render(<SubscribeButtonWithTooltip active={false} onPress={jest.fn()} />)
 
-    expect(
-      screen.getByText('Suis ce thème pour recevoir de l’actualité sur ce sujet !')
-    ).toBeOnTheScreen()
+    jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS - 1)
+
+    expect(screen.queryByText(TOOLTIP_TEXT)).not.toBeOnTheScreen()
   })
 
-  it('should not render tooltip when user is already subscribed', () => {
+  it('should show tooltip after 1 second when user is not subscribed', async () => {
+    render(<SubscribeButtonWithTooltip active={false} onPress={jest.fn()} />)
+
+    await act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+
+    expect(screen.getByText(TOOLTIP_TEXT)).toBeOnTheScreen()
+  })
+
+  it('should not show tooltip when user is already subscribed', () => {
     render(<SubscribeButtonWithTooltip active onPress={jest.fn()} />)
 
-    expect(
-      screen.queryByText('Suis ce thème pour recevoir de l’actualité sur ce sujet !')
-    ).not.toBeOnTheScreen()
+    act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+
+    expect(screen.queryByText(TOOLTIP_TEXT)).not.toBeOnTheScreen()
+  })
+
+  it('should hide tooltip 8 seconds after display', async () => {
+    render(<SubscribeButtonWithTooltip active={false} onPress={jest.fn()} />)
+
+    await act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+    await act(() => jest.advanceTimersByTime(8000))
+
+    expect(screen.queryByText(TOOLTIP_TEXT)).not.toBeOnTheScreen()
   })
 })
--- src/features/home/components/SubscribeButtonWithTooltip.tsx
+++ src/features/home/components/SubscribeButtonWithTooltip.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import styled from 'styled-components/native'
 
 import { SubscribeButton } from 'features/home/components/SubscribeButton'
@@ -7,15 +7,35 @@ import { getSpacing } from 'ui/theme'
 
 const WIDGET_HEIGHT = getSpacing(10 + 1 + 4) // roundedButton + padding + caption
 const TOOLTIP_WIDTH = getSpacing(65)
+const DISPLAY_START_OFFSET_IN_MS = 1000
+const DISPLAY_DURATION_IN_MS = 8000
 
 export const SubscribeButtonWithTooltip = (props: { active: boolean; onPress: () => void }) => {
+  const [isTooltipVisible, setIsTooltipVisible] = React.useState(false)
+
+  const displayTooltip = useCallback(() => setIsTooltipVisible(true), [setIsTooltipVisible])
+  const hideTooltip = useCallback(() => setIsTooltipVisible(false), [setIsTooltipVisible])
+
+  useEffect(() => {
+    if (props.active) return
+
+    const timeoutOn = setTimeout(displayTooltip, DISPLAY_START_OFFSET_IN_MS)
+    const timeoutOff = setTimeout(hideTooltip, DISPLAY_START_OFFSET_IN_MS + DISPLAY_DURATION_IN_MS)
+
+    return () => {
+      clearTimeout(timeoutOn)
+      clearTimeout(timeoutOff)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [displayTooltip, hideTooltip])
+
   return (
     <React.Fragment>
       <SubscribeButton {...props} />
       <StyledTooltip
         label="Suis ce thème pour recevoir de l’actualité sur ce sujet&nbsp;!"
         pointerDirection="bottom"
-        isVisible={!props.active}
+        isVisible={isTooltipVisible}
       />
     </React.Fragment>
   )
