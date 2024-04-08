a1e970e11a8eeae318fe00321a88ec39c2cba1e9
Ali RAIKI <46637324+aliraiki@users.noreply.github.com>
2024-04-08 16:15:05 +0200

    (PC-28570) feat(SubscribeButton): Handle tooltip display (#6258)
    * (PC-28570) feat(Tooltip): Make pointer direction flexible
    
    * (PC-28570) feat(subscription): Add subscribe button with tooltip
    
    * (PC-28570) feat(SubscribeTooltip): Show after 1 second and hide after 8 seconds
    
    * (PC-28570) feat(SubscribeTooltip): Handle max display times
    
    * (PC-28570) feat(SubscribeTooltip): Hide tooltip on close
    
    * (PC-28570) feat(SubscribeTooltip): Use tooltip in fake thematic home
    
    * (PC-28570) chore(prune): Update deadcode

---
+++ src/features/home/components/SubscribeButtonWithTooltip.native.test.tsx
@@ -0,0 +1,88 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+
+import { SubscribeButtonWithTooltip } from 'features/home/components/SubscribeButtonWithTooltip'
+import { storage } from 'libs/storage'
+import { act, fireEvent, render, screen } from 'tests/utils'
+
+const DISPLAY_START_OFFSET_IN_MS = 1000
+const TOOLTIP_TEXT = 'Suis ce thème pour recevoir de l’actualité sur ce sujet !'
+
+jest.unmock('@react-navigation/native')
+jest.useFakeTimers()
+
+describe('<SubscribeButtonWithTooltip />', () => {
+  beforeEach(() => storage.clear('times_subscription_tooltip_has_been_displayed'))
+
+  it('should not show tooltip before 1 second has elapsed', () => {
+    renderSubscribeButtonWithTooltip()
+
+    jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS - 1)
+
+    expect(screen.queryByText(TOOLTIP_TEXT)).not.toBeOnTheScreen()
+  })
+
+  it('should show tooltip after 1 second when user is not subscribed', async () => {
+    renderSubscribeButtonWithTooltip()
+
+    await act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+
+    expect(screen.getByText(TOOLTIP_TEXT)).toBeOnTheScreen()
+  })
+
+  it('should not show tooltip when user is already subscribed', () => {
+    renderSubscribeButtonWithTooltip({ active: true })
+
+    act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+
+    expect(screen.queryByText(TOOLTIP_TEXT)).not.toBeOnTheScreen()
+  })
+
+  it('should hide tooltip 8 seconds after display', async () => {
+    renderSubscribeButtonWithTooltip()
+
+    await act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+    await act(() => jest.advanceTimersByTime(8000))
+
+    expect(screen.queryByText(TOOLTIP_TEXT)).not.toBeOnTheScreen()
+  })
+
+  it('should not show tooltip more than 3 times', async () => {
+    renderSubscribeButtonWithTooltip()
+    await act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+
+    expect(screen.getByText(TOOLTIP_TEXT)).toBeOnTheScreen()
+
+    renderSubscribeButtonWithTooltip()
+    await act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+
+    expect(screen.getByText(TOOLTIP_TEXT)).toBeOnTheScreen()
+
+    renderSubscribeButtonWithTooltip()
+    await act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+
+    expect(screen.getByText(TOOLTIP_TEXT)).toBeOnTheScreen()
+
+    renderSubscribeButtonWithTooltip()
+    await act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+
+    expect(screen.queryByText(TOOLTIP_TEXT)).not.toBeOnTheScreen()
+  })
+
+  it('should hide tooltip when pressing close button', async () => {
+    renderSubscribeButtonWithTooltip()
+
+    await act(() => jest.advanceTimersByTime(DISPLAY_START_OFFSET_IN_MS))
+
+    fireEvent.press(screen.getByLabelText('Fermer le tooltip'))
+
+    expect(screen.queryByText(TOOLTIP_TEXT)).not.toBeOnTheScreen()
+  })
+})
+
+const renderSubscribeButtonWithTooltip = ({ active }: { active: boolean } = { active: false }) =>
+  render(
+    <NavigationContainer>
+      <SubscribeButtonWithTooltip active={active} onPress={jest.fn()} />
+    </NavigationContainer>
+  )
---
+++ src/features/home/components/SubscribeButtonWithTooltip.tsx
@@ -0,0 +1,65 @@
+import React, { useCallback, useEffect } from 'react'
+import styled from 'styled-components/native'
+
+import { SubscribeButton } from 'features/home/components/SubscribeButton'
+import { storage } from 'libs/storage'
+import { Tooltip } from 'ui/components/Tooltip'
+import { getSpacing } from 'ui/theme'
+
+const WIDGET_HEIGHT = getSpacing(10 + 1 + 4) // roundedButton + padding + caption
+const TOOLTIP_WIDTH = getSpacing(65)
+const DISPLAY_START_OFFSET_IN_MS = 1000
+const DISPLAY_DURATION_IN_MS = 8000
+const MAX_TOOLTIP_DISPLAYS = 3
+
+export const SubscribeButtonWithTooltip = (props: { active: boolean; onPress: () => void }) => {
+  const [isTooltipVisible, setIsTooltipVisible] = React.useState(false)
+
+  const displayTooltipIfNeeded = useCallback(async () => {
+    const timesTooltipHasBeenDisplayed = Number(
+      await storage.readString('times_subscription_tooltip_has_been_displayed')
+    )
+
+    setIsTooltipVisible(timesTooltipHasBeenDisplayed < MAX_TOOLTIP_DISPLAYS)
+
+    await storage.saveString(
+      'times_subscription_tooltip_has_been_displayed',
+      String(timesTooltipHasBeenDisplayed + 1)
+    )
+  }, [setIsTooltipVisible])
+
+  const hideTooltip = useCallback(() => setIsTooltipVisible(false), [setIsTooltipVisible])
+
+  useEffect(() => {
+    if (props.active) return
+
+    const timeoutOn = setTimeout(displayTooltipIfNeeded, DISPLAY_START_OFFSET_IN_MS)
+    const timeoutOff = setTimeout(hideTooltip, DISPLAY_START_OFFSET_IN_MS + DISPLAY_DURATION_IN_MS)
+
+    return () => {
+      clearTimeout(timeoutOn)
+      clearTimeout(timeoutOff)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [displayTooltipIfNeeded, hideTooltip])
+
+  return (
+    <React.Fragment>
+      <SubscribeButton {...props} />
+      <StyledTooltip
+        label="Suis ce thème pour recevoir de l’actualité sur ce sujet&nbsp;!"
+        pointerDirection="bottom"
+        isVisible={isTooltipVisible}
+        onHide={hideTooltip}
+      />
+    </React.Fragment>
+  )
+}
+
+const StyledTooltip = styled(Tooltip)(({ theme }) => ({
+  position: 'absolute',
+  top: -WIDGET_HEIGHT - getSpacing(0.5),
+  right: -getSpacing(1),
+  zIndex: theme.zIndex.header,
+  width: TOOLTIP_WIDTH,
+}))
