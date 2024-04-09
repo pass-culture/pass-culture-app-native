7cc77227c575b966126af4da9ab1ae3878edd3a4
Hugo Voisin <hugo.voisin@passculture.app>
2024-04-09 02:26:55 +0200

    (PC-25857) feat(offerPreview): add carousel (#6263)
    * chore: move babel plugin dep to dev deps
    
    * feat: add carousel to offer preview
    
    * feat: disable pan when zoomed out
    so that we can pan to navigate in the carousel, instead of moving the image in the zoomable view
    
    * feat: update index in the header
    
    * test: fix tests

--- src/features/offer/components/PinchableBox/PinchableBox.tsx
+++ src/features/offer/components/PinchableBox/PinchableBox.tsx
@@ -1,5 +1,9 @@
-import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view'
+import {
+  ReactNativeZoomableView,
+  ZoomableViewEvent,
+} from '@openspacelabs/react-native-zoomable-view'
 import React from 'react'
+import { PanResponderGestureState, GestureResponderEvent } from 'react-native'
 // Importing FastImage for displaying offer images without resizing, prioritizing optimal quality for potential zooming.
 // eslint-disable-next-line no-restricted-imports
 import FastImage from 'react-native-fast-image'
@@ -9,9 +13,24 @@ type Props = {
   imageUrl: string
 }
 
+const blockNativeResponderOnInitialZoom = (
+  _event: GestureResponderEvent,
+  _gestureState: PanResponderGestureState,
+  zoomableViewEventObject: ZoomableViewEvent
+): boolean => {
+  return zoomableViewEventObject.zoomLevel !== 1
+}
+
 export function PinchableBox({ imageUrl }: Readonly<Props>) {
   return (
-    <ZoomableView maxZoom={2} minZoom={1} doubleTapDelay={500} zoomStep={1}>
+    <ZoomableView
+      maxZoom={2}
+      minZoom={1}
+      doubleTapDelay={500}
+      zoomStep={1}
+      // We disable the pan on initial zoom to avoid the image moving when the user moves the carousel.
+      onShouldBlockNativeResponder={blockNativeResponderOnInitialZoom}
+      disablePanOnInitialZoom>
       <Image
         source={{
           uri: imageUrl,
