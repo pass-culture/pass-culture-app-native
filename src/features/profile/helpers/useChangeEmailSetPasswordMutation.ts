754859e86f1530722c268b8f615b4f4ffdb7fd4c
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-04-10 11:46:35 +0200

    (PC-28721) feat(api): Add mutation to set new password

---
+++ src/features/profile/helpers/useChangeEmailSetPasswordMutation.ts
@@ -0,0 +1,17 @@
+import { useMutation } from 'react-query'
+
+import { api } from 'api/api'
+import { ResetPasswordRequest } from 'api/gen'
+
+export const useChangeEmailSetPasswordMutation = ({
+  onSuccess,
+  onError,
+}: {
+  onSuccess: () => void
+  onError: () => void
+}) => {
+  return useMutation(
+    (body: ResetPasswordRequest) => api.postNativeV2ProfileEmailUpdateNewPassword(body),
+    { onSuccess, onError }
+  )
+}
