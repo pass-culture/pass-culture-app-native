c36fe112883f821ab292da7656baaaf78fb24c75
marineshaw <marines@bam.tech>
2024-04-15 10:41:31 +0200

    :sparkles: (OnboardingSubscription) add page content

--- src/features/subscription/page/OnboardingSubscription.native.test.tsx
+++ src/features/subscription/page/OnboardingSubscription.native.test.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
+import { mockGoBack } from 'features/navigation/__mocks__/useGoBack'
 import { OnboardingSubscription } from 'features/subscription/page/OnboardingSubscription'
-import { render, screen } from 'tests/utils'
+import { fireEvent, render, screen } from 'tests/utils'
 
 describe('OnboardingSubscription', () => {
   it('should render correctly', () => {
     render(<OnboardingSubscription />)
 
     expect(screen).toMatchSnapshot()
+  })
+
+  it('should go back when user presses "Non merci"', () => {
+    render(<OnboardingSubscription />)
+
+    fireEvent.press(screen.getByText('Non merci'))
+
+    expect(mockGoBack).toHaveBeenCalledTimes(1)
+  })
+
+  it('should toggle theme when user presses a category button', () => {
+    render(<OnboardingSubscription />)
+
+    fireEvent.press(screen.getByLabelText('Activités créatives'))
+
+    expect(screen.getByLabelText('Activités créatives')).toHaveAccessibilityState({ checked: true })
   })
 })
--- src/features/subscription/page/OnboardingSubscription.tsx
+++ src/features/subscription/page/OnboardingSubscription.tsx
@@ -1,20 +1,111 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { FlatList } from 'react-native'
 import styled from 'styled-components/native'
 
-import { SecondaryPageWithBlurHeader } from 'ui/pages/SecondaryPageWithBlurHeader'
-import { Typo } from 'ui/theme'
+import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
+import { useGoBack } from 'features/navigation/useGoBack'
+import { SubscriptionCategoryButton } from 'features/subscription/components/buttons/SubscriptionCategoryButton'
+import { mapSubscriptionThemeToName } from 'features/subscription/helpers/mapSubscriptionThemeToName'
+import { SubscriptionTheme } from 'features/subscription/types'
+import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
+import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
+import { BlurHeader } from 'ui/components/headers/BlurHeader'
+import {
+  PageHeaderWithoutPlaceholder,
+  useGetHeaderHeight,
+} from 'ui/components/headers/PageHeaderWithoutPlaceholder'
+import { Invalidate } from 'ui/svg/icons/Invalidate'
+import { getSpacing, Spacer, Typo } from 'ui/theme'
 import { getHeadingAttrs } from 'ui/theme/typographyAttrs/getHeadingAttrs'
 
 export const OnboardingSubscription = () => {
+  const { goBack } = useGoBack(...getTabNavConfig('Home'))
+  const headerHeight = useGetHeaderHeight()
+
+  const [subscribedThemes, toggleSubscribedTheme] = useReducer(
+    (state: SubscriptionTheme[], action: SubscriptionTheme) => {
+      if (state.includes(action)) {
+        return state.filter((theme) => theme !== action)
+      }
+      return [...state, action]
+    },
+    []
+  )
+
+  const isThemeToggled = (theme: SubscriptionTheme) => subscribedThemes.includes(theme)
+
+  const renderItem = ({ item }: { item: SubscriptionTheme }) => {
+    return (
+      <SubscriptionCategoryButtonContainer>
+        <SubscriptionCategoryButton
+          thematic={item}
+          checked={isThemeToggled(item)}
+          onPress={() => toggleSubscribedTheme(item)}
+        />
+      </SubscriptionCategoryButtonContainer>
+    )
+  }
+
   return (
-    <SecondaryPageWithBlurHeader title="">
-      <StyledTitle3>Choisis des thèmes à suivre</StyledTitle3>
-      <Typo.Body>
-        Tu recevras des notifs et/ou des mails pour ne rien rater des dernières sorties et
-        actus&nbsp;!
-      </Typo.Body>
-    </SecondaryPageWithBlurHeader>
+    <React.Fragment>
+      <PageHeaderWithoutPlaceholder title="" shouldDisplayBackButton />
+      <StyledFlatList
+        data={Object.values(SubscriptionTheme)}
+        // @ts-ignore because of styled-components that doesn't pass the type
+        renderItem={renderItem}
+        keyExtractor={(item) => mapSubscriptionThemeToName[item as SubscriptionTheme]}
+        numColumns={2}
+        ListHeaderComponent={
+          <React.Fragment>
+            <Placeholder height={headerHeight} />
+            <StyledTitle3>Choisis des thèmes à suivre</StyledTitle3>
+            <Spacer.Column numberOfSpaces={4} />
+            <Typo.Body>
+              Tu recevras des notifs et/ou des mails pour ne rien rater des dernières sorties et
+              actus&nbsp;!
+            </Typo.Body>
+            <Spacer.Column numberOfSpaces={6} />
+          </React.Fragment>
+        }
+        ListFooterComponent={
+          <React.Fragment>
+            <Spacer.Column numberOfSpaces={10} />
+            <ButtonPrimary wording="Suivre la sélection" />
+            <Spacer.Column numberOfSpaces={6} />
+            <ButtonTertiaryBlack
+              wording="Non merci"
+              accessibilityLabel="Ne pas suivre de thème"
+              icon={Invalidate}
+              onPress={goBack}
+            />
+          </React.Fragment>
+        }
+      />
+
+      <BlurHeader height={headerHeight} />
+    </React.Fragment>
   )
 }
 
 const StyledTitle3 = styled(Typo.Title3).attrs(() => getHeadingAttrs(1))``
+
+const Placeholder = styled.View<{ height: number }>(({ height }) => ({
+  height,
+}))
+
+const StyledFlatList = styled(FlatList).attrs(({ theme }) => ({
+  contentContainerStyle: {
+    paddingHorizontal: theme.contentPage.marginHorizontal,
+    paddingVertical: theme.contentPage.marginVertical,
+    maxWidth: theme.contentPage.maxWidth,
+    width: '100%',
+    alignSelf: 'center',
+  },
+}))``
+
+const SubscriptionCategoryButtonContainer = styled.View(() => ({
+  paddingHorizontal: getSpacing(5),
+  paddingVertical: getSpacing(3),
+  height: 172,
+  flexBasis: '50%',
+}))
