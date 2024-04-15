6c45ff899a8a837948e31f1f6371d35949d544a6
marineshaw <marines@bam.tech>
2024-04-15 11:57:15 +0200

    :white_check_mark: (OnboardingSubscription) fix web snapshot

--- src/features/subscription/page/OnboardingSubscription.web.test.tsx
+++ src/features/subscription/page/OnboardingSubscription.web.test.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 import { OnboardingSubscription } from 'features/subscription/page/OnboardingSubscription'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { render, screen } from 'tests/utils/web'
+import { render } from 'tests/utils/web'
 
 describe('OnboardingSubscription', () => {
   it('should render correctly', () => {
-    render(reactQueryProviderHOC(<OnboardingSubscription />))
+    const { container } = render(reactQueryProviderHOC(<OnboardingSubscription />))
 
-    expect(screen).toMatchSnapshot()
+    expect(container).toMatchSnapshot()
   })
 })
