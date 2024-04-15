9dc6070c3d67b8dcada84fea4015b768cfbc45b6
Hugo Voisin <hugo.voisin@passculture.app>
2024-04-15 15:13:38 +0200

    (PC-28985) feat(maps): zoom out when map is empty (#6300)

--- src/features/venueMap/hook/useGetVenuesInRegion.ts
+++ src/features/venueMap/hook/useGetVenuesInRegion.ts
@@ -1,3 +1,5 @@
+import { useMemo } from 'react'
+
 import { Venue } from 'features/venue/types'
 import { GeolocatedVenue } from 'features/venueMap/components/VenueMapView/types'
 import { calculateRoundRadiusInKilometers } from 'features/venueMap/helpers/calculateDistanceMap'
@@ -13,5 +15,8 @@ export const useGetVenuesInRegion = (
   const radius = calculateRoundRadiusInKilometers(region)
   const { venues = [] } = useGetAllVenues({ region, radius, initialVenues })
 
-  return getGeolocatedVenues(venues, selectedVenue)
+  // We want to add the selected venue to the list of venues if it's not already there
+  // but it can only happen if the venues change
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useMemo(() => getGeolocatedVenues(venues, selectedVenue), [venues])
 }
