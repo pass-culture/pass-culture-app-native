a5f20e33372da23db1ccd523af28fd634e49a55b
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-04-23 15:41:25 +0200

    (PC-28720) fix(ChangeEmailSetPassword): Fix keyboard and field behavior

--- src/features/profile/pages/ChangeEmailSetPassword/ChangeEmailSetPassword.tsx
+++ src/features/profile/pages/ChangeEmailSetPassword/ChangeEmailSetPassword.tsx
@@ -1,7 +1,9 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useNavigation, useRoute } from '@react-navigation/native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { Platform } from 'react-native'
+import styled from 'styled-components/native'
 
 import { UseNavigationType, UseRouteType } from 'features/navigation/RootNavigator/types'
 import { useChangeEmailSetPasswordMutation } from 'features/profile/helpers/useChangeEmailSetPasswordMutation'
@@ -9,6 +11,7 @@ import { PasswordInputController } from 'shared/forms/controllers/PasswordInputC
 import { newPasswordSchema } from 'shared/forms/schemas/newPasswordSchema'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { Form } from 'ui/components/Form'
+import { useForHeightKeyboardEvents } from 'ui/components/keyboard/useKeyboardEvents'
 import { useSnackBarContext, SNACK_BAR_TIME_OUT } from 'ui/components/snackBar/SnackBarContext'
 import { SecondaryPageWithBlurHeader } from 'ui/pages/SecondaryPageWithBlurHeader'
 import { Spacer, Typo } from 'ui/theme'
@@ -23,6 +26,8 @@ export const ChangeEmailSetPassword = () => {
   const { replace } = useNavigation<UseNavigationType>()
   const { params } = useRoute<UseRouteType<'ChangeEmailSetPassword'>>()
   const { showErrorSnackBar, showSuccessSnackBar } = useSnackBarContext()
+  const [keyboardHeight, setKeyboardHeight] = useState(0)
+  useForHeightKeyboardEvents(setKeyboardHeight)
 
   const {
     control,
@@ -58,45 +63,48 @@ export const ChangeEmailSetPassword = () => {
   )
 
   return (
-    <SecondaryPageWithBlurHeader
-      title="Créer mon mot de passe"
-      scrollViewProps={{
-        automaticallyAdjustKeyboardInsets: true,
-      }}>
-      <Typo.Title3 {...getHeadingAttrs(2)}>Crée ton mot de passe</Typo.Title3>
-      <Spacer.Column numberOfSpaces={4} />
-      <Typo.Body>
-        Tu t’es inscrit via Google, tu ne possèdes donc pas de mot de passe actuellement.
-      </Typo.Body>
-      <Spacer.Column numberOfSpaces={4} />
-      <Typo.Body>
-        Ce mot de passe te permettra de te connecter avec ta nouvelle adresse e-mail.
-      </Typo.Body>
-      <Spacer.Column numberOfSpaces={10} />
-      <Form.MaxWidth flex={1}>
-        <PasswordInputController
-          name="newPassword"
-          label="Mot de passe"
-          control={control}
-          isRequiredField
-          withSecurityRules
-          securityRulesAlwaysVisible
-        />
+    <SecondaryPageWithBlurHeader title="Créer mon mot de passe">
+      <StyledView paddingBottom={Platform.OS === 'ios' ? keyboardHeight : 0}>
+        <Typo.Title3 {...getHeadingAttrs(2)}>Crée ton mot de passe</Typo.Title3>
+        <Spacer.Column numberOfSpaces={4} />
+        <Typo.Body>
+          Tu t’es inscrit via Google, tu ne possèdes donc pas de mot de passe actuellement.
+        </Typo.Body>
+        <Spacer.Column numberOfSpaces={4} />
+        <Typo.Body>
+          Ce mot de passe te permettra de te connecter avec ta nouvelle adresse e-mail.
+        </Typo.Body>
         <Spacer.Column numberOfSpaces={10} />
-        <PasswordInputController
-          name="confirmedPassword"
-          label="Confirmer le mot de passe"
-          control={control}
-          isRequiredField
-        />
-        <Spacer.Column numberOfSpaces={10} />
-        <ButtonPrimary
-          wording="Créer mon mot de passe"
-          disabled={!isValid}
-          isLoading={isLoading}
-          onPress={onSubmit}
-        />
-      </Form.MaxWidth>
+        <Form.MaxWidth flex={1}>
+          <PasswordInputController
+            name="newPassword"
+            label="Mot de passe"
+            control={control}
+            isRequiredField
+            autoFocus
+            withSecurityRules
+            securityRulesAlwaysVisible
+          />
+          <Spacer.Column numberOfSpaces={10} />
+          <PasswordInputController
+            name="confirmedPassword"
+            label="Confirmer le mot de passe"
+            control={control}
+            isRequiredField
+          />
+          <Spacer.Column numberOfSpaces={10} />
+          <ButtonPrimary
+            wording="Créer mon mot de passe"
+            disabled={!isValid}
+            isLoading={isLoading}
+            onPress={onSubmit}
+          />
+        </Form.MaxWidth>
+      </StyledView>
     </SecondaryPageWithBlurHeader>
   )
 }
+
+const StyledView = styled.View<{ paddingBottom: number }>(({ paddingBottom }) => ({
+  paddingBottom,
+}))
