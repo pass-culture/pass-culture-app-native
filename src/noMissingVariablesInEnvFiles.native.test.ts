7498dbf436a5648f0c6c0372012b71aa0f861dcc
Hugo Voisin <hugo.voisin@passculture.app>
2024-04-24 12:17:39 +0200

    (BSR) ci(env): check variables in env files (#6318)
    * ci: check if missing variables in env files
    
    * chore: add variables to integration env
    
    * prune: remove unused script
    
    * ci: check missing env variables within jest tests

---
+++ src/noMissingVariablesInEnvFiles.native.test.ts
@@ -0,0 +1,49 @@
+import { config } from 'dotenv'
+
+function loadEnvVariables(filePath: string) {
+  const { parsed, error } = config({ path: filePath })
+  if (error) {
+    throw error
+  }
+  if (!parsed) {
+    throw new Error(`No variables found in ${filePath}`)
+  }
+
+  return Object.keys(parsed)
+}
+
+function compareEnvFiles(envFiles: string[]) {
+  const envData: Record<string, string[]> = {}
+  envFiles.forEach((file) => {
+    envData[file] = loadEnvVariables(file)
+  })
+
+  const allKeys = new Set<string>()
+  for (const data of Object.values(envData)) {
+    for (const key of data) {
+      allKeys.add(key)
+    }
+  }
+
+  const missing: Record<string, string[]> = {}
+  for (const [file, data] of Object.entries(envData)) {
+    missing[file] = Array.from(allKeys).filter((x) => !data.includes(x))
+  }
+
+  return missing
+}
+
+describe('.env files', () => {
+  const envFiles = ['.env.testing', '.env.staging', '.env.integration', '.env.production']
+
+  test('all variables should be present in all .env files', () => {
+    const missingVariables = compareEnvFiles(envFiles)
+    for (const [file, variables] of Object.entries(missingVariables)) {
+      if (variables.length > 0) {
+        throw new Error(`Missing variables in ${file}: ${variables.join(', ')}`)
+      }
+    }
+
+    expect(true).toBe(true) // Pass if no error is thrown
+  })
+})
