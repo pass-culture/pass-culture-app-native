3e426d4f3112db0b90e5d14f7417522cb9902cb6
Hugo Voisin <hugov@bam.tech>
2024-04-25 16:15:53 +0200

    test: add test to log when having validation issues

--- src/libs/environment/parseEnvironment.test.ts
+++ src/libs/environment/parseEnvironment.test.ts
@@ -1,13 +1,19 @@
-import { parseBooleanVariables } from './parseEnvironment'
+import { eventMonitoring } from 'libs/monitoring'
+
+import { parseBooleanVariables, parseEnvironment } from './parseEnvironment'
+
+jest.mock('libs/monitoring')
+const mockCaptureException = eventMonitoring.captureException as jest.Mock
+
+const mockedConfig = {
+  ENV: 'staging',
+  API_BASE_URL: 'your-api.com',
+  FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING: 'false',
+  SENTRY_DSN: 'sentry-dsn',
+  URL_PREFIX: 'passculture',
+}
 
 describe('parseBooleanVariables', () => {
-  const mockedConfig = {
-    ENV: 'staging',
-    API_BASE_URL: 'your-api.com',
-    FEATURE_FLIPPING_ONLY_VISIBLE_ON_TESTING: 'false',
-    SENTRY_DSN: 'sentry-dsn',
-    URL_PREFIX: 'passculture',
-  }
   const convertedConfig = parseBooleanVariables(mockedConfig)
 
   it('should generate falsy values for feature flags used with false', () => {
@@ -27,5 +33,28 @@ describe('parseBooleanVariables', () => {
     expect(convertedConfig.API_BASE_URL).toEqual('your-api.com')
     expect(convertedConfig.SENTRY_DSN).toEqual('sentry-dsn')
     expect(convertedConfig.URL_PREFIX).toEqual('passculture')
+  })
+})
+
+describe('parseEnvironment', () => {
+  it('should log error when having a validation error', () => {
+    const mockConsoleError = jest.fn()
+    jest.spyOn(global.console, 'error').mockImplementationOnce(mockConsoleError)
+
+    parseEnvironment(mockedConfig)
+
+    expect(mockConsoleError).toHaveBeenCalledWith(
+      'Error parsing .env file: WEBAPP_V2_DOMAIN is a required field'
+    )
+  })
+
+  it('should log to Sentry when having a validation error', () => {
+    jest.spyOn(global.console, 'error').mockImplementationOnce(() => null)
+
+    parseEnvironment(mockedConfig)
+
+    expect(mockCaptureException).toHaveBeenCalledWith(
+      'Error parsing .env file: WEBAPP_V2_DOMAIN is a required field'
+    )
   })
 })
