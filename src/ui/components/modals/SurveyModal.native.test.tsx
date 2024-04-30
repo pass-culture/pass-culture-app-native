9e850d600e00908eb04ed93676538192dd5930f9
NolwenMajorFrances <144016890+NolwenMajorFrances@users.noreply.github.com>
2024-04-30 10:48:13 +0200

    (PC-29181) fix(MusicLiveOffers): survey modal on mobile (#6368)
    * Revert partially "BSR: clean code of a useless state, props as string instead of component and variable naming. (#6360)"
    
    This reverts commit 547f9dcf06c68049a410defc65442c6183ec9060.
    
    * hide surveyModal on buttonSurvey click
    
    * noUncheckedIndexedAccess:update due to line change
    
    * fix test that needed an await because an onbeforeNavigate addition on component

--- src/ui/components/modals/SurveyModal.native.test.tsx
+++ src/ui/components/modals/SurveyModal.native.test.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { ComponentProps } from 'react'
 
 import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
-import { fireEvent, render, screen } from 'tests/utils'
+import { fireEvent, render, screen, waitFor } from 'tests/utils'
 import { SurveyModal } from 'ui/components/modals/SurveyModal'
 import { BicolorCircledClock } from 'ui/svg/icons/BicolorCircledClock'
 
@@ -10,29 +10,60 @@ const openUrl = jest.spyOn(NavigationHelpers, 'openUrl')
 const hideModalMock = jest.fn()
 
 describe('<SurveyModal />', () => {
-  it('should redirect to survey when pressing "Répondre au questionnaire" button', () => {
-    renderSurveyModal()
+  it('should redirect to survey when pressing "Répondre au questionnaire" button', async () => {
+    renderSurveyModal({ surveyUrl: 'https://fr.wikipedia.org/wiki/FIEALD' })
 
     fireEvent.press(screen.getByText('Répondre au questionnaire'))
 
-    expect(openUrl).toHaveBeenCalledWith('https://fr.wikipedia.org/wiki/FIEALD', undefined, true)
+    await waitFor(() => {
+      expect(openUrl).toHaveBeenNthCalledWith(
+        1,
+        'https://fr.wikipedia.org/wiki/FIEALD',
+        undefined,
+        true
+      )
+    })
   })
 
   it('should call hideModal function when pressing close icon', () => {
-    renderSurveyModal()
+    renderSurveyModal({ surveyUrl: 'https://fr.wikipedia.org/wiki/FIEALD' })
     const rightIcon = screen.getByTestId('Fermer la modale')
     fireEvent.press(rightIcon)
 
     expect(hideModalMock).toHaveBeenCalledTimes(1)
   })
+
+  it('should display "Répondre au questionnaire" button when survey url defined', () => {
+    renderSurveyModal({ surveyUrl: 'https://fr.wikipedia.org/wiki/FIEALD' })
+
+    expect(screen.getByText('Répondre au questionnaire')).toBeOnTheScreen()
+  })
+
+  it('should not display "Répondre au questionnaire" button when survey url not defined', () => {
+    renderSurveyModal({})
+
+    expect(screen.queryByText('Répondre au questionnaire')).not.toBeOnTheScreen()
+  })
+
+  it('should call hideModal function  when pressing "Répondre au questionnaire" button', async () => {
+    renderSurveyModal({ surveyUrl: 'https://fr.wikipedia.org/wiki/FIEALD' })
+
+    fireEvent.press(screen.getByText('Répondre au questionnaire'))
+
+    await waitFor(() => {
+      expect(hideModalMock).toHaveBeenCalledTimes(1)
+    })
+  })
 })
 
-const renderSurveyModal = () => {
+type RenderSurveyModalType = Pick<ComponentProps<typeof SurveyModal>, 'surveyUrl'>
+
+const renderSurveyModal = ({ surveyUrl }: RenderSurveyModalType) => {
   return render(
     <SurveyModal
       visible
       hideModal={hideModalMock}
-      surveyUrl="https://fr.wikipedia.org/wiki/FIEALD"
+      surveyUrl={surveyUrl}
       title="Wikipedia"
       Icon={BicolorCircledClock}
     />
