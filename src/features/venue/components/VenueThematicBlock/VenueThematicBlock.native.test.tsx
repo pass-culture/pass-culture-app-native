d524146c0779feeaa70c3c29dc2decdfd435beea
marineshaw <marines@bam.tech>
2024-05-02 15:53:38 +0200

    :sparkles: (Subscription) Create VEnueThematicBlock with all modals and basic logic

---
+++ src/features/venue/components/VenueThematicBlock/VenueThematicBlock.native.test.tsx
@@ -0,0 +1,136 @@
+import React from 'react'
+
+import { VenueTypeCodeKey } from 'api/gen'
+import { useAuthContext } from 'features/auth/context/AuthContext'
+import { SubscriptionTheme } from 'features/subscription/types'
+import { VenueThematicBlock } from 'features/venue/components/VenueThematicBlock/VenueThematicBlock'
+import { venueResponseSnap } from 'features/venue/fixtures/venueResponseSnap'
+import { beneficiaryUser } from 'fixtures/user'
+import { mockServer } from 'tests/mswServer'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { fireEvent, render, screen, waitFor } from 'tests/utils'
+import { SNACK_BAR_TIME_OUT } from 'ui/components/snackBar/SnackBarContext'
+
+jest.mock('features/auth/context/AuthContext')
+const mockUseAuthContext = useAuthContext as jest.MockedFunction<typeof useAuthContext>
+const baseAuthContext = {
+  isLoggedIn: true,
+  setIsLoggedIn: jest.fn(),
+  user: beneficiaryUser,
+  refetchUser: jest.fn(),
+  isUserLoading: false,
+}
+mockUseAuthContext.mockReturnValue(baseAuthContext)
+
+const venueFixture = { ...venueResponseSnap, venueTypeCode: VenueTypeCodeKey.MOVIE }
+
+const mockShowSuccessSnackBar = jest.fn()
+const mockShowErrorSnackBar = jest.fn()
+jest.mock('ui/components/snackBar/SnackBarContext', () => ({
+  useSnackBarContext: () => ({
+    showSuccessSnackBar: mockShowSuccessSnackBar,
+    showErrorSnackBar: mockShowErrorSnackBar,
+  }),
+}))
+
+const alreadySubscribedUser = {
+  ...beneficiaryUser,
+  subscriptions: {
+    marketingEmail: true,
+    marketingPush: true,
+    subscribedThemes: [SubscriptionTheme.CINEMA],
+  },
+}
+
+describe('<VenueThematicBlock/>', () => {
+  it('should render null if venue has no thematic', async () => {
+    const venue = { ...venueFixture, venueTypeCode: VenueTypeCodeKey.ADMINISTRATIVE }
+    render(reactQueryProviderHOC(<VenueThematicBlock venue={venue} />))
+
+    await waitFor(() => {
+      expect(screen.toJSON()).toBeNull()
+    })
+  })
+
+  it('should render cinema block when venue is a movie theater', async () => {
+    render(reactQueryProviderHOC(<VenueThematicBlock venue={venueFixture} />))
+
+    expect(await screen.findByText('Fan de cinéma ?')).toBeOnTheScreen()
+  })
+
+  describe('when the user presses SubscribeButton', () => {
+    it('should notifies when subscription successes', async () => {
+      mockServer.postApi('/v1/profile', {})
+
+      render(reactQueryProviderHOC(<VenueThematicBlock venue={venueFixture} />))
+
+      fireEvent.press(screen.getByText('Suivre le thème'))
+
+      await waitFor(() => {
+        expect(mockShowSuccessSnackBar).toHaveBeenCalledWith({
+          message: 'Tu suis le thème “Cinéma”\u00a0! Tu peux gérer tes alertes depuis ton profil.',
+          timeout: SNACK_BAR_TIME_OUT,
+        })
+      })
+    })
+
+    it('should notifies when subscription fails', async () => {
+      mockServer.postApi('/v1/profile', {
+        responseOptions: { statusCode: 400, data: {} },
+      })
+
+      render(reactQueryProviderHOC(<VenueThematicBlock venue={venueFixture} />))
+
+      fireEvent.press(screen.getByText('Suivre le thème'))
+
+      await waitFor(() => {
+        expect(mockShowErrorSnackBar).toHaveBeenCalledWith({
+          message: 'Une erreur est survenue, veuillez réessayer',
+          timeout: SNACK_BAR_TIME_OUT,
+        })
+      })
+    })
+
+    it('should show logged out modal when user is not logged in', async () => {
+      const loggedOutAuthContext = { ...baseAuthContext, isLoggedIn: false, user: undefined }
+      mockUseAuthContext.mockReturnValueOnce(loggedOutAuthContext)
+      render(reactQueryProviderHOC(<VenueThematicBlock venue={venueFixture} />))
+
+      fireEvent.press(screen.getByText('Suivre le thème'))
+
+      expect(await screen.findByText('Identifie-toi pour t’abonner à un thème')).toBeOnTheScreen()
+    })
+
+    it('should show notifications modal when user has no notifications activated', async () => {
+      const userWithNoNotifications = {
+        ...beneficiaryUser,
+        subscriptions: { marketingEmail: false, marketingPush: false, subscribedThemes: [] },
+      }
+      mockUseAuthContext.mockReturnValueOnce({ ...baseAuthContext, user: userWithNoNotifications })
+      render(reactQueryProviderHOC(<VenueThematicBlock venue={venueFixture} />))
+
+      fireEvent.press(screen.getByText('Suivre le thème'))
+
+      expect(await screen.findByText('Autoriser l’envoi d’e-mails')).toBeOnTheScreen()
+    })
+
+    it('should show unsubscribing confirmation modal when user is already subscribed', async () => {
+      const alreadySubscribedUser = {
+        ...beneficiaryUser,
+        subscriptions: {
+          marketingEmail: true,
+          marketingPush: true,
+          subscribedThemes: [SubscriptionTheme.CINEMA],
+        },
+      }
+      mockUseAuthContext.mockReturnValueOnce({ ...baseAuthContext, user: alreadySubscribedUser })
+      render(reactQueryProviderHOC(<VenueThematicBlock venue={venueFixture} />))
+
+      fireEvent.press(screen.getByText('Thème suivi'))
+
+      expect(
+        await screen.findByText('Es-tu sûr de ne plus vouloir suivre ce thème ?')
+      ).toBeOnTheScreen()
+    })
+  })
+})
---
+++ src/features/venue/components/VenueThematicBlock/VenueThematicBlock.tsx
@@ -0,0 +1,104 @@
+import React, { FunctionComponent } from 'react'
+
+import { VenueResponse } from 'api/gen'
+import { useAuthContext } from 'features/auth/context/AuthContext'
+import { UnsubscribingConfirmationModal } from 'features/subscription/components/modals/UnsubscribingConfirmationModal'
+import { ThematicSubscriptionBlock } from 'features/subscription/components/ThematicSubscriptionBlock'
+import { mapSubscriptionThemeToName } from 'features/subscription/helpers/mapSubscriptionThemeToName'
+import { useThematicSubscription } from 'features/subscription/helpers/useThematicSubscription'
+import { NotificationsLoggedOutModal } from 'features/subscription/NotificationsLoggedOutModal'
+import { NotificationsSettingsModal } from 'features/subscription/NotificationsSettingsModal'
+import { SubscriptionTheme } from 'features/subscription/types'
+import { mapVenueTypeToCategory } from 'features/venue/helpers/mapVenueTypeToCategory'
+import { useModal } from 'ui/components/modals/useModal'
+import { SNACK_BAR_TIME_OUT, useSnackBarContext } from 'ui/components/snackBar/SnackBarContext'
+
+interface Props {
+  venue: VenueResponse
+}
+
+export const VenueThematicBlock: FunctionComponent<Props> = ({ venue }: Props) => {
+  const thematic = venue.venueTypeCode ? mapVenueTypeToCategory[venue.venueTypeCode] : null
+  const { user, isLoggedIn } = useAuthContext()
+
+  const { showSuccessSnackBar } = useSnackBarContext()
+  const {
+    visible: visibleLoggedOutModal,
+    showModal: showLoggedOutModal,
+    hideModal: hideLoggedOutModal,
+  } = useModal(false)
+  const {
+    visible: visibleNotificationsSettingsModal,
+    showModal: showNotificationsSettingsModal,
+    hideModal: hideNotificationsSettingsModal,
+  } = useModal(false)
+  const {
+    visible: isUnsubscribingModalVisible,
+    showModal: showUnsubscribingModal,
+    hideModal: hideUnsubscribingModal,
+  } = useModal(false)
+
+  const onUpdateSubscriptionSuccess = async (thematic: SubscriptionTheme) => {
+    showSuccessSnackBar({
+      message: `Tu suis le thème “${mapSubscriptionThemeToName[thematic]}”\u00a0! Tu peux gérer tes alertes depuis ton profil.`,
+      timeout: SNACK_BAR_TIME_OUT,
+    })
+  }
+
+  const {
+    isSubscribeButtonActive,
+    isAtLeastOneNotificationTypeActivated,
+    updateSubscription,
+    updateSettings,
+  } = useThematicSubscription({
+    user,
+    onUpdateSubscriptionSuccess,
+    thematic,
+    venueId: venue.id.toString(),
+  })
+
+  const onSubscribePress = () => {
+    if (!isLoggedIn) {
+      showLoggedOutModal()
+    } else if (!isAtLeastOneNotificationTypeActivated) {
+      showNotificationsSettingsModal()
+    } else if (isSubscribeButtonActive) {
+      showUnsubscribingModal()
+    } else {
+      updateSubscription()
+    }
+  }
+
+  const onUnsubscribeConfirmationPress = () => {
+    updateSubscription()
+    hideUnsubscribingModal()
+  }
+
+  if (!thematic) return null
+
+  return (
+    <React.Fragment>
+      <ThematicSubscriptionBlock
+        thematic={thematic}
+        isSubscribeButtonActive={isSubscribeButtonActive}
+        onSubscribePress={onSubscribePress}
+      />
+      <NotificationsLoggedOutModal
+        visible={visibleLoggedOutModal}
+        dismissModal={hideLoggedOutModal}
+      />
+      <NotificationsSettingsModal
+        visible={visibleNotificationsSettingsModal}
+        dismissModal={hideNotificationsSettingsModal}
+        theme={thematic}
+        onPressSaveChanges={updateSettings}
+      />
+      <UnsubscribingConfirmationModal
+        visible={isUnsubscribingModalVisible}
+        dismissModal={hideUnsubscribingModal}
+        theme={thematic}
+        onUnsubscribePress={onUnsubscribeConfirmationPress}
+      />
+    </React.Fragment>
+  )
+}
