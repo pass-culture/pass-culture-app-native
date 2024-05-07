af52bd557294bea4b7bce21bb04323c7f723ec93
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-05-07 10:47:59 +0200

    (PC-29438) fix(OnboardingSubscription): Removed accessibleRadioProps

--- src/features/subscription/components/buttons/SubscriptionThematicButton.tsx
+++ src/features/subscription/components/buttons/SubscriptionThematicButton.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { SubscriptionThematicIllustration } from 'features/subscription/components/SubscriptionThematicIllustration'
 import { mapSubscriptionThemeToName } from 'features/subscription/helpers/mapSubscriptionThemeToName'
 import { SubscriptionTheme } from 'features/subscription/types'
-import { accessibleRadioProps } from 'shared/accessibilityProps/accessibleRadioProps'
 import { CheckboxBlock } from 'ui/components/CheckboxBlock/CheckboxBlock'
 
 interface Props {
@@ -15,7 +14,6 @@ interface Props {
 export const SubscriptionThematicButton = ({ thematic, checked, onPress }: Props) => {
   return (
     <CheckboxBlock
-      {...accessibleRadioProps({ label: mapSubscriptionThemeToName[thematic], checked })}
       checked={checked}
       label={mapSubscriptionThemeToName[thematic]}
       onPress={onPress}
