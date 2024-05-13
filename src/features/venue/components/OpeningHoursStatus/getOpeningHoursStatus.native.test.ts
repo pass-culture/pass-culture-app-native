ed6fa1f086f910d0aa28a1e1c150289c1b03d71a
Maxime Le Duc <maxime.leduc@passculture.app>
2024-05-13 10:44:30 +0200

    GetOpeningHoursStatus viewmodel

---
+++ src/features/venue/components/OpeningHoursStatus/getOpeningHoursStatus.native.test.ts
@@ -0,0 +1,213 @@
+import { getOpeningHoursStatus } from './getOpeningHoursStatus'
+
+describe('OpeningHoursStatusViewModel', () => {
+  it.each([
+    {
+      openingHours: 'MONDAY',
+      currentDate: new Date('2024-05-13T09:00:00'),
+    },
+    {
+      openingHours: 'TUESDAY',
+      currentDate: new Date('2024-05-14T09:00:00'),
+    },
+    {
+      openingHours: 'WEDNESDAY',
+      currentDate: new Date('2024-05-15T09:00:00'),
+    },
+    {
+      openingHours: 'THURSDAY',
+      currentDate: new Date('2024-05-16T09:00:00'),
+    },
+    {
+      openingHours: 'FRIDAY',
+      currentDate: new Date('2024-05-17T09:00:00'),
+    },
+    {
+      openingHours: 'SATURDAY',
+      currentDate: new Date('2024-05-18T09:00:00'),
+    },
+    {
+      openingHours: 'SUNDAY',
+      currentDate: new Date('2024-05-19T09:00:00'),
+    },
+  ])('should use current day $openingHours', ({ openingHours, currentDate }) => {
+    const viewModel = getOpeningHoursStatus({
+      openingHours: {
+        [openingHours]: [{ open: '09:00', close: '19:00' }],
+      },
+      currentDate,
+    })
+
+    expect(viewModel.text).toEqual('Ouvert jusqu’à 19h')
+  })
+
+  describe('Venue is open', () => {
+    const currentDate = new Date('2024-05-13T09:00:00')
+
+    it.each([
+      {
+        openingHours: {
+          MONDAY: [{ open: '09:00', close: '19:00' }],
+        },
+        currentDate: new Date('2024-05-13T09:00:00'),
+      },
+      {
+        openingHours: {
+          MONDAY: [
+            { open: '09:00', close: '12:00' },
+            { open: '14:00', close: '19:00' },
+          ],
+        },
+        currentDate: new Date('2024-05-13T16:00:00'),
+      },
+    ])('should be in open state', ({ openingHours, currentDate }) => {
+      const viewModel = getOpeningHoursStatus({ openingHours, currentDate })
+
+      expect(viewModel.state).toEqual('open')
+    })
+
+    it.each([
+      {
+        openingHours: {
+          MONDAY: [{ open: '09:00', close: '19:00' }],
+        },
+        expected: 'Ouvert jusqu’à 19h',
+      },
+      {
+        openingHours: {
+          MONDAY: [{ open: '09:00', close: '20:00' }],
+        },
+        expected: 'Ouvert jusqu’à 20h',
+      },
+      {
+        openingHours: {
+          MONDAY: [{ open: '09:00', close: '19:30' }],
+        },
+        expected: 'Ouvert jusqu’à 19h30',
+      },
+    ])('should have correct text $expected', ({ openingHours, expected }) => {
+      const viewModel = getOpeningHoursStatus({ openingHours, currentDate })
+
+      expect(viewModel.text).toEqual(expected)
+    })
+  })
+
+  describe('Venue open soon', () => {
+    it('should be in open-soon state', () => {
+      const openingHours = {
+        MONDAY: [{ open: '09:00', close: '19:00' }],
+      }
+      const currentDate = new Date('2024-05-13T08:00:00')
+      const viewModel = getOpeningHoursStatus({ openingHours, currentDate })
+
+      expect(viewModel.state).toEqual('open-soon')
+    })
+
+    it.each([
+      {
+        openingHours: {
+          MONDAY: [{ open: '09:00', close: '19:00' }],
+        },
+        currentDate: new Date('2024-05-13T08:00:00'),
+        expected: 'Ouvre bientot - 9h',
+      },
+      {
+        openingHours: {
+          MONDAY: [{ open: '11:00', close: '19:00' }],
+        },
+        currentDate: new Date('2024-05-13T10:30:00'),
+        expected: 'Ouvre bientot - 11h',
+      },
+      {
+        openingHours: {
+          MONDAY: [{ open: '11:30', close: '19:00' }],
+        },
+        currentDate: new Date('2024-05-13T10:30:00'),
+        expected: 'Ouvre bientot - 11h30',
+      },
+    ])('should have correct text $expected', ({ openingHours, currentDate, expected }) => {
+      const viewModel = getOpeningHoursStatus({ openingHours, currentDate })
+
+      expect(viewModel.text).toEqual(expected)
+    })
+  })
+
+  describe('Venue close soon', () => {
+    it('should be in close-soon state', () => {
+      const openingHours = {
+        MONDAY: [{ open: '09:00', close: '19:00' }],
+      }
+      const currentDate = new Date('2024-05-13T18:00:00')
+      const viewModel = getOpeningHoursStatus({ openingHours, currentDate })
+
+      expect(viewModel.state).toEqual('close-soon')
+    })
+
+    it.each([
+      {
+        openingHours: {
+          MONDAY: [{ open: '09:00', close: '19:00' }],
+        },
+        currentDate: new Date('2024-05-13T18:00:00'),
+        expected: 'Ferme bientot - 19h',
+      },
+      {
+        openingHours: {
+          MONDAY: [{ open: '09:00', close: '20:00' }],
+        },
+        currentDate: new Date('2024-05-13T19:00:00'),
+        expected: 'Ferme bientot - 20h',
+      },
+      {
+        openingHours: {
+          MONDAY: [{ open: '09:00', close: '20:30' }],
+        },
+        currentDate: new Date('2024-05-13T20:00:00'),
+        expected: 'Ferme bientot - 20h30',
+      },
+    ])('should have correct text $expected', ({ openingHours, currentDate, expected }) => {
+      const viewModel = getOpeningHoursStatus({ openingHours, currentDate })
+
+      expect(viewModel.text).toEqual(expected)
+    })
+  })
+
+  describe('Venue is closed', () => {
+    it.each([
+      {
+        openingHours: {
+          MONDAY: [{ open: '09:00', close: '19:00' }],
+        },
+        currentDate: new Date('2024-05-13T20:00:00'),
+      },
+      {
+        openingHours: {
+          MONDAY: [
+            { open: '09:00', close: '12:00' },
+            { open: '14:00', close: '19:00' },
+          ],
+        },
+        currentDate: new Date('2024-05-13T20:00:00'),
+      },
+      {
+        openingHours: {
+          MONDAY: undefined,
+        },
+        currentDate: new Date('2024-05-13T20:00:00'),
+      },
+    ])('should be in close state', ({ openingHours, currentDate }) => {
+      const viewModel = getOpeningHoursStatus({ openingHours, currentDate })
+
+      expect(viewModel.state).toEqual('close')
+    })
+
+    it('should have correct text', () => {
+      const viewModel = getOpeningHoursStatus({
+        openingHours: { MONDAY: undefined },
+        currentDate: new Date('2024-05-13T20:00:00'),
+      })
+
+      expect(viewModel.text).toEqual('Fermé')
+    })
+  })
+})
---
+++ src/features/venue/components/OpeningHoursStatus/getOpeningHoursStatus.ts
@@ -0,0 +1,132 @@
+import { addHours, set, isWithinInterval } from 'date-fns'
+
+export type OpeningHours = Partial<{
+  MONDAY: OpeningHour
+  TUESDAY: OpeningHour
+  WEDNESDAY: OpeningHour
+  THURSDAY: OpeningHour
+  FRIDAY: OpeningHour
+  SATURDAY: OpeningHour
+  SUNDAY: OpeningHour
+}>
+
+type OpeningHour = {
+  open: string
+  close: string
+}[]
+
+type OpeningHoursStatusViewmodelParams = {
+  openingHours: OpeningHours
+  currentDate: Date
+}
+
+export type OpeningHoursStatusViewmodelState = 'open' | 'open-soon' | 'close-soon' | 'close'
+export type OpeningHoursStatusViewmodel = {
+  state: OpeningHoursStatusViewmodelState
+  text: string
+}
+
+export const getOpeningHoursStatus = ({
+  openingHours,
+  currentDate,
+}: OpeningHoursStatusViewmodelParams): OpeningHoursStatusViewmodel => {
+  const currentOpeningHours = openingHours[getHoursFromCurrentDate(currentDate)] || []
+  const periods = currentOpeningHours.map(({ open, close }) => {
+    const openAt = getDateFromOpeningHour(currentDate, open)
+    const closeAt = getDateFromOpeningHour(currentDate, close)
+    return createPeriod(openAt, closeAt)
+  })
+  return getStateFromPeriods(periods, currentDate)
+}
+
+const getStateFromPeriods = (
+  periods: Period[],
+  currentDate: Date,
+  index = 0
+): OpeningHoursStatusViewmodel => {
+  const period = periods[index]
+  if (!period) {
+    return closeState()
+  }
+
+  if (period.isPassed(currentDate)) {
+    return getStateFromPeriods(periods, currentDate, index + 1)
+  }
+
+  if (period.isOpen(currentDate)) {
+    if (period.isClosingSoon(currentDate)) {
+      return closeSoonState(period.closeAt)
+    }
+    return openState(period.closeAt)
+  }
+
+  if (period.isOpenningSoon(currentDate)) {
+    return openSoonState(period.openAt)
+  }
+
+  return getStateFromPeriods(periods, currentDate, index + 1)
+}
+
+type Period = ReturnType<typeof createPeriod>
+
+const createPeriod = (openAt: Date, closeAt: Date) => ({
+  closeAt,
+  openAt,
+  isOpenningSoon: (currentDate: Date) => addHours(currentDate, 1) >= openAt,
+  isClosingSoon: (currentDate: Date) => addHours(currentDate, 1) >= closeAt,
+  isOpen: (currentDate: Date) => isWithinInterval(currentDate, { start: openAt, end: closeAt }),
+  isPassed: (currentDate: Date) => currentDate > closeAt,
+})
+
+const dayToOpeningHours: Record<number, keyof OpeningHours> = {
+  0: 'SUNDAY',
+  1: 'MONDAY',
+  2: 'TUESDAY',
+  3: 'WEDNESDAY',
+  4: 'THURSDAY',
+  5: 'FRIDAY',
+  6: 'SATURDAY',
+}
+
+const getHoursFromCurrentDate = (currentDate: Date): keyof OpeningHours => {
+  const day = currentDate.getDay()
+  const openingHours = dayToOpeningHours[day]
+  if (!openingHours) {
+    throw new Error('Invalid day')
+  }
+  return openingHours
+}
+
+const getDateFromOpeningHour = (currentDate: Date, openingHour: string): Date => {
+  const [hour, minute] = openingHour.split(':').map((value) => parseInt(value))
+  return set(currentDate, {
+    hours: hour,
+    minutes: minute,
+  })
+}
+
+const closeState = (): OpeningHoursStatusViewmodel => ({
+  state: 'close',
+  text: 'Fermé',
+})
+
+const openState = (openDate: Date): OpeningHoursStatusViewmodel => ({
+  state: 'open',
+  text: `Ouvert jusqu’à ${formatDate(openDate)}`,
+})
+
+const openSoonState = (openDate: Date): OpeningHoursStatusViewmodel => ({
+  state: 'open-soon',
+  text: `Ouvre bientot - ${formatDate(openDate)}`,
+})
+
+const closeSoonState = (closeDate: Date): OpeningHoursStatusViewmodel => ({
+  state: 'close-soon',
+  text: `Ferme bientot - ${formatDate(closeDate)}`,
+})
+
+const formatDate = (date: Date): string => {
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  return `${hours}h${minutes ? minutes : ''}`
+}
