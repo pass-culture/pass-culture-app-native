23fe5a67e14dd377bc3a1c6f23fb29d461365262
marineshaw <marines@bam.tech>
2024-05-13 16:15:22 +0200

    :recycle: (FAQWebView) Remove use of PageHeaderSecondary

--- src/features/culturalSurvey/pages/FAQWebview.tsx
+++ src/features/culturalSurvey/pages/FAQWebview.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { WebView } from 'react-native-webview'
+import styled from 'styled-components/native'
 
 import { FAQ_LINK_USER_DATA } from 'features/culturalSurvey/constants'
-import { PageHeaderSecondary } from 'ui/components/headers/PageHeaderSecondary'
+import {
+  PageHeaderWithoutPlaceholder,
+  useGetHeaderHeight,
+} from 'ui/components/headers/PageHeaderWithoutPlaceholder'
 
 export const FAQWebview: React.FC = () => {
+  const headerHeight = useGetHeaderHeight()
   return (
     <React.Fragment>
-      <PageHeaderSecondary title="Traitement des données utilisateurs" />
+      <PageHeaderWithoutPlaceholder title="Traitement des données utilisateurs" />
+      <Placeholder height={headerHeight} />
       <WebView source={{ uri: FAQ_LINK_USER_DATA }} testID="FAQ-webview" />
     </React.Fragment>
   )
 }
+
+const Placeholder = styled.View<{ height: number }>(({ height }) => ({
+  height,
+}))
