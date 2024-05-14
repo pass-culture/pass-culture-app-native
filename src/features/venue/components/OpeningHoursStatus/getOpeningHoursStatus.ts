b50efd5bd89084af6734502b2127bd365a34f570
Maxime Le Duc <maxime.leduc@passculture.app>
2024-05-14 12:10:11 +0200

    Remove ternary for minutes assignement

--- src/features/venue/components/OpeningHoursStatus/getOpeningHoursStatus.ts
+++ src/features/venue/components/OpeningHoursStatus/getOpeningHoursStatus.ts
@@ -107,5 +107,5 @@ const getDateFromOpeningHour = (currentDate: Date, openingHour: string): Date =>
 const formatDate = (date: Date): string => {
   const hours = date.getHours()
   const minutes = date.getMinutes()
-  return `${hours}h${minutes ? minutes : ''}`
+  return `${hours}h${minutes || ''}`
 }
