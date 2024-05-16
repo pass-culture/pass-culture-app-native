fa64724d076083b8843cf1c04cdd03823cb4f03d
YassinL <yassin.leclercq1@gmail.com>
2024-05-16 16:25:52 +0200

    (PC-29761) feat(FF): add new feature flag wipAppV2VenueList

--- src/libs/firebase/firestore/featureFlags/getFeatureFlag.native.test.ts
+++ src/libs/firebase/firestore/featureFlags/getFeatureFlag.native.test.ts
@@ -11,6 +11,7 @@ describe('getFeatureFlag', () => {
   it.each([
     RemoteStoreFeatureFlags.DISABLE_OLD_CHANGE_EMAIL,
     RemoteStoreFeatureFlags.FAKE_DOOR_ARTIST,
+    RemoteStoreFeatureFlags.WIP_APP_V2_VENUE_LIST,
     RemoteStoreFeatureFlags.WIP_CINEMA_OFFER_VENUE_BLOCK,
     RemoteStoreFeatureFlags.WIP_DISABLE_STORE_REVIEW,
     RemoteStoreFeatureFlags.WIP_DISPLAY_SEARCH_NB_FACET_RESULTS,
