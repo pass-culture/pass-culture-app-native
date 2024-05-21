f7076f6e6ad2f686d6626cb48ac9f8b3bbcb5fbe
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-05-21 09:43:47 +0200

    (PC-29736) fix(tabbar): Use tabbar height from theme

---
+++ src/ui/hooks/useGetFooterHeight/useGetFooterHeight.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import * as useFeatureFlagAPI from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { ThemeProvider } from 'libs/styled'
+import { computedTheme } from 'tests/computedTheme'
+import { renderHook } from 'tests/utils'
+import { useGetFooterHeight } from 'ui/hooks/useGetFooterHeight/useGetFooterHeight'
+
+jest.spyOn(useFeatureFlagAPI, 'useFeatureFlag').mockReturnValue(false)
+
+describe('useGetFooterHeight', () => {
+  it('should return correct footer height', () => {
+    const footerHeight = 40
+    const { result } = renderHook(() => useGetFooterHeight(footerHeight), {
+      wrapper: ({ children }) => <ThemeProvider theme={computedTheme}>{children}</ThemeProvider>,
+    })
+
+    expect(result.current).toEqual(49)
+  })
+})
