2d67a7a94027a7d8fd9f1feb73d2510f9c6d5f19
bruno ebstein <bruno.ebstein@passculture.app>
2024-05-21 14:31:01 +0200

    install DevBox with Nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.mkShellNoCC {
+          packages = [
+            pkgs.nix # ensure to have always the same version
+            pkgs.devbox
+          ];
+        };
+    });
+}
