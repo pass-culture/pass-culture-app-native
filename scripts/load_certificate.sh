d6be1c713da7a5c5b13edbc3fa0887b0ae0a44e2
bruno ebstein <bruno.ebstein@passculture.app>
2024-05-22 16:59:12 +0200

    add certificates for Nix

--- scripts/load_certificate.sh
+++ scripts/load_certificate.sh
@@ -4,4 +4,5 @@ set -o errexit -o nounset -o pipefail
 NODE_EXTRA_CA_CERTS='/Library/Application Support/Netskope/STAgent/data/nscacert.pem'
 if [ -f "$NODE_EXTRA_CA_CERTS" ]; then
 	export NODE_EXTRA_CA_CERTS
+	export NIX_SSL_CERT_FILE="$NODE_EXTRA_CA_CERTS"
 fi
