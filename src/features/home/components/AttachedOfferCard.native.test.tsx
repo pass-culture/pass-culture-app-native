c79968d2b03c129fcd531c67fdbdfad877968100
Inès Mouandjo Lobé <ines.mouandjolobe@passculture.app>
2024-05-27 16:08:56 +0200

    Update tests

---
+++ src/features/home/components/AttachedOfferCard.native.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { CategoryIdEnum, SubcategoryIdEnum } from 'api/gen'
+import { AttachedOfferCard } from 'features/home/components/AttachedOfferCard'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render, screen } from 'tests/utils'
+
+describe('<AttachedOfferCard>', () => {
+  it('matches snapshot if all proprieties are defined', () => {
+    render(
+      reactQueryProviderHOC(
+        <AttachedOfferCard
+          subcategoryId={SubcategoryIdEnum.CONCERT}
+          withRightArrow
+          imageUrl="https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/9MPGW"
+          price="Gratuit"
+          categoryId={CategoryIdEnum.MUSIQUE_LIVE}
+          title="Soirée super trop drôle de fou malade&nbsp;!"
+          date="Du 12/06 au 24/06"
+        />
+      )
+    )
+
+    expect(screen).toMatchSnapshot()
+  })
+
+  it('matches snapshot if there is no distance and arrow', () => {
+    render(
+      reactQueryProviderHOC(
+        <AttachedOfferCard
+          subcategoryId={SubcategoryIdEnum.CONCERT}
+          imageUrl="https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/9MPGW"
+          price="Gratuit"
+          categoryId={CategoryIdEnum.MUSIQUE_LIVE}
+          title="Soirée super trop drôle de fou malade&nbsp;!"
+        />
+      )
+    )
+
+    expect(screen).toMatchSnapshot()
+  })
+})
