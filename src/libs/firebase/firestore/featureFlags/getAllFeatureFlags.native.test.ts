6f8902a95a416a4516f3a53397aae8ae63abe4a7
Hugo Voisin <hugov@bam.tech>
2024-05-27 14:56:15 +0200

    test: add test for FF error

--- src/libs/firebase/firestore/featureFlags/getAllFeatureFlags.native.test.ts
+++ src/libs/firebase/firestore/featureFlags/getAllFeatureFlags.native.test.ts
@@ -1,15 +1,28 @@
 import { getAllFeatureFlags } from 'libs/firebase/firestore/featureFlags/getAllFeatureFlags'
+import { RemoteStoreCollections } from 'libs/firebase/firestore/types'
 import firestore from 'libs/firebase/shims/firestore'
-import { renderHook } from 'tests/utils'
+import { eventMonitoring } from 'libs/monitoring'
+import { waitFor } from 'tests/utils'
 
 jest.mock('@react-native-firebase/firestore')
 
 const { collection } = firestore()
+const { get } = collection(RemoteStoreCollections.FEATURE_FLAGS).doc()
+const mockGet = get as jest.Mock
 
 describe('getAllFeatureFlags', () => {
   it('should call the right firestore collection: featureFlags', () => {
-    renderHook(getAllFeatureFlags)
+    getAllFeatureFlags()
 
     expect(collection).toHaveBeenCalledWith('featureFlags')
+  })
+
+  it('should send log to Sentry when Firestore throws an error', async () => {
+    mockGet.mockRejectedValueOnce(new Error('error'))
+    getAllFeatureFlags()
+
+    await waitFor(() => {
+      expect(eventMonitoring.captureException).toHaveBeenCalledWith(new Error('error'))
+    })
   })
 })
