a95fc95532bf59aebc6496816319e7eb54757c07
Xavier Le Cunff <xavier.lecunff@passculture.app>
2024-05-31 09:55:36 +0200

    feat: improve feature flag component

--- src/shared/FeatureFlag/FeatureFlag.tsx
+++ src/shared/FeatureFlag/FeatureFlag.tsx
@@ -1,15 +1,67 @@
-import React from 'react'
+import React, { FC, PropsWithChildren, ReactNode } from 'react'
+import { View } from 'react-native'
 
 import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 
 type FeatureFlagProps = {
-  featureFlag: RemoteStoreFeatureFlags
-  children: React.JSX.Element
+  featureFlag: keyof typeof RemoteStoreFeatureFlags
+  children: ReactNode
 }
 
-export function FeatureFlag({ featureFlag, children }: FeatureFlagProps) {
-  const featureFlagEnabled = useFeatureFlag(featureFlag)
+type SubComponentProps = PropsWithChildren
 
-  return featureFlagEnabled ? children : null
+/**
+ * FeatureFlag component to conditionally render children based on the feature flag state.
+ *
+ * @param {keyof RemoteStoreFeatureFlags} featureFlag - The name of the feature flag to check.
+ * @param {ReactNode} children - The children components, typically `FeatureFlag.On` and `FeatureFlag.Off`.
+ * @example
+ * <FeatureFlag featureFlag={RemoteStoreFeatureFlags.WIP_APP_V2_CATEGORY_BLOCK}>
+ *   <FeatureFlag.On>
+ *     <View>
+ *       <Text>Feature is ON</Text>
+ *     </View>
+ *   </FeatureFlag.On>
+ *   <FeatureFlag.Off>
+ *     <View>
+ *       <Text>Feature is OFF</Text>
+ *     </View>
+ *   </FeatureFlag.Off>
+ * </FeatureFlag>
+ */
+const FeatureFlagComponent: FC<FeatureFlagProps> & {
+  On: FC<SubComponentProps>
+  Off: FC<SubComponentProps>
+} = ({ featureFlag, children }) => {
+  const isEnabled = useFeatureFlag(RemoteStoreFeatureFlags[featureFlag])
+
+  return (
+    <View>
+      {React.Children.map(children, (child) => {
+        if (!React.isValidElement(child)) {
+          return null
+        }
+
+        if (child.type === FeatureFlagComponent.On && isEnabled) {
+          return child
+        }
+
+        if (child.type === FeatureFlagComponent.Off && !isEnabled) {
+          return child
+        }
+
+        return null
+      })}
+    </View>
+  )
 }
+
+const On: FC<SubComponentProps> = ({ children }) => <View>{children}</View>
+
+const Off: FC<SubComponentProps> = ({ children }) => <View>{children}</View>
+
+FeatureFlagComponent.On = On
+FeatureFlagComponent.Off = Off
+
+export const FeatureFlag = FeatureFlagComponent
