c47bbf66d997dfd59a1127beafd3156e06fc781b
LucasBeneston <benestonlucas@hotmail.fr>
2024-06-03 08:39:09 +0200

    (PC-29958) feat(home): update BlurryWrapper

---
+++ src/ui/components/BlurryWrapper/BlurAmount.ts
@@ -0,0 +1,4 @@
+export enum BlurAmount {
+  LIGHT = 5,
+  INTENSE = 15,
+}
--- src/ui/components/BlurryWrapper/BlurryWrapper.native.test.tsx
+++ src/ui/components/BlurryWrapper/BlurryWrapper.native.test.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
 
 import { render, screen } from 'tests/utils'
+import { BlurAmount } from 'ui/components/BlurryWrapper/BlurAmount'
 import { BlurryWrapper } from 'ui/components/BlurryWrapper/BlurryWrapper'
 
 describe('<BlurryWrapper />', () => {
-  it('should display correctly the blur', () => {
+  it('should apply default blur amount when no prop is provided', () => {
     render(<BlurryWrapper />)
 
-    expect(screen.getByTestId('blurry-wrapper')).toHaveStyle({ backgroundColor: 'transparent' })
+    const blurryWrapper = screen.getByTestId('blurry-wrapper')
+
+    expect(blurryWrapper.props.blurAmount).toEqual(5)
+  })
+
+  it('should apply light blur amount when blurAmount is set to LIGHT', () => {
+    render(<BlurryWrapper blurAmount={BlurAmount.LIGHT} />)
+
+    const blurryWrapper = screen.getByTestId('blurry-wrapper')
+
+    expect(blurryWrapper.props.blurAmount).toEqual(5)
+  })
+
+  it('should apply intense blur amount when blurAmount is set to INTENSE', () => {
+    render(<BlurryWrapper blurAmount={BlurAmount.INTENSE} />)
+
+    const blurryWrapper = screen.getByTestId('blurry-wrapper')
+
+    expect(blurryWrapper.props.blurAmount).toEqual(15)
   })
 })
