26ae1d3ff509a0d8a904415c47505b33f0f61a58
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-06-04 10:58:30 +0200

    (PC-30076) refactor(ui): use direclty SystemBanner in GeolocationBanner

--- src/features/home/components/banners/GeolocationBanner.tsx
+++ src/features/home/components/banners/GeolocationBanner.tsx
@@ -1,8 +1,11 @@
 import React, { FunctionComponent, useCallback } from 'react'
 import styled from 'styled-components/native'
 
-import { useLocation, GeolocPermissionState } from 'libs/location'
+import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
+import { GeolocPermissionState, useLocation } from 'libs/location'
 import { GenericBanner } from 'ui/components/ModuleBanner/GenericBanner'
+import { SystemBanner } from 'ui/components/ModuleBanner/SystemBanner'
 import { Touchable } from 'ui/components/touchable/Touchable'
 import { BicolorEverywhere as Everywhere } from 'ui/svg/icons/BicolorEverywhere'
 import { Spacer, Typo } from 'ui/theme'
@@ -15,6 +18,7 @@ type Props = {
 
 export const GeolocationBanner: FunctionComponent<Props> = ({ title, subtitle, onPress }) => {
   const { permissionState, requestGeolocPermission, showGeolocPermissionModal } = useLocation()
+  const enableSystemBanner = useFeatureFlag(RemoteStoreFeatureFlags.WIP_APP_V2_SYSTEM_BLOCK)
 
   const onPressGeolocationBanner = useCallback(async () => {
     if (permissionState === GeolocPermissionState.NEVER_ASK_AGAIN) {
@@ -24,12 +28,21 @@ export const GeolocationBanner: FunctionComponent<Props> = ({ title, subtitle, o
     }
   }, [permissionState, requestGeolocPermission, showGeolocPermissionModal])
 
-  return (
+  return enableSystemBanner ? (
+    <SystemBanner
+      LeftIcon={<StyledLocationIcon />}
+      accessibilityLabel="Active ta géolocalisation"
+      subtitle={subtitle}
+      title={title}
+      // Possibility to use the onPress externally to avoid opening problems in modals
+      onPress={onPress ?? onPressGeolocationBanner}
+    />
+  ) : (
     <Touchable
       // Possibility to use the onPress externally to avoid opening problems in modals
       onPress={onPress ?? onPressGeolocationBanner}
       accessibilityLabel="Active ta géolocalisation">
-      <GenericBanner LeftIcon={<LocationIcon />}>
+      <GenericBanner LeftIcon={<LocationIcon />} testID="genericBanner">
         <Typo.ButtonText>{title}</Typo.ButtonText>
         <Spacer.Column numberOfSpaces={1} />
         <Typo.Body numberOfLines={2}>{subtitle}</Typo.Body>
@@ -40,4 +53,9 @@ export const GeolocationBanner: FunctionComponent<Props> = ({ title, subtitle, o
 
 const LocationIcon = styled(Everywhere).attrs(({ theme }) => ({
   size: theme.icons.sizes.standard,
+}))``
+
+const StyledLocationIcon = styled(Everywhere).attrs(({ theme }) => ({
+  size: theme.icons.sizes.standard,
+  color: theme.colors.secondaryLight200,
 }))``
