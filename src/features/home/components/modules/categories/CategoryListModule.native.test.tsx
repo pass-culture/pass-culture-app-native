fafed9d2b1e630b5ffcaac086f19a1690f0e33ed
xlecunff-pass <163895831+xlecunff-pass@users.noreply.github.com>
2024-06-04 09:45:38 +0200

    (PC-29811) feat(Home): change CategoryBlockV2 size (#6504)
    * feat: change CategoryBlockV2 size
    
    * fix: fix tests

--- src/features/home/components/modules/categories/CategoryListModule.native.test.tsx
+++ src/features/home/components/modules/categories/CategoryListModule.native.test.tsx
@@ -1,128 +1,136 @@
 import React from 'react'
 
 import { navigate } from '__mocks__/@react-navigation/native'
-import { CategoryListModule } from 'features/home/components/modules/categories/CategoryListModule'
+import {
+  CategoryListModule,
+  CategoryListProps,
+} from 'features/home/components/modules/categories/CategoryListModule'
 import { categoryBlockList } from 'features/home/fixtures/categoryBlockList.fixture'
 import { analytics } from 'libs/analytics'
 import { ContentTypes } from 'libs/contentful/types'
 import * as useFeatureFlag from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { act, fireEvent, render, screen } from 'tests/utils'
 
 const useFeatureFlagSpy = jest.spyOn(useFeatureFlag, 'useFeatureFlag')
 
 describe('CategoryListModule', () => {
-  beforeEach(() => useFeatureFlagSpy.mockReturnValue(false))
+  describe('with FF enableAppV2CategoryBlock false', () => {
+    beforeEach(() => useFeatureFlagSpy.mockReturnValue(false))
 
-  it('should call analytics when the module is displayed', () => {
-    render(
-      <CategoryListModule
-        id="123"
-        title="module"
-        categoryBlockList={categoryBlockList}
-        index={1}
-        homeEntryId="homeEntryId"
-      />
-    )
+    it('should call analytics when the module is displayed', () => {
+      render(
+        <CategoryListModule
+          id="123"
+          title="module"
+          categoryBlockList={categoryBlockList}
+          index={1}
+          homeEntryId="homeEntryId"
+        />
+      )
 
-    expect(analytics.logModuleDisplayedOnHomepage).toHaveBeenCalledWith(
-      '123',
-      ContentTypes.CATEGORY_LIST,
-      1,
-      'homeEntryId'
-    )
-  })
-
-  it('should call analytics when a categoryBlock is clicked', () => {
-    render(
-      <CategoryListModule
-        id="123"
-        title="module"
-        categoryBlockList={categoryBlockList}
-        index={1}
-        homeEntryId="homeEntryId"
-      />
-    )
-
-    const bloc = screen.getByText('Toto au cinéma')
-
-    fireEvent.press(bloc)
-
-    expect(analytics.logCategoryBlockClicked).toHaveBeenCalledWith({
-      moduleId: '2',
-      moduleListID: '123',
-      entryId: 'homeEntryId',
-      toEntryId: '6DCThxvbPFKAo04SVRZtwY',
+      expect(analytics.logModuleDisplayedOnHomepage).toHaveBeenCalledWith(
+        '123',
+        ContentTypes.CATEGORY_LIST,
+        1,
+        'homeEntryId'
+      )
     })
-  })
 
-  it('should navigate to thematic home when a categoryBlock is clicked', async () => {
-    render(
-      <CategoryListModule
-        id="123"
-        title="module"
-        categoryBlockList={categoryBlockList}
-        index={1}
-        homeEntryId="6DCThxvbPFKAo04SVRZtwY"
-      />
-    )
+    it('should call analytics when a categoryBlock is clicked', () => {
+      render(
+        <CategoryListModule
+          id="123"
+          title="module"
+          categoryBlockList={categoryBlockList}
+          index={1}
+          homeEntryId="homeEntryId"
+        />
+      )
 
-    const bloc = screen.getByText('Toto au cinéma')
+      const bloc = screen.getByText('Toto au cinéma')
 
-    await act(async () => {
       fireEvent.press(bloc)
+
+      expect(analytics.logCategoryBlockClicked).toHaveBeenCalledWith({
+        moduleId: '2',
+        moduleListID: '123',
+        entryId: 'homeEntryId',
+        toEntryId: '6DCThxvbPFKAo04SVRZtwY',
+      })
     })
 
-    expect(navigate).toHaveBeenCalledWith('ThematicHome', {
-      homeId: '6DCThxvbPFKAo04SVRZtwY',
-      from: 'category_block',
-      moduleId: '2',
-      moduleListId: '123',
+    it('should navigate to thematic home when a categoryBlock is clicked', async () => {
+      render(
+        <CategoryListModule
+          id="123"
+          title="module"
+          categoryBlockList={categoryBlockList}
+          index={1}
+          homeEntryId="6DCThxvbPFKAo04SVRZtwY"
+        />
+      )
+
+      const bloc = screen.getByText('Toto au cinéma')
+
+      await act(async () => {
+        fireEvent.press(bloc)
+      })
+
+      expect(navigate).toHaveBeenCalledWith('ThematicHome', {
+        homeId: '6DCThxvbPFKAo04SVRZtwY',
+        from: 'category_block',
+        moduleId: '2',
+        moduleListId: '123',
+      })
+    })
+
+    it('should NOT display circle nav buttons when feature is disabled', () => {
+      render(
+        <CategoryListModule
+          id="123"
+          title="module"
+          categoryBlockList={categoryBlockList}
+          index={1}
+          homeEntryId="6DCThxvbPFKAo04SVRZtwY"
+        />
+      )
+
+      expect(screen.queryByText('Ce week-end')).not.toBeOnTheScreen()
     })
   })
 
-  it('should render properly when enableAppV2CategoryBlock FF is activated', () => {
-    useFeatureFlagSpy.mockReturnValueOnce(true).mockReturnValueOnce(true)
+  describe('with FF enableAppV2CategoryBlock true', () => {
+    beforeEach(() => useFeatureFlagSpy.mockReturnValue(true))
 
-    render(
-      <CategoryListModule
-        id="123"
-        title="module"
-        categoryBlockList={categoryBlockList}
-        index={1}
-        homeEntryId="homeEntryId"
-      />
-    )
+    it('should render properly when enableAppV2CategoryBlock FF is activated', () => {
+      render(
+        <CategoryListModule
+          id="123"
+          title="module"
+          categoryBlockList={categoryBlockList}
+          index={1}
+          homeEntryId="homeEntryId"
+        />
+      )
 
-    expect(screen).toMatchSnapshot()
-  })
+      expect(screen).toMatchSnapshot()
+    })
 
-  it('should display circle nav buttons when feature is enabled', () => {
-    useFeatureFlagSpy.mockReturnValue(true).mockReturnValueOnce(true)
+    it('should display circle nav buttons when feature is enabled', () => {
+      renderCategoryListModule({
+        id: '123',
+        title: 'module',
+        categoryBlockList,
+        index: 1,
+        homeEntryId: '6DCThxvbPFKAo04SVRZtwY',
+      })
 
-    render(
-      <CategoryListModule
-        id="123"
-        title="module"
-        categoryBlockList={categoryBlockList}
-        index={1}
-        homeEntryId="6DCThxvbPFKAo04SVRZtwY"
-      />
-    )
-
-    expect(screen.getByText('Ce week-end')).toBeOnTheScreen()
-  })
-
-  it('should NOT display circle nav buttons when feature is disabled', () => {
-    render(
-      <CategoryListModule
-        id="123"
-        title="module"
-        categoryBlockList={categoryBlockList}
-        index={1}
-        homeEntryId="6DCThxvbPFKAo04SVRZtwY"
-      />
-    )
-
-    expect(screen.queryByText('Ce week-end')).not.toBeOnTheScreen()
+      expect(screen.getByText('Ce week-end')).toBeOnTheScreen()
+    })
   })
 })
+
+const renderCategoryListModule = (props: CategoryListProps) => {
+  return render(reactQueryProviderHOC(<CategoryListModule {...props} />))
+}
