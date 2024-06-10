19e838d7ca4a15d9392903844e2591bdd4b2ba72
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-06-10 15:57:09 +0200

    (PC-30173) feat(home): use shouldApplyGraphicRedesign remote config with wipAppV2BusinessBlock FF

--- src/features/home/components/modules/business/BusinessModule.native.test.tsx
+++ src/features/home/components/modules/business/BusinessModule.native.test.tsx
@@ -5,7 +5,10 @@ import {
   BusinessModuleProps,
 } from 'features/home/components/modules/business/BusinessModule'
 import { BusinessModuleCTAWording } from 'features/home/types'
+import { REDESIGN_AB_TESTING_HOME_MODULES } from 'libs/contentful/constants'
 import * as useFeatureFlag from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { DEFAULT_REMOTE_CONFIG } from 'libs/firebase/remoteConfig/remoteConfig.constants'
+import * as useRemoteConfigContext from 'libs/firebase/remoteConfig/RemoteConfigProvider'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { render, screen } from 'tests/utils'
 
@@ -27,15 +30,55 @@ const props: BusinessModuleProps = {
 }
 
 const mockFeatureFlag = jest.spyOn(useFeatureFlag, 'useFeatureFlag').mockReturnValue(false)
+const useRemoteConfigContextSpy = jest.spyOn(useRemoteConfigContext, 'useRemoteConfigContext')
 
 jest.mock('libs/firebase/analytics/analytics')
 
 describe('BusinessModule component', () => {
-  it('should render NewBusinessModule if FF WIP_APP_V2_BUSINESS_BLOCK is on', () => {
-    mockFeatureFlag.mockReturnValueOnce(true)
-    renderBusinessModule(props)
+  describe('When shouldApplyGraphicRedesign remote config is false', () => {
+    beforeAll(() => {
+      useRemoteConfigContextSpy.mockReturnValue({
+        ...DEFAULT_REMOTE_CONFIG,
+        shouldApplyGraphicRedesign: false,
+      })
+    })
 
-    expect(screen.getByText('En savoir plus')).toBeOnTheScreen()
+    it('should render NewBusinessModule if FF WIP_APP_V2_BUSINESS_BLOCK is on and home id not in REDESIGN_AB_TESTING_HOME_MODULES', () => {
+      mockFeatureFlag.mockReturnValueOnce(true)
+      renderBusinessModule(props)
+
+      expect(screen.getByText('En savoir plus')).toBeOnTheScreen()
+    })
+
+    it('should render OldBusinessModule if FF WIP_APP_V2_BUSINESS_BLOCK is on and home id in REDESIGN_AB_TESTING_HOME_MODULES', () => {
+      mockFeatureFlag.mockReturnValueOnce(true)
+      renderBusinessModule({ ...props, homeEntryId: REDESIGN_AB_TESTING_HOME_MODULES[0] })
+
+      expect(screen.queryByText('En savoir plus')).not.toBeOnTheScreen()
+    })
+  })
+
+  describe('When shouldApplyGraphicRedesign remote config is true', () => {
+    beforeAll(() => {
+      useRemoteConfigContextSpy.mockReturnValue({
+        ...DEFAULT_REMOTE_CONFIG,
+        shouldApplyGraphicRedesign: true,
+      })
+    })
+
+    it('should render NewBusinessModule if FF WIP_APP_V2_BUSINESS_BLOCK is on and home id not in REDESIGN_AB_TESTING_HOME_MODULES', () => {
+      mockFeatureFlag.mockReturnValueOnce(true)
+      renderBusinessModule(props)
+
+      expect(screen.getByText('En savoir plus')).toBeOnTheScreen()
+    })
+
+    it('should render NewBusinessModule if FF WIP_APP_V2_BUSINESS_BLOCK is on and home id in REDESIGN_AB_TESTING_HOME_MODULES', () => {
+      mockFeatureFlag.mockReturnValueOnce(true)
+      renderBusinessModule({ ...props, homeEntryId: REDESIGN_AB_TESTING_HOME_MODULES[0] })
+
+      expect(screen.getByText('En savoir plus')).toBeOnTheScreen()
+    })
   })
 
   it('should render OldBusinessModule if FF WIP_APP_V2_BUSINESS_BLOCK is off', async () => {
