0a075a3bc6c31207dad62cc1853f03d27b7ed1b9
LucasBeneston <benestonlucas@hotmail.fr>
2024-06-12 17:40:31 +0200

    Remove EventCardList snapshot and add story

---
+++ src/ui/components/eventCard/EventCardList.stories.tsx
@@ -0,0 +1,95 @@
+import { action } from '@storybook/addon-actions'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+
+import { theme } from 'theme'
+
+import { EventCardList } from './EventCardList'
+
+const meta: ComponentMeta<typeof EventCardList> = {
+  title: 'ui/EventCardList',
+  component: EventCardList,
+}
+export default meta
+
+const Template: ComponentStory<typeof EventCardList> = (props) => <EventCardList {...props} />
+
+export const EventCardListDefault = Template.bind({})
+EventCardListDefault.args = {
+  data: [
+    {
+      title: '10h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+    {
+      title: '11h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+    {
+      title: '12h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+    {
+      title: '13h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+    {
+      title: '14h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+    {
+      title: '15h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+    {
+      title: '16h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+    {
+      title: '17h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+    {
+      title: '19h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+    {
+      title: '21h35',
+      subtitleLeft: 'VO,3D max',
+      subtitleRight: '7,99€',
+      isDisabled: false,
+      onPress: action('pressed'),
+    },
+  ],
+}
+
+EventCardListDefault.parameters = {
+  chromatic: { viewports: [theme.breakpoints.xs, theme.breakpoints.xl] },
+}
