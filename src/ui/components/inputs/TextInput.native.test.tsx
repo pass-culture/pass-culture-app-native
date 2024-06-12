d88046faab3407d8a48169084096d68007ec3525
LucasBeneston <benestonlucas@hotmail.fr>
2024-06-12 17:34:46 +0200

    Remove TextInput snapshot

--- src/ui/components/inputs/TextInput.native.test.tsx
+++ src/ui/components/inputs/TextInput.native.test.tsx
@@ -1,29 +1,15 @@
 import React from 'react'
 import { TextInput as RNTextInput } from 'react-native'
 
-import { render, screen } from 'tests/utils'
+import { render } from 'tests/utils'
 
 import { TextInput } from './TextInput'
 
-const NormalStateTextInput = (
-  <TextInput placeholder="placeholder" defaultValue="defaultValue" onChangeText={doNothingFn} />
-)
-
 describe('<TextInput />', () => {
-  it('should render correctly when NOT focused', () => {
-    render(NormalStateTextInput)
-
-    expect(screen).toMatchSnapshot()
-  })
-
   it('should render ref correctly', () => {
     const myRef = React.createRef<RNTextInput>()
-    render(<TextInput placeholder="placeholder" onChangeText={doNothingFn} ref={myRef} />)
+    render(<TextInput placeholder="placeholder" onChangeText={jest.fn()} ref={myRef} />)
 
     expect(myRef.current).toBeTruthy()
   })
 })
-
-function doNothingFn() {
-  /* do nothing */
-}
