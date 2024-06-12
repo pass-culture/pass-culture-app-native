3dfdcc9d0ebce3091d07cb953452b0d0aa767016
LucasBeneston <benestonlucas@hotmail.fr>
2024-06-12 17:43:44 +0200

    Remove NoBookingsView snapshot

--- src/features/bookings/components/NoBookingsView.native.test.tsx
+++ src/features/bookings/components/NoBookingsView.native.test.tsx
@@ -5,6 +5,7 @@ import { analytics } from 'libs/analytics'
 import * as useNetInfoContextDefault from 'libs/network/NetInfoWrapper'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { fireEvent, render, screen, waitFor } from 'tests/utils'
+import { DOUBLE_LINE_BREAK } from 'ui/theme/constants'
 
 import { NoBookingsView } from './NoBookingsView'
 
@@ -21,14 +22,24 @@ describe('<NoBookingsView />', () => {
     mockUseNetInfoContext.mockReturnValueOnce({ isConnected: true })
     render(reactQueryProviderHOC(<NoBookingsView />))
 
-    expect(screen).toMatchSnapshot()
+    const explanations = screen.getByText(
+      'Tu n’as pas de réservation en cours. Explore le catalogue pour trouver ton bonheur !'
+    )
+
+    expect(explanations).toBeOnTheScreen()
   })
 
   it('should render offline no bookings view when netInfo.isConnected is false', () => {
     mockUseNetInfoContext.mockReturnValueOnce({ isConnected: false })
     render(reactQueryProviderHOC(<NoBookingsView />))
 
-    expect(screen).toMatchSnapshot()
+    const explanations = screen.getByText(
+      'Aucune réservations en cours.' +
+        DOUBLE_LINE_BREAK +
+        'Il est possible que certaines réservations ne s’affichent pas hors connexion. Connecte-toi à internet pour vérifier.'
+    )
+
+    expect(explanations).toBeOnTheScreen()
   })
 
   it('should navigate to Search when pressing button and log event', async () => {
