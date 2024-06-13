e29316487569480b4944653dccb7af8cc555f8bf
bruno ebstein <bruno.ebstein@passculture.app>
2024-06-13 18:06:06 +0200

    ajout de SSL_CERT_FILE qui est plus standard

--- scripts/load_certificate.sh
+++ scripts/load_certificate.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-NODE_EXTRA_CA_CERTS='/Library/Application Support/Netskope/STAgent/data/nscacert.pem'
+SSL_CERT_FILE='/Library/Application Support/Netskope/STAgent/data/nscacert.pem'
 
-if [ -f "$NODE_EXTRA_CA_CERTS" ]; then
-	export NODE_EXTRA_CA_CERTS="$NODE_EXTRA_CA_CERTS"
+if [ -f "$SSL_CERT_FILE" ]; then
+	export SSL_CERT_FILE="$SSL_CERT_FILE"
+	export NODE_EXTRA_CA_CERTS="$SSL_CERT_FILE"
 fi
