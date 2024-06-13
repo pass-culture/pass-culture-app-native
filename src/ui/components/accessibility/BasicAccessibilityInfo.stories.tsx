bf3a511041323d5006ea11c52716d2488ab08ee7
LucasBeneston <benestonlucas@hotmail.fr>
2024-06-13 14:00:01 +0200

    Add BasicAccessibilityInfo component

---
+++ src/ui/components/accessibility/BasicAccessibilityInfo.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import React from 'react'
+
+import { BasicAccessibilityInfo } from './BasicAccessibilityInfo'
+
+const meta: ComponentMeta<typeof BasicAccessibilityInfo> = {
+  title: 'ui/accessibility/BasicAccessibilityInfo',
+  component: BasicAccessibilityInfo,
+}
+export default meta
+
+const Template: ComponentStory<typeof BasicAccessibilityInfo> = (props) => (
+  <BasicAccessibilityInfo {...props} />
+)
+export const Default = Template.bind({})
+Default.args = {
+  accessibility: {
+    audioDisability: false,
+    mentalDisability: false,
+    motorDisability: true,
+    visualDisability: true,
+  },
+}
