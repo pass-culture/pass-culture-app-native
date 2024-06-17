7dbc597fdb29516772175fffbf03b91b5ca04c1d
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-06-17 12:50:37 +0200

    (PC-30082) feat(offer): extract carousel logic in a specific component

---
+++ src/features/offer/components/OfferImageCarousel.tsx
@@ -0,0 +1,100 @@
+import React, { FunctionComponent, useRef } from 'react'
+import { SharedValue } from 'react-native-reanimated'
+import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel'
+import styled, { useTheme } from 'styled-components/native'
+
+import { OfferBodyImage } from 'features/offer/components/OfferBodyImage'
+import { OfferImageCarouselPagination } from 'features/offer/components/OfferImageCarouselPagination/OfferImageCarouselPagination'
+import { OfferImageWrapper } from 'features/offer/components/OfferImageWrapper/OfferImageWrapper'
+import { useOfferImageContainerDimensions } from 'features/offer/helpers/useOfferImageContainerDimensions'
+import { useGetHeaderHeight } from 'ui/components/headers/PageHeaderWithoutPlaceholder'
+import { Spacer } from 'ui/theme'
+
+type Props = {
+  progressValue: SharedValue<number>
+  setIndex: React.Dispatch<React.SetStateAction<number>>
+  offerImages: string[]
+  shouldDisplayOfferPreview?: boolean
+  hasScrollEnabled?: boolean
+  isSticky?: boolean
+}
+
+export const OfferImageCarousel: FunctionComponent<Props> = ({
+  progressValue,
+  setIndex,
+  offerImages,
+  shouldDisplayOfferPreview,
+  hasScrollEnabled,
+  isSticky,
+}) => {
+  const { imageStyle } = useOfferImageContainerDimensions()
+  const headerHeight = useGetHeaderHeight()
+  const theme = useTheme()
+  const carouselRef = useRef<ICarouselInstance>(null)
+  const carouselStyle = {
+    borderRadius: theme.borderRadius.radius,
+  }
+
+  const handlePressPreviousButton = () => {
+    const newIndex = Math.max(0, Math.round(progressValue.value) - 1)
+    progressValue.value = newIndex
+    setIndex(newIndex)
+    carouselRef.current?.scrollTo({ index: newIndex, animated: true })
+  }
+
+  const handlePressNextButton = () => {
+    const newIndex = Math.min(offerImages.length - 1, Math.round(progressValue.value) + 1)
+    progressValue.value = newIndex
+    setIndex(newIndex)
+    carouselRef.current?.scrollTo({ index: newIndex, animated: true })
+  }
+
+  return (
+    <CarouselContainer
+      style={isSticky ? imageStyle : undefined}
+      headerHeight={headerHeight}
+      isSticky={isSticky}>
+      <Carousel
+        ref={carouselRef}
+        testID="offerImageContainerCarousel"
+        vertical={false}
+        height={imageStyle.height}
+        width={imageStyle.width}
+        loop={false}
+        enabled={hasScrollEnabled}
+        scrollAnimationDuration={500}
+        onProgressChange={(_, absoluteProgress) => {
+          progressValue.value = absoluteProgress
+          setIndex(Math.round(absoluteProgress))
+        }}
+        data={offerImages}
+        renderItem={({ item: image }) => (
+          <OfferImageWrapper
+            imageUrl={offerImages[0]}
+            shouldDisplayOfferPreview={shouldDisplayOfferPreview}
+            isInCarousel>
+            <OfferBodyImage imageUrl={image} isInCarousel />
+          </OfferImageWrapper>
+        )}
+        style={carouselStyle}
+      />
+      {progressValue ? (
+        <React.Fragment>
+          <Spacer.Column numberOfSpaces={theme.isDesktopViewport ? 6 : 4} />
+          <OfferImageCarouselPagination
+            progressValue={progressValue}
+            offerImages={offerImages}
+            onPressPreviousButton={handlePressPreviousButton}
+            onPressNextButton={handlePressNextButton}
+          />
+        </React.Fragment>
+      ) : null}
+    </CarouselContainer>
+  )
+}
+
+const CarouselContainer = styled.View<{ headerHeight: number; isSticky?: boolean }>(
+  ({ headerHeight, isSticky }) => ({
+    ...(isSticky ? { position: 'sticky', top: 48 + headerHeight, zIndex: 1 } : {}),
+  })
+)
