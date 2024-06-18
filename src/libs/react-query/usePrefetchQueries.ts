6277ae2a214348542df0696f72fe43567e025125
Maxime Le Duc <maxime.leduc@passculture.app>
2024-06-18 14:56:58 +0200

    Fix usePrefetchQueries

--- src/libs/react-query/usePrefetchQueries.ts
+++ src/libs/react-query/usePrefetchQueries.ts
@@ -1,14 +1,15 @@
 import { useEffect } from 'react'
 
 import { api } from 'api/api'
-import { fetchHomepageNatifContent } from 'libs/contentful/fetchHomepageNatifContent'
+import { GetHomeData } from 'features/home/api/useHomepageData'
+import { useDependencies } from 'libs/dependencies/DependenciesContext'
 import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
 import { QueryKeys } from 'libs/queryKeys'
 import { queryClient } from 'libs/react-query/queryClient'
 
-const prefetchQueries = async () => {
+const prefetchQueries = async (getHomeData: GetHomeData) => {
   try {
-    await queryClient.prefetchQuery([QueryKeys.HOMEPAGE_MODULES], fetchHomepageNatifContent)
+    await queryClient.prefetchQuery([QueryKeys.HOMEPAGE_MODULES], getHomeData)
     await queryClient.prefetchQuery([QueryKeys.SETTINGS], () => api.getNativeV1Settings())
   } catch (err) {
     // do nothing in case the pretching of queries fails
@@ -17,9 +18,10 @@ const prefetchQueries = async () => {
 
 export const usePrefetchQueries = () => {
   const { isConnected } = useNetInfoContext()
+  const { getHomeData } = useDependencies()
   useEffect(() => {
     if (isConnected) {
-      prefetchQueries()
+      prefetchQueries(getHomeData)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isConnected])
