4c5c09ffa1fd1ac6b09cb3c552ab74f11e0c6023
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-06-18 11:20:12 +0200

    (PC-30082) feat(offer): separate web and native log for OfferImageCarouselPagination

--- src/features/offer/components/OfferImageCarouselPagination/OfferImageCarouselPagination.native.test.tsx
+++ src/features/offer/components/OfferImageCarouselPagination/OfferImageCarouselPagination.native.test.tsx
@@ -3,17 +3,12 @@ import React from 'react'
 import { OfferImageCarouselPagination } from 'features/offer/components/OfferImageCarouselPagination/OfferImageCarouselPagination'
 import { render, screen } from 'tests/utils'
 
-const mockOnPressPreviousButton = jest.fn()
-const mockOnPressNextButton = jest.fn()
-
 describe('<OfferImageCarouselPagination />', () => {
   it('should display pagination with only dots', () => {
     render(
       <OfferImageCarouselPagination
         progressValue={{ value: 0 }}
         offerImages={['image1', 'image2']}
-        onPressPreviousButton={mockOnPressPreviousButton}
-        onPressNextButton={mockOnPressNextButton}
       />
     )
 
@@ -25,8 +20,6 @@ describe('<OfferImageCarouselPagination />', () => {
       <OfferImageCarouselPagination
         progressValue={{ value: 0 }}
         offerImages={['image1', 'image2']}
-        onPressPreviousButton={mockOnPressPreviousButton}
-        onPressNextButton={mockOnPressNextButton}
       />
     )
 
--- src/features/offer/components/OfferImageCarouselPagination/WebOfferImageCarouselPagination.tsx
+++ src/features/offer/components/OfferImageCarouselPagination/WebOfferImageCarouselPagination.tsx
@@ -4,26 +4,41 @@ import styled from 'styled-components/native'
 import { v4 as uuidv4 } from 'uuid'
 
 import { CarouselDot } from 'ui/CarouselDot/CarouselDot'
+import { RoundedButton } from 'ui/components/buttons/RoundedButton'
 import { ViewGap } from 'ui/components/ViewGap/ViewGap'
 
 type Props = {
   progressValue: SharedValue<number>
   offerImages: string[]
+  handlePressButton: (direction: 1 | -1) => void
 }
 
-export const OfferImageCarouselPagination: FunctionComponent<Props> = ({
+export const WebOfferImageCarouselPagination: FunctionComponent<Props> = ({
   progressValue,
   offerImages,
+  handlePressButton,
 }) => {
   const carouselDotId = uuidv4()
 
   if (!progressValue) return null
 
   return (
-    <Container gap={2} testID="onlyDotsContainer">
+    <Container gap={4} testID="buttonsAndDotsContainer">
+      <RoundedButton
+        iconName="previous"
+        onPress={() => handlePressButton(-1)}
+        accessibilityLabel="Image précédente"
+      />
+
       {offerImages.map((_, index) => (
         <CarouselDot animValue={progressValue} index={index} key={index + carouselDotId} />
       ))}
+
+      <RoundedButton
+        iconName="next"
+        onPress={() => handlePressButton(1)}
+        accessibilityLabel="Image suivante"
+      />
     </Container>
   )
 }
