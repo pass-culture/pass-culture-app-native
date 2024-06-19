602bd33e1bde53b94cf725816bd200a1414ea514
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-06-19 18:13:59 +0200

    (PC-30404) feat(venueMap): reset venue type code in store when pressing back button

--- src/features/venueMap/pages/VenueMap/VenueMap.native.test.tsx
+++ src/features/venueMap/pages/VenueMap/VenueMap.native.test.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
+import * as useGoBack from 'features/navigation/useGoBack'
 import { VenueMap } from 'features/venueMap/pages/VenueMap/VenueMap'
+import { useVenueTypeCodeActions } from 'features/venueMap/store/venueTypeCodeStore'
 import * as useFeatureFlag from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { render, screen, waitFor } from 'tests/utils'
+import { fireEvent, render, screen, waitFor } from 'tests/utils'
 
 jest.spyOn(useFeatureFlag, 'useFeatureFlag').mockReturnValue(true)
+
+const mockGoBack = jest.fn()
+jest.spyOn(useGoBack, 'useGoBack').mockReturnValue({
+  goBack: mockGoBack,
+  canGoBack: jest.fn(() => true),
+})
+
+jest.mock('features/venueMap/store/venueTypeCodeStore')
+const mockSetVenueTypeCode = jest.fn()
+const mockUseVenueTypeCodeActions = useVenueTypeCodeActions as jest.Mock
+mockUseVenueTypeCodeActions.mockReturnValue({ setVenueTypeCode: mockSetVenueTypeCode })
 
 describe('<VenueMap />', () => {
   it('Should display venue map header', async () => {
@@ -14,5 +27,21 @@ describe('<VenueMap />', () => {
     await waitFor(() => {
       expect(screen.getByText('Carte des lieux')).toBeOnTheScreen()
     })
+  })
+
+  it('Should handle go back action when pressing go back button', async () => {
+    render(reactQueryProviderHOC(<VenueMap />))
+
+    fireEvent.press(await screen.findByTestId('Revenir en arrière'))
+
+    expect(mockGoBack).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should reset venue type code in store when pressing go back button', async () => {
+    render(reactQueryProviderHOC(<VenueMap />))
+
+    fireEvent.press(await screen.findByTestId('Revenir en arrière'))
+
+    expect(mockSetVenueTypeCode).toHaveBeenNthCalledWith(1, null)
   })
 })
