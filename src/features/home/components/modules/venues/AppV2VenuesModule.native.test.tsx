ec2232fa1f346fdf59273bab8c7a168009e78db7
cedricls1 <78556024+cedricls1@users.noreply.github.com>
2024-06-19 17:35:21 +0200

    (PC-30429) feat(home): use ModuleDisplayedOnHomepage log on AppV2VenuesModule (#6580)
    * (PC-30429) feat(home): use ModuleDisplayedOnHomepage log on AppV2VenuesModule
    
    * (PC-30429) test(home): add logModuleDisplayedOnHomepage test
    
    ---------
    
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

--- src/features/home/components/modules/venues/AppV2VenuesModule.native.test.tsx
+++ src/features/home/components/modules/venues/AppV2VenuesModule.native.test.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { AppV2VenuesModule } from 'features/home/components/modules/venues/AppV2VenuesModule'
 import { ModuleData } from 'features/home/types'
 import { venuesSearchFixture } from 'libs/algolia/fixtures/venuesSearchFixture'
+import { analytics } from 'libs/analytics'
 import * as useFeatureFlagAPI from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { DEFAULT_REMOTE_CONFIG } from 'libs/firebase/remoteConfig/remoteConfig.constants'
 import * as useRemoteConfigContext from 'libs/firebase/remoteConfig/RemoteConfigProvider'
@@ -13,6 +14,8 @@ const mockFeatureFlag = jest.spyOn(useFeatureFlagAPI, 'useFeatureFlag').mockRetu
 const useRemoteConfigContextSpy = jest.spyOn(useRemoteConfigContext, 'useRemoteConfigContext')
 
 const props = {
+  moduleId: 'fakemoduleid',
+  index: 1,
   data: {
     playlistItems: venuesSearchFixture.hits,
     nbPlaylistResults: venuesSearchFixture.hits.length,
@@ -26,6 +29,18 @@ describe('<AppV2VenuesModule />', () => {
     renderAppV2VenuesModule()
 
     expect(screen).toMatchSnapshot()
+  })
+
+  it('should log ModuleDisplayedOnHomePage event when seeing the module', () => {
+    renderAppV2VenuesModule()
+
+    expect(analytics.logModuleDisplayedOnHomepage).toHaveBeenNthCalledWith(1, {
+      moduleId: 'fakemoduleid',
+      moduleType: 'venuesPlaylistAppV2',
+      index: 1,
+      homeEntryId: 'homeEntryId',
+      venues: ['5543', '5544', '5545', '5546'],
+    })
   })
 
   it('should not render list when data is undefined', () => {
--- src/features/home/components/modules/venues/AppV2VenuesModule.web.tsx
+++ src/features/home/components/modules/venues/AppV2VenuesModule.web.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Platform } from 'react-native'
 import { useTheme } from 'styled-components/native'
 
 import { VenueListModule } from 'features/home/components/modules/VenueListModule.web'
 import { AppV2VenuesModule as AppV2VenuesModuleType, ModuleData } from 'features/home/types'
 import { VenueHit } from 'libs/algolia/types'
+import { analytics } from 'libs/analytics'
+import { ContentTypes } from 'libs/contentful/types'
+import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 
 type Props = {
   displayParameters: AppV2VenuesModuleType['displayParameters']
   moduleId: string
+  index: number
+  homeEntryId: string
   data?: ModuleData
-  homeVenuesListEntryId?: string
 }
 
 const isWeb = Platform.OS === 'web'
@@ -18,13 +23,30 @@ const isWeb = Platform.OS === 'web'
 export const AppV2VenuesModule = ({
   data,
   moduleId,
+  index,
   displayParameters,
-  homeVenuesListEntryId,
+  homeEntryId,
 }: Props) => {
   const { playlistItems = [] } = data ?? { playlistItems: [] }
   const { isDesktopViewport } = useTheme()
+  const enableAppV2VenueList = useFeatureFlag(RemoteStoreFeatureFlags.WIP_APP_V2_VENUE_LIST)
 
-  if (playlistItems.length === 0) return null
+  const shouldModuleBeDisplayed = playlistItems.length > 0 && enableAppV2VenueList
+
+  useEffect(() => {
+    if (shouldModuleBeDisplayed) {
+      analytics.logModuleDisplayedOnHomepage({
+        moduleId,
+        moduleType: ContentTypes.VENUES_PLAYLIST_APP_V2,
+        index,
+        homeEntryId: homeEntryId,
+        venues: (playlistItems.slice(0, 4) as VenueHit[]).map((item) => String(item.id)),
+      })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [shouldModuleBeDisplayed])
+
+  if (!shouldModuleBeDisplayed) return null
 
   const MAX_ITEMS = isWeb && isDesktopViewport ? 6 : 4
   const moduleName = displayParameters.title
@@ -34,7 +56,7 @@ export const AppV2VenuesModule = ({
       venues={playlistItems.slice(0, MAX_ITEMS) as VenueHit[]}
       moduleId={moduleId}
       moduleName={moduleName}
-      homeVenuesListEntryId={homeVenuesListEntryId}
+      homeVenuesListEntryId={homeEntryId}
     />
   )
 }
