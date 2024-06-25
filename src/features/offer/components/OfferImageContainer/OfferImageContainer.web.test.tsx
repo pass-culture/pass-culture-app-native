21e3f0d8e7e8ef8f4d022fe2261e05f27e678001
cedricls1 <78556024+cedricls1@users.noreply.github.com>
2024-06-25 18:03:56 +0200

    (PC-30464) fix(home): use Paris coordinates to open venue map from trend button when user not share his position (#6604)
    * (PC-30464) fix(home): use Paris coordinates to open venue map from trend button when user not share his position
    
    * (PC-30464) test(offer): fix flaky tests
    
    * (PC-30464) test(offer): fix flaky tests
    
    ---------
    
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

---
+++ src/features/offer/components/OfferImageContainer/OfferImageContainer.web.test.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import { CategoryIdEnum } from 'api/gen'
+import { OfferImageContainer } from 'features/offer/components/OfferImageContainer/OfferImageContainer'
+import * as useFeatureFlagAPI from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { render, screen } from 'tests/utils/web'
+
+jest.mock('libs/firebase/remoteConfig/remoteConfig.services')
+
+const useFeatureFlagSpy = jest.spyOn(useFeatureFlagAPI, 'useFeatureFlag').mockReturnValue(false)
+
+const mockOnPress = jest.fn()
+
+describe('<OfferImageContainer />', () => {
+  it('should not display carousel with one image', () => {
+    useFeatureFlagSpy.mockReturnValueOnce(true)
+    render(
+      <OfferImageContainer
+        imageUrls={['some_url_to_some_resource']}
+        categoryId={CategoryIdEnum.CINEMA}
+        onPress={mockOnPress}
+      />,
+      { theme: { isDesktopViewport: true } }
+    )
+
+    expect(screen.getByTestId('offerImageWithoutCarousel')).toBeInTheDocument()
+  })
+
+  it('should not display carousel with feature flag off', () => {
+    useFeatureFlagSpy.mockReturnValueOnce(false)
+    render(
+      <OfferImageContainer
+        imageUrls={['some_url_to_some_resource', 'some_url2_to_some_resource']}
+        categoryId={CategoryIdEnum.CINEMA}
+        onPress={mockOnPress}
+      />,
+      { theme: { isDesktopViewport: true } }
+    )
+
+    expect(screen.getByTestId('offerImageWithoutCarousel')).toBeInTheDocument()
+  })
+
+  // TODO(PC-30559) : test flaky sur la CI
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip('should display carousel with several images', () => {
+    useFeatureFlagSpy.mockReturnValueOnce(true)
+
+    render(
+      <OfferImageContainer
+        imageUrls={['some_url_to_some_resource', 'some_url2_to_some_resource']}
+        categoryId={CategoryIdEnum.CINEMA}
+        onPress={mockOnPress}
+      />,
+      { theme: { isDesktopViewport: true } }
+    )
+
+    expect(screen.queryByTestId('offerImageWithoutCarousel')).not.toBeInTheDocument()
+  })
+})
