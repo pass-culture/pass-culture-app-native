1dc4bde030abc9588eeaea09d303ba3131e6690e
Ali RAIKI <ali.raiki@polyconseil.fr>
2024-06-26 15:56:53 +0200

    Fix story with useRoute issue

--- src/ui/components/buttons/SubcategoryButton/SubcategoryButton.stories.tsx
+++ src/ui/components/buttons/SubcategoryButton/SubcategoryButton.stories.tsx
@@ -1,24 +1,42 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import React from 'react'
 import styled from 'styled-components/native'
 
-import { ReactQueryClientProvider } from 'libs/react-query/ReactQueryClientProvider'
+import { SearchWrapper } from 'features/search/context/SearchWrapper'
+import { PLACEHOLDER_DATA } from 'libs/subcategories/placeholderData'
 import { theme } from 'theme'
 import { SubcategoryButton } from 'ui/components/buttons/SubcategoryButton/SubcategoryButton'
 import { getSpacing } from 'ui/theme'
+
+const Stack = createStackNavigator()
+
 const meta: ComponentMeta<typeof SubcategoryButton> = {
   title: 'ui/buttons/SubcategoryButton',
   component: SubcategoryButton,
   decorators: [
     (Story) => (
-      <ReactQueryClientProvider>
-        <NavigationContainer>
-          <Story />
-        </NavigationContainer>
-      </ReactQueryClientProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false, cardStyle: { flexBasis: 'content' } }}>
+          <Stack.Screen
+            name="SubcategoryButton"
+            component={() => (
+              <SearchWrapper>
+                <Story />
+              </SearchWrapper>
+            )}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     ),
   ],
+  parameters: {
+    useQuery: {
+      subcategories: PLACEHOLDER_DATA.subcategories,
+    },
+  },
 }
 export default meta
 
@@ -30,8 +48,7 @@ const Template: ComponentStory<typeof SubcategoryButton> = (props) => (
 
 const label = 'Société & Politique'
 
-//TODO(PC-28526): Fix this stories
-const Default = Template.bind({})
+export const Default = Template.bind({})
 Default.args = {
   label,
   colors: [theme.colors.aquamarine, theme.colors.aquamarineDark],
