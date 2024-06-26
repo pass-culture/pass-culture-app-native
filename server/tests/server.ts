aef59dde115d94fbd92c18dcb07c0db4a36533b2
Dan Nguyen <dan.nguyen@passculture.app>
2024-06-26 15:12:58 +0200

    (BSR) feat: bump msw to latest

--- server/tests/server.ts
+++ server/tests/server.ts
@@ -11,26 +11,26 @@ import {
 
 export const server = setupServer(
   // offer
-  rest.get(
+  http.get(
     `${env.API_BASE_URL}/${env.API_BASE_PATH_NATIVE_V1}/offer/${OFFER_RESPONSE_SNAPSHOT.id}`,
-    (req, res, ctx) => res(ctx.status(200), ctx.json(OFFER_RESPONSE_SNAPSHOT))
+    (info) => res(ctx.status(200), ctx.json(OFFER_RESPONSE_SNAPSHOT))
   ),
   // 404 offer
-  rest.get(`${env.API_BASE_URL}/${env.API_BASE_PATH_NATIVE_V1}/offer/0`, (req, res, ctx) =>
+  http.get(`${env.API_BASE_URL}/${env.API_BASE_PATH_NATIVE_V1}/offer/0`, (info) =>
     res(ctx.status(200), ctx.json({}))
   ),
   // 502 offer
-  rest.get(`${env.API_BASE_URL}/${env.API_BASE_PATH_NATIVE_V1}/offer/502`, (req, res, ctx) =>
+  http.get(`${env.API_BASE_URL}/${env.API_BASE_PATH_NATIVE_V1}/offer/502`, (info) =>
     res(ctx.status(502))
   ),
   // venue
-  rest.get(
+  http.get(
     `${env.API_BASE_URL}/${env.API_BASE_PATH_NATIVE_V1}/venue/${VENUE_WITH_BANNER_RESPONSE_SNAPSHOT.id}`,
-    (req, res, ctx) => res(ctx.status(200), ctx.json(VENUE_WITH_BANNER_RESPONSE_SNAPSHOT))
+    (info) => res(ctx.status(200), ctx.json(VENUE_WITH_BANNER_RESPONSE_SNAPSHOT))
   ),
   // venue alternative
-  rest.get(
+  http.get(
     `${env.API_BASE_URL}/${env.API_BASE_PATH_NATIVE_V1}/venue/${VENUE_WITHOUT_BANNER_RESPONSE_SNAPSHOT.id}`,
-    (req, res, ctx) => res(ctx.status(200), ctx.json(VENUE_WITHOUT_BANNER_RESPONSE_SNAPSHOT))
+    (info) => res(ctx.status(200), ctx.json(VENUE_WITHOUT_BANNER_RESPONSE_SNAPSHOT))
   )
 )
