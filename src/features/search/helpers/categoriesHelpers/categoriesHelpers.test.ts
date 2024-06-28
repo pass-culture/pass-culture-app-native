fae0ee4ff368a134d2069cf79aa8cdea7ee7dd5f
marineshaw <marines@bam.tech>
2024-06-28 14:42:26 +0200

    :pencil2: (Tests) rename some subcategories with fixture instead of placeholderdata

--- src/features/search/helpers/categoriesHelpers/categoriesHelpers.test.ts
+++ src/features/search/helpers/categoriesHelpers/categoriesHelpers.test.ts
@@ -16,24 +16,26 @@ import {
 import { createMappingTree } from 'features/search/helpers/categoriesHelpers/mapping-tree'
 import { BooksNativeCategoriesEnum, SearchState } from 'features/search/types'
 import { FACETS_FILTERS_ENUM } from 'libs/algolia/enums/facetsEnums'
-import { PLACEHOLDER_DATA as mockData } from 'libs/subcategories/placeholderData'
+import {
+  searchGroupsFixture,
+  subcategoriesResponseFixture,
+} from 'libs/subcategories/fixtures/subcategoriesResponse'
 
 let mockSearchState: SearchState = {
   ...initialSearchState,
 }
 
-const mockedSubcateroriesV2Response = mockData
 const mockedFacets = undefined
 const mockedNewMappingEnabled = true
 
-const tree = createMappingTree(mockedSubcateroriesV2Response, mockedFacets, mockedNewMappingEnabled)
+const tree = createMappingTree(subcategoriesResponseFixture, mockedFacets, mockedNewMappingEnabled)
 
 jest.mock('libs/firebase/analytics/analytics')
 jest.mock('libs/firebase/remoteConfig/remoteConfig.services')
 
 describe('categoriesHelpers', () => {
   it('should sort categories by alphabetical order', () => {
-    const categories = mockData.searchGroups
+    const categories = searchGroupsFixture
       .filter((category) => category.name !== SearchGroupNameEnumv2.NONE)
       .sort(searchGroupOrNativeCategorySortComparator)
 
@@ -95,7 +97,7 @@ describe('categoriesHelpers', () => {
 
   it('should sort native subcategories by alphabetical order', () => {
     const nativeSubcategories = getNativeCategories(
-      mockData,
+      subcategoriesResponseFixture,
       SearchGroupNameEnumv2.ARTS_LOISIRS_CREATIFS
     )
 
@@ -125,32 +127,38 @@ describe('categoriesHelpers', () => {
 
   describe('isOnlyOnline', () => {
     it('should return false when category and native category are undefined', () => {
-      const value = isOnlyOnline(mockData)
+      const value = isOnlyOnline(subcategoriesResponseFixture)
 
       expect(value).toEqual(false)
     })
 
     describe('Category', () => {
       it('should return true when all native categories of the category are online platform', () => {
-        const value = isOnlyOnline(mockData, SearchGroupNameEnumv2.EVENEMENTS_EN_LIGNE)
+        const value = isOnlyOnline(
+          subcategoriesResponseFixture,
+          SearchGroupNameEnumv2.EVENEMENTS_EN_LIGNE
+        )
 
         expect(value).toEqual(true)
       })
 
       it('should return false when all native categories of the category are offline', () => {
-        const value = isOnlyOnline(mockData, SearchGroupNameEnumv2.LIVRES)
+        const value = isOnlyOnline(subcategoriesResponseFixture, SearchGroupNameEnumv2.LIVRES)
 
         expect(value).toEqual(false)
       })
 
       it('should return false when native categories of the category are online and offline platform', () => {
-        const value = isOnlyOnline(mockData, SearchGroupNameEnumv2.SPECTACLES)
+        const value = isOnlyOnline(subcategoriesResponseFixture, SearchGroupNameEnumv2.SPECTACLES)
 
         expect(value).toEqual(false)
       })
 
       it('should return false when native categories of the category are online, offline and online or offline platform', () => {
-        const value = isOnlyOnline(mockData, SearchGroupNameEnumv2.ARTS_LOISIRS_CREATIFS)
+        const value = isOnlyOnline(
+          subcategoriesResponseFixture,
+          SearchGroupNameEnumv2.ARTS_LOISIRS_CREATIFS
+        )
 
         expect(value).toEqual(false)
       })
@@ -159,7 +167,7 @@ describe('categoriesHelpers', () => {
     describe('Native category', () => {
       it('should return true when all pro subcategories of the native category are online platform', () => {
         const value = isOnlyOnline(
-          mockData,
+          subcategoriesResponseFixture,
           undefined,
           NativeCategoryIdEnumv2.PRATIQUE_ARTISTIQUE_EN_LIGNE
         )
@@ -169,7 +177,7 @@ describe('categoriesHelpers', () => {
 
       it('should return false when all pro subcategories of the native category are offline', () => {
         const value = isOnlyOnline(
-          mockData,
+          subcategoriesResponseFixture,
           undefined,
           NativeCategoryIdEnumv2.ACHAT_LOCATION_INSTRUMENT
         )
@@ -178,13 +186,21 @@ describe('categoriesHelpers', () => {
       })
 
       it('should return false when pro subcategories of the native category are online and offline platform', () => {
-        const value = isOnlyOnline(mockData, undefined, NativeCategoryIdEnumv2.VISITES_CULTURELLES)
+        const value = isOnlyOnline(
+          subcategoriesResponseFixture,
+          undefined,
+          NativeCategoryIdEnumv2.VISITES_CULTURELLES
+        )
 
         expect(value).toEqual(false)
       })
 
       it('should return false when pro subcategories of the native category are offline and online or offline platform', () => {
-        const value = isOnlyOnline(mockData, undefined, NativeCategoryIdEnumv2.ARTS_VISUELS)
+        const value = isOnlyOnline(
+          subcategoriesResponseFixture,
+          undefined,
+          NativeCategoryIdEnumv2.ARTS_VISUELS
+        )
 
         expect(value).toEqual(false)
       })
@@ -199,13 +215,16 @@ describe('categoriesHelpers', () => {
     })
 
     it('should return undefined when native category id is undefined', () => {
-      const value = getNativeCategoryFromEnum(mockData, undefined)
+      const value = getNativeCategoryFromEnum(subcategoriesResponseFixture, undefined)
 
       expect(value).toEqual(undefined)
     })
 
     it('should return the native category from native category id', () => {
-      const value = getNativeCategoryFromEnum(mockData, NativeCategoryIdEnumv2.ARTS_VISUELS)
+      const value = getNativeCategoryFromEnum(
+        subcategoriesResponseFixture,
+        NativeCategoryIdEnumv2.ARTS_VISUELS
+      )
 
       expect(value).toEqual({ genreType: null, name: 'ARTS_VISUELS', value: 'Arts visuels' })
     })
@@ -220,7 +239,7 @@ describe('categoriesHelpers', () => {
       })
 
       it('without native category in parameter', () => {
-        const value = getSearchGroupsEnumArrayFromNativeCategoryEnum(mockData)
+        const value = getSearchGroupsEnumArrayFromNativeCategoryEnum(subcategoriesResponseFixture)
 
         expect(value).toEqual([])
       })
@@ -228,7 +247,7 @@ describe('categoriesHelpers', () => {
 
     it('should return an array of categories id', () => {
       const value = getSearchGroupsEnumArrayFromNativeCategoryEnum(
-        mockData,
+        subcategoriesResponseFixture,
         NativeCategoryIdEnumv2.ARTS_VISUELS
       )
 
@@ -248,7 +267,7 @@ describe('categoriesHelpers', () => {
 
     it('should return false when native category not associated to category', () => {
       const value = isNativeCategoryOfCategory(
-        mockData,
+        subcategoriesResponseFixture,
         SearchGroupNameEnumv2.CONCERTS_FESTIVALS,
         NativeCategoryIdEnumv2.ACHAT_LOCATION_INSTRUMENT
       )
@@ -258,7 +277,7 @@ describe('categoriesHelpers', () => {
 
     it('should return true when native category associated to category', () => {
       const value = isNativeCategoryOfCategory(
-        mockData,
+        subcategoriesResponseFixture,
         SearchGroupNameEnumv2.ARTS_LOISIRS_CREATIFS,
         NativeCategoryIdEnumv2.ARTS_VISUELS
       )
@@ -401,7 +420,7 @@ describe('categoriesHelpers', () => {
         genreType: null,
       }
 
-      const result = buildBookSearchPayloadValues(mockData, mockedForm)
+      const result = buildBookSearchPayloadValues(subcategoriesResponseFixture, mockedForm)
 
       expect(result).toEqual({
         offerCategories: [SearchGroupNameEnumv2.LIVRES],
@@ -450,7 +469,7 @@ describe('categoriesHelpers', () => {
         genreType: 'KODOMO',
       }
 
-      const result = buildBookSearchPayloadValues(mockData, mockedForm)
+      const result = buildBookSearchPayloadValues(subcategoriesResponseFixture, mockedForm)
 
       expect(result).toEqual({
         offerCategories: [SearchGroupNameEnumv2.LIVRES],
