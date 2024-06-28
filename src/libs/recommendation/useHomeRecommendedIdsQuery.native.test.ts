605a0e813a6ca7a014d04b169514d818c3e6e472
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-06-28 15:27:01 +0200

    Added a test

--- src/libs/recommendation/useHomeRecommendedIdsQuery.native.test.ts
+++ src/libs/recommendation/useHomeRecommendedIdsQuery.native.test.ts
@@ -1,6 +1,7 @@
 import { PlaylistResponse } from 'api/gen'
 import { EmptyResponse } from 'libs/fetch'
 import { eventMonitoring } from 'libs/monitoring'
+import { mockUseAuthContext } from 'tests/AuthContextUtils'
 import { mockServer } from 'tests/mswServer'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { renderHook, waitFor } from 'tests/utils'
@@ -9,6 +10,7 @@ import { useHomeRecommendedIdsQuery } from './useHomeRecommendedIdsQuery'
 
 jest.mock('libs/monitoring')
 jest.mock('libs/jwt/jwt')
+jest.mock('features/auth/context/AuthContext')
 
 describe('useHomeRecommendedIdsQuery', () => {
   it('should capture an exception when fetch call fails', async () => {
@@ -65,6 +67,34 @@ describe('useHomeRecommendedIdsQuery', () => {
         '102249',
         '102310',
       ])
+    })
+  })
+
+  it('should not make request if user is disconnected (and undefined in context)', async () => {
+    mockServer.postApi<PlaylistResponse>('/v1/recommendation/playlist', {
+      responseOptions: {
+        statusCode: 200,
+        data: {
+          params: {},
+          playlistRecommendedOffers: ['102280', '102272', '102249', '102310'],
+        },
+      },
+    })
+    mockUseAuthContext()
+
+    const { result } = renderHook(
+      () =>
+        useHomeRecommendedIdsQuery({
+          playlistRequestBody: {},
+          playlistRequestQuery: {},
+          userId: 1,
+        }),
+      {
+        wrapper: (props) => reactQueryProviderHOC(props.children),
+      }
+    )
+    await waitFor(() => {
+      expect(result.current.isFetching).toEqual(false)
     })
   })
 })
