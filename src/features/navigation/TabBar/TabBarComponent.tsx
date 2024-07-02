ad28ee4d5d3245e773efa2c05d77405f8cbf43c2
YassinL <yassin.leclercq1@gmail.com>
2024-07-02 10:52:48 +0200

    (PC-30519) feat(FF): handle reaction feature under FF

--- src/features/navigation/TabBar/TabBarComponent.tsx
+++ src/features/navigation/TabBar/TabBarComponent.tsx
@@ -7,6 +7,8 @@ import { menu } from 'features/navigation/TabBar/menu'
 import { TabBarInnerComponent } from 'features/navigation/TabBar/TabBarInnerComponent'
 import { TabBarInnerComponentV2 } from 'features/navigation/TabBar/TabBarInnerComponentV2'
 import { TabRouteName } from 'features/navigation/TabBar/types'
+import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
 import { InternalNavigationProps } from 'ui/components/touchableLink/types'
 
@@ -29,7 +31,8 @@ export const TabBarComponent: React.FC<Props> = ({
   onPress,
   tabName,
 }) => {
-  const BicolorIcon = mapTabRouteToBicolorIcon(tabName, v2)
+  const enableReactionFeature = useFeatureFlag(RemoteStoreFeatureFlags.WIP_REACTION_FEATURE)
+  const BicolorIcon = mapTabRouteToBicolorIcon({ route: tabName, v2, enableReactionFeature })
   const InnerComponent = v2 ? TabBarInnerComponentV2 : TabBarInnerComponent
 
   return (
