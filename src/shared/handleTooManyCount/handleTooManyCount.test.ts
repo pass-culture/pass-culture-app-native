7da5bab59d9944e8f8f35165c1a7b90accbeaa27
YassinL <yassin.leclercq1@gmail.com>
2024-07-02 17:20:07 +0200

    (PC-30519) remarks refacto

--- src/shared/handleTooManyCount/handleTooManyCount.test.ts
+++ src/shared/handleTooManyCount/handleTooManyCount.test.ts
@@ -5,69 +5,41 @@ describe('handleTooManyCount', () => {
     const count = 50
     const result = handleTooManyCount(count)
 
-    expect(result).toEqual({
-      accessibilityLabel: '50',
-      fullCountLabel: '50',
-    })
+    expect(result).toEqual('50')
   })
 
   it('should return correct labels when count is equal to COUNT_MAX', () => {
     const count = 100
     const result = handleTooManyCount(count)
 
-    expect(result).toEqual({
-      accessibilityLabel: '100',
-      fullCountLabel: '99+',
-    })
+    expect(result).toEqual('99+')
   })
 
   it('should return correct labels when count is greater than COUNT_MAX', () => {
     const count = 150
     const result = handleTooManyCount(count)
 
-    expect(result).toEqual({
-      accessibilityLabel: '150',
-      fullCountLabel: '99+',
-    })
+    expect(result).toEqual('99+')
   })
 
   it('should return correct labels when count is zero', () => {
     const count = 0
     const result = handleTooManyCount(count)
 
-    expect(result).toEqual({
-      accessibilityLabel: '0',
-      fullCountLabel: '0',
-    })
-  })
-
-  it('should return correct labels when count is negative', () => {
-    const count = -10
-    const result = handleTooManyCount(count)
-
-    expect(result).toEqual({
-      accessibilityLabel: '-10',
-      fullCountLabel: '-10',
-    })
+    expect(result).toEqual('0')
   })
 
   it('should return "0" for displayedCount when count is undefined', () => {
     const count = undefined
     const result = handleTooManyCount(count)
 
-    expect(result).toEqual({
-      accessibilityLabel: '0',
-      fullCountLabel: '0',
-    })
+    expect(result).toEqual('0')
   })
 
   it('should return "0" for displayedCount when count is null', () => {
     const count = null
     const result = handleTooManyCount(count)
 
-    expect(result).toEqual({
-      accessibilityLabel: '0',
-      fullCountLabel: '0',
-    })
+    expect(result).toEqual('0')
   })
 })
--- src/shared/handleTooManyCount/handleTooManyCount.ts
+++ src/shared/handleTooManyCount/handleTooManyCount.ts
@@ -1,15 +1,8 @@
 const COUNT_MAX = 100
 
-type CountResult = {
-  accessibilityLabel: string
-  fullCountLabel: string
-}
-
-export function handleTooManyCount(count: number | null | undefined): CountResult {
+export function handleTooManyCount(count: number | null | undefined): string {
   const hasTooMany = count !== null && count !== undefined && count >= COUNT_MAX
   const displayedCount = hasTooMany ? COUNT_MAX - 1 : count || '0'
   const plusSign = hasTooMany ? '+' : ''
-  const accessibilityLabel = count !== null && count !== undefined ? count.toString() : '0'
-  const fullCountLabel = `${displayedCount}${plusSign}`
-  return { accessibilityLabel, fullCountLabel }
+  return `${displayedCount}${plusSign}`
 }
