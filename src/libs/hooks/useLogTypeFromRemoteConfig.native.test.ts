62718dd6ceceffb58fef00bd09b02b0ad3e43b58
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-07-02 12:33:35 +0200

    (PC-30462) feat(monitoring): use shouldLogInfo remote config with log type enum

---
+++ src/libs/hooks/useLogTypeFromRemoteConfig.native.test.ts
@@ -0,0 +1,47 @@
+import { DEFAULT_REMOTE_CONFIG } from 'libs/firebase/remoteConfig/remoteConfig.constants'
+import * as useRemoteConfigContext from 'libs/firebase/remoteConfig/RemoteConfigProvider'
+import { useLogTypeFromRemoteConfig } from 'libs/hooks/useLogTypeFromRemoteConfig'
+import { LogTypeEnum } from 'libs/monitoring/errors'
+import { renderHook } from 'tests/utils'
+
+const useRemoteConfigContextSpy = jest.spyOn(useRemoteConfigContext, 'useRemoteConfigContext')
+
+describe('useLogTypeFromRemoteConfig', () => {
+  describe('When shouldLogInfo remote config is false', () => {
+    beforeAll(() => {
+      useRemoteConfigContextSpy.mockReturnValue({
+        ...DEFAULT_REMOTE_CONFIG,
+        shouldLogInfo: false,
+      })
+    })
+
+    afterAll(() => {
+      useRemoteConfigContextSpy.mockReturnValue(DEFAULT_REMOTE_CONFIG)
+    })
+
+    it('should return ignored as log type', () => {
+      const { result } = renderHook(() => useLogTypeFromRemoteConfig())
+
+      expect(result.current.logType).toEqual(LogTypeEnum.IGNORED)
+    })
+  })
+
+  describe('When shouldLogInfo remote config is true', () => {
+    beforeAll(() => {
+      useRemoteConfigContextSpy.mockReturnValue({
+        ...DEFAULT_REMOTE_CONFIG,
+        shouldLogInfo: true,
+      })
+    })
+
+    afterAll(() => {
+      useRemoteConfigContextSpy.mockReturnValue(DEFAULT_REMOTE_CONFIG)
+    })
+
+    it('should return info as log type', () => {
+      const { result } = renderHook(() => useLogTypeFromRemoteConfig())
+
+      expect(result.current.logType).toEqual(LogTypeEnum.INFO)
+    })
+  })
+})
