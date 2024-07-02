33a4a842681af121aa34ad3e13a40b303fa13c8c
YassinL <yassin.leclercq1@gmail.com>
2024-07-02 10:51:36 +0200

    (PC-30519) feat(BicolorBookingsCountV2): add new bicolor bookings count

---
+++ src/features/bookings/components/BicolorBookingsCountV2.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Animated } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+import { useTheme } from 'styled-components'
+import styled from 'styled-components/native'
+
+import { useScaleAnimation } from 'features/favorites/helpers/useScaleFavoritesAnimation'
+import { handleTooManyCount } from 'shared/handleTooManyCount/handleTooManyCount'
+import { BicolorBookingsV2 } from 'ui/svg/icons/BicolorBookingsV2'
+import { AccessibleBicolorIcon } from 'ui/svg/icons/types'
+import { getSpacing, Typo } from 'ui/theme'
+
+export const BicolorBookingsCountV2: React.FC<AccessibleBicolorIcon> = ({
+  size,
+  color,
+  color2,
+  testID,
+}) => {
+  const theme = useTheme()
+
+  //TODO(PC-29452): Update when got back info
+  let bookingsCount: number
+  // eslint-disable-next-line prefer-const
+  bookingsCount = 1
+
+  const scale = useScaleAnimation(bookingsCount)
+
+  if (bookingsCount === 0) {
+    return <BicolorBookingsV2 size={size} color={color} color2={color2} testID={testID} />
+  }
+
+  const { accessibilityLabel, fullCountLabel } = handleTooManyCount(bookingsCount)
+
+  return (
+    <Container>
+      <BicolorBookingsV2 size={size} color={color} color2={color2} />
+      <StyledAnimatedView style={{ transform: [{ scale }] }}>
+        <PastilleContainer accessibilityLabel={accessibilityLabel}>
+          <StyledLinearGradient colors={[theme.colors.primary, theme.colors.secondary]}>
+            <Counter>{fullCountLabel}</Counter>
+          </StyledLinearGradient>
+        </PastilleContainer>
+      </StyledAnimatedView>
+    </Container>
+  )
+}
+
+const Container = styled.View({ position: 'relative' })
+
+const StyledAnimatedView = styled(Animated.View)({
+  position: 'absolute',
+  right: getSpacing(2),
+  top: 0,
+})
+
+const StyledLinearGradient = styled(LinearGradient).attrs(({ colors }) => ({
+  colors,
+  start: { x: 1, y: 0 },
+  end: { x: 0, y: 1 },
+}))({
+  width: '100%',
+  height: '100%',
+  borderRadius: getSpacing(3.25),
+  paddingHorizontal: getSpacing(1),
+})
+
+const PastilleContainer = styled.View({
+  position: 'absolute',
+})
+
+const Counter = styled(Typo.Hint)(({ theme }) => ({
+  color: theme.colors.white,
+}))
