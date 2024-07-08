a9fe31b6dc9bd8c64e8cfa44f0148c155f188a2f
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-07-08 15:43:13 +0200

    removed comment

--- src/shared/AccessibilityFooter/AccessibilityFooter.web.test.tsx
+++ src/shared/AccessibilityFooter/AccessibilityFooter.web.test.tsx
@@ -5,7 +5,6 @@ import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
 import { AccessibilityFooter } from 'shared/AccessibilityFooter/AccessibilityFooter'
 import { fireEvent, render, screen } from 'tests/utils/web'
 
-// This mock should not be necessary (test should not make any requests)
 jest.mock('libs/firebase/analytics/analytics')
 jest.mock('libs/firebase/remoteConfig/remoteConfig.services')
 
