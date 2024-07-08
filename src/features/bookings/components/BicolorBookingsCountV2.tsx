33cf429df59587842340d991c86a9aa32c23079d
YassinL <yassin.leclercq1@gmail.com>
2024-07-08 09:50:12 +0200

    (PC-30519) fix(BicolorBookingsCountV2):  use primary color instead of linear gradient

--- src/features/bookings/components/BicolorBookingsCountV2.tsx
+++ src/features/bookings/components/BicolorBookingsCountV2.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Animated } from 'react-native'
-import LinearGradient from 'react-native-linear-gradient'
-import { useTheme } from 'styled-components'
 import styled from 'styled-components/native'
 
 import { useScaleAnimation } from 'features/favorites/helpers/useScaleFavoritesAnimation'
@@ -16,8 +14,6 @@ export const BicolorBookingsCountV2: React.FC<AccessibleBicolorIcon> = ({
   color2,
   testID,
 }) => {
-  const theme = useTheme()
-
   //TODO(PC-29452): Update when got back info
   let bookingsCount: number
   // eslint-disable-next-line prefer-const
@@ -36,9 +32,7 @@ export const BicolorBookingsCountV2: React.FC<AccessibleBicolorIcon> = ({
       <BicolorBookingsV2 size={size} color={color} color2={color2} />
       <StyledAnimatedView style={{ transform: [{ scale }] }}>
         <PastilleContainer accessibilityLabel={accessibilityLabel}>
-          <StyledLinearGradient colors={[theme.colors.primary, theme.colors.secondary]}>
-            <Counter>{fullCountLabel}</Counter>
-          </StyledLinearGradient>
+          <Counter>{fullCountLabel}</Counter>
         </PastilleContainer>
       </StyledAnimatedView>
     </Container>
@@ -53,20 +47,12 @@ const StyledAnimatedView = styled(Animated.View)({
   top: 0,
 })
 
-const StyledLinearGradient = styled(LinearGradient).attrs(({ colors }) => ({
-  colors,
-  start: { x: 1, y: 0 },
-  end: { x: 0, y: 1 },
-}))({
-  width: '100%',
-  height: '100%',
+const PastilleContainer = styled.View(({ theme }) => ({
+  position: 'absolute',
+  backgroundColor: theme.colors.primary,
   borderRadius: getSpacing(3.25),
   paddingHorizontal: getSpacing(1),
-})
-
-const PastilleContainer = styled.View({
-  position: 'absolute',
-})
+}))
 
 const Counter = styled(Typo.Hint)(({ theme }) => ({
   color: theme.colors.white,
