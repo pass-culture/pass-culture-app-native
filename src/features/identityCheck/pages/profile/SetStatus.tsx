ed0dfedf8d7f39abd79b538d3a47937797eeec6e
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-07-14 18:29:57 +0200

    Refactor SetStatus to use FlatList

--- src/features/identityCheck/pages/profile/SetStatus.tsx
+++ src/features/identityCheck/pages/profile/SetStatus.tsx
@@ -1,27 +1,30 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useForm, Controller } from 'react-hook-form'
-import { View } from 'react-native'
+import { LayoutChangeEvent, View, ListRenderItem, FlatList } from 'react-native'
+import styled from 'styled-components'
 import { v4 as uuidv4 } from 'uuid'
 
-import { ActivityIdEnum } from 'api/gen'
+import { ActivityIdEnum, ActivityResponseModel } from 'api/gen'
 import { useActivityTypes } from 'features/identityCheck/api/useActivityTypes'
 import { usePatchProfile } from 'features/identityCheck/api/usePatchProfile'
 import { CenteredTitle } from 'features/identityCheck/components/CenteredTitle'
-import { PageWithHeader } from 'features/identityCheck/components/layout/PageWithHeader'
 import { useNavigateForwardToStepper } from 'features/identityCheck/helpers/useNavigateForwardToStepper'
 import { useSaveStep } from 'features/identityCheck/pages/helpers/useSaveStep'
 import { useAddress } from 'features/identityCheck/pages/profile/store/addressStore'
 import { useCity } from 'features/identityCheck/pages/profile/store/cityStore'
 import { useName } from 'features/identityCheck/pages/profile/store/nameStore'
 import { IdentityCheckStep } from 'features/identityCheck/types'
-import { AccessibilityRole } from 'libs/accessibilityRole/accessibilityRole'
 import { analytics } from 'libs/analytics'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { Form } from 'ui/components/Form'
+import { BlurHeader } from 'ui/components/headers/BlurHeader'
+import {
+  PageHeaderWithoutPlaceholder,
+  useGetHeaderHeight,
+} from 'ui/components/headers/PageHeaderWithoutPlaceholder'
 import { Li } from 'ui/components/Li'
 import { RadioSelector } from 'ui/components/radioSelector/RadioSelector'
-import { VerticalUl } from 'ui/components/Ul'
-import { Spacer } from 'ui/theme'
+import { getSpacing, Spacer } from 'ui/theme'
 
 type StatusForm = {
   selectedStatus: ActivityIdEnum | null
@@ -70,37 +73,57 @@ export const SetStatus = () => {
     [storedName, storedCity, storedAddress, patchProfile, saveStep, navigateForwardToStepper]
   )
 
+  const [bottomChildrenViewHeight, setBottomChildrenViewHeight] = useState(0)
+
+  const headerHeight = useGetHeaderHeight()
+
+  function onFixedBottomChildrenViewLayout(event: LayoutChangeEvent) {
+    const { height } = event.nativeEvent.layout
+    setBottomChildrenViewHeight(height)
+  }
+
+  const renderItem: ListRenderItem<ActivityResponseModel> = ({ item }) => (
+    <Li key={item.label}>
+      <Controller
+        control={control}
+        name="selectedStatus"
+        render={({ field: { value, onChange } }) => (
+          <RadioSelector
+            checked={item.id === value}
+            label={item.label}
+            description={item.description}
+            onPress={() => onChange(item.id)}
+          />
+        )}
+      />
+      <Spacer.Column numberOfSpaces={3} />
+    </Li>
+  )
+  const flatListContainer = {
+    flexGrow: 1,
+    paddingBottom: bottomChildrenViewHeight,
+    paddingHorizontal: getSpacing(5),
+  }
+
   return (
-    <PageWithHeader
-      title="Profil"
-      scrollChildren={
-        <Form.MaxWidth>
-          <CenteredTitle titleID={titleID} title="Sélectionne ton statut" />
-          <Spacer.Column numberOfSpaces={5} />
-          <View accessibilityRole={AccessibilityRole.RADIOGROUP} accessibilityLabelledBy={titleID}>
-            <Controller
-              control={control}
-              name="selectedStatus"
-              render={({ field: { value, onChange } }) => (
-                <VerticalUl>
-                  {activities?.map((activity) => (
-                    <Li key={activity.label}>
-                      <RadioSelector
-                        checked={activity.id === value}
-                        label={activity.label}
-                        description={activity.description}
-                        onPress={() => onChange(activity.id)}
-                      />
-                      <Spacer.Column numberOfSpaces={3} />
-                    </Li>
-                  ))}
-                </VerticalUl>
-              )}
-            />
-          </View>
-        </Form.MaxWidth>
-      }
-      fixedBottomChildren={
+    <React.Fragment>
+      <PageHeaderWithoutPlaceholder title="Profil" />
+      <FlatList
+        contentContainerStyle={flatListContainer}
+        data={activities}
+        keyExtractor={(item) => item.label}
+        ListHeaderComponent={
+          <React.Fragment>
+            <View style={{ height: headerHeight }} />
+            <Form.MaxWidth>
+              <CenteredTitle titleID={titleID} title="Sélectionne ton statut" />
+              <Spacer.Column numberOfSpaces={5} />
+            </Form.MaxWidth>
+          </React.Fragment>
+        }
+        renderItem={renderItem}
+      />
+      <FixedBottomView onLayout={onFixedBottomChildrenViewLayout}>
         <ButtonPrimary
           type="submit"
           onPress={handleSubmit(submitStatus)}
@@ -111,7 +134,20 @@ export const SetStatus = () => {
           isLoading={isLoading}
           disabled={!selectedStatus}
         />
-      }
-    />
+        <Spacer.BottomScreen />
+      </FixedBottomView>
+      <BlurHeader height={headerHeight} />
+    </React.Fragment>
   )
 }
+
+const FixedBottomView = styled(View)(({ theme }) => ({
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  paddingBottom: getSpacing(5),
+  paddingTop: getSpacing(3),
+  backgroundColor: theme.colors.white,
+  paddingHorizontal: getSpacing(5),
+}))
