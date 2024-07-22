9429a910f8292ad5fa771282aaec7fcc17075377
bruno ebstein <bruno.ebstein@passculture.app>
2024-07-22 19:35:32 +0200

    (BSR) fix(dependencies): allow to git clone https dependencies
    without this, we got the following error :
    
    ```sh
    git clone 'https://github.com/openid/AppAuth-iOS.git/'
    ```
    
    ```txt
    fatal : impossible d'accéder à 'https://github.com/openid/AppAuth-iOS.git/' : OpenSSL/3.0.14: error:16000069:STORE routines::unregistered scheme
    ````

--- scripts/load_certificate.sh
+++ scripts/load_certificate.sh
@@ -6,4 +6,5 @@ SSL_CERT_FILE='/Library/Application Support/Netskope/STAgent/data/nscacert.pem'
 if [ -f "$SSL_CERT_FILE" ]; then
 	export SSL_CERT_FILE="$SSL_CERT_FILE"
 	export NODE_EXTRA_CA_CERTS="$SSL_CERT_FILE"
+	export NIX_SSL_CERT_FILE="$SSL_CERT_FILE"
 fi
