2da2040944eaef9776a15cf4d4f7b0b6448a09d7
NolwenMajorFrances <144016890+NolwenMajorFrances@users.noreply.github.com>
2024-07-25 14:43:03 +0200

    test(VideoPlayerWeb) : create mock and add tests

---
+++ __mocks__/react-youtube.ts
@@ -0,0 +1,79 @@
+import React, { ForwardedRef, ReactInstance } from 'react'
+import YouTube, { YouTubeProps } from 'react-youtube'
+
+import { PlayerState } from 'features/home/components/modules/video/types'
+
+let mockState = PlayerState.UNSTARTED
+let mockError = false
+const mockEvent = { data: YouTube.PlayerState.ENDED, target: jest.fn() }
+const YouTubePlayerMock = React.forwardRef(function Component(
+  props: YouTubeProps,
+  ref: ForwardedRef<YouTube>
+) {
+  React.useEffect(() => {
+    if (props.onReady) props.onReady(mockEvent)
+    if (props.onStateChange) props.onStateChange(mockEvent)
+    if (props.onError && mockError) props.onError(mockEvent)
+  }, [props])
+
+  if (typeof ref === 'object' && ref !== null) {
+    ref.current = {
+      internalPlayer: {
+        getCurrentTime: jest.fn().mockReturnValue(134.9),
+        getDuration: jest.fn().mockReturnValue(267.4),
+        getVideoUrl: jest.fn(),
+        isMuted: jest.fn(),
+        getVolume: jest.fn(),
+        getPlaybackRate: jest.fn(),
+        getAvailablePlaybackRates: jest.fn(),
+        seekTo: jest.fn(),
+      },
+      container: null,
+      destroyPlayerPromise: undefined,
+      componentDidMount: jest.fn(),
+      componentDidUpdate: jest.fn(),
+      componentWillUnmount: jest.fn(),
+      onPlayerReady: jest.fn(),
+      onPlayerError: jest.fn(),
+      onPlayerStateChange: jest.fn(),
+      onPlayerPlaybackRateChange: jest.fn(),
+      onPlayerPlaybackQualityChange: jest.fn(),
+      destroyPlayer: jest.fn(),
+      createPlayer: jest.fn(),
+      resetPlayer: jest.fn(),
+      updatePlayer: jest.fn(),
+      getInternalPlayer: jest.fn(),
+      updateVideo: jest.fn(),
+      refContainer: jest.fn(),
+      render: jest.fn(),
+      context: jest.fn(),
+      setState: jest.fn(),
+      forceUpdate: jest.fn(),
+      state: mockState,
+      props,
+      refs: null as unknown as { [key: string]: ReactInstance },
+    }
+  }
+  // @ts-ignore avoid internal typing complexity
+  return React.createElement(YouTube, props)
+})
+
+const setPlayerState = (playerState: PlayerState) => {
+  mockState = playerState
+}
+
+const setError = (error: boolean) => {
+  mockError = error
+}
+
+const MockedYoutubePlayer = YouTubePlayerMock as typeof YouTubePlayerMock & {
+  setPlayerState: (playerState: PlayerState) => void
+  setError: (error: boolean) => void
+  PlayerState: typeof YouTube.PlayerState
+}
+
+MockedYoutubePlayer.setPlayerState = setPlayerState
+MockedYoutubePlayer.setError = setError
+MockedYoutubePlayer.PlayerState = YouTube.PlayerState
+
+export default MockedYoutubePlayer
---
+++ src/features/home/components/modules/video/VideoPlayerWeb.web.test.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import YouTube from 'react-youtube'
+
+import MockedYouTubePlayer from '__mocks__/react-youtube'
+import { PlayerState } from 'features/home/components/modules/video/types'
+import { VideoPlayerWeb } from 'features/home/components/modules/video/VideoPlayerWeb.web'
+import { videoModuleFixture } from 'features/home/fixtures/videoModule.fixture'
+import { mockedAlgoliaResponse } from 'libs/algolia/fixtures/algoliaFixtures'
+import { analytics } from 'libs/analytics'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render, waitFor } from 'tests/utils/web'
+
+const mockOffer = mockedAlgoliaResponse.hits[0]
+const hideModalMock = jest.fn()
+
+const mockRef = {
+  current: {
+    internalPlayer: {
+      getCurrentTime: jest.fn(),
+      getDuration: jest.fn(),
+      getVideoUrl: jest.fn(),
+      isMuted: jest.fn(),
+      getVolume: jest.fn(),
+      getPlaybackRate: jest.fn(),
+      getAvailablePlaybackRates: jest.fn(),
+      seekTo: jest.fn(),
+    },
+  },
+}
+
+jest.mock('libs/firebase/analytics/analytics')
+
+describe('VideoPlayer', () => {
+  describe('analytics', () => {
+    it('should logHasSeenAllVideo when all video were seen', async () => {
+      MockedYouTubePlayer.setPlayerState(PlayerState.ENDED)
+
+      renderVideoPlayer()
+
+      await waitFor(() => {
+        expect(analytics.logHasSeenAllVideo).toHaveBeenCalledWith({
+          moduleId: 'abcd',
+          videoDuration: 267,
+          seenDuration: 135,
+        })
+      })
+    })
+
+    it('should logConsultVideo when player is ready', async () => {
+      MockedYouTubePlayer.setPlayerState(PlayerState.UNSTARTED)
+
+      renderVideoPlayer()
+
+      await waitFor(() => {
+        expect(analytics.logConsultVideo).toHaveBeenCalledWith({
+          from: 'home',
+          homeEntryId: 'xyz',
+          moduleId: 'abcd',
+        })
+      })
+    })
+  })
+})
+
+function renderVideoPlayer() {
+  render(
+    reactQueryProviderHOC(
+      <VideoPlayerWeb
+        youtubeVideoId={videoModuleFixture.youtubeVideoId}
+        offer={mockOffer}
+        onPressSeeOffer={hideModalMock}
+        moduleId="abcd"
+        moduleName="lujipeka"
+        homeEntryId="xyz"
+        playerRef={mockRef as React.RefObject<YouTube>}
+      />
+    )
+  )
+}
