172acb4a38afd34028be48785ece0ead169e74cb
NolwenMajorFrances <144016890+NolwenMajorFrances@users.noreply.github.com>
2024-07-29 11:46:28 +0200

    Add more testing coverage

--- src/features/home/components/modules/video/VideoPlayer.native.test.tsx
+++ src/features/home/components/modules/video/VideoPlayer.native.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import MockedYouTubePlayer from '__mocks__/react-native-youtube-iframe'
+import { PlayerState } from 'features/home/components/modules/video/types'
 import { VideoPlayer } from 'features/home/components/modules/video/VideoPlayer'
 import { videoModuleFixture } from 'features/home/fixtures/videoModule.fixture'
+import { AccessibilityRole } from 'libs/accessibilityRole/accessibilityRole'
 import { mockedAlgoliaResponse } from 'libs/algolia/fixtures/algoliaFixtures'
-import { render, screen } from 'tests/utils'
+import { analytics } from 'libs/analytics'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { fireEvent, render, screen, waitFor } from 'tests/utils'
+
 const mockOffer = mockedAlgoliaResponse.hits[0]
 const hideModalMock = jest.fn()
 
@@ -25,6 +31,7 @@ const mockRef = {
 
 const showError: unknown = true
 const hideError: unknown = false
+jest.mock('libs/firebase/analytics/analytics')
 
 describe('VideoPlayer', () => {
   it('should render error view when showErrorView is true', async () => {
@@ -52,18 +59,63 @@ describe('VideoPlayer', () => {
 
     expect(errorMessage).not.toBeOnTheScreen()
   })
+
+  it('should not have replay button visible after clicked', async () => {
+    MockedYouTubePlayer.setPlayerState(PlayerState.ENDED)
+
+    renderVideoPlayer()
+
+    const replayButton = await screen.findByRole(AccessibilityRole.BUTTON, {
+      name: 'Revoir la vidéo',
+    })
+    fireEvent.press(replayButton)
+
+    expect(replayButton).not.toBeOnTheScreen()
+  })
+
+  describe('analytics', () => {
+    it('should logHasSeenAllVideo when all video were seen', async () => {
+      MockedYouTubePlayer.setPlayerState(PlayerState.ENDED)
+
+      renderVideoPlayer()
+
+      await waitFor(() => {
+        expect(analytics.logHasSeenAllVideo).toHaveBeenCalledWith({
+          moduleId: 'abcd',
+          videoDuration: 267,
+          seenDuration: 135,
+        })
+      })
+    })
+
+    it('should logConsultVideo when player is ready', async () => {
+      MockedYouTubePlayer.setPlayerState(PlayerState.UNSTARTED)
+
+      renderVideoPlayer()
+
+      await waitFor(() => {
+        expect(analytics.logConsultVideo).toHaveBeenCalledWith({
+          from: 'home',
+          homeEntryId: 'xyz',
+          moduleId: 'abcd',
+        })
+      })
+    })
+  })
 })
 
 function renderVideoPlayer() {
   render(
-    <VideoPlayer
-      youtubeVideoId={videoModuleFixture.youtubeVideoId}
-      offer={mockOffer}
-      onPressSeeOffer={hideModalMock}
-      moduleId="abcd"
-      moduleName="lujipeka"
-      homeEntryId="xyz"
-      playerRef={mockRef}
-    />
+    reactQueryProviderHOC(
+      <VideoPlayer
+        youtubeVideoId={videoModuleFixture.youtubeVideoId}
+        offer={mockOffer}
+        onPressSeeOffer={hideModalMock}
+        moduleId="abcd"
+        moduleName="lujipeka"
+        homeEntryId="xyz"
+        playerRef={mockRef}
+      />
+    )
   )
 }
