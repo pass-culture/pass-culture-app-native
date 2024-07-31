bc8da693d011c66db61b5dc70b61805a01993c60
Maxime Le Duc <maxime.leduc@passculture.app>
2024-07-31 17:06:58 +0200

    Add accesibility test for change status

---
+++ src/features/profile/pages/ChangeStatus/ChangeStatus.web.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { ActivityTypesResponse } from 'api/gen'
+import { ActivityTypesSnap } from 'features/identityCheck/pages/profile/fixtures/mockedActivityTypes'
+import { ChangeStatus } from 'features/profile/pages/ChangeStatus/ChangeStatus'
+import { mockServer } from 'tests/mswServer'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render, checkAccessibilityFor, act } from 'tests/utils/web'
+
+jest.mock('features/identityCheck/context/SubscriptionContextProvider')
+
+describe('<ChangeStatus/>', () => {
+  beforeEach(() => {
+    mockServer.getApi<ActivityTypesResponse>('/v1/subscription/activity_types', ActivityTypesSnap)
+  })
+
+  describe('Accessibility', () => {
+    it('should not have basic accessibility issues', async () => {
+      const { container } = render(reactQueryProviderHOC(<ChangeStatus />))
+
+      await act(async () => {
+        const results = await checkAccessibilityFor(container)
+
+        expect(results).toHaveNoViolations()
+      })
+    })
+  })
+})
