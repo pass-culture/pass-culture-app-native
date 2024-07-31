f242cc9216d65f979a08d15a48a7a8ced3e72a71
bruno ebstein <bruno.ebstein@passculture.app>
2024-07-31 18:36:18 +0200

    add brew-nix to add required dependencies

--- flake.nix
+++ flake.nix
@@ -2,20 +2,39 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
+  inputs.brew-api.url = "github:BatteredBunny/brew-api";
+  inputs.brew-api.flake = false;
+  inputs.brew-nix.url = "github:BatteredBunny/brew-nix";
+  inputs.brew-nix.inputs.brew-api.follows = "brew-api";
+  inputs.brew-nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.brew-nix.inputs.flake-utils.follows = "flake-utils";
+
   outputs =
     { self
     , nixpkgs
     , flake-utils
+    , brew-nix
+    , ...
     }:
     flake-utils.lib.eachDefaultSystem (system: {
       devShell =
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              brew-nix.overlays.default
+            ];
+          };
+          inherit (pkgs) lib;
         in
         pkgs.mkShellNoCC {
           packages = [
             pkgs.devbox
-          ];
+          ]
+          ++
+          (lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+            pkgs.brewCasks.firefox
+          ]);
         };
     });
 }
