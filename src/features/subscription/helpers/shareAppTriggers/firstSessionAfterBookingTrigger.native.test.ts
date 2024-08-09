39161b09c78e5ec4d5259433b346a072d5b4a3fc
Maxime Le Duc <maxime.leduc@passculture.app>
2024-08-09 11:39:37 +0200

    move booking fixture

--- src/features/subscription/helpers/shareAppTriggers/firstSessionAfterBookingTrigger.native.test.ts
+++ src/features/subscription/helpers/shareAppTriggers/firstSessionAfterBookingTrigger.native.test.ts
@@ -1,52 +1,6 @@
-import { BookingsResponse, SubcategoryIdEnum } from 'api/gen'
+import { bookingFixture } from 'features/bookings/fixtures/bookingsSnap'
 
 import { firstSessionAfterBookingTrigger } from './firstSessionAfterBookingTrigger'
-
-const booking: BookingsResponse['ongoing_bookings'][0] = {
-  id: 123,
-  cancellationDate: null,
-  cancellationReason: null,
-  confirmationDate: '2021-03-15T23:01:37.925926',
-  dateCreated: '2021-02-15T23:01:37.925926',
-  dateUsed: null,
-  expirationDate: null,
-  totalAmount: 1900,
-  token: '352UW4',
-  quantity: 10,
-  qrCodeData: 'PASSCULTURE:v3;TOKEN:352UW4',
-  stock: {
-    id: 150230,
-    beginningDatetime: '2021-03-15T20:00:00',
-    price: 400,
-    priceCategoryLabel: 'Cat 4',
-    features: ['VOSTFR', '3D', 'IMAX'],
-    offer: {
-      id: 147874,
-      bookingContact: null,
-      name: 'Avez-vous déjà vu\u00a0?',
-      extraData: {
-        ean: '123456789',
-      },
-      isPermanent: false,
-      isDigital: true,
-      subcategoryId: SubcategoryIdEnum.EVENEMENT_PATRIMOINE,
-      venue: {
-        id: 2185,
-        city: 'Drancy',
-        name: 'Maison de la Brique',
-        coordinates: {
-          latitude: 48.91683,
-          longitude: 2.43884,
-        },
-        address: '1 boulevard de la brique',
-        postalCode: '93700',
-        timezone: 'Europe/Paris',
-      },
-      withdrawalDetails: null,
-    },
-  },
-  externalBookings: [],
-}
 
 describe('First session after booking trigger', () => {
   it('should be true when first session after last booking was consumed', () => {
@@ -54,7 +8,7 @@ describe('First session after booking trigger', () => {
     const dateUsed = new Date('2024-07-26T12:00:00').toISOString()
     const shouldTrigger = firstSessionAfterBookingTrigger({
       currentDate,
-      ongoingBookings: [{ ...booking, dateUsed }],
+      ongoingBookings: [{ ...bookingFixture, dateUsed }],
     })()
 
     expect(shouldTrigger).toBe(true)
@@ -65,7 +19,7 @@ describe('First session after booking trigger', () => {
     const dateUsed = undefined
     const shouldTrigger = firstSessionAfterBookingTrigger({
       currentDate,
-      ongoingBookings: [{ ...booking, dateUsed }],
+      ongoingBookings: [{ ...bookingFixture, dateUsed }],
     })()
 
     expect(shouldTrigger).toBe(false)
