4edf6c341fb70e161d07ea130068354bb194c06c
Maxime Le Duc <maxime.leduc@passculture.app>
2024-08-19 14:52:35 +0200

    Add tests

--- src/features/profile/pages/DeleteProfile/DeleteProfileAccountHacked.native.test.tsx
+++ src/features/profile/pages/DeleteProfile/DeleteProfileAccountHacked.native.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { render, screen } from 'tests/utils'
+import { navigate } from '__mocks__/@react-navigation/native'
+import { fireEvent, render, screen } from 'tests/utils'
 
 import { DeleteProfileAccountHacked } from './DeleteProfileAccountHacked'
 
@@ -9,5 +10,32 @@ describe('DeleteProfileAccountHacked', () => {
     render(<DeleteProfileAccountHacked />)
 
     expect(screen).toMatchSnapshot()
+  })
+
+  it('should navigate to profile on press ne pas sécuriser mon compte', async () => {
+    render(<DeleteProfileAccountHacked />)
+    const button = screen.getByText('Ne pas sécuriser mon compte')
+
+    fireEvent.press(button)
+
+    expect(navigate).toHaveBeenCalledWith('TabNavigator', { params: undefined, screen: 'Profile' })
+  })
+
+  it('should navigate to change password on press Modifier mon mot de passe', async () => {
+    render(<DeleteProfileAccountHacked />)
+    const button = screen.getByText('Modifier mon mot de passe')
+
+    fireEvent.press(button)
+
+    expect(navigate).toHaveBeenCalledWith('ChangePassword')
+  })
+
+  it('should navigate to confirm delete profile on press Susprendre mon compte', async () => {
+    render(<DeleteProfileAccountHacked />)
+    const button = screen.getByText('Suspendre mon compte')
+
+    fireEvent.press(button)
+
+    expect(navigate).toHaveBeenCalledWith('ConfirmDeleteProfile')
   })
 })
--- src/features/profile/pages/DeleteProfile/DeleteProfileEmailHacked.native.test.tsx
+++ src/features/profile/pages/DeleteProfile/DeleteProfileEmailHacked.native.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { render, screen } from 'tests/utils'
+import { navigate } from '__mocks__/@react-navigation/native'
+import { fireEvent, render, screen } from 'tests/utils'
 
 import { DeleteProfileEmailHacked } from './DeleteProfileEmailHacked'
 
@@ -9,5 +10,32 @@ describe('DeleteProfileEmailHacked', () => {
     render(<DeleteProfileEmailHacked />)
 
     expect(screen).toMatchSnapshot()
+  })
+
+  it('should navigate to profile on press ne pas sécuriser mon compte', async () => {
+    render(<DeleteProfileEmailHacked />)
+    const button = screen.getByText('Ne pas sécuriser mon compte')
+
+    fireEvent.press(button)
+
+    expect(navigate).toHaveBeenCalledWith('TabNavigator', { params: undefined, screen: 'Profile' })
+  })
+
+  it('should navigate to change email on press Modifier mon adresse e-mail', async () => {
+    render(<DeleteProfileEmailHacked />)
+    const button = screen.getByText('Modifier mon adresse e-mail')
+
+    fireEvent.press(button)
+
+    expect(navigate).toHaveBeenCalledWith('ChangeEmail')
+  })
+
+  it('should navigate to confirm delete profile on press Susprendre mon compte', async () => {
+    render(<DeleteProfileEmailHacked />)
+    const button = screen.getByText('Suspendre mon compte')
+
+    fireEvent.press(button)
+
+    expect(navigate).toHaveBeenCalledWith('ConfirmDeleteProfile')
   })
 })
