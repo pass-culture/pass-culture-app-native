d5057473457e74aa15dd6851d8269e7d5f8101bb
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-08-19 16:57:00 +0200

    (PC-31361) fix(shared): add sentry error when duration calculation in useTrackDuration hook <= 0

--- src/shared/hook/useTrackDuration.ts
+++ src/shared/hook/useTrackDuration.ts
@@ -1,6 +1,7 @@
 import { useCallback, useRef } from 'react'
 
 import { useAppStateChange } from 'libs/appState'
+import { eventMonitoring } from 'libs/monitoring'
 
 /**
  * Use it in a useEffect or useFocusEffect, The passed callback should be wrapped in React.useCallback to avoid running the effect too often.
@@ -27,6 +28,18 @@ export const useTrackDuration = (callback: (durationInSeconds: number) => void) 
       const endTime = new Date()
       const totalDurationOnPage = (endTime.getTime() - startTime.getTime()) / 1000
       const durationWithoutBackgroundTime = totalDurationOnPage - timeInBackground.current
+      if (durationWithoutBackgroundTime <= 0) {
+        eventMonitoring.captureException(
+          `Error with useTrackDuration hook, duration calculated <= 0`,
+          {
+            extra: {
+              timeInBackground: timeInBackground.current,
+              startTimeBackground: startTimeBackground.current,
+              totalDurationOnPage,
+            },
+          }
+        )
+      }
       callback(Number(durationWithoutBackgroundTime.toFixed(3)))
 
       // Resetting the refs to initial values
