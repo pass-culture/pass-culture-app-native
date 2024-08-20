661fd829858b036be59c8effa30d359279f82f51
Christopher Gerrard <christopher.gerrard@passculture.app>
2024-08-20 11:54:15 +0200

    DRY

--- src/features/profile/helpers/getEmailUpdateStep.ts
+++ src/features/profile/helpers/getEmailUpdateStep.ts
@@ -4,13 +4,14 @@ export function getEmailUpdateStep(
   hasRecentlyResetPassword: boolean,
   step?: EmailHistoryEventTypeEnum
 ) {
+  const hasReset = hasRecentlyResetPassword ? 1 : 0
   switch (step) {
     case EmailHistoryEventTypeEnum.CONFIRMATION:
-      return 1 + Number(hasRecentlyResetPassword)
+      return 1 + hasReset
     case EmailHistoryEventTypeEnum.NEW_EMAIL_SELECTION:
-      return 2 + Number(hasRecentlyResetPassword)
+      return 2 + hasReset
     case EmailHistoryEventTypeEnum.VALIDATION:
-      return 3 + Number(hasRecentlyResetPassword)
+      return 3 + hasReset
     default:
       return 0
   }
