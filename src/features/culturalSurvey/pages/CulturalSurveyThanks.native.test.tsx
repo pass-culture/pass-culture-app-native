4c29b1c894d9946422ca835ab88ffbe90369689f
Maxime Le Duc <maxime.leduc@passculture.app>
2024-09-02 01:33:35 +0200

    Remove shareapp after QPI

--- src/features/culturalSurvey/pages/CulturalSurveyThanks.native.test.tsx
+++ src/features/culturalSurvey/pages/CulturalSurveyThanks.native.test.tsx
@@ -2,24 +2,11 @@ import React from 'react'
 
 import { reset } from '__mocks__/@react-navigation/native'
 import { CulturalSurveyThanks } from 'features/culturalSurvey/pages/CulturalSurveyThanks'
-import * as ShareAppWrapperModule from 'features/share/context/ShareAppWrapper'
-import { ShareAppModalType } from 'features/share/types'
-import { storage } from 'libs/storage'
-import { render, fireEvent, screen, act } from 'tests/utils'
+import { render, fireEvent, screen } from 'tests/utils'
 
 jest.mock('features/navigation/helpers/navigateToHome')
-const mockShowAppModal = jest.fn()
-jest
-  .spyOn(ShareAppWrapperModule, 'useShareAppContext')
-  .mockReturnValue({ showShareAppModal: mockShowAppModal })
-
-const SHARE_APP_MODAL_STORAGE_KEY = 'has_seen_share_app_modal'
 
 describe('CulturalSurveyThanksPage page', () => {
-  beforeEach(() => {
-    storage.clear(SHARE_APP_MODAL_STORAGE_KEY)
-  })
-
   it('should render the page with correct layout', () => {
     render(<CulturalSurveyThanks />)
 
@@ -35,40 +22,5 @@ describe('CulturalSurveyThanksPage page', () => {
       index: 0,
       routes: [{ name: 'TabNavigator' }],
     })
-  })
-
-  it('should show ShareAppModal when pressing Découvrir le catalogue', async () => {
-    render(<CulturalSurveyThanks />)
-
-    const discoverButton = screen.getByText('Découvrir le catalogue')
-    await act(() => {
-      fireEvent.press(discoverButton)
-    })
-
-    expect(mockShowAppModal).toHaveBeenNthCalledWith(1, ShareAppModalType.BENEFICIARY)
-  })
-
-  it('should not show ShareAppModal when ShareAppModal already shown', async () => {
-    storage.saveObject(SHARE_APP_MODAL_STORAGE_KEY, true)
-
-    render(<CulturalSurveyThanks />)
-    const discoverButton = screen.getByText('Découvrir le catalogue')
-    await act(() => {
-      fireEvent.press(discoverButton)
-    })
-
-    expect(mockShowAppModal).not.toHaveBeenCalled()
-  })
-
-  it('should save that ShareAppModal was shown in storage', async () => {
-    render(<CulturalSurveyThanks />)
-    const discoverButton = screen.getByText('Découvrir le catalogue')
-    await act(() => {
-      fireEvent.press(discoverButton)
-    })
-
-    const hasSeenShareAppModal = await storage.readObject(SHARE_APP_MODAL_STORAGE_KEY)
-
-    expect(hasSeenShareAppModal).toBe(true)
   })
 })
--- src/features/culturalSurvey/pages/CulturalSurveyThanks.tsx
+++ src/features/culturalSurvey/pages/CulturalSurveyThanks.tsx
@@ -4,29 +4,19 @@ import styled from 'styled-components/native'
 
 import { navigateToHomeConfig } from 'features/navigation/helpers/navigateToHome'
 import { UseNavigationType } from 'features/navigation/RootNavigator/types'
-import { useShareAppContext } from 'features/share/context/ShareAppWrapper'
-import { ShareAppModalType } from 'features/share/types'
-import { storage } from 'libs/storage'
 import QpiThanks from 'ui/animations/qpi_thanks.json'
 import { ButtonPrimary } from 'ui/components/buttons/ButtonPrimary'
 import { GenericInfoPageWhite } from 'ui/pages/GenericInfoPageWhite'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 
-const SHARE_APP_MODAL_STORAGE_KEY = 'has_seen_share_app_modal'
-
 export const CulturalSurveyThanks: React.FC = () => {
   const { reset } = useNavigation<UseNavigationType>()
-  const { showShareAppModal } = useShareAppContext()
 
   const navigateToHomeAndShowShareAppModal = async () => {
     reset({
       index: 0,
       routes: [{ name: navigateToHomeConfig.screen }],
     })
-    const hasSeenShareAppModal = await storage.readObject(SHARE_APP_MODAL_STORAGE_KEY)
-    if (hasSeenShareAppModal) return
-    showShareAppModal(ShareAppModalType.BENEFICIARY)
-    await storage.saveObject(SHARE_APP_MODAL_STORAGE_KEY, true)
   }
 
   return (
