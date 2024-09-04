7f29a65443ab657080e9da2111873db1f20bcb8c
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-09-04 11:19:57 +0200

    (PC-31542) test(app): fix tests

--- src/features/profile/pages/DeleteProfile/DeleteProfileAccountNotDeletable.native.test.tsx
+++ src/features/profile/pages/DeleteProfile/DeleteProfileAccountNotDeletable.native.test.tsx
@@ -6,6 +6,19 @@ import { fireEvent, render, screen } from 'tests/utils'
 import { DeleteProfileAccountNotDeletable } from './DeleteProfileAccountNotDeletable'
 
 jest.mock('libs/firebase/analytics/analytics')
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
+jest.mock('@batch.com/react-native-plugin', () =>
+  jest.requireActual('__mocks__/libs/react-native-batch')
+)
+jest.mock('react-native-safe-area-context', () => ({
+  ...(jest.requireActual('react-native-safe-area-context') as Record<string, unknown>),
+  useSafeAreaInsets: () => ({ bottom: 16, right: 16, left: 16, top: 16 }),
+}))
+jest.mock('react-native/Libraries/Animated/createAnimatedComponent', () => {
+  return function createAnimatedComponent(Component: unknown) {
+    return Component
+  }
+})
 
 describe('DeleteProfileAccountNotDeletable', () => {
   it('should render correctly', () => {
