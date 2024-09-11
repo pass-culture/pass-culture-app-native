8c8b3c91d75aeb031390724140ad330465d55701
Maxime Le Duc <maxime.leduc@passculture.app>
2024-09-11 11:48:26 +0200

    Create screen SuspendAccountConfirmationNoToken

---
+++ src/features/profile/pages/DeleteProfile/SuspendAccountConfirmationNoToken.native.test.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+
+import { navigate } from '__mocks__/@react-navigation/native'
+import * as API from 'api/api'
+import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
+import { analytics } from 'libs/analytics/__mocks__/provider'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { fireEvent, render, screen, waitFor } from 'tests/utils'
+
+import { SuspendAccountConfirmationNoToken } from './SuspendAccountConfirmationNoToken'
+
+jest.spyOn(NavigationHelpers, 'openUrl')
+
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
+jest.mock('libs/firebase/analytics/analytics')
+
+jest.mock('@batch.com/react-native-plugin', () =>
+  jest.requireActual('__mocks__/libs/react-native-batch')
+)
+
+jest.mock('react-native-safe-area-context', () => ({
+  ...(jest.requireActual('react-native-safe-area-context') as Record<string, unknown>),
+  useSafeAreaInsets: () => ({ bottom: 16, right: 16, left: 16, top: 16 }),
+}))
+
+const mockShowErrorSnackBar = jest.fn()
+jest.mock('ui/components/snackBar/SnackBarContext', () => ({
+  useSnackBarContext: () => ({ showErrorSnackBar: mockShowErrorSnackBar }),
+}))
+
+const mockApiSuspendAccount = jest.spyOn(API.api, 'postNativeV1AccountSuspend')
+
+describe('SuspendAccountConfirmationNoToken', () => {
+  it('should render correctly', () => {
+    renderSuspendAccountConfirmationNoToken()
+
+    expect(screen).toMatchSnapshot()
+  })
+
+  it('should send analytics event when clicking on "Contacter le support" button', () => {
+    renderSuspendAccountConfirmationNoToken()
+
+    whenPressingContactFraudTeamButton()
+
+    expect(analytics.logContactFraudTeam).toHaveBeenCalledWith({
+      from: 'suspendaccountconfirmation',
+    })
+  })
+
+  it('should show error snackbar when an error occur during account suspension', async () => {
+    renderSuspendAccountConfirmationNoToken()
+    givenAccountSuspendWillFail()
+
+    whenPressingSuspendAccountButton()
+
+    await waitFor(() => {
+      expect(mockShowErrorSnackBar).toHaveBeenCalledWith({
+        message:
+          'Une erreur est survenue. Pour suspendre ton compte, contacte le support par e-mail.',
+      })
+    })
+  })
+
+  it('should navigate to SuspiciousLoginSuspendedAccount when account is suspended', async () => {
+    renderSuspendAccountConfirmationNoToken()
+
+    whenPressingSuspendAccountButton()
+
+    await waitFor(() => {
+      expect(navigate).toHaveBeenCalledWith('SuspiciousLoginSuspendedAccount')
+    })
+  })
+
+  function renderSuspendAccountConfirmationNoToken() {
+    return render(reactQueryProviderHOC(<SuspendAccountConfirmationNoToken />))
+  }
+
+  function whenPressingContactFraudTeamButton() {
+    fireEvent.press(screen.getByText('Contacter le service fraude'))
+  }
+
+  function givenAccountSuspendWillFail() {
+    mockApiSuspendAccount.mockRejectedValueOnce(new Error('An error occurred'))
+  }
+
+  function whenPressingSuspendAccountButton() {
+    fireEvent.press(screen.getByText('Oui, suspendre mon compte'))
+  }
+})
---
+++ src/features/profile/pages/DeleteProfile/SuspendAccountConfirmationNoToken.web.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render, checkAccessibilityFor } from 'tests/utils/web'
+
+import { SuspendAccountConfirmationNoToken } from './SuspendAccountConfirmationNoToken'
+
+jest.spyOn(NavigationHelpers, 'openUrl')
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
+jest.mock('libs/firebase/analytics/analytics')
+jest.mock('libs/firebase/remoteConfig/remoteConfig.services')
+
+jest.mock('react-native-safe-area-context', () => ({
+  ...(jest.requireActual('react-native-safe-area-context') as Record<string, unknown>),
+  useSafeAreaInsets: () => ({ bottom: 16, right: 16, left: 16, top: 16 }),
+}))
+
+describe('SuspendAccountConfirmationNoToken', () => {
+  describe('Accessibility', () => {
+    it('should not have basic accessibility issues', async () => {
+      const { container } = render(reactQueryProviderHOC(<SuspendAccountConfirmationNoToken />))
+      const results = await checkAccessibilityFor(container)
+
+      expect(results).toHaveNoViolations()
+    })
+  })
+})
