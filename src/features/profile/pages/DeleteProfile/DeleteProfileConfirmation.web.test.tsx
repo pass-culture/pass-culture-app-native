fd3627c5221e721de2606cad6ae570e7acbf2a28
Maxime Le Duc <maxime.leduc@passculture.app>
2024-09-16 13:04:01 +0200

    Fix web tests

--- src/features/profile/pages/DeleteProfile/DeleteProfileConfirmation.web.test.tsx
+++ src/features/profile/pages/DeleteProfile/DeleteProfileConfirmation.web.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { checkAccessibilityFor, render } from 'tests/utils/web'
 
 import { DeleteProfileConfirmation } from './DeleteProfileConfirmation'
 
 jest.mock('libs/firebase/remoteConfig/remoteConfig.services')
 
+jest.mock('libs/firebase/analytics/analytics')
+
+jest.spyOn(NavigationHelpers, 'openUrl')
 jest.mock('react-native-safe-area-context', () => ({
   ...(jest.requireActual('react-native-safe-area-context') as Record<string, unknown>),
   useSafeAreaInsets: () => ({ bottom: 16, right: 16, left: 16, top: 16 }),
@@ -14,7 +19,7 @@ jest.mock('react-native-safe-area-context', () => ({
 describe('DeleteProfileConfirmation', () => {
   describe('Accessibility', () => {
     it('should not have basic accessibility issues', async () => {
-      const { container } = render(<DeleteProfileConfirmation />)
+      const { container } = render(reactQueryProviderHOC(<DeleteProfileConfirmation />))
       const results = await checkAccessibilityFor(container)
 
       expect(results).toHaveNoViolations()
