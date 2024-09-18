ab8a7f64e84b59188c78d664403c0b313dc635a8
Maxime Le Duc <maxime.leduc@passculture.app>
2024-09-18 16:46:09 +0200

    Simplify

--- src/libs/contentful/contentfulGetHomeData.native.test.ts
+++ src/libs/contentful/contentfulGetHomeData.native.test.ts
@@ -2,24 +2,15 @@ import { homepageList } from 'features/home/fixtures/homepageList.fixture'
 import { homepageEntriesAPIResponse } from 'libs/contentful/fixtures/homepageEntriesAPIResponse'
 import { mockServer } from 'tests/mswServer'
 
-import { ContentfulAdapterFactory } from './adapters/ContentfulAdapterFactory'
-import { adaptBusinessModule } from './adapters/modules/adaptBusinessModule'
-import { adaptExclusivityModule } from './adapters/modules/adaptExclusivityModule'
-import { adaptOffersModule } from './adapters/modules/adaptOffersModule'
-import { Contentful, createContentful } from './contentful'
-import { ContentfulGetHomeData, createContentfulGetHomeData } from './contentfulGetHomeData'
-import { ContentTypes } from './types'
+import { createContentful } from './contentful'
+import { createContentfulGetHomeData } from './contentfulGetHomeData'
 
 describe('Contentful contentfulGetHomeData', () => {
-  let contentfulGetHomeData: ContentfulGetHomeData
-  let contentfulAdapterFactory: ContentfulAdapterFactory
-  let contentful: Contentful
-
-  beforeEach(() => {
+  it('should retrieve a list of adapted homepages', async () => {
     const domain = 'https://cdn.contentful.com'
     const environment = 'master'
     const spaceId = 'space-id'
-    contentful = createContentful({
+    const contentful = createContentful({
       accessToken: 'accessToken',
       domain,
       environment,
@@ -27,15 +18,7 @@ describe('Contentful contentfulGetHomeData', () => {
     })
     const url = `${domain}/spaces/${spaceId}/environments/${environment}/entries`
     mockServer.universalGet(url, { ...homepageEntriesAPIResponse })
-    contentfulAdapterFactory = new ContentfulAdapterFactory()
-    contentfulAdapterFactory.register(ContentTypes.ALGOLIA, adaptOffersModule)
-    contentfulAdapterFactory.register(ContentTypes.BUSINESS, adaptBusinessModule)
-    contentfulAdapterFactory.register(ContentTypes.EXCLUSIVITY, adaptExclusivityModule)
-
-    contentfulGetHomeData = createContentfulGetHomeData(contentfulAdapterFactory, contentful)
-  })
-
-  it('should retrieve a list of adapted homepages', async () => {
+    const contentfulGetHomeData = createContentfulGetHomeData(contentful)
     const result = await contentfulGetHomeData()
 
     expect(result).toEqual(homepageList)
--- src/libs/contentful/index.ts
+++ src/libs/contentful/index.ts
@@ -1,37 +1,7 @@
 import { env } from 'libs/environment'
 
-import { ContentfulAdapterFactory } from './adapters/ContentfulAdapterFactory'
-import { adaptAppV2VenuesModule } from './adapters/modules/adaptAppV2VenuesModule'
-import { adaptBusinessModule } from './adapters/modules/adaptBusinessModule'
-import { adaptCategoryListModule } from './adapters/modules/adaptCategoryListModule'
-import { adaptExclusivityModule } from './adapters/modules/adaptExclusivityModule'
-import { adaptHighlightOfferModule } from './adapters/modules/adaptHighlightOfferModule'
-import { adaptOffersModule } from './adapters/modules/adaptOffersModule'
-import { adaptRecommendationModule } from './adapters/modules/adaptRecommendationModule'
-import { adaptThematicHighlightModule } from './adapters/modules/adaptThematicHighlightModule'
-import { adaptTrendsModule } from './adapters/modules/adaptTrendsModule'
-import { adaptVenueMapModule } from './adapters/modules/adaptVenueMapModule'
-import { adaptVenuesModule } from './adapters/modules/adaptVenuesModule'
-import { adaptVideoCarouselModule } from './adapters/modules/adaptVideoCarouselModule'
-import { adaptVideoModule } from './adapters/modules/adaptVideoModule'
 import { createContentful } from './contentful'
 import { createContentfulGetHomeData } from './contentfulGetHomeData'
-import { ContentTypes } from './types'
-
-const contentfulAdapterFactory = new ContentfulAdapterFactory()
-contentfulAdapterFactory.register(ContentTypes.ALGOLIA, adaptOffersModule)
-contentfulAdapterFactory.register(ContentTypes.BUSINESS, adaptBusinessModule)
-contentfulAdapterFactory.register(ContentTypes.RECOMMENDATION, adaptRecommendationModule)
-contentfulAdapterFactory.register(ContentTypes.THEMATIC_HIGHLIGHT, adaptThematicHighlightModule)
-contentfulAdapterFactory.register(ContentTypes.VENUES_PLAYLIST, adaptVenuesModule)
-contentfulAdapterFactory.register(ContentTypes.VENUES_PLAYLIST_APP_V2, adaptAppV2VenuesModule)
-contentfulAdapterFactory.register(ContentTypes.EXCLUSIVITY, adaptExclusivityModule)
-contentfulAdapterFactory.register(ContentTypes.CATEGORIES, adaptCategoryListModule)
-contentfulAdapterFactory.register(ContentTypes.VIDEO, adaptVideoModule)
-contentfulAdapterFactory.register(ContentTypes.HIGHLIGHT_OFFER, adaptHighlightOfferModule)
-contentfulAdapterFactory.register(ContentTypes.VENUE_MAP_BLOCK, adaptVenueMapModule)
-contentfulAdapterFactory.register(ContentTypes.VIDEO_CAROUSEL, adaptVideoCarouselModule)
-contentfulAdapterFactory.register(ContentTypes.TRENDS, adaptTrendsModule)
 
 const contentful = createContentful({
   accessToken: env.CONTENTFUL_PUBLIC_ACCESS_TOKEN,
@@ -40,7 +10,4 @@ const contentful = createContentful({
   spaceId: env.CONTENTFUL_SPACE_ID,
 })
 
-export const contentfulGetHomeData = createContentfulGetHomeData(
-  contentfulAdapterFactory,
-  contentful
-)
+export const contentfulGetHomeData = createContentfulGetHomeData(contentful)
