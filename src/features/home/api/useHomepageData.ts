df0243f45f242cac162586df8c524738a19a7051
Maxime Le Duc <maxime.leduc@passculture.app>
2024-09-18 16:55:53 +0200

    Remove getHomepageNatifContent

--- src/features/home/api/useHomepageData.ts
+++ src/features/home/api/useHomepageData.ts
@@ -7,33 +7,21 @@ import { Homepage } from 'features/home/types'
 import { useDependencies } from 'libs/dependencies/DependenciesContext'
 import { useLogTypeFromRemoteConfig } from 'libs/hooks/useLogTypeFromRemoteConfig'
 import { ScreenError } from 'libs/monitoring'
-import { LogTypeEnum } from 'libs/monitoring/errors'
 import { QueryKeys } from 'libs/queryKeys'
 
 const STALE_TIME = 5 * 60 * 1000
 export type GetHomeData = () => Promise<Homepage[]>
 
-const getHomepageNatifContent = (getHomeData: GetHomeData, logType: LogTypeEnum) => async () => {
-  try {
-    return await getHomeData()
-  } catch (e) {
-    const error = e as Error
-    throw new ScreenError(error.message, {
-      Screen: NoContentError,
-      logType,
-    })
-  }
-}
 const useGetHomepageList = () => {
   const { logType } = useLogTypeFromRemoteConfig()
   const { getHomeData } = useDependencies()
-  const { data: homepages } = useQuery<Homepage[]>(
-    [QueryKeys.HOMEPAGE_MODULES],
-    getHomepageNatifContent(getHomeData, logType),
-    {
-      staleTime: STALE_TIME,
-    }
-  )
+  const { data: homepages } = useQuery<Homepage[]>([QueryKeys.HOMEPAGE_MODULES], getHomeData, {
+    staleTime: STALE_TIME,
+    onError: (e) => {
+      const error = e as Error
+      throw new ScreenError(error.message, { Screen: NoContentError, logType })
+    },
+  })
   return homepages
 }
 
