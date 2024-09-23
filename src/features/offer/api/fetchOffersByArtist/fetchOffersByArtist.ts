786e9bd1e8e29641fc97f8af996172e56809251c
YassinL <62058919+YassinL@users.noreply.github.com>
2024-09-23 16:25:39 +0200

    (PC-31967) fix(Artist): use alegolia index offers B and userLocation on same artist playlist (#6913)
    * (PC-31967) refacto(ArtistBody): missing title on condition
    
    * (PC-31967) fix(Artist): use alegolia index offers B and userLocation on same artist playlist
    
    * (PC-31967) after review refacto

--- src/features/offer/api/fetchOffersByArtist/fetchOffersByArtist.ts
+++ src/features/offer/api/fetchOffersByArtist/fetchOffersByArtist.ts
@@ -4,6 +4,7 @@ import { captureAlgoliaError } from 'libs/algolia/fetchAlgolia/AlgoliaError'
 import { offerAttributesToRetrieve } from 'libs/algolia/fetchAlgolia/buildAlgoliaParameters/offerAttributesToRetrieve'
 import { client } from 'libs/algolia/fetchAlgolia/clients'
 import { env } from 'libs/environment'
+import { Position } from 'libs/location'
 import { HitOffer, Offer } from 'shared/offer/types'
 
 type BuildAlgoliaFilterType = {
@@ -12,6 +13,7 @@ type BuildAlgoliaFilterType = {
 
 export type FetchOfferByArtist = BuildAlgoliaFilterType & {
   searchGroupName: SearchGroupNameEnumv2
+  userLocation: Position
 }
 
 export type HitOfferWithArtistAndEan = Offer & {
@@ -24,8 +26,9 @@ export type HitOfferWithArtistAndEan = Offer & {
 export const fetchOffersByArtist = async ({
   artists,
   searchGroupName,
+  userLocation,
 }: FetchOfferByArtist): Promise<HitOfferWithArtistAndEan[]> => {
-  const index = client.initIndex(env.ALGOLIA_TOP_OFFERS_INDEX_NAME)
+  const index = client.initIndex(env.ALGOLIA_OFFERS_INDEX_NAME_B)
 
   if (
     !artists ||
@@ -42,6 +45,10 @@ export const fetchOffersByArtist = async ({
       hitsPerPage: 100,
       attributesToRetrieve: [...offerAttributesToRetrieve, 'offer.artist', 'offer.ean'],
       attributesToHighlight: [], // We disable highlighting because we don't need it
+      aroundRadius: 'all',
+      aroundLatLng: userLocation
+        ? `${userLocation.latitude}, ${userLocation.longitude}`
+        : undefined,
     })
 
     return response.hits
