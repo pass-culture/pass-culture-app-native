b672f695fe99caafba9d6a928fc6d524f2657e56
LucasBeneston <lucas.beneston@passculture.app>
2024-09-24 15:25:09 +0200

    feat: use useCodeScanner

--- src/features/scan/ScanButton.tsx
+++ src/features/scan/ScanButton.tsx
@@ -1,5 +1,12 @@
-import React, { FC } from 'react'
+import React, { FunctionComponent, useRef, useState } from 'react'
+import { Alert } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
+import {
+  Camera,
+  useCameraPermission,
+  useCameraDevice,
+  useCodeScanner,
+} from 'react-native-vision-camera'
 import styled, { useTheme } from 'styled-components/native'
 
 import { Touchable } from 'ui/components/touchable/Touchable'
@@ -7,20 +14,57 @@ import { ScanIllustration } from 'ui/svg/icons/ScanIllustration'
 import { getSpacing, Typo } from 'ui/theme'
 import { customFocusOutline } from 'ui/theme/customFocusOutline/customFocusOutline'
 
-type Props = {
-  onPress: () => void
-}
-export const ScanButton: FC<Props> = ({ onPress }) => {
+export const ScanButton: FunctionComponent = () => {
+  const isScanned = useRef<boolean>(false)
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: (codes) => {
+      if (!isScanned.current) {
+        isScanned.current = true
+        Alert.alert(`Scanned ${codes.length} codes!`, `${codes[0]?.value}`, [
+          {
+            text: 'Fermer',
+            onPress: () => {
+              isScanned.current = false
+            },
+          },
+        ])
+      }
+    },
+  })
+
   const theme = useTheme()
+  const device = useCameraDevice('back')
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const [showCamera, setShowCamera] = useState(false)
+
+  const onPress = () => {
+    if (!hasPermission) requestPermission()
+    else setShowCamera(true)
+  }
+  console.log('showCamera :', showCamera)
+  console.log('device: ', device)
 
   return (
-    <StyledTouchable onPress={onPress}>
-      <StyledLinearGradient />
-      <CardText>SCANNE LE RÉEL</CardText>
-      <IllustrationContainer>
-        <ScanIllustration size={getSpacing(18)} color={theme.colors.greyMedium} />
-      </IllustrationContainer>
-    </StyledTouchable>
+    <React.Fragment>
+      {device && showCamera ? (
+        <Camera
+          style={{ height: 400, width: '100%' }}
+          device={device}
+          isActive={showCamera}
+          codeScanner={codeScanner}
+        />
+      ) : (
+        <React.Fragment />
+      )}
+      <StyledTouchable onPress={onPress}>
+        <StyledLinearGradient />
+        <CardText>SCANNE LE RÉEL</CardText>
+        <IllustrationContainer>
+          <ScanIllustration size={getSpacing(18)} color={theme.colors.greyMedium} />
+        </IllustrationContainer>
+      </StyledTouchable>
+    </React.Fragment>
   )
 }
 const StyledLinearGradient = styled(LinearGradient).attrs({
