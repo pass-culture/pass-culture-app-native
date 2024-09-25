ffff15c589eb6f466696b8f14514a6940049301d
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-09-25 16:17:47 +0200

    (PC-31851) feat(bookings): use validation modal bpdy when opening reaction modal from an ended booking

--- src/features/bookings/components/EndedBookingItem.tsx
+++ src/features/bookings/components/EndedBookingItem.tsx
@@ -7,7 +7,7 @@ import { BookingItemTitle } from 'features/bookings/components/BookingItemTitle'
 import { isEligibleBookingsForArchive } from 'features/bookings/helpers/expirationDateUtils'
 import { BookingItemProps } from 'features/bookings/types'
 import { ReactionChoiceModal } from 'features/reactions/components/ReactionChoiceModal/ReactionChoiceModal'
-import { ReactionFromEnum } from 'features/reactions/enum'
+import { ReactionChoiceModalBodyEnum, ReactionFromEnum } from 'features/reactions/enum'
 import { getShareOffer } from 'features/share/helpers/getShareOffer'
 import { WebShareModal } from 'features/share/pages/WebShareModal'
 import { analytics } from 'libs/analytics'
@@ -238,6 +238,7 @@ export const EndedBookingItem = ({ booking, onSaveReaction }: BookingItemProps) 
           defaultReaction={userReaction}
           onSave={handleSaveReaction}
           from={ReactionFromEnum.ENDED_BOOKING}
+          bodyType={ReactionChoiceModalBodyEnum.VALIDATION}
         />
       ) : null}
     </Container>
