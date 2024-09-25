444095d41b654d7e19fdc421cc66fcaaa36d3bed
Cédric Le Sausse <cedric.lesausse@passculture.app>
2024-09-25 12:05:09 +0200

    (PC-31851) feat(reaction): add enum and function to define which modal body used

--- src/features/reactions/enum.ts
+++ src/features/reactions/enum.ts
@@ -2,3 +2,9 @@ export enum ReactionFromEnum {
   HOME = 'home',
   ENDED_BOOKING = 'ended_booking',
 }
+
+export enum ReactionChoiceModalBodyEnum {
+  VALIDATION = 'validation',
+  BOOKINGS_WITH_IMAGE = 'bookings_with_image',
+  BOOKINGS_WITHOUT_IMAGE = 'bookings_without_image',
+}
