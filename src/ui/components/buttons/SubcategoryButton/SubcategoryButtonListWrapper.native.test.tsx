a30b5f0749232e99843a8c8d00395c95cf3fff51
NolwenMajorFrances <144016890+NolwenMajorFrances@users.noreply.github.com>
2024-10-07 15:13:17 +0200

    (PC-32110) feat(SearchN1): add SearchN1 Cinema (#6972)

---
+++ src/ui/components/buttons/SubcategoryButton/SubcategoryButtonListWrapper.native.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { SearchGroupNameEnumv2 } from 'api/gen'
+import { initialSearchState } from 'features/search/context/reducer'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
+import { render, screen } from 'tests/utils'
+import { SubcategoryButtonListWrapper } from 'ui/components/buttons/SubcategoryButton/SubcategoryButtonListWrapper'
+
+const mockSearchState = initialSearchState
+jest.mock('features/search/context/SearchWrapper', () => ({
+  useSearch: () => ({
+    searchState: mockSearchState,
+    dispatch: jest.fn(),
+  }),
+}))
+
+jest.mock('libs/firebase/analytics/analytics')
+jest.mock('features/navigation/TabBar/routes')
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
+jest.mock('@batch.com/react-native-plugin', () =>
+  jest.requireActual('__mocks__/libs/react-native-batch')
+)
+
+describe('<SubcategoryButtonListWrapper/>', () => {
+  it('should display "Films à l’affiche" instead of "Séances de cinéma" if offerCategory is "Cinema"', async () => {
+    render(
+      reactQueryProviderHOC(
+        <SubcategoryButtonListWrapper offerCategory={SearchGroupNameEnumv2.CINEMA} />
+      )
+    )
+
+    expect(await screen.findByText('Films à l’affiche')).toBeOnTheScreen()
+  })
+
+  it('should display "Romans et littérature" if offerCategory is "Livres"', async () => {
+    render(
+      reactQueryProviderHOC(
+        <SubcategoryButtonListWrapper offerCategory={SearchGroupNameEnumv2.LIVRES} />
+      )
+    )
+
+    expect(await screen.findByText('Romans et littérature')).toBeOnTheScreen()
+  })
+})
