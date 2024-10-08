d1da1d732aaf34d21f7c0d633e118f2dc1247eb4
Maxime Le Duc <maxime.leduc@passculture.app>
2024-10-08 15:27:29 +0200

    Set synchronizeTabs to true in firestare persistence

--- src/libs/firebase/firestore/client.web.ts
+++ src/libs/firebase/firestore/client.web.ts
@@ -2,4 +2,4 @@ import firestore from 'libs/firebase/shims/firestore/index.web'
 
 export const firestoreRemoteStore = firestore()
 firestoreRemoteStore.settings({ experimentalAutoDetectLongPolling: true, merge: true })
-firestoreRemoteStore.enablePersistence()
+firestoreRemoteStore.enablePersistence({ synchronizeTabs: true })
