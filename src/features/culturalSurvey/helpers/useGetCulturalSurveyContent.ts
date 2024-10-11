8f2aac0255b36d010b53b6382b0588ddeaebf54e
LucasBeneston <lucas.beneston@passculture.app>
2024-10-11 17:08:49 +0200

    feat(culturalSurvey): use enableQpiInIdentityCheck FF in CulturalSurveyIntro

---
+++ src/features/culturalSurvey/helpers/useGetCulturalSurveyContent.ts
@@ -0,0 +1,56 @@
+import { useNavigation } from '@react-navigation/native'
+
+import { navigateToHomeConfig } from 'features/navigation/helpers/navigateToHome'
+import { UseNavigationType } from 'features/navigation/RootNavigator/types'
+import { homeNavConfig } from 'features/navigation/TabBar/helpers'
+import { useGoBack } from 'features/navigation/useGoBack'
+import { analytics } from 'libs/analytics'
+import { ClockFilled } from 'ui/svg/icons/ClockFilled'
+import { PlainArrowPrevious } from 'ui/svg/icons/PlainArrowPrevious'
+
+export const useGetCulturalSurveyContent = (enableQPIInIdentityCheck: boolean) => {
+  const { reset } = useNavigation<UseNavigationType>()
+  const { goBack } = useGoBack(...homeNavConfig)
+
+  const navigateSendAnalyticsAndShowShareAppModal = async () => {
+    reset({
+      index: 0,
+      routes: [{ name: navigateToHomeConfig.screen }],
+    })
+    analytics.logHasSkippedCulturalSurvey()
+  }
+
+  if (enableQPIInIdentityCheck) {
+    return {
+      intro: {
+        title: 'Tu y es presque\u00a0!',
+        subtitle: undefined,
+        customSubtitle: `Quelques questions et ton crédit est à toi.`,
+        bodyText:
+          'Parle nous de tes activités culturelles préférées. Tes réponses vont nous aider à mieux te connaître.',
+        showFAQLink: false,
+        secondaryButton: {
+          icon: PlainArrowPrevious,
+          text: 'Retour',
+          onPress: goBack,
+        },
+      },
+    }
+  } else {
+    return {
+      intro: {
+        title: 'Prends 1 minute',
+        subtitle: 'pour nous parler de tes activités culturelles préférées',
+        customSubtitle: undefined,
+        bodyText:
+          'En continuant, tu acceptes que nous utilisions les réponses au questionnaire qui va suivre pour améliorer l’application.',
+        showFAQLink: true,
+        secondaryButton: {
+          icon: ClockFilled,
+          text: 'Plus tard',
+          onPress: navigateSendAnalyticsAndShowShareAppModal,
+        },
+      },
+    }
+  }
+}
