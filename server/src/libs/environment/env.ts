3db7c3ce784c67fad5ed92c4da12a9d121c86dd6
Inès Mouandjo Lobé <ines.mouandjolobe@passculture.app>
2024-10-16 10:22:57 +0200

    (BSR) fix(proxy): Attempt to fix proxy

--- server/src/libs/environment/env.ts
+++ server/src/libs/environment/env.ts
@@ -5,17 +5,15 @@ import { config as dotEnvConfig } from 'dotenv'
 
 import { parseBooleanVariables } from './parseBooleanVariables'
 
-if (typeof process.env.ENV === 'undefined') {
-  throw new Error('The "ENV" environnement variable must be set')
-}
+const envVariable = process.env.ENV || 'this should never happen'
 
-const pathDevSource = join(__dirname, '../../..', `.env.${process.env.ENV}`)
+const pathDevSource = join(__dirname, '../../..', `.env.${envVariable}`)
 const pathBuildSource = join(__dirname, '../../../..', '.env')
 const path = existsSync(pathDevSource) ? resolve(pathDevSource) : resolve(pathBuildSource)
 const __DEV__ = pathDevSource === path
 
 if (!existsSync(path)) {
-  throw new Error(`DotEnv configuration not found for environment ${process.env.ENV} (${path})`)
+  throw new Error(`DotEnv configuration not found for environment ${envVariable} (${path})`)
 }
 
 dotEnvConfig({ path })
