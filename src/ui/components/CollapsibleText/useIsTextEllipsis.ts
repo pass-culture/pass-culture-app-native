8d9eb037cdeaeda253bfd9d99c367cc4f4d926fb
cedricls1 <78556024+cedricls1@users.noreply.github.com>
2024-10-18 11:41:52 +0200

    (PC-32382) fix(offer): use line height design system typo to calculate when text is ellipsis (#7049)
    Co-authored-by: Cédric Le Sausse <cedric.lesausse@passculture.app>

--- src/ui/components/CollapsibleText/useIsTextEllipsis.ts
+++ src/ui/components/CollapsibleText/useIsTextEllipsis.ts
@@ -3,12 +3,15 @@ import { useTheme } from 'styled-components/native'
 
 import { IsTextEllipsisOutput } from './types'
 
+const REM_TO_PX = 16
+
 // React Native Web doesn't support onTextLayout
 // https://github.com/necolas/react-native-web/issues/1685
 export const useIsTextEllipsis = (numberOfLines: number): IsTextEllipsisOutput => {
   const [isTextEllipsis, setIsTextEllipsis] = useState(false)
   const theme = useTheme()
-  const lineHeight = Number(theme.typography.body.lineHeight.slice(0, -2))
+
+  const lineHeight = Number(theme.designSystem.typography.body.lineHeight.slice(0, -3)) * REM_TO_PX
 
   const onLayout: Required<IsTextEllipsisOutput>['onLayout'] = useCallback(
     (event) => {
