911b5ebf0c4f924c6ecdbd59010e8d0dc40218f1
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2024-10-18 12:42:08 +0200

    (PC-32483) feat(profile): change FAQ url in DeleteProfileAccountNotDeletable (#7053)

--- src/features/profile/pages/DeleteProfile/DeleteProfileAccountNotDeletable.native.test.tsx
+++ src/features/profile/pages/DeleteProfile/DeleteProfileAccountNotDeletable.native.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 import { navigate } from '__mocks__/@react-navigation/native'
+import * as OpenUrlAPI from 'features/navigation/helpers/openUrl'
+import { env } from 'libs/environment/fixtures'
 import { fireEvent, render, screen } from 'tests/utils'
 
 import { DeleteProfileAccountNotDeletable } from './DeleteProfileAccountNotDeletable'
+
+const openUrl = jest.spyOn(OpenUrlAPI, 'openUrl')
 
 jest.mock('libs/firebase/analytics/analytics')
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
@@ -29,8 +33,8 @@ describe('DeleteProfileAccountNotDeletable', () => {
 
   it('should navigate to profile on press Retourner sur mon profil', async () => {
     render(<DeleteProfileAccountNotDeletable />)
-    const button = screen.getByText('Retourner sur mon profil')
 
+    const button = screen.getByText('Retourner sur mon profil')
     fireEvent.press(button)
 
     expect(navigate).toHaveBeenCalledWith('TabNavigator', { params: undefined, screen: 'Profile' })
@@ -38,10 +42,19 @@ describe('DeleteProfileAccountNotDeletable', () => {
 
   it('should navigate to notifications settings on press Désactiver mes notifications', async () => {
     render(<DeleteProfileAccountNotDeletable />)
-    const button = screen.getByText('Désactiver mes notifications')
 
+    const button = screen.getByText('Désactiver mes notifications')
     fireEvent.press(button)
 
     expect(navigate).toHaveBeenCalledWith('NotificationsSettings')
+  })
+
+  it('should open FAQ link when clicking on "consultant cette page" button', () => {
+    render(<DeleteProfileAccountNotDeletable />)
+
+    const faqButton = screen.getByText('consultant cette page')
+    fireEvent.press(faqButton)
+
+    expect(openUrl).toHaveBeenNthCalledWith(1, env.FAQ_LINK_RIGHT_TO_ERASURE, undefined, true)
   })
 })
