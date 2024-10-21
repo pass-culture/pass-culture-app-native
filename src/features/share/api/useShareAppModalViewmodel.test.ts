997ee2f3b827118d925fbd1d3ae3983db7ffcfd5
Maxime Le Duc <maxime.leduc@passculture.app>
2024-10-21 14:27:16 +0200

    Create share app viewmodel

---
+++ src/features/share/api/useShareAppModalViewmodel.test.ts
@@ -0,0 +1,140 @@
+import * as shareApp from 'features/share/helpers/shareApp'
+import { ShareAppModalType } from 'features/share/types'
+import { analytics } from 'libs/analytics/__mocks__/provider'
+import { DEFAULT_REMOTE_CONFIG } from 'libs/firebase/remoteConfig/remoteConfig.constants'
+import { CustomRemoteConfig } from 'libs/firebase/remoteConfig/remoteConfig.types'
+import * as useRemoteConfigContext from 'libs/firebase/remoteConfig/RemoteConfigProvider'
+import { renderHook } from 'tests/utils'
+
+import {
+  ShareAppModalSelectorViewmodelParams,
+  useShareAppModalSelectorViewmodel,
+} from './useShareAppModalViewmodel'
+
+const useRemoteConfigContextSpy = jest.spyOn(useRemoteConfigContext, 'useRemoteConfigContext')
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
+
+const mockShareApp = jest.spyOn(shareApp, 'shareApp')
+mockShareApp.mockImplementation(jest.fn())
+
+const useShareAppModalSelectorViewmodelTest = ({
+  hideModal = jest.fn(),
+  type: type = ShareAppModalType.BENEFICIARY,
+  showModal = jest.fn(),
+  setType = jest.fn(),
+}: Partial<ShareAppModalSelectorViewmodelParams> = {}) => {
+  const hook = renderHook(() =>
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useShareAppModalSelectorViewmodel({
+      type,
+      hideModal,
+      showModal,
+      setType,
+    })
+  )
+
+  return hook.result.current
+}
+
+const givenRemoteConfigShareAppModalVersion = (
+  version: CustomRemoteConfig['shareAppModalVersion']
+) => {
+  useRemoteConfigContextSpy.mockReturnValue({
+    ...DEFAULT_REMOTE_CONFIG,
+    shareAppModalVersion: version,
+  })
+}
+
+describe('useShareAppModalViewmodel', () => {
+  describe('version', () => {
+    test.each`
+      expectedVersion
+      ${'default'}
+      ${'version_1'}
+    `('get version $expectedVersion from remote config', async ({ expectedVersion }) => {
+      givenRemoteConfigShareAppModalVersion(expectedVersion)
+      const { version } = useShareAppModalSelectorViewmodelTest()
+
+      expect(version).toEqual(expectedVersion)
+    })
+  })
+
+  describe('close', () => {
+    test('close modal on close', async () => {
+      const hideModal = jest.fn()
+
+      const { close } = useShareAppModalSelectorViewmodelTest({ hideModal })
+
+      close()
+
+      expect(hideModal).toHaveBeenCalledWith()
+    })
+
+    test.each`
+      type
+      ${ShareAppModalType.BENEFICIARY}
+      ${ShareAppModalType.NOT_ELIGIBLE}
+    `('log dismiss share app on close with type $type', async ({ type }) => {
+      const { close } = useShareAppModalSelectorViewmodelTest({ type })
+
+      close()
+
+      expect(analytics.logDismissShareApp).toHaveBeenCalledWith(type)
+    })
+  })
+
+  describe('share', () => {
+    test('close modal on share', async () => {
+      const hideModal = jest.fn()
+
+      const { share } = useShareAppModalSelectorViewmodelTest({ hideModal })
+
+      await share()
+
+      expect(hideModal).toHaveBeenCalledWith()
+    })
+
+    test.each`
+      type
+      ${ShareAppModalType.BENEFICIARY}
+      ${ShareAppModalType.NOT_ELIGIBLE}
+    `('log share app on share with type $type', async ({ type }) => {
+      const { share } = useShareAppModalSelectorViewmodelTest({ type })
+
+      await share()
+
+      expect(analytics.logShareApp).toHaveBeenCalledWith({ type })
+    })
+  })
+
+  describe('show', () => {
+    test.each`
+      type
+      ${ShareAppModalType.BENEFICIARY}
+      ${ShareAppModalType.NOT_ELIGIBLE}
+    `('show modal on show with type $type', async ({ type }) => {
+      const showModal = jest.fn()
+      const setType = jest.fn()
+      const { show } = useShareAppModalSelectorViewmodelTest({ showModal, setType })
+
+      show(type)
+
+      expect(setType).toHaveBeenCalledWith(type)
+      expect(showModal).toHaveBeenCalledWith()
+    })
+
+    test.each`
+      type
+      ${ShareAppModalType.BENEFICIARY}
+      ${ShareAppModalType.NOT_ELIGIBLE}
+    `('send analytics on show with $type', ({ type }) => {
+      const { show } = useShareAppModalSelectorViewmodelTest()
+
+      show(type)
+
+      expect(analytics.logShowShareAppModal).toHaveBeenCalledWith({
+        type,
+      })
+    })
+  })
+})
