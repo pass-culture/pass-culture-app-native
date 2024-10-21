5494a8257222972b6f17f09bf155126d2c64ea63
Inès Mouandjo Lobé <ines.mouandjolobe@passculture.app>
2024-10-21 17:32:36 +0200

    (PC-32585) fix(search): bug fix on autocomplete

--- src/features/search/components/AutocompleteOfferItem/AutocompleteOfferItem.tsx
+++ src/features/search/components/AutocompleteOfferItem/AutocompleteOfferItem.tsx
@@ -115,7 +115,7 @@ export function AutocompleteOfferItem({
 
   const shouldDisplayNativeCategory =
     hasMostPopularHitNativeCategory &&
-    isAssociatedMostPopularNativeCategoryToMostPopularCategory &&
+    ((isQueryFromSearchN1 && isNativeCategoryRelatedToSearchGroup) || !isQueryFromSearchN1) &&
     !hasSeveralCategoriesFromNativeCategory
 
   const isLivresPapierNativeCategory =
@@ -190,18 +190,19 @@ export function AutocompleteOfferItem({
     hideSuggestions()
   }
 
-  const shouldDisplaySuggestion =
+  const shouldDisplayCategorySuggestion =
     shouldShowCategory && (hasMostPopularHitNativeCategory || hasMostPopularHitCategory)
 
-  const categoryToDisplay = shouldUseSearchGroupInsteadOfNativeCategory
-    ? searchGroupLabel
-    : mostPopularNativeCategoryValue
+  const categoryToDisplay =
+    shouldUseSearchGroupInsteadOfNativeCategory && searchGroupLabel !== 'None'
+      ? searchGroupLabel
+      : mostPopularNativeCategoryValue
 
   const testID = `autocompleteOfferItem_${hit.objectID}`
 
   return (
     <SuggestionContainer hit={hit} onPress={onPress} testID={testID}>
-      {shouldDisplaySuggestion && categoryToDisplay ? (
+      {shouldDisplayCategorySuggestion && categoryToDisplay ? (
         <Suggestion categoryToDisplay={categoryToDisplay} />
       ) : undefined}
     </SuggestionContainer>
