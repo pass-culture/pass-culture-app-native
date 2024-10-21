ddecaf550a0beb504d2bcbd4f0499de6f3c63fda
Maxime Le Duc <maxime.leduc@passculture.app>
2024-10-21 14:30:53 +0200

    Update shareAppModal

--- src/features/share/pages/ShareAppModal.native.test.tsx
+++ src/features/share/pages/ShareAppModal.native.test.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
 
-import * as Share from 'features/share/helpers/shareApp'
-import { ShareAppModalType } from 'features/share/types'
-import { analytics } from 'libs/analytics'
-import { fireEvent, render, screen, waitFor } from 'tests/utils'
+import { render, screen } from 'tests/utils'
 
 import { ShareAppModal } from './ShareAppModal'
-
-const visible = true
-const hideModal = jest.fn()
-const shareApp = jest.spyOn(Share, 'shareApp').mockResolvedValue()
 
 jest.mock('react-native/Libraries/Animated/createAnimatedComponent', () => {
   return function createAnimatedComponent(Component: unknown) {
@@ -19,72 +12,8 @@ jest.mock('react-native/Libraries/Animated/createAnimatedComponent', () => {
 
 describe('ShareAppModal', () => {
   it('should match snapshot', () => {
-    render(
-      <ShareAppModal
-        visible={visible}
-        hideModal={hideModal}
-        modalType={ShareAppModalType.NOT_ELIGIBLE}
-      />
-    )
+    render(<ShareAppModal visible close={jest.fn()} share={jest.fn()} />)
 
     expect(screen).toMatchSnapshot()
-  })
-
-  it('should open native share modal when clicking on "Partager l’appli" button', async () => {
-    render(
-      <ShareAppModal
-        visible={visible}
-        hideModal={hideModal}
-        modalType={ShareAppModalType.NOT_ELIGIBLE}
-      />
-    )
-
-    const shareButton = screen.getByTestId('Partager l’appli')
-    fireEvent.press(shareButton)
-
-    await waitFor(() => {
-      expect(shareApp).toHaveBeenCalledTimes(1)
-    })
-  })
-
-  it('should close modal when clicking on "Partager l’appli" button', () => {
-    render(
-      <ShareAppModal
-        visible={visible}
-        hideModal={hideModal}
-        modalType={ShareAppModalType.NOT_ELIGIBLE}
-      />
-    )
-
-    const shareButton = screen.getByText('Partager l’appli')
-    fireEvent.press(shareButton)
-
-    expect(hideModal).toHaveBeenCalledTimes(1)
-  })
-
-  it.each([
-    ShareAppModalType.NOT_ELIGIBLE,
-    ShareAppModalType.BENEFICIARY,
-    ShareAppModalType.ON_BOOKING_SUCCESS,
-  ])('should log analytics when clicking on "Partager l’appli" button', (modalType) => {
-    render(<ShareAppModal visible={visible} hideModal={hideModal} modalType={modalType} />)
-
-    const shareButton = screen.getByTestId('Partager l’appli')
-    fireEvent.press(shareButton)
-
-    expect(analytics.logShareApp).toHaveBeenNthCalledWith(1, { type: modalType })
-  })
-
-  it.each([
-    ShareAppModalType.NOT_ELIGIBLE,
-    ShareAppModalType.BENEFICIARY,
-    ShareAppModalType.ON_BOOKING_SUCCESS,
-  ])('should log analytics when clicking on "Fermer la modale"', (modalType) => {
-    render(<ShareAppModal visible={visible} hideModal={hideModal} modalType={modalType} />)
-
-    const closeButton = screen.getByTestId('Fermer la modale')
-    fireEvent.press(closeButton)
-
-    expect(analytics.logDismissShareApp).toHaveBeenNthCalledWith(1, modalType)
   })
 })
--- src/features/share/pages/ShareAppModal.tsx
+++ src/features/share/pages/ShareAppModal.tsx
@@ -1,71 +1,51 @@
-import React, { FunctionComponent, useCallback } from 'react'
+import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 
 import { SHARE_APP_IMAGE_SOURCE } from 'features/share/components/shareAppImage'
-import { shareApp } from 'features/share/helpers/shareApp'
-import { ShareAppModalType } from 'features/share/types'
-import { analytics } from 'libs/analytics'
 import { ButtonQuaternaryBlack } from 'ui/components/buttons/ButtonQuaternaryBlack'
 import { ButtonWithLinearGradient } from 'ui/components/buttons/buttonWithLinearGradient/ButtonWithLinearGradient'
 import { MarketingModal } from 'ui/components/modals/MarketingModal'
+import { ViewGap } from 'ui/components/ViewGap/ViewGap'
 import { Share } from 'ui/svg/icons/BicolorShare'
 import { Invalidate } from 'ui/svg/icons/Invalidate'
-import { Spacer, Typo } from 'ui/theme'
+import { Typo } from 'ui/theme'
 import { LINE_BREAK } from 'ui/theme/constants'
 
 type Props = {
   visible: boolean
-  hideModal: () => void
-  modalType: ShareAppModalType
+  close: () => void
+  share: () => void
 }
 
-export const ShareAppModal: FunctionComponent<Props> = ({ visible, hideModal, modalType }) => {
-  const openShareAppModal = useCallback(() => {
-    analytics.logShareApp({ type: modalType })
-    hideModal()
-    const utmMedium =
-      modalType === ShareAppModalType.BENEFICIARY ? 'beneficiary_modal' : 'uneligible_modal'
-    setTimeout(() => shareApp(utmMedium), 0)
-  }, [modalType, hideModal])
-
-  const closeModal = useCallback(() => {
-    analytics.logDismissShareApp(modalType)
-    hideModal()
-  }, [modalType, hideModal])
-
+export const ShareAppModal: FunctionComponent<Props> = ({ visible, close, share }) => {
   return (
     <MarketingModal
       visible={visible}
-      title={'Fais tourner le bon plan\u00a0!'}
+      title="Fais tourner le bon plan&nbsp;!"
       imageSource={SHARE_APP_IMAGE_SOURCE}
-      onBackdropPress={closeModal}>
-      <StyledBody>
-        <Typo.ButtonText>
-          35&nbsp;% des jeunes en France n’ont pas encore le pass Culture.
-        </Typo.ButtonText>
-        {LINE_BREAK}
-        Fais découvrir le pass à tes amis&nbsp;!
-      </StyledBody>
-      <Spacer.Column numberOfSpaces={6} />
-      <ButtonContainer>
-        <ButtonWithLinearGradient
-          wording="Partager l’appli"
-          icon={Share}
-          onPress={openShareAppModal}
-        />
-        <Spacer.Column numberOfSpaces={4} />
-        <ButtonQuaternaryBlack
-          wording="Non merci"
-          accessibilityLabel="Fermer la modale"
-          icon={Invalidate}
-          onPress={closeModal}
-        />
-      </ButtonContainer>
+      onBackdropPress={close}>
+      <ViewGap gap={6}>
+        <StyledBody>
+          <Typo.ButtonText>
+            35 % des jeunes en France n’ont pas encore le pass Culture.
+          </Typo.ButtonText>
+          {LINE_BREAK}
+          Fais découvrir le pass à tes amis&nbsp;!
+        </StyledBody>
+        <ViewGap gap={4}>
+          <ButtonWithLinearGradient wording="Partager l’appli" icon={Share} onPress={share} />
+          <ButtonQuaternaryBlack
+            wording="Non merci"
+            accessibilityLabel="Fermer la modale"
+            icon={Invalidate}
+            onPress={close}
+          />
+        </ViewGap>
+      </ViewGap>
     </MarketingModal>
   )
 }
 
-const ButtonContainer = styled.View({ width: '100%' })
 const StyledBody = styled(Typo.Body)({
   textAlign: 'center',
 })
