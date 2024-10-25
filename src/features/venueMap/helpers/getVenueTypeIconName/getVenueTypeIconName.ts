bf171438d5b2d6a447405a10602409200708467f
bpeyrou-pass <bruno.peyrou@passculture.app>
2024-10-25 17:00:58 +0200

    (PC-32629) fix(getVenueTypeIconName): set default icon to center (#7098)
    * (PC-32629) fix(getVenueTypeIconName): set default icon to center
    
    * test(getVenueTypeIconName): fix tests for icon name

--- src/features/venueMap/helpers/getVenueTypeIconName/getVenueTypeIconName.ts
+++ src/features/venueMap/helpers/getVenueTypeIconName/getVenueTypeIconName.ts
@@ -3,7 +3,7 @@ import { VenueTypeCode } from 'libs/parsers/venueType'
 
 export const getVenueTypeIconName = (selected: boolean, venueType?: VenueTypeCode | null) => {
   const venueTypeIcon = venueType ? venueTypesIconNameMapping[venueType] : null
-  const iconName = venueTypeIcon ? `map_pin_${venueTypeIcon}` : 'map_pin'
+  const iconName = venueTypeIcon ? `map_pin_${venueTypeIcon}` : 'map_pin_center'
   if (selected) {
     return `${iconName}_selected`
   }
