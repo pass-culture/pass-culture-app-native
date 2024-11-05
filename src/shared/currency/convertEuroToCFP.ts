57e6b9303fc4b21c90f4d2b2f48d6f260c954c6c
Lucas BENESTON <62059034+Lucasbeneston@users.noreply.github.com>
2024-11-05 14:24:42 +0100

    (PC-32780) feat(NC): update convertEuroToCFP with rounded possibility (#7146)

--- src/shared/currency/convertEuroToCFP.ts
+++ src/shared/currency/convertEuroToCFP.ts
@@ -1,3 +1,19 @@
-export const convertEuroToCFP = (priceInEuro: number, euroToCPFRate: number) => {
-  return Math.ceil(priceInEuro * euroToCPFRate)
+export enum RoundUnit {
+  NONE = 0,
+  UNITS = 1,
+  TENS = 10,
+  HUNDREDS = 100,
+}
+
+export const convertEuroToCFP = (
+  priceInEuro: number,
+  euroToCPFRate: number,
+  rounding: RoundUnit = RoundUnit.NONE
+): number => {
+  const cfpValue = priceInEuro * euroToCPFRate
+  if (rounding === RoundUnit.NONE) {
+    const result = parseFloat(cfpValue.toFixed(2))
+    return result % 1 === 0 ? Number(result.toFixed(0)) : Number(result.toFixed(2))
+  }
+  return Math.ceil(cfpValue / rounding) * rounding
 }
