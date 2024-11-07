4fdaf378623ede484e68898883764c1f75e80874
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2024-11-07 18:47:56 +0100

    (PC-32887) feat(booking): use offer address rather than offer venue for see itinerary button (#7164)
    * (PC-32887) feat(booking): use offer address rather than offer venue for see itinerary button
    
    * (PC-32887) refactor(booking): code improvement
    
    * (PC-32887) refactor(bookings): remove some @ts-expect-error: because of noUncheckedIndexedAccess

--- src/features/bookings/pages/BookingDetails/BookingDetails.native.test.tsx
+++ src/features/bookings/pages/BookingDetails/BookingDetails.native.test.tsx
@@ -22,7 +22,7 @@ import * as useNetInfoContextDefault from 'libs/network/NetInfoWrapper'
 import { subcategoriesDataTest } from 'libs/subcategories/fixtures/subcategoriesResponse'
 import { mockServer } from 'tests/mswServer'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { act, fireEvent, render, screen, waitFor } from 'tests/utils'
+import { act, render, screen, userEvent, waitFor } from 'tests/utils'
 import { SNACK_BAR_TIME_OUT } from 'ui/components/snackBar/SnackBarContext'
 
 import { BookingDetails as BookingDetailsDefault } from './BookingDetails'
@@ -52,7 +52,7 @@ jest.mock('ui/components/snackBar/SnackBarContext', () => ({
   })),
 }))
 
-let mockBookings = { ...bookingsSnap }
+let mockBookings: BookingsResponse = { ...bookingsSnap }
 
 jest.mock('features/bookings/api/useBookings', () => ({
   useBookings: jest.fn(() => ({
@@ -75,13 +75,15 @@ jest.mock('@batch.com/react-native-plugin', () =>
   jest.requireActual('__mocks__/libs/react-native-batch')
 )
 
+const user = userEvent.setup()
+
 describe('BookingDetails', () => {
-  let ongoingBookings = mockBookings.ongoing_bookings[0]
-  let endedBookings = mockBookings.ended_bookings[0]
+  let ongoingBookings: BookingReponse = bookingsSnap.ongoing_bookings[0]
+  let endedBookings: BookingReponse = bookingsSnap.ended_bookings[0]
 
   beforeEach(() => {
-    ongoingBookings = mockBookings.ongoing_bookings[0]
-    endedBookings = mockBookings.ended_bookings[0]
+    ongoingBookings = bookingsSnap.ongoing_bookings[0]
+    endedBookings = bookingsSnap.ended_bookings[0]
 
     mockServer.getApi<SubcategoriesResponseModelv2>('/v1/subcategories/v2', subcategoriesDataTest)
     mockUseNetInfoContext.mockReturnValue({ isConnected: true })
@@ -139,9 +141,7 @@ describe('BookingDetails', () => {
       renderBookingDetails(booking)
 
       const offerButton = screen.getByText('Accéder à l’offre en ligne')
-      await act(async () => {
-        fireEvent.press(offerButton)
-      })
+      await user.press(offerButton)
 
       expect(mockedOpenUrl).toHaveBeenCalledWith(
         booking.completedUrl,
@@ -248,7 +248,6 @@ describe('BookingDetails', () => {
 
   describe('withdrawalDetails', () => {
     it('should display withdrawal details', async () => {
-      // @ts-expect-error: because of noUncheckedIndexedAccess
       ongoingBookings.stock.offer.withdrawalDetails = 'Voici comment récupérer ton bien'
 
       renderBookingDetails(ongoingBookings)
@@ -299,11 +298,9 @@ describe('BookingDetails', () => {
       renderBookingDetails(booking)
       await screen.findByText('Ma réservation')
 
-      fireEvent.press(screen.getByText('bookingContactTest@email.com'))
+      await user.press(screen.getByText('bookingContactTest@email.com'))
 
       expect(analytics.logClickEmailOrganizer).toHaveBeenCalledTimes(1)
-
-      await act(() => {})
 
       expect(mockedOpenUrl).toHaveBeenCalledWith(
         `mailto:bookingContactTest@email.com`,
@@ -318,9 +315,8 @@ describe('BookingDetails', () => {
     renderBookingDetails(booking)
 
     const text = screen.getByText('Voir le détail de l’offre')
-    await act(async () => {
-      fireEvent.press(text)
-    })
+
+    await user.press(text)
 
     const offerId = booking.stock.offer.id
 
@@ -337,9 +333,8 @@ describe('BookingDetails', () => {
     renderBookingDetails(ongoingBookings)
 
     const text = screen.getByText('Voir le détail de l’offre')
-    await act(async () => {
-      fireEvent.press(text)
-    })
+
+    await user.press(text)
 
     const offerId = ongoingBookings.stock.offer.id
 
@@ -363,11 +358,10 @@ describe('BookingDetails', () => {
       renderBookingDetails(booking)
 
       const cancelButton = screen.getAllByTestId('Annuler ma réservation')[0]
-      await act(async () => {
-        if (cancelButton) {
-          fireEvent.press(cancelButton)
-        }
-      })
+
+      if (cancelButton) {
+        await user.press(cancelButton)
+      }
 
       expect(analytics.logCancelBooking).toHaveBeenCalledWith(booking.stock.offer.id)
     })
@@ -391,7 +385,6 @@ describe('BookingDetails', () => {
 
         mockBookings = {
           ...mockBookings,
-          // @ts-expect-error: because of noUncheckedIndexedAccess
           ended_bookings: [booking],
         }
 
@@ -419,7 +412,6 @@ describe('BookingDetails', () => {
 
         mockBookings = {
           ...mockBookings,
-          // @ts-expect-error: because of noUncheckedIndexedAccess
           ended_bookings: [booking],
         }
 
@@ -450,7 +442,6 @@ describe('BookingDetails', () => {
 
         mockBookings = {
           ...mockBookings,
-          // @ts-expect-error: because of noUncheckedIndexedAccess
           ended_bookings: [booking],
         }
 
@@ -494,7 +485,6 @@ describe('BookingDetails', () => {
 
         mockBookings = {
           ...mockBookings,
-          // @ts-expect-error: because of noUncheckedIndexedAccess
           ended_bookings: [booking],
         }
 
@@ -522,7 +512,6 @@ describe('BookingDetails', () => {
 
         mockBookings = {
           ...mockBookings,
-          // @ts-expect-error: because of noUncheckedIndexedAccess
           ended_bookings: [booking],
         }
 
@@ -553,7 +542,6 @@ describe('BookingDetails', () => {
 
         mockBookings = {
           ...mockBookings,
-          // @ts-expect-error: because of noUncheckedIndexedAccess
           ended_bookings: [booking],
         }
 
