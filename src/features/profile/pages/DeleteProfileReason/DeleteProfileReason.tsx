afa6bc1e24015f027b8ae007a85000f611314b40
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2024-11-13 13:40:27 +0100

    Revert "(PC-32885) chore(DeleteProfileConfirmation): removed no-non-null-assertion"
    This reverts commit 100ff0258e193fade173b119b8bc5e6b0b44cdc1.

--- src/features/profile/pages/DeleteProfileReason/DeleteProfileReason.tsx
+++ src/features/profile/pages/DeleteProfileReason/DeleteProfileReason.tsx
@@ -75,9 +75,13 @@ export function DeleteProfileReason() {
   const headerHeight = useGetHeaderHeight()
   const gradientRef = useRef<AnimatedViewRefType>(null)
   const { user } = useAuthContext()
-  const userIsDefinedAndAbove21 = user?.birthDate && getAge(user?.birthDate) >= 21
-  const canDeleteProfile = user?.isBeneficiary || userIsDefinedAndAbove21
-  const reasons = reasonButtons(!!canDeleteProfile)
+  // TODO(PC-32885): Fix this typescript error
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const userHas21YearsOld = getAge(user!.birthDate!) >= 21
+  // TODO(PC-32885): Fix this typescript error
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const canDeleteProfile = !user!.isBeneficiary || userHas21YearsOld
+  const reasons = reasonButtons(canDeleteProfile)
   const { onViewableItemsChanged } = useOnViewableItemsChanged(gradientRef, reasons)
 
   return (
