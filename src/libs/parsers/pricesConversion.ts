04937ec77a75a6f712664094395a0baef4a0ea32
LucasBeneston <lucas.beneston@passculture.app>
2024-11-14 11:34:13 +0100

    (PC-32912) feat(NC): update priceConversion

--- src/libs/parsers/pricesConversion.ts
+++ src/libs/parsers/pricesConversion.ts
@@ -1,7 +1,9 @@
 export const CENTS_IN_EURO = 100
 
-export const convertEuroToCents = (p: number): number => {
-  return Math.floor(Number((p * CENTS_IN_EURO).toFixed(2)))
+export const convertEuroToCents = (value: number): number => {
+  return Math.floor(Number((value * CENTS_IN_EURO).toFixed(2)))
 }
 
-export const convertCentsToEuros = (p: number): number => Math.floor(p / CENTS_IN_EURO)
+export const convertCentsToEuros = (value: number): number => {
+  return Math.floor(value / CENTS_IN_EURO)
+}
