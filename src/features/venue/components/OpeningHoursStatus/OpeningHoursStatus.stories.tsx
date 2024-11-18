72971631171f66e0c6e71273fc17a95b2850490f
LucasBeneston <lucas.beneston@passculture.app>
2024-11-18 17:57:16 +0100

    (PC-33062) feat(storybook): update OpeningHoursStatus stories

--- src/features/venue/components/OpeningHoursStatus/OpeningHoursStatus.stories.tsx
+++ src/features/venue/components/OpeningHoursStatus/OpeningHoursStatus.stories.tsx
@@ -60,31 +60,3 @@ const Template: ComponentStory<typeof VariantsTemplate> = () => (
 
 export const AllVariants = Template.bind({})
 AllVariants.storyName = 'OpeningHoursStatus'
-
-// export const Close = Template.bind({})
-// Close.args = {
-//   openingHours,
-//   currentDate: new Date('2024-05-13T20:00:00Z'),
-//   timezone: TIMEZONE,
-// }
-
-// export const Open = Template.bind({})
-// Open.args = {
-//   openingHours,
-//   currentDate: new Date('2024-05-13T12:00:00Z'),
-//   timezone: TIMEZONE,
-// }
-
-// export const OpenSoon = Template.bind({})
-// OpenSoon.args = {
-//   openingHours,
-//   currentDate: new Date('2024-05-13T06:00:00Z'),
-//   timezone: TIMEZONE,
-// }
-
-// export const CloseSoon = Template.bind({})
-// CloseSoon.args = {
-//   openingHours,
-//   currentDate: new Date('2024-05-13T16:00:00Z'),
-//   timezone: TIMEZONE,
-// }
