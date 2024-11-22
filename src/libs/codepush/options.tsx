aa8bb68e98235e0f51af38ce924367f9dae02053
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2024-11-22 09:49:31 +0100

    (PC-31602) docs(CodePush): fix comment to align with reality (#7259)
    * (PC-31602) docs(CodePush): fix comment to align with reality
    
    * Added comment for mandatory option

--- src/libs/codepush/options.tsx
+++ src/libs/codepush/options.tsx
@@ -1,6 +1,6 @@
 import CodePush, { CodePushOptions } from 'react-native-code-push' // @codepush
 
-// useful for testing and staging environments
+// useful for testing
 // to get the up-to-date app all the time
 export const AutoImmediate: CodePushOptions = {
   installMode: CodePush.InstallMode.IMMEDIATE,
@@ -10,9 +10,11 @@ export const AutoImmediate: CodePushOptions = {
 const TEN_MINUTES = 60 * 10
 
 // for production environment, we want a more discreet update
+// we applied this to staging too.
 // On each app start/resume, we check for update, that we install on
 // next app start/resume. This means the app doesn't flicker and the update
 // is not visible to the user.
+// it would seem that this behavior is over-written when setting the CodePush to mandatory via the app center options. If set to mandatory, the CodePush will be applied ASAP (causing flickering)
 export const NextResume: CodePushOptions = {
   installMode: CodePush.InstallMode.ON_NEXT_RESUME,
   minimumBackgroundDuration: TEN_MINUTES,
