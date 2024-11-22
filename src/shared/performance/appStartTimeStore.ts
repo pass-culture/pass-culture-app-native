8ec536571dae03a925fe42aab25685947e18fbbc
pcanthelou-pass <119601402+pcanthelou-pass@users.noreply.github.com>
2024-11-22 09:41:55 +0100

    Introduction de appStartTime le temps où l'app a lu la première ligne de code

--- src/shared/performance/appStartTimeStore.ts
+++ src/shared/performance/appStartTimeStore.ts
@@ -1,12 +1,21 @@
-/* eslint-disable no-restricted-imports */
 import { create } from 'zustand'
 
-interface AppStartTimeState {
+export interface AppStartTimeState {
   appStartTime: number
   setAppStartTime: (time: number) => void
 }
 
-export const useAppStartTimeStore = create<AppStartTimeState>((set) => ({
-  appStartTime: Date.now(),
-  setAppStartTime: (time) => set({ appStartTime: time }),
-}))
+// Fonction utilitaire pour fournir un timestamp
+const defaultDateProvider = () => Date.now()
+
+export const createAppStartTimeStore = (dateProvider = defaultDateProvider) =>
+  create<AppStartTimeState>((set) => {
+    const firstTime = new Date(dateProvider())
+    return {
+      appStartTime: firstTime.valueOf(),
+      setAppStartTime: (time) => set({ appStartTime: time }),
+    }
+  })
+
+// Utilisation par défaut avec Date.now()
+export const useAppStartTimeStore = createAppStartTimeStore()
