8bf211b6588d6e0e5acb2004fb66b0cc3a924a04
Xavier Le Cunff <xavier.lecunff@passculture.app>
2024-11-22 17:30:31 +0100

    fix: remove firestore config

--- jest/jest.web.setup.ts
+++ jest/jest.web.setup.ts
@@ -3,7 +3,6 @@ import 'jest-canvas-mock'
 jest.unmock('react-native-modal')
 jest.mock('libs/firebase/firestore/client.web', () => {
   const firestoreRemoteStore = jest.requireActual('libs/firebase/shims/firestore/index.web')
-  firestoreRemoteStore.settings({ experimentalAutoDetectLongPolling: true, merge: true })
   return {
     __esModule: true,
     default: firestoreRemoteStore,
