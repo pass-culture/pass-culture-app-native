f7c366f7726327375977210bd8cc7c65ba725242
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2024-11-25 09:28:11 +0100

    (PC-32701) feat(NC): add check for user registered in Pacific Franc region in computeIdentificationMethod (#7269)

--- src/features/identityCheck/pages/helpers/computeIdentificationMethod.native.test.ts
+++ src/features/identityCheck/pages/helpers/computeIdentificationMethod.native.test.ts
@@ -10,20 +10,63 @@ const educonnectOnlyMethod = [IdentityCheckMethod.educonnect]
 const ubbleOnlyMethod = [IdentityCheckMethod.ubble]
 const bothMethodsAllowed = [IdentityCheckMethod.ubble, IdentityCheckMethod.educonnect]
 
+const isUserRegisteredInPacificFrancRegion = true
+const isNotUserRegisteredInPacificFrancRegion = false
+
 describe('computeIdentificationMethod', () => {
-  it('should return fork flow with two methods allowed', () => {
-    expect(computeIdentificationMethod(bothMethodsAllowed)).toEqual(forkScreen)
+  describe('bothMethodsAllowed', () => {
+    it('should return fork flow with two methods allowed when user is NOT registered in Pacific Franc region', () => {
+      expect(
+        computeIdentificationMethod(isNotUserRegisteredInPacificFrancRegion, bothMethodsAllowed)
+      ).toEqual(forkScreen)
+    })
+
+    it('should return ubbleFlowFirstScreen flow with two methods allowed when user is registered in Pacific Franc region', () => {
+      expect(
+        computeIdentificationMethod(isUserRegisteredInPacificFrancRegion, bothMethodsAllowed)
+      ).toEqual(ubbleFlowFirstScreen)
+    })
   })
 
-  it('should return ubble flow with if only ubble method is allowed', () => {
-    expect(computeIdentificationMethod(ubbleOnlyMethod)).toEqual(ubbleFlowFirstScreen)
+  describe('ubbleOnlyMethod', () => {
+    it('should return ubble flow if only ubble method is allowed when user is NOT registered in Pacific Franc region', () => {
+      expect(
+        computeIdentificationMethod(isNotUserRegisteredInPacificFrancRegion, ubbleOnlyMethod)
+      ).toEqual(ubbleFlowFirstScreen)
+    })
+
+    it('should return ubble flow if only ubble method is allowed when user is registered in Pacific Franc region', () => {
+      expect(
+        computeIdentificationMethod(isUserRegisteredInPacificFrancRegion, ubbleOnlyMethod)
+      ).toEqual(ubbleFlowFirstScreen)
+    })
   })
 
-  it('should return educonnect flow with if only educonnect method is allowed', () => {
-    expect(computeIdentificationMethod(educonnectOnlyMethod)).toEqual(educonnectFlowFirstScreen)
+  describe('educonnectOnlyMethod', () => {
+    it('should return educonnect flow if only educonnect method is allowed when user is NOT registered in Pacific Franc region', () => {
+      expect(
+        computeIdentificationMethod(isNotUserRegisteredInPacificFrancRegion, educonnectOnlyMethod)
+      ).toEqual(educonnectFlowFirstScreen)
+    })
+
+    it('should return ubbleFlowFirstScreen flow if only educonnect method is allowed when user is registered in Pacific Franc region', () => {
+      expect(
+        computeIdentificationMethod(isUserRegisteredInPacificFrancRegion, educonnectOnlyMethod)
+      ).toEqual(ubbleFlowFirstScreen)
+    })
   })
 
-  it('should return ubble flow by default', () => {
-    expect(computeIdentificationMethod()).toEqual(ubbleFlowFirstScreen)
+  describe('ubbleFlowFirstScreen', () => {
+    it('should return ubble flow by default when user is NOT registered in Pacific Franc region', () => {
+      expect(computeIdentificationMethod(isNotUserRegisteredInPacificFrancRegion)).toEqual(
+        ubbleFlowFirstScreen
+      )
+    })
+
+    it('should return ubble flow by default when user is registered in Pacific Franc region', () => {
+      expect(computeIdentificationMethod(isUserRegisteredInPacificFrancRegion)).toEqual(
+        ubbleFlowFirstScreen
+      )
+    })
   })
 })
--- src/features/identityCheck/pages/helpers/computeIdentificationMethod.ts
+++ src/features/identityCheck/pages/helpers/computeIdentificationMethod.ts
@@ -2,25 +2,22 @@ import { IdentityCheckMethod } from 'api/gen'
 import { SubscriptionScreen } from 'features/identityCheck/types'
 
 const educonnectFlowFirstScreen: SubscriptionScreen = 'EduConnectForm'
-
 const ubbleFlowFirstScreen: SubscriptionScreen = 'SelectIDOrigin'
-
 const forkScreen: SubscriptionScreen = 'IdentificationFork'
 
 export const computeIdentificationMethod = (
+  isUserRegisteredInPacificFrancRegion: boolean,
   identificationMethods?: IdentityCheckMethod[] | null
 ) => {
   if (identificationMethods) {
-    if (identificationMethods.length === 2) {
-      return forkScreen
-    }
+    if (isUserRegisteredInPacificFrancRegion) return ubbleFlowFirstScreen
 
-    if (
-      identificationMethods.length === 1 &&
-      identificationMethods[0] === IdentityCheckMethod.educonnect
-    ) {
-      return educonnectFlowFirstScreen
-    }
+    const hasTwoMethods = identificationMethods.length === 2
+    if (hasTwoMethods) return forkScreen
+
+    const hasOneMethod = identificationMethods.length === 1
+    const isEduconnectMethod = identificationMethods[0] === IdentityCheckMethod.educonnect
+    if (hasOneMethod && isEduconnectMethod) return educonnectFlowFirstScreen
   }
 
   return ubbleFlowFirstScreen
