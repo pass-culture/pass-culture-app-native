e7a9a2b306bb28ba048bd946c0cce7c424adb3e3
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2024-11-26 11:25:16 +0100

    (PC-33100) feat(venueMap): display content venue map filters list

---
+++ src/features/venueMap/components/VenueMapFiltersList/VenueMapFiltersList.native.test.tsx
@@ -0,0 +1,77 @@
+import { StackScreenProps } from '@react-navigation/stack'
+import React from 'react'
+
+import { VenueMapFiltersList } from 'features/venueMap/components/VenueMapFiltersList/VenueMapFiltersList'
+import { VenueMapFiltersModalStackParamList } from 'features/venueMap/types'
+import { render, screen, userEvent } from 'tests/utils'
+
+const mockNavigate = jest.fn()
+const mockGoBack = jest.fn()
+
+const mockNavigation: StackScreenProps<
+  VenueMapFiltersModalStackParamList,
+  'VenueMapFiltersList'
+>['navigation'] = {
+  navigate: mockNavigate,
+  goBack: mockGoBack,
+  dispatch: jest.fn(),
+  reset: jest.fn(),
+  isFocused: jest.fn(),
+  canGoBack: jest.fn(),
+  getId: jest.fn(),
+  getState: jest.fn(),
+  getParent: jest.fn(),
+  setParams: jest.fn(),
+  setOptions: jest.fn(),
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+  replace: jest.fn(),
+  push: jest.fn(),
+  pop: jest.fn(),
+  popToTop: jest.fn(),
+}
+
+const user = userEvent.setup()
+
+jest.useFakeTimers()
+
+describe('VenueMapFiltersList', () => {
+  it('should render filters list correctly', () => {
+    render(
+      <VenueMapFiltersList
+        navigation={mockNavigation}
+        route={{ key: 'VenueMapFiltersList', name: 'VenueMapFiltersList' }}
+      />
+    )
+
+    expect(screen.getByText('Sorties')).toBeTruthy()
+    expect(screen.getByText('Boutiques')).toBeTruthy()
+    expect(screen.getByText('Autres')).toBeTruthy()
+  })
+
+  it('should navigate to venue map type filter when pressing a row', async () => {
+    render(
+      <VenueMapFiltersList
+        navigation={mockNavigation}
+        route={{ key: 'VenueMapFiltersList', name: 'VenueMapFiltersList' }}
+      />
+    )
+
+    await user.press(screen.getByText('Sorties'))
+
+    expect(mockNavigate).toHaveBeenCalledWith('VenueMapTypeFilter')
+  })
+
+  it('should trigger goBack when pressing modal close button', async () => {
+    render(
+      <VenueMapFiltersList
+        navigation={mockNavigation}
+        route={{ key: 'VenueMapFiltersList', name: 'VenueMapFiltersList' }}
+      />
+    )
+
+    await user.press(screen.getByLabelText('Fermer'))
+
+    expect(mockGoBack).toHaveBeenCalledTimes(1)
+  })
+})
--- src/features/venueMap/types.ts
+++ src/features/venueMap/types.ts
@@ -14,3 +14,8 @@ export type VenueTypeMapping = {
 export type VenuesCountByType = {
   [key in VenueTypeCode]: number
 }
+
+export type VenueMapFiltersModalStackParamList = {
+  VenueMapFiltersList: undefined
+  VenueMapTypeFilter: undefined
+}
