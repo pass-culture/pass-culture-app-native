dca0eb74383476438c83e86ea25bd08d31e14f57
NolwenMajorFrances <144016890+nmajorfrances-pass@users.noreply.github.com>
2024-11-26 11:58:41 +0100

    (BSR): more precise type for ThematicSearch

--- src/features/navigation/SearchStackNavigator/types.ts
+++ src/features/navigation/SearchStackNavigator/types.ts
@@ -1,13 +1,27 @@
+import { SearchGroupNameEnumv2 } from 'api/gen'
 import { DisabilitiesProperties } from 'features/accessibility/types'
 import { GenericRoute } from 'features/navigation/RootNavigator/types'
 import { SearchState } from 'features/search/types'
 
 export type SearchStackRouteName = keyof SearchStackParamList
 
+type HasAThematicSearch =
+  | SearchGroupNameEnumv2.CINEMA
+  | SearchGroupNameEnumv2.LIVRES
+  | SearchGroupNameEnumv2.MUSIQUE
+  | SearchGroupNameEnumv2.FILMS_DOCUMENTAIRES_SERIES
+
+type ThematicSearchCategories = Extract<SearchGroupNameEnumv2, HasAThematicSearch>
+
 export type SearchStackParamList = {
   SearchLanding?: Partial<SearchState & { accessibilityFilter: Partial<DisabilitiesProperties> }>
   SearchResults?: Partial<SearchState & { accessibilityFilter: Partial<DisabilitiesProperties> }>
-  ThematicSearch?: Partial<SearchState & { accessibilityFilter: Partial<DisabilitiesProperties> }>
+  ThematicSearch?: Partial<
+    SearchState & {
+      offerCategories: ThematicSearchCategories[]
+      accessibilityFilter: Partial<DisabilitiesProperties>
+    }
+  >
 }
 
 export type SearchStackScreenNames = keyof SearchStackParamList
