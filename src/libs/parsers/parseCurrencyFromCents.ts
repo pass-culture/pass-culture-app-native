0ec26ef2e66fb01b9b9b9521d15b8e222e576226
LucasBeneston <lucas.beneston@passculture.app>
2024-11-26 15:33:49 +0100

    (PC-32821) feat(NC): create parseCurrencyFromCents file

---
+++ src/libs/parsers/parseCurrencyFromCents.ts
@@ -0,0 +1,48 @@
+import { useGetPacificFrancToEuroRate } from 'libs/firebase/firestore/exchangeRates/useGetPacificFrancToEuroRate'
+import { convertCentsToEuros } from 'libs/parsers/pricesConversion'
+import { RoundUnit, convertEuroToPacificFranc } from 'shared/currency/convertEuroToPacificFranc'
+import { Currency, useGetCurrencyToDisplay } from 'shared/currency/useGetCurrencyToDisplay'
+
+import { FormatPriceOptions } from './getDisplayPrice'
+
+const getFractionDigits = (princeInEuro: number, options?: FormatPriceOptions): number => {
+  if (options?.fractionDigits) return options.fractionDigits
+  return princeInEuro === Math.floor(princeInEuro) ? 0 : 2
+}
+
+export const parseCurrencyFromCents = (
+  priceInCents: number,
+  currency: Currency,
+  euroToPacificFrancRate: number,
+  options?: FormatPriceOptions
+) => {
+  const priceInEuro = convertCentsToEuros(priceInCents)
+
+  if (currency === Currency.PACIFIC_FRANC_SHORT) {
+    const priceInPacificFrancs = convertEuroToPacificFranc(
+      priceInEuro,
+      euroToPacificFrancRate,
+      RoundUnit.UNITS
+    )
+    const XPFformatter = new Intl.NumberFormat('fr-FR', {
+      style: 'currency',
+      currency: 'XPF',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    })
+    return XPFformatter.format(priceInPacificFrancs).replace('FCFP', 'F')
+  } else {
+    const EURformatter = new Intl.NumberFormat('fr-FR', {
+      style: 'currency',
+      currency: 'EUR',
+      minimumFractionDigits: getFractionDigits(priceInEuro, options),
+    })
+    return EURformatter.format(priceInEuro)
+  }
+}
+
+export const useParseCurrencyFromCents = (priceInCents: number, options?: FormatPriceOptions) => {
+  const currency = useGetCurrencyToDisplay()
+  const euroToPacificFrancRate = useGetPacificFrancToEuroRate()
+  return parseCurrencyFromCents(priceInCents, currency, euroToPacificFrancRate, options)
+}
