802d8c4a08f818b462da78547df9854ffa98af2d
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2024-11-26 11:31:21 +0100

    (PC-33100) feat(venueMap): prepare displaying venue map type filter content

---
+++ src/features/venueMap/components/VenueMapTypeFilter/VenueMapTypeFilter.native.test.tsx
@@ -0,0 +1,64 @@
+import { StackScreenProps } from '@react-navigation/stack'
+import React from 'react'
+
+import { navigate } from '__mocks__/@react-navigation/native'
+import { VenueMapTypeFilter } from 'features/venueMap/components/VenueMapTypeFilter/VenueMapTypeFilter'
+import { VenueMapFiltersModalStackParamList } from 'features/venueMap/types'
+import { render, screen, userEvent } from 'tests/utils'
+
+const mockGoBack = jest.fn()
+
+const mockNavigation: StackScreenProps<
+  VenueMapFiltersModalStackParamList,
+  'VenueMapTypeFilter'
+>['navigation'] = {
+  navigate: jest.fn(),
+  goBack: mockGoBack,
+  dispatch: jest.fn(),
+  reset: jest.fn(),
+  isFocused: jest.fn(),
+  canGoBack: jest.fn(),
+  getId: jest.fn(),
+  getState: jest.fn(),
+  getParent: jest.fn(),
+  setParams: jest.fn(),
+  setOptions: jest.fn(),
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+  replace: jest.fn(),
+  push: jest.fn(),
+  pop: jest.fn(),
+  popToTop: jest.fn(),
+}
+
+const user = userEvent.setup()
+
+jest.useFakeTimers()
+
+describe('VenueMapTypeFilter', () => {
+  it('should navigate to venue map when pressing modal close button', async () => {
+    render(
+      <VenueMapTypeFilter
+        navigation={mockNavigation}
+        route={{ key: 'VenueMapTypeFilter', name: 'VenueMapTypeFilter' }}
+      />
+    )
+
+    await user.press(screen.getByLabelText('Fermer'))
+
+    expect(navigate).toHaveBeenNthCalledWith(1, 'VenueMap')
+  })
+
+  it('should trigger goBack when pressing modal back button', async () => {
+    render(
+      <VenueMapTypeFilter
+        navigation={mockNavigation}
+        route={{ key: 'VenueMapTypeFilter', name: 'VenueMapTypeFilter' }}
+      />
+    )
+
+    await user.press(screen.getByLabelText('Revenir en arrière'))
+
+    expect(mockGoBack).toHaveBeenCalledTimes(1)
+  })
+})
---
+++ src/features/venueMap/components/VenueMapTypeFilter/VenueMapTypeFilter.tsx
@@ -0,0 +1,37 @@
+import { useNavigation } from '@react-navigation/native'
+import { StackScreenProps } from '@react-navigation/stack'
+import React, { FunctionComponent } from 'react'
+import { View } from 'react-native'
+import { v4 as uuidv4 } from 'uuid'
+
+import { UseNavigationType } from 'features/navigation/RootNavigator/types'
+import { VenueMapFiltersModal } from 'features/venueMap/pages/modals/VenueMapFiltersModal/VenueMapFiltersModal'
+import { VenueMapFiltersModalStackParamList } from 'features/venueMap/types'
+
+type Props = StackScreenProps<VenueMapFiltersModalStackParamList, 'VenueMapTypeFilter'>
+
+const titleId = uuidv4()
+
+export const VenueMapTypeFilter: FunctionComponent<Props> = ({ navigation }) => {
+  const { navigate } = useNavigation<UseNavigationType>()
+
+  const onGoBack = () => {
+    navigation.goBack()
+  }
+
+  const onClose = () => {
+    navigate('VenueMap')
+  }
+
+  return (
+    <VenueMapFiltersModal
+      titleId={titleId}
+      title="Type de lieu"
+      handleGoBack={onGoBack}
+      handleOnClose={onClose}
+      shouldDisplayBackButton
+      shouldDisplayCloseButton>
+      <View />
+    </VenueMapFiltersModal>
+  )
+}
