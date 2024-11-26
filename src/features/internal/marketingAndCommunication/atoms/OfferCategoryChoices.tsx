467f08621c254abe12adf447e23c4929cc844b77
NolwenMajorFrances <144016890+nmajorfrances-pass@users.noreply.github.com>
2024-11-26 14:41:11 +0100

    (BSR): reduce complexity

--- src/features/internal/marketingAndCommunication/atoms/OfferCategoryChoices.tsx
+++ src/features/internal/marketingAndCommunication/atoms/OfferCategoryChoices.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components/native'
 
 import { SearchGroupNameEnumv2 } from 'api/gen'
 import { SelectionLabel } from 'features/search/components/SelectionLabel/SelectionLabel'
+import { CategoryCriteria } from 'features/search/enums'
 import { useAvailableCategories } from 'features/search/helpers/useAvailableCategories/useAvailableCategories'
 import { useSearchGroupLabelMapping } from 'libs/subcategories/mappings'
 import { Li } from 'ui/components/Li'
@@ -12,11 +13,16 @@ import { getSpacing } from 'ui/theme'
 type Props = {
   onChange: (selection: SearchGroupNameEnumv2[]) => void
   selection: SearchGroupNameEnumv2[] | undefined
+  categories: CategoryCriteria[]
 }
 
-export const OfferCategoryChoices = ({ onChange, selection }: Props) => {
-  const searchGroupLabelMapping = useSearchGroupLabelMapping()
+export const OfferCategoryChoices = (props: Props) => {
   const categories = useAvailableCategories()
+  return <CategoryChoices {...props} categories={categories} />
+}
+
+export const CategoryChoices = ({ onChange, selection, categories }: Props) => {
+  const searchGroupLabelMapping = useSearchGroupLabelMapping()
 
   if (categories.length === 0) {
     return null
--- src/features/search/helpers/useAvailableCategories/useAvailableCategories.tsx
+++ src/features/search/helpers/useAvailableCategories/useAvailableCategories.tsx
@@ -1,7 +1,8 @@
+import { CategoryCriteria } from 'features/search/enums'
 import { availableCategories } from 'features/search/helpers/availableCategories/availableCategories'
 import { useSubcategories } from 'libs/subcategories/useSubcategories'
 
-export const useAvailableCategories = () => {
+export const useAvailableCategories = (): CategoryCriteria[] => {
   const { data } = useSubcategories()
   const searchGroupsEnum = data?.searchGroups.map((searchGroup) => searchGroup.name) ?? []
   const categories = Object.values(availableCategories).filter((category) =>
