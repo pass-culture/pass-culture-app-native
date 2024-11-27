79cf79f58a8a01f89f6139ae7fd3f084673bba2d
Maxime Le Duc <maxime.leduc@passculture.app>
2024-11-27 16:47:58 +0100

    Stop using "as"

--- src/features/identityCheck/pages/phoneValidation/helpers/isPhoneNumberValid.ts
+++ src/features/identityCheck/pages/phoneValidation/helpers/isPhoneNumberValid.ts
@@ -1,6 +1,13 @@
-import parsePhoneNumberFromString, { CountryCode } from 'libphonenumber-js'
+import parsePhoneNumberFromString, { CountryCode, getCountries } from 'libphonenumber-js'
+
+function isCountryCode(code: string): code is CountryCode {
+  const countries: string[] = getCountries()
+  return countries.includes(code)
+}
 
 export function isPhoneNumberValid(number: string, country: string) {
-  const phoneNumber = parsePhoneNumberFromString(number, country as CountryCode)
+  if (!isCountryCode(country)) return false
+
+  const phoneNumber = parsePhoneNumberFromString(number, country)
   return phoneNumber?.isValid() ?? false
 }
