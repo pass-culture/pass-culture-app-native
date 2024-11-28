94a7b1294e4daa4a4c3a1ce1165779a998ce4ea8
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2024-11-28 14:28:29 +0100

    (PC-33187) feat(venueMap): apply filter when pressing checkbox in venue type modal filter (#7291)
    * (PC-33187) feat(venueMap): apply filter when pressing checkbox in venue type modal filter
    
    * (PC-33187) feat(venueMap): filtered only the selected venue type when previous filter is all venue type group

--- src/features/venueMap/components/VenueMapTypeFilter/VenueMapTypeFilter.tsx
+++ src/features/venueMap/components/VenueMapTypeFilter/VenueMapTypeFilter.tsx
@@ -4,10 +4,12 @@ import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 import { v4 as uuidv4 } from 'uuid'
 
+import { VenueTypeCodeKey } from 'api/gen'
 import { UseNavigationType } from 'features/navigation/RootNavigator/types'
 import { VenueMapFiltersModalStackParamList } from 'features/navigation/VenueMapFiltersStackNavigator/types'
 import { FILTERS_VENUE_TYPE_MAPPING } from 'features/venueMap/constant'
 import { VenueMapFiltersModal } from 'features/venueMap/pages/modals/VenueMapFiltersModal/VenueMapFiltersModal'
+import { useVenuesFilter, useVenuesFilterActions } from 'features/venueMap/store/venuesFilterStore'
 import { MAP_VENUE_TYPE_TO_LABEL, VenueTypeCode } from 'libs/parsers/venueType'
 import { Checkbox } from 'ui/components/inputs/Checkbox/Checkbox'
 import { ViewGap } from 'ui/components/ViewGap/ViewGap'
@@ -20,6 +22,8 @@ const titleId = uuidv4()
 export const VenueMapTypeFilter: FunctionComponent<Props> = ({ navigation, route }) => {
   const { title, filterGroup } = route.params
   const { navigate } = useNavigation<UseNavigationType>()
+  const venueFilters = useVenuesFilter()
+  const { addVenuesFilters, removeVenuesFilters, setVenuesFilters } = useVenuesFilterActions()
 
   const onGoBack = () => {
     navigation.goBack()
@@ -29,9 +33,34 @@ export const VenueMapTypeFilter: FunctionComponent<Props> = ({ navigation, route
     navigate('VenueMap')
   }
 
-  const items: VenueTypeCode[] = FILTERS_VENUE_TYPE_MAPPING[filterGroup].filter(
+  const toggleFilters = (isChecked: boolean, filters: VenueTypeCodeKey[]) => {
+    if (isChecked) {
+      if (hasAllFilters) {
+        // Only the current selection is filtered
+        setVenuesFilters(filters)
+        return
+      }
+      addVenuesFilters(filters)
+      return
+    }
+    removeVenuesFilters(filters)
+  }
+
+  const toggleAll = (isChecked: boolean) => {
+    toggleFilters(isChecked, FILTERS_VENUE_TYPE_MAPPING[filterGroup])
+  }
+
+  const toggleVenueType = (isChecked: boolean, venueType: VenueTypeCodeKey) => {
+    toggleFilters(isChecked, [venueType])
+  }
+
+  const venueTypes: VenueTypeCode[] = FILTERS_VENUE_TYPE_MAPPING[filterGroup].filter(
     (item): item is VenueTypeCode => item in MAP_VENUE_TYPE_TO_LABEL
   )
+  const hasAllFilters = FILTERS_VENUE_TYPE_MAPPING[filterGroup].every((filter) =>
+    venueFilters.includes(filter)
+  )
+  const isCheckedShowAll = venueFilters.length === 0 || hasAllFilters
 
   return (
     <VenueMapFiltersModal
@@ -43,13 +72,13 @@ export const VenueMapTypeFilter: FunctionComponent<Props> = ({ navigation, route
       shouldDisplayCloseButton>
       <Container gap={6}>
         <TypoDS.Title1>{title}</TypoDS.Title1>
-        <Checkbox label="Tout afficher" isChecked onPress={() => null} />
-        {items.map((item) => (
+        <Checkbox label="Tout afficher" isChecked={isCheckedShowAll} onPress={toggleAll} />
+        {venueTypes.map((venueType) => (
           <Checkbox
-            key={item}
-            label={MAP_VENUE_TYPE_TO_LABEL[item]}
-            isChecked={false}
-            onPress={() => null}
+            key={venueType}
+            label={MAP_VENUE_TYPE_TO_LABEL[venueType]}
+            isChecked={venueFilters.includes(venueType) && !hasAllFilters}
+            onPress={(isChecked: boolean) => toggleVenueType(isChecked, venueType)}
           />
         ))}
       </Container>
