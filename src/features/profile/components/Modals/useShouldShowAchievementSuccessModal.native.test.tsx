48f624aa2398d37f82862ebd0fcc1c72f6f63c96
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2024-12-09 19:08:40 +0100

    Added success modal to home

---
+++ src/features/profile/components/Modals/useShouldShowAchievementSuccessModal.native.test.tsx
@@ -0,0 +1,92 @@
+import { AchievementEnum, AchievementResponse } from 'api/gen'
+import { useShouldShowAchievementSuccessModal } from 'features/profile/components/Modals/useShouldShowAchievementSuccessModal'
+import { beneficiaryUser } from 'fixtures/user'
+import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/__tests__/setFeatureFlags'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
+import { mockAuthContextWithUser } from 'tests/AuthContextUtils'
+import { renderHook } from 'tests/utils'
+
+jest.mock('features/auth/context/AuthContext')
+
+const achievements: AchievementResponse[] = [
+  {
+    name: AchievementEnum.FIRST_ART_LESSON_BOOKING,
+    seenDate: undefined,
+    unlockedDate: new Date().toDateString(),
+  },
+]
+
+describe('useShouldShowAchievementSuccessModal', () => {
+  describe('enableAchievements FF is false', () => {
+    beforeEach(() => {
+      setFeatureFlags()
+    })
+
+    it('should return false', () => {
+      const { result } = renderHook(useShouldShowAchievementSuccessModal)
+
+      expect(result.current.shouldShowAchievementSuccessModal).toBeFalsy()
+    })
+
+    it('should return false even if there are achievements to show to the user', () => {
+      mockAuthContextWithUser({
+        ...beneficiaryUser,
+        achievements: achievements,
+      })
+
+      const { result } = renderHook(useShouldShowAchievementSuccessModal)
+
+      expect(result.current.shouldShowAchievementSuccessModal).toBeFalsy()
+    })
+  })
+
+  describe('enableAchievements FF is true', () => {
+    beforeEach(() => {
+      setFeatureFlags([RemoteStoreFeatureFlags.ENABLE_ACHIEVEMENTS])
+    })
+
+    it('should return false if there no achievements to show to the user', () => {
+      mockAuthContextWithUser({
+        ...beneficiaryUser,
+        achievements: [],
+      })
+
+      const { result } = renderHook(useShouldShowAchievementSuccessModal)
+
+      expect(result.current.shouldShowAchievementSuccessModal).toBeFalsy()
+    })
+
+    it('should return an empty array if there are no achievements to show to the user', () => {
+      mockAuthContextWithUser({
+        ...beneficiaryUser,
+        achievements: [],
+      })
+
+      const { result } = renderHook(useShouldShowAchievementSuccessModal)
+
+      expect(result.current.achievementsToShow).toEqual([])
+    })
+
+    it('should return true if there are achievements to show to the user', () => {
+      mockAuthContextWithUser({
+        ...beneficiaryUser,
+        achievements: achievements,
+      })
+
+      const { result } = renderHook(useShouldShowAchievementSuccessModal)
+
+      expect(result.current.shouldShowAchievementSuccessModal).toBeTruthy()
+    })
+
+    it('should return an array with the names of the achievements to show to the user', () => {
+      mockAuthContextWithUser({
+        ...beneficiaryUser,
+        achievements: achievements,
+      })
+
+      const { result } = renderHook(useShouldShowAchievementSuccessModal)
+
+      expect(result.current.achievementsToShow).toEqual(['FIRST_ART_LESSON_BOOKING'])
+    })
+  })
+})
