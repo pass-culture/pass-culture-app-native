976d3bf9c7ce29cd0dbe76df5a755bf9a4be12f2
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2024-12-11 16:06:38 +0100

    (PC-33547) fix(OfferTile): fix spacing

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -84,6 +84,7 @@ export const theme = {
     borderRadius: getSpacing(1),
     maxCaptionHeight: {
       venue: getSpacing(18),
+      newOfferTile: getSpacing(32),
       offer: getSpacing(18),
       videoModuleOffer: getSpacing(14),
     },
