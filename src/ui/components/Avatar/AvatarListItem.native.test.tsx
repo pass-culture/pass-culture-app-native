1c5dc547b79c2ada456bcdbec16781caed581d50
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2024-12-11 16:05:20 +0100

    (PC-33458) fix missing props typing

--- src/ui/components/Avatar/AvatarListItem.native.test.tsx
+++ src/ui/components/Avatar/AvatarListItem.native.test.tsx
@@ -6,19 +6,19 @@ import { Profile } from 'ui/svg/icons/Profile'
 
 describe('<AvatarListItem />', () => {
   it('should display artist avatar name', () => {
-    render(<AvatarListItem name="Oda" />)
+    render(<AvatarListItem id={1} name="Oda" />)
 
     expect(screen.getByText('Oda')).toBeOnTheScreen()
   })
 
   it('should display image avatar', () => {
-    render(<AvatarListItem name="Oda" image={<Profile testID="profil" />} />)
+    render(<AvatarListItem id={1} name="Oda" image={<Profile testID="profil" />} />)
 
     expect(screen.getByTestId('profil')).toBeOnTheScreen()
   })
 
   it('should display default image avatar', () => {
-    render(<AvatarListItem name="Oda" />)
+    render(<AvatarListItem id={1} name="Oda" />)
 
     expect(screen.getByTestId('BicolorProfile')).toBeOnTheScreen()
   })
