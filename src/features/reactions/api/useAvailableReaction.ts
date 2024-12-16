1606dc42b9777aa98fbead6cfe76ea8c49ccc387
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2024-12-16 20:49:53 +0100

    (PC-33018) refacto(EndedBookings): use new available reaction route

--- src/features/reactions/api/useAvailableReaction.ts
+++ src/features/reactions/api/useAvailableReaction.ts
@@ -1,31 +1,11 @@
 import { useQuery } from 'react-query'
 
 import { api } from 'api/api'
-import { SubcategoryIdEnum } from 'api/gen'
-
-type Booking = {
-  name: string
-  offerId: number
-  subcategoryId: SubcategoryIdEnum
-  dateUsed: string
-  image?: string | null
-}
-
-type AvailableReactions = {
-  numberOfReactableBookings: number
-  bookings: Booking[]
-}
-
-const fetchAvailableReactions = async (): Promise<AvailableReactions> => {
-  const response = await api.getNativeV1ReactionAvailable()
-  const adaptedResponse: AvailableReactions = {
-    numberOfReactableBookings: response?.numberOfReactableBookings || 0,
-    bookings: response?.bookings || [],
-  }
-
-  return adaptedResponse
-}
+import { GetAvailableReactionsResponse } from 'api/gen'
+import { QueryKeys } from 'libs/queryKeys'
 
 export const useAvailableReaction = () => {
-  return useQuery<AvailableReactions>('availableReactions', fetchAvailableReactions)
+  return useQuery<GetAvailableReactionsResponse>([QueryKeys.AVAILABLE_REACTION], () =>
+    api.getNativeV1ReactionAvailable()
+  )
 }
