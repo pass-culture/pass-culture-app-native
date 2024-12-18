caa589ff8268d2abea1d1b325d07a1b6ac0b6d64
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2024-12-18 17:31:48 +0100

    created useAchievementsMarkAsSeen hook

---
+++ src/features/achievements/api/useMarkAchievementsAsSeen.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from 'react-query'
+
+import { api } from 'api/api'
+import { QueryKeys } from 'libs/queryKeys'
+
+export function useAchievementsMarkAsSeen(
+  onSuccess: () => void,
+  onError: (error: unknown) => void
+) {
+  const queryClient = useQueryClient()
+
+  return useMutation(
+    (achievementIds: number[]) => api.postNativeV1AchievementsMarkAsSeen({ achievementIds }),
+    {
+      onSuccess: () => {
+        queriesToInvalidateOnMarkAsSeen.forEach((queryKey) =>
+          queryClient.invalidateQueries([queryKey])
+        )
+        onSuccess()
+      },
+      onError,
+    }
+  )
+}
+
+const queriesToInvalidateOnMarkAsSeen: QueryKeys[] = [QueryKeys.USER_PROFILE] // To re-fetch the user's achievements
