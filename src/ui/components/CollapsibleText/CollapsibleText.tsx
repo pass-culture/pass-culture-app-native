9bac9929dbba3d190ee454cf2539bc31b5a0760f
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2024-12-18 12:04:44 +0100

    (PC-33535) feat(ui): CollapsibleText can use markdown part

--- src/ui/components/CollapsibleText/CollapsibleText.tsx
+++ src/ui/components/CollapsibleText/CollapsibleText.tsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react'
-import styled from 'styled-components/native'
 
 import { parseMarkdown } from 'features/offer/helpers/parseMarkdown/parseMarkdown'
-import { MarkdownPart } from 'features/offer/types'
+import { MarkdownPartProps } from 'features/offer/types'
 import { highlightLinks } from 'libs/parsers/highlightLinks'
-import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
-import { styledButton } from 'ui/components/buttons/styledButton'
-import { ArrowDown } from 'ui/svg/icons/ArrowDown'
-import { ArrowUp } from 'ui/svg/icons/ArrowUp'
-import { TypoDS } from 'ui/theme'
+import { MarkdownPart } from 'ui/components/MarkdownPart/MarkdownPart'
 
-import { useIsTextEllipsis } from './useIsTextEllipsis'
+import { CollapsibleTextContent } from './CollapsibleTextContent/CollapsibleTextContent'
 
 type Props = {
   children: string
@@ -19,32 +14,19 @@ type Props = {
   isMarkdown?: boolean
 }
 
+// TODO(PC-33655): see the possibilities for improving the component
 export function CollapsibleText({ children, numberOfLines, isMarkdown }: Readonly<Props>) {
   const [expanded, setExpanded] = useState(false)
-  const {
-    isTextEllipsis: shouldDisplayButton,
-    onTextLayout,
-    onLayout,
-  } = useIsTextEllipsis(numberOfLines)
 
-  const onPress = () => setExpanded((prevExpanded) => !prevExpanded)
-
-  const buttonText = expanded ? 'Voir moins' : 'Voir plus'
-  const accessibilityLabel = expanded ? 'Réduire le texte' : 'Étendre le texte'
-  const icon = expanded ? ArrowUp : ArrowDown
+  const onButtonPress = () => setExpanded((prevExpanded) => !prevExpanded)
 
   const renderContent = () => {
     if (isMarkdown) {
-      const parsedText: MarkdownPart[] = parseMarkdown(children)
-      return parsedText.map(({ text, isBold, isItalic, isUnderline }, index) => (
-        <StyledTypo
-          key={index}
-          isBold={isBold}
-          isItalic={isItalic}
-          isUnderline={isUnderline}
-          testID="styledTypo">
-          {highlightLinks(text)}
-        </StyledTypo>
+      const parsedText: MarkdownPartProps[] = parseMarkdown(children)
+      return parsedText.map((part: MarkdownPartProps) => (
+        // A text can contain several times the same part therefore has no unique identifier
+        // If you have better than Math.random() you can update
+        <MarkdownPart key={`markdown-part-${Math.random()}`} {...part} />
       ))
     }
 
@@ -52,43 +34,11 @@ export function CollapsibleText({ children, numberOfLines, isMarkdown }: Readonl
   }
 
   return (
-    <React.Fragment>
-      <TypoDS.Body
-        numberOfLines={expanded ? undefined : numberOfLines}
-        onLayout={onLayout}
-        onTextLayout={onTextLayout}>
-        {renderContent()}
-      </TypoDS.Body>
-      {shouldDisplayButton ? (
-        <ButtonContainer>
-          <SeeMoreButton
-            wording={buttonText}
-            onPress={onPress}
-            accessibilityLabel={accessibilityLabel}
-            icon={icon}
-            buttonHeight="extraSmall"
-          />
-        </ButtonContainer>
-      ) : null}
-    </React.Fragment>
+    <CollapsibleTextContent
+      expanded={expanded}
+      numberOfLines={numberOfLines}
+      renderContent={renderContent}
+      onButtonPress={onButtonPress}
+    />
   )
 }
-
-const StyledTypo = styled(TypoDS.Body)<{
-  isBold?: boolean
-  isItalic?: boolean
-  isUnderline?: boolean
-}>(({ isBold, isItalic, isUnderline }) => ({
-  fontWeight: isBold ? 'bold' : 'normal',
-  fontStyle: isItalic ? 'italic' : 'normal',
-  textDecorationLine: isUnderline ? 'underline' : 'none',
-}))
-
-const ButtonContainer = styled.View({
-  flexDirection: 'row',
-  justifyContent: 'flex-end',
-})
-
-const SeeMoreButton = styledButton(ButtonTertiaryBlack)({
-  maxWidth: 120,
-})
