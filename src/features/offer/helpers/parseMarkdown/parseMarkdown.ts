482482db5f960ea09d6b8a0d32c09b338e5596ab
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2024-12-19 16:20:46 +0100

    (PC-33535) feat(offer): improve parseMarkdown function

--- src/features/offer/helpers/parseMarkdown/parseMarkdown.ts
+++ src/features/offer/helpers/parseMarkdown/parseMarkdown.ts
@@ -3,36 +3,39 @@ import { MarkdownPartProps } from 'features/offer/types'
 // Regular expression to detect bold and italic
 const STYLES_REGEXP = /\*\*(.*?)\*\*|_(.*?)_/g
 
-export function parseMarkdown(markdown: string) {
-  function parse(markdown: string, styles: Record<string, boolean> = {}) {
-    const parts: MarkdownPartProps[] = []
-    let lastIndex = 0
+export function parseMarkdown(
+  markdown: string,
+  styles: Record<string, boolean> = {}
+): MarkdownPartProps[] {
+  const parts: MarkdownPartProps[] = []
+  let lastIndex = 0
 
-    markdown.replace(STYLES_REGEXP, (match, isBold, isItalic, offset) => {
-      // Add plain text before matching
-      if (lastIndex < offset) {
-        parts.push({ text: markdown.slice(lastIndex, offset), ...styles })
-      }
+  if (!markdown) return parts
 
-      const newStyles: Record<string, boolean> = { ...styles }
-      if (isBold) newStyles.isBold = true
-      if (isItalic) newStyles.isItalic = true
-
-      // Recursively parse styled content to handle nesting
-      const styledText = isBold || isItalic
-      parts.push(...parse(styledText, newStyles))
-
-      lastIndex = offset + match.length
-      return match
-    })
-
-    // Add the rest of the plain text
-    if (lastIndex < markdown.length) {
-      parts.push({ text: markdown.slice(lastIndex), ...styles })
+  markdown.replace(STYLES_REGEXP, (match, isBold, isItalic, offset) => {
+    // Add plain text before the styled match
+    if (lastIndex < offset) {
+      parts.push({ text: markdown.slice(lastIndex, offset), ...styles })
     }
 
-    return parts
+    // Determine the styles for the current match
+    const newStyles: Record<string, boolean> = { ...styles }
+    if (isBold) newStyles.isBold = true
+    if (isItalic) newStyles.isItalic = true
+
+    // Recursively parse styled content to handle nesting
+    const styledText = isBold || isItalic
+    parts.push(...parseMarkdown(styledText, newStyles))
+
+    // Update the last index processed
+    lastIndex = offset + match.length
+    return match
+  })
+
+  // Add the rest of the plain text
+  if (lastIndex < markdown.length) {
+    parts.push({ text: markdown.slice(lastIndex), ...styles })
   }
 
-  return parse(markdown)
+  return parts
 }
