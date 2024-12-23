7dc58974f8f28858ea1278be7500d3d91de8afe0
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2024-12-23 15:45:15 +0100

    (PC-33018) refacto(Bookings): use new available reaction routes to display notification

--- src/features/navigation/helpers/useTabBarItemBadges.test.ts
+++ src/features/navigation/helpers/useTabBarItemBadges.test.ts
@@ -1,14 +1,20 @@
+import { availableReactionsSnap } from 'features/bookings/fixtures/availableReactionSnap'
 import { useTabBarItemBadges } from 'features/navigation/helpers/useTabBarItemBadges'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { renderHook } from 'tests/utils'
 
-jest.mock('features/bookings/helpers/useBookingsAwaitingReaction', () => ({
-  useBookingsAwaitingReaction: () => 10,
+jest.mock('features/reactions/api/useAvailableReaction', () => ({
+  useAvailableReaction: jest.fn(() => ({
+    data: availableReactionsSnap,
+  })),
 }))
 
 describe('useTabBarItemBadges', () => {
   it('should return badges by route', () => {
-    const { result } = renderHook(() => useTabBarItemBadges())
+    const { result } = renderHook(() => useTabBarItemBadges(), {
+      wrapper: ({ children }) => reactQueryProviderHOC(children),
+    })
 
-    expect(result.current.Bookings).toBe(10)
+    expect(result.current.Bookings).toEqual(2)
   })
 })
