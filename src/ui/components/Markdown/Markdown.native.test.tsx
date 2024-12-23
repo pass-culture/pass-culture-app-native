2b070c82d855fb8185b7d346e262658f8fb24093
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2024-12-23 11:38:09 +0100

    (PC-33535) refactor(ui): use index rather than MAth.random and PropsWithChildren with typeof

--- src/ui/components/Markdown/Markdown.native.test.tsx
+++ src/ui/components/Markdown/Markdown.native.test.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { render, screen } from 'tests/utils'
 import { Markdown } from 'ui/components/Markdown/Markdown'
+import { Typo } from 'ui/theme'
 
 const TEXT =
   'Lorem **ipsum dolor** sit amet, consectetur _adipiscing_ elit. Maecenas nec tellus in magna convallis egestas eget **_id justo_**. Donec lorem ante, tempor eu diam quis, laoreet rhoncus tortor.'
@@ -18,5 +19,15 @@ describe('Markdown', () => {
     expect(screen.getByText('ipsum dolor')).toHaveProp('testID', 'styledBodyAccent')
     expect(screen.getByText('adipiscing')).toHaveProp('testID', 'styledBodyItalic')
     expect(screen.getByText('id justo')).toHaveProp('testID', 'styledBodyItalicAccent')
+  })
+
+  it('should not render text when children is not a string', () => {
+    render(
+      <Markdown>
+        <Typo.Body>Lorem</Typo.Body>
+      </Markdown>
+    )
+
+    expect(screen.queryByText('Lorem')).not.toBeOnTheScreen()
   })
 })
--- src/ui/components/Markdown/Markdown.tsx
+++ src/ui/components/Markdown/Markdown.tsx
@@ -1,22 +1,19 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, PropsWithChildren } from 'react'
 
 import { parseMarkdown } from 'libs/parsers/parseMarkdown/parseMarkdown'
 import { MarkdownPart } from 'ui/components/MarkdownPart/MarkdownPart'
 import { MarkdownPartProps } from 'ui/components/types'
 
-type Props = {
-  children: string
-}
-
-export const Markdown: FunctionComponent<Props> = ({ children }) => {
-  const parsedText: MarkdownPartProps[] = parseMarkdown(children)
+export const Markdown: FunctionComponent<PropsWithChildren> = ({ children }) => {
+  const parsedText: MarkdownPartProps[] =
+    typeof children === 'string' ? parseMarkdown(children) : []
 
   return (
     <React.Fragment>
-      {parsedText.map((part: MarkdownPartProps) => (
+      {parsedText.map((part: MarkdownPartProps, index) => (
         // A text can contain several times the same part therefore has no unique identifier
-        // If you have better than Math.random() you can update
-        <MarkdownPart key={`markdown-part-${Math.random()}`} {...part} />
+        // If you have better than index you can update
+        <MarkdownPart key={`markdown-part-${index}`} {...part} />
       ))}
     </React.Fragment>
   )
