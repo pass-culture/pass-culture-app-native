062a8cb2a323128e5dd16b94253121a2f602e231
LucasBeneston <lucas.beneston@passculture.app>
2024-12-26 16:16:39 +0100

    (PC-33482) feat(NC): create formatCurrencyFromCentsWithoutCurrenySymbol

---
+++ src/libs/parsers/formatCurrencyFromCentsWithoutCurrenySymbol.native.test.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import { DEFAULT_PACIFIC_FRANC_TO_EURO_RATE } from 'libs/firebase/firestore/exchangeRates/useGetPacificFrancToEuroRate'
+import { Currency } from 'shared/currency/useGetCurrencyToDisplay'
+
+import { formatCurrencyFromCentsWithoutCurrenySymbol } from './formatCurrencyFromCentsWithoutCurrenySymbol'
+
+describe('formatCurrencyFromCentsWithoutCurrenySymbol()', () => {
+  it.each`
+    priceInCents | currency                        | expected
+    ${0}         | ${Currency.EURO}                | ${0}
+    ${100}       | ${Currency.EURO}                | ${1}
+    ${500}       | ${Currency.EURO}                | ${5}
+    ${-500}      | ${Currency.EURO}                | ${-5}
+    ${1050}      | ${Currency.EURO}                | ${10.5}
+    ${-1050}     | ${Currency.EURO}                | ${-10.5}
+    ${1110}      | ${Currency.EURO}                | ${11.1}
+    ${-1110}     | ${Currency.EURO}                | ${-11.1}
+    ${1190}      | ${Currency.EURO}                | ${11.9}
+    ${-1190}     | ${Currency.EURO}                | ${-11.9}
+    ${1199}      | ${Currency.EURO}                | ${11.99}
+    ${-1199}     | ${Currency.EURO}                | ${-11.99}
+    ${100}       | ${Currency.PACIFIC_FRANC_SHORT} | ${120}
+  `(
+    'formatCurrencyFromCentsWithoutCurrenySymbol($priceInCents) = $expected without format price options',
+    ({ priceInCents, currency, expected }) => {
+      expect(
+        formatCurrencyFromCentsWithoutCurrenySymbol(
+          priceInCents,
+          currency,
+          DEFAULT_PACIFIC_FRANC_TO_EURO_RATE
+        )
+      ).toBe(expected)
+    }
+  )
+})
---
+++ src/libs/parsers/formatCurrencyFromCentsWithoutCurrenySymbol.ts
@@ -0,0 +1,16 @@
+import { convertCentsToEuros } from 'libs/parsers/pricesConversion'
+import { RoundUnit, convertEuroToPacificFranc } from 'shared/currency/convertEuroToPacificFranc'
+import { Currency } from 'shared/currency/useGetCurrencyToDisplay'
+
+export const formatCurrencyFromCentsWithoutCurrenySymbol = (
+  priceInCents: number,
+  currency: Currency,
+  euroToPacificFrancRate: number
+): number => {
+  const priceInEuro = convertCentsToEuros(priceInCents)
+
+  if (currency === Currency.PACIFIC_FRANC_SHORT || currency === Currency.PACIFIC_FRANC_FULL) {
+    return convertEuroToPacificFranc(priceInEuro, euroToPacificFrancRate, RoundUnit.UNITS)
+  }
+  return Math.floor(priceInEuro * 100) / 100
+}
