e6c576e070b98f20240234c8469374018592fecc
LucasBeneston <lucas.beneston@passculture.app>
2024-12-26 17:15:45 +0100

    (PC-33428) feat(NC): use formatCurrencyFromCents instead useFormatCurrencyFromCents in useDepositAmountsByAge

--- src/shared/user/useDepositAmountsByAge.ts
+++ src/shared/user/useDepositAmountsByAge.ts
@@ -1,18 +1,39 @@
 import { useSettingsContext } from 'features/auth/context/SettingsContext'
-import { useFormatCurrencyFromCents } from 'libs/parsers/formatCurrencyFromCents'
+import { useGetPacificFrancToEuroRate } from 'libs/firebase/firestore/exchangeRates/useGetPacificFrancToEuroRate'
+import { formatCurrencyFromCents } from 'libs/parsers/formatCurrencyFromCents'
+import { useGetCurrencyToDisplay } from 'shared/currency/useGetCurrencyToDisplay'
 
 export function useDepositAmountsByAge() {
   const { data: settings } = useSettingsContext()
+  const currency = useGetCurrencyToDisplay()
+  const euroToPacificFrancRate = useGetPacificFrancToEuroRate()
+
   const fifteenYearsOldAmount = settings?.depositAmountsByAge?.age_15 ?? 2000
   const sixteenYearsOldAmount = settings?.depositAmountsByAge?.age_16 ?? 3000
   const seventeenYearsOldAmount = settings?.depositAmountsByAge?.age_17 ?? 3000
   const eighteenYearsOldAmount = settings?.depositAmountsByAge?.age_18 ?? 30000
 
   const amountsByAge = {
-    fifteenYearsOldDeposit: useFormatCurrencyFromCents(fifteenYearsOldAmount),
-    sixteenYearsOldDeposit: useFormatCurrencyFromCents(sixteenYearsOldAmount),
-    seventeenYearsOldDeposit: useFormatCurrencyFromCents(seventeenYearsOldAmount),
-    eighteenYearsOldDeposit: useFormatCurrencyFromCents(eighteenYearsOldAmount),
+    fifteenYearsOldDeposit: formatCurrencyFromCents(
+      fifteenYearsOldAmount,
+      currency,
+      euroToPacificFrancRate
+    ),
+    sixteenYearsOldDeposit: formatCurrencyFromCents(
+      sixteenYearsOldAmount,
+      currency,
+      euroToPacificFrancRate
+    ),
+    seventeenYearsOldDeposit: formatCurrencyFromCents(
+      seventeenYearsOldAmount,
+      currency,
+      euroToPacificFrancRate
+    ),
+    eighteenYearsOldDeposit: formatCurrencyFromCents(
+      eighteenYearsOldAmount,
+      currency,
+      euroToPacificFrancRate
+    ),
   }
   return { ...amountsByAge }
 }
