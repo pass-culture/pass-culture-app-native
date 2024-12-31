6baf025dba868ed486ed243178034721d8e61f2a
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2024-12-31 17:01:39 +0100

    (PC-33473) feat(tutorial): add age value in storage when user choose under 15 or over 18 years in AgeSelectionFork (#7461)

--- src/features/tutorial/pages/AgeSelectionFork.tsx
+++ src/features/tutorial/pages/AgeSelectionFork.tsx
@@ -1,13 +1,14 @@
 import { StackScreenProps } from '@react-navigation/stack'
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useCallback } from 'react'
 import styled from 'styled-components/native'
 
 import { navigateToHomeConfig } from 'features/navigation/helpers/navigateToHome'
 import { TutorialRootStackParamList } from 'features/navigation/RootNavigator/types'
 import { AgeButton } from 'features/tutorial/components/AgeButton'
 import { useOnboardingContext } from 'features/tutorial/context/OnboardingWrapper'
-import { NonEligible } from 'features/tutorial/enums'
+import { NonEligible, TutorialTypes } from 'features/tutorial/enums'
 import { TutorialPage } from 'features/tutorial/pages/TutorialPage'
+import { storage } from 'libs/storage'
 import { AccessibleUnorderedList } from 'ui/components/accessibility/AccessibleUnorderedList'
 import { InternalNavigationProps } from 'ui/components/touchableLink/types'
 import { Spacer, TypoDS } from 'ui/theme'
@@ -18,13 +19,26 @@ type AgeButtonProps = {
   age: string
   endButtonTitle: string
   navigateTo: InternalNavigationProps['navigateTo']
-  onBeforeNavigate?: () => void
+  onBeforeNavigate?: () => Promise<void>
 }
 
 type Props = StackScreenProps<TutorialRootStackParamList, 'AgeSelectionFork'>
 
 export const AgeSelectionFork: FunctionComponent<Props> = ({ route }: Props) => {
   const type = route.params.type
+  const isOnboarding = type === TutorialTypes.ONBOARDING
+
+  const { showNonEligibleModal } = useOnboardingContext()
+
+  const onUnder15Press = useCallback(async () => {
+    showNonEligibleModal(NonEligible.UNDER_15, type)
+    if (isOnboarding) await storage.saveObject('user_age', NonEligible.UNDER_15)
+  }, [type, isOnboarding, showNonEligibleModal])
+
+  const onOver18Press = useCallback(async () => {
+    showNonEligibleModal(NonEligible.OVER_18, type)
+    if (isOnboarding) await storage.saveObject('user_age', NonEligible.OVER_18)
+  }, [type, isOnboarding, showNonEligibleModal])
 
   const ageButtons: AgeButtonProps[] = [
     {
@@ -32,7 +46,7 @@ export const AgeSelectionFork: FunctionComponent<Props> = ({ route }: Props) => 
       age: '14 ans',
       endButtonTitle: ' ou moins',
       navigateTo: navigateToHomeConfig,
-      onBeforeNavigate: () => showNonEligibleModal(NonEligible.UNDER_15, type),
+      onBeforeNavigate: onUnder15Press,
     },
     {
       startButtonTitle: 'J’ai entre ',
@@ -45,10 +59,9 @@ export const AgeSelectionFork: FunctionComponent<Props> = ({ route }: Props) => 
       age: '19 ans',
       endButtonTitle: ' ou plus',
       navigateTo: { screen: 'OnboardingGeneralPublicWelcome' },
+      onBeforeNavigate: onOver18Press,
     },
   ]
-
-  const { showNonEligibleModal } = useOnboardingContext()
 
   return (
     <TutorialPage title="Pour commencer, peux-tu nous dire ton âge&nbsp;?">
