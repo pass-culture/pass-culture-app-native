f4a9995a8e77f08e1cb719e5b5ec36941529c74d
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-01-03 09:38:13 +0100

    Added cookies acceptance to conditions

--- src/features/home/components/helpers/useShouldShowReactionModal.native.test.ts
+++ src/features/home/components/helpers/useShouldShowReactionModal.native.test.ts
@@ -9,6 +9,7 @@ import {
 import { CURRENT_DATE } from 'features/auth/fixtures/fixtures'
 import { useBookings } from 'features/bookings/api'
 import { bookingsSnap } from 'features/bookings/fixtures/bookingsSnap'
+import * as CookiesUpToDate from 'features/cookies/helpers/useIsCookiesListUpToDate'
 import { useShouldShowReactionModal } from 'features/home/components/helpers/useShouldShowReactionModal'
 import { TWENTY_FOUR_HOURS } from 'features/home/constants'
 import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/__tests__/setFeatureFlags'
@@ -30,6 +31,14 @@ jest.mock('libs/subcategories/useSubcategories', () => ({
     data: mockData,
   }),
 }))
+
+const mockUseIsCookiesListUpToDate = jest
+  .spyOn(CookiesUpToDate, 'useIsCookiesListUpToDate')
+  .mockReturnValue({
+    isCookiesListUpToDate: true,
+    cookiesLastUpdate: { lastUpdated: new Date('10/12/2022'), lastUpdateBuildVersion: 10208002 },
+  })
+jest.mock('libs/firebase/analytics/analytics') // mocking analytics in useIsCookiesListUpToDate
 
 jest.useFakeTimers()
 
@@ -75,6 +84,19 @@ describe('useShouldShowReactionModal', () => {
           ],
         },
       })
+      const { result } = renderHook(useShouldShowReactionModal)
+
+      expect(result.current.shouldShowReactionModal).toBeFalsy()
+    })
+
+    it('should return false if cookies where not accepted', () => {
+      const dateUsed = new Date(CURRENT_DATE.getTime() - TWENTY_FOUR_HOURS - 1000).toISOString()
+      mockUseIsCookiesListUpToDate.mockReturnValueOnce({
+        isCookiesListUpToDate: true,
+        cookiesLastUpdate: undefined,
+      })
+      mockUseBookingsReturnValueOnce(dateUsed)
+
       const { result } = renderHook(useShouldShowReactionModal)
 
       expect(result.current.shouldShowReactionModal).toBeFalsy()
