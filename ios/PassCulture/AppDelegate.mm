7bf9c0787e0c8940313772dd0623b4a3105e66bd
NolwenMajorFrances <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-01-07 16:48:20 +0100

    AppDelegate

--- ios/PassCulture/AppDelegate.mm
+++ ios/PassCulture/AppDelegate.mm
@@ -87,6 +87,11 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
+  return [self getBundleURL];
+}
+ 
+- (NSURL *)getBundleURL
+{
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
