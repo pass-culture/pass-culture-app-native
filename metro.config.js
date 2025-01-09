f9132ce6f6a19916b887ce88b2832a1fa39ae6dc
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-01-09 11:32:50 +0100

    WIP

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,20 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {}
+const config = {
+  resolver: {
+    blockList: [
+      /*
+      Removes the following error that appears sometimes when starting metro
+
+      metro-file-map: Haste module naming collision: @pass-culture/web-proxy
+      The following files share their name; please adjust your hasteImpl:
+          * <rootDir>/server/package.json
+          * <rootDir>/server/build/package.json
+      */
+      /^\/server/, // ça résout rien du tout, c'est flaky as fuck
+    ]
+  }
+}
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config)
