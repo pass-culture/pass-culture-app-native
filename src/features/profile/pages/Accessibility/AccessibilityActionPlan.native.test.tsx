d317fb8c6fab453c7acae1585629a3a014993576
LucasBeneston <lucas.beneston@passculture.app>
2025-01-10 14:38:38 +0100

    (PC-33786) feat(accessibility): use userEvent instead fireEvent in accessibility tests

--- src/features/profile/pages/Accessibility/AccessibilityActionPlan.native.test.tsx
+++ src/features/profile/pages/Accessibility/AccessibilityActionPlan.native.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { ReactTestInstance } from 'react-test-renderer'
 
 import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
 import { AccessibilityActionPlan } from 'features/profile/pages/Accessibility/AccessibilityActionPlan'
-import { render, fireEvent, screen } from 'tests/utils'
+import { render, userEvent, screen } from 'tests/utils'
 
 const openURLSpy = jest.spyOn(NavigationHelpers, 'openUrl')
 
@@ -14,6 +15,9 @@ jest.mock('react-native/Libraries/Animated/createAnimatedComponent', () => {
   }
 })
 
+const user = userEvent.setup()
+jest.useFakeTimers()
+
 describe('AccessibilityActionPlan', () => {
   it('should render correctly', () => {
     render(<AccessibilityActionPlan />)
@@ -21,18 +25,33 @@ describe('AccessibilityActionPlan', () => {
     expect(screen).toMatchSnapshot()
   })
 
-  it.each(['https://pass.culture.fr/', 'https://passculture.app/', 'https://passculture.pro/'])(
-    'should open $link when clicking help center link',
-    (link) => {
-      render(<AccessibilityActionPlan />)
+  it('should open https://pass.culture.fr/ when clicking on "https://pass.culture.fr/" link', async () => {
+    render(<AccessibilityActionPlan />)
 
-      const linkComponents = screen.getAllByText(link)
-      linkComponents.forEach((link) => {
-        fireEvent.press(link)
-      })
+    const links = screen.getAllByText('https://pass.culture.fr/')
+    // Using as because links is never undefined and the typing is not correct
+    await user.press(links[0] as ReactTestInstance)
 
-      expect(openURLSpy).toHaveBeenCalledTimes(linkComponents.length)
-      expect(openURLSpy).toHaveBeenCalledWith(link, undefined, true)
-    }
-  )
+    expect(openURLSpy).toHaveBeenCalledWith('https://pass.culture.fr/', undefined, true)
+  })
+
+  it('should open https://passculture.app/ when clicking on "https://passculture.app/" link', async () => {
+    render(<AccessibilityActionPlan />)
+
+    const links = screen.getAllByText('https://passculture.app/')
+    // Using as because links is never undefined and the typing is not correct
+    await user.press(links[0] as ReactTestInstance)
+
+    expect(openURLSpy).toHaveBeenCalledWith('https://passculture.app/', undefined, true)
+  })
+
+  it('should open https://passculture.pro/ when clicking on "https://passculture.pro/" link', async () => {
+    render(<AccessibilityActionPlan />)
+
+    const links = screen.getAllByText('https://passculture.pro/')
+    // Using as because links is never undefined and the typing is not correct
+    await user.press(links[0] as ReactTestInstance)
+
+    expect(openURLSpy).toHaveBeenCalledWith('https://passculture.pro/', undefined, true)
+  })
 })
