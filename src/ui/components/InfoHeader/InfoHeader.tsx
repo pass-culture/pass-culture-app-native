7782efde3209d9fd63605349ed09a01c9758fe80
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2025-01-10 12:07:19 +0100

    (PC-33886) refactor(VenuePreview): Create a reusable/generic component InfoHeader
    Co-authored-by: mmeissonnier-pass <mmeissonnier-pass@users.noreply.github.com>
    Co-authored-by: Corentin Normant <corentin.normant@gmail.com>
    Co-authored-by: Henri-cloud <Henri-cloud@users.noreply.github.com>

---
+++ src/ui/components/InfoHeader/InfoHeader.tsx
@@ -0,0 +1,67 @@
+import React, { FunctionComponent, PropsWithChildren, ReactNode } from 'react'
+import styled from 'styled-components/native'
+
+import { ThumbnailPlaceholder } from 'ui/components/InfoHeader/ThumbnailPlaceHolder'
+import { TypoDS, getSpacing } from 'ui/theme'
+
+type InfoHeaderProps = PropsWithChildren<{
+  title: string
+  defaultThumbnailSize: number
+  subtitle?: string
+  thumbnailComponent?: ReactNode
+  rightComponent?: ReactNode
+}>
+
+export const InfoHeader: FunctionComponent<InfoHeaderProps> = ({
+  title,
+  subtitle,
+  rightComponent,
+  defaultThumbnailSize,
+  thumbnailComponent,
+  children,
+}) => (
+  <StyledView>
+    {thumbnailComponent ? (
+      thumbnailComponent
+    ) : (
+      <ThumbnailPlaceholder
+        width={defaultThumbnailSize}
+        height={defaultThumbnailSize}
+        testID="VenuePreviewPlaceholder"
+      />
+    )}
+    <RightContainer>
+      {children}
+      <TitleContainer>
+        <Title>{title}</Title>
+        {rightComponent ? rightComponent : null}
+      </TitleContainer>
+      {subtitle ? <Subtitle>{subtitle}</Subtitle> : null}
+    </RightContainer>
+  </StyledView>
+)
+
+const StyledView = styled.View({
+  flexShrink: 1,
+  flexDirection: 'row',
+  columnGap: getSpacing(2),
+})
+
+const RightContainer = styled.View({
+  flexShrink: 1,
+  justifyContent: 'center',
+})
+
+const TitleContainer = styled.View({
+  flexDirection: 'row',
+  alignItems: 'center',
+  columnGap: getSpacing(1),
+})
+
+const Title = styled(TypoDS.BodyAccent).attrs({ numberOfLines: 1 })({
+  flexShrink: 1,
+})
+
+const Subtitle = styled(TypoDS.BodyAccentXs).attrs({ numberOfLines: 2 })(({ theme }) => ({
+  color: theme.colors.greyDark,
+}))
