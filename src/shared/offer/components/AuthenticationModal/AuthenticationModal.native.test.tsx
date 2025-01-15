9097878ed15d05fffa8a9004ab1bad14973fdbc6
LucasBeneston <lucas.beneston@passculture.app>
2025-01-15 11:32:02 +0100

    (PC-33962) feat(creditV3): change subtitle banner with enableCreditV3 in AuthenticationModal

--- src/shared/offer/components/AuthenticationModal/AuthenticationModal.native.test.tsx
+++ src/shared/offer/components/AuthenticationModal/AuthenticationModal.native.test.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { navigate } from '__mocks__/@react-navigation/native'
 import { StepperOrigin } from 'features/navigation/RootNavigator/types'
 import { analytics } from 'libs/analytics'
+import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/__tests__/setFeatureFlags'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 import { fireEvent, render, waitFor, screen } from 'tests/utils'
 
 import { AuthenticationModal } from './AuthenticationModal'
@@ -17,6 +19,25 @@ jest.mock('react-native/Libraries/Animated/createAnimatedComponent', () => {
 })
 
 describe('<AuthenticationModal />', () => {
+  beforeEach(() => {
+    setFeatureFlags()
+  })
+
+  it('should display banner with credit V2 subtitle', () => {
+    render(
+      <AuthenticationModal
+        visible
+        offerId={OFFER_ID}
+        hideModal={hideModal}
+        from={StepperOrigin.BOOKING}
+      />
+    )
+
+    const subtitle = 'Tu as entre 15 et 18 ans\u00a0?'
+
+    expect(screen.getByText(subtitle)).toBeOnTheScreen()
+  })
+
   it('should match previous snapshot', () => {
     render(
       <AuthenticationModal
@@ -153,6 +174,27 @@ describe('<AuthenticationModal />', () => {
 
     await waitFor(() => {
       expect(analytics.logQuitAuthenticationModal).toHaveBeenNthCalledWith(1, OFFER_ID)
+    })
+  })
+
+  describe('when enableCreditV3 activated', () => {
+    beforeEach(() => {
+      setFeatureFlags([RemoteStoreFeatureFlags.ENABLE_CREDIT_V3])
+    })
+
+    it('should display banner with credit V3 subtitle', () => {
+      render(
+        <AuthenticationModal
+          visible
+          offerId={OFFER_ID}
+          hideModal={hideModal}
+          from={StepperOrigin.BOOKING}
+        />
+      )
+
+      const subtitle = 'Tu as entre 17 et 18 ans\u00a0?'
+
+      expect(screen.getByText(subtitle)).toBeOnTheScreen()
     })
   })
 })
