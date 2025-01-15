23cc983f3cebe4691a3a78e955683c429ba89655
Xavier Le Cunff <xavier.lecunff@passculture.app>
2025-01-15 20:05:02 +0100

    refactor: move up useWhichModalToShow content up

--- src/features/home/components/HomeModals.tsx
+++ src/features/home/components/HomeModals.tsx
@@ -1,19 +1,28 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
+import { useShouldShowAchievementSuccessModal } from 'features/achievements/hooks/useShouldShowAchievementSuccessModal'
 import { AchievementSuccessModal } from 'features/achievements/pages/AchievementSuccessModal'
 import { useAuthContext } from 'features/auth/context/AuthContext'
 import { useBookings } from 'features/bookings/api'
+import {
+  ModalDisplayState,
+  useShouldShowReactionModal,
+} from 'features/home/components/helpers/useShouldShowReactionModal'
 import { IncomingReactionModalContainer } from 'features/home/components/IncomingReactionModalContainer/IncomingReactionModalContainer'
-import { ModalToShow, useWhichModalToShow } from 'features/home/helpers/useWhichModalToShow'
 import { useModal } from 'ui/components/modals/useModal'
+
+enum ModalToShow {
+  PENDING = 'pending',
+  ACHIEVEMENT = 'achievement',
+  REACTION = 'reaction',
+  NONE = 'none',
+}
 
 export const HomeModals: FC = () => {
   const { data: bookings, isLoading: isBookingsLoading } = useBookings()
   const { isUserLoading, user } = useAuthContext()
 
   const isModalContainerReady = !isUserLoading && !isBookingsLoading
-
-  const { modalToShow } = useWhichModalToShow(bookings, isBookingsLoading)
 
   const achievementsToShow =
     user?.achievements?.filter((achievement) => !achievement.seenDate) || []
@@ -22,6 +31,22 @@ export const HomeModals: FC = () => {
     bookings?.ended_bookings?.filter(
       (booking) => booking.enablePopUpReaction && !booking.userReaction
     ) ?? []
+
+  const [modalToShow, setModalToShow] = useState<ModalToShow>(ModalToShow.PENDING)
+  const shouldShowReactionModal = useShouldShowReactionModal(bookings, isBookingsLoading)
+  const shouldShowAchievementSuccessModal = useShouldShowAchievementSuccessModal()
+  if (!isBookingsLoading && modalToShow === ModalToShow.PENDING) {
+    if (shouldShowReactionModal === ModalDisplayState.SHOULD_SHOW) {
+      setModalToShow(ModalToShow.REACTION)
+    } else if (shouldShowAchievementSuccessModal === ModalDisplayState.SHOULD_SHOW) {
+      setModalToShow(ModalToShow.ACHIEVEMENT)
+    } else if (
+      shouldShowReactionModal === ModalDisplayState.SHOULD_NOT_SHOW &&
+      shouldShowAchievementSuccessModal === ModalDisplayState.SHOULD_NOT_SHOW
+    ) {
+      setModalToShow(ModalToShow.NONE)
+    }
+  }
 
   const {
     visible: visibleAchievementModal,
