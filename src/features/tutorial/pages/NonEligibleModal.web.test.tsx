3bc5bfe7d225e7ed765290cb8ceadb65ea8d8892
LucasBeneston <lucas.beneston@passculture.app>
2025-01-15 16:44:47 +0100

    (PC-33970) feat(creditV3): add setFeatureFlags in NonEligibleModal web test

--- src/features/tutorial/pages/NonEligibleModal.web.test.tsx
+++ src/features/tutorial/pages/NonEligibleModal.web.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { NonEligible, TutorialTypes } from 'features/tutorial/enums'
+import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/__tests__/setFeatureFlags'
 import { checkAccessibilityFor, render } from 'tests/utils/web'
 
 import { NonEligibleModal } from './NonEligibleModal'
@@ -9,6 +10,8 @@ jest.mock('libs/firebase/analytics/analytics')
 jest.mock('libs/firebase/remoteConfig/remoteConfig.services')
 
 describe('<NonEligibleModal/>', () => {
+  beforeEach(() => setFeatureFlags())
+
   describe('Accessibility', () => {
     it('should render correctly for onboarding', async () => {
       const { container } = render(
