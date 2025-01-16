ffc161a183afe586845d741608b4e48ee7aaad95
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-01-16 09:50:55 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -3,20 +3,35 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
-    { self
-    , nixpkgs
-    , flake-utils
+    {
+      self,
+      nixpkgs,
+      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShells.default =
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        pkgs.mkShellNoCC {
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
           packages = [
             pkgs.devbox
-            pkgs.sdkmanager
+            self.packages."${system}".sdkmanager
           ];
         };
-    });
+
+        packages.sdkmanager = pkgs.stdenv.mkDerivation {
+          name = "sdkmanager-with-certificates";
+          src = pkgs.sdkmanager;
+          buildInputs = [ pkgs.makeWrapper ];
+          postInstall = ''
+            mkdir --parents "$out/bin"
+            cp "${pkgs.sdkmanager}/bin/sdkmanager" "$out/bin/sdkmanager"
+            wrapProgram "$out/bin/sdkmanager" \
+              --set NIX_SSL_CERT_FILE "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+          '';
+        };
+      }
+    );
 }
