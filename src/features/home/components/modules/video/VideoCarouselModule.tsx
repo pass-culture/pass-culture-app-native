bfc1ecccbed6431ce781ceb8bd7d5e3ecdbc8128
Tanguy <47600889+tprache-pass@users.noreply.github.com>
2025-01-20 15:34:53 +0100

    fix: issues with carousel

--- src/features/home/components/modules/video/VideoCarouselModule.tsx
+++ src/features/home/components/modules/video/VideoCarouselModule.tsx
@@ -191,7 +191,9 @@ export const VideoCarouselModule: FunctionComponent<VideoCarouselModuleBaseProps
               testID="videoCarousel"
               vertical={false}
               height={CAROUSEL_HEIGHT}
-              panGestureHandlerProps={{ activeOffsetX: [-5, 5] }}
+              onConfigurePanGesture={(gestureChain) => {
+                gestureChain.activeOffsetX([-5, 5])
+              }}
               width={windowWidth}
               loop={false}
               scrollAnimationDuration={CAROUSEL_ANIMATION_DURATION}
