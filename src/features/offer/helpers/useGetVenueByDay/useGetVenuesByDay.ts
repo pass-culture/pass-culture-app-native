cbef60bad8b700c1820eb75f9d60ddae1f05927c
Inès Mouandjo Lobé <158567429+imouandjolobe-pass@users.noreply.github.com>
2025-01-22 22:57:26 +0100

    refactor: remove some useMemo

--- src/features/offer/helpers/useGetVenueByDay/useGetVenuesByDay.ts
+++ src/features/offer/helpers/useGetVenueByDay/useGetVenuesByDay.ts
@@ -1,5 +1,4 @@
 import { isToday } from 'date-fns'
-import { useMemo } from 'react'
 
 import { OfferResponseV2 } from 'api/gen'
 import { moviesOfferBuilder } from 'features/offer/components/MoviesScreeningCalendar/moviesOffer.builder'
@@ -8,40 +7,27 @@ import { useUserLocation } from 'features/offer/helpers/useUserLocation/useUserL
 export const useGetVenuesByDay = (date: Date, offers: OfferResponseV2[]) => {
   const location = useUserLocation()
 
-  const dayOffers = useMemo(
-    () =>
-      moviesOfferBuilder(offers)
-        .sortedByDistance(location)
-        .withScreeningsOnDay(date)
-        .withoutScreeningsAfterNbDays(15)
-        .build(),
-    [date, location, offers]
-  )
+  const dayOffers = moviesOfferBuilder(offers)
+    .sortedByDistance(location)
+    .withScreeningsOnDay(date)
+    .build()
 
-  const nextOffers = useMemo(() => {
-    return moviesOfferBuilder(offers)
-      .withoutScreeningsAfterNbDays(15)
-      .withoutScreeningsOnDay(date)
-      .withNextScreeningFromDate(date)
-      .sortedByDistance(location)
-      .build()
-  }, [date, location, offers])
+  const nextOffers = moviesOfferBuilder(offers)
+    .withoutScreeningsAfterNbDays(15)
+    .withoutScreeningsOnDay(date)
+    .withNextScreeningFromDate(date)
+    .sortedByDistance(location)
+    .build()
 
-  const after15DaysOffers = useMemo(
-    () =>
-      moviesOfferBuilder(offers).sortedByDistance(location).withScreeningsAfterNbDays(15).build(),
-    [location, offers]
-  )
+  const after15DaysOffers = moviesOfferBuilder(offers)
+    .sortedByDistance(location)
+    .withScreeningsAfterNbDays(15)
+    .build()
 
-  const movieOffers = useMemo(
-    () => [...dayOffers, ...nextOffers, ...after15DaysOffers],
-    [dayOffers, nextOffers, after15DaysOffers]
-  )
+  const movieOffers = [...dayOffers, ...nextOffers, ...after15DaysOffers]
 
-  const hasStocksOnlyAfter15Days = useMemo(
-    () => !dayOffers.length && !nextOffers.length && after15DaysOffers.length > 0,
-    [after15DaysOffers.length, dayOffers.length, nextOffers.length]
-  )
+  const hasStocksOnlyAfter15Days =
+    !dayOffers.length && !nextOffers.length && after15DaysOffers.length > 0
 
   return {
     movieOffers,
