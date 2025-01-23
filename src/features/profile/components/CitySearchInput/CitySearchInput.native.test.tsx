c2a2aa0db14d826d922e74d42bc4beb1a0d22b20
NolwenMajorFrances <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-01-23 10:48:47 +0100

    (BSR): use userEvent

--- src/features/profile/components/CitySearchInput/CitySearchInput.native.test.tsx
+++ src/features/profile/components/CitySearchInput/CitySearchInput.native.test.tsx
@@ -5,13 +5,15 @@ import { mockedSuggestedCities } from 'libs/place/fixtures/mockedSuggestedCities
 import { CitiesResponse, CITIES_API_URL } from 'libs/place/useCities'
 import { mockServer } from 'tests/mswServer'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { fireEvent, render, screen, waitFor } from 'tests/utils'
+import { render, screen, userEvent, waitFor } from 'tests/utils'
 
 jest.mock('libs/subcategories/useSubcategory')
 jest.mock('libs/network/NetInfoWrapper')
 
 const POSTAL_CODE = '83570'
 const NEW_CALEDONIA_NORTHERN_PROVINCE_POSTAL_CODE = '98825'
+const user = userEvent.setup()
+jest.useFakeTimers()
 
 describe('<CitySearchInput />', () => {
   it('should display error message when the user enters a valid postal code but no city found', async () => {
@@ -19,7 +21,7 @@ describe('<CitySearchInput />', () => {
     render(reactQueryProviderHOC(<CitySearchInput />))
 
     const input = screen.getByPlaceholderText('Ex\u00a0: 75017')
-    fireEvent.changeText(input, POSTAL_CODE)
+    await user.type(input, POSTAL_CODE)
 
     await waitFor(() => {
       expect(
@@ -35,15 +37,13 @@ describe('<CitySearchInput />', () => {
     render(reactQueryProviderHOC(<CitySearchInput />))
 
     const input = screen.getByPlaceholderText('Ex\u00a0: 75017')
-    fireEvent.changeText(input, NEW_CALEDONIA_NORTHERN_PROVINCE_POSTAL_CODE)
+    await user.type(input, NEW_CALEDONIA_NORTHERN_PROVINCE_POSTAL_CODE)
 
-    await waitFor(() => {
-      expect(
-        screen.getByText(
-          'Malheureusement, ton code postal correspond à une zone qui n’est pas éligible au pass Culture.'
-        )
-      ).toBeOnTheScreen()
-    })
+    expect(
+      screen.getByText(
+        'Malheureusement, ton code postal correspond à une zone qui n’est pas éligible au pass Culture.'
+      )
+    ).toBeOnTheScreen()
   })
 
   it('should display cities when the user enters a valid postal code', async () => {
@@ -51,12 +51,10 @@ describe('<CitySearchInput />', () => {
     render(reactQueryProviderHOC(<CitySearchInput />))
 
     const input = screen.getByPlaceholderText('Ex\u00a0: 75017')
-    fireEvent.changeText(input, POSTAL_CODE)
+    await user.type(input, POSTAL_CODE)
 
-    await waitFor(() => {
-      expect(screen.getByText(mockedSuggestedCities[0].nom)).toBeOnTheScreen()
-      expect(screen.getByText(mockedSuggestedCities[1].nom)).toBeOnTheScreen()
-    })
+    expect(screen.getByText(mockedSuggestedCities[0].nom)).toBeOnTheScreen()
+    expect(screen.getByText(mockedSuggestedCities[1].nom)).toBeOnTheScreen()
   })
 
   it('should reset the search input when pressing the reset icon', async () => {
@@ -64,13 +62,11 @@ describe('<CitySearchInput />', () => {
     render(reactQueryProviderHOC(<CitySearchInput />))
 
     const input = screen.getByPlaceholderText('Ex\u00a0: 75017')
-    fireEvent.changeText(input, POSTAL_CODE)
+    await user.type(input, POSTAL_CODE)
 
     const resetIcon = screen.getByTestId('Réinitialiser la recherche')
-    fireEvent.press(resetIcon)
+    await user.press(resetIcon)
 
-    await waitFor(() => {
-      expect(input.props.value).toBe('')
-    })
+    expect(input.props.value).toBe('')
   })
 })
