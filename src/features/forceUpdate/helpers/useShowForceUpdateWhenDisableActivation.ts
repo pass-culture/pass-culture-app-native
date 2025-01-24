e6ae86e31b590b170530c352ca09bce5e4a0f4c9
LucasBeneston <lucas.beneston@passculture.app>
2025-01-24 14:53:10 +0100

    (PC-34157) feat(creditV3): add useShowForceUpdateWhenDisableActivation

---
+++ src/features/forceUpdate/helpers/useShowForceUpdateWhenDisableActivation.ts
@@ -0,0 +1,14 @@
+import { ForceUpdateWithResetErrorBoundary } from 'features/forceUpdate/pages/ForceUpdateWithResetErrorBoundary'
+import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
+import { ScreenError } from 'libs/monitoring'
+import { LogTypeEnum } from 'libs/monitoring/errors'
+
+export const useShowForceUpdateWhenDisableActivation = () => {
+  const disableAction = useFeatureFlag(RemoteStoreFeatureFlags.DISABLE_ACTIVATION)
+  if (disableAction)
+    throw new ScreenError('Must update app', {
+      Screen: ForceUpdateWithResetErrorBoundary,
+      logType: LogTypeEnum.IGNORED,
+    })
+}
