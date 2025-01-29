e6a6e6213bb8df833f92712cd61b2f9e90ada554
imouandjolobe-pass <158567429+imouandjolobe-pass@users.noreply.github.com>
2025-01-29 13:11:20 +0100

    (PC-33896) fix(Url): fix web app url preview (#7579)

---
+++ src/features/navigation/RootNavigator/Header/getTabNavigatorConfig.ts
@@ -0,0 +1,24 @@
+import { defaultDisabilitiesProperties } from 'features/accessibility/context/AccessibilityFiltersWrapper'
+import { getTabNavConfig } from 'features/navigation/TabBar/helpers'
+import { TabStateRoute } from 'features/navigation/TabBar/types'
+import { initialSearchState } from 'features/search/context/reducer'
+import { SearchState } from 'features/search/types'
+
+export const getTabNavigatorConfig = (route: TabStateRoute, searchState: SearchState) => {
+  if (route.name !== 'SearchStackNavigator') {
+    return getTabNavConfig(route.name)
+  }
+
+  const searchParams = route.isSelected
+    ? {
+        ...initialSearchState,
+        locationFilter: searchState.locationFilter,
+        accessibilityFilter: defaultDisabilitiesProperties,
+      }
+    : {}
+
+  return getTabNavConfig(route.name, {
+    screen: 'SearchLanding',
+    params: searchParams,
+  })
+}
