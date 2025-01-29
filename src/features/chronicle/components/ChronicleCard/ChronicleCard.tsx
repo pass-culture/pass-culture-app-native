10a1b8eaf19d3d957d89152bf51c3e1cac58f933
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-01-29 17:03:12 +0100

    (PC-33638) feat(chronicle): add see more button on chronicle card

--- src/features/chronicle/components/ChronicleCard/ChronicleCard.tsx
+++ src/features/chronicle/components/ChronicleCard/ChronicleCard.tsx
@@ -1,17 +1,25 @@
 import React, { FunctionComponent } from 'react'
+import { View } from 'react-native'
 import styled from 'styled-components/native'
 
 import { ChronicleCardData } from 'features/chronicle/type'
+import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
+import { styledButton } from 'ui/components/buttons/styledButton'
 import { InfoHeader } from 'ui/components/InfoHeader/InfoHeader'
 import { Separator } from 'ui/components/Separator'
+import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
+import { InternalNavigationProps } from 'ui/components/touchableLink/types'
 import { ViewGap } from 'ui/components/ViewGap/ViewGap'
 import { BookClubCertification } from 'ui/svg/BookClubCertification'
+import { PlainMore } from 'ui/svg/icons/PlainMore'
 import { TypoDS, getShadow, getSpacing } from 'ui/theme'
 
 const CHRONICLE_THUMBNAIL_SIZE = getSpacing(14)
 
 type Props = ChronicleCardData & {
   cardWidth?: number
+  shouldShowSeeMoreButton?: boolean
+  navigateTo?: InternalNavigationProps['navigateTo']
 }
 
 export const ChronicleCard: FunctionComponent<Props> = ({
@@ -21,6 +29,8 @@ export const ChronicleCard: FunctionComponent<Props> = ({
   description,
   date,
   cardWidth,
+  shouldShowSeeMoreButton,
+  navigateTo,
 }) => {
   return (
     <Container gap={3} testID={`chronicle-card-${id.toString()}`} width={cardWidth}>
@@ -32,7 +42,18 @@ export const ChronicleCard: FunctionComponent<Props> = ({
       />
       <Separator.Horizontal />
       <Description>{description}</Description>
-      <PublicationDate>{date}</PublicationDate>
+      <BottomCardContainer>
+        <PublicationDate>{date}</PublicationDate>
+        {shouldShowSeeMoreButton && navigateTo ? (
+          <View>
+            <InternalTouchableLink
+              as={StyledButtonTertiaryBlack}
+              wording="Voir plus"
+              navigateTo={navigateTo}
+            />
+          </View>
+        ) : null}
+      </BottomCardContainer>
     </Container>
   )
 }
@@ -58,6 +79,22 @@ const Description = styled(TypoDS.BodyAccentS)(({ theme }) => ({
   flexGrow: 1,
 }))
 
+const BottomCardContainer = styled.View({
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+})
+
 const PublicationDate = styled(TypoDS.BodyAccentXs)(({ theme }) => ({
   color: theme.colors.greyDark,
+  alignSelf: 'center',
 }))
+
+const StyledPlainMore = styled(PlainMore).attrs(({ theme }) => ({
+  size: theme.icons.sizes.extraSmall,
+}))``
+
+const StyledButtonTertiaryBlack = styledButton(ButtonTertiaryBlack).attrs({
+  icon: StyledPlainMore,
+  iconPosition: 'right',
+  buttonHeight: 'extraSmall',
+})``
