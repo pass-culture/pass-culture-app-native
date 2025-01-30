f695bcc400e04051687f82ea5bfca96a0bc9b48b
Inès Mouandjo Lobé <158567429+imouandjolobe-pass@users.noreply.github.com>
2025-01-30 11:00:47 +0100

    (PC-31482) feat(GtlPlaylist): allow categories filtering

--- src/features/gtlPlaylist/api/fetchGTLPlaylistConfig.ts
+++ src/features/gtlPlaylist/api/fetchGTLPlaylistConfig.ts
@@ -7,7 +7,7 @@ import { GtlPlaylist } from 'libs/contentful/types'
 import { env } from 'libs/environment'
 import { getExternal } from 'libs/fetch'
 
-const PARAMS = `?content_type=gtlPlaylist&access_token=${env.CONTENTFUL_PUBLIC_ACCESS_TOKEN}`
+const PARAMS = `?include=2&content_type=gtlPlaylist&access_token=${env.CONTENTFUL_PUBLIC_ACCESS_TOKEN}`
 const URL = `${CONTENTFUL_BASE_URL}/entries${PARAMS}`
 
 export async function fetchGTLPlaylistConfig() {
