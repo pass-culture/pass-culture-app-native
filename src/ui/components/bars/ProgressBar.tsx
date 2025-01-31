a69aed4f50289fcbf87f4fcf0dd7319d46607946
xlecunff-pass <163895831+xlecunff-pass@users.noreply.github.com>
2025-01-31 10:06:21 +0100

    (BSR) refactor(Linter): remove no unchecked index (#7583)

--- src/ui/components/bars/ProgressBar.tsx
+++ src/ui/components/bars/ProgressBar.tsx
@@ -16,8 +16,7 @@ const ProgressBarComponent: React.FC<ProgressBarProps> = ({ colors, progress, he
   const LinearColors = colors.length === 1 ? [colors[0], colors[0]] : colors
 
   const LinearGradientBar = styled(LinearGradient).attrs({
-    // @ts-expect-error: because of noUncheckedIndexedAccess
-    colors: LinearColors,
+    colors: LinearColors.map(String),
     useAngle: true,
     angle: 90,
   })({
