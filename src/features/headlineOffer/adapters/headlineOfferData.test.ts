4925f76f24f744d702e0db2a8d775c8416b6c3fe
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2025-01-31 15:08:43 +0100

    (PC-32988) fix(VenueOffers): missing props on test

--- src/features/headlineOffer/adapters/headlineOfferData.test.ts
+++ src/features/headlineOffer/adapters/headlineOfferData.test.ts
@@ -1,18 +1,7 @@
-import { CategoryIdEnum, SubcategoryIdEnumv2 } from 'api/gen'
 import { headlineOfferData } from 'features/headlineOffer/adapters/headlineOfferData'
-import { CategoryHomeLabelMapping, CategoryIdMapping } from 'libs/subcategories/types'
+import { mockLabelMapping, mockMapping } from 'features/headlineOffer/fixtures/mockMapping'
 import { Currency } from 'shared/currency/useGetCurrencyToDisplay'
 import { offersFixture } from 'shared/offer/offer.fixture'
-
-const mockMapping = {
-  LIVRE_PAPIER: CategoryIdEnum.LIVRE,
-  SEANCE_CINE: CategoryIdEnum.CINEMA,
-} as CategoryIdMapping
-
-const mockLabelMapping = {
-  [SubcategoryIdEnumv2.SEANCE_CINE]: 'Séance de cinéma',
-  [SubcategoryIdEnumv2.LIVRE_PAPIER]: 'Livre papier',
-} as CategoryHomeLabelMapping
 
 describe('headlineOfferData', () => {
   it('should transform headline offer data correctly', () => {
