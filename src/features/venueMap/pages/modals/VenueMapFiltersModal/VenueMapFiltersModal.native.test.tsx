f64e72ddcdc58894f678b17729916dd085dfb58b
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-02-04 11:30:39 +0100

    (PC-34130) feat(venueMap): add search and reset buttons on filters modal (#7623)

--- src/features/venueMap/pages/modals/VenueMapFiltersModal/VenueMapFiltersModal.native.test.tsx
+++ src/features/venueMap/pages/modals/VenueMapFiltersModal/VenueMapFiltersModal.native.test.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { View } from 'react-native'
 
 import { VenueMapFiltersModal } from 'features/venueMap/pages/modals/VenueMapFiltersModal/VenueMapFiltersModal'
+import { venuesFilterActions } from 'features/venueMap/store/venuesFilterStore'
 import { render, screen, userEvent } from 'tests/utils'
 
 const mockHandleOnClose = jest.fn()
 const mockHandleGoBack = jest.fn()
+
+const mockResetFilters = jest.spyOn(venuesFilterActions, 'reset')
 
 const user = userEvent.setup()
 
@@ -45,5 +48,39 @@ describe('<VenueMapFiltersModal />', () => {
     await user.press(screen.getByLabelText('Revenir en arrière'))
 
     expect(mockHandleOnClose).toHaveBeenCalledTimes(1)
+  })
+
+  it('should close the modal when pressing search button', async () => {
+    render(
+      <VenueMapFiltersModal
+        titleId="uuid"
+        title="Filtres"
+        shouldDisplayBackButton
+        shouldDisplayCloseButton
+        handleOnClose={mockHandleOnClose}>
+        <View />
+      </VenueMapFiltersModal>
+    )
+
+    await user.press(screen.getByText('Rechercher'))
+
+    expect(mockHandleOnClose).toHaveBeenCalledTimes(1)
+  })
+
+  it('should reset the filters when pressing reset button', async () => {
+    render(
+      <VenueMapFiltersModal
+        titleId="uuid"
+        title="Filtres"
+        shouldDisplayBackButton
+        shouldDisplayCloseButton
+        handleOnClose={mockHandleOnClose}>
+        <View />
+      </VenueMapFiltersModal>
+    )
+
+    await user.press(screen.getByText('Réinitialiser'))
+
+    expect(mockResetFilters).toHaveBeenCalledTimes(1)
   })
 })
