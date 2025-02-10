5f47170c724bcaa31a83f0298782f7d0ce32d859
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-02-10 13:47:05 +0100

    (PC-32105) feat(offer): change venue button icon (#7655)

--- src/features/offer/components/OfferVenueButton/OfferVenueButton.tsx
+++ src/features/offer/components/OfferVenueButton/OfferVenueButton.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components/native'
 import { OfferVenueResponse } from 'api/gen'
 import { analytics } from 'libs/analytics/provider'
 import { HeroButtonList } from 'ui/components/buttons/HeroButtonList'
-import { LocationPointer } from 'ui/svg/icons/LocationPointer'
+import { LocationBuildingFilled } from 'ui/svg/icons/LocationBuildingFilled'
 import { TypoDS } from 'ui/theme'
 
 interface Props {
@@ -22,7 +22,7 @@ export function OfferVenueButton({ venue }: Readonly<Props>) {
       Subtitle={
         venue.city ? <SubtitleText testID="subtitle">{venue.city}</SubtitleText> : undefined
       }
-      Icon={<LocationPointer color={theme.colors.black} size={theme.icons.sizes.small} />}
+      Icon={<LocationBuildingFilled color={theme.colors.black} size={theme.icons.sizes.small} />}
       navigateTo={{ screen: 'Venue', params: { id: venue.id } }}
       onBeforeNavigate={() => analytics.logConsultVenue({ venueId: venue.id, from: 'offer' })}
       accessibilityLabel={`Accéder à la page du lieu ${venueName}`}
