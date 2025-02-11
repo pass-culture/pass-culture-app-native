8fb6c1167bbb9bd751d26f1fc44193e7be2ce9e1
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-02-11 19:22:46 +0100

    refactor: reformat file

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
-    { self
-    , nixpkgs
-    , flake-utils
+    {
+      self,
+      nixpkgs,
+      flake-utils,
     }:
     flake-utils.lib.eachDefaultSystem (system: {
       devShells.default =
