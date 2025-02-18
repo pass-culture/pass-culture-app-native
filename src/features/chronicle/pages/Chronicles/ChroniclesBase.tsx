dfbf954486ebccfd2a8690ed1e7654d03af93f85
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-02-18 11:01:33 +0100

    (PC-34542) feat(chronicle): use new header in chronicles list

--- src/features/chronicle/pages/Chronicles/ChroniclesBase.tsx
+++ src/features/chronicle/pages/Chronicles/ChroniclesBase.tsx
@@ -6,12 +6,13 @@ import { useTheme } from 'styled-components'
 import styled from 'styled-components/native'
 
 import { ChronicleCardList } from 'features/chronicle/components/ChronicleCardList/ChronicleCardList'
+import { ChronicleCardListHeader } from 'features/chronicle/components/ChronicleCardListHeader/ChronicleCardListHeader'
 import { ChroniclesHeader } from 'features/chronicle/components/ChroniclesHeader/ChroniclesHeader'
 import { ChroniclesWebMetaHeader } from 'features/chronicle/components/ChroniclesWebMetaHeader/ChroniclesWebMetaHeader'
 import { ChronicleCardData } from 'features/chronicle/type'
 import { UseNavigationType, UseRouteType } from 'features/navigation/RootNavigator/types'
 import { useOpacityTransition } from 'ui/animations/helpers/useOpacityTransition'
-import { TypoDS, getSpacing } from 'ui/theme'
+import { getSpacing } from 'ui/theme'
 
 type Props = PropsWithChildren<{
   offerId: number
@@ -71,7 +72,7 @@ export const ChroniclesBase: FunctionComponent<Props> = ({
           data={chronicleCardsData}
           horizontal={false}
           separatorSize={6}
-          headerComponent={<StyledTitle2>Tous les avis</StyledTitle2>}
+          headerComponent={<ChronicleCardListHeader />}
           ref={chroniclesListRef}
           onScroll={onScroll}
           contentContainerStyle={{
@@ -102,8 +103,4 @@ const FullFlexView = styled.View({
 const FullFlexRow = styled(FullFlexView)({
   flexDirection: 'row',
   columnGap: getSpacing(18),
-})
-
-const StyledTitle2 = styled(TypoDS.Title2)({
-  marginBottom: getSpacing(6),
 })
