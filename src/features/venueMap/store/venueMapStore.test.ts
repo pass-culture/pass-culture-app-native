42bc04795232908ae98a3df5f5843ec2da657e8f
Mathieu Meissonnier <173043140+mmeissonnier-pass@users.noreply.github.com>
2025-02-19 18:26:59 +0100

    refactor: venueMap store

---
+++ src/features/venueMap/store/venueMapStore.test.ts
@@ -0,0 +1,77 @@
+import { VenueTypeCodeKey } from 'api/gen'
+import { PlaylistType } from 'features/offer/enums'
+import { venuesFixture } from 'libs/algolia/fetchAlgolia/fetchVenues/fixtures/venuesFixture'
+import { act, renderHook } from 'tests/utils'
+
+import {
+  clearVenueMapStore,
+  setInitialRegion,
+  setOffersPlaylistType,
+  setRegion,
+  setSelectedVenue,
+  setVenueTypeCode,
+  setVenues,
+  useVenueMapStore,
+} from './venueMapStore'
+
+describe('VenueMapStore', () => {
+  it('should set default state at first', () => {
+    const { result } = renderHook(() => useVenueMapStore())
+
+    expect(result.current.venues).toStrictEqual([])
+    expect(result.current.initialRegion).toBeUndefined()
+    expect(result.current.region).toBeUndefined()
+    expect(result.current.offersPlaylistType).toBe(PlaylistType.TOP_OFFERS)
+    expect(result.current.selectedVenue).toBeUndefined()
+    expect(result.current.venueTypeCode).toBeUndefined()
+  })
+
+  it('should set venues', async () => {
+    const { result } = renderHook(() => useVenueMapStore())
+
+    act(() => {
+      setVenues([venuesFixture[0]])
+    })
+
+    expect(result.current.venues).toStrictEqual([venuesFixture[0]])
+
+    act(() => {
+      setVenues([venuesFixture[1]])
+    })
+
+    expect(result.current.venues).toStrictEqual([venuesFixture[1]])
+  })
+
+  it('should clear store to default state', () => {
+    const { result } = renderHook(() => useVenueMapStore())
+    act(() => {
+      setVenues(venuesFixture)
+    })
+
+    act(() => {
+      clearVenueMapStore()
+    })
+
+    expect(result.current.venues).toStrictEqual([])
+  })
+
+  it('should set values', () => {
+    const { result } = renderHook(() => useVenueMapStore())
+    const REGION = { latitude: 22.9, longitude: 33.2, longitudeDelta: 2, latitudeDelta: 3 }
+    act(() => {
+      setVenues(venuesFixture)
+      setVenueTypeCode(VenueTypeCodeKey.BOOKSTORE)
+      setOffersPlaylistType(PlaylistType.SEARCH_RESULTS)
+      setInitialRegion(REGION)
+      setRegion(REGION)
+      setSelectedVenue(venuesFixture[1])
+    })
+
+    expect(result.current.venues).toStrictEqual(venuesFixture)
+    expect(result.current.initialRegion).toStrictEqual(REGION)
+    expect(result.current.region).toStrictEqual(REGION)
+    expect(result.current.offersPlaylistType).toBe(PlaylistType.SEARCH_RESULTS)
+    expect(result.current.selectedVenue).toStrictEqual(venuesFixture[1])
+    expect(result.current.venueTypeCode).toStrictEqual(VenueTypeCodeKey.BOOKSTORE)
+  })
+})
