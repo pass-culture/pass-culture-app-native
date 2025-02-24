c8479a87c3c573d2b7ac3471b182cfcf603b3fe7
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-02-24 17:48:02 +0100

    userEvent instead of fireEvent and remove unnecessary waitFor

--- src/features/search/components/Buttons/FilterButton/FilterButton.native.test.tsx
+++ src/features/search/components/Buttons/FilterButton/FilterButton.native.test.tsx
@@ -1,65 +1,54 @@
 import React from 'react'
 
 import { navigate } from '__mocks__/@react-navigation/native'
-import { fireEvent, render, waitFor, screen } from 'tests/utils'
+import { userEvent, render, screen } from 'tests/utils'
 
 import { FilterButton } from './FilterButton'
 
+const user = userEvent.setup()
+jest.useFakeTimers()
+
 describe('FilterButton', () => {
-  it('should contains the number of active filters', async () => {
+  it('should contains the number of active filters', () => {
     render(<FilterButton activeFilters={2} navigateTo={{ screen: 'SearchFilter' }} />)
 
-    await waitFor(() => {
-      expect(screen.getByText('2')).toBeOnTheScreen()
-    })
+    expect(screen.getByText('2')).toBeOnTheScreen()
   })
 
-  it('should not display badge when there are no active filters', async () => {
+  it('should not display badge when there are no active filters', () => {
     render(<FilterButton activeFilters={0} navigateTo={{ screen: 'SearchFilter' }} />)
 
-    await waitFor(() => {
-      expect(screen.queryByTestId('searchFilterBadge')).not.toBeOnTheScreen()
-    })
+    expect(screen.queryByTestId('searchFilterBadge')).not.toBeOnTheScreen()
   })
 
   it('should navigate with undefined params when pressing filter button', async () => {
     render(<FilterButton activeFilters={1} navigateTo={{ screen: 'SearchFilter' }} />)
 
     const filterButton = screen.getByTestId('searchFilterBadge')
-    fireEvent.press(filterButton)
+    await user.press(filterButton)
 
-    await waitFor(() => {
-      expect(navigate).toHaveBeenCalledWith('SearchFilter', undefined)
-    })
+    expect(navigate).toHaveBeenCalledWith('SearchFilter', undefined)
   })
 
   describe('Accessibility', () => {
-    it('should have an accessible label with the number of active filters', async () => {
+    it('should have an accessible label with the number of active filters', () => {
       render(<FilterButton activeFilters={2} navigateTo={{ screen: 'SearchFilter' }} />)
 
-      await waitFor(() => {
-        expect(
-          screen.getByLabelText('Voir tous les filtres\u00a0: 2 filtres actifs')
-        ).toBeOnTheScreen()
-      })
+      expect(
+        screen.getByLabelText('Voir tous les filtres\u00a0: 2 filtres actifs')
+      ).toBeOnTheScreen()
     })
 
-    it('should have an accessible label with one active filter', async () => {
+    it('should have an accessible label with one active filter', () => {
       render(<FilterButton activeFilters={1} navigateTo={{ screen: 'SearchFilter' }} />)
 
-      await waitFor(() => {
-        expect(
-          screen.getByLabelText('Voir tous les filtres\u00a0: 1 filtre actif')
-        ).toBeOnTheScreen()
-      })
+      expect(screen.getByLabelText('Voir tous les filtres\u00a0: 1 filtre actif')).toBeOnTheScreen()
     })
 
-    it('should have an accessible label without active filter', async () => {
+    it('should have an accessible label without active filter', () => {
       render(<FilterButton activeFilters={0} navigateTo={{ screen: 'SearchFilter' }} />)
 
-      await waitFor(() => {
-        expect(screen.getByLabelText('Voir tous les filtres')).toBeOnTheScreen()
-      })
+      expect(screen.getByLabelText('Voir tous les filtres')).toBeOnTheScreen()
     })
   })
 })
