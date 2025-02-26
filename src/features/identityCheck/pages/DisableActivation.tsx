df1d9e999ce628a17395fc88d1cd89c745dbec24
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-02-26 17:46:24 +0100

    (PC-34837) feat(creditV3): use RemoteBanner infos in DisableActivatio… (#7757)
    * (PC-34837) feat(creditV3): use RemoteBanner infos in DisableActivation screen
    
    * (PC-34837) feat(creditV3): update FAQ_LINK_CREDIT_V3

--- src/features/identityCheck/pages/DisableActivation.tsx
+++ src/features/identityCheck/pages/DisableActivation.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 import { navigateToHomeConfig } from 'features/navigation/helpers/navigateToHome'
+import { validateRemoteBanner } from 'features/remoteBanner/components/remoteBannerSchema'
 import { analytics } from 'libs/analytics/provider'
 import { env } from 'libs/environment/env'
+import { useFeatureFlagOptions } from 'libs/firebase/firestore/featureFlags/useFeatureFlagOptions'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
 import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
 import { ExternalTouchableLink } from 'ui/components/touchableLink/ExternalTouchableLink'
@@ -13,10 +16,17 @@ import { Hourglass } from 'ui/svg/icons/Hourglass'
 import { LINE_BREAK } from 'ui/theme/constants'
 
 export const DisableActivation = () => {
+  const { options } = useFeatureFlagOptions(RemoteStoreFeatureFlags.SHOW_REMOTE_BANNER)
+  const validatedOptions = validateRemoteBanner(options)
+  const title =
+    validatedOptions?.title ??
+    `Tu as 17 ou 18 ans\u00a0?${LINE_BREAK}Reviens lundi pour débloquer ton crédit\u00a0!`
+  const url = validatedOptions?.redirectionUrl ?? env.FAQ_LINK_CREDIT_V3
+
   return (
     <GenericInfoPage
       icon={Hourglass}
-      title={`Tu as 17 ou 18 ans\u00a0?${LINE_BREAK}Reviens lundi pour débloquer ton crédit\u00a0!`}
+      title={title}
       buttons={[
         <InternalTouchableLink
           key={2}
@@ -27,7 +37,7 @@ export const DisableActivation = () => {
         <ExternalTouchableLink
           key={2}
           as={ButtonTertiaryWhite}
-          externalNav={{ url: env.FAQ_LINK_CREDIT_V3 }}
+          externalNav={{ url }}
           wording="Plus d’infos dans notre FAQ"
           onBeforeNavigate={() => analytics.logHasClickedTutorialFAQ()}
           icon={ExternalSiteFilled}
--- src/features/identityCheck/pages/DisableActivation.web.test.tsx
+++ src/features/identityCheck/pages/DisableActivation.web.test.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import { DisableActivation } from 'features/identityCheck/pages/DisableActivation'
+import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/__tests__/setFeatureFlags'
 import { checkAccessibilityFor, render } from 'tests/utils/web'
 
 jest.mock('libs/firebase/analytics/analytics')
 jest.mock('libs/firebase/remoteConfig/remoteConfig.services')
 
 describe('<DisableActivation/>', () => {
+  beforeEach(() => setFeatureFlags())
+
   describe('Accessibility', () => {
     it('should not have basic accessibility issues', async () => {
       const { container } = render(<DisableActivation />)
