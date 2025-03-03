743b820c8e0935b5fa81eeabd62690033ec69b2d
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-03-03 17:37:35 +0100

    (PC-33493) feat(fixture): add venueSearchParams

---
+++ src/fixtures/venueSearchParams.ts
@@ -0,0 +1,31 @@
+import { SearchState } from 'features/search/types'
+import mockVenueResponse from 'fixtures/venueResponse'
+import { LocationMode } from 'libs/location/types'
+
+export default {
+  beginningDatetime: undefined,
+  endingDatetime: undefined,
+  hitsPerPage: 50,
+  locationFilter: {
+    locationType: LocationMode.EVERYWHERE,
+  },
+  venue: {
+    label: mockVenueResponse.publicName || '',
+    info: mockVenueResponse.city || '',
+    _geoloc: {
+      lat: mockVenueResponse.latitude,
+      lng: mockVenueResponse.longitude,
+    },
+    venueId: mockVenueResponse.id,
+  },
+  offerCategories: [],
+  offerSubcategories: [],
+  offerIsDuo: false,
+  offerIsFree: false,
+  isDigital: false,
+  priceRange: [0, 300],
+  tags: [],
+  date: null,
+  timeRange: null,
+  query: '',
+} satisfies SearchState
