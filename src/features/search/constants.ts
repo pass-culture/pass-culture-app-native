883d0ed12646f1911df42de6d8bc513135edc6b3
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-03-04 14:40:59 +0100

    (PC-34926) feat(search): add default venue types associated to search groups

--- src/features/search/constants.ts
+++ src/features/search/constants.ts
@@ -1,3 +1,5 @@
+import { SearchGroupNameEnumv2, VenueTypeCodeKey } from 'api/gen'
+
 export const HISTORY_KEY = 'search_history'
 export const MAX_HISTORY_RESULTS = 20
 export const MIN_HISTORY_RESULTS = 2
@@ -6,3 +8,25 @@ export const SCROLL_TO_TOP_BUTTON_LINEAR_GRADIENT_START = { x: 0, y: 0 }
 export const SCROLL_TO_TOP_BUTTON_LINEAR_GRADIENT_END = { x: 0, y: 1 }
 export const DEFAULT_RADIUS = 50
 export const ALL_CATEGORIES_LABEL = 'Toutes les catégories'
+
+export const VENUE_TYPES_BY_SEARCH_GROUP: Partial<
+  Record<SearchGroupNameEnumv2, VenueTypeCodeKey[]>
+> = {
+  [SearchGroupNameEnumv2.LIVRES]: [
+    VenueTypeCodeKey.BOOKSTORE,
+    VenueTypeCodeKey.DISTRIBUTION_STORE,
+    VenueTypeCodeKey.LIBRARY,
+  ],
+  [SearchGroupNameEnumv2.CINEMA]: [VenueTypeCodeKey.MOVIE],
+  [SearchGroupNameEnumv2.FILMS_DOCUMENTAIRES_SERIES]: [VenueTypeCodeKey.DISTRIBUTION_STORE],
+  [SearchGroupNameEnumv2.MUSIQUE]: [
+    VenueTypeCodeKey.RECORD_STORE,
+    VenueTypeCodeKey.DISTRIBUTION_STORE,
+    VenueTypeCodeKey.CONCERT_HALL,
+    VenueTypeCodeKey.FESTIVAL,
+  ],
+  [SearchGroupNameEnumv2.CONCERTS_FESTIVALS]: [
+    VenueTypeCodeKey.FESTIVAL,
+    VenueTypeCodeKey.CONCERT_HALL,
+  ],
+}
