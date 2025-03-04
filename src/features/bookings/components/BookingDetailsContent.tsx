b3961586d8a339049902432e08f03606c1b175e3
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-03-04 11:22:59 +0100

    (PC-34303) feat(BookingPage): organizer section

--- src/features/bookings/components/BookingDetailsContent.tsx
+++ src/features/bookings/components/BookingDetailsContent.tsx
@@ -3,14 +3,17 @@ import { ScrollView } from 'react-native'
 import styled from 'styled-components/native'
 
 import { BookingOfferResponseAddress, BookingReponse, BookingVenueResponse } from 'api/gen'
+import { BookingPrecisions } from 'features/bookings/components/BookingPrecision'
 import { TicketCutout } from 'features/bookings/components/TicketCutout'
 import { getBookingLabels } from 'features/bookings/helpers'
 import { BookingProperties } from 'features/bookings/types'
 import { VenueBlockAddress, VenueBlockVenue } from 'features/offer/components/OfferVenueBlock/type'
 import { VenueBlockWithItinerary } from 'features/offer/components/OfferVenueBlock/VenueBlockWithItinerary'
+import { analytics } from 'libs/analytics/provider'
 import { formatFullAddress } from 'shared/address/addressFormatter'
 import { ErrorBanner } from 'ui/components/banners/ErrorBanner'
 import { InfoBanner } from 'ui/components/banners/InfoBanner'
+import { Separator } from 'ui/components/Separator'
 import { IdCard } from 'ui/svg/icons/IdCard'
 import { getSpacing, TypoDS } from 'ui/theme'
 
@@ -33,6 +36,10 @@ export const BookingDetailsContent = ({
     !!offerFullAddress && (properties.isEvent || (properties.isPhysical && !properties.isDigital))
 
   const { hourLabel, dayLabel } = getBookingLabels(booking, properties)
+
+  const onEmailPress = () => {
+    analytics.logClickEmailOrganizer()
+  }
 
   return (
     <ScrollView>
@@ -62,6 +69,17 @@ export const BookingDetailsContent = ({
       <ErrorBannerContainer>
         <ErrorBanner message="Tu n’as pas le droit de céder ou de revendre ton billet." />
       </ErrorBannerContainer>
+      {booking.stock.offer.bookingContact || offer.withdrawalDetails ? (
+        <React.Fragment>
+          <StyledSeparator height={getSpacing(8)} />
+          <BookingPrecisions
+            bookingContactEmail={booking.stock.offer.bookingContact}
+            withdrawalDetails={offer.withdrawalDetails}
+            onEmailPress={onEmailPress}
+          />
+        </React.Fragment>
+      ) : null}
+      <StyledSeparator height={getSpacing(8)} />
     </ScrollView>
   )
 }
@@ -79,4 +97,8 @@ function getVenueBlockAddress(
 const ErrorBannerContainer = styled.View({
   marginHorizontal: getSpacing(6),
   marginTop: getSpacing(8),
+})
+const StyledSeparator = styled(Separator.Horizontal)({
+  marginVertical: getSpacing(8),
+  height: getSpacing(2),
 })
