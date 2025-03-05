bde0daa185a1d5c091522cef5951f9f7a91f7e45
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-03-05 17:15:30 +0100

    (PC-34965) feat(location): remove wipAppV2LocationWidget FF

--- src/libs/firebase/firestore/types.ts
+++ src/libs/firebase/firestore/types.ts
@@ -58,7 +58,6 @@ export enum RemoteStoreFeatureFlags {
   TARGET_XP_CINE_FROM_OFFER = 'targetXpCineFromOffer',
   WIP_APP_V2_BUSINESS_BLOCK = 'wipAppV2BusinessBlock',
   WIP_APP_V2_CATEGORY_BLOCK = 'wipAppV2CategoryBlock',
-  WIP_APP_V2_LOCATION_WIDGET = 'wipAppV2LocationWidget',
   WIP_APP_V2_MULTI_VIDEO_MODULE = 'wipAppV2MultiVideoModule',
   WIP_APP_V2_SEARCH_LANDING_HEADER = 'wipAppV2SearchLandingHeader',
   WIP_APP_V2_THEMATIC_HOME_HEADER = 'wipAppV2ThematicHomeHeader',
