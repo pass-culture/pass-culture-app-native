8ab4931d45c7306e717eb5106986b4c5a878fa61
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-03-06 14:58:28 +0100

    (PC-34660) feat(creditV3): remove credit V1

--- src/features/profile/helpers/computeCredit.native.test.ts
+++ src/features/profile/helpers/computeCredit.native.test.ts
@@ -1,10 +1,10 @@
-import { domains_credit_v1 } from 'features/profile/fixtures/domainsCredit'
+import { domains_credit_v3 } from 'features/profile/fixtures/domainsCredit'
 
 import { computeCredit } from './computeCredit'
 
 describe('computeCredit', () => {
   it('should compute credit', () => {
-    expect(computeCredit(domains_credit_v1)).toEqual(40000)
+    expect(computeCredit(domains_credit_v3)).toEqual(75_00)
   })
 
   it('should compute credit equal to zero when no domainsCredit', () => {
---
+++ src/shared/credits/defaultDigitalCredit.ts
@@ -0,0 +1,4 @@
+export const defaultDigitalCredit = {
+  v2: 100_00,
+  v3: 50_00,
+}
