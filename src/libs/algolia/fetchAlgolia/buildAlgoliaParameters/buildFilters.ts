9ebe6ba3a6de74f0bb697bbc958cc1b5341f2e14
Inès Mouandjo Lobé <158567429+imouandjolobe-pass@users.noreply.github.com>
2025-03-14 00:38:23 +0100

    feat: filter future offers returned by algolia

--- src/libs/algolia/fetchAlgolia/buildAlgoliaParameters/buildFilters.ts
+++ src/libs/algolia/fetchAlgolia/buildAlgoliaParameters/buildFilters.ts
@@ -1,9 +1,9 @@
 export const buildFilters = ({ excludedObjectIds }: { excludedObjectIds?: string[] }) => {
-  let filters = ''
-  if (excludedObjectIds && excludedObjectIds.length > 0) {
-    filters = excludedObjectIds
-      .map((objectId, index) => `${index > 0 ? 'AND ' : ''}NOT objectID:${objectId}`)
-      .join(' ')
-  }
-  return filters.length > 0 ? { filters } : {}
+  const noFutureOffersFilter = 'NOT _tags:"is_future"'
+  const excludedObjectIdsFilter =
+    excludedObjectIds && excludedObjectIds.length > 0
+      ? excludedObjectIds.map((objectId) => `AND NOT objectID:${objectId}`).join(' ')
+      : ''
+
+  return { filters: `${noFutureOffersFilter} ${excludedObjectIdsFilter}` }
 }
