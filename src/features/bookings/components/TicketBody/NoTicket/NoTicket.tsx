2e00e1624ec5da025d417bf7210563696f10255f
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-03-14 09:58:23 +0100

    (PC-34307) feat(BookingPage): noTicket xp

--- src/features/bookings/components/TicketBody/NoTicket/NoTicket.tsx
+++ src/features/bookings/components/TicketBody/NoTicket/NoTicket.tsx
@@ -2,21 +2,21 @@ import React, { FunctionComponent } from 'react'
 import styled from 'styled-components/native'
 
 import { BicolorCircledCheck as InitialBicolorCircledCheck } from 'ui/svg/icons/BicolorCircledCheck'
-import { getSpacing, TypoDS } from 'ui/theme'
+import { TypoDS, getSpacing } from 'ui/theme'
 
 export const NoTicket: FunctionComponent = () => (
   <TicketContainer testID="withdrawal-info-no-ticket">
     <IconContainer>
       <BicolorCircledCheck />
     </IconContainer>
-    <StyledBody>
-      Tu n’as pas besoin de billet&nbsp;! Rends toi directement sur place le jour de l’évènement.
-    </StyledBody>
+    <StyledBody>Tu n’as pas besoin de billet pour profiter de cette offre&nbsp;!</StyledBody>
   </TicketContainer>
 )
 
 const TicketContainer = styled.View({
   width: '100%',
+  gap: getSpacing(4),
+  flexDirection: 'column',
 })
 
 const StyledBody = styled(TypoDS.Body)({
@@ -27,12 +27,10 @@ const StyledBody = styled(TypoDS.Body)({
 const IconContainer = styled.View({
   alignItems: 'center',
   width: '100%',
-  marginTop: -getSpacing(4),
-  marginBottom: getSpacing(3),
 })
 
 const BicolorCircledCheck = styled(InitialBicolorCircledCheck).attrs(({ theme }) => ({
-  size: theme.illustrations.sizes.medium,
-  color: theme.colors.primary,
-  color2: theme.colors.secondary,
+  size: theme.illustrations.sizes.small,
+  color: theme.colors.black,
+  color2: theme.colors.black,
 }))``
