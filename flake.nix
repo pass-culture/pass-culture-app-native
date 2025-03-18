a44744590f6688bffce9ac4c3cdcc32178134ed4
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-03-18 13:40:19 +0100

    update some tools

--- flake.nix
+++ flake.nix
@@ -16,16 +16,16 @@
           pkgs = nixpkgs.legacyPackages.${system};
           pkgs-old = nixpkgs-old.legacyPackages.${system};
         in
-        pkgs-old.mkShellNoCC {
+        pkgs.mkShellNoCC {
           packages = [
             pkgs-old.devbox
-            pkgs-old.jdk17 # needed by Android
-            pkgs-old.jq # needed by some scripts run in the pipeline
-            pkgs-old.python3 # needed by scripts/add_tracker.py
+            pkgs.jdk17 # needed by Android
+            pkgs.jq # needed by some scripts run in the pipeline
+            pkgs.python3 # needed by scripts/add_tracker.py
             pkgs-old.maestro # needed to run end to end test locally
-            pkgs-old.act # needed to debug pipeline locally
-            pkgs-old.gh # needed to debug pipeline locally
-            pkgs-old.podman # needed to debug pipeline locally
+            pkgs.act # needed to debug pipeline locally
+            pkgs.gh # needed to debug pipeline locally
+            pkgs.podman # needed to debug pipeline locally
           ];
         };
     });
