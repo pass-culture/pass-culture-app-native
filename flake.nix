e05852da1a4183738678dc21dc9a352c1e4b238c
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-03-18 12:44:37 +0100

    freeze current Nix version
    pin dependencies to old nixpkgs that is known to work

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=5775c2583f1801df7b790bf7f7d710a19bac66f4";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
