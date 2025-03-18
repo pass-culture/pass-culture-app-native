d2f7af560c8faec0e5e5d184a7c4e13a50c0d8bf
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-03-18 12:45:56 +0100

    rename to old

--- flake.nix
+++ flake.nix
@@ -1,28 +1,28 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=5775c2583f1801df7b790bf7f7d710a19bac66f4";
+  inputs.nixpkgs-old.url = "github:nixos/nixpkgs?rev=5775c2583f1801df7b790bf7f7d710a19bac66f4";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
     {
       self,
-      nixpkgs,
+      nixpkgs-old,
       flake-utils,
     }:
     flake-utils.lib.eachDefaultSystem (system: {
       devShells.default =
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs-old = nixpkgs-old.legacyPackages.${system};
         in
-        pkgs.mkShellNoCC {
+        pkgs-old.mkShellNoCC {
           packages = [
-            pkgs.devbox
-            pkgs.jdk17 # needed by Android
-            pkgs.jq # needed by some scripts run in the pipeline
-            pkgs.python3 # needed by scripts/add_tracker.py
-            pkgs.maestro # needed to run end to end test locally
-            pkgs.act # needed to debug pipeline locally
-            pkgs.gh # needed to debug pipeline locally
-            pkgs.podman # needed to debug pipeline locally
+            pkgs-old.devbox
+            pkgs-old.jdk17 # needed by Android
+            pkgs-old.jq # needed by some scripts run in the pipeline
+            pkgs-old.python3 # needed by scripts/add_tracker.py
+            pkgs-old.maestro # needed to run end to end test locally
+            pkgs-old.act # needed to debug pipeline locally
+            pkgs-old.gh # needed to debug pipeline locally
+            pkgs-old.podman # needed to debug pipeline locally
           ];
         };
     });
