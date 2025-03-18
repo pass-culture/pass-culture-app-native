3cad32a2dae9f7b668202056e09f35d2e81e817b
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-03-18 14:25:25 +0100

    remove old nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,10 @@
 {
-  inputs.nixpkgs-old.url = "github:nixos/nixpkgs?rev=5775c2583f1801df7b790bf7f7d710a19bac66f4";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
     {
       self,
-      nixpkgs-old,
       nixpkgs,
       flake-utils,
     }:
@@ -14,7 +12,6 @@
       devShells.default =
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          pkgs-old = nixpkgs-old.legacyPackages.${system};
         in
         pkgs.mkShellNoCC {
           packages = [
