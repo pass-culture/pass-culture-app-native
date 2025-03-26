5114a0894c3f25c044b0ac84a0b330c363d9546d
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-03-26 10:58:47 +0100

    (PC-35242) refactor(artist): remove useless code

--- src/features/venue/helpers/isArtistPageCompatible/isArtistPageCompatible.ts
+++
@@ -1,6 +0,0 @@
-import { SubcategoryIdEnum } from 'api/gen'
-import { ARTIST_PAGE_SUBCATEGORIES } from 'features/artist/constants'
-
-export function isArtistPageCompatible(subcategoryId: SubcategoryIdEnum) {
-  return ARTIST_PAGE_SUBCATEGORIES.includes(subcategoryId)
-}
