b3a578e7e121a31c7bdff8970172e8bede53ab4d
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-03-27 13:19:30 +0100

    (PC-35273) feat(ui): Simplify header.web

--- src/features/navigation/RootNavigator/Header/Header.web.tsx
+++ src/features/navigation/RootNavigator/Header/Header.web.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useEffect, memo } from 'react'
-import { Animated } from 'react-native'
+import React, { memo } from 'react'
 import webStyled from 'styled-components'
 import styled, { useTheme } from 'styled-components/native'
 
+import { useAuthContext } from 'features/auth/context/AuthContext'
 import { useTabBarItemBadges } from 'features/navigation/helpers/useTabBarItemBadges'
 import { homeNavConfig } from 'features/navigation/TabBar/helpers'
 import { useMediaQuery } from 'libs/react-responsive/useMediaQuery'
@@ -14,76 +14,21 @@ import { QuickAccess } from 'ui/web/link/QuickAccess'
 
 import { Nav } from './Nav'
 
-const MINIMUM_BRAND_SIZE = 140
-
 export const Header = memo(function Header({ mainId }: { mainId: string }) {
   const theme = useTheme()
   const routeBadgeMap = useTabBarItemBadges()
 
-  const fadeAnim = useRef({
-    opacity: new Animated.Value(0),
-  }).current
-  const fadeAnimSmall = useRef({
-    opacity: new Animated.Value(0),
-  }).current
-  const margin = getSpacing(6)
-
-  // we add the left and right margin to trigger the media query early than the display none and get the smooth effect
-  const isDesktopOffset = useMediaQuery({ minWidth: theme.breakpoints.lg + margin * 2 })
-
-  // we hide brand when we can't display them at minimum brand size
-  const isBrandVisible = useMediaQuery({
-    minWidth: theme.breakpoints.lg + margin * 2 + MINIMUM_BRAND_SIZE * 2,
-  })
-
-  function fadeInDesktop() {
-    Animated.timing(fadeAnim.opacity, {
-      toValue: 1,
-      useNativeDriver: false,
-      duration: 700,
-    }).start()
-  }
-
-  function fadeOutDesktop() {
-    Animated.timing(fadeAnim.opacity, {
-      toValue: 0,
-      useNativeDriver: false,
-      duration: 700,
-    }).start()
-  }
-
-  function fadeInTablette() {
-    Animated.timing(fadeAnimSmall.opacity, {
-      toValue: 1,
-      useNativeDriver: false,
-      duration: 700,
-    }).start()
-  }
-
-  function fadeOutTablette() {
-    Animated.timing(fadeAnimSmall.opacity, {
-      toValue: 0,
-      useNativeDriver: false,
-      duration: 700,
-    }).start()
-  }
-
-  useEffect(() => {
-    if (isBrandVisible) {
-      fadeInDesktop()
-      fadeOutTablette()
-      return
-    }
-    fadeOutDesktop()
-    fadeInTablette()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isBrandVisible])
+  const { user } = useAuthContext()
+  const marginHorizontal = user ? theme.contentPage.marginHorizontal * 2 : 0
+  const breakpoint = user ? theme.breakpoints.lg : theme.breakpoints.md
+  const minWidth = breakpoint + marginHorizontal
+  const isDesktopOffset = useMediaQuery({ minWidth })
 
   return (
     <HeaderContainer>
       <QuickAccess href={`#${mainId}`} title="Aller au contenu principal" />
-      <LeftContainer margin={margin} isVisible={!!isDesktopOffset} style={fadeAnim}>
-        {isBrandVisible ? (
+      {isDesktopOffset ? (
+        <LeftContainer>
           <LogoContainer
             navigateTo={{
               screen: homeNavConfig[0],
@@ -91,53 +36,23 @@ export const Header = memo(function Header({ mainId }: { mainId: string }) {
               fromRef: true,
               withPush: true,
             }}>
-            <LogoPassCulture
-              color={theme.uniqueColors.brand}
-              height={getSpacing(10)}
-              width="100%"
-            />
+            <LogoPassCulture color={theme.uniqueColors.brand} />
           </LogoContainer>
-        ) : null}
-        <FlexContainer />
-      </LeftContainer>
-      <CenterContainer isDesktop={theme.isDesktopViewport}>
-        <LeftContainer
-          margin={margin}
-          isVisible
-          style={fadeAnimSmall}
-          accessibilityHidden={!!isBrandVisible}>
-          <LogoPassCulture color={theme.uniqueColors.brand} height={getSpacing(6.5)} width={80} />
         </LeftContainer>
+      ) : null}
+      <CenterContainer>
         <Nav
           maxWidth={theme.appContentWidth}
           height={theme.navTopHeight}
-          noShadow={theme.isDesktopViewport}
           routeBadgeMap={routeBadgeMap}
+          noShadow
         />
-        <RightContainer
-          margin={margin}
-          isVisible
-          style={fadeAnimSmall}
-          accessibilityHidden={!!isBrandVisible}>
-          <FlexContainer alignItems="flex-end">
-            <LogoFrenchRepublicContainer height={getSpacing(11)} width={getSpacing(15.5)}>
-              <LogoFrenchRepublic />
-            </LogoFrenchRepublicContainer>
-          </FlexContainer>
-        </RightContainer>
       </CenterContainer>
-      <RightContainer
-        margin={margin}
-        isVisible={!!isDesktopOffset}
-        style={fadeAnim}
-        accessibilityHidden={!isBrandVisible}>
-        <FlexContainer />
-        <FlexContainer>
-          <LogoFrenchRepublicContainer>
-            <LogoFrenchRepublic />
-          </LogoFrenchRepublicContainer>
-        </FlexContainer>
-      </RightContainer>
+      {isDesktopOffset ? (
+        <RightContainer>
+          <LogoFrenchRepublic />
+        </RightContainer>
+      ) : null}
     </HeaderContainer>
   )
 })
@@ -145,17 +60,13 @@ export const Header = memo(function Header({ mainId }: { mainId: string }) {
 const HeaderContainer = webStyled.header.attrs({ role: 'banner' })(({ theme }) => {
   return {
     display: 'flex',
-    flexGrow: 1,
-    maxHeight: theme.navTopHeight,
-    justifyContent: 'space-between',
     flexDirection: 'row',
-    backgroundColor: theme.colors.white,
+    backgroundColor: theme.designSystem.color.background.default,
+    height: theme.navTopHeight,
+    justifyContent: 'space-between',
     zIndex: theme.zIndex.header,
     ...getShadow({
-      shadowOffset: {
-        width: 0,
-        height: getSpacing(1),
-      },
+      shadowOffset: { width: 0, height: getSpacing(1) },
       shadowRadius: getSpacing(3),
       shadowColor: theme.colors.greyDark,
       shadowOpacity: 0.1,
@@ -163,47 +74,31 @@ const HeaderContainer = webStyled.header.attrs({ role: 'banner' })(({ theme }) =
   }
 })
 
-const LeftContainer = styled(Animated.View)<{ margin: number; isVisible: boolean }>(
-  ({ margin, isVisible }) => ({
-    flex: 1,
-    flexDirection: 'row',
-    marginLeft: margin,
-    alignItems: 'center',
-    ...(isVisible ? {} : { display: 'none' }),
-  })
-)
-
-const CenterContainer = styled.View<{ isDesktop?: boolean }>(({ theme, isDesktop }) => ({
-  ...(isDesktop ? { minWidth: theme.breakpoints.lg } : {}),
-  width: '100%',
-  alignItems: 'center',
+const CenterContainer = styled.View({
+  flex: 1,
   flexDirection: 'row',
-  justifyContent: isDesktop ? 'center' : 'space-between',
-  flex: 1,
-}))
-
-const RightContainer = styled(Animated.View)<{ margin: number; isVisible: boolean }>(
-  ({ margin, isVisible }) => ({
-    flex: 1,
-    flexDirection: 'row',
-    marginRight: margin,
-    alignItems: 'center',
-    ...(isVisible ? {} : { display: 'none' }),
-  })
-)
-
-const FlexContainer = styled.View<{ alignItems?: string }>(({ alignItems }) => ({
-  flex: 1,
-  alignItems,
-}))
-
-const LogoContainer = styled(InternalTouchableLink)({
-  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
 })
 
-const LogoFrenchRepublicContainer = styled.View<{ width?: number; height?: number }>(
-  ({ height, width }) => ({
-    height: height ?? getSpacing(15),
-    width,
-  })
-)
+const SideContainer = styled.View({
+  width: getSpacing(30),
+  flexDirection: 'row',
+  alignItems: 'center',
+})
+
+const LogoContainer = styled(InternalTouchableLink)({
+  justifyContent: 'center',
+  height: '100%',
+  width: '100%',
+})
+
+const LeftContainer = styled(SideContainer)(({ theme }) => ({
+  marginLeft: theme.contentPage.marginHorizontal,
+  justifyContent: 'flex-start',
+}))
+
+const RightContainer = styled(SideContainer)(({ theme }) => ({
+  marginRight: theme.contentPage.marginHorizontal,
+  justifyContent: 'flex-end',
+}))
