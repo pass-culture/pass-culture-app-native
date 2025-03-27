d20487cf8778d6df9fa5167f54d0dc6df6bb105a
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-03-27 18:14:24 +0100

    (PC-35279) feat(ui): use GenericInfoPageWhite in IdentityCheckUnavailable

--- src/features/identityCheck/pages/identification/IdentityCheckUnavailable.tsx
+++ src/features/identityCheck/pages/identification/IdentityCheckUnavailable.tsx
@@ -6,15 +6,12 @@ import { DMSModal } from 'features/identityCheck/components/modals/DMSModal'
 import { navigateToHomeConfig } from 'features/navigation/helpers/navigateToHome'
 import { UseRouteType } from 'features/navigation/RootNavigator/types'
 import { analytics } from 'libs/analytics/provider'
-import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
-import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
 import { useModal } from 'ui/components/modals/useModal'
-import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
-import { GenericInfoPageDeprecated } from 'ui/pages/GenericInfoPageDeprecated'
+import { ViewGap } from 'ui/components/ViewGap/ViewGap'
+import { GenericInfoPageWhite } from 'ui/pages/GenericInfoPageWhite'
 import { ExternalSite } from 'ui/svg/icons/ExternalSite'
 import { HappyFace } from 'ui/svg/icons/HappyFace'
-import { PlainArrowPrevious } from 'ui/svg/icons/PlainArrowPrevious'
-import { Spacer, Typo } from 'ui/theme'
+import { Typo } from 'ui/theme'
 
 export function IdentityCheckUnavailable() {
   const { params } = useRoute<UseRouteType<'IdentityCheckUnavailable'>>()
@@ -25,46 +22,39 @@ export function IdentityCheckUnavailable() {
     analytics.logStartDMSTransmission()
     showModal()
   }
+
   return (
-    <GenericInfoPageDeprecated
+    <GenericInfoPageWhite
+      illustration={HappyFace}
       title="Victime de notre succès&nbsp;!"
-      icon={HappyFace}
-      buttons={[
-        params?.withDMS ? (
-          <ButtonPrimaryWhite
-            key={1}
-            wording="Transmettre un dossier"
-            onPress={showDMSModal}
-            icon={ExternalSite}
-          />
-        ) : null,
-        <InternalTouchableLink
-          key={2}
-          as={ButtonTertiaryWhite}
-          wording="Retourner à l’accueil"
-          navigateTo={navigateToHomeConfig}
-          icon={PlainArrowPrevious}
-        />,
-      ].filter(Boolean)}>
-      <StyledBody>
-        Vous êtes actuellement très nombreux à vouloir créer un compte, notre service rencontre
-        quelques difficultés.
-      </StyledBody>
-      <Spacer.Column numberOfSpaces={5} />
-      <StyledBody>Nous reviendrons vers toi dès que le service sera rétabli.</StyledBody>
-      <Spacer.Column numberOfSpaces={6} />
-      {params?.withDMS ? (
+      buttonPrimary={
+        params?.withDMS
+          ? { wording: 'Transmettre un dossier', onPress: showDMSModal, icon: ExternalSite }
+          : { wording: 'Retourner à l’accueil', navigateTo: navigateToHomeConfig }
+      }
+      buttonSecondary={
+        params?.withDMS
+          ? { wording: 'Retourner à l’accueil', navigateTo: navigateToHomeConfig }
+          : undefined
+      }>
+      <ViewGap gap={5}>
         <StyledBody>
-          Tu peux nous transmettre ton dossier via la plateforme Démarches Simplifiées. Nous
-          reviendrons vers toi d’ici quelques jours.
+          Vous êtes actuellement très nombreux à vouloir créer un compte, notre service rencontre
+          quelques difficultés.
         </StyledBody>
-      ) : null}
+        <StyledBody>Nous reviendrons vers toi dès que le service sera rétabli.</StyledBody>
+        {params?.withDMS ? (
+          <StyledBody>
+            Tu peux nous transmettre ton dossier via la plateforme Démarches Simplifiées. Nous
+            reviendrons vers toi d’ici quelques jours.
+          </StyledBody>
+        ) : null}
+      </ViewGap>
       <DMSModal visible={visible} hideModal={hideModal} />
-    </GenericInfoPageDeprecated>
+    </GenericInfoPageWhite>
   )
 }
 
-const StyledBody = styled(Typo.Body)(({ theme }) => ({
-  color: theme.colors.white,
+const StyledBody = styled(Typo.Body)({
   textAlign: 'center',
-}))
+})
