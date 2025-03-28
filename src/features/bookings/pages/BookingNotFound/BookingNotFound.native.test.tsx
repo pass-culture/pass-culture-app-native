dea63a6666e6494837889fe0931c4d3a18c192f3
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-03-28 12:47:35 +0100

    (PC-35279) feat(ui): use GenericInfoPageWhite in BookingNotFound

--- src/features/bookings/pages/BookingNotFound/BookingNotFound.native.test.tsx
+++ src/features/bookings/pages/BookingNotFound/BookingNotFound.native.test.tsx
@@ -6,6 +6,8 @@ import { render, screen, userEvent } from 'tests/utils'
 
 import { BookingNotFound } from './BookingNotFound'
 
+jest.mock('libs/firebase/analytics/analytics')
+
 jest.mock('react-native/Libraries/Animated/createAnimatedComponent', () => {
   return function createAnimatedComponent(Component: unknown) {
     return Component
