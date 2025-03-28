cb7d2cf81dce186e907ed22ef1df73d4a88fbd1b
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-03-28 15:49:22 +0100

    (PC-34310) feat(BookingDetails): onSiteWithdrawal xp

--- src/features/bookings/components/TicketBody/ticketBodyMessages.ts
+++ src/features/bookings/components/TicketBody/ticketBodyMessages.ts
@@ -1,17 +1,4 @@
-import { WithdrawalTypeEnum } from 'api/gen'
 import { formatSecondsToString } from 'features/bookings/helpers'
-
-export const getStartMessage = (withdrawalType: WithdrawalTypeEnum): string | null => {
-  switch (withdrawalType) {
-    case WithdrawalTypeEnum.on_site:
-      return 'Présente le code ci-dessus sur place '
-    case WithdrawalTypeEnum.by_email:
-      return 'Tu vas recevoir ton billet par e-mail '
-    case WithdrawalTypeEnum.no_ticket:
-    case WithdrawalTypeEnum.in_app:
-      return null
-  }
-}
 
 export const getDelayMessage = (withdrawalDelay: number): string | null => {
   if (withdrawalDelay > 0) return `${formatSecondsToString(withdrawalDelay)} `
