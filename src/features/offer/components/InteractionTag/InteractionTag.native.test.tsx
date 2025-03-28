33366a279792384bc0f50c57e5e71d9cd98829ea
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2025-03-28 12:01:59 +0100

    (PC-35361) refactor(InteractionTag): separate the logic

--- src/features/offer/components/InteractionTag/InteractionTag.native.test.tsx
+++ src/features/offer/components/InteractionTag/InteractionTag.native.test.tsx
@@ -2,77 +2,20 @@ import React from 'react'
 
 import { InteractionTag } from 'features/offer/components/InteractionTag/InteractionTag'
 import { render, screen } from 'tests/utils'
+import { theme } from 'theme'
 
 describe('<InteractionTag />', () => {
-  it('should not return interaction tag when no counter defined', () => {
-    render(<InteractionTag />)
+  it('should display the correct label', () => {
+    render(<InteractionTag label="Recommended" backgroundColor={theme.colors.goldLight100} />)
 
-    expect(screen.toJSON()).toBeNull()
+    expect(screen.getByText('Recommended')).toBeOnTheScreen()
   })
 
-  it('should not return interaction tag when all counter defined at 0', () => {
-    render(<InteractionTag chroniclesCount={0} headlineCount={0} likesCount={0} />)
+  it('should apply the correct background color', () => {
+    render(<InteractionTag label="Book Club" backgroundColor={theme.colors.skyBlueLight} />)
 
-    expect(screen.toJSON()).toBeNull()
-  })
-
-  it('should return headline tag when headline counter defined and likes counter < 20', () => {
-    render(<InteractionTag headlineCount={10} likesCount={19} />)
-
-    expect(screen.getByText('Reco par les lieux')).toBeOnTheScreen()
-  })
-
-  it('should return likes tag when headline counter defined and likes counter = 20', () => {
-    render(<InteractionTag headlineCount={10} likesCount={20} />)
-
-    expect(screen.getByText('20 j’aime')).toBeOnTheScreen()
-  })
-
-  it('should return likes tag when headline counter defined and likes counter > 20', () => {
-    render(<InteractionTag headlineCount={10} likesCount={21} />)
-
-    expect(screen.getByText('21 j’aime')).toBeOnTheScreen()
-  })
-
-  it('should return book club tag when headline counter, chronicles counter defined and likes counter < 50', () => {
-    render(<InteractionTag headlineCount={10} chroniclesCount={10} likesCount={49} />)
-
-    expect(screen.getByText('Reco du Book Club')).toBeOnTheScreen()
-  })
-
-  it('should return likes tag when headline counter, chronicles counter defined and likes counter = 50', () => {
-    render(<InteractionTag headlineCount={10} chroniclesCount={10} likesCount={50} />)
-
-    expect(screen.getByText('50 j’aime')).toBeOnTheScreen()
-  })
-
-  it('should return likes tag when headline counter, chronicles counter defined and likes counter > 50', () => {
-    render(<InteractionTag headlineCount={10} chroniclesCount={10} likesCount={51} />)
-
-    expect(screen.getByText('51 j’aime')).toBeOnTheScreen()
-  })
-
-  it('should return likes tag when likes counter defined and counter > 0', () => {
-    render(<InteractionTag likesCount={18} />)
-
-    expect(screen.getByText('18 j’aime')).toBeOnTheScreen()
-  })
-
-  it('should return book club tag when book club counter defined and counter > 0', () => {
-    render(<InteractionTag chroniclesCount={5} />)
-
-    expect(screen.getByText('Reco du Book Club')).toBeOnTheScreen()
-  })
-
-  it('should return book club tag when book club and headline counters defined and counters > 0', () => {
-    render(<InteractionTag chroniclesCount={5} headlineCount={5} />)
-
-    expect(screen.getByText('Reco du Book Club')).toBeOnTheScreen()
-  })
-
-  it('should return headline tag when headline counter defined and counter > 0', () => {
-    render(<InteractionTag headlineCount={5} />)
-
-    expect(screen.getByText('Reco par les lieux')).toBeOnTheScreen()
+    expect(screen.getByTestId('interaction-tag')).toHaveStyle({
+      backgroundColor: theme.colors.skyBlueLight,
+    })
   })
 })
--- src/features/offer/components/InteractionTag/InteractionTag.tsx
+++ src/features/offer/components/InteractionTag/InteractionTag.tsx
@@ -1,84 +1,27 @@
 import React, { FunctionComponent } from 'react'
-import styled, { useTheme } from 'styled-components/native'
 
 import { Tag } from 'ui/components/Tag/Tag'
-import { BookClubCertification } from 'ui/svg/BookClubCertification'
-import { ThumbUpFilled } from 'ui/svg/icons/ThumbUpFilled'
-import { Star } from 'ui/svg/Star'
+import { AccessibleIcon, ColorsTypeLegacy } from 'ui/svg/icons/types'
 import { getSpacing } from 'ui/theme'
 
-type Props = {
-  likesCount?: number
-  chroniclesCount?: number
-  headlineCount?: number
+export type InteractionTagProps = {
+  label: string
+  backgroundColor?: ColorsTypeLegacy
+  Icon?: React.FunctionComponent<AccessibleIcon> | undefined
 }
 
-export const InteractionTag: FunctionComponent<Props> = ({
-  likesCount,
-  chroniclesCount,
-  headlineCount,
+export const InteractionTag: FunctionComponent<InteractionTagProps> = ({
+  label,
+  backgroundColor,
+  Icon,
 }) => {
-  const { colors } = useTheme()
-
-  const getTagConfig = (likesCount?: number, chroniclesCount?: number, headlineCount?: number) => {
-    const headlineTagConfig = {
-      label: 'Reco par les lieux',
-      backgroundColor: colors.goldLight100,
-      Icon: CustomStar,
-    }
-    const likesTagConfig = {
-      label: likesCount ? `${likesCount} j’aime` : '',
-      backgroundColor: colors.white,
-      Icon: CustomThumbUp,
-    }
-    const bookClubTagConfig = {
-      label: 'Reco du Book Club',
-      backgroundColor: colors.skyBlueLight,
-      Icon: CustomBookClub,
-    }
-
-    const hasHeadline = !!headlineCount
-    const hasChronicles = !!chroniclesCount
-    const hasLikes = !!likesCount
-
-    if (hasHeadline && !hasChronicles) {
-      return likesCount && likesCount >= 20 ? likesTagConfig : headlineTagConfig
-    }
-
-    if (hasHeadline && hasChronicles) {
-      return likesCount && likesCount >= 50 ? likesTagConfig : bookClubTagConfig
-    }
-
-    if (hasLikes) return likesTagConfig
-    if (hasChronicles) return bookClubTagConfig
-    if (hasHeadline) return headlineTagConfig
-
-    return null
-  }
-
-  const tagConfig = getTagConfig(likesCount, chroniclesCount, headlineCount)
-
-  if (!tagConfig) return null
-
   return (
     <Tag
-      label={tagConfig.label}
-      backgroundColor={tagConfig.backgroundColor}
-      Icon={tagConfig.Icon}
+      testID="interaction-tag"
+      label={label}
+      backgroundColor={backgroundColor}
+      Icon={Icon}
       paddingHorizontal={getSpacing(1)}
     />
   )
 }
-
-const CustomThumbUp = styled(ThumbUpFilled).attrs(({ theme }) => ({
-  size: theme.icons.sizes.extraSmall,
-  color: theme.colors.primary,
-}))``
-
-const CustomBookClub = styled(BookClubCertification).attrs(({ theme }) => ({
-  size: theme.icons.sizes.extraSmall,
-}))``
-
-const CustomStar = styled(Star).attrs(({ theme }) => ({
-  size: theme.icons.sizes.extraSmall,
-}))``
