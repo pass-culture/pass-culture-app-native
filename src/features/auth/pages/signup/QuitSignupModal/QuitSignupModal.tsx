606cacbc629cf45a1848ec4a043b14d22371671d
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-03-28 12:01:59 +0100

    (PC-35279) feat(ui): use GenericInfoPageWhite in QuitSignupModal

--- src/features/auth/pages/signup/QuitSignupModal/QuitSignupModal.tsx
+++ src/features/auth/pages/signup/QuitSignupModal/QuitSignupModal.tsx
@@ -1,17 +1,12 @@
 import React, { FunctionComponent, useCallback } from 'react'
-import styled from 'styled-components/native'
 
 import { SignupStep } from 'features/auth/enums'
 import { navigateToHome } from 'features/navigation/helpers/navigateToHome'
 import { analytics } from 'libs/analytics/provider'
-import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
-import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
 import { AppFullPageModal } from 'ui/components/modals/AppFullPageModal'
-import { Spacer } from 'ui/components/spacer/Spacer'
-import { GenericInfoPageDeprecated } from 'ui/pages/GenericInfoPageDeprecated'
+import { GenericInfoPageWhite } from 'ui/pages/GenericInfoPageWhite'
 import { BicolorError } from 'ui/svg/icons/BicolorError'
 import { Clear } from 'ui/svg/icons/Clear'
-import { Typo } from 'ui/theme'
 
 interface Props {
   visible: boolean
@@ -37,37 +32,22 @@ export const QuitSignupModal: FunctionComponent<Props> = ({
     resume()
   }, [resume])
 
-  const title = 'Veux-tu abandonner l’inscription\u00a0?'
-  const description = 'Les informations que tu as renseignées ne seront pas enregistrées.'
-
   return (
     <AppFullPageModal visible={visible} testIdSuffix={testIdSuffix} onRequestClose={continueSignup}>
-      <GenericInfoPageDeprecated
-        title={title}
-        icon={ErrorIllustration}
-        flex={false}
-        buttons={[
-          <ButtonPrimaryWhite key={1} wording="Continuer l’inscription" onPress={continueSignup} />,
-          <ButtonTertiaryWhite
-            key={2}
-            wording="Abandonner l’inscription"
-            onPress={quitSignup}
-            icon={Clear}
-          />,
-        ]}>
-        <StyledBody>{description}</StyledBody>
-        <Spacer.Column numberOfSpaces={8} />
-      </GenericInfoPageDeprecated>
+      <GenericInfoPageWhite
+        illustration={BicolorError}
+        title="Veux-tu abandonner l’inscription&nbsp;?"
+        subtitle="Les informations que tu as renseignées ne seront pas enregistrées."
+        buttonPrimary={{
+          wording: 'Continuer l’inscription',
+          onPress: continueSignup,
+        }}
+        buttonTertiary={{
+          wording: 'Abandonner l’inscription',
+          onPress: quitSignup,
+          icon: Clear,
+        }}
+      />
     </AppFullPageModal>
   )
 }
-
-const StyledBody = styled(Typo.Body)(({ theme }) => ({
-  color: theme.colors.white,
-  textAlign: 'center',
-}))
-
-const ErrorIllustration = styled(BicolorError).attrs(({ theme }) => ({
-  color: theme.colors.white,
-  color2: theme.colors.white,
-}))``
