45897ea9588d221488edf156a18923f2edf0fb17
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-03-28 17:27:48 +0100

    (PC-35279) feat(ui): use GenericInfoPageWhite in DeleteProfileSuccess

--- src/features/profile/pages/DeleteProfile/DeleteProfileSuccess.tsx
+++ src/features/profile/pages/DeleteProfile/DeleteProfileSuccess.tsx
@@ -3,12 +3,11 @@ import styled from 'styled-components/native'
 
 import { useLogoutRoutine } from 'features/auth/helpers/useLogoutRoutine'
 import { navigateToHomeConfig } from 'features/navigation/helpers/navigateToHome'
-import { ButtonPrimaryWhite } from 'ui/components/buttons/ButtonPrimaryWhite'
 import { Emoji } from 'ui/components/Emoji'
-import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
-import { GenericInfoPageDeprecated } from 'ui/pages/GenericInfoPageDeprecated'
+import { ViewGap } from 'ui/components/ViewGap/ViewGap'
+import { GenericInfoPageWhite } from 'ui/pages/GenericInfoPageWhite'
 import { ProfileDeletion } from 'ui/svg/icons/ProfileDeletion'
-import { Spacer, Typo } from 'ui/theme'
+import { Typo } from 'ui/theme'
 
 export function DeleteProfileSuccess() {
   const signOut = useLogoutRoutine()
@@ -18,31 +17,27 @@ export function DeleteProfileSuccess() {
   }, [signOut])
 
   return (
-    <GenericInfoPageDeprecated
+    <GenericInfoPageWhite
+      illustration={ProfileDeletion}
       title="Ton compte a été supprimé"
-      icon={ProfileDeletion}
-      buttons={[
-        <InternalTouchableLink
-          key={1}
-          as={ButtonPrimaryWhite}
-          wording="Retourner à l’accueil"
-          navigateTo={{ ...navigateToHomeConfig, params: { ...navigateToHomeConfig.params } }}
-        />,
-      ]}>
-      <StyledBody>
-        <Emoji.CryingFace withSpaceAfter />
-        On est super triste de te voir partir.
-      </StyledBody>
-      <Spacer.Column numberOfSpaces={4} />
-      <StyledBody>
-        Tu peux malgré tout continuer à découvrir toute l’actu culturelle en consultant le
-        catalogue&nbsp;!
-      </StyledBody>
-    </GenericInfoPageDeprecated>
+      buttonPrimary={{
+        wording: 'Retourner à l’accueil',
+        navigateTo: { ...navigateToHomeConfig, params: { ...navigateToHomeConfig.params } },
+      }}>
+      <ViewGap gap={4}>
+        <StyledBody>
+          <Emoji.CryingFace withSpaceAfter />
+          On est super triste de te voir partir.
+        </StyledBody>
+        <StyledBody>
+          Tu peux malgré tout continuer à découvrir toute l’actu culturelle en consultant le
+          catalogue&nbsp;!
+        </StyledBody>
+      </ViewGap>
+    </GenericInfoPageWhite>
   )
 }
 
-const StyledBody = styled(Typo.Body)(({ theme }) => ({
-  color: theme.colors.white,
+const StyledBody = styled(Typo.Body)({
   textAlign: 'center',
-}))
+})
