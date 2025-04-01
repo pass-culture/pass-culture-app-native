fa23acac994c0e9cda9ae4fb3b567c02affbcd4d
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-04-01 10:59:10 +0200

    (PC-35432) fix(firebase): add try/catch when fetching remote config + sentry log (#7937)
    * (PC-35432) fix(firebase): add try/catch when fetching remote config + sentry log
    
    * (PC-35432) test(perf): update
    
    * (PC-35432) test(firebase): add tests for useRemoteConfigQuery

--- src/libs/firebase/remoteConfig/queries/useRemoteConfigQuery.ts
+++ src/libs/firebase/remoteConfig/queries/useRemoteConfigQuery.ts
@@ -1,15 +1,25 @@
 import { useQuery } from 'react-query'
 
+import { eventMonitoring } from 'libs/monitoring/services'
 import { QueryKeys } from 'libs/queryKeys'
+import { getErrorMessage } from 'shared/getErrorMessage/getErrorMessage'
 
 import { DEFAULT_REMOTE_CONFIG } from '../remoteConfig.constants'
 import { remoteConfig } from '../remoteConfig.services'
 import { CustomRemoteConfig } from '../remoteConfig.types'
 
 const fetchRemoteConfig = async (): Promise<CustomRemoteConfig> => {
-  await remoteConfig.configure()
-  await remoteConfig.refresh()
-  return remoteConfig.getValues()
+  try {
+    await remoteConfig.configure()
+    await remoteConfig.refresh()
+    return remoteConfig.getValues()
+  } catch (error) {
+    const errorMessage = getErrorMessage(error)
+    eventMonitoring.captureException(`Error fetching remote config: ${errorMessage}`, {
+      extra: { error },
+    })
+    return DEFAULT_REMOTE_CONFIG
+  }
 }
 
 export function useRemoteConfigQuery() {
