4b57b2437bc96e652eb8aa4918fc895bf5d9e925
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-04-02 16:16:12 +0200

    (PC-35420) feat(ui): remove unused contactSupport button in LayoutExpiredLink

--- src/ui/components/LayoutExpiredLink.native.test.tsx
+++ src/ui/components/LayoutExpiredLink.native.test.tsx
@@ -7,7 +7,6 @@ import { LayoutExpiredLink } from 'ui/components/LayoutExpiredLink'
 const onResendEmail = jest.fn()
 const isFetching = false
 const url = 'https://passculture.zendesk.com/hc/fr/'
-const contactSupport = jest.fn()
 
 const renderResendEmailButton = () => (
   <ButtonPrimaryWhite wording="Renvoyer l’email" onPress={onResendEmail} disabled={isFetching} />
@@ -23,13 +22,7 @@ jest.mock('react-native/Libraries/Animated/createAnimatedComponent', () => {
 
 describe('<LayoutExpiredLink/>', () => {
   it('should render correctly', () => {
-    render(
-      <LayoutExpiredLink
-        renderCustomButton={renderResendEmailButton}
-        urlFAQ={url}
-        contactSupport={contactSupport}
-      />
-    )
+    render(<LayoutExpiredLink renderCustomButton={renderResendEmailButton} urlFAQ={url} />)
 
     expect(screen).toMatchSnapshot()
   })
--- src/ui/components/LayoutExpiredLink.tsx
+++ src/ui/components/LayoutExpiredLink.tsx
@@ -5,8 +5,8 @@ import { navigateToHomeConfig } from 'features/navigation/helpers/navigateToHome
 import { ButtonTertiaryWhite } from 'ui/components/buttons/ButtonTertiaryWhite'
 import { ExternalTouchableLink } from 'ui/components/touchableLink/ExternalTouchableLink'
 import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
+import { ViewGap } from 'ui/components/ViewGap/ViewGap'
 import { GenericInfoPageDeprecated } from 'ui/pages/GenericInfoPageDeprecated'
-import { EmailFilled } from 'ui/svg/icons/EmailFilled'
 import { ExternalSite } from 'ui/svg/icons/ExternalSite'
 import { PlainArrowPrevious } from 'ui/svg/icons/PlainArrowPrevious'
 import { SadFace } from 'ui/svg/icons/SadFace'
@@ -15,16 +15,13 @@ import { Spacer, Typo } from 'ui/theme'
 type Props = {
   renderCustomButton?: () => React.ReactNode
   urlFAQ?: string
-  contactSupport?: () => void
   customBodyText?: string
 }
 
-export function LayoutExpiredLink({
-  renderCustomButton,
-  urlFAQ,
-  contactSupport,
-  customBodyText,
-}: Props) {
+export function LayoutExpiredLink({ renderCustomButton, urlFAQ, customBodyText }: Props) {
+  const subtitle =
+    customBodyText ?? 'Clique sur «\u00a0Renvoyer l’e-mail\u00a0» pour recevoir un nouveau lien.'
+
   return (
     <GenericInfoPageDeprecated
       title="Oups&nbsp;!"
@@ -40,39 +37,26 @@ export function LayoutExpiredLink({
         />,
       ].filter(Boolean)}>
       <StyledBody>Le lien est expiré&nbsp;!</StyledBody>
-      <StyledBody>
-        {customBodyText ||
-          'Clique sur «\u00a0Renvoyer l’e-mail\u00a0» pour recevoir un nouveau lien.'}
-      </StyledBody>
+      <StyledBody>{subtitle}</StyledBody>
       <Spacer.Column numberOfSpaces={6} />
-
-      {!!urlFAQ || !!contactSupport ? (
-        <React.Fragment>
-          <StyledBody>Si tu as besoin d’aide n’hésite pas à&nbsp;:</StyledBody>
-          <Spacer.Column numberOfSpaces={2} />
-        </React.Fragment>
-      ) : null}
-
       {urlFAQ ? (
-        <ExternalTouchableLink
-          as={ButtonTertiaryWhite}
-          wording="Consulter l’article d’aide"
-          externalNav={{ url: urlFAQ }}
-          icon={ExternalSite}
-        />
-      ) : null}
-
-      {contactSupport ? (
-        <ButtonTertiaryWhite
-          wording="Contacter le support"
-          accessibilityLabel="Ouvrir le gestionnaire mail pour contacter le support"
-          onPress={contactSupport}
-          icon={EmailFilled}
-        />
+        <FAQContainer gap={2}>
+          <StyledBody>Si tu as besoin d’aide n’hésite pas à&nbsp;:</StyledBody>
+          <ExternalTouchableLink
+            as={ButtonTertiaryWhite}
+            wording="Consulter l’article d’aide"
+            externalNav={{ url: urlFAQ }}
+            icon={ExternalSite}
+          />
+        </FAQContainer>
       ) : null}
     </GenericInfoPageDeprecated>
   )
 }
+
+const FAQContainer = styled(ViewGap)({
+  alignItems: 'center',
+})
 
 const StyledBody = styled(Typo.Body)(({ theme }) => ({
   color: theme.colors.white,
