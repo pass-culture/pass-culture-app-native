39ab587daa2f6bb0554b3d371482bee36b338b70
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-04-03 17:13:25 +0200

    (PC-35429) feat(ui): add designToken in GenericErrorPage

--- src/features/home/pages/NoContentError.tsx
+++ src/features/home/pages/NoContentError.tsx
@@ -1,57 +1,30 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import styled from 'styled-components/native'
 
 import { UseNavigationType } from 'features/navigation/RootNavigator/types'
 import { getSearchStackConfig } from 'features/navigation/SearchStackNavigator/searchStackHelpers'
-import { Helmet } from 'libs/react-helmet/Helmet'
-import { ButtonSecondaryWhite } from 'ui/components/buttons/ButtonSecondaryWhite'
 import { GenericErrorPage } from 'ui/pages/GenericErrorPage'
 import { BicolorBrokenConnection } from 'ui/svg/BicolorBrokenConnection'
 import { MagnifyingGlass } from 'ui/svg/icons/MagnifyingGlass'
-import { Typo } from 'ui/theme'
 
 export const NoContentError = () => {
   const { navigate } = useNavigation<UseNavigationType>()
-
-  const navigateToSearchTab = () => {
-    navigate(...getSearchStackConfig('SearchLanding'))
-  }
+  const navigateToSearchTab = () => navigate(...getSearchStackConfig('SearchLanding'))
 
   const helmetTitle =
     'Page erreur\u00a0: Erreur pendant le chargement de nos recommandations | pass Culture'
 
   return (
-    <React.Fragment>
-      <Helmet>
-        <title>{helmetTitle}</title>
-      </Helmet>
-      <GenericErrorPage
-        title="Oups&nbsp;!"
-        icon={BrokenConnection}
-        buttons={[
-          <ButtonSecondaryWhite
-            key={1}
-            wording="Rechercher une offre"
-            icon={MagnifyingGlass}
-            onPress={navigateToSearchTab}
-            buttonHeight="tall"
-          />,
-        ]}>
-        <StyledBody>
-          Une erreur s’est produite pendant le chargement de nos recommandations.
-        </StyledBody>
-      </GenericErrorPage>
-    </React.Fragment>
+    <GenericErrorPage
+      helmetTitle={helmetTitle}
+      title="Oups&nbsp;!"
+      subtitle="Une erreur s’est produite pendant le chargement de nos recommandations."
+      illustration={BicolorBrokenConnection}
+      button={{
+        wording: 'Rechercher une offre',
+        icon: MagnifyingGlass,
+        onPress: navigateToSearchTab,
+      }}
+    />
   )
 }
-
-const StyledBody = styled(Typo.Body)(({ theme }) => ({
-  color: theme.colors.white,
-  textAlign: 'center',
-}))
-
-const BrokenConnection = styled(BicolorBrokenConnection).attrs(({ theme }) => ({
-  color: theme.colors.white,
-  color2: theme.colors.white,
-}))``
