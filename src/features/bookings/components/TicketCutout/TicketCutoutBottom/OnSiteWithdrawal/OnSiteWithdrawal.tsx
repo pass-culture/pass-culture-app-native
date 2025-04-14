66e48bfa30d4dbf0c72e4232cbb689243fb03c98
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-04-14 11:34:39 +0200

    (PC-35511):remove useless ReactFragment

--- src/features/bookings/components/TicketCutout/TicketCutoutBottom/OnSiteWithdrawal/OnSiteWithdrawal.tsx
+++ src/features/bookings/components/TicketCutout/TicketCutoutBottom/OnSiteWithdrawal/OnSiteWithdrawal.tsx
@@ -10,7 +10,7 @@ export const OnSiteWithdrawal = ({ token }: { token: string }) => {
   return (
     <React.Fragment>
       <TicketVisual>
-        <TicketCodeTitleContainer>
+        <TicketCodeTitleContainer testID="withdrawal-on-site">
           <TicketCodeTitle>{token}</TicketCodeTitle>
         </TicketCodeTitleContainer>
       </TicketVisual>
