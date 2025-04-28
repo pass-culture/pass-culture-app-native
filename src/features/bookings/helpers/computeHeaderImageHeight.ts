fa622f4b3ddffb4f7d3ea76e455a6c6c7bfbf4f9
Tanguy <47600889+tprache-pass@users.noreply.github.com>
2025-04-28 15:42:56 +0200

    feat: add booking header

---
+++ src/features/bookings/helpers/computeHeaderImageHeight.ts
@@ -0,0 +1,22 @@
+import { TICKET_SEPARATION_HEIGHT } from 'features/bookings/components/TicketCutout/TicketCutout'
+import {
+  blurImageHeight,
+  offerImageContainerMarginTop,
+} from 'features/offer/helpers/useOfferImageContainerDimensions'
+import { getSpacing } from 'ui/theme'
+
+interface ComputeParams {
+  topBlockHeight: number
+  windowHeight: number
+}
+
+export function computeHeaderImageHeight({ topBlockHeight, windowHeight }: ComputeParams) {
+  const extra = getSpacing(offerImageContainerMarginTop)
+
+  const ticketSeparationOffset = TICKET_SEPARATION_HEIGHT / 2
+
+  const headerImageHeight = topBlockHeight + extra + ticketSeparationOffset
+  const scrollContentHeight = windowHeight - blurImageHeight
+
+  return { headerImageHeight, scrollContentHeight }
+}
