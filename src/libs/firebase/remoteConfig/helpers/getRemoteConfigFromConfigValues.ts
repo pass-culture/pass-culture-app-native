c8bf18205167695d49e76df4c814abc603b34235
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2025-04-29 11:19:01 +0200

    (PC-35832) refactor(RemoteConfig): remove unused code (#8061)

--- src/libs/firebase/remoteConfig/helpers/getRemoteConfigFromConfigValues.ts
+++ src/libs/firebase/remoteConfig/helpers/getRemoteConfigFromConfigValues.ts
@@ -30,7 +30,6 @@ export const getRemoteConfigFromConfigValues = (
   reactionFakeDoorCategories: JSON.parse(
     getConfigValue(parameters.reactionFakeDoorCategories).asString()
   ),
-  maxLikesValue: getConfigValue(parameters.maxLikesValue).asNumber(),
   minLikesValue: getConfigValue(parameters.minLikesValue).asNumber(),
   sameAuthorPlaylist: getConfigValue(parameters.sameAuthorPlaylist).asString(),
   shouldDisplayReassuranceMention: getConfigValue(
