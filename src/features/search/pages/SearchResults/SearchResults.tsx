619bb8a3f2429d1052c6cf13386653c33da3088c
Mathieu Meissonnier <173043140+mmeissonnier-pass@users.noreply.github.com>
2025-05-02 13:07:30 +0200

    tests: fix tests

--- src/features/search/pages/SearchResults/SearchResults.tsx
+++ src/features/search/pages/SearchResults/SearchResults.tsx
@@ -1,5 +1,5 @@
 import { useNavigationState } from '@react-navigation/native'
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { Configure, InstantSearch } from 'react-instantsearch-core'
 import { StatusBar } from 'react-native'
 import AlgoliaSearchInsights from 'search-insights'
@@ -11,11 +11,14 @@ import { SearchResultsContent } from 'features/search/components/SearchResultsCo
 import { SearchSuggestions } from 'features/search/components/SearchSuggestions/SearchSuggestions'
 import { useSearch } from 'features/search/context/SearchWrapper'
 import { getSearchClient } from 'features/search/helpers/getSearchClient'
+import { usePrevious } from 'features/search/helpers/usePrevious'
 import { useSearchHistory } from 'features/search/helpers/useSearchHistory/useSearchHistory'
 import { useSync } from 'features/search/helpers/useSync/useSync'
+import { analytics } from 'libs/analytics/provider'
 import { env } from 'libs/environment/env'
 import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
+import { useLocation } from 'libs/location'
 import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
 import { OfflinePage } from 'libs/network/OfflinePage'
 import { Form } from 'ui/components/Form'
@@ -31,7 +34,7 @@ export const SearchResults = () => {
   useSync(currentRoute === 'SearchResults')
 
   const netInfo = useNetInfoContext()
-  const { isFocusOnSuggestions } = useSearch()
+  const { isFocusOnSuggestions, searchState } = useSearch()
   const { setQueryHistory, queryHistory, addToHistory, removeFromHistory, filteredHistory } =
     useSearchHistory()
 
@@ -40,9 +43,47 @@ export const SearchResults = () => {
     [setQueryHistory]
   )
 
+  const { geolocPosition } = useLocation()
+  const previousGeolocPosition = usePrevious(geolocPosition)
+
   const isArtistInSearchActive = useFeatureFlag(RemoteStoreFeatureFlags.WIP_ARTIST_PAGE_IN_SEARCH)
 
-  const { hits, ...rest } = useSearchResults()
+  const {
+    hits,
+    hasNextPage,
+    fetchNextPage,
+    data,
+    refetch,
+    nbHits,
+    isLoading,
+    isFetching,
+    isFetchingNextPage,
+    userData,
+    venuesUserData,
+    facets,
+    offerVenues,
+  } = useSearchResults()
+
+  const shouldRefetchResults = Boolean(
+    (geolocPosition && !previousGeolocPosition) || (!geolocPosition && previousGeolocPosition)
+  )
+
+  useEffect(() => {
+    if (shouldRefetchResults) {
+      refetch()
+    }
+  }, [refetch, shouldRefetchResults])
+
+  const handleEndReached = useCallback(() => {
+    if (data && hasNextPage) {
+      const [lastPage] = data.pages.slice(-1)
+
+      if (lastPage && lastPage.offers.page > 0) {
+        analytics.logSearchScrollToPage(lastPage.offers.page, searchState.searchId)
+      }
+      fetchNextPage()
+    }
+  }, [hasNextPage, data, fetchNextPage, searchState.searchId])
 
   const searchResultHits = isArtistInSearchActive ? hits : { ...hits, artists: [] }
 
@@ -73,7 +114,19 @@ export const SearchResults = () => {
               filteredHistory={filteredHistory}
             />
           ) : (
-            <SearchResultsContent hits={searchResultHits} {...rest} />
+            <SearchResultsContent
+              hits={searchResultHits}
+              onEndReached={handleEndReached}
+              onSearchResultsRefresh={refetch}
+              nbHits={nbHits}
+              isLoading={isLoading}
+              isFetching={isFetching}
+              isFetchingNextPage={isFetchingNextPage}
+              userData={userData}
+              venuesUserData={venuesUserData}
+              facets={facets}
+              offerVenues={offerVenues}
+            />
           )}
         </InstantSearch>
       </Form.Flex>
