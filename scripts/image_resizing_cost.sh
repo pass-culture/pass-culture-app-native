89a5c33b4abe99d08efc9ad7d1334b5b934f76a1
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-05-05 19:23:08 +0200

    WIP

--- scripts/image_resizing_cost.sh
+++ scripts/image_resizing_cost.sh
@@ -4,10 +4,9 @@ set -o errexit -o nounset -o pipefail
 GCP_LOGGIN_QUERY='
 resource.labels.project_id="passculture-metier-prod"
 resource.labels.target_proxy_name="image-resizing-https-proxy"
-timestamp>="2025-04-01T00:00:00"
+timestamp>="2025-04-30T00:00:00"
 timestamp<"2025-05-01T00:00:00"
 '
-# timestamp<"2025-04-01T00:00:30"
 JQ_SUM_RESPONSE_SIZE='[.[].httpRequest.responseSize | select(. != null) | tonumber] | add'
 
 FILE_OUTPUT="node_modules/image_resizing_request.json" # in node_modules just because it is ignored and we delete it often
