6f185e15577291a140fd023696180cd1cb1aab62
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-05-06 17:59:19 +0200

    WIP

--- scripts/image_resizing_cost.sh
+++ scripts/image_resizing_cost.sh
@@ -4,12 +4,12 @@ set -o errexit -o nounset -o pipefail
 GCP_LOGGIN_QUERY='
 resource.labels.project_id="passculture-metier-prod"
 resource.labels.target_proxy_name="image-resizing-https-proxy"
-timestamp>="2025-04-30T12:00:00"
-timestamp<"2025-05-01T00:00:00"
+timestamp>="2025-04-30T17:00:00"
+timestamp<"2025-04-30T18:00:00"
 '
 JQ_SUM_RESPONSE_SIZE='[.[].httpRequest.responseSize | select(. != null) | tonumber] | add'
 
-FILE_OUTPUT="node_modules/image_resizing_request.json" # in node_modules just because it is ignored and we delete it often
+FILE_OUTPUT="node_modules/image_resizing_request_small.json" # in node_modules just because it is ignored and we delete it often
 
 gcloud logging --project="passculture-metier-prod" read "$GCP_LOGGIN_QUERY" --format=json >"$FILE_OUTPUT"
 
