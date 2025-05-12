f1184b5083515da37f33e87d7c077d6d07c9a8bd
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-05-12 10:34:29 +0200

    (PC-36016) refactor(offer): remove distance tag in playlist card

--- src/features/offer/components/OfferTile/PlaylistCardOffer.tsx
+++ src/features/offer/components/OfferTile/PlaylistCardOffer.tsx
@@ -4,9 +4,7 @@ import styled from 'styled-components/native'
 import { CategoryIdEnum } from 'api/gen'
 import { ImageTile } from 'ui/components/ImageTile'
 import { NewOfferCaption } from 'ui/components/NewOfferCaption'
-import { Tag } from 'ui/components/Tag/Tag'
 import { ViewGap } from 'ui/components/ViewGap/ViewGap'
-import { getSpacing } from 'ui/theme'
 
 type Props = {
   categoryId?: CategoryIdEnum | null
@@ -38,7 +36,6 @@ export const PlaylistCardOffer: FC<Props> = ({
       <ImageTile categoryId={categoryId} uri={thumbnailUrl} width={width} height={height} />
       {interactionTag ?? null}
       <NewOfferCaption name={name} date={date} price={price} categoryLabel={categoryLabel} />
-      {distance ? <DistanceTag label={`à ${distance}`} /> : null}
     </Container>
   )
 }
@@ -47,14 +44,4 @@ const Container = styled(ViewGap).attrs({
   gap: 2,
 })<{ maxWidth: number }>(({ maxWidth }) => ({
   maxWidth,
-}))
-
-const DistanceTag = styled(Tag).attrs(() => ({
-  testID: 'DistanceId',
-}))(({ theme }) => ({
-  position: 'absolute',
-  top: getSpacing(2),
-  left: getSpacing(2),
-  zIndex: 1,
-  backgroundColor: theme.colors.white,
 }))
