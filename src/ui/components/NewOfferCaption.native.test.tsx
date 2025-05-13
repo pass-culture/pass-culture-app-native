aa39e3e130a51cac639b908ae9cdea3efe1bb413
nmajorfrances-pass <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-05-13 14:50:06 +0200

    (PC-36092) fix(NewOfferCaption): remove unecessary and not unique id (#8120)

--- src/ui/components/NewOfferCaption.native.test.tsx
+++ src/ui/components/NewOfferCaption.native.test.tsx
@@ -14,12 +14,27 @@ const props = {
 describe('NewOfferCaption component', () => {
   afterAll(() => jest.resetAllMocks())
 
-  it('should display offer infos', () => {
+  it('should display offer price', () => {
     render(<NewOfferCaption {...props} />)
 
-    expect(screen.getByTestId('priceIsDuo').children[0]).toBe('Dès 5€')
+    expect(screen.getByText('Dès 5€')).toBeOnTheScreen()
+  })
+
+  it('should display offer date', () => {
+    render(<NewOfferCaption {...props} />)
+
     expect(screen.getByText('Dès le 2 mars 2020')).toBeOnTheScreen()
+  })
+
+  it('should display offer title', () => {
+    render(<NewOfferCaption {...props} />)
+
     expect(screen.getByText('Mensch ! Où sont les Hommes ?')).toBeOnTheScreen()
+  })
+
+  it('should display offer category', () => {
+    render(<NewOfferCaption {...props} />)
+
     expect(screen.getByText('Cinéma')).toBeOnTheScreen()
   })
 })
