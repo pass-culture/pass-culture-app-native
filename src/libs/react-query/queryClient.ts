25d03eaf31efb53cd50d71a973c8561f419d18b1
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-05-14 15:57:23 +0200

    Revert "use same default options of react query in test"
    This reverts commit aa8bb7fcd0a03542e01d9a2ad28df9140b96a5ac.

--- src/libs/react-query/queryClient.ts
+++ src/libs/react-query/queryClient.ts
@@ -1,16 +1,13 @@
-import { DefaultOptions, QueryCache, QueryClient } from 'react-query'
+import { QueryCache, QueryClient } from 'react-query'
 
 const queryCache = new QueryCache()
-
-export const defaultOptions: DefaultOptions<unknown> = {
-  queries: {
-    retry: 0,
-    useErrorBoundary: true,
-  },
-}
-
 // Read https://tkdodo.eu/blog/placeholder-and-initial-data-in-react-query
 export const queryClient = new QueryClient({
   queryCache,
-  defaultOptions,
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      useErrorBoundary: true,
+    },
+  },
 })
