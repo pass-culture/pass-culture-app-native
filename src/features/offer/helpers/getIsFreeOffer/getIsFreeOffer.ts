2433bb025fdce6c4e02f3a3f647dc4f48d5c4023
Xavier Le Cunff <163895831+xlecunff-pass@users.noreply.github.com>
2025-05-16 07:48:14 +0200

    (PC35936) fix(isFreeOffer): change behavior of isFreeOffer

--- src/features/offer/helpers/getIsFreeOffer/getIsFreeOffer.ts
+++ src/features/offer/helpers/getIsFreeOffer/getIsFreeOffer.ts
@@ -1,7 +1,4 @@
 import { OfferResponseV2 } from 'api/gen'
 
-export function getIsFreeOffer(offer: OfferResponseV2): boolean {
-  const price = offer.stocks[0]?.price
-  if (price === 0) return true
-  return false
-}
+export const getIsFreeOffer = (offer: OfferResponseV2) =>
+  !!offer.stocks.length && !offer.stocks.some((stock) => stock.price > 0)
