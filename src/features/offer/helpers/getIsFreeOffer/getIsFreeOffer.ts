5c7e5e8ffc86684dabe8c4863ebf550db3cebeab
Xavier Le Cunff <163895831+xlecunff-pass@users.noreply.github.com>
2025-05-16 10:26:00 +0200

    fix: take reviews

--- src/features/offer/helpers/getIsFreeOffer/getIsFreeOffer.ts
+++ src/features/offer/helpers/getIsFreeOffer/getIsFreeOffer.ts
@@ -1,4 +1,5 @@
 import { OfferResponseV2 } from 'api/gen'
 
-export const getIsFreeOffer = (offer: OfferResponseV2) =>
+// Une offre est considérée gratuite si elle contient des stocks et qu'ils sont tous gratuits
+export const getIsFreeOffer = (offer: OfferResponseV2): boolean =>
   !!offer.stocks.length && !offer.stocks.some((stock) => stock.price > 0)
