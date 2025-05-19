ad48bba0f91087a3fd483165a4380cc2a3d55b77
nmajorfrances-pass <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-05-19 11:54:47 +0200

    (PC-36149) feat(BookingDetails): add user's email on message not sent yet and adapt messages if duo (#8137)
    * (PC-36149) remove unnecessary component
    
    * (PC-36149) error banner is plural when booking duo
    
    * (PC-36149): add plural to OnSite withdrawal
    
    * (PC-36149) add email and plural to EmailWillBeSend
    
    * (PC-36149) refactor : check user in higher component

---
+++ src/features/bookings/components/TicketCutout/TicketCutoutBottom/EmailWithdrawal/EmailWillBeSend.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+import { UserProfileResponse } from 'api/gen'
+import { TicketVisual } from 'features/bookings/components/TicketCutout/TicketCutoutBottom/TicketVisual'
+import { getDelayMessage } from 'features/bookings/helpers/getDelayMessage'
+import { EmailSent } from 'ui/svg/icons/EmailSent'
+import { Typo, getSpacing } from 'ui/theme'
+
+export const EmailWillBeSend = ({
+  isDuo,
+  userEmail,
+  withdrawalDelay,
+}: {
+  withdrawalDelay?: number | null
+  isDuo: boolean
+  userEmail: UserProfileResponse['email']
+}) => {
+  const startText = `Tu vas recevoir ${isDuo ? 'tes billets' : 'ton billet'} par e-mail, à l’adresse ${userEmail}, `
+  const endText = `avant le début de l’évènement.`
+
+  return (
+    <React.Fragment>
+      <TicketVisual>
+        <EmailSent />
+      </TicketVisual>
+      <WithDrawalContainer testID="withdrawal-email-will-be-send">
+        {startText}
+        {withdrawalDelay ? (
+          <TicketWithdrawalDelay>{getDelayMessage(withdrawalDelay)}</TicketWithdrawalDelay>
+        ) : (
+          ' '
+        )}
+        {endText}
+      </WithDrawalContainer>
+    </React.Fragment>
+  )
+}
+
+const WithDrawalContainer = styled(Typo.Body)({
+  textAlign: 'center',
+  maxWidth: '100%',
+  paddingBottom: getSpacing(6),
+})
+
+const TicketWithdrawalDelay = styled(Typo.Body)(({ theme }) => ({
+  fontFamily: theme.fontFamily.bold,
+}))
