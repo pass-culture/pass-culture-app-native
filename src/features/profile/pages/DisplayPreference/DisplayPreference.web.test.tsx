4e60d74b0fa207b4957bf64a47f8f982ac8df7e3
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-05-20 10:07:39 +0200

    (PC-35459) feat(ui): add switch theme in DisplayPreference

--- src/features/profile/pages/DisplayPreference/DisplayPreference.web.test.tsx
+++ src/features/profile/pages/DisplayPreference/DisplayPreference.web.test.tsx
@@ -1,30 +1,38 @@
 import React from 'react'
 
+import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/tests/setFeatureFlags'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { checkAccessibilityFor, render, screen } from 'tests/utils/web'
 
 import { DisplayPreference } from './DisplayPreference'
 
 jest.mock('ui/theme/customFocusOutline/customFocusOutline')
+jest.mock('libs/firebase/remoteConfig/remoteConfig.services')
 
 describe('DisplayPreference', () => {
+  beforeEach(() => setFeatureFlags([RemoteStoreFeatureFlags.WIP_ENABLE_DARK_MODE]))
+
   it('should not have basic accessibility issues', async () => {
-    const { container } = render(<DisplayPreference />)
+    const { container } = renderDisplayPreference()
     const results = await checkAccessibilityFor(container)
 
     expect(results).toHaveNoViolations()
   })
 
-  it('should display correct subtitle', () => {
-    render(<DisplayPreference />)
+  it('should display correct subtitle', async () => {
+    renderDisplayPreference()
 
     const webSubtitle = screen.getByText('L’affichage en mode paysage n’est pas disponible en web')
 
     expect(webSubtitle).toBeInTheDocument()
   })
 
-  it('should disable phone rotation toggle', () => {
-    render(<DisplayPreference />)
+  it('should disable phone rotation toggle', async () => {
+    renderDisplayPreference()
 
     expect(screen.getByTestId('Interrupteur Rotation')).toHaveAttribute('aria-disabled', 'true')
   })
 })
+
+const renderDisplayPreference = () => render(reactQueryProviderHOC(<DisplayPreference />))
--- src/libs/styled/useColorScheme.tsx
+++ src/libs/styled/useColorScheme.tsx
@@ -1,36 +1,53 @@
-import { ColorSchemeName, Appearance } from 'react-native'
+import { useColorScheme as useSystemColorScheme } from 'react-native'
 
+import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 import { createStore } from 'libs/store/createStore'
 
-export type ColorScheme = 'dark' | 'light' | 'system'
-
-type State = { colorScheme: ColorScheme }
-
-const DEFAULT_COLOR_SCHEME = 'light'
-
-const defaultState: State = {
-  colorScheme: DEFAULT_COLOR_SCHEME,
+export enum ColorSchemeEnum {
+  LIGHT = 'light',
+  DARK = 'dark',
+  SYSTEM = 'system',
 }
+
+export type ColorScheme = ColorSchemeEnum.LIGHT | ColorSchemeEnum.DARK
+export type ColorSchemeFull = ColorSchemeEnum
+
+type State = { colorScheme: ColorSchemeFull }
+
+const DEFAULT_COLOR_SCHEME: ColorSchemeFull = ColorSchemeEnum.LIGHT
+const defaultState: State = { colorScheme: DEFAULT_COLOR_SCHEME }
 
 const colorSchemeStore = createStore({
   name: 'colorScheme',
   defaultState,
   actions: (set) => ({
-    init: () => set({ colorScheme: Appearance.getColorScheme() || DEFAULT_COLOR_SCHEME }),
-    setColorScheme: ({ colorScheme }: { colorScheme: ColorSchemeName }) => {
-      set({ colorScheme: colorScheme || DEFAULT_COLOR_SCHEME })
+    setColorScheme: ({ colorScheme }: { colorScheme: ColorSchemeFull }) => {
+      set({ colorScheme })
     },
   }),
   selectors: {
     selectColorScheme:
       () =>
-      (state): ColorScheme =>
+      (state): ColorSchemeFull =>
         state.colorScheme,
   },
   options: { persist: true },
 })
 
 export const colorSchemeActions = colorSchemeStore.actions
-export const { useColorScheme } = colorSchemeStore.hooks
+export const useStoredColorScheme: () => ColorSchemeFull = colorSchemeStore.hooks.useColorScheme
 
-Appearance.addChangeListener(colorSchemeStore.actions.setColorScheme)
+export const useColorScheme = (): ColorScheme => {
+  const storedScheme = useStoredColorScheme()
+  const systemScheme = useSystemColorScheme()
+
+  const enableDarkMode = useFeatureFlag(RemoteStoreFeatureFlags.WIP_ENABLE_DARK_MODE)
+  if (!enableDarkMode) return ColorSchemeEnum.LIGHT
+
+  return storedScheme === ColorSchemeEnum.SYSTEM
+    ? systemScheme === 'dark'
+      ? ColorSchemeEnum.DARK
+      : ColorSchemeEnum.LIGHT
+    : storedScheme
+}
