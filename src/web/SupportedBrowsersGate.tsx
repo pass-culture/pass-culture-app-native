142819584811846f3ef27a7a2690578f8344b84e
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-05-22 11:53:37 +0200

    (PC-36244) feat(SupportedBrowserGate): add headless browsers to supported browsers for e2e tests'

--- src/web/SupportedBrowsersGate.tsx
+++ src/web/SupportedBrowsersGate.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react'
 // eslint-disable-next-line no-restricted-imports
-import * as DeviceDetect from 'react-device-detect'
+import { browserName, browserVersion } from 'react-device-detect'
 import { ScrollView, ViewStyle } from 'react-native'
 import styled from 'styled-components/native'
 
@@ -9,29 +9,30 @@ import { Li } from 'ui/components/Li'
 import { VerticalUl } from 'ui/components/Ul'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 
-type SupportedBrowsers = Array<{
-  browser: string
-  active: boolean
-  version: number
-}>
+// The following list was made manually using SauceLab. If it's been a while, feel free to make a new audit to update this list.
+export const supportedBrowsers = {
+  Chrome: 80,
+  Firefox: 80,
+  Edge: 80,
+  Safari: 14,
+} as const
+
+type SupportedBrowsers = typeof supportedBrowsers
+
+const isBrowserSupported = () =>
+  Object.entries(supportedBrowsers).some(
+    ([name, version]) => browserName.includes(name) && Number(browserVersion) >= version
+  )
 
 export const SupportedBrowsersGate: React.FC<PropsWithChildren> = ({ children }) => {
-  const browserVersion = Number(DeviceDetect.browserVersion)
-  const supportedBrowsers: SupportedBrowsers = [
-    { browser: 'Chrome', active: DeviceDetect.isChrome, version: 80 },
-    { browser: 'Firefox', active: DeviceDetect.isFirefox, version: 80 },
-    { browser: 'Edge sur Windows', active: DeviceDetect.isEdge, version: 80 },
-    { browser: 'Safari sur macOS', active: DeviceDetect.isSafari, version: 14 },
-  ]
+  const userbrowserVersion = Number(browserVersion)
 
-  const [shouldDisplayApp, setShouldDisplayApp] = React.useState(() =>
-    isBrowserSupported(browserVersion, supportedBrowsers)
-  )
+  const [shouldDisplayApp, setShouldDisplayApp] = React.useState(() => isBrowserSupported())
 
   if (!shouldDisplayApp) {
     return (
       <BrowserNotSupportedPage
-        browserVersion={browserVersion}
+        browserVersion={userbrowserVersion}
         supportedBrowsers={supportedBrowsers}
         onPress={() => setShouldDisplayApp(true)}
       />
@@ -40,21 +41,12 @@ export const SupportedBrowsersGate: React.FC<PropsWithChildren> = ({ children })
   return <React.Fragment>{children}</React.Fragment>
 }
 
-function isBrowserSupported(browserVersion: number, supportedBrowsers: SupportedBrowsers) {
-  for (const supportedBrowser of supportedBrowsers) {
-    if (supportedBrowser.active && browserVersion >= supportedBrowser.version) {
-      return true
-    }
-  }
-  return false
-}
-
 export const BrowserNotSupportedPage: React.FC<{
   browserVersion: number
   supportedBrowsers: SupportedBrowsers
   onPress: () => void
 }> = ({ browserVersion, supportedBrowsers, onPress }) => {
-  const title = `Oups\u00a0! Nous ne pouvons afficher correctement l’application car ton navigateur (${DeviceDetect.browserName} v${browserVersion}) n’est pas à jour`
+  const title = `Oups\u00a0! Nous ne pouvons afficher correctement l’application car ton navigateur (${browserName} v${browserVersion}) n’est pas à jour`
   return (
     <ScrollView contentContainerStyle={contentContainerStyle}>
       <Container>
@@ -69,7 +61,7 @@ export const BrowserNotSupportedPage: React.FC<{
           Voici ceux que nous te conseillons pour profiter pleinement du pass Culture&nbsp;:
         </Typo.Body>
         <VerticalUl>
-          {supportedBrowsers.map(({ browser, version }) => {
+          {Object.entries(supportedBrowsers).map(([browser, version]) => {
             let displayedMessage = `- ${browser}`
             if (version > 0) {
               displayedMessage += ` (version > ${version})`
@@ -86,8 +78,12 @@ export const BrowserNotSupportedPage: React.FC<{
           Mets vite à jour ton navigateur en allant dans les paramètres
         </Typo.BodyAccentXs>
         <Spacer.Column numberOfSpaces={2} />
+        <Typo.BodyAccentXs>
+          J’accède au pass Culture sans mettre à jour mon navigateur
+        </Typo.BodyAccentXs>
+        <Spacer.Column numberOfSpaces={2} />
         <ButtonPrimary
-          wording="J’accède au pass Culture sans mettre à jour mon navigateur"
+          wording="Continuer quand même"
           onPress={onPress}
           buttonHeight="tall"
           numberOfLines={2}
