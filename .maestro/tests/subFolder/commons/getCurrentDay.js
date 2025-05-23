fb4b3247378c8035fedbda8d53f01f23ddbe5cf7
arenault-pass <153505101+arenault-pass@users.noreply.github.com>
2025-05-23 17:51:53 +0200

    (PC-36254) fix(e2e): make date iso on cloud runs and fix offer selection (#8169)

--- .maestro/tests/subFolder/commons/getCurrentDay.js
+++ .maestro/tests/subFolder/commons/getCurrentDay.js
@@ -1,8 +1,15 @@
 const MINIMUM_CANCELLATION_DAYS = 3;
 
+const days = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
 const targetDate = new Date();
 targetDate.setDate(targetDate.getDate() + MINIMUM_CANCELLATION_DAYS);
 
 output.targetDateIso = targetDate.toISOString().slice(0, 10);
 
-output.targetDateTest = `.*${targetDate.getDate()}.*`;
+const dayName = days[targetDate.getDay()];
+const day = targetDate.getDate();
+const monthName = months[targetDate.getMonth()];
+const formattedDate = `${dayName} ${day} ${monthName}`;
+output.targetDateTest = `.*${formattedDate}.*`;
