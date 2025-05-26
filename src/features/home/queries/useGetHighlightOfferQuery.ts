75c8407996032ebc3b5f8bb0ea5ea8eb1c848f9b
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-05-26 15:28:11 +0200

    (PC-36220) refactor(home): queryfy useHighlightOffer (#8159)

---
+++ src/features/home/queries/useGetHighlightOfferQuery.ts
@@ -0,0 +1,16 @@
+import { useQuery } from 'react-query'
+
+import { QueryKeys } from 'libs/queryKeys'
+import { Offer } from 'shared/offer/types'
+
+export const useGetHighlightOfferQuery = ({
+  id,
+  getHighlightOffer,
+}: {
+  id: string
+  getHighlightOffer: () => Promise<Offer[] | undefined>
+}) =>
+  useQuery({
+    queryKey: [QueryKeys.HIGHLIGHT_OFFER, id],
+    queryFn: getHighlightOffer,
+  })
