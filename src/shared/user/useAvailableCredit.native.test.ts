398b5a636aee695115186b879dfd62a87cf25ef0
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-05-26 14:26:41 +0200

    (PC-36278) fix(useAvailableCredit): more specific conditions (#8175)

--- src/shared/user/useAvailableCredit.native.test.ts
+++ src/shared/user/useAvailableCredit.native.test.ts
@@ -1,6 +1,6 @@
 import mockdate from 'mockdate'
 
-import { UserProfileResponse } from 'api/gen'
+import { DomainsCredit, UserProfileResponse } from 'api/gen'
 import { nonBeneficiaryUser } from 'fixtures/user'
 
 import { hasOngoingCredit } from './useAvailableCredit'
@@ -10,25 +10,43 @@ jest.mock('libs/firebase/analytics/analytics')
 describe('useAvailableCredit', () => {
   mockdate.set(new Date('2020-12-01T00:00:00.000Z'))
 
+  const noMoreCredit = {
+    all: { initial: 0, remaining: 0 },
+    physical: { initial: 0, remaining: 0 },
+    digital: { initial: 0, remaining: 0 },
+  }
+  const remainingCredit = {
+    all: { initial: 150, remaining: 20 },
+    physical: { initial: 0, remaining: 0 },
+    digital: { initial: 0, remaining: 0 },
+  }
+
   describe('hasOngoingCredit', () => {
     it.each`
-      depositExpirationDate         | expected
-      ${'2022-12-01T00:00:00.000Z'} | ${true}
-      ${'2019-12-01T00:00:00.000Z'} | ${false}
-      ${undefined}                  | ${false}
-      ${'2020-12-01T00:00:00.000Z'} | ${true}
+      depositExpirationDate         | domainsCreditAllRemaining | expected
+      ${'2022-12-01T00:00:00.000Z'} | ${remainingCredit}        | ${true}
+      ${'2019-12-01T00:00:00.000Z'} | ${remainingCredit}        | ${false}
+      ${undefined}                  | ${remainingCredit}        | ${false}
+      ${'2020-12-01T00:00:00.000Z'} | ${remainingCredit}        | ${true}
+      ${'2022-12-01T00:00:00.000Z'} | ${noMoreCredit}           | ${false}
+      ${'2019-12-01T00:00:00.000Z'} | ${noMoreCredit}           | ${false}
+      ${undefined}                  | ${noMoreCredit}           | ${false}
+      ${'2020-12-01T00:00:00.000Z'} | ${noMoreCredit}           | ${false}
     `(
-      'should return $expected when depositExpirationDate equals $depositExpirationDate',
+      'should return $expected when depositExpirationDate equals $depositExpirationDate, and domainsCredit?.all.remaining equals $domainsCreditAllRemaining',
       ({
         depositExpirationDate,
+        domainsCreditAllRemaining,
         expected,
       }: {
         depositExpirationDate: string
+        domainsCreditAllRemaining: DomainsCredit
         expected: boolean
       }) => {
         const mockedUser: UserProfileResponse = {
           ...nonBeneficiaryUser,
           depositExpirationDate: depositExpirationDate,
+          domainsCredit: domainsCreditAllRemaining,
         }
         const result = hasOngoingCredit(mockedUser)
 
