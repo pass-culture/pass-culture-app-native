55210790836f6b0def3f7906504564522c9e23fa
Tanguy <47600889+tprache-pass@users.noreply.github.com>
2025-06-02 15:58:04 +0200

    feat: update color mapping

--- src/ui/theme/gradientImagesMapping.ts
+++ src/ui/theme/gradientImagesMapping.ts
@@ -1,15 +1,11 @@
-import Aquamarine from 'features/home/images/headerGradient/Aquamarine.png'
-import Coral from 'features/home/images/headerGradient/Coral.png'
-import DeepPink from 'features/home/images/headerGradient/DeepPink.png'
-import Gold from 'features/home/images/headerGradient/Gold.png'
-import Lilac from 'features/home/images/headerGradient/Lilac.png'
-import SkyBlue from 'features/home/images/headerGradient/SkyBlue.png'
+// eslint-disable-next-line no-restricted-imports
+import { ColorsEnum } from 'ui/theme/colors'
 
 export const gradientImagesMapping = {
-  Gold,
-  Aquamarine,
-  SkyBlue,
-  DeepPink,
-  Coral,
-  Lilac,
+  Gold: [ColorsEnum.GOLD_LIGHT_100, ColorsEnum.LILAC_LIGHT],
+  Aquamarine: [ColorsEnum.AQUAMARINE_LIGHT, ColorsEnum.LILAC_LIGHT],
+  SkyBlue: [ColorsEnum.SKY_BLUE_LIGHT, ColorsEnum.DEEP_PINK_LIGHT],
+  DeepPink: [ColorsEnum.DEEP_PINK_LIGHT, ColorsEnum.AQUAMARINE_LIGHT],
+  Coral: [ColorsEnum.CORAL_LIGHT, ColorsEnum.SKY_BLUE_LIGHT],
+  Lilac: [ColorsEnum.LILAC_LIGHT, ColorsEnum.DEEP_PINK_LIGHT],
 }
