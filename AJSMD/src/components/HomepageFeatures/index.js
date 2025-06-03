e3a8cb247cb262ef673cc3832bef5a88ba9a9082
Nturchetti <nicolas.turchetti7@gmail.com>
2025-06-03 12:10:48 +0200

    hackathon AJSMD

---
+++ AJSMD/src/components/HomepageFeatures/index.js
@@ -0,0 +1,61 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Règles de gestions',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Comprendre la couverture des tests unitaires et E2E, les bonnes pratiques.
+      </>
+    ),
+  },
+  {
+    title: 'Résumé des tests',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+       'Disposez d\'un résumé de vos tests unitaires et E2E, <code>docs</code>.''
+      </>
+    ),
+  },
+  {
+    title: 'Base de connaissance',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Fini les recherches compliquées, vous pouvez rechercher par fonctionnalité par parcours utilisateurs et même par mot clé.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
