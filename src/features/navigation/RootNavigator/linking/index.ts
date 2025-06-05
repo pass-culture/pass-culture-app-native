cb5c6dede76a91ba16f1f8b833e175c6605f817d
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-06-05 14:17:53 +0200

    (PC-35664) refactor(navigation): removed onboarding from abstraction

--- src/features/navigation/RootNavigator/linking/index.ts
+++ src/features/navigation/RootNavigator/linking/index.ts
@@ -1,6 +1,7 @@
 import { LinkingOptions } from '@react-navigation/native'
 
 import { CheatcodesStackParamList } from 'features/navigation/CheatcodesStackNavigator/types'
+import { onboardingStackNavigatorConfig } from 'features/navigation/OnboardingStackNavigator/onboardingStackNavigatorConfig'
 import { rootScreensConfig } from 'features/navigation/RootNavigator/screens'
 import { RootStackParamList } from 'features/navigation/RootNavigator/types'
 import { WEBAPP_V2_URL } from 'libs/environment/useWebAppUrl'
@@ -10,6 +11,7 @@ import { getInitialURL } from './getInitialUrl'
 import { customGetPathFromState } from './getPathFromState'
 import { customGetStateFromPath } from './getStateFromPath'
 import { subscribe } from './subscribe'
+import { cheatcodesStackNavigatorConfig } from 'features/navigation/CheatcodesStackNavigator/CheatcodesStackNavigatorConfig'
 
 const PASS_CULTURE_PREFIX_URL = 'passculture://'
 
