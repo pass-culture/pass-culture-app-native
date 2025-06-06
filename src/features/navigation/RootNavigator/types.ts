7b7bb1c00b4fedfb0b5b71520c6f4c72e20fa62c
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-06-06 14:54:58 +0200

    Move EighteenBirthday

--- src/features/navigation/RootNavigator/types.ts
+++ src/features/navigation/RootNavigator/types.ts
@@ -252,6 +252,7 @@ export type RootStackParamList = {
   CulturalSurvey: undefined
   DeeplinksGenerator: undefined
   EighteenBirthday: undefined
+  _DeeplinkOnlyEighteenBirthday1: undefined
   FavoritesSorts: undefined
   ForgottenPassword: undefined
   FraudulentSuspendedAccount: undefined
