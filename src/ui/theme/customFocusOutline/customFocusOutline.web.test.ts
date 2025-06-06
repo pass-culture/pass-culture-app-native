4ccf847073e9bab0cdb473b094e4b36a406b0b2e
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-06-06 14:34:20 +0200

    (PC-36354) refactor(ui): fix customFocusOutline test

--- src/ui/theme/customFocusOutline/customFocusOutline.web.test.ts
+++ src/ui/theme/customFocusOutline/customFocusOutline.web.test.ts
@@ -1,14 +1,14 @@
 import { theme } from 'theme'
 import { customFocusOutline } from 'ui/theme/customFocusOutline/customFocusOutline'
 
-const color = theme.colors.white
+const color = theme.designSystem.color.outline.inverted
 
 describe('customWebFocusOutline', () => {
   it('should display the custom outline if isFocus = true', () => {
     const result = customFocusOutline({ isFocus: true })
 
     expect(result).toEqual({
-      outlineColor: theme.outline.color,
+      outlineColor: theme.designSystem.color.outline.default,
       outlineStyle: theme.outline.style,
       outlineWidth: theme.outline.width,
       outlineOffset: theme.outline.offSet,
@@ -19,7 +19,7 @@ describe('customWebFocusOutline', () => {
     const result = customFocusOutline({ color, isFocus: true })
 
     expect(result).toEqual({
-      outlineColor: theme.colors.white,
+      outlineColor: theme.designSystem.color.outline.inverted,
       outlineStyle: theme.outline.style,
       outlineWidth: theme.outline.width,
       outlineOffset: theme.outline.offSet,
@@ -30,7 +30,7 @@ describe('customWebFocusOutline', () => {
     const result = customFocusOutline({ width: 1, isFocus: true })
 
     expect(result).toEqual({
-      outlineColor: theme.outline.color,
+      outlineColor: theme.designSystem.color.outline.default,
       outlineStyle: theme.outline.style,
       outlineWidth: 1,
       outlineOffset: theme.outline.offSet,
@@ -43,7 +43,7 @@ describe('customWebFocusOutline', () => {
     expect(result).toEqual({
       '&:active': { outline: 'none', opacity: theme.activeOpacity },
       '&:focus-visible': {
-        outlineColor: theme.outline.color,
+        outlineColor: theme.designSystem.color.outline.default,
         outlineStyle: theme.outline.style,
         outlineWidth: theme.outline.width,
         outlineOffset: theme.outline.offSet,
@@ -57,7 +57,7 @@ describe('customWebFocusOutline', () => {
     expect(result).toEqual({
       '&:active': { outline: 'none', opacity: theme.activeOpacity },
       '&:focus-visible': {
-        outlineColor: theme.colors.white,
+        outlineColor: theme.designSystem.color.outline.inverted,
         outlineStyle: theme.outline.style,
         outlineWidth: theme.outline.width,
         outlineOffset: theme.outline.offSet,
@@ -71,7 +71,7 @@ describe('customWebFocusOutline', () => {
     expect(result).toEqual({
       '&:active': { outline: 'none', opacity: theme.activeOpacity },
       '&:focus-visible': {
-        outlineColor: theme.outline.color,
+        outlineColor: theme.designSystem.color.outline.default,
         outlineStyle: theme.outline.style,
         outlineWidth: 1,
         outlineOffset: theme.outline.offSet,
