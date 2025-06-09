1a994dd01398968a3f1452efeb0f2f08cee9a377
Mathieu Meissonnier <173043140+mmeissonnier-pass@users.noreply.github.com>
2025-06-09 15:44:25 +0200

    refactor: remove unused hook isTextEllipsis

--- src/ui/components/CollapsibleText/types.ts
+++
@@ -1,7 +0,0 @@
-import { LayoutChangeEvent, NativeSyntheticEvent, TextLayoutEventData } from 'react-native'
-
-export type IsTextEllipsisOutput = {
-  isTextEllipsis: boolean
-  onTextLayout?: (event: NativeSyntheticEvent<TextLayoutEventData>) => void
-  onLayout?: (event: LayoutChangeEvent) => void
-}
