6e907a4e175bee1c899f4640105a8c405d522f36
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-06-13 09:42:10 +0200

    wip added tests to getScreenPath

--- src/features/navigation/RootNavigator/linking/getScreenPath.ts
+++ src/features/navigation/RootNavigator/linking/getScreenPath.ts
@@ -13,6 +13,7 @@ export function getScreenPath<RouteName extends keyof AllNavParamList>(
     const { screen: nestedScreen, params: nestedParams } = params
     const nestedRoutes = [{ name: nestedScreen, params: nestedParams }]
     state = { routes: [{ name: screen, state: { routes: nestedRoutes } }] }
+    // console.log(JSON.stringify(state, null, 2))
 
     if (isScreen('SearchStackNavigator', nestedScreen, nestedParams) && nestedParams) {
       const { screen: searchStackScreen, params: searchStackParams } = nestedParams
@@ -28,17 +29,33 @@ export function getScreenPath<RouteName extends keyof AllNavParamList>(
     }
   }
   if (isScreen('ProfileStackNavigator', screen, params)) {
-    const nestedRoutes = [
-      {
-        name: screen,
-        params: params,
-      },
-    ]
     state = {
       routes: [
         {
-          name: screen,
-          state: { routes: nestedRoutes },
+          name: 'ProfileStackNavigator',
+          state: {
+            routes: [
+              {
+                name: params?.screen ?? '',
+              },
+            ],
+          },
+        },
+      ],
+    }
+  }
+  if (isScreen('OnboardingStackNavigator', screen, params)) {
+    state = {
+      routes: [
+        {
+          name: 'OnboardingStackNavigator',
+          state: {
+            routes: [
+              {
+                name: params?.screen ?? '',
+              },
+            ],
+          },
         },
       ],
     }
--- src/features/search/helpers/useNavigateToSearch/useNavigateToSearch.ts
+++ src/features/search/helpers/useNavigateToSearch/useNavigateToSearch.ts
@@ -3,22 +3,36 @@ import { StackActions, useNavigation } from '@react-navigation/native'
 import { defaultDisabilitiesProperties } from 'features/accessibility/context/AccessibilityFiltersWrapper'
 import { DisabilitiesProperties } from 'features/accessibility/types'
 import { UseNavigationType } from 'features/navigation/RootNavigator/types'
-import { getSearchStackConfig } from 'features/navigation/SearchStackNavigator/getSearchStackConfig'
-import { SearchStackRouteName } from 'features/navigation/SearchStackNavigator/SearchStackTypes'
 import { SearchState } from 'features/search/types'
 
-export const useNavigateToSearch = (routeName: SearchStackRouteName) => {
+export const useNavigateToSearch = (routeName) => {
   const { navigate, dispatch } = useNavigation<UseNavigationType>()
   const navigateToSearch = (
     newSearchState: SearchState,
     newAccessibilityFilter?: DisabilitiesProperties
   ): void => {
-    navigate(
-      ...getSearchStackConfig(routeName, {
-        ...newSearchState,
-        accessibilityFilter: newAccessibilityFilter ?? defaultDisabilitiesProperties,
-      })
-    )
+    // navigate(
+    //   ...getSearchStackConfig(routeName, {
+    //     ...newSearchState,
+    //     accessibilityFilter: newAccessibilityFilter ?? defaultDisabilitiesProperties,
+    //   })
+    // )
+    // navigate(
+    //   'TabNavigator', { screen: routeName, params: {
+    //     ...newSearchState,
+    //     accessibilityFilter: newAccessibilityFilter ?? defaultDisabilitiesProperties,
+    //   } }
+    // )
+    navigate('TabNavigator', {
+      screen: 'SearchStackNavigator',
+      params: {
+        screen: routeName,
+        params: {
+          ...newSearchState,
+          accessibilityFilter: newAccessibilityFilter ?? defaultDisabilitiesProperties,
+        },
+      },
+    })
   }
 
   const replaceToSearch = (
