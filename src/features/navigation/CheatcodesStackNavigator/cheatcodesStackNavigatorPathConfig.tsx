8a0d7aec191ba1be19c8cfe1e6e3095ee3838219
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-06-13 14:49:32 +0200

    remove comments

--- src/features/navigation/CheatcodesStackNavigator/cheatcodesStackNavigatorPathConfig.tsx
+++ src/features/navigation/CheatcodesStackNavigator/cheatcodesStackNavigatorPathConfig.tsx
@@ -1,6 +1,5 @@
 export const cheatcodesStackNavigatorPathConfig = {
   CheatcodesStackNavigator: {
-    // initialRouteName: 'cheatcodes',
     screens: {
       CheatcodesMenu: 'cheatcodes',
       CheatcodesNavigationAccountManagement: 'cheatcodes/other/account-management',
