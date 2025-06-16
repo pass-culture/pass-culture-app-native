96695b1eacb843653ca56ae178a2e9766a8fd22e
Mathieu Meissonnier <173043140+mmeissonnier-pass@users.noreply.github.com>
2025-06-16 20:05:52 +0200

    fix: fix ModalCalendar test

---
+++ src/features/search/helpers/schema/calendarSchema/getCalendarSchema.ts
@@ -0,0 +1,16 @@
+import { startOfDay } from 'date-fns'
+import { date, object } from 'yup'
+
+export const getCalendarSchema = () =>
+  object().shape({
+    selectedStartDate: date()
+      .optional()
+      .min(startOfDay(new Date()), 'La date de début ne peut pas être dans le passé'),
+    selectedEndDate: date()
+      .optional()
+      .when('selectedStartDate', (selectedStartDate, schema) =>
+        selectedStartDate
+          ? schema.min(selectedStartDate, 'La date de fin ne peut pas être avant la date de début')
+          : schema
+      ),
+  })
