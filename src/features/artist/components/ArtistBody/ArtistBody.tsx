560d1c178bb99e2720d3a8ca7a536b987c19e425
yleclercq-pass <62058919+yleclercq-pass@users.noreply.github.com>
2025-06-16 15:52:10 +0200

    (PC-36550) after review fix condition

--- src/features/artist/components/ArtistBody/ArtistBody.tsx
+++ src/features/artist/components/ArtistBody/ArtistBody.tsx
@@ -83,7 +83,7 @@ export const ArtistBody: FunctionComponent<Props> = ({
         <ViewGap gap={8}>
           <ViewGap gap={6}>
             <ArtistHeader name={name} avatarImage={avatarImage} />
-            {description ? (
+            {capitalizedDescriptionWithDot ? (
               <Description gap={1}>
                 <Typo.BodyAccent>Quelques infos à son sujet</Typo.BodyAccent>
                 <CollapsibleText numberOfLines={NUMBER_OF_LINES_OF_DESCRIPTION_SECTION}>
