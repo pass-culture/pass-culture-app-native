73a8ba78c1299b0a719fb3c97c2ac12bed4867ca
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-06-17 15:03:23 +0200

    refactor: reuse variable

--- src/features/artist/pages/ArtistContainer.tsx
+++ src/features/artist/pages/ArtistContainer.tsx
@@ -1,19 +1,15 @@
-import { useRoute } from '@react-navigation/native'
 import React, { FunctionComponent } from 'react'
 
 import { ArtistBody } from 'features/artist/components/ArtistBody/ArtistBody'
 import { useArtistQuery } from 'features/artist/queries/useArtistQuery'
 import { PageNotFound } from 'features/navigation/pages/PageNotFound'
-import { UseRouteType } from 'features/navigation/RootNavigator/types'
 import { useArtistResultsQuery } from 'queries/offer/useArtistResultsQuery'
 
 export const ArtistContainer: FunctionComponent<{ artistId: string }> = ({ artistId }) => {
-  const { params } = useRoute<UseRouteType<'Artist'>>()
-
   const { artistPlaylist, artistTopOffers } = useArtistResultsQuery({
     artistId,
   })
-  const { data: artist } = useArtistQuery(params.id)
+  const { data: artist } = useArtistQuery(artistId)
 
   if (!artist) return <PageNotFound />
 
