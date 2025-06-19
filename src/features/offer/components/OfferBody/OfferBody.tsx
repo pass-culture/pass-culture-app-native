893c742a6f28c692b7f9cb4bc3aceb3ea5b5b109
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-06-19 18:37:00 +0200

    fixup! refactor: use select instead of returning partial data of react query hook

--- src/features/offer/components/OfferBody/OfferBody.tsx
+++ src/features/offer/components/OfferBody/OfferBody.tsx
@@ -27,7 +27,7 @@ import {
   getIfPricesShouldBeFixed,
 } from 'libs/parsers/getDisplayedPrice'
 import { Subcategory } from 'libs/subcategories/types'
-import { useArtistResultsQuery } from 'queries/offer/useArtistResultsQuery'
+import { defaultArtistResults, useArtistResultsQuery } from 'queries/offer/useArtistResultsQuery'
 import { formatFullAddress } from 'shared/address/addressFormatter'
 import { useGetCurrencyToDisplay } from 'shared/currency/useGetCurrencyToDisplay'
 import { useGetPacificFrancToEuroRate } from 'shared/exchangeRates/useGetPacificFrancToEuroRate'
@@ -81,7 +81,7 @@ export const OfferBody: FunctionComponent<Props> = ({
     { fractionDigits: 2 }
   )
 
-  const { artistPlaylist: artistOffers } = useArtistResultsQuery({
+  const { data: { artistPlaylist: artistOffers } = defaultArtistResults } = useArtistResultsQuery({
     artistId: artists.length > 0 ? artists[0]?.id : undefined,
     subcategoryId: offer.subcategoryId,
   })
