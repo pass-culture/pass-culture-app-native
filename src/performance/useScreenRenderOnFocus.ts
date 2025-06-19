2298929fc67e219711afbec8f42a4215facfb200
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-06-19 11:02:57 +0200

    Combined ios & web

--- src/performance/useScreenRenderOnFocus.ts
+++ src/performance/useScreenRenderOnFocus.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line local-rules/no-useless-hook
-export const useScreenRenderOnFocus = () => {
+export const useScreenRenderOnFocus = (_screenName: string) => {
   // perf().startScreenTrace not available on iOS
   // https://rnfirebase.io/perf/usage#custom-screen-traces
+  // no measure of screen render performance on web
 }
