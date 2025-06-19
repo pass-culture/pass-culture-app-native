740186886aa7756931c4c8a002e46df122c571dd
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-06-19 13:34:37 +0200

    Revert "refactor: replace switch with a record of all possibilities"
    This reverts commit 466610ad2e1c59fbc11cf1f5bcce920b9e50ddcc.

--- src/features/artist/pages/ArtistContainer.tsx
+++ src/features/artist/pages/ArtistContainer.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps, FunctionComponent } from 'react'
+import React, { FunctionComponent } from 'react'
 
 import { ArtistBody } from 'features/artist/components/ArtistBody/ArtistBody'
 import { useArtistQuery } from 'features/artist/queries/useArtistQuery'
@@ -6,23 +6,28 @@ import { PageNotFound } from 'features/navigation/pages/PageNotFound'
 import { useArtistResultsQuery } from 'queries/offer/useArtistResultsQuery'
 import { LoadingPage } from 'ui/pages/LoadingPage'
 
-const toto = {
-  loading: () => <LoadingPage />,
-  idle: () => {
-    throw new Error('Not implemented yet: "idle" ')
-  },
-  success: ({ artist, artistPlaylist, artistTopOffers }: ComponentProps<typeof ArtistBody>) => (
-    <ArtistBody artist={artist} artistPlaylist={artistPlaylist} artistTopOffers={artistTopOffers} />
-  ),
-  error: () => <PageNotFound />,
-}
-
 export const ArtistContainer: FunctionComponent<{ artistId: string }> = ({ artistId }) => {
   const { artistPlaylist, artistTopOffers } = useArtistResultsQuery({ artistId })
   const { data: artist, status } = useArtistQuery(artistId)
 
-  const Component = toto[status]
-  return (
-    <Component artist={artist} artistPlaylist={artistPlaylist} artistTopOffers={artistTopOffers} />
-  )
+  switch (status) {
+    case 'loading':
+      return <LoadingPage />
+
+    case 'idle': {
+      throw new Error('Not implemented yet: "idle" case')
+    }
+
+    case 'success':
+      return (
+        <ArtistBody
+          artist={artist}
+          artistPlaylist={artistPlaylist}
+          artistTopOffers={artistTopOffers}
+        />
+      )
+
+    case 'error':
+      return <PageNotFound />
+  }
 }
