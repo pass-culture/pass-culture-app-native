18a947b1324baf5a22ccfe5e0b47190421f910e3
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2025-06-20 11:40:24 +0200

    (PC-36555) fix(Home): emojis are inlined and title is accessible on the three platform

--- src/features/home/components/AccessibleTitle.tsx
+++ src/features/home/components/AccessibleTitle.tsx
@@ -1,4 +1,5 @@
 import React, { ComponentProps, ComponentType } from 'react'
+import { Platform, useWindowDimensions } from 'react-native'
 import styled from 'styled-components/native'
 
 import { Typo } from 'ui/theme'
@@ -14,26 +15,39 @@ export const separateTitleAndEmojis = (title: string) => {
 export const AccessibleTitle = ({
   title,
   testID,
-  TitleComponent,
+  TitleComponent = Typo.Title3,
 }: {
   title: string
   testID?: string
   TitleComponent?: ComponentType<ComponentProps<typeof Typo.Title3>>
 }) => {
+  const { width: windowWidth } = useWindowDimensions()
   const { titleText, titleEmoji } = separateTitleAndEmojis(title)
 
   const StyledTitleComponent = styled(TitleComponent || Typo.Title3)({})
 
   return (
-    <InlineView numberOfLines={2} testID={testID}>
-      <StyledTitleComponent>{titleText}</StyledTitleComponent>
-      <StyledTitleComponent accessibilityHidden>{titleEmoji}</StyledTitleComponent>
-    </InlineView>
+    <TitleWrapper
+      testID={testID}
+      windowWidth={Platform.OS === 'ios' ? undefined : windowWidth}
+      accessibilityLabel={titleText}
+      accessiblityRole="text">
+      <StyledTitleComponent
+        numberOfLines={2}
+        accessibilityHidden
+        accessiblityRole="text"
+        accessibilityLabel={titleText} /* for iOS */
+      >
+        {titleText}
+        {titleEmoji}
+      </StyledTitleComponent>
+    </TitleWrapper>
   )
 }
 
-const InlineView = styled.View(({ theme }) => ({
-  marginHorizontal: theme.contentPage.marginHorizontal,
-  flexDirection: 'row',
-  alignItems: 'center',
-}))
+const TitleWrapper = styled.View<{ windowWidth?: number }>(({ windowWidth, theme }) => {
+  return {
+    marginHorizontal: theme.contentPage.marginHorizontal,
+    width: windowWidth,
+  }
+})
