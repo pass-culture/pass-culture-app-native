7e307cdb3bbf8f264f0fec52dd3c248c8cdb4ab9
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-06-23 16:03:39 +0200

    Revert "wip"
    This reverts commit 67d82523ade94fc951cf193eff099c43ecff7f15.

--- src/features/offer/components/OfferBody/OfferBody.native.test.tsx
+++ src/features/offer/components/OfferBody/OfferBody.native.test.tsx
@@ -11,17 +11,17 @@ import {
   SubcategoryIdEnum,
   SubcategoryIdEnumv2,
 } from 'api/gen'
-import { mockArtist } from 'features/artist/fixtures/mockArtist'
 import { OfferBody } from 'features/offer/components/OfferBody/OfferBody'
 import { mockSubcategory, mockSubcategoryBook } from 'features/offer/fixtures/mockSubcategory'
 import { offerResponseSnap } from 'features/offer/fixtures/offerResponse'
+import { mockedAlgoliaOffersWithSameArtistResponse } from 'libs/algolia/fixtures/algoliaFixtures'
 import { analytics } from 'libs/analytics/provider'
 import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/tests/setFeatureFlags'
 import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 import { Position } from 'libs/location'
 import { SuggestedPlace } from 'libs/place/types'
 import { Subcategory } from 'libs/subcategories/types'
-import { mockServer } from 'tests/mswServer'
+import * as useArtistResultsAPI from 'queries/offer/useArtistResultsQuery'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { render, screen, userEvent } from 'tests/utils'
 
@@ -60,6 +60,14 @@ jest.mock('react-native/Libraries/Animated/createAnimatedComponent', () => {
   }
 })
 
+const useArtistResultsSpy = jest
+  .spyOn(useArtistResultsAPI, 'useArtistResultsQuery')
+  .mockImplementation()
+  .mockReturnValue({
+    artistPlaylist: mockedAlgoliaOffersWithSameArtistResponse,
+    artistTopOffers: mockedAlgoliaOffersWithSameArtistResponse.slice(0, 4),
+  })
+
 const user = userEvent.setup()
 
 jest.useFakeTimers()
@@ -72,7 +80,6 @@ describe('<OfferBody />', () => {
       RemoteStoreFeatureFlags.WIP_REACTION_FEATURE,
       RemoteStoreFeatureFlags.WIP_OFFER_CHRONICLE_SECTION,
     ])
-    mockServer.getApi(`/v1/artists/${mockArtist.id}`, mockArtist)
   })
 
   describe('Tags section', () => {
@@ -506,6 +513,11 @@ describe('<OfferBody />', () => {
       artists: [{ id: '3', name: 'J.K Rowling' }],
     }
 
+    useArtistResultsSpy.mockReturnValueOnce({
+      artistPlaylist: mockedAlgoliaOffersWithSameArtistResponse.slice(0, 1),
+      artistTopOffers: mockedAlgoliaOffersWithSameArtistResponse.slice(0, 4),
+    })
+
     renderOfferBody({
       offer,
       subcategory: mockSubcategoryBook,
@@ -520,7 +532,7 @@ describe('<OfferBody />', () => {
     const offer: OfferResponseV2 = {
       ...offerResponseSnap,
       subcategoryId: SubcategoryIdEnum.LIVRE_PAPIER,
-      artists: [mockArtist],
+      artists: [{ id: '1', name: 'Stephen King' }],
     }
 
     renderOfferBody({
