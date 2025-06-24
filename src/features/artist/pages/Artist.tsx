598e3da68d892c96f31c375da0755e2c25e39589
Xavier Le Cunff <163895831+xlecunff-pass@users.noreply.github.com>
2025-06-24 14:35:17 +0200

    refactor(Artists): avant la pause dej de mardi

--- src/features/artist/pages/Artist.tsx
+++ src/features/artist/pages/Artist.tsx
@@ -4,16 +4,30 @@ import React, { FunctionComponent } from 'react'
 import { ArtistBody } from 'features/artist/components/ArtistBody/ArtistBody'
 import { useArtistQuery } from 'features/artist/queries/useArtistQuery'
 import { UseRouteType } from 'features/navigation/RootNavigator/types'
+import { useTransformOfferHits } from 'libs/algolia/fetchAlgolia/transformOfferHit'
 import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
-import { useArtistResultsQuery } from 'queries/offer/useArtistResultsQuery'
+import { useLocation } from 'libs/location'
+import {
+  useArtistOffersPlaylistQuery,
+  useArtistTopOffersPlaylistQuery,
+} from 'queries/offer/useOffersByArtistQuery'
 
 export const Artist: FunctionComponent = () => {
   const enableArtistPage = useFeatureFlag(RemoteStoreFeatureFlags.WIP_ARTIST_PAGE)
   const { params } = useRoute<UseRouteType<'Artist'>>()
+  const { userLocation } = useLocation()
+  const transformHits = useTransformOfferHits()
 
-  const { artistPlaylist, artistTopOffers } = useArtistResultsQuery({
+  const { data: artistPlaylist } = useArtistOffersPlaylistQuery({
     artistId: params.id,
+    userLocation,
+    transformHits,
+  })
+  const { data: artistTopOffers } = useArtistTopOffersPlaylistQuery({
+    artistId: params.id,
+    userLocation,
+    transformHits,
   })
   const { data: artist } = useArtistQuery(params.id)
 
@@ -21,6 +35,10 @@ export const Artist: FunctionComponent = () => {
   if (!artist || !enableArtistPage) return null
 
   return (
-    <ArtistBody artist={artist} artistPlaylist={artistPlaylist} artistTopOffers={artistTopOffers} />
+    <ArtistBody
+      artist={artist}
+      artistPlaylist={artistPlaylist ?? []}
+      artistTopOffers={artistTopOffers ?? []}
+    />
   )
 }
---
+++ src/queries/offer/useOffersByArtistQuery.ts
@@ -0,0 +1,120 @@
+import { Hit } from '@algolia/client-search'
+import { useQuery } from 'react-query'
+
+import { AlgoliaOfferWithArtistAndEan } from 'libs/algolia/types'
+import { Position } from 'libs/location'
+import { formatDistance } from 'libs/parsers/formatDistance'
+import { QueryKeys } from 'libs/queryKeys'
+import { Prettify } from 'types/Prettify'
+
+import { fetchOffersByArtist } from './fetchOffersByArtist'
+
+// yarn test:unit src/features/artist/ src/queries/offer/ src/features/offer/components/OfferBody/ src/features/offer/pages/
+
+type FetchOfferByArtistResult = {
+  playlistHits: Hit<AlgoliaOfferWithArtistAndEan>[]
+  topOffersHits: Hit<AlgoliaOfferWithArtistAndEan>[]
+}
+
+type UseOffersByArtistParams = {
+  artistId: string
+  userLocation: Position
+  enabled?: boolean
+}
+
+type UseOffersByArtistSelectParams = Prettify<
+  UseOffersByArtistParams & {
+    select?: (data: FetchOfferByArtistResult) => AlgoliaOfferWithArtistAndEan[]
+  }
+>
+
+type UseArtistOffersPlaylistParams = UseOffersByArtistParams & {
+  transformHits: (hit: AlgoliaOfferWithArtistAndEan) => AlgoliaOfferWithArtistAndEan
+}
+
+type GetSortedHitsInputs = {
+  hits: Hit<AlgoliaOfferWithArtistAndEan>[]
+  userLocation: Position
+  transformHits: (hit: AlgoliaOfferWithArtistAndEan) => AlgoliaOfferWithArtistAndEan
+}
+
+const useOffersByArtistQuery = ({
+  artistId,
+  userLocation,
+  select,
+  enabled = true,
+}: UseOffersByArtistSelectParams) =>
+  useQuery({
+    queryKey: [QueryKeys.ARTIST_PLAYLIST, artistId],
+    queryFn: async () => {
+      const { playlistHits, topOffersHits } = await fetchOffersByArtist({
+        artistId,
+        userLocation,
+      })
+      return { playlistHits, topOffersHits }
+    },
+    initialData: { playlistHits: [], topOffersHits: [] },
+    enabled: enabled && !!artistId,
+    select,
+  })
+
+export const useArtistOffersPlaylistQuery = ({
+  artistId,
+  userLocation,
+  enabled,
+  transformHits,
+}: UseArtistOffersPlaylistParams) =>
+  useOffersByArtistQuery({
+    artistId,
+    userLocation,
+    enabled,
+    select: (data) =>
+      getSortedHits({
+        transformHits,
+        userLocation,
+        hits: data.playlistHits,
+      }),
+  })
+
+export const useArtistTopOffersPlaylistQuery = ({
+  artistId,
+  userLocation,
+  enabled,
+  transformHits,
+}: UseArtistOffersPlaylistParams) =>
+  useOffersByArtistQuery({
+    artistId,
+    userLocation,
+    enabled,
+    select: (data) =>
+      getSortedHits({
+        transformHits,
+        userLocation,
+        hits: data.topOffersHits,
+      }),
+  })
+
+const getSortedHits = ({
+  hits,
+  userLocation,
+  transformHits,
+}: GetSortedHitsInputs): AlgoliaOfferWithArtistAndEan[] => {
+  if (hits.length === 0) return []
+
+  const transformedHitsWithDistance = hits.map((hit) => {
+    const transformedHit = transformHits(hit)
+    const distance = formatDistance({ lat: hit._geoloc.lat, lng: hit._geoloc.lng }, userLocation)
+
+    return { ...transformedHit, distance: parseDistance(distance || '0') }
+  })
+
+  const sortedHits = [...transformedHitsWithDistance].sort((a, b) => a.distance - b.distance)
+
+  return sortedHits.map(({ distance: _distance, ...rest }) => rest)
+}
+
+const parseDistance = (distanceString: string) => {
+  const distance = parseFloat(distanceString)
+  const unitInMeters = distanceString.includes('km') ? 1000 : 1
+  return distance * unitInMeters
+}
