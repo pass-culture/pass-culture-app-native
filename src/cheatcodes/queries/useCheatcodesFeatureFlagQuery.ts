732e68c650eb3fd1cfa49165e10fe141d630da3d
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-06-25 16:24:24 +0200

    (PC-34997) feat(cheatcodes): show ffs deleted from code (#8334)
    * (PC-34997) feat(cheatcodes): show ffs deleted from code
    
    * Show dedicated section
    
    * removed com

--- src/cheatcodes/queries/useCheatcodesFeatureFlagQuery.ts
+++ src/cheatcodes/queries/useCheatcodesFeatureFlagQuery.ts
@@ -19,11 +19,11 @@ const isFeatureFlagActive = (
 }
 
 export type FeatureFlagAll = {
-  featureFlag: RemoteStoreFeatureFlags
+  featureFlag: string
   isFeatureFlagActive: boolean
 }
 
-export const useFeatureFlagAllQuery = () => {
+export const useCheatcodesFeatureFlagQuery = () => {
   const appBuildVersion = getAppBuildVersion()
   const {
     data: docSnapshot,
@@ -38,20 +38,32 @@ export const useFeatureFlagAllQuery = () => {
     return {}
   }
 
-  const featureFlags = Object.values(RemoteStoreFeatureFlags).reduce(
-    (flags, key) => {
-      const config = docSnapshot.get<FeatureFlagConfig>(key)
-      const owner = (config?.owner?.toLowerCase() as squads) ?? 'squad non-definie'
+  const allFlagsData = docSnapshot.data()
+
+  if (!allFlagsData) {
+    return {}
+  }
+
+  const knownFlags = new Set(Object.values(RemoteStoreFeatureFlags))
+
+  const featureFlags = Object.entries(allFlagsData).reduce(
+    (flags, [key, config]) => {
+      const isKnownFlag = knownFlags.has(key as RemoteStoreFeatureFlags)
+      const owner = isKnownFlag
+        ? ((config?.owner?.toLowerCase() as squads) ?? 'squad non-definie')
+        : 'not used in code'
+
       if (!flags[owner]) {
         flags[owner] = []
       }
+
       flags[owner].push({
         featureFlag: key,
         isFeatureFlagActive: isFeatureFlagActive(config, appBuildVersion),
       })
       return flags
     },
-    {} as Record<squads | 'squad non-definie', FeatureFlagAll[]>
+    {} as Record<squads | 'squad non-definie' | 'removed from code', FeatureFlagAll[]>
   )
 
   return featureFlags
