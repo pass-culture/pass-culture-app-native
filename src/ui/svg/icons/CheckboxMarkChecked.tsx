a92cabc2c810c6b05deb7b6126110b71d58a88d0
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-06-30 10:32:13 +0200

    BSR CheckboxMarkSvg

--- src/ui/svg/icons/CheckboxMarkChecked.tsx
+++ src/ui/svg/icons/CheckboxMarkChecked.tsx
@@ -20,8 +20,8 @@ const CheckboxMarkSvg: React.FunctionComponent<AccessibleRectangleIcon> = ({
     accessibilityLabel={accessibilityLabel}
     testID={testID}>
     <Path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M11.7367 2.28718C12.0901 2.66761 12.0874 3.28158 11.7308 3.65852L5.47631 10.2689C4.89792 10.8799 3.96207 10.88 3.38368 10.269L0.269379 6.97902C-0.0873538 6.60216 -0.0901338 5.98819 0.263169 5.60768C0.616473 5.22716 1.19207 5.2242 1.5488 5.60105L4.42987 8.64464L10.451 2.28087C10.8077 1.90393 11.3833 1.90675 11.7367 2.28718Z"
       fill={color}
     />
