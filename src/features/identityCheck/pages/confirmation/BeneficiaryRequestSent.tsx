10f086be1170b3a7cf66fcfdfd9bc5d4088b270e
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-06-30 14:31:39 +0200

    Use LINE_BREAK

--- src/features/identityCheck/pages/confirmation/BeneficiaryRequestSent.tsx
+++ src/features/identityCheck/pages/confirmation/BeneficiaryRequestSent.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import { GenericInfoPage } from 'ui/pages/GenericInfoPage'
 import { RequestSent } from 'ui/svg/icons/RequestSent'
+import { LINE_BREAK } from 'ui/theme/constants'
 
 export function BeneficiaryRequestSent() {
-  const subtitle =
-    'Nous étudions ton dossier...\nTu recevras un e-mail lorsque ta demande sera validée. En attendant, aide-nous à en savoir plus sur tes pratiques culturelles\u00a0!'
+  const subtitle = `Nous étudions ton dossier...${LINE_BREAK}Tu recevras un e-mail lorsque ta demande sera validée. En attendant, aide-nous à en savoir plus sur tes pratiques culturelles\u00a0!`
 
   return (
     <GenericInfoPage
