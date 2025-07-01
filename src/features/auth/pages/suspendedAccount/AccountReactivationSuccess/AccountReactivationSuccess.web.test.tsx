cc228686680d59cbc5484b89bfd1c7ca774d91a6
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-07-01 17:02:28 +0200

    test(worksOffline): unit tests should work offline and not perform actual request (#8361)

--- src/features/auth/pages/suspendedAccount/AccountReactivationSuccess/AccountReactivationSuccess.web.test.tsx
+++ src/features/auth/pages/suspendedAccount/AccountReactivationSuccess/AccountReactivationSuccess.web.test.tsx
@@ -4,6 +4,8 @@ import { render, checkAccessibilityFor } from 'tests/utils/web'
 
 import { AccountReactivationSuccess } from './AccountReactivationSuccess'
 
+jest.mock('libs/firebase/analytics/analytics')
+
 describe('<AccountReactivationSuccess/>', () => {
   describe('Accessibility', () => {
     it('should not have basic accessibility issues', async () => {
--- src/features/identityCheck/pages/identification/ubble/ComeBackLater.web.test.tsx
+++ src/features/identityCheck/pages/identification/ubble/ComeBackLater.web.test.tsx
@@ -4,6 +4,8 @@ import { render, checkAccessibilityFor } from 'tests/utils/web'
 
 import { ComeBackLater } from './ComeBackLater'
 
+jest.mock('libs/firebase/analytics/analytics')
+
 describe('<ComeBackLater/>', () => {
   describe('Accessibility', () => {
     it('should not have basic accessibility issues', async () => {
