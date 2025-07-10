07711d4bd7150ae4354493202cf7579b07d9c5e7
Tanguy <47600889+tprache-pass@users.noreply.github.com>
2025-07-10 11:27:16 +0200

    clean: remove unused home entry remote configs

--- src/libs/firebase/remoteConfig/helpers/getRemoteConfigFromConfigValues.ts
+++ src/libs/firebase/remoteConfig/helpers/getRemoteConfigFromConfigValues.ts
@@ -16,20 +16,8 @@ export const getRemoteConfigFromConfigValues = (
   homeEntryIdBeneficiary: getConfigValue(parameters.homeEntryIdBeneficiary).asString(),
   homeEntryIdFreeBeneficiary: getConfigValue(parameters.homeEntryIdFreeBeneficiary).asString(),
   homeEntryIdFreeOffers: getConfigValue(parameters.homeEntryIdFreeOffers).asString(),
-  homeEntryIdNotConnected: getConfigValue(parameters.homeEntryIdNotConnected).asString(),
   homeEntryIdGeneral: getConfigValue(parameters.homeEntryIdGeneral).asString(),
-  homeEntryIdOnboardingGeneral: getConfigValue(parameters.homeEntryIdOnboardingGeneral).asString(),
-  homeEntryIdOnboardingUnderage: getConfigValue(
-    parameters.homeEntryIdOnboardingUnderage
-  ).asString(),
-  homeEntryIdOnboarding_18: getConfigValue(parameters.homeEntryIdOnboarding_18).asString(),
   homeEntryIdWithoutBooking: getConfigValue(parameters.homeEntryIdWithoutBooking).asString(),
-  homeEntryIdWithoutBooking_18: getConfigValue(parameters.homeEntryIdWithoutBooking_18).asString(),
-  homeEntryIdWithoutBooking_15_17: getConfigValue(
-    parameters.homeEntryIdWithoutBooking_15_17
-  ).asString(),
-  homeEntryId_18: getConfigValue(parameters.homeEntryId_18).asString(),
-  homeEntryId_15_17: getConfigValue(parameters.homeEntryId_15_17).asString(),
   reactionFakeDoorCategories: JSON.parse(
     getConfigValue(parameters.reactionFakeDoorCategories).asString()
   ),
