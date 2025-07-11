9523d7344b5f1037a6a2d174ce34e3b416af9599
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-07-11 08:23:40 +0200

    (PC-36947) fix(offer): move setFreeOfferId in useCtaWordingAndAction instead getCtaWordingAndAction" (#8399)
    * (PC-36947) fix(offer): move setFreeOfferId in useCtaWordingAndAction instead getCtaWordingAndAction
    
    * (PC-36947) fix(offer): add background in BottomBanner

--- src/features/offer/helpers/useCtaWordingAndAction/useCtaWordingAndAction.native.test.ts
+++ src/features/offer/helpers/useCtaWordingAndAction/useCtaWordingAndAction.native.test.ts
@@ -12,7 +12,6 @@ import {
 import { ProfileTypes } from 'features/identityCheck/pages/profile/enums'
 import { PlaylistType } from 'features/offer/enums'
 import { offerResponseSnap as baseOffer } from 'features/offer/fixtures/offerResponse'
-import { freeOfferIdActions } from 'features/offer/store/freeOfferIdStore'
 import { beneficiaryUser, nonBeneficiaryUser } from 'fixtures/user'
 import { analytics } from 'libs/analytics/provider'
 import { subcategoriesDataTest } from 'libs/subcategories/fixtures/subcategoriesResponse'
@@ -44,8 +43,6 @@ const defaultParameters = {
 }
 
 jest.mock('libs/firebase/analytics/analytics')
-
-const setFreeOfferIdSpy = jest.spyOn(freeOfferIdActions, 'setFreeOfferId')
 
 describe('getCtaWordingAndAction', () => {
   describe('Logged out user', () => {
@@ -99,7 +96,6 @@ describe('getCtaWordingAndAction', () => {
         wording: 'Réserver l’offre',
         navigateTo: { screen: 'SetName', params: { type: ProfileTypes.BOOKING_FREE_OFFER_15_16 } },
       })
-      expect(setFreeOfferIdSpy).toHaveBeenCalledTimes(1)
     })
 
     it('should display "Réserver l’offre" wording with navigate to ProfileInformationValidation screen and params type', () => {
@@ -125,7 +121,6 @@ describe('getCtaWordingAndAction', () => {
           params: { type: ProfileTypes.BOOKING_FREE_OFFER_15_16 },
         },
       })
-      expect(setFreeOfferIdSpy).toHaveBeenCalledTimes(1)
     })
 
     it('should display "Réserver l’offre" wording and open booking modal when user profile complete', () => {
