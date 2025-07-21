382103fc76ed9ea19ab7667005d573dc9da84967
xlecunff-pass <163895831+xlecunff-pass@users.noreply.github.com>
2025-07-21 16:13:11 +0200

    (PC-36960) chore(deps): bump react-native@0.77 (#8412)
    * feat: bump react-native
    
    * fix(ios): fix AppDelegate.swift errors
    
    ---------
    
    Co-authored-by: Xavier <username@users.noreply.github.com>

--- ios/PassCulture-Bridging-Header.h
+++ ios/PassCulture-Bridging-Header.h
@@ -4,6 +4,9 @@
 #ifndef PassCulture_Bridging_Header_h
 #define PassCulture_Bridging_Header_h
 
-#import "RNSplashScreen.h"
+#import "RNSplashScreen.h" // react-native-lottie-splash-screen
+#import "ReactNativeConfig.h"
+#import "Orientation.h"
+#import "BatchFirebaseDispatcher.h"
 
 #endif
