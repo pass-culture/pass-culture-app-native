ea07b953f21dea560615a73d986ed87c2023e46e
Xavier <username@users.noreply.github.com>
2025-07-22 16:12:49 +0200

    (PC-37169) fix(ios): fix build

---
+++ ios/AppDelegate.mm
@@ -0,0 +1,114 @@
+#import "Orientation.h"
+
+#import "AppDelegate.h"
+
+#import <React/RCTBundleURLProvider.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
+
+
+#import "RNSplashScreen.h" // react-native-lottie-splash-screen
+#import <HotUpdater/HotUpdater.h> // @hot-updater
+#import <RNBatchPush/RNBatch.h>
+#import <Firebase.h>
+#import <React/RCTLinkingManager.h>
+#import "BatchFirebaseDispatcher.h"
+#import "ReactNativeConfig.h"
+
+#import <PassCulture-Swift.h>
+
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.moduleName = @"PassCulture";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.dependencyProvider = [RCTAppDependencyProvider new];
+  self.initialProps = @{};
+
+  // Get environment
+  NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
+  NSDictionary *plistConfig = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+  NSString* Env = [plistConfig valueForKey:@"Env"];
+
+  // Enable Firebase debug view on testing environment
+  if ([Env isEqualToString:@"testing"]) {
+    NSMutableArray *newArguments = [NSMutableArray arrayWithArray:[[NSProcessInfo processInfo] arguments]];
+    [newArguments addObject:@"-FIRAnalyticsDebugEnabled"];
+    [newArguments addObject:@"-FIRDebugEnabled"];
+    [[NSProcessInfo processInfo] setValue:[newArguments copy] forKey:@"arguments"];
+  }
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+
+  // Setup Batch
+  NSString* AssociatedDomain = [ReactNativeConfig envFor:@"WEBAPP_V2_DOMAIN"];
+  [BatchSDK setAssociatedDomains:@[AssociatedDomain]];
+  [BatchEventDispatcher addDispatcher:[BatchFirebaseDispatcher instance]];
+  [RNBatch start];
+  [BatchUNUserNotificationCenterDelegate registerAsDelegate];
+  [BatchUNUserNotificationCenterDelegate sharedInstance].showForegroundNotifications = true;
+
+  // react-native-lottie-splash-screen
+  BOOL success = [super application:application didFinishLaunchingWithOptions:launchOptions];
+
+  if (success) {
+    //We add this logic to get access to rootview
+    UIView *rootView = self.window.rootViewController.view;
+
+    Dynamic *t = [Dynamic new];
+    UIView *animationUIView = (UIView *)[t createAnimationViewWithRootView:rootView lottieName:@"splashscreen"];
+
+    // register LottieSplashScreen to RNSplashScreen
+    [RNSplashScreen showLottieSplash:animationUIView inRootView:rootView];
+    // casting UIView type to AnimationView type
+    AnimationView *animationView = (AnimationView *) animationUIView;
+    // play lottie animation
+    [t playWithAnimationView:animationView];
+    // We want the animation layout to be forced to remove when hide is called, so we use this
+    [RNSplashScreen setAnimationFinished:true];
+  }
+
+  return success;
+}
+
+
+// Deeplink configuration
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+// Handle Universal Links
+- (BOOL)application:(UIApplication *)application
+  continueUserActivity:(nonnull NSUserActivity *)userActivity
+  restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+  return [RCTLinkingManager application:application
+                   continueUserActivity:userActivity
+                     restorationHandler:restorationHandler];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [HotUpdater bundleURL]; // @hot-updater
+#endif
+}
+
+- (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
+  return [Orientation getOrientation];
+}
+
+@end
