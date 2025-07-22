96f5946603ad92a92673eee773bef24b0acf52b2
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-07-22 15:09:26 +0200

    Turn off theme on QpiThanks animation

--- src/features/culturalSurvey/pages/CulturalSurveyThanks.tsx
+++ src/features/culturalSurvey/pages/CulturalSurveyThanks.tsx
@@ -16,6 +16,7 @@ export const CulturalSurveyThanks: React.FC = () => {
   return (
     <GenericInfoPage
       animation={QpiThanks}
+      temporarilyDeactivateColors
       title="Un grand merci pour tes réponses&nbsp;!"
       subtitle={thanks.subtitle}
       buttonPrimary={{
