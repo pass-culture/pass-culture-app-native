26a933920ce0ad932559efe1c1363ea9113aca67
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-07-29 12:12:43 +0200

    Revert "(PC-37211) feat(navigation): remove Firebase Dynamic Links from iOS files (#8475)"
    This reverts commit dbcdd8f2da5cc9d086d71e43cf59a76aca119eed.

--- src/features/navigation/RootNavigator/linking/getInitialUrl.ios.ts
+++ src/features/navigation/RootNavigator/linking/getInitialUrl.ios.ts
@@ -1,7 +1,16 @@
 import { WEBAPP_V2_URL } from 'libs/environment/useWebAppUrl'
+import dynamicLinks from 'libs/firebase/shims/dynamic-links'
 import { BatchPush } from 'libs/react-native-batch'
 
+// Override of default `getInitialURL` of `linking` config required to make
+// firebase dynamic links work on iOS :
+// See: https://medium.com/tribalscale/working-with-react-navigation-v5-firebase-cloud-messaging-and-firebase-dynamic-links-f8580c3b83ad
 export async function getInitialURL(): Promise<string> {
+  const dynamicLinkUrl = await dynamicLinks().getInitialLink()
+  if (dynamicLinkUrl) {
+    return dynamicLinkUrl.url
+  }
+
   const url = await BatchPush.getInitialURL()
   if (url) {
     return url
---
+++ src/libs/firebase/shims/dynamic-links/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-restricted-imports */
+export type { FirebaseDynamicLinksTypes } from '@react-native-firebase/dynamic-links'
+export { default } from '@react-native-firebase/dynamic-links'
+/* eslint-enable no-restricted-imports */
