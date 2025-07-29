5c70e71b9ba443e259138232529526580e117644
Axel <153505101+arenault-pass@users.noreply.github.com>
2025-07-29 18:44:31 +0200

    (PC-37232) fix(e2e): replace FDL by custom links in tests

--- .maestro/tests/subFolder/links/DeepLinks.js
+++ .maestro/tests/subFolder/links/DeepLinks.js
@@ -1,11 +1,24 @@
 output.links = {
-    staging: {
-        home: "https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Faccueil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1",
-        profile: "https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Fprofil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1"
+    Android: {
+        staging: {
+            home: "passculture-staging://accueil",
+            profile: "passculture-staging://profil"
+        },
+        production: {
+            searchResult: "https://passculture.app/recherche/resultats?locationFilter=%7B%22locationType%22%3A%22EVERYWHERE%22%7D&from=deeplink&tags=%5B%22optinlecture_focusjapon%22%5D&utm_campaign=test&utm_source=test&utm_medium=test",
+            venue: "https://passculture.app/lieu/29732?from=deeplink&utm_campaign=test&utm_source=test&utm_medium=test",
+            video: "https://passculture.app/accueil-thematique?from=deeplink&homeId=2oJB7P984lwH2sFjjMCcp8&videoModuleId=62e8Zwo7Mo8PNwz6RAUuqN&utm_campaign=test&utm_source=test&utm_medium=test"
+        }
     },
-    production: {
-        searchResult: "https://passculture.app/recherche/resultats?locationFilter=%7B%22locationType%22%3A%22EVERYWHERE%22%7D&from=deeplink&tags=%5B%22optinlecture_focusjapon%22%5D&utm_campaign=test&utm_source=test&utm_medium=test",
-        venue: "https://passculture.app/lieu/29732?from=deeplink&utm_campaign=test&utm_source=test&utm_medium=test",
-        video: "https://passculture.app/accueil-thematique?from=deeplink&homeId=2oJB7P984lwH2sFjjMCcp8&videoModuleId=62e8Zwo7Mo8PNwz6RAUuqN&utm_campaign=test&utm_source=test&utm_medium=test"
+    iOS: {
+        staging: {
+            home: "https://app.staging.passculture.team/accueil",
+            profile: "https://app.staging.passculture.team/profil"
+        },
+        production: {
+            searchResult: "https://passculture.app/recherche/resultats?locationFilter=%7B%22locationType%22%3A%22EVERYWHERE%22%7D&from=deeplink&tags=%5B%22optinlecture_focusjapon%22%5D&utm_campaign=test&utm_source=test&utm_medium=test",
+            venue: "https://passculture.app/lieu/29732?from=deeplink&utm_campaign=test&utm_source=test&utm_medium=test",
+            video: "https://passculture.app/accueil-thematique?from=deeplink&homeId=2oJB7P984lwH2sFjjMCcp8&videoModuleId=62e8Zwo7Mo8PNwz6RAUuqN&utm_campaign=test&utm_source=test&utm_medium=test"
+        }
     }
 }
--- src/features/navigation/RootNavigator/linking/index.ts
+++ src/features/navigation/RootNavigator/linking/index.ts
@@ -11,16 +11,23 @@ import { customGetStateFromPath } from './getStateFromPath'
 import { subscribe } from './subscribe'
 
 const PASS_CULTURE_PREFIX_URL = 'passculture://'
+const PASS_CULTURE_STAGING_PREFIX_URL = 'passculture-staging://'
+
+const prefixes = [
+  // must NOT be empty
+  WEBAPP_V2_URL,
+  PASS_CULTURE_PREFIX_URL,
+]
+
+if (process.env.APP_ENV === 'staging') {
+  prefixes.push(PASS_CULTURE_STAGING_PREFIX_URL)
+}
 
 export const linking: RequireField<
   LinkingOptions<RootStackParamList>,
   'getStateFromPath' | 'getPathFromState'
 > = {
-  prefixes: [
-    // must NOT be empty
-    WEBAPP_V2_URL,
-    PASS_CULTURE_PREFIX_URL,
-  ],
+  prefixes,
   getInitialURL: getInitialURL,
   subscribe: subscribe,
   getStateFromPath: customGetStateFromPath,
