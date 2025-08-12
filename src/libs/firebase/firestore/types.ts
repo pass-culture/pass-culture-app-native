213629b593e2b9d94cd0dea3640a016ed8c59823
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-08-12 10:53:53 +0200

    (PC-36075) feat(search): add wipArtistsSuggestionsInSearch FF

--- src/libs/firebase/firestore/types.ts
+++ src/libs/firebase/firestore/types.ts
@@ -56,6 +56,7 @@ export enum RemoteStoreFeatureFlags {
   TARGET_XP_CINE_FROM_OFFER = 'targetXpCineFromOffer',
   WIP_ARTIST_PAGE = 'wipArtistPage',
   WIP_ARTIST_PAGE_IN_SEARCH = 'wipArtistPageInSearch',
+  WIP_ARTISTS_SUGGESTIONS_IN_SEARCH = 'wipArtistsSuggestionsInSearch',
   WIP_DISABLE_STORE_REVIEW = 'wipDisabledStoreReview',
   WIP_DISPLAY_SEARCH_NB_FACET_RESULTS = 'wipDisplaySearchNbFacetResults',
   WIP_ENABLE_ACCES_LIBRE = 'wipEnableAccesLibre',
