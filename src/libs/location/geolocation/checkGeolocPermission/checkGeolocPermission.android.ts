5485d9773204b4964fbe48905c6c72f75ad2aadc
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-08-13 15:16:37 +0200

    (PC-37217) feat(location): use approximative geolocation for Android

--- src/libs/location/geolocation/checkGeolocPermission/checkGeolocPermission.android.ts
+++ src/libs/location/geolocation/checkGeolocPermission/checkGeolocPermission.android.ts
@@ -1,18 +1,12 @@
-import { checkMultiple, PERMISSIONS } from 'react-native-permissions'
+import { check, PERMISSIONS } from 'react-native-permissions'
 
-import { GeolocPermissionState } from 'libs/location/geolocation/enums'
 import { ReadGeolocPermission } from 'libs/location/types'
 
+import { GeolocPermissionState } from '../enums'
+
 export const checkGeolocPermission: ReadGeolocPermission = async () => {
-  const permissions = await checkMultiple([
-    PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-    PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
-  ])
-  const finePermission = permissions[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION]
-  const coarsePermission = permissions[PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION]
-  if (finePermission === 'granted' || coarsePermission === 'granted')
-    return GeolocPermissionState.GRANTED
-  // on Android, we only have access to information actived: yes/no
-  // in that case 'no', we want to display our custom modale, so we return NEVER_ASK_AGAIN
-  return GeolocPermissionState.NEVER_ASK_AGAIN
+  const status = await check(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION)
+  return status === 'granted'
+    ? GeolocPermissionState.GRANTED
+    : GeolocPermissionState.NEVER_ASK_AGAIN
 }
--- src/libs/location/geolocation/requestGeolocPermission/requestGeolocPermission.android.test.ts
+++ src/libs/location/geolocation/requestGeolocPermission/requestGeolocPermission.android.test.ts
@@ -1,34 +1,27 @@
-import { Permission, PermissionsAndroid, PermissionStatus, Platform } from 'react-native'
+import { PermissionsAndroid, Platform } from 'react-native'
 
 import { GeolocPermissionState } from '../enums'
 
 import { requestGeolocPermission } from './requestGeolocPermission.android'
 
+const requestSpy = jest.spyOn(PermissionsAndroid, 'request')
+
 describe('requestGeolocPermission android', () => {
   beforeAll(() => (Platform.OS = 'android'))
 
   it('should ask for android permission and return right state if granted', async () => {
-    // eslint-disable-next-line local-rules/independent-mocks
-    jest.spyOn(PermissionsAndroid, 'requestMultiple').mockResolvedValue({
-      'android.permission.ACCESS_FINE_LOCATION': 'granted',
-      'android.permission.ACCESS_COARSE_LOCATION': 'granted',
-    } as { [key in Permission]: PermissionStatus })
+    requestSpy.mockResolvedValueOnce(PermissionsAndroid.RESULTS.GRANTED)
 
     const permissionState = await requestGeolocPermission()
 
-    expect(PermissionsAndroid.requestMultiple).toHaveBeenCalledWith([
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
-    ])
+    expect(PermissionsAndroid.request).toHaveBeenCalledWith(
+      PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
+    )
     expect(permissionState).toEqual(GeolocPermissionState.GRANTED)
   })
 
   it('should return right state if permission not granted', async () => {
-    // eslint-disable-next-line local-rules/independent-mocks
-    jest.spyOn(PermissionsAndroid, 'requestMultiple').mockResolvedValue({
-      'android.permission.ACCESS_FINE_LOCATION': 'denied',
-      'android.permission.ACCESS_COARSE_LOCATION': 'denied',
-    } as { [key in Permission]: PermissionStatus })
+    requestSpy.mockResolvedValueOnce(PermissionsAndroid.RESULTS.DENIED)
 
     const permissionState = await requestGeolocPermission()
 
@@ -36,11 +29,7 @@ describe('requestGeolocPermission android', () => {
   })
 
   it('should return right state if permission not granted and ask for never_ask_again', async () => {
-    // eslint-disable-next-line local-rules/independent-mocks
-    jest.spyOn(PermissionsAndroid, 'requestMultiple').mockResolvedValue({
-      'android.permission.ACCESS_FINE_LOCATION': 'never_ask_again',
-      'android.permission.ACCESS_COARSE_LOCATION': 'never_ask_again',
-    } as { [key in Permission]: PermissionStatus })
+    requestSpy.mockResolvedValueOnce(PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN)
 
     const permissionState = await requestGeolocPermission()
 
