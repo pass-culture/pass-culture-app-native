37470f8cb79ff7f8ffa9057f005c3f6fedf809eb
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-08-14 16:30:08 +0200

    (PC-37385) feat(profile): udpate MandatoryUpdatePersonalData informations

--- src/features/profile/pages/MandatoryUpdatePersonalData/MandatoryUpdatePersonalData.tsx
+++ src/features/profile/pages/MandatoryUpdatePersonalData/MandatoryUpdatePersonalData.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 
 import { getProfilePropConfig } from 'features/navigation/ProfileStackNavigator/getProfilePropConfig'
+import { env } from 'libs/environment/env'
+import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
+import { ExternalTouchableLink } from 'ui/components/touchableLink/ExternalTouchableLink'
 import { GenericInfoPage } from 'ui/pages/GenericInfoPage'
+import { ExternalSiteFilled } from 'ui/svg/icons/ExternalSiteFilled'
 import { UserError } from 'ui/svg/UserError'
+import { LINE_BREAK } from 'ui/theme/constants'
 
 export const MandatoryUpdatePersonalData = () => (
   <GenericInfoPage
     illustration={UserError}
-    title="Une petite mise à jour de tes informations personnelles&nbsp;?"
-    subtitle="Avant de continuer, assure-toi que tes informations personnelles sont toujours correctes. C’est rapide, promis."
+    title="Mets à jour ton profil"
+    subtitle={`Pour mieux t’accompagner, on a besoin de vérifier que tes informations personnelles sont toujours à jour.${LINE_BREAK}C’est rapide, promis.`}
     buttonPrimary={{
       wording: 'Commencer',
       navigateTo: getProfilePropConfig('ProfileInformationValidationUpdate'),
-    }}
-  />
+    }}>
+    <ExternalTouchableLink
+      as={ButtonTertiaryBlack}
+      wording="Charte des données personnelles"
+      externalNav={{ url: env.PRIVACY_POLICY_LINK }}
+      icon={ExternalSiteFilled}
+      numberOfLines={2}
+    />
+  </GenericInfoPage>
 )
