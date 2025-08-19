9cb61a773d211c7b357cb46682b9621ba4773174
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-08-19 15:15:19 +0200

    fix lint

--- src/ui/components/headers/BackButton.tsx
+++ src/ui/components/headers/BackButton.tsx
@@ -1,4 +1,4 @@
-import { useNavigation } from '@react-navigation/core'
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import styled from 'styled-components/native'
 
