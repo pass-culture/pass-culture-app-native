2d1924a715a24b2ddcc104f809713f71a787f043
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2025-08-19 15:40:42 +0200

    mock query

---
+++ src/features/search/pages/NearMePage/fixtures/offerNearMeHits.ts
@@ -0,0 +1,305 @@
+export const offerNearMeHits = [
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      name: 'A Made In France Selection By Sofiane Pamart',
+      prices: [
+        {
+          source: '0.0',
+          parsedValue: 0,
+        },
+      ],
+      subcategoryId: 'TELECHARGEMENT_MUSIQUE',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/BJ688',
+    },
+    venue: {
+      banner_url:
+        'https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/assets/venue_default_images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.png',
+      id: 49204,
+      isPermanent: false,
+      name: 'Offre numérique',
+      venue_type: 'DIGITAL',
+    },
+    _geoloc: {
+      lat: 47.158459,
+      lng: 2.409289,
+    },
+    objectID: '81768105',
+  },
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      name: 'Cérémonie d’ouverture du Festival de Cannes',
+      prices: [
+        {
+          source: '0.0',
+          parsedValue: 0,
+        },
+      ],
+      subcategoryId: 'VOD',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/36M76',
+    },
+    venue: {
+      address: '136 Rue Ernest Renan',
+      city: 'Bordeaux',
+      departmentCode: '33',
+      id: 118989,
+      isPermanent: true,
+      name: 'France 3 Aquitaine',
+      postalCode: '33000',
+      publicName: 'France 3 Aquitaine',
+      venue_type: 'OTHER',
+    },
+    _geoloc: {
+      lat: 44.85155,
+      lng: -0.59213,
+    },
+    objectID: '317604030',
+  },
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      name: 'Deezer Premium / 3 mois',
+      prices: [17.97],
+      subcategoryId: 'TELECHARGEMENT_MUSIQUE',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/TA4HA',
+    },
+    venue: {
+      address: '26 Rue de Calais',
+      city: 'Paris',
+      departmentCode: '75',
+      id: 423,
+      isPermanent: true,
+      name: 'DEEZER',
+      postalCode: '75009',
+      venue_type: 'OTHER',
+    },
+    _geoloc: {
+      lat: 48.88282,
+      lng: 2.33015,
+    },
+    objectID: '1115',
+  },
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      name: 'Vidéo du président du Prix Joséphine des artistes, Eddy de Pretto',
+      prices: [
+        {
+          source: '0.0',
+          parsedValue: 0,
+        },
+      ],
+      subcategoryId: 'AUTRE_SUPPORT_NUMERIQUE',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/BS8YY',
+    },
+    venue: {
+      banner_url:
+        'https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/assets/venue_default_images/rodion-kutsaiev-0VGG7cqTwCo-unsplash.png',
+      id: 5855,
+      isPermanent: false,
+      name: 'Offre numérique',
+      venue_type: 'DIGITAL',
+    },
+    _geoloc: {
+      lat: 47.158459,
+      lng: 2.409289,
+    },
+    objectID: '94662034',
+  },
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      name: 'Le 1 hebdo - Offre découverte 1 mois ',
+      prices: [0.99],
+      subcategoryId: 'ABO_PRESSE_EN_LIGNE',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/BRWXK',
+    },
+    venue: {
+      banner_url:
+        'https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/assets/venue_default_images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.png',
+      id: 4628,
+      isPermanent: false,
+      name: 'Offre numérique',
+      venue_type: 'DIGITAL',
+    },
+    _geoloc: {
+      lat: 47.158459,
+      lng: 2.409289,
+    },
+    objectID: '93722598',
+  },
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      likes: 0,
+      name: '"Elles" les femmes à l’honneur',
+      prices: [
+        {
+          source: '0.0',
+          parsedValue: 0,
+        },
+      ],
+      subcategoryId: 'TELECHARGEMENT_MUSIQUE',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/BMED9',
+    },
+    venue: {
+      banner_url:
+        'https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/assets/venue_default_images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.png',
+      id: 49204,
+      isPermanent: false,
+      name: 'Offre numérique',
+      venue_type: 'DIGITAL',
+    },
+    _geoloc: {
+      lat: 47.158459,
+      lng: 2.409289,
+    },
+    objectID: '85095585',
+  },
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      likes: 0,
+      name: 'A Made In France Selection By Voyou',
+      prices: [
+        {
+          source: '0.0',
+          parsedValue: 0,
+        },
+      ],
+      subcategoryId: 'TELECHARGEMENT_MUSIQUE',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/A6ZZ8',
+    },
+    venue: {
+      banner_url:
+        'https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/assets/venue_default_images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.png',
+      id: 49204,
+      isPermanent: false,
+      name: 'Offre numérique',
+      venue_type: 'DIGITAL',
+    },
+    _geoloc: {
+      lat: 47.158459,
+      lng: 2.409289,
+    },
+    objectID: '65149056',
+  },
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      name: 'YouScribe Premium / 1 mois (livres, BD, mangas...)',
+      prices: [7.99],
+      subcategoryId: 'LIVRE_NUMERIQUE',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/BR7E4',
+    },
+    venue: {
+      banner_url:
+        'https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/assets/venue_default_images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.png',
+      id: 4968,
+      isPermanent: false,
+      name: 'Offre numérique',
+      venue_type: 'DIGITAL',
+    },
+    _geoloc: {
+      lat: 47.158459,
+      lng: 2.409289,
+    },
+    objectID: '33909960',
+  },
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      name: 'Esclavage et cinéma 🎞️',
+      prices: [
+        {
+          source: '0.0',
+          parsedValue: 0,
+        },
+      ],
+      subcategoryId: 'AUTRE_SUPPORT_NUMERIQUE',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/3RFGM',
+    },
+    venue: {
+      address: '87 Rue la Boétie',
+      banner_url:
+        'https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/assets/venue_default_images/krists-luhaers-AtPWnYNDJnM-unsplash.png',
+      city: 'Paris',
+      departmentCode: '75',
+      id: 94207,
+      isPermanent: false,
+      name: 'PASS CULTURE',
+      postalCode: '75008',
+      publicName: '',
+      venue_type: 'MOVIE',
+    },
+    _geoloc: {
+      lat: 48.87179,
+      lng: 2.30848,
+    },
+    objectID: '316104623',
+  },
+  {
+    offer: {
+      dates: [],
+      isDigital: true,
+      isDuo: false,
+      isEducational: false,
+      likes: 0,
+      name: 'Playlists musicales 🎵',
+      prices: [
+        {
+          source: '0.0',
+          parsedValue: 0,
+        },
+      ],
+      subcategoryId: 'APP_CULTURELLE',
+      thumbUrl: '/passculture-metier-prod-production-assets-fine-grained/thumbs/mediations/3RFCU',
+    },
+    venue: {
+      address: '20b Rue du Général de Gaulle',
+      banner_url:
+        'https://storage.googleapis.com/passculture-metier-prod-production-assets-fine-grained/assets/venue_default_images/jan-antonin-kolar-hN_zCni3ILg-unsplash_1.png',
+      city: 'Boulay-Moselle',
+      departmentCode: '57',
+      id: 120590,
+      isPermanent: false,
+      name: 'PASS CULTURE',
+      postalCode: '57220',
+      publicName: '',
+      venue_type: 'CULTURAL_CENTRE',
+    },
+    _geoloc: {
+      lat: 49.18351,
+      lng: 6.49097,
+    },
+    objectID: '316103939',
+  },
+] as const
