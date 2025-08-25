858814bda200ef96b63f9738b258ec3962f497c9
Bruno Peyrou <bruno.peyrou@passculture.app>
2025-08-25 16:34:05 +0200

    (PC-37474) feat(OfferAbout): replace CollapsibleText for cine offer description

--- src/features/offer/components/OfferAbout/OfferAbout.tsx
+++ src/features/offer/components/OfferAbout/OfferAbout.tsx
@@ -1,13 +1,17 @@
-import React, { FunctionComponent } from 'react'
-import { View } from 'react-native'
+import React, { FunctionComponent, useRef, useState } from 'react'
+import { ScrollView, View } from 'react-native'
+import styled from 'styled-components/native'
 
 import { OfferResponseV2 } from 'api/gen'
 import { OfferAccessibility } from 'features/offer/components/OfferAccessibility/OfferAccessibility'
 import { OfferMetadataItemProps } from 'features/offer/components/OfferMetadataItem/OfferMetadataItem'
 import { OfferMetadataList } from 'features/offer/components/OfferMetadataList/OfferMetadataList'
-import { CollapsibleText } from 'ui/components/CollapsibleText/CollapsibleText'
+import { ButtonTertiaryBlack } from 'ui/components/buttons/ButtonTertiaryBlack'
+import { styledButton } from 'ui/components/buttons/styledButton'
 import { Markdown } from 'ui/components/Markdown/Markdown'
 import { ViewGap } from 'ui/components/ViewGap/ViewGap'
+import { ArrowDown } from 'ui/svg/icons/ArrowDown'
+import { ArrowUp } from 'ui/svg/icons/ArrowUp'
 import { Typo } from 'ui/theme'
 import { getHeadingAttrs } from 'ui/theme/typographyAttrs/getHeadingAttrs'
 
@@ -20,12 +24,23 @@ type Props = {
 
 const NUMBER_OF_LINES_OF_DESCRIPTION_SECTION = 5
 
+const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
+  return layoutMeasurement.height + contentOffset.y >= contentSize.height
+}
+
+const isCloseToTop = ({ contentOffset }) => {
+  return contentOffset.y < 1
+}
+
 export const OfferAbout: FunctionComponent<Props> = ({
   offer,
   metadata,
   hasMetadata,
   shouldDisplayAccessibilitySection,
 }) => {
+  const scrollViewRef = useRef<ScrollView>(null)
+  const [scrollOffset, setScrollOffset] = useState(0)
+  const [seeButtonMode, setSeeButtonMode] = useState('down')
   return (
     <ViewGap gap={2}>
       <Typo.Title3 {...getHeadingAttrs(2)}>À propos</Typo.Title3>
@@ -36,9 +51,41 @@ export const OfferAbout: FunctionComponent<Props> = ({
           {offer.description ? (
             <View>
               <Typo.BodyAccent>Description&nbsp;:</Typo.BodyAccent>
-              <CollapsibleText numberOfLines={NUMBER_OF_LINES_OF_DESCRIPTION_SECTION}>
+              {/* <CollapsibleText numberOfLines={NUMBER_OF_LINES_OF_DESCRIPTION_SECTION}>
                 <Markdown>{offer.description}</Markdown>
-              </CollapsibleText>
+              </CollapsibleText> */}
+              <ScrollView
+                style={{ height: '120px', overflow: 'scroll' }}
+                showsHorizontalScrollIndicator={false}
+                ref={scrollViewRef}
+                onScroll={({ nativeEvent }) => {
+                  if (isCloseToBottom(nativeEvent)) setSeeButtonMode('up')
+                  if (isCloseToTop(nativeEvent)) setSeeButtonMode('down')
+                  const { contentOffset, contentSize, layoutMeasurement } = nativeEvent
+                  setScrollOffset(
+                    contentOffset.y < contentSize.height - layoutMeasurement.height
+                      ? contentOffset.y
+                      : contentSize.height - layoutMeasurement.height
+                  )
+                }}
+                scrollEventThrottle={400}
+                fadingEdgeLength={100}>
+                <Markdown>{offer.description}</Markdown>
+              </ScrollView>
+              <ButtonContainer>
+                <SeeMoreButton
+                  wording={seeButtonMode === 'down' ? 'Voir plus' : 'Haut'}
+                  onPress={() => {
+                    scrollViewRef.current?.scrollTo({
+                      x: 0,
+                      y: seeButtonMode === 'down' ? scrollOffset + 40 : scrollOffset - 40,
+                    })
+                  }}
+                  accessibilityLabel="Faire défiler le texte"
+                  icon={seeButtonMode === 'down' ? ArrowDown : ArrowUp}
+                  buttonHeight="extraSmall"
+                />
+              </ButtonContainer>
             </View>
           ) : null}
           {shouldDisplayAccessibilitySection ? (
@@ -49,3 +96,12 @@ export const OfferAbout: FunctionComponent<Props> = ({
     </ViewGap>
   )
 }
+
+const ButtonContainer = styled.View({
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+})
+
+const SeeMoreButton = styledButton(ButtonTertiaryBlack)({
+  maxWidth: 120,
+})
