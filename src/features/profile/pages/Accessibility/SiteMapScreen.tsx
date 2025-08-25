a3bc0f008c85e6d44818f7ed16ecdb0db2665f58
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-08-25 11:31:32 +0200

    (PC-37562) fix(SiteMap): Use AppButton instead of ButtonInsideText (#8567)
    * Use AppButton instead of ButtonInsideText
    
    * update snap
    
    * fix import

--- src/features/profile/pages/Accessibility/SiteMapScreen.tsx
+++ src/features/profile/pages/Accessibility/SiteMapScreen.tsx
@@ -7,10 +7,13 @@ import { useGoBack } from 'features/navigation/useGoBack'
 import { getSiteMapLinks } from 'features/profile/helpers/getSiteMapLinks'
 import { useSortedSearchCategories } from 'features/search/helpers/useSortedSearchCategories/useSortedSearchCategories'
 import { BulletListItem } from 'ui/components/BulletListItem'
-import { ButtonInsideText } from 'ui/components/buttons/buttonInsideText/ButtonInsideText'
+import { AppButton } from 'ui/components/buttons/AppButton/AppButton'
+import { BaseButtonProps } from 'ui/components/buttons/AppButton/types'
+import { styledButton } from 'ui/components/buttons/styledButton'
 import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
 import { VerticalUl } from 'ui/components/Ul'
 import { SecondaryPageWithBlurHeader } from 'ui/pages/SecondaryPageWithBlurHeader'
+import { Typo } from 'ui/theme'
 
 export function SiteMapScreen() {
   const { goBack } = useGoBack(...getProfileHookConfig('Accessibility'))
@@ -29,16 +32,12 @@ export function SiteMapScreen() {
               <BulletListItem
                 key={wording}
                 text={
-                  <InternalTouchableLink
-                    as={ButtonInsideTextBlack}
-                    wording={wording}
-                    navigateTo={navigateTo}
-                  />
+                  <InternalTouchableLink as={Button} wording={wording} navigateTo={navigateTo} />
                 }
                 nestedListTexts={filteredSubPages.map(({ wording, navigateTo }) => (
                   <InternalTouchableLink
                     key={wording}
-                    as={ButtonInsideTextBlack}
+                    as={Button}
                     typography="BodyAccentXs"
                     wording={wording}
                     navigateTo={navigateTo}
@@ -52,6 +51,14 @@ export function SiteMapScreen() {
   )
 }
 
-const ButtonInsideTextBlack = styled(ButtonInsideText).attrs(({ theme }) => ({
-  buttonColor: theme.designSystem.color.text.default,
-}))``
+const Button = styledButton(AppButton).attrs<BaseButtonProps>(({ theme }) => {
+  const Title = styled(Typo.Button)({
+    color: theme.designSystem.color.text.default,
+  })
+
+  return {
+    title: Title,
+    justifyContent: 'flex-start',
+    hoverUnderlineColor: theme.designSystem.color.text.default,
+  }
+})``
