6f7ce78e311c1bd58f5d17cc118d06f3ca8d3252
Axel <153505101+arenault-pass@users.noreply.github.com>
2025-08-26 18:18:58 +0200

    (PC-37590) fix(e2e): replace FDL by universal link on ios

--- .maestro/tests/subFolder/links/DeepLinks.js
+++ .maestro/tests/subFolder/links/DeepLinks.js
@@ -1,11 +1,12 @@
 output.links = {
-    staging: {
-        home: "https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Faccueil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1",
-        profile: "https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Fprofil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1"
+  staging: {
+    android: {
+      home: 'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Faccueil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
+      profile: 'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Fprofil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
     },
-    production: {
-        searchResult: "https://passculture.app/recherche/resultats?locationFilter=%7B%22locationType%22%3A%22EVERYWHERE%22%7D&from=deeplink&tags=%5B%22optinlecture_focusjapon%22%5D&utm_campaign=test&utm_source=test&utm_medium=test",
-        venue: "https://passculture.app/lieu/29732?from=deeplink&utm_campaign=test&utm_source=test&utm_medium=test",
-        video: "https://passculture.app/accueil-thematique?from=deeplink&homeId=2oJB7P984lwH2sFjjMCcp8&videoModuleId=62e8Zwo7Mo8PNwz6RAUuqN&utm_campaign=test&utm_source=test&utm_medium=test"
-    }
+    ios: {
+      home: 'https://app.staging.passculture.team/accueil?from=deeplink&utm_gen=marketing',
+      profile: 'https://app.staging.passculture.team/profil?from=deeplink&utm_gen=marketing',
+    },
+  },
 }
