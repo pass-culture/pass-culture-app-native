c6a048fef052b1f635d1aecb61b4415c4561bea7
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-08-28 09:27:29 +0200

    (PC-37623) test(venueMap): fix last tests

--- src/features/venueMap/components/VenueMapView/VenueMapView.android.test.tsx
+++ src/features/venueMap/components/VenueMapView/VenueMapView.android.test.tsx
@@ -1,7 +1,6 @@
 import { Platform } from 'react-native'
 
 Platform.OS = 'android'
-jest.mock('./Marker/Marker', () => require('./Marker/Marker.android'))
 jest.mock('../VenueMapLabel/LabelContainer', () =>
   require('../VenueMapLabel/LabelContainer.android')
 )
