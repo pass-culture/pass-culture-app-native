5068429737e6785d3d3cf8f55936edc738b8060e
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-09-04 17:37:57 +0200

    (BSR) feat(maestro): testing deeplink (#8629)

--- .maestro/tests/subFolder/links/DeepLinks.js
+++ .maestro/tests/subFolder/links/DeepLinks.js
@@ -2,11 +2,23 @@ output.links = {
   staging: {
     android: {
       home: 'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Faccueil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
-      profile: 'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Fprofil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
+      profile:
+        'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Fprofil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
     },
     ios: {
       home: 'https://app.staging.passculture.team/accueil?from=deeplink&utm_gen=marketing',
       profile: 'https://app.staging.passculture.team/profil?from=deeplink&utm_gen=marketing',
+    },
+  },
+  testing: {
+    android: {
+      home: 'https://passcultureapptesting.page.link/?link=https%3A%2F%2Fapp.testing.passculture.team%2Faccueil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.testing&isi=1557887412&ibi=app.passculture.testing&efr=1',
+      profile:
+        'https://passcultureapptesting.page.link/?link=https%3A%2F%2Fapp.testing.passculture.team%2Fprofil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.testing&isi=1557887412&ibi=app.passculture.testing&efr=1',
+    },
+    ios: {
+      home: 'https://app.testing.passculture.team/accueil?from=deeplink&utm_gen=marketing',
+      profile: 'https://app.testing.passculture.team/profil?from=deeplink&utm_gen=marketing',
     },
   },
 }
