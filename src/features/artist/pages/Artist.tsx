446ff087aad10edd0104ed7a9e09875589605b3f
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-09-04 09:47:21 +0200

    (PC-37506) feat(app): extract hook to init ViewItem tracking to avoid code duplication

--- src/features/artist/pages/Artist.tsx
+++ src/features/artist/pages/Artist.tsx
@@ -1,58 +1,25 @@
-import { useFocusEffect, useRoute } from '@react-navigation/native'
-import React, { FunctionComponent, useCallback, useEffect } from 'react'
+import { useRoute } from '@react-navigation/native'
+import React, { FunctionComponent } from 'react'
 import { ViewToken } from 'react-native'
 
 import { ArtistBody } from 'features/artist/components/ArtistBody/ArtistBody'
 import { useArtistQuery } from 'features/artist/queries/useArtistQuery'
 import { UseRouteType } from 'features/navigation/RootNavigator/types'
-import { analytics } from 'libs/analytics/provider'
-import { useAppStateChange } from 'libs/appState'
 import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 import { useArtistResultsQuery } from 'queries/offer/useArtistResultsQuery'
-import { logViewItem, setViewOfferTrackingFn } from 'shared/analytics/logViewItem'
-import {
-  resetPageTrackingInfo,
-  setPageTrackingInfo,
-  setPlaylistTrackingInfo,
-  useOfferPlaylistTrackingStore,
-} from 'store/tracking/playlistTrackingStore'
+import { useViewItemTracking } from 'shared/hook/useViewItemTracking'
+import { setPlaylistTrackingInfo } from 'store/tracking/playlistTrackingStore'
 
 export const Artist: FunctionComponent = () => {
   const enableArtistPage = useFeatureFlag(RemoteStoreFeatureFlags.WIP_ARTIST_PAGE)
   const { params, name } = useRoute<UseRouteType<'Artist'>>()
+  useViewItemTracking(name)
 
   const { artistPlaylist, artistTopOffers } = useArtistResultsQuery({
     artistId: params.id,
   })
   const { data: artist } = useArtistQuery(params.id)
-
-  useEffect(() => {
-    setViewOfferTrackingFn(analytics.logViewItem)
-  }, [])
-
-  useFocusEffect(
-    useCallback(() => {
-      if (!name) {
-        return
-      }
-      setPageTrackingInfo({
-        pageId: '',
-        pageLocation: name,
-      })
-    }, [name])
-  )
-
-  useAppStateChange(undefined, () => logViewItem(useOfferPlaylistTrackingStore.getState()))
-
-  useFocusEffect(
-    useCallback(() => {
-      return () => {
-        logViewItem(useOfferPlaylistTrackingStore.getState())
-        resetPageTrackingInfo()
-      }
-    }, [])
-  )
 
   const handleViewableItemsChanged = (items: Pick<ViewToken, 'key' | 'index'>[]) => {
     setPlaylistTrackingInfo({
