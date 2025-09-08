fa6eddedb5b5f0898f5c4f016f53d5033ae6163f
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2025-09-08 16:42:40 +0200

    (PC-37506) refactor(venue): add dependances useCallback

--- src/features/venue/components/VenueOffers/VenueOffersList.tsx
+++ src/features/venue/components/VenueOffers/VenueOffersList.tsx
@@ -140,6 +140,13 @@ export const VenueOffersList: FunctionComponent<VenueOffersListProps> = ({
     [onViewableItemsChanged]
   )
 
+  const handleGtlViewableItemsChanged = useCallback(
+    (playlistTitle: string) => (items: Pick<ViewToken, 'key' | 'index'>[]) => {
+      onViewableItemsChanged(items, playlistTitle, 'offer')
+    },
+    [onViewableItemsChanged]
+  )
+
   return (
     <Container>
       <ObservedPlaylist onViewableItemsChanged={handleAllOffersViewableItemsChanged}>
@@ -180,9 +187,7 @@ export const VenueOffersList: FunctionComponent<VenueOffersListProps> = ({
         ? playlists.map((playlist) => (
             <ObservedPlaylist
               key={playlist.entryId}
-              onViewableItemsChanged={(items) =>
-                onViewableItemsChanged(items, playlist.title, 'offer')
-              }>
+              onViewableItemsChanged={handleGtlViewableItemsChanged(playlist.title)}>
               {({ listRef, handleViewableItemsChanged }) => (
                 <GtlPlaylist
                   venue={venue}
