85c6f0131c0652262a74de41e331d2d67353d4e0
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-09-09 15:34:39 +0200

    (PC-37751) feat(NC): update useGetCurrencyToDisplay with params check

--- src/shared/currency/useGetCurrencyToDisplay.ts
+++ src/shared/currency/useGetCurrencyToDisplay.ts
@@ -5,6 +5,7 @@ import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureF
 import { RemoteStoreFeatureFlags as featureFlags } from 'libs/firebase/firestore/types'
 import { useLocation } from 'libs/location/location'
 import { LocationMode } from 'libs/location/types'
+import { getCurrencyFromParam } from 'shared/currency/useCurrencyParam'
 
 export enum Currency {
   EURO = '€',
@@ -21,24 +22,29 @@ export const useGetCurrencyToDisplay = (
   const disablePacificFrancCurrency = !enablePacificFrancCurrency
 
   const { user } = useAuthContext()
-  const isUserRegisteredInEuroRegion = user?.currency === CurrencyEnum.EUR
-  const isUserRegisteredInPacificFrancRegion = user?.currency === CurrencyEnum.XPF
-
   const { selectedPlace, selectedLocationMode } = useLocation()
-  const isNewCaledonianLocationSelected = selectedPlace?.info === 'Nouvelle-Calédonie'
-  const isNotNewCaledonianLocationSelected = selectedPlace?.info !== 'Nouvelle-Calédonie'
-  const isEverywhereLocationMode = selectedLocationMode === LocationMode.EVERYWHERE
 
   const pacificFrancCurrency =
     displayFormat === 'full' ? Currency.PACIFIC_FRANC_FULL : Currency.PACIFIC_FRANC_SHORT
 
+  const currencyParam = getCurrencyFromParam()
+  if (currencyParam === CurrencyEnum.EUR) return Currency.EURO
+  if (currencyParam === CurrencyEnum.XPF) return pacificFrancCurrency
+
   if (disablePacificFrancCurrency) return Currency.EURO
+
+  const isEverywhereLocationMode = selectedLocationMode === LocationMode.EVERYWHERE
+  const isNotNewCaledonianLocationSelected = selectedPlace?.info !== 'Nouvelle-Calédonie'
+  const isNewCaledonianLocationSelected = selectedPlace?.info === 'Nouvelle-Calédonie'
 
   if (selectedPlace) {
     if (isEverywhereLocationMode) return Currency.EURO
     if (isNotNewCaledonianLocationSelected) return Currency.EURO
     if (isNewCaledonianLocationSelected) return pacificFrancCurrency
   }
+
+  const isUserRegisteredInEuroRegion = user?.currency === CurrencyEnum.EUR
+  const isUserRegisteredInPacificFrancRegion = user?.currency === CurrencyEnum.XPF
 
   if (user) {
     if (isUserRegisteredInEuroRegion) return Currency.EURO
