c4d89932c0398bf59b324b51037aa46e699dc679
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-09-09 11:27:15 +0200

    wip

--- src/shared/currency/useCurrencyParam.ts
+++ src/shared/currency/useCurrencyParam.ts
@@ -4,11 +4,11 @@ import { CurrencyEnum } from 'api/gen'
 
 const isWeb = Platform.OS === 'web'
 
-export function getCurrencyFromParam(): CurrencyEnum {
+export function getCurrencyFromParam(): CurrencyEnum | undefined {
   if (isWeb) {
     const searchParams = new URLSearchParams(window.location.search)
     const currency = searchParams.get('currency')
     if (currency === CurrencyEnum.XPF) return CurrencyEnum.XPF
   }
-  return CurrencyEnum.EUR
+  return undefined
 }
