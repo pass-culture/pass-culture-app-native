ef9778fc1a8da14154af7a356bf522a62b93c0f1
Bruno Peyrou <bruno.peyrou@passculture.app>
2025-09-24 11:45:45 +0200

    (PC-37876) feat(ReactQueryClientProvider): add react query persister for native app

--- src/libs/react-query/ReactQueryClientProvider.tsx
+++ src/libs/react-query/ReactQueryClientProvider.tsx
@@ -1,20 +1,23 @@
-import {
-  QueryClientProvider,
-  onlineManager,
-  focusManager as reactQueryFocusManager,
-} from '@tanstack/react-query'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import { onlineManager, focusManager as reactQueryFocusManager } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import React from 'react'
 import { AppState, AppStateStatus } from 'react-native'
 
 import { queryClient } from 'libs/react-query/queryClient'
 import { usePrefetchQueries } from 'libs/react-query/usePrefetchQueries'
 
+const persister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+})
+
 // By default, on the web, if a user leaves the app and returns to stale data,
 // React Query automatically requests fresh data in the background.
 // To have the equivalent behaviour for React-Native, we provide focus information through
 // the AppState module :
 reactQueryFocusManager.setEventListener((handleFocus) => {
-  function triggerReactQueryFocusOnBecomeActive(appState: AppStateStatus) {
+  const triggerReactQueryFocusOnBecomeActive = (appState: AppStateStatus) => {
     if (appState === 'active') {
       // When we open the app that was in the background, for logged in user, we refresh the access_token
       // if it is expired. /refresh_access_token takes 300ms in average, so we delay the
@@ -32,5 +35,14 @@ onlineManager.setOnline(false)
 
 export const ReactQueryClientProvider = ({ children }: { children: React.JSX.Element }) => {
   usePrefetchQueries()
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{
+        persister,
+        dehydrateOptions: { shouldDehydrateQuery: (query) => !!query.meta?.persist },
+      }}>
+      {children}
+    </PersistQueryClientProvider>
+  )
 }
