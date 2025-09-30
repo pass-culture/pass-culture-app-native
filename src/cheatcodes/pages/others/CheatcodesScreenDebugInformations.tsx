0f868d3256d62af5cf8f23e41b9985918637daef
Tanguy <47600889+tprache-pass@users.noreply.github.com>
2025-09-30 17:31:37 +0200

    feat: add HF infos to debug cheatcodes

--- src/cheatcodes/pages/others/CheatcodesScreenDebugInformations.tsx
+++ src/cheatcodes/pages/others/CheatcodesScreenDebugInformations.tsx
@@ -1,26 +1,25 @@
+import { HotUpdater, getUpdateSource } from '@hot-updater/react-native'
 import React, { FunctionComponent, useEffect, useState } from 'react'
-import { Alert, Button, Text } from 'react-native'
+import { Alert, Button } from 'react-native'
 
 import { api } from 'api/api'
-import { CodePushButton } from 'cheatcodes/components/CodePushButton'
 import { CrashTestButton } from 'cheatcodes/components/CrashTestButton'
 import { env } from 'libs/environment/env'
 import { decodeToken } from 'libs/jwt/jwt'
 import { clearRefreshToken } from 'libs/keychain/keychain'
-import { highlightLinks } from 'libs/parsers/highlightLinks'
 import { BatchUser } from 'libs/react-native-batch'
 import { storage } from 'libs/storage'
 import { getErrorMessage } from 'shared/getErrorMessage/getErrorMessage'
+import { Separator } from 'ui/components/Separator'
+import { ViewGap } from 'ui/components/ViewGap/ViewGap'
 import { SecondaryPageWithBlurHeader } from 'ui/pages/SecondaryPageWithBlurHeader'
-import { Spacer, Typo } from 'ui/theme'
+import { Typo } from 'ui/theme'
 
 const oldAccesstoken =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDc1OTE1MzUsIm5iZiI6MTYwNzU5MTUzNSwianRpIjoiNjM' +
   'xNzU4MGItYjU0ZS00YzdhLWExZTAtYWJlNjdkMTI5NTljIiwiZXhwIjoxNjA3NTkxNTk1LCJpZGVudGl0eSI6InBjdGVzd' +
   'C5qZXVuZTkzLmhhcy1ib29rZWQtc29tZUBidG14LmZyIiwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.hvQDl6h' +
   '_RCkrxrsWIlAFqszwE9AN3Q_SV1W_mAu_fS0'
-
-const someOfferDescription = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. https://www.google.com/search?q=pass+culture&oq=pass+culture&aqs=chrome.0.0i433j0l2j69i60l3j69i65l2.1136j0j7&sourceid=chrome&ie=UTF-8 Amet justo donec enim diam vulputate.`
 
 const getUserId = async () => {
   const accessToken = await storage.readString('access_token')
@@ -31,10 +30,37 @@ const getUserId = async () => {
   return tokenContent?.user_claims?.user_id ?? null
 }
 
+async function checkForAppUpdate() {
+  try {
+    const updateInfo = await HotUpdater.checkForUpdate({
+      source: getUpdateSource(`${env.HOT_UPDATER_FUNCTION_URL}`, {
+        updateStrategy: 'appVersion',
+      }),
+    })
+
+    if (!updateInfo) {
+      return {
+        status: 'No update info',
+      }
+    }
+
+    return updateInfo
+  } catch (error) {
+    return null
+  }
+}
+
 export const CheatcodesScreenDebugInformations: FunctionComponent = function () {
   const [batchInstallationId, setBatchInstallationId] = useState('none')
   const [userEmail, setUserEmail] = useState('')
   const [userId, setUserId] = useState<null | number>(null)
+  const [bundleId, setBundleId] = useState<string | null>(null)
+  const [updateStatus, setUpdateStatus] = useState<string | null>(null)
+
+  useEffect(() => {
+    const bundleId = HotUpdater.getBundleId()
+    setBundleId(bundleId)
+  }, [])
 
   useEffect(() => {
     getBatchInstallationID().then(setBatchInstallationId)
@@ -44,14 +70,25 @@ export const CheatcodesScreenDebugInformations: FunctionComponent = function () 
     getUserId().then(setUserId)
   }, [])
 
-  const ParsedDescription = highlightLinks(someOfferDescription)
-
   async function fetchMe() {
     try {
       const response = await api.getNativeV1Me()
       setUserEmail(response.email)
     } catch {
       //
+    }
+  }
+
+  async function handleCheckForAppUpdate() {
+    try {
+      const updateInfo = await checkForAppUpdate()
+      if (updateInfo?.status) {
+        setUpdateStatus(updateInfo.status)
+      } else {
+        setUpdateStatus(`${updateInfo === null ? 'NO UPDATE' : updateInfo.status}`)
+      }
+    } catch (error) {
+      setUpdateStatus('ERROR')
     }
   }
 
@@ -66,22 +103,32 @@ export const CheatcodesScreenDebugInformations: FunctionComponent = function () 
 
   return (
     <SecondaryPageWithBlurHeader title="CheatCodes">
-      <CrashTestButton />
-      <Spacer.Flex />
-      <Button title="Utiliser un ancien token" onPress={setOldToken} />
-      <Button title="Invalider l’access token" onPress={setOldToken} />
-      <Button title="Invalider les 2 tokens" onPress={invalidateBothTokens} />
-      <Button title="/ME" onPress={fetchMe} />
-      <Text>{userEmail}</Text>
-      <Spacer.Flex />
-      <Text>Batch installation ID: {batchInstallationId}</Text>
-      <Text>User ID: {userId}</Text>
-      <Spacer.Flex />
-      <Typo.Body>{someOfferDescription}</Typo.Body>
-      <Spacer.Flex />
-      <Typo.Body>{ParsedDescription}</Typo.Body>
-      <Spacer.Flex />
-      {env.ENV === 'testing' ? <CodePushButton /> : null}
+      <ViewGap gap={2}>
+        <CrashTestButton />
+        <Button title="Utiliser un ancien token" onPress={setOldToken} />
+        <Button title="Invalider l’access token" onPress={setOldToken} />
+        <Button title="Invalider les 2 tokens" onPress={invalidateBothTokens} />
+        <Button title="/ME" onPress={fetchMe} />
+        <Typo.Body>User email: {userEmail ?? 'unknown'}</Typo.Body>
+        <Typo.Body>Batch installation ID: {batchInstallationId}</Typo.Body>
+        <Typo.Body>User ID: {userId}</Typo.Body>
+        <Separator.Horizontal />
+        <Typo.Title4>HOT UPDATER</Typo.Title4>
+        <Typo.Body>Bundle ID: {bundleId ?? 'unknown'}</Typo.Body>
+        <Typo.Body>Channel: {HotUpdater.getChannel()}</Typo.Body>
+        <Typo.Body>App Version: {HotUpdater.getAppVersion()}</Typo.Body>
+        <Button title="Reload" onPress={() => HotUpdater.reload()} />
+        <Button
+          title="HotUpdater.runUpdateProcess()"
+          onPress={() =>
+            HotUpdater.runUpdateProcess({
+              source: `${env.HOT_UPDATER_FUNCTION_URL}`,
+            })
+          }
+        />
+        <Button title="Check for App Update" onPress={handleCheckForAppUpdate} />
+        <Typo.Body>Update Status: {updateStatus ?? 'unknown'}</Typo.Body>
+      </ViewGap>
     </SecondaryPageWithBlurHeader>
   )
 }
