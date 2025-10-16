2e90c3715d5b75e9223c5304ef8f22ba79434a77
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2025-10-16 16:34:37 +0200

    (PC-37501) feat(a11y): add marginHorizontal when isLandscape mode

--- src/ui/pages/Page.tsx
+++ src/ui/pages/Page.tsx
@@ -1,6 +1,20 @@
+import React from 'react'
+import { ViewProps } from 'react-native'
 import styled from 'styled-components/native'
 
-export const Page = styled.View(({ theme }) => ({
+import { useIsLandscape } from 'shared/useIsLandscape/useIsLandscape'
+
+const StyledPage = styled.View<{ isLandscape: boolean }>(({ theme, isLandscape }) => ({
   backgroundColor: theme.designSystem.color.background.default,
   flex: 1,
+  marginHorizontal: isLandscape ? theme.designSystem.size.spacing.xxxxl : 0,
 }))
+
+export const Page: React.FC<ViewProps> = ({ children, ...rest }) => {
+  const isLandscape = useIsLandscape()
+  return (
+    <StyledPage isLandscape={isLandscape} {...rest}>
+      {children}
+    </StyledPage>
+  )
+}
