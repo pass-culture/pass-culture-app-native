d1d9fb5c2b851509784773c9d2c4573c0798afe2
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-10-21 13:54:12 +0200

    Add schema test

---
+++ src/features/bonification/schemas/BonificationBirthPlaceSchema.test.ts
@@ -0,0 +1,67 @@
+// BonificationBirthPlaceSchema.test.js
+
+import { ValidationError } from 'yup'
+
+import { BonificationBirthPlaceSchema } from './BonificationBirthPlaceSchema'
+
+describe('BonificationBirthPlaceSchema', () => {
+  describe('valid schemas', () => {
+    it.each([
+      { birthCountrySelection: 'France', birthCity: 'Paris' },
+      { birthCountrySelection: 'États-Unis', birthCity: 'New York' },
+      { birthCountrySelection: 'Royaume-Uni' },
+      { birthCountrySelection: 'Espagne', birthCity: 'Madrid' },
+    ])('should accept a valid object: %p', async (data) => {
+      const result = await BonificationBirthPlaceSchema.validate(data)
+
+      expect(result).toEqual(data)
+    })
+  })
+
+  describe('birthCountrySelection validation', () => {
+    it('should reject if birthCountrySelection is missing', async () => {
+      const input = { birthCity: 'Paris' }
+      const validation = BonificationBirthPlaceSchema.validate(input)
+
+      await expect(validation).rejects.toEqual(
+        new ValidationError('Le pays de naissance est obligatoire')
+      )
+    })
+
+    it.each(['France123', 'France!', 'Allemagne_'])(
+      'should reject if birthCountrySelection contains invalid characters: %s',
+      async (country) => {
+        const input = { birthCountrySelection: country, birthCity: 'Paris' }
+        const validation = BonificationBirthPlaceSchema.validate(input)
+
+        await expect(validation).rejects.toEqual(
+          new ValidationError('Le pays ne doit pas contenir de chiffres ou de caractères spéciaux.')
+        )
+      }
+    )
+  })
+
+  describe('birthCity validation', () => {
+    describe('when country is "France"', () => {
+      it('should reject if birthCity is missing', async () => {
+        const input = { birthCountrySelection: 'France' }
+        const validation = BonificationBirthPlaceSchema.validate(input)
+
+        await expect(validation).rejects.toEqual(
+          new ValidationError(
+            'La ville de naissance est obligatoire pour les personnes nées en France'
+          )
+        )
+      })
+    })
+
+    describe('when country is not "France"', () => {
+      it('should be valid if birthCity is missing', async () => {
+        const input = { birthCountrySelection: 'Espagne' }
+        const result = await BonificationBirthPlaceSchema.validate(input)
+
+        expect(result).toEqual(input)
+      })
+    })
+  })
+})
--- src/features/bonification/schemas/BonificationBirthPlaceSchema.ts
+++ src/features/bonification/schemas/BonificationBirthPlaceSchema.ts
@@ -10,16 +10,10 @@ export const BonificationBirthPlaceSchema = object().shape({
       'Le pays ne doit pas contenir de chiffres ou de caractères spéciaux.',
       (name) => !!name && isNameValid(name)
     ),
-  birthCity: string()
-    .when('birthCountrySelection', {
-      is: (country) => country === 'France',
-      then: (schema) =>
-        schema.required('La ville de naissance est obligatoire pour les personnes nées en France'),
-      otherwise: (schema) => schema.optional(),
-    })
-    .test(
-      'isCityValid',
-      'La ville ne doit pas contenir de chiffres ou de caractères spéciaux.',
-      (name) => !!name && isNameValid(name)
-    ),
+  birthCity: string().when('birthCountrySelection', {
+    is: (country) => country === 'France',
+    then: (schema) =>
+      schema.required('La ville de naissance est obligatoire pour les personnes nées en France'),
+    otherwise: (schema) => schema.optional(),
+  }),
 })
