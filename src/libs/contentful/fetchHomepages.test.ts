646689c2d23244f29b1c604b0285c08b2e47ceca
thconte <128476306+thconte@users.noreply.github.com>
2025-11-07 22:51:56 +0100

    (PC-38505) refactor(Home): load only one home from Contentful

--- src/libs/contentful/fetchHomepages.test.ts
+++ src/libs/contentful/fetchHomepages.test.ts
@@ -1,17 +1,17 @@
 import { homepageList } from 'features/home/fixtures/homepageList.fixture'
 import { CONTENTFUL_BASE_URL } from 'libs/contentful/constants'
-import { fetchHomepageNatifContent } from 'libs/contentful/fetchHomepageNatifContent'
+import { fetchHomepageById } from 'libs/contentful/fetchHomepages'
 import { homepageEntriesAPIResponse } from 'libs/contentful/fixtures/homepageEntriesAPIResponse'
 import { mockServer } from 'tests/mswServer'
 
-describe('Contentful fetchHomepageNatifContent', () => {
+describe('Contentful fetchHomepageById', () => {
   beforeEach(() => {
     mockServer.universalGet(`${CONTENTFUL_BASE_URL}/entries`, homepageEntriesAPIResponse)
   })
 
-  it('should retrieve a list of adapted homepages', async () => {
-    const result = await fetchHomepageNatifContent()
+  it('should retrieve one homepage', async () => {
+    const result = await fetchHomepageById('6DCThxvbPFKAo04SVRZtwY')
 
-    expect(result).toEqual(homepageList)
+    expect(result).toEqual(homepageList[0])
   })
 })
