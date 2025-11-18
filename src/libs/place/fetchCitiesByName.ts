cf18dc09c94cfecc6bdc2a1785ec97d782cd209a
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-11-18 15:13:06 +0100

    (PC-38851) feat(city): set city limit to 20

--- src/libs/place/fetchCitiesByName.ts
+++ src/libs/place/fetchCitiesByName.ts
@@ -5,7 +5,7 @@ const CITIES_API_URL = 'https://geo.api.gouv.fr/communes'
 export const fetchCitiesByName = async (name: string): Promise<CitiesResponse> => {
   const queryParams = new URLSearchParams()
   queryParams.append('nom', name)
-  queryParams.append('limit', '10')
+  queryParams.append('limit', '20')
   queryParams.append('boost', 'population') // sort results by biggest cities in terms of population
   const url = `${CITIES_API_URL}?${queryParams.toString()}`
 
