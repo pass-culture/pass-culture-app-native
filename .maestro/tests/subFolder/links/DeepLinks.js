a241791cbcfbf03d06184c17f4f881c752874861
Axel <153505101+arenault-pass@users.noreply.github.com>
2025-11-20 14:32:35 +0100

    (PC-38744) feat(e2e): create scripts for visual regression detection

--- .maestro/tests/subFolder/links/DeepLinks.js
+++ .maestro/tests/subFolder/links/DeepLinks.js
@@ -4,10 +4,12 @@ output.links = {
       home: 'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Faccueil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
       profile:
         'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Fprofil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
+      thematicSearch: 'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Frecherche%2Fthematique%3FofferCategories%3D%5B%22CINEMA%22%5D%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1'
     },
     ios: {
       home: 'https://app.staging.passculture.team/accueil?from=deeplink&utm_gen=marketing',
       profile: 'https://app.staging.passculture.team/profil?from=deeplink&utm_gen=marketing',
+      thematicSearch: 'https://app.staging.passculture.team/recherche/thematique?from=deeplink&offerCategories=%5B%22CINEMA%22%5D&utm_gen=marketing'
     },
   },
   testing: {
