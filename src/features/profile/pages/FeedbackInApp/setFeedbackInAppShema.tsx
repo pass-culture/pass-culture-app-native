5ef7accefc2c4961e154c91c4267287bb9c5eed8
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-12-08 08:56:33 +0100

    (PC-38748) feat(DebugScreen): UX changes (#8983)
    * (PC-38748) feat(DebugScreen): UX changes
    
    * Tests
    
    * remove testId and add role

--- src/features/profile/pages/FeedbackInApp/setFeedbackInAppShema.tsx
+++ src/features/profile/pages/FeedbackInApp/setFeedbackInAppShema.tsx
@@ -3,5 +3,7 @@ import { object, string } from 'yup'
 const FEEDBACK_IN_APP_VALUE_MAX_LENGTH = 800
 
 export const setFeedbackInAppSchema = object().shape({
-  feedback: string().min(1).max(FEEDBACK_IN_APP_VALUE_MAX_LENGTH),
+  feedback: string()
+    .min(1, 'Remplis le champ ci-dessus pour continuer')
+    .max(FEEDBACK_IN_APP_VALUE_MAX_LENGTH, 'Maximun 800 caractères'),
 })
