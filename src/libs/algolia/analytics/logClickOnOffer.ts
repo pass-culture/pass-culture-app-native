b113169045211441146f25b8454dfd676de3876f
imouandjolobe-pass <158567429+imouandjolobe-pass@users.noreply.github.com>
2025-12-09 09:50:02 +0100

    (PC-38666) feat(Venue): log click on venue to algolia (#8965)

--- src/libs/algolia/analytics/logClickOnOffer.ts
+++ src/libs/algolia/analytics/logClickOnOffer.ts
@@ -1,37 +1,26 @@
-import { useMemo } from 'react'
 import AlgoliaSearchInsights from 'search-insights'
 
 import { CookieNameEnum } from 'features/cookies/enums'
 import { getAcceptedCookieConsent } from 'features/cookies/helpers/getAcceptedCookieConsent'
-import { useSearchAnalyticsState } from 'libs/algolia/analytics/SearchAnalyticsWrapper'
+import { AlgoliaAnalyticsEvents } from 'libs/algolia/types'
 import { env } from 'libs/environment/env'
 import { captureMonitoringError } from 'libs/monitoring/errors'
 
-export const logClickOnOffer =
-  (currentQueryID?: string) =>
-  async ({ objectID, position }: { objectID: string; position: number }) => {
-    const hasAcceptedAlgoliaInsights = await getAcceptedCookieConsent(
-      CookieNameEnum.ALGOLIA_INSIGHTS
-    )
+export const logClickOnOffer = async ({ objectID, position, queryID }: AlgoliaAnalyticsEvents) => {
+  const hasAcceptedAlgoliaInsights = await getAcceptedCookieConsent(CookieNameEnum.ALGOLIA_INSIGHTS)
+  if (!hasAcceptedAlgoliaInsights) return
 
-    if (!hasAcceptedAlgoliaInsights) return
-
-    if (currentQueryID === undefined) {
-      captureMonitoringError('Algolia Analytics: useLogClickOnOffer called without any QueryID set')
-      return
-    }
-
-    AlgoliaSearchInsights('clickedObjectIDsAfterSearch', {
-      eventName: 'Offer Clicked',
-      index: env.ALGOLIA_OFFERS_INDEX_NAME,
-      queryID: currentQueryID,
-      objectIDs: [objectID],
-      // for Algolia, position start at 1 instead of 0
-      positions: [position + 1],
-    })
+  if (queryID === undefined) {
+    captureMonitoringError('Algolia Analytics: logClickOnOffer called without any QueryID set')
+    return
   }
 
-export const useLogClickOnOffer = () => {
-  const { currentQueryID } = useSearchAnalyticsState()
-  return useMemo(() => ({ logClickOnOffer: logClickOnOffer(currentQueryID) }), [currentQueryID])
+  await AlgoliaSearchInsights('clickedObjectIDsAfterSearch', {
+    eventName: 'Offer Clicked',
+    index: env.ALGOLIA_OFFERS_INDEX_NAME,
+    queryID,
+    objectIDs: [objectID],
+    // for Algolia, position start at 1 instead of 0
+    positions: [position + 1],
+  })
 }
