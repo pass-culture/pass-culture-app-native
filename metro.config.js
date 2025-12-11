b37e13fc10c6353a4e0d59a09c627251585f5326
npamart-pass <240470573+npamart-pass@users.noreply.github.com>
2025-12-11 13:38:33 +0100

    (PC-38313) chore(hotupdater): update hotupdater sourcemaps to sentry

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
+const { getDefaultConfig } = require('@react-native/metro-config')
+const { withSentryConfig } = require("@sentry/react-native/metro");
 
 /**
  * Metro configuration
@@ -6,6 +7,5 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {}
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+const config = getDefaultConfig(__dirname);
+module.exports = withSentryConfig(config);
