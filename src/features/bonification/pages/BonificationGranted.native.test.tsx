eaf86f73ff6176190a9d146487ac546dc2549dc5
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-12-15 16:21:29 +0100

    Add call to backend to reset recreditAmountToShow

--- src/features/bonification/pages/BonificationGranted.native.test.tsx
+++ src/features/bonification/pages/BonificationGranted.native.test.tsx
@@ -1,21 +1,53 @@
 import React from 'react'
 
-import { navigate } from '__mocks__/@react-navigation/native'
+import { reset } from '__mocks__/@react-navigation/native'
 import { BonificationGranted } from 'features/bonification/pages/BonificationGranted'
 import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/tests/setFeatureFlags'
+import { mockServer } from 'tests/mswServer'
+import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
 import { render, screen, userEvent } from 'tests/utils'
 
 jest.mock('libs/firebase/analytics/analytics')
+jest.mock('libs/jwt/jwt')
+
+const mockShowSuccessSnackBar = jest.fn()
+const mockShowErrorSnackBar = jest.fn()
+jest.mock('ui/components/snackBar/SnackBarContext', () => ({
+  useSnackBarContext: () => ({
+    showSuccessSnackBar: mockShowSuccessSnackBar,
+    showErrorSnackBar: mockShowErrorSnackBar,
+  }),
+}))
 
 describe('BonificationGranted', () => {
   beforeEach(() => setFeatureFlags())
 
-  it('should go navigate to home when pressing "J’en profite"', async () => {
-    render(<BonificationGranted />)
+  it('should go reset to home when pressing "J’en profite"', async () => {
+    mockServer.postApi('/v1/reset_recredit_amount_to_show', {
+      responseOptions: { statusCode: 200, data: {} },
+    })
+
+    render(reactQueryProviderHOC(<BonificationGranted />))
 
     const button = screen.getByText('J’en profite')
     await userEvent.press(button)
 
-    expect(navigate).toHaveBeenCalledWith('TabNavigator', { params: undefined, screen: 'Home' })
+    expect(reset).toHaveBeenCalledWith({
+      index: 0,
+      routes: [{ name: 'TabNavigator' }],
+    })
+  })
+
+  it('should show snackbar when pressing "J’en profite" but call fails', async () => {
+    mockServer.postApi('/v1/reset_recredit_amount_to_show', {
+      responseOptions: { statusCode: 500, data: {} },
+    })
+
+    render(reactQueryProviderHOC(<BonificationGranted />))
+
+    const button = screen.getByText('J’en profite')
+    await userEvent.press(button)
+
+    expect(mockShowErrorSnackBar).toHaveBeenCalledWith({ message: 'Une erreur est survenue' })
   })
 })
