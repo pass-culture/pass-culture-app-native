9d49c5a27d73185e6880214f75012f210e220365
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2025-12-17 14:12:10 +0100

    wip analytics

--- src/libs/firebase/shims/analytics/index.web.ts
+++ src/libs/firebase/shims/analytics/index.web.ts
@@ -1,9 +1,18 @@
-import firebase from 'firebase/compat/app'
-/* eslint-disable-next-line no-restricted-imports */
-import 'firebase/compat/analytics'
+import {
+  getAnalytics,
+  logEvent,
+  setAnalyticsCollectionEnabled,
+  setUserId,
+} from 'firebase/analytics'
 
 import initializeApp from '../firebase-init'
-initializeApp()
 
-const analytics = firebase.analytics
-export default analytics
+// 1. Initialize App
+const app = initializeApp()
+
+// 2. Initialize Analytics
+// Note: We create a getter to match the Native Shim's behavior
+const analyticsInstance = getAnalytics(app)
+const getAnalyticsInstance = () => analyticsInstance
+
+export { getAnalyticsInstance as getAnalytics, logEvent, setAnalyticsCollectionEnabled, setUserId }
