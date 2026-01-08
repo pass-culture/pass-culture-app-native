1e2ae883006d33346cf51ea64bf1f1926d8cc617
mlassauzay-pass <240954952+mlassauzay-pass@users.noreply.github.com>
2026-01-08 15:40:56 +0100

    move prefetchSettingsQuery close to useSettingsQuery

--- src/libs/react-query/usePrefetchQueries.ts
+++ src/libs/react-query/usePrefetchQueries.ts
@@ -1,24 +1,17 @@
 import { useEffect } from 'react'
 
-import { api } from 'api/api'
-import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
-import { QueryKeys } from 'libs/queryKeys'
-import { queryClient } from 'libs/react-query/queryClient'
+import { prefetchSettingsQuery } from 'queries/settings/settingsQuery'
 
 const prefetchQueries = async () => {
   try {
-    await queryClient.prefetchQuery({
-      queryKey: [QueryKeys.SETTINGS],
-      queryFn: () => api.getNativeV1Settings(),
-    })
+    await prefetchSettingsQuery()
   } catch (err) {
     // do nothing in case the pretching of queries fails
   }
 }
 
 export const usePrefetchQueries = () => {
-  const { isConnected } = useNetInfoContext()
   useEffect(() => {
-    if (isConnected) prefetchQueries()
-  }, [isConnected])
+    prefetchQueries()
+  }, [])
 }
