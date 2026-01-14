a3b22207076ae7c7bcf4c53856a890a89ec6d2cf
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2026-01-14 15:52:57 +0100

    (PC-39526) refactor(offer): add offer bridge to manage use refacto or not under wipOfferRefacto FF

---
+++ src/features/offer/bridge/OfferPageBridge.native.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { OfferPageBridge } from 'features/offer/bridge/OfferPageBridge'
+import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/tests/setFeatureFlags'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
+import { render, screen } from 'tests/utils'
+
+jest.mock('features/offer/pages/Offer/Offer', () => ({
+  Offer: () => {
+    const { View } = jest.requireActual('react-native')
+    return <View testID="offer" />
+  },
+}))
+
+jest.mock('features/offerRefacto/pages/Offer/Offer', () => ({
+  Offer: () => {
+    const { View } = jest.requireActual('react-native')
+    return <View testID="offerRefacto" />
+  },
+}))
+
+describe('OfferPageBridge', () => {
+  it('should use old offer code when wipOfferRefacto FF deactivated', () => {
+    setFeatureFlags()
+    render(<OfferPageBridge />)
+
+    expect(screen.getByTestId('offer')).toBeOnTheScreen()
+    expect(screen.queryByTestId('offerRefacto')).not.toBeOnTheScreen()
+  })
+
+  it('should use refacto offer code when wipOfferRefacto FF activated', () => {
+    setFeatureFlags([RemoteStoreFeatureFlags.WIP_OFFER_REFACTO])
+
+    render(<OfferPageBridge />)
+
+    expect(screen.getByTestId('offerRefacto')).toBeOnTheScreen()
+    expect(screen.queryByTestId('offer')).not.toBeOnTheScreen()
+  })
+})
---
+++ src/features/offer/bridge/OfferPageBridge.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Offer as OldOffer } from 'features/offer/pages/Offer/Offer'
+import { Offer as NewOffer } from 'features/offerRefacto/pages/Offer/Offer'
+import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
+import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
+
+export const OfferPageBridge = () => {
+  const isOfferRefactoEnabled = useFeatureFlag(RemoteStoreFeatureFlags.WIP_OFFER_REFACTO)
+
+  return isOfferRefactoEnabled ? <NewOffer /> : <OldOffer />
+}
