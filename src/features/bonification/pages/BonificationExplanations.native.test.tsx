2d48ac7a01c1b23d2d440bf36bd56e47dcb1c04d
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2026-01-16 10:09:15 +0100

    (PC-39508) feat(bonif): show message to new caledonian users

--- src/features/bonification/pages/BonificationExplanations.native.test.tsx
+++ src/features/bonification/pages/BonificationExplanations.native.test.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import { goBack, navigate } from '__mocks__/@react-navigation/native'
+import { CurrencyEnum } from 'api/gen'
 import { BonificationExplanations } from 'features/bonification/pages/BonificationExplanations'
 import * as NavigationHelpers from 'features/navigation/helpers/openUrl'
+import { beneficiaryUser } from 'fixtures/user'
 import { setFeatureFlags } from 'libs/firebase/firestore/featureFlags/tests/setFeatureFlags'
+import { mockAuthContextWithUser } from 'tests/AuthContextUtils'
 import { render, screen, userEvent } from 'tests/utils'
 
+jest.mock('features/auth/context/AuthContext')
 jest.mock('libs/firebase/analytics/analytics')
 const openUrl = jest.spyOn(NavigationHelpers, 'openUrl')
 
@@ -46,5 +50,26 @@ describe('BonificationExplanations', () => {
     await userEvent.press(button)
 
     expect(goBack).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should show banner to users with euro currency', async () => {
+    render(<BonificationExplanations />)
+
+    expect(screen.getByText('Si ce n’est pas ton cas, ta demande sera refusée.')).toBeTruthy()
+  })
+
+  it('Should show banner to users with pacific franc currency', async () => {
+    mockAuthContextWithUser({
+      ...beneficiaryUser,
+      currency: CurrencyEnum.XPF,
+    })
+
+    render(<BonificationExplanations />)
+
+    expect(
+      screen.getByText(
+        'Si tu habites en Nouvelle-Calédonie, tu ne pourras malheureusement pas bénéficier du bonus.'
+      )
+    ).toBeTruthy()
   })
 })
