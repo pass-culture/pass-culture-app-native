b1c504fa1f7704e920bc0a4553f7ba40bbdeea08
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2026-01-19 10:48:56 +0100

    (PC-39526) refactor(offer): rebase following of master branch conflict

--- src/features/offer/bridge/OfferPageBridge.tsx
+++ src/features/offer/bridge/OfferPageBridge.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Offer as OldOffer } from 'features/offer/pages/Offer/Offer'
-import { Offer as NewOffer } from 'features/offerRefacto/pages/OfferPage'
+import { Offer as NewOffer } from 'features/offerRefacto/pages/Offer'
 import { useFeatureFlag } from 'libs/firebase/firestore/featureFlags/useFeatureFlag'
 import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 
