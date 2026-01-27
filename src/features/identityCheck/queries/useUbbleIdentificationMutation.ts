7f6c72bb90ad021b5d155f068d97fd96e8aa53d1
cgerrard-pass <187540148+cgerrard-pass@users.noreply.github.com>
2026-01-27 10:23:39 +0100

    Feedback

--- src/features/identityCheck/queries/useUbbleIdentificationMutation.ts
+++ src/features/identityCheck/queries/useUbbleIdentificationMutation.ts
@@ -4,9 +4,8 @@ import { api } from 'api/api'
 import { REDIRECT_URL_UBBLE } from 'features/identityCheck/constants'
 import { MutationKeys } from 'libs/queryKeys'
 
-export const useUbbleIdentificationMutation = () => {
-  return useMutation({
+export const useUbbleIdentificationMutation = () =>
+  useMutation({
     mutationKey: [MutationKeys.IDENTIFICATION_URL],
     mutationFn: () => api.postNativeV1UbbleIdentification({ redirectUrl: REDIRECT_URL_UBBLE }),
   })
-}
