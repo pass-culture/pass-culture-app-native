270525376e1a6158d526d70c36312efbdcff497a
Inès Mouandjo Lobé <158567429+imouandjolobe-pass@users.noreply.github.com>
2026-02-02 17:27:16 +0100

    feat: add snapshots test and fix focus input zoom on mobile

--- src/features/profile/pages/Chatbot/chatbotWebviewHTML.ts
+++ src/features/profile/pages/Chatbot/chatbotWebviewHTML.ts
@@ -3,7 +3,7 @@ export const chatbotWebviewHTML = `
   <html lang="fr">
     <head>
       <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
       <title>Chatbot</title>
       <style>
         * {
