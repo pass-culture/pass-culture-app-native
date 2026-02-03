184caef61b96e7d2f5a091c7f14a84939ce20e95
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2026-02-03 10:34:35 +0100

    (PC-39793) feat(artist): use artist webview page when pressing Wikipedia button

--- src/features/artist/fixtures/mockArtist.ts
+++ src/features/artist/fixtures/mockArtist.ts
@@ -4,6 +4,8 @@ export const mockArtist = {
   image: 'http://commons.wikimedia.org/wiki/Special:FilePath/Virginie%20Despentes%202012.jpg',
   description:
     'Il s’agit d’une chanteuse canadienne qui a connu un grand succès au début des années 2000. Son style musical mélange le pop punk et le rock alternatif, ce qui lui a permis de se démarquer dans l’industrie musicale. Avec des tubes comme "Complicated" et "Sk8er Boi", elle a su captiver un large public et devenir une icône pour de nombreux jeunes. En plus de sa carrière musicale, Avril Lavigne est également reconnue pour son engagement dans diverses causes humanitaires, notamment la sensibilisation à la maladie de Lyme, qu’elle a elle-même combattue. Son influence s’étend au-delà de la musique, faisant d’elle une figure importante de la culture pop contemporaine.',
+  descriptionCredit: '© Contenu généré par IA',
+  descriptionSource: 'https://fr.wikipedia.org/wiki/Avril_Lavigne',
 }
 
 export const mockArtists = [
