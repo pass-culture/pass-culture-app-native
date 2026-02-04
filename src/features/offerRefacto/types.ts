b4ff87a0029ddd6a8bd8a2aef2b2141c09e3c383
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2026-02-04 12:14:44 +0100

    (PC-39044) feat(offer): function to organize artists by role in possible several lines

--- src/features/offerRefacto/types.ts
+++ src/features/offerRefacto/types.ts
@@ -1,3 +1,10 @@
+import { OfferArtist } from 'api/gen'
+
 export type HasEnoughCreditType =
   | { hasEnoughCredit: true; message?: never }
   | { hasEnoughCredit: false; message?: string }
+
+export type ArtistsLine = {
+  prefix: string
+  artists: OfferArtist[]
+}
