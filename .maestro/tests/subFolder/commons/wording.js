1c52cca0c03ded069091baf4e56596d434948be7
Axel <153505101+arenault-pass@users.noreply.github.com>
2026-02-12 17:15:24 +0100

    (PC-40303) chore(e2e): implement POM in Cookies scripts

---
+++ .maestro/tests/subFolder/commons/wording.js
@@ -0,0 +1,48 @@
+output.wording = {
+  common: {
+    browserUpdate: "J’accède au pass Culture sans mettre à jour mon navigateur",
+    ios: {
+      permissionRegex: "Autorisez-vous.*",
+      allow: "Autoriser",
+    },
+    back: "Revenir à l’étape précédente",
+  },
+  cookies: {
+    consent: {
+      title: "Respect de ta vie privée",
+      acceptAll: "Tout accepter",
+      refuseAll: "Tout refuser",
+      choose: "Choisir les cookies",
+    },
+    settings: {
+      title: "Réglages des cookies",
+      subtitle: "Je choisis mes cookies",
+      policyLink: ".*politique de gestion des cookies",
+      save: "Enregistrer mes choix",
+    },
+    switches: {
+      customization: {
+        off: "Personnaliser ta navigation - Interrupteur à bascule - non coché",
+        on: "Personnaliser ta navigation - Interrupteur à bascule - coché",
+      },
+      statistics: {
+        off: "Enregistrer des statistiques de navigation - Interrupteur à bascule - non coché",
+        on: "Enregistrer des statistiques de navigation - Interrupteur à bascule - coché",
+      },
+      marketing: {
+        off: "Mesurer l’efficacité de nos publicités - Interrupteur à bascule - non coché",
+        on: "Mesurer l’efficacité de nos publicités - Interrupteur à bascule - coché",
+      },
+      video: {
+        off: "Lire les contenus vidéos - Interrupteur à bascule - non coché",
+        on: "Lire les contenus vidéos - Interrupteur à bascule - coché",
+      },
+      all: {
+        on: "Tout accepter - Interrupteur à bascule - coché",
+      },
+    },
+  },
+  booking: {
+    wording: "Réserver l’offre",
+  },
+}
