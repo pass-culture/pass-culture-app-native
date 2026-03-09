f056d3a5f3cfeb6b7efdee03b6d5fbf0a6499873
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2026-03-09 14:48:14 +0100

    (PC-40163) refactor(chronicle): extract chronicle card body in an external component to avoid duplication

---
+++ src/features/chronicle/components/ChronicleCardBody/ChronicleCardBody.tsx
@@ -0,0 +1,89 @@
+import React, { FunctionComponent, PropsWithChildren, ReactNode, useState } from 'react'
+import { LayoutChangeEvent, Platform } from 'react-native'
+import styled, { useTheme } from 'styled-components/native'
+
+import { ChronicleCardData } from 'features/chronicle/type'
+import { getLineHeightPx } from 'libs/parsers/getLineHeightPx'
+import { ViewGap } from 'ui/components/ViewGap/ViewGap'
+import { Typo } from 'ui/theme'
+
+type Props = PropsWithChildren<
+  Pick<ChronicleCardData, 'description' | 'date'> & {
+    cardWidth?: number
+    shouldTruncate?: boolean
+    tag?: ReactNode
+  }
+>
+
+const MAX_LINES = 3
+
+export const ChronicleCardBody: FunctionComponent<Props> = ({
+  description,
+  date,
+  cardWidth,
+  children,
+  tag,
+  shouldTruncate = false,
+}) => {
+  const { designSystem } = useTheme()
+
+  const [currentNumberOfLines, setCurrentNumberOfLines] = useState<number | undefined>(undefined)
+  const [shouldDisplayButton, setShouldDisplayButton] = useState(false)
+
+  // height depending on the platform
+  const lineHeight = designSystem.typography.bodyAccentS.lineHeight
+  const DEFAULT_HEIGHT_WEB = getLineHeightPx(lineHeight, true) * MAX_LINES
+  const DEFAULT_HEIGHT_MOBILE = getLineHeightPx(lineHeight, false) * MAX_LINES
+  const defaultHeight = Platform.OS === 'web' ? DEFAULT_HEIGHT_WEB : DEFAULT_HEIGHT_MOBILE
+
+  const handleOnLayout = (event: LayoutChangeEvent) => {
+    // We use Math.floor to avoid floating-point precision issues when comparing heights
+    const actualHeight = Math.floor(event.nativeEvent.layout.height)
+    const expectedMaxHeight = Math.floor(defaultHeight)
+
+    if (actualHeight > expectedMaxHeight) {
+      setShouldDisplayButton(true)
+      setCurrentNumberOfLines(3)
+    }
+  }
+
+  const descriptionWithoutEndSpace = description?.trimEnd()
+
+  return (
+    <Container gap={3} width={cardWidth}>
+      <DescriptionContainer defaultHeight={defaultHeight} shouldTruncate={shouldTruncate}>
+        <Typo.BodyS
+          testID="description"
+          onLayout={shouldTruncate ? handleOnLayout : undefined}
+          numberOfLines={currentNumberOfLines}>
+          {`«\u00a0${descriptionWithoutEndSpace}\u00a0»`}
+        </Typo.BodyS>
+      </DescriptionContainer>
+      <PublicationDate>{date}</PublicationDate>
+      <BottomCardContainer>
+        {tag}
+        {shouldDisplayButton && children}
+      </BottomCardContainer>
+    </Container>
+  )
+}
+
+const Container = styled(ViewGap)<{ width?: number }>(({ theme, width }) => ({
+  ...(width === undefined ? undefined : { width }),
+  backgroundColor: theme.designSystem.color.background.default,
+}))
+
+const DescriptionContainer = styled.View<{ defaultHeight: number; shouldTruncate?: boolean }>(
+  ({ defaultHeight, shouldTruncate }) =>
+    shouldTruncate ? { maxHeight: MAX_LINES * defaultHeight, overflow: 'hidden', flexGrow: 1 } : {}
+)
+
+const BottomCardContainer = styled.View({
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
+
+const PublicationDate = styled(Typo.BodyAccentXs)(({ theme }) => ({
+  color: theme.designSystem.color.text.subtle,
+}))
