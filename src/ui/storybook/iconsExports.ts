fa5ce8e7e0aacb34c4598fde114082e3c5aa3824
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2026-03-16 12:17:18 +0100

    (PC-40532) feat(ui): add search IA icon

--- src/ui/storybook/iconsExports.ts
+++ src/ui/storybook/iconsExports.ts
@@ -97,6 +97,7 @@ import { Question } from 'ui/svg/icons/Question'
 import { RightFilled } from 'ui/svg/icons/RightFilled'
 import { ScrollToTop } from 'ui/svg/icons/ScrollToTop'
 import { Search } from 'ui/svg/icons/Search'
+import { SearchIA } from 'ui/svg/icons/SearchIA'
 import { Share } from 'ui/svg/icons/Share'
 import { Show } from 'ui/svg/icons/Show'
 import { Signal } from 'ui/svg/icons/Signal'
@@ -223,6 +224,7 @@ export const SecondaryAndBiggerIcons = {
   Profile,
   ScrollToTop,
   Search,
+  SearchIA,
   Share,
   Signal,
   SignOut,
