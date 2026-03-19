3cf9700617d044e1f7c0fa05a151facf7630b716
npamart-pass <240470573+npamart-pass@users.noreply.github.com>
2026-03-19 16:00:02 +0100

    (PC-40028) refactor(Algolia): exclude non-search queries from analytics (#9373)

--- src/libs/algolia/fetchAlgolia/clients.ts
+++ src/libs/algolia/fetchAlgolia/clients.ts
@@ -1,5 +1,28 @@
-import { liteClient } from 'algoliasearch/lite'
+import { liteClient, SearchMethodParams } from 'algoliasearch/lite'
 
 import { env } from 'libs/environment/env'
 
-export const client = liteClient(env.ALGOLIA_APPLICATION_ID, env.ALGOLIA_SEARCH_API_PUBLIC_KEY)
+const baseClient = liteClient(env.ALGOLIA_APPLICATION_ID, env.ALGOLIA_SEARCH_API_PUBLIC_KEY)
+
+const disableAnalyticsByDefault = (
+  searchMethodParams: Parameters<typeof baseClient.search>[0]
+): SearchMethodParams => {
+  const params = Array.isArray(searchMethodParams)
+    ? searchMethodParams
+    : searchMethodParams.requests
+  const requests = params.map((req) => ({ analytics: false as const, ...req }))
+  const normalized = Array.isArray(searchMethodParams)
+    ? { requests }
+    : { ...searchMethodParams, requests }
+  return normalized
+}
+
+export const client: typeof baseClient = {
+  ...baseClient,
+  search(searchMethodParams, requestOptions) {
+    return baseClient.search(disableAnalyticsByDefault(searchMethodParams), requestOptions)
+  },
+  searchForHits(searchMethodParams, requestOptions) {
+    return baseClient.searchForHits(disableAnalyticsByDefault(searchMethodParams), requestOptions)
+  },
+}
--- src/libs/algolia/fetchAlgolia/fetchSearchResults/fetchSearchResults.ts
+++ src/libs/algolia/fetchAlgolia/fetchSearchResults/fetchSearchResults.ts
@@ -69,6 +69,7 @@ export const fetchSearchResults = async ({
     /* Is needed to get a queryID, in order to send analytics events
      https://www.algolia.com/doc/api-reference/api-parameters/clickAnalytics/ */
     clickAnalytics: true,
+    analytics: true,
   }
 
   const queries = [
@@ -101,6 +102,7 @@ export const fetchSearchResults = async ({
         parameters.venue
       ),
       clickAnalytics: true,
+      analytics: true,
     },
     // Offers without duplication limit
     {
