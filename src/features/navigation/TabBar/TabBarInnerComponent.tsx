052b468d754a98a267b46ef8e4f8b7e531f0a407
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2026-03-23 11:10:59 +0100

    (PC-40874) feat(a11y): change tabBar in zoom 200 (#9404)

--- src/features/navigation/TabBar/TabBarInnerComponent.tsx
+++ src/features/navigation/TabBar/TabBarInnerComponent.tsx
@@ -1,12 +1,13 @@
 import colorAlpha from 'color-alpha'
 import React from 'react'
 import LinearGradient from 'react-native-linear-gradient'
-import styled from 'styled-components/native'
+import styled, { useTheme } from 'styled-components/native'
 
 import { menu } from 'features/navigation/TabBar/menu'
 import { TabBarBadge } from 'features/navigation/TabBar/TabBarBadge'
 import { TabBarTitle } from 'features/navigation/TabBar/TabBarTitle'
 import { TabInnerComponentProps } from 'features/navigation/TabBar/TabStackNavigatorTypes'
+import { useFontScaleValue } from 'shared/accessibility/helpers/useFontScaleValue'
 import { LogoDetailed } from 'ui/svg/icons/LogoDetailed'
 
 export const TabBarInnerComponent: React.FC<TabInnerComponentProps> = ({
@@ -16,7 +17,20 @@ export const TabBarInnerComponent: React.FC<TabInnerComponentProps> = ({
   badgeValue,
   showBadge,
 }) => {
+  const { icons } = useTheme()
+
   const accessibilityLabel = menu[tabName].accessibilityLabel
+
+  const title = useFontScaleValue({
+    default: <TabBarTitle selected={isSelected} displayName={menu[tabName].displayName} />,
+    at200PercentZoom: null,
+  })
+
+  const iconSize = useFontScaleValue({
+    default: icons.sizes.small,
+    at200PercentZoom: icons.sizes.standard,
+  })
+
   return (
     <React.Fragment>
       {isSelected ? (
@@ -27,11 +41,12 @@ export const TabBarInnerComponent: React.FC<TabInnerComponentProps> = ({
           <StyledIcon
             as={BicolorIcon}
             selected={isSelected}
+            iconSize={iconSize}
             badgeValue={showBadge ? undefined : badgeValue}
           />
           {showBadge ? <TabBarBadge testID={`${tabName}-new-feature-badge`} /> : null}
         </IconWrapper>
-        <TabBarTitle selected={isSelected} displayName={menu[tabName].displayName} />
+        {title}
       </MainContent>
       {isSelected ? <BicolorSelectorPlaceholder /> : null}
     </React.Fragment>
@@ -55,13 +70,15 @@ const Gradient = styled(LinearGradient).attrs<{ colors?: string[] }>(({ theme })
   end: { x: 1, y: 0 },
 }))(({ theme }) => ({ height: theme.designSystem.size.spacing.xxs, width: '100%' }))
 
-const StyledIcon = styled(LogoDetailed).attrs<{ selected?: boolean }>(({ theme, selected }) => ({
-  color: selected
-    ? theme.designSystem.color.icon.brandPrimary
-    : theme.designSystem.color.icon.subtle,
-  size: theme.icons.sizes.small,
-  thin: true,
-}))<{ selected?: boolean }>``
+const StyledIcon = styled(LogoDetailed).attrs<{ selected?: boolean; iconSize: number }>(
+  ({ theme, selected, iconSize }) => ({
+    color: selected
+      ? theme.designSystem.color.icon.brandPrimary
+      : theme.designSystem.color.icon.subtle,
+    size: iconSize,
+    thin: true,
+  })
+)``
 
 const BicolorSelectorPlaceholder = styled.View(({ theme }) => ({
   height: theme.designSystem.size.spacing.xxs,
