63c00ca87b9558d54addb56676e1f6491c07df90
Inès Mouandjo Lobé <158567429+imouandjolobe-pass@users.noreply.github.com>
2026-03-25 15:17:38 +0100

    (PC-40739) feat(ThematicSearch): add perform search log on ThematicSearch

--- src/features/search/components/SearchResultsContent/SearchResultsContent.native.test.tsx
+++ src/features/search/components/SearchResultsContent/SearchResultsContent.native.test.tsx
@@ -12,7 +12,7 @@ import {
 } from 'features/accessibility/context/AccessibilityFiltersWrapper'
 import { initialSearchState } from 'features/search/context/reducer'
 import { MAX_RADIUS } from 'features/search/helpers/reducer.helpers'
-import { SearchState } from 'features/search/types'
+import { SearchState, SearchView } from 'features/search/types'
 import * as useVenueMapStore from 'features/venueMap/store/venueMapStore'
 import { beneficiaryUser } from 'fixtures/user'
 import { venuesFixture } from 'libs/algolia/fetchAlgolia/fetchVenues/fixtures/venuesFixture'
@@ -222,6 +222,11 @@ const initSearchResultsFlashlist = async () => {
   return flashList
 }
 
+let mockPreviousRoute = SearchView.Landing
+jest.mock('features/navigation/helpers/usePreviousRoute', () => ({
+  usePreviousRoute: jest.fn(() => mockPreviousRoute),
+}))
+
 describe('SearchResultsContent component', () => {
   beforeEach(() => {
     setFeatureFlags()
@@ -245,6 +250,8 @@ describe('SearchResultsContent component', () => {
       userLocation: mockedPosition,
       hasGeolocPosition: false,
     })
+
+    mockPreviousRoute = SearchView.Landing
   })
 
   it('should render correctly', async () => {
@@ -336,6 +343,7 @@ describe('SearchResultsContent component', () => {
     })
 
     rerender({ ...DEFAULT_SEARCH_RESULT_CONTENT_PROPS, isLoading: false })
+    rerender({ ...DEFAULT_SEARCH_RESULT_CONTENT_PROPS, isLoading: false })
 
     expect(analytics.logPerformSearch).toHaveBeenCalledTimes(1)
   })
@@ -364,7 +372,37 @@ describe('SearchResultsContent component', () => {
       mockSearchState,
       mockAccessibilityFilter,
       4,
-      'SearchResults'
+      SearchView.Results
+    )
+  })
+
+  it('should log PerformSearch with ThematicSearch when previous route is ThematicSearch', async () => {
+    mockPreviousRoute = SearchView.Thematic
+
+    const { rerender } = renderSearchResultContent({
+      ...DEFAULT_SEARCH_RESULT_CONTENT_PROPS,
+      isLoading: true,
+    })
+
+    mockUseSearch.mockReturnValueOnce({
+      searchState: mockSearchState,
+      dispatch: mockDispatch,
+    })
+
+    const mockAccessibilityFilter = {
+      isAudioDisabilityCompliant: undefined,
+      isMentalDisabilityCompliant: undefined,
+      isMotorDisabilityCompliant: undefined,
+      isVisualDisabilityCompliant: undefined,
+    }
+    rerender({ ...DEFAULT_SEARCH_RESULT_CONTENT_PROPS, isLoading: false })
+
+    expect(analytics.logPerformSearch).toHaveBeenNthCalledWith(
+      1,
+      mockSearchState,
+      mockAccessibilityFilter,
+      4,
+      SearchView.Thematic
     )
   })
 
