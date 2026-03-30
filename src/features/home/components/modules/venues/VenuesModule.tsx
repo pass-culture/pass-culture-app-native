79122ca6e33702fc972477a2c08512c75b39f3c4
clesausse-pass <187269096+clesausse-pass@users.noreply.github.com>
2026-03-30 11:26:21 +0200

    (PC-40475) feat(home): add feedback component below volunteering venue playlist

--- src/features/home/components/modules/venues/VenuesModule.tsx
+++ src/features/home/components/modules/venues/VenuesModule.tsx
@@ -1,9 +1,11 @@
 import React, { useCallback, useEffect } from 'react'
 import { ViewToken } from 'react-native'
 import { useTheme } from 'styled-components'
+import { styled } from 'styled-components/native'
 
 import { VenueTile } from 'features/home/components/modules/venues/VenueTile'
 import { ModuleData } from 'features/home/types'
+import { FeedBack } from 'features/reactions/components/FeedBack'
 import { VenueHit } from 'libs/algolia/types'
 import { analytics } from 'libs/analytics/provider'
 import { ContentTypes, DisplayParametersFields } from 'libs/contentful/types'
@@ -12,6 +14,7 @@ import { RemoteStoreFeatureFlags } from 'libs/firebase/firestore/types'
 import { ObservedPlaylist } from 'shared/ObservedPlaylist/ObservedPlaylist'
 import { PassPlaylist } from 'ui/components/PassPlaylist'
 import { CustomListRenderItem } from 'ui/components/Playlist'
+import { ViewGap } from 'ui/components/ViewGap/ViewGap'
 import { LENGTH_S } from 'ui/theme'
 
 type VenuesModuleProps = {
@@ -37,6 +40,9 @@ export const VenuesModule = ({
   onViewableItemsChanged,
 }: VenuesModuleProps) => {
   const enableVolunteerNewTag = useFeatureFlag(RemoteStoreFeatureFlags.WIP_ENABLE_VOLUNTEER_NEW_TAG)
+  const enableVolunteerFeedback = useFeatureFlag(
+    RemoteStoreFeatureFlags.WIP_ENABLE_VOLUNTEER_FEEDBACK
+  )
   const moduleName = displayParameters.title
   const { playlistItems = [] } = data ?? { playlistItems: [] }
   const { designSystem } = useTheme()
@@ -57,6 +63,7 @@ export const VenuesModule = ({
   const shouldModuleBeDisplayed = playlistItems.length > displayParameters.minOffers
   const isExclusiveVolunteering = displayParameters.isExclusiveVolunteering ?? false
   const showNewTag = enableVolunteerNewTag && isExclusiveVolunteering
+  const showFeedback = enableVolunteerFeedback && isExclusiveVolunteering
 
   useEffect(() => {
     if (shouldModuleBeDisplayed) {
@@ -74,24 +81,47 @@ export const VenuesModule = ({
   if (!shouldModuleBeDisplayed) return null
 
   return (
-    <ObservedPlaylist onViewableItemsChanged={onViewableItemsChanged}>
-      {({ listRef, handleViewableItemsChanged }) => (
-        <PassPlaylist
-          title={displayParameters.title}
-          subtitle={displayParameters.subtitle}
-          data={playlistItems || []}
-          itemHeight={ITEM_HEIGHT}
-          itemWidth={ITEM_WIDTH}
-          renderItem={renderItem}
-          keyExtractor={keyExtractor}
-          tileType="venue"
-          withMargin
-          contentContainerStyle={{ paddingHorizontal: designSystem.size.spacing.xl }}
-          onViewableItemsChanged={handleViewableItemsChanged}
-          playlistRef={listRef}
-          showNewTag={showNewTag}
+    <Container gap={4}>
+      <ObservedPlaylist onViewableItemsChanged={onViewableItemsChanged}>
+        {({ listRef, handleViewableItemsChanged }) => (
+          <PassPlaylist
+            title={displayParameters.title}
+            subtitle={displayParameters.subtitle}
+            data={playlistItems || []}
+            itemHeight={ITEM_HEIGHT}
+            itemWidth={ITEM_WIDTH}
+            renderItem={renderItem}
+            keyExtractor={keyExtractor}
+            tileType="venue"
+            withMargin
+            contentContainerStyle={{ paddingHorizontal: designSystem.size.spacing.xl }}
+            onViewableItemsChanged={handleViewableItemsChanged}
+            playlistRef={listRef}
+            showNewTag={showNewTag}
+            noMarginBottom
+          />
+        )}
+      </ObservedPlaylist>
+      {showFeedback ? (
+        <StyledFeedBack
+          storageKey="volunteering_feedback"
+          likeQuiz="https://passculture.qualtrics.com/jfe/form/SV_3sGi4gI6EEOmfsy"
+          dislikeQuiz="https://passculture.qualtrics.com/jfe/form/SV_3sGi4gI6EEOmfsy"
+          title="Le bénévolat sur le pass t’intéresse t-il&nbsp;?"
+          // TODO(PC-40467): add tracking
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          onLogReaction={() => {}}
         />
-      )}
-    </ObservedPlaylist>
+      ) : null}
+    </Container>
   )
 }
+
+const Container = styled(ViewGap)(({ theme }) => ({
+  paddingBottom: theme.home.spaceBetweenModules,
+}))
+
+const StyledFeedBack = styled(FeedBack)(({ theme }) => ({
+  marginHorizontal: theme.designSystem.size.spacing.xl,
+  width: theme.isDesktopViewport ? '50%' : 'auto',
+}))
