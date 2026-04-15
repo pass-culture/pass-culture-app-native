b0ccf9410526fbf1dfba896ece8ed6428d113cda
aminepass <155538488+aminepass@users.noreply.github.com>
2026-04-15 15:50:05 +0200

    (BSR) ci(tests) add vitest

---
+++ vitest.config.js
@@ -0,0 +1,87 @@
+const { excludeCollectCoverageFrom } = require('./jest.excludeCollectCoverageFrom.config')
+
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+    'android.tsx',
+    'android.ts',
+    'android.jsx',
+    'android.js',
+  ],
+  testEnvironmentOptions: { customExportConditions: [''] },
+  moduleNameMapper: {
+    // if you change those lines, check this doc https://github.com/pass-culture/pass-culture-app-native/blob/5ff5fba596244a759d60f8c9cdb67d56ac86a1a7/doc/development/alias.md
+    '^__mocks__(.*)$': '<rootDir>/__mocks__$1',
+    '^api(.*)$': '<rootDir>/src/api$1',
+    '^cheatcodes(.*)$': '<rootDir>/src/cheatcodes$1',
+    '^features(.*)$': '<rootDir>/src/features$1',
+    '^fixtures(.*)$': '<rootDir>/src/fixtures$1',
+    '^libs(.*)$': '<rootDir>/src/libs$1',
+    '^performance(.*)$': '<rootDir>/src/performance$1',
+    '^queries(.*)$': '<rootDir>/src/queries$1',
+    '^shared(.*)$': '<rootDir>/src/shared$1',
+    '^store(.*)$': '<rootDir>/src/store$1',
+    '^tests(.*)$': '<rootDir>/src/tests$1',
+    '^theme(.*)$': '<rootDir>/src/theme$1',
+    '^types(.*)$': '<rootDir>/src/types$1',
+    '^ui(.*)$': '<rootDir>/src/ui$1',
+    '^algoliasearch/lite$': '<rootDir>/__mocks__/algoliasearch.ts',
+  },
+  snapshotResolver: '<rootDir>/jest/custom-snapshot-resolver-native.js',
+  setupFiles: [
+    '<rootDir>/jest/jest.setup.ts',
+    'react-native-gesture-handler/jestSetup.js',
+    '@react-native-google-signin/google-signin/jest/build/setup.js',
+  ],
+  setupFilesAfterEnv: ['./src/tests/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!' + // Start ignoring node_modules unless it's one of the following:
+      [
+        '(jest-)?react-native',
+        '@react-navigation',
+        '@react-native',
+        '@ptomasroos/react-native-multi-slider',
+        '@react-native-firebase/analytics',
+        '@react-native-firebase/app',
+        '@react-native-firebase/remote-config',
+        '@sentry/react-native',
+        'react-native-geolocation-service',
+        'instantsearch.js',
+        '@pass-culture/design-system',
+        'search-insights',
+        '@d11/react-native-fast-image',
+        '@gorhom/bottom-sheet',
+      ].join('|') +
+      ').+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+  ],
+  testMatch: ['**/*(?<!.(web|perf)).(?:test|spec).[jt]s?(x)'],
+  testPathIgnorePatterns: [
+    '\\.snap$',
+    '\\.native-snap$',
+    '\\.web-snap$',
+    '<rootDir>/node_modules/',
+    '<rootDir>/server/',
+  ],
+  cacheDirectory: '.jest/cache',
+  clearMocks: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    'eslint-custom-rules/*.{js,jsx,ts,tsx}',
+    '!src/**/*.web.{js,jsx,ts,tsx}',
+    ...excludeCollectCoverageFrom,
+  ],
+  coveragePathIgnorePatterns: ['\\.web\\.(test|spec)', '/node_modules/', '/src/environment'],
+  coverageReporters: process.env.CI === 'true' ? ['json'] : ['text'],
+  collectCoverage: false,
+  // TODO(PC-20887): Investigate how to avoid timeouts in CI without increasing default timeout
+  testTimeout: 10_000,
+}
---
+++ vitest.web.config.js
@@ -0,0 +1,60 @@
+const base = require('./jest.config')
+const { excludeCollectCoverageFrom } = require('./jest.excludeCollectCoverageFrom.config')
+
+module.exports = {
+  ...base,
+  preset: '',
+  // Since RN0.73 we use this workaround : https://github.com/mswjs/jest-fixed-jsdom
+  testEnvironment: 'jest-fixed-jsdom',
+  snapshotResolver: '<rootDir>/jest/custom-snapshot-resolver-web.js',
+  setupFiles: [...base.setupFiles, '<rootDir>/jest/jest.web.setup.ts'],
+  setupFilesAfterEnv: [...base.setupFilesAfterEnv, '<rootDir>/jest/jest.web.setupAfterEnv.ts'],
+  testMatch: ['**/*(?<!.(?:native|ios|android|perf)).(?:test|spec).[jt]s?(x)'],
+  moduleFileExtensions: ['web.tsx', 'web.ts', 'web.js', 'web.jsx', ...base.moduleFileExtensions],
+  moduleNameMapper: {
+    ...base.moduleNameMapper,
+    '^react-native$': 'react-native-web',
+    '^react-native-svg$': 'react-native-svg-web',
+    '^react-native-linear-gradient$': 'react-native-web-linear-gradient',
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.ts',
+
+    // temporary mock things until implemented
+    '@d11/react-native-fast-image': '<rootDir>/__mocks__/fragmentMock.ts',
+  },
+  collectCoverageFrom: ['src/**/*.web.{js,jsx,ts,tsx}', ...excludeCollectCoverageFrom],
+  testPathIgnorePatterns: [
+    ...base.testPathIgnorePatterns,
+    '.*(/tests?/.*.(native|ios|android).(test|spec)).(tsx?)$',
+  ],
+  transform: { ...base.transform },
+  transformIgnorePatterns: [
+    'node_modules/(?!' +
+      'firebase' +
+      '|@firebase' +
+      '|@ptomasroos/react-native-multi-slider' +
+      '|react-native-svg-web' +
+      '|react-native-animatable' +
+      '|react-native-web' +
+      '|react-native-modal' +
+      '|react-native-calendars' +
+      '|react-native-swipe-gestures' +
+      '|react-native-permissions' +
+      '|react-native-qrcode-svg' +
+      '|instantsearch.js' +
+      '|react-native-intersection-observer' +
+      '|react-native-reanimated' +
+      '|react-native-worklets' +
+      '|@pass-culture/design-system' +
+      '|search-insights' +
+      '|react-native-gesture-handler' +
+      '|@react-navigation' +
+      ')',
+  ],
+  coverageReporters: ['json'],
+  verbose: true,
+  globals: {
+    __DEV__: true,
+  },
+}
