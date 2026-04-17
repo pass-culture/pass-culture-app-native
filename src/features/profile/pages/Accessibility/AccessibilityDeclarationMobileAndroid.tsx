75935acb349dbcf2ac4feb570710aca5064962a8
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2026-04-17 12:47:39 +0200

    (PC-41357) feat(a11y): update accessibility declaration mobile (#9525)

--- src/features/profile/pages/Accessibility/AccessibilityDeclarationMobileAndroid.tsx
+++ src/features/profile/pages/Accessibility/AccessibilityDeclarationMobileAndroid.tsx
@@ -6,21 +6,18 @@ import { env } from 'libs/environment/env'
 export function AccessibilityDeclarationMobileAndroid() {
   return (
     <AccessibilityDeclarationMobileBase
-      appVersion="1.374.0"
+      appVersion="1.386.1"
       platformName="Android"
       osVersion="15"
       storeLink={{ url: `https://play.google.com/store/apps/details?id=${env.ANDROID_APP_ID}` }}
-      auditDate="02/02/2026"
-      conformityEN="56,25%"
-      conformityRAAM="76,74%"
-      averageConformityRAAM="95.25%"
+      auditDate="16/04/2026"
+      conformityEN="72,92%"
+      conformityRAAM="83,72%"
+      averageConformityRAAM="96,52%"
       nonAccessibleContent={[
         'Critère 3.5 - Chaque média temporel synchronisé pré-enregistré a-t-il, si nécessaire, une alternative\u00a0?',
         'Critère 3.7 - Chaque média temporel synchronisé pré-enregistré a-t-il, si nécessaire, des sous-titres synchronisés\u00a0?',
-        'Critère 3.8.a - Pour chaque média temporel synchronisé pré-enregistré ayant des sous-titres synchronisés, ceux-ci sont-ils pertinents\u00a0?',
-        'Critère 5.1.a - a) Chaque composant d’interface est-il, si nécessaire, compatible avec les technologies d’assistance\u00a0?',
         'Critère 5.4 - Dans chaque écran, les messages de statut sont-ils correctement restitués par les technologies d’assistance\u00a0?',
-        'Critère 8.2 - Dans chaque écran, l’utilisateur peut-il augmenter la taille des caractères de 200% au moins\u00a0?',
         'Critère 9.11 - Pour chaque formulaire qui modifie ou supprime des données, ou qui transmet des réponses à un test ou à un examen, ou dont la validation a des conséquences financières ou juridiques, les données saisies peuvent-elles être modifiées, mises à jour ou récupérées par l’utilisateur\u00a0?',
         'Critère 10.2 - Dans chaque écran, l’ordre de restitution par les technologies d’assistance est-il cohérent\u00a0?',
         'Critère 11.10 - Dans chaque écran, les fonctionnalités activables au moyen d’un geste complexe sont-elles activables au moyen d’un geste simple\u00a0?',
