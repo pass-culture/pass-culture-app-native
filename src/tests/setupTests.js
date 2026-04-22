44c08c9be9c1b82f6e587985e7d9a4be91b0495d
bpeyrou-pass <bruno.peyrou@passculture.app>
2026-04-22 17:32:28 +0200

    (PC-41373) build(yarn): update dep (#9549)
    * (PC-41373) build(yarn): update dep
    
    * (PC-41373) build(yarn): remove reassure lib after performances removed

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,14 +1,13 @@
+/* eslint-disable no-restricted-imports */
+import '@testing-library/react-native'
+
 import '@testing-library/jest-native/extend-expect'
 
 import { TextEncoder } from 'util'
 
 import { toHaveNoViolations } from 'jest-axe'
-import { configure } from 'reassure'
 
 import { queryCache, mutationCache } from './reactQueryProviderHOC'
-
-// Configuration for performance tests
-configure({ testingLibrary: 'react-native' })
 
 global.expect.extend(toHaveNoViolations)
 global.TextEncoder = TextEncoder
