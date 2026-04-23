28326c88cb83541489ba5a35646d92162b27be22
clesausse2-pass <277683118+clesausse2-pass@users.noreply.github.com>
2026-04-23 09:45:23 +0200

    (PC-40648) refactor(home): remove useless code

--- src/features/home/components/modules/video/types.ts
+++ src/features/home/components/modules/video/types.ts
@@ -1,13 +1,4 @@
-import { YouTubePlayer } from 'react-youtube'
-
-import { VideoModule } from 'features/home/types'
 import { Offer } from 'shared/offer/types'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export declare type YouTubeEvent<T = any> = {
-  data: T
-  target: YouTubePlayer
-}
 
 export enum PlayerState {
   UNSTARTED = 'unstarted',
@@ -20,17 +11,7 @@ export enum PlayerState {
 export interface VideoPlayerProps {
   youtubeVideoId: string
   offer?: Offer
-  onPressSeeOffer: () => void
   moduleId: string
   moduleName: string
   homeEntryId: string
-}
-
-export interface VideoModalProps extends VideoModule {
-  offers: Offer[]
-  visible: boolean
-  hideModal: () => void
-  moduleId: string
-  homeEntryId: string
-  isMultiOffer: boolean
 }
