6c3c918d65c2cd74dab8b958001c9d2d706d678e
Inès Mouandjo Lobé <158567429+imouandjolobe-pass@users.noreply.github.com>
2026-04-23 16:30:53 +0200

    fix tests

--- src/features/trustedDevice/helpers/getDeviceInfo.test.ts
+++ src/features/trustedDevice/helpers/getDeviceInfo.test.ts
@@ -1,6 +1,14 @@
 import { getDeviceInfo } from 'features/trustedDevice/helpers/getDeviceInfo'
 
-jest.mock('libs/react-native-device-info/getDeviceId')
+jest.mock('libs/react-native-device-info/getDeviceId', () => ({
+  getDeviceId: jest.fn(() => 'ad7b7b5a169641e27cadbdb35adad9c4ca23099a'),
+}))
+jest.mock('react-native-device-info', () => ({
+  getSystemName: jest.fn(() => 'iOS'),
+  getModel: jest.fn(() => 'iPhone 13'),
+  getBaseOs: jest.fn(() => Promise.resolve('iOS')),
+  isLandscape: jest.fn(() => false),
+}))
 
 describe('getDeviceInfo', () => {
   it('returns informations about the device', async () => {
