369b2c3622685ba2463395fe6f2d8ffcccddef81
Inès Mouandjo Lobé <158567429+imouandjolobe-pass@users.noreply.github.com>
2026-04-27 15:56:49 +0200

    update api contract

--- src/api/gen/api.ts
+++ src/api/gen/api.ts
@@ -1800,6 +1800,27 @@ export enum DepositType {
 }
 /**
  * @export
+ * @interface DeviceInfoV2
+ */
+export interface DeviceInfoV2 {
+  /**
+   * @type {string}
+   * @memberof DeviceInfoV2
+   */
+  deviceId: string
+  /**
+   * @type {string}
+   * @memberof DeviceInfoV2
+   */
+  os?: string | null
+  /**
+   * @type {string}
+   * @memberof DeviceInfoV2
+   */
+  source?: string | null
+}
+/**
+ * @export
  * @interface DomainsCredit
  */
 export interface DomainsCredit {
@@ -3742,6 +3763,17 @@ export enum RecreditType {
 }
 /**
  * @export
+ * @interface RefreshRequestV2
+ */
+export interface RefreshRequestV2 {
+  /**
+   * @type {DeviceInfoV2}
+   * @memberof RefreshRequestV2
+   */
+  deviceInfo: DeviceInfoV2
+}
+/**
+ * @export
  * @interface RefreshResponse
  */
 export interface RefreshResponse {
@@ -3750,6 +3782,22 @@ export interface RefreshResponse {
    * @memberof RefreshResponse
    */
   accessToken: string
+}
+/**
+ * @export
+ * @interface RefreshResponseV2
+ */
+export interface RefreshResponseV2 {
+  /**
+   * @type {string}
+   * @memberof RefreshResponseV2
+   */
+  accessToken: string
+  /**
+   * @type {string}
+   * @memberof RefreshResponseV2
+   */
+  refreshToken: string
 }
 /**
  * @export
@@ -4041,6 +4089,32 @@ export interface SigninRequest {
 }
 /**
  * @export
+ * @interface SigninRequestV2
+ */
+export interface SigninRequestV2 {
+  /**
+   * @type {DeviceInfoV2}
+   * @memberof SigninRequestV2
+   */
+  deviceInfo: DeviceInfoV2
+  /**
+   * @type {string}
+   * @memberof SigninRequestV2
+   */
+  identifier: string
+  /**
+   * @type {string}
+   * @memberof SigninRequestV2
+   */
+  password: string
+  /**
+   * @type {string}
+   * @memberof SigninRequestV2
+   */
+  token?: string | null
+}
+/**
+ * @export
  * @interface SigninResponse
  */
 export interface SigninResponse {
@@ -4057,6 +4131,27 @@ export interface SigninResponse {
   /**
    * @type {string}
    * @memberof SigninResponse
+   */
+  refreshToken: string
+}
+/**
+ * @export
+ * @interface SigninResponseV2
+ */
+export interface SigninResponseV2 {
+  /**
+   * @type {string}
+   * @memberof SigninResponseV2
+   */
+  accessToken: string
+  /**
+   * @type {AccountState}
+   * @memberof SigninResponseV2
+   */
+  accountState: AccountState
+  /**
+   * @type {string}
+   * @memberof SigninResponseV2
    */
   refreshToken: string
 }
@@ -7406,6 +7501,60 @@ export const DefaultApiFetchParamCreator = function (configuration?: Configurati
       }
     },
     /**
+     * @summary refresh <POST>
+     * @param {RefreshRequestV2} body 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    async postNativeV2RefreshAccessToken(body: RefreshRequestV2, options: any = {}): Promise<FetchArgs> {
+      // verify required parameter 'body' is not null or undefined
+      if (body === null || body === undefined) {
+        throw new RequiredError(
+          'body',
+          'Required parameter body was null or undefined when calling postNativeV2RefreshAccessToken.'
+        )
+      }      
+      const pathname = `/native/v2/refresh_access_token`
+      const secureOptions = Object.assign(options, { credentials: 'omit' })
+      const localVarRequestOptions = Object.assign({ method: 'POST' }, secureOptions)
+      const localVarHeaderParameter = await getAuthenticationHeaders(secureOptions)
+      localVarHeaderParameter['Content-Type'] = 'application/json'
+      localVarRequestOptions.headers = Object.assign({}, localVarHeaderParameter, options.headers)
+      const needsSerialization = (<any>"RefreshRequestV2" !== "string") || localVarRequestOptions.headers['Content-Type'] === 'application/json'
+      localVarRequestOptions.body =  needsSerialization ? JSON.stringify(body || {}) : (body || "")
+      return {
+        url: pathname,
+        options: localVarRequestOptions,
+      }
+    }, 
+    /**
+     * @summary signin <POST>
+     * @param {SigninRequestV2} body 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    async postNativeV2Signin(body: SigninRequestV2, options: any = {}): Promise<FetchArgs> {
+      // verify required parameter 'body' is not null or undefined
+      if (body === null || body === undefined) {
+        throw new RequiredError(
+          'body',
+          'Required parameter body was null or undefined when calling postNativeV2Signin.'
+        )
+      }      
+      const pathname = `/native/v2/signin`
+      const secureOptions = Object.assign(options, { credentials: 'omit' })
+      const localVarRequestOptions = Object.assign({ method: 'POST' }, secureOptions)
+      const localVarHeaderParameter = await getAuthenticationHeaders(secureOptions)
+      localVarHeaderParameter['Content-Type'] = 'application/json'
+      localVarRequestOptions.headers = Object.assign({}, localVarHeaderParameter, options.headers)
+      const needsSerialization = (<any>"SigninRequestV2" !== "string") || localVarRequestOptions.headers['Content-Type'] === 'application/json'
+      localVarRequestOptions.body =  needsSerialization ? JSON.stringify(body || {}) : (body || "")
+      return {
+        url: pathname,
+        options: localVarRequestOptions,
+      }
+    },
+    /**
      * @summary validate_user_email <PUT>
      * @param {ChangeBeneficiaryEmailBody} body 
      * @param {*} [options] Override http request option.
@@ -8369,6 +8518,30 @@ export const DefaultApiFp = function(api: DefaultApi, configuration?: Configurat
     },
     /**
      * 
+     * @summary refresh <POST>
+     * @param {RefreshRequestV2} body 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    async postNativeV2RefreshAccessToken(body: RefreshRequestV2, options?: any): Promise<RefreshResponseV2> {
+      const localVarFetchArgs = await DefaultApiFetchParamCreator(configuration).postNativeV2RefreshAccessToken(body, options)
+      const response = await safeFetch(configuration?.basePath + localVarFetchArgs.url, localVarFetchArgs.options, api)
+      return handleGeneratedApiResponse(response)
+    },
+    /**
+     * 
+     * @summary signin <POST>
+     * @param {SigninRequestV2} body 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    async postNativeV2Signin(body: SigninRequestV2, options?: any): Promise<SigninResponseV2> {
+      const localVarFetchArgs = await DefaultApiFetchParamCreator(configuration).postNativeV2Signin(body, options)
+      const response = await safeFetch(configuration?.basePath + localVarFetchArgs.url, localVarFetchArgs.options, api)
+      return handleGeneratedApiResponse(response)
+    },
+    /**
+     * 
      * @summary validate_user_email <PUT>
      * @param {ChangeBeneficiaryEmailBody} body 
      * @param {*} [options] Override http request option.
@@ -9306,6 +9479,30 @@ export class DefaultApi extends BaseAPI {
   public async postNativeV2ProfileUpdateEmail(options?: any) {
     const configuration = this.getConfiguration()
     return DefaultApiFp(this, configuration).postNativeV2ProfileUpdateEmail(options)
+  }
+  /**
+    * 
+    * @summary refresh <POST>
+    * @param {RefreshRequestV2} body 
+    * @param {*} [options] Override http request option.
+    * @throws {RequiredError}
+    * @memberof DefaultApi
+    */
+  public async postNativeV2RefreshAccessToken(body: RefreshRequestV2, options?: any) {
+    const configuration = this.getConfiguration()
+    return DefaultApiFp(this, configuration).postNativeV2RefreshAccessToken(body, options)
+  }
+  /**
+    * 
+    * @summary signin <POST>
+    * @param {SigninRequestV2} body 
+    * @param {*} [options] Override http request option.
+    * @throws {RequiredError}
+    * @memberof DefaultApi
+    */
+  public async postNativeV2Signin(body: SigninRequestV2, options?: any) {
+    const configuration = this.getConfiguration()
+    return DefaultApiFp(this, configuration).postNativeV2Signin(body, options)
   }
   /**
     * 
