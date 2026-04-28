464da50309a5cd359455e1e334789ef7d60104d0
xdekoninck-pass <167757543+xdekoninck-pass@users.noreply.github.com>
2026-04-28 09:04:26 +0200

    (PC-40906) fix(apptheme): move the stats after firebase initialization

--- src/libs/styled/ThemeWrapper.tsx
+++ src/libs/styled/ThemeWrapper.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 import { ThemeProvider } from 'libs/styled'
 import { useColorScheme } from 'libs/styled/useColorScheme'
-import { useLogAppThemeStatusOnLaunch } from 'libs/styled/useLogAppThemeStatusOnLaunch'
 // eslint-disable-next-line local-rules/no-theme-from-theme
 import { theme } from 'theme'
 
 export const ThemeWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const colorScheme = useColorScheme()
-  useLogAppThemeStatusOnLaunch()
 
   return (
     <ThemeProvider theme={theme} colorScheme={colorScheme}>
