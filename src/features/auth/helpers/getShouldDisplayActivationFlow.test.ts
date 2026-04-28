0f38c34c93780e759ad4ace9549bdb1bfc1791a5
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2026-04-28 17:12:31 +0200

    fix cases when activation flow should be displayed

--- src/features/auth/helpers/getShouldDisplayActivationFlow.test.ts
+++ src/features/auth/helpers/getShouldDisplayActivationFlow.test.ts
@@ -8,17 +8,28 @@ import { getShouldDisplayActivationFlow } from './getShouldDisplayActivationFlow
 describe('getShouldDisplayActivationFlow', () => {
   describe('mapped eligibility rules', () => {
     it.each([
-      [UserEligibilityType.ELIGIBLE_CREDIT_V2_15, UserCreditType.CREDIT_UNKNOWN, true],
       [UserEligibilityType.ELIGIBLE_CREDIT_V2_15, UserCreditType.CREDIT_V2_15, false],
-      [UserEligibilityType.ELIGIBLE_CREDIT_V2_16, UserCreditType.CREDIT_UNKNOWN, true],
-      [UserEligibilityType.ELIGIBLE_CREDIT_V2_16, UserCreditType.CREDIT_V2_15, true],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V2_16, UserCreditType.CREDIT_V2_15, false],
       [UserEligibilityType.ELIGIBLE_CREDIT_V2_16, UserCreditType.CREDIT_V2_16, false],
-      [UserEligibilityType.ELIGIBLE_CREDIT_V2_17, UserCreditType.CREDIT_V2_16, true],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V2_17, UserCreditType.CREDIT_V2_15, false],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V2_17, UserCreditType.CREDIT_V2_16, false],
       [UserEligibilityType.ELIGIBLE_CREDIT_V2_17, UserCreditType.CREDIT_V2_17, false],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V2_18, UserCreditType.CREDIT_V2_15, true],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V2_18, UserCreditType.CREDIT_V2_16, true],
       [UserEligibilityType.ELIGIBLE_CREDIT_V2_18, UserCreditType.CREDIT_V2_17, true],
       [UserEligibilityType.ELIGIBLE_CREDIT_V2_18, UserCreditType.CREDIT_V2_18, false],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_16, UserCreditType.CREDIT_V3_FREE, false],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_17, UserCreditType.CREDIT_V3_FREE, true],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_17, UserCreditType.CREDIT_V2_15, false],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_17, UserCreditType.CREDIT_V2_16, false],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_17, UserCreditType.CREDIT_V2_17, false],
       [UserEligibilityType.ELIGIBLE_CREDIT_V3_17, UserCreditType.CREDIT_V3_FREE, true],
       [UserEligibilityType.ELIGIBLE_CREDIT_V3_17, UserCreditType.CREDIT_V3_17, false],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_18, UserCreditType.CREDIT_V2_15, true],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_18, UserCreditType.CREDIT_V2_16, true],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_18, UserCreditType.CREDIT_V2_17, true],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_18, UserCreditType.CREDIT_V2_18, false],
+      [UserEligibilityType.ELIGIBLE_CREDIT_V3_18, UserCreditType.CREDIT_V3_FREE, true],
       [UserEligibilityType.ELIGIBLE_CREDIT_V3_18, UserCreditType.CREDIT_V3_17, true],
       [UserEligibilityType.ELIGIBLE_CREDIT_V3_18, UserCreditType.CREDIT_V3_18, false],
     ])('for eligibility=%s and credit=%s returns %s', (eligibilityType, creditType, expected) => {
@@ -28,46 +39,6 @@ describe('getShouldDisplayActivationFlow', () => {
           eligibilityType,
           creditType,
           statusType: UserStatusType.UNKNOWN,
-        })
-      ).toBe(expected)
-    })
-  })
-
-  describe('CREDIT_UNKNOWN', () => {
-    it.each([
-      UserEligibilityType.NOT_ELIGIBLE,
-      UserEligibilityType.ELIGIBLE_BONUS,
-      UserEligibilityType.ELIGIBLE_CREDIT_V1_18,
-      UserEligibilityType.ELIGIBLE_CREDIT_V3_15,
-      UserEligibilityType.ELIGIBLE_CREDIT_V3_16,
-    ])('returns false when eligibility=%s when CREDIT_UNKNOWN', (eligibilityType) => {
-      expect(
-        getShouldDisplayActivationFlow({
-          ...nonBeneficiaryUserV2,
-          eligibilityType,
-          creditType: UserCreditType.CREDIT_UNKNOWN,
-          statusType: UserStatusType.ELIGIBLE,
-        })
-      ).toBe(false)
-    })
-  })
-
-  describe('CREDIT_EMPTY', () => {
-    it.each([
-      [UserStatusType.ELIGIBLE, true],
-      [UserStatusType.ELIGIBLE_AND_BENEFICIARY, true],
-      [UserStatusType.BENEFICIARY, false],
-      [UserStatusType.EX_BENEFICIARY, false],
-      [UserStatusType.GENERAL_PUBLIC, false],
-      [UserStatusType.SUSPENDED, false],
-      [UserStatusType.UNKNOWN, false],
-    ])('returns %s when CREDIT_EMPTY statusType=%s', (statusType, expected) => {
-      expect(
-        getShouldDisplayActivationFlow({
-          ...nonBeneficiaryUserV2,
-          eligibilityType: UserEligibilityType.NOT_ELIGIBLE,
-          creditType: UserCreditType.CREDIT_EMPTY,
-          statusType,
         })
       ).toBe(expected)
     })
--- src/features/auth/helpers/getShouldDisplayActivationFlow.ts
+++ src/features/auth/helpers/getShouldDisplayActivationFlow.ts
@@ -1,21 +1,13 @@
 import { UserCreditType } from 'features/auth/helpers/getCreditType'
 import { UserEligibilityType } from 'features/auth/helpers/getEligibilityType'
-import { UserStatusType } from 'features/auth/helpers/getStatusType'
 import { UserProfile } from 'features/share/types'
 
 const activationFlowByEligibility: Partial<
   Record<UserEligibilityType, ReadonlyArray<UserCreditType>>
 > = {
   [UserEligibilityType.ELIGIBLE_CREDIT_V2_15]: [UserCreditType.CREDIT_UNKNOWN],
-  [UserEligibilityType.ELIGIBLE_CREDIT_V2_16]: [
-    UserCreditType.CREDIT_UNKNOWN,
-    UserCreditType.CREDIT_V2_15,
-  ],
-  [UserEligibilityType.ELIGIBLE_CREDIT_V2_17]: [
-    UserCreditType.CREDIT_UNKNOWN,
-    UserCreditType.CREDIT_V2_15,
-    UserCreditType.CREDIT_V2_16,
-  ],
+  [UserEligibilityType.ELIGIBLE_CREDIT_V2_16]: [UserCreditType.CREDIT_UNKNOWN],
+  [UserEligibilityType.ELIGIBLE_CREDIT_V2_17]: [UserCreditType.CREDIT_UNKNOWN],
   [UserEligibilityType.ELIGIBLE_CREDIT_V2_18]: [
     UserCreditType.CREDIT_UNKNOWN,
     UserCreditType.CREDIT_V2_15,
@@ -24,8 +16,6 @@ const activationFlowByEligibility: Partial<
   ],
   [UserEligibilityType.ELIGIBLE_CREDIT_V3_17]: [
     UserCreditType.CREDIT_UNKNOWN,
-    UserCreditType.CREDIT_V2_15,
-    UserCreditType.CREDIT_V2_16,
     UserCreditType.CREDIT_V3_FREE,
   ],
   [UserEligibilityType.ELIGIBLE_CREDIT_V3_18]: [
@@ -38,17 +28,9 @@ const activationFlowByEligibility: Partial<
   ],
 }
 
-const statusAllowedForEmptyCredit = new Set<UserStatusType>([
-  UserStatusType.ELIGIBLE,
-  UserStatusType.ELIGIBLE_AND_BENEFICIARY,
-])
-
 export const getShouldDisplayActivationFlow = (user?: UserProfile | undefined) => {
   if (!user) return false
-  const { creditType, statusType, eligibilityType } = user
-  if (creditType === UserCreditType.CREDIT_EMPTY) {
-    return statusType ? statusAllowedForEmptyCredit.has(statusType) : false
-  }
+  const { creditType, eligibilityType } = user
 
   const authorizedCreditTypes = activationFlowByEligibility[eligibilityType]
   if (!authorizedCreditTypes) return false
