aa9f273eaef4f5ef7bd20b5452d93b71c20bb0d3
arenault-pass <153505101+arenault-pass@users.noreply.github.com>
2026-05-04 18:34:29 +0200

    (PC-41206) chore(e2e): configure V3 e2e archi and commons (#9540)
    * (PC-41206) chore(e2e): configure V3 e2e archi and commons
    
    * (PC-41206) chore(e2e): split onboardings flows
    
    * (PC-41206) chore(e2e): split cookies policies flows
    
    * (PC-41206) chore(e2e): split login flows
    
    * (PC-41206) chore(e2e): add first profil flow
    
    * (PC-41206) chore(e2e): add deeplink to profile subflow and profile folder
    
    * (PC-41206) chore(e2e): split signup flows in smaller test case
    
    * (PC-41206) chore(e2e): split profile flows
    
    * (PC-41206) chore(e2e): split searchJourney flows
    
    * (PC-41206) chore(e2e): split booking flows
    
    * (PC-41206) fix(e2e): adapt profile flows for iOS
    
    * (PC-41206) chore(e2e): add artist flows and re-organise tests folder
    
    * (PC-41206) chore(e2e): split UnloggedJourney flow
    
    * (PC-41206) review: delete Button.js and harmonize deeplinks files

---
+++ .maestro/testsV3/common/DeepLink.js
@@ -0,0 +1,31 @@
+output.links = {
+  staging: {
+    android: {
+      home: 'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Faccueil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
+      profile:
+        'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Fprofil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
+      thematicSearch:
+        'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Frecherche%2Fthematique%3FofferCategories%3D%5B%22CINEMA%22%5D%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
+      favorites:
+        'https://passcultureappstaging.page.link/?link=https%3A%2F%2Fapp.staging.passculture.team%2Ffavoris%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.staging&isi=1557887412&ibi=app.passculture.staging&efr=1',
+    },
+    ios: {
+      home: 'https://app.staging.passculture.team/accueil?from=deeplink&utm_gen=marketing',
+      profile: 'https://app.staging.passculture.team/profil?from=deeplink&utm_gen=marketing',
+      thematicSearch:
+        'https://app.staging.passculture.team/recherche/thematique?from=deeplink&offerCategories=%5B%22CINEMA%22%5D&utm_gen=marketing',
+      favorites: 'https://app.staging.passculture.team/favoris?from=deeplink&utm_gen=marketing',
+    },
+  },
+  testing: {
+    android: {
+      home: 'https://passcultureapptesting.page.link/?link=https%3A%2F%2Fapp.testing.passculture.team%2Faccueil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.testing&isi=1557887412&ibi=app.passculture.testing&efr=1',
+      profile:
+        'https://passcultureapptesting.page.link/?link=https%3A%2F%2Fapp.testing.passculture.team%2Fprofil%3Ffrom%3Ddeeplink%26utm_gen%3Dmarketing&apn=app.passculture.testing&isi=1557887412&ibi=app.passculture.testing&efr=1',
+    },
+    ios: {
+      home: 'https://app.testing.passculture.team/accueil?from=deeplink&utm_gen=marketing',
+      profile: 'https://app.testing.passculture.team/profil?from=deeplink&utm_gen=marketing',
+    },
+  },
+}
