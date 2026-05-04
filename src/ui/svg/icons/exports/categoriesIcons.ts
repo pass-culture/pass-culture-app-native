670ab75aeecc9408f9c1df5e61f44c55d441037f
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2026-05-04 14:00:24 +0200

    fix category

--- src/ui/svg/icons/exports/categoriesIcons.ts
+++ src/ui/svg/icons/exports/categoriesIcons.ts
@@ -4,26 +4,16 @@ import { Bookstore } from 'ui/svg/icons/venueAndCategories/Bookstore'
 import { Brush as Workshop } from 'ui/svg/icons/venueAndCategories/Brush'
 import { Card } from 'ui/svg/icons/venueAndCategories/Card'
 import { Cinema } from 'ui/svg/icons/venueAndCategories/Cinema'
-import { Comics } from 'ui/svg/icons/venueAndCategories/Comics'
-import { Detective } from 'ui/svg/icons/venueAndCategories/Detective'
 import { Disk } from 'ui/svg/icons/venueAndCategories/Disk'
-import { Electro } from 'ui/svg/icons/venueAndCategories/Electro'
-import { Fantasy } from 'ui/svg/icons/venueAndCategories/Fantasy'
 import { FineArts } from 'ui/svg/icons/venueAndCategories/FineArts'
-import { Jazz } from 'ui/svg/icons/venueAndCategories/Jazz'
-import { Litterature } from 'ui/svg/icons/venueAndCategories/Litterature'
 import { LiveEvent } from 'ui/svg/icons/venueAndCategories/LiveEvent'
 import { Micro as Conference } from 'ui/svg/icons/venueAndCategories/Micro'
 import { Microphone } from 'ui/svg/icons/venueAndCategories/Microphone'
 import { Museum } from 'ui/svg/icons/venueAndCategories/Museum'
 import { Music } from 'ui/svg/icons/venueAndCategories/Music'
 import { Palette } from 'ui/svg/icons/venueAndCategories/Palette'
-import { Philosophy } from 'ui/svg/icons/venueAndCategories/Philosophy'
 import { Piano as Instrument } from 'ui/svg/icons/venueAndCategories/Piano'
 import { Press } from 'ui/svg/icons/venueAndCategories/Press'
-import { Rap } from 'ui/svg/icons/venueAndCategories/Rap'
-import { Rock } from 'ui/svg/icons/venueAndCategories/Rock'
-import { Romance } from 'ui/svg/icons/venueAndCategories/Romance'
 import { Show } from 'ui/svg/icons/venueAndCategories/Show'
 import { Streaming } from 'ui/svg/icons/venueAndCategories/Streaming'
 import { Thing } from 'ui/svg/icons/venueAndCategories/Thing'
@@ -35,26 +25,16 @@ export const categoriesIcons = {
   Bookstore,
   Card,
   Cinema,
-  Comics,
   Conference,
-  Detective,
   Disk,
-  Electro,
-  Fantasy,
   FineArts,
   Instrument,
-  Jazz,
-  Litterature,
   LiveEvent,
   Microphone,
   Museum,
   Music,
   Palette,
-  Philosophy,
   Press,
-  Rap,
-  Rock,
-  Romance,
   Show,
   Streaming,
   Thing,
