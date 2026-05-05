dab4d253a1dcfadbb5511bac6c4f22e3294ab05f
imouandjolobe-pass <158567429+imouandjolobe-pass@users.noreply.github.com>
2026-05-05 16:07:01 +0200

    (PC-41616) fix(Home): remove unwanted margin (#9615)
    * (PC-41616) fix(Home): remove unwanted margin
    
    * update snapshots
    
    * dev review

--- src/ui/components/Playlist.tsx
+++ src/ui/components/Playlist.tsx
@@ -206,7 +206,7 @@ const InnerPlaylist = forwardRef<FlatList, Props>(function Playlist(props, ref) 
         updateCellsBatchingPeriod={100}
         getItemLayout={getItemLayout}
         ItemSeparatorComponent={MemoizedItemSeparatorComponent}
-        ListHeaderComponent={<HorizontalMargin width={withMargins ? horizontalMargin : 0} />}
+        ListHeaderComponent={withMargins ? MemoizedHorizontalMargin : undefined}
         ListFooterComponent={MemoizedHorizontalMargin}
         onEndReached={onEndReached}
         onEndReachedThreshold={0.2}
