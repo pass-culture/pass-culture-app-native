afadb4136a8cad604e9212409cab7e996ab1a6b2
arenault-pass <153505101+arenault-pass@users.noreply.github.com>
2026-05-12 13:35:34 +0200

    (PC-41762) fix(e2e): use secret to disable recaptcha in staging env (#9637)

--- scripts/enableNativeAppRecaptcha.ts
+++ scripts/enableNativeAppRecaptcha.ts
@@ -1,5 +1,10 @@
 /// <reference types="node" />
 
+import path from 'path'
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import dotenv from 'dotenv'
+
 import { getErrorMessage } from '../src/shared/getErrorMessage/getErrorMessage'
 
 type Feature = { name: string; isActive: boolean }
@@ -10,6 +15,14 @@ const RED = '\x1b[31m'
 const RESET_COLOR = '\x1b[0m'
 const API_BASE_URL = process.argv[2] ? `https://backend.${process.argv[2]}.passculture.team` : ''
 
+dotenv.config({ path: path.resolve(__dirname, '../.maestro/.env.secret') })
+
+const MAESTRO_E2E_API_KEY = process.env.MAESTRO_E2E_API_KEY
+
+if (!MAESTRO_E2E_API_KEY) {
+  throw new Error('Missing MAESTRO_E2E_API_KEY. Locally it is read from .maestro/.env.secret.')
+}
+
 async function patchFeatures(features: Features) {
   try {
     const response = await fetch(`${API_BASE_URL}/testing/features`, {
@@ -18,6 +31,7 @@ async function patchFeatures(features: Features) {
       headers: new Headers({
         'Content-Type': 'application/json',
         Accept: 'application/json',
+        'x-api-key': MAESTRO_E2E_API_KEY as string,
       }),
       body: JSON.stringify({
         features,
@@ -53,6 +67,6 @@ const enableNativeAppRecaptcha = async (isActive: boolean) => {
   ])
 }
 
-enableNativeAppRecaptcha(process.argv[3] as unknown as boolean)
+void enableNativeAppRecaptcha(process.argv[3] as unknown as boolean)
 
 export {}
