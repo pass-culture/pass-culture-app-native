319a1fa9a13eb859b2380ef6dcc5b91a42356c1b
xlecunff-pass <163895831+xlecunff-pass@users.noreply.github.com>
2026-05-26 11:39:11 +0200

    (PC-41954) refactor(location): move locationType logic to zustand (#9666)

--- src/libs/analytics/provider.test.ts
+++ src/libs/analytics/provider.test.ts
@@ -2,10 +2,12 @@
 import { resetDedupCache } from 'libs/analytics/eventDeduplication'
 // eslint-disable-next-line no-restricted-imports
 import { analytics } from 'libs/analytics/provider'
+import { LocationType } from 'libs/analytics/types'
 // eslint-disable-next-line no-restricted-imports
 import { firebaseAnalytics } from 'libs/firebase/analytics/analytics'
 import { AnalyticsEvent } from 'libs/firebase/analytics/events'
-import { storage } from 'libs/storage'
+import { LocationMode } from 'libs/location/types'
+import { locationActions } from 'libs/locationV2/location.store'
 import { act } from 'tests/utils'
 
 const EVENT_PARAMS = { param: 1 }
@@ -17,60 +19,41 @@ jest.mock('libs/firebase/analytics/analytics')
 
 describe('analyticsProvider - logEvent', () => {
   afterEach(() => {
-    storage.clear('location_type')
+    locationActions.setLocationMode(LocationMode.EVERYWHERE)
     resetDedupCache()
   })
 
   describe('with firebase', () => {
-    it('should log event when firebase event name is specified', async () => {
-      await analytics.logEvent({ firebase: AnalyticsEvent.CONSULT_OFFER }, EVENT_PARAMS)
-
-      expect(firebaseAnalytics.logEvent).toHaveBeenCalledWith(AnalyticsEvent.CONSULT_OFFER, {
-        ...EVENT_PARAMS,
-        locationType: 'undefined',
-      })
-    })
-
     it.each`
-      locationType
-      ${'UserGeolocation'}
-      ${'UserSpecificLocation'}
-    `('should log firebase event with locationType=$locationType', async ({ locationType }) => {
-      await storage.saveString('location_type', locationType)
+      locationType              | locationMode
+      ${'UserGeolocation'}      | ${LocationMode.AROUND_ME}
+      ${'UserSpecificLocation'} | ${LocationMode.AROUND_PLACE}
+      ${'undefined'}            | ${LocationMode.EVERYWHERE}
+    `(
+      'should log firebase event with locationType=$locationType and locationMode=$locationMode',
+      async ({
+        locationType,
+        locationMode,
+      }: {
+        locationType: LocationType
+        locationMode: LocationMode
+      }) => {
+        locationActions.setLocationMode(locationMode)
 
-      await analytics.logEvent({ firebase: AnalyticsEvent.CONSULT_OFFER }, EVENT_PARAMS)
+        await analytics.logEvent({ firebase: AnalyticsEvent.CONSULT_OFFER }, EVENT_PARAMS)
 
-      expect(firebaseAnalytics.logEvent).toHaveBeenCalledWith(AnalyticsEvent.CONSULT_OFFER, {
-        ...EVENT_PARAMS,
-        locationType: locationType,
-      })
-    })
-
-    it('should log firebase event with locationType: undefined when locationType is cleared', async () => {
-      await storage.clear('location_type')
-
-      await analytics.logEvent({ firebase: AnalyticsEvent.CONSULT_OFFER }, EVENT_PARAMS)
-
-      expect(firebaseAnalytics.logEvent).toHaveBeenCalledWith(AnalyticsEvent.CONSULT_OFFER, {
-        ...EVENT_PARAMS,
-        locationType: 'undefined',
-      })
-    })
+        expect(firebaseAnalytics.logEvent).toHaveBeenCalledWith(AnalyticsEvent.CONSULT_OFFER, {
+          ...EVENT_PARAMS,
+          locationType,
+        })
+      }
+    )
 
     it('should log screen view when logScreenView is called', async () => {
-      analytics.logScreenView(SCREEN_NAME)
+      await analytics.logScreenView(SCREEN_NAME)
       await act(() => {})
 
       expect(firebaseAnalytics.logScreenView).toHaveBeenCalledWith(SCREEN_NAME, 'undefined')
-    })
-
-    it('should set default event parameters when setEventLocationType is called', async () => {
-      analytics.setEventLocationType()
-      await act(() => {})
-
-      expect(firebaseAnalytics.setDefaultEventParameters).toHaveBeenCalledWith({
-        locationType: 'undefined',
-      })
     })
   })
 
@@ -90,16 +73,16 @@ describe('analyticsProvider - logEvent', () => {
     })
 
     it('should not send the same screen view twice within the dedup window', async () => {
-      analytics.logScreenView(SCREEN_NAME)
-      analytics.logScreenView(SCREEN_NAME)
+      await analytics.logScreenView(SCREEN_NAME)
+      await analytics.logScreenView(SCREEN_NAME)
       await act(() => {})
 
       expect(firebaseAnalytics.logScreenView).toHaveBeenCalledTimes(1)
     })
 
     it('should send different screen views', async () => {
-      analytics.logScreenView('Home')
-      analytics.logScreenView('Offer')
+      await analytics.logScreenView('Home')
+      await analytics.logScreenView('Offer')
       await act(() => {})
 
       expect(firebaseAnalytics.logScreenView).toHaveBeenCalledTimes(2)
