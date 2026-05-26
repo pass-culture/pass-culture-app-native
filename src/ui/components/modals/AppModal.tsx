c54766aeb556af455c516986f03e2e9f5478eee2
nmajorfrances-pass <144016890+nmajorfrances-pass@users.noreply.github.com>
2026-05-26 17:23:29 +0200

    (PC-42027) fix(modals): fix truncated bottom (#9685)

--- src/ui/components/modals/AppModal.tsx
+++ src/ui/components/modals/AppModal.tsx
@@ -340,7 +340,7 @@ const ScrollViewContainer = styled.View.attrs<{ backdropColor?: string }>(({ the
 }))<{ paddingBottom: number; modalSpacing?: ModalSpacing }>(({ paddingBottom, modalSpacing }) => ({
   width: '100%', // do not use `flex: 1` here if you want full width
   maxWidth: getSpacing(120),
-  flex: 1,
+  height: '100%',
   paddingBottom,
   ...(modalSpacing ? { paddingHorizontal: modalSpacing } : {}),
 }))
