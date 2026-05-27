4aadcc9b707b021b482ffe36ed1f7353259e0a81
Inès Mouandjo Lobé <158567429+imouandjolobe-pass@users.noreply.github.com>
2026-05-27 09:42:49 +0200

    Improve query cache handling by removing unnecessary params

--- src/features/search/queries/useSearchVenuesQuery/types.ts
+++ src/features/search/queries/useSearchVenuesQuery/types.ts
@@ -1,7 +1,14 @@
 import { VenuesUserData } from 'features/search/types'
-import { AlgoliaVenue } from 'libs/algolia/types'
+import { AlgoliaVenue, AlgoliaVenueOfferListItem } from 'libs/algolia/types'
 
 export type FetchSearchVenuesResponse = {
   venuesResponse: { hits: AlgoliaVenue[]; userData?: VenuesUserData | null } | undefined
   venueNotOpenToPublic: { hits: AlgoliaVenue[] } | undefined
+}
+
+export type SelectedSearchVenues = {
+  algoliaVenues: AlgoliaVenue[]
+  venues: AlgoliaVenueOfferListItem[]
+  venuesUserData: VenuesUserData | null | undefined
+  venueNotOpenToPublic: AlgoliaVenue[]
 }
