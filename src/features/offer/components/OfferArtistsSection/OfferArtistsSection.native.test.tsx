2bb2627dda78527e98a47c5414a2ff38994711a1
clesausse2-pass <277683118+clesausse2-pass@users.noreply.github.com>
2026-06-02 10:29:30 +0200

    (PC-42033) feat(offer): use InfoHeader when single artist rather than playlist

--- src/features/offer/components/OfferArtistsSection/OfferArtistsSection.native.test.tsx
+++ src/features/offer/components/OfferArtistsSection/OfferArtistsSection.native.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-import { CategoryIdEnum, SubcategoryIdEnum } from 'api/gen'
+import { navigate } from '__mocks__/@react-navigation/native'
+import { ArtistType, CategoryIdEnum, SubcategoryIdEnum } from 'api/gen'
 import { OfferArtistsSection } from 'features/offer/components/OfferArtistsSection/OfferArtistsSection'
-import { render, screen } from 'tests/utils'
+import { render, screen, userEvent } from 'tests/utils'
 
 const mockArtist = { id: '1', name: 'Edith Piaf' }
 const mockMultiArtists = [
@@ -11,58 +12,121 @@ const mockMultiArtists = [
   { id: '3', name: 'Sigourney Weaver' },
 ]
 
+const user = userEvent.setup()
+
+jest.useFakeTimers()
+
 describe('<OfferArtistsSection />', () => {
-  it('should display single artist', () => {
-    render(
-      <OfferArtistsSection
-        artists={[mockArtist]}
-        offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
-        offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
-        onPlaylistItemPress={jest.fn()}
-      />
-    )
+  describe('When single artist', () => {
+    it('should display correctly', () => {
+      render(
+        <OfferArtistsSection
+          artists={[mockArtist]}
+          offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
+          offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
+          onPlaylistItemPress={jest.fn()}
+        />
+      )
 
-    expect(screen.getByText('Edith Piaf')).toBeOnTheScreen()
+      expect(screen.getByText('Edith Piaf')).toBeOnTheScreen()
+    })
+
+    it('should display default avatar when image not defined', () => {
+      render(
+        <OfferArtistsSection
+          artists={[mockArtist]}
+          offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
+          offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
+          onPlaylistItemPress={jest.fn()}
+        />
+      )
+
+      expect(screen.getByTestId('defaultArtistAvatar')).toBeOnTheScreen()
+    })
+
+    it('should display artist image when image defined', () => {
+      render(
+        <OfferArtistsSection
+          artists={[{ ...mockArtist, image: 'url' }]}
+          offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
+          offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
+          onPlaylistItemPress={jest.fn()}
+        />
+      )
+
+      expect(screen.getByTestId('ArtistImage')).toBeOnTheScreen()
+    })
+
+    it('should display role as subtitle when role defined', () => {
+      render(
+        <OfferArtistsSection
+          artists={[{ ...mockArtist, role: ArtistType.author }]}
+          offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
+          offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
+          onPlaylistItemPress={jest.fn()}
+        />
+      )
+
+      expect(screen.getByText('Auteur')).toBeOnTheScreen()
+      expect(screen.getByTestId('subtitleWithTitle')).toBeOnTheScreen()
+    })
+
+    it('should redirect to artist page when pressing button', async () => {
+      render(
+        <OfferArtistsSection
+          artists={[mockArtist]}
+          offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
+          offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
+          onPlaylistItemPress={jest.fn()}
+        />
+      )
+
+      await user.press(screen.getByLabelText('Accéder à la page artiste de Edith Piaf'))
+
+      expect(navigate).toHaveBeenCalledWith('Artist', { id: '1' })
+    })
+
+    it('should display singular section title', () => {
+      render(
+        <OfferArtistsSection
+          artists={[mockArtist]}
+          offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
+          offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
+          onPlaylistItemPress={jest.fn()}
+        />
+      )
+
+      expect(screen.getAllByText('Artiste')[0]).toBeOnTheScreen()
+    })
   })
 
-  it('should display singular section title when single artist', () => {
-    render(
-      <OfferArtistsSection
-        artists={[mockArtist]}
-        offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
-        offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
-        onPlaylistItemPress={jest.fn()}
-      />
-    )
+  describe('When multi artists', () => {
+    it('should display correctly', () => {
+      render(
+        <OfferArtistsSection
+          artists={mockMultiArtists}
+          offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
+          offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
+          onPlaylistItemPress={jest.fn()}
+        />
+      )
 
-    expect(screen.getAllByText('Artiste')[0]).toBeOnTheScreen()
-  })
+      expect(screen.getByText('Sam Worthington')).toBeOnTheScreen()
+      expect(screen.getByText('Zoe Saldana')).toBeOnTheScreen()
+      expect(screen.getByText('Sigourney Weaver')).toBeOnTheScreen()
+    })
 
-  it('should display multi artists', () => {
-    render(
-      <OfferArtistsSection
-        artists={mockMultiArtists}
-        offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
-        offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
-        onPlaylistItemPress={jest.fn()}
-      />
-    )
+    it('should display plural section title', () => {
+      render(
+        <OfferArtistsSection
+          artists={mockMultiArtists}
+          offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
+          offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
+          onPlaylistItemPress={jest.fn()}
+        />
+      )
 
-    expect(screen.getByText('Sam Worthington')).toBeOnTheScreen()
-    expect(screen.getByText('Zoe Saldana')).toBeOnTheScreen()
-    expect(screen.getByText('Sigourney Weaver')).toBeOnTheScreen()
-  })
-
-  it('should display plural section title when multi artists', () => {
-    render(
-      <OfferArtistsSection
-        artists={mockMultiArtists}
-        offerCategoryId={CategoryIdEnum.MUSIQUE_ENREGISTREE}
-        offerSubcategoryId={SubcategoryIdEnum.SUPPORT_PHYSIQUE_MUSIQUE_VINYLE}
-        onPlaylistItemPress={jest.fn()}
-      />
-    )
-
-    expect(screen.getByText('Artistes')).toBeOnTheScreen()
+      expect(screen.getByText('Artistes')).toBeOnTheScreen()
+    })
   })
 })
