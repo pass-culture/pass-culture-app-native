7dc0169abfc603a950e9f07c4cc0606324e17da3
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2026-06-02 14:27:22 +0200

    (PC-41689) feat(qpi): add new icons

--- src/ui/svg/icons/exports/culturalSurveyIcons.ts
+++ src/ui/svg/icons/exports/culturalSurveyIcons.ts
@@ -1,3 +1,4 @@
+import { Earth } from 'ui/svg/icons/Earth'
 import { Accordion } from 'ui/svg/icons/venueAndCategories/Accordion'
 import { All } from 'ui/svg/icons/venueAndCategories/All'
 import { Book } from 'ui/svg/icons/venueAndCategories/Book'
@@ -17,6 +18,7 @@ import { Microphone } from 'ui/svg/icons/venueAndCategories/Microphone'
 import { Museum } from 'ui/svg/icons/venueAndCategories/Museum'
 import { Music } from 'ui/svg/icons/venueAndCategories/Music'
 import { Opera } from 'ui/svg/icons/venueAndCategories/Opera'
+import { Palette } from 'ui/svg/icons/venueAndCategories/Palette'
 import { PencilTip } from 'ui/svg/icons/venueAndCategories/PencilTip'
 import { PerformingArts } from 'ui/svg/icons/venueAndCategories/PerformingArts'
 import { Philosophy } from 'ui/svg/icons/venueAndCategories/Philosophy'
@@ -29,12 +31,13 @@ import { Romance } from 'ui/svg/icons/venueAndCategories/Romance'
 import { Show } from 'ui/svg/icons/venueAndCategories/Show'
 import { Streaming } from 'ui/svg/icons/venueAndCategories/Streaming'
 import { Theater } from 'ui/svg/icons/venueAndCategories/Theater'
+import { Turntable } from 'ui/svg/icons/venueAndCategories/Turntable'
 import { Video } from 'ui/svg/icons/venueAndCategories/Video'
 import { VideoGame } from 'ui/svg/icons/venueAndCategories/VideoGame'
 
 export const culturalSurveyIcons = {
-  All,
   Accordion,
+  All,
   Book,
   Bookstore,
   Brush,
@@ -42,6 +45,7 @@ export const culturalSurveyIcons = {
   Comics,
   DanceFeet,
   Detective,
+  Earth,
   Electro,
   Fantasy,
   Festival,
@@ -52,6 +56,7 @@ export const culturalSurveyIcons = {
   Museum,
   Music,
   Opera,
+  Palette,
   PencilTip,
   PerformingArts,
   Philosophy,
@@ -64,6 +69,7 @@ export const culturalSurveyIcons = {
   Show,
   Streaming,
   Theater,
-  VideoGame,
+  Turntable,
   Video,
+  VideoGame,
 }
