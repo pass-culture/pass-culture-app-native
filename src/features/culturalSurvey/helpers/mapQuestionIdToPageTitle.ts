5d7a317f1ba4e06dc8b640e9b9fe892842266e1f
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2026-06-03 15:18:40 +0200

    (PC-41689) feat(qpi): update title in QPI (#9737)

--- src/features/culturalSurvey/helpers/mapQuestionIdToPageTitle.ts
+++ src/features/culturalSurvey/helpers/mapQuestionIdToPageTitle.ts
@@ -5,12 +5,14 @@ export const mapQuestionIdToPageTitle = (id: CulturalSurveyQuestionEnum | undefi
     case CulturalSurveyQuestionEnum.SORTIES:
     case CulturalSurveyQuestionEnum.FESTIVALS:
     case CulturalSurveyQuestionEnum.SPECTACLES:
+    case CulturalSurveyQuestionEnum.PROJECTIONS:
       return 'Tes sorties'
     case CulturalSurveyQuestionEnum.ACTIVITES:
-    case CulturalSurveyQuestionEnum.LIVRES:
-    case CulturalSurveyQuestionEnum.MUSIQUES:
       return 'Tes activités'
-    case CulturalSurveyQuestionEnum.PROJECTIONS:
+    case CulturalSurveyQuestionEnum.LIVRES:
+      return 'Tes lectures'
+    case CulturalSurveyQuestionEnum.MUSIQUES:
+      return 'Tes musiques'
     default:
       return ''
   }
