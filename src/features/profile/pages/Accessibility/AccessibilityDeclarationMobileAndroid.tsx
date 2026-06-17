2b458127e3e227145547db936ed0797cbda7191c
lbeneston-pass <62059034+lbeneston-pass@users.noreply.github.com>
2026-06-17 12:54:15 +0200

    (PC-42426) feat(a11y): update audit follow up after audit

--- src/features/profile/pages/Accessibility/AccessibilityDeclarationMobileAndroid.tsx
+++ src/features/profile/pages/Accessibility/AccessibilityDeclarationMobileAndroid.tsx
@@ -6,18 +6,17 @@ import { env } from 'libs/environment/env'
 export function AccessibilityDeclarationMobileAndroid() {
   return (
     <AccessibilityDeclarationMobileBase
-      appVersion="1.386.1"
+      appVersion="1.395.0"
       platformName="Android"
       osVersion="15"
       storeLink={{ url: `https://play.google.com/store/apps/details?id=${env.ANDROID_APP_ID}` }}
-      auditDate="16/04/2026"
-      conformityEN="75,00%"
-      conformityRAAM="86,05%"
-      averageConformityRAAM="96,68%"
+      auditDate="16/06/2026"
+      conformityEN="79,59%"
+      conformityRAAM="90,91%"
+      averageConformityRAAM="97,67%"
       nonAccessibleContent={[
-        'Critère 3.5 - Chaque média temporel synchronisé pré-enregistré a-t-il, si nécessaire, une alternative\u00a0?',
+        'Critère 5.1.c - Chaque composant d’interface est-il, si nécessaire, compatible avec les technologies d’assistance\u00a0?',
         'Critère 5.4 - Dans chaque écran, les messages de statut sont-ils correctement restitués par les technologies d’assistance\u00a0?',
-        'Critère 9.11 - Pour chaque formulaire qui modifie ou supprime des données, ou qui transmet des réponses à un test ou à un examen, ou dont la validation a des conséquences financières ou juridiques, les données saisies peuvent-elles être modifiées, mises à jour ou récupérées par l’utilisateur\u00a0?',
         'Critère 10.2 - Dans chaque écran, l’ordre de restitution par les technologies d’assistance est-il cohérent\u00a0?',
         'Critère 11.10 - Dans chaque écran, les fonctionnalités activables au moyen d’un geste complexe sont-elles activables au moyen d’un geste simple\u00a0?',
       ]}
