b50e0ea7e65afb88987fa7110712c0f97c3dd37a
xlecunff-pass <163895831+xlecunff-pass@users.noreply.github.com>
2026-06-19 17:46:47 +0200

    (PC-42446) refactor(location): use locationModalStore inside location modal (#9825)

--- src/features/location/components/HomeLocationModal.tsx
+++ src/features/location/components/HomeLocationModal.tsx
@@ -1,34 +1,5 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 import { LocationModal } from 'features/location/components/LocationModal'
-import { useLocation } from 'libs/location/useLocation'
-import {
-  useCanSubmitLocationModal,
-  useLocationModal,
-  useLocationModalAddressInputValue,
-  useLocationModalPlace,
-} from 'libs/locationV2/locationModal.store'
 
-export const HomeLocationModal = () => {
-  const { hasGeolocPosition, setPlaceQuery, setSelectedPlace, onResetPlace } = useLocation()
-  const selectedPlace = useLocationModalPlace()
-  const placeQuery = useLocationModalAddressInputValue()
-  const { locationMode } = useLocationModal()
-  const canSubmit = useCanSubmitLocationModal()
-
-  return (
-    <LocationModal
-      from="home"
-      hasGeolocPosition={hasGeolocPosition}
-      tempLocationMode={locationMode}
-      selectedPlace={selectedPlace}
-      setSelectedPlace={setSelectedPlace}
-      placeQuery={placeQuery}
-      setPlaceQuery={setPlaceQuery}
-      onResetPlace={onResetPlace}
-      shouldShowRadiusSlider={false}
-      isSubmitDisabled={!canSubmit}
-      shouldDisplayEverywhereSection
-    />
-  )
-}
+export const HomeLocationModal: FC = () => <LocationModal from="home" />
