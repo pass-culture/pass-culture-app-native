3ab658b66a7c7d660ea2f6d59655a542ed5989ef
clesausse2-pass <277683118+clesausse2-pass@users.noreply.github.com>
2026-06-23 09:54:21 +0200

    (PC-42438) feat(verticalPlaylist): add artist button when pressing see all button from artist playlist module (#9834)

--- src/shared/verticalPlaylist/components/VerticalPlaylistOffersModule.tsx
+++ src/shared/verticalPlaylist/components/VerticalPlaylistOffersModule.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { ArtistPlaylistModule, OffersModule } from 'features/home/types'
+import { ArtistPlaylistModule, HomepageModuleType, OffersModule } from 'features/home/types'
 import { VerticalPlaylistOffersView } from 'shared/verticalPlaylist/components/VerticalPlaylistOffersView'
 import { useGetOffersFromPlaylist } from 'shared/verticalPlaylist/helpers/useGetOffersFromPlaylist'
 
@@ -17,6 +17,9 @@ export const VerticalPlaylistOffersModule = ({ module }: Props) => {
       searchId={data.searchId}
       searchQuery={data.searchQuery}
       analyticsFrom="verticalplaylistoffers"
+      artistId={
+        module.type === HomepageModuleType.ArtistPlaylistModule ? module.artistId : undefined
+      }
     />
   )
 }
