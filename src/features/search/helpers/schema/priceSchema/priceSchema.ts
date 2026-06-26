9519751b367a9e5d65d12dffc666dee792e8e0b5
tconte-pass <thomas.conte@passculture.app>
2026-06-26 21:59:58 +0200

    simplify priceSchema

--- src/features/search/helpers/schema/priceSchema/priceSchema.ts
+++ src/features/search/helpers/schema/priceSchema/priceSchema.ts
@@ -1,49 +1,34 @@
 import { boolean, object, string } from 'yup'
 
-import { Currency } from 'shared/currency/useGetCurrencyToDisplay'
-
-type PriceValidationParams = {
-  initialCredit: number
-  currency: Currency
-}
-
 const PRICE_REGEX = /^\d+(?:[,.]\d{0,2})?$/
 const formatPriceError = `Format du prix incorrect. Exemple de format attendu\u00a0: 10,00`
 const minPriceError = `Le montant minimum ne peut pas dépasser le montant maximum`
 
-const maxPriceError = ({ initialCredit, currency }: PriceValidationParams) => {
-  return `Le prix indiqué ne doit pas dépasser ${initialCredit}\u00a0${currency}`
-}
-
-const maxPriceValidationSchema = ({ initialCredit, currency }: PriceValidationParams) =>
+const priceValidationSchema = () =>
   string()
     .trim()
     .test('validPrice', formatPriceError, (value) => {
       if (!value) return true
       return PRICE_REGEX.test(value.trim())
     })
-    .test('validMaxPrice', maxPriceError({ initialCredit, currency }), (value) => {
-      if (!value) return true
-      return Number(value.trim().replaceAll(',', '.')) <= Number(initialCredit)
-    })
 
-const minPriceWithConditions = ({ initialCredit, currency }: PriceValidationParams) =>
-  maxPriceValidationSchema({ initialCredit, currency }).when(['maxPrice'], (maxPrice, schema) => {
-    if (!!maxPrice && maxPrice.trim().length > 0) {
-      return schema.test('validMinPrice', minPriceError, (minPrice) => {
+const minPriceWithConditions = () =>
+  priceValidationSchema().when(['validPrice'], (price, schema) => {
+    if (!!price && price.trim().length > 0) {
+      return schema.test('minPriceLowerThanMax', minPriceError, (minPrice) => {
         if (!minPrice) return true
         const min = Number(minPrice.trim().replace(',', '.'))
-        const max = Number(maxPrice.trim().replace(',', '.'))
+        const max = Number(price.trim().replace(',', '.'))
         return min <= max
       })
     }
     return schema
   })
 
-export const priceSchema = ({ initialCredit, currency }: PriceValidationParams) =>
+export const priceSchema = () =>
   object().shape({
-    minPrice: minPriceWithConditions({ initialCredit, currency }),
-    maxPrice: maxPriceValidationSchema({ initialCredit, currency }),
+    minPrice: minPriceWithConditions(),
+    maxPrice: priceValidationSchema(),
     isLimitCreditSearch: boolean(),
     isOnlyFreeOffersSearch: boolean(),
   })
