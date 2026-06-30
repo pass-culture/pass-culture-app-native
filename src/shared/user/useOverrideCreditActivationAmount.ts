26c92872ffcb157cdf3f61b9e317c215417a2012
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2026-06-30 09:37:30 +0200

    (PC-42579) fix(ActivationBanner): credit amount should be correct when free beneficiary is eligible

--- src/shared/user/useOverrideCreditActivationAmount.ts
+++ src/shared/user/useOverrideCreditActivationAmount.ts
@@ -1,9 +1,11 @@
 import { CurrencyEnum } from 'api/gen'
 import { useAuthContext } from 'features/auth/context/AuthContext'
+import { UserCreditType } from 'features/auth/helpers/getCreditType'
 import { getShouldDisplayActivationFlow } from 'features/auth/helpers/getShouldDisplayActivationFlow'
 import { useGetStepperInfoQuery } from 'features/identityCheck/queries/useGetStepperInfoQuery'
 import { useLocation } from 'libs/location/location'
 import { LocationMode } from 'libs/location/types'
+import { getAge } from 'shared/user/getAge'
 import { useGetDepositAmountsByAge } from 'shared/user/useGetDepositAmountsByAge'
 
 export function useOverrideCreditActivationAmount() {
@@ -12,7 +14,6 @@ export function useOverrideCreditActivationAmount() {
   const { data: subscription } = useGetStepperInfoQuery()
 
   const amount = useGetDepositAmountsByAge(user?.birthDate)
-
   const nextSubscriptionStepEnable = !!subscription?.nextSubscriptionStep
   const shouldDisplayActivationFlow = getShouldDisplayActivationFlow(user)
   const isActivationProcessEnable = nextSubscriptionStepEnable || shouldDisplayActivationFlow
@@ -21,7 +22,11 @@ export function useOverrideCreditActivationAmount() {
   const isUserRegisteredInPacificFrancRegion = user?.currency === CurrencyEnum.XPF
   const isUserInRelevantLocation = isUserLocated || isUserRegisteredInPacificFrancRegion
 
-  if (isActivationProcessEnable || isUserInRelevantLocation) {
+  const userAge = user?.birthDate ? getAge(user.birthDate) : undefined
+  const isFreeBeneficiaryWithRelevantAge =
+    user?.creditType === UserCreditType.CREDIT_V3_FREE && (userAge === 17 || userAge === 18)
+
+  if (isActivationProcessEnable || isUserInRelevantLocation || isFreeBeneficiaryWithRelevantAge) {
     return { shouldBeOverriden: true, amount }
   }
   return { shouldBeOverriden: false, amount }
