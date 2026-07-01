9cd8932e66865661701d0111272088fb1a6b2304
Tanguy <47600889+tprache-pass@users.noreply.github.com>
2026-07-01 15:59:42 +0200

    fix: handle artist query error

--- src/queries/artist/useArtistQuery.ts
+++ src/queries/artist/useArtistQuery.ts
@@ -1,11 +1,18 @@
-import { useSuspenseQuery } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 
 import { api } from 'api/api'
 import { ArtistResponse } from 'api/gen'
 import { QueryKeys } from 'libs/queryKeys'
 
 export const useArtistQuery = (artistId: string) =>
-  useSuspenseQuery<ArtistResponse, string>({
+  useQuery<ArtistResponse | null, string>({
     queryKey: [QueryKeys.ARTIST, artistId],
-    queryFn: async () => api.getNativeV1ArtistsartistId(artistId),
+    queryFn: async () => {
+      try {
+        return await api.getNativeV1ArtistsartistId(artistId)
+      } catch (error) {
+        return null
+      }
+    },
+    staleTime: 60 * 60 * 1000,
   })
