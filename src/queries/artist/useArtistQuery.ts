a347f23611b826a93fa9d8cba4826079ac51eca0
clesausse2-pass <277683118+clesausse2-pass@users.noreply.github.com>
2026-07-08 14:22:31 +0200

    (PC-42765) fix(verticalPlaylist): display correctly artist used in ArtistPlaylistModule when pressing see all button (#9900)

--- src/queries/artist/useArtistQuery.ts
+++ src/queries/artist/useArtistQuery.ts
@@ -10,9 +10,10 @@ export const useArtistSuspenseQuery = (artistId: string) =>
     queryFn: async () => api.getNativeV1ArtistsartistId(artistId),
   })
 
-export const useArtistQuery = (artistId: string, { throwOnError = true } = {}) =>
+export const useArtistQuery = (artistId: string, { throwOnError = true, enabled = true } = {}) =>
   useQuery<ArtistResponse, string>({
     queryKey: [QueryKeys.ARTIST, artistId],
     queryFn: async () => api.getNativeV1ArtistsartistId(artistId),
     throwOnError,
+    enabled,
   })
--- src/shared/verticalPlaylist/components/VerticalPlaylistOffersModule.tsx
+++ src/shared/verticalPlaylist/components/VerticalPlaylistOffersModule.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { ArtistPlaylistModule, HomepageModuleType, OffersModule } from 'features/home/types'
+import { useArtistQuery } from 'queries/artist/useArtistQuery'
 import { VerticalPlaylistOffersView } from 'shared/verticalPlaylist/components/VerticalPlaylistOffersView'
 import { useGetOffersFromPlaylist } from 'shared/verticalPlaylist/helpers/useGetOffersFromPlaylist'
 
@@ -8,6 +9,13 @@ type Props = { module: OffersModule | ArtistPlaylistModule }
 
 export const VerticalPlaylistOffersModule = ({ module }: Props) => {
   const data = useGetOffersFromPlaylist(module)
+  const { data: artist } = useArtistQuery(
+    module.type === HomepageModuleType.ArtistPlaylistModule ? module.artistId : '',
+    {
+      throwOnError: false,
+      enabled: module.type === HomepageModuleType.ArtistPlaylistModule,
+    }
+  )
 
   return (
     <VerticalPlaylistOffersView
@@ -17,9 +25,7 @@ export const VerticalPlaylistOffersModule = ({ module }: Props) => {
       searchId={data.searchId}
       searchQuery={data.searchQuery}
       analyticsFrom="verticalplaylistoffers"
-      artistId={
-        module.type === HomepageModuleType.ArtistPlaylistModule ? module.artistId : undefined
-      }
+      artist={artist}
       moduleId={module.type === HomepageModuleType.ArtistPlaylistModule ? module.id : undefined}
     />
   )
