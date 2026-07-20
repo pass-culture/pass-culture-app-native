14bd6f8c39e92fe736deac555d53295bf99077e5
Xavier <163895831+xlecunff-pass@users.noreply.github.com>
2026-07-20 10:27:43 +0200

    (BSR) chore(build): use only swift files

--- ios/PassCulture/AppDelegate.swift
+++ ios/PassCulture/AppDelegate.swift
@@ -2,107 +2,130 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
-import HotUpdater
-import RNBatchPush
 import Firebase
+import HotUpdater // @hot-updater/react-native
 import Batch
-import Lottie
-
+import BatchFirebaseDispatcher
+import RNBatchPush // @batch.com/react-native-plugin
+import react_native_config // react-native-config
+import react_native_lottie_splash_screen // react-native-lottie-splash-screen
+import react_native_orientation_locker // react-native-orientation-locker
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
- 
+
   var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
 
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
- 
+
     reactNativeDelegate = delegate
     reactNativeFactory = factory
 
     window = UIWindow(frame: UIScreen.main.bounds)
-    
-    // Get environment
+
+    // Get environment (Info.plist → Env, set per build configuration / scheme)
     guard let plistPath = Bundle.main.path(forResource: "Info", ofType: "plist"),
           let plistConfig = NSDictionary(contentsOfFile: plistPath),
           let env = plistConfig["Env"] as? String else {
       print("Could not load environment configuration")
       return false
     }
-    
+
     // Enable Firebase debug view on testing environment
     if env == "testing" {
-        UserDefaults.standard.set(true, forKey: "FIRAnalyticsDebugEnabled")
-        UserDefaults.standard.set(true, forKey: "FIRDebugEnabled")
+      UserDefaults.standard.set(true, forKey: "FIRAnalyticsDebugEnabled")
+      UserDefaults.standard.set(true, forKey: "FIRDebugEnabled")
     }
-    
+
     if FirebaseApp.app() == nil {
       FirebaseApp.configure()
     }
-    
-    // Setup Batch
-    let associatedDomain = RNCConfig.env(for: "WEBAPP_V2_DOMAIN")
-    BatchSDK.associatedDomains = associatedDomain.map { [$0] } ?? []
-    BatchEventDispatcher.add(BatchFirebaseDispatcher.instance())
-    RNBatch.start()
-    BatchUNUserNotificationCenterDelegate.registerAsDelegate()
-    BatchUNUserNotificationCenterDelegate.sharedInstance.showForegroundNotifications = true
 
+    // Setup Batch (push, universal links domain, Firebase dispatcher)
+    configureBatch()
+
+    // Start React Native
     factory.startReactNative(
       withModuleName: "PassCulture",
       in: window,
       launchOptions: launchOptions
     )
-    
-    if window {
-      // We add this logic to get access to rootview
+
+    if let window {
+      // We add this logic to get access to rootView
       guard let rootView = window.rootViewController?.view else {
         return true
       }
-      
+
       let t = Dynamic()
       let animationView = t.createAnimationView(rootView: rootView, lottieName: "splashscreen")
-      
-      // register LottieSplashScreen to RNSplashScreen
+
+      // Register LottieSplashScreen to RNSplashScreen
       RNSplashScreen.showLottieSplash(animationView, inRootView: rootView)
-      // play lottie animation
+      // Play Lottie animation
       t.play(animationView: animationView)
-      // We want the animation layout to be forced to remove when hide is called, so we use this
+      // Force the animation layout to be removed when hide is called
       RNSplashScreen.setAnimationFinished(true)
     }
-    
+
     return true
   }
-  
+
   // Deeplink configuration
-  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    return RCTLinkingManager.application(app, open: url, options: options)
-  }
-  
-  // Handle Universal Links
-  override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-    return RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
+  func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    RCTLinkingManager.application(app, open: url, options: options)
   }
 
-  override func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-    return Orientation.getOrientation()
+  // Handle Universal Links
+  func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+  ) -> Bool {
+    RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
-}  
+
+  func application(
+    _ application: UIApplication,
+    supportedInterfaceOrientationsFor window: UIWindow?
+  ) -> UIInterfaceOrientationMask {
+    // react-native-orientation-locker
+    Orientation.getOrientation()
+  }
+
+  private func configureBatch() {
+    if let associatedDomain = RNCConfig.env(for: "WEBAPP_V2_DOMAIN") {
+      BatchSDK.setAssociatedDomains([associatedDomain])
+    }
+    BatchEventDispatcher.addDispatcher(BatchFirebaseDispatcher.instance())
+    RNBatch.start()
+    BatchUNUserNotificationCenterDelegate.registerAsDelegate()
+    BatchUNUserNotificationCenterDelegate.sharedInstance().showForegroundNotifications = true
+  }
+}
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
-    return self.bundleURL()
+    bundleURL()
   }
-  
+
   override func bundleURL() -> URL? {
 #if DEBUG
-    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
-    return HotUpdater.bundleURL()
+    HotUpdater.bundleURL() // @hot-updater/react-native
 #endif
-  } 
-} 
+  }
+}
