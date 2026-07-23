d4602460520c26642db7a2bfe86c273deba502f8
clesausse2-pass <277683118+clesausse2-pass@users.noreply.github.com>
2026-07-23 17:54:39 +0200

    (PC-42681) feat(ui): add possibility to use blockedPaintingLarge, brokenDinosaurSkeletonLarge, hourglass and trashMosaic illustrations

--- src/shared/illustrations/genericInfoPageIllustrations.ts
+++ src/shared/illustrations/genericInfoPageIllustrations.ts
@@ -1,10 +1,21 @@
 import { buildCategoryIllustrationUrl } from 'shared/illustrations/buildCategoryIllustrationUrl'
 
-const genericInfoPageIllustrationNames = ['emptyDigitalWindowLarge', 'emptyWalletLarge'] as const
+const genericInfoPageIllustrationNames = [
+  'blockedPaintingLarge',
+  'brokenDinosaurSkeletonLarge',
+  'emptyDigitalWindowLarge',
+  'emptyWalletLarge',
+  'hourglass',
+  'trashMosaic',
+] as const
 
 type GenericInfoPageIllustrationName = (typeof genericInfoPageIllustrationNames)[number]
 
 export const genericInfoPageIllustrationUrls = {
+  blockedPaintingLarge: buildCategoryIllustrationUrl('blockedPaintingLarge.png'),
+  brokenDinosaurSkeletonLarge: buildCategoryIllustrationUrl('brokenDinosaurSkeletonLarge.png'),
   emptyDigitalWindowLarge: buildCategoryIllustrationUrl('emptyDigitalWindowLarge.png'),
   emptyWalletLarge: buildCategoryIllustrationUrl('emptyWalletLarge.png'),
+  hourglass: buildCategoryIllustrationUrl('emptyWalletLarge.png'),
+  trashMosaic: buildCategoryIllustrationUrl('trashMosaic.png'),
 } as const satisfies Record<GenericInfoPageIllustrationName, string>
