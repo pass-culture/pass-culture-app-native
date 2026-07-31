c3194619a611b5aaa5ea8a56f8c696309669eee3
mlassauzay-pass <240954952+mlassauzay-pass@users.noreply.github.com>
2026-07-31 12:35:20 +0200

    (PC-42928) feat(qpi): remove useGetCulturalSurveyContent.ts

--- src/features/culturalSurvey/pages/CulturalSurveyIntro.tsx
+++ src/features/culturalSurvey/pages/CulturalSurveyIntro.tsx
@@ -1,60 +1,48 @@
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { Platform, View } from 'react-native'
 import styled from 'styled-components/native'
 
-import { FAQ_LINK_USER_DATA } from 'features/culturalSurvey/constants'
-import { useGetCulturalSurveyContent } from 'features/culturalSurvey/helpers/useGetCulturalSurveyContent'
-import { getSubscriptionPropConfig } from 'features/navigation/navigators/SubscriptionStackNavigator/getSubscriptionPropConfig'
-import { ExternalTouchableLink } from 'ui/components/touchableLink/ExternalTouchableLink'
-import { InternalTouchableLink } from 'ui/components/touchableLink/InternalTouchableLink'
-import { Button } from 'ui/designSystem/Button/Button'
+import { useCulturalSurveyContext } from 'features/culturalSurvey/context/CulturalSurveyContextProvider'
+import { UseNavigationType } from 'features/navigation/navigators/RootNavigator/types'
+import { getSubscriptionHookConfig } from 'features/navigation/navigators/SubscriptionStackNavigator/getSubscriptionHookConfig'
+import { homeNavigationConfig } from 'features/navigation/TabBar/helpers'
+import { useGoBack } from 'features/navigation/useGoBack'
+import { analytics } from 'libs/analytics/provider'
 import { GenericInfoPage } from 'ui/pages/GenericInfoPage'
-import { InfoPlain } from 'ui/svg/icons/InfoPlain'
 import { PhonePending } from 'ui/svg/icons/PhonePending'
+import { PlainArrowPrevious } from 'ui/svg/icons/PlainArrowPrevious'
 import { Typo } from 'ui/theme'
 
-const FAQTouchableLinkProps = {
-  as: Button,
-  wording: 'En savoir plus',
-  icon: InfoPlain,
-  accessibilityLabel: 'En savoir plus sur ce qu’on fait de tes données',
-}
-
 export const CulturalSurveyIntro = (): React.JSX.Element => {
-  const { intro } = useGetCulturalSurveyContent()
+  const { navigate } = useNavigation<UseNavigationType>()
+  const { goBack } = useGoBack(...homeNavigationConfig)
+
+  const navigateToCulturalSurvey = () => {
+    void analytics.logHasStartedCulturalSurvey()
+    const firstQuestion = initialQuestions[0]
+    if (!firstQuestion) return
+    navigate(...getSubscriptionHookConfig('CulturalSurveyQuestions', { question: firstQuestion }))
+  }
 
   return (
     <GenericInfoPage
       illustration={PhonePending}
-      title={intro.title}
-      subtitle={intro.customSubtitle}
+      subtitle={`Quelques questions et ton\u00a0crédit\u00a0est\u00a0à\u00a0toi.`}
+      title={'Tu y es presque\u00a0!'}
       buttonPrimary={{
-        wording: intro.button.wording,
-        onPress: intro.button.onPress,
+        wording: 'Commencer le questionnaire',
+        onPress: navigateToCulturalSurvey,
       }}
       buttonTertiary={{
-        wording: intro.secondaryButton.wording,
-        icon: intro.secondaryButton.icon,
-        onPress: intro.secondaryButton.onPress,
+        wording: 'Retour',
+        icon: PlainArrowPrevious,
+        onPress: goBack,
       }}>
-      <StyledBody>{intro.bodyText}</StyledBody>
-      {intro.showFAQLink ? (
-        <View>
-          {Platform.OS === 'web' ? (
-            <ExternalTouchableLink
-              key={1}
-              externalNav={{ url: FAQ_LINK_USER_DATA }}
-              {...FAQTouchableLinkProps}
-            />
-          ) : (
-            <InternalTouchableLink
-              key={1}
-              navigateTo={getSubscriptionPropConfig('FAQWebview')}
-              {...FAQTouchableLinkProps}
-            />
-          )}
-        </View>
-      ) : null}
+      <StyledBody>
+        {
+          'Parle nous de tes activités culturelles préférées. Tes réponses vont nous aider à mieux te connaître.'
+        }
+      </StyledBody>
     </GenericInfoPage>
   )
 }
--- src/features/culturalSurvey/pages/CulturalSurveyThanks.tsx
+++ src/features/culturalSurvey/pages/CulturalSurveyThanks.tsx
@@ -1,11 +1,24 @@
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 
-import { useGetCulturalSurveyContent } from 'features/culturalSurvey/helpers/useGetCulturalSurveyContent'
+import { UseNavigationType } from 'features/navigation/navigators/RootNavigator/types'
 import QpiThanks from 'ui/animations/qpi_thanks.json'
 import { GenericInfoPage } from 'ui/pages/GenericInfoPage'
 
 export const CulturalSurveyThanks: React.FC = () => {
-  const { thanks } = useGetCulturalSurveyContent()
+  const { reset } = useNavigation<UseNavigationType>()
+
+  const navigateToIdentityCheckHonor = () => {
+    reset({
+      index: 0,
+      routes: [
+        {
+          name: 'SubscriptionStackNavigator',
+          state: { index: 0, routes: [{ name: 'IdentityCheckHonor' }] },
+        },
+      ],
+    })
+  }
 
   return (
     <GenericInfoPage
@@ -14,10 +27,10 @@ export const CulturalSurveyThanks: React.FC = () => {
       animationTargetShapeNames={['Fond 1', 'Gradient Fill 1']}
       animationTargetLayerNames={['étoile', 'cadre']}
       title="Un grand merci pour tes réponses&nbsp;!"
-      subtitle={thanks.subtitle}
+      subtitle="Elles nous permettent de suivre l’évolution de tes pratiques culturelles sur l’application."
       buttonPrimary={{
-        wording: thanks.button.wording,
-        onPress: thanks.button.onPress,
+        wording: 'Continuer',
+        onPress: navigateToIdentityCheckHonor,
       }}
     />
   )
