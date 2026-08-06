403ee3faf901ccac263376f3e45fdc579344bb56
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2026-08-06 17:50:09 +0200

    wip

--- src/features/search/components/CategoriesListDumb/CategoriesListDumb.tsx
+++ src/features/search/components/CategoriesListDumb/CategoriesListDumb.tsx
@@ -3,6 +3,7 @@ import styled, { useTheme } from 'styled-components/native'
 
 import { ListCategoryButtonProps } from 'features/search/helpers/useSortedSearchCategories/useSortedSearchCategories'
 import { VenueMapBlock } from 'features/venueMap/components/VenueMapBlock/VenueMapBlock'
+import { AccessibilityRole } from 'libs/accessibilityRole/accessibilityRole'
 import { useMobileFontScaleToDisplay } from 'shared/accessibility/helpers/zoomHelpers'
 import { CategoryButton } from 'shared/categoryButton/CategoryButton'
 import { NewCategoryButton } from 'shared/categoryButton/NewCategoryButton'
@@ -59,29 +60,28 @@ export const CategoriesListDumb: FunctionComponent<Props> = ({
       ) : null}
       <CategoriesTitleV2 />
       <CategoriesButtonsContainer>
-        {sortedCategories.map((item) => {
-          return enableNewCategoryBlocks ? (
-            <StyledNewCategoryButton
-              key={item.label}
-              {...item}
-              fillColor={designSystem.color.illustration[item.fillColor]}
-              borderColor={designSystem.color.border[item.borderColor]}
-              mobileMinWidth={mobileMinWidth}
-              mobileMaxWidth={mobileMaxWidth}
-              height={NEW_CATEGORY_BUTTON_HEIGHT}
-            />
-          ) : (
-            <StyledCategoryButton
-              key={item.label}
-              {...item}
-              fillColor={designSystem.color.background[item.fillColor]}
-              borderColor={designSystem.color.border[item.borderColor]}
-              mobileMinWidth={mobileMinWidth}
-              mobileMaxWidth={mobileMaxWidth}
-              height={CATEGORY_BUTTON_HEIGHT}
-            />
-          )
-        })}
+        {sortedCategories.map((item) => (
+          <CategoryListItem
+            key={item.label}
+            mobileMinWidth={mobileMinWidth}
+            mobileMaxWidth={mobileMaxWidth}>
+            {enableNewCategoryBlocks ? (
+              <NewCategoryButton
+                {...item}
+                fillColor={designSystem.color.illustration[item.fillColor]}
+                borderColor={designSystem.color.border[item.borderColor]}
+                height={NEW_CATEGORY_BUTTON_HEIGHT}
+              />
+            ) : (
+              <CategoryButton
+                {...item}
+                fillColor={designSystem.color.background[item.fillColor]}
+                borderColor={designSystem.color.border[item.borderColor]}
+                height={CATEGORY_BUTTON_HEIGHT}
+              />
+            )}
+          </CategoryListItem>
+        ))}
       </CategoriesButtonsContainer>
       <Spacer.BottomScreen />
     </StyledScrollView>
@@ -93,7 +93,7 @@ const StyledScrollView = styled.ScrollView(({ theme }) => ({
   marginTop: theme.isMobileViewport ? 0 : theme.designSystem.size.spacing.s,
 }))
 
-const StyledCategoryButton = styled(CategoryButton)<{
+const CategoryListItem = styled.View.attrs({ accessibilityRole: AccessibilityRole.LISTITEM })<{
   mobileMinWidth: string
   mobileMaxWidth: string
 }>(({ theme, mobileMinWidth, mobileMaxWidth }) => ({
@@ -104,18 +104,9 @@ const StyledCategoryButton = styled(CategoryButton)<{
   maxWidth: theme.isMobileViewport ? mobileMaxWidth : DESKTOP_MAX_WIDTH,
 }))
 
-const StyledNewCategoryButton = styled(NewCategoryButton)<{
-  mobileMinWidth: string
-  mobileMaxWidth: string
-}>(({ theme, mobileMinWidth, mobileMaxWidth }) => ({
-  flexGrow: 1,
-  flexShrink: 0,
-  flexBasis: 0,
-  minWidth: theme.isMobileViewport ? mobileMinWidth : DESKTOP_MIN_WIDTH,
-  maxWidth: theme.isMobileViewport ? mobileMaxWidth : DESKTOP_MAX_WIDTH,
-}))
-
-const CategoriesButtonsContainer = styled.View(({ theme }) => ({
+const CategoriesButtonsContainer = styled.View.attrs({
+  accessibilityRole: AccessibilityRole.LIST,
+})(({ theme }) => ({
   width: '100%',
   ...(theme.isMobileViewport
     ? {
