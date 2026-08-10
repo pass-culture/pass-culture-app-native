68bc06b69ddbd1ebb0173aabbca1bdd8935d7337
Nolwen <144016890+nmajorfrances-pass@users.noreply.github.com>
2026-08-10 14:02:22 +0200

    (PC-43002) feat(a11y): P09 → categries buttons should be a list

--- src/features/search/components/CategoriesListDumb/CategoriesListDumb.tsx
+++ src/features/search/components/CategoriesListDumb/CategoriesListDumb.tsx
@@ -6,6 +6,8 @@ import { VenueMapBlock } from 'features/venueMap/components/VenueMapBlock/VenueM
 import { useMobileFontScaleToDisplay } from 'shared/accessibility/helpers/zoomHelpers'
 import { CategoryButton } from 'shared/categoryButton/CategoryButton'
 import { NewCategoryButton } from 'shared/categoryButton/NewCategoryButton'
+import { Li } from 'ui/components/Li'
+import { Ul } from 'ui/components/Ul'
 import { getSpacing, Spacer, Typo } from 'ui/theme'
 // eslint-disable-next-line no-restricted-imports
 import { getTextSemanticAttrs } from 'ui/theme/typographyAttrs/getTextSemanticAttrs'
@@ -61,25 +63,29 @@ export const CategoriesListDumb: FunctionComponent<Props> = ({
       <CategoriesButtonsContainer>
         {sortedCategories.map((item) => {
           return enableNewCategoryBlocks ? (
-            <StyledNewCategoryButton
+            <StyledLi
               key={item.label}
-              {...item}
-              fillColor={designSystem.color.illustration[item.fillColor]}
-              borderColor={designSystem.color.border[item.borderColor]}
               mobileMinWidth={mobileMinWidth}
-              mobileMaxWidth={mobileMaxWidth}
-              height={NEW_CATEGORY_BUTTON_HEIGHT}
-            />
+              mobileMaxWidth={mobileMaxWidth}>
+              <StyledNewCategoryButton
+                {...item}
+                fillColor={designSystem.color.illustration[item.fillColor]}
+                borderColor={designSystem.color.border[item.borderColor]}
+                height={NEW_CATEGORY_BUTTON_HEIGHT}
+              />
+            </StyledLi>
           ) : (
-            <StyledCategoryButton
+            <StyledLi
               key={item.label}
-              {...item}
-              fillColor={designSystem.color.background[item.fillColor]}
-              borderColor={designSystem.color.border[item.borderColor]}
               mobileMinWidth={mobileMinWidth}
-              mobileMaxWidth={mobileMaxWidth}
-              height={CATEGORY_BUTTON_HEIGHT}
-            />
+              mobileMaxWidth={mobileMaxWidth}>
+              <StyledCategoryButton
+                {...item}
+                fillColor={designSystem.color.background[item.fillColor]}
+                borderColor={designSystem.color.border[item.borderColor]}
+                height={CATEGORY_BUTTON_HEIGHT}
+              />
+            </StyledLi>
           )
         })}
       </CategoriesButtonsContainer>
@@ -93,29 +99,25 @@ const StyledScrollView = styled.ScrollView(({ theme }) => ({
   marginTop: theme.isMobileViewport ? 0 : theme.designSystem.size.spacing.s,
 }))
 
-const StyledCategoryButton = styled(CategoryButton)<{
-  mobileMinWidth: string
-  mobileMaxWidth: string
-}>(({ theme, mobileMinWidth, mobileMaxWidth }) => ({
-  flexGrow: 1,
-  flexShrink: 0,
-  flexBasis: 0,
-  minWidth: theme.isMobileViewport ? mobileMinWidth : DESKTOP_MIN_WIDTH,
-  maxWidth: theme.isMobileViewport ? mobileMaxWidth : DESKTOP_MAX_WIDTH,
-}))
+const StyledCategoryButton = styled(CategoryButton)({
+  width: '100%',
+})
 
-const StyledNewCategoryButton = styled(NewCategoryButton)<{
-  mobileMinWidth: string
-  mobileMaxWidth: string
-}>(({ theme, mobileMinWidth, mobileMaxWidth }) => ({
-  flexGrow: 1,
-  flexShrink: 0,
-  flexBasis: 0,
-  minWidth: theme.isMobileViewport ? mobileMinWidth : DESKTOP_MIN_WIDTH,
-  maxWidth: theme.isMobileViewport ? mobileMaxWidth : DESKTOP_MAX_WIDTH,
-}))
+const StyledNewCategoryButton = styled(NewCategoryButton)({
+  width: '100%',
+})
 
-const CategoriesButtonsContainer = styled.View(({ theme }) => ({
+const StyledLi = styled(Li)<{ mobileMinWidth: string; mobileMaxWidth: string }>(
+  ({ theme, mobileMinWidth, mobileMaxWidth }) => ({
+    flexGrow: 1,
+    flexShrink: 0,
+    flexBasis: 0,
+    minWidth: theme.isMobileViewport ? mobileMinWidth : DESKTOP_MIN_WIDTH,
+    maxWidth: theme.isMobileViewport ? mobileMaxWidth : DESKTOP_MAX_WIDTH,
+  })
+)
+
+const CategoriesButtonsContainer = styled(Ul)(({ theme }) => ({
   width: '100%',
   ...(theme.isMobileViewport
     ? {
