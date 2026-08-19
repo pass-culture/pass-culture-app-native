4d44f9e49c67296386e2a64b42f6b2431f02f11d
clesausse2-pass <277683118+clesausse2-pass@users.noreply.github.com>
2026-08-19 16:49:54 +0200

    (PC-43304) fix(app): reserve tab bar space for page not in tab bar navigator (#10064)

--- src/features/navigation/navigators/RootNavigator/RootStackNavigator.tsx
+++ src/features/navigation/navigators/RootNavigator/RootStackNavigator.tsx
@@ -580,7 +580,7 @@ const RootStackNavigator = withWebWrapper(
 export const RootNavigator: React.FC<{ currentRoute?: Route<string> }> = ({ currentRoute }) => {
   const mainId = uuidv4()
   const tabBarId = uuidv4()
-  const { showTabBar } = useTheme()
+  const { showTabBar, isMobileViewport } = useTheme()
   const { isLoggedIn } = useAuthContext()
   const { isSplashScreenHidden } = useSplashScreenContext()
 
@@ -613,10 +613,19 @@ export const RootNavigator: React.FC<{ currentRoute?: Route<string> }> = ({ curr
     currentRoute ?? null
   )
 
+  const shouldReserveTabBarSpace = !!(
+    Platform.OS === 'web' &&
+    isMobileViewport &&
+    currentRoute?.name !== 'TabNavigator'
+  )
+
   return (
     <TabNavigationStateProvider>
       {showTabBar ? headerWithQuickAccess : <Header mainId={mainId} />}
-      <Main nativeID={mainId} accessibilityRole={mainAccessibilityRole}>
+      <Main
+        nativeID={mainId}
+        accessibilityRole={mainAccessibilityRole}
+        shouldReserveTabBarSpace={shouldReserveTabBarSpace}>
         <RootStackNavigator initialRouteName={initialScreen} />
       </Main>
       {showTabBar ? <AccessibleTabBar id={tabBarId} /> : null}
@@ -628,8 +637,11 @@ export const RootNavigator: React.FC<{ currentRoute?: Route<string> }> = ({ curr
   )
 }
 
-const Main = styled.View({
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: 1,
-})
+const Main = styled.View<{ shouldReserveTabBarSpace: boolean }>(
+  ({ theme, shouldReserveTabBarSpace }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    paddingBottom: shouldReserveTabBarSpace ? theme.tabBar.height : undefined,
+  })
+)
