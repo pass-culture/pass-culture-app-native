34aab26e8a34b16a198d64970196d04b0006e76d
pcanthelou-pass <119601402+pcanthelou-pass@users.noreply.github.com>
2026-08-21 18:32:18 +0200

    PC-43350 Semgrep Finding: python.lang.security.audit.subprocess-shell-true.subprocess-shell-true mauvaise rpatique remontée par semgrep, on remplace les shell=True par shell=False. Ajout des tests pour les éviter à l'avenir et un autre pour tester red/green la correction

--- scripts/set_resolution.py
+++ scripts/set_resolution.py
@@ -98,28 +98,28 @@ def set_resolutions(dependencies_data: str, manifest: str, package_json: str):
 
 
 def run_yarn():
-    subprocess.run(["yarn"], shell=True)
+    subprocess.run(["yarn"])
 
 
 def pull_master():
-    subprocess.run(["git switch master"], shell=True)
-    subprocess.run(["git pull"], shell=True)
+    subprocess.run(["git switch master"])
+    subprocess.run(["git pull"])
 
 
 def create_git_branch(branch: str):
-    subprocess.run([f"git switch -c {branch}"], shell=True)
+    subprocess.run([f"git switch -c {branch}"])
 
 
 def stage_modifications():
-    subprocess.run(["git add package.json yarn.lock"], shell=True)
+    subprocess.run(["git add package.json yarn.lock"])
 
 
 def commit_modifications(branch: str):
-    subprocess.run([f'git commit -m "({branch}) build(yarn): update dep"'], shell=True)
+    subprocess.run([f'git commit -m "({branch}) build(yarn): update dep"'])
 
 
 def push_modifications(branch: str):
-    subprocess.run([f"git push origin {branch}"], shell=True)
+    subprocess.run([f"git push origin {branch}"])
 
 
 def get_dependabot_alerts(manifest: str):
