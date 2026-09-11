349117fe0f5a815c86abe1b10afa3206c53c0f88
aminepass <155538488+aminepass@users.noreply.github.com>
2026-09-11 10:38:38 +0200

    Fix tests

--- src/ui/components/TouchableOpacity.tsx
+++ src/ui/components/TouchableOpacity.tsx
@@ -56,6 +56,7 @@ export function TouchableOpacity({
       isFocus={isFocus}
       onFocus={onStyledFocus}
       onBlur={onStyledBlur}
+      disabled={disabled}
       {...(isWeb && !disabled ? { tabIndex: 0 } : {})}
       {...props}
       {...accessibilityAndTestId(accessibilityLabel, testID)}>
